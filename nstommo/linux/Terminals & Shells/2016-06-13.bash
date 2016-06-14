43070f760fcb5760ad5126703895f16a7f74e404
nstommo <bluewhitegalaxy@gmail.com>
2016-06-14 15:07:36 +0100

    2016-06-13's linux workout

---
+++ nstommo/linux/Terminals & Shells/2016-06-13.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in the history
+Find any Unix / Linux command
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
