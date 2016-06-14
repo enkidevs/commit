2757998c66192c87292434964f9a8b77186f3edd
lokyi <roy.yi.yue@gmail.com>
2016-06-14 19:34:04 +0100

    2016-06-14's java workout

---
+++ lokyi/java/Error Handling/2016-06-14.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Using exceptions in Java
+Handling Null Pointer Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
