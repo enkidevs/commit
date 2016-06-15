d1658c0debaf2f34f9a25361de3af71e2de1c068
punkaid <punkaid@gmail.com>
2016-06-15 21:09:11 +0100

    2016-06-15's linux workout

---
+++ punkaid/linux/System Management/2016-06-15.bash
@@ -0,0 +1,7 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
