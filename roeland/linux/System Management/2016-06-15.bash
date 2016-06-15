0caf017f214999f92187206ae12a8e9ececca731
roeland <roelandvanbatenburg@gmail.com>
2016-06-15 17:34:17 +0100

    2016-06-15's linux workout

---
+++ roeland/linux/System Management/2016-06-15.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
