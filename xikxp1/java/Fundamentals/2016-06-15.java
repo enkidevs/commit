d2e990e132a3716cd644c5cd041c56570cef07af
xikxp1 <xikxp1@gmail.com>
2016-06-15 04:34:08 +0100

    2016-06-15's java workout

---
+++ xikxp1/java/Fundamentals/2016-06-15.java
@@ -0,0 +1,5 @@
+Cropping images
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
