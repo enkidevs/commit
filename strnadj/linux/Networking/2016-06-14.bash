5cc2a68bcb169b096ae84cc757b353b113e1d26c
strnadj <jan.strnadek@gmail.com>
2016-06-15 06:41:09 +0100

    2016-06-14's linux workout

---
+++ strnadj/linux/Networking/2016-06-14.bash
@@ -0,0 +1,5 @@
+Logging dropped firewall packages
+Get real network statistics with `slurm`
+List your IPv6 configuration
+Getting the connection speed from the terminal
+Preventing ssh from timing out
