7762a5fc37cdc3b29fac4205fd823ae1db8fa5e5
encore <kingkonig@gmail.com>
2016-06-15 08:28:15 +0100

    2016-06-15's linux workout

---
+++ encore/linux/Terminals & Shells/2016-06-15.bash
@@ -0,0 +1,7 @@
+Find any Unix / Linux command
+Useful aliasing in bash
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
