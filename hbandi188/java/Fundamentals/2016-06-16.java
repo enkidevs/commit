4519fcbbb5aac8c8fec2333e0f2fd39551e92284
hbandi188 <hbandi188@gmail.com>
2016-06-16 10:13:15 +0100

    2016-06-16's java workout

---
+++ hbandi188/java/Fundamentals/2016-06-16.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Overloading Methods in Java
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
