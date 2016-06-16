75c8c1b90ab064fe893ce3ff204fc2ba022589cf
bmo77 <basha.others@gmail.com>
2016-06-16 13:27:30 +0100

    2016-06-16's java workout

---
+++ bmo77/java/Error Handling/2016-06-16.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Creating new Exceptions
+Throwing an `exception`
+Careful Numeric Data Conversions
