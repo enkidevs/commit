810ad5332dc4ce0ba16a8953f36984408a3c9a3e
HarisOne <haris.kumalic@icloud.com>
2016-06-16 07:52:33 +0100

    2016-06-16's linux workout

---
+++ HarisOne/linux/System Management/2016-06-16.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
