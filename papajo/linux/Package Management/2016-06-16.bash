200658f88b969ab440e89a3fd8b6d3a365a62abd
papajo <joshipv2@gmail.com>
2016-06-16 15:25:57 +0100

    2016-06-16's linux workout

---
+++ papajo/linux/Package Management/2016-06-16.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a deb based system
+Installing a `.deb` package from the terminal
+List available packages in RPM based systems
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
