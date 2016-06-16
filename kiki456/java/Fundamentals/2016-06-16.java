cbd291d33ba850e70bc2b32349a6c20493869033
kiki456 <kristijan456@gmail.com>
2016-06-16 20:03:06 +0100

    2016-06-16's java workout

---
+++ kiki456/java/Fundamentals/2016-06-16.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Using the `Console`
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+`Autoboxing` and `Unboxing`
