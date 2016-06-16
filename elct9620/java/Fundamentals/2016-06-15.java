be2d6be713ffc2a642ce277d2947d6c8684fee1c
elct9620 <elct9620@frost.tw>
2016-06-16 13:18:41 +0100

    2016-06-15's java workout

---
+++ elct9620/java/Fundamentals/2016-06-15.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Using `Printstream.format` to format a string before printing
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
