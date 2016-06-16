60f1d95d8cdc54246e1d3ce978c02d068a921431
tedf20 <va.sinikov@gmail.com>
2016-06-16 11:30:53 +0100

    2016-06-15's linux workout

---
+++ tedf20/linux/System Management/2016-06-15.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Cancel a system shutdown
