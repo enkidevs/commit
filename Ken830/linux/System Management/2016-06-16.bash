1b80cfcd0b4224c6bbe8ec865633b42c5fdb7ba3
Ken830 <kleung1@gmail.com>
2016-06-16 19:57:44 +0100

    2016-06-16's linux workout

---
+++ Ken830/linux/System Management/2016-06-16.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Preventing a user from logging into the system
+Basic service management with `systemd`
