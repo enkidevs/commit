2caf2a58d0e368975013c159b5775e1c84152239
spacehelmetboy <chris.mitchell@hbgusa.com>
2016-06-16 14:08:49 +0100

    2016-06-16's javascript workout

---
+++ spacehelmetboy/javascript/Node/2016-06-16.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Wrapping errors in Node.js using _node-verror_
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
