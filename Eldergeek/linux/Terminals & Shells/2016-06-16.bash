3e27f1688f62bc5b53452dbe25a424732636d54e
Eldergeek <david@geekguytech.com>
2016-06-16 16:05:20 +0100

    2016-06-16's linux workout

---
+++ Eldergeek/linux/Terminals & Shells/2016-06-16.bash
@@ -0,0 +1,5 @@
+Sha-bang `#!` explained 
+Find any Unix / Linux command
+Useful aliasing in bash
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
