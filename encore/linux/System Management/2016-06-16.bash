a2aedb827cc06e08aac481fc6602c5822cdb91c2
encore <kingkonig@gmail.com>
2016-06-16 07:09:58 +0100

    2016-06-16's linux workout

---
+++ encore/linux/System Management/2016-06-16.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Incorrect time on dual boot systems
+Basic service management with `systemd`
+Cancel a system shutdown
