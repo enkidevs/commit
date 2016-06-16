ae634e0394245dd7c3af2b47a84242591f65df9e
Geroxan <alik810@seznam.cz>
2016-06-16 10:24:25 +0100

    2016-06-16's javascript workout

---
+++ Geroxan/javascript/Core/2016-06-16.js
@@ -0,0 +1,5 @@
+Shuffle an array using `sort` and `random`.
+Precision
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+Inaccuracy of binary floating-point format
