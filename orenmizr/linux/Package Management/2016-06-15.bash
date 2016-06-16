6bfe03aeb0e1343c6f74bf113c39b84a3f348924
orenmizr <oren.mizrahi.inc@gmail.com>
2016-06-16 01:25:33 +0100

    2016-06-15's linux workout

---
+++ orenmizr/linux/Package Management/2016-06-15.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a deb based system
+Backporting in Debian
+
+Installing a `.deb` package from the terminal
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
