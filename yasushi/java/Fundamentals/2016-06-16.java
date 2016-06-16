e1a9c03e8562634a3e1e427e5057c91b1f0c43fe
yasushi <yasushi.ohtsuka@gmail.com>
2016-06-17 00:26:12 +0100

    2016-06-16's java workout

---
+++ yasushi/java/Fundamentals/2016-06-16.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Using the `Console`
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Do not perform bitwise and arithmetic operations on the same data
