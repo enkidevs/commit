69d1edd563897f89462d8fe9613956137230c576
arko <arkoch2015@gmail.com>
2016-06-17 02:55:20 +0100

    2016-06-16's java workout

---
+++ arko/java/Fundamentals/2016-06-16.java
@@ -0,0 +1,5 @@
+Ternary operator
+Using Inheritance to reduce code repetition
+Metadata: setting a file's owner
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
