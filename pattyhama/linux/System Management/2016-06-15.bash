13dd7f269dd7fc639bce3b72640080a0ebbc7fd4
pattyhama <strodr@gmail.com>
2016-06-17 02:36:59 +0100

    2016-06-15's linux workout

---
+++ pattyhama/linux/System Management/2016-06-15.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
