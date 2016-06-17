6811567b90c595f42599ddd52b52e36985955800
Juancho_Villa <xjuanchovillax@gmail.com>
2016-06-17 06:14:39 +0100

    2016-06-16's linux workout

---
+++ Juancho_Villa/linux/Package Management/2016-06-16.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a deb based system
+Backporting in Debian
+
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
+Keeping repos updated with `cron`
