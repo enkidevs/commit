b5b0af45b91cc0ededc9efd874227617f59988b7
bhoneycutt <bh@hny.io>
2016-06-17 16:55:05 +0100

    2016-06-17's linux workout

---
+++ bhoneycutt/linux/File Management/2016-06-17.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Working with `.gz` files
+Find the unknown process preventing deleting of files
