09182bb6b801a83937256127bf92cc0ff0da5adb
DrShavargo <gassi@teldio.com>
2016-06-17 18:07:15 +0100

    2016-06-17's javascript workout

---
+++ DrShavargo/javascript/Browser APIs/2016-06-17.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+CSS for when JavaScript is enabled
+Navigating the browser history
+Window.sessionStorage
+Network Information API
