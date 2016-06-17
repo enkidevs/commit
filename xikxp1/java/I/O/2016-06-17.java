e787eec717e72cc365fd8c965c5329b4bd9fc5de
xikxp1 <xikxp1@gmail.com>
2016-06-17 05:06:44 +0100

    2016-06-17's java workout

---
+++ xikxp1/java/I/O/2016-06-17.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+How to test if file or directory exists in Java
+Create system file paths using the `Path` class
+The `Console` class
