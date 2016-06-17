c66c7004bc95005f8e97787d3bc2b2c738d26fc7
XTender <paul.kamma@gmail.com>
2016-06-17 18:53:43 +0100

    2016-06-17's javascript workout

---
+++ XTender/javascript/Ecmascript 2015/2016-06-17.js
@@ -0,0 +1,5 @@
+ES6 dynamic property keys in mongo-like set clauses
+Defaults values apply only to `undefined` (and not to `null`)
+Hoisting with `var` and `let`
+Rest parameters
+`let` and `const` in ES6
