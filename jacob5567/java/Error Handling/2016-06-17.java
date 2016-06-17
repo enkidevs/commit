52574ea25a45eeae801e6dc7e423038affb34d9d
jacob5567 <jacob5567@gmail.com>
2016-06-17 21:16:18 +0100

    2016-06-17's java workout

---
+++ jacob5567/java/Error Handling/2016-06-17.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Checking a double for `NaN` value
