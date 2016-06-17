7c29aeff1930a7cff4cfb433c586a16a47b0c809
swayzed <officialswayzed@gmail.com>
2016-06-17 15:14:29 +0100

    2016-06-17's javascript workout

---
+++ swayzed/javascript/Core/2016-06-17.js
@@ -0,0 +1,5 @@
+Shuffle an array using `sort` and `random`.
+Use `splice()` to remove an item from an array
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+Avoid using `with`
