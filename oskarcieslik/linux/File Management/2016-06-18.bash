a8e3e60337f7e57c27f4c14292df3a3c76f30091
oskarcieslik <oskar.cieslik@gmail.com>
2016-06-18 14:21:31 +0100

    2016-06-18's linux workout

---
+++ oskarcieslik/linux/File Management/2016-06-18.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm -rf`
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Working with `.gz` files
