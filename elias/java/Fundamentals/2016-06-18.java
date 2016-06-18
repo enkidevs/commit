33c6e6d08cea7298af6533d75e76e1e91c2282de
elias <elias.john@nolteundlauth.de>
2016-06-18 22:55:48 +0100

    2016-06-18's java workout

---
+++ elias/java/Fundamentals/2016-06-18.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Supply `toString()` in all classes
