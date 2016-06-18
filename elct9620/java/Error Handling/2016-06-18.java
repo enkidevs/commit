db4d998c043a3422fd3f3d54d2b521df2a81d4ac
elct9620 <elct9620@frost.tw>
2016-06-18 23:57:43 +0100

    2016-06-18's java workout

---
+++ elct9620/java/Error Handling/2016-06-18.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Converting stack trace to a string
+Handling Null Pointer Exceptions
+Prevent `exceptions` while logging Data
+Checking a double for `NaN` value
