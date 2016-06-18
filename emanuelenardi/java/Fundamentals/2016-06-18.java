5201e29ec454194cf9756a29e529f81bc21069ee
emanuelenardi <nardiemanuele6@gmail.com>
2016-06-18 10:23:12 +0100

    2016-06-18's java workout

---
+++ emanuelenardi/java/Fundamentals/2016-06-18.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
+Using an interface as a parameter
