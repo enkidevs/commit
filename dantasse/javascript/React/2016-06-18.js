adf85ca6862c05dba64393c7de9c7498875708b3
dantasse <dan.tasse@gmail.com>
2016-06-18 20:05:25 +0100

    2016-06-18's javascript workout

---
+++ dantasse/javascript/React/2016-06-18.js
@@ -0,0 +1,5 @@
+Functional Stateless Components in React
+Pixel values in style props for **React**
+Dangerously set `innerHTML`
+Dealing with `this.props.children`
+Immutability helpers in **React**
