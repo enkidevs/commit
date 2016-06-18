9941b66f0079d86240ad3dfbb314978645888ed0
cyantist <calvinhyan@gmail.com>
2016-06-18 04:19:53 +0100

    2016-06-17's javascript workout

---
+++ cyantist/javascript/Core/2016-06-17.js
@@ -0,0 +1,5 @@
+Shuffle an array using `sort` and `random`.
+`if/else` shortcut `conditional operator`
+Undefined / Empty / Null Checks
+Use `splice()` to remove an item from an array
+Hoisting applies only to variable declarations, not initializations
