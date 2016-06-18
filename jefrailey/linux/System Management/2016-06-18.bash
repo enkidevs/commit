b222da988d0d952c8e7e94071c5f71f6b3a72113
jefrailey <jcaetof+enki@gmail.com>
2016-06-18 18:28:58 +0100

    2016-06-18's linux workout

---
+++ jefrailey/linux/System Management/2016-06-18.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
