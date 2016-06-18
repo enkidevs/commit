5549c314052153167a553c2d44be02fd4865f35f
mixture <kevin.tian.cn@gmail.com>
2016-06-18 17:08:40 +0100

    2016-06-18's linux workout

---
+++ mixture/linux/System Management/2016-06-18.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
