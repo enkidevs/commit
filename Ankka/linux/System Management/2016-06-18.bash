568c5c22c3c70e65f8740828731f85840ce1e9ad
Ankka <antero.juutinen@gmail.com>
2016-06-18 22:39:36 +0100

    2016-06-18's linux workout

---
+++ Ankka/linux/System Management/2016-06-18.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
