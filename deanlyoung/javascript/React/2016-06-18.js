ca25ba3627da1f09d5b2561c02240a0558bf7bf7
deanlyoung <dlyoung119@me.com>
2016-06-18 17:46:54 +0100

    2016-06-18's javascript workout

---
+++ deanlyoung/javascript/React/2016-06-18.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+`null` value for *Controlled Components* in **React**
+Using `false` in **JSX**
+Dealing with `this.props.children`
+Shallow compare in **React**
