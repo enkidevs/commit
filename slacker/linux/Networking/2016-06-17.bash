4b213b02c73024834fcf40d69246067c18ff5627
slacker <happyslacking@gmail.com>
2016-06-18 12:06:16 +0100

    2016-06-17's linux workout

---
+++ slacker/linux/Networking/2016-06-17.bash
@@ -0,0 +1,5 @@
+Stress testing with `ab`
+Check `iptables` firewall status
+Sending emails from terminal / scripts
+Preventing ssh from timing out
+Use `tcpdump` to listen to network interface traffic
