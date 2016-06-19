33c3743ee16105209a4e1eaeda2264698c818647
Linuxman <tim.herren@gmx.ch>
2016-06-19 10:39:19 +0100

    2016-06-19's linux workout

---
+++ Linuxman/linux/File Management/2016-06-19.bash
@@ -0,0 +1,6 @@
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Working with `.gz` files
+Find the unknown process preventing deleting of files
