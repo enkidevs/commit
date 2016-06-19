382593bbaf31fdcd3f628be1013caad5ce0dcc1e
Grasseh <steve.gagnev4si@gmail.com>
2016-06-19 13:26:08 +0100

    2016-06-19's javascript workout

---
+++ Grasseh/javascript/Browser APIs/2016-06-19.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Form a `URL` from its parts 
+Drawing with HTML5 `Canvas` API
+Warn user if **Back** button is pressed
+Permission API
