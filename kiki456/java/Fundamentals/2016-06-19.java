fe75a71bd68447abf6b2a827c2b51e811de6d338
kiki456 <kristijan456@gmail.com>
2016-06-19 23:49:14 +0100

    2016-06-19's java workout

---
+++ kiki456/java/Fundamentals/2016-06-19.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Methods performing *Security Checks* must be declared `Private` or `Final`
