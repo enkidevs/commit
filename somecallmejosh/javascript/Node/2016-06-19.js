886408b8e46eb16ff3fffe44279ba58f478740af
somecallmejosh <josh@thebrileys.com>
2016-06-19 12:21:33 +0100

    2016-06-19's javascript workout

---
+++ somecallmejosh/javascript/Node/2016-06-19.js
@@ -0,0 +1,5 @@
+REPL in Node.js
+First-error callbacks in **Node**
+Use conventional property names for error handling in Node.js
+`uncaughtException` listener in Node.js
+`exports` vs. `module.exports` in **Node**
