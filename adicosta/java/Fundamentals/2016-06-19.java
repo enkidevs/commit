5e13732ad0ec00337c61e2569ef807917211412e
adicosta <alex@thepieuvre.com>
2016-06-19 07:51:22 +0100

    2016-06-19's java workout

---
+++ adicosta/java/Fundamentals/2016-06-19.java
@@ -0,0 +1,5 @@
+Ternary operator
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
