c31850d898db6cedd10eeba87fdc987448b1a783
sequiter <sirmuj@gmail.com>
2016-06-19 03:14:28 +0100

    2016-06-18's javascript workout

---
+++ sequiter/javascript/Node/2016-06-18.js
@@ -0,0 +1,5 @@
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+`try-catch` only for **sync** code
