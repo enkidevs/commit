d20bb4ede9aaf13fe500fa31589c60d2d681ec47
DeadSector <dirtyarthd@gmail.com>
2016-06-19 21:21:11 +0100

    2016-06-19's linux workout

---
+++ DeadSector/linux/Terminals & Shells/2016-06-19.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in the history
+Quick `bash` shortcuts 
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
