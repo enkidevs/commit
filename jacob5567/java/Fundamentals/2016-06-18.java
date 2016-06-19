2b93a50b9e2d756ba2394196b637d017b0a4b41b
jacob5567 <jacob5567@gmail.com>
2016-06-19 02:37:26 +0100

    2016-06-18's java workout

---
+++ jacob5567/java/Fundamentals/2016-06-18.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Distinction between `public` and `private` methods
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+`Autoboxing` and `Unboxing`
