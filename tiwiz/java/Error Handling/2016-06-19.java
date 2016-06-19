7fefb0ec1ea2f24936f522e45ccaa7c3fff4ea55
tiwiz <roberto.orgiu@gmail.com>
2016-06-20 00:22:06 +0100

    2016-06-19's java workout

---
+++ tiwiz/java/Error Handling/2016-06-19.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Using exceptions in Java
+Most common reason behind **stack overflow** error
+Throwing an `exception`
