cb58098ab963c6aba71c1e66b7b814f4f740e4cf
martasusuk <martasusuk@gmail.com>
2016-06-19 11:32:41 +0100

    2016-06-19's java workout

---
+++ martasusuk/java/Fundamentals/2016-06-19.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
