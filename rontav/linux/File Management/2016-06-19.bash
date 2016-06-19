6543241338b13496bb2f07340fec3f4e78c69c71
rontav <mr_vali@yahoo.ro>
2016-06-19 13:02:28 +0100

    2016-06-19's linux workout

---
+++ rontav/linux/File Management/2016-06-19.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm -rf`
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
