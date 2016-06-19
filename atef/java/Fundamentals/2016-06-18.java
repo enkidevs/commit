acf7ef4226411151e75c935458bbfd1ac866de9c
atef <atef.najar@icloud.com>
2016-06-19 02:25:26 +0100

    2016-06-18's java workout

---
+++ atef/java/Fundamentals/2016-06-18.java
@@ -0,0 +1,5 @@
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
