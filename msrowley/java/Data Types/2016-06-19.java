fd3682366359d572cf4162e7c585279cdd70b500
msrowley <msrowley@gmail.com>
2016-06-19 12:30:08 +0100

    2016-06-19's java workout

---
+++ msrowley/java/Data Types/2016-06-19.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Numeric Conversion - Widening
