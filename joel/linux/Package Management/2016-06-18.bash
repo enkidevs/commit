20f160a04d593f535e470a2c66e75c26d8a7d804
joel <joel.mong.au@gmail.com>
2016-06-19 14:24:44 +0100

    2016-06-18's linux workout

---
+++ joel/linux/Package Management/2016-06-18.bash
@@ -0,0 +1,6 @@
+Install specific version of a package on debian based systems
+Backporting in Debian
+
+List installed packages
+Installing a `.deb` package from the terminal
+Downgrade a package via `apt-get`
