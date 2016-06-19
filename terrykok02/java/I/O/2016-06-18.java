acbffd85237ce52cb20c8caa14f4956cb0c9412f
terrykok02 <kok_terry@hotmail.com>
2016-06-19 03:08:01 +0100

    2016-06-18's java workout

---
+++ terrykok02/java/I/O/2016-06-18.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+How to test if file or directory exists in Java
+How to list all files in a directory that match a filename extension in Java
+Checking for the existence of files and directories
+Listing a file system's root directories
