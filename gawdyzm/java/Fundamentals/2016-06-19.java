0c2579212a5b9c04e7b431ee867b1de4d45f2420
gawdyzm <a.m.gawda@gmail.com>
2016-06-19 20:41:59 +0100

    2016-06-19's java workout

---
+++ gawdyzm/java/Fundamentals/2016-06-19.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
