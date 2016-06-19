d3f89620dff3c0b5d7fc42fdf223398f200255c1
bmo77 <basha.others@gmail.com>
2016-06-19 09:50:33 +0100

    2016-06-19's linux workout

---
+++ bmo77/linux/System Management/2016-06-19.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Executing scripts on system boot
