1a80c28fdec7639bb4bc33cd5cd37332184d07b5
vladiim <vladiim@yahoo.com.au>
2016-06-19 02:45:22 +0100

    2016-06-18's linux workout

---
+++ vladiim/linux/File Management/2016-06-18.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm -rf`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Deleting non-empty directories
