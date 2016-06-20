43b5e669506c23ea78c6f56c096d908ddb0c45cf
josh058221 <joshphillips.jp@googlemail.com>
2016-06-20 13:32:35 +0100

    2016-06-20's linux workout

---
+++ josh058221/linux/Terminals & Shells/2016-06-20.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in the history
+Clear the terminal instantly
+Find any Unix / Linux command
+Terminal based browser
+Keep useful commands in your shell history with tags
