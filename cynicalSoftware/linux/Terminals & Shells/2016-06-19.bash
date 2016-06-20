247133fea4084df8a463a807276dcad6967bc6f0
cynicalSoftware <andersm223@gmail.com>
2016-06-20 03:23:03 +0100

    2016-06-19's linux workout

---
+++ cynicalSoftware/linux/Terminals & Shells/2016-06-19.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in the history
+Aliasing ssh connections
+Clear bash history
+Useful aliasing in bash
