b1b44028a48fceff92e53c899e47ce36b74aba48
alexkorovyansky <alexk@handsome.is>
2016-06-20 17:49:04 +0100

    2016-06-20's linux workout

---
+++ alexkorovyansky/linux/Terminals & Shells/2016-06-20.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Sha-bang `#!` explained 
+Find any Unix / Linux command
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
