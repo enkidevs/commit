5bb63962afddfdb9ba7af2019bb9a5a56c0b6cd3
bmo77 <basha.others@gmail.com>
2016-06-20 22:19:47 +0100

    2016-06-20's java workout

---
+++ bmo77/java/Fundamentals/2016-06-20.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
