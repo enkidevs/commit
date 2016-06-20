9abc3c99f2a971d2672a09ebce5c1e734cc34857
augustochaves <chaves.augusto@gmail.com>
2016-06-20 12:59:18 +0100

    2016-06-20's linux workout

---
+++ augustochaves/linux/System Management/2016-06-20.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
