e74df596bd044f8e3e25267a6673b34e67d14b4c
BenjaLempereur <benjilempereur@gmail.com>
2016-06-20 13:45:03 +0100

    2016-06-20's java workout

---
+++ BenjaLempereur/java/Fundamentals/2016-06-20.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Using an interface as a parameter
