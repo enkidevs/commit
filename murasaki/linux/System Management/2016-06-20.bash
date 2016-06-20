dfaf543462f4d24556c824910f1da23d54622bd0
murasaki <claudine_ziyuefl@hotmail.com>
2016-06-20 09:47:42 +0100

    2016-06-20's linux workout

---
+++ murasaki/linux/System Management/2016-06-20.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Cancel a system shutdown
