189f0ffdd91a3346da022bb1d079335ef872f922
celia_zheng <yuehyunzheng@outlook.com>
2016-06-20 07:16:20 +0100

    2016-06-19's javascript workout

---
+++ celia_zheng/javascript/Core/2016-06-19.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Using assignment operators
+Hoisting applies only to variable declarations, not initializations
+Avoid using `with`
+Extra function arguments are undefined by default
