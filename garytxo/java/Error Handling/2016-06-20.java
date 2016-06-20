be0a7c42ebfdcf19e22468723fe215cfcd311e72
garytxo <garytxo@gmail.com>
2016-06-20 06:10:05 +0100

    2016-06-20's java workout

---
+++ garytxo/java/Error Handling/2016-06-20.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Throwing an `exception`
+Careful Numeric Data Conversions
