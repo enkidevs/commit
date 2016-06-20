2e2f9eb5717cbb8e1ab3fed7204f17edef9b2e0f
gmrodgers <glenmrodgers@gmail.com>
2016-06-20 01:51:06 +0100

    2016-06-19's java workout

---
+++ gmrodgers/java/Fundamentals/2016-06-19.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: setting a file's owner
+Using `synchronized` statements
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
