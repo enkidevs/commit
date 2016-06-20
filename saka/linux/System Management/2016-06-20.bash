922d2356a9980a9f9df3620d136c79d114ec1c06
saka <saka71996@gmail.com>
2016-06-20 06:44:43 +0100

    2016-06-20's linux workout

---
+++ saka/linux/System Management/2016-06-20.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Basic service management with `systemd`
