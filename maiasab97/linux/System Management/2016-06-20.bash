fc7d8045f5810909940a9f8233290bf2d5526b7b
maiasab97 <maiasab97@gmail.com>
2016-06-20 13:34:49 +0100

    2016-06-20's linux workout

---
+++ maiasab97/linux/System Management/2016-06-20.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Executing scripts on system boot
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
