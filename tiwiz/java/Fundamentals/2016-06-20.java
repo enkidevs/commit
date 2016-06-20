cb75d4df0a0f1c8ac6fcb602f23d9995e0b1efc2
tiwiz <roberto.orgiu@gmail.com>
2016-06-21 00:39:15 +0100

    2016-06-20's java workout

---
+++ tiwiz/java/Fundamentals/2016-06-20.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Use meaningful names
+Java naming conventions
+Do not return *references* to private *mutable* class members
