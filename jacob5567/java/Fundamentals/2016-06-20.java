4ceef1116b027672762dd92cc7ecac2a597db428
jacob5567 <jacob5567@gmail.com>
2016-06-20 17:34:28 +0100

    2016-06-20's java workout

---
+++ jacob5567/java/Fundamentals/2016-06-20.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Using Inheritance to reduce code repetition
+Using `synchronized` statements
+Methods performing *Security Checks* must be declared `Private` or `Final`
+How range operations work
