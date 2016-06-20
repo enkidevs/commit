06ec63a5b5631b203a3cdc12d9fce7a311bacd71
yasushi <yasushi.ohtsuka@gmail.com>
2016-06-20 23:56:44 +0100

    2016-06-20's javascript workout

---
+++ yasushi/javascript/Browser APIs/2016-06-20.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+CSS for when JavaScript is enabled
+Interaction with the user
+Allow an element to go fullscreen
+Drag and Drop API
