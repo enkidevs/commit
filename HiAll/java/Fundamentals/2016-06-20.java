61d36d8cdc526e490fe9098b583356b418e1a956
HiAll <r.burke@outlook.ie>
2016-06-20 22:36:19 +0100

    2016-06-20's java workout

---
+++ HiAll/java/Fundamentals/2016-06-20.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
