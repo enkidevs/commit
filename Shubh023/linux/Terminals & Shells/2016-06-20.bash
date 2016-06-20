1918566ea54fbb3e5d00d2d5523049235ee3e45a
Shubh023 <patelshubh1999@gmail.com>
2016-06-20 16:34:31 +0100

    2016-06-20's linux workout

---
+++ Shubh023/linux/Terminals & Shells/2016-06-20.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
