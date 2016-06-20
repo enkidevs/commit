2e8f5eaccb5ced98d5b098b9869759f386b81966
kaldes <kaldes@gmail.com>
2016-06-20 15:20:56 +0100

    2016-06-20's linux workout

---
+++ kaldes/linux/Terminals & Shells/2016-06-20.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Execute a command without saving it in the history
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
