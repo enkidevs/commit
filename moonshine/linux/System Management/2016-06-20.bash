a15ec9f4d7edc868f06780d36d1f99d497c675b0
moonshine <davisnaglis2@gmail.com>
2016-06-20 12:57:50 +0100

    2016-06-20's linux workout

---
+++ moonshine/linux/System Management/2016-06-20.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
