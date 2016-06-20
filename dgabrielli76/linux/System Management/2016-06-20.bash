5898bba15ee5b8bfea1bfb65877d49ee63f2d42c
dgabrielli76 <dgabrielli76@gmail.com>
2016-06-20 08:00:51 +0100

    2016-06-20's linux workout

---
+++ dgabrielli76/linux/System Management/2016-06-20.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
