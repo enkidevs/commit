efc171250637b6dfae4d086854f32dd25a28bb49
lovesUnicorns <lrcbaybay@gmail.com>
2016-06-20 21:38:17 +0100

    2016-06-20's javascript workout

---
+++ lovesUnicorns/javascript/Functions/2016-06-20.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Get query/url variables
+Social sharing without widgets
