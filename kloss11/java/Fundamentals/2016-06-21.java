ee7840d054f257f450423010b6fa3de65ad8f78f
kloss11 <nadolny.marcin@wp.pl>
2016-06-21 21:36:02 +0100

    2016-06-21's java workout

---
+++ kloss11/java/Fundamentals/2016-06-21.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
