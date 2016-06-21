c0efc08f9d328a9f3da6a111879e0993efde36c6
roeland <roelandvanbatenburg@gmail.com>
2016-06-21 13:09:06 +0100

    2016-06-21's java workout

---
+++ roeland/java/Fundamentals/2016-06-21.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Ternary operator
+Using `Printstream.format` to format a string before printing
+Metadata: setting a file's owner
+How range operations work
