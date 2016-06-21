f1856197cab10f6dc344e2f4410b50ea7530bb97
harshs08 <harshs08@gmail.com>
2016-06-21 16:56:20 +0100

    2016-06-21's java workout

---
+++ harshs08/java/Fundamentals/2016-06-21.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Using `synchronized` statements
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+How range operations work
