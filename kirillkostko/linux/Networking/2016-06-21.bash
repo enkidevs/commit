f130c9f286020e7feb426cac0bf92a8850dcba88
kirillkostko <kirill.kostko@gmail.com>
2016-06-21 21:06:32 +0100

    2016-06-21's linux workout

---
+++ kirillkostko/linux/Networking/2016-06-21.bash
@@ -0,0 +1,5 @@
+Disable `IPv6`
+Get real network statistics with `slurm`
+Sending emails from terminal / scripts
+Setting up password-free authentication
+Use `tcpdump` to listen to network interface traffic
