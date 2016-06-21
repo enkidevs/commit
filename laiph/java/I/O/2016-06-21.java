56101b2fb421e76ddcd7d758f22f9ba73c9ccd14
laiph <gabi@inosphere.com>
2016-06-21 06:32:24 +0100

    2016-06-21's java workout

---
+++ laiph/java/I/O/2016-06-21.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+Listing a file system's root directories
