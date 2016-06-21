9171f5faae1fbfd5dd8ad73e8e015767d63fc0ff
elct9620 <elct9620@frost.tw>
2016-06-21 13:33:17 +0100

    2016-06-20's java workout

---
+++ elct9620/java/Error Handling/2016-06-20.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Most common reason behind **stack overflow** error
+Careful Numeric Data Conversions
