c59a777fc52da54e5f1b7fbcdbc277afbd58c507
sagiben <ilnextbus@gmail.com>
2016-06-21 04:42:37 +0100

    2016-06-21's linux workout

---
+++ sagiben/linux/Package Management/2016-06-21.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a deb based system
+Install specific version of a package on debian based systems
+Prevent updating a specific package in Debian systems
+Using `Tasksel` for software installation
+Using `PPAs`
