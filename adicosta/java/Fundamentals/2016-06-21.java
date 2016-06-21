ea7d8ad7d6e191ef61faa64c54840899e24a6da9
adicosta <alex@thepieuvre.com>
2016-06-21 07:13:39 +0100

    2016-06-21's java workout

---
+++ adicosta/java/Fundamentals/2016-06-21.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
