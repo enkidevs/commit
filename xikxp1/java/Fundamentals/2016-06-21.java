86a08604c78913fa3b4a75e328d9eba7240beb2e
xikxp1 <xikxp1@gmail.com>
2016-06-21 05:10:41 +0100

    2016-06-21's java workout

---
+++ xikxp1/java/Fundamentals/2016-06-21.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Ternary operator
+Using `synchronized` statements
+Detect or prevent integer overflow
