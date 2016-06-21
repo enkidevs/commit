d32697a6b670a4466d14a89b1cc35361e959ffd1
elct9620 <elct9620@frost.tw>
2016-06-22 00:25:11 +0100

    2016-06-21's java workout

---
+++ elct9620/java/Fundamentals/2016-06-21.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
