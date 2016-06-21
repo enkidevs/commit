a86827ce45fe544394cd8820e71d5fa88ce1c454
Laani <kathyhchiang@gmail.com>
2016-06-21 09:10:36 +0100

    2016-06-20's java workout

---
+++ Laani/java/Fundamentals/2016-06-20.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Methods performing *Security Checks* must be declared `Private` or `Final`
