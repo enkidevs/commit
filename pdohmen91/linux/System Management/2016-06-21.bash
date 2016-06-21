852061aeac7047fe07a05dca4811ac066bba8362
pdohmen91 <pdohmen91@gmail.com>
2016-06-21 21:49:40 +0100

    2016-06-21's linux workout

---
+++ pdohmen91/linux/System Management/2016-06-21.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
