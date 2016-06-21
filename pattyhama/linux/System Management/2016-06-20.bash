b08ac41df625735a39a1c541cef046ba83df345f
pattyhama <strodr@gmail.com>
2016-06-21 10:37:17 +0100

    2016-06-20's linux workout

---
+++ pattyhama/linux/System Management/2016-06-20.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Executing scripts on system boot
