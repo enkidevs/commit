96e60cde5a790921215b26629bfd172ae2452d56
joselazovargas <jose.lazo.vargas@gmail.com>
2016-06-21 06:53:20 +0100

    2016-06-20's javascript workout

---
+++ joselazovargas/javascript/Browser APIs/2016-06-20.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use hosted scripts to increase performance
+Modify a website's URL
+Allow an element to go fullscreen
+Warn user if **Back** button is pressed
