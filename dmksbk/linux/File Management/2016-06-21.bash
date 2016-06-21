64b8355fdc0461b12a6057377bcc4bd99c8e1f93
dmksbk <dmksbk@gmail.com>
2016-06-21 06:20:36 +0100

    2016-06-21's linux workout

---
+++ dmksbk/linux/File Management/2016-06-21.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm -rf`
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
