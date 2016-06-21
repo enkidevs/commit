285aa19b5a9bdc166ea41b06f404c7f7ae295d99
sivanpatel <sivan.patel@gmail.com>
2016-06-21 10:51:42 +0100

    2016-06-21's javascript workout

---
+++ sivanpatel/javascript/Node/2016-06-21.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Asynchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
+Avoid using `sudo` when running Node
