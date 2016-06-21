6371c3e1b1f2da1814e84288ff53a6e035d09d43
lee.dugas <olivier.dugas.1@gmail.com>
2016-06-21 22:45:57 +0100

    2016-06-21's java workout

---
+++ lee.dugas/java/Fundamentals/2016-06-21.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
