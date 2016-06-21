b7caf5a12dec79a43b230bf2d97f5ccc023483d1
bmo77 <basha.others@gmail.com>
2016-06-21 14:40:20 +0100

    2016-06-21's java workout

---
+++ bmo77/java/Fundamentals/2016-06-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
