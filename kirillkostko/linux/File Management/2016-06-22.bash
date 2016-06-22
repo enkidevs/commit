9bec1d8860cd73cf3ee21d0a383a4b77407ff804
kirillkostko <kirill.kostko@gmail.com>
2016-06-22 18:14:16 +0100

    2016-06-22's linux workout

---
+++ kirillkostko/linux/File Management/2016-06-22.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm -rf`
+Get disk space usage with `df`
+
+Using `touch` to alter files modification time
+Deleting non-empty directories
