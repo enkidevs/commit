13935c0036de757ccefbdb32ca155c9e110b0d69
oskarcieslik <oskar.cieslik@gmail.com>
2016-06-22 08:03:00 +0100

    2016-06-22's linux workout

---
+++ oskarcieslik/linux/Terminals & Shells/2016-06-22.bash
@@ -0,0 +1,5 @@
+Sha-bang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
