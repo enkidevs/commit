3f05bc1171a3d5faafee25ad422176b535be5bfc
Senopratomo <lord.ernesto@gmail.com>
2016-06-22 16:33:52 +0100

    2016-06-22's java workout

---
+++ Senopratomo/java/Fundamentals/2016-06-22.java
@@ -0,0 +1,5 @@
+Cropping images
+Common mistake on switch statements
+Connecting to FTP using Java
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
