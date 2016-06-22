63ed206d218ebe2db4a99a39ee22c8fe732986f9
iwata <gootonori+enki@gmail.com>
2016-06-22 05:31:20 +0100

    2016-06-21's linux workout

---
+++ iwata/linux/System Management/2016-06-21.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Executing scripts on system boot
+Monitor System Memory using `vmstat`
