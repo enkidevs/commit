1139f6f2f4eb3fbaf45548f645565f657760fe5f
punkaid <punkaid@gmail.com>
2016-06-22 14:34:23 +0100

    2016-06-22's linux workout

---
+++ punkaid/linux/File Management/2016-06-22.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Converting between Unix and Windows text files
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Deleting non-empty directories
