2f2d38c37066c290806ec9d08a26ad93f2207251
deekayen <deekayen@deekayen.net>
2016-06-22 13:12:09 +0100

    2016-06-22's linux workout

---
+++ deekayen/linux/File Management/2016-06-22.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Get disk space usage with `df`
+
+Working with `.gz` files
+Find the unknown process preventing deleting of files
