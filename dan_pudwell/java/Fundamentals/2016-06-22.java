93bf459369b469adf19ba63efefe6b6f270804d2
dan_pudwell <dan_pudwell@hotmail.com>
2016-06-22 21:13:45 +0100

    2016-06-22's java workout

---
+++ dan_pudwell/java/Fundamentals/2016-06-22.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
