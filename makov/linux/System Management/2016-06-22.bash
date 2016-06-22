d45a0fd7452b15af95e8ab163f8f6734d2fddd28
makov <buzmakov@gmail.com>
2016-06-22 14:23:14 +0100

    2016-06-22's linux workout

---
+++ makov/linux/System Management/2016-06-22.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Executing scripts on system boot
+Fixing `locale` issues in Debian systems
