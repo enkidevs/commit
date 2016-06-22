913eddf461da6f96dc8a7b56869e3f8eac14dc95
FatCat32 <antoine.mctaggart@gmail.com>
2016-06-22 16:47:35 +0100

    2016-06-22's javascript workout

---
+++ FatCat32/javascript/Core/2016-06-22.js
@@ -0,0 +1,5 @@
+Meaning of polyfill
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+Extra function arguments are undefined by default
+Round numbers to `N` decimals 
