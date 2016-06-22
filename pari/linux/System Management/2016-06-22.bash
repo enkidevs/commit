9aa3c724fa0a20a93ab64897f84bce36b3ecf995
pari <parijat.ghoshal@gmail.com>
2016-06-22 19:02:01 +0100

    2016-06-22's linux workout

---
+++ pari/linux/System Management/2016-06-22.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Executing scripts on system boot
+Monitor System Memory using `vmstat`
