66e69ede40e8299799c18886147cfac6e00c5236
rontav <mr_vali@yahoo.ro>
2016-06-22 14:57:35 +0100

    2016-06-22's linux workout

---
+++ rontav/linux/Terminals & Shells/2016-06-22.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Clear the terminal instantly
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
