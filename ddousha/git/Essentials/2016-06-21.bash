373ff8490c0eb0decfa5ebd1eaf0cf5a092a9728
ddousha <ddousha@gmail.com>
2016-06-22 11:38:04 +0100

    2016-06-21's git workout

---
+++ ddousha/git/Essentials/2016-06-21.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+The three states in git
+Use `short` status to make output more compact
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
