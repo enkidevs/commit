278ec6162034830a85ce9ddec0147e233b0d3915
Gelassen <dmitrii.kazakov@gmail.com>
2016-06-23 00:30:07 +0100

    2016-06-22's java workout

---
+++ Gelassen/java/Error Handling/2016-06-22.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Throwing an `exception`
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
