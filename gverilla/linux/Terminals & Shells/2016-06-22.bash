d838e9abb9f28ac33a5557b45d007d669bf14a1d
gverilla <gverilla@mailbox.org>
2016-06-22 07:53:17 +0100

    2016-06-22's linux workout

---
+++ gverilla/linux/Terminals & Shells/2016-06-22.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
