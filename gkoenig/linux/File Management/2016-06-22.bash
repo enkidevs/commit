5726600ceef2afa379eeae8f14cf7d8246c61473
gkoenig <mail@gregorkoenig.com>
2016-06-22 21:08:05 +0100

    2016-06-22's linux workout

---
+++ gkoenig/linux/File Management/2016-06-22.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
