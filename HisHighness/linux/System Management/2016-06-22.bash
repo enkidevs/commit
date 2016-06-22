2f17f4e4fee753c28da6555c91a2723470685861
HisHighness <pchojka@gmail.com>
2016-06-22 13:59:44 +0100

    2016-06-22's linux workout

---
+++ HisHighness/linux/System Management/2016-06-22.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
