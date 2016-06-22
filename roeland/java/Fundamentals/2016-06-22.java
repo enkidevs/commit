7613f08c6590ed6839cc8fa567847c34d65ed8d1
roeland <roelandvanbatenburg@gmail.com>
2016-06-22 14:51:56 +0100

    2016-06-22's java workout

---
+++ roeland/java/Fundamentals/2016-06-22.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use meaningful names
+Distinction between `public` and `private` methods
+Using Inheritance to reduce code repetition
+Locks in `static synchronized` methods
