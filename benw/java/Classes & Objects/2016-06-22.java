93e287391e961fd6a6fd59c767cabba35d5f4429
benw <bena.white53@gmail.com>
2016-06-22 10:38:38 +0100

    2016-06-22's java workout

---
+++ benw/java/Classes & Objects/2016-06-22.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+The `abstract` keyword
+Java object references
+Final object contents are not immutable
+String formatting and parsing with `SimpleDateFormat`
