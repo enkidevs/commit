87c2b40f97971fbc2f727c3bfb80cd166eea3d7e
kiki456 <kristijan456@gmail.com>
2016-06-22 10:44:08 +0100

    2016-06-22's java workout

---
+++ kiki456/java/Fundamentals/2016-06-22.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Using Inheritance to reduce code repetition
+Using bounded type parameters in generic methods
+Do not perform bitwise and arithmetic operations on the same data
+Implementing an `interface`
