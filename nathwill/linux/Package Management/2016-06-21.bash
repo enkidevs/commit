1a0d211086a4322af2306d0b218e4bd3b39692e8
nathwill <nath.e.will@gmail.com>
2016-06-22 05:09:30 +0100

    2016-06-21's linux workout

---
+++ nathwill/linux/Package Management/2016-06-21.bash
@@ -0,0 +1,5 @@
+Install specific version of a package on debian based systems
+List available packages in RPM based systems
+Removing PPAs
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
