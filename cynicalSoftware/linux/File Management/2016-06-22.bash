eaeed39e8f7980cd10638d5ac8a857c8beaf1581
cynicalSoftware <andersm223@gmail.com>
2016-06-22 22:37:08 +0100

    2016-06-22's linux workout

---
+++ cynicalSoftware/linux/File Management/2016-06-22.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Deleting non-empty directories
