d43b07f12cdabb729d21765d3a45e1fe1d97a67e
dgrant <dagrant82@gmail.com>
2016-06-22 14:29:12 +0100

    2016-06-22's java workout

---
+++ dgrant/java/Fundamentals/2016-06-22.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
