bd1cc436113e3aaf4cd2cc1b62fbc5089795d487
abdel-quddous <abdquddos@gmail.com>
2016-06-23 15:48:02 +0100

    2016-06-23's java workout

---
+++ abdel-quddous/java/Data Types/2016-06-23.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Double brace initialization
+String concatenation in Java
