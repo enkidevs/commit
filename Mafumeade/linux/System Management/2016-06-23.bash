1de9f641758cb071f0281dbffde5c296ad8b1fab
Mafumeade <mafumeade@gmail.com>
2016-06-23 18:49:35 +0100

    2016-06-23's linux workout

---
+++ Mafumeade/linux/System Management/2016-06-23.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
