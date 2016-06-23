4be7dd4a3e78c87d40409ba00f3ea56c10dfb56e
papajo <joshipv2@gmail.com>
2016-06-23 14:53:14 +0100

    2016-06-23's java workout

---
+++ papajo/java/Error Handling/2016-06-23.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Handling Null Pointer Exceptions
+Throwing an `exception`
+Checking a double for `NaN` value
