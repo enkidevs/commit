44a0c9a594e37d6116d547437d42fa742b7da031
jlor <jakob.rosenlund@gmail.com>
2016-06-23 05:53:28 +0100

    2016-06-23's java workout

---
+++ jlor/java/Fundamentals/2016-06-23.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Ternary operator
+Using `Printstream.format` to format a string before printing
+Do not return *references* to private *mutable* class members
+How range operations work
