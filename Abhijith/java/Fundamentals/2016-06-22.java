1b30848f2a9493080f4db18d258537e172208945
Abhijith <mail4abhijithkumar@gmail.com>
2016-06-23 17:45:37 +0100

    2016-06-22's java workout

---
+++ Abhijith/java/Fundamentals/2016-06-22.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
