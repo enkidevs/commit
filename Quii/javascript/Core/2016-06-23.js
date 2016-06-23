f09ff631ae9cf6d95285fc7ac2c701e9b5e8b56c
Quii <schnarchii@me.com>
2016-06-23 08:56:08 +0100

    2016-06-23's javascript workout

---
+++ Quii/javascript/Core/2016-06-23.js
@@ -0,0 +1,5 @@
+Performing a function at timed intervals
+Use `splice()` to remove an item from an array
+Scope and strict mode
+Extra function arguments are undefined by default
+Detect an error type
