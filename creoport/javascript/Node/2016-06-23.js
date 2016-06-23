b6eb0c6eb4df42a650751789aa134aacc7c3c75c
creoport <psiber@yandex.ru>
2016-06-23 20:57:45 +0100

    2016-06-23's javascript workout

---
+++ creoport/javascript/Node/2016-06-23.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+First-error callbacks in **Node**
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+`exports` vs. `module.exports` in **Node**
