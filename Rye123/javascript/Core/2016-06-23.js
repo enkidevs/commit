d04a7fb5e570ac2d1f854403ba4e9374a5f7b641
Rye123 <ryedralisk@gmail.com>
2016-06-24 00:03:30 +0100

    2016-06-23's javascript workout

---
+++ Rye123/javascript/Core/2016-06-23.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Performing a function at timed intervals
+Use `splice()` to remove an item from an array
+Data type comparison in `switch` statements
+Base convertion
