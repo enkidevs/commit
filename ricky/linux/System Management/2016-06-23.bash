15058726f483d5b4793ab1bc92fc0a03abef5934
ricky <riccardotonini@gmail.com>
2016-06-23 08:21:45 +0100

    2016-06-23's linux workout

---
+++ ricky/linux/System Management/2016-06-23.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Preventing a user from logging into the system
+Monitor System Memory using `vmstat`
