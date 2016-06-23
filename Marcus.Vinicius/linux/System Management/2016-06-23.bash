0877d4dc5d268594c0f6f2b6887f97319c627a00
Marcus.Vinicius <marcusviniciusaguiar@gmail.com>
2016-06-23 16:09:14 +0100

    2016-06-23's linux workout

---
+++ Marcus.Vinicius/linux/System Management/2016-06-23.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
