4287223847c881b9f03b1f65fcc2a994606f1f05
whoisjk <jmskim19@gmail.com>
2016-06-23 02:46:51 +0100

    2016-06-22's linux workout

---
+++ whoisjk/linux/System Management/2016-06-22.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Executing scripts on system boot
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
