4cbff33d783f7fdcd719209fc508cb1ac42f7f0d
gverilla <gverilla@mailbox.org>
2016-06-23 08:09:28 +0100

    2016-06-23's linux workout

---
+++ gverilla/linux/Terminals & Shells/2016-06-23.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in the history
+Terminal based browser
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
