c3d09f953d379b06bc4b5e30bc72ed582c5dc743
rajeshspr <rajesh.sp.cts@gmail.com>
2016-06-23 16:53:12 +0100

    2016-06-22's linux workout

---
+++ rajeshspr/linux/Terminals & Shells/2016-06-22.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
