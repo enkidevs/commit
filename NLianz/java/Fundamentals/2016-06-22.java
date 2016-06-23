eb0c46ae48aedcf06c9969142d9b8f69ba0b4fe5
NLianz <nungliansum@gmail.com>
2016-06-23 19:41:46 +0100

    2016-06-22's java workout

---
+++ NLianz/java/Fundamentals/2016-06-22.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Connecting to FTP using Java
+Distinction between `public` and `private` methods
+Using `Printstream.format` to format a string before printing
+How range operations work
