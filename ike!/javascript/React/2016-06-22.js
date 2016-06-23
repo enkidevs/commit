89a662aea2a228b37fb44bf7bace8996099f5d37
ike! <michaelhbwood@gmail.com>
2016-06-23 02:22:25 +0100

    2016-06-22's javascript workout

---
+++ ike!/javascript/React/2016-06-22.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+`null` value for *Controlled Components* in **React**
+Using `false` in **JSX**
+Clone Elements in **React**
+Immutability helpers in **React**
