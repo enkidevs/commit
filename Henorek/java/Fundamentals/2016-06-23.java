008323a9a11faa45fcfd843c3902441d326f31aa
Henorek <jaroslaw.jankowski@audioteka.com>
2016-06-23 12:16:57 +0100

    2016-06-23's java workout

---
+++ Henorek/java/Fundamentals/2016-06-23.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
