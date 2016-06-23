8c7022c04533a0ce5cfc0cc3915dccd7513569a9
Geroxan <alik810@seznam.cz>
2016-06-23 09:50:52 +0100

    2016-06-23's javascript workout

---
+++ Geroxan/javascript/Node/2016-06-23.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+Use conventional property names for error handling in Node.js
+`process.nextTick()` callback pattern
+Basic web server with Express
+Middleware/pipeline design pattern
