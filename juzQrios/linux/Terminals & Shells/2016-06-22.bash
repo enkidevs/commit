3c9274dddeb6275721f7c4d14c459d83603c9673
juzQrios <darshanjayadev@gmail.com>
2016-06-23 15:13:47 +0100

    2016-06-22's linux workout

---
+++ juzQrios/linux/Terminals & Shells/2016-06-22.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in the history
+Aliasing ssh connections
+Clear the terminal instantly
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
