7132af500acaadbf597415d3a2350834351eb225
ThomasEhardt <thomas.ehardt@gmail.com>
2016-06-23 18:02:16 +0100

    2016-06-23's java workout

---
+++ ThomasEhardt/java/Fundamentals/2016-06-23.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Common mistake on switch statements
+Limit Accessibility of `Fields`
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
