9a1f9755576305b23b03412bdeb0e2fc4f2a29f2
akos.hochrein <hoch.akos@gmail.com>
2016-06-23 12:47:21 +0100

    2016-06-23's linux workout

---
+++ akos.hochrein/linux/Jobs & Processes/2016-06-23.bash
@@ -0,0 +1,5 @@
+Backgrounding tasks 
+Keeping processes running after you log out
+`Cron` dot-in-filename issues when using `run-parts`
+Set a process priority
+How to pause a process and run others in the background
