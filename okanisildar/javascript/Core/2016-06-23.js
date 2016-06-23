c092d6570dfc112b00354d1876c75e2b1a4bd74b
okanisildar <okanisildar@gmail.com>
2016-06-23 07:57:29 +0100

    2016-06-23's javascript workout

---
+++ okanisildar/javascript/Core/2016-06-23.js
@@ -0,0 +1,5 @@
+Performing a function at timed intervals
+Hoisting applies only to variable declarations, not initializations
+Prototype methods
+Prevent background image flicker in **IE**
+Detect an error type
