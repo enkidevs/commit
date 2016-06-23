b3369ab3f8ecacf914f4e9f0244136e98c6da1fb
zggb <yar.vla.pri@gmail.com>
2016-06-23 07:58:44 +0100

    2016-06-23's javascript workout

---
+++ zggb/javascript/Node/2016-06-23.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Streams in **Node**
+Use conventional property names for error handling in Node.js
+`uncaughtException` listener in Node.js
+`try-catch` only for **sync** code
