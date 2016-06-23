f84cb991d3d0d0925c4b425f61749b280161cdb7
Prashant <94.prashantsingh@gmail.com>
2016-06-23 18:11:29 +0100

    2016-06-22's linux workout

---
+++ Prashant/linux/Terminals & Shells/2016-06-22.bash
@@ -0,0 +1,7 @@
+Useful aliasing in bash
+Random password generator
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
