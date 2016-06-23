eead78b635c68a8f5abf4121df6b0c26c54ba932
dyadra <dia.gerhold@gmail.com>
2016-06-23 07:24:16 +0100

    2016-06-23's git workout

---
+++ dyadra/git/Essentials/2016-06-23.bash
@@ -0,0 +1,5 @@
+The three states in git
+Limiting log output by time
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
