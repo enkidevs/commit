66e9b07a5efd1b16cf9c92735a35fd62d3e8f7ed
Treggats <tonko@tonkomulder.nl>
2016-06-23 08:18:40 +0100

    2016-06-23's linux workout

---
+++ Treggats/linux/Package Management/2016-06-23.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a deb based system
+Install specific version of a package on debian based systems
+Backporting in Debian
+
+List available packages in RPM based systems
+Downgrade a package via `apt-get`
