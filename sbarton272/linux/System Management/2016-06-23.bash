a2a0ef08f9203b05d427b8afc708057f31d3cb24
sbarton272 <sbarton272@gmail.com>
2016-06-23 16:06:35 +0100

    2016-06-23's linux workout

---
+++ sbarton272/linux/System Management/2016-06-23.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
