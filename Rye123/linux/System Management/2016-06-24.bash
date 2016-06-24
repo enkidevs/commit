a3abd5b754e0808d2cda80c1554f2dd9eb7d8912
Rye123 <ryedralisk@gmail.com>
2016-06-25 00:06:34 +0100

    2016-06-24's linux workout

---
+++ Rye123/linux/System Management/2016-06-24.bash
@@ -0,0 +1,5 @@
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
