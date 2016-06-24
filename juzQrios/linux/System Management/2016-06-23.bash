93d49e0530525428252502a9447be5f1c22464ef
juzQrios <darshanjayadev@gmail.com>
2016-06-24 03:21:39 +0100

    2016-06-23's linux workout

---
+++ juzQrios/linux/System Management/2016-06-23.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
