e25ff9d2f256e0b5c9ad7e932397992629de8beb
kaelig <kaelig@deloumeau.fr>
2016-06-24 15:58:38 +0100

    2016-06-24's linux workout

---
+++ kaelig/linux/Terminals & Shells/2016-06-24.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Sha-bang `#!` explained 
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
