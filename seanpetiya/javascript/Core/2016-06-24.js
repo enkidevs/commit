8ae743f77211a2101ec46e8560167d644c14f911
seanpetiya <sean@petiya.com>
2016-06-24 15:36:53 +0100

    2016-06-24's javascript workout

---
+++ seanpetiya/javascript/Core/2016-06-24.js
@@ -0,0 +1,5 @@
+Data type comparison in `switch` statements
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+Scope and strict mode
+Getting the *real* dimensions of an image
