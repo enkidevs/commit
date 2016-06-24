7623b639f1528f07f35ac88beabf21eea6b3554c
jammowl <jammowl@gmail.com>
2016-06-24 04:53:15 +0100

    2016-06-23's linux workout

---
+++ jammowl/linux/Terminals & Shells/2016-06-23.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Clear the terminal instantly
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
