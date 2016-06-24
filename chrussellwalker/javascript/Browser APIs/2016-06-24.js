ee14da7d251cf5fef5a23bca25707853e602b0e2
chrussellwalker <c.russell.walker@gmail.com>
2016-06-25 00:54:39 +0100

    2016-06-24's javascript workout

---
+++ chrussellwalker/javascript/Browser APIs/2016-06-24.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+CSS for when JavaScript is enabled
+Warn user if **Back** button is pressed
+`Window.localStorage`
+Network Information API
