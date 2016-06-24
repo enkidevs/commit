c7a7eb9d76387589b4924decc45f8e904bba174a
jlor <jakob.rosenlund@gmail.com>
2016-06-24 05:57:02 +0100

    2016-06-24's java workout

---
+++ jlor/java/Fundamentals/2016-06-24.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Floating garbage and how to deal with it
+How to play a sound file in Java
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
