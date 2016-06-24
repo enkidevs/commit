a0974901af0bad53367e32bdbf8c2a395a2d71e7
rykuno <rykuno@gmail.com>
2016-06-24 08:57:31 +0100

    2016-06-23's linux workout

---
+++ rykuno/linux/Terminals & Shells/2016-06-23.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Execute a command without saving it in the history
+Clear the terminal instantly
+Random password generator
+Conditional command execution
+
+(`&&` operator)
