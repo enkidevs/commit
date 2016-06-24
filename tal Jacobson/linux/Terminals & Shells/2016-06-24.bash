9d705be8c528f142dee41faaf611b2d3a8eff190
tal Jacobson <tal1989@gmail.com>
2016-06-24 17:11:23 +0100

    2016-06-24's linux workout

---
+++ tal Jacobson/linux/Terminals & Shells/2016-06-24.bash
@@ -0,0 +1,7 @@
+Find any Unix / Linux command
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
