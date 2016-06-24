5632f8b5ba289ae2e1d448667aac48a2350a0c82
JLouis <jlsalbego@gmail.com>
2016-06-24 10:48:39 +0100

    2016-06-24's linux workout

---
+++ JLouis/linux/Terminals & Shells/2016-06-24.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in the history
+Clear the terminal instantly
+Find any Unix / Linux command
+Random password generator
+`else` statements using the `||` operator 
