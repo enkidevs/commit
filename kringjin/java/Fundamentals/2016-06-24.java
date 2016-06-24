3f21d0b548c11ef7da534f5c57a89dc23e3047c8
kringjin <antnunessilva@gmail.com>
2016-06-24 14:49:11 +0100

    2016-06-24's java workout

---
+++ kringjin/java/Fundamentals/2016-06-24.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Cropping images
+Using `Printstream.format` to format a string before printing
+Methods performing *Security Checks* must be declared `Private` or `Final`
+`Autoboxing` and `Unboxing`
