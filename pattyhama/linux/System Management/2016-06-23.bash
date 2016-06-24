a48c188d4e81fe68c4fe736f9ef9014e7f52d0ac
pattyhama <strodr@gmail.com>
2016-06-24 09:53:53 +0100

    2016-06-23's linux workout

---
+++ pattyhama/linux/System Management/2016-06-23.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Preventing a user from logging into the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
