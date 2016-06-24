ddcbee5ba9f378071d19994ec46048657dc522c5
Etai <etaisela@gmail.com>
2016-06-24 16:26:54 +0100

    2016-06-24's linux workout

---
+++ Etai/linux/Terminals & Shells/2016-06-24.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
