168cbd6779b5908399db1ce8740032f7a9d1586c
CrazyNano <aka03dev@gmail.com>
2016-06-24 05:41:07 +0100

    2016-06-24's git workout

---
+++ CrazyNano/git/Essentials/2016-06-24.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+What is stored in a commit?
+How to set your username and email
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
