adea73ae185536897003546a3d910a3d95427571
shannon189 <shannonemyers189@gmail.com>
2016-06-24 17:59:01 +0100

    2016-06-24's javascript workout

---
+++ shannon189/javascript/React/2016-06-24.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+Pixel values in style props for **React**
+Expose Component Functions in **React**
+Dealing with `this.props.children`
+Immutability helpers in **React**
