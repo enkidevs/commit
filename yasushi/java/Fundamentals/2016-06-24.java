6bf7da33a335c58009e16d93625f2b2a556e61fe
yasushi <yasushi.ohtsuka@gmail.com>
2016-06-25 00:59:00 +0100

    2016-06-24's java workout

---
+++ yasushi/java/Fundamentals/2016-06-24.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use meaningful names
+Distinction between `public` and `private` methods
+Supply `toString()` in all classes
+Implementing an `interface`
