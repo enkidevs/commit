d363dd7f823d1f8ecb536ab3ec63779ec0f8975d
bendobot <whatsbending@gmail.com>
2016-06-24 17:17:37 +0100

    2016-06-24's linux workout

---
+++ bendobot/linux/Terminals & Shells/2016-06-24.bash
@@ -0,0 +1,5 @@
+Terminal based browser
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
