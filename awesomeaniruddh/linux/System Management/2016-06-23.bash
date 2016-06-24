909a38f8c346a1d2f53feca7348bb92c23729377
awesomeaniruddh <aaniruddh99@gmail.com>
2016-06-24 03:28:02 +0100

    2016-06-23's linux workout

---
+++ awesomeaniruddh/linux/System Management/2016-06-23.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Cancel a system shutdown
