2832e2a815d67319b694d70da26ec4b7b52eceb7
ifyouseewendy <ifyouseewendy@gmail.com>
2016-06-24 11:27:51 +0100

    2016-06-23's linux workout

---
+++ ifyouseewendy/linux/Package Management/2016-06-23.bash
@@ -0,0 +1,6 @@
+Install specific version of a package on debian based systems
+Backporting in Debian
+
+Get `apt` to use a mirror / faster mirror
+Using `Tasksel` for software installation
+Using `PPAs`
