809b7540346d05519bd8d5038ca9dd109f0c898f
dmksbk <dmksbk@gmail.com>
2016-06-24 06:25:13 +0100

    2016-06-24's java workout

---
+++ dmksbk/java/Java 8/2016-06-24.java
@@ -0,0 +1,4 @@
+How to check for static dependencies of applications and libraries from the command line.
+Use `Optional(s)` type in order to avoid `NullPointerException(s)`
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
