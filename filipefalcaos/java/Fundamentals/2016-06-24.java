5cd0d12c2293bd6eb9c413e1ea038aae7d34d59c
filipefalcaos <filipefalcaosantos@gmail.com>
2016-06-24 16:01:07 +0100

    2016-06-24's java workout

---
+++ filipefalcaos/java/Fundamentals/2016-06-24.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
+Using an interface as a parameter
