fb17bbec8b67694dd33e23a146bcc824e8f58ca3
emkk <korley.em@gmail.com>
2016-06-24 14:58:18 +0100

    2016-06-24's linux workout

---
+++ emkk/linux/File Management/2016-06-24.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm -rf`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
