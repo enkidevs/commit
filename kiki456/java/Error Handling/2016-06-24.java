413faa82c838237016060bd09d232b97f31c614b
kiki456 <kristijan456@gmail.com>
2016-06-24 21:19:22 +0100

    2016-06-24's java workout

---
+++ kiki456/java/Error Handling/2016-06-24.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Careful Numeric Data Conversions
