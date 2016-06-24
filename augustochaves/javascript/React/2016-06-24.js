fdf7e27545ea4e0fcb7fc1f4c90f5f1a5ddfa4e4
augustochaves <chaves.augusto@gmail.com>
2016-06-24 15:21:25 +0100

    2016-06-24's javascript workout

---
+++ augustochaves/javascript/React/2016-06-24.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+Shallow compare in **React**
+Keyed fragments in **React**
