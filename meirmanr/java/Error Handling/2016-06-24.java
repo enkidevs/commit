1fec466d37f90b99e189f8a25ba5a4eb976bdba3
meirmanr <ran.meirman@gmail.com>
2016-06-24 16:54:02 +0100

    2016-06-24's java workout

---
+++ meirmanr/java/Error Handling/2016-06-24.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
