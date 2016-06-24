f42a5362900f62346741d4078a932dc03eae9616
d0mbee87 <d0mbee87@gmail.com>
2016-06-24 18:01:39 +0100

    2016-06-24's java workout

---
+++ d0mbee87/java/Fundamentals/2016-06-24.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Using the `Console`
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Supply `toString()` in all classes
