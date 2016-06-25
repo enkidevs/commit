b5aa375fe16d72928a25a9870794efd8f432dbf3
dmksbk <dmksbk@gmail.com>
2016-06-25 10:39:13 +0100

    2016-06-25's java workout

---
+++ dmksbk/java/Fundamentals/2016-06-25.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Using `Printstream.format` to format a string before printing
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
