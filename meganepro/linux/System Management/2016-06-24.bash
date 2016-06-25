8cdcd2c9f6cf7ec2c98ad5be2341014f59148108
meganepro <136.tajima@gmail.com>
2016-06-25 05:42:26 +0100

    2016-06-24's linux workout

---
+++ meganepro/linux/System Management/2016-06-24.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot
