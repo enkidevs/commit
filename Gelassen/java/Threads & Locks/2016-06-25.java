d33832b2c1b8462dc56fcbe0d0956318bfe57569
Gelassen <dmitrii.kazakov@gmail.com>
2016-06-26 00:22:17 +0100

    2016-06-25's java workout

---
+++ Gelassen/java/Threads & Locks/2016-06-25.java
@@ -0,0 +1,5 @@
+Multithreading: use local variables
+Multithreading: minimize locking scope
+Using `Thread.join`
+`Thread.sleep` and `Thread.yield` do not have synchronization mechanisms
+Thread interruptions
