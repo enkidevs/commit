af569eb7cd8b431173ee8bdd3dcbb957a3ab734c
manabu <manabu.ishii.q+enki@gmail.com>
2016-06-25 01:58:17 +0100

    2016-06-24's java workout

---
+++ manabu/java/I/O/2016-06-24.java
@@ -0,0 +1,5 @@
+How to list all files in a directory that match a filename extension in Java
+Translating `Scanner` tokens into primitive types
+Listing a file system's root directories
+Deleting files and directories
+The `Console` class
