0e6dcb07d368c724af2d626b130dd6b37d5de11b
darnox <0darnox0@gmail.com>
2016-06-25 07:53:03 +0100

    2016-06-25's java workout

---
+++ darnox/java/Fundamentals/2016-06-25.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
