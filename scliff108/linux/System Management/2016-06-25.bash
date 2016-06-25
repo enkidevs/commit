81a4635441c613f40b8ad0491900b6a3c8e82c47
scliff108 <shamusc@umass.edu>
2016-06-25 13:59:54 +0100

    2016-06-25's linux workout

---
+++ scliff108/linux/System Management/2016-06-25.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
