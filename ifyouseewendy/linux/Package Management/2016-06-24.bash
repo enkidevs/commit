894132a79a010db40e67264527a9c9997c6178d2
ifyouseewendy <ifyouseewendy@gmail.com>
2016-06-25 11:00:43 +0100

    2016-06-24's linux workout

---
+++ ifyouseewendy/linux/Package Management/2016-06-24.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a deb based system
+Installing a `.deb` package from the terminal
+List available packages in RPM based systems
+Removing PPAs
+Downgrade a package via `apt-get`
