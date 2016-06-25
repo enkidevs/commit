5fa1427bd7d4abdc20a4220955b163447e1d3ccd
raycastle <ray.castle@liveauctioneers.com>
2016-06-25 14:37:15 +0100

    2016-06-25's javascript workout

---
+++ raycastle/javascript/Browser APIs/2016-06-25.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use hosted scripts to increase performance
+Navigating the browser history
+Warn user if **Back** button is pressed
+Blobs
