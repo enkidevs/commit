635bb6b185aa84f01a34e5e55a01475bc0ca6da4
kyezil <louisclergue@gmail.com>
2016-06-25 09:09:41 +0100

    2016-06-25's linux workout

---
+++ kyezil/linux/Terminals & Shells/2016-06-25.bash
@@ -0,0 +1,7 @@
+Sha-bang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear bash history
+Random password generator
+Conditional command execution
+
+(`&&` operator)
