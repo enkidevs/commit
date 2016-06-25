6e5a4f878e5541c1eaf11545aae4411a7ca0e8e8
nathwill <nath.e.will@gmail.com>
2016-06-25 08:13:16 +0100

    2016-06-24's linux workout

---
+++ nathwill/linux/System Management/2016-06-24.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Executing scripts on system boot
