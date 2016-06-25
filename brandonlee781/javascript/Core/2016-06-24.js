460c672eaf5622858f69461ac2945796edb4e01e
brandonlee781 <brandonlee781@gmail.com>
2016-06-25 02:47:19 +0100

    2016-06-24's javascript workout

---
+++ brandonlee781/javascript/Core/2016-06-24.js
@@ -0,0 +1,5 @@
+Performing a function at timed intervals
+Filtering items out of an array
+The difference between null, undefined and NaN
+Closures
+Extra function arguments are undefined by default
