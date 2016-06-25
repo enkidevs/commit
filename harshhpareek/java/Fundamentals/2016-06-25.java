49219fbec97862097b07b56eb681d015bd249a83
harshhpareek <harshhpareek@gmail.com>
2016-06-25 19:21:56 +0100

    2016-06-25's java workout

---
+++ harshhpareek/java/Fundamentals/2016-06-25.java
@@ -0,0 +1,5 @@
+Ternary operator
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
