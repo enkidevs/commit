48fad5ca01a524d9945be8a1bded91e4e41c00c2
Catmondoo <drewebelhar@gmail.com>
2016-06-25 17:19:32 +0100

    2016-06-25's java workout

---
+++ Catmondoo/java/Fundamentals/2016-06-25.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Using an interface as a parameter
