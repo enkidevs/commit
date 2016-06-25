57ad91457f0981734c61227d8990089fe67ffd52
yasushi <yasushi.ohtsuka@gmail.com>
2016-06-26 00:29:05 +0100

    2016-06-25's java workout

---
+++ yasushi/java/Fundamentals/2016-06-25.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Methods performing *Security Checks* must be declared `Private` or `Final`
