60b2453fc3b6dc8d36c51c2fde54fbee6292351f
mattj <jarosmatt@gmail.com>
2016-06-25 08:20:47 +0100

    2016-06-25's linux workout

---
+++ mattj/linux/System Management/2016-06-25.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
