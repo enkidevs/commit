6f1786540979044dcdc98ce9e7afdf2da15d7594
Treggats <tonko@tonkomulder.nl>
2016-06-25 08:04:22 +0100

    2016-06-25's java workout

---
+++ Treggats/java/Fundamentals/2016-06-25.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Do not return *references* to private *mutable* class members
