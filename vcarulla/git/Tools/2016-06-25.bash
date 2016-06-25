7284c7978ff862a96be8e89ed6c351c0e5ad1781
vcarulla <carulla.victor@gmail.com>
2016-06-26 00:25:23 +0100

    2016-06-25's git workout

---
+++ vcarulla/git/Tools/2016-06-25.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Create a new branch from a stash
+Show history of a function
+Remember the results of previous hunk conflicts
+Sharing data by bundling
