5ac21bce45a0026c3f49241833e56fb9d14e05c0
atakan <atakangktepe@gmail.com>
2016-06-25 09:23:58 +0100

    2016-06-25's linux workout

---
+++ atakan/linux/System Management/2016-06-25.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
