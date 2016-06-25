9be3c125c035e95705f7deeda2dc3306b190fbc9
ianmartorell <ianmartorell@gmail.com>
2016-06-25 17:53:31 +0100

    2016-06-25's linux workout

---
+++ ianmartorell/linux/Terminals & Shells/2016-06-25.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Quick `bash` shortcuts 
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
