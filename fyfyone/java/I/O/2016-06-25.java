266067ae15e8f2646c35160453fd845ecc1bb8f1
fyfyone <fyfyone@gmail.com>
2016-06-25 17:33:43 +0100

    2016-06-25's java workout

---
+++ fyfyone/java/I/O/2016-06-25.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Create system file paths using the `Path` class
+Reading and writing text files
+Using buffered streams
+Checking for the existence of files and directories
