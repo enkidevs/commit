d882ad354759cca729eac599f73ea851893f7e67
jequals5 <dump.jack.acct@gmail.com>
2016-06-25 14:43:35 +0100

    2016-06-25's java workout

---
+++ jequals5/java/Data Types/2016-06-25.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Double brace initialization
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
