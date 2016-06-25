efa88b28ff43f03e7bfee6ea7d840bdf861f98cc
danto <danielj@outlook.de>
2016-06-25 08:33:08 +0100

    2016-06-25's linux workout

---
+++ danto/linux/System Management/2016-06-25.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
