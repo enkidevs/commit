1fdd71abec1811dba4da73934f4e75dfa22480aa
mkirk <mkirk@animate-it.org>
2016-06-25 20:22:40 +0100

    2016-06-25's linux workout

---
+++ mkirk/linux/System Management/2016-06-25.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot
+Basic service management with `systemd`
