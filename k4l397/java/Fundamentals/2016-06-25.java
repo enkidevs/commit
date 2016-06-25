a81414fcb2bb8cda38822e687918ca1736a2df32
k4l397 <kyle.jake.kw@gmail.com>
2016-06-25 12:29:29 +0100

    2016-06-25's java workout

---
+++ k4l397/java/Fundamentals/2016-06-25.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
