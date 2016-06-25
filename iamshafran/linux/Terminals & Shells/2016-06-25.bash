6927cde87d71aad3cf031e6d238eb93814c3e6f2
iamshafran <shafran.nawaz@gmail.com>
2016-06-25 06:35:17 +0100

    2016-06-25's linux workout

---
+++ iamshafran/linux/Terminals & Shells/2016-06-25.bash
@@ -0,0 +1,5 @@
+Sha-bang `#!` explained 
+Execute a command without saving it in the history
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
