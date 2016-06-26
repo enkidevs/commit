ad8cfaef01f5542c444f8b0192697ec746be7d90
krusea1 <taylor.s.kruse@gmail.com>
2016-06-26 22:34:17 +0100

    2016-06-26's javascript workout

---
+++ krusea1/javascript/Functions/2016-06-26.js
@@ -0,0 +1,5 @@
+Access a function's `arguments` as an array
+Use the `apply` function to get the `min` or `max` of an array
+IIFE pattern
+Move cursor at the end of text input
+Check if a document is done loading
