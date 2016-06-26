97a63904581fc6f3acbdfb10bf6954d8717d02b0
rajeshspr <rajesh.sp.cts@gmail.com>
2016-06-26 02:58:14 +0100

    2016-06-25's linux workout

---
+++ rajeshspr/linux/System Management/2016-06-25.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot
