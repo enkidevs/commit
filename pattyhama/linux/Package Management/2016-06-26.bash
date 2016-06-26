d0efff8e227241e12e66f0fa4962be1968802167
pattyhama <strodr@gmail.com>
2016-06-27 00:31:59 +0100

    2016-06-26's linux workout

---
+++ pattyhama/linux/Package Management/2016-06-26.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a deb based system
+Backporting in Debian
+
+List installed packages
+List available packages in RPM based systems
+Keeping repos updated with `cron`
