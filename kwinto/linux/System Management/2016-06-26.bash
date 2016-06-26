777e44d19f8d31fda3da2db018041b884984860d
kwinto <acc@ostasevich.com>
2016-06-26 14:55:21 +0100

    2016-06-26's linux workout

---
+++ kwinto/linux/System Management/2016-06-26.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
