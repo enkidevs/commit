8f2d208377da4f5b5b476c5a25c86b09571b42de
jelic98 <jelic.ecloga@gmail.com>
2016-06-26 12:16:15 +0100

    2016-06-26's java workout

---
+++ jelic98/java/Fundamentals/2016-06-26.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
