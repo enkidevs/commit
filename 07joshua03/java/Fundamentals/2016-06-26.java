f67e487912c40d4035aeccec22673330dc418fa1
07joshua03 <joshua0703@outlook.com>
2016-06-26 23:24:57 +0100

    2016-06-26's java workout

---
+++ 07joshua03/java/Fundamentals/2016-06-26.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+How to play a sound file in Java
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+`Autoboxing` and `Unboxing`
