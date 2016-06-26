8f2f014aee0e776b34676ef242b828ba66147e17
eduardo <earomero@gmail.com>
2016-06-27 00:20:34 +0100

    2016-06-26's javascript workout

---
+++ eduardo/javascript/React/2016-06-26.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+Functional Stateless Components in React
+Pixel values in style props for **React**
+Dangerously set `innerHTML`
+`PureRenderMixin` in **React**
