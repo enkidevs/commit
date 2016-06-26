ddb7ca57d383eeb9b4307231a36ae3c27eca611a
samschmidt <tgcapnsam@gmail.com>
2016-06-26 14:04:48 +0100

    2016-06-26's linux workout

---
+++ samschmidt/linux/Terminals & Shells/2016-06-26.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Random password generator
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
