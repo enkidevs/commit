e5e2447cb4530dad12de73e43c5486a601fdc8bb
IronBeagle <noamhofshi@gmail.com>
2016-06-26 18:02:54 +0100

    2016-06-26's linux workout

---
+++ IronBeagle/linux/Terminals & Shells/2016-06-26.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
