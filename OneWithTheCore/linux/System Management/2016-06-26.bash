acbb15145a75d5cd1477f9d8c9930a43ab77e2f6
OneWithTheCore <ahamilton@linux.com>
2016-06-26 15:39:12 +0100

    2016-06-26's linux workout

---
+++ OneWithTheCore/linux/System Management/2016-06-26.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
