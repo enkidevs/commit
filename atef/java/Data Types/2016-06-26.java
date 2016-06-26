046375ad8e9b41c99e85444373136e17540fa73b
atef <atef.najar@icloud.com>
2016-06-26 14:34:07 +0100

    2016-06-26's java workout

---
+++ atef/java/Data Types/2016-06-26.java
@@ -0,0 +1,5 @@
+`ArrayList` vs `LinkedList`
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Double brace initialization
+Retrieve the component type of an array
