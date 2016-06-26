c7e56ff44bb92339047709e4b77e24c719c1b50a
network2501 <pete@network2501.com>
2016-06-26 05:58:04 +0100

    2016-06-25's linux workout

---
+++ network2501/linux/Package Management/2016-06-25.bash
@@ -0,0 +1,6 @@
+Backporting in Debian
+
+Installing a `.deb` package from the terminal
+Prevent updating a specific package in Debian systems
+Using `Tasksel` for software installation
+Keeping repos updated with `cron`
