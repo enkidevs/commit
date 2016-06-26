c18aadb655b252573e4cc264bd1777c2a9a5f5eb
alan <acbrist@gmail.com>
2016-06-26 18:14:44 +0100

    2016-06-26's linux workout

---
+++ alan/linux/System Management/2016-06-26.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot
