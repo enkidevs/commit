4df0cf26a63bf0f22458cdd56914fcdf9e545e0d
moulin <marco@point.nl>
2016-06-26 15:03:10 +0100

    2016-06-26's linux workout

---
+++ moulin/linux/System Management/2016-06-26.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
