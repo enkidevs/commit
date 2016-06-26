01a4f0f5edb3b5e6e47e54fc8309c8780f24836d
elvispt <elvispt@gmail.com>
2016-06-27 00:03:14 +0100

    2016-06-26's java workout

---
+++ elvispt/java/Fundamentals/2016-06-26.java
@@ -0,0 +1,5 @@
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+`Autoboxing` and `Unboxing`
+How range operations work
