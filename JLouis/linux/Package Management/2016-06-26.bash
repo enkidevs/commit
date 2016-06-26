5a86dce2d54b05cf68603f2173583cb7622072f2
JLouis <jlsalbego@gmail.com>
2016-06-26 12:35:11 +0100

    2016-06-26's linux workout

---
+++ JLouis/linux/Package Management/2016-06-26.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a deb based system
+Installing a `.deb` package from the terminal
+Get `apt` to use a mirror / faster mirror
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
