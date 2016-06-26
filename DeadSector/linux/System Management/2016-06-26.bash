0bcb070c9e43ef3c509a8951e53da6e6fdbc8e0a
DeadSector <dirtyarthd@gmail.com>
2016-06-26 08:29:07 +0100

    2016-06-26's linux workout

---
+++ DeadSector/linux/System Management/2016-06-26.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
