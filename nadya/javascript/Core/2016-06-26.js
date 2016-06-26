240364b8393eea7e544f3d6903f256fdedf61e9e
nadya <nadsvezh@yandex.ru>
2016-06-26 07:54:45 +0100

    2016-06-26's javascript workout

---
+++ nadya/javascript/Core/2016-06-26.js
@@ -0,0 +1,5 @@
+Performing a function at timed intervals
+Labeling your loops
+Hoisting applies only to variable declarations, not initializations
+Closures
+Getting the *real* dimensions of an image
