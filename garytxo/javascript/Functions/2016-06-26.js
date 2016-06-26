274b55850784629b3ba88fc1514c33a8f0cdbdad
garytxo <garytxo@gmail.com>
2016-06-26 08:21:50 +0100

    2016-06-26's javascript workout

---
+++ garytxo/javascript/Functions/2016-06-26.js
@@ -0,0 +1,5 @@
+Access a function's `arguments` as an array
+Difference between **.call** and **.apply** methods
+Function call method
+Move cursor at the end of text input
+Check if a document is done loading
