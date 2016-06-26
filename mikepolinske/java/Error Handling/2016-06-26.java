a815f27ba93d18f98e6c84da4c3ecb0e514c6862
mikepolinske <mikepolinske@gmail.com>
2016-06-26 18:48:08 +0100

    2016-06-26's java workout

---
+++ mikepolinske/java/Error Handling/2016-06-26.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Using exceptions in Java
+Most common reason behind **stack overflow** error
