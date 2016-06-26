24b6fbee479502fd1805f6b8e1b2e2f5e847efc1
nstommo <bluewhitegalaxy@gmail.com>
2016-06-26 23:12:55 +0100

    2016-06-26's java workout

---
+++ nstommo/java/Fundamentals/2016-06-26.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+The distinction between overloading and overriding methods
+Use meaningful names
+The trade-off between short and descriptive names
+Implementing an `interface`
