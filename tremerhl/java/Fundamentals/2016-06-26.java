6486e553c949e0a1abc702de58dda6b96543e1dd
tremerhl <tremerhl@gmail.com>
2016-06-26 09:32:32 +0100

    2016-06-26's java workout

---
+++ tremerhl/java/Fundamentals/2016-06-26.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+The distinction between overloading and overriding methods
+Detect or prevent integer overflow
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
