95a1e37f06576d2f05c37e768172c1df8caa9e23
fdornak <fdornak@gmail.com>
2016-06-26 20:17:18 +0100

    2016-06-26's linux workout

---
+++ fdornak/linux/File Management/2016-06-26.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm -rf`
+Using `touch` to alter files modification time
+Working with `.gz` files
+Find the unknown process preventing deleting of files
