6f4394e31ce74a0fe5627e8be18b90c30a080d38
yikjin <yikjinlim.lyj@gmail.com>
2016-06-26 07:21:18 +0100

    2016-06-25's java workout

---
+++ yikjin/java/Fundamentals/2016-06-25.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Distinction between `public` and `private` methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+How range operations work
