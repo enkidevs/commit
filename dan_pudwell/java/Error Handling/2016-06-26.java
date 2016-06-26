f056e089b8a04ca0e2b731fbb5501802d6486b58
dan_pudwell <dan_pudwell@hotmail.com>
2016-06-26 22:03:07 +0100

    2016-06-26's java workout

---
+++ dan_pudwell/java/Error Handling/2016-06-26.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
