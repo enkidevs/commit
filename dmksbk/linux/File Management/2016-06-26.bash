e657c44ac1c04738bbbf648c0b4ea6c4766e0a1c
dmksbk <dmksbk@gmail.com>
2016-06-26 10:10:14 +0100

    2016-06-26's linux workout

---
+++ dmksbk/linux/File Management/2016-06-26.bash
@@ -0,0 +1,6 @@
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Deleting non-empty directories
+Working with `.gz` files
+Find the unknown process preventing deleting of files
