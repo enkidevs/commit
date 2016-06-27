031713b9959052e9ae2b480992713cf0e19506c5
garytxo <garytxo@gmail.com>
2016-06-27 06:53:20 +0100

    2016-06-27's java workout

---
+++ garytxo/java/Fundamentals/2016-06-27.java
@@ -0,0 +1,5 @@
+Cropping images
+Using the `Console`
+Altering format string output by changing a format specifier's `argument_index`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+How range operations work
