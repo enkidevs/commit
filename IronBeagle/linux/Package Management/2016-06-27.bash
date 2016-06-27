ceb537c3a2ce8a9de20ddf6acefb280cd44ed685
IronBeagle <noamhofshi@gmail.com>
2016-06-27 17:33:18 +0100

    2016-06-27's linux workout

---
+++ IronBeagle/linux/Package Management/2016-06-27.bash
@@ -0,0 +1,5 @@
+Installing a `.deb` package from the terminal
+Prevent updating a specific package in Debian systems
+Get `apt` to use a mirror / faster mirror
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
