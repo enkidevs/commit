f92eec2e4fd8014d322f3ed0bb4d0aea82b9ff42
poupougnac <pierre@digitevent.net>
2016-06-27 13:40:37 +0100

    2016-06-27's linux workout

---
+++ poupougnac/linux/Package Management/2016-06-27.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a deb based system
+List installed packages
+Installing a `.deb` package from the terminal
+Removing PPAs
+Get `apt` to use a mirror / faster mirror
