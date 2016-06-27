9af1486fa3f87c0ef8e8f53c3621c41f17f1867d
cynicalSoftware <andersm223@gmail.com>
2016-06-27 14:34:10 +0100

    2016-06-27's linux workout

---
+++ cynicalSoftware/linux/File Management/2016-06-27.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm -rf`
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Matching Multiple Files With Wildcards
