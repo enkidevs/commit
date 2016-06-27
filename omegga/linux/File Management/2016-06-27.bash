1551a6e463ed38fab94c1bbf403cbc67c84519d1
omegga <megadeveloppeur@gmail.com>
2016-06-27 08:03:15 +0100

    2016-06-27's linux workout

---
+++ omegga/linux/File Management/2016-06-27.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm -rf`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Working with `.gz` files
