3cde916c89d86aea358689c172c7c5ac3c8a4a1f
d0mbee87 <d0mbee87@gmail.com>
2016-06-27 18:34:39 +0100

    2016-06-27's javascript workout

---
+++ d0mbee87/javascript/Functions/2016-06-27.js
@@ -0,0 +1,5 @@
+Access a function's `arguments` as an array
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Anonymous functions
+Social sharing without widgets
