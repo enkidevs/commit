bd69056c1bd2589ef8a3c36d9d52b6f731ae4983
emorse <morse.eric@gmail.com>
2016-06-27 18:44:34 +0100

    2016-06-27's linux workout

---
+++ emorse/linux/Terminals & Shells/2016-06-27.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in the history
+Aliasing ssh connections
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
