ce54533bd8fbbad233453e27bb1c7a5dd880e0a9
dotperinch <dperinch@gmail.com>
2016-06-27 21:21:43 +0100

    2016-06-27's javascript workout

---
+++ dotperinch/javascript/Core/2016-06-27.js
@@ -0,0 +1,5 @@
+Arrays can be used as stack
+Most efficient way to build `HTML` strings
+Multi-line string variables
+Hoisting applies only to variable declarations, not initializations
+JavaScript compilation
