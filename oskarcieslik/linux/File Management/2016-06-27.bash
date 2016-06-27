1f8bbcfbb696460736f84df1bdd3be5e41f295c6
oskarcieslik <oskar.cieslik@gmail.com>
2016-06-27 11:46:03 +0100

    2016-06-27's linux workout

---
+++ oskarcieslik/linux/File Management/2016-06-27.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm -rf`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Working with `.gz` files
