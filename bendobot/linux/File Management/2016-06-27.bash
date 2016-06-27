d2567c5716411255af2acd0f90d1667e65478baf
bendobot <whatsbending@gmail.com>
2016-06-27 18:22:34 +0100

    2016-06-27's linux workout

---
+++ bendobot/linux/File Management/2016-06-27.bash
@@ -0,0 +1,7 @@
+Truncate files with `cat` instead of `rm -rf`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Working with `.gz` files
