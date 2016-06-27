c7fa8e0827154843cb766981864a8c8e1e8ab803
Kar1o <carlos.diego.fig@gmail.com>
2016-06-27 14:08:56 +0100

    2016-06-27's java workout

---
+++ Kar1o/java/Fundamentals/2016-06-27.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Distinction between `public` and `private` methods
+Methods performing *Security Checks* must be declared `Private` or `Final`
+How range operations work
