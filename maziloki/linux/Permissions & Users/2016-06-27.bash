2a042c8ebc25bb205d4edd661d11951e7df0abae
maziloki <maz.spam@gmail.com>
2016-06-27 09:26:13 +0100

    2016-06-27's linux workout

---
+++ maziloki/linux/Permissions & Users/2016-06-27.bash
@@ -0,0 +1,6 @@
+Executing commands with `sudo` without password
+Making a file unalterable with `chattr`
+Linux User management 
+
+Run previous command as root
+Running a command as another local user
