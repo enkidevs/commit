3098904b004d3af3d714b0ce86993c7e95d3ea86
Intie <ludek@rolecek.cz>
2016-06-27 08:01:59 +0100

    2016-06-27's javascript workout

---
+++ Intie/javascript/Functions/2016-06-27.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Method chaining
+Get query/url variables
+Easily generate a random `HEX` color
