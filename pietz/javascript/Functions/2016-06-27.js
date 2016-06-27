466c477be72d9986d10e4226a0dd156086916423
pietz <enki@plpp.de>
2016-06-27 22:51:07 +0100

    2016-06-27's javascript workout

---
+++ pietz/javascript/Functions/2016-06-27.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Get query/url variables
+Easily generate a random `HEX` color
+Move cursor at the end of text input
