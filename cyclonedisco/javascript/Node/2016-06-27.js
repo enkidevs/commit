2b1aac9b23078e3d4cca7665addb7ba8b246f2a9
cyclonedisco <enkichiro.kesh@recursor.net>
2016-06-27 18:57:27 +0100

    2016-06-27's javascript workout

---
+++ cyclonedisco/javascript/Node/2016-06-27.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+`uncaughtException` listener in Node.js
+Clustering
+`try-catch` only for **sync** code
