994dbd47850eabb16af4f2c1ceba3df12a79782f
overspilta <hehewins@163.com>
2016-06-27 02:29:33 +0100

    2016-06-26's javascript workout

---
+++ overspilta/javascript/Core/2016-06-26.js
@@ -0,0 +1,5 @@
+Keywords in hash keys
+Using the double tilde `~~` 
+Hoisting applies only to variable declarations, not initializations
+Getting the *real* dimensions of an image
+Inaccuracy of binary floating-point format
