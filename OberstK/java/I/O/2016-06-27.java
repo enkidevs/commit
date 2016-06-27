49b1e888b1001e9560a062c15e57bcaba9d1d8ef
OberstK <benjamin.kurczyk@gmail.com>
2016-06-27 17:33:24 +0100

    2016-06-27's java workout

---
+++ OberstK/java/I/O/2016-06-27.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to test if file or directory exists in Java
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
