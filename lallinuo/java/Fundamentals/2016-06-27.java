6247ab09a09a6490e29e234526f054ccbdb013b3
lallinuo <lalli.nuorteva@relex.fi>
2016-06-27 07:37:40 +0100

    2016-06-27's java workout

---
+++ lallinuo/java/Fundamentals/2016-06-27.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+The trade-off between short and descriptive names
+Distinction between `public` and `private` methods
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
