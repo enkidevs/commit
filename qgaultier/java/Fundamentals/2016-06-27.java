2e6ed2a8aa5c8dbaa7d04459b401568d65042f3a
qgaultier <quentin.gaultier@gmail.com>
2016-06-27 10:15:57 +0100

    2016-06-27's java workout

---
+++ qgaultier/java/Fundamentals/2016-06-27.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Java naming conventions
+Connecting to FTP using Java
+Distinction between `public` and `private` methods
+Using an interface as a parameter
