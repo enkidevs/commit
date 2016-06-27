2890b0192cccfe234504e96e94aa964bcc352a1e
dgabrielli76 <dgabrielli76@gmail.com>
2016-06-27 07:51:48 +0100

    2016-06-27's linux workout

---
+++ dgabrielli76/linux/Terminals & Shells/2016-06-27.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Sha-bang `#!` explained 
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
