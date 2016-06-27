7738d742eb68900e18aade0fb2ae4952d1cdd428
just2jays <just2jays@gmail.com>
2016-06-27 14:48:26 +0100

    2016-06-27's javascript workout

---
+++ just2jays/javascript/Core/2016-06-27.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Use `splice()` to remove an item from an array
+Hoisting applies only to variable declarations, not initializations
+The difference between null, undefined and NaN
+Base convertion
