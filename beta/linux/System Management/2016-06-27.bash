c7406eb994381718f425b3b45aa68b2f2607b00c
beta <tag@adnm.ch>
2016-06-27 09:14:15 +0100

    2016-06-27's linux workout

---
+++ beta/linux/System Management/2016-06-27.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Incorrect time on dual boot systems
+Basic service management with `systemd`
+Cancel a system shutdown
