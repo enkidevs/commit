1c5a7e39b033690791413f797876e96a6afa9c39
xikxp1 <xikxp1@gmail.com>
2016-06-27 05:14:22 +0100

    2016-06-27's java workout

---
+++ xikxp1/java/Fundamentals/2016-06-27.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Connecting to FTP using Java
+Using the `Console`
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
