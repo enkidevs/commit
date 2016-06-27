cceb14538f35ec850eb056d9ba9c183f68ac8019
admix <admix.snurnikov@gmail.com>
2016-06-27 21:02:36 +0100

    2016-06-27's linux workout

---
+++ admix/linux/File Management/2016-06-27.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Practical `du` command
+Converting between Unix and Windows text files
+Monitor the progress of data through a pipe with `pv`
