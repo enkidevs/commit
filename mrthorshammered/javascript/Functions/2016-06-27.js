cbb690dc18fec41923ebd70ff8e4084991c617ef
mrthorshammered <michaeljones821@hotmail.com>
2016-06-27 12:43:54 +0100

    2016-06-27's javascript workout

---
+++ mrthorshammered/javascript/Functions/2016-06-27.js
@@ -0,0 +1,5 @@
+Access a function's `arguments` as an array
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` with `call` and `apply` methods
+Anonymous functions
+Easily generate a random `HEX` color
