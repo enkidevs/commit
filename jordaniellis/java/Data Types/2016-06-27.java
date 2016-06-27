eec91a2cb65f373f066d0f93faf6fa1d6388b5d4
jordaniellis <jellis1994@gmail.com>
2016-06-27 08:17:01 +0100

    2016-06-27's java workout

---
+++ jordaniellis/java/Data Types/2016-06-27.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Double brace initialization
+Retrieve the component type of an array
