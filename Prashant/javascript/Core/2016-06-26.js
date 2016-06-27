c4f8240b7dbe9b3dc3e893fe417ee7e39e884976
Prashant <94.prashantsingh@gmail.com>
2016-06-27 18:55:04 +0100

    2016-06-26's javascript workout

---
+++ Prashant/javascript/Core/2016-06-26.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Weak vs Strict equality operator
+Most efficient way to build `HTML` strings
+Hoisting applies only to variable declarations, not initializations
+Inaccuracy of binary floating-point format
