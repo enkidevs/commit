8239819a9db56ea15e136a9a8d94b4bc2fb69a5f
yasushi <yasushi.ohtsuka@gmail.com>
2016-06-28 00:14:47 +0100

    2016-06-27's linux workout

---
+++ yasushi/linux/Data Manipulation/2016-06-27.bash
@@ -0,0 +1,5 @@
+Using `lynx` to convert HTML to text
+Image manipulation  using `convert`
+Clean configuration files using `sed`
+The `head` And `tail` Commands
+Selecting Portions of A Line Using `cut`
