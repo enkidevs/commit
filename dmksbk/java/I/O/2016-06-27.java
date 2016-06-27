85f844bb6e004385d017a762220fca3b26187624
dmksbk <dmksbk@gmail.com>
2016-06-27 06:19:01 +0100

    2016-06-27's java workout

---
+++ dmksbk/java/I/O/2016-06-27.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to test if file or directory exists in Java
+Using buffered streams
+Checking for the existence of files and directories
+The `Console` class
