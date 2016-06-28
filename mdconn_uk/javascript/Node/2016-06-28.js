40dc1bcd128d6f118b9ccf286cebbc7bbb20f621
mdconn_uk <mdconn_uk@hotmail.com>
2016-06-28 10:58:06 +0100

    2016-06-28's javascript workout

---
+++ mdconn_uk/javascript/Node/2016-06-28.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Asynchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+Wrapping errors in Node.js using _node-verror_
+Basic web server with Express
