30c1dc2ed1ad0705f0270add7da4adca5b1e60f9
inan <minantr@gmail.com>
2016-06-28 01:20:44 +0100

    2016-06-28's java workout

---
+++ inan/java/Fundamentals/2016-06-28.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
