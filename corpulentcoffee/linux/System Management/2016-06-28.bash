a67777ebbc442d6d373bfbb029280621c86113c6
corpulentcoffee <dave@corpulentcoffee.com>
2016-06-28 16:17:58 +0100

    2016-06-28's linux workout

---
+++ corpulentcoffee/linux/System Management/2016-06-28.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
