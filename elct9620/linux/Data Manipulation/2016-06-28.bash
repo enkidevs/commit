902a1ba73d7d048d942fb64d6818a6ee9c6ca693
elct9620 <elct9620@frost.tw>
2016-06-29 00:31:03 +0100

    2016-06-28's linux workout

---
+++ elct9620/linux/Data Manipulation/2016-06-28.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+Getting rid of duplicate lines
+Image manipulation  using `convert`
+The `head` And `tail` Commands
+Selecting Portions of A Line Using `cut`
