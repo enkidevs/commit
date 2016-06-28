e760c8d43c717163fc1e4767eb20a002b1eedd33
tiwiz <roberto.orgiu@gmail.com>
2016-06-28 18:13:57 +0100

    2016-06-28's java workout

---
+++ tiwiz/java/Fundamentals/2016-06-28.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+`Autoboxing` and `Unboxing`
