fbeda729693af0338266b0f7bd2601856c838622
elias <elias.john@nolteundlauth.de>
2016-06-28 08:12:08 +0100

    2016-06-28's java workout

---
+++ elias/java/Fundamentals/2016-06-28.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+How to play a sound file in Java
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Using an interface as a parameter
