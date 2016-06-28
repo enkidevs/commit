b285d1a03be1fbc1efd1bfd9d03456dc9057a17a
rabizzio <robert.j.valta@gmail.com>
2016-06-28 19:03:44 +0100

    2016-06-28's java workout

---
+++ rabizzio/java/Java 8/2016-06-28.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Java 8 Lambda expressions
+How to check for static dependencies of applications and libraries from the command line.
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
