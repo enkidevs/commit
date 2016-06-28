785c07c353fe7f14fbe95a525229a9f21a12deb2
laurent83 <laurent.morissette@gmail.com>
2016-06-28 13:22:12 +0100

    2016-06-28's java workout

---
+++ laurent83/java/Fundamentals/2016-06-28.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
