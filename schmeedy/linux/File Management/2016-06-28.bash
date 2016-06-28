a10d82057be8f82fcdfc57fe4a4ab08c5babe8bd
schmeedy <mschmied@gmail.com>
2016-06-28 08:42:59 +0100

    2016-06-28's linux workout

---
+++ schmeedy/linux/File Management/2016-06-28.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
