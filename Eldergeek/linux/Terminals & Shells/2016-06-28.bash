052167531e14c1def8a92730e8ec5b00cd4fe5bd
Eldergeek <david@geekguytech.com>
2016-06-28 15:28:26 +0100

    2016-06-28's linux workout

---
+++ Eldergeek/linux/Terminals & Shells/2016-06-28.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Sha-bang `#!` explained 
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
