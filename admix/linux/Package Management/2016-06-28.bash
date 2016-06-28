c8b7e05bf8952d4bdf16f108d0d9611c415043e4
admix <admix.snurnikov@gmail.com>
2016-06-29 00:07:48 +0100

    2016-06-28's linux workout

---
+++ admix/linux/Package Management/2016-06-28.bash
@@ -0,0 +1,6 @@
+Install specific version of a package on debian based systems
+Backporting in Debian
+
+List available packages in RPM based systems
+Removing PPAs
+Using `Tasksel` for software installation
