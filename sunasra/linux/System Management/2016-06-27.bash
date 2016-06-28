e08694ef09cadfdff2a63e1b9ce68a0206550743
sunasra <sunasra@gmail.com>
2016-06-28 17:13:42 +0100

    2016-06-27's linux workout

---
+++ sunasra/linux/System Management/2016-06-27.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
