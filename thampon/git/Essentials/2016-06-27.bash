84ef9a3782d99c50002aaba8d3e01b898c1e36b4
thampon <mtham101@gmail.com>
2016-06-28 07:58:13 +0100

    2016-06-27's git workout

---
+++ thampon/git/Essentials/2016-06-27.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+Make your log output pretty
+Search by commit message keyword
+Use `short` status to make output more compact
+Remote repositories: fetching and pushing
