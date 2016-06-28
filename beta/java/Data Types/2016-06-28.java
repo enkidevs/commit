b03047533f6bb3064223f14412cebff47cc8e5bb
beta <tag@adnm.ch>
2016-06-28 03:35:59 +0100

    2016-06-28's java workout

---
+++ beta/java/Data Types/2016-06-28.java
@@ -0,0 +1,5 @@
+`ArrayList` vs `LinkedList`
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
