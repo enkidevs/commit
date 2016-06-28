e3aaec1fcfb28bce12f024de4780a10cb73bd349
cchchoi1986 <cchchoi1986@gmail.com>
2016-06-28 15:52:32 +0100

    2016-06-27's javascript workout

---
+++ cchchoi1986/javascript/Node/2016-06-27.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+Avoid using `sudo` when running Node
