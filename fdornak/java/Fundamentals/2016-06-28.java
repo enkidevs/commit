499aa73d2f6c9678c7a4dc50a273f586fe819579
fdornak <fdornak@gmail.com>
2016-06-28 09:18:51 +0100

    2016-06-28's java workout

---
+++ fdornak/java/Fundamentals/2016-06-28.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
