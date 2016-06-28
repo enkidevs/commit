80023036166aa9272e6ad481260c77eb003ec197
Beano <beeano@gmail.com>
2016-06-28 08:27:22 +0100

    2016-06-28's linux workout

---
+++ Beano/linux/Terminals & Shells/2016-06-28.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in the history
+Terminal based browser
+Terminal incognito mode
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
