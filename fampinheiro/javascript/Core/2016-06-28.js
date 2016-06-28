3b6d5e44187aa237d8ef7813bc086f31d701a18c
fampinheiro <fampinh3iro+apps@gmail.com>
2016-06-28 14:16:36 +0100

    2016-06-28's javascript workout

---
+++ fampinheiro/javascript/Core/2016-06-28.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Types of numbers
+Setting the length of an array 
+Labeling your loops
+Hoisting applies only to variable declarations, not initializations
