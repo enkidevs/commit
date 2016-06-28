99e03048afb59c1dd455f265a5918ab28b8c926c
augustochaves <chaves.augusto@gmail.com>
2016-06-28 15:03:13 +0100

    2016-06-28's linux workout

---
+++ augustochaves/linux/File Management/2016-06-28.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Matching Multiple Files With Wildcards
