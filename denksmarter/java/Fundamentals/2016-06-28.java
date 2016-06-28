429e993baea3b18b2c45cdfc708b39b23349277e
denksmarter <info@denksmarter.de>
2016-06-28 19:48:02 +0100

    2016-06-28's java workout

---
+++ denksmarter/java/Fundamentals/2016-06-28.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Methods performing *Security Checks* must be declared `Private` or `Final`
