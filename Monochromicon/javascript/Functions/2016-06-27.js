360612d2e15591f2c496a0348c21b031955754fd
Monochromicon <grahammbarber@gmail.com>
2016-06-28 07:56:41 +0100

    2016-06-27's javascript workout

---
+++ Monochromicon/javascript/Functions/2016-06-27.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Method chaining
+Get query/url variables
+Check if a document is done loading
