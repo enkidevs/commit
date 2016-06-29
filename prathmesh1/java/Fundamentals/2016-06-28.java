13ccd111c95f48180cea6b4516a1e2d1470687d6
prathmesh1 <prathmesh01991@gmail.com>
2016-06-29 16:20:57 +0100

    2016-06-28's java workout

---
+++ prathmesh1/java/Fundamentals/2016-06-28.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
