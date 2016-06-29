027ad2ca503d2620769969ace4d6a4af06a89767
Senopratomo <lord.ernesto@gmail.com>
2016-06-29 18:14:52 +0100

    2016-06-29's java workout

---
+++ Senopratomo/java/Fundamentals/2016-06-29.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Metadata: setting a file's owner
+Using an interface as a parameter
