c9d7e845a80f397015afaf57ebb88d5874773b77
jma99 <jaredanderson99@yahoo.com>
2016-06-29 01:34:53 +0100

    2016-06-28's linux workout

---
+++ jma99/linux/File Management/2016-06-28.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Using `touch` to alter files modification time
+Working with `.gz` files
+Matching Multiple Files With Wildcards
