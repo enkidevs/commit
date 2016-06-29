63ee78b59f9f868e14393b8d6872633dafdade44
AliMalek <ali.malek.71@gmail.com>
2016-06-29 06:00:52 +0100

    2016-06-28's java workout

---
+++ AliMalek/java/Classes & Objects/2016-06-28.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Keeping fields `private`
+Double Brace Initialization
+How and when to use `WeakHashMap` class
