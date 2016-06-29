d95047733150a11e819ecc3b589d838ec32f8486
adamyala <adamkyala@gmail.com>
2016-06-29 17:29:35 +0100

    2016-06-29's javascript workout

---
+++ adamyala/javascript/Core/2016-06-29.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Use `splice()` to remove an item from an array
+Data type comparison in `switch` statements
+Prevent background image flicker in **IE**
+Base convertion
