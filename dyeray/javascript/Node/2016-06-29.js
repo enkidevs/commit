7b29123fd1cf20ff04b159ea0f110e4190841a36
dyeray <dyeray@gmail.com>
2016-06-29 20:07:39 +0100

    2016-06-29's javascript workout

---
+++ dyeray/javascript/Node/2016-06-29.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+How to debug Node.js
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
