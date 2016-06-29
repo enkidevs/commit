a4293f5753bfb473264566dca8116f7dbe3659e7
akshithg <akshith573@gmail.com>
2016-06-29 10:52:30 +0100

    2016-06-28's linux workout

---
+++ akshithg/linux/Terminals & Shells/2016-06-28.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
