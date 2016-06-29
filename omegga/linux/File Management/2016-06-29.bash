9821ca414dd5d876d1b130c098abab3c8ca6d39a
omegga <megadeveloppeur@gmail.com>
2016-06-29 08:03:15 +0100

    2016-06-29's linux workout

---
+++ omegga/linux/File Management/2016-06-29.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
