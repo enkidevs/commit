4002421e484f0e7c4c0c93aa085022550a960cc7
polopollo <paul.vayssiere@gmail.com>
2016-06-29 21:13:08 +0100

    2016-06-29's linux workout

---
+++ polopollo/linux/System Management/2016-06-29.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Basic service management with `systemd`
+Cancel a system shutdown
