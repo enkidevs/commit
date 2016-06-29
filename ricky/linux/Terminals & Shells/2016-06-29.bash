b5b5315b7c4f386f31afbea395f1b8a208616ff5
ricky <riccardotonini@gmail.com>
2016-06-29 08:56:47 +0100

    2016-06-29's linux workout

---
+++ ricky/linux/Terminals & Shells/2016-06-29.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Sha-bang `#!` explained 
+Clear the terminal instantly
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
