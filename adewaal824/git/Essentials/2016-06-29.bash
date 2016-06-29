2b5fa3ee21092e63c2d2ede01847a1edfef5706f
adewaal824 <adewaal824@gmail.com>
2016-06-29 09:37:05 +0100

    2016-06-29's git workout

---
+++ adewaal824/git/Essentials/2016-06-29.bash
@@ -0,0 +1,5 @@
+Adding a remote repository
+Search for commits by author
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
