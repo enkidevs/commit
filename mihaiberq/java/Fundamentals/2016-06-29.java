759c7e5f5feacec10aea0ac3a4649f7492819c88
mihaiberq <mihaiberq@gmail.com>
2016-06-29 08:47:44 +0100

    2016-06-29's java workout

---
+++ mihaiberq/java/Fundamentals/2016-06-29.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Common mistake on switch statements
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
