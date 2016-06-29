68757de72f231dc39c4f77ef393cf86422e68bef
pattyhama <strodr@gmail.com>
2016-06-29 01:05:32 +0100

    2016-06-28's linux workout

---
+++ pattyhama/linux/Data Manipulation/2016-06-28.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+Getting rid of duplicate lines
+Count non-empty lines with `nl` utility
+The `head` And `tail` Commands
+Selecting Portions of A Line Using `cut`
