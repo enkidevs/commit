d84c0ac884542aa49a7f45d3f9d1902cb3e6919b
tarpanZBridge <tarparab@outlook.com>
2016-06-29 20:33:59 +0100

    2016-06-29's java workout

---
+++ tarpanZBridge/java/I/O/2016-06-29.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to list all files in a directory that match a filename extension in Java
+Checking for the existence of files and directories
+Retrieving file store attributes
+The `Console` class
