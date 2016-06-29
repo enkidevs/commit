d9ec1ad19c4a677ab3e44e6fc17a3743c23a5f80
mkirk <mkirk@animate-it.org>
2016-06-29 10:09:31 +0100

    2016-06-29's linux workout

---
+++ mkirk/linux/File Management/2016-06-29.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Get disk space usage with `df`
+
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
+Matching Multiple Files With Wildcards
