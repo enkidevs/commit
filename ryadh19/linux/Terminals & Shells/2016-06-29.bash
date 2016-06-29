dbc12924102aaeaaf8d12bda84792339920b2fa6
ryadh19 <ryadh19@yahoo.com>
2016-06-29 16:03:42 +0100

    2016-06-29's linux workout

---
+++ ryadh19/linux/Terminals & Shells/2016-06-29.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
