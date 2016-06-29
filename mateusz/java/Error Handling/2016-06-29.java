ab794f952872cbc2d91bcc3fcba085f7776a1a36
mateusz <mateusz.gazdziak@gmail.com>
2016-06-29 23:30:13 +0100

    2016-06-29's java workout

---
+++ mateusz/java/Error Handling/2016-06-29.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Using exceptions in Java
+Throwing an `exception`
+Checking a double for `NaN` value
