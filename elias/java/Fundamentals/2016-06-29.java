11410d29a4f62e4442711260b0510e525184186b
elias <elias.john@nolteundlauth.de>
2016-06-29 15:57:43 +0100

    2016-06-29's java workout

---
+++ elias/java/Fundamentals/2016-06-29.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
