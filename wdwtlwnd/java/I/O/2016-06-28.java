c7635da454be2a2681cecacb29d17369958219e6
wdwtlwnd <wdwtlwnd@gmail.com>
2016-06-29 13:27:46 +0100

    2016-06-28's java workout

---
+++ wdwtlwnd/java/I/O/2016-06-28.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+List contents of a directory with `newDirectoryStream`
+The `Console` class
