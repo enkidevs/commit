7ac2db61982fffdd6145f24abaa76c89539812a2
bbofrk <mingliwanglau@gmail.com>
2016-06-29 22:35:59 +0100

    2016-06-29's javascript workout

---
+++ bbofrk/javascript/React/2016-06-29.js
@@ -0,0 +1,5 @@
+Self-closing tags in **JSX**
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+Shallow compare in **React**
+Immutability helpers in **React**
