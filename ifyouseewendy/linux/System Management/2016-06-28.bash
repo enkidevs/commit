a5ae63d67e1ea2495e24da65f7c1456f94fbed57
ifyouseewendy <ifyouseewendy@gmail.com>
2016-06-29 10:15:21 +0100

    2016-06-28's linux workout

---
+++ ifyouseewendy/linux/System Management/2016-06-28.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
