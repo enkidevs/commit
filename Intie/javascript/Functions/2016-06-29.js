82d61a181a8318826f2570c3a00d8dfcdff5fbcf
Intie <ludek@rolecek.cz>
2016-06-29 16:34:25 +0100

    2016-06-29's javascript workout

---
+++ Intie/javascript/Functions/2016-06-29.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function constructor vs. function declaration vs. function expression
+Anonymous functions
+Function call method
+Check if a document is done loading
