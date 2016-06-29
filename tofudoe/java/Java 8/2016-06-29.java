8a8d04c191e943c101c8a407cbae1dbcf1e309a4
tofudoe <melvinlai@outlook.com>
2016-06-29 16:47:03 +0100

    2016-06-29's java workout

---
+++ tofudoe/java/Java 8/2016-06-29.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Java 8 Lambda expressions
+How to check for static dependencies of applications and libraries from the command line.
+Control OS `Process`es in `Java8`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
