a56546c84f53af8b1c44310de6fdf1dadae59fbb
Enlightened_Indigo <jackson.kasha5@gmail.com>
2016-06-29 21:02:03 +0100

    2016-06-29's java workout

---
+++ Enlightened_Indigo/java/Fundamentals/2016-06-29.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
