7e3bb515167a371e9049186edfdf3b562090894e
the1andonlydave <the1andonlydave@gmail.com>
2016-06-29 16:19:26 +0100

    2016-06-29's linux workout

---
+++ the1andonlydave/linux/System Management/2016-06-29.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
