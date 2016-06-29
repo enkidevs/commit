ba862378081517cd3e09f81bec9d4630ecd51ffd
josharmi <josh.armitage@outlook.com>
2016-06-29 03:36:54 +0100

    2016-06-28's linux workout

---
+++ josharmi/linux/System Management/2016-06-28.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
