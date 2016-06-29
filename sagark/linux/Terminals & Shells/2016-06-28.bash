843bc22afbb2fc81088a3ff045d2bde33b7919b5
sagark <sagarkarira1992@gmail.com>
2016-06-29 21:41:18 +0100

    2016-06-28's linux workout

---
+++ sagark/linux/Terminals & Shells/2016-06-28.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Useful aliasing in bash
+Get to know  your commands with `type`
