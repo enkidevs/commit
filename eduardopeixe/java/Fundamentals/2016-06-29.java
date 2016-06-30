cac64b968c8927bdcf045f4f07972879590571b5
eduardopeixe <walthfish@yahoo.com>
2016-06-30 03:47:10 +0100

    2016-06-29's java workout

---
+++ eduardopeixe/java/Fundamentals/2016-06-29.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Using the `Console`
+Using Inheritance to reduce code repetition
+Metadata: creating a user-defined file attribute
