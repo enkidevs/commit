89552cd2b1717d0eebc561fc896df8f84bfc136f
Prashant <94.prashantsingh@gmail.com>
2016-06-30 20:21:52 +0100

    2016-06-29's java workout

---
+++ Prashant/java/Data Types/2016-06-29.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
+Numeric Conversion - Widening
