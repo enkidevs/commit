7d2663d5c81fdd4acbd93419f69a8f25662ab693
manabu <manabu.ishii.q+enki@gmail.com>
2016-06-30 10:46:01 +0100

    2016-06-28's java workout

---
+++ manabu/java/Fundamentals/2016-06-28.java
@@ -0,0 +1,4 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Do not perform bitwise and arithmetic operations on the same data
