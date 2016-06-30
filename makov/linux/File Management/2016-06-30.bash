95daf7cc46f897447dfd8231a9d10164c76796ab
makov <buzmakov@gmail.com>
2016-06-30 09:49:01 +0100

    2016-06-30's linux workout

---
+++ makov/linux/File Management/2016-06-30.bash
@@ -0,0 +1,5 @@
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Working with `.gz` files
