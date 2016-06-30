46b717a6c933bf04ad0a5a89e7182bf04f2ca490
Beelzenef <elena.guzbla@gmail.com>
2016-06-30 06:52:51 +0100

    2016-06-30's linux workout

---
+++ Beelzenef/linux/Terminals & Shells/2016-06-30.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Clear the terminal instantly
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
