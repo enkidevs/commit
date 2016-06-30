077478af39e02db15f3803e8c1756b6a98327ba5
hamatoyogi <yoav.ganbar@gmail.com>
2016-06-30 08:11:45 +0100

    2016-06-30's linux workout

---
+++ hamatoyogi/linux/Terminals & Shells/2016-06-30.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in the history
+Clear the terminal instantly
+Useful aliasing in bash
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
