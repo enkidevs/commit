724edaed1569cfbad66f268aa67ea6b6289d1717
92tonywills <92antwilliams@gmail.com>
2016-06-30 23:06:31 +0100

    2016-06-30's java workout

---
+++ 92tonywills/java/Classes & Objects/2016-06-30.java
@@ -0,0 +1,5 @@
+Template methods in abstract classes
+Utility classes and `static` methods
+Compare classes and not class names
+Final object contents are not immutable
+Fields with Java **Reflection API**
