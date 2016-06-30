a5513404ce37381c374122e794572c8f9bebcab4
saka <saka71996@gmail.com>
2016-06-30 05:36:28 +0100

    2016-06-30's javascript workout

---
+++ saka/javascript/Functions/2016-06-30.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Get query/url variables
+Move cursor at the end of text input
+Social sharing without widgets
