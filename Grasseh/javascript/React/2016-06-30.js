4fac40de42bdd8b5add96f64b9cec7c0368dc839
Grasseh <steve.gagnev4si@gmail.com>
2016-06-30 11:58:41 +0100

    2016-06-30's javascript workout

---
+++ Grasseh/javascript/React/2016-06-30.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+`null` value for *Controlled Components* in **React**
+Using `false` in **JSX**
+Expose Component Functions in **React**
+Shallow compare in **React**
