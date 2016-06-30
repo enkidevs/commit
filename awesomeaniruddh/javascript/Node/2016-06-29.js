0d5a8b5237958e4aa5335e1f7124a38bdda725eb
awesomeaniruddh <aaniruddh99@gmail.com>
2016-06-30 05:33:37 +0100

    2016-06-29's javascript workout

---
+++ awesomeaniruddh/javascript/Node/2016-06-29.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+`process.nextTick()` callback pattern
+`exports` vs. `module.exports` in **Node**
