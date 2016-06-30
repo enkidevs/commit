66dc081869632133edc2241894222ec98c6b3b89
benoliver999 <benoliver999@gmail.com>
2016-06-30 09:09:22 +0100

    2016-06-30's linux workout

---
+++ benoliver999/linux/Package Management/2016-06-30.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a deb based system
+Install specific version of a package on debian based systems
+Backporting in Debian
+
+Using `PPAs`
+Keeping repos updated with `cron`
