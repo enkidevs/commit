b84dd1bd5cc5347d3c8ee85f89c57cdeac36f408
elias <elias.john@nolteundlauth.de>
2016-06-30 16:35:16 +0100

    2016-06-30's java workout

---
+++ elias/java/Fundamentals/2016-06-30.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
