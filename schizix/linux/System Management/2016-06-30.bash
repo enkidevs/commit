4a896fbc8f08a7611d08477be0ab334129871a27
schizix <atodorovic@gmail.com>
2016-06-30 16:38:01 +0100

    2016-06-30's linux workout

---
+++ schizix/linux/System Management/2016-06-30.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Basic service management with `systemd`
+Commands to shutdown or restart the system
