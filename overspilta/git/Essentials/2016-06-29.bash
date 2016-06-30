89f97746862262c178940ceb54aca56eaa576913
overspilta <hehewins@163.com>
2016-06-30 07:17:16 +0100

    2016-06-29's git workout

---
+++ overspilta/git/Essentials/2016-06-29.bash
@@ -0,0 +1,5 @@
+Pushing tags to a server
+Make your log output pretty
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
