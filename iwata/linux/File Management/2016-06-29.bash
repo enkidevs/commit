2153094012678d2f788c25362f581f7b2e463d7d
iwata <gootonori+enki@gmail.com>
2016-06-30 04:31:04 +0100

    2016-06-29's linux workout

---
+++ iwata/linux/File Management/2016-06-29.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Truncate files with `cat` instead of `rm -rf`
+Extracting `tar` files to a specific directory
+Matching Multiple Files With Wildcards
