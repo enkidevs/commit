acb1abb79a34785b4d0338538beb0934cce727a4
tiwiz <roberto.orgiu@gmail.com>
2016-06-30 20:14:42 +0100

    2016-06-30's java workout

---
+++ tiwiz/java/Fundamentals/2016-06-30.java
@@ -0,0 +1,5 @@
+Using `Printstream.format` to format a string before printing
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
