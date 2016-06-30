a14f001d94377d1bb8932879989d0370fdfd8323
wux1992 <wux1992@126.com>
2016-06-30 11:28:07 +0100

    2016-06-30's java workout

---
+++ wux1992/java/Fundamentals/2016-06-30.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Java naming conventions
+Using `Printstream.format` to format a string before printing
+Do not perform bitwise and arithmetic operations on the same data
+How range operations work
