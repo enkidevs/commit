02d3ac27b8a92e79ec2671938f5cb5ae12f2e76f
jgsqware <jgonzalez@wemanity.com>
2016-06-30 05:55:24 +0100

    2016-06-30's linux workout

---
+++ jgsqware/linux/Package Management/2016-06-30.bash
@@ -0,0 +1,5 @@
+List available packages in RPM based systems
+Get `apt` to use a mirror / faster mirror
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
+Keeping repos updated with `cron`
