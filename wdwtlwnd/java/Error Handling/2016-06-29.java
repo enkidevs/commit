12c1d9c4c4f08705cfd954acad58748c7bfa4b5c
wdwtlwnd <wdwtlwnd@gmail.com>
2016-06-30 03:38:25 +0100

    2016-06-29's java workout

---
+++ wdwtlwnd/java/Error Handling/2016-06-29.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using exceptions in Java
+Handling Null Pointer Exceptions
+Prevent `exceptions` while logging Data
+Checking a double for `NaN` value
