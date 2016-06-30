d113896ea56e96d5281d1bd77688d7a334abf473
javiruiz <javi.ruiz.gonzalez@gmail.com>
2016-06-30 16:47:32 +0100

    2016-06-30's linux workout

---
+++ javiruiz/linux/System Management/2016-06-30.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot
+Fixing `locale` issues in Debian systems
