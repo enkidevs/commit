d085382b17d586cda240c25e9e09b6e0664e7bf4
AaronM04 <aaron.miller04@gmail.com>
2016-06-30 02:15:33 +0100

    2016-06-29's java workout

---
+++ AaronM04/java/Error Handling/2016-06-29.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Handling Null Pointer Exceptions
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
