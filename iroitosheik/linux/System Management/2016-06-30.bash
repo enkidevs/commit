cbad85afab9c0a09d8f3883baf33ecd5aeaa9642
iroitosheik <endi.pons@gmail.com>
2016-06-30 08:11:06 +0100

    2016-06-30's linux workout

---
+++ iroitosheik/linux/System Management/2016-06-30.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
