4e7a612bca5a3ed125ff070824057518d6bf10c3
viktor.kondin <victor.kondin@hotmail.com>
2016-06-30 08:00:24 +0100

    2016-06-30's linux workout

---
+++ viktor.kondin/linux/System Management/2016-06-30.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
