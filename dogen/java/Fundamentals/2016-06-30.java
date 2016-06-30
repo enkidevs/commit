81cae7060fc984bc92a1000c4033e1d61b189969
dogen <samuel.sh79@gmail.com>
2016-06-30 06:50:13 +0100

    2016-06-30's java workout

---
+++ dogen/java/Fundamentals/2016-06-30.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
