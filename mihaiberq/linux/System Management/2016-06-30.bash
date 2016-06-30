6297e1add43538e32676436ffdea6e57965d99cf
mihaiberq <mihaiberq@gmail.com>
2016-06-30 08:51:21 +0100

    2016-06-30's linux workout

---
+++ mihaiberq/linux/System Management/2016-06-30.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot
