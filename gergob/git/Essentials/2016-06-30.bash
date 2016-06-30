ef9c93357f25c131035fd2ae7fa112de5c8ff31a
gergob <bogdangergo@gmail.com>
2016-06-30 09:36:14 +0100

    2016-06-30's git workout

---
+++ gergob/git/Essentials/2016-06-30.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Finding a tag
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
