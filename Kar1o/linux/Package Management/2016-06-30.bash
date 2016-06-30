2d782f0224b21897b55ecc45e58b70d0ec7f0e70
Kar1o <carlos.diego.fig@gmail.com>
2016-06-30 20:00:23 +0100

    2016-06-30's linux workout

---
+++ Kar1o/linux/Package Management/2016-06-30.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a deb based system
+Installing a `.deb` package from the terminal
+Removing PPAs
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
