c6ff5abdd5e0a199554172b91fe44f456b92e65e
lokyi <roy.yi.yue@gmail.com>
2016-06-30 21:13:43 +0100

    2016-06-30's java workout

---
+++ lokyi/java/Fundamentals/2016-06-30.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using the `Console`
+Using `Printstream.format` to format a string before printing
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
