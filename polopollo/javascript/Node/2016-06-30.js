e4c7162fbfa1dcf97a272789848cdc0cb1e08505
polopollo <paul.vayssiere@gmail.com>
2016-06-30 18:14:59 +0100

    2016-06-30's javascript workout

---
+++ polopollo/javascript/Node/2016-06-30.js
@@ -0,0 +1,5 @@
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+Manage child processes in **Node**
+Bulk-Write in **Node** with `cork()`
+Avoid using `sudo` when running Node
