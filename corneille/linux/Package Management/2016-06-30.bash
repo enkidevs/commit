20728959433606c07b38a380099105c65bb4e203
corneille <cora.neille@gmail.com>
2016-06-30 10:43:49 +0100

    2016-06-30's linux workout

---
+++ corneille/linux/Package Management/2016-06-30.bash
@@ -0,0 +1,5 @@
+Install specific version of a package on debian based systems
+Installing a `.deb` package from the terminal
+List available packages in RPM based systems
+Using `Tasksel` for software installation
+Solving `BADSIG GPG` errors
