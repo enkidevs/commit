8e046c697d0dbd8f5ab145e6466709ded31210fb
mikepolinske <mikepolinske@gmail.com>
2016-06-30 18:55:59 +0100

    2016-06-30's javascript workout

---
+++ mikepolinske/javascript/Node/2016-06-30.js
@@ -0,0 +1,5 @@
+First-error callbacks in **Node**
+Streams in **Node**
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+`exports` vs. `module.exports` in **Node**
