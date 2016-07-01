d4b0eb23ca0064b4173dc59f81d8d5a795db2a5d
Lital Hassine <litalhassine@gmail.com>
2016-07-01 18:04:40 +0100

    2016-07-day1's git workout

---
+++ Lital Hassine/git/Essentials/2016-07-day1.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Search by commit message keyword
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
+Perform a dry run
