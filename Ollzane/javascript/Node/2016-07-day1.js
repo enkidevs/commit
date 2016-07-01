deaa8db22c6cc6d45b9a48229332bec09b1d7d87
Ollzane <ollie.p.carter@gmail.com>
2016-07-01 08:57:43 +0100

    2016-07-day1's javascript workout

---
+++ Ollzane/javascript/Node/2016-07-day1.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+**Node** `os` module
+Wrapping errors in Node.js using _node-verror_
+`exports` vs. `module.exports` in **Node**
