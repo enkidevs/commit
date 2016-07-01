6b476ecb13ced9bf3402bc8324e6bd4079ab6e1a
m2gikbb <machnik.pawel@gmail.com>
2016-07-01 06:45:08 +0100

    2016-07-day1's javascript workout

---
+++ m2gikbb/javascript/Node/2016-07-day1.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+First-error callbacks in **Node**
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
+Avoid using `sudo` when running Node
