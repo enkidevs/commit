781c12ab7a02b44d0c8ab04ff5fb9bc2f62ae084
corpulentcoffee <dave@corpulentcoffee.com>
2016-07-01 01:53:03 +0100

    2016-06-30's javascript workout

---
+++ corpulentcoffee/javascript/Functions/2016-06-30.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+IIFE pattern
+Move cursor at the end of text input
+Social sharing without widgets
