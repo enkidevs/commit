643e76eb62346760cb188e5b8ab8450203ec753d
omegga <megadeveloppeur@gmail.com>
2016-07-01 11:27:50 +0100

    2016-07-day1's linux workout

---
+++ omegga/linux/File Management/2016-07-day1.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
