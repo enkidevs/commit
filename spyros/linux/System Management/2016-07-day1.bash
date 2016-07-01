d782311790916d5228a2d4d23fd4d6f656c283c1
spyros <spyros@enki.com>
2016-07-01 09:48:00 +0100

    2016-07-day1's linux workout

---
+++ spyros/linux/System Management/2016-07-day1.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Preventing a user from logging into the system
+Cancel a system shutdown
