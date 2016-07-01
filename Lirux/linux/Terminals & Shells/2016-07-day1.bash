6c4b19792f71ef3ed4d86800150c8c77f46a71fd
Lirux <liranvaknin@gmail.com>
2016-07-01 10:29:13 +0100

    2016-07-day1's linux workout

---
+++ Lirux/linux/Terminals & Shells/2016-07-day1.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in the history
+Find any Unix / Linux command
+Quick `bash` shortcuts 
+Sequential execution using the `;` statement separator
