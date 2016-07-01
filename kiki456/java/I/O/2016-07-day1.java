bcd4d42a737dd02fb8d3d86419f168340a8ebe9f
kiki456 <kristijan456@gmail.com>
2016-07-01 12:19:09 +0100

    2016-07-day1's java workout

---
+++ kiki456/java/I/O/2016-07-day1.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to create directories in Java
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Listing a file system's root directories
