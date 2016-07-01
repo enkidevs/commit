0fac5f369c598203a23959204e15fdd40b71b57c
kevglx <kevglx@gmail.com>
2016-07-01 08:14:18 +0100

    2016-07-day1's java workout

---
+++ kevglx/java/I/O/2016-07-day1.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Reading file attributes
+Retrieving file store attributes
