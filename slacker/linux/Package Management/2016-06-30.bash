81aa553dd0ff6ef94121aa5aafeeb500b61124da
slacker <happyslacking@gmail.com>
2016-07-01 15:05:57 +0100

    2016-06-30's linux workout

---
+++ slacker/linux/Package Management/2016-06-30.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a deb based system
+Backporting in Debian
+
+List installed packages
+List available packages in RPM based systems
+Downgrade a package via `apt-get`
