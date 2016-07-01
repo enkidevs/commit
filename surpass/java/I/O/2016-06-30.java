09acbb22df652b5ddd134256494e93cda5312a4f
surpass <yanfei_fdu@163.com>
2016-07-01 16:42:19 +0100

    2016-06-30's java workout

---
+++ surpass/java/I/O/2016-06-30.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Checking for the existence of files and directories
+Reading file attributes
+Retrieving file store attributes
+The `Console` class
