d3ce8f9fb99d87cb0c9a956d149cbbdebc347166
deekayen <deekayen@deekayen.net>
2016-07-01 12:26:20 +0100

    2016-07-day1's linux workout

---
+++ deekayen/linux/Terminals & Shells/2016-07-day1.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Terminal based browser
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
