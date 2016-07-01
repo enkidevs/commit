3a51fbde103d406c07fbfbeae1f598dae9ec8011
kostia <viatkinkostia@gmail.com>
2016-07-01 11:16:40 +0100

    2016-07-day1's linux workout

---
+++ kostia/linux/File Management/2016-07-day1.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Using `touch` to alter files modification time
