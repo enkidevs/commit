3093d789f0c9e19f3675fd4284eb735cb70e8af4
makov <buzmakov@gmail.com>
2016-07-01 10:09:17 +0100

    2016-07-day1's linux workout

---
+++ makov/linux/Package Management/2016-07-day1.bash
@@ -0,0 +1,5 @@
+List installed packages
+Installing a `.deb` package from the terminal
+Get `apt` to use a mirror / faster mirror
+Using `Tasksel` for software installation
+Keeping repos updated with `cron`
