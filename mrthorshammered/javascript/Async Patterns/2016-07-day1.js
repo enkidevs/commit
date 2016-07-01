5ac3d193741fa3140fcb250a4962b00da9da14ad
mrthorshammered <michaeljones821@hotmail.com>
2016-07-01 12:34:41 +0100

    2016-07-day1's javascript workout

---
+++ mrthorshammered/javascript/Async Patterns/2016-07-day1.js
@@ -0,0 +1,5 @@
+Name your callback functions to avoid 'callback hell'
+Error-first Callbacks
+The difference between _parallel_, _asynchronous_ and _concurrent_
+Inversion of Control
+Using timers with extra arguments
