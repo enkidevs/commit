ae6c51a9631a5c51d725b5c8355c85255e82e3e1
alexjfno1 <alex.foster@sky.uk>
2016-07-01 08:23:44 +0100

    2016-07-day1's javascript workout

---
+++ alexjfno1/javascript/Node/2016-07-day1.js
@@ -0,0 +1,5 @@
+First-error callbacks in **Node**
+Use conventional property names for error handling in Node.js
+Clustering
+Bulk-Write in **Node** with `cork()`
+Middleware/pipeline design pattern
