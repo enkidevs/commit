7ccbaea4f99ac23bfe10ad645a232df44a8b55b9
ShaeOuloul <louis.grignon@gmail.com>
2016-07-01 16:26:16 +0100

    2016-07-day1's linux workout

---
+++ ShaeOuloul/linux/Terminals & Shells/2016-07-day1.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in the history
+Useful aliasing in bash
