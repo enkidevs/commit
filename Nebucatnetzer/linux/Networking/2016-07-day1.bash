b7d5147ddb3de65ad6f5225ee5d58ee473f4bf6c
Nebucatnetzer <throwaway@2li.ch>
2016-07-01 06:35:42 +0100

    2016-07-day1's linux workout

---
+++ Nebucatnetzer/linux/Networking/2016-07-day1.bash
@@ -0,0 +1,5 @@
+List your IPv6 configuration
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
+Preventing ssh from timing out
+Use `tcpdump` to listen to network interface traffic
