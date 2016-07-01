39cec9ed68a45d641576b70a2a340909db98e8b1
CallMeFabioo <callmefabioo@gmail.com>
2016-07-01 22:19:15 +0100

    2016-07-day1's javascript workout

---
+++ CallMeFabioo/javascript/Core/2016-07-day1.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Using assignment operators
+Data type comparison in `switch` statements
+Extra function arguments are undefined by default
+`console.*` in JavaScript
