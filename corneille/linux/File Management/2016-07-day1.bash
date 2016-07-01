10327adbaa72113f20366d5b782a79cb002b5001
corneille <cora.neille@gmail.com>
2016-07-01 09:54:23 +0100

    2016-07-day1's linux workout

---
+++ corneille/linux/File Management/2016-07-day1.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
