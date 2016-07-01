3598cb835246382264c3d02ad45d740ae3a4223b
Zrugnus <gaustad.lars@gmail.com>
2016-07-01 19:34:33 +0100

    2016-07-day1's linux workout

---
+++ Zrugnus/linux/Terminals & Shells/2016-07-day1.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Aliasing ssh connections
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
