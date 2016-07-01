12150e8b786b9568ca898585d353af427d750002
Ki3i11 <antimozgpunk@gmail.com>
2016-07-01 11:01:21 +0100

    2016-07-day1's java workout

---
+++ Ki3i11/java/Fundamentals/2016-07-day1.java
@@ -0,0 +1,5 @@
+Ternary operator
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Implementing an `interface`
