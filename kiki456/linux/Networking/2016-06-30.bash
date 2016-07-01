ed869cac07004363eaa8540802943af2ed920bba
kiki456 <kristijan456@gmail.com>
2016-07-01 01:08:10 +0100

    2016-06-30's linux workout

---
+++ kiki456/linux/Networking/2016-06-30.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Quick port test with `netcat`
+Check `iptables` firewall status
+Setting up password-free authentication
+Use `tcpdump` to listen to network interface traffic
