9fb524428e478f23eb88aaad781a32f6025e2927
cserban <serban.chiricescu@gmail.com>
2016-07-01 09:28:24 +0100

    2016-07-day1's linux workout

---
+++ cserban/linux/System Management/2016-07-day1.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Executing scripts on system boot
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
