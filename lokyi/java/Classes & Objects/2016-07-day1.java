94b5de018cc4be3b0f07cc715d43e7aecb6c28f3
lokyi <roy.yi.yue@gmail.com>
2016-07-01 21:08:47 +0100

    2016-07-day1's java workout

---
+++ lokyi/java/Classes & Objects/2016-07-day1.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Using inner classes
+Compare classes and not class names
+Final object contents are not immutable
+String formatting and parsing with `SimpleDateFormat`
