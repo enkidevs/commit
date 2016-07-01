a0c5e38d0756de5a459511a98305934c3e7dbc37
marysieek <stoklosama@gmail.com>
2016-07-01 12:44:26 +0100

    2016-07-day1's java workout

---
+++ marysieek/java/Error Handling/2016-07-day1.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Handling Null Pointer Exceptions
+Careful Numeric Data Conversions
