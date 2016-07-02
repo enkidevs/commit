ac68b87eddc19f0de13d7fcae5b4bc7b051f3bbf
jefrailey <jcaetof+enki@gmail.com>
2016-07-02 03:02:16 +0100

    2016-07-day1's linux workout

---
+++ jefrailey/linux/Permissions & Users/2016-07-day1.bash
@@ -0,0 +1,5 @@
+Executing commands with `sudo` without password
+Making a file unalterable with `chattr`
+Understanding `umask`
+Run previous command as root
+Running a command as another local user
