6ff98faefafeb078ed990cac7bd229793a4d35a9
paritoshgpt1 <paritoshgpt1@yahoo.co.uk>
2016-07-02 05:39:56 +0100

    2016-07-day1's linux workout

---
+++ paritoshgpt1/linux/File Management/2016-07-day1.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm -rf`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
