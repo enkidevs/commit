163def994407b76b63dcf3de77493834c3f687fa
jwood <woodjon@gmail.com>
2016-07-02 14:55:38 +0100

    2016-07-day2's javascript workout

---
+++ jwood/javascript/Browser APIs/2016-07-day2.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+CSS for when JavaScript is enabled
+Warn user if **Back** button is pressed
+Network Information API
+Drag and Drop API
