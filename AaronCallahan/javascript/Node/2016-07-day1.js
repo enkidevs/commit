7148c5a16506ffca8b462ed74588f7eb62de880f
AaronCallahan <aaron5682@gmail.com>
2016-07-02 13:56:20 +0100

    2016-07-day1's javascript workout

---
+++ AaronCallahan/javascript/Node/2016-07-day1.js
@@ -0,0 +1,5 @@
+Http Server in **Node**
+Streams in **Node**
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+Avoid using `sudo` when running Node
