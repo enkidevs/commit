8d992ceb77c03bd4f3ad2338c3228718ec122092
sethalexander <itz.saga@gmail.com>
2016-07-02 17:16:25 +0100

    2016-07-day2's java workout

---
+++ sethalexander/java/Fundamentals/2016-07-day2.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Supply `toString()` in all classes
+Using an interface as a parameter
