662168d7f3d14fd2cd499bedc7aafb835a483204
Ollzane <ollie.p.carter@gmail.com>
2016-07-02 09:13:49 +0100

    2016-07-day2's java workout

---
+++ Ollzane/java/Fundamentals/2016-07-day2.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
