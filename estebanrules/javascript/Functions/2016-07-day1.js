e379deb54bd93558a405807adef9fb7475cab468
estebanrules <damian.esteban@gmail.com>
2016-07-02 02:47:34 +0100

    2016-07-day1's javascript workout

---
+++ estebanrules/javascript/Functions/2016-07-day1.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Get query/url variables
+Social sharing without widgets
