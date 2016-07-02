723b2f9a6b008530b2c44f2c57aa013a39d60d2a
ichi2410 <ichi2410@gmail.com>
2016-07-02 07:26:23 +0100

    2016-07-day1's linux workout

---
+++ ichi2410/linux/Terminals & Shells/2016-07-day1.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Sha-bang `#!` explained 
+Execute a command without saving it in the history
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
