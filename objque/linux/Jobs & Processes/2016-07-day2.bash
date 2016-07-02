433d0ba1a9efa023458564fe5b352d4a4005a5f5
objque <wtf.object@gmail.com>
2016-07-02 12:03:27 +0100

    2016-07-day2's linux workout

---
+++ objque/linux/Jobs & Processes/2016-07-day2.bash
@@ -0,0 +1,5 @@
+The `2>&1` redirection
+Keeping processes running after you log out
+`Crontab` newline issue
+Common issue with scripts in `cron`
+How to pause a process and run others in the background
