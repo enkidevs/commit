6934da2f4c048852e49207e13cbe5e10f23fc134
attilag <gluck.attila.bela@gmail.com>
2016-07-02 14:20:00 +0100

    2016-07-day2's java workout

---
+++ attilag/java/Fundamentals/2016-07-day2.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Java naming conventions
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
