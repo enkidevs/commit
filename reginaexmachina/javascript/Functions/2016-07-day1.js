b4cfbf876d1ef827b6acd662789f8d1368551ce7
reginaexmachina <we.love.rachel.day.lots@gmail.com>
2016-07-02 02:51:03 +0100

    2016-07-day1's javascript workout

---
+++ reginaexmachina/javascript/Functions/2016-07-day1.js
@@ -0,0 +1,5 @@
+Access a function's `arguments` as an array
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Move cursor at the end of text input
+Check if a document is done loading
