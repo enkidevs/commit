b7f5a6063fcd7774b3661854966297e871901de9
danto <danielj@outlook.de>
2016-07-02 07:19:56 +0100

    2016-07-day2's linux workout

---
+++ danto/linux/System Management/2016-07-day2.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Preventing a user from logging into the system
+Executing scripts on system boot
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
