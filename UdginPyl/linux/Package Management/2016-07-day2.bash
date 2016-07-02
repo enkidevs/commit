147c7abafa8dad9c773e64a955023cd506fbf8d1
UdginPyl <gromkaktus@gmail.com>
2016-07-02 12:18:28 +0100

    2016-07-day2's linux workout

---
+++ UdginPyl/linux/Package Management/2016-07-day2.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a deb based system
+Installing a `.deb` package from the terminal
+Prevent updating a specific package in Debian systems
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
