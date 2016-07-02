facb958c93ed215c2b8c81c8dbc86e299de496bb
ShaeOuloul <louis.grignon@gmail.com>
2016-07-02 19:02:41 +0100

    2016-07-day2's java workout

---
+++ ShaeOuloul/java/Error Handling/2016-07-day2.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
