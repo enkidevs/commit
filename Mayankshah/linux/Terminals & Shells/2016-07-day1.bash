0bdfbb03dc3da3bf472cacadb1cd5467c383a926
Mayankshah <mayankshah11996@gmail.com>
2016-07-02 15:47:35 +0100

    2016-07-day1's linux workout

---
+++ Mayankshah/linux/Terminals & Shells/2016-07-day1.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Repeating commands with `watch`
+Execute a command without saving it in the history
+Terminal incognito mode
+Sequential execution using the `;` statement separator
