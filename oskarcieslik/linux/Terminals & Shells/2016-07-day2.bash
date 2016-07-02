caff77b400186a240656db2b8d9d8f750ebea9ff
oskarcieslik <oskar.cieslik@gmail.com>
2016-07-02 05:44:33 +0100

    2016-07-day2's linux workout

---
+++ oskarcieslik/linux/Terminals & Shells/2016-07-day2.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in the history
+Clear the terminal instantly
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+The Basics of Environment Variables
