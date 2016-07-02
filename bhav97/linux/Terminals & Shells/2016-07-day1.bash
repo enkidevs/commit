0438aae37cae181af1efe6cbb9a8de719f6d2719
bhav97 <anubhav7991@gmail.com>
2016-07-02 05:52:54 +0100

    2016-07-day1's linux workout

---
+++ bhav97/linux/Terminals & Shells/2016-07-day1.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
