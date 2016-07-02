e683d3e0ffec8d2b4451ba6177629e5e0655e7d7
tiwiz <roberto.orgiu@gmail.com>
2016-07-02 17:31:50 +0100

    2016-07-day2's java workout

---
+++ tiwiz/java/Fundamentals/2016-07-day2.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Implementing an `interface`
+Using an interface as a parameter
