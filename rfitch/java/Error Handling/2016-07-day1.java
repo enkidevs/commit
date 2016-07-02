c69028969877a09a7c2c3ab6b133311965e70b82
rfitch <rfitch@me.com>
2016-07-02 03:35:29 +0100

    2016-07-day1's java workout

---
+++ rfitch/java/Error Handling/2016-07-day1.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Use try-with-resources instead of `finally`
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
