f9513f71530abaa4c44eb30b7ffe552eb44c25e6
overspilt <clipclop182@gmail.com>
2016-07-02 16:23:44 +0100

    2016-07-day1's javascript workout

---
+++ overspilt/javascript/React/2016-07-day1.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+Functional Stateless Components in React
+Expose Component Functions in **React**
+Dealing with `this.props.children`
+Immutability helpers in **React**
