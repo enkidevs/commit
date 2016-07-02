58ccadcb9c1ee691ececa302694b6dc4de2c093c
afbroman <afbroman@icloud.com>
2016-07-02 19:16:01 +0100

    2016-07-day2's javascript workout

---
+++ afbroman/javascript/Functions/2016-07-day2.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Method chaining
+Easily generate a random `HEX` color
