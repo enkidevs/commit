48dba4a8c1b31756e94da38312699dfa1d2762d8
anandagarwaal <anandagarwaal@gmail.com>
2016-07-03 14:38:32 +0100

    2016-07-day2's java workout

---
+++ anandagarwaal/java/I/O/2016-07-day2.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Reading and writing text files
+Reading file attributes
+Retrieving file store attributes
+Listing a file system's root directories
