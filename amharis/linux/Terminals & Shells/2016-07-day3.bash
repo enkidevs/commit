de2e549b2d5a950294848e83e3267952c3bed527
amharis <abdullah.m.haris@gmail.com>
2016-07-03 10:22:44 +0100

    2016-07-day3's linux workout

---
+++ amharis/linux/Terminals & Shells/2016-07-day3.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Sha-bang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Get to know  your commands with `type`
+The Basics of Environment Variables
