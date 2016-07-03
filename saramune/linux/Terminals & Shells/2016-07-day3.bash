8f0e4e5b145c23d278ebd9ac64653124d5e37ede
saramune <si17erad0@gmail.com>
2016-07-04 00:25:22 +0100

    2016-07-day3's linux workout

---
+++ saramune/linux/Terminals & Shells/2016-07-day3.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in the history
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
