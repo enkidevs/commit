4c78a6b394496f08fa6246c9907d56ace9bc0167
Zanex <alessio.zanella88@gmail.com>
2016-07-03 11:24:46 +0100

    2016-07-day3's javascript workout

---
+++ Zanex/javascript/Node/2016-07-day3.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+`try-catch` only for **sync** code
+Avoid using `sudo` when running Node
