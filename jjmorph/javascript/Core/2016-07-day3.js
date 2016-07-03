c94477ae06d530e40b8fdf5fcfdf206d798a56cc
jjmorph <jjmorphew92@gmail.com>
2016-07-03 09:59:37 +0100

    2016-07-day3's javascript workout

---
+++ jjmorph/javascript/Core/2016-07-day3.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Undefined / Empty / Null Checks
+Hoisting applies only to variable declarations, not initializations
+Scope and strict mode
+`console.*` in JavaScript
