e27919137286e4676d6489efe581a09f4a4e74fe
sagark <sagarkarira1992@gmail.com>
2016-07-03 20:08:37 +0100

    2016-07-day2's linux workout

---
+++ sagark/linux/System Management/2016-07-day2.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
