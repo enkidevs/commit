722b5cc715b81dd3704af2b8ab87ffb18abdb2f0
punkaid <punkaid@gmail.com>
2016-07-03 15:45:12 +0100

    2016-07-day3's linux workout

---
+++ punkaid/linux/Permissions & Users/2016-07-day3.bash
@@ -0,0 +1,5 @@
+Executing commands with `sudo` without password
+Get file permissions in octal form
+Set file permissions for users
+Run previous command as root
+Running a command as another local user
