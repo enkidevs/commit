050dcbb2b44e9e8c03f743d3ca7937931cd1d815
vlad.bezden <vladbezden@yahoo.com>
2016-07-03 18:32:26 +0100

    2016-07-day3's javascript workout

---
+++ vlad.bezden/javascript/Node/2016-07-day3.js
@@ -0,0 +1,5 @@
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
