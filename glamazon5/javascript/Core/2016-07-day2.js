9d4a41a11e086b2c998a45f8831fb5f2e562c907
glamazon5 <glamazon5@gmail.com>
2016-07-03 06:06:22 +0100

    2016-07-day2's javascript workout

---
+++ glamazon5/javascript/Core/2016-07-day2.js
@@ -0,0 +1,5 @@
+Performing a function at timed intervals
+Prototype methods
+Prevent background image flicker in **IE**
+Getting the *real* dimensions of an image
+Inaccuracy of binary floating-point format
