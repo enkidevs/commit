ca2441ed9b7727733d70e343b1019a483ab5afcc
ericfennis <eric.fennis@gmail.com>
2016-07-03 21:53:46 +0100

    2016-07-day3's javascript workout

---
+++ ericfennis/javascript/Core/2016-07-day3.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Use `splice()` to remove an item from an array
+Data type comparison in `switch` statements
+The difference between null, undefined and NaN
+Scope and strict mode
