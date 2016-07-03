80743ecec1e4c0c5ff33fd3d755849b413e88ffd
manreet <chahal.reet@gmail.com>
2016-07-03 04:20:04 +0100

    2016-07-day2's java workout

---
+++ manreet/java/Fundamentals/2016-07-day2.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
