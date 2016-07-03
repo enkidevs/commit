09a04b9fe6e8c6ca1f9e3e9275c9ca8ac216f577
numaan <n7m03n@gmail.com>
2016-07-03 18:19:15 +0100

    2016-07-day2's linux workout

---
+++ numaan/linux/File Management/2016-07-day2.bash
@@ -0,0 +1,7 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Deleting non-empty directories
