91bb6d4355e089545a69a15b8a518e56af08306a
dmksbk <dmksbk@gmail.com>
2016-07-03 11:18:49 +0100

    2016-07-day3's linux workout

---
+++ dmksbk/linux/File Management/2016-07-day3.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm -rf`
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Working with `.gz` files
