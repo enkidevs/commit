5c5ea838510ad3c42864a55cd2fb0d718686f9c8
jordaniellis <jellis1994@gmail.com>
2016-07-03 10:45:35 +0100

    2016-07-day3's javascript workout

---
+++ jordaniellis/javascript/React/2016-07-day3.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+Using `false` in **JSX**
+Shallow compare in **React**
