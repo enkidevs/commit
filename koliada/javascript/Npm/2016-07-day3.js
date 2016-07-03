17899dcf6f1e2f1814221bdabc8473275dc6f6ba
koliada <alex.fiator@gmail.com>
2016-07-03 10:18:27 +0100

    2016-07-day3's javascript workout

---
+++ koliada/javascript/Npm/2016-07-day3.js
@@ -0,0 +1,5 @@
+Calling remote scripts with npm
+List binaries for scripting in npm
+Environmental variables and flags for `npm`
+devDependencies in `npm`
+Lock down dependency versions by shrinkwrapping
