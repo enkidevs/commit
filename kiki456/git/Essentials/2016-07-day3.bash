e8567937fb81f807fad71568803d882671a4b06a
kiki456 <kristijan456@gmail.com>
2016-07-04 00:15:49 +0100

    2016-07-day3's git workout

---
+++ kiki456/git/Essentials/2016-07-day3.bash
@@ -0,0 +1,5 @@
+Search for commits by author
+Use `short` status to make output more compact
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
+Recover lost code
