f4785e3a30bea3dee4c44037fbc505cec0b83f9f
sagiben <ilnextbus@gmail.com>
2016-07-03 05:11:59 +0100

    2016-07-day3's linux workout

---
+++ sagiben/linux/System Management/2016-07-day3.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Common log files
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
