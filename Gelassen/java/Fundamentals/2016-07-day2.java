d2ae4f893fcbbf5fe1ea4040ab19c6b575318bd9
Gelassen <dmitrii.kazakov@gmail.com>
2016-07-03 05:30:17 +0100

    2016-07-day2's java workout

---
+++ Gelassen/java/Fundamentals/2016-07-day2.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Overloading Methods in Java
+Using Inheritance to reduce code repetition
+Detect or prevent integer overflow
+How range operations work
