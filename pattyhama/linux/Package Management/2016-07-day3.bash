318d0a26501bd7c4704f44a1d255aa8e67d71688
pattyhama <strodr@gmail.com>
2016-07-04 00:58:19 +0100

    2016-07-day3's linux workout

---
+++ pattyhama/linux/Package Management/2016-07-day3.bash
@@ -0,0 +1,5 @@
+Install specific version of a package on debian based systems
+Removing PPAs
+Prevent updating a specific package in Debian systems
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
