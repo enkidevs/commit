ff053d396b9b70c52b035e17503c5240c119dba9
kiki456 <kristijan456@gmail.com>
2016-07-03 15:49:17 +0100

    2016-07-day2's java workout

---
+++ kiki456/java/Fundamentals/2016-07-day2.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Distinction between `public` and `private` methods
+Using `Printstream.format` to format a string before printing
+Metadata: setting a file's owner
+How range operations work
