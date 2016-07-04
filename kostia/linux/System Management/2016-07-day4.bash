29c05f7920693dca026883c30f1c736a2e5e0c5b
kostia <viatkinkostia@gmail.com>
2016-07-04 10:11:34 +0100

    2016-07-day4's linux workout

---
+++ kostia/linux/System Management/2016-07-day4.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Executing scripts on system boot
+Get hardware stack details with `lspci`
