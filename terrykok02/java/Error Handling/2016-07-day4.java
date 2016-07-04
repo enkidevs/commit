149ecf1dd09394912e6704711bf927a394a49a45
terrykok02 <kok_terry@hotmail.com>
2016-07-04 10:08:06 +0100

    2016-07-day4's java workout

---
+++ terrykok02/java/Error Handling/2016-07-day4.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Use try-with-resources instead of `finally`
+Handling Null Pointer Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
