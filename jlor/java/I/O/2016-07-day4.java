7ab82c02f4c4a268ff170a4e736a9f29a69f622d
jlor <jakob.rosenlund@gmail.com>
2016-07-04 08:11:36 +0100

    2016-07-day4's java workout

---
+++ jlor/java/I/O/2016-07-day4.java
@@ -0,0 +1,5 @@
+How to test if file or directory exists in Java
+How to list all files in a directory that match a filename extension in Java
+Using buffered streams
+List contents of a directory with `newDirectoryStream`
+The `Console` class
