9d50c385039cf86a471ac3226f21b3ef995ee61a
dogen <samuel.sh79@gmail.com>
2016-07-04 18:28:02 +0100

    2016-07-04's java workout

---
+++ dogen/java/Classes & Objects/2016-07-04.java
@@ -0,0 +1,5 @@
+Template methods in abstract classes
+Diamond Operator
+Final object contents are not immutable
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
