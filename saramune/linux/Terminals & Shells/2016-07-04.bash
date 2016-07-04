f196d775a4709a8b584141f089ff8b916d76328c
saramune <si17erad0@gmail.com>
2016-07-05 00:19:51 +0100

    2016-07-04's linux workout

---
+++ saramune/linux/Terminals & Shells/2016-07-04.bash
@@ -0,0 +1,5 @@
+Sha-bang `#!` explained 
+Execute a command without saving it in the history
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
