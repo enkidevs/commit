54cd1c4b2678cc48e7f918fd0deda413b8c923bc
DrShavargo <gassi@teldio.com>
2016-07-04 14:16:55 +0100

    2016-07-04's javascript workout

---
+++ DrShavargo/javascript/Functions/2016-07-04.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+IIFE pattern
+Move cursor at the end of text input
+Social sharing without widgets
+Check if a document is done loading
