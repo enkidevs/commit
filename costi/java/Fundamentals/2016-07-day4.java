4b36cc8f5ed450efb5de77b5d9b06129ee141eb8
costi <assaf2b@gmail.com>
2016-07-04 09:41:50 +0100

    2016-07-day4's java workout

---
+++ costi/java/Fundamentals/2016-07-day4.java
@@ -0,0 +1,5 @@
+Cropping images
+Ternary operator
+Distinction between `public` and `private` methods
+Updating interfaces by using `default` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
