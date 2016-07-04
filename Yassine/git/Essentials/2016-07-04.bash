f31d7b73b6e93289a5a0e7274cf724235d40cf0c
Yassine <r.yassine42@gmail.com>
2016-07-04 23:51:32 +0100

    2016-07-04's git workout

---
+++ Yassine/git/Essentials/2016-07-04.bash
@@ -0,0 +1,5 @@
+The three states in git
+Search for commits by author
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
