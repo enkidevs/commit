84d1bed15755f4e82dc0726d6f249f4df00dca8c
bhav97 <anubhav7991@gmail.com>
2016-07-04 05:26:31 +0100

    2016-07-day3's linux workout

---
+++ bhav97/linux/System Management/2016-07-day3.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Executing scripts on system boot
