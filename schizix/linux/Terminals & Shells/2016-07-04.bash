d63c2870b2e217c7755f6eba9963cea9fbe02ae7
schizix <atodorovic@gmail.com>
2016-07-04 17:13:18 +0100

    2016-07-04's linux workout

---
+++ schizix/linux/Terminals & Shells/2016-07-04.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
