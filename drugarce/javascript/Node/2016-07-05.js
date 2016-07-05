d1c423676d35ce67780bac344afeabb95c5e5725
drugarce <drugarce@mt.net.mk>
2016-07-05 22:45:46 +0100

    2016-07-05's javascript workout

---
+++ drugarce/javascript/Node/2016-07-05.js
@@ -0,0 +1,5 @@
+Streams in **Node**
+Use conventional property names for error handling in Node.js
+Bulk-Write in **Node** with `cork()`
+Middleware/pipeline design pattern
+Avoid using `sudo` when running Node
