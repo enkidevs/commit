cad094c46c427966885e4921daa1502a64d8206d
costi <assaf2b@gmail.com>
2016-07-05 08:39:24 +0100

    2016-07-05's java workout

---
+++ costi/java/I/O/2016-07-05.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Using buffered streams
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
