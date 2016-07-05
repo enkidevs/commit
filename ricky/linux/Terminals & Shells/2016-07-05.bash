51287d99d2a3e4d0074d36d28177aeb02131fc16
ricky <riccardotonini@gmail.com>
2016-07-05 06:16:44 +0100

    2016-07-05's linux workout

---
+++ ricky/linux/Terminals & Shells/2016-07-05.bash
@@ -0,0 +1,5 @@
+Sha-bang `#!` explained 
+Execute a command without saving it in the history
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
