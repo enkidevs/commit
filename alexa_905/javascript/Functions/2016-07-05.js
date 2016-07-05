3cbd796d613ad0835bcd025218d1c55374768fd7
alexa_905 <alexeenkovatanya@gmail.com>
2016-07-05 09:53:38 +0100

    2016-07-05's javascript workout

---
+++ alexa_905/javascript/Functions/2016-07-05.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Functions can be declared after use
+Method chaining
+Easily generate a random `HEX` color
