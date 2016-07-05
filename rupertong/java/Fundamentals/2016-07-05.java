14a5463afacd2221a914d119fab2f190d87a0d6b
rupertong <rupert.ong@gmail.com>
2016-07-05 14:26:03 +0100

    2016-07-05's java workout

---
+++ rupertong/java/Fundamentals/2016-07-05.java
@@ -0,0 +1,5 @@
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
