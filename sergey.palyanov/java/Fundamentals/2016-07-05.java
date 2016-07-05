4b974f9b4515b4f571017c799bff7245a687f9c1
sergey.palyanov <sergey.palyanov@gmail.com>
2016-07-05 11:45:17 +0100

    2016-07-05's java workout

---
+++ sergey.palyanov/java/Fundamentals/2016-07-05.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using Inheritance to reduce code repetition
+Using `synchronized` statements
+Methods performing *Security Checks* must be declared `Private` or `Final`
+How range operations work
