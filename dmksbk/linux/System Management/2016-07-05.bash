f3f47397f0054aacaba6ea838e3f34175ca9a518
dmksbk <dmksbk@gmail.com>
2016-07-05 06:19:16 +0100

    2016-07-05's linux workout

---
+++ dmksbk/linux/System Management/2016-07-05.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
