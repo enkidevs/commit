15203bf7705d73751b905f56a667485c8e853cbe
punkaid <punkaid@gmail.com>
2016-07-05 13:11:07 +0100

    2016-07-05's linux workout

---
+++ punkaid/linux/File Management/2016-07-05.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Truncate files with `cat` instead of `rm -rf`
+Extracting `tar` files to a specific directory
+Working with `.gz` files
