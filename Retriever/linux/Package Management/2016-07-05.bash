7d9dd2a26fb9cad33c3e96ed78e761ea673f740b
Retriever <cal-f@hotmail.com>
2016-07-05 18:50:29 +0100

    2016-07-05's linux workout

---
+++ Retriever/linux/Package Management/2016-07-05.bash
@@ -0,0 +1,6 @@
+Install specific version of a package on debian based systems
+Backporting on Debian
+
+Installing a `.deb` package from the terminal
+List available packages in RPM based systems
+Keeping repos updated with `cron`
