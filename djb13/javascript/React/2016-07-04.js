4fde9167823791d986edff2f541bf065bb0d498e
djb13 <dipta.basu@gmail.com>
2016-07-05 02:54:23 +0100

    2016-07-04's javascript workout

---
+++ djb13/javascript/React/2016-07-04.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+Pixel values in style props for **React**
+Using `false` in **JSX**
+Expose Component Functions in **React**
+Immutability helpers in **React**
