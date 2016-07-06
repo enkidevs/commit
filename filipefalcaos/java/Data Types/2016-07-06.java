317807f2323a28c61f0d181d466ac8f06c6a3dc4
filipefalcaos <filipefalcaosantos@gmail.com>
2016-07-06 14:14:29 +0100

    2016-07-06's java workout

---
+++ filipefalcaos/java/Data Types/2016-07-06.java
@@ -0,0 +1,5 @@
+`ArrayList` vs `LinkedList`
+Use primitive types instead of wrapper classes when possible
+Double brace initialization
+String concatenation in Java
+Do not attempt comparisons with NaN
