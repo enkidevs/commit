5365d1c01c5fdae2ffdd2c7ce1aa159d651a3794
mtamazlicaru <mariustamazlicaru@gmail.com>
2016-07-06 22:36:36 +0100

    2016-07-06's linux workout

---
+++ mtamazlicaru/linux/Terminals & Shells/2016-07-06.bash
@@ -0,0 +1,5 @@
+Quick `bash` shortcuts 
+Terminal based browser
+Useful aliasing in bash
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
