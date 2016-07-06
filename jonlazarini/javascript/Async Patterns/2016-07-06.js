94acb6946d7519989596d725932b1a2034f2126c
jonlazarini <jonathan.lazarini@wanadoo.fr>
2016-07-06 20:34:02 +0100

    2016-07-06's javascript workout

---
+++ jonlazarini/javascript/Async Patterns/2016-07-06.js
@@ -0,0 +1,5 @@
+Name your callback functions to avoid 'callback hell'
+Transform values into promises
+Error-first Callbacks
+The difference between _parallel_, _asynchronous_ and _concurrent_
+Using timers with extra arguments
