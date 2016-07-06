d6e7b41987a3717c533032e0e9e66d42b7fc2eb9
Ronvis <ronvisbord@gmail.com>
2016-07-06 12:12:49 +0100

    2016-07-06's linux workout

---
+++ Ronvis/linux/Terminals & Shells/2016-07-06.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in the history
+Find any Unix / Linux command
+Terminal based browser
+Useful aliasing in bash
+Keep useful commands in your shell history with tags
