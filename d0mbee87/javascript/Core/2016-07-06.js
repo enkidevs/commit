09ad14f4a374eef2ec0c473b02ee8dbee50c3063
d0mbee87 <d0mbee87@gmail.com>
2016-07-06 18:39:38 +0100

    2016-07-06's javascript workout

---
+++ d0mbee87/javascript/Core/2016-07-06.js
@@ -0,0 +1,5 @@
+Shuffle an array using `sort` and `random`.
+Get a random item from an Array
+Use `splice()` to remove an item from an array
+Infix operators are left-associative
+The difference between null, undefined and NaN
