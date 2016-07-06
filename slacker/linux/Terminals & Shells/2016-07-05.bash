4997f831ea2255f7d72b5863a393ca14431fc33e
slacker <happyslacking@gmail.com>
2016-07-06 12:03:15 +0100

    2016-07-05's linux workout

---
+++ slacker/linux/Terminals & Shells/2016-07-05.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Random password generator
+Sequential execution using the `;` statement separator
