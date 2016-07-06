34b337a69adc6e2e278b353c2820ddd7a2a5f92f
kcinimod <dominick.hqe@gmail.com>
2016-07-06 08:50:28 +0100

    2016-07-05's linux workout

---
+++ kcinimod/linux/Networking/2016-07-05.bash
@@ -0,0 +1,5 @@
+Find services running on your host
+Get real network statistics with `slurm`
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
