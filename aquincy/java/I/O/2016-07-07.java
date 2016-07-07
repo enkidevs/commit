f14adfee0921e77e566ed2fcaae296d87ccbf5a2
aquincy <brennanjl003@gmail.com>
2016-07-07 20:57:43 +0100

    2016-07-07's java workout

---
+++ aquincy/java/I/O/2016-07-07.java
@@ -0,0 +1,5 @@
+How to test if file or directory exists in Java
+Reading and writing text files
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
+The `Console` class
