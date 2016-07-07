6db1e157d7a1ff4b8cf49711a836f989ea45f986
edgenard <edgenard@gmail.com>
2016-07-07 23:20:39 +0100

    2016-07-07's javascript workout

---
+++ edgenard/javascript/React/2016-07-07.js
@@ -0,0 +1,5 @@
+Self-closing tags in **JSX**
+`null` value for *Controlled Components* in **React**
+Expose Component Functions in **React**
+`PureRenderMixin` in **React**
+Keyed fragments in **React**
