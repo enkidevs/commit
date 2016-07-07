f1acb54ed6e20b773c6c5fdfaa08bb7afb803653
itsssrk <b984cec3@opayq.com>
2016-07-07 17:21:32 +0100

    2016-07-07's java workout

---
+++ itsssrk/java/Fundamentals/2016-07-07.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
