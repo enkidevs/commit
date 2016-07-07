2d332873ca4ae7cca644f5b5e260a349e4afd5d9
chaimann <nutaksho@gmail.com>
2016-07-07 10:15:58 +0100

    2016-07-07's linux workout

---
+++ chaimann/linux/Terminals & Shells/2016-07-07.bash
@@ -0,0 +1,7 @@
+Sha-bang `#!` explained 
+Random password generator
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
