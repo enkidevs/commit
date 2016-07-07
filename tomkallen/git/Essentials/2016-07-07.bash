efb60e217a363ff76843717b748df042165456db
tomkallen <comradebykoff@gmail.com>
2016-07-07 08:46:40 +0100

    2016-07-07's git workout

---
+++ tomkallen/git/Essentials/2016-07-07.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+The three states in git
+Check the status of your files
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
