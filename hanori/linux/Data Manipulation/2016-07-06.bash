4c451453ff5047da0c5892a46872c7cd63db230b
hanori <hanori@gmail.com>
2016-07-07 14:17:41 +0100

    2016-07-06's linux workout

---
+++ hanori/linux/Data Manipulation/2016-07-06.bash
@@ -0,0 +1,5 @@
+Short intro to `grep`
+Count non-empty lines with `nl` utility
+Transforming Text With `sed`
+Selecting Portions of A Line Using `cut`
+The AWK Programming Language And `awk` Command
