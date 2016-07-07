0407c070434eb779485111134e909b56808454fe
mic-css <mic.cassano@gmail.com>
2016-07-07 11:00:05 +0100

    2016-07-07's javascript workout

---
+++ mic-css/javascript/React/2016-07-07.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+Functional Stateless Components in React
+Pixel values in style props for **React**
+Expose Component Functions in **React**
+Dealing with `this.props.children`
