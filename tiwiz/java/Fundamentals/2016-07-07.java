eccf596d89d7588d90797c99d91b5fc98965538f
tiwiz <roberto.orgiu@gmail.com>
2016-07-07 18:04:53 +0100

    2016-07-07's java workout

---
+++ tiwiz/java/Fundamentals/2016-07-07.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Cropping images
+Distinction between `public` and `private` methods
+Using `Printstream.format` to format a string before printing
+Using `synchronized` statements
