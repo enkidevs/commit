b542cb5252ea27b83191b2bf6d88769f28065a13
jholl <jacobhollenbeck@u.boisestate.edu>
2016-07-07 15:55:46 +0100

    2016-07-07's java workout

---
+++ jholl/java/I/O/2016-07-07.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to list all files in a directory that match a filename extension in Java
+Create system file paths using the `Path` class
+Checking for the existence of files and directories
+The `Console` class
