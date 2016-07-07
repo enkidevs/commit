518c670fb925ad961786f40a79bf9eb8fbc38a69
kcmartin <kcmartin2@mac.com>
2016-07-07 18:17:42 +0100

    2016-07-07's git workout

---
+++ kcmartin/git/Tools/2016-07-07.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Interactively unstage changes
+Specify a range of commits using double dot syntax
+Show history of a function
+Debug using binary search
