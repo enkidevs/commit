c95408dac9c2955fa672a95a4e56d58ed77fd14f
shruti.srivstv <shruti.cs17@gmail.com>
2016-07-07 14:01:37 +0100

    2016-07-07's linux workout

---
+++ shruti.srivstv/linux/System Management/2016-07-07.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
