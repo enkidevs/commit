4ebf50d8f1c9560ee8df3239779bab1fb46d3ab5
wi4vis <wi4vis@live.ru>
2016-07-07 19:46:33 +0100

    2016-07-07's linux workout

---
+++ wi4vis/linux/Data Manipulation/2016-07-07.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+Short intro to `grep`
+Getting rid of duplicate lines
+Count non-empty lines with `nl` utility
+Clean configuration files using `sed`
