1244964bef03ad3751055af5f1c970fca5be17d1
mositis <mositis@yahoo.com>
2016-07-07 15:42:19 +0100

    2016-07-07's java workout

---
+++ mositis/java/Fundamentals/2016-07-07.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
