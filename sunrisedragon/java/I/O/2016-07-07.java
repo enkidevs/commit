756a289c58b961e99bdfa4e39a10f48713ba7cc6
sunrisedragon <highcomunity@gmail.com>
2016-07-07 07:37:48 +0100

    2016-07-07's java workout

---
+++ sunrisedragon/java/I/O/2016-07-07.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Deleting files and directories
