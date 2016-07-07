e9d73b3e1aa3593f4d8c0762850e513e3bbccf81
bofink <jim.saari97@outlook.com>
2016-07-07 17:44:44 +0100

    2016-07-07's javascript workout

---
+++ bofink/javascript/Functions/2016-07-07.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Move cursor at the end of text input
+Social sharing without widgets
