879739c00a7a57beaf07fc88d57e71ca110e9a44
Tyzanol <daena1303@gmail.com>
2016-07-09 00:04:51 +0100

    2016-07-08's java workout

---
+++ Tyzanol/java/Fundamentals/2016-07-08.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Do not return *references* to private *mutable* class members
