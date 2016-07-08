288315771049e00bddc07fe7d156afc63a3eae57
wirtaw <float45@gmail.com>
2016-07-08 05:10:14 +0100

    2016-07-08's javascript workout

---
+++ wirtaw/javascript/Node/2016-07-08.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Asynchronous File Write/Read in Node.js
+Handle `JSON.parse` error in Node.js
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
