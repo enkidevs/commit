2e2defe222a9ad5dd5b2cd68538266e54e3d4674
trizzle21 <tdross21@gmail.com>
2016-07-08 19:03:12 +0100

    2016-07-08's linux workout

---
+++ trizzle21/linux/Terminals & Shells/2016-07-08.bash
@@ -0,0 +1,5 @@
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
