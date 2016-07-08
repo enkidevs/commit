67fbb0decd23403dd0d3ecf8025a2a6480655624
stonean <andrew@stonean.com>
2016-07-08 15:14:45 +0100

    2016-07-08's linux workout

---
+++ stonean/linux/Terminals & Shells/2016-07-08.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in the history
+Conditional command execution
+
+(`&&` operator)
+The Basics of Environment Variables
