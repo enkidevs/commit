68c1b2affffd677b12044c7c000006e33d6db6b8
tylerthorne <tt.solarboltproductions@gmail.com>
2016-07-08 20:41:18 +0100

    2016-07-08's java workout

---
+++ tylerthorne/java/I/O/2016-07-08.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to test if file or directory exists in Java
+How to list all files in a directory that match a filename extension in Java
+Reading and writing text files
+The `Console` class
