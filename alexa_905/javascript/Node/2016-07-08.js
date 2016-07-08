fcbffc08961757e5d6b1ae5b589b032e1314d9ed
alexa_905 <alexeenkovatanya@gmail.com>
2016-07-08 20:19:48 +0100

    2016-07-08's javascript workout

---
+++ alexa_905/javascript/Node/2016-07-08.js
@@ -0,0 +1,5 @@
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
