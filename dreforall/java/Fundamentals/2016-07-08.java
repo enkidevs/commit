f559e3d7def31cdf93ebe3c485c1ee49ee66347f
dreforall <ravel.marques@gmail.com>
2016-07-08 14:52:22 +0100

    2016-07-08's java workout

---
+++ dreforall/java/Fundamentals/2016-07-08.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
+Methods performing *Security Checks* must be declared `Private` or `Final`
