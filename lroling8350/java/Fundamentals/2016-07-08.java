e0ab75e43180dbe7af362fa84bc32678e58241cd
lroling8350 <lroling8350@gmail.com>
2016-07-08 13:52:32 +0100

    2016-07-08's java workout

---
+++ lroling8350/java/Fundamentals/2016-07-08.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
