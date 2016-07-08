e246ac9badd666af3e8a632f7aaef3b4852edd27
Retriever <cal-f@hotmail.com>
2016-07-08 23:23:14 +0100

    2016-07-08's linux workout

---
+++ Retriever/linux/File Management/2016-07-08.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm -rf`
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
