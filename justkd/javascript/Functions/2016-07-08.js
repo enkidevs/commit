9b50bd0bc11b2a13cee7597a8c32e04925b3e833
justkd <kdegani@gmail.com>
2016-07-08 11:28:50 +0100

    2016-07-08's javascript workout

---
+++ justkd/javascript/Functions/2016-07-08.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Get query/url variables
+Check if a document is done loading
