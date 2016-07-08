731a9e33eb92d56cab0095fce52bb88c4ca951af
tremerhl <tremerhl@gmail.com>
2016-07-08 15:58:07 +0100

    2016-07-08's java workout

---
+++ tremerhl/java/Fundamentals/2016-07-08.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Cropping images
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
