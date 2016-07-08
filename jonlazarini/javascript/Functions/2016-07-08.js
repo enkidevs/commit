8871efa4a838e32187aa908d36a7069a6749761d
jonlazarini <jonathan.lazarini@wanadoo.fr>
2016-07-08 14:45:11 +0100

    2016-07-08's javascript workout

---
+++ jonlazarini/javascript/Functions/2016-07-08.js
@@ -0,0 +1,5 @@
+Access a function's `arguments` as an array
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+`.bind()` 
+Move cursor at the end of text input
