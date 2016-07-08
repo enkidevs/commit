8fef5b4ba084b37123189ddc359198fd33136079
garytxo <garytxo@gmail.com>
2016-07-08 06:41:14 +0100

    2016-07-08's java workout

---
+++ garytxo/java/Fundamentals/2016-07-08.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+The trade-off between short and descriptive names
+Use `DecimalFormat` class to format numbers
+Methods performing *Security Checks* must be declared `Private` or `Final`
