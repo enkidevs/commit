64605017682fd4b760dbe21caa3cde37cf3b148c
seans66 <seanstreet@hotmail.com>
2016-07-08 03:09:54 +0100

    2016-07-07's linux workout

---
+++ seans66/linux/Terminals & Shells/2016-07-07.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in the history
+Clear bash history
+Useful aliasing in bash
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
