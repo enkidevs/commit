56620c178d66f2158c4696c44d1f12dfdff1deb9
bebe <bb.benjamin.benz@gmail.com>
2016-07-08 07:51:03 +0100

    2016-07-08's java workout

---
+++ bebe/java/Fundamentals/2016-07-08.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
