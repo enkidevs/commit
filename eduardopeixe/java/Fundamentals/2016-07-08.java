ac85e6fc1c9f4c655bad2cfb5cbcf97db3ce0c3b
eduardopeixe <walthfish@yahoo.com>
2016-07-08 15:47:08 +0100

    2016-07-08's java workout

---
+++ eduardopeixe/java/Fundamentals/2016-07-08.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
