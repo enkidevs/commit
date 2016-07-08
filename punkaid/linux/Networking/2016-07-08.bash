7beb4b96b8fd7c80f79f04101f0d5a06c25930a0
punkaid <punkaid@gmail.com>
2016-07-08 14:44:34 +0100

    2016-07-08's linux workout

---
+++ punkaid/linux/Networking/2016-07-08.bash
@@ -0,0 +1,5 @@
+ Intro to `iptables`
+Get real network statistics with `slurm`
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
+Preventing ssh from timing out
