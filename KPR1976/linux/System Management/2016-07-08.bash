2ae978bf99a1e6af9658a0804cf82688cf46350f
KPR1976 <pkinyapin+enki@gmail.com>
2016-07-08 06:57:06 +0100

    2016-07-08's linux workout

---
+++ KPR1976/linux/System Management/2016-07-08.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Executing scripts on system boot
