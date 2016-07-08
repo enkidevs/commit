4eebdcd249cacbc58ba9e667c3e713c104133c74
wux1992 <wux1992@126.com>
2016-07-08 12:44:59 +0100

    2016-07-08's java workout

---
+++ wux1992/java/Fundamentals/2016-07-08.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Connecting to FTP using Java
+Using Inheritance to reduce code repetition
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
