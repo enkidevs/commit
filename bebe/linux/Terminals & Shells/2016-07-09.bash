e44158b84f570a4a2ad5b1ba47312638cfb80cba
bebe <bb.benjamin.benz@gmail.com>
2016-07-09 15:25:34 +0100

    2016-07-09's linux workout

---
+++ bebe/linux/Terminals & Shells/2016-07-09.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
