31436edde4d41508184a6b7fd84c4c85eb6f1d84
nowsthethyme <eli.thyme@yahoo.com>
2016-07-09 17:13:14 +0100

    2016-07-09's java workout

---
+++ nowsthethyme/java/Error Handling/2016-07-09.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Converting stack trace to a string
+Creating new Exceptions
+Handling Null Pointer Exceptions
+Most common reason behind **stack overflow** error
