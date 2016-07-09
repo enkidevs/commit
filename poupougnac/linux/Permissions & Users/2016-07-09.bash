76c379c4238f4b68d7d8eb81328e385473e6fc33
poupougnac <pierre@digitevent.net>
2016-07-09 08:57:48 +0100

    2016-07-09's linux workout

---
+++ poupougnac/linux/Permissions & Users/2016-07-09.bash
@@ -0,0 +1,5 @@
+The `setuid` permission
+Executing commands with `sudo` without password
+Making a file unalterable with `chattr`
+Get file permissions in octal form
+Running a command as another local user
