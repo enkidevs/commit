33fc61c84dc5b14a75fbbdbd1f500d9e3e77c73b
segfault <thibaut.constant92@gmail.com>
2016-07-09 18:19:35 +0100

    2016-07-09's java workout

---
+++ segfault/java/Error Handling/2016-07-09.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Creating new Exceptions
+Checking a double for `NaN` value
