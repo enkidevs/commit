6816557223614d54189c15cc73b52aa721f9050f
theJoker <roshanpisharody1994@gmail.com>
2016-07-09 07:06:27 +0100

    2016-07-08's java workout

---
+++ theJoker/java/Fundamentals/2016-07-08.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Common mistake on switch statements
+Using `Printstream.format` to format a string before printing
+Locks in `static synchronized` methods
+How range operations work
