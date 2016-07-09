72f9001f81fee536721d8a2896b9443b89ed9563
DaveLosert <david.losert@gmx.de>
2016-07-09 12:23:41 +0100

    2016-07-09's linux workout

---
+++ DaveLosert/linux/Terminals & Shells/2016-07-09.bash
@@ -0,0 +1,5 @@
+Useful aliasing in bash
+Random password generator
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
