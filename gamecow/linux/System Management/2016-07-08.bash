e49125982b778c8d3b3899da9dcd98a6a1735fcc
gamecow <chris.miranda11@gmail.com>
2016-07-09 02:42:51 +0100

    2016-07-08's linux workout

---
+++ gamecow/linux/System Management/2016-07-08.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
