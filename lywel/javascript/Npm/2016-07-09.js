43c1508a64fd6b223037d621fca812d3fecc9c15
lywel <maxime.lewandowski@epita.fr>
2016-07-09 08:51:40 +0100

    2016-07-09's javascript workout

---
+++ lywel/javascript/Npm/2016-07-09.js
@@ -0,0 +1,5 @@
+Create projects with `npm init`
+Bump package version in npm
+List binaries for scripting in npm
+Environmental variables and flags for `npm`
+Lock down dependency versions by shrinkwrapping
