050f1dee765d7db8a2bd169e31ed149e0af46fb4
mclark <mclark4386@gmail.com>
2016-07-09 18:36:39 +0100

    2016-07-09's linux workout

---
+++ mclark/linux/File Management/2016-07-09.bash
@@ -0,0 +1,6 @@
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Working with `.gz` files
+Find the unknown process preventing deleting of files
