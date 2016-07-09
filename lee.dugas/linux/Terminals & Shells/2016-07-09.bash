9954c8f013c2abb11d9d25ef19d3ccc5848cba1e
lee.dugas <olivier.dugas.1@gmail.com>
2016-07-09 20:07:16 +0100

    2016-07-09's linux workout

---
+++ lee.dugas/linux/Terminals & Shells/2016-07-09.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in the history
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
