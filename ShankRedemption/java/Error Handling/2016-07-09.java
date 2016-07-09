994e9e700d0313df7019c56881f7a33122837d6e
ShankRedemption <ssaxen4@illinois.edu>
2016-07-09 15:48:30 +0100

    2016-07-09's java workout

---
+++ ShankRedemption/java/Error Handling/2016-07-09.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using exceptions in Java
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
