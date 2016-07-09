8d28bb337f99fb060d0c19415e66815da7ef370d
trungbnguyen <trung.tmac.nguyen@gmail.com>
2016-07-09 23:49:45 +0100

    2016-07-09's java workout

---
+++ trungbnguyen/java/Fundamentals/2016-07-09.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
