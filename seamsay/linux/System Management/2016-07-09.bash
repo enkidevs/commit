d26f8ffdfb1732b8ec1f30fb8eb700a3f6033892
seamsay <srm.1708@gmail.com>
2016-07-09 08:47:47 +0100

    2016-07-09's linux workout

---
+++ seamsay/linux/System Management/2016-07-09.bash
@@ -0,0 +1,7 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Executing scripts on system boot
+Get hardware stack details with `lspci`
