a4873844e950865f505b45cdde581816293cd7c6
arminius <arminius75@gmail.com>
2016-07-09 09:30:39 +0100

    2016-07-08's javascript workout

---
+++ arminius/javascript/Core/2016-07-08.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Most efficient way to build `HTML` strings
+Prevent background image flicker in **IE**
+Detect an error type
+Inaccuracy of binary floating-point format
