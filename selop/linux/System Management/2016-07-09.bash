b6624564940443b1504531543e48fc5cac5b6204
selop <lopatkin.sergej@googlemail.com>
2016-07-09 08:03:00 +0100

    2016-07-09's linux workout

---
+++ selop/linux/System Management/2016-07-09.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
