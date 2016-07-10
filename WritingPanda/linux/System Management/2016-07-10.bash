3e57e4fda6cb79a88ff37efcb9232911c4dfa598
WritingPanda <omar.quimbaya4@gmail.com>
2016-07-10 15:44:36 +0100

    2016-07-10's linux workout

---
+++ WritingPanda/linux/System Management/2016-07-10.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
