84d4411b0b6f5c100441023a50bef0634cd80e50
chadbowen248 <chadbowen248@gmail.com>
2016-07-10 12:20:25 +0100

    2016-07-10's javascript workout

---
+++ chadbowen248/javascript/Core/2016-07-10.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Hoisting applies only to variable declarations, not initializations
+Apply `map` function to array items
+How to merge two arrays
+Getting the *real* dimensions of an image
