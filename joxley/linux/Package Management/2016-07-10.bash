70cb0a39998f5b4ea8feff4d3d80b3af3455895d
joxley <john.oxley@gmail.com>
2016-07-10 09:56:00 +0100

    2016-07-10's linux workout

---
+++ joxley/linux/Package Management/2016-07-10.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+List installed packages
+Installing a `.deb` package from the terminal
+Prevent updating a specific package in Debian systems
+Keeping repos updated with `cron`
