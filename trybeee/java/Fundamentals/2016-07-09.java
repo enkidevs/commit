50bb1575df1032b5e1d32a65b260c7f35edef181
trybeee <dmitry@virool.com>
2016-07-10 05:07:29 +0100

    2016-07-09's java workout

---
+++ trybeee/java/Fundamentals/2016-07-09.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use `DecimalFormat` class to format numbers
+Do not return *references* to private *mutable* class members
+`Autoboxing` and `Unboxing`
+Supply `toString()` in all classes
