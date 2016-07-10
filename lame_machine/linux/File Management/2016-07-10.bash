e2ad83b2e7b13bee9b528df37493d7d9f4d92cb1
lame_machine <stanislav.polishchuk@gmail.com>
2016-07-10 15:35:35 +0100

    2016-07-10's linux workout

---
+++ lame_machine/linux/File Management/2016-07-10.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Working with `.gz` files
