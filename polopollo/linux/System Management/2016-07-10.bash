234c9c72bc743683cfbbac354442e97263b69b2c
polopollo <paul.vayssiere@gmail.com>
2016-07-10 16:04:23 +0100

    2016-07-10's linux workout

---
+++ polopollo/linux/System Management/2016-07-10.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Using `dd` To Create Disk Images
