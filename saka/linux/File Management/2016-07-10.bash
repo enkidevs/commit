e27facf72f5a4dc769566f24d08ad118def340fd
saka <saka71996@gmail.com>
2016-07-10 06:04:17 +0100

    2016-07-10's linux workout

---
+++ saka/linux/File Management/2016-07-10.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Deleting non-empty directories
