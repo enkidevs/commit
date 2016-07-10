b60b0b95a8c2fa26026f5d1a586a0a56948a7d97
encore <kingkonig@gmail.com>
2016-07-10 20:18:51 +0100

    2016-07-10's linux workout

---
+++ encore/linux/Terminals & Shells/2016-07-10.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in the history
+Aliasing ssh connections
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
