e65ae7654bf204a3b1f5c7086bebabd13396a6fd
2fiks <tofiksa@gmail.com>
2016-07-10 09:07:06 +0100

    2016-07-10's linux workout

---
+++ 2fiks/linux/File Management/2016-07-10.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm -rf`
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Using `touch` to alter files modification time
