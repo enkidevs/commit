450f1395172e83e11c29e5bbab3b1e6835ee8768
exo_duz <exo_duz@yahoo.com>
2016-07-11 00:41:21 +0100

    2016-07-10's javascript workout

---
+++ exo_duz/javascript/React/2016-07-10.js
@@ -0,0 +1,5 @@
+Functional Stateless Components in React
+Pixel values in style props for **React**
+Expose Component Functions in **React**
+`PureRenderMixin` in **React**
+Clone Elements in **React**
