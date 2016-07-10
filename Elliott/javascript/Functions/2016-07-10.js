ec7bcd0fd3e6b26b0626f61ad253d36c2a3deadd
Elliott <elliott.leclaire@gmail.com>
2016-07-11 00:46:22 +0100

    2016-07-10's javascript workout

---
+++ Elliott/javascript/Functions/2016-07-10.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+IIFE pattern
+Social sharing without widgets
