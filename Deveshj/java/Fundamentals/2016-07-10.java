783f487e7f5259de9ecefc89fdc66599aa9fece2
Deveshj <deveshj4@gmail.com>
2016-07-11 00:22:28 +0100

    2016-07-10's java workout

---
+++ Deveshj/java/Fundamentals/2016-07-10.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
