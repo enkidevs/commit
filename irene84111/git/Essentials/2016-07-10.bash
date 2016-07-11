9f6ca13f7d2bdebf1890c7475c22b0eeb8e5ed2c
irene84111 <irene84111@gmail.com>
2016-07-11 03:18:07 +0100

    2016-07-10's git workout

---
+++ irene84111/git/Essentials/2016-07-10.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+Make your log output pretty
+Check the status of your files
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
