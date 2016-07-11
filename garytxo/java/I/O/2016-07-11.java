fd9f5daf9866dc573bd3c8af6aead68a0b2d1fd3
garytxo <garytxo@gmail.com>
2016-07-11 07:08:48 +0100

    2016-07-11's java workout

---
+++ garytxo/java/I/O/2016-07-11.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+How to test if file or directory exists in Java
+Translating `Scanner` tokens into primitive types
+List contents of a directory with `newDirectoryStream`
