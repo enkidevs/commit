eb8810839e826240a023be41ba2713f9806a3ebe
moggyboy <moggyboy@gmail.com>
2016-07-11 23:38:56 +0100

    2016-07-11's linux workout

---
+++ moggyboy/linux/File Management/2016-07-11.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
