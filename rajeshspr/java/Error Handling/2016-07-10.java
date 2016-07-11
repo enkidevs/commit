4cc5b8fe8c0a0bb0e7523103cd38c1ae9fc38a30
rajeshspr <rajesh.sp.cts@gmail.com>
2016-07-11 14:50:13 +0100

    2016-07-10's java workout

---
+++ rajeshspr/java/Error Handling/2016-07-10.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Prevent `exceptions` while logging Data
