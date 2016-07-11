375a0be99b810b779508cf3d18d232a21fe0de27
pnama <palani.nama@gmail.com>
2016-07-11 07:29:20 +0100

    2016-07-10's java workout

---
+++ pnama/java/Fundamentals/2016-07-10.java
@@ -0,0 +1,5 @@
+Use meaningful names
+The trade-off between short and descriptive names
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Implementing an `interface`
