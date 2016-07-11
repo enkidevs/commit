6dea88836de640c8beb9ebed118df488c20aeed5
kcmartin <kcmartin2@mac.com>
2016-07-11 14:26:01 +0100

    2016-07-11's javascript workout

---
+++ kcmartin/javascript/React/2016-07-11.js
@@ -0,0 +1,5 @@
+Self-closing tags in **JSX**
+`null` value for *Controlled Components* in **React**
+Expose Component Functions in **React**
+`PureRenderMixin` in **React**
+Immutability helpers in **React**
