b83f01499847bdc7ac97f2739963702d599896fc
boris-ulyanov <bu0507870@gmail.com>
2016-07-11 17:05:41 +0100

    2016-07-11's linux workout

---
+++ boris-ulyanov/linux/File Management/2016-07-11.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Working with `.gz` files
+Find the unknown process preventing deleting of files
