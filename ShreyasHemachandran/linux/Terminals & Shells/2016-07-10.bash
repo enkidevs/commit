4464a05064ccfc9716f3ec15ea03007cab9c36e7
ShreyasHemachandran <shreyas.hemachandran@gmail.com>
2016-07-11 02:41:14 +0100

    2016-07-10's linux workout

---
+++ ShreyasHemachandran/linux/Terminals & Shells/2016-07-10.bash
@@ -0,0 +1,7 @@
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
