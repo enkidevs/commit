47978093cf8c1f24e62d2c854d2ac3c8d3510435
mbchoa <mbchoa@gmail.com>
2016-07-11 17:47:21 +0100

    2016-07-11's linux workout

---
+++ mbchoa/linux/System Management/2016-07-11.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
