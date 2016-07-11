f7c275b3d7beef22216cabdc0f64fd1c4217a358
tristan971 <tristandeloche@gmail.com>
2016-07-11 07:59:00 +0100

    2016-07-11's java workout

---
+++ tristan971/java/Fundamentals/2016-07-11.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+How to play a sound file in Java
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+Methods performing *Security Checks* must be declared `Private` or `Final`
