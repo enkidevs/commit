0e4864a204122e5f401a059febbb3b5edb8087f8
estebanrules <damian.esteban@gmail.com>
2016-07-12 00:13:10 +0100

    2016-07-11's javascript workout

---
+++ estebanrules/javascript/Node/2016-07-11.js
@@ -0,0 +1,5 @@
+Http Server in **Node**
+Global Objects and Environment Variables in **Node**
+The built-in Node debugger
+`uncaughtException` listener in Node.js
+Middleware/pipeline design pattern
