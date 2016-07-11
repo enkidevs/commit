ea4a078cf07984cf98442520a6085aac668e838b
beta <tag@adnm.ch>
2016-07-11 02:39:51 +0100

    2016-07-11's javascript workout

---
+++ beta/javascript/Functions/2016-07-11.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Anonymous functions
+Method chaining
+Move cursor at the end of text input
