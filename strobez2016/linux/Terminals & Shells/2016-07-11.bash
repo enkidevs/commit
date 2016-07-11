b0d6d45e435d2c43348f354340e534d880942e8e
strobez2016 <cwmbranmathstutor@gmail.com>
2016-07-12 00:03:44 +0100

    2016-07-11's linux workout

---
+++ strobez2016/linux/Terminals & Shells/2016-07-11.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Random password generator
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
