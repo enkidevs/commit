789a72f855a89045f8dc341368fa337af45e9b3d
sattel <aturubar@gmail.com>
2016-07-12 21:47:28 +0100

    2016-07-12's linux workout

---
+++ sattel/linux/Package Management/2016-07-12.bash
@@ -0,0 +1,5 @@
+List installed packages
+Installing a `.deb` package from the terminal
+Prevent updating a specific package in Debian systems
+Downgrade a package via `apt-get`
+Keeping repos updated with `cron`
