50c398f18bb1707619feeef9c639959d3d0f33aa
onozy <onozawa2106@gmail.com>
2016-07-13 00:46:13 +0100

    2016-07-12's javascript workout

---
+++ onozy/javascript/Functions/2016-07-12.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Get query/url variables
+Social sharing without widgets
+Check if a document is done loading
