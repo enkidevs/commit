6fe927764647f143a27443456e382fa7af35bc67
dgrant <dagrant82@gmail.com>
2016-07-12 14:08:25 +0100

    2016-07-12's java workout

---
+++ dgrant/java/Error Handling/2016-07-12.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using exceptions in Java
+Handling Null Pointer Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
