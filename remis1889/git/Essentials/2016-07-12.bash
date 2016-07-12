20c6643fb54aec65d91bb8aa15cd7f80b672817d
remis1889 <bluerose1889@gmail.com>
2016-07-12 07:03:57 +0100

    2016-07-12's git workout

---
+++ remis1889/git/Essentials/2016-07-12.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to unmodify a modified file
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
+Perform a dry run
