83b942baf2ec6deb4a13543166ada454ad7a900b
Gelassen <dmitrii.kazakov@gmail.com>
2016-07-12 02:55:07 +0100

    2016-07-11's java workout

---
+++ Gelassen/java/Fundamentals/2016-07-11.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+How to play a sound file in Java
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
