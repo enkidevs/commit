48ed679c4c2f63f8701a7be8e6bde144c0c1cd8a
paulkarayan <paulkarayan@gmail.com>
2016-07-12 02:21:22 +0100

    2016-07-11's linux workout

---
+++ paulkarayan/linux/System Management/2016-07-11.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
