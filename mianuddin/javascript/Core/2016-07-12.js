cc63f9ae860fb8ce16c692c0f6ce770209d56834
mianuddin <mianuddin@gmail.com>
2016-07-12 15:46:53 +0100

    2016-07-12's javascript workout

---
+++ mianuddin/javascript/Core/2016-07-12.js
@@ -0,0 +1,5 @@
+Using assignment operators
+Truthiness
+Hoisting applies only to variable declarations, not initializations
+Prevent background image flicker in **IE**
+Round numbers to `N` decimals 
