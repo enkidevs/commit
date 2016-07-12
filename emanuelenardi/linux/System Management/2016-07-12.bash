b78c940142c9e88268c2bd64de7f73f2e82e64c6
emanuelenardi <nardiemanuele6@gmail.com>
2016-07-12 08:44:53 +0100

    2016-07-12's linux workout

---
+++ emanuelenardi/linux/System Management/2016-07-12.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Basic service management with `systemd`
