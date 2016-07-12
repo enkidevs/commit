ff2a5c793f06b4773835a5ce810580a6a13d73bc
cdaloisio <chris.daloisio@gmail.com>
2016-07-12 13:54:00 +0100

    2016-07-11's linux workout

---
+++ cdaloisio/linux/Terminals & Shells/2016-07-11.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Find any Unix / Linux command
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
