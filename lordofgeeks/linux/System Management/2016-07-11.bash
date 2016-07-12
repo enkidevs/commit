58cd27150f44b4483c0414bd5df2459816af060b
lordofgeeks <xtechbizsub@gmail.com>
2016-07-12 07:53:59 +0100

    2016-07-11's linux workout

---
+++ lordofgeeks/linux/System Management/2016-07-11.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
