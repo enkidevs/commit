85ec395d8094955a96181fb3edf069381e4a55ea
fffl <kuepfer.christopher@gmail.com>
2016-07-12 16:50:00 +0100

    2016-07-12's java workout

---
+++ fffl/java/Fundamentals/2016-07-12.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Using Inheritance to reduce code repetition
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
