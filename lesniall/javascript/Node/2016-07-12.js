85c4d6e997083268d1e9444cfd029af76e68c033
lesniall <nialljoemaher@gmail.com>
2016-07-12 05:44:30 +0100

    2016-07-12's javascript workout

---
+++ lesniall/javascript/Node/2016-07-12.js
@@ -0,0 +1,5 @@
+First-error callbacks in **Node**
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
