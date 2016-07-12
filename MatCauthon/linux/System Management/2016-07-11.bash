7bf317be8e93eaccb7f57aef2eacd8deb2555e7f
MatCauthon <matthew.metzger131@gmail.com>
2016-07-12 03:53:40 +0100

    2016-07-11's linux workout

---
+++ MatCauthon/linux/System Management/2016-07-11.bash
@@ -0,0 +1,5 @@
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
