af58e2472ad2f1ce62567965488a6f419df574b3
dreforall <ravel.marques@gmail.com>
2016-07-12 18:17:53 +0100

    2016-07-12's java workout

---
+++ dreforall/java/Fundamentals/2016-07-12.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
