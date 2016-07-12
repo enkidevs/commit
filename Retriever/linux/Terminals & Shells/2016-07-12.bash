0d8ebd273002a3dcef349fc42f05e2f8f6b21cc3
Retriever <cal-f@hotmail.com>
2016-07-12 16:07:44 +0100

    2016-07-12's linux workout

---
+++ Retriever/linux/Terminals & Shells/2016-07-12.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Clear the terminal instantly
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
