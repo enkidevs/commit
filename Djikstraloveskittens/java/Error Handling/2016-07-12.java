5386559c59eb66aa3df3d1dacda4b665ef1c3bcd
Djikstraloveskittens <djikstra.loves.kittens@gmail.com>
2016-07-12 06:41:45 +0100

    2016-07-12's java workout

---
+++ Djikstraloveskittens/java/Error Handling/2016-07-12.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Prevent `exceptions` while logging Data
+Checking a double for `NaN` value
