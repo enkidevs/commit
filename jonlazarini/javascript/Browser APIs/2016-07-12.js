67ffa8c1068303a9778864b0ea66e475d2f36520
jonlazarini <jonathan.lazarini@wanadoo.fr>
2016-07-12 09:54:20 +0100

    2016-07-12's javascript workout

---
+++ jonlazarini/javascript/Browser APIs/2016-07-12.js
@@ -0,0 +1,4 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+Interaction with the user
+Allow an element to go fullscreen
