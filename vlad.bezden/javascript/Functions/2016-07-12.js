13baab0c8fb90c3be7a04d36cee7879a178e764e
vlad.bezden <vladbezden@yahoo.com>
2016-07-12 11:35:41 +0100

    2016-07-12's javascript workout

---
+++ vlad.bezden/javascript/Functions/2016-07-12.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Get query/url variables
+Move cursor at the end of text input
+Check if a document is done loading
