3bfa2335f34c5792e40445333f83fa99d2f54208
fosterfication <haileyhasmail@gmail.com>
2016-07-12 23:44:16 +0100

    2016-07-12's git workout

---
+++ fosterfication/git/Essentials/2016-07-12.bash
@@ -0,0 +1,5 @@
+Limiting log output by time
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Ignore files in git
