c47a0b88d5ccc1d42d2723329e9d53f23981e0bf
lokyi <roy.yi.yue@gmail.com>
2016-07-12 15:07:00 +0100

    2016-07-12's java workout

---
+++ lokyi/java/Fundamentals/2016-07-12.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+The distinction between overloading and overriding methods
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
