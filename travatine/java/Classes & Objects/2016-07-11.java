d2d0ed114283af7e2fca4a2171d9d2dee4885ee3
travatine <travatine@hotmail.com>
2016-07-12 01:01:13 +0100

    2016-07-11's java workout

---
+++ travatine/java/Classes & Objects/2016-07-11.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Utility classes and `static` methods
+The `abstract` keyword
+Compare classes and not class names
