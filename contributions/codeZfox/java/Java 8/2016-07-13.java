b4781cd6ae0a340d4e21e7dfabb6c9c58639c46b
codeZfox <codezfox@live.ru>
2016-07-13 19:41:00 +0100

    2016-07-13's java workout

---
+++ contributions/codeZfox/java/Java 8/2016-07-13.java
@@ -0,0 +1,5 @@
+Using `Arrays.parallelSort` 
+How to check for static dependencies of applications and libraries from the command line.
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
