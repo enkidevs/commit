f1f40efa7b1baf34a2b4c2bbc693671da7c68c45
markus.sve <markus.sve@gmail.com>
2016-07-13 18:47:53 +0100

    2016-07-13's java workout

---
+++ contributions/markus.sve/java/Error Handling/2016-07-13.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Use try-with-resources instead of `finally`
+Handling Null Pointer Exceptions
+Most common reason behind **stack overflow** error
+Throwing an `exception`
