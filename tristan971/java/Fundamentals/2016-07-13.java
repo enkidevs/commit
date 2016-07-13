1cd647005c6464876a29fe101d36ccb67825fa68
tristan971 <tristandeloche@gmail.com>
2016-07-13 12:29:25 +0100

    2016-07-13's java workout

---
+++ tristan971/java/Fundamentals/2016-07-13.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Do not return *references* to private *mutable* class members
