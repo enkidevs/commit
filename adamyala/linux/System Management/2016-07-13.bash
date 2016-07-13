6cb7de62944d6cdef445b05cfbf515e2070f6838
adamyala <adamkyala@gmail.com>
2016-07-13 14:27:26 +0100

    2016-07-13's linux workout

---
+++ adamyala/linux/System Management/2016-07-13.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Executing scripts on system boot
+Basic service management with `systemd`
