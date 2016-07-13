9bbe739fd7dbc181c8854284fc3ec2a4aba42d1b
patriciaorgan82 <patriciaorgan82@gmail.com>
2016-07-13 23:50:29 +0100

    2016-07-13's javascript workout

---
+++ contributions/patriciaorgan82/javascript/Browser APIs/2016-07-13.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use hosted scripts to increase performance
+Modify a website's URL
+Warn user if **Back** button is pressed
+Network Information API
