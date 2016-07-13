7a99242cd7f3404d6273bdb86dcf75d05347ac8c
Aberard123 <aberard123@gmail.com>
2016-07-13 03:12:15 +0100

    2016-07-12's java workout

---
+++ Aberard123/java/Error Handling/2016-07-12.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Handling Null Pointer Exceptions
+Throwing an `exception`
+Checking a double for `NaN` value
