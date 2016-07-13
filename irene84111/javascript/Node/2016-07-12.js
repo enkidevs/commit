07df5f1692528ca84b56f99764765f9190ce18e8
irene84111 <irene84111@gmail.com>
2016-07-13 14:49:24 +0100

    2016-07-12's javascript workout

---
+++ irene84111/javascript/Node/2016-07-12.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Global Objects and Environment Variables in **Node**
+`uncaughtException` listener in Node.js
+Manage child processes in **Node**
+Middleware/pipeline design pattern
