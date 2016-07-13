076e86cd508b291102280fc2df91a808fb6c501c
coleman.rollins <crollins199@gmail.com>
2016-07-13 02:29:23 +0100

    2016-07-12's linux workout

---
+++ coleman.rollins/linux/File Management/2016-07-12.bash
@@ -0,0 +1,7 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
