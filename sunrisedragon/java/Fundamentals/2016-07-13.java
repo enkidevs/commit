0345f5e808391607e0a4f9039ce5bae5c9c82c8b
sunrisedragon <highcomunity@gmail.com>
2016-07-13 10:15:21 +0100

    2016-07-13's java workout

---
+++ sunrisedragon/java/Fundamentals/2016-07-13.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Ternary operator
+Using Inheritance to reduce code repetition
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
