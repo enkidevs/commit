5be750dea100fbf3b6872452e60dee46e32f338e
jjmorph <jjmorphew92@gmail.com>
2016-07-13 19:25:26 +0100

    2016-07-13's java workout

---
+++ contributions/jjmorph/java/Fundamentals/2016-07-13.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+The trade-off between short and descriptive names
+Do not perform bitwise and arithmetic operations on the same data
+How range operations work
+Implementing an `interface`
