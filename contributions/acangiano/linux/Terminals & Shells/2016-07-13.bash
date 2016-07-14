b79649c42da6fddb2b18a2917b4e148029cc6847
acangiano <acangiano@gmail.com>
2016-07-14 04:10:02 +0100

    2016-07-13's linux workout

---
+++ contributions/acangiano/linux/Terminals & Shells/2016-07-13.bash
@@ -0,0 +1,5 @@
+Sha-bang `#!` explained 
+Execute a command without saving it in the history
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
