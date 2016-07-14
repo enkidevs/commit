f5649c753e84e98de32687a2c7f64ec5c4dc90e8
yasada <y.asada216@gmail.com>
2016-07-14 17:03:25 +0100

    2016-07-13's javascript workout

---
+++ contributions/yasada/javascript/Functions/2016-07-13.js
@@ -0,0 +1,5 @@
+Access a function's `arguments` as an array
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` with `call` and `apply` methods
+Anonymous functions
+Social sharing without widgets
