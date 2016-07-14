c51f8dff9fceb3c90a9c4722b58d195b550d7b47
ruadrift <joe.dante@gmail.com>
2016-07-14 15:36:05 +0100

    2016-07-14's linux workout

---
+++ contributions/ruadrift/linux/System Management/2016-07-14.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
