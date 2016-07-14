951c45068123d2aa82be93b3545fc650fc2f4db5
tpalmerstudios <tpalmerstudios@gmail.com>
2016-07-14 19:03:24 +0100

    2016-07-14's linux workout

---
+++ contributions/tpalmerstudios/linux/System Management/2016-07-14.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
