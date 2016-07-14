8c778e9f0e900e08fd891f4496eb67c82d35303c
garvamel <garvamel@gmail.com>
2016-07-14 14:05:07 +0100

    2016-07-14's linux workout

---
+++ contributions/garvamel/linux/System Management/2016-07-14.bash
@@ -0,0 +1,7 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
