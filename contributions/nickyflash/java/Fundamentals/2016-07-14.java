4ed4b6f3e477d6b373411f379a857cd3f74321d5
nickyflash <danuwidjajanicole@gmail.com>
2016-07-14 20:52:11 +0100

    2016-07-14's java workout

---
+++ contributions/nickyflash/java/Fundamentals/2016-07-14.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Java naming conventions
+Using `Printstream.format` to format a string before printing
+Locks in `static synchronized` methods
