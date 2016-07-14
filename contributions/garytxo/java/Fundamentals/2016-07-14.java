8eb4a8286d5089ae502f4127453de6e5e1d4d050
garytxo <garytxo@gmail.com>
2016-07-14 06:16:55 +0100

    2016-07-14's java workout

---
+++ contributions/garytxo/java/Fundamentals/2016-07-14.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+Using Inheritance to reduce code repetition
+Locks in `static synchronized` methods
