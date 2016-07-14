28225b5286e8c3defe8f8ae4a2a9bdc617b15ee4
jefrailey <jcaetof+enki@gmail.com>
2016-07-14 15:54:55 +0100

    2016-07-14's linux workout

---
+++ contributions/jefrailey/linux/System Management/2016-07-14.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
