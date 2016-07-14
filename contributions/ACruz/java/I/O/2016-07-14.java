41c192162aa28c380d8ebacf9f535b6b15d6f376
ACruz <astefan@live.com.pt>
2016-07-14 13:56:08 +0100

    2016-07-14's java workout

---
+++ contributions/ACruz/java/I/O/2016-07-14.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+Create system file paths using the `Path` class
+Reading and writing text files
+Deleting files and directories
