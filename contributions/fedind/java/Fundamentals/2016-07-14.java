9c3cfad7bca6b9e0349c8cfe46f07a58b12de871
fedind <fedind@gmail.com>
2016-07-14 18:23:30 +0100

    2016-07-14's java workout

---
+++ contributions/fedind/java/Fundamentals/2016-07-14.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
