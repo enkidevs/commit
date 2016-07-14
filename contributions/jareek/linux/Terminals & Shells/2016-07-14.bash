dbd035770e0f55765ccacbbbbb593cead12de8c7
jareek <jarek.pabich@gmail.com>
2016-07-15 00:13:10 +0100

    2016-07-14's linux workout

---
+++ contributions/jareek/linux/Terminals & Shells/2016-07-14.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Repeating commands with `watch`
+Execute a command without saving it in the history
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
