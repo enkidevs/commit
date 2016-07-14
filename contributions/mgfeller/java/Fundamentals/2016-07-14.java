432b616293ba7da9f36be71a1264df9b0d4f8fe7
mgfeller <mgfeller@mgfeller.net>
2016-07-14 08:37:45 +0100

    2016-07-14's java workout

---
+++ contributions/mgfeller/java/Fundamentals/2016-07-14.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+The distinction between overloading and overriding methods
+Metadata: creating a user-defined file attribute
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Do not return *references* to private *mutable* class members
