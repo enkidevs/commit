5f0f9fda2f7c978f1dce7e9c1e11e8a4106edad1
atakanarikan <marqhen@gmail.com>
2016-07-14 22:01:00 +0100

    2016-07-14's linux workout

---
+++ contributions/atakanarikan/linux/System Management/2016-07-14.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
