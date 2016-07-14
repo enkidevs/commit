9d84fd8451a3e5e1b3764ca722db3fb77c1c6704
dreforall <ravel.marques@gmail.com>
2016-07-14 13:44:47 +0100

    2016-07-14's java workout

---
+++ contributions/dreforall/java/Fundamentals/2016-07-14.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Cropping images
+Do not return *references* to private *mutable* class members
+`Autoboxing` and `Unboxing`
+Using an interface as a parameter
