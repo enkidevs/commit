fb06a5c65bfe9f952da0f6eb30e6f59efdead37f
mpm000 <mpm000@mac.com>
2016-07-14 03:08:43 +0100

    2016-07-13's javascript workout

---
+++ contributions/mpm000/javascript/Node/2016-07-13.js
@@ -0,0 +1,5 @@
+Http Server in **Node**
+Use conventional property names for error handling in Node.js
+**Node** `os` module
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
