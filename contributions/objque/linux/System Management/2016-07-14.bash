4826fa96dcf2af9b0a0d24b4ef83d149e9a5eeae
objque <wtf.object@gmail.com>
2016-07-14 22:39:05 +0100

    2016-07-14's linux workout

---
+++ contributions/objque/linux/System Management/2016-07-14.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Check the HDD with `badblocks`
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
