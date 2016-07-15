b8b6be91c1846a24e30502959b4d117d79e6d0af
punkaid <punkaid@gmail.com>
2016-07-15 04:11:11 +0100

    2016-07-14's linux workout

---
+++ contributions/punkaid/linux/System Management/2016-07-14.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Executing scripts on system boot
+Commands to shutdown or restart the system
