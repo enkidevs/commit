8b42051148dff22b7c498205b4cb92ce2fd3e235
osuvaldo <osuvaldo.ramos@gmail.com>
2016-07-15 12:32:25 +0100

    2016-07-15's linux workout

---
+++ contributions/osuvaldo/linux/File Management/2016-07-15.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
