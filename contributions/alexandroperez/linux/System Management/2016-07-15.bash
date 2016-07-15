ca25b401f42caafc217358e98f9ceb4218344927
alexandroperez <apgfriend@yahoo.com>
2016-07-15 20:41:33 +0100

    2016-07-15's linux workout

---
+++ contributions/alexandroperez/linux/System Management/2016-07-15.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Removing old kernels in Debian based systems
+Basic service management with `systemd`
