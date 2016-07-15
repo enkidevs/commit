a88b185ee5e65a4fd400bc54e8b00cd2fe506347
mgarrett <melissa_garrett@hotmail.com>
2016-07-15 21:29:26 +0100

    2016-07-15's java workout

---
+++ contributions/mgarrett/java/I/O/2016-07-15.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to test if file or directory exists in Java
+Using buffered streams
+Checking for the existence of files and directories
+List contents of a directory with `newDirectoryStream`
