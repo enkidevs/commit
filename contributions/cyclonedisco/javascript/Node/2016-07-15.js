322c4a8f2f1bfdff54c9ceadaf3cb6bb208806a4
cyclonedisco <enkichiro.kesh@recursor.net>
2016-07-15 07:09:24 +0100

    2016-07-15's javascript workout

---
+++ contributions/cyclonedisco/javascript/Node/2016-07-15.js
@@ -0,0 +1,5 @@
+Streams in **Node**
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+Middleware/pipeline design pattern
