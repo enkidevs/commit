f0cdabc3811b4b5706d1e9da47b082bc528069c2
akos.hochrein <hoch.akos@gmail.com>
2016-07-15 15:51:56 +0100

    2016-07-15's linux workout

---
+++ contributions/akos.hochrein/linux/Terminals & Shells/2016-07-15.bash
@@ -0,0 +1,5 @@
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
