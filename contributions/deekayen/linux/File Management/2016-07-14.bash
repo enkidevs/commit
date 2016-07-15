4808b1a24477b9efb8d3b7a1d580373ccc1eda04
deekayen <deekayen@deekayen.net>
2016-07-15 04:18:15 +0100

    2016-07-14's linux workout

---
+++ contributions/deekayen/linux/File Management/2016-07-14.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Deleting non-empty directories
