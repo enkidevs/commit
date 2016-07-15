db637b11d9c79851871165ea0a6198935e42bfb0
adewaal824 <adewaal824@gmail.com>
2016-07-15 11:51:39 +0100

    2016-07-15's linux workout

---
+++ contributions/adewaal824/linux/File Management/2016-07-15.bash
@@ -0,0 +1,6 @@
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Deleting non-empty directories
+Working with `.gz` files
+Find the unknown process preventing deleting of files
