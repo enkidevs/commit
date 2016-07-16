822e143535a8fa1d01296905c9404b945464f8e4
chrswndrs <christoph.wanders@gmail.com>
2016-07-16 16:15:50 +0100

    2016-07-16's linux workout

---
+++ contributions/chrswndrs/linux/Terminals & Shells/2016-07-16.bash
@@ -0,0 +1,5 @@
+Sha-bang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear bash history
+Adding directories to your `$PATH`
+The Basics of Environment Variables
