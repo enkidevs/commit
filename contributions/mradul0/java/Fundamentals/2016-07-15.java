66dea1d0f5ce41b1ebac782ef3d7d939c6c1ef24
mradul0 <mohit00fbd@gmail.com>
2016-07-16 14:38:04 +0100

    2016-07-15's java workout

---
+++ contributions/mradul0/java/Fundamentals/2016-07-15.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
