fe8813cdcdfddf2adf8f3bae963514201d10f01c
krishn <krishnr@gmail.com>
2016-07-16 15:44:00 +0100

    2016-07-16's linux workout

---
+++ contributions/krishn/linux/System Management/2016-07-16.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Monitor System Memory using `vmstat`
