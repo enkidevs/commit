beeb11cf48ceb2cbb4602ad44dc28373acd30b9c
Veega <pippoviga1995@gmail.com>
2016-07-17 00:41:15 +0100

    2016-07-16's javascript workout

---
+++ contributions/Veega/javascript/Functions/2016-07-16.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Social sharing without widgets
