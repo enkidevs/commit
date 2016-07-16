6c0cd37a17661478fa96f2f7802f213592d0b1e0
2fiks <tofiksa@gmail.com>
2016-07-16 20:45:46 +0100

    2016-07-16's java workout

---
+++ contributions/2fiks/java/Fundamentals/2016-07-16.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use meaningful names
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
