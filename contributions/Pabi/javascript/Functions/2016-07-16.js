6327d977f6fab0e2f212c032c300966549d2a908
Pabi <pablogamito@gmail.com>
2016-07-16 18:27:43 +0100

    2016-07-16's javascript workout

---
+++ contributions/Pabi/javascript/Functions/2016-07-16.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Anonymous functions
+Method chaining
+Move cursor at the end of text input
