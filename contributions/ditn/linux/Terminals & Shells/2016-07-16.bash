a00bc9593e1083a2431ae3936a5cc54f060bf619
ditn <abennett.kva@gmail.com>
2016-07-16 16:31:18 +0100

    2016-07-16's linux workout

---
+++ contributions/ditn/linux/Terminals & Shells/2016-07-16.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Clear the terminal instantly
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
