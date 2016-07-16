6403ab64bf3f4776ac05d873543e05291c00a43e
Lagaffe212 <robapaulhenry@gmail.com>
2016-07-16 10:38:30 +0100

    2016-07-16's java workout

---
+++ contributions/Lagaffe212/java/Fundamentals/2016-07-16.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+`Autoboxing` and `Unboxing`
