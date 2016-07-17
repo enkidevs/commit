92a982e0828f314be3b7b78333d68879e4c697ce
fffl <kuepfer.christopher@gmail.com>
2016-07-17 07:45:00 +0100

    2016-07-17's java workout

---
+++ contributions/fffl/java/Fundamentals/2016-07-17.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Use meaningful names
+Altering format string output by changing a format specifier's `argument_index`
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
