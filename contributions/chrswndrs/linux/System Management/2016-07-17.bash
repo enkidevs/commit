94c280c64815960bf5856455baf4458b875534fe
chrswndrs <christoph.wanders@gmail.com>
2016-07-18 00:24:30 +0100

    2016-07-17's linux workout

---
+++ contributions/chrswndrs/linux/System Management/2016-07-17.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Get hardware stack details with `lspci`
