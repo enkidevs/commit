01eee7caf810b3cbf8ff1111528c0982261a36da
exo_duz <exo_duz@yahoo.com>
2016-07-17 17:31:37 +0100

    2016-07-17's javascript workout

---
+++ contributions/exo_duz/javascript/Functions/2016-07-17.js
@@ -0,0 +1,5 @@
+Access a function's `arguments` as an array
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Get query/url variables
