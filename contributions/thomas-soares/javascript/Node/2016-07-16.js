255ac4d8d330de8cc0b422e27cccb9256839c750
thomas-soares <thomas__soares@hotmail.com>
2016-07-17 02:43:59 +0100

    2016-07-16's javascript workout

---
+++ contributions/thomas-soares/javascript/Node/2016-07-16.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
