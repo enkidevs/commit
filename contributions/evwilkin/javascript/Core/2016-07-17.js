44bcc84d1484d1db43714804412c077ec090811f
evwilkin <evan.wilkinson@gmail.com>
2016-07-17 22:51:55 +0100

    2016-07-17's javascript workout

---
+++ contributions/evwilkin/javascript/Core/2016-07-17.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Performing a function at timed intervals
+Functions return `undefined` by default
+The difference between null, undefined and NaN
+Detect online connection
