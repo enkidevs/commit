28dfda869078ee1aeabbd8ae90fe9b99b0d004a2
michaelOH <michaelbotes21@gmail.com>
2016-07-18 00:07:15 +0100

    2016-07-17's java workout

---
+++ contributions/michaelOH/java/Error Handling/2016-07-17.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Converting stack trace to a string
+Creating new Exceptions
+Using exceptions in Java
+Most common reason behind **stack overflow** error
