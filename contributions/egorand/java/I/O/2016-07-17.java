df7bcf991c3e73e6e45fd1050ad5271ab97f4f71
egorand <andreevich.egor@gmail.com>
2016-07-17 17:56:16 +0100

    2016-07-17's java workout

---
+++ contributions/egorand/java/I/O/2016-07-17.java
@@ -0,0 +1,5 @@
+How to test if file or directory exists in Java
+How to list all files in a directory that match a filename extension in Java
+Create system file paths using the `Path` class
+Retrieving file store attributes
+Listing a file system's root directories
