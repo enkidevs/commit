4f8875142df48cacfdcea6d561537bc5e909728b
Se7en_Sins <mar4enko.v.k@gmail.com>
2016-07-17 07:42:03 +0100

    2016-07-16's javascript workout

---
+++ contributions/Se7en_Sins/javascript/Functions/2016-07-16.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Easily generate a random `HEX` color
+Function call method
+Social sharing without widgets
