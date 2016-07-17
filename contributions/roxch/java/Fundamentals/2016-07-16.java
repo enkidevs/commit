a610090e32c460e304131403102ddee5f4fd0595
roxch <roshakz@gmail.com>
2016-07-17 10:37:28 +0100

    2016-07-16's java workout

---
+++ contributions/roxch/java/Fundamentals/2016-07-16.java
@@ -0,0 +1,5 @@
+Cropping images
+Using the `Console`
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
