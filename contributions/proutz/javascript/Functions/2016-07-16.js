91179bfa3188e81cfc9c3482d085d70383bdcf8e
proutz <thomasschmalz@gmail.com>
2016-07-17 09:07:53 +0100

    2016-07-16's javascript workout

---
+++ contributions/proutz/javascript/Functions/2016-07-16.js
@@ -0,0 +1,5 @@
+Access a function's `arguments` as an array
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Easily generate a random `HEX` color
