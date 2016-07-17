68d15b5d7823ef75e5af0068a9f759f3955a95b4
juliawong <juliawong0@gmail.com>
2016-07-17 14:46:24 +0100

    2016-07-16's java workout

---
+++ contributions/juliawong/java/Error Handling/2016-07-16.java
@@ -0,0 +1,5 @@
+Use try-with-resources instead of `finally`
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Checking a double for `NaN` value
