1059ec17940717ed581e8af39075367cfd084e16
pokemog <matrang@hotmail.com>
2016-07-17 02:15:27 +0100

    2016-07-16's linux workout

---
+++ contributions/pokemog/linux/Terminals & Shells/2016-07-16.bash
@@ -0,0 +1,5 @@
+Sha-bang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
