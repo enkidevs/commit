6e7e117e44190cbd891bf901e3d391273dcde388
wirtaw <float45@gmail.com>
2016-07-18 21:47:11 +0100

    2016-07-18's javascript workout

---
+++ contributions/wirtaw/javascript/Node/2016-07-18.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+Streams in **Node**
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+`process.nextTick()` callback pattern
