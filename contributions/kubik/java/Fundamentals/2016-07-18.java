4310a8410cbf25c96bfda6070602a8ddb0c4a3d7
kubik <lkubic@poczta.onet.pl>
2016-07-18 07:22:55 +0100

    2016-07-18's java workout

---
+++ contributions/kubik/java/Fundamentals/2016-07-18.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
