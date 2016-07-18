76d0bbbb3a77db3f8f5a525216d226f69e1ee153
michaelmior <michael.mior@gmail.com>
2016-07-18 16:18:24 +0100

    2016-07-18's linux workout

---
+++ contributions/michaelmior/linux/File Management/2016-07-18.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
