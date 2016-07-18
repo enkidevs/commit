a4d1c8d4921ad0c476c601b40f80b14f6e1b5cd9
jefrailey <jcaetof+enki@gmail.com>
2016-07-18 14:57:17 +0100

    2016-07-18's linux workout

---
+++ contributions/jefrailey/linux/File Management/2016-07-18.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Practical `du` command
+Truncate files with `cat` instead of `rm -rf`
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
