fdea670f2940e8c89483e119430c503eb23d1f02
benw <bena.white53@gmail.com>
2016-07-18 23:23:28 +0100

    2016-07-18's java workout

---
+++ contributions/benw/java/Fundamentals/2016-07-18.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use meaningful names
+Using `Printstream.format` to format a string before printing
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
