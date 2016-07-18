4194df773acf6099081f0276cf83a06a107a6a5f
saka <saka71996@gmail.com>
2016-07-18 05:42:26 +0100

    2016-07-18's java workout

---
+++ contributions/saka/java/Data Types/2016-07-18.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Double brace initialization
+Retrieve the component type of an array
+String concatenation in Java
