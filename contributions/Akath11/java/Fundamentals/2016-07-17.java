ab2e47cbb504d322540c2852eae5c5ba6580d05d
Akath11 <akath19@gmail.com>
2016-07-18 02:08:48 +0100

    2016-07-17's java workout

---
+++ contributions/Akath11/java/Fundamentals/2016-07-17.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The trade-off between short and descriptive names
+Using `Printstream.format` to format a string before printing
+Do not perform bitwise and arithmetic operations on the same data
+Implementing an `interface`
