9fe46cbc51a24269012fa5fd0c3ee02b25140de9
andrey.gershengoren <andrey.gershengoren@googlemail.com>
2016-07-18 08:53:09 +0100

    2016-07-18's java workout

---
+++ contributions/andrey.gershengoren/java/Fundamentals/2016-07-18.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
+Using an interface as a parameter
