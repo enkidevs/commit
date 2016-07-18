1c0fc1b160a109ac9ec84417ca7d9ea0bedac185
magician03 <diwakar03121997@gmail.com>
2016-07-18 12:15:31 +0100

    2016-07-17's javascript workout

---
+++ contributions/magician03/javascript/Core/2016-07-17.js
@@ -0,0 +1,5 @@
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+Filtering items out of an array
+Getting the *real* dimensions of an image
