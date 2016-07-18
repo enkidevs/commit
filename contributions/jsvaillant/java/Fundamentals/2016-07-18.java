bfb35a2fab87e30526d7f09636827f46d15b67e1
jsvaillant <jean.simon.vaillant@gmail.com>
2016-07-18 15:06:33 +0100

    2016-07-18's java workout

---
+++ contributions/jsvaillant/java/Fundamentals/2016-07-18.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+The distinction between overloading and overriding methods
+Use meaningful names
+The trade-off between short and descriptive names
+Using `synchronized` statements
