ffd59a1fc14210a8bee21f3cf60f6777a7406ce4
ditn <abennett.kva@gmail.com>
2016-07-18 13:41:10 +0100

    2016-07-18's java workout

---
+++ contributions/ditn/java/Fundamentals/2016-07-18.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+`Autoboxing` and `Unboxing`
