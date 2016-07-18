e9924fc93e7e087f4b48917cf8532baa72d344f2
vanporman <vanporman@gmail.com>
2016-07-18 08:23:21 +0100

    2016-07-18's javascript workout

---
+++ contributions/vanporman/javascript/Functions/2016-07-18.js
@@ -0,0 +1,5 @@
+Access a function's `arguments` as an array
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Move cursor at the end of text input
