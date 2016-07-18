587017f51287c38977bfb31ab97a25fde6d4af98
Intie <ludek@rolecek.cz>
2016-07-18 13:07:41 +0100

    2016-07-18's javascript workout

---
+++ contributions/Intie/javascript/Browser APIs/2016-07-18.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+Modify a website's URL
+`Window.localStorage`
+Drag and Drop API
