65464b37f8d871d6c5950baf9d432d2b22779881
phunx <justin.ferrieu@protonmail.com>
2016-07-18 02:17:21 +0100

    2016-07-18's linux workout

---
+++ contributions/phunx/linux/System Management/2016-07-18.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
