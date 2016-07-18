1f68144e7e1ed7af55ae368a6957030f87a351c5
lawck <lawck@me.com>
2016-07-18 07:10:00 +0100

    2016-07-17's linux workout

---
+++ contributions/lawck/linux/System Management/2016-07-17.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Monitor System Memory using `vmstat`
