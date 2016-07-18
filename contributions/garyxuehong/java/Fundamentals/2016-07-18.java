13bb159951d74c8dc007eba993a6dd91ca6672a3
garyxuehong <gary.xuehong@gmail.com>
2016-07-18 23:42:39 +0100

    2016-07-18's java workout

---
+++ contributions/garyxuehong/java/Fundamentals/2016-07-18.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Methods performing *Security Checks* must be declared `Private` or `Final`
