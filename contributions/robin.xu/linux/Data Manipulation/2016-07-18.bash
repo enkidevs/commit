9f73f05688667b1a633a7ce491a9f40ae40dfb68
robin.xu <robin.qd.2011@gmail.com>
2016-07-18 21:39:04 +0100

    2016-07-18's linux workout

---
+++ contributions/robin.xu/linux/Data Manipulation/2016-07-18.bash
@@ -0,0 +1,5 @@
+How to analyse a server's log files
+Using `lynx` to convert HTML to text
+Count non-empty lines with `nl` utility
+Image manipulation  using `convert`
+Clean configuration files using `sed`
