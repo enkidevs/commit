109198d950dd0e7ef37bfbb9c92a1fb8812ba80b
tylerthorne <tt.solarboltproductions@gmail.com>
2016-07-18 03:44:54 +0100

    2016-07-17's java workout

---
+++ contributions/tylerthorne/java/Fundamentals/2016-07-17.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Overloading Methods in Java
+Use meaningful names
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
