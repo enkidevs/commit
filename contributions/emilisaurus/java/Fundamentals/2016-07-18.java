56e8b4de7407043f511d5254681df3263e251b51
emilisaurus <emilie.westberg@gmail.com>
2016-07-18 11:51:08 +0100

    2016-07-18's java workout

---
+++ contributions/emilisaurus/java/Fundamentals/2016-07-18.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Using an interface as a parameter
