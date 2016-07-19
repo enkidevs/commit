7e60bb32a975db3a4dd39d6b418488a1e7a8c570
Pticed <cedric.finance@finandric.com>
2016-07-19 08:19:41 +0100

    2016-07-19's linux workout

---
+++ contributions/Pticed/linux/File Management/2016-07-19.bash
@@ -0,0 +1,7 @@
+Finding file with regexes
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
