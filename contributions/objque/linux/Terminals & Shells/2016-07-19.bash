43da4de45d4794f1704bd365a493e14fc161bed7
objque <wtf.object@gmail.com>
2016-07-19 01:32:51 +0100

    2016-07-19's linux workout

---
+++ contributions/objque/linux/Terminals & Shells/2016-07-19.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Get to know  your commands with `type`
