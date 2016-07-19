7b12e3c2120de6e83a5aab82db797e56c25acb4c
kitsune <buttercup.gle@gmail.com>
2016-07-19 17:31:47 +0100

    2016-07-18's linux workout

---
+++ contributions/kitsune/linux/System Management/2016-07-18.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Executing scripts on system boot
+Cancel a system shutdown
