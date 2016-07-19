0fc7003ce44acbfb630c9860272a2f56d7ac0e02
Tanker405th <thenuttyirishman@gmail.com>
2016-07-19 04:12:27 +0100

    2016-07-18's linux workout

---
+++ contributions/Tanker405th/linux/Package Management/2016-07-18.bash
@@ -0,0 +1,5 @@
+Install specific version of packages
+Prevent updating a specific package in Debian systems
+Using `Tasksel` for software installation
+Using `PPAs`
+Downgrade a package via `apt-get`
