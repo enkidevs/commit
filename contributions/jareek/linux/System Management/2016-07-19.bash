2f5d1ae622dca4f30285f371aeb78b5ec1138337
jareek <jarek.pabich@gmail.com>
2016-07-19 07:26:44 +0100

    2016-07-19's linux workout

---
+++ contributions/jareek/linux/System Management/2016-07-19.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Monitor System Memory using `vmstat`
