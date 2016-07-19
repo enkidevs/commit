bea5ac3a2da1ff0fa7e4face56005310cb0412de
DoomyTheFroomy <m.rudolph@email.de>
2016-07-19 21:17:47 +0100

    2016-07-19's javascript workout

---
+++ contributions/DoomyTheFroomy/javascript/Core/2016-07-19.js
@@ -0,0 +1,5 @@
+Be careful when using `typeof`.
+Milliseconds since Epoch
+The difference between null, undefined and NaN
+Prevent background image flicker in **IE**
+Getting the *real* dimensions of an image
