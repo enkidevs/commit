ac6b2f0ca909e5199822057b8252a4280c42b20e
vedu <darth.kadiri@gmail.com>
2016-07-19 02:43:19 +0100

    2016-07-18's linux workout

---
+++ contributions/vedu/linux/File Management/2016-07-18.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
