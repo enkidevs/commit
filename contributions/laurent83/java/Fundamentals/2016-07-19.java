0744d1a404a7c47c2d17292ea63bf5a23919aed7
laurent83 <laurent.morissette@gmail.com>
2016-07-19 18:23:02 +0100

    2016-07-19's java workout

---
+++ contributions/laurent83/java/Fundamentals/2016-07-19.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+How to play a sound file in Java
+Updating interfaces by using `default` methods
+Do not return *references* to private *mutable* class members
