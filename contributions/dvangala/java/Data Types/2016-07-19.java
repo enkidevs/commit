04ae6086a6d01ffea7595581ca02ba69e73e102e
dvangala <dvangala@gmail.com>
2016-07-20 00:50:42 +0100

    2016-07-19's java workout

---
+++ contributions/dvangala/java/Data Types/2016-07-19.java
@@ -0,0 +1,5 @@
+`ArrayList` vs `LinkedList`
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Double brace initialization
+Retrieve the component type of an array
