9070c237d4e5b670287540024812507551c2659d
robin.xu <robin.qd.2011@gmail.com>
2016-07-19 21:43:15 +0100

    2016-07-19's linux workout

---
+++ contributions/robin.xu/linux/Terminals & Shells/2016-07-19.bash
@@ -0,0 +1,5 @@
+Random password generator
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
