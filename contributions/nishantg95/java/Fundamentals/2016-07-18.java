a79b8e94d103d369fa5505248616ced91434c782
nishantg95 <nishant.groveris@gmail.com>
2016-07-19 03:10:29 +0100

    2016-07-18's java workout

---
+++ contributions/nishantg95/java/Fundamentals/2016-07-18.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
