218b16de5a13f81fa551ee2fbbca9fb8c1bc5d70
travatine <travatine@hotmail.com>
2016-07-19 04:48:37 +0100

    2016-07-18's java workout

---
+++ contributions/travatine/java/Fundamentals/2016-07-18.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Using an interface as a parameter
