a1fea2224bd747fbc561007f2ef76f63a2d099ef
greenren <slamboy182@hotmail.com>
2016-07-19 16:41:27 +0100

    2016-07-19's linux workout

---
+++ contributions/greenren/linux/Terminals & Shells/2016-07-19.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Find any Unix / Linux command
+Terminal incognito mode
+Keep useful commands in your shell history with tags
