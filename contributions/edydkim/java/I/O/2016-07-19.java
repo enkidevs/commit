b6028027aa6c9e70186725cc668cdd1a6864da9f
edydkim <eddie@app-creative.com>
2016-07-19 23:41:39 +0100

    2016-07-19's java workout

---
+++ contributions/edydkim/java/I/O/2016-07-19.java
@@ -0,0 +1,5 @@
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
