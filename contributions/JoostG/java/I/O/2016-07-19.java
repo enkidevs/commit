ff79c1b9a1094fd558bd3280ff2c704fc0943157
JoostG <jtgaag@gmail.com>
2016-07-19 22:47:43 +0100

    2016-07-19's java workout

---
+++ contributions/JoostG/java/I/O/2016-07-19.java
@@ -0,0 +1,5 @@
+How to test if file or directory exists in Java
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
