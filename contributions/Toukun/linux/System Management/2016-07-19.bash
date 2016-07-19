fc6407260d388c32e584238ca08e80b7de76a4fb
Toukun <toukunproduction@gmail.com>
2016-07-20 00:45:38 +0100

    2016-07-19's linux workout

---
+++ contributions/Toukun/linux/System Management/2016-07-19.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot
+Monitor System Memory using `vmstat`
