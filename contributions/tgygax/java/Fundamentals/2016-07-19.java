3c62f6bb2a0b19b33931849954687534fa378d84
tgygax <t.web@gyg.ch>
2016-07-19 21:09:51 +0100

    2016-07-19's java workout

---
+++ contributions/tgygax/java/Fundamentals/2016-07-19.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
