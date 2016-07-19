5e5ebb2192636c56a87f723ca2da40cc14d22405
benoliver999 <benoliver999@gmail.com>
2016-07-19 09:10:22 +0100

    2016-07-19's linux workout

---
+++ contributions/benoliver999/linux/File Management/2016-07-19.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm -rf`
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
