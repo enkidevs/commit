968891ea205187f2e403e91de202a7c77b44f4a6
negzi <negarin.fazeli@gmail.com>
2016-07-19 10:47:30 +0100

    2016-07-19's linux workout

---
+++ contributions/negzi/linux/Terminals & Shells/2016-07-19.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Quick `bash` shortcuts 
+Random password generator
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
