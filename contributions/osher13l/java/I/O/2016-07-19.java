cb75b4334b001fd1958befd8e36cadfe6eaa6721
osher13l <osher13l@gmail.com>
2016-07-19 09:07:53 +0100

    2016-07-19's java workout

---
+++ contributions/osher13l/java/I/O/2016-07-19.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+List contents of a directory with `newDirectoryStream`
+Deleting files and directories
