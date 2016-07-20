b73b25e5b09913de58af9423145cb057835c1fae
hanori <hanori@gmail.com>
2016-07-20 12:35:44 +0100

    2016-07-19's java workout

---
+++ contributions/hanori/java/Fundamentals/2016-07-19.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
+Using an interface as a parameter
