7dd6f8bda03a2d22d239a8d03376cb44e7794f6c
rishav <rishav.rastogi@gmail.com>
2016-07-20 16:25:37 +0100

    2016-07-20's linux workout

---
+++ contributions/rishav/linux/Terminals & Shells/2016-07-20.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
