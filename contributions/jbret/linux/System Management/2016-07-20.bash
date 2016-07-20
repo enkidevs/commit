5608577c7145309059a5c4d4b52966b2b9262795
jbret <jbretheim@gmail.com>
2016-07-20 14:02:32 +0100

    2016-07-20's linux workout

---
+++ contributions/jbret/linux/System Management/2016-07-20.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
