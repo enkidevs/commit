ceba3e8c58806de9510be24a6f3bd43e71a397f2
kevglx <kevglx@gmail.com>
2016-07-20 08:01:58 +0100

    2016-07-20's java workout

---
+++ contributions/kevglx/java/I/O/2016-07-20.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+How to test if file or directory exists in Java
+Create system file paths using the `Path` class
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
