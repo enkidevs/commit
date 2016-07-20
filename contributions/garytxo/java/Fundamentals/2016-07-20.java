296caab95756deec3d0fd25a7b58e06356345bc0
garytxo <garytxo@gmail.com>
2016-07-20 05:53:20 +0100

    2016-07-20's java workout

---
+++ contributions/garytxo/java/Fundamentals/2016-07-20.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
