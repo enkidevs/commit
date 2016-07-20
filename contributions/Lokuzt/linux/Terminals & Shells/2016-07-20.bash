0e9eddd4d94a7cb5cd12614eb868352b622068c7
Lokuzt <jomi_93@hotmail.com>
2016-07-20 10:42:38 +0100

    2016-07-20's linux workout

---
+++ contributions/Lokuzt/linux/Terminals & Shells/2016-07-20.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Shebang `#!` explained 
+Execute a command without saving it in the history
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
