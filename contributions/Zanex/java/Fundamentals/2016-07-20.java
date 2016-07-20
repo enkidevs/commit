6ab11a720fbffae3d077093c23739688df6c356c
Zanex <alessio.zanella88@gmail.com>
2016-07-20 07:28:28 +0100

    2016-07-20's java workout

---
+++ contributions/Zanex/java/Fundamentals/2016-07-20.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
