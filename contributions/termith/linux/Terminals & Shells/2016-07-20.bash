0812d62fd4884fa8d8525c754e657d8b957747e7
termith <dmitdemidov@gmail.com>
2016-07-20 07:19:27 +0100

    2016-07-20's linux workout

---
+++ contributions/termith/linux/Terminals & Shells/2016-07-20.bash
@@ -0,0 +1,7 @@
+Quick `bash` shortcuts 
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
