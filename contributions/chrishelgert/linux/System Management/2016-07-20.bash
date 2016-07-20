c2319c99937a3607c4e4b553365aac9a7083f4fc
chrishelgert <chris.helgert@yahoo.com>
2016-07-20 07:24:39 +0100

    2016-07-20's linux workout

---
+++ contributions/chrishelgert/linux/System Management/2016-07-20.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Incorrect time on dual boot systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
