582fb9ca9c30ff7b1a98ecb9c364d623b954df43
stevencknight <stevencknight@me.com>
2016-07-20 14:08:55 +0100

    2016-07-20's java workout

---
+++ contributions/stevencknight/java/Error Handling/2016-07-20.java
@@ -0,0 +1,5 @@
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Handling Null Pointer Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
