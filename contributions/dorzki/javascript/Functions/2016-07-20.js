0151ac993210f5ad6eb9de68f3aa092d7b94c63c
dorzki <me@dorzki.co.il>
2016-07-20 19:56:20 +0100

    2016-07-20's javascript workout

---
+++ contributions/dorzki/javascript/Functions/2016-07-20.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Easily generate a random `HEX` color
+Move cursor at the end of text input
