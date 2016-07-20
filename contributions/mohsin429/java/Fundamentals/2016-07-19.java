63b0e437f0b619e8450bac41851280541d663427
mohsin429 <mohsinali623@gmail.com>
2016-07-20 17:13:34 +0100

    2016-07-19's java workout

---
+++ contributions/mohsin429/java/Fundamentals/2016-07-19.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
