55caa36801ebfec9f5f1e4e0d6dbcb44309d58e2
bhank <brian.a.hankins@gmail.com>
2016-07-20 03:44:22 +0100

    2016-07-19's javascript workout

---
+++ contributions/bhank/javascript/Browser APIs/2016-07-19.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+Interaction with the user
+Navigating the browser history
+Blobs
