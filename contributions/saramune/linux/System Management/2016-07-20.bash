bae6bf42fddecf5c5d47c7de32a557196a4a4de3
saramune <si17erad0@gmail.com>
2016-07-21 00:24:34 +0100

    2016-07-20's linux workout

---
+++ contributions/saramune/linux/System Management/2016-07-20.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
