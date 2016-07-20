4cb797acf953e4c3745a7ed1977d150d1af8a00c
Geroxan <alik810@seznam.cz>
2016-07-20 10:15:45 +0100

    2016-07-20's javascript workout

---
+++ contributions/Geroxan/javascript/React/2016-07-20.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+Dangerously set `innerHTML`
+Immutability helpers in **React**
