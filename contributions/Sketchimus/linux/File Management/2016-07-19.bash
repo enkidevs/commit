a97d890ec06f1f56305a5cb6add6dadc6b87cb05
Sketchimus <stephenlee@hostgator.com>
2016-07-20 02:07:52 +0100

    2016-07-19's linux workout

---
+++ contributions/Sketchimus/linux/File Management/2016-07-19.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm -rf`
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
