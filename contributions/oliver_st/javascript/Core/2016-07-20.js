26d31bc54ff0f800240f3cb9a10b7fe44ce1bf6a
oliver_st <marinstaicu@gmail.com>
2016-07-20 22:52:32 +0100

    2016-07-20's javascript workout

---
+++ contributions/oliver_st/javascript/Core/2016-07-20.js
@@ -0,0 +1,5 @@
+Deleting properties
+Setting default values with `short circuiting`
+Performing a function at timed intervals
+The difference between null, undefined and NaN
+Check if a function exists
