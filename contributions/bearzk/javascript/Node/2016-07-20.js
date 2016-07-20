fa3ef3cfb82414d7176f6800aef2b79dd81eba87
bearzk <kai.zhangxa@gmail.com>
2016-07-20 06:00:57 +0100

    2016-07-20's javascript workout

---
+++ contributions/bearzk/javascript/Node/2016-07-20.js
@@ -0,0 +1,5 @@
+Streams in **Node**
+Use conventional property names for error handling in Node.js
+`uncaughtException` listener in Node.js
+Manage child processes in **Node**
+Middleware/pipeline design pattern
