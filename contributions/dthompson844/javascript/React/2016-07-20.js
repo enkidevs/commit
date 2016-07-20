f3d90607d70647090900b6d9583ceb08e8ffdd4f
dthompson844 <dthompson844@gmail.com>
2016-07-20 18:42:26 +0100

    2016-07-20's javascript workout

---
+++ contributions/dthompson844/javascript/React/2016-07-20.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+Self-closing tags in **JSX**
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+Keyed fragments in **React**
