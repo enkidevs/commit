0ceb6a2f8a8bb2ad26a7e165a8e67a3ea34f2cf1
sanjaymhj <snjmhj@gmail.com>
2016-07-20 22:22:19 +0100

    2016-07-19's linux workout

---
+++ contributions/sanjaymhj/linux/System Management/2016-07-19.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
