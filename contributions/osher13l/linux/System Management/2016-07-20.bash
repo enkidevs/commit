ad05acd1437e220db296912339823b22b6997598
osher13l <osher13l@gmail.com>
2016-07-20 16:50:13 +0100

    2016-07-20's linux workout

---
+++ contributions/osher13l/linux/System Management/2016-07-20.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
