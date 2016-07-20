6dc5139c222a2c0f79114c9d16503a77c353659b
Genghis Sean <sean.burton@civitaslearning.com>
2016-07-20 19:33:37 +0100

    2016-07-20's javascript workout

---
+++ contributions/Genghis Sean/javascript/Functions/2016-07-20.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Method chaining
+Easily generate a random `HEX` color
+Move cursor at the end of text input
