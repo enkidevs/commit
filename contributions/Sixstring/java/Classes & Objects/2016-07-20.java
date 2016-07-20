caa6a1329e6a404948a9ba45e0adc8df18c9e7eb
Sixstring <davidbush4@gmail.com>
2016-07-20 23:58:11 +0100

    2016-07-20's java workout

---
+++ contributions/Sixstring/java/Classes & Objects/2016-07-20.java
@@ -0,0 +1,5 @@
+Template methods in abstract classes
+Keeping fields `private`
+The `abstract` keyword
+Final object contents are not immutable
+String formatting and parsing with `SimpleDateFormat`
