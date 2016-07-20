d8f3f1d8b0834f5877c18080d31b8767550c1c9d
gbleaney <gbleaney@gmail.com>
2016-07-20 17:11:53 +0100

    2016-07-20's java workout

---
+++ contributions/gbleaney/java/Classes & Objects/2016-07-20.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Compare classes and not class names
+Final object contents are not immutable
+Fields with Java **Reflection API**
