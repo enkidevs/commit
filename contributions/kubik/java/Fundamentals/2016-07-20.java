5cf1b62170b73ca3fe7af6e5d06c53b6d4f6e294
kubik <lkubic@poczta.onet.pl>
2016-07-20 07:15:39 +0100

    2016-07-20's java workout

---
+++ contributions/kubik/java/Fundamentals/2016-07-20.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
