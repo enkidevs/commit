3aeaa84b048a8d81e28b41f4ae477f6bcf8a3160
KPR1976 <pkinyapin+enki@gmail.com>
2016-07-20 13:29:07 +0100

    2016-07-20's linux workout

---
+++ contributions/KPR1976/linux/System Management/2016-07-20.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
