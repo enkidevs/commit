9626036e99d9fbd6d156753923687ccb6212fb38
TotKindGott <konstantin.a.kolosov@gmail.com>
2016-07-20 10:35:18 +0100

    2016-07-20's linux workout

---
+++ contributions/TotKindGott/linux/File Management/2016-07-20.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Using `touch` to alter files modification time
+Deleting non-empty directories
