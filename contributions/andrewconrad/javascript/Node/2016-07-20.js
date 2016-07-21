12ba6c1e507d9845cfb01fc50e85c07842194a83
andrewconrad <andrewconrad@sloan.mit.edu>
2016-07-21 04:36:00 +0100

    2016-07-20's javascript workout

---
+++ contributions/andrewconrad/javascript/Node/2016-07-20.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+`process.nextTick()` callback pattern
+`exports` vs. `module.exports` in **Node**
+Middleware/pipeline design pattern
