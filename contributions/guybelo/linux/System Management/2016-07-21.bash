e9ded99b43522a31083d5063e92a69dc8e1a3de2
guybelo <guybelo@me.com>
2016-07-21 19:26:29 +0100

    2016-07-21's linux workout

---
+++ contributions/guybelo/linux/System Management/2016-07-21.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
