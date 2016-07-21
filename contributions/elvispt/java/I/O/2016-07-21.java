6ae6bed75d04bd56f9776143afd2618de3ef1b0c
elvispt <elvispt@gmail.com>
2016-07-21 23:45:59 +0100

    2016-07-21's java workout

---
+++ contributions/elvispt/java/I/O/2016-07-21.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Reading file attributes
+Retrieving file store attributes
