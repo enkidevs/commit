45a12de3c06abcf9676648427de2e4da4fa80de3
acangiano <acangiano@gmail.com>
2016-07-21 04:06:39 +0100

    2016-07-20's linux workout

---
+++ contributions/acangiano/linux/File Management/2016-07-20.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Deleting files in a secure manner
