b188ffcb846514e5d199ace3c56346f9c92a3fd1
James.couperthwaite <james.couperthwaite@gmail.com>
2016-07-21 21:40:48 +0100

    2016-07-21's linux workout

---
+++ contributions/James.couperthwaite/linux/System Management/2016-07-21.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Executing scripts on system boot
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Cancel a system shutdown
