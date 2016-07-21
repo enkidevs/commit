9568a6fa11dc945ea9482057784078c1119baae4
gamecow <chris.miranda11@gmail.com>
2016-07-21 17:03:02 +0100

    2016-07-21's linux workout

---
+++ contributions/gamecow/linux/File Management/2016-07-21.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm -rf`
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
