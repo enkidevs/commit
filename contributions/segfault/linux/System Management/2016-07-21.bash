7763e72791bb7e1c549518a5e3fa5318f7b83584
segfault <thibaut.constant92@gmail.com>
2016-07-21 21:10:30 +0100

    2016-07-21's linux workout

---
+++ contributions/segfault/linux/System Management/2016-07-21.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Basic service management with `systemd`
+Finding Open Files With `lsof`
