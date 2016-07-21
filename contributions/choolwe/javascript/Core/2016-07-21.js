dae9860e64b7f76d2db242030b5f4033ae860f00
choolwe <nakalonga@gmail.com>
2016-07-21 10:47:48 +0100

    2016-07-21's javascript workout

---
+++ contributions/choolwe/javascript/Core/2016-07-21.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Double and single quotes
+Hoisting applies only to variable declarations, not initializations
+The difference between null, undefined and NaN
+Context in JavaScript
