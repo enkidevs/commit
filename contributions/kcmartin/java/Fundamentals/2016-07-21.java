0f2d4ae5e01ecc0c3bc3625c1a02bbf622f53e5b
kcmartin <kcmartin2@mac.com>
2016-07-21 17:50:39 +0100

    2016-07-21's java workout

---
+++ contributions/kcmartin/java/Fundamentals/2016-07-21.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Supply `toString()` in all classes
+Using an interface as a parameter
