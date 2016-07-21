66cb78bc0345474d33663db74a10c1b8e3e54e3c
attila.csanyi <attila.csnyi@yahoo.co.uk>
2016-07-21 08:28:12 +0100

    2016-07-21's java workout

---
+++ contributions/attila.csanyi/java/Fundamentals/2016-07-21.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
+Using an interface as a parameter
