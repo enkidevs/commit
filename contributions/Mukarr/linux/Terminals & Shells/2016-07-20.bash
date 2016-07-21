2a17032c002da62f094e5c007aa32c4b1aaf1752
Mukarr <mukarramtailor.02@gmail.com>
2016-07-21 16:05:02 +0100

    2016-07-20's linux workout

---
+++ contributions/Mukarr/linux/Terminals & Shells/2016-07-20.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
