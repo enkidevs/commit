a9d6ddaa343a059842bc04cd44ab9a1d9a4df8ba
Mavwreck <mavwreck@hotmail.com>
2016-07-21 20:26:23 +0100

    2016-07-21's java workout

---
+++ contributions/Mavwreck/java/I/O/2016-07-21.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+How to test if file or directory exists in Java
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
