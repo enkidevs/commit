8f906c57cd9fa19221a3a6294e2fa345f541750a
mohsin429 <mohsinali623@gmail.com>
2016-07-21 14:43:40 +0100

    2016-07-20's java workout

---
+++ contributions/mohsin429/java/Fundamentals/2016-07-20.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
