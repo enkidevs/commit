c21e9defe81d295df46577230016d43094802898
matusiak.artur91 <matusiak.artur91@gmail.com>
2016-07-21 05:43:09 +0100

    2016-07-20's git workout

---
+++ contributions/matusiak.artur91/git/Tools/2016-07-20.bash
@@ -0,0 +1,5 @@
+Let's play the blame game
+Cache your authentication details to save time
+Create a new branch from a stash
+Show history of a function
+Remember the results of previous hunk conflicts
