e501a1498f35e88e5917687ffed6caa742053b93
mistymagich <yamaguchi.adfproject@gmail.com>
2016-07-21 13:49:17 +0100

    2016-07-20's linux workout

---
+++ contributions/mistymagich/linux/System Management/2016-07-20.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot
+Get hardware stack details with `lspci`
