0dfa0fa5c7f673d1e07178f534bb888d044757ce
yasmuru <yasmuru@gmail.com>
2016-07-21 03:40:09 +0100

    2016-07-20's javascript workout

---
+++ contributions/yasmuru/javascript/Core/2016-07-20.js
@@ -0,0 +1,5 @@
+Hoisting applies only to variable declarations, not initializations
+`catch` is block scoped
+Extra function arguments are undefined by default
+Detect online connection
+Getting the *real* dimensions of an image
