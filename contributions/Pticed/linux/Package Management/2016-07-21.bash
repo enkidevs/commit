e746fbe5bbf9e907067a043d6d99ea70373be69d
Pticed <cedric.finance@finandric.com>
2016-07-21 08:25:16 +0100

    2016-07-21's linux workout

---
+++ contributions/Pticed/linux/Package Management/2016-07-21.bash
@@ -0,0 +1,6 @@
+Install specific version of packages
+Backporting on Debian
+
+Prevent updating a specific package in Debian systems
+Downgrade a package via `apt-get`
+Keeping repos updated with `cron`
