c635ffd4b3458b06f62b27ffa07ae85d2adfa057
ragoeken <rachel.goeken@gmail.com>
2016-07-21 19:29:26 +0100

    2016-07-21's linux workout

---
+++ contributions/ragoeken/linux/System Management/2016-07-21.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Executing scripts on system boot
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
