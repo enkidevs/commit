f2943eb53716f948cc313d5e2edd78509ff15e66
Beelzenef <elena.guzbla@gmail.com>
2016-07-21 09:11:32 +0100

    2016-07-21's java workout

---
+++ contributions/Beelzenef/java/Fundamentals/2016-07-21.java
@@ -0,0 +1,5 @@
+Cropping images
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Detect or prevent integer overflow
+Supply `toString()` in all classes
