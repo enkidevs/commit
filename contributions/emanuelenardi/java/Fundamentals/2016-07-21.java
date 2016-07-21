9754e288f864ad895c1cae00d6c167b2ff76724e
emanuelenardi <nardiemanuele6@gmail.com>
2016-07-21 09:52:45 +0100

    2016-07-21's java workout

---
+++ contributions/emanuelenardi/java/Fundamentals/2016-07-21.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+Common mistake on switch statements
+Using Inheritance to reduce code repetition
+Do not return *references* to private *mutable* class members
+How range operations work
