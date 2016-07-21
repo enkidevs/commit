b3930aeb6ed6362a3f1a50c814dfac9a72046879
codeaholic <krebs-adrian@bluewin.ch>
2016-07-21 09:01:03 +0100

    2016-07-21's java workout

---
+++ contributions/codeaholic/java/Fundamentals/2016-07-21.java
@@ -0,0 +1,5 @@
+Cropping images
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
