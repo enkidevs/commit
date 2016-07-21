6be26fabec8d3d6cc9e3c43ebb632832c00175a7
EvdokimovN <sarpen47@gmail.com>
2016-07-21 10:59:29 +0100

    2016-07-21's java workout

---
+++ contributions/EvdokimovN/java/Fundamentals/2016-07-21.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
