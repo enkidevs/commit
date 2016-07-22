22f3d1488f6f4f936a06e9e090a7a599979c7833
sirotin <sirotin@gmail.com>
2016-07-22 20:12:56 +0100

    2016-07-22's linux workout

---
+++ contributions/sirotin/linux/Terminals & Shells/2016-07-22.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Find any Unix / Linux command
+Random password generator
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
