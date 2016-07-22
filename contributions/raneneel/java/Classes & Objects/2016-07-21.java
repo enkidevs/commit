3c1fe18014e3cd5e231f255ca285134cf2a334b2
raneneel <raneneel@yahoo.in>
2016-07-22 10:18:29 +0100

    2016-07-21's java workout

---
+++ contributions/raneneel/java/Classes & Objects/2016-07-21.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Utility classes and `static` methods
+Java object references
+Compare classes and not class names
