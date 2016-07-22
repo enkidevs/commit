ae94b2405e603e69367cb854a20a9913f31b3179
lurker <zhousha.cu@gmail.com>
2016-07-22 07:04:01 +0100

    2016-07-21's java workout

---
+++ contributions/lurker/java/Fundamentals/2016-07-21.java
@@ -0,0 +1,5 @@
+Cropping images
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Using an interface as a parameter
