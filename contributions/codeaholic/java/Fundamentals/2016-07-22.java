be77fd627202b5c8f5c366cbde52df2ee189c84d
codeaholic <krebs-adrian@bluewin.ch>
2016-07-22 16:02:03 +0100

    2016-07-22's java workout

---
+++ contributions/codeaholic/java/Fundamentals/2016-07-22.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
