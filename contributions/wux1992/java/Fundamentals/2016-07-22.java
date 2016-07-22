724f908a945dcabfb0ffc97ea5fea21c9b0f333e
wux1992 <wux1992@126.com>
2016-07-22 14:56:24 +0100

    2016-07-22's java workout

---
+++ contributions/wux1992/java/Fundamentals/2016-07-22.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Do not return *references* to private *mutable* class members
