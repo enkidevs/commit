be8c4ec3148690f8285319ed8b37e92dc9da011f
mwender <michael@michaelwender.com>
2016-07-22 02:51:39 +0100

    2016-07-21's linux workout

---
+++ contributions/mwender/linux/System Management/2016-07-21.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
