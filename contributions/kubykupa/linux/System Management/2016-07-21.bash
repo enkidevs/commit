15ec517da595cbbb5fab012de2473fc3450bcd6b
kubykupa <kubykupa@yandex.ru>
2016-07-22 10:36:06 +0100

    2016-07-21's linux workout

---
+++ contributions/kubykupa/linux/System Management/2016-07-21.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Detect your linux distribution
+Preventing a user from logging into the system
+Commands to shutdown or restart the system
