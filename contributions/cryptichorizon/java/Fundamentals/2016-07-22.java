f9e85e10844e430e2278b6dd337c5ba3a36cba6d
cryptichorizon <cryptichorizon@gmail.com>
2016-07-22 06:00:33 +0100

    2016-07-22's java workout

---
+++ contributions/cryptichorizon/java/Fundamentals/2016-07-22.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+The distinction between overloading and overriding methods
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
