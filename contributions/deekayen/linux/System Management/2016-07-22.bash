f3e8c2431655235ba65a7277a3e3a95c704f4571
deekayen <deekayen@deekayen.net>
2016-07-22 12:14:31 +0100

    2016-07-22's linux workout

---
+++ contributions/deekayen/linux/System Management/2016-07-22.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
