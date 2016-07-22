18e75a5c512203cb0bf4f7fe10ec14aaf8dd1cfc
xialiu <xialiumit@gmail.com>
2016-07-22 18:40:29 +0100

    2016-07-22's javascript workout

---
+++ contributions/xialiu/javascript/Functions/2016-07-22.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Method chaining
+Move cursor at the end of text input
+Check if a document is done loading
