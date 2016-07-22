b834fa86c02a308460a79a8a1b13796ec5e5907c
koliada <alex.fiator@gmail.com>
2016-07-22 20:58:09 +0100

    2016-07-22's linux workout

---
+++ contributions/koliada/linux/System Management/2016-07-22.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Find out if the system's architecture is 32 or 64 bit
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
