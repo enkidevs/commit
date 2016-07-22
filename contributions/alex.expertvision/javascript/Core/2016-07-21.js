0ae13a5dc89d921b52856b8082e8b761e201caac
alex.expertvision <alexandru.balasa@rocketmail.com>
2016-07-22 07:23:18 +0100

    2016-07-21's javascript workout

---
+++ contributions/alex.expertvision/javascript/Core/2016-07-21.js
@@ -0,0 +1,5 @@
+Performing a function at timed intervals
+The difference between null, undefined and NaN
+`catch` is block scoped
+Avoid using `with`
+Extra function arguments are undefined by default
