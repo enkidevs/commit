0d1825055ebdfb1f0907914a78a8ed8578c2ca12
cnkaptan <cihan-kaptan@windowslive.com>
2016-07-22 10:36:40 +0100

    2016-07-22's java workout

---
+++ contributions/cnkaptan/java/Fundamentals/2016-07-22.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Methods performing *Security Checks* must be declared `Private` or `Final`
