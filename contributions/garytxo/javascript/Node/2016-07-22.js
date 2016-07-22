f3f3140cfa1797032c1358698651ffdb67937ff8
garytxo <garytxo@gmail.com>
2016-07-22 06:09:15 +0100

    2016-07-22's javascript workout

---
+++ contributions/garytxo/javascript/Node/2016-07-22.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Global Objects and Environment Variables in **Node**
+First-error callbacks in **Node**
+Wrapping errors in Node.js using _node-verror_
+Handle `JSON.parse` error in Node.js
