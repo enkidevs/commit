564123521d42a4b82229dd2c23c8c11b26862287
hemalshah <hemal.shah1996@gmail.com>
2016-07-22 05:30:10 +0100

    2016-07-21's java workout

---
+++ contributions/hemalshah/java/Fundamentals/2016-07-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Supply `toString()` in all classes
