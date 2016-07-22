6ee576eb4b1d9a5c15be73b2e3c8c7c29936174b
lalo <leandro.gz73@gmail.com>
2016-07-22 12:11:45 +0100

    2016-07-22's java workout

---
+++ contributions/lalo/java/Fundamentals/2016-07-22.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Methods performing *Security Checks* must be declared `Private` or `Final`
