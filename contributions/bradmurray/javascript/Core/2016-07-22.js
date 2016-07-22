b2d8e85e21b24a449cd1227c3a448d79aa02fc77
bradmurray <brad@bradmurray.com>
2016-07-22 18:24:18 +0100

    2016-07-22's javascript workout

---
+++ contributions/bradmurray/javascript/Core/2016-07-22.js
@@ -0,0 +1,5 @@
+Multi-line string variables
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Functions return `undefined` by default
+Prevent background image flicker in **IE**
