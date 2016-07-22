a9edc89b3eff56c500f510a23c8808d698a5e85f
yedincisenol <o@yedincisenol.com>
2016-07-22 08:50:55 +0100

    2016-07-22's javascript workout

---
+++ contributions/yedincisenol/javascript/React/2016-07-22.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+Self-closing tags in **JSX**
+Pixel values in style props for **React**
+Expose Component Functions in **React**
+Dangerously set `innerHTML`
