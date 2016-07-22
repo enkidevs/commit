8e62f1824da6a751cd246c934983e2af092f8574
paul589180 <paul58914080@gmail.com>
2016-07-22 16:39:04 +0100

    2016-07-21's java workout

---
+++ contributions/paul589180/java/Fundamentals/2016-07-21.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
