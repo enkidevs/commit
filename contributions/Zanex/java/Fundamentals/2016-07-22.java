bae428b9f22a3a1fc75637fcecdbbf4ef023e82e
Zanex <alessio.zanella88@gmail.com>
2016-07-22 13:48:22 +0100

    2016-07-22's java workout

---
+++ contributions/Zanex/java/Fundamentals/2016-07-22.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Common mistake on switch statements
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
