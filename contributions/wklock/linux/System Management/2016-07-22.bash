f197590ea645ec4c93f54db7a2406e4175eae956
wklock <william.klock@gmail.com>
2016-07-22 16:03:27 +0100

    2016-07-22's linux workout

---
+++ contributions/wklock/linux/System Management/2016-07-22.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
