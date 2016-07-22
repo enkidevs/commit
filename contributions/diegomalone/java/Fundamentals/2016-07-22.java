3511921b6e738ecde8140f5d81f8faa96e62fc28
diegomalone <diegomalone@gmail.com>
2016-07-22 13:36:49 +0100

    2016-07-22's java workout

---
+++ contributions/diegomalone/java/Fundamentals/2016-07-22.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Detect or prevent integer overflow
