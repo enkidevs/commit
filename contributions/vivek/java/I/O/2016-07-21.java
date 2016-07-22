f71f8cfebf78eeebe4eb208ce2b807433bd4d8a5
vivek <vivek_thakur22@yahoo.com>
2016-07-22 14:52:46 +0100

    2016-07-21's java workout

---
+++ contributions/vivek/java/I/O/2016-07-21.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+How to test if file or directory exists in Java
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Reading file attributes
