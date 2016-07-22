cb4a4c7154fc6e56ab5138855c096a8cd501d247
ptrickg <ptrickg@hotmail.com>
2016-07-22 17:57:39 +0100

    2016-07-22's java workout

---
+++ contributions/ptrickg/java/Fundamentals/2016-07-22.java
@@ -0,0 +1,5 @@
+Using the `Console`
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
