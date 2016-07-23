639845e62b9de21b8eeebbcdff88a425ab47cf50
Marcus.Vinicius <marcusviniciusaguiar@gmail.com>
2016-07-23 13:14:56 +0100

    2016-07-21's linux workout

---
+++ contributions/Marcus.Vinicius/linux/Package Management/2016-07-21.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Backporting on Debian
+
+Prevent updating a specific package in Debian systems
+Get `apt` to use a mirror / faster mirror
+Keeping repos updated with `cron`
