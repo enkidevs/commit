10e087a2feb88f422bd4801242fc18df9d2476c5
alta0815 <alta0815@yandex.ru>
2016-07-23 22:01:43 +0100

    2016-07-23's java workout

---
+++ contributions/alta0815/java/Fundamentals/2016-07-23.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+How to play a sound file in Java
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
