e62dc7c7c651962fd122a68748b8602e800d4deb
Robocrypt <wickie.leejr@gmail.com>
2016-07-23 18:10:50 +0100

    2016-07-23's java workout

---
+++ contributions/Robocrypt/java/Fundamentals/2016-07-23.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
