f31a754ea6331270e439a67af02741964bab9bee
BijuAle <bijuale@hotmail.com>
2016-07-23 04:13:27 +0100

    2016-07-22's java workout

---
+++ contributions/BijuAle/java/Error Handling/2016-07-22.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Careful Numeric Data Conversions
