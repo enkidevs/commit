4bea3802f08230466caa00523778621553b9b598
masaruhoshi <mhoshi@gmail.com>
2016-07-23 13:49:24 +0100

    2016-07-23's java workout

---
+++ contributions/masaruhoshi/java/Fundamentals/2016-07-23.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+Connecting to FTP using Java
+Using the `Console`
+Do not return *references* to private *mutable* class members
