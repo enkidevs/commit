53b00582aae1eb9c46dc95e4b23bd71d2f83e8cd
yeehaw <pavel1994-09@mail.ru>
2016-07-23 16:46:55 +0100

    2016-07-23's javascript workout

---
+++ contributions/yeehaw/javascript/Core/2016-07-23.js
@@ -0,0 +1,5 @@
+`hasOwnProperty` method
+Setting the length of an array 
+Data type comparison in `switch` statements
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
