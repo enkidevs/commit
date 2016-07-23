f650820a773068c73e95c6b020609af4bf2fdee2
sandor.dargo <sandor.dargo@gmail.com>
2016-07-23 08:03:29 +0100

    2016-07-23's linux workout

---
+++ contributions/sandor.dargo/linux/System Management/2016-07-23.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
