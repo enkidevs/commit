d9172aedb0b0366ae4604792f7e009ac3f0bc3c8
seans66 <seanstreet@hotmail.com>
2016-07-23 07:51:49 +0100

    2016-07-22's linux workout

---
+++ contributions/seans66/linux/File Management/2016-07-22.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm -rf`
+Deleting files in a secure manner
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
