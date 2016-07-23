74d8a6dbc581eaad0b2cce1e726d0dd046bfdab5
mohsin429 <mohsinali623@gmail.com>
2016-07-23 09:57:05 +0100

    2016-07-22's java workout

---
+++ contributions/mohsin429/java/Fundamentals/2016-07-22.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Cropping images
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
