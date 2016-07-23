ba66ec3762de61871c29685e80edeb3866e69019
abhisheknair <abhisheknair@live.in>
2016-07-23 17:43:24 +0100

    2016-07-22's linux workout

---
+++ contributions/abhisheknair/linux/File Management/2016-07-22.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm -rf`
+Get disk space usage with `df`
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
