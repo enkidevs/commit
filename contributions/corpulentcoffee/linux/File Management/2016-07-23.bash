84ead39ec3230a655e2a78ffab01be41475fd7aa
corpulentcoffee <dave@corpulentcoffee.com>
2016-07-23 14:34:41 +0100

    2016-07-23's linux workout

---
+++ contributions/corpulentcoffee/linux/File Management/2016-07-23.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Truncate files with `cat` instead of `rm -rf`
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
