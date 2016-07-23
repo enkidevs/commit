075422ee28a200b5978e052c5e291655d8aa675e
coro55 <ciprian.corogianu@gmail.com>
2016-07-23 19:37:57 +0100

    2016-07-23's javascript workout

---
+++ contributions/coro55/javascript/Core/2016-07-23.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Data type comparison in `switch` statements
+The distinction between `==` and `===`
+The difference between null, undefined and NaN
+JavaScript compilation
