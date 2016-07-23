32bd7244ec299c9bd5b2a428fd1089ace347c8be
nikema <nikema@gmail.com>
2016-07-23 07:11:06 +0100

    2016-07-22's javascript workout

---
+++ contributions/nikema/javascript/Core/2016-07-22.js
@@ -0,0 +1,5 @@
+Get a random item from an Array
+Using `.test()` with RegExp
+Hoisting applies only to variable declarations, not initializations
+Prevent background image flicker in **IE**
+`console.*` in JavaScript
