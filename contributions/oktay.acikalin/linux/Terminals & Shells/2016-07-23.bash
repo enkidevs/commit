61aba4eed29155e6f81abbe810fc705de79a3087
oktay.acikalin <oktay.acikalin@gmail.com>
2016-07-23 08:17:02 +0100

    2016-07-23's linux workout

---
+++ contributions/oktay.acikalin/linux/Terminals & Shells/2016-07-23.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Aliasing ssh connections
+Quick `bash` shortcuts 
+Sequential execution using the `;` statement separator
