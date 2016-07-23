1edfc704435b6b22756a93ce3fdc9c0be33aac7b
rpm0618 <rpm0618@gmail.com>
2016-07-23 15:48:00 +0100

    2016-07-23's linux workout

---
+++ contributions/rpm0618/linux/Data Manipulation/2016-07-23.bash
@@ -0,0 +1,5 @@
+Getting rid of duplicate lines
+Using `lynx` to convert HTML to text
+Count non-empty lines with `nl` utility
+Image manipulation  using `convert`
+Clean configuration files using `sed`
