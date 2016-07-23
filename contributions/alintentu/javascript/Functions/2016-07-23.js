f7702e1f5f0fd028b3a224e4dd23e887883f5ac8
alintentu <tentualin@gmail.com>
2016-07-23 09:18:18 +0100

    2016-07-23's javascript workout

---
+++ contributions/alintentu/javascript/Functions/2016-07-23.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Move cursor at the end of text input
+Social sharing without widgets
