00476ab2238333dc9112f2bba2c30fd0a685c892
dabizk <dabizk@yahoo.com>
2016-07-23 02:19:05 +0100

    2016-07-22's java workout

---
+++ contributions/dabizk/java/I/O/2016-07-22.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Create system file paths using the `Path` class
+Retrieving file store attributes
+Deleting files and directories
