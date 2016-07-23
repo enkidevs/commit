059ee362bc1c99129094ef44157c13c2ced4b28f
paul589180 <paul58914080@gmail.com>
2016-07-23 03:54:54 +0100

    2016-07-22's linux workout

---
+++ contributions/paul589180/linux/Terminals & Shells/2016-07-22.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Clear the terminal instantly
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
