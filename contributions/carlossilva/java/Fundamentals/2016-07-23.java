5f36069000a3824be7625e840d5d9b669d7fb0a3
carlossilva <carlossilva.82@gmail.com>
2016-07-23 20:20:17 +0100

    2016-07-23's java workout

---
+++ contributions/carlossilva/java/Fundamentals/2016-07-23.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
