6c4b82c2ed447eaa4af89c8513542e3299a52842
kirby15 <kirbyjamessimmons@gmail.com>
2016-07-23 18:50:21 +0100

    2016-07-23's linux workout

---
+++ contributions/kirby15/linux/System Management/2016-07-23.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
