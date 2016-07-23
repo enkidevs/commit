c8530c6d23b853c6e0317065063e470c4cfb5b02
muschel <jonathan.youett@web.de>
2016-07-23 15:28:50 +0100

    2016-07-23's linux workout

---
+++ contributions/muschel/linux/Terminals & Shells/2016-07-23.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+`else` statements using the `||` operator 
