19400d2514f3b7db21649ab90d64131aa16ac034
Reyes <tred27@gmail.com>
2016-07-23 15:33:51 +0100

    2016-07-23's linux workout

---
+++ contributions/Reyes/linux/Terminals & Shells/2016-07-23.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
