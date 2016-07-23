26799f54edc0f1e02699115a6fe639e27642c41b
fffl <kuepfer.christopher@gmail.com>
2016-07-23 16:53:11 +0100

    2016-07-23's java workout

---
+++ contributions/fffl/java/I/O/2016-07-23.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Reading file attributes
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
