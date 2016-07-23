309b73fd6d29a2814c5c8a2782c7795acc826487
termith <dmitdemidov@gmail.com>
2016-07-23 06:57:24 +0100

    2016-07-23's linux workout

---
+++ contributions/termith/linux/System Management/2016-07-23.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Monitor System Memory using `vmstat`
