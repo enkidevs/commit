1b3d3e8b3924e2e1466922744279e173f847f3b5
PizzaPoindexter <travis.tatsch@gmail.com>
2016-07-23 21:08:23 +0100

    2016-07-23's linux workout

---
+++ contributions/PizzaPoindexter/linux/System Management/2016-07-23.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Preventing a user from logging into the system
+Basic service management with `systemd`
