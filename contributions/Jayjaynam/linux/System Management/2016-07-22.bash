b35752a9a2ca5175427508761aa91a9a613fc4c9
Jayjaynam <jeraimy.ragot@gmail.com>
2016-07-23 01:35:31 +0100

    2016-07-22's linux workout

---
+++ contributions/Jayjaynam/linux/System Management/2016-07-22.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Cancel a system shutdown
