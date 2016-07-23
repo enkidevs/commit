b6e58a857ef0d1a467733a33ae13840d7279aa48
seamsay <srm.1708@gmail.com>
2016-07-23 10:52:10 +0100

    2016-07-23's linux workout

---
+++ contributions/seamsay/linux/System Management/2016-07-23.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Finding Open Files With `lsof`
