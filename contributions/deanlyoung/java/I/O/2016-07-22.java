0e43d0f8fa6bd33b3409bb3cde246b5ccc1ba36b
deanlyoung <dlyoung119@me.com>
2016-07-23 07:48:13 +0100

    2016-07-22's java workout

---
+++ contributions/deanlyoung/java/I/O/2016-07-22.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Checking for the existence of files and directories
+Reading file attributes
+Retrieving file store attributes
