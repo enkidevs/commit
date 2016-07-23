696f928151386c09ce7a1bb8fb5fddebc519ccfb
colinular <colinular@gmail.com>
2016-07-23 06:14:00 +0100

    2016-07-22's javascript workout

---
+++ contributions/colinular/javascript/Node/2016-07-22.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+`process.nextTick()` callback pattern
+Middleware/pipeline design pattern
