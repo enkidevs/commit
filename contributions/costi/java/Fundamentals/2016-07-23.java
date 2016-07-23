c721a173756095c68c4268790dffa4efd5787d95
costi <assaf2b@gmail.com>
2016-07-23 12:42:25 +0100

    2016-07-23's java workout

---
+++ contributions/costi/java/Fundamentals/2016-07-23.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+How to play a sound file in Java
+Common mistake on switch statements
+Connecting to FTP using Java
+Do not return *references* to private *mutable* class members
