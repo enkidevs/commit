1126137b9854e8cc042dd401763e34fce1a26e3e
Zanex <alessio.zanella88@gmail.com>
2016-07-23 12:06:08 +0100

    2016-07-23's java workout

---
+++ contributions/Zanex/java/Fundamentals/2016-07-23.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
