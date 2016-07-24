a57fd16652911311ffc0764d01e6cac1964acdae
alebaffa <alessandro.baffa@gmail.com>
2016-07-24 06:19:50 +0100

    2016-07-24's java workout

---
+++ contributions/alebaffa/java/Fundamentals/2016-07-24.java
@@ -0,0 +1,5 @@
+Cropping images
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
