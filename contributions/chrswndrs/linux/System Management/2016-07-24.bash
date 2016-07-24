594616bd40a003cf07ccf78ceeab2782ab2d3cec
chrswndrs <christoph.wanders@gmail.com>
2016-07-24 20:14:57 +0100

    2016-07-24's linux workout

---
+++ contributions/chrswndrs/linux/System Management/2016-07-24.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Find out if the system's architecture is 32 or 64 bit
+Common log files
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
