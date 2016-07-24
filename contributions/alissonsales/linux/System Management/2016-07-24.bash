6c7f18aa7f816edc6a535978055736eb127d74ab
alissonsales <alissonsales@live.com>
2016-07-24 13:39:12 +0100

    2016-07-24's linux workout

---
+++ contributions/alissonsales/linux/System Management/2016-07-24.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
