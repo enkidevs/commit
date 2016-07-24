b82c15ee535e8989cbc8e255354ba38a732d754f
Tanker405th <thenuttyirishman@gmail.com>
2016-07-24 01:28:56 +0100

    2016-07-23's linux workout

---
+++ contributions/Tanker405th/linux/File Management/2016-07-23.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Deleting non-empty directories
+Working with `.gz` files
