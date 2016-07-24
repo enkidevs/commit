2a4a584d53bfd4fbf68fb97665c550157cd7a327
tremerhl <tremerhl@gmail.com>
2016-07-24 09:13:35 +0100

    2016-07-24's linux workout

---
+++ contributions/tremerhl/linux/Terminals & Shells/2016-07-24.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
