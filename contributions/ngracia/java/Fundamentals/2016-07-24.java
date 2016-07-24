4aea65a4d573a0e3ce93a8eb298f97cd05835baf
ngracia <nestor.gracia@gmail.com>
2016-07-24 20:00:49 +0100

    2016-07-24's java workout

---
+++ contributions/ngracia/java/Fundamentals/2016-07-24.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
