217e1e9975c4abf090355d139cfd95c875e3f804
drnanjo <drnanjo@aol.com>
2016-07-24 22:57:24 +0100

    2016-07-24's java workout

---
+++ contributions/drnanjo/java/Fundamentals/2016-07-24.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
