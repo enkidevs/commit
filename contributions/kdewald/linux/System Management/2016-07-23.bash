6ced9c2c1a866653174521346002ab548633ec9d
kdewald <kevin.dewald@ieee.org>
2016-07-24 13:30:29 +0100

    2016-07-23's linux workout

---
+++ contributions/kdewald/linux/System Management/2016-07-23.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Cancel a system shutdown
