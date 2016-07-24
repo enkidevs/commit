0bd7a61a00f9bb196b88695f3b0520d378a7c19d
spideynn <spideynn@gmail.com>
2016-07-24 18:08:18 +0100

    2016-07-24's java workout

---
+++ contributions/spideynn/java/Fundamentals/2016-07-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
