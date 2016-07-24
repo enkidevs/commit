ad5b7eb15aa337340c51e5212ed8974cebd06f00
mgarrett <melissa_garrett@hotmail.com>
2016-07-24 14:05:35 +0100

    2016-07-24's java workout

---
+++ contributions/mgarrett/java/Error Handling/2016-07-24.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
