69f0fc8b5a1b391d3c5b72b979c4d9bebc934b8c
fffl <kuepfer.christopher@gmail.com>
2016-07-24 19:05:22 +0100

    2016-07-24's java workout

---
+++ contributions/fffl/java/Classes & Objects/2016-07-24.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Using `static` fields
+Final object contents are not immutable
+How and when to use `WeakHashMap` class
+Fields with Java **Reflection API**
