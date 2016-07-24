8af892c0ae3179cc7f9cb31fb4ca56230db62185
Deveshj <deveshj4@gmail.com>
2016-07-24 17:03:07 +0100

    2016-07-24's java workout

---
+++ contributions/Deveshj/java/Fundamentals/2016-07-24.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
