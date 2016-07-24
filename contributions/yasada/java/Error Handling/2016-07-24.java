d0ffd619449ec4d21847390116d27eec645f70ce
yasada <y.asada216@gmail.com>
2016-07-25 00:24:42 +0100

    2016-07-24's java workout

---
+++ contributions/yasada/java/Error Handling/2016-07-24.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Careful Numeric Data Conversions
