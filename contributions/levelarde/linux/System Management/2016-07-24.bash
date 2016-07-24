c39b89d68c423f434b5129e2f5e50d5aa43189a8
levelarde <leonardo.velarde.ldvc@gmail.com>
2016-07-24 19:01:50 +0100

    2016-07-24's linux workout

---
+++ contributions/levelarde/linux/System Management/2016-07-24.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
