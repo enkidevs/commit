32b14dea71ab91d2543ad9a7de946493d3f454be
sumitspace <sumitthewildspirit@gmail.com>
2016-07-24 16:20:37 +0100

    2016-07-23's linux workout

---
+++ contributions/sumitspace/linux/Terminals & Shells/2016-07-23.bash
@@ -0,0 +1,7 @@
+Clear the terminal instantly
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
