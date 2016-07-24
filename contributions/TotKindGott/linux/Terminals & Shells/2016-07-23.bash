d1ed4ef6b3a27d5d15a051a32a4a32caa3c41e7d
TotKindGott <konstantin.a.kolosov@gmail.com>
2016-07-24 01:52:47 +0100

    2016-07-23's linux workout

---
+++ contributions/TotKindGott/linux/Terminals & Shells/2016-07-23.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Terminal incognito mode
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
