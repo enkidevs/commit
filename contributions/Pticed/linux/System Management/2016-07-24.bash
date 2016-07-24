63b5aab07515be60275c270784c25cee25ec6542
Pticed <cedric.finance@finandric.com>
2016-07-24 08:16:50 +0100

    2016-07-24's linux workout

---
+++ contributions/Pticed/linux/System Management/2016-07-24.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
