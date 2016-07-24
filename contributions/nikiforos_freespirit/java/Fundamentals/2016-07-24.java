23b33d4683a15c2fe773af52c524f11e3efeb964
nikiforos_freespirit <michele.caci@gmail.com>
2016-07-24 22:48:24 +0100

    2016-07-24's java workout

---
+++ contributions/nikiforos_freespirit/java/Fundamentals/2016-07-24.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
