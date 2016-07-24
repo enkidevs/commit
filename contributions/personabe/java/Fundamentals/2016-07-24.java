fce8df5c1a068dd15d006e7a27633f23f073be37
personabe <personabe1984@gmail.com>
2016-07-25 00:29:46 +0100

    2016-07-24's java workout

---
+++ contributions/personabe/java/Fundamentals/2016-07-24.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use meaningful names
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
