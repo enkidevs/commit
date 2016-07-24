8f392cd19c3c5b61e84a52794f49195780af5a63
shockawave123 <shockawave123@gmail.com>
2016-07-24 18:00:54 +0100

    2016-07-24's javascript workout

---
+++ contributions/shockawave123/javascript/Node/2016-07-24.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
