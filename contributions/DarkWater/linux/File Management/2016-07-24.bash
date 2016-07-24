5b875885b2b3d744cf3d92b7e575b357e3bcf10b
DarkWater <darkwater666@mail.ru>
2016-07-24 13:52:11 +0100

    2016-07-24's linux workout

---
+++ contributions/DarkWater/linux/File Management/2016-07-24.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Extracting `tar` files to a specific directory
+Using `touch` to alter files modification time
+Working with `.gz` files
+Find the unknown process preventing deleting of files
