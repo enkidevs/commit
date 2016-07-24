f33c53438ef6dce6f4449eedf50158615ae4d52f
Mukarr <mukarramtailor.02@gmail.com>
2016-07-24 18:48:11 +0100

    2016-07-23's java workout

---
+++ contributions/Mukarr/java/Fundamentals/2016-07-23.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
