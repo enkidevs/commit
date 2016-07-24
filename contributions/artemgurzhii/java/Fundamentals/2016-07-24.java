37cc569622a6e03d7e3eeb9ece8d856353933e47
artemgurzhii <artem.gurzhii@gmail.com>
2016-07-24 18:41:39 +0100

    2016-07-24's java workout

---
+++ contributions/artemgurzhii/java/Fundamentals/2016-07-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
