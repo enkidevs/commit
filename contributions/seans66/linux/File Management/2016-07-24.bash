64772d2c3a568926bd46176fc83156dd3f382f6b
seans66 <seanstreet@hotmail.com>
2016-07-25 00:13:00 +0100

    2016-07-24's linux workout

---
+++ contributions/seans66/linux/File Management/2016-07-24.bash
@@ -0,0 +1,7 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
