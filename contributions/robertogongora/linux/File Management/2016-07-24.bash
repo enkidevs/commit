663a0006191e775c1f1c34f351726ce19336b4e4
robertogongora <roberto@gongora.mx>
2016-07-25 00:38:48 +0100

    2016-07-24's linux workout

---
+++ contributions/robertogongora/linux/File Management/2016-07-24.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm -rf`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
