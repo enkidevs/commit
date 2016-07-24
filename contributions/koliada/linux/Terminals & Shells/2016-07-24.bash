91633c335e33e8c940d84856e218111f614446dd
koliada <alex.fiator@gmail.com>
2016-07-24 09:15:54 +0100

    2016-07-24's linux workout

---
+++ contributions/koliada/linux/Terminals & Shells/2016-07-24.bash
@@ -0,0 +1,7 @@
+Quick `bash` shortcuts 
+Terminal based browser
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
