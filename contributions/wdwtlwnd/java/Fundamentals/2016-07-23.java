e5a0be7834b36972ae1766b68bde1306d5f37be8
wdwtlwnd <wdwtlwnd@gmail.com>
2016-07-24 16:30:58 +0100

    2016-07-23's java workout

---
+++ contributions/wdwtlwnd/java/Fundamentals/2016-07-23.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Common mistake on switch statements
+Connecting to FTP using Java
+Distinction between `public` and `private` methods
+Using `synchronized` statements
