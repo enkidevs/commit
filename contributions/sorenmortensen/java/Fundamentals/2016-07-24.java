6a30fbc7238f1975654c7184f1faf981c81e30a5
sorenmortensen <soren@sorenmortensen.com>
2016-07-24 08:48:43 +0100

    2016-07-24's java workout

---
+++ contributions/sorenmortensen/java/Fundamentals/2016-07-24.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
