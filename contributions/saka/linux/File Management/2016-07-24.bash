5b057b0cea60783dcb9808e0a46646358a1cb6d0
saka <saka71996@gmail.com>
2016-07-24 06:39:54 +0100

    2016-07-24's linux workout

---
+++ contributions/saka/linux/File Management/2016-07-24.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm -rf`
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
