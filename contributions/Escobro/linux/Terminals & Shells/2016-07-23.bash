ea1fbf03bb0102a093d411dadda7eadaf54a9dbe
Escobro <escobar.michael2@gmail.com>
2016-07-24 01:51:14 +0100

    2016-07-23's linux workout

---
+++ contributions/Escobro/linux/Terminals & Shells/2016-07-23.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Find any Unix / Linux command
+Quick `bash` shortcuts 
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
