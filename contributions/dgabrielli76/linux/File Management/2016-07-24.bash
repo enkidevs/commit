f408fd63961b47d67d01b69e91af904d226271a1
dgabrielli76 <dgabrielli76@gmail.com>
2016-07-24 10:05:04 +0100

    2016-07-24's linux workout

---
+++ contributions/dgabrielli76/linux/File Management/2016-07-24.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
