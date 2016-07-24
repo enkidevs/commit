cf704b65eda5d509f579988913378eae032c1a69
shakedl <shaked.lokits@gmail.com>
2016-07-24 15:28:05 +0100

    2016-07-24's javascript workout

---
+++ contributions/shakedl/javascript/Browser APIs/2016-07-24.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+CSS for when JavaScript is enabled
+Form a `URL` from its parts 
+Interaction with the user
+Drawing with HTML5 `Canvas` API
