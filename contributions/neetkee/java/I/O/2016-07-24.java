10c347a6725b98f052fe2955902ed8e03b1f6e5f
neetkee <neetkee@live.com>
2016-07-24 17:49:59 +0100

    2016-07-24's java workout

---
+++ contributions/neetkee/java/I/O/2016-07-24.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to test if file or directory exists in Java
+Retrieving file store attributes
+Listing a file system's root directories
+The `Console` class
