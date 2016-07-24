ef5d54e0a810e4c882817a0f488298486c05f5ab
aranganathan <arangates@gmail.com>
2016-07-24 05:21:40 +0100

    2016-07-23's javascript workout

---
+++ contributions/aranganathan/javascript/Core/2016-07-23.js
@@ -0,0 +1,5 @@
+Shuffle an array using `sort` and `random`.
+`if/else` shortcut `conditional operator`
+Use `splice()` to remove an item from an array
+Using assignment operators
+Inaccuracy of binary floating-point format
