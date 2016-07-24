bf55f4c3799e36c48ce9643b56b353d8996e62b3
KPR1976 <pkinyapin+enki@gmail.com>
2016-07-24 06:49:49 +0100

    2016-07-24's linux workout

---
+++ contributions/KPR1976/linux/File Management/2016-07-24.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm -rf`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
+Working with `.gz` files
