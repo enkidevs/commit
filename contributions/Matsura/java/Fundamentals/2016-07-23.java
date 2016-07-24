b800b471e016f6084912a4da71f6b465cf541cac
Matsura <snipeh@gmail.com>
2016-07-24 01:26:20 +0100

    2016-07-23's java workout

---
+++ contributions/Matsura/java/Fundamentals/2016-07-23.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
+Using an interface as a parameter
