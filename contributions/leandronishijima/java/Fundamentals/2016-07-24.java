117ef9529d502cd84f0ceebb1f759ec4d30cc4b7
leandronishijima <leandro.nishijima@gmail.com>
2016-07-24 16:10:12 +0100

    2016-07-24's java workout

---
+++ contributions/leandronishijima/java/Fundamentals/2016-07-24.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Methods performing *Security Checks* must be declared `Private` or `Final`
