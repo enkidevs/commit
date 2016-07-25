b598d1bd0d76de9a5ab47ef7b23ca3aac0638e18
swap <swapnilagarwal1994@gmail.com>
2016-07-25 16:44:26 +0100

    2016-07-24's java workout

---
+++ contributions/swap/java/Error Handling/2016-07-24.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using exceptions in Java
+Handling Null Pointer Exceptions
+Most common reason behind **stack overflow** error
+Throwing an `exception`
