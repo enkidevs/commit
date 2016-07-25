f314dfecc4a2d9753fe47f07681cdce367ddfeee
davidodw <dodwei.dev@gmail.com>
2016-07-25 05:25:53 +0100

    2016-07-24's linux workout

---
+++ contributions/davidodw/linux/System Management/2016-07-24.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
