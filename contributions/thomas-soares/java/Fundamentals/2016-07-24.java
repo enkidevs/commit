f9b83a955a64d971e9b32fef113aa7839ba026f6
thomas-soares <thomas__soares@hotmail.com>
2016-07-25 02:07:53 +0100

    2016-07-24's java workout

---
+++ contributions/thomas-soares/java/Fundamentals/2016-07-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Detect or prevent integer overflow
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
