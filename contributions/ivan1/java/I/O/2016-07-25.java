7907568915b0d278da8f6adbf5bc04a8c4669f45
ivan1 <ivanli@kth.se>
2016-07-25 08:44:36 +0100

    2016-07-25's java workout

---
+++ contributions/ivan1/java/I/O/2016-07-25.java
@@ -0,0 +1,5 @@
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Reading file attributes
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
