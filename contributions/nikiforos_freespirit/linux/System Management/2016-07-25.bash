56d44329bf46eee4d196047014830f4b09df77a2
nikiforos_freespirit <michele.caci@gmail.com>
2016-07-25 07:48:44 +0100

    2016-07-25's linux workout

---
+++ contributions/nikiforos_freespirit/linux/System Management/2016-07-25.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Detect your linux distribution
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
