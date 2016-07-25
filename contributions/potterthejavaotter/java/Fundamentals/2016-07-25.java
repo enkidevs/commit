8c643c255a8d3aae71796a2bbf41cf3ee826ecb0
potterthejavaotter <mslee03@syr.edu>
2016-07-25 13:47:49 +0100

    2016-07-25's java workout

---
+++ contributions/potterthejavaotter/java/Fundamentals/2016-07-25.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Methods performing *Security Checks* must be declared `Private` or `Final`
