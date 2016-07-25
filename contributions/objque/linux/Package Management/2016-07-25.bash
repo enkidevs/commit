f30f97a3555506256fdd55ac63206703083afe3b
objque <wtf.object@gmail.com>
2016-07-26 00:11:20 +0100

    2016-07-25's linux workout

---
+++ contributions/objque/linux/Package Management/2016-07-25.bash
@@ -0,0 +1,5 @@
+Install specific version of packages
+Installing a `.deb` package from the terminal
+Get `apt` to use a mirror / faster mirror
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
