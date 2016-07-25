b45c4188589b2dc8cdec413f3e79f6df1ed4bb42
urvaratyagi <urvaratyagi95@gmail.com>
2016-07-25 17:22:34 +0100

    2016-07-24's java workout

---
+++ contributions/urvaratyagi/java/Fundamentals/2016-07-24.java
@@ -0,0 +1,4 @@
+Java naming conventions
+Altering format string output by changing a format specifier's `argument_index`
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Implementing an `interface`
