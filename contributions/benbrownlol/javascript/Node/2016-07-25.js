4de7a50e808963f2350f88d4cb1d351f6f5c5b86
benbrownlol <3en.brown@gmail.com>
2016-07-25 07:42:18 +0100

    2016-07-25's javascript workout

---
+++ contributions/benbrownlol/javascript/Node/2016-07-25.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+Middleware/pipeline design pattern
+Avoid using `sudo` when running Node
