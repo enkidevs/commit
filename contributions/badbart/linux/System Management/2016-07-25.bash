41574afcdcfc5fc2361b71fa42854a60aa21ed95
badbart <tom.zille@gmail.com>
2016-07-25 06:30:30 +0100

    2016-07-25's linux workout

---
+++ contributions/badbart/linux/System Management/2016-07-25.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Executing scripts on system boot
+Commands to shutdown or restart the system
