9dbff5e1c8f12a185f3328547b642dffeab483fc
arytbk <arytebeka@mac.com>
2016-07-25 10:05:52 +0100

    2016-07-25's linux workout

---
+++ contributions/arytbk/linux/File Management/2016-07-25.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm -rf`
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Working with `.gz` files
