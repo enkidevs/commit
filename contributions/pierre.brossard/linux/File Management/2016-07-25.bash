37ce24e94fd8ba23ab1f7d21bd4a093a32c1730e
pierre.brossard <pierre.brossard@apptitude.ch>
2016-07-25 08:48:59 +0100

    2016-07-25's linux workout

---
+++ contributions/pierre.brossard/linux/File Management/2016-07-25.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm -rf`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Deleting non-empty directories
