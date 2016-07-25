2c0f724a1e27675884137ab36ae16fe0b8968b04
kyezil <louisclergue@gmail.com>
2016-07-25 16:24:26 +0100

    2016-07-25's javascript workout

---
+++ contributions/kyezil/javascript/Functions/2016-07-25.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Method chaining
+Function call method
+Move cursor at the end of text input
