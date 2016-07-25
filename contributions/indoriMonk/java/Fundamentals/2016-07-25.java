8cf24f1a7f6d74ecda8ce235ed65167a85e919a5
indoriMonk <amanshulvyas@gmail.com>
2016-07-25 16:57:44 +0100

    2016-07-25's java workout

---
+++ contributions/indoriMonk/java/Fundamentals/2016-07-25.java
@@ -0,0 +1,5 @@
+Distinction between `public` and `private` methods
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
