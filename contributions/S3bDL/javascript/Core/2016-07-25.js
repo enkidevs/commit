468748f5779f329cd603a700b00e349149a198c7
S3bDL <sebastien.delarquier@gmail.com>
2016-07-25 16:47:54 +0100

    2016-07-25's javascript workout

---
+++ contributions/S3bDL/javascript/Core/2016-07-25.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Setting the length of an array 
+Hoisting applies only to variable declarations, not initializations
+`catch` is block scoped
+How to merge two arrays
