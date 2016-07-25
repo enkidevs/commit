57576ab0d75ff6350ac60711a55e0e8d3699c2e3
gpapam <gpapam@villosophy.com>
2016-07-25 17:12:59 +0100

    2016-07-25's linux workout

---
+++ contributions/gpapam/linux/System Management/2016-07-25.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
