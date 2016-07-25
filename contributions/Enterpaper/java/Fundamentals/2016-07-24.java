23eabfb2fe5f90a05e253873ad1b33627338b208
Enterpaper <byebye99@qq.com>
2016-07-25 01:44:37 +0100

    2016-07-24's java workout

---
+++ contributions/Enterpaper/java/Fundamentals/2016-07-24.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
