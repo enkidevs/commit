f7fb2ad9ae11aabde955ea629b329fa9671af90d
nathang21 <nathanpguenther@gmail.com>
2016-07-25 15:10:02 +0100

    2016-07-25's javascript workout

---
+++ contributions/nathang21/javascript/Core/2016-07-25.js
@@ -0,0 +1,5 @@
+Setting default values with `short circuiting`
+Most efficient way to build `HTML` strings
+Use `splice()` to remove an item from an array
+The distinction between `==` and `===`
+Prototype methods
