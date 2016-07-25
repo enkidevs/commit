7ee33b2c4885b28fc2c82837a02003eaee32aa04
colinular <colinular@gmail.com>
2016-07-25 16:23:39 +0100

    2016-07-24's linux workout

---
+++ contributions/colinular/linux/System Management/2016-07-24.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
