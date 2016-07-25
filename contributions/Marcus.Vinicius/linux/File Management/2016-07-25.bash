16b2711e11e807db5f807d26514cbdb9c8cca262
Marcus.Vinicius <marcusviniciusaguiar@gmail.com>
2016-07-25 14:03:50 +0100

    2016-07-25's linux workout

---
+++ contributions/Marcus.Vinicius/linux/File Management/2016-07-25.bash
@@ -0,0 +1,5 @@
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
+Working with `.gz` files
+Find the unknown process preventing deleting of files
