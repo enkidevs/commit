82a9b65f58de29757b9fed7191c38fc0fab7da17
Heidik <heidiks@gmail.com>
2016-07-25 13:34:17 +0100

    2016-07-25's java workout

---
+++ contributions/Heidik/java/Fundamentals/2016-07-25.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Cropping images
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Limit Accessibility of `Fields`
