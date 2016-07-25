d7fa0be77b575a12f9b49d1d228a9a83e4851fda
nicolasmdf <nicolasmdf@gmail.com>
2016-07-25 10:20:23 +0100

    2016-07-25's java workout

---
+++ contributions/nicolasmdf/java/Fundamentals/2016-07-25.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
