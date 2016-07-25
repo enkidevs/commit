8f94c236587e9ecabc4c3b4fe47fa2f76545e139
benoliver999 <benoliver999@gmail.com>
2016-07-25 08:51:23 +0100

    2016-07-25's linux workout

---
+++ contributions/benoliver999/linux/File Management/2016-07-25.bash
@@ -0,0 +1,6 @@
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
