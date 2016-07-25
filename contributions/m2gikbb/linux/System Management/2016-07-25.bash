158b31a80c154d700e7bd22e8459692a8ff5612c
m2gikbb <machnik.pawel@gmail.com>
2016-07-25 09:28:35 +0100

    2016-07-25's linux workout

---
+++ contributions/m2gikbb/linux/System Management/2016-07-25.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
