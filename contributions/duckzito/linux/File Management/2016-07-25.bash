2f8355e90eae1160fc1f570e4e574c518afb5ea9
duckzito <duckzito@gmail.com>
2016-07-25 22:26:23 +0100

    2016-07-25's linux workout

---
+++ contributions/duckzito/linux/File Management/2016-07-25.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm -rf`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
