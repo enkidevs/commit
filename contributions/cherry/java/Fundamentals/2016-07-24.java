cf8c34eaaff4c2fc0a8b3c7c9bfb1e1fb7dc9c01
cherry <cherry.naresh52@gmail.com>
2016-07-25 11:23:10 +0100

    2016-07-24's java workout

---
+++ contributions/cherry/java/Fundamentals/2016-07-24.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+How to play a sound file in Java
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
