356ac4de728c2cea005496a2f6dd1e42a216e8be
z3t0 <zetoslab@gmail.com>
2016-07-25 01:07:27 +0100

    2016-07-24's linux workout

---
+++ contributions/z3t0/linux/Networking/2016-07-24.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Check `iptables` firewall status
+Get real network statistics with `slurm`
+List your IPv6 configuration
+Use `tcpdump` to listen to network interface traffic
