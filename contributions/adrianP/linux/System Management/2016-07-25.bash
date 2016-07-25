8725678e3b040d72a9bdaf2db8e3f1e64ec6bb6f
adrianP <plesca.adrian@gmail.com>
2016-07-25 05:00:01 +0100

    2016-07-25's linux workout

---
+++ contributions/adrianP/linux/System Management/2016-07-25.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
