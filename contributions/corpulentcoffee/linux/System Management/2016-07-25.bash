302b106df9d398ae7762946d688a1cec5ee105ec
corpulentcoffee <dave@corpulentcoffee.com>
2016-07-25 14:06:50 +0100

    2016-07-25's linux workout

---
+++ contributions/corpulentcoffee/linux/System Management/2016-07-25.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
