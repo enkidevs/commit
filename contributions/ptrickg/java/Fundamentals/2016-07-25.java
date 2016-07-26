0e11448fb08bffbe0172526e8f12dabaf9dc2e29
ptrickg <ptrickg@hotmail.com>
2016-07-26 01:45:38 +0100

    2016-07-25's java workout

---
+++ contributions/ptrickg/java/Fundamentals/2016-07-25.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+The trade-off between short and descriptive names
+Ternary operator
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
