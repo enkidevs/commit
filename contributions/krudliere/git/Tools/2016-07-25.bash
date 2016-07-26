f88e07573d1bf2ed420e1ff246c055093562838e
krudliere <mcrudder@gmail.com>
2016-07-26 06:27:20 +0100

    2016-07-25's git workout

---
+++ contributions/krudliere/git/Tools/2016-07-25.bash
@@ -0,0 +1,5 @@
+Interactively unstage changes
+Use a short checksum to find a commit
+Specify a commit by its ancestry
+Specify a range of commits using double dot syntax
+Create a new branch from a stash
