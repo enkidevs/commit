6a526983a77836ade60f19ed5ceae46e857a5728
WetwareDefect <john.piatras@gmail.com>
2016-07-26 15:45:35 +0100

    2016-07-26's java workout

---
+++ contributions/WetwareDefect/java/I/O/2016-07-26.java
@@ -0,0 +1,5 @@
+How to test if file or directory exists in Java
+Create system file paths using the `Path` class
+Using buffered streams
+List contents of a directory with `newDirectoryStream`
+Deleting files and directories
