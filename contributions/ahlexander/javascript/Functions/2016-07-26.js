cfd676ecb0dd41955bf9eaa21230a905f6be374e
ahlexander <alex@devmotion.ro>
2016-07-26 11:14:59 +0100

    2016-07-26's javascript workout

---
+++ contributions/ahlexander/javascript/Functions/2016-07-26.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Social sharing without widgets
+Check if a document is done loading
