56cc40d002d7fdab28ef7f3abc053a70bf2e7530
neetkee <neetkee@live.com>
2016-07-26 08:46:38 +0100

    2016-07-26's java workout

---
+++ contributions/neetkee/java/Classes & Objects/2016-07-26.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Java object references
+Compare classes and not class names
+Final object contents are not immutable
+String formatting and parsing with `SimpleDateFormat`
