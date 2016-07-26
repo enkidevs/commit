251b5a919b61fd46af62e5f96d47710b91b0372a
vlad_eremia <marian.vlad.eremia@gmail.com>
2016-07-26 08:51:05 +0100

    2016-07-26's java workout

---
+++ contributions/vlad_eremia/java/I/O/2016-07-26.java
@@ -0,0 +1,5 @@
+How to test if file or directory exists in Java
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
