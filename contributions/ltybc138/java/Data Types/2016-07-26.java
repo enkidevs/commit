a02f1787851fb059124ef25282e610bf0951a4e4
ltybc138 <semenov200108@gmail.com>
2016-07-26 16:34:13 +0100

    2016-07-26's java workout

---
+++ contributions/ltybc138/java/Data Types/2016-07-26.java
@@ -0,0 +1,5 @@
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
+Numeric Conversion - Widening
