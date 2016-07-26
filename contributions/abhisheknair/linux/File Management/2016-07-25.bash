cb2ad910f4986d7257f938fedd2cc80a09e2331b
abhisheknair <abhisheknair@live.in>
2016-07-26 17:37:31 +0100

    2016-07-25's linux workout

---
+++ contributions/abhisheknair/linux/File Management/2016-07-25.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
