587224c7309f14cd4c54115f5080092c365690be
orenmizr <oren.mizrahi.inc@gmail.com>
2016-07-26 01:30:25 +0100

    2016-07-26's git workout

---
+++ contributions/orenmizr/git/Tools/2016-07-26.bash
@@ -0,0 +1,5 @@
+Specify a commit by its ancestry
+Specify a range of commits using double dot syntax
+Remember the results of previous hunk conflicts
+Debug using binary search
+Sharing data by bundling
