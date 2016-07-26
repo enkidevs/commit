d9e6a659b96c3978577f3892580301359fee9a79
laurent83 <laurent.morissette@gmail.com>
2016-07-26 18:36:27 +0100

    2016-07-26's java workout

---
+++ contributions/laurent83/java/Fundamentals/2016-07-26.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Limit Accessibility of `Fields`
