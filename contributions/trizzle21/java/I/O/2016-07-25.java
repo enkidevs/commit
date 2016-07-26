b07afcec36b1ffdae459dac1fec7a1d0757cb218
trizzle21 <tdross21@gmail.com>
2016-07-26 06:46:14 +0100

    2016-07-25's java workout

---
+++ contributions/trizzle21/java/I/O/2016-07-25.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+How to test if file or directory exists in Java
+Checking for the existence of files and directories
+Listing a file system's root directories
