9b5fa0e8dd08922d1a0e17790aa284938b4c962c
naveen.mannam <naveen.mannam@gmail.com>
2016-07-26 01:35:37 +0100

    2016-07-25's linux workout

---
+++ contributions/naveen.mannam/linux/Terminals & Shells/2016-07-25.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear bash history
+Random password generator
+`else` statements using the `||` operator 
