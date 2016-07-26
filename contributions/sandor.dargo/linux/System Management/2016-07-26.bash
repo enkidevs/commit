e7bf1fcf7372eacba1488831a2fb8ebcd63d22ad
sandor.dargo <sandor.dargo@gmail.com>
2016-07-26 05:14:32 +0100

    2016-07-26's linux workout

---
+++ contributions/sandor.dargo/linux/System Management/2016-07-26.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+List current logged on users with `w`
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
