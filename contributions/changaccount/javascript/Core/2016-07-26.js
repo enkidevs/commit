1adebca8bc94296c5b2fd2ea5d47df9bb32114f7
changaccount <emailkathy@gmail.com>
2016-07-26 18:38:07 +0100

    2016-07-26's javascript workout

---
+++ contributions/changaccount/javascript/Core/2016-07-26.js
@@ -0,0 +1,5 @@
+Setting default values with `short circuiting`
+Hoisting applies only to variable declarations, not initializations
+`catch` is block scoped
+Check if a function exists
+Detect online connection
