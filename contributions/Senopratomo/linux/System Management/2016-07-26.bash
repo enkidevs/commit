2d32ea2c51afa4b2093eef6a330d6e211fc7bf31
Senopratomo <lord.ernesto@gmail.com>
2016-07-26 18:10:57 +0100

    2016-07-26's linux workout

---
+++ contributions/Senopratomo/linux/System Management/2016-07-26.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
