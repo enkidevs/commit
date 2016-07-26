9c931fd2529c7054977ce1360460c774e4924c1c
robin.xu <robin.qd.2011@gmail.com>
2016-07-26 22:48:32 +0100

    2016-07-26's linux workout

---
+++ contributions/robin.xu/linux/System Management/2016-07-26.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Common log files
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
