91b563879084fa95084551673fdd0b862a3f48f4
agr2003 <agr2003@gmail.com>
2016-07-26 09:37:40 +0100

    2016-07-26's java workout

---
+++ contributions/agr2003/java/Fundamentals/2016-07-26.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+How to play a sound file in Java
+Locks in `static synchronized` methods
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
