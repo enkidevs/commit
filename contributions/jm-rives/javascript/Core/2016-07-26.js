f81410529480ccab5c4f3fb2896ddec4f2d9b979
jm-rives <joanna.rives@gmail.com>
2016-07-26 18:02:57 +0100

    2016-07-26's javascript workout

---
+++ contributions/jm-rives/javascript/Core/2016-07-26.js
@@ -0,0 +1,5 @@
+Using `.test()` with RegExp
+The distinction between `==` and `===`
+Apply `map` function to array items
+Extra function arguments are undefined by default
+Detect **DO NOT TRACK** status
