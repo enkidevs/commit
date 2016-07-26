16d9c6b158c7b5ec0ea84a85c74a16f62879c673
Toukun <toukunproduction@gmail.com>
2016-07-26 01:49:51 +0100

    2016-07-25's java workout

---
+++ contributions/Toukun/java/Fundamentals/2016-07-25.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
