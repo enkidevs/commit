e836969209d10b831740e9d3d686a13a5a29a3ac
fchristopher239 <fchristopher239@gmail.com>
2016-07-26 17:37:54 +0100

    2016-07-26's java workout

---
+++ contributions/fchristopher239/java/Java 8/2016-07-26.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Using `Arrays.parallelSort` 
+How to check for static dependencies of applications and libraries from the command line.
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
