b2d2f4d9bec3dc3cc7289e0897ad2ab6b0a95152
wowplus <wowplus2@gmail.com>
2016-07-26 03:33:50 +0100

    2016-07-25's java workout

---
+++ contributions/wowplus/java/Fundamentals/2016-07-25.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use meaningful names
+Using `Printstream.format` to format a string before printing
+Using `synchronized` statements
+How range operations work
