8a1d0035235bada8bddc43062c3afba1ceeb20d0
seamsay <srm.1708@gmail.com>
2016-07-26 17:11:46 +0100

    2016-07-26's linux workout

---
+++ contributions/seamsay/linux/File Management/2016-07-26.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm -rf`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Find the unknown process preventing deleting of files
+Easy way of sharing files
