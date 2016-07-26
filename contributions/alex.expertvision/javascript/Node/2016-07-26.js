503ab2449675f925a7266f0f573592799dfe75ad
alex.expertvision <alexandru.balasa@rocketmail.com>
2016-07-26 09:00:30 +0100

    2016-07-26's javascript workout

---
+++ contributions/alex.expertvision/javascript/Node/2016-07-26.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Http Server in **Node**
+Global Objects and Environment Variables in **Node**
+Use conventional property names for error handling in Node.js
+**Node** `os` module
