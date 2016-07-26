90b73c0d79ae07d64f1b454e238c823671e2c91c
PatrikLord <patrik.j.lord@gmail.com>
2016-07-26 17:25:54 +0100

    2016-07-26's java workout

---
+++ contributions/PatrikLord/java/Fundamentals/2016-07-26.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
