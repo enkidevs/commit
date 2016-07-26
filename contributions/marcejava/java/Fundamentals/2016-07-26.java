44f69f2da23f6638db0f7c89edeecac1eb07d3bb
marcejava <marce7j@gmail.com>
2016-07-26 14:08:30 +0100

    2016-07-26's java workout

---
+++ contributions/marcejava/java/Fundamentals/2016-07-26.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The trade-off between short and descriptive names
+Distinction between `public` and `private` methods
+Updating interfaces by using `default` methods
+Implementing an `interface`
