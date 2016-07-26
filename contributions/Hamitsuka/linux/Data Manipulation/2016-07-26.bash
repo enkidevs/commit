a005b4d1db8ef625a8343ab2e374c58a5e79e675
Hamitsuka <ametluka@gmail.com>
2016-07-26 18:56:22 +0100

    2016-07-26's linux workout

---
+++ contributions/Hamitsuka/linux/Data Manipulation/2016-07-26.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+How to analyse a server's log files
+Using `lynx` to convert HTML to text
+Count non-empty lines with `nl` utility
+Image manipulation  using `convert`
