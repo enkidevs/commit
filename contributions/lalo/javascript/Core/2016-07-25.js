ffda2394b533136e3b8d2cc0623a6317fad49e29
lalo <leandro.gz73@gmail.com>
2016-07-26 03:15:38 +0100

    2016-07-25's javascript workout

---
+++ contributions/lalo/javascript/Core/2016-07-25.js
@@ -0,0 +1,5 @@
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Avoid using `with`
+Prototype methods
+Getting the *real* dimensions of an image
