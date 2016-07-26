78feca192e4d72cc94b471ad12ca8926b0e62620
blackbeard <sb1k994@gmail.com>
2016-07-26 18:40:36 +0100

    2016-07-25's linux workout

---
+++ contributions/blackbeard/linux/System Management/2016-07-25.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
