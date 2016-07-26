d7a6f1b881301253b1a827ea406c78eb5ed1a189
Beelzenef <elena.guzbla@gmail.com>
2016-07-26 08:45:19 +0100

    2016-07-26's java workout

---
+++ contributions/Beelzenef/java/Fundamentals/2016-07-26.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Using `Printstream.format` to format a string before printing
+Metadata: setting a file's owner
+Methods performing *Security Checks* must be declared `Private` or `Final`
+`Autoboxing` and `Unboxing`
