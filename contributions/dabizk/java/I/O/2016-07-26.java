19fca0937020b61f5554011499df43fd8636c4d5
dabizk <dabizk@yahoo.com>
2016-07-26 10:27:23 +0100

    2016-07-26's java workout

---
+++ contributions/dabizk/java/I/O/2016-07-26.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to test if file or directory exists in Java
+Reading and writing text files
+Reading file attributes
+List contents of a directory with `newDirectoryStream`
