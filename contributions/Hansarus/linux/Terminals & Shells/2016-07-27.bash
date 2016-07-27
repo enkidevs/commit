48cc9a1281537b02fa74e0fbf72bf2d4496d853f
Hansarus <jonniv86@gmail.com>
2016-07-27 10:48:00 +0100

    2016-07-27's linux workout

---
+++ contributions/Hansarus/linux/Terminals & Shells/2016-07-27.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
