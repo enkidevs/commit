d21da00ef294d500f2530d8e150cb76bcba8403c
pattyhama <strodr@gmail.com>
2016-07-27 15:17:32 +0100

    2016-07-26's linux workout

---
+++ contributions/pattyhama/linux/File Management/2016-07-26.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Working with `.gz` files
