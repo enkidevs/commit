70ab0db931ef0987d81fc1aae333634ffb34cfaa
teunissenstefan <stefanteunissen1@gmail.com>
2016-07-27 01:35:04 +0100

    2016-07-26's java workout

---
+++ contributions/teunissenstefan/java/Fundamentals/2016-07-26.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
