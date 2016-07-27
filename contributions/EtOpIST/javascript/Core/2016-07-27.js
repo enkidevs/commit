a0137f16c5b612b8dacbcf1bf155600442df2f2e
EtOpIST <etopist@outlook.com>
2016-07-27 06:40:23 +0100

    2016-07-27's javascript workout

---
+++ contributions/EtOpIST/javascript/Core/2016-07-27.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Setting default values with `short circuiting`
+Using the double tilde `~~` 
+Using `.test()` with RegExp
+The difference between null, undefined and NaN
