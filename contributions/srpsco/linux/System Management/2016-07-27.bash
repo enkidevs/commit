2cd8da7f6b37c4595d558c9ed7779b73dfbf8d31
srpsco <steverobillard@gmail.com>
2016-07-28 00:26:41 +0100

    2016-07-27's linux workout

---
+++ contributions/srpsco/linux/System Management/2016-07-27.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Finding Open Files With `lsof`
