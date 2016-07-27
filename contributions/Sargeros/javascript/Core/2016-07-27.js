e53f389caf2d27fc649ed8ad1848d91567c654d7
Sargeros <sargeros@gmail.com>
2016-07-27 08:29:31 +0100

    2016-07-27's javascript workout

---
+++ contributions/Sargeros/javascript/Core/2016-07-27.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Performing a function at timed intervals
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+`catch` is block scoped
