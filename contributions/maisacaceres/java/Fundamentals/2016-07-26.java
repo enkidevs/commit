7603dc5eda64b9eee1a0b67c72aaf6363b8e003b
maisacaceres <pedrografael@gmail.com>
2016-07-27 03:21:41 +0100

    2016-07-26's java workout

---
+++ contributions/maisacaceres/java/Fundamentals/2016-07-26.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Using `Printstream.format` to format a string before printing
+Supply `toString()` in all classes
