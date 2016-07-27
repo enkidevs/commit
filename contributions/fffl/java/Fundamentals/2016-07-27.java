179bb1b38c68a4f15a69aed99d4f762d8de59d43
fffl <kuepfer.christopher@gmail.com>
2016-07-27 18:01:38 +0100

    2016-07-27's java workout

---
+++ contributions/fffl/java/Fundamentals/2016-07-27.java
@@ -0,0 +1,5 @@
+Cropping images
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
