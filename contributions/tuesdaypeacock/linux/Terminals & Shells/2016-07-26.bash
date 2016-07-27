2ee380cd9c33e78f02a224a1520a50352a2e6a5c
tuesdaypeacock <tuesdaypeacock@gmail.com>
2016-07-27 14:51:24 +0100

    2016-07-26's linux workout

---
+++ contributions/tuesdaypeacock/linux/Terminals & Shells/2016-07-26.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Aliasing ssh connections
+Terminal incognito mode
+Sequential execution using the `;` statement separator
