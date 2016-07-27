2460501d6eb3eb08c0ab25df0a95a46ea2dab473
thorseye <jonas@thoursie.net>
2016-07-27 23:15:39 +0100

    2016-07-27's linux workout

---
+++ contributions/thorseye/linux/File Management/2016-07-27.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Delete commands aliases
