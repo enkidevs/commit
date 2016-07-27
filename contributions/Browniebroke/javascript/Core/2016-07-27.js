6c3f34a648c770d1a06dec569f60d60fe45e58c0
Browniebroke <alla.brunoo@gmail.com>
2016-07-27 08:09:21 +0100

    2016-07-27's javascript workout

---
+++ contributions/Browniebroke/javascript/Core/2016-07-27.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Meaning of polyfill
+Multi-line string variables
+Hoisting applies only to variable declarations, not initializations
+Getting the *real* dimensions of an image
