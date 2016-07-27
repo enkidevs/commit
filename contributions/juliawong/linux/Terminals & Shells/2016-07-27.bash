acb96c3676cbb1f1dd306ada24b1705ff2d7f9d7
juliawong <juliawong0@gmail.com>
2016-07-27 18:44:28 +0100

    2016-07-27's linux workout

---
+++ contributions/juliawong/linux/Terminals & Shells/2016-07-27.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
