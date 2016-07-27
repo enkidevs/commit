7a34d6584e95f1088407d38cecfbcbaafc535c0b
chrisdarden <c.darden@me.com>
2016-07-27 04:38:49 +0100

    2016-07-26's linux workout

---
+++ contributions/chrisdarden/linux/Terminals & Shells/2016-07-26.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Quick `bash` shortcuts 
+Useful aliasing in bash
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
