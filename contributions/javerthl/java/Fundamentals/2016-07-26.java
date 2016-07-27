9ff1cbd16a133616810df311416e06c7bcb2137f
javerthl <javertv@yahoo.com>
2016-07-27 06:11:06 +0100

    2016-07-26's java workout

---
+++ contributions/javerthl/java/Fundamentals/2016-07-26.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
