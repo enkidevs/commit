59b3d9a19e7e043bb404f78ddb0a99e0df1ad868
reginaexmachina <we.love.rachel.day.lots@gmail.com>
2016-07-27 03:54:45 +0100

    2016-07-26's linux workout

---
+++ contributions/reginaexmachina/linux/File Management/2016-07-26.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
