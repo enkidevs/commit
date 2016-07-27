9c3c4e0380294aa8ad673134b67606e8419ffac2
samschmidt <tgcapnsam@gmail.com>
2016-07-27 13:26:22 +0100

    2016-07-27's linux workout

---
+++ contributions/samschmidt/linux/System Management/2016-07-27.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Finding Open Files With `lsof`
