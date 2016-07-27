7025373cbe6971e998a84b31c93bc09181287041
agoowong <augustine.wongbh@gmail.com>
2016-07-27 02:39:30 +0100

    2016-07-26's java workout

---
+++ contributions/agoowong/java/Fundamentals/2016-07-26.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
+Using an interface as a parameter
