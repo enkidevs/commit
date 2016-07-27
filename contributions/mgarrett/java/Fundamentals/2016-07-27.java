d4ff5071f91d698443cdaee27e52c24f306ac1e9
mgarrett <melissa_garrett@hotmail.com>
2016-07-27 23:25:41 +0100

    2016-07-27's java workout

---
+++ contributions/mgarrett/java/Fundamentals/2016-07-27.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
+Using an interface as a parameter
