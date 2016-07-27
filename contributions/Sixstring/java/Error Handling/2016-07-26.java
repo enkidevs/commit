7147947aea41b2839132383ce96625ee3b25db74
Sixstring <davidbush4@gmail.com>
2016-07-27 08:16:44 +0100

    2016-07-26's java workout

---
+++ contributions/Sixstring/java/Error Handling/2016-07-26.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
