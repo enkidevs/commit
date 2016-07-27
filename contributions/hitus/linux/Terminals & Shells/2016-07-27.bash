5bd7b43547eb1bf5b9a7fa455f60ced0402b8458
hitus <hitus@hotmail.com>
2016-07-27 15:18:45 +0100

    2016-07-27's linux workout

---
+++ contributions/hitus/linux/Terminals & Shells/2016-07-27.bash
@@ -0,0 +1,7 @@
+Shebang `#!` explained 
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
