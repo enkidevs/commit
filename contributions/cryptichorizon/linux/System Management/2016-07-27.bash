b401cecba7928b2743ef74d293fecf4b98bd5ae6
cryptichorizon <cryptichorizon@gmail.com>
2016-07-27 07:05:34 +0100

    2016-07-27's linux workout

---
+++ contributions/cryptichorizon/linux/System Management/2016-07-27.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Removing old kernels in Debian based systems
+Basic service management with `systemd`
+Finding Open Files With `lsof`
