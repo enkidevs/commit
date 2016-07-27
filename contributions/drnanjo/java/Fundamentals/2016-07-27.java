6d75fe64818c81c4f3a011ac248d5cb19847afda
drnanjo <drnanjo@aol.com>
2016-07-27 20:50:24 +0100

    2016-07-27's java workout

---
+++ contributions/drnanjo/java/Fundamentals/2016-07-27.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Common mistake on switch statements
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+`Autoboxing` and `Unboxing`
