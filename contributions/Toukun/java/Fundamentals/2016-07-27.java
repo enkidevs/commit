0ddae7e9c419e71623391b09c58c7f9b9b218aee
Toukun <toukunproduction@gmail.com>
2016-07-28 00:42:47 +0100

    2016-07-27's java workout

---
+++ contributions/Toukun/java/Fundamentals/2016-07-27.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
