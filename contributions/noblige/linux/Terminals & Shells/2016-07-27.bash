6de45dea876487adde5d5823ed3b8df1f01033b5
noblige <noblige@gmail.com>
2016-07-28 00:57:33 +0100

    2016-07-27's linux workout

---
+++ contributions/noblige/linux/Terminals & Shells/2016-07-27.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
