7a8fab8db1fda68b41a8e1e4ee17eec4421711d4
chrswndrs <christoph.wanders@gmail.com>
2016-07-27 11:42:37 +0100

    2016-07-27's linux workout

---
+++ contributions/chrswndrs/linux/System Management/2016-07-27.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Common log files
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
