897888b74b54125a883076d7d0966209dca0b643
bendobot <whatsbending@gmail.com>
2016-07-27 02:41:37 +0100

    2016-07-26's linux workout

---
+++ contributions/bendobot/linux/System Management/2016-07-26.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+CPU benchmark with `dd`
+Removing old kernels in Debian based systems
+Fixing `locale` issues in Debian systems
