07504de923dd3a8a3ce65e50c2bda67be8bba532
Senopratomo <lord.ernesto@gmail.com>
2016-07-28 22:22:45 +0100

    2016-07-28's java workout

---
+++ contributions/Senopratomo/java/Fundamentals/2016-07-28.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Locks in `static synchronized` methods
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
