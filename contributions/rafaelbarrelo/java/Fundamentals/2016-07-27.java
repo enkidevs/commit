b62e758251d2136e8b17183beb69c1cc2aaa8126
rafaelbarrelo <rafaelbarrelo@gmail.com>
2016-07-28 02:05:21 +0100

    2016-07-27's java workout

---
+++ contributions/rafaelbarrelo/java/Fundamentals/2016-07-27.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+Methods performing *Security Checks* must be declared `Private` or `Final`
