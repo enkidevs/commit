88404f671c026efe0fd3db69cf6077298eff8312
alex-pl <alex@neatnet.de>
2016-07-28 07:12:58 +0100

    2016-07-28's java workout

---
+++ contributions/alex-pl/java/Fundamentals/2016-07-28.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Using `Printstream.format` to format a string before printing
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
