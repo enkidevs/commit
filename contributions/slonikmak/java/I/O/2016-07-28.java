8850107c949a285ab08d14fbf9e18ff4ae77eea5
slonikmak <slonikmak@gmail.com>
2016-07-28 07:55:21 +0100

    2016-07-28's java workout

---
+++ contributions/slonikmak/java/I/O/2016-07-28.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to create directories in Java
+How to test if file or directory exists in Java
+Reading and writing text files
+Checking for the existence of files and directories
