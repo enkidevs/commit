345af20e9c610fbe360514f5b1e384d98ff23ab1
dorzki <me@dorzki.co.il>
2016-07-28 11:24:49 +0100

    2016-07-28's linux workout

---
+++ contributions/dorzki/linux/Terminals & Shells/2016-07-28.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+The Basics of Environment Variables
