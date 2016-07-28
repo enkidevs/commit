706d9c34d1baa6d013ec03fea4b8f82efb83c8d6
obeigel <obeigel@gmail.com>
2016-07-28 07:50:17 +0100

    2016-07-28's javascript workout

---
+++ contributions/obeigel/javascript/Core/2016-07-28.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+Extra function arguments are undefined by default
+Base conversion 
