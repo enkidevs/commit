83f7305c4417cfdaac7560e47efd242870cbd132
emilp <emiper@kth.se>
2016-07-28 10:49:39 +0100

    2016-07-28's linux workout

---
+++ contributions/emilp/linux/System Management/2016-07-28.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Removing old kernels in Debian based systems
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
