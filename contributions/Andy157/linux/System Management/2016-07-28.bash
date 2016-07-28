b484a553960f94945e0e86703e37d64aedb15581
Andy157 <andychoxi@gmail.com>
2016-07-28 15:32:57 +0100

    2016-07-28's linux workout

---
+++ contributions/Andy157/linux/System Management/2016-07-28.bash
@@ -0,0 +1,5 @@
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
