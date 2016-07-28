d3467dc7de47d148094ea8e8a4e4235f69ac56cc
TotKindGott <konstantin.a.kolosov@gmail.com>
2016-07-28 10:31:52 +0100

    2016-07-28's linux workout

---
+++ contributions/TotKindGott/linux/System Management/2016-07-28.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
