4e916add285fe00882fd5be864da344433212e4b
indoriMonk <amanshulvyas@gmail.com>
2016-07-28 23:01:28 +0100

    2016-07-28's java workout

---
+++ contributions/indoriMonk/java/Error Handling/2016-07-28.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Handling Null Pointer Exceptions
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
