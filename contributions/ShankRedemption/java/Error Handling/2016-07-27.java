152c6563914b5e4df1314c48ebfa95ae8a6ea2e8
ShankRedemption <ssaxen4@illinois.edu>
2016-07-28 03:35:11 +0100

    2016-07-27's java workout

---
+++ contributions/ShankRedemption/java/Error Handling/2016-07-27.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Handling Null Pointer Exceptions
+Prevent `exceptions` while logging Data
+Throwing an `exception`
