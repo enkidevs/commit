de976aebc7aeef1498554d8bd46a5be25e48a1b2
hemalshah <hemal.shah1996@gmail.com>
2016-07-28 05:40:53 +0100

    2016-07-27's java workout

---
+++ contributions/hemalshah/java/Fundamentals/2016-07-27.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Do not return *references* to private *mutable* class members
