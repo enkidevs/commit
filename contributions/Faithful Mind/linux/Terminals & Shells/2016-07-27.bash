95778e6c83a7a82e2298a4154691a50c7e44f9fe
Faithful Mind <lpyy15@gmail.com>
2016-07-28 18:55:42 +0100

    2016-07-27's linux workout

---
+++ contributions/Faithful Mind/linux/Terminals & Shells/2016-07-27.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear the terminal instantly
+Random password generator
+Conditional command execution
+
+(`&&` operator)
