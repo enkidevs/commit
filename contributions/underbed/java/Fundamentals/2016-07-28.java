7d14e8d725750ec095af5398670a3f679416d956
underbed <vali.bors@gmail.com>
2016-07-28 07:23:55 +0100

    2016-07-28's java workout

---
+++ contributions/underbed/java/Fundamentals/2016-07-28.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Methods performing *Security Checks* must be declared `Private` or `Final`
