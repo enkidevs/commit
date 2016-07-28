4486e27c74a528bd8c2a09185f7bb4371f849541
corpulentcoffee <dave@corpulentcoffee.com>
2016-07-28 16:52:52 +0100

    2016-07-28's linux workout

---
+++ contributions/corpulentcoffee/linux/File Management/2016-07-28.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Working with `.gz` files
+Delete commands aliases
