4eb52a5b6af44cc95daf863fec15049bbc5e0ee9
swettw <swettw@gmail.com>
2016-07-28 18:36:34 +0100

    2016-07-28's linux workout

---
+++ contributions/swettw/linux/Terminals & Shells/2016-07-28.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
