f27bc1bca28fbc0c03f74ac4fbc5a01267128e31
idelfonsog2 <idelfonsog2@gmail.com>
2016-07-28 21:36:46 +0100

    2016-07-28's git workout

---
+++ contributions/idelfonsog2/git/Essentials/2016-07-28.bash
@@ -0,0 +1,5 @@
+Using tags for version control
+Search for commits by author
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
