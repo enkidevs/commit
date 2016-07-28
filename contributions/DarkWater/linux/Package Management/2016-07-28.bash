7edfe33fb3ad346d75f980a5c0628abe32a6f6d3
DarkWater <darkwater666@mail.ru>
2016-07-28 12:45:55 +0100

    2016-07-28's linux workout

---
+++ contributions/DarkWater/linux/Package Management/2016-07-28.bash
@@ -0,0 +1,5 @@
+List installed packages
+Prevent updating a specific package in Debian systems
+Get `apt` to use a mirror / faster mirror
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
