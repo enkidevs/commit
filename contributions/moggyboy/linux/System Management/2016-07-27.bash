d826eecb62375cbc87c825824a7c099da0aef001
moggyboy <moggyboy@gmail.com>
2016-07-28 01:46:59 +0100

    2016-07-27's linux workout

---
+++ contributions/moggyboy/linux/System Management/2016-07-27.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Cancel a system shutdown
