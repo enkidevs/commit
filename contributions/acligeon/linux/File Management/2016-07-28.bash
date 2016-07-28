2cee795a1ca676ef453a0b3878c311b858634666
acligeon <alekscl@gmail.com>
2016-07-28 13:43:45 +0100

    2016-07-28's linux workout

---
+++ contributions/acligeon/linux/File Management/2016-07-28.bash
@@ -0,0 +1,5 @@
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Deleting non-empty directories
+Working with `.gz` files
