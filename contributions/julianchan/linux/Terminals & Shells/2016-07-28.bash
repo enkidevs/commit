1d31262c17770102c071e4fffd3066014f32e455
julianchan <mrjulianchan@gmail.com>
2016-07-28 13:44:08 +0100

    2016-07-28's linux workout

---
+++ contributions/julianchan/linux/Terminals & Shells/2016-07-28.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Quick `bash` shortcuts 
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
