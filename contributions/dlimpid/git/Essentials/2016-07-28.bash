4ab4dfbb21c966d016413e63eba983403626d2e1
dlimpid <dlimpid@gmail.com>
2016-07-28 23:24:19 +0100

    2016-07-28's git workout

---
+++ contributions/dlimpid/git/Essentials/2016-07-28.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Adding a remote repository
+The three states in git
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
