c1b780ccb02f379b4b4cd517b2294bd2a9676119
ravi_mura <joisa.ravi@gmail.com>
2016-07-28 09:46:09 +0100

    2016-07-27's java workout

---
+++ contributions/ravi_mura/java/Fundamentals/2016-07-27.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Locks in `static synchronized` methods
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
