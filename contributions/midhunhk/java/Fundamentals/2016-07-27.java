a1eb5008e41aa5e5b74e99aa2dd1cf5c0f2f1798
midhunhk <midhunhk@gmail.com>
2016-07-28 03:54:06 +0100

    2016-07-27's java workout

---
+++ contributions/midhunhk/java/Fundamentals/2016-07-27.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
