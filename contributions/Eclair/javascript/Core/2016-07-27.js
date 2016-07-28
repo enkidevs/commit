79acd81506027a90221974f13d892790241e7adc
Eclair <jenny.filcheck@gmail.com>
2016-07-28 04:26:30 +0100

    2016-07-27's javascript workout

---
+++ contributions/Eclair/javascript/Core/2016-07-27.js
@@ -0,0 +1,5 @@
+Using assignment operators
+Hoisting applies only to variable declarations, not initializations
+Apply `map` function to array items
+Prevent background image flicker in **IE**
+How to merge two arrays
