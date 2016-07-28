7b5e9aca3f90a115ee2fda55b91be9e13e207509
WetwareDefect <john.piatras@gmail.com>
2016-07-28 15:10:36 +0100

    2016-07-28's java workout

---
+++ contributions/WetwareDefect/java/Fundamentals/2016-07-28.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Java naming conventions
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
