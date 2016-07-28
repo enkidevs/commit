ecb2454d6a2a4da5ed9bd338b1777075b6f95b16
adewaal824 <adewaal824@gmail.com>
2016-07-28 14:04:24 +0100

    2016-07-28's linux workout

---
+++ contributions/adewaal824/linux/System Management/2016-07-28.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
