531aa158fdfb2db88569f3a44269845478f907c8
Darsstar <darsstar+enki@gmail.com>
2016-07-29 13:07:18 +0100

    2016-07-29's java workout

---
+++ contributions/Darsstar/java/Fundamentals/2016-07-29.java
@@ -0,0 +1,5 @@
+Cropping images
+The distinction between overloading and overriding methods
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
