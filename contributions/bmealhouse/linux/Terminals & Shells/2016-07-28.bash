87c938c6d4dbc7c9c0f02627c998e2d9937b53f2
bmealhouse <bmealhouse@gmail.com>
2016-07-29 02:08:32 +0100

    2016-07-28's linux workout

---
+++ contributions/bmealhouse/linux/Terminals & Shells/2016-07-28.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Useful aliasing in bash
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
