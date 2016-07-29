9a71988b70256d4028a4d95ce30fa2f4d5fdf6f3
Sargeros <sargeros@gmail.com>
2016-07-29 18:29:43 +0100

    2016-07-29's linux workout

---
+++ contributions/Sargeros/linux/Terminals & Shells/2016-07-29.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Aliasing ssh connections
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
