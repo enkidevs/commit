87d286afeeff77d58465bd1919fb43cb955b9f5d
fffl <kuepfer.christopher@gmail.com>
2016-07-29 19:20:47 +0100

    2016-07-29's java workout

---
+++ contributions/fffl/java/Fundamentals/2016-07-29.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
