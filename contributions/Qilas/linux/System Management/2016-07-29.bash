959fc5ae698e7410a15db7ece922f50d438966ad
Qilas <eriklindgren0802@gmail.com>
2016-07-29 08:34:39 +0100

    2016-07-29's linux workout

---
+++ contributions/Qilas/linux/System Management/2016-07-29.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Monitor System Memory using `vmstat`
