990b2eac07bb2fe8e1ba944e745611362c6d8aa4
ditn <abennett.kva@gmail.com>
2016-07-29 09:02:46 +0100

    2016-07-29's java workout

---
+++ contributions/ditn/java/Fundamentals/2016-07-29.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Cropping images
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
