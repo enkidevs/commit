897b158cab2a42bb5f0d9564ba3293dadcbe0a5a
Maximus <max.marius@outlook.com>
2016-07-29 05:23:06 +0100

    2016-07-29's linux workout

---
+++ contributions/Maximus/linux/Terminals & Shells/2016-07-29.bash
@@ -0,0 +1,7 @@
+Random password generator
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+The Basics of Environment Variables
