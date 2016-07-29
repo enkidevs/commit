a1b17ed2fdc749c73649236d736e787801537988
Maiatoday <maiatoday@gmail.com>
2016-07-29 22:08:47 +0100

    2016-07-29's linux workout

---
+++ contributions/Maiatoday/linux/System Management/2016-07-29.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Find the `MAC` address of all network interfaces
+
+Removing old kernels in Debian based systems
+Preventing a user from logging into the system
+Monitor System Memory using `vmstat`
