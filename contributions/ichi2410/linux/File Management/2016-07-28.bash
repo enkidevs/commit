49071f56dfa4aa3542034a071991a27b3fea8fcd
ichi2410 <ichi2410@gmail.com>
2016-07-29 12:48:15 +0100

    2016-07-28's linux workout

---
+++ contributions/ichi2410/linux/File Management/2016-07-28.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
+Delete commands aliases
