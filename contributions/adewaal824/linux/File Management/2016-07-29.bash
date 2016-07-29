c2da9576362fd3f1caba143e5ecb2986b1300c7b
adewaal824 <adewaal824@gmail.com>
2016-07-29 16:53:03 +0100

    2016-07-29's linux workout

---
+++ contributions/adewaal824/linux/File Management/2016-07-29.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Delete commands aliases
+Easy way of sharing files
