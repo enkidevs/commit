b844396c15150248e917533bb237e002ebae6cbc
maxcell <maxcell.wilson@gmail.com>
2016-07-29 13:33:27 +0100

    2016-07-29's javascript workout

---
+++ contributions/maxcell/javascript/Functions/2016-07-29.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Method chaining
+Move cursor at the end of text input
