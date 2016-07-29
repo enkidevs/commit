386757eeebbcaeec1cac8bfd7e1356426d389a25
kenhua-l <l_kenhua93@hotmail.com>
2016-07-29 06:21:03 +0100

    2016-07-28's linux workout

---
+++ contributions/kenhua-l/linux/File Management/2016-07-28.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm -rf`
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Deleting non-empty directories
+Working with `.gz` files
