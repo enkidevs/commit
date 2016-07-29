3e6dcb365dc1d45f5a62cc06f4c214008711c616
seans66 <seanstreet@hotmail.com>
2016-07-29 14:24:03 +0100

    2016-07-28's linux workout

---
+++ contributions/seans66/linux/System Management/2016-07-28.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Basic service management with `systemd`
