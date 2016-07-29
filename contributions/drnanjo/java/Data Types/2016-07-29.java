3c8ca47f08b90e0855af4c0421a6e9f804501f38
drnanjo <drnanjo@aol.com>
2016-07-29 17:06:48 +0100

    2016-07-29's java workout

---
+++ contributions/drnanjo/java/Data Types/2016-07-29.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Double brace initialization
+Retrieve the component type of an array
+Numeric Conversion - Widening
