b8eb3964fe5d798cbd52fdaa4b7ce03cb7567297
Eldergeek <david@geekguytech.com>
2016-07-29 20:01:31 +0100

    2016-07-29's linux workout

---
+++ contributions/Eldergeek/linux/File Management/2016-07-29.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Working with `.gz` files
+Find the unknown process preventing deleting of files
