9c42c1d118c2c4f3f0882aa1b56fc4339ca93b9d
emanuelenardi <nardiemanuele6@gmail.com>
2016-07-29 09:19:18 +0100

    2016-07-29's java workout

---
+++ contributions/emanuelenardi/java/Data Types/2016-07-29.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Double brace initialization
+Do not attempt comparisons with NaN
