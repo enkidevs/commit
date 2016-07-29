2b74e871aed0c8e1c24fbd196126649da50e2fe0
Escobro <escobar.michael2@gmail.com>
2016-07-29 05:18:48 +0100

    2016-07-28's linux workout

---
+++ contributions/Escobro/linux/File Management/2016-07-28.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Truncate files with `cat` instead of `rm -rf`
+Extracting `tar` files to a specific directory
+Working with `.gz` files
