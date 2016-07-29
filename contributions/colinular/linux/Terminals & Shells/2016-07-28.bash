3d7cdba9f0a14074d9073b238e2910e20f83441f
colinular <colinular@gmail.com>
2016-07-29 10:55:46 +0100

    2016-07-28's linux workout

---
+++ contributions/colinular/linux/Terminals & Shells/2016-07-28.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
