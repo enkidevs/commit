7903b6498c914b12a762cf743df3f07a500b3d6c
borino <brrshh@gmail.com>
2016-07-29 10:49:38 +0100

    2016-07-29's java workout

---
+++ contributions/borino/java/Error Handling/2016-07-29.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Handling Null Pointer Exceptions
+Most common reason behind **stack overflow** error
