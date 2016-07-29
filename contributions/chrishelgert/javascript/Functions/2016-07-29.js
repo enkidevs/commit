fc46ed2c3951822a7d1985e4e59935c04c4441df
chrishelgert <chris.helgert@yahoo.com>
2016-07-29 10:14:59 +0100

    2016-07-29's javascript workout

---
+++ contributions/chrishelgert/javascript/Functions/2016-07-29.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Function call method
+Move cursor at the end of text input
+Check if a document is done loading
