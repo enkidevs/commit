c497dbcb5c0835357ceccb1258f7fd60db94a26d
Marcus.Vinicius <marcusviniciusaguiar@gmail.com>
2016-07-29 20:28:48 +0100

    2016-07-29's linux workout

---
+++ contributions/Marcus.Vinicius/linux/File Management/2016-07-29.bash
@@ -0,0 +1,7 @@
+Truncate files with `cat` instead of `rm -rf`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Easy way of sharing files
