da1b726ba24a5b9950ace1eecd679ee0e271ef1e
WetwareDefect <john.piatras@gmail.com>
2016-07-29 17:32:57 +0100

    2016-07-29's java workout

---
+++ contributions/WetwareDefect/java/I/O/2016-07-29.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+How to list all files in a directory that match a filename extension in Java
+Reading and writing text files
+Reading file attributes
+The `Console` class
