b0451bf224b6d80445009636fb7a42a621ad3f5c
cemal şen <cemal.sen@gmail.com>
2016-07-29 10:20:38 +0100

    2016-07-29's javascript workout

---
+++ contributions/cemal şen/javascript/Functions/2016-07-29.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Method chaining
+Move cursor at the end of text input
