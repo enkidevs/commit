512f79bbd85496f06445109ecb3f51f916d53768
ronba <ronbadur02@gmail.com>
2016-07-29 09:42:21 +0100

    2016-07-29's java workout

---
+++ contributions/ronba/java/Error Handling/2016-07-29.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Handling Null Pointer Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
