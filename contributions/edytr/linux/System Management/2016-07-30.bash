77f97e4161eb208e0f92971a68f05a3abbdc3f70
edytr <edytr1997@gmail.com>
2016-07-30 15:53:40 +0100

    2016-07-30's linux workout

---
+++ contributions/edytr/linux/System Management/2016-07-30.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
