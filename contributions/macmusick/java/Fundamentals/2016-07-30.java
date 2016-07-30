acc24a3de5c6398f479dcf602f1c07c5ee9e0a44
macmusick <macmusick@outlook.com>
2016-07-30 18:32:34 +0100

    2016-07-30's java workout

---
+++ contributions/macmusick/java/Fundamentals/2016-07-30.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+How to play a sound file in Java
+Using Inheritance to reduce code repetition
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
