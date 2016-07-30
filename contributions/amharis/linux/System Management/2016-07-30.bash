c43cf17ac12a690888b0011b7340c413ef96c276
amharis <abdullah.m.haris@gmail.com>
2016-07-30 08:05:57 +0100

    2016-07-30's linux workout

---
+++ contributions/amharis/linux/System Management/2016-07-30.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
