c4a9093fc56ddd63221d47ad11980146e15146bd
reebz <mitchell.ribar@gmail.com>
2016-07-30 16:52:51 +0100

    2016-07-30's javascript workout

---
+++ contributions/reebz/javascript/Core/2016-07-30.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+`catch` is block scoped
+Avoid using `with`
