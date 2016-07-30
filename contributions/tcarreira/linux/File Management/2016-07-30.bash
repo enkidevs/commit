5e558cfb4a9d65381df336a8c953e30db6f9b3e3
tcarreira <tiago.g.carreira@gmail.com>
2016-07-30 12:18:29 +0100

    2016-07-30's linux workout

---
+++ contributions/tcarreira/linux/File Management/2016-07-30.bash
@@ -0,0 +1,7 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm -rf`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
