aa2bbb839e70f011ced599b58b2133632c9d7973
raduenuca <raduenuca@gmail.com>
2016-07-30 15:21:25 +0100

    2016-07-30's linux workout

---
+++ contributions/raduenuca/linux/Terminals & Shells/2016-07-30.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
