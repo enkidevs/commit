13e57bcd83cfc682f5831ef20ee4b811128e93cc
Senopratomo <lord.ernesto@gmail.com>
2016-07-30 18:01:23 +0100

    2016-07-30's linux workout

---
+++ contributions/Senopratomo/linux/System Management/2016-07-30.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Cancel a system shutdown
