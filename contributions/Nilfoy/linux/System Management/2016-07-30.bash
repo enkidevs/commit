27141f09ac027a99ee5dabc67b0961db09a56274
Nilfoy <aditya4mvp@gmail.com>
2016-07-30 16:24:21 +0100

    2016-07-30's linux workout

---
+++ contributions/Nilfoy/linux/System Management/2016-07-30.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Removing old kernels in Debian based systems
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
