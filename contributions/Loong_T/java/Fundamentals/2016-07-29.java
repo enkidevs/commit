e18991d2e6782c1efc7bb4f2f0334de91114659a
Loong_T <talentloong@gmail.com>
2016-07-30 14:41:08 +0100

    2016-07-29's java workout

---
+++ contributions/Loong_T/java/Fundamentals/2016-07-29.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Metadata: creating a user-defined file attribute
+Methods performing *Security Checks* must be declared `Private` or `Final`
