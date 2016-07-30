444245acf30568e3dc7802473f2f9b9d064aa864
aivsim <aivaras.simulis@outlook.com>
2016-07-30 11:05:19 +0100

    2016-07-30's javascript workout

---
+++ contributions/aivsim/javascript/Node/2016-07-30.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+`try-catch` only for **sync** code
