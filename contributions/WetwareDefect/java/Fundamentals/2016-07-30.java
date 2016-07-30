e1987954ab0a2dfbaec5b72e29687fff0b4b20c5
WetwareDefect <john.piatras@gmail.com>
2016-07-30 16:19:06 +0100

    2016-07-30's java workout

---
+++ contributions/WetwareDefect/java/Fundamentals/2016-07-30.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+The trade-off between short and descriptive names
+Using Inheritance to reduce code repetition
+Using `Printstream.format` to format a string before printing
+Limit Accessibility of `Fields`
