c4eaa629ac1b24f115c28a1f332622a4a35d7ae1
ptrickg <ptrickg@hotmail.com>
2016-07-30 04:36:52 +0100

    2016-07-29's java workout

---
+++ contributions/ptrickg/java/Threads & Locks/2016-07-29.java
@@ -0,0 +1,5 @@
+Multithreading: use local variables
+Multithreading: minimize locking scope
+`Thread.sleep` and `Thread.yield` do not have synchronization mechanisms
+Thread interruptions
+Implementing your own `Thread` 
