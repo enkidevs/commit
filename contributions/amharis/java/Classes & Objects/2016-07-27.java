cb119763f9271629db24d8ccbeab483552a3bf1d
amharis <abdullah.m.haris@gmail.com>
2016-07-30 08:00:03 +0100

    2016-07-27's java workout

---
+++ contributions/amharis/java/Classes & Objects/2016-07-27.java
@@ -0,0 +1,5 @@
+Utility classes and `static` methods
+Compare classes and not class names
+Final object contents are not immutable
+How and when to use `WeakHashMap` class
+Fields with Java **Reflection API**
