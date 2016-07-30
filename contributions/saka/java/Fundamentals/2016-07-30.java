17db68fadae9d6c5b7be9e39c5a15cdb1bb04232
saka <saka71996@gmail.com>
2016-07-30 06:25:09 +0100

    2016-07-30's java workout

---
+++ contributions/saka/java/Fundamentals/2016-07-30.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Common mistake on switch statements
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
