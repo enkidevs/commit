f37d9251d9c595537fbb991cab9f1f0dca2d7e6a
owen <oweny@outlook.com>
2016-07-30 08:29:49 +0100

    2016-07-29's javascript workout

---
+++ contributions/owen/javascript/Node/2016-07-29.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Asynchronous File Write/Read in Node.js
+Http Server in **Node**
+Use conventional property names for error handling in Node.js
+`process.nextTick()` callback pattern
