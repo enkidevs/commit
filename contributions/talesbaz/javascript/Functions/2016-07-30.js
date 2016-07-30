e8d85d7db8589578eea13202545d2b2d1124c2a6
talesbaz <talesbaz@gmail.com>
2016-07-30 13:11:22 +0100

    2016-07-30's javascript workout

---
+++ contributions/talesbaz/javascript/Functions/2016-07-30.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+`.bind()` 
+IIFE pattern
+Changing a functions context with `fn.call(object)`
