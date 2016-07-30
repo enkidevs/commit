bb30fa858255beedb5b9387f37f633f05634cd3d
maximelouet <maximelouet14+enki@gmail.com>
2016-07-30 10:22:09 +0100

    2016-07-30's linux workout

---
+++ contributions/maximelouet/linux/File Management/2016-07-30.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm -rf`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
