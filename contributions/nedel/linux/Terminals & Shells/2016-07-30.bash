bb583750ccaa4af9d981cc57d347a1b69750e68e
nedel <nedel@gmx.com>
2016-07-30 07:08:22 +0100

    2016-07-30's linux workout

---
+++ contributions/nedel/linux/Terminals & Shells/2016-07-30.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Clear the terminal instantly
+Find any Unix / Linux command
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
