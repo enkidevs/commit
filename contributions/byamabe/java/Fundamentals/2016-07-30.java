017da9e2b0d926a3337ab46f84eb8f9701c3b4d3
byamabe <brian@yamabe.net>
2016-07-30 15:23:40 +0100

    2016-07-30's java workout

---
+++ contributions/byamabe/java/Fundamentals/2016-07-30.java
@@ -0,0 +1,5 @@
+Cropping images
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
