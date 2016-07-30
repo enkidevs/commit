aeaf7bc667a6b8bbba897eba2d0f6e783acab698
saronet <sarahillel436@gmail.com>
2016-07-30 08:16:08 +0100

    2016-07-29's linux workout

---
+++ contributions/saronet/linux/File Management/2016-07-29.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Practical `du` command
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
