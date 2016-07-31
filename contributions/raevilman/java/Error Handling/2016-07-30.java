48f168bac0f49f47d93d81511f1369a5268994e1
raevilman <raevilman@gmail.com>
2016-07-31 16:01:23 +0100

    2016-07-30's java workout

---
+++ contributions/raevilman/java/Error Handling/2016-07-30.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Handling Null Pointer Exceptions
+Prevent `exceptions` while logging Data
