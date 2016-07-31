cf3384f3cd2af1e62ff2efba88fecbd702ee6bc4
drnanjo <drnanjo@aol.com>
2016-07-31 16:51:16 +0100

    2016-07-31's java workout

---
+++ contributions/drnanjo/java/Error Handling/2016-07-31.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Using exceptions in Java
+Prevent `exceptions` while logging Data
