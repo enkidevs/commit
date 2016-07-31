7b3ae5839a2955979a7609fd5f3030419a3733e3
ptrickg <ptrickg@hotmail.com>
2016-07-31 04:12:41 +0100

    2016-07-30's java workout

---
+++ contributions/ptrickg/java/Fundamentals/2016-07-30.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Java naming conventions
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Using `synchronized` statements
