d416f10fdf19c729653399b34d2a3fa891b1893e
Starjams <robertbmerriman@gmail.com>
2016-07-31 13:03:23 +0100

    2016-07-31's linux workout

---
+++ contributions/Starjams/linux/File Management/2016-07-31.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm -rf`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Delete commands aliases
