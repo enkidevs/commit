c6235eccd98b293ab65924edc14752da98ac54cf
estebanrules <damian.esteban@gmail.com>
2016-07-31 23:33:01 +0100

    2016-07-31's linux workout

---
+++ contributions/estebanrules/linux/Terminals & Shells/2016-07-31.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Terminal incognito mode
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
