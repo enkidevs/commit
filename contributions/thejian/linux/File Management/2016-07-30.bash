045f61ce04b0d55fe57724e3a861ed1c8f132f98
thejian <thejianmail@gmail.com>
2016-07-31 02:04:29 +0100

    2016-07-30's linux workout

---
+++ contributions/thejian/linux/File Management/2016-07-30.bash
@@ -0,0 +1,5 @@
+Converting between Unix and Windows text files
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
