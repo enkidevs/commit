63e6557486f492689a1d83689d4814fcf73ec057
chrishelgert <chris.helgert@yahoo.com>
2016-07-31 17:27:03 +0100

    2016-07-31's linux workout

---
+++ contributions/chrishelgert/linux/System Management/2016-07-31.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
