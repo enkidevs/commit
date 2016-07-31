d2c99458f75cb5a1995705ad4cd2d5d2794f9e78
reginaexmachina <we.love.rachel.day.lots@gmail.com>
2016-07-31 04:20:58 +0100

    2016-07-30's javascript workout

---
+++ contributions/reginaexmachina/javascript/Browser APIs/2016-07-30.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Interaction with the user
+Navigating the browser history
+Warn user if **Back** button is pressed
+`Window.localStorage`
