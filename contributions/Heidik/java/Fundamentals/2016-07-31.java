6b76781ea49050495d177b46da60f07a628d65b1
Heidik <heidiks@gmail.com>
2016-07-31 07:10:10 +0100

    2016-07-31's java workout

---
+++ contributions/Heidik/java/Fundamentals/2016-07-31.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
