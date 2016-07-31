0da703c390555f2628f7517562c2fa4d424a58df
Beelzenef <elena.guzbla@gmail.com>
2016-07-31 10:16:50 +0100

    2016-07-31's linux workout

---
+++ contributions/Beelzenef/linux/System Management/2016-07-31.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Cancel a system shutdown
