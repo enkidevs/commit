bc389493e93cdd5b444ab7c4e811742b01e6ff91
jayceef <jayceef@yahoo.com>
2016-07-31 04:16:38 +0100

    2016-07-30's javascript workout

---
+++ contributions/jayceef/javascript/Functions/2016-07-30.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+IIFE pattern
+Get query/url variables
+Move cursor at the end of text input
