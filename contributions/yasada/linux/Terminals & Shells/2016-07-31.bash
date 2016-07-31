04cc054688d25017e4247fc508f80f8ca956e923
yasada <y.asada216@gmail.com>
2016-08-01 00:36:16 +0100

    2016-07-31's linux workout

---
+++ contributions/yasada/linux/Terminals & Shells/2016-07-31.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
+The Basics of Environment Variables
