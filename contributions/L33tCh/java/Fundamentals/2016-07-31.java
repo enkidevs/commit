f6d2842df50a9a74a1a5372aa3fec277093ce479
L33tCh <l33tch@gmail.com>
2016-07-31 07:57:57 +0100

    2016-07-31's java workout

---
+++ contributions/L33tCh/java/Fundamentals/2016-07-31.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+How to play a sound file in Java
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Supply `toString()` in all classes
