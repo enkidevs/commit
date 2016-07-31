8f31da68a4169ded863c3ea226d0598c232c37e4
adewaal824 <adewaal824@gmail.com>
2016-07-31 15:50:17 +0100

    2016-07-31's linux workout

---
+++ contributions/adewaal824/linux/Terminals & Shells/2016-07-31.bash
@@ -0,0 +1,5 @@
+Find any Unix / Linux command
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
