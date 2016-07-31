6dd13b42f6f9e3b96f11ea58b0659016608baac5
jvtbatman <j3h002@gmail.com>
2016-07-31 16:27:39 +0100

    2016-07-31's linux workout

---
+++ contributions/jvtbatman/linux/Networking/2016-07-31.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+Disable `IPv6`
+Sending emails from terminal / scripts
+Use `tcpdump` to listen to network interface traffic
+Staying connected remotely via SSH without an interactive shell
