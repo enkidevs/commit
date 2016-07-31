4a91eb57bf8553c6a618ec44b67979f3323a20f5
fish88hotmail <roberto.pesce.ext@gmail.com>
2016-07-31 19:56:31 +0100

    2016-07-31's javascript workout

---
+++ contributions/fish88hotmail/javascript/Node/2016-07-31.js
@@ -0,0 +1,5 @@
+Http Server in **Node**
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
