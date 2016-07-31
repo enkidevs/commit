43f15e50e70481912a697642e37b597cb6d33b8a
intro <introvic.petr@gmail.com>
2016-07-31 08:53:47 +0100

    2016-07-31's java workout

---
+++ contributions/intro/java/Fundamentals/2016-07-31.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+How to play a sound file in Java
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
