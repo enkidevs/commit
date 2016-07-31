3437eb53675208b06bde41e2400f9c3f3a8c9d8a
juliawong <juliawong0@gmail.com>
2016-07-31 06:12:53 +0100

    2016-07-30's java workout

---
+++ contributions/juliawong/java/Fundamentals/2016-07-30.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
