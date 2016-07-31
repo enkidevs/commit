ea0d4337580cb0de066f8dac7a140b45cd436c29
nathanrjones <me@nathanrjones.com>
2016-07-31 15:34:20 +0100

    2016-07-31's java workout

---
+++ contributions/nathanrjones/java/Fundamentals/2016-07-31.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Cropping images
+Common mistake on switch statements
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
