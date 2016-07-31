f90a28fffa0b8bec30ac09b5718daae29f72094b
dermeck <martinrunge83@web.de>
2016-07-31 07:50:33 +0100

    2016-07-31's javascript workout

---
+++ contributions/dermeck/javascript/Browser APIs/2016-07-31.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Async and defer scripts
+Use hosted scripts to increase performance
+Form a `URL` from its parts 
+MediaDevices.getUserMedia()
