9c6eecf4eac1c2a38ca0b3f27c315ea688c20e85
andreas_bergstrom <andreas.bergstrom@gmail.com>
2016-07-31 09:28:06 +0100

    2016-07-31's javascript workout

---
+++ contributions/andreas_bergstrom/javascript/Core/2016-07-31.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Most efficient way to build `HTML` strings
+Use `splice()` to remove an item from an array
+Prototype methods
+Detect **DO NOT TRACK** status
