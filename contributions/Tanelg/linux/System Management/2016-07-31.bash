648a372b453ae26e9bf7e04318ae5079e821a88e
Tanelg <derickpalos93@gmail.com>
2016-07-31 13:37:06 +0100

    2016-07-31's linux workout

---
+++ contributions/Tanelg/linux/System Management/2016-07-31.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
+System uptime
