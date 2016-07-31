391a0cf8c6f6ce11f3c6d4336ff995b0d04c85a2
Senopratomo <lord.ernesto@gmail.com>
2016-07-31 16:43:55 +0100

    2016-07-31's java workout

---
+++ contributions/Senopratomo/java/Fundamentals/2016-07-31.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Detect or prevent integer overflow
