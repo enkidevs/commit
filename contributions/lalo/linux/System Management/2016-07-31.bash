f85917ba3c9017d29572df4364f48111b25ca35f
lalo <leandro.gz73@gmail.com>
2016-07-31 15:00:19 +0100

    2016-07-31's linux workout

---
+++ contributions/lalo/linux/System Management/2016-07-31.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
