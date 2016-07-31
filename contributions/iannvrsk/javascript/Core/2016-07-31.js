b18de3f7960b834118884f6563bb7f6dcdf0ec12
iannvrsk <kaverin1990@gmail.com>
2016-07-31 17:06:48 +0100

    2016-07-31's javascript workout

---
+++ contributions/iannvrsk/javascript/Core/2016-07-31.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+The difference between null, undefined and NaN
+Prevent background image flicker in **IE**
+Detect online connection
+Detect **DO NOT TRACK** status
