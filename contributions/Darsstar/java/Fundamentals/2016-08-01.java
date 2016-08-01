497bbe93337130b8a36aef4d7a947aa856763280
Darsstar <darsstar+enki@gmail.com>
2016-08-01 20:01:49 +0100

    2016-08-01's java workout

---
+++ contributions/Darsstar/java/Fundamentals/2016-08-01.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
