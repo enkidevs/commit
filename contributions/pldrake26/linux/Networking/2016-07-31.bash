2e7746b8553e6ac512e835af942f77fda75ce68f
pldrake26 <pldrake26@outlook.com>
2016-08-01 06:55:28 +0100

    2016-07-31's linux workout

---
+++ contributions/pldrake26/linux/Networking/2016-07-31.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Get real network statistics with `slurm`
+Sending emails from terminal / scripts
+Setting up password-free authentication
+Getting the connection speed from the terminal
