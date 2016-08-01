191eb2db0279afa6b9fff6d450e9f4a6641ef363
ionakathryn <ionakathryn123@googlemail.com>
2016-08-01 07:42:24 +0100

    2016-08-01's java workout

---
+++ contributions/ionakathryn/java/Fundamentals/2016-08-01.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
