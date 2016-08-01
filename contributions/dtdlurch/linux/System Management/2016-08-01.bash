ec765690f7436f849cbbbe56a2c333a7c216cb3a
dtdlurch <adamjones077@gmail.com>
2016-08-01 14:54:26 +0100

    2016-08-01's linux workout

---
+++ contributions/dtdlurch/linux/System Management/2016-08-01.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
