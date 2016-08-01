2684e980c37825d7e9a3af939eec6f4716cc9ec8
jgsqware <jgonzalez@wemanity.com>
2016-08-01 06:29:42 +0100

    2016-08-01's linux workout

---
+++ contributions/jgsqware/linux/System Management/2016-08-01.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
