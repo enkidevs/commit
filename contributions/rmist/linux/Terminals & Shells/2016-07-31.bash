bea3b5e45b0352b8167dd32b96ddb72d302fa805
rmist <rakesh_dta@live.in>
2016-08-01 12:41:39 +0100

    2016-07-31's linux workout

---
+++ contributions/rmist/linux/Terminals & Shells/2016-07-31.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
