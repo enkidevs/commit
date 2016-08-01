430b3118ae00b90c07112840e595cd59d9c54877
midhunhk <midhunhk@gmail.com>
2016-08-01 16:40:58 +0100

    2016-07-31's java workout

---
+++ contributions/midhunhk/java/Fundamentals/2016-07-31.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
