a273c92ffa682e1c13fc247fbdae6f3ad431332d
nikivi <nikita.voloboev@gmail.com>
2016-08-01 23:15:14 +0100

    2016-08-01's linux workout

---
+++ contributions/nikivi/linux/Terminals & Shells/2016-08-01.bash
@@ -0,0 +1,7 @@
+Shebang `#!` explained 
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
