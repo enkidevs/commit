4b34eea24dff5bed904e64cd059e313eb3bbf465
gamecow <chris.miranda11@gmail.com>
2016-08-01 15:06:47 +0100

    2016-08-01's linux workout

---
+++ contributions/gamecow/linux/File Management/2016-08-01.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
