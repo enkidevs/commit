7c117ddaaebc352d4f6f384e6a99d57b63082eda
nsieber <nsieber@gmail.com>
2016-08-01 15:26:28 +0100

    2016-08-01's linux workout

---
+++ contributions/nsieber/linux/Terminals & Shells/2016-08-01.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
