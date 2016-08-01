8ee9251db8988e690a42082d4789d29b0aebe84e
florian.soare <florian.soare@takmate.com>
2016-08-01 06:48:14 +0100

    2016-08-01's linux workout

---
+++ contributions/florian.soare/linux/System Management/2016-08-01.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+System uptime
