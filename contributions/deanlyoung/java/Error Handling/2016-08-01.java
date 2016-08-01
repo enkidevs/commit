23c7045530f2da5beb67f79512c88705b33b8317
deanlyoung <dlyoung119@me.com>
2016-08-01 16:47:29 +0100

    2016-08-01's java workout

---
+++ contributions/deanlyoung/java/Error Handling/2016-08-01.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Use try-with-resources instead of `finally`
+Prevent `exceptions` while logging Data
+Throwing an `exception`
+Checking a double for `NaN` value
