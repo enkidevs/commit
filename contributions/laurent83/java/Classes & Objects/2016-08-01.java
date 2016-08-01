17d8346fa7609cfed6a7d42a1f91c8ed68c559a1
laurent83 <laurent.morissette@gmail.com>
2016-08-01 16:36:05 +0100

    2016-08-01's java workout

---
+++ contributions/laurent83/java/Classes & Objects/2016-08-01.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Compare classes and not class names
+How and when to use `WeakHashMap` class
+Fields with Java **Reflection API**
