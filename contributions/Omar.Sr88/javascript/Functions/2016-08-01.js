c27dc35afc7d3495b814ee8f1263aa0bb3973763
Omar.Sr88 <omardarksr@gmail.com>
2016-08-01 13:20:32 +0100

    2016-08-01's javascript workout

---
+++ contributions/Omar.Sr88/javascript/Functions/2016-08-01.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+`.bind()` 
+Easily generate a random `HEX` color
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
