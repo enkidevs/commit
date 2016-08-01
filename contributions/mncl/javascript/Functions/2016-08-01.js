dcbe7ab7cbb0dc918bcd78cd27348ad4aea2399e
mncl <emladenoff@gmail.com>
2016-08-01 18:40:34 +0100

    2016-08-01's javascript workout

---
+++ contributions/mncl/javascript/Functions/2016-08-01.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Easily generate a random `HEX` color
+Function call method
+Changing a functions context with `fn.call(object)`
