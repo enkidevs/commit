5e7093c15261581b1cfcd7090eaacbb4db06dc63
Retriever <cal-f@hotmail.com>
2016-08-01 13:08:40 +0100

    2016-08-01's linux workout

---
+++ contributions/Retriever/linux/System Management/2016-08-01.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
