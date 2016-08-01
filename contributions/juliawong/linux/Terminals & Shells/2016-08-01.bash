6bf703f86003eb7e8668e9e2cf2d8ffcc995865d
juliawong <juliawong0@gmail.com>
2016-08-01 18:01:55 +0100

    2016-08-01's linux workout

---
+++ contributions/juliawong/linux/Terminals & Shells/2016-08-01.bash
@@ -0,0 +1,7 @@
+Clear the terminal instantly
+Find any Unix / Linux command
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
