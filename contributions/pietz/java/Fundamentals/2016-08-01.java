067ca4fae2d1127260a51280733d35ee33b6f707
pietz <enki@plpp.de>
2016-08-01 08:58:05 +0100

    2016-08-01's java workout

---
+++ contributions/pietz/java/Fundamentals/2016-08-01.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Supply `toString()` in all classes
