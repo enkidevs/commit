d6d6ac004c717ec8cad368538605e0ee0b1b1f87
Shockcom <joshmolive@gmail.com>
2016-08-01 04:42:33 +0100

    2016-08-01's linux workout

---
+++ contributions/Shockcom/linux/System Management/2016-08-01.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
