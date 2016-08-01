a9f7930701ee34a377cb4f8c8db8b9f9a35b6eab
duckzito <duckzito@gmail.com>
2016-08-01 04:39:03 +0100

    2016-07-31's linux workout

---
+++ contributions/duckzito/linux/Terminals & Shells/2016-07-31.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Aliasing ssh connections
+Terminal incognito mode
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
