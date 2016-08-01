46bb981e7fc096847449361c606051e701b8b77e
ioanab <balea.ioana@gmail.com>
2016-08-01 16:56:15 +0100

    2016-08-01's linux workout

---
+++ contributions/ioanab/linux/Terminals & Shells/2016-08-01.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
