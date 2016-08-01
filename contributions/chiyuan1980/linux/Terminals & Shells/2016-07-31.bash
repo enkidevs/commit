8aa73ccb9bea449be2ac9cc3a0c8aab8c8902eb1
chiyuan1980 <chiyuan1980@gmail.com>
2016-08-01 17:11:18 +0100

    2016-07-31's linux workout

---
+++ contributions/chiyuan1980/linux/Terminals & Shells/2016-07-31.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Useful aliasing in bash
+Random password generator
+`else` statements using the `||` operator 
