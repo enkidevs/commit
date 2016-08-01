f57cb58c3994197760d3382ac19888e1133e8989
Axenu <simonseregon@gmail.com>
2016-08-01 06:27:45 +0100

    2016-08-01's git workout

---
+++ contributions/Axenu/git/Essentials/2016-08-01.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Using aliases for git commands
+What is a checksum?
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
