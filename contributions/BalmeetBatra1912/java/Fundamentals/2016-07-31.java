bd0427ca868aeee3581d37729477ecdd2794206a
BalmeetBatra1912 <balmeetbatra19@gmail.com>
2016-08-01 17:30:21 +0100

    2016-07-31's java workout

---
+++ contributions/BalmeetBatra1912/java/Fundamentals/2016-07-31.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
