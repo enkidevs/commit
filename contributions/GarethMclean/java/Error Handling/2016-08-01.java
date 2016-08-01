7e8f7e42ec9bec5f8b9a6841387cf1bc8bc77f67
GarethMclean <mc2217@hiltoncollege.com>
2016-08-01 17:32:35 +0100

    2016-08-01's java workout

---
+++ contributions/GarethMclean/java/Error Handling/2016-08-01.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Converting stack trace to a string
+Handling Null Pointer Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
