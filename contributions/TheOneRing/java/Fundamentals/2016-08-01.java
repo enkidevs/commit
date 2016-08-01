f60519206ba83dfa0b9a8bfbc6165fdc1211cf94
TheOneRing <aalecos10@gmail.com>
2016-08-01 13:37:42 +0100

    2016-08-01's java workout

---
+++ contributions/TheOneRing/java/Fundamentals/2016-08-01.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
