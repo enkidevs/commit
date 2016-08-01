4f3733bf7b8e238df05b5955580eef145757ab18
slonikmak <slonikmak@gmail.com>
2016-08-01 06:50:47 +0100

    2016-08-01's java workout

---
+++ contributions/slonikmak/java/Fundamentals/2016-08-01.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Methods performing *Security Checks* must be declared `Private` or `Final`
