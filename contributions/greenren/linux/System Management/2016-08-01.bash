4fc24e13a56ca8951b317c8b06a28c767002545d
greenren <slamboy182@hotmail.com>
2016-08-01 16:37:32 +0100

    2016-08-01's linux workout

---
+++ contributions/greenren/linux/System Management/2016-08-01.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
