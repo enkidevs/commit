22132b9032b46e5c9e23ae3a998a3e3f6a9c4d77
mositis <mositis@yahoo.com>
2016-08-01 07:40:37 +0100

    2016-08-01's java workout

---
+++ contributions/mositis/java/Error Handling/2016-08-01.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Careful Numeric Data Conversions
