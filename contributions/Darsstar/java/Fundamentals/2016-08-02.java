00339507005964e9c5ce24cc26942487e99678bb
Darsstar <darsstar+enki@gmail.com>
2016-08-02 19:09:13 +0100

    2016-08-02's java workout

---
+++ contributions/Darsstar/java/Fundamentals/2016-08-02.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Using an interface as a parameter
