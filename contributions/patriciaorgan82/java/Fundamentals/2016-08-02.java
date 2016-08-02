043c46013ab09c8da119c8d747cab1f8505d051d
patriciaorgan82 <patriciaorgan82@gmail.com>
2016-08-02 23:36:01 +0100

    2016-08-02's java workout

---
+++ contributions/patriciaorgan82/java/Fundamentals/2016-08-02.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Cropping images
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
