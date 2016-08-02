5127cd970c619039618774f6a08ddcc61639e44f
tsjagan <tsjagan@gmail.com>
2016-08-02 21:23:35 +0100

    2016-08-02's git workout

---
+++ contributions/tsjagan/git/Essentials/2016-08-02.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Pushing tags to a server
+Make your log output pretty
+Use `short` status to make output more compact
+Remote repositories: fetching and pushing
