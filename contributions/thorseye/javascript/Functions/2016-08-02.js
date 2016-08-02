496fbdba9ca4d54e700d26dc920a161693a01184
thorseye <jonas@thoursie.net>
2016-08-02 12:10:06 +0100

    2016-08-02's javascript workout

---
+++ contributions/thorseye/javascript/Functions/2016-08-02.js
@@ -0,0 +1,5 @@
+Functions can be declared after use
+Move cursor at the end of text input
+Social sharing without widgets
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
