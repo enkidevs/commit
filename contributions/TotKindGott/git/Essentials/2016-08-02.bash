b0a7cc406c4cddda219a14012b3dec7245e686ae
TotKindGott <konstantin.a.kolosov@gmail.com>
2016-08-02 10:29:12 +0100

    2016-08-02's git workout

---
+++ contributions/TotKindGott/git/Essentials/2016-08-02.bash
@@ -0,0 +1,5 @@
+Using tags for version control
+Using aliases for git commands
+Check the status of your files
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
