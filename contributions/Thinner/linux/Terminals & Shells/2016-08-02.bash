b70384c993d973ca4ae67a4e1328c372f178bfbd
Thinner <sprattsmurf@gmail.com>
2016-08-02 08:01:35 +0100

    2016-08-02's linux workout

---
+++ contributions/Thinner/linux/Terminals & Shells/2016-08-02.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Find any Unix / Linux command
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
