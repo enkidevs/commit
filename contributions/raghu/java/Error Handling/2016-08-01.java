4a5c905e213878af2491c12ab76eeb09b9409865
raghu <raghu.9208@gmail.com>
2016-08-02 04:36:20 +0100

    2016-08-01's java workout

---
+++ contributions/raghu/java/Error Handling/2016-08-01.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Using exceptions in Java
+Careful Numeric Data Conversions
