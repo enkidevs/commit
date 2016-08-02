228d035f50a61785edf8b8b120644c239d8981bb
ryskalo <sibit11@gmail.com>
2016-08-02 22:34:39 +0100

    2016-08-02's java workout

---
+++ contributions/ryskalo/java/Fundamentals/2016-08-02.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Methods performing *Security Checks* must be declared `Private` or `Final`
