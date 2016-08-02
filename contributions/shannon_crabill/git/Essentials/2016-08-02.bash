815b8d973eccf81d4502bc22813272ffa63b11ef
shannon_crabill <shannon.crabill@gmail.com>
2016-08-02 21:54:39 +0100

    2016-08-02's git workout

---
+++ contributions/shannon_crabill/git/Essentials/2016-08-02.bash
@@ -0,0 +1,5 @@
+Using tags for version control
+You can use git offline!
+Check the status of your files
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
