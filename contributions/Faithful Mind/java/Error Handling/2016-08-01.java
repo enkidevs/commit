f617d45dd9232df084dc233709096918561ed2f9
Faithful Mind <lpyy15@gmail.com>
2016-08-02 06:33:57 +0100

    2016-08-01's java workout

---
+++ contributions/Faithful Mind/java/Error Handling/2016-08-01.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Careful Numeric Data Conversions
