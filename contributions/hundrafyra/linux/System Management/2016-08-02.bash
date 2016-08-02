59b24832daafc015d064c6c7976cc37373379bd1
hundrafyra <niklas.brynolf@gmail.com>
2016-08-02 15:57:06 +0100

    2016-08-02's linux workout

---
+++ contributions/hundrafyra/linux/System Management/2016-08-02.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
