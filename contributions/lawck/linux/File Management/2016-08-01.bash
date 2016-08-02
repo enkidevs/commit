0d7055f7db75e569a29ee7f02b62e5576c9f2237
lawck <lawck@me.com>
2016-08-02 09:45:45 +0100

    2016-08-01's linux workout

---
+++ contributions/lawck/linux/File Management/2016-08-01.bash
@@ -0,0 +1,7 @@
+Practical `du` command
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
