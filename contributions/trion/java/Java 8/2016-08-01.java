c636eae32679bc251964aca2315372cbe2accc63
trion <parmsingh129@yahoo.com>
2016-08-02 17:37:37 +0100

    2016-08-01's java workout

---
+++ contributions/trion/java/Java 8/2016-08-01.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+How to check for static dependencies of applications and libraries from the command line.
+Control OS `Process`es in `Java8`
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
