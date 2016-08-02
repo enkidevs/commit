e18c3cfd8764ab6269b58d1cd2cf0b99237b5974
mr.hyman63 <mr.hyman63@gmail.com>
2016-08-02 09:55:06 +0100

    2016-08-01's java workout

---
+++ contributions/mr.hyman63/java/Error Handling/2016-08-01.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Handling Null Pointer Exceptions
+Most common reason behind **stack overflow** error
