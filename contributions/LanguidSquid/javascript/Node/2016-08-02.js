962564786d9468345922d47f023a89f87b308712
LanguidSquid <blakecameronyarbrough@gmail.com>
2016-08-02 18:26:31 +0100

    2016-08-02's javascript workout

---
+++ contributions/LanguidSquid/javascript/Node/2016-08-02.js
@@ -0,0 +1,5 @@
+First-error callbacks in **Node**
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+`exports` vs. `module.exports` in **Node**
+Middleware/pipeline design pattern
