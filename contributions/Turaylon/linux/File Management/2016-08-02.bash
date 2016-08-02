a76f9f10c472096bfb12d39bb76c5479a56bca35
Turaylon <djdrake88@gmail.com>
2016-08-02 07:55:56 +0100

    2016-08-02's linux workout

---
+++ contributions/Turaylon/linux/File Management/2016-08-02.bash
@@ -0,0 +1,7 @@
+Practical `du` command
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
