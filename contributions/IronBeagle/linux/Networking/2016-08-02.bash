2dfb41bd917de6ec4acb33b0fd569edcfa161525
IronBeagle <noamhofshi@gmail.com>
2016-08-02 18:21:31 +0100

    2016-08-02's linux workout

---
+++ contributions/IronBeagle/linux/Networking/2016-08-02.bash
@@ -0,0 +1,5 @@
+How to clear `iptables` rules
+Disable `IPv6`
+Useful ssh client optimizations 
+Getting the connection speed from the terminal
+Staying connected remotely via SSH without an interactive shell
