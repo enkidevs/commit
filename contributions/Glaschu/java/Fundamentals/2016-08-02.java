69a10c8057fc0297fabbab1afb29b9ba69115c34
Glaschu <jamesfglasgow@gmail.com>
2016-08-02 21:27:44 +0100

    2016-08-02's java workout

---
+++ contributions/Glaschu/java/Fundamentals/2016-08-02.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Using an interface as a parameter
