d660e6a2b34b1199316b9a876bfb2248981cdcf6
jmg2107 <jenjengoo@gmail.com>
2016-08-02 15:11:04 +0100

    2016-08-02's javascript workout

---
+++ contributions/jmg2107/javascript/Core/2016-08-02.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Functions return `undefined` by default
+Avoid using `with`
