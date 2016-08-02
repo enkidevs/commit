72f642f84ea9715e90ee70d3276e41937fe19eae
aaronbiller <aaronbiller@gmail.com>
2016-08-02 13:36:19 +0100

    2016-08-02's linux workout

---
+++ contributions/aaronbiller/linux/Terminals & Shells/2016-08-02.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Find any Unix / Linux command
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
