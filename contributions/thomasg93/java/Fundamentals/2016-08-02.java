49026250b28aad65b17c4d3944a55f43f562e6ec
thomasg93 <thomas.gauroy@gmail.com>
2016-08-02 07:09:28 +0100

    2016-08-02's java workout

---
+++ contributions/thomasg93/java/Fundamentals/2016-08-02.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
