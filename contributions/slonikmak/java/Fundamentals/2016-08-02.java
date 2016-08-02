1d0283a68138dfb3151cab05ec356a003a22e0fb
slonikmak <slonikmak@gmail.com>
2016-08-02 07:57:11 +0100

    2016-08-02's java workout

---
+++ contributions/slonikmak/java/Fundamentals/2016-08-02.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Locks in `static synchronized` methods
+Implementing an `interface`
