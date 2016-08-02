49a6524ee253cf15368cce31c8b55d8477e5436f
Ke-obi <kobikelvin@gmail.com>
2016-08-02 22:34:46 +0100

    2016-08-02's linux workout

---
+++ contributions/Ke-obi/linux/System Management/2016-08-02.bash
@@ -0,0 +1,7 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Basic service management with `systemd`
