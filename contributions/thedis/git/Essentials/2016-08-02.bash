200c56a2b8ae486ce0dad6a565f3ee7560333419
thedis <thedis95@gmail.com>
2016-08-02 08:41:46 +0100

    2016-08-02's git workout

---
+++ contributions/thedis/git/Essentials/2016-08-02.bash
@@ -0,0 +1,5 @@
+Finding a tag
+Limiting log output by time
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
