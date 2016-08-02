225c7e7dc4dde14491f0cb3350e004372bd18255
zanaticul <cosmin@cioba.ro>
2016-08-02 12:08:16 +0100

    2016-08-02's linux workout

---
+++ contributions/zanaticul/linux/System Management/2016-08-02.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Detect your linux distribution
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
