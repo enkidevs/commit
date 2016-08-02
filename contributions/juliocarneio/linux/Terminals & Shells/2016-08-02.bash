f60985f5f548f0ab162808e9b3f2dc2175e07331
juliocarneio <juliocarneiro@gmail.com>
2016-08-02 13:35:07 +0100

    2016-08-02's linux workout

---
+++ contributions/juliocarneio/linux/Terminals & Shells/2016-08-02.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
