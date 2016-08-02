7a6c3e24d058f8a7d1d5c62a33fe5efad8581492
estebandiazm <juesdimo@gmail.com>
2016-08-02 15:10:25 +0100

    2016-08-02's java workout

---
+++ contributions/estebandiazm/java/Fundamentals/2016-08-02.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+Supply `toString()` in all classes
+Using an interface as a parameter
