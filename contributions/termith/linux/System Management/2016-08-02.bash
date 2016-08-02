18111595afd65609167033cd84d5692599e5df04
termith <dmitdemidov@gmail.com>
2016-08-02 12:25:15 +0100

    2016-08-02's linux workout

---
+++ contributions/termith/linux/System Management/2016-08-02.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Preventing a user from logging into the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
