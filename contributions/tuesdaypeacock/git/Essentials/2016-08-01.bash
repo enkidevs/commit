38e1e8d113839abd52e374ba0574b3b340357a2f
tuesdaypeacock <tuesdaypeacock@gmail.com>
2016-08-02 07:54:03 +0100

    2016-08-01's git workout

---
+++ contributions/tuesdaypeacock/git/Essentials/2016-08-01.bash
@@ -0,0 +1,5 @@
+Adding a remote repository
+Using tags for version control
+Search by commit message keyword
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
