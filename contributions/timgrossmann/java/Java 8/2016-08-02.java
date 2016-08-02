b0f05bda7ede43f08eab996a4f7756428c491751
timgrossmann <t.grossmann1994@web.de>
2016-08-02 08:08:45 +0100

    2016-08-02's java workout

---
+++ contributions/timgrossmann/java/Java 8/2016-08-02.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Java 8 Lambda expressions
+How to check for static dependencies of applications and libraries from the command line.
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
