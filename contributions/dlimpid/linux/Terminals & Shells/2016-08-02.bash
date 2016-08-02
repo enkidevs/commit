3610e03bb7e225b5d597c564e2201740a710c31b
dlimpid <dlimpid@gmail.com>
2016-08-03 00:50:33 +0100

    2016-08-02's linux workout

---
+++ contributions/dlimpid/linux/Terminals & Shells/2016-08-02.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Clear the terminal instantly
+Find any Unix / Linux command
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
