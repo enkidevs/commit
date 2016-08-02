baad034a9a58a19eeab09d24986db8d635a7c111
Rugudorull <rugudorull@gmx.de>
2016-08-02 16:57:00 +0100

    2016-08-02's linux workout

---
+++ contributions/Rugudorull/linux/Terminals & Shells/2016-08-02.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
