9bb11899a3bc501dee848ef511772ae8e6d1f760
groundhogdad <groundhogdad@gmail.com>
2016-08-02 07:59:21 +0100

    2016-08-02's javascript workout

---
+++ contributions/groundhogdad/javascript/React/2016-08-02.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+Functional Stateless Components in React
+`null` value for *Controlled Components* in **React**
+Expose Component Functions in **React**
+Specify a single child
