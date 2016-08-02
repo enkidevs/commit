bc9976e5a5cd9f4466ca7c1f3be8ceffaa3a7a11
kaitoii11 <kaitoii11@hotmail.com>
2016-08-02 02:16:13 +0100

    2016-08-01's linux workout

---
+++ contributions/kaitoii11/linux/System Management/2016-08-01.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
