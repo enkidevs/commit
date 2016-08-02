3093c485a563dc0f9b8ee1fcd8c62bee9a75d8cc
jefrailey <jcaetof+enki@gmail.com>
2016-08-02 15:24:59 +0100

    2016-08-02's linux workout

---
+++ contributions/jefrailey/linux/Terminals & Shells/2016-08-02.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Repeating commands with `watch`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
