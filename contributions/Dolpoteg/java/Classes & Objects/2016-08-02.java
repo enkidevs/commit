95c3890d06160cc1e274b9b46d2b5dc7dcbd763f
Dolpoteg <benjamin.stermecki@gmail.com>
2016-08-02 07:35:41 +0100

    2016-08-02's java workout

---
+++ contributions/Dolpoteg/java/Classes & Objects/2016-08-02.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Utility classes and `static` methods
+The `abstract` keyword
+Java object references
+String formatting and parsing with `SimpleDateFormat`
