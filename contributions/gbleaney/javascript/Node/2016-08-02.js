d7be737c4c51d487d750246f8a279b5bf2050d66
gbleaney <gbleaney@gmail.com>
2016-08-02 16:36:19 +0100

    2016-08-02's javascript workout

---
+++ contributions/gbleaney/javascript/Node/2016-08-02.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+First-error callbacks in **Node**
+Use conventional property names for error handling in Node.js
+`uncaughtException` listener in Node.js
+Middleware/pipeline design pattern
