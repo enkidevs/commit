5445aefa6ef7b5bb18f82d01db806035e2c241f9
deekayen <deekayen@deekayen.net>
2016-08-02 18:55:45 +0100

    2016-08-02's linux workout

---
+++ contributions/deekayen/linux/Package Management/2016-08-02.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+List installed packages
+Prevent updating a specific package in Debian systems
+Using `PPAs`
+Downgrade a package via `apt-get`
