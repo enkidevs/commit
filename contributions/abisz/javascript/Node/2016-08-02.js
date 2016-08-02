76f7f256b15b5c8de23bbd6bcc7cb401bc9ea574
abisz <reinsperger.smn@gmail.com>
2016-08-02 08:08:33 +0100

    2016-08-02's javascript workout

---
+++ contributions/abisz/javascript/Node/2016-08-02.js
@@ -0,0 +1,5 @@
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+Bulk-Write in **Node** with `cork()`
+Middleware/pipeline design pattern
+Avoid using `sudo` when running Node
