3ca1ad344f35471ba041aea8871cc86db358b2a8
KhairaneMurad <khairane.mourad@gmail.com>
2016-08-02 17:13:16 +0100

    2016-08-02's java workout

---
+++ contributions/KhairaneMurad/java/Fundamentals/2016-08-02.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
