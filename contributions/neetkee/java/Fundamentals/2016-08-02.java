32e6c8cd1fd69b574c4c771cc26a957380a8f5f3
neetkee <neetkee@live.com>
2016-08-02 06:48:26 +0100

    2016-08-02's java workout

---
+++ contributions/neetkee/java/Fundamentals/2016-08-02.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Supply `toString()` in all classes
