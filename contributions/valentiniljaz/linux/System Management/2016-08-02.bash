42e8c4698add4aa6f70dbe4d877ba2f1fdbca36d
valentiniljaz <valentin.iljaz@gmail.com>
2016-08-02 07:57:55 +0100

    2016-08-02's linux workout

---
+++ contributions/valentiniljaz/linux/System Management/2016-08-02.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
