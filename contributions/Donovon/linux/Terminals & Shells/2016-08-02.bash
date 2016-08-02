2460e85e97a2710b0dfafa407865aa6b6847c1e2
Donovon <donovonjbacon@gmail.com>
2016-08-02 20:09:25 +0100

    2016-08-02's linux workout

---
+++ contributions/Donovon/linux/Terminals & Shells/2016-08-02.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear bash history
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
