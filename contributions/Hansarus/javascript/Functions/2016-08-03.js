02a0a32013d90e7b22012304ef2c76795912e26d
Hansarus <jonniv86@gmail.com>
2016-08-03 18:56:17 +0100

    2016-08-03's javascript workout

---
+++ contributions/Hansarus/javascript/Functions/2016-08-03.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Functions can be declared after use
+Get query/url variables
+Easily generate a random `HEX` color
+Function call method
