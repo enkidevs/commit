4f172a1e00e1294a98e5dcc6dffa7d361985df18
KingPixil <simplecooldude1@gmail.com>
2016-08-03 19:14:36 +0100

    2016-08-03's javascript workout

---
+++ contributions/KingPixil/javascript/Functions/2016-08-03.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Move cursor at the end of text input
+Social sharing without widgets
+Check if a document is done loading
