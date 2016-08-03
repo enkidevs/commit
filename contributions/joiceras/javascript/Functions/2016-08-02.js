94716d111a47ec46c63c7db746dee2c9a8584979
joiceras <joice.ras11@gmail.com>
2016-08-03 02:04:07 +0100

    2016-08-02's javascript workout

---
+++ contributions/joiceras/javascript/Functions/2016-08-02.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Get query/url variables
+Easily generate a random `HEX` color
