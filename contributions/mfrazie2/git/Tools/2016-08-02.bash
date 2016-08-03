28110e275d284498e71ecd807436162ff53db485
mfrazie2 <michael.s.frazier@gmail.com>
2016-08-03 04:04:45 +0100

    2016-08-02's git workout

---
+++ contributions/mfrazie2/git/Tools/2016-08-02.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Specify a commit by its ancestry
+Interactively stage patches
+Remember the results of previous hunk conflicts
+Sharing data by bundling
