1b8a2511d9cf4330fb654540fd83e486b186a102
neetkee <neetkee@live.com>
2016-08-03 09:45:36 +0100

    2016-08-03's java workout

---
+++ contributions/neetkee/java/I/O/2016-08-03.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+List contents of a directory with `newDirectoryStream`
