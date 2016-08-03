ea84f13f15ad251f3b212b40c320b2848df91e3f
n6g7 <n6g7@gnab.fr>
2016-08-03 17:36:25 +0100

    2016-08-03's linux workout

---
+++ contributions/n6g7/linux/File Management/2016-08-03.bash
@@ -0,0 +1,7 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
