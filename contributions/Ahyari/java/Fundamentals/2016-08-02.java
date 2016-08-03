ce45cb26268759ff075546a725d2c158bed2efd4
Ahyari <ahmad.alhyari@gmail.com>
2016-08-03 07:42:57 +0100

    2016-08-02's java workout

---
+++ contributions/Ahyari/java/Fundamentals/2016-08-02.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Using an interface as a parameter
