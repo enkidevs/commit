124973120f8dfa987907fedd9226ff718f9db190
justkd <kdegani@gmail.com>
2016-08-03 20:11:48 +0100

    2016-08-03's javascript workout

---
+++ contributions/justkd/javascript/Functions/2016-08-03.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Anonymous functions
+Easily generate a random `HEX` color
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
