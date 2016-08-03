2d308156ef31e54eddcb7c440eb627457adf6abe
trion <parmsingh129@yahoo.com>
2016-08-03 03:06:25 +0100

    2016-08-02's java workout

---
+++ contributions/trion/java/Fundamentals/2016-08-02.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Common mistake on switch statements
+Using the `Console`
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
