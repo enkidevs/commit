30341b09ff97e5693acba76a7d717392cfbd5d83
slonikmak <slonikmak@gmail.com>
2016-08-03 06:53:47 +0100

    2016-08-03's java workout

---
+++ contributions/slonikmak/java/Fundamentals/2016-08-03.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
