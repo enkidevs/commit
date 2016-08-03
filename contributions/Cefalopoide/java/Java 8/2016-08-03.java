178500774294cdcb3d03c2949a300ffe1afa0a1e
Cefalopoide <luca.filipponi89@gmail.com>
2016-08-03 08:45:26 +0100

    2016-08-03's java workout

---
+++ contributions/Cefalopoide/java/Java 8/2016-08-03.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Java 8 Lambda expressions
+Use `Optional(s)` type in order to avoid `NullPointerException(s)`
+Control OS `Process`es in `Java8`
+Avoid numerical overflows with *Java8* `exact` methods.
