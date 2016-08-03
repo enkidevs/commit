bba628f5889a93e9fffa27d9f6ab481e84c2b544
Moiz.lkw <moizlokhandwala9@gmail.com>
2016-08-03 07:36:47 +0100

    2016-08-02's linux workout

---
+++ contributions/Moiz.lkw/linux/Terminals & Shells/2016-08-02.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Useful aliasing in bash
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
