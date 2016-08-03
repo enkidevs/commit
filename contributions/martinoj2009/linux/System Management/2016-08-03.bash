2d9a3e0a273d9579154d97f9853a7c4c97640f5e
martinoj2009 <martinoj2009@gmail.com>
2016-08-04 00:29:30 +0100

    2016-08-03's linux workout

---
+++ contributions/martinoj2009/linux/System Management/2016-08-03.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Executing scripts on system boot using `systemd`
+Using `dd` To Create Disk Images
