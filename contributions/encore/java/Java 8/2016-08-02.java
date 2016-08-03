7fdc49ad1d3f87532666f3f54f6e3c6a24048a83
encore <kingkonig@gmail.com>
2016-08-03 03:02:08 +0100

    2016-08-02's java workout

---
+++ contributions/encore/java/Java 8/2016-08-02.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Java 8 Lambda expressions
+Using `Arrays.parallelSort` 
+Avoid numerical overflows with *Java8* `exact` methods.
+`JJS` - Oracle Nashorn, newest JavaScript Engine
