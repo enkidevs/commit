52e7d34fd68cc51285fb3cf2ef6bf637bb0e0eb6
menio <przemek.grupa@gmail.com>
2016-08-03 21:07:34 +0100

    2016-08-03's linux workout

---
+++ contributions/menio/linux/Jobs & Processes/2016-08-03.bash
@@ -0,0 +1,5 @@
+Backgrounding tasks 
+`Crontab` newline issue
+`Cron` dot-in-filename issues when using `run-parts`
+Common issue with scripts in `cron`
+How to pause a process and run others in the background
