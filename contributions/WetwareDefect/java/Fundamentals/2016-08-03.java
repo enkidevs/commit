674a3540dc08ea6cfe494190eff5528153fb1be5
WetwareDefect <john.piatras@gmail.com>
2016-08-03 13:39:28 +0100

    2016-08-03's java workout

---
+++ contributions/WetwareDefect/java/Fundamentals/2016-08-03.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Methods performing *Security Checks* must be declared `Private` or `Final`
