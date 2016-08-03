229de9023c054c13555b3d2d0a588f654b2548b2
jeznag <jeremy.nagel@learnosity.com>
2016-08-03 23:59:25 +0100

    2016-08-03's linux workout

---
+++ contributions/jeznag/linux/System Management/2016-08-03.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
