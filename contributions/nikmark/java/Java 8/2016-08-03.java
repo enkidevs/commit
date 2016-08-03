998e4e0716d57796826f85f3a5076139efc013d0
nikmark <marchi.nicolo@gmail.com>
2016-08-03 18:26:33 +0100

    2016-08-03's java workout

---
+++ contributions/nikmark/java/Java 8/2016-08-03.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Java 8 Lambda expressions
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
