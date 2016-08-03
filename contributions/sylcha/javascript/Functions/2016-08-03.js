bb44194bb67eb4efa06d59d0c5fe70b976e624a2
sylcha <chambon.sylvain@gmail.com>
2016-08-04 00:52:22 +0100

    2016-08-03's javascript workout

---
+++ contributions/sylcha/javascript/Functions/2016-08-03.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Method chaining
+Easily generate a random `HEX` color
+Check if a document is done loading
