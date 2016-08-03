583c1554ef7ab81fa3087885b7e7be08e185fd38
eduardopeixe <walthfish@yahoo.com>
2016-08-03 15:06:31 +0100

    2016-08-03's java workout

---
+++ contributions/eduardopeixe/java/Fundamentals/2016-08-03.java
@@ -0,0 +1,5 @@
+Distinction between `public` and `private` methods
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
