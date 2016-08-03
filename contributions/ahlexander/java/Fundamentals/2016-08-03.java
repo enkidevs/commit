7016a398470f69ff4c3a808433d123a9eb445027
ahlexander <alex@devmotion.ro>
2016-08-03 08:31:00 +0100

    2016-08-03's java workout

---
+++ contributions/ahlexander/java/Fundamentals/2016-08-03.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
