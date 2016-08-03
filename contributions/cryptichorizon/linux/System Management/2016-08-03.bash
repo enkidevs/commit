6d8a38fbb5b0f26c39dd8a5451df206f30733ea0
cryptichorizon <cryptichorizon@gmail.com>
2016-08-03 07:38:03 +0100

    2016-08-03's linux workout

---
+++ contributions/cryptichorizon/linux/System Management/2016-08-03.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Cancel a system shutdown
+Using `dd` To Create Disk Images
