2a4b6b0a5b131fc6585e3b96033d68fb44051888
pleasedodisturb <hjacker@gmail.com>
2016-08-03 13:42:01 +0100

    2016-08-03's javascript workout

---
+++ contributions/pleasedodisturb/javascript/Core/2016-08-03.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Hoisting applies only to variable declarations, not initializations
+NaN === NaN?
+Prevent background image flicker in **IE**
+Detect online connection
