bb5e7b8913318b2acb786e169c95caef05425763
owen <oweny@outlook.com>
2016-08-03 01:09:49 +0100

    2016-08-02's java workout

---
+++ contributions/owen/java/Data Types/2016-08-02.java
@@ -0,0 +1,5 @@
+`ArrayList` vs `LinkedList`
+Default values for unassigned data types
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
