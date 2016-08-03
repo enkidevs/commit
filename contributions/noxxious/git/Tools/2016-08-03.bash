a3c7a5aa7e4283e176f2982018569aad046d47a0
noxxious <gytis.raciukaitis@gmail.com>
2016-08-03 14:00:54 +0100

    2016-08-03's git workout

---
+++ contributions/noxxious/git/Tools/2016-08-03.bash
@@ -0,0 +1,5 @@
+Use a short checksum to find a commit
+Specify a commit by its ancestry
+Specify a range of commits using double dot syntax
+Interactively stage patches
+Create a new branch from a stash
