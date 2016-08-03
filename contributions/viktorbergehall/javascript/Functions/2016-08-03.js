cb0a8002b81f590d939e76540eb7c19c6bf967cc
viktorbergehall <viktorbergehall@gmail.com>
2016-08-03 09:03:30 +0100

    2016-08-03's javascript workout

---
+++ contributions/viktorbergehall/javascript/Functions/2016-08-03.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Function call method
+Check if a document is done loading
