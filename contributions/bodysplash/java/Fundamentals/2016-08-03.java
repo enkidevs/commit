0296f6647e2dbb828e3a23eb3ba24666b0b512f5
bodysplash <jbdusseaut@arpinum.fr>
2016-08-03 19:20:27 +0100

    2016-08-03's java workout

---
+++ contributions/bodysplash/java/Fundamentals/2016-08-03.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Cropping images
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
