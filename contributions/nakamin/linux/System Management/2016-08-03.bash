709f130ef938d51b230c07cd73aa14b7ca4fe7b7
nakamin <antonwizkam@gmail.com>
2016-08-03 14:18:15 +0100

    2016-08-03's linux workout

---
+++ contributions/nakamin/linux/System Management/2016-08-03.bash
@@ -0,0 +1,5 @@
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Commands to shutdown or restart the system
+Cancel a system shutdown
