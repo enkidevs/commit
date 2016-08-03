f47b52e08abba2449aab5c84402a3c0c967cea9c
Zanex <alessio.zanella88@gmail.com>
2016-08-03 13:09:58 +0100

    2016-08-03's javascript workout

---
+++ contributions/Zanex/javascript/Npm/2016-08-03.js
@@ -0,0 +1,5 @@
+List binaries for scripting in npm
+Environmental variables and flags for `npm`
+`optionalDependencies` in npm
+Package distribution tags
+Lock down dependency versions by shrinkwrapping
