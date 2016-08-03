b7fd67012475af473b2d3dcb1c17dcadb784461f
hendori <hendri.eleveld@gmail.com>
2016-08-03 08:57:01 +0100

    2016-08-03's java workout

---
+++ contributions/hendori/java/Fundamentals/2016-08-03.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Do not return *references* to private *mutable* class members
