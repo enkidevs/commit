86bf858f4e0e6a468444eebaf10f066e1fcdd290
masaruhoshi <mhoshi@gmail.com>
2016-08-03 13:57:11 +0100

    2016-08-03's javascript workout

---
+++ contributions/masaruhoshi/javascript/Node/2016-08-03.js
@@ -0,0 +1,5 @@
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+Middleware/pipeline design pattern
+Avoid using `sudo` when running Node
