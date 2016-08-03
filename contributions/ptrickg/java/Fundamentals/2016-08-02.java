268246eea5bce04ac25f57f5b71486819ff0ad95
ptrickg <ptrickg@hotmail.com>
2016-08-03 05:01:17 +0100

    2016-08-02's java workout

---
+++ contributions/ptrickg/java/Fundamentals/2016-08-02.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use meaningful names
+Using `Printstream.format` to format a string before printing
+There is no such thing as *pass-by-reference* in Java
+Implementing an `interface`
