1840ede80e48c60aea72831d929d00ae5bde40a5
groundhogdad <groundhogdad@gmail.com>
2016-08-03 06:22:26 +0100

    2016-08-03's linux workout

---
+++ contributions/groundhogdad/linux/Terminals & Shells/2016-08-03.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Shebang `#!` explained 
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
