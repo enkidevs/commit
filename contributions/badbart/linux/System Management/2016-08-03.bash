8c56dc3ae9280115b48b90c94679d2765a8a887c
badbart <tom.zille@gmail.com>
2016-08-03 03:23:23 +0100

    2016-08-03's linux workout

---
+++ contributions/badbart/linux/System Management/2016-08-03.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
