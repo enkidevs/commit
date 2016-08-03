42565e245ae8a0eacb9f51e01d3572ff4589bac4
thomasg93 <thomas.gauroy@gmail.com>
2016-08-03 07:33:49 +0100

    2016-08-03's linux workout

---
+++ contributions/thomasg93/linux/System Management/2016-08-03.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
