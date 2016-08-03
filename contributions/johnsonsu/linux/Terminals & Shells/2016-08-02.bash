8e786cc3f6f4473272e8ffbbfc689228782ca0a7
johnsonsu <johnsonsu0603@gmail.com>
2016-08-03 10:51:36 +0100

    2016-08-02's linux workout

---
+++ contributions/johnsonsu/linux/Terminals & Shells/2016-08-02.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Useful aliasing in bash
+Random password generator
+Sequential execution using the `;` statement separator
