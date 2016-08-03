08372f2bbe87eec8cce4e8f5a1b8bbed0b6a1694
darrenclaughton <darrenclaughton@gmail.com>
2016-08-03 07:51:35 +0100

    2016-08-03's linux workout

---
+++ contributions/darrenclaughton/linux/File Management/2016-08-03.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
