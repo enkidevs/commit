a862f8adc5f9d17dd4a3c5495a6deb6b797c9252
mpatino117 <mpatino117@gmail.com>
2016-08-03 04:26:47 +0100

    2016-08-02's java workout

---
+++ contributions/mpatino117/java/Fundamentals/2016-08-02.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
