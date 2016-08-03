689d6f81e35315c3eb4708ffc417f57552719fda
L33tCh <l33tch@gmail.com>
2016-08-03 20:17:25 +0100

    2016-08-03's java workout

---
+++ contributions/L33tCh/java/Fundamentals/2016-08-03.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
