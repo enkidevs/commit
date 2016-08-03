1c6cc3ef037cb9b724d6606b7bfb45d75f9a8a7c
fadeenk <fadeekannah@gmail.com>
2016-08-03 17:22:24 +0100

    2016-08-03's javascript workout

---
+++ contributions/fadeenk/javascript/Functions/2016-08-03.js
@@ -0,0 +1,5 @@
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Function call method
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
