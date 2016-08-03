6f2b6366189c1e4c724d93d8299e834befa74d78
xazax <johan.daniel.nystrom@gmail.com>
2016-08-03 16:04:58 +0100

    2016-08-03's linux workout

---
+++ contributions/xazax/linux/File Management/2016-08-03.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Easy way of sharing files
