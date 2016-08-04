63fea6222994b4dd5e3fea523a1d2d5b274cb5ab
corpulentcoffee <dave@corpulentcoffee.com>
2016-08-04 21:18:04 +0100

    2016-08-04's linux workout

---
+++ contributions/corpulentcoffee/linux/Terminals & Shells/2016-08-04.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
