7e00436c23c484e616653d914e83c072c464da3c
Arturio1970 <arthur@arthurvanacker.com>
2016-08-04 09:41:12 +0100

    2016-08-04's javascript workout

---
+++ contributions/Arturio1970/javascript/Functions/2016-08-04.js
@@ -0,0 +1,5 @@
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+IIFE pattern
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
