ef65c9f27d15a697f2ddb4cb896550bac6c65a4c
kensportsfan <kenneth813@gmail.com>
2016-08-04 15:11:32 +0100

    2016-08-04's java workout

---
+++ contributions/kensportsfan/java/Data Types/2016-08-04.java
@@ -0,0 +1,5 @@
+`ArrayList` vs `LinkedList`
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Use `Arrays.asList()` to initialise lists
+String concatenation in Java
