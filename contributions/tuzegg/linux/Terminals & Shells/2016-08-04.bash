c5e3b7b8d5365357375d7746a649160e47ca8603
tuzegg <alexeymayboroda23@gmail.com>
2016-08-04 08:46:53 +0100

    2016-08-04's linux workout

---
+++ contributions/tuzegg/linux/Terminals & Shells/2016-08-04.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
