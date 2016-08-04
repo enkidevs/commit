d820ae4a4824b38920ca3ce04a0c2014f84b414e
Akath11 <akath19@gmail.com>
2016-08-04 19:27:58 +0100

    2016-08-04's javascript workout

---
+++ contributions/Akath11/javascript/Node/2016-08-04.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Wrapping errors in Node.js using _node-verror_
+Bulk-Write in **Node** with `cork()`
+Middleware/pipeline design pattern
+Avoid using `sudo` when running Node
