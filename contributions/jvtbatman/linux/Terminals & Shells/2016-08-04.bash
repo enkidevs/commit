bf7c141e9349c9bb327dcccd8001e749149b0038
jvtbatman <j3h002@gmail.com>
2016-08-04 12:30:18 +0100

    2016-08-04's linux workout

---
+++ contributions/jvtbatman/linux/Terminals & Shells/2016-08-04.bash
@@ -0,0 +1,5 @@
+Quick `bash` shortcuts 
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
