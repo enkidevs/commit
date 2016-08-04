9bb96fb4c53eaf375d30ead6f46146d7f170c25d
dtdlurch <adamjones077@gmail.com>
2016-08-04 15:52:34 +0100

    2016-08-04's linux workout

---
+++ contributions/dtdlurch/linux/File Management/2016-08-04.bash
@@ -0,0 +1,6 @@
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
+Easy way of sharing files
