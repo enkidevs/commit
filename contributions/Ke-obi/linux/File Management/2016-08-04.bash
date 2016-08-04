b124deddafb014c65beed9d7cc36d16ff40a0412
Ke-obi <kobikelvin@gmail.com>
2016-08-04 22:37:11 +0100

    2016-08-04's linux workout

---
+++ contributions/Ke-obi/linux/File Management/2016-08-04.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Identify files using the `file` command
+
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
+Easy way of sharing files
