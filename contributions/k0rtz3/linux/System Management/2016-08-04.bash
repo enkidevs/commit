b64c618f4269d049dddd5cb060a23cd593ab0cc7
k0rtz3 <johnmecnx12@gmail.com>
2016-08-04 13:26:59 +0100

    2016-08-04's linux workout

---
+++ contributions/k0rtz3/linux/System Management/2016-08-04.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
