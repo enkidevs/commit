d6036a8de796e10842efaeacb0303f7e94d51878
frender <kevteq@gmail.com>
2016-08-04 11:07:04 +0100

    2016-08-04's linux workout

---
+++ contributions/frender/linux/Terminals & Shells/2016-08-04.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+The Basics of Environment Variables
