cc96a520fda1c5bcc583012d7917e63f2a001e97
edgarshurtado <edsanhu@gmail.com>
2016-08-04 06:27:26 +0100

    2016-08-04's javascript workout

---
+++ contributions/edgarshurtado/javascript/Core/2016-08-04.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Use `splice()` to remove an item from an array
+Infix operators are left-associative
+Functions return `undefined` by default
+JavaScript compilation
