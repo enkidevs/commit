9872826c903392c94df10169b41c2f8c6751ce8a
tarl25 <tarl_hutch@yahoo.com>
2016-08-04 17:28:24 +0100

    2016-08-04's java workout

---
+++ contributions/tarl25/java/Error Handling/2016-08-04.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Throwing an `exception`
+Checking a double for `NaN` value
