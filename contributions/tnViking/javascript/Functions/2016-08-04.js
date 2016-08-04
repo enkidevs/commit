4984e1a83bfc49ed62d82546c5588341295f7c90
tnViking <ole-martin.bratteng@syntact.co.uk>
2016-08-04 20:27:33 +0100

    2016-08-04's javascript workout

---
+++ contributions/tnViking/javascript/Functions/2016-08-04.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
