e7557898b95cba64b244e4600741d69e01fc02c0
randyf <randfer@gmail.com>
2016-08-04 21:07:36 +0100

    2016-08-04's git workout

---
+++ contributions/randyf/git/Essentials/2016-08-04.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Limiting log output by time
+Search for commits by author
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
