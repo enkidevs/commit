fff33d57c96077dbe78c26152eac746a4f47e10a
zymtom <lolipopzere@gmail.com>
2016-08-04 10:38:35 +0100

    2016-08-04's linux workout

---
+++ contributions/zymtom/linux/System Management/2016-08-04.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
