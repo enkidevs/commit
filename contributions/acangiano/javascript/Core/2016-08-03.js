57e60aca5587237375326d1887bda4056b5b9b0f
acangiano <acangiano@gmail.com>
2016-08-04 05:46:42 +0100

    2016-08-03's javascript workout

---
+++ contributions/acangiano/javascript/Core/2016-08-03.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Infix operators are left-associative
+Counting the months from zero
+Filtering items out of an array
+Extra function arguments are undefined by default
