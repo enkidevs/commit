35dee685a9a3d7ea3ca7e5c7cc8cfa6c967af8a9
marg <mksalawa@gmail.com>
2016-08-04 06:37:33 +0100

    2016-08-04's linux workout

---
+++ contributions/marg/linux/Terminals & Shells/2016-08-04.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Clear the terminal instantly
+Find any Unix / Linux command
+Sequential execution using the `;` statement separator
