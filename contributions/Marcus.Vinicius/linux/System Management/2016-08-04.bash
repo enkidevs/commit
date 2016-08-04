4472e939526f712851458e8054853a5e3079ee6f
Marcus.Vinicius <marcusviniciusaguiar@gmail.com>
2016-08-04 20:18:50 +0100

    2016-08-04's linux workout

---
+++ contributions/Marcus.Vinicius/linux/System Management/2016-08-04.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+System uptime
