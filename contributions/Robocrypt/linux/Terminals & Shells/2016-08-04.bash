6888cdd38bd35490b4ff39b3f71d4dbb7d742fe7
Robocrypt <wickie.leejr@gmail.com>
2016-08-04 16:40:53 +0100

    2016-08-04's linux workout

---
+++ contributions/Robocrypt/linux/Terminals & Shells/2016-08-04.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Repeating commands with `watch`
+Random password generator
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
