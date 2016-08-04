524a97958909a5a358944c4b4063316c5f822aef
neetkee <neetkee@live.com>
2016-08-04 11:27:01 +0100

    2016-08-04's java workout

---
+++ contributions/neetkee/java/Fundamentals/2016-08-04.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Connecting to FTP using Java
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
