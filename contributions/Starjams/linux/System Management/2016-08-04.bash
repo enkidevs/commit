a61936137bf0b06cf76c5ea172fa23b350953b2e
Starjams <robertbmerriman@gmail.com>
2016-08-04 09:32:00 +0100

    2016-08-04's linux workout

---
+++ contributions/Starjams/linux/System Management/2016-08-04.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Monitor System Memory using `vmstat`
