2271addf07235a09b37943fc73f87a2ca4102cee
PatrikLord <patrik.j.lord@gmail.com>
2016-08-04 12:26:20 +0100

    2016-08-04's javascript workout

---
+++ contributions/PatrikLord/javascript/Functions/2016-08-04.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+IIFE pattern
+Social sharing without widgets
+Check if a document is done loading
