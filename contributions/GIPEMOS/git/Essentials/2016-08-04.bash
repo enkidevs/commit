af4b701655644630f713850fad186339250f8d6b
GIPEMOS <sc.webit@gmail.com>
2016-08-04 22:09:19 +0100

    2016-08-04's git workout

---
+++ contributions/GIPEMOS/git/Essentials/2016-08-04.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+Use `short` status to make output more compact
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
