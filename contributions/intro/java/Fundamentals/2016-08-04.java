5eaa7aeff2e614ba138614c1861284b3ef985424
intro <introvic.petr@gmail.com>
2016-08-04 07:31:10 +0100

    2016-08-04's java workout

---
+++ contributions/intro/java/Fundamentals/2016-08-04.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
+Using an interface as a parameter
