88b6af007b55ed58525125019f2327019ba225d3
nedel <nedel@gmx.com>
2016-08-04 06:36:26 +0100

    2016-08-04's linux workout

---
+++ contributions/nedel/linux/Package Management/2016-08-04.bash
@@ -0,0 +1,5 @@
+List installed packages
+Installing a `.deb` package from the terminal
+Prevent updating a specific package in Debian systems
+Get `apt` to use a mirror / faster mirror
+Using `PPAs`
