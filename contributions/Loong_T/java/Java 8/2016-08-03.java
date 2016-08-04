883c7694ed3086f57ac2474ce01428c992b6448d
Loong_T <talentloong@gmail.com>
2016-08-04 02:34:15 +0100

    2016-08-03's java workout

---
+++ contributions/Loong_T/java/Java 8/2016-08-03.java
@@ -0,0 +1,4 @@
+How to check for static dependencies of applications and libraries from the command line.
+Control OS `Process`es in `Java8`
+Avoid numerical overflows with *Java8* `exact` methods.
+`JJS` - Oracle Nashorn, newest JavaScript Engine
