ab8919a506af82c1d91b7a666eeb78d1a9196661
seans66 <seanstreet@hotmail.com>
2016-08-05 00:20:51 +0100

    2016-08-04's linux workout

---
+++ contributions/seans66/linux/File Management/2016-08-04.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
