4ad4c2785f4abf65d109795aeae7fe0fb00188ce
ybrjk <nick.kryloff@gmail.com>
2016-08-04 06:04:51 +0100

    2016-08-03's java workout

---
+++ contributions/ybrjk/java/Fundamentals/2016-08-03.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Methods performing *Security Checks* must be declared `Private` or `Final`
