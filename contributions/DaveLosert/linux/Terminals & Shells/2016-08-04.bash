aae65be23808dd8599f787a09b6d32ead548e6a6
DaveLosert <david.losert@gmx.de>
2016-08-04 06:40:51 +0100

    2016-08-04's linux workout

---
+++ contributions/DaveLosert/linux/Terminals & Shells/2016-08-04.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Clear bash history
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
