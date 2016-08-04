baadab1a6dd4777381676ac09613b5e96066da77
the1andonlydave <the1andonlydave@gmail.com>
2016-08-04 07:19:56 +0100

    2016-08-04's linux workout

---
+++ contributions/the1andonlydave/linux/System Management/2016-08-04.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Basic service management with `systemd`
