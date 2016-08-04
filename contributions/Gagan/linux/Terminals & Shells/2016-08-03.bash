3ce73a78ff95869a67e0e4f9518afe44357c72e2
Gagan <gagan.deep4850@gmail.com>
2016-08-04 21:54:38 +0100

    2016-08-03's linux workout

---
+++ contributions/Gagan/linux/Terminals & Shells/2016-08-03.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Terminal incognito mode
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
