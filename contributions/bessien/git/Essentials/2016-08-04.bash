7482e42b5640cf31e58dd48105f6f68de1de4b52
bessien <barbara@dynamic.vision>
2016-08-04 16:10:37 +0100

    2016-08-04's git workout

---
+++ contributions/bessien/git/Essentials/2016-08-04.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+How to set your username and email
+Make your log output pretty
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
