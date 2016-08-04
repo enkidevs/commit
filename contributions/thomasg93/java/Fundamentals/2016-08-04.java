c0c380220bd549c6f2f23dfdb6c5118a0e9685dd
thomasg93 <thomas.gauroy@gmail.com>
2016-08-04 07:05:35 +0100

    2016-08-04's java workout

---
+++ contributions/thomasg93/java/Fundamentals/2016-08-04.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
