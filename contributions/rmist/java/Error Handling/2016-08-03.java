76e095c32d69661da0391e854f91710d7f8c798a
rmist <rakesh_dta@live.in>
2016-08-04 02:48:43 +0100

    2016-08-03's java workout

---
+++ contributions/rmist/java/Error Handling/2016-08-03.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Prevent `exceptions` while logging Data
