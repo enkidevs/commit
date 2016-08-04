064ab0a3e6c78a30e30311dc93ec756ac618889b
adamyala <adamkyala@gmail.com>
2016-08-04 14:40:46 +0100

    2016-08-04's linux workout

---
+++ contributions/adamyala/linux/System Management/2016-08-04.bash
@@ -0,0 +1,7 @@
+CPU benchmark with `dd`
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Preventing a user from logging into the system
