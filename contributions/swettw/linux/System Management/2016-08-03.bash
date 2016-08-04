5f62471d427f1f2c6da42aef0ab083a569acff9d
swettw <swettw@gmail.com>
2016-08-04 01:39:02 +0100

    2016-08-03's linux workout

---
+++ contributions/swettw/linux/System Management/2016-08-03.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Cancel a system shutdown
