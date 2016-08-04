de30a5a47d9db0f2adfd3ffecae993beffd7a2f4
mrhub <niklas.huss@gmail.com>
2016-08-04 14:50:39 +0100

    2016-08-04's linux workout

---
+++ contributions/mrhub/linux/System Management/2016-08-04.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
