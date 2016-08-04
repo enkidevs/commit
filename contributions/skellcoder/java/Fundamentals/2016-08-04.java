ca489d2eb66fec65b5097f3b611bcb8a0ba25033
skellcoder <boman.jakob@gmail.com>
2016-08-04 18:43:02 +0100

    2016-08-04's java workout

---
+++ contributions/skellcoder/java/Fundamentals/2016-08-04.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
