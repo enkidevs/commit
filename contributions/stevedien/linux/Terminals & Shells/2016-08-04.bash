892a5d684dddac98e5d75c9010c4f66b1cf117b5
stevedien <lulzsecd@gmail.com>
2016-08-05 00:17:51 +0100

    2016-08-04's linux workout

---
+++ contributions/stevedien/linux/Terminals & Shells/2016-08-04.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Repeating commands with `watch`
+Clear the terminal instantly
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
