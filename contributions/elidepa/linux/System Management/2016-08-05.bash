f66071542616c0a80c1093ea9f3c9f4a8639a937
elidepa <samuli.depascale@helsinki.fi>
2016-08-05 08:12:31 +0100

    2016-08-05's linux workout

---
+++ contributions/elidepa/linux/System Management/2016-08-05.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Common log files
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Basic service management with `systemd`
