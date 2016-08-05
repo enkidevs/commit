2404d48911c4dbf7ad4f819aa576003a1668dbe7
mgarrett <melissa_garrett@hotmail.com>
2016-08-05 15:55:02 +0100

    2016-08-05's java workout

---
+++ contributions/mgarrett/java/Fundamentals/2016-08-05.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Methods performing *Security Checks* must be declared `Private` or `Final`
+How range operations work
