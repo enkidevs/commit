b7d6ee11fd04c536016f69853b8b06ba9740ff2c
ichi2410 <ichi2410@gmail.com>
2016-08-05 01:11:00 +0100

    2016-08-04's linux workout

---
+++ contributions/ichi2410/linux/Networking/2016-08-04.bash
@@ -0,0 +1,5 @@
+ Intro to `iptables`
+Quick port test with `netcat`
+How to clear `iptables` rules
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
