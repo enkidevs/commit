fb3cdaffccb71544a4d732224ade87a99eca789e
byt3b0n3 <byt3b0n3@gmail.com>
2016-08-05 13:41:33 +0100

    2016-08-05's linux workout

---
+++ contributions/byt3b0n3/linux/Terminals & Shells/2016-08-05.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Shebang `#!` explained 
+Execute a command without saving it in history
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
