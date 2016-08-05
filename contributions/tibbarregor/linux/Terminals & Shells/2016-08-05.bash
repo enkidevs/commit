500b10d02bab5ac0d292fbd2214c37dd48269f65
tibbarregor <romu42@gmail.com>
2016-08-05 10:22:28 +0100

    2016-08-05's linux workout

---
+++ contributions/tibbarregor/linux/Terminals & Shells/2016-08-05.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Execute a command without saving it in history
+Aliasing ssh connections
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
