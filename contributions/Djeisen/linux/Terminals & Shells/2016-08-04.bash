3bca63cbe3b1b17d55fbfc9625d1d832aad95d0e
Djeisen <jasonssuttles@gmail.com>
2016-08-05 03:07:46 +0100

    2016-08-04's linux workout

---
+++ contributions/Djeisen/linux/Terminals & Shells/2016-08-04.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Useful aliasing in bash
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
