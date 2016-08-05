54f106e036c5bda1dab0b092113429e8217fb56f
jefrailey <jcaetof+enki@gmail.com>
2016-08-05 16:26:35 +0100

    2016-08-05's linux workout

---
+++ contributions/jefrailey/linux/System Management/2016-08-05.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
