9476ac3771946d8831c4e578dfaeb3ecf0e8725d
kirillku <kubryakov@gmail.com>
2016-08-05 15:38:19 +0100

    2016-08-05's linux workout

---
+++ contributions/kirillku/linux/File Management/2016-08-05.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Using `touch` to alter files modification time
+Easy way of sharing files
