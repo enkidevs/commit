91b8b882e9cb43039e17cc04147674ff18d5024f
sidorov <rr.sidorov@gmail.com>
2016-08-05 23:17:28 +0100

    2016-08-05's java workout

---
+++ contributions/sidorov/java/Fundamentals/2016-08-05.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Methods performing *Security Checks* must be declared `Private` or `Final`
