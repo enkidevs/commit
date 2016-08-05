f92949e961b0468f1ca4c4384d03d012f9689c37
paulp <paulp@hotmail.sk>
2016-08-05 05:38:34 +0100

    2016-08-05's java workout

---
+++ contributions/paulp/java/Fundamentals/2016-08-05.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
