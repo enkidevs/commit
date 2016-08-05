02d53a1cce06bc3a26ce731be5f0731c32bc66f3
ivandevp <medinadiazivan@gmail.com>
2016-08-05 14:27:38 +0100

    2016-08-05's linux workout

---
+++ contributions/ivandevp/linux/System Management/2016-08-05.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Cancel a system shutdown
