4bf6d534acf9e1476074045079088ac542441fa7
treeleafmedia <steven@treeleaf.be>
2016-08-05 08:14:51 +0100

    2016-08-05's java workout

---
+++ contributions/treeleafmedia/java/Fundamentals/2016-08-05.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Using an interface as a parameter
