c968aafc1d051c672d4e375e51b90ed820b4b918
dragoss <dragos.stefanache@gmail.com>
2016-08-05 07:36:32 +0100

    2016-08-05's javascript workout

---
+++ contributions/dragoss/javascript/Node/2016-08-05.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+Use conventional property names for error handling in Node.js
+**Node** `os` module
+Wrapping errors in Node.js using _node-verror_
+Middleware/pipeline design pattern
