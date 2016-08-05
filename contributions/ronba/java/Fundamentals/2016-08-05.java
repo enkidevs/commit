ab9b14fe2b0eb562fff55a12bb4139f009a78af0
ronba <ronbadur02@gmail.com>
2016-08-05 01:46:28 +0100

    2016-08-05's java workout

---
+++ contributions/ronba/java/Fundamentals/2016-08-05.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+How to play a sound file in Java
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Supply `toString()` in all classes
