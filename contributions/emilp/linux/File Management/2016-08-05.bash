032bbbd50fa89eafcf9d30bc2c19f0fd38c31852
emilp <emiper@kth.se>
2016-08-05 11:15:15 +0100

    2016-08-05's linux workout

---
+++ contributions/emilp/linux/File Management/2016-08-05.bash
@@ -0,0 +1,5 @@
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Deleting non-empty directories
+Delete commands aliases
