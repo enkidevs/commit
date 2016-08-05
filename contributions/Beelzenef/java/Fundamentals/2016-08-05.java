919a08c47a23f8d5ed62b306133ce7a975750918
Beelzenef <elena.guzbla@gmail.com>
2016-08-05 09:42:45 +0100

    2016-08-05's java workout

---
+++ contributions/Beelzenef/java/Fundamentals/2016-08-05.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Java naming conventions
+Ternary operator
+Do not perform bitwise and arithmetic operations on the same data
+Implementing an `interface`
