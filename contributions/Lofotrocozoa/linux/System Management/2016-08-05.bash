d17794b9c314d1ef987923488a762b4b684cb1a4
Lofotrocozoa <lofotrocozoa@gmail.com>
2016-08-06 00:00:45 +0100

    2016-08-05's linux workout

---
+++ contributions/Lofotrocozoa/linux/System Management/2016-08-05.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
