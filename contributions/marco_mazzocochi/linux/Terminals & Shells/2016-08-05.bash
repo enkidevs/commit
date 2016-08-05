e5b70b9af505fda6c2433912f41e87275987a4ca
marco_mazzocochi <marco.mazzocchi.pc@gmail.com>
2016-08-05 13:01:15 +0100

    2016-08-05's linux workout

---
+++ contributions/marco_mazzocochi/linux/Terminals & Shells/2016-08-05.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Find any Unix / Linux command
+Random password generator
+Terminal incognito mode
+Keep useful commands in your shell history with tags
