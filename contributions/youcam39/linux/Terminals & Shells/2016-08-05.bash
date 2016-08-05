970c2f990b03ac7c6101b3822ec0e56cb8fce75f
youcam39 <youcam39@gmail.com>
2016-08-05 18:33:10 +0100

    2016-08-05's linux workout

---
+++ contributions/youcam39/linux/Terminals & Shells/2016-08-05.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Random password generator
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
