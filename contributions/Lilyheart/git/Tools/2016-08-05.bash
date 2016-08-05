65f99cda146dc95c01f967571cb9f3bca462d771
Lilyheart <lily.programs@gmail.com>
2016-08-05 14:58:39 +0100

    2016-08-05's git workout

---
+++ contributions/Lilyheart/git/Tools/2016-08-05.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Use a short checksum to find a commit
+Specify a commit by its ancestry
+Specify a range of commits using double dot syntax
+Sharing data by bundling
