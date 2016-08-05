60a6cdaeef941c5066a144457c9ebab68361a606
hundrafyra <niklas.brynolf@gmail.com>
2016-08-05 09:58:19 +0100

    2016-08-05's linux workout

---
+++ contributions/hundrafyra/linux/File Management/2016-08-05.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Working with `.gz` files
