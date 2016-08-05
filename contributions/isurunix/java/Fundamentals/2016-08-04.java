172f494fec855f8a4073a347986ba4dd29a2c0e7
isurunix <isurunix@gmail.com>
2016-08-05 07:32:11 +0100

    2016-08-04's java workout

---
+++ contributions/isurunix/java/Fundamentals/2016-08-04.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Supply `toString()` in all classes
