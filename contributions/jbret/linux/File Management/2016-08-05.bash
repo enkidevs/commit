b5923eb8bc9d48cbe5151e046fafbc436c8b121e
jbret <jbretheim@gmail.com>
2016-08-05 15:13:08 +0100

    2016-08-05's linux workout

---
+++ contributions/jbret/linux/File Management/2016-08-05.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
