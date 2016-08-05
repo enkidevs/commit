d2b15bda8a131baf353a0fcc627ff4ea2f860517
bmo77 <basha.others@gmail.com>
2016-08-05 17:23:39 +0100

    2016-08-05's linux workout

---
+++ contributions/bmo77/linux/File Management/2016-08-05.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
