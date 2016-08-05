f3e251f6fb6471503daed185aa4e8f3073ab5c4b
aquincy <brennanjl003@gmail.com>
2016-08-05 16:16:10 +0100

    2016-08-05's linux workout

---
+++ contributions/aquincy/linux/System Management/2016-08-05.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
