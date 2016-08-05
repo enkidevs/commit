b422d62317d380c7f60423690ae896e35d448b3d
martinoj2009 <martinoj2009@gmail.com>
2016-08-05 23:09:04 +0100

    2016-08-05's linux workout

---
+++ contributions/martinoj2009/linux/File Management/2016-08-05.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
