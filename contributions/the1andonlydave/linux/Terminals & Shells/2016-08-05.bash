aab47c0d40564177082b42cc1d4d8b79351836aa
the1andonlydave <the1andonlydave@gmail.com>
2016-08-05 18:46:03 +0100

    2016-08-05's linux workout

---
+++ contributions/the1andonlydave/linux/Terminals & Shells/2016-08-05.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
