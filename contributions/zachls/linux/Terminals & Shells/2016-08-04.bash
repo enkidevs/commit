0a95ca1f824dcd9a15b8a197bc97c653238ae2b0
zachls <zach.ls11@gmail.com>
2016-08-05 02:27:45 +0100

    2016-08-04's linux workout

---
+++ contributions/zachls/linux/Terminals & Shells/2016-08-04.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
