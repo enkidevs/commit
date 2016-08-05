9b047bd37dc357b047d041efd7b35f39ed0b1ab6
jpe.pinho <jpe.pinho@gmail.com>
2016-08-06 00:42:55 +0100

    2016-08-05's linux workout

---
+++ contributions/jpe.pinho/linux/File Management/2016-08-05.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
