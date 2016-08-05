14d949c5468708370437f8c4a081197cd48c5805
natoen <hgmatsuda@gmail.com>
2016-08-05 06:59:24 +0100

    2016-08-04's javascript workout

---
+++ contributions/natoen/javascript/Functions/2016-08-04.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Function call method
+Changing a functions context with `fn.call(object)`
