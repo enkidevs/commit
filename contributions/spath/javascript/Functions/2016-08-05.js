973b5bdaa51929089d46c748a9aa6ca7d25094e8
spath <samuel.path@gmail.com>
2016-08-05 06:42:09 +0100

    2016-08-05's javascript workout

---
+++ contributions/spath/javascript/Functions/2016-08-05.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function declarations vs function expressions
+Move cursor at the end of text input
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
