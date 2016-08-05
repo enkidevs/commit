f971035144082c5bd71a2608735df3ad90790215
josh_lang <joshuaclang1@gmail.com>
2016-08-05 16:41:22 +0100

    2016-08-05's linux workout

---
+++ contributions/josh_lang/linux/File Management/2016-08-05.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
