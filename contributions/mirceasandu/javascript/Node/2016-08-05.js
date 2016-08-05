11cf4fe688d68b32fb85ed8f56a2828fa4305efd
mirceasandu <mirceas17@gmail.com>
2016-08-05 09:58:00 +0100

    2016-08-05's javascript workout

---
+++ contributions/mirceasandu/javascript/Node/2016-08-05.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+First-error callbacks in **Node**
+**Node** `os` module
+`try-catch` only for **sync** code
