be7f4b665ea2b3b8a1ffac8798a920961b4fd2ea
Enterpaper <byebye99@qq.com>
2016-08-06 17:24:17 +0100

    2016-08-05's java workout

---
+++ contributions/Enterpaper/java/Data Types/2016-08-05.java
@@ -0,0 +1,5 @@
+`ArrayList` vs `LinkedList`
+`Math` vs `StrictMath` in java
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Retrieve the component type of an array
