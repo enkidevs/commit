d936c8b48f4d15575b04a12c02394958224b72e0
n3v3l <neve.loic@gmail.com>
2016-08-06 11:31:45 +0100

    2016-08-06's linux workout

---
+++ contributions/n3v3l/linux/System Management/2016-08-06.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
