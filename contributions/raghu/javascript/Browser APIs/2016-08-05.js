bc6b2717ce647fb9cf66f4b93a9b515cfcb1c8c9
raghu <raghu.9208@gmail.com>
2016-08-06 06:32:34 +0100

    2016-08-05's javascript workout

---
+++ contributions/raghu/javascript/Browser APIs/2016-08-05.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+`top.location.href`
+Page Visibility API
+MediaDevices.getUserMedia()
