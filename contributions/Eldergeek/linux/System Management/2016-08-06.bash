57ef92e35956a2373bd75e35d37dbf5df2279854
Eldergeek <david@geekguytech.com>
2016-08-06 18:22:06 +0100

    2016-08-06's linux workout

---
+++ contributions/Eldergeek/linux/System Management/2016-08-06.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
