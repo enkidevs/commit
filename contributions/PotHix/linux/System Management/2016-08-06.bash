beaa9e9bfc321ccad0436b73bda7650f8e8de5f6
PotHix <pothix@pothix.com>
2016-08-06 11:39:31 +0100

    2016-08-06's linux workout

---
+++ contributions/PotHix/linux/System Management/2016-08-06.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
