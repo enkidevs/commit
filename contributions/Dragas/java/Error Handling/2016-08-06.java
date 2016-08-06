9a5b64e817ec4cdbfe6f2374d058a0a96616ac79
Dragas <mantas@cpartner.lt>
2016-08-06 16:40:43 +0100

    2016-08-06's java workout

---
+++ contributions/Dragas/java/Error Handling/2016-08-06.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
