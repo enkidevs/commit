2da42199de6e9c3c293f56461b4fa5303f176736
kdewald <kevin.dewald@ieee.org>
2016-08-06 01:26:56 +0100

    2016-08-05's linux workout

---
+++ contributions/kdewald/linux/System Management/2016-08-05.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Find out if the system's architecture is 32 or 64 bit
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
