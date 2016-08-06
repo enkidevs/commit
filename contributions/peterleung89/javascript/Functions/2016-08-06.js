b8099d6be30f9334804288537298a8dbdf72be18
peterleung89 <peterleung89@gmail.com>
2016-08-06 10:29:54 +0100

    2016-08-06's javascript workout

---
+++ contributions/peterleung89/javascript/Functions/2016-08-06.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Hoisting
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Function call method
