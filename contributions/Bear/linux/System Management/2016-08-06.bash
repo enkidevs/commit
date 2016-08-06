23a1f3dc265ca7108ca4906074ae6eccc4c65d59
Bear <sc4ryb3ar@gmail.com>
2016-08-06 17:40:41 +0100

    2016-08-06's linux workout

---
+++ contributions/Bear/linux/System Management/2016-08-06.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
