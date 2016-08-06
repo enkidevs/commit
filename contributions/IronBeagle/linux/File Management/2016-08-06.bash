93418dbfd2678ee172b8ba758fdfc3a2c1ff5750
IronBeagle <noamhofshi@gmail.com>
2016-08-06 14:55:57 +0100

    2016-08-06's linux workout

---
+++ contributions/IronBeagle/linux/File Management/2016-08-06.bash
@@ -0,0 +1,7 @@
+Finding file with regexes
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
