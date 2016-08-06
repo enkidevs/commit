6a5be340dbcc4a9c759f2f52899fafc6ff9b295b
koliada <alex.fiator@gmail.com>
2016-08-06 10:29:20 +0100

    2016-08-06's linux workout

---
+++ contributions/koliada/linux/Terminals & Shells/2016-08-06.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
