b53864d2e138832829600b6c34ca7b69433ffd26
andreas_bergstrom <andreas.bergstrom@gmail.com>
2016-08-06 15:42:03 +0100

    2016-08-06's linux workout

---
+++ contributions/andreas_bergstrom/linux/Terminals & Shells/2016-08-06.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Random password generator
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
