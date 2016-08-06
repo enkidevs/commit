07285b5f6ca960634e2756ef26113c067757553c
Starjams <robertbmerriman@gmail.com>
2016-08-06 22:53:14 +0100

    2016-08-06's java workout

---
+++ contributions/Starjams/java/Fundamentals/2016-08-06.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
+Using an interface as a parameter
