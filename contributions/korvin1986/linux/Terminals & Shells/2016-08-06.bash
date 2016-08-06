491c48c2b659478bd6019e752df74c0a8b1e6d99
korvin1986 <korvin1986@gmail.com>
2016-08-06 08:15:46 +0100

    2016-08-06's linux workout

---
+++ contributions/korvin1986/linux/Terminals & Shells/2016-08-06.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Execute a command without saving it in history
+Aliasing ssh connections
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
