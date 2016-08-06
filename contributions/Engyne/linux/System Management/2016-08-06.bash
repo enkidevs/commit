96297452d71876ce765fff775134c74a1020f014
Engyne <batmadam@hotmail.com>
2016-08-06 22:13:36 +0100

    2016-08-06's linux workout

---
+++ contributions/Engyne/linux/System Management/2016-08-06.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
