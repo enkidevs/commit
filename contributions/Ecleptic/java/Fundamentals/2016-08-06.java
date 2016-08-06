4730015e69f358c241f3a73f5a57234f69cbeb58
Ecleptic <cameron@camerongreens.com>
2016-08-06 17:42:26 +0100

    2016-08-06's java workout

---
+++ contributions/Ecleptic/java/Fundamentals/2016-08-06.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Supply `toString()` in all classes
