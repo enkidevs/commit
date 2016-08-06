6466ac64fe619c4fedc5010d1f295c8055c73cc0
tahaburak <tahaburak.koc@gmail.com>
2016-08-06 19:39:45 +0100

    2016-08-06's java workout

---
+++ contributions/tahaburak/java/Fundamentals/2016-08-06.java
@@ -0,0 +1,5 @@
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
+Using an interface as a parameter
