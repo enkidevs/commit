18ef294829e62ae37ade25cdd42029e4093f4f13
emilp <emiper@kth.se>
2016-08-06 12:38:32 +0100

    2016-08-06's linux workout

---
+++ contributions/emilp/linux/File Management/2016-08-06.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Working with `.gz` files
