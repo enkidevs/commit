98ac4992c1d878799176c4355647f4e5d99d0c95
Thinner <sprattsmurf@gmail.com>
2016-08-06 22:25:43 +0100

    2016-08-06's linux workout

---
+++ contributions/Thinner/linux/File Management/2016-08-06.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
