5fe160dbc8885bac73d38050f626f9ada8700dcb
ronba <ronbadur02@gmail.com>
2016-08-06 02:12:17 +0100

    2016-08-06's java workout

---
+++ contributions/ronba/java/Fundamentals/2016-08-06.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Detect or prevent integer overflow
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
