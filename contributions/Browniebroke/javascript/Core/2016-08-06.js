b94cfa8d0cf297eb1a032cf8b16468805654a885
Browniebroke <alla.brunoo@gmail.com>
2016-08-06 08:16:30 +0100

    2016-08-06's javascript workout

---
+++ contributions/Browniebroke/javascript/Core/2016-08-06.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Meaning of polyfill
+Use `splice()` to remove an item from an array
+Double and single quotes
+Hoisting applies only to variable declarations, not initializations
