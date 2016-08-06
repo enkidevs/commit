4e1415dd6795fce84280a07f95f9234dacb48434
qgaultier <quentin.gaultier@gmail.com>
2016-08-06 04:59:48 +0100

    2016-08-05's linux workout

---
+++ contributions/qgaultier/linux/File Management/2016-08-05.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Extracting `tar` files to a specific directory
+Find the unknown process preventing deleting of files
+Easy way of sharing files
