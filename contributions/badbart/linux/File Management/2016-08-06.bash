1ebc27e2fa90dd52caa977935c9eff9ba7566c6e
badbart <tom.zille@gmail.com>
2016-08-06 04:52:29 +0100

    2016-08-06's linux workout

---
+++ contributions/badbart/linux/File Management/2016-08-06.bash
@@ -0,0 +1,6 @@
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
+Delete commands aliases
