d333441676e944140b5eadd172cb38fa0cb928f7
xazax <johan.daniel.nystrom@gmail.com>
2016-08-06 09:01:38 +0100

    2016-08-06's linux workout

---
+++ contributions/xazax/linux/System Management/2016-08-06.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+System uptime
