a0aa3f8f16a386e66bff077803010038521e0e6a
e7krgc <enam93@gmail.com>
2016-08-06 08:24:00 +0100

    2016-08-06's linux workout

---
+++ contributions/e7krgc/linux/System Management/2016-08-06.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
