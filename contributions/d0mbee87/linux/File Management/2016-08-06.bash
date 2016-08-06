9c999288c6c67d0e3a7d3e5498710f04c230e6c5
d0mbee87 <d0mbee87@gmail.com>
2016-08-06 09:07:38 +0100

    2016-08-06's linux workout

---
+++ contributions/d0mbee87/linux/File Management/2016-08-06.bash
@@ -0,0 +1,5 @@
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Delete commands aliases
+Easy way of sharing files
