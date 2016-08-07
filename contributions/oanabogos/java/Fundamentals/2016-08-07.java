cf72eff11267bba96f78ae72a09756231ac6a8f7
oanabogos <catalina.bogos@gmail.com>
2016-08-07 12:43:40 +0100

    2016-08-07's java workout

---
+++ contributions/oanabogos/java/Fundamentals/2016-08-07.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
