6e128e00726c43b6171951db236b743d4d165a7e
IronBeagle <noamhofshi@gmail.com>
2016-08-07 18:05:26 +0100

    2016-08-07's linux workout

---
+++ contributions/IronBeagle/linux/Terminals & Shells/2016-08-07.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Shebang `#!` explained 
+Execute a command without saving it in history
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
