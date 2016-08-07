13108abb8f33d53a3c9e4f2e2e954e9ec03f8a99
ivandevp <medinadiazivan@gmail.com>
2016-08-07 14:41:38 +0100

    2016-08-07's java workout

---
+++ contributions/ivandevp/java/Fundamentals/2016-08-07.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Using an interface as a parameter
