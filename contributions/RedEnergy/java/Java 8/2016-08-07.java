5c0b287df0786c815b4b5ab65fcb98dc650c3059
RedEnergy <forwot163@gmail.com>
2016-08-07 22:16:00 +0100

    2016-08-07's java workout

---
+++ contributions/RedEnergy/java/Java 8/2016-08-07.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Using `Arrays.parallelSort` 
+Use `Optional(s)` type in order to avoid `NullPointerException(s)`
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
