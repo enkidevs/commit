0f6099bbb008c95193a14315e40dae1fd34d7526
hardcorehall <josh.hall.tech@gmail.com>
2016-08-07 02:26:37 +0100

    2016-08-06's linux workout

---
+++ contributions/hardcorehall/linux/System Management/2016-08-06.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
