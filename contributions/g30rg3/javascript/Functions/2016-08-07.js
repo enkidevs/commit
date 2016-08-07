5f05a6c66a085c8178a8cf011a7c23d81ca93386
g30rg3 <g30rg3_ro@yahoo.com>
2016-08-07 08:22:00 +0100

    2016-08-07's javascript workout

---
+++ contributions/g30rg3/javascript/Functions/2016-08-07.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Easily generate a random `HEX` color
+Move cursor at the end of text input
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
