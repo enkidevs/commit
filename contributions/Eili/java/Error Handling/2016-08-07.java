0b6076a65270ccc600bc5fa2fbcb7df34484eb39
Eili <eilenberger.dominik@gmail.com>
2016-08-07 06:58:19 +0100

    2016-08-07's java workout

---
+++ contributions/Eili/java/Error Handling/2016-08-07.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
