5c93fa8da78040c7b5b4572c67299a028aa64a40
beacom <beacom@gmail.com>
2016-08-07 17:13:07 +0100

    2016-08-07's linux workout

---
+++ contributions/beacom/linux/System Management/2016-08-07.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Using `dd` To Create Disk Images
