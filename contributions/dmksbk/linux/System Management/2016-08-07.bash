b5f9302ba1f63995ce7a996947c77e5ac2b8941b
dmksbk <dmksbk@gmail.com>
2016-08-07 15:25:38 +0100

    2016-08-07's linux workout

---
+++ contributions/dmksbk/linux/System Management/2016-08-07.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Preventing a user from logging into the system
+System uptime
