543eeeffba98a986dbd3b644f48a474308e5239d
guybelo <guybelo@me.com>
2016-08-07 21:09:56 +0100

    2016-08-07's linux workout

---
+++ contributions/guybelo/linux/Networking/2016-08-07.bash
@@ -0,0 +1,5 @@
+How to clear `iptables` rules
+Useful ssh client optimizations 
+Getting the connection speed from the terminal
+Staying connected remotely via SSH without an interactive shell
+Short Introduction to `Wget` 
