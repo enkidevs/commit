5745b963ef9a4c9c3c9e80fec45f3de1adc89ed0
failfish <pirogoeth+enki@maio.me>
2016-08-07 23:09:44 +0100

    2016-08-07's java workout

---
+++ contributions/failfish/java/Fundamentals/2016-08-07.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
