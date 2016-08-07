33ba05ab962cca39ff18fd5b1b6083608358ffc4
sagiben <ilnextbus@gmail.com>
2016-08-07 05:17:06 +0100

    2016-08-07's linux workout

---
+++ contributions/sagiben/linux/Networking/2016-08-07.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Check `iptables` firewall status
+Disable `IPv6`
+Get real network statistics with `slurm`
+Staying connected remotely via SSH without an interactive shell
