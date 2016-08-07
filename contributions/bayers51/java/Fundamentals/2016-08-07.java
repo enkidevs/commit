d9c04f9c0ccdc19bd3b0b864ef4af95c3f0eaf17
bayers51 <bayers@tds.net>
2016-08-07 23:16:51 +0100

    2016-08-07's java workout

---
+++ contributions/bayers51/java/Fundamentals/2016-08-07.java
@@ -0,0 +1,5 @@
+Cropping images
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
