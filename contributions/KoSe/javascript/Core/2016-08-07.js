701b713f453d23eaf12e6883cb53c77c31c39211
KoSe <perfemum@gmail.com>
2016-08-07 08:11:38 +0100

    2016-08-07's javascript workout

---
+++ contributions/KoSe/javascript/Core/2016-08-07.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Use `splice()` to remove an item from an array
+Hoisting applies only to variable declarations, not initializations
+JavaScript compilation
+Detect online connection
