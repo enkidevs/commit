96183000c2ff2ae4de641382e457ec522e1a7571
Pohnibal <mihai.ppscu@gmail.com>
2016-08-07 07:27:19 +0100

    2016-08-07's linux workout

---
+++ contributions/Pohnibal/linux/Terminals & Shells/2016-08-07.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Aliasing ssh connections
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
