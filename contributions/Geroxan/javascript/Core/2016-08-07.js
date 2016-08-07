4ecc2f36251618ee5fbb2d9ea7d139895afd3718
Geroxan <alik810@seznam.cz>
2016-08-07 11:51:25 +0100

    2016-08-07's javascript workout

---
+++ contributions/Geroxan/javascript/Core/2016-08-07.js
@@ -0,0 +1,5 @@
+Treating a boolean as number
+Performing a function at timed intervals
+Truthiness
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
