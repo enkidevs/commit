bfa8e396e64035ab38014669774f3bbb6876c286
onozy <onozawa2106@gmail.com>
2016-08-08 00:39:13 +0100

    2016-08-07's javascript workout

---
+++ contributions/onozy/javascript/React/2016-08-07.js
@@ -0,0 +1,5 @@
+Functional Stateless Components in React
+Self-closing tags in **JSX**
+`null` value for *Controlled Components* in **React**
+Dealing with `this.props.children`
+Immutability helpers in **React**
