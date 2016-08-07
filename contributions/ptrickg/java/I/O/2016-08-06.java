d80110c92d14b9cff081bfc78e2064ef693db849
ptrickg <ptrickg@hotmail.com>
2016-08-07 01:11:39 +0100

    2016-08-06's java workout

---
+++ contributions/ptrickg/java/I/O/2016-08-06.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Deleting files and directories
+The `Console` class
