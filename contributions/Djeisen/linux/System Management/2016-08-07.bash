0e743bb61dff23a8291856ea0c6a8addece8da29
Djeisen <jasonssuttles@gmail.com>
2016-08-07 17:06:34 +0100

    2016-08-07's linux workout

---
+++ contributions/Djeisen/linux/System Management/2016-08-07.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Basic service management with `systemd`
+System uptime
