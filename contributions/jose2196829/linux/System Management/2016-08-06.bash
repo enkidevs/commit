3d32e5a563b2f1122635a1b7883457aaeb8eb05c
jose2196829 <santocarre@outlook.com>
2016-08-07 02:11:11 +0100

    2016-08-06's linux workout

---
+++ contributions/jose2196829/linux/System Management/2016-08-06.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
