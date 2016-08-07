5900ec0eb3d157a54618a1526478ff213c7cc284
maximelouet <maximelouet14+enki@gmail.com>
2016-08-07 11:12:19 +0100

    2016-08-07's git workout

---
+++ contributions/maximelouet/git/Essentials/2016-08-07.bash
@@ -0,0 +1,5 @@
+How to set your username and email
+Make your log output pretty
+Check the status of your files
+Use `short` status to make output more compact
+Remote repositories: fetching and pushing
