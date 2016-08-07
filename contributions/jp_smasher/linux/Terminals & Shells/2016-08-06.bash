e383a77bee430be6ab9ef6132fadeb2ff6f6c051
jp_smasher <wongjingping@gmail.com>
2016-08-07 15:49:53 +0100

    2016-08-06's linux workout

---
+++ contributions/jp_smasher/linux/Terminals & Shells/2016-08-06.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
