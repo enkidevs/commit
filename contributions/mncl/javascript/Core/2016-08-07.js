8344c938d2ed8de2293bd4323ad0154b8fedafce
mncl <emladenoff@gmail.com>
2016-08-07 10:47:58 +0100

    2016-08-07's javascript workout

---
+++ contributions/mncl/javascript/Core/2016-08-07.js
@@ -0,0 +1,5 @@
+Setting default values with `short circuiting`
+Use `splice()` to remove an item from an array
+Infinity
+The difference between null, undefined and NaN
+Prevent background image flicker in **IE**
