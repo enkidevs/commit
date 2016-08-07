27f70bb958787fce7d0bcfc7bac4a6adaa67ede2
kensportsfan <kenneth813@gmail.com>
2016-08-07 16:40:21 +0100

    2016-08-07's git workout

---
+++ contributions/kensportsfan/git/Essentials/2016-08-07.bash
@@ -0,0 +1,5 @@
+Using tags for version control
+Limiting log output by time
+Check the status of your files
+Use `short` status to make output more compact
+Remote repositories: fetching and pushing
