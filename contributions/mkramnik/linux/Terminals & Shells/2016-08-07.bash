9b237c0aeb31e003cfebfd81a5128e8363698686
mkramnik <mkramnik@gmail.com>
2016-08-08 00:55:52 +0100

    2016-08-07's linux workout

---
+++ contributions/mkramnik/linux/Terminals & Shells/2016-08-07.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
