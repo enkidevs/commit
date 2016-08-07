2c131f6d5ade87602efb2966cdf240579dd04048
liquidtheory <souan.edouard0@gmail.com>
2016-08-07 14:37:55 +0100

    2016-08-07's linux workout

---
+++ contributions/liquidtheory/linux/Terminals & Shells/2016-08-07.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Useful aliasing in bash
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
