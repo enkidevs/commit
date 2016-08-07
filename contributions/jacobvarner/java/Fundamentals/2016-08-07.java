964fe8c0578a585fb17cceb4c294ff7a386cff94
jacobvarner <jacob.varner14@gmail.com>
2016-08-07 15:42:12 +0100

    2016-08-07's java workout

---
+++ contributions/jacobvarner/java/Fundamentals/2016-08-07.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
