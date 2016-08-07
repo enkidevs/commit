ba8bd487d0992cfdd0fe62782aad2f864ff065f0
Perfectloser <carver@justcarver.com>
2016-08-07 22:06:48 +0100

    2016-08-07's javascript workout

---
+++ contributions/Perfectloser/javascript/Functions/2016-08-07.js
@@ -0,0 +1,5 @@
+Difference between **.call** and **.apply** methods
+Get query/url variables
+Social sharing without widgets
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
