541ded8f304ffe4fc05e7b07f7282b4894e98937
ahlexander <alex@devmotion.ro>
2016-08-07 15:37:06 +0100

    2016-08-07's java workout

---
+++ contributions/ahlexander/java/Fundamentals/2016-08-07.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Do not return *references* to private *mutable* class members
