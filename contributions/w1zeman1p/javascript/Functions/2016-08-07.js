f5fd8c5599f4b8f26c39d639b48c70765997ad86
w1zeman1p <cjavilla@gmail.com>
2016-08-07 15:04:28 +0100

    2016-08-07's javascript workout

---
+++ contributions/w1zeman1p/javascript/Functions/2016-08-07.js
@@ -0,0 +1,5 @@
+Hoisting
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Method chaining
+Easily generate a random `HEX` color
