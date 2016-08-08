090445ed832aa6f6c8bafec1727970052425db0e
kesegotumisang <kesego.tumisang@gmail.com>
2016-08-08 15:29:20 +0100

    2016-08-08's java workout

---
+++ contributions/kesegotumisang/java/Fundamentals/2016-08-08.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Use meaningful names
+Using the `Console`
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
