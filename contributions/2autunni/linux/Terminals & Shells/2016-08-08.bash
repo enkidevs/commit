61366f745ee9dfdeaf4aeb18688e683a6623fccf
2autunni <marcello.cerruti@gmail.com>
2016-08-08 06:03:49 +0100

    2016-08-08's linux workout

---
+++ contributions/2autunni/linux/Terminals & Shells/2016-08-08.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
