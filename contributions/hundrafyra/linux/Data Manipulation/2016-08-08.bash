dc8d712fc2d281c1edc8ceb2226eca917df88d1a
hundrafyra <niklas.brynolf@gmail.com>
2016-08-08 09:03:04 +0100

    2016-08-08's linux workout

---
+++ contributions/hundrafyra/linux/Data Manipulation/2016-08-08.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+Getting rid of duplicate lines
+Image manipulation  using `convert`
+Clean configuration files using `sed`
+`sort` lines in a text file
