289a71941b14ef96accaea96121be9a1a7b6d597
mryraghi <romeo.bellon@me.com>
2016-08-08 22:15:56 +0100

    2016-08-08's linux workout

---
+++ contributions/mryraghi/linux/System Management/2016-08-08.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
