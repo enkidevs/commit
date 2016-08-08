64e8fcc5600cd9d6de0097747b45a7f3cab2caf8
GeekChicGoddess <mtempletoncantu@gmail.com>
2016-08-08 01:31:37 +0100

    2016-08-07's javascript workout

---
+++ contributions/GeekChicGoddess/javascript/Functions/2016-08-07.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Method chaining
+Get query/url variables
+Changing a functions context with `fn.call(object)`
