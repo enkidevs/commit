0ebce440b9ae08b3e84e46f3b7ba2dd1312e3cdd
Kelem <cedric.deturche@gmail.com>
2016-08-08 05:36:46 +0100

    2016-08-08's java workout

---
+++ contributions/Kelem/java/Fundamentals/2016-08-08.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
