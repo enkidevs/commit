4d9ef54b6b44125301ccc4e63a180faaf588a174
indoriMonk <amanshulvyas@gmail.com>
2016-08-08 20:23:23 +0100

    2016-08-08's java workout

---
+++ contributions/indoriMonk/java/Data Types/2016-08-08.java
@@ -0,0 +1,5 @@
+`ArrayList` vs `LinkedList`
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Retrieve the component type of an array
