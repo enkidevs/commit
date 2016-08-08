70eece70b971d6efd5cef150a43a459067710e65
byt3b0n3 <byt3b0n3@gmail.com>
2016-08-08 09:48:45 +0100

    2016-08-08's linux workout

---
+++ contributions/byt3b0n3/linux/System Management/2016-08-08.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+System uptime
