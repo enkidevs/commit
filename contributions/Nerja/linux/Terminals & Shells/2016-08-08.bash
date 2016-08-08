ca542873122a55c32f2f47819ab26b7d6008d15b
Nerja <marcusrodan@gmail.com>
2016-08-08 17:34:06 +0100

    2016-08-08's linux workout

---
+++ contributions/Nerja/linux/Terminals & Shells/2016-08-08.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Clear the terminal instantly
+Find any Unix / Linux command
+Keep useful commands in your shell history with tags
