3ad5b805928a6ccf91260ca6842b1662ad45962b
xazax <johan.daniel.nystrom@gmail.com>
2016-08-08 08:48:31 +0100

    2016-08-08's linux workout

---
+++ contributions/xazax/linux/Terminals & Shells/2016-08-08.bash
@@ -0,0 +1,5 @@
+Find any Unix / Linux command
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
