2900f629a87a4f1171c72cf51a806c63f25abce4
ozymandias <stenbackfelix@gmail.com>
2016-08-08 17:30:40 +0100

    2016-08-08's linux workout

---
+++ contributions/ozymandias/linux/System Management/2016-08-08.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
