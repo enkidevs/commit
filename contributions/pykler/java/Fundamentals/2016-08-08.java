acf79823bbf4b94e326ac3af1395b637ce53744a
pykler <hnassrat@gmail.com>
2016-08-08 10:29:48 +0100

    2016-08-08's java workout

---
+++ contributions/pykler/java/Fundamentals/2016-08-08.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Using `Printstream.format` to format a string before printing
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+How range operations work
