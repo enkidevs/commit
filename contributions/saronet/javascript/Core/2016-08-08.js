d8365579b57dbb461a609463e8a9dd1660d10e0b
saronet <sarahillel436@gmail.com>
2016-08-08 13:56:34 +0100

    2016-08-08's javascript workout

---
+++ contributions/saronet/javascript/Core/2016-08-08.js
@@ -0,0 +1,5 @@
+Meaning of polyfill
+Setting the length of an array 
+Hoisting applies only to variable declarations, not initializations
+Apply `map` function to array items
+Getting the *real* dimensions of an image
