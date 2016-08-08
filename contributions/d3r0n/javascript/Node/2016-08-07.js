8a0999afae848895a2900b24490febeba26314af
d3r0n <mider2+enki@gmail.com>
2016-08-08 02:51:58 +0100

    2016-08-07's javascript workout

---
+++ contributions/d3r0n/javascript/Node/2016-08-07.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+`process.nextTick()` callback pattern
+`try-catch` only for **sync** code
