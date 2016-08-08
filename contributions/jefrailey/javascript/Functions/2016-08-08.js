5e0161b11b226b5897674cfaf99130b882686cf4
jefrailey <jcaetof+enki@gmail.com>
2016-08-08 15:44:26 +0100

    2016-08-08's javascript workout

---
+++ contributions/jefrailey/javascript/Functions/2016-08-08.js
@@ -0,0 +1,5 @@
+Difference between **.call** and **.apply** methods
+Anonymous functions
+Social sharing without widgets
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
