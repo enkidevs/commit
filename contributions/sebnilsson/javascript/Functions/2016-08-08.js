9a9e4d355b4df7e66509e31fdc4d51d0c927cd61
sebnilsson <seb.nilsson@gmail.com>
2016-08-08 06:33:17 +0100

    2016-08-08's javascript workout

---
+++ contributions/sebnilsson/javascript/Functions/2016-08-08.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Easily generate a random `HEX` color
+Check if a document is done loading
