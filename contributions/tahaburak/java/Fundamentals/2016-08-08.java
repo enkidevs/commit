ae9e5feceded42f61e90e368445aff84537c000c
tahaburak <tahaburak.koc@gmail.com>
2016-08-08 07:18:29 +0100

    2016-08-08's java workout

---
+++ contributions/tahaburak/java/Fundamentals/2016-08-08.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Overloading Methods in Java
+Uses of the `final` keyword
+The trade-off between short and descriptive names
+Metadata: setting a file's owner
