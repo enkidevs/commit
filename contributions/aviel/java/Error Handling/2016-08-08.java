bccac3a997288e682acc1ef4eb64ac97190f921b
aviel <avielgross@gmail.com>
2016-08-08 12:57:20 +0100

    2016-08-08's java workout

---
+++ contributions/aviel/java/Error Handling/2016-08-08.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Handling Null Pointer Exceptions
+Throwing an `exception`
