5dfb4e1d58d701f5751d23b51bd67a4c41ba1441
vivek <vivek_thakur22@yahoo.com>
2016-08-08 02:43:05 +0100

    2016-08-07's java workout

---
+++ contributions/vivek/java/Fundamentals/2016-08-07.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
