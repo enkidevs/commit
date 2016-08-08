2c7dc9fe6b76734271c2ef9dae67708db7f7104b
v_coder <vlad_rym@mail.ru>
2016-08-08 06:06:33 +0100

    2016-08-08's java workout

---
+++ contributions/v_coder/java/Error Handling/2016-08-08.java
@@ -0,0 +1,5 @@
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
