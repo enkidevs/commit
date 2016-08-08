f3ac1283fe937cc18a0a7d3446777c07837f134b
vano <boris.avney@imperva.com>
2016-08-08 06:58:06 +0100

    2016-08-08's linux workout

---
+++ contributions/vano/linux/System Management/2016-08-08.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Using `dd` To Create Disk Images
