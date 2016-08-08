107b7a92b1f40d119c58168df0aa52f9e841a566
Alonski <alonzorz@gmail.com>
2016-08-08 09:11:15 +0100

    2016-08-08's linux workout

---
+++ contributions/Alonski/linux/Terminals & Shells/2016-08-08.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
