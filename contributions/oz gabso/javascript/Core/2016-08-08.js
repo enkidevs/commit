ce6fe68795881556f9ca8e0eacf60506d516c024
oz gabso <oz.gabso@gmail.com>
2016-08-08 19:10:14 +0100

    2016-08-08's javascript workout

---
+++ contributions/oz gabso/javascript/Core/2016-08-08.js
@@ -0,0 +1,5 @@
+Data type comparison in `switch` statements
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+Context in JavaScript
+Prevent background image flicker in **IE**
