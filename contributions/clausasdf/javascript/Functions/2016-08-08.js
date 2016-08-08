d7e7531bfe6d14f848a38e827af3eb6e9d6c971e
clausasdf <claudiu.florea@outlook.com>
2016-08-08 05:13:19 +0100

    2016-08-08's javascript workout

---
+++ contributions/clausasdf/javascript/Functions/2016-08-08.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Anonymous functions
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
