eabd4d6efe9291ba076f831f18a2ec91e38024d7
nakamin <antonwizkam@gmail.com>
2016-08-08 14:17:27 +0100

    2016-08-08's linux workout

---
+++ contributions/nakamin/linux/File Management/2016-08-08.bash
@@ -0,0 +1,6 @@
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
+Easy way of sharing files
