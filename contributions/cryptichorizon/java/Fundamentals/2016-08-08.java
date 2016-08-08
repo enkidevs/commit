e57516c72796a25701c02fd352a863b257221239
cryptichorizon <cryptichorizon@gmail.com>
2016-08-08 09:43:06 +0100

    2016-08-08's java workout

---
+++ contributions/cryptichorizon/java/Fundamentals/2016-08-08.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Cropping images
+Altering format string output by changing a format specifier's `argument_index`
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
