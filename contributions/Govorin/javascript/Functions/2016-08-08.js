fe7c1d130f38451bda59db78b1dd4ecfedbc705b
Govorin <govorinviktor@gmail.com>
2016-08-08 10:58:41 +0100

    2016-08-08's javascript workout

---
+++ contributions/Govorin/javascript/Functions/2016-08-08.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Anonymous functions
+Get query/url variables
+Easily generate a random `HEX` color
