510fdfd3f3bb92e831b90404e450df988f3418ba
mr.hyman63 <mr.hyman63@gmail.com>
2016-08-08 09:44:53 +0100

    2016-08-07's java workout

---
+++ contributions/mr.hyman63/java/Error Handling/2016-08-07.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Throwing an `exception`
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
