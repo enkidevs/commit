35cf4717cd996b2356b0d7d5be9073ad5693b315
kcmartin <kcmartin2@mac.com>
2016-08-09 07:49:54 +0100

    2016-08-08's java workout

---
+++ contributions/kcmartin/java/Threads & Locks/2016-08-08.java
@@ -0,0 +1,5 @@
+Multithreading: use local variables
+Use thread pool executors
+`Thread.sleep` and `Thread.yield` do not have synchronization mechanisms
+Thread interruptions
+Implementing your own `Thread` 
