fb6425d99fd02f4dd4ecedc37e46dd3f1974caa2
materemias <materemias@gmail.com>
2016-08-09 08:41:34 +0100

    2016-08-09's java workout

---
+++ contributions/materemias/java/Fundamentals/2016-08-09.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
