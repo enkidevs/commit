439809442e0749b235431de39652ec1db9b29c6c
WizApp <daniel.rosendorf@googlemail.com>
2016-08-09 19:51:27 +0100

    2016-08-09's javascript workout

---
+++ contributions/WizApp/javascript/Core/2016-08-09.js
@@ -0,0 +1,5 @@
+Multi-line string variables
+Data type comparison in `switch` statements
+The distinction between `==` and `===`
+Extra function arguments are undefined by default
+Prevent background image flicker in **IE**
