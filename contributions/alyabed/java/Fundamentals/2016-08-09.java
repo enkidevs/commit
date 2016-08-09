2f5c5ffe844a28e5ce985e1e04cd41053d63f95f
alyabed <alyabed95@gmail.com>
2016-08-09 12:59:20 +0100

    2016-08-09's java workout

---
+++ contributions/alyabed/java/Fundamentals/2016-08-09.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
