9f028b17766435816c3419595b22adab2d6ddd87
maznika <gmasnica@gmail.com>
2016-08-09 09:33:04 +0100

    2016-08-08's linux workout

---
+++ contributions/maznika/linux/System Management/2016-08-08.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
