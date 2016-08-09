5e6dad2ff67d7409704c72c0441cc846edb1dc9a
y3ddet <ted@xassembly.com>
2016-08-09 19:03:40 +0100

    2016-08-09's linux workout

---
+++ contributions/y3ddet/linux/Terminals & Shells/2016-08-09.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Aliasing ssh connections
+Random password generator
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
