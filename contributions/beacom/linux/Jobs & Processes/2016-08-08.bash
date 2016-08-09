235b2e9a0630f1de1c45580a3e4079b65274818a
beacom <beacom@gmail.com>
2016-08-09 01:31:12 +0100

    2016-08-08's linux workout

---
+++ contributions/beacom/linux/Jobs & Processes/2016-08-08.bash
@@ -0,0 +1,5 @@
+Keeping processes running after you log out
+Fixing the shell in cron
+Common issue with scripts in `cron`
+Set a process priority
+How to pause a process and run others in the background
