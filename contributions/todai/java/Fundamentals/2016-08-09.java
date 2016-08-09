5a2526edba3c3647cde6d68adab04f1b18b34114
todai <joabaj88@gmail.com>
2016-08-09 12:00:01 +0100

    2016-08-09's java workout

---
+++ contributions/todai/java/Fundamentals/2016-08-09.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
