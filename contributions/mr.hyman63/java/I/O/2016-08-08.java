d80ec11ea009f56136d5382433561e50d51c7536
mr.hyman63 <mr.hyman63@gmail.com>
2016-08-09 07:25:38 +0100

    2016-08-08's java workout

---
+++ contributions/mr.hyman63/java/I/O/2016-08-08.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+Create system file paths using the `Path` class
+Checking for the existence of files and directories
+Retrieving file store attributes
+Listing a file system's root directories
