6dd6eb19d0e9e1f5a819197086bfd60c70855452
agoowong <augustine.wongbh@gmail.com>
2016-08-09 09:50:33 +0100

    2016-08-08's java workout

---
+++ contributions/agoowong/java/Fundamentals/2016-08-08.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Use meaningful names
+Altering format string output by changing a format specifier's `argument_index`
+Using `Printstream.format` to format a string before printing
