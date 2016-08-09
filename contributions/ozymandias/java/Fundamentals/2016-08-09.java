59fc69abdad2198282456034e2d38be69579c50a
ozymandias <stenbackfelix@gmail.com>
2016-08-09 08:45:06 +0100

    2016-08-09's java workout

---
+++ contributions/ozymandias/java/Fundamentals/2016-08-09.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
