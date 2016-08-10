93aadf9dc3776f9eea5fdd013ae67edea022fefb
areksu <ktmsah@gmail.com>
2016-08-10 16:09:01 +0100

    2016-08-10's java workout

---
+++ contributions/areksu/java/Fundamentals/2016-08-10.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
