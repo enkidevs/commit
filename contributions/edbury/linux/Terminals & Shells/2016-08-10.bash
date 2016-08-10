d84e0ccee60573d79e18ce52667779d541799afe
edbury <edbury.enegren@gmail.com>
2016-08-10 16:29:34 +0100

    2016-08-10's linux workout

---
+++ contributions/edbury/linux/Terminals & Shells/2016-08-10.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Quick `bash` shortcuts 
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
