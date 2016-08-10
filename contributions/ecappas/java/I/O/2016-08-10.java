b766454852cbf60fdfd08bcc3ced50cff2e87b58
ecappas <e_cappas@hotmail.com>
2016-08-10 12:17:14 +0100

    2016-08-10's java workout

---
+++ contributions/ecappas/java/I/O/2016-08-10.java
@@ -0,0 +1,5 @@
+Using buffered streams
+Checking for the existence of files and directories
+Reading file attributes
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
