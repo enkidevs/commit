3f7fce75bcddc78d6160677894cc6dfc37bd806d
c0d3r <billylg@gmail.com>
2016-08-10 17:27:10 +0100

    2016-08-09's javascript workout

---
+++ contributions/c0d3r/javascript/Core/2016-08-09.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Performing a function at timed intervals
+Use `splice()` to remove an item from an array
+Using assignment operators
+Inaccuracy of binary floating-point format
