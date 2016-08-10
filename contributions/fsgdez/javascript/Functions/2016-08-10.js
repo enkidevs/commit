610d17fbaf9b124974c73ad9cd54b6658ddab18c
fsgdez <fadii925@gmail.com>
2016-08-10 18:02:21 +0100

    2016-08-10's javascript workout

---
+++ contributions/fsgdez/javascript/Functions/2016-08-10.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+`.bind()` 
+Easily generate a random `HEX` color
