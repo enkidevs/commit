7dad8839b3d8cb77497436b82c7e998bb5258440
pascalf79 <pascalf79@gmail.com>
2016-08-10 12:59:04 +0100

    2016-08-10's linux workout

---
+++ contributions/pascalf79/linux/Terminals & Shells/2016-08-10.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Useful aliasing in bash
+Adding directories to your `$PATH`
