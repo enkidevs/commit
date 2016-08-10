7acb92c740dd6d04e93cb9cb3cbcae6b0446277c
egantz <eyal@gantz.co.il>
2016-08-10 22:59:57 +0100

    2016-08-10's linux workout

---
+++ contributions/egantz/linux/System Management/2016-08-10.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
