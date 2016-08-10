8849dc07551d2e23e626412861eceec191223cec
e7krgc <enam93@gmail.com>
2016-08-10 07:04:33 +0100

    2016-08-10's linux workout

---
+++ contributions/e7krgc/linux/Terminals & Shells/2016-08-10.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Useful aliasing in bash
+Get to know  your commands with `type`
+The Basics of Environment Variables
