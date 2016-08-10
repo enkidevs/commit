589183d4db1c67211e57483137f3025600b2aa1c
TotKindGott <konstantin.a.kolosov@gmail.com>
2016-08-10 10:13:01 +0100

    2016-08-10's linux workout

---
+++ contributions/TotKindGott/linux/System Management/2016-08-10.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Commands to shutdown or restart the system
