723fff131f9111efd0654baf43f6ac29a2bec2e2
elidepa <samuli.depascale@helsinki.fi>
2016-08-10 08:11:00 +0100

    2016-08-10's javascript workout

---
+++ contributions/elidepa/javascript/Functions/2016-08-10.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Method chaining
+Changing a functions context with `fn.call(object)`
