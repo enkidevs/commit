2e3dc9c7aae43577472f40004feefe666a0e5571
marioso <hello@mariosommer.at>
2016-08-10 05:52:04 +0100

    2016-08-10's git workout

---
+++ contributions/marioso/git/Essentials/2016-08-10.bash
@@ -0,0 +1,5 @@
+What is stored in a commit?
+How to set your username and email
+Make your log output pretty
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
