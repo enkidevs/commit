16d666bd55276a7b73df504e51eda5e9d5cecdbf
robertofd1995 <robertofd1995@gmail.com>
2016-08-10 09:10:44 +0100

    2016-08-10's java workout

---
+++ contributions/robertofd1995/java/Fundamentals/2016-08-10.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
