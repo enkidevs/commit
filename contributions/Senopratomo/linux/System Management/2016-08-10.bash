34e958afc9aabda23eb7adfea68fe57371a2b48a
Senopratomo <lord.ernesto@gmail.com>
2016-08-10 18:05:55 +0100

    2016-08-10's linux workout

---
+++ contributions/Senopratomo/linux/System Management/2016-08-10.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
