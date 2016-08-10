249e8f185d3f7f7bdf2df3173413c851c904e28c
mihaiberq <mihaiberq@gmail.com>
2016-08-10 15:33:11 +0100

    2016-08-10's linux workout

---
+++ contributions/mihaiberq/linux/System Management/2016-08-10.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
