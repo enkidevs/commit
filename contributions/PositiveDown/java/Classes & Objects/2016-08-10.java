ed94c52083c2a9487d63ee9427a7673e2ba38caf
PositiveDown <svensketrash@gmail.com>
2016-08-10 23:40:12 +0100

    2016-08-10's java workout

---
+++ contributions/PositiveDown/java/Classes & Objects/2016-08-10.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Utility classes and `static` methods
+Compare classes and not class names
+How and when to use `WeakHashMap` class
+Fields with Java **Reflection API**
