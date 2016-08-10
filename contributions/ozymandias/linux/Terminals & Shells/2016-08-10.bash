f059f937edfdd52845632c940e28d13f5ed8c8a0
ozymandias <stenbackfelix@gmail.com>
2016-08-10 20:26:02 +0100

    2016-08-10's linux workout

---
+++ contributions/ozymandias/linux/Terminals & Shells/2016-08-10.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
