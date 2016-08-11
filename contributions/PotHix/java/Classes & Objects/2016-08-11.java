b6dec33e52d8713af6f525a74d7584829d53a0c3
PotHix <pothix@pothix.com>
2016-08-11 11:56:52 +0100

    2016-08-11's java workout

---
+++ contributions/PotHix/java/Classes & Objects/2016-08-11.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Compare classes and not class names
+Diamond Operator
+String formatting and parsing with `SimpleDateFormat`
+Fields with Java **Reflection API**
