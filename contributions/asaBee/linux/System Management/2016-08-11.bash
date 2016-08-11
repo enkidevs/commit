74d67d387dc086f5b11e8ed807c8820614ed254c
asaBee <hlovers.forever@gmail.com>
2016-08-11 21:42:57 +0100

    2016-08-11's linux workout

---
+++ contributions/asaBee/linux/System Management/2016-08-11.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
