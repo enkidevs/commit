615224f86eb1d018d8e124d51c98e6b7cb1c29a3
yoleks <yoan.aleksandar.spasov@gmail.com>
2016-08-11 15:08:11 +0100

    2016-08-11's linux workout

---
+++ contributions/yoleks/linux/Terminals & Shells/2016-08-11.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
