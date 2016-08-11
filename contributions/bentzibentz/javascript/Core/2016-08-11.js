829326ad13c9a1b1e3d3a50f65e77060ff395094
bentzibentz <info@fabianbentz.de>
2016-08-11 10:38:29 +0100

    2016-08-11's javascript workout

---
+++ contributions/bentzibentz/javascript/Core/2016-08-11.js
@@ -0,0 +1,5 @@
+Types of numbers
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Prototype methods
+Extra function arguments are undefined by default
