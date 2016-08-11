963685c66695b966ac196a5412b67970c9648b8b
KoSe <perfemum@gmail.com>
2016-08-11 06:14:07 +0100

    2016-08-11's javascript workout

---
+++ contributions/KoSe/javascript/Core/2016-08-11.js
@@ -0,0 +1,5 @@
+Setting default values with `short circuiting`
+Format currency in JavaScript
+Performing a function at timed intervals
+Using `eval`
+Extra function arguments are undefined by default
