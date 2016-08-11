6c6b11298cdf6a2fc17c797c029617c88c1214db
ibrahim <ibrahim2221916@gmail.com>
2016-08-11 23:02:54 +0100

    2016-08-11's java workout

---
+++ contributions/ibrahim/java/Fundamentals/2016-08-11.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
