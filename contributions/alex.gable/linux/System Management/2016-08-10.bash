9d0301ce32b4b1873eece97dd71b546229e154c7
alex.gable <alex.gable@gmail.com>
2016-08-11 02:57:54 +0100

    2016-08-10's linux workout

---
+++ contributions/alex.gable/linux/System Management/2016-08-10.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
