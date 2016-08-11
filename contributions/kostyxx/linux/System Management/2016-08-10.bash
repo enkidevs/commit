5f6251205cbd1d2c3ba08c11a00ca33bbbfd0093
kostyxx <malax07@gmail.com>
2016-08-11 04:19:15 +0100

    2016-08-10's linux workout

---
+++ contributions/kostyxx/linux/System Management/2016-08-10.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
