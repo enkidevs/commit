dfacffcc630a26dc2045d8d64f1037efad840e95
sachinkmr <sachin.roy12345@gmail.com>
2016-08-11 19:29:51 +0100

    2016-08-10's java workout

---
+++ contributions/sachinkmr/java/Fundamentals/2016-08-10.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Ternary operator
+Using Inheritance to reduce code repetition
+Using `Printstream.format` to format a string before printing
+How range operations work
