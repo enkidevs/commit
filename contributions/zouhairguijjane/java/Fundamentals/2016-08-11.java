01c1d266f03be02a9143cab0fbc594775b2977d7
zouhairguijjane <zh.guijjane@gmail.com>
2016-08-11 07:28:09 +0100

    2016-08-11's java workout

---
+++ contributions/zouhairguijjane/java/Fundamentals/2016-08-11.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Supply `toString()` in all classes
