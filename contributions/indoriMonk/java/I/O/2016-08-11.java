f9ee64a024fcdebe7ad0350ae99752e5d23cd426
indoriMonk <amanshulvyas@gmail.com>
2016-08-11 20:02:20 +0100

    2016-08-11's java workout

---
+++ contributions/indoriMonk/java/I/O/2016-08-11.java
@@ -0,0 +1,5 @@
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
+Deleting files and directories
