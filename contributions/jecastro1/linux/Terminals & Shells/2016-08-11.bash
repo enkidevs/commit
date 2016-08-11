eb7ca112bae18348365a192ff001594abe91aa82
jecastro1 <jecastro.ie@gmail.com>
2016-08-11 12:50:00 +0100

    2016-08-11's linux workout

---
+++ contributions/jecastro1/linux/Terminals & Shells/2016-08-11.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Terminal based browser
+Random password generator
+Sequential execution using the `;` statement separator
