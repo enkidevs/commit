9154cc6173327e715f07a0101b216ce1387f6259
rmist <rakesh_dta@live.in>
2016-08-12 09:25:19 +0100

    2016-08-11's linux workout

---
+++ contributions/rmist/linux/System Management/2016-08-11.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
+System uptime
