f25f655d312ba56b25cb41f477afa6179eae1fbb
mezkur7 <erdoan_ahmet@hotmail.com>
2016-08-12 13:18:11 +0100

    2016-08-12's javascript workout

---
+++ contributions/mezkur7/javascript/Core/2016-08-12.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Setting the length of an array 
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
