ea071c1237467da976cd105b98a053f2d8633a3f
naps62 <miguel@subvisual.co>
2016-08-12 09:11:27 +0100

    2016-08-12's linux workout

---
+++ contributions/naps62/linux/Terminals & Shells/2016-08-12.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Repeating commands with `watch`
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Keep useful commands in your shell history with tags
