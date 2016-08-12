87c7e389f141ee4e9569a0810c541cc2522ae23a
sylcha <chambon.sylvain@gmail.com>
2016-08-12 23:02:59 +0100

    2016-08-12's javascript workout

---
+++ contributions/sylcha/javascript/Node/2016-08-12.js
@@ -0,0 +1,5 @@
+`uncaughtException` listener in Node.js
+Manage child processes in **Node**
+Bulk-Write in **Node** with `cork()`
+Middleware/pipeline design pattern
+Avoid using `sudo` when running Node
