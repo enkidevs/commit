a33bcff5704d12a4f4df1426540208a44039c238
pascalf79 <pascalf79@gmail.com>
2016-08-12 12:51:12 +0100

    2016-08-12's linux workout

---
+++ contributions/pascalf79/linux/Terminals & Shells/2016-08-12.bash
@@ -0,0 +1,7 @@
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
