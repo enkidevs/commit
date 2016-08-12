ac2e900321380049d60f12b52722a302030ee999
vadix <vadim.molchanov@gmail.com>
2016-08-12 09:19:14 +0100

    2016-08-12's linux workout

---
+++ contributions/vadix/linux/Terminals & Shells/2016-08-12.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
