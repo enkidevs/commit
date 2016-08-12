1ca8d6903a2c77ef04520d257e998c1b32e840f6
simon82 <simon@koop-bremen.de>
2016-08-12 15:51:43 +0100

    2016-08-12's linux workout

---
+++ contributions/simon82/linux/Terminals & Shells/2016-08-12.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Quick `bash` shortcuts 
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
