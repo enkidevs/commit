ffb11f05d4af58d655d2483daf0ebfa145edaa00
matt.carron <matt.carron@gmail.com>
2016-08-12 08:52:26 +0100

    2016-08-12's java workout

---
+++ contributions/matt.carron/java/Fundamentals/2016-08-12.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
