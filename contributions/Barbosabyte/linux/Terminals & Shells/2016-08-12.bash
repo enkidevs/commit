4ed80d555ea779b6de95b52ae4183ccc2747c96a
Barbosabyte <barbosabyte@gmail.com>
2016-08-12 09:34:41 +0100

    2016-08-12's linux workout

---
+++ contributions/Barbosabyte/linux/Terminals & Shells/2016-08-12.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
