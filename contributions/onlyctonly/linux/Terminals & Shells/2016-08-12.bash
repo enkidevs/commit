ce778337d963e597db9e22def2da69692054db9e
onlyctonly <onlyctonly@icloud.com>
2016-08-12 19:41:39 +0100

    2016-08-12's linux workout

---
+++ contributions/onlyctonly/linux/Terminals & Shells/2016-08-12.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Terminal incognito mode
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
