53ce20315085a904395798665295f23651c6e16a
dehrk <dekay46@gmail.com>
2016-08-13 16:13:52 +0100

    2016-08-13's linux workout

---
+++ contributions/dehrk/linux/System Management/2016-08-13.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
