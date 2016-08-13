1f5c75566b7897c8fabc8038c38622571cafc6e8
walouster <walouster@gmail.com>
2016-08-13 20:44:21 +0100

    2016-08-13's linux workout

---
+++ contributions/walouster/linux/System Management/2016-08-13.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
