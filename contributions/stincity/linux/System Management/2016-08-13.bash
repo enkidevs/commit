df1db29dad12cca4776839e58a6e83d83b372688
stincity <jstinson.dev@gmail.com>
2016-08-13 18:37:27 +0100

    2016-08-13's linux workout

---
+++ contributions/stincity/linux/System Management/2016-08-13.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
