2a383a86b3911c824aca9ed5d5ae03b736b05155
Cube <cubejg@outlook.com>
2016-08-13 16:29:54 +0100

    2016-08-13's java workout

---
+++ contributions/Cube/java/Fundamentals/2016-08-13.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Do not return *references* to private *mutable* class members
