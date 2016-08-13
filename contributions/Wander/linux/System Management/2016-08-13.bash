b3c2e439a4e68c3cd54a848e19989b14100532ac
Wander <sherifq@rambler.ru>
2016-08-13 10:58:52 +0100

    2016-08-13's linux workout

---
+++ contributions/Wander/linux/System Management/2016-08-13.bash
@@ -0,0 +1,7 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
