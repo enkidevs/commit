eb49b2895650bf0c42253b3126c5208da4104383
sachinkmr <sachin.roy12345@gmail.com>
2016-08-13 19:52:52 +0100

    2016-08-12's linux workout

---
+++ contributions/sachinkmr/linux/System Management/2016-08-12.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
