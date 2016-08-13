a24cfb23d096daae33216b3554c82e39d9a20afb
GerardYin <gerard.yin@gmail.com>
2016-08-13 08:48:24 +0100

    2016-08-13's linux workout

---
+++ contributions/GerardYin/linux/System Management/2016-08-13.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Cancel a system shutdown
