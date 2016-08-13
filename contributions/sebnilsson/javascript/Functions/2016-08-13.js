fc3301fc3e2e8b045d28f287634ca94f1e94d5e4
sebnilsson <seb.nilsson@gmail.com>
2016-08-13 09:26:05 +0100

    2016-08-13's javascript workout

---
+++ contributions/sebnilsson/javascript/Functions/2016-08-13.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Method chaining
+Easily generate a random `HEX` color
+Move cursor at the end of text input
+Social sharing without widgets
