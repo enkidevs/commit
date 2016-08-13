e4cfb0e12f13f35de33cced9de1f8b9d56b40ed3
Adamgo963 <adamgo963@gmail.com>
2016-08-13 13:17:50 +0100

    2016-08-13's java workout

---
+++ contributions/Adamgo963/java/Fundamentals/2016-08-13.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Using an interface as a parameter
