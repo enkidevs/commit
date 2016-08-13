acf100bf85495f9590408a92d08a13e075e66bf8
ShadRS <shadrs@gmail.com>
2016-08-13 18:17:26 +0100

    2016-08-13's java workout

---
+++ contributions/ShadRS/java/Fundamentals/2016-08-13.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Methods performing *Security Checks* must be declared `Private` or `Final`
