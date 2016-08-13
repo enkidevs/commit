df7a1040f4d0499d21a70554f3b843660114b359
asaBee <hlovers.forever@gmail.com>
2016-08-13 13:38:29 +0100

    2016-08-13's java workout

---
+++ contributions/asaBee/java/Fundamentals/2016-08-13.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
