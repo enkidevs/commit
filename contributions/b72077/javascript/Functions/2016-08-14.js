cb15eba16795cebca597c7fbdca0bd2d1cac3208
b72077 <b72077@gmail.com>
2016-08-14 19:09:01 +0100

    2016-08-14's javascript workout

---
+++ contributions/b72077/javascript/Functions/2016-08-14.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+`.bind()` 
+Easily generate a random `HEX` color
+Social sharing without widgets
+Check if a document is done loading
