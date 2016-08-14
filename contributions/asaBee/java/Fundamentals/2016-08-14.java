c569b2559d45cd3e581321c53b0a1a6e979aa0b6
asaBee <hlovers.forever@gmail.com>
2016-08-14 22:34:28 +0100

    2016-08-14's java workout

---
+++ contributions/asaBee/java/Fundamentals/2016-08-14.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
