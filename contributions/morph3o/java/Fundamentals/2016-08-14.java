3d3c0db7ff2aacfc7801a339c3a1ce816a7fb0d4
morph3o <pdivasto@gmail.com>
2016-08-14 15:51:41 +0100

    2016-08-14's java workout

---
+++ contributions/morph3o/java/Fundamentals/2016-08-14.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
