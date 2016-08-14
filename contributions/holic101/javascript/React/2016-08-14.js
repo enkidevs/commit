78eacb045992845934a0dfbf13bd204c9575bf13
holic101 <dietrich.kriworotow@gmail.com>
2016-08-14 12:11:35 +0100

    2016-08-14's javascript workout

---
+++ contributions/holic101/javascript/React/2016-08-14.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+Pixel values in style props for **React**
+Expose Component Functions in **React**
+Shallow compare in **React**
+Immutability helpers in **React**
