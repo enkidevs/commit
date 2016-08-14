45716858c2bec754c3c3c7b7b3d8582a5b595202
umutkorkmaz <info@umutkorkmaz.net>
2016-08-14 16:42:10 +0100

    2016-08-14's linux workout

---
+++ contributions/umutkorkmaz/linux/File Management/2016-08-14.bash
@@ -0,0 +1,5 @@
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Deleting non-empty directories
+Working with `.gz` files
