9494cf2dac0648abbe7611b6a05370cc32946c32
kensportsfan <kenneth813@gmail.com>
2016-08-14 16:49:35 +0100

    2016-08-14's linux workout

---
+++ contributions/kensportsfan/linux/File Management/2016-08-14.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Delete commands aliases
+Easy way of sharing files
