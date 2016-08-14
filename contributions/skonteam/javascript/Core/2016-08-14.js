267fbb78037f71269235c24ac4a2f15e06d02b6f
skonteam <skonteam@gmail.com>
2016-08-14 17:13:46 +0100

    2016-08-14's javascript workout

---
+++ contributions/skonteam/javascript/Core/2016-08-14.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Most efficient way to build `HTML` strings
+Setting the length of an array 
+Counting the months from zero
+Getting the *real* dimensions of an image
