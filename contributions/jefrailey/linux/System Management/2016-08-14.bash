c4fa93d413d14761bc6569db838f7f0b24ba4f5a
jefrailey <jcaetof+enki@gmail.com>
2016-08-14 15:59:17 +0100

    2016-08-14's linux workout

---
+++ contributions/jefrailey/linux/System Management/2016-08-14.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Basic service management with `systemd`
