fbb20a8330c1e33bfaf24ce1f69e47b8792269df
divayprakash <divayprakash3@gmail.com>
2016-08-14 23:50:59 +0100

    2016-08-14's java workout

---
+++ contributions/divayprakash/java/Fundamentals/2016-08-14.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Supply `toString()` in all classes
