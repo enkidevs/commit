dedb7c45ae93fe8d98424b8d017dbea94709d828
AndriyPolukhin <andriy.polukhin@gmail.com>
2016-08-14 20:06:20 +0100

    2016-08-14's linux workout

---
+++ contributions/AndriyPolukhin/linux/Terminals & Shells/2016-08-14.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
