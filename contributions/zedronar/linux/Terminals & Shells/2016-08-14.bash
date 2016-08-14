e529e0947efd2ad895f2e0216f1ce95beeb410c4
zedronar <prietoaxel@gmail.com>
2016-08-14 15:42:51 +0100

    2016-08-14's linux workout

---
+++ contributions/zedronar/linux/Terminals & Shells/2016-08-14.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Terminal based browser
+Random password generator
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
