9fbf9ba2419954c2131337484630f10a16baee93
MasterJedi <vadimjedi@gmail.com>
2016-08-14 01:26:38 +0100

    2016-08-14's java workout

---
+++ contributions/MasterJedi/java/Data Types/2016-08-14.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Double brace initialization
+Numeric Conversion - Widening
