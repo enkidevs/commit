74b2a62d272e8f386dff8eaa20ba8f277430eefd
Lukas <lukithove@gmail.com>
2016-08-15 17:48:48 +0100

    2016-08-15's linux workout

---
+++ contributions/Lukas/linux/Package Management/2016-08-15.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Backporting on Debian
+
+Installing a `.deb` package from the terminal
+Get `apt` to use a mirror / faster mirror
+Solving `BADSIG GPG` errors
