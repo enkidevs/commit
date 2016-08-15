ce79877b0c4e0be7e50884777dfbc016a5192b20
e7krgc <enam93@gmail.com>
2016-08-15 15:07:37 +0100

    2016-08-15's linux workout

---
+++ contributions/e7krgc/linux/System Management/2016-08-15.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
