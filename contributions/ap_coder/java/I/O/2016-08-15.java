06ec6e2bba9956c7756aab3dbc7a20829c2d0724
ap_coder <arun.pattni01@gmail.com>
2016-08-15 07:29:31 +0100

    2016-08-15's java workout

---
+++ contributions/ap_coder/java/I/O/2016-08-15.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to create directories in Java
+Create system file paths using the `Path` class
+Reading and writing text files
+List contents of a directory with `newDirectoryStream`
