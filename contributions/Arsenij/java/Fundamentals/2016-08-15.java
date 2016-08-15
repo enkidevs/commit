3b8a741c3f91020f69a5db672fa3a0bf33295e92
Arsenij <arsenij.smth@gmail.com>
2016-08-15 11:29:14 +0100

    2016-08-15's java workout

---
+++ contributions/Arsenij/java/Fundamentals/2016-08-15.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
