a7c416258d8f3d3b62cd62fa691afa98a8d2f2e7
raghu <raghu.9208@gmail.com>
2016-08-15 05:12:10 +0100

    2016-08-14's java workout

---
+++ contributions/raghu/java/Fundamentals/2016-08-14.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+How range operations work
+Using an interface as a parameter
