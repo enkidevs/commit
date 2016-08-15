35898049ab67f31373d07eaa2d712987961d118e
Reberd <ismaelpa91@gmail.com>
2016-08-15 19:56:43 +0100

    2016-08-15's java workout

---
+++ contributions/Reberd/java/Fundamentals/2016-08-15.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Overloading Methods in Java
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Supply `toString()` in all classes
