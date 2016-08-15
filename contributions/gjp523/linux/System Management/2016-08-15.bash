152e9d6b4a603116ae7249d3c17f572d9033a655
gjp523 <gjp523@gmail.com>
2016-08-15 22:51:56 +0100

    2016-08-15's linux workout

---
+++ contributions/gjp523/linux/System Management/2016-08-15.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
