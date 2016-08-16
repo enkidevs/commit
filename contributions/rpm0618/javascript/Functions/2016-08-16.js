968c6698809187257a071a95b66b2919b176bafa
rpm0618 <rpm0618@gmail.com>
2016-08-16 15:39:11 +0100

    2016-08-16's javascript workout

---
+++ contributions/rpm0618/javascript/Functions/2016-08-16.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Move cursor at the end of text input
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
