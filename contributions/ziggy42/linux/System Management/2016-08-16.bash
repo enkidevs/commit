aaf33c36aa1cae908042facdf26a99a49b1aa29e
ziggy42 <vanpivix@gmail.com>
2016-08-16 21:11:51 +0100

    2016-08-16's linux workout

---
+++ contributions/ziggy42/linux/System Management/2016-08-16.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
