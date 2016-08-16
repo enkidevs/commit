3d0cbbfe92696dfef14ad602ca70dd5e21937e55
ozymandias <stenbackfelix@gmail.com>
2016-08-16 14:07:05 +0100

    2016-08-16's java workout

---
+++ contributions/ozymandias/java/Fundamentals/2016-08-16.java
@@ -0,0 +1,5 @@
+Cropping images
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Using `synchronized` statements
