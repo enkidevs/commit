7ee046cdc29f544f8445c665c02e021a5f28edaf
dehrk <dekay46@gmail.com>
2016-08-16 22:44:10 +0100

    2016-08-16's linux workout

---
+++ contributions/dehrk/linux/File Management/2016-08-16.bash
@@ -0,0 +1,5 @@
+Using `touch` to alter files modification time
+Deleting non-empty directories
+Working with `.gz` files
+Find the unknown process preventing deleting of files
+Easy way of sharing files
