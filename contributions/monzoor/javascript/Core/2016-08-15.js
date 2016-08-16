f4eeb7f003adfb3aca3a4cbcb46385b729866986
monzoor <mmtamal@gmail.com>
2016-08-16 16:36:07 +0100

    2016-08-15's javascript workout

---
+++ contributions/monzoor/javascript/Core/2016-08-15.js
@@ -0,0 +1,5 @@
+`hasOwnProperty` method
+Deleting properties
+Hoisting applies only to variable declarations, not initializations
+`catch` is block scoped
+Prevent background image flicker in **IE**
