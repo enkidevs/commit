d32a7063bf5e8e1e2965a5341364d9d5aee3dda8
tarl25 <tarl_hutch@yahoo.com>
2016-08-16 17:42:32 +0100

    2016-08-16's java workout

---
+++ contributions/tarl25/java/Error Handling/2016-08-16.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Handling Null Pointer Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
