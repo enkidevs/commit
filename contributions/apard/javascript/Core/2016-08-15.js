aef73770f45357bd333296a381fa980674871577
apard <a.pard@protonmail.com>
2016-08-16 20:38:21 +0100

    2016-08-15's javascript workout

---
+++ contributions/apard/javascript/Core/2016-08-15.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Use `splice()` to remove an item from an array
+Scope and strict mode
+Prevent background image flicker in **IE**
+Detect online connection
