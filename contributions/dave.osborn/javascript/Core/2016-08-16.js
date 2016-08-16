7d3069a0f55de6465d0dc628f37516d762ce0f58
dave.osborn <dave.osborn.uk@gmail.com>
2016-08-16 08:54:54 +0100

    2016-08-16's javascript workout

---
+++ contributions/dave.osborn/javascript/Core/2016-08-16.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+Filtering items out of an array
+`catch` is block scoped
+Extra function arguments are undefined by default
+Round numbers to `N` decimals 
