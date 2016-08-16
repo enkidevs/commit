aef81f260a0a0c622b669c8a1fcea221046fd16f
vbajs <yahyawessam2002@gmail.com>
2016-08-16 16:34:24 +0100

    2016-08-16's linux workout

---
+++ contributions/vbajs/linux/System Management/2016-08-16.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
