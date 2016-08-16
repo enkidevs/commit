dbf886eeb143287b6c5246651ad2f894282bd275
fntc <fntc@list.ru>
2016-08-16 10:08:23 +0100

    2016-08-15's linux workout

---
+++ contributions/fntc/linux/System Management/2016-08-15.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
