744371e8519f5ec15528171e379efce621d551be
PotHix <pothix@pothix.com>
2016-08-16 12:50:34 +0100

    2016-08-16's linux workout

---
+++ contributions/PotHix/linux/System Management/2016-08-16.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Using `dd` To Create Disk Images
