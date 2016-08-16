9b3069213d27d492aca1ce4ace0300cc8b1a40c1
raghu <raghu.9208@gmail.com>
2016-08-16 05:03:57 +0100

    2016-08-15's java workout

---
+++ contributions/raghu/java/Fundamentals/2016-08-15.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Overloading Methods in Java
+The trade-off between short and descriptive names
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
