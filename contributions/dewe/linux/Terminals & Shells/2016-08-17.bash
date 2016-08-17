04da5e6984f5259e963ae00a5ab5a9ccc9117fc9
dewe <johan@dewe.net>
2016-08-17 13:24:16 +0100

    2016-08-17's linux workout

---
+++ contributions/dewe/linux/Terminals & Shells/2016-08-17.bash
@@ -0,0 +1,7 @@
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
