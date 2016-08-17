49f23d56f620c47c2288a767a5bda48b474a6a50
TotKindGott <konstantin.a.kolosov@gmail.com>
2016-08-17 09:46:33 +0100

    2016-08-17's linux workout

---
+++ contributions/TotKindGott/linux/System Management/2016-08-17.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
