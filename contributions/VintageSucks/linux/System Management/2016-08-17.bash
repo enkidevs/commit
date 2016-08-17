989901456d87a6fa16bbdfc988c94cbc405ff85a
VintageSucks <nikolas.evers@me.com>
2016-08-17 11:11:05 +0100

    2016-08-17's linux workout

---
+++ contributions/VintageSucks/linux/System Management/2016-08-17.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Commands to shutdown or restart the system
