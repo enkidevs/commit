bcc6f35315b60bb849bdfdbd9ceaf940df28452c
JoakimKLarsson <joakim.karl.larsson@gmail.com>
2016-08-17 19:42:48 +0100

    2016-08-17's javascript workout

---
+++ contributions/JoakimKLarsson/javascript/Browser APIs/2016-08-17.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Async and defer scripts
+Use hosted scripts to increase performance
+Warn user if **Back** button is pressed
+Permission API
