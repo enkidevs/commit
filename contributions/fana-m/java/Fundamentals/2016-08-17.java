e15c26105bf93952ab319fd023fab02b70618968
fana-m <grandisbelua@gmail.com>
2016-08-17 21:06:23 +0100

    2016-08-17's java workout

---
+++ contributions/fana-m/java/Fundamentals/2016-08-17.java
@@ -0,0 +1,5 @@
+Cropping images
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
