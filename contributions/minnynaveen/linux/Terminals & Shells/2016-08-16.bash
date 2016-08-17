c68cd1bf438fae7981eadd19ae0018d5b8988935
minnynaveen <minnynaveen@gmail.com>
2016-08-17 06:14:50 +0100

    2016-08-16's linux workout

---
+++ contributions/minnynaveen/linux/Terminals & Shells/2016-08-16.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Terminal based browser
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
