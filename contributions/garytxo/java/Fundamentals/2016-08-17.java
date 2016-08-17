770f97362a8a1ec4bd44a40469da993f80d45fb0
garytxo <garytxo@gmail.com>
2016-08-17 06:40:38 +0100

    2016-08-17's java workout

---
+++ contributions/garytxo/java/Fundamentals/2016-08-17.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The trade-off between short and descriptive names
+Ternary operator
+Distinction between `public` and `private` methods
+Locks in `static synchronized` methods
