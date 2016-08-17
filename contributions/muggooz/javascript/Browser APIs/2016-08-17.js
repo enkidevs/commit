91b866be93bb09e9f9f87b460c0973631e03fa42
muggooz <inknito@gmail.com>
2016-08-17 12:02:01 +0100

    2016-08-17's javascript workout

---
+++ contributions/muggooz/javascript/Browser APIs/2016-08-17.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use hosted scripts to increase performance
+Modify a website's URL
+Navigating the browser history
+MediaDevices.getUserMedia()
