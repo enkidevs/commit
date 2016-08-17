50195364f207039e8aa043fcb32ab0c1cfab0497
onozy <onozawa2106@gmail.com>
2016-08-17 01:53:03 +0100

    2016-08-16's java workout

---
+++ contributions/onozy/java/Fundamentals/2016-08-16.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Do not return *references* to private *mutable* class members
