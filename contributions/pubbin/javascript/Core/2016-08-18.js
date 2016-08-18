9f37544ad19ae6e24e2738657f61e56cc58df904
pubbin <alpha.pubmail@gmail.com>
2016-08-19 00:42:08 +0100

    2016-08-18's javascript workout

---
+++ contributions/pubbin/javascript/Core/2016-08-18.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Counting the months from zero
+The difference between null, undefined and NaN
+Prevent background image flicker in **IE**
+Getting the *real* dimensions of an image
