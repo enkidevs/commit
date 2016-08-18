9426b6a56cbf963dc51bf0999a643607d053e83e
Jaygo <jaygo8417@gmail.com>
2016-08-18 17:31:41 +0100

    2016-08-18's linux workout

---
+++ contributions/Jaygo/linux/File Management/2016-08-18.bash
@@ -0,0 +1,5 @@
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Easy way of sharing files
