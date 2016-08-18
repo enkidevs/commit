4b59f3f132f6f23b1658cb1049c8f57b67da19de
ozymandias <stenbackfelix@gmail.com>
2016-08-18 07:57:23 +0100

    2016-08-18's java workout

---
+++ contributions/ozymandias/java/Fundamentals/2016-08-18.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
+Methods performing *Security Checks* must be declared `Private` or `Final`
+Using an interface as a parameter
