7f79de80e9b8c723afc6e3b945378b74ca69d881
gtrefs <gregor.trefs@gmail.com>
2016-08-18 11:21:34 +0100

    2016-08-18's linux workout

---
+++ contributions/gtrefs/linux/System Management/2016-08-18.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Cancel a system shutdown
