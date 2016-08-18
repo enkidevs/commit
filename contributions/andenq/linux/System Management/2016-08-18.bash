50ff8c97781e0f4fb472dcc5b70bedecb9a58986
andenq <andreas.coroiu@gmail.com>
2016-08-18 20:51:37 +0100

    2016-08-18's linux workout

---
+++ contributions/andenq/linux/System Management/2016-08-18.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Cancel a system shutdown
