d4140a896fab59bc315b2842eefe881af8af38e9
ludo <jargot.ludovic1@gmail.com>
2016-08-18 12:48:36 +0100

    2016-08-18's linux workout

---
+++ contributions/ludo/linux/System Management/2016-08-18.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
