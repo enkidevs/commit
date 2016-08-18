4540b72c892715afdc0a3500bd9786828997724e
GoodVibesDev <artur.piersa@gmail.com>
2016-08-18 09:55:24 +0100

    2016-08-18's javascript workout

---
+++ contributions/GoodVibesDev/javascript/Node/2016-08-18.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+First-error callbacks in **Node**
+Streams in **Node**
+Use conventional property names for error handling in Node.js
+**Node** `os` module
