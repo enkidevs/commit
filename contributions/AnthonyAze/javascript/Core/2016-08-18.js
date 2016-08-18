76374c33ffdc84f25041bf9e33c156edf307e7cb
AnthonyAze <graindorgeanthony@gmail.com>
2016-08-18 09:57:15 +0100

    2016-08-18's javascript workout

---
+++ contributions/AnthonyAze/javascript/Core/2016-08-18.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+`console.*` in JavaScript
+Inaccuracy of binary floating-point format
