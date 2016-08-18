7235a19f36d1e4b5965f6ed0795a17fdf97ddf6a
Chris_Savage <lpthegreatest@gmail.com>
2016-08-18 15:18:24 +0100

    2016-08-18's linux workout

---
+++ contributions/Chris_Savage/linux/System Management/2016-08-18.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Basic service management with `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
