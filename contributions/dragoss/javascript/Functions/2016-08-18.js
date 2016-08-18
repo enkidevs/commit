af86838f8c2491e90ea1868c37174daf0d8fd805
dragoss <dragos.stefanache@gmail.com>
2016-08-18 07:27:57 +0100

    2016-08-18's javascript workout

---
+++ contributions/dragoss/javascript/Functions/2016-08-18.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Function call method
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
