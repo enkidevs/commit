67b3300b7d9bfe85f4adde06d1cdf752a7ae2bf5
Wrgl0 <wargloo@gmail.com>
2016-08-18 06:45:02 +0100

    2016-08-18's java workout

---
+++ contributions/Wrgl0/java/Fundamentals/2016-08-18.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+How to play a sound file in Java
+Altering format string output by changing a format specifier's `argument_index`
+Distinction between `public` and `private` methods
+Locks in `static synchronized` methods
