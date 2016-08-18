c04d4e5dafe7959b756b68862d6ea80e23040a79
asaBee <hlovers.forever@gmail.com>
2016-08-18 22:20:16 +0100

    2016-08-18's linux workout

---
+++ contributions/asaBee/linux/System Management/2016-08-18.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+List current logged on users with `w`
+Incorrect time on dual boot systems
+Basic service management with `systemd`
