2efec1691306f96639c3a572476570734d6815a9
cansu <cansukavili@gmail.com>
2016-08-18 11:44:27 +0100

    2016-08-18's linux workout

---
+++ contributions/cansu/linux/System Management/2016-08-18.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
