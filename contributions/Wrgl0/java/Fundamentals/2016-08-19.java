210cd6df38528d08f93f538feaea27ff232accf5
Wrgl0 <wargloo@gmail.com>
2016-08-19 06:44:46 +0100

    2016-08-19's java workout

---
+++ contributions/Wrgl0/java/Fundamentals/2016-08-19.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Connecting to FTP using Java
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+`Autoboxing` and `Unboxing`
