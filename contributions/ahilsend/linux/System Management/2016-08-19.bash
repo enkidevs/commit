01742c3ddd1a15ea0a8248728d665b99766f9628
ahilsend <andre.hilsendeger@gmail.com>
2016-08-19 19:56:43 +0100

    2016-08-19's linux workout

---
+++ contributions/ahilsend/linux/System Management/2016-08-19.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Cancel a system shutdown
