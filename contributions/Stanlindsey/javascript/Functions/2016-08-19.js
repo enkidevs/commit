07b15ab76996071782eeb02d19a68ad6d4ad7b4f
Stanlindsey <contact@stanlindsey.net>
2016-08-19 07:55:07 +0100

    2016-08-19's javascript workout

---
+++ contributions/Stanlindsey/javascript/Functions/2016-08-19.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Get query/url variables
+Social sharing without widgets
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
