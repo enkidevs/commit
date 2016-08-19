0ded4f851f96e7c09bff1fedf6ade833b23c870a
saka <saka71996@gmail.com>
2016-08-19 08:03:43 +0100

    2016-08-19's java workout

---
+++ contributions/saka/java/Fundamentals/2016-08-19.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
