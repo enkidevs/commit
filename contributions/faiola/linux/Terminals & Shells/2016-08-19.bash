a6d85426f3da40a6017272a0946e0b8838dae621
faiola <faiolaj@gmail.com>
2016-08-19 19:40:03 +0100

    2016-08-19's linux workout

---
+++ contributions/faiola/linux/Terminals & Shells/2016-08-19.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
