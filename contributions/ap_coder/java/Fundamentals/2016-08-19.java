3caa1d60f8a0e3c7552a01db5958d7ee9719ba86
ap_coder <arun.pattni01@gmail.com>
2016-08-19 08:55:39 +0100

    2016-08-19's java workout

---
+++ contributions/ap_coder/java/Fundamentals/2016-08-19.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Altering format string output by changing a format specifier's `argument_index`
+Using `Printstream.format` to format a string before printing
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
