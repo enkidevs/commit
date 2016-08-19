ab802fd772729802691641623f62ef656a958b46
Kutsan <daghan.k5@gmail.com>
2016-08-19 06:47:49 +0100

    2016-08-19's linux workout

---
+++ contributions/Kutsan/linux/System Management/2016-08-19.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
