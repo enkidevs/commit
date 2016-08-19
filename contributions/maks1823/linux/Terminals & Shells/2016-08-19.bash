eee40f4a0348fca7e253ef1f1cdc5fa2152bb0cc
maks1823 <maks1823@gmail.com>
2016-08-19 21:07:13 +0100

    2016-08-19's linux workout

---
+++ contributions/maks1823/linux/Terminals & Shells/2016-08-19.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
