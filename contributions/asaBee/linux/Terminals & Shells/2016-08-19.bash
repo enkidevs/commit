4e791abad52e16b52219d9a05330af12888252aa
asaBee <hlovers.forever@gmail.com>
2016-08-19 23:00:32 +0100

    2016-08-19's linux workout

---
+++ contributions/asaBee/linux/Terminals & Shells/2016-08-19.bash
@@ -0,0 +1,5 @@
+Terminal based browser
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
