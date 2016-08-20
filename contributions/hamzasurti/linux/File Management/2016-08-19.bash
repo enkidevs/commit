e930abe5984c375eccefc45b9ad2d3c5b34044df
hamzasurti <hamza.surti1@gmail.com>
2016-08-20 02:08:44 +0100

    2016-08-19's linux workout

---
+++ contributions/hamzasurti/linux/File Management/2016-08-19.bash
@@ -0,0 +1,7 @@
+Finding file with regexes
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Deleting non-empty directories
