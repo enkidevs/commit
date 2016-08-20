669cb0d6caf0a9e73d9bf501c02698c1810df8cd
nilendra <nknilubharti@gmail.com>
2016-08-20 19:52:54 +0100

    2016-08-19's linux workout

---
+++ contributions/nilendra/linux/File Management/2016-08-19.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
+Find the unknown process preventing deleting of files
