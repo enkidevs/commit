524aa22b847a5942d57bf0245f82a65c56eaa2c5
spadoinkle <david.aeberhard@gmail.com>
2016-08-20 22:44:46 +0100

    2016-08-20's java workout

---
+++ contributions/spadoinkle/java/Fundamentals/2016-08-20.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
