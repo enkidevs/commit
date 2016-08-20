7d68b44ce56dc7b193aee028ea816e663e8b4d54
indoriMonk <amanshulvyas@gmail.com>
2016-08-20 06:30:27 +0100

    2016-08-19's java workout

---
+++ contributions/indoriMonk/java/Fundamentals/2016-08-19.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use meaningful names
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Implementing an `interface`
