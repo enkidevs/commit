fd2b8ab4ee7e1170641cd2c943e9f67af8d82b01
gerbano <gerbano@gmail.com>
2016-08-20 13:42:06 +0100

    2016-08-20's linux workout

---
+++ contributions/gerbano/linux/System Management/2016-08-20.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
