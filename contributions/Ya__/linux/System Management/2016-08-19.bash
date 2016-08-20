5a81e268d13d4270191bcc7f58b609b73555d182
Ya__ <qsnhywadej@gmail.com>
2016-08-20 15:54:57 +0100

    2016-08-19's linux workout

---
+++ contributions/Ya__/linux/System Management/2016-08-19.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
