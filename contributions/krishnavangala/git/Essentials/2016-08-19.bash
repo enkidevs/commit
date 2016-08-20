da60b06b3a893e2769258572a0992e4c0319e381
krishnavangala <saichaitanya6@gmail.com>
2016-08-20 03:02:02 +0100

    2016-08-19's git workout

---
+++ contributions/krishnavangala/git/Essentials/2016-08-19.bash
@@ -0,0 +1,5 @@
+Using aliases for git commands
+Use `short` status to make output more compact
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
+Perform a dry run
