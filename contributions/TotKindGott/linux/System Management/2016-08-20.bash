90e03e2bf7408fa2b054d66ff5901d27fb2b9d34
TotKindGott <konstantin.a.kolosov@gmail.com>
2016-08-20 12:45:02 +0100

    2016-08-20's linux workout

---
+++ contributions/TotKindGott/linux/System Management/2016-08-20.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
