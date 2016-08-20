4a5b174776726586f48ae1e6fb06e536bb4d985e
tarunv <tarunv1997@gmail.com>
2016-08-20 04:03:41 +0100

    2016-08-19's java workout

---
+++ contributions/tarunv/java/Fundamentals/2016-08-19.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+Detect or prevent integer overflow
