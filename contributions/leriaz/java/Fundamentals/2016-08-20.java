2c63ba9724fd81b3e610fbaa6a686f5fb7ab3e84
leriaz <leriaz17@gmail.com>
2016-08-20 09:59:15 +0100

    2016-08-20's java workout

---
+++ contributions/leriaz/java/Fundamentals/2016-08-20.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
