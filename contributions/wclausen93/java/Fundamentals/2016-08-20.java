8e21169150b33ca4591525e1354a92ecae13df92
wclausen93 <william.h.f.clausen@gmail.com>
2016-08-20 15:36:14 +0100

    2016-08-20's java workout

---
+++ contributions/wclausen93/java/Fundamentals/2016-08-20.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
