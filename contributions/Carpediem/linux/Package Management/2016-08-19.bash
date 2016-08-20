990de088cdacf3a18953d391060877fc17490b7f
Carpediem <1185329223@qq.com>
2016-08-20 03:37:33 +0100

    2016-08-19's linux workout

---
+++ contributions/Carpediem/linux/Package Management/2016-08-19.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Backporting on Debian
+
+List installed packages
+Prevent updating a specific package in Debian systems
+Using `Tasksel` for software installation
