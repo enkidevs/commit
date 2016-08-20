c4e98e0055842a05d76190b7820f04188f6a532a
hsssjv9 <niklas@lyxell.net>
2016-08-20 06:24:59 +0100

    2016-08-20's linux workout

---
+++ contributions/hsssjv9/linux/Terminals & Shells/2016-08-20.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
