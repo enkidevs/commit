b86f2fbe68b5b2dd727fe52328feb662def91b56
ImTheLe <leongrdic@gmail.com>
2016-08-20 09:46:41 +0100

    2016-08-20's javascript workout

---
+++ contributions/ImTheLe/javascript/Core/2016-08-20.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+Filtering items out of an array
