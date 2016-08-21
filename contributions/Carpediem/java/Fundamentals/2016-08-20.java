ccc1cf3200c475c45729abfc6fc3b7ae65cbad76
Carpediem <1185329223@qq.com>
2016-08-21 09:29:54 +0100

    2016-08-20's java workout

---
+++ contributions/Carpediem/java/Fundamentals/2016-08-20.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
+Using an interface as a parameter
