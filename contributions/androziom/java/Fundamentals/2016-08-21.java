9cef1725a613d90c219af90d160fec3751c1a757
androziom <androziom@gmail.com>
2016-08-21 03:29:32 +0100

    2016-08-21's java workout

---
+++ contributions/androziom/java/Fundamentals/2016-08-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
+Using an interface as a parameter
