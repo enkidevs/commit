30b4a9e7cee37be4eda10030ee74783e31d8d0c1
kuntsali <iweb.89@gmail.com>
2016-08-21 13:54:54 +0100

    2016-08-21's java workout

---
+++ contributions/kuntsali/java/Fundamentals/2016-08-21.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Overloading Methods in Java
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+How range operations work
