c1d558700d35fefbb7c348bb0f015d0e0dbbea91
zero9 <c.lobrano@gmail.com>
2016-08-21 18:43:26 +0100

    2016-08-21's linux workout

---
+++ contributions/zero9/linux/System Management/2016-08-21.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
