93d09b891e0e51c9a6c575022775656018f9879a
lucifer <korotayev.dmitriy@gmail.com>
2016-08-21 08:00:04 +0100

    2016-08-21's linux workout

---
+++ contributions/lucifer/linux/Terminals & Shells/2016-08-21.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Find any Unix / Linux command
+Terminal incognito mode
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
