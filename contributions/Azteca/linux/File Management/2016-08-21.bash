63eb7ed5486df64fa28b2d4af2bc694165bad0c7
Azteca <eugenio.mg@gmail.com>
2016-08-21 19:57:39 +0100

    2016-08-21's linux workout

---
+++ contributions/Azteca/linux/File Management/2016-08-21.bash
@@ -0,0 +1,5 @@
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
+Delete commands aliases
+Easy way of sharing files
