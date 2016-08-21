3db3fa8600bdfc99693620d8946c59774897390c
mustafaismail22 <mustafa.ismail22@gmail.com>
2016-08-21 02:59:28 +0100

    2016-08-21's javascript workout

---
+++ contributions/mustafaismail22/javascript/Core/2016-08-21.js
@@ -0,0 +1,5 @@
+Treating a boolean as number
+Hoisting applies only to variable declarations, not initializations
+Closures
+Extra function arguments are undefined by default
+Prevent background image flicker in **IE**
