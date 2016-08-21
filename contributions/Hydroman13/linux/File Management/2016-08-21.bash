b800be43653b9a2ef33f7317c4116823c4537171
Hydroman13 <bhc.karate@gmail.com>
2016-08-21 15:31:36 +0100

    2016-08-21's linux workout

---
+++ contributions/Hydroman13/linux/File Management/2016-08-21.bash
@@ -0,0 +1,6 @@
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Delete commands aliases
