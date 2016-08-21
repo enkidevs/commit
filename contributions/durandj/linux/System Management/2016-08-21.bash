df4735094253227bdc98f5707b077c949283525b
durandj <james.durand@alumni.msoe.edu>
2016-08-21 14:45:48 +0100

    2016-08-21's linux workout

---
+++ contributions/durandj/linux/System Management/2016-08-21.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Cancel a system shutdown
