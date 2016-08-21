a42aa89d890f482172efbdf0b8b5b6680c81f64d
ole kaps <0112oleg@gmail.com>
2016-08-21 16:51:57 +0100

    2016-08-21's javascript workout

---
+++ contributions/ole kaps/javascript/Core/2016-08-21.js
@@ -0,0 +1,5 @@
+`hasOwnProperty` method
+Weak vs Strict equality operator
+Hoisting applies only to variable declarations, not initializations
+Scope and strict mode
+Getting the *real* dimensions of an image
