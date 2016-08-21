8135f26f75821d16ba8955e434cd1224fe84b69e
rjmarwil <rjmarwil@gmail.com>
2016-08-21 04:43:31 +0100

    2016-08-20's linux workout

---
+++ contributions/rjmarwil/linux/Terminals & Shells/2016-08-20.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Aliasing ssh connections
+Quick `bash` shortcuts 
+Keep useful commands in your shell history with tags
