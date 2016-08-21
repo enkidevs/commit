65b336898bef34b5f936b2b77385165a50b4ab40
damay <rothbacherc@gmail.com>
2016-08-21 11:35:28 +0100

    2016-08-21's java workout

---
+++ contributions/damay/java/Fundamentals/2016-08-21.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
