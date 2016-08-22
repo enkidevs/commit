863e373172e449e45aabaeb672e7e6a4de44b322
x4fyr <benedikt_v@web.de>
2016-08-22 16:37:26 +0100

    2016-08-22's java workout

---
+++ contributions/x4fyr/java/Fundamentals/2016-08-22.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
