5d88a221dc69b8824a571b54615e43b5961ca300
tsu_4 <hosoyan.tsu@gmail.com>
2016-08-23 00:16:19 +0100

    2016-08-22's linux workout

---
+++ contributions/tsu_4/linux/File Management/2016-08-22.bash
@@ -0,0 +1,5 @@
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
+Delete commands aliases
