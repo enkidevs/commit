d37219648e15ae39ca4f857deb453551ea0740ad
Eldergeek <david@geekguytech.com>
2016-08-22 15:56:13 +0100

    2016-08-22's linux workout

---
+++ contributions/Eldergeek/linux/System Management/2016-08-22.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
