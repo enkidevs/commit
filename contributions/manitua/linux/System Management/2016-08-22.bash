7aa3d997adf7e58ff93a27334109d2b032e13053
manitua <jasmin.beharic@gmail.com>
2016-08-22 06:12:30 +0100

    2016-08-22's linux workout

---
+++ contributions/manitua/linux/System Management/2016-08-22.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
