323248bfe68f8eaf26fcf55e24674acc9bce3957
ggtk14 <ggtk14@gmail.com>
2016-08-22 07:00:04 +0100

    2016-08-22's linux workout

---
+++ contributions/ggtk14/linux/System Management/2016-08-22.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
