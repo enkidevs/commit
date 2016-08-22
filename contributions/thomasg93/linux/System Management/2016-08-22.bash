361b19143b33fb55908cc1a2608cca1ebb3edca8
thomasg93 <thomas.gauroy@gmail.com>
2016-08-22 06:25:27 +0100

    2016-08-22's linux workout

---
+++ contributions/thomasg93/linux/System Management/2016-08-22.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
