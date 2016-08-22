c01ba06d9ad0ee31b5978142b0946e55a0599344
srnice <nice2killu.ds@gmail.com>
2016-08-22 08:09:21 +0100

    2016-08-22's java workout

---
+++ contributions/srnice/java/Fundamentals/2016-08-22.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+The distinction between overloading and overriding methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
