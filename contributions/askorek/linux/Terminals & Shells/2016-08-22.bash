8ba1f50ba6e7a0af0cbe2601b31904b16d43b0a2
askorek <askorek@gmail.com>
2016-08-22 10:22:43 +0100

    2016-08-22's linux workout

---
+++ contributions/askorek/linux/Terminals & Shells/2016-08-22.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Find any Unix / Linux command
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
