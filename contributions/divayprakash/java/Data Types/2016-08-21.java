a26ce7107d15a610274bd9b146d40fb77b922bac
divayprakash <divayprakash3@gmail.com>
2016-08-22 22:41:06 +0100

    2016-08-21's java workout

---
+++ contributions/divayprakash/java/Data Types/2016-08-21.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Double brace initialization
+Retrieve the component type of an array
