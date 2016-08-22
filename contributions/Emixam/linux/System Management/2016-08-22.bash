b441e4040dbc29fdd9da3e2159ee30169c51b573
Emixam <derzelle.maxime@gmail.com>
2016-08-22 10:17:42 +0100

    2016-08-22's linux workout

---
+++ contributions/Emixam/linux/System Management/2016-08-22.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
