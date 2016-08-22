db1340b666427a5a1ea07a35ce1814927ba602a4
Snackhat <vaniljvisp@gmail.com>
2016-08-22 09:18:52 +0100

    2016-08-22's java workout

---
+++ contributions/Snackhat/java/Fundamentals/2016-08-22.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
