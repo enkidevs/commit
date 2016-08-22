8a877ee94a2731e52073193b185df9e0bb9f8159
Enterpaper <byebye99@qq.com>
2016-08-22 16:27:11 +0100

    2016-08-21's java workout

---
+++ contributions/Enterpaper/java/Fundamentals/2016-08-21.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Distinction between `public` and `private` methods
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
