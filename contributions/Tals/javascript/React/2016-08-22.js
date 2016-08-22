e22ef316bef7244906abb59939361a6e11e7e206
Tals <talschuster@gmail.com>
2016-08-22 17:49:02 +0100

    2016-08-22's javascript workout

---
+++ contributions/Tals/javascript/React/2016-08-22.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+**React** in-line styles
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+Dealing with `this.props.children`
