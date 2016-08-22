cd234783757fabda5d86aea06e6aeaa842549aae
vhundef <vhundef@gmail.com>
2016-08-22 19:42:51 +0100

    2016-08-22's git workout

---
+++ contributions/vhundef/git/Essentials/2016-08-22.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+Using aliases for git commands
+Search by commit message keyword
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
