dc426b38f8bde34d63444d2942e7ffe7d3b2f8cd
MxRay <maksym.ch@gmail.com>
2016-08-22 20:05:58 +0100

    2016-08-22's javascript workout

---
+++ contributions/MxRay/javascript/Functions/2016-08-22.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Check if a document is done loading
