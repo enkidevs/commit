23a666bfc96dc6605ede4284bd7cde4d7f4b496c
Urist <exmortis45@gmail.com>
2016-08-22 12:25:40 +0100

    2016-08-22's java workout

---
+++ contributions/Urist/java/Fundamentals/2016-08-22.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Using an interface as a parameter
