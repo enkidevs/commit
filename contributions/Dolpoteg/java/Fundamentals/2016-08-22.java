281c326f2d563c1f77c25905826f3bef3b9b98ca
Dolpoteg <benjamin.stermecki@gmail.com>
2016-08-22 14:05:14 +0100

    2016-08-22's java workout

---
+++ contributions/Dolpoteg/java/Fundamentals/2016-08-22.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Connecting to FTP using Java
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
