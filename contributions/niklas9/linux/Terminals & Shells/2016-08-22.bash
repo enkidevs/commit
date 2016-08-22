19feaaee46332761d26e1498f8b0f107ec2e2ecc
niklas9 <nandersson900@gmail.com>
2016-08-22 19:34:09 +0100

    2016-08-22's linux workout

---
+++ contributions/niklas9/linux/Terminals & Shells/2016-08-22.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Useful aliasing in bash
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
