b969d95618475d5626ffcc617bf3517561fee1d0
apinto <apintocr@gmail.com>
2016-08-22 21:21:00 +0100

    2016-08-22's linux workout

---
+++ contributions/apinto/linux/System Management/2016-08-22.bash
@@ -0,0 +1,5 @@
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
