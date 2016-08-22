fc643e484d5ec2b7e4070765772b01f39e37b894
topilo <felipe2855@gmail.com>
2016-08-22 13:58:38 +0100

    2016-08-22's java workout

---
+++ contributions/topilo/java/Fundamentals/2016-08-22.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
+Using an interface as a parameter
