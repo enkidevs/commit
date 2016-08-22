1a3e5a0066744a3986c7b07e61db91ece23dec09
bece <albertobf@gmail.com>
2016-08-22 09:04:22 +0100

    2016-08-22's java workout

---
+++ contributions/bece/java/Fundamentals/2016-08-22.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
+Using an interface as a parameter
