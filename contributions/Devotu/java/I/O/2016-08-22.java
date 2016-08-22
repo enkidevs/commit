12f9da9d8cd8d44ea18e8abd769b6a140bc458f2
Devotu <otuvesson@gmail.com>
2016-08-22 12:01:09 +0100

    2016-08-22's java workout

---
+++ contributions/Devotu/java/I/O/2016-08-22.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+List contents of a directory with `newDirectoryStream`
