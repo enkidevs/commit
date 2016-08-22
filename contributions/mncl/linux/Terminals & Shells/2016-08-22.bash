6e22f6e77cec0463e74cf6524bdc72bfbb881315
mncl <emladenoff@gmail.com>
2016-08-22 18:23:36 +0100

    2016-08-22's linux workout

---
+++ contributions/mncl/linux/Terminals & Shells/2016-08-22.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
