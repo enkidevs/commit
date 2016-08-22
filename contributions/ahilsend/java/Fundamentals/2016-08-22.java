1c9ebb1e1070a11992a26184aef6c90fb46083db
ahilsend <andre.hilsendeger@gmail.com>
2016-08-22 13:36:33 +0100

    2016-08-22's java workout

---
+++ contributions/ahilsend/java/Fundamentals/2016-08-22.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Connecting to FTP using Java
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
