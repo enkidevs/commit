f421ad3ea328e966fd5b2f581b751b9b944f8543
Vandalis <marangon.claudio@gmail.com>
2016-08-22 22:31:03 +0100

    2016-08-22's linux workout

---
+++ contributions/Vandalis/linux/System Management/2016-08-22.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Cancel a system shutdown
