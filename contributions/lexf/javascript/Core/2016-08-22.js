b51783043e16962f58e3992e89706e12c8ade985
lexf <aleksf87@gmail.com>
2016-08-22 19:36:27 +0100

    2016-08-22's javascript workout

---
+++ contributions/lexf/javascript/Core/2016-08-22.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Hoisting applies only to variable declarations, not initializations
+Detect online connection
+Detect **DO NOT TRACK** status
+`console.*` in JavaScript
