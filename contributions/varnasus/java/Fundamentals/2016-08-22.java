56833a8a150b898b00d8ba4220db0a79a5442989
varnasus <varnasus@gmail.com>
2016-08-22 18:59:37 +0100

    2016-08-22's java workout

---
+++ contributions/varnasus/java/Fundamentals/2016-08-22.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Supply `toString()` in all classes
+Using an interface as a parameter
