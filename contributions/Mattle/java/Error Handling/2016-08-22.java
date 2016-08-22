8e2f22ee1d0a242f7895a8300b5b42bfc069c4b8
Mattle <mcallier@gmail.com>
2016-08-22 17:04:24 +0100

    2016-08-22's java workout

---
+++ contributions/Mattle/java/Error Handling/2016-08-22.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using exceptions in Java
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Checking a double for `NaN` value
