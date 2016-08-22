7248e21c5d44d6ba0a45d71588e2e5345b18661f
PotHix <pothix@pothix.com>
2016-08-22 15:26:50 +0100

    2016-08-22's linux workout

---
+++ contributions/PotHix/linux/System Management/2016-08-22.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Common log files
+Preventing a user from logging into the system
