5eb659b55c10f0b24ac3903f777e556482afa372
doodloo <hickscorp@gmail.com>
2016-08-22 10:30:04 +0100

    2016-08-22's linux workout

---
+++ contributions/doodloo/linux/System Management/2016-08-22.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
