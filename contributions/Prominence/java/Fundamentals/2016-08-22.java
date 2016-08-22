c3530d35d91e332fc224362ca3202aa7291c73e3
Prominence <prominence96@gmail.com>
2016-08-22 07:39:48 +0100

    2016-08-22's java workout

---
+++ contributions/Prominence/java/Fundamentals/2016-08-22.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
