dcda34d8cdf69595bc01e4ea1a5245a5262a3e5e
AlexOlival <lex.olival@gmail.com>
2016-08-22 17:46:28 +0100

    2016-08-22's java workout

---
+++ contributions/AlexOlival/java/I/O/2016-08-22.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+Reading file attributes
+List contents of a directory with `newDirectoryStream`
+Deleting files and directories
