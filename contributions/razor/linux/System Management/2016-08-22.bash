f7d619e4d26e1773bdba919e4156589b2d1c6d90
razor <imad.mbox@gmail.com>
2016-08-23 00:07:04 +0100

    2016-08-22's linux workout

---
+++ contributions/razor/linux/System Management/2016-08-22.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
