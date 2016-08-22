17c04dabd7bfd167690149b4b37806fba1aa2e76
cristianstenico <cristianstenico@gmail.com>
2016-08-22 18:44:13 +0100

    2016-08-22's linux workout

---
+++ contributions/cristianstenico/linux/File Management/2016-08-22.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Easy way of sharing files
