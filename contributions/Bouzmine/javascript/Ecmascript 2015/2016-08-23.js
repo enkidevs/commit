f5c463b51b4422fa07a7f252017f9c1585ae1d8a
Bouzmine <clavette.francis@gmail.com>
2016-08-23 17:32:48 +0100

    2016-08-23's javascript workout

---
+++ contributions/Bouzmine/javascript/Ecmascript 2015/2016-08-23.js
@@ -0,0 +1,6 @@
+Computed property names in object literals
+Defaults values apply only to `undefined` (and not to `null`)
+Internationalization & Localization
+
+The `for-of` loop in ES6
+Proxies in ES6
