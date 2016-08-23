0137ed8b7c1900d449d09a860b10dc7a765db951
niklas9 <nandersson900@gmail.com>
2016-08-23 21:16:54 +0100

    2016-08-23's linux workout

---
+++ contributions/niklas9/linux/System Management/2016-08-23.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Cancel a system shutdown
