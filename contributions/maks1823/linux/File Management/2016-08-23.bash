d8797331595d414233d05f2ae0d2f800597ff9bf
maks1823 <maks1823@gmail.com>
2016-08-23 12:39:30 +0100

    2016-08-23's linux workout

---
+++ contributions/maks1823/linux/File Management/2016-08-23.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Deleting non-empty directories
