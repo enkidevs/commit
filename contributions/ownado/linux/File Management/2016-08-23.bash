07c15b3af514b67e0aef29aed22f03059202b571
ownado <ruben_l-10@hotmail.com>
2016-08-23 20:53:03 +0100

    2016-08-23's linux workout

---
+++ contributions/ownado/linux/File Management/2016-08-23.bash
@@ -0,0 +1,7 @@
+Practical `du` command
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
