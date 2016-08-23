5c2d78284043ac14f56328f7fc09579cdfb191c1
itaysel <itaymailsela@gmail.com>
2016-08-23 21:16:26 +0100

    2016-08-23's linux workout

---
+++ contributions/itaysel/linux/File Management/2016-08-23.bash
@@ -0,0 +1,7 @@
+Finding file with regexes
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
