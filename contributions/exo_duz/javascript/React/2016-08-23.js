35b95a0399b94c0bb09dfff4c98237c2792dd007
exo_duz <exo_duz@yahoo.com>
2016-08-23 19:46:51 +0100

    2016-08-23's javascript workout

---
+++ contributions/exo_duz/javascript/React/2016-08-23.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Load Initial Data via AJAX
+**React** `this.props.children`
+Validate for required props
