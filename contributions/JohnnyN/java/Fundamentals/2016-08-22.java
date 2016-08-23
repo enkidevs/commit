592509bec813b53b3965ac97782e0926e1588e67
JohnnyN <johnnytn.mail@gmail.com>
2016-08-23 06:11:35 +0100

    2016-08-22's java workout

---
+++ contributions/JohnnyN/java/Fundamentals/2016-08-22.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
