2b018d016be90fed69aee1450b38c17b48cfcf5f
ProTo <stefan.oberdoerfer@hotmail.de>
2016-08-23 18:54:37 +0100

    2016-08-23's linux workout

---
+++ contributions/ProTo/linux/Terminals & Shells/2016-08-23.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Terminal incognito mode
+Sequential execution using the `;` statement separator
