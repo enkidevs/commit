93ea8b65d86a415763564e10f6021dfc6387fbe0
Enterpaper <byebye99@qq.com>
2016-08-23 03:44:33 +0100

    2016-08-22's java workout

---
+++ contributions/Enterpaper/java/Fundamentals/2016-08-22.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Using `synchronized` statements
