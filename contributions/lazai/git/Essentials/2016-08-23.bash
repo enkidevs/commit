c6fbbb7c1592ce8a7370fa3318104ec9a8e8f648
lazai <mariusdotspinu@gmail.com>
2016-08-23 08:26:47 +0100

    2016-08-23's git workout

---
+++ contributions/lazai/git/Essentials/2016-08-23.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+You can use git offline!
+Search by commit message keyword
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
