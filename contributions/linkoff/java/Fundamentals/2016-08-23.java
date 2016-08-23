1298bad713bef54a1ff09b242c8764cea14100f4
linkoff <alban.nuel@gmail.com>
2016-08-23 07:18:00 +0100

    2016-08-23's java workout

---
+++ contributions/linkoff/java/Fundamentals/2016-08-23.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Ternary operator
+Distinction between `public` and `private` methods
+Using `Printstream.format` to format a string before printing
+Using an interface as a parameter
