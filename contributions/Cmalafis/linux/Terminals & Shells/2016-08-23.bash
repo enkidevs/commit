3ead6119e7d603ea898cedbbe024df7b031125d3
Cmalafis <cmalafis@gmail.com>
2016-08-23 11:42:32 +0100

    2016-08-23's linux workout

---
+++ contributions/Cmalafis/linux/Terminals & Shells/2016-08-23.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Random password generator
+Keep useful commands in your shell history with tags
