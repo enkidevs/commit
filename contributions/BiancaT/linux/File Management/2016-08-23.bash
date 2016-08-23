1b62dd4802ff522227b636b2392ef2f08ff50002
BiancaT <bianca.dianat@gmail.com>
2016-08-23 17:27:16 +0100

    2016-08-23's linux workout

---
+++ contributions/BiancaT/linux/File Management/2016-08-23.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Easy way of sharing files
