b663ede7baa729cd40c54560d711d8e21645960c
damay <rothbacherc@gmail.com>
2016-08-23 11:31:21 +0100

    2016-08-23's java workout

---
+++ contributions/damay/java/Fundamentals/2016-08-23.java
@@ -0,0 +1,5 @@
+Cropping images
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
