4b4720ebe013450ab8fec07fd2cd48effe620248
jtryon <tryonjf@gmail.com>
2016-08-23 04:06:04 +0100

    2016-08-22's javascript workout

---
+++ contributions/jtryon/javascript/Core/2016-08-22.js
@@ -0,0 +1,5 @@
+Types of numbers
+Using the double tilde `~~` 
+Hoisting applies only to variable declarations, not initializations
+The difference between null, undefined and NaN
+Scope and strict mode
