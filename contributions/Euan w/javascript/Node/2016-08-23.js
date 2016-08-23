2a741e2918eec24c49278ebfbc459d06af8b9bdc
Euan w <fromeuan@gmail.com>
2016-08-23 15:01:14 +0100

    2016-08-23's javascript workout

---
+++ contributions/Euan w/javascript/Node/2016-08-23.js
@@ -0,0 +1,5 @@
+The built-in Node debugger
+Node Inspector
+Use conventional property names for error handling in Node.js
+`uncaughtException` listener in Node.js
+Manage child processes in **Node**
