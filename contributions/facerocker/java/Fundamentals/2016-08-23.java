75c71ec1cca1ccdbe4a57850075807c9f773248e
facerocker <carlos.aguirre.us@gmail.com>
2016-08-24 00:57:32 +0100

    2016-08-23's java workout

---
+++ contributions/facerocker/java/Fundamentals/2016-08-23.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Connecting to FTP using Java
+Using Inheritance to reduce code repetition
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
