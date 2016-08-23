66b79617c4d95a86e7ba59d12d8099ad46836a38
AlexandreRideau <arideau75@gmail.com>
2016-08-23 12:08:30 +0100

    2016-08-23's linux workout

---
+++ contributions/AlexandreRideau/linux/Data Manipulation/2016-08-23.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+Getting rid of duplicate lines
+Count non-empty lines with `nl` utility
+Clean configuration files using `sed`
+Wrap text with `fold`
