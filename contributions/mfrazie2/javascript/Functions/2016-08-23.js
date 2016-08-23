a710924e0ebaa8d39faa1a830b97af8672bec19e
mfrazie2 <michael.s.frazier@gmail.com>
2016-08-23 19:28:06 +0100

    2016-08-23's javascript workout

---
+++ contributions/mfrazie2/javascript/Functions/2016-08-23.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
