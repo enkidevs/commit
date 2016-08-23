7fb0dbe06eb58d8de33da559c9433749ae3e1c14
ondra <ondra@cifka.com>
2016-08-23 08:24:19 +0100

    2016-08-23's linux workout

---
+++ contributions/ondra/linux/Terminals & Shells/2016-08-23.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Random password generator
+Keep useful commands in your shell history with tags
