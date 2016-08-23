b1d582e4b485eacbda33fc90489a46a8595bc56c
endetti <endetti@endetti.net>
2016-08-23 19:42:16 +0100

    2016-08-23's linux workout

---
+++ contributions/endetti/linux/Terminals & Shells/2016-08-23.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Shebang `#!` explained 
+Terminal incognito mode
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
