0c9eb4905ffb0e5a8a1996d1f5b06668ef59e906
seldonplan <jonas0226@gmail.com>
2016-08-23 14:38:37 +0100

    2016-08-23's java workout

---
+++ contributions/seldonplan/java/I/O/2016-08-23.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Using buffered streams
+Checking for the existence of files and directories
+List contents of a directory with `newDirectoryStream`
