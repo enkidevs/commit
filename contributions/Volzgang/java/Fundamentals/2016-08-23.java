816903310cffa3f4cd9841b341e824094c4ada7a
Volzgang <volzgang@gmail.com>
2016-08-23 17:22:20 +0100

    2016-08-23's java workout

---
+++ contributions/Volzgang/java/Fundamentals/2016-08-23.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Using `synchronized` statements
+Supply `toString()` in all classes
+Using an interface as a parameter
