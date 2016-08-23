181894d4e21d6ca6af2f1ed960e6c10485c562cc
raghu <raghu.9208@gmail.com>
2016-08-23 04:03:18 +0100

    2016-08-22's java workout

---
+++ contributions/raghu/java/Fundamentals/2016-08-22.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+How to play a sound file in Java
+Ternary operator
+Connecting to FTP using Java
+Do not perform bitwise and arithmetic operations on the same data
