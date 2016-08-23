53da95fd107399b8a9dde0bbda2df21157789f8e
ESii85 <efraim.sanchez.ii@gmail.com>
2016-08-23 09:19:12 +0100

    2016-08-23's linux workout

---
+++ contributions/ESii85/linux/System Management/2016-08-23.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Force a time update with `ntp`
+
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
