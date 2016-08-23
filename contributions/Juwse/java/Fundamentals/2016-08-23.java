92718f076702f60cb83f918c3c441fc29e6db594
Juwse <j.jagestedt@gmail.com>
2016-08-23 22:49:10 +0100

    2016-08-23's java workout

---
+++ contributions/Juwse/java/Fundamentals/2016-08-23.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Cropping images
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Do not return *references* to private *mutable* class members
