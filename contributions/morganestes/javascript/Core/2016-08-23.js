a450b813f5a1fa477e4fa8453a21f7c16d67487b
morganestes <morgan.estes@gmail.com>
2016-08-23 23:09:40 +0100

    2016-08-23's javascript workout

---
+++ contributions/morganestes/javascript/Core/2016-08-23.js
@@ -0,0 +1,5 @@
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+`catch` is block scoped
+Prevent background image flicker in **IE**
