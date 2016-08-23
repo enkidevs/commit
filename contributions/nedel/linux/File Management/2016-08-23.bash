b84998956da745baebe463d9b6c6de2370ba4a0a
nedel <nedel@gmx.com>
2016-08-23 05:32:45 +0100

    2016-08-23's linux workout

---
+++ contributions/nedel/linux/File Management/2016-08-23.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
