f1b704851ddac9c11fe7e4a2e55965045d77d8a8
c.murphy360 <c.murphy360@gmail.com>
2016-08-23 06:16:36 +0100

    2016-08-22's java workout

---
+++ contributions/c.murphy360/java/Fundamentals/2016-08-22.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
+Using an interface as a parameter
