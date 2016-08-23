f5d5008451e6db3d568ea8b24412c7b3f172fd6e
ajey <ajey.charantimath@gmail.com>
2016-08-23 04:36:19 +0100

    2016-08-22's linux workout

---
+++ contributions/ajey/linux/Terminals & Shells/2016-08-22.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
