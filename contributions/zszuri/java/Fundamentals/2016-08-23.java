f1cf396f474af1071eabb4c3feaeb6ae5657acb2
zszuri <szuri90@gmail.com>
2016-08-23 06:47:53 +0100

    2016-08-23's java workout

---
+++ contributions/zszuri/java/Fundamentals/2016-08-23.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
