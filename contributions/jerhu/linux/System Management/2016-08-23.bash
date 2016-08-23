0b6f3508bc200472f24928c40cb5cd09f8ef853c
jerhu <huet.jeremy@gmail.com>
2016-08-23 08:07:19 +0100

    2016-08-23's linux workout

---
+++ contributions/jerhu/linux/System Management/2016-08-23.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
