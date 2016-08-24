0bb6004de33f721d261e1131c0c3b0ef4a1e96de
jjmiranda <jjmiranda@magia.digital>
2016-08-24 17:03:54 +0100

    2016-08-24's linux workout

---
+++ contributions/jjmiranda/linux/Terminals & Shells/2016-08-24.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Clear the terminal instantly
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
