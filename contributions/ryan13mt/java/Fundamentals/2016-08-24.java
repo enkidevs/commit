afb4785ed95c674b6a4bacbea0d6053d9af0167d
ryan13mt <ryan13mt@gmail.com>
2016-08-24 06:10:16 +0100

    2016-08-24's java workout

---
+++ contributions/ryan13mt/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Detect or prevent integer overflow
