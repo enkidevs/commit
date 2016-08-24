2da57b30d3d935b9619fe22d347eb2c3fc443f2a
Pfor <iah@protonmail.com>
2016-08-24 11:31:05 +0100

    2016-08-24's linux workout

---
+++ contributions/Pfor/linux/System Management/2016-08-24.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
