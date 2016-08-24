c0071fd644e37bbb53aa3d9a502f3613419d09a3
liquidtheory <souan.edouard0@gmail.com>
2016-08-24 11:02:55 +0100

    2016-08-24's linux workout

---
+++ contributions/liquidtheory/linux/File Management/2016-08-24.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Deleting files in a secure manner
+Working with `.gz` files
+Find the unknown process preventing deleting of files
