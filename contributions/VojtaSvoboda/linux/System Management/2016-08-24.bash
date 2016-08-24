0eb2ec41bef6f8780147c404f5ce200389d69c9f
VojtaSvoboda <vojtasvoboda.cz@gmail.com>
2016-08-24 18:54:47 +0100

    2016-08-24's linux workout

---
+++ contributions/VojtaSvoboda/linux/System Management/2016-08-24.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
