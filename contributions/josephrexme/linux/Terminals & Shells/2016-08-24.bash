f05931b9af0cfc5f9e9074aa51d835aa4dafab31
josephrexme <joerexko@gmail.com>
2016-08-25 00:07:23 +0100

    2016-08-24's linux workout

---
+++ contributions/josephrexme/linux/Terminals & Shells/2016-08-24.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Useful aliasing in bash
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
