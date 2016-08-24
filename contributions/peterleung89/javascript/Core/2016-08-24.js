49cd6ad12ef7ce6e3c4783f103687366087ae45d
peterleung89 <peterleung89@gmail.com>
2016-08-24 15:12:09 +0100

    2016-08-24's javascript workout

---
+++ contributions/peterleung89/javascript/Core/2016-08-24.js
@@ -0,0 +1,5 @@
+Multi-line string variables
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+Context in JavaScript
+Prevent background image flicker in **IE**
