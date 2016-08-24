cb96abd88905dacd581859910adc9815cdc5b621
attila19 <attila19@mail.com>
2016-08-24 08:19:49 +0100

    2016-08-24's java workout

---
+++ contributions/attila19/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+How to play a sound file in Java
+The trade-off between short and descriptive names
+Using `Printstream.format` to format a string before printing
+How range operations work
