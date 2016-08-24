3fa7302c6d34f37d415811706fb59b1f26e9d87d
cpthappy <jgvogelhuber@gmail.com>
2016-08-24 18:17:40 +0100

    2016-08-24's java workout

---
+++ contributions/cpthappy/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
