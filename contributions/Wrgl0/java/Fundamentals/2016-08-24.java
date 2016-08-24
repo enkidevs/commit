dad0ec4818ae16bf49dc9668143aa24d4938a7cf
Wrgl0 <wargloo@gmail.com>
2016-08-24 09:23:20 +0100

    2016-08-24's java workout

---
+++ contributions/Wrgl0/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
