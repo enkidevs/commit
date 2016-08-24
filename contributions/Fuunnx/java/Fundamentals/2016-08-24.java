076a876987b2db21f1edf67ac14ef72a22523bc1
Fuunnx <geraud.henrion@gmail.com>
2016-08-24 07:56:26 +0100

    2016-08-24's java workout

---
+++ contributions/Fuunnx/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use meaningful names
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Implementing an `interface`
