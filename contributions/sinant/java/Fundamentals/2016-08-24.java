afd2367e4582037f8439d50fbf152cdecd91d936
sinant <sinantalebi@gmail.com>
2016-08-24 20:37:09 +0100

    2016-08-24's java workout

---
+++ contributions/sinant/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
