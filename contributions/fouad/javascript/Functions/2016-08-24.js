98f4999ce4b93b26ac237d48ec22206e7b0afc0a
fouad <fouad.j@live.fr>
2016-08-24 08:33:31 +0100

    2016-08-24's javascript workout

---
+++ contributions/fouad/javascript/Functions/2016-08-24.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Method chaining
+Changing a functions context with `fn.call(object)`
