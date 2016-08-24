13830a36071401a99df78989961d05dd8ae65320
gregor.eesmaa <gregoreesmaa1@gmail.com>
2016-08-24 06:08:15 +0100

    2016-08-24's linux workout

---
+++ contributions/gregor.eesmaa/linux/System Management/2016-08-24.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
