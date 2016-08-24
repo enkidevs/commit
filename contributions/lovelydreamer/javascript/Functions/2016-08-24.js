c67e9c7659dfa2e7f0f3c553767bce268d42eec8
lovelydreamer <christinamitchell08@gmail.com>
2016-08-24 13:22:44 +0100

    2016-08-24's javascript workout

---
+++ contributions/lovelydreamer/javascript/Functions/2016-08-24.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Method chaining
+Get query/url variables
+Function call method
