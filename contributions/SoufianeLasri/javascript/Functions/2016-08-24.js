2d0ba2833eaf42000416083ade554b1db31a20b5
SoufianeLasri <soufiane.lasri@gmail.com>
2016-08-24 21:06:22 +0100

    2016-08-24's javascript workout

---
+++ contributions/SoufianeLasri/javascript/Functions/2016-08-24.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` with `call` and `apply` methods
+Social sharing without widgets
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
