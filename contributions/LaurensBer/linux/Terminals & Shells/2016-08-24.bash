ff52fad90d88576647fa1439c5247dce10dee4c9
LaurensBer <laurens@laurensbosscher.nl>
2016-08-24 11:24:24 +0100

    2016-08-24's linux workout

---
+++ contributions/LaurensBer/linux/Terminals & Shells/2016-08-24.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Find any Unix / Linux command
+Get to know  your commands with `type`
