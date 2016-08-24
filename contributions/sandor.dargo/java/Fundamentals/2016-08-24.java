e795812c3687c9c1dd12508dc53a517783a4d4df
sandor.dargo <sandor.dargo@gmail.com>
2016-08-24 05:17:35 +0100

    2016-08-24's java workout

---
+++ contributions/sandor.dargo/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Connecting to FTP using Java
+Using the `Console`
+There is no such thing as *pass-by-reference* in Java
