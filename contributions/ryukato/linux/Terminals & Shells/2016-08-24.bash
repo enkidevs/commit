fc452a91ed11a706765dfa2961c7d0d097112b78
ryukato <ryukato79@gmail.com>
2016-08-25 00:56:47 +0100

    2016-08-24's linux workout

---
+++ contributions/ryukato/linux/Terminals & Shells/2016-08-24.bash
@@ -0,0 +1,7 @@
+Clear the terminal instantly
+Useful aliasing in bash
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
