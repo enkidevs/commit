31b757e53f5fea6474c4b1827d13e66d5b8c7b88
AbiGail <abbie.senior@cerberos.co.uk>
2016-08-24 22:23:50 +0100

    2016-08-24's javascript workout

---
+++ contributions/AbiGail/javascript/Functions/2016-08-24.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Difference between **.call** and **.apply** methods
+Method chaining
+Move cursor at the end of text input
+Check if a document is done loading
