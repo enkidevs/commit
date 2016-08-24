80dd82df0c7a5fc5ee46dd144d0ef5fd0cc2fed2
pavlaras <paulisaris@gmail.com>
2016-08-24 06:48:43 +0100

    2016-08-24's java workout

---
+++ contributions/pavlaras/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Distinction between `public` and `private` methods
+Using bounded type parameters in generic methods
