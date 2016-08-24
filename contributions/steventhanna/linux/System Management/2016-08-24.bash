c98745502c004b98c45a27d8a6cf2287f7d0e0df
steventhanna <steventhanna@gmail.com>
2016-08-24 17:28:40 +0100

    2016-08-24's linux workout

---
+++ contributions/steventhanna/linux/System Management/2016-08-24.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
