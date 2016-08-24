e70a5f7a2a81e132a533a57cf7f89209933a4a8a
LittleWladi <wladimirgramacho@gmail.com>
2016-08-24 12:38:13 +0100

    2016-08-24's java workout

---
+++ contributions/LittleWladi/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
