afd710da79dfa78f83c7c7656e110c1c1208d8bf
tdiwv5 <ingham_tyler@yahoo.com>
2016-08-24 18:29:05 +0100

    2016-08-24's java workout

---
+++ contributions/tdiwv5/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Cropping images
+Use meaningful names
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
