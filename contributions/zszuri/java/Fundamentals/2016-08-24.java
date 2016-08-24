03fca26ec42448ba457173add07a6af6bab49e67
zszuri <szuri90@gmail.com>
2016-08-24 06:52:13 +0100

    2016-08-24's java workout

---
+++ contributions/zszuri/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Cropping images
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
