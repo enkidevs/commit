d4ea8d1848a1a310f2b79874c0421821e525ebfa
mfrazie2 <michael.s.frazier@gmail.com>
2016-08-24 23:23:14 +0100

    2016-08-24's javascript workout

---
+++ contributions/mfrazie2/javascript/Npm/2016-08-24.js
@@ -0,0 +1,5 @@
+Registry user accounts for npm
+`npm`  as an alternative to Gulp
+`npm` verifies `sha1` hashes of packages
+`optionalDependencies` in npm
+Lock down dependency versions by shrinkwrapping
