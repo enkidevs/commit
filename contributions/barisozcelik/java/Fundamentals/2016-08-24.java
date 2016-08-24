6206bab34705c56094ed72d2c110f7b260859fc2
barisozcelik <barisozcelikmail@gmail.com>
2016-08-24 06:58:23 +0100

    2016-08-24's java workout

---
+++ contributions/barisozcelik/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
+Using an interface as a parameter
