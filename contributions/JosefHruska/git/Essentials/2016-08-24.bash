4e050093678a6d966ded89a19f5c721a8f283537
JosefHruska <pepa.hruska@gmail.com>
2016-08-24 20:54:06 +0100

    2016-08-24's git workout

---
+++ contributions/JosefHruska/git/Essentials/2016-08-24.bash
@@ -0,0 +1,5 @@
+Using tags for version control
+The three states in git
+How to set your username and email
+Use `short` status to make output more compact
+Remote repositories: fetching and pushing
