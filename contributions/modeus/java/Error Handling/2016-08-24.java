69c91bd6d2970b4e47f02411c5875717728f4afa
modeus <david.zaragoza90@gmail.com>
2016-08-24 18:08:43 +0100

    2016-08-24's java workout

---
+++ contributions/modeus/java/Error Handling/2016-08-24.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Use try-with-resources instead of `finally`
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
