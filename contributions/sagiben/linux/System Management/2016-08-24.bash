d5b7c1dd4a840d5073924fd24daa6da7b3e7f319
sagiben <ilnextbus@gmail.com>
2016-08-24 06:52:43 +0100

    2016-08-24's linux workout

---
+++ contributions/sagiben/linux/System Management/2016-08-24.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Common log files
