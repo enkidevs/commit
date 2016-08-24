9de3df70c5dd8998bd1e9a9a7756cc0477007d57
vincentriemer <vincentriemer@gmail.com>
2016-08-24 14:36:14 +0100

    2016-08-24's javascript workout

---
+++ contributions/vincentriemer/javascript/Npm/2016-08-24.js
@@ -0,0 +1,5 @@
+Manipulate npm packages cache
+npm can handle multiple versions of a dependency
+Tab completion in `npm`
+Reduce package duplication 
+Lock down dependency versions by shrinkwrapping
