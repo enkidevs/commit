90f8a1fd8038a15c8966fb3a90cd7f33bcbf565f
Spidey <deni.munjas@gmail.com>
2016-08-24 03:20:56 +0100

    2016-08-24's linux workout

---
+++ contributions/Spidey/linux/System Management/2016-08-24.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+List current logged on users with `w`
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
