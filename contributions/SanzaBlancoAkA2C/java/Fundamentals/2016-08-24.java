9a79f9eefa3f98203b6ee1c60d8e86bc33cf2765
SanzaBlancoAkA2C <sanzablancoaka2c@gmail.com>
2016-08-24 14:16:09 +0100

    2016-08-24's java workout

---
+++ contributions/SanzaBlancoAkA2C/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Distinction between `public` and `private` methods
+Using Inheritance to reduce code repetition
+Using `synchronized` statements
+Supply `toString()` in all classes
