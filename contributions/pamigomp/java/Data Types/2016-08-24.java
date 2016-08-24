cf857d2dc198b160be5c3b646d529eef2a8c3ad2
pamigomp <pamigomp@op.pl>
2016-08-24 16:11:04 +0100

    2016-08-24's java workout

---
+++ contributions/pamigomp/java/Data Types/2016-08-24.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Retrieve the component type of an array
