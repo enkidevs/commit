7cbd2227e159d5a66f1c03503078bcd45f679e5b
asaBee <hlovers.forever@gmail.com>
2016-08-24 22:19:15 +0100

    2016-08-24's linux workout

---
+++ contributions/asaBee/linux/System Management/2016-08-24.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
