db08375e6fe6b9d71198428eec57d56886cdf7e5
neetkee <neetkee@live.com>
2016-08-24 13:46:19 +0100

    2016-08-24's java workout

---
+++ contributions/neetkee/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Distinction between `public` and `private` methods
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
