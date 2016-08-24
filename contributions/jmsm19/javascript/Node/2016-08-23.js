cc418b8f6b8609ff0fd62fbcb55806bbde960381
jmsm19 <jorgemsm19@gmail.com>
2016-08-24 05:21:51 +0100

    2016-08-23's javascript workout

---
+++ contributions/jmsm19/javascript/Node/2016-08-23.js
@@ -0,0 +1,5 @@
+Node Inspector
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+Automatic compilation for Node with **Nodemon** 
+Manage child processes in **Node**
