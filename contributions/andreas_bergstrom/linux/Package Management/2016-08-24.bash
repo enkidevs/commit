c313394d38cf9f1a8fbcc71b22c9c3ea39e65660
andreas_bergstrom <andreas.bergstrom@gmail.com>
2016-08-24 06:35:56 +0100

    2016-08-24's linux workout

---
+++ contributions/andreas_bergstrom/linux/Package Management/2016-08-24.bash
@@ -0,0 +1,5 @@
+Install specific version of packages
+Installing a `.deb` package from the terminal
+Prevent updating a specific package in Debian systems
+Get `apt` to use a mirror / faster mirror
+Keeping repos updated with `cron`
