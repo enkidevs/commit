1324e04654711c1288a89918775fbae6ef34bd25
Borken <erice.borja@hotmail.com>
2016-08-24 08:05:20 +0100

    2016-08-24's java workout

---
+++ contributions/Borken/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using an interface as a parameter
