b772a3b77afbd46290b2288e221759a862cd3920
corpulentcoffee <dave@corpulentcoffee.com>
2016-08-24 15:40:02 +0100

    2016-08-24's linux workout

---
+++ contributions/corpulentcoffee/linux/System Management/2016-08-24.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Cancel a system shutdown
