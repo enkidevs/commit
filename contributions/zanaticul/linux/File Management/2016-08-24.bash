c9d66df5b31e3503317b1c5b2cd584a4161ac273
zanaticul <cosmin@cioba.ro>
2016-08-24 08:35:54 +0100

    2016-08-24's linux workout

---
+++ contributions/zanaticul/linux/File Management/2016-08-24.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
