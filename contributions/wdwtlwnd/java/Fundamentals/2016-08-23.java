807ded7495c8f058605adb1e4bf6666c4aebf7d5
wdwtlwnd <wdwtlwnd@gmail.com>
2016-08-24 13:58:49 +0100

    2016-08-23's java workout

---
+++ contributions/wdwtlwnd/java/Fundamentals/2016-08-23.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Updating interfaces by using `default` methods
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Implementing an `interface`
