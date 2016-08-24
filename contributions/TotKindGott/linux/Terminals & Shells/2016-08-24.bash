c2d14048935dfa6c6766f1d68d6091ac97ed799e
TotKindGott <konstantin.a.kolosov@gmail.com>
2016-08-24 19:38:29 +0100

    2016-08-24's linux workout

---
+++ contributions/TotKindGott/linux/Terminals & Shells/2016-08-24.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Find any Unix / Linux command
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
