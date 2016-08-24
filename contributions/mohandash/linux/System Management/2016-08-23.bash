a031c47f80d654b3ca753e34075bdb9ee253c510
mohandash <mohandsh176@gmail.com>
2016-08-24 08:13:07 +0100

    2016-08-23's linux workout

---
+++ contributions/mohandash/linux/System Management/2016-08-23.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
