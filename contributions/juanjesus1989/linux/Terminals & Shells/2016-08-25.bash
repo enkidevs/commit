58def041c3134366d1a862242843e2cff61f531d
juanjesus1989 <juanjesusizquierdodomenech@gmail.com>
2016-08-25 07:37:22 +0100

    2016-08-25's linux workout

---
+++ contributions/juanjesus1989/linux/Terminals & Shells/2016-08-25.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Clear the terminal instantly
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
