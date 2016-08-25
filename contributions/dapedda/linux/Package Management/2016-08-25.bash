e066311541a7573529581057b1518ca337e1ee54
dapedda <tschamberger@gmx.de>
2016-08-25 21:59:02 +0100

    2016-08-25's linux workout

---
+++ contributions/dapedda/linux/Package Management/2016-08-25.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Backporting on Debian
+
+Prevent updating a specific package in Debian systems
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
