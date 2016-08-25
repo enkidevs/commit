59daa5935060db2405f5a45b2bd4019268fd660f
tazyman2 <dlees094@uottawa.ca>
2016-08-25 13:30:01 +0100

    2016-08-25's linux workout

---
+++ contributions/tazyman2/linux/System Management/2016-08-25.bash
@@ -0,0 +1,7 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Executing scripts on system boot using `systemd`
