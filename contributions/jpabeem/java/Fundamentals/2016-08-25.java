0839db04782fef08c0fca096eafad96423389e6c
jpabeem <jpa.beemsterboer@gmail.com>
2016-08-25 23:30:04 +0100

    2016-08-25's java workout

---
+++ contributions/jpabeem/java/Fundamentals/2016-08-25.java
@@ -0,0 +1,5 @@
+Cropping images
+Uses of the `final` keyword
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
