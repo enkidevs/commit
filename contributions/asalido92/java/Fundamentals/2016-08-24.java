9c06a4d93c475c0943178804000dd3a80d62db6d
asalido92 <asalido92@gmail.com>
2016-08-25 02:03:06 +0100

    2016-08-24's java workout

---
+++ contributions/asalido92/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
+Using an interface as a parameter
