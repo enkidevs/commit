ed99d942926c8e1e9630a143b14924df22fab4ef
Andee <andytruong94@gmail.com>
2016-08-25 21:41:50 +0100

    2016-08-25's java workout

---
+++ contributions/Andee/java/Fundamentals/2016-08-25.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
