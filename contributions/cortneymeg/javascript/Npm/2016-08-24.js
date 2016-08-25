7e0c968b5545ec4b9d755365e0e38ff3d383839a
cortneymeg <cortney.munna@gmail.com>
2016-08-25 02:20:35 +0100

    2016-08-24's javascript workout

---
+++ contributions/cortneymeg/javascript/Npm/2016-08-24.js
@@ -0,0 +1,5 @@
+Create projects with `npm init`
+Calling remote scripts with npm
+Manage local node modules with `npm link`
+`config` object in `package.json`
+Lock down dependency versions by shrinkwrapping
