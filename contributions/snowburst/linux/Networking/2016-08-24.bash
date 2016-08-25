324304b6fbd79788348f88297c9a32a948c0b19b
snowburst <puresouledge@gmail.com>
2016-08-25 05:35:25 +0100

    2016-08-24's linux workout

---
+++ contributions/snowburst/linux/Networking/2016-08-24.bash
@@ -0,0 +1,5 @@
+Logging dropped firewall packets
+Disable `IPv6`
+Sending emails from terminal / scripts
+Bandwidth monitoring tools 
+Staying connected remotely via SSH without an interactive shell
