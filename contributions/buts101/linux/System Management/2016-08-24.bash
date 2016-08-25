e7520d59d6ce6f028f22d0374038a0e0fb697fd9
buts101 <ashish.batajoo@gmail.com>
2016-08-25 02:43:26 +0100

    2016-08-24's linux workout

---
+++ contributions/buts101/linux/System Management/2016-08-24.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
