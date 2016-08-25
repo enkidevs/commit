13e407f0fbdd9005143b7dc889651888c86b024b
sadasd12340 <amitshinde966@gmail.com>
2016-08-25 05:49:16 +0100

    2016-08-24's java workout

---
+++ contributions/sadasd12340/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Supply `toString()` in all classes
