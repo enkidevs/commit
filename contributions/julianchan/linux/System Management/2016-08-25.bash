aa69685e3cf315c6f8f1c3efd227daffdc8cf9a9
julianchan <mrjulianchan@gmail.com>
2016-08-25 14:32:18 +0100

    2016-08-25's linux workout

---
+++ contributions/julianchan/linux/System Management/2016-08-25.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
