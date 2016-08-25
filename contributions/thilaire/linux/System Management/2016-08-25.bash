24794318438c86a16977476f5dd18ff833793b35
thilaire <thomas.hilaire@free.fr>
2016-08-25 19:39:24 +0100

    2016-08-25's linux workout

---
+++ contributions/thilaire/linux/System Management/2016-08-25.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Common log files
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
