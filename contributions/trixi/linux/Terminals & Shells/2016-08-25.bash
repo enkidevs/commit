67869cbcaa32b45b627265262292a6dcf114b1cf
trixi <beatrix.bodo@gmail.com>
2016-08-25 23:34:09 +0100

    2016-08-25's linux workout

---
+++ contributions/trixi/linux/Terminals & Shells/2016-08-25.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear the terminal instantly
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
