86700c750f7ba1057f15376ccd545e3b89828880
brumeregan <brumeregan@gmail.com>
2016-08-25 12:59:28 +0100

    2016-08-25's linux workout

---
+++ contributions/brumeregan/linux/Terminals & Shells/2016-08-25.bash
@@ -0,0 +1,7 @@
+Shebang `#!` explained 
+Execute a command without saving it in history
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
