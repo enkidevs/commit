b96422e9ebd9a540b29b091dbbf4d0797a87afb8
cncgpp <kitamwhort@gmail.com>
2016-08-25 07:54:09 +0100

    2016-08-25's linux workout

---
+++ contributions/cncgpp/linux/System Management/2016-08-25.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
