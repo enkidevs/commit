003cccddcdbce9e5f856bf2060bc77e881efd042
AlexOlival <lex.olival@gmail.com>
2016-08-25 20:16:35 +0100

    2016-08-25's java workout

---
+++ contributions/AlexOlival/java/Error Handling/2016-08-25.java
@@ -0,0 +1,5 @@
+Use try-with-resources instead of `finally`
+Using exceptions in Java
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
