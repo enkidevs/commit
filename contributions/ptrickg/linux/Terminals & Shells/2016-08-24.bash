a777422c092f36ebd7854b77426ea1a969f01284
ptrickg <ptrickg@hotmail.com>
2016-08-25 02:07:43 +0100

    2016-08-24's linux workout

---
+++ contributions/ptrickg/linux/Terminals & Shells/2016-08-24.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Terminal incognito mode
+Get to know  your commands with `type`
