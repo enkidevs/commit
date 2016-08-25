14b021f71881bcf3947e886b14b0fa5a54ce7c44
rca86 <robert.arnott86@gmail.com>
2016-08-25 22:38:40 +0100

    2016-08-25's linux workout

---
+++ contributions/rca86/linux/Terminals & Shells/2016-08-25.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
