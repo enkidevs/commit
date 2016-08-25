fa4644e799f2fe03f49d41e39035c7980635a10d
dtychko <dtychko@gmail.com>
2016-08-25 08:06:45 +0100

    2016-08-24's java workout

---
+++ contributions/dtychko/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
