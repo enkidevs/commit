366941a4ce851ed5212bee6bf5cb5d5a11cdc3ae
Tansito <david.valinhas@gmail.com>
2016-08-25 22:54:37 +0100

    2016-08-25's javascript workout

---
+++ contributions/Tansito/javascript/Core/2016-08-25.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Functions return `undefined` by default
+`catch` is block scoped
+Extra function arguments are undefined by default
+Prevent background image flicker in **IE**
