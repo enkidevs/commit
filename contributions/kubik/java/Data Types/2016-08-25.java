5314b8cbe79993b3474c4edb8d8616df3f815e4a
kubik <lkubic@poczta.onet.pl>
2016-08-25 07:30:07 +0100

    2016-08-25's java workout

---
+++ contributions/kubik/java/Data Types/2016-08-25.java
@@ -0,0 +1,5 @@
+`ArrayList` vs `LinkedList`
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+String concatenation in Java
+Do not attempt comparisons with NaN
