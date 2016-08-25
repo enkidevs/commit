d6777deca466a26138aeac4250a7e66365a2696d
badbart <tom.zille@gmail.com>
2016-08-25 04:21:19 +0100

    2016-08-25's linux workout

---
+++ contributions/badbart/linux/File Management/2016-08-25.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Deleting non-empty directories
+Easy way of sharing files
