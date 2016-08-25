6dfb1c55fee0cb8cb0f0721d8ae9ced1f0bd7552
AndrewLosseff <andrew.losseff@gmail.com>
2016-08-25 08:05:13 +0100

    2016-08-25's javascript workout

---
+++ contributions/AndrewLosseff/javascript/Core/2016-08-25.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+The difference between null, undefined and NaN
+Scope and strict mode
+Getting the *real* dimensions of an image
+`console.*` in JavaScript
