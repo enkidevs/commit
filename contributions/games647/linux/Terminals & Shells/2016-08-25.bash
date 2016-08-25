5cc4232b9cb0eb836f556ab9f2a33cb1cfcb0172
games647 <games@tsprivat.de>
2016-08-25 07:41:08 +0100

    2016-08-25's linux workout

---
+++ contributions/games647/linux/Terminals & Shells/2016-08-25.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Breaking out of a terminal when `ssh` locks
