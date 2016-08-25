ddb5b12932f6ffa497f2430092f2d09ff9506e9a
oroszgy <oroszgy@gmail.com>
2016-08-25 12:43:55 +0100

    2016-08-25's java workout

---
+++ contributions/oroszgy/java/Fundamentals/2016-08-25.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Detect or prevent integer overflow
+Supply `toString()` in all classes
+Using an interface as a parameter
