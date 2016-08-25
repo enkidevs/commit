3a33e4da9badc128f2aa5a4d249611a4259c0272
ownado <ruben_l-10@hotmail.com>
2016-08-25 08:59:27 +0100

    2016-08-25's linux workout

---
+++ contributions/ownado/linux/File Management/2016-08-25.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Deleting non-empty directories
