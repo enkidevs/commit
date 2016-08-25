a06eeed7306c8b5cc8bfc8d448db316e3cd30c9e
lammypham <lammypham@gmail.com>
2016-08-25 15:30:43 +0100

    2016-08-25's javascript workout

---
+++ contributions/lammypham/javascript/Functions/2016-08-25.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Method chaining
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
