2e01083d85d0133f1991d3511089199e02ef7baa
Turegrappa <cozzubo@gmail.com>
2016-08-25 22:51:26 +0100

    2016-08-25's linux workout

---
+++ contributions/Turegrappa/linux/System Management/2016-08-25.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
