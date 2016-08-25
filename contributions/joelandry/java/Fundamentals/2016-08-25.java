0ff774403a438625acef773f55ac61503cecad92
joelandry <joelandry80@gmail.com>
2016-08-25 13:40:27 +0100

    2016-08-25's java workout

---
+++ contributions/joelandry/java/Fundamentals/2016-08-25.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The trade-off between short and descriptive names
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
