83b52cc378e702194121ba8644d9eb53a6f9d8d3
Engyne <batmadam@hotmail.com>
2016-08-25 04:50:16 +0100

    2016-08-25's java workout

---
+++ contributions/Engyne/java/Fundamentals/2016-08-25.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Using an interface as a parameter
