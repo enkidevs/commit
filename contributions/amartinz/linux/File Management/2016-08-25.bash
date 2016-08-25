584537533c857934cef96f543c5bbb40bfe17f6b
amartinz <eviscerationls@gmail.com>
2016-08-25 11:58:58 +0100

    2016-08-25's linux workout

---
+++ contributions/amartinz/linux/File Management/2016-08-25.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
