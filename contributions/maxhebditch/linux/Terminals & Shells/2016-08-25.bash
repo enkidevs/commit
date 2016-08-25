9985b22ea5aa1f2d9c1fe1a2f7c454c8a903ac43
maxhebditch <maxhebditch@gmail.com>
2016-08-25 07:05:18 +0100

    2016-08-25's linux workout

---
+++ contributions/maxhebditch/linux/Terminals & Shells/2016-08-25.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Find any Unix / Linux command
+Terminal incognito mode
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
