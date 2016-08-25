ce4553c453b705d866b72797edaf90343c07819a
kgresmer <kgresmer@gmail.com>
2016-08-25 06:26:36 +0100

    2016-08-24's java workout

---
+++ contributions/kgresmer/java/Fundamentals/2016-08-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
