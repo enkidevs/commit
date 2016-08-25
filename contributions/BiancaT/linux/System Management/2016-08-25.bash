962c9b832f8fcc7400c98d8c625a91d5454bf8cd
BiancaT <bianca.dianat@gmail.com>
2016-08-25 18:26:44 +0100

    2016-08-25's linux workout

---
+++ contributions/BiancaT/linux/System Management/2016-08-25.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
