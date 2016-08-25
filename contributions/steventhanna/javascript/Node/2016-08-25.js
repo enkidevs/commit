c17bf8425eb9e4e93d3b13ad5e026558d92e4012
steventhanna <steventhanna@gmail.com>
2016-08-25 15:22:48 +0100

    2016-08-25's javascript workout

---
+++ contributions/steventhanna/javascript/Node/2016-08-25.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+Clustering
+`try-catch` only for **sync** code
