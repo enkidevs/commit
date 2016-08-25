9cb08b6f0809b11f7ddcd5744075b647ae520141
Beelzenef <elena.guzbla@gmail.com>
2016-08-25 11:51:53 +0100

    2016-08-25's java workout

---
+++ contributions/Beelzenef/java/Fundamentals/2016-08-25.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Overloading Methods in Java
+Using Inheritance to reduce code repetition
+Locks in `static synchronized` methods
+Limit Accessibility of `Fields`
