d7bef28458b135aa085184c0ffce79fe80ac21ac
alexh <alexhirschberg97+enki@gmail.com>
2016-08-25 14:23:15 +0100

    2016-08-25's linux workout

---
+++ contributions/alexh/linux/Terminals & Shells/2016-08-25.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Find any Unix / Linux command
+Terminal based browser
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
