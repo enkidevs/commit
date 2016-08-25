b14ac99216bbc6940ce2f83bee309078f9fb1e3d
dorharr <dorharr@gmail.com>
2016-08-25 15:38:15 +0100

    2016-08-25's linux workout

---
+++ contributions/dorharr/linux/System Management/2016-08-25.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
