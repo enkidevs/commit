67f4e118ca81fb877829e6164c820e158ac23897
antoligy <a@ax.gy>
2016-08-25 11:42:16 +0100

    2016-08-25's javascript workout

---
+++ contributions/antoligy/javascript/Functions/2016-08-25.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Social sharing without widgets
+Check if a document is done loading
