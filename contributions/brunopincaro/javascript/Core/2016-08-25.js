0cb0338f861e43d097bbb47e7c27f911fa203798
brunopincaro <brunopincaro@gmail.com>
2016-08-25 16:46:46 +0100

    2016-08-25's javascript workout

---
+++ contributions/brunopincaro/javascript/Core/2016-08-25.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Using assignment operators
+Hoisting applies only to variable declarations, not initializations
+Getting the *real* dimensions of an image
+Round numbers to `N` decimals 
