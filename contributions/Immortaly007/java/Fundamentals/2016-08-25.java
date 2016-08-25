94f0ea7b1bacb5228de5fd8737f2ceec47debcbc
Immortaly007 <immortaly007@gmail.com>
2016-08-25 07:31:34 +0100

    2016-08-25's java workout

---
+++ contributions/Immortaly007/java/Fundamentals/2016-08-25.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
