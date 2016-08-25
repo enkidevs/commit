4dfd96cd8dc5194bf3ad8d8753e8e8486ea2bffd
DracZakk <nadir1310@gmail.com>
2016-08-25 21:01:12 +0100

    2016-08-25's linux workout

---
+++ contributions/DracZakk/linux/System Management/2016-08-25.bash
@@ -0,0 +1,5 @@
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
