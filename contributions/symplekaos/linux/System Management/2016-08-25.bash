2f1d97e0234f3b959dc76c44f4fff1b5bc9286fa
symplekaos <symplekaos@gmail.com>
2016-08-25 16:57:40 +0100

    2016-08-25's linux workout

---
+++ contributions/symplekaos/linux/System Management/2016-08-25.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
