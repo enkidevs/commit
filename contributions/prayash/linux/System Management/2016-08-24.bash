d538005897b575edb7dc5e740418c380318216e4
prayash <catchprayash@gmail.com>
2016-08-25 17:40:07 +0100

    2016-08-24's linux workout

---
+++ contributions/prayash/linux/System Management/2016-08-24.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
