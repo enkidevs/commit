da6bc2b66594898287375a7fd2964aa7b2b946c8
adamyala <adamkyala@gmail.com>
2016-08-25 12:41:40 +0100

    2016-08-25's linux workout

---
+++ contributions/adamyala/linux/Terminals & Shells/2016-08-25.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Aliasing ssh connections
+Retrieve previous arguments
+The Basics of Environment Variables
