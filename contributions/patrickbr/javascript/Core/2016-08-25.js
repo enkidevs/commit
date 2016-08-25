4011d6d3229741249c169bace6e3a1a5ce8bb642
patrickbr <spelbrekerpatrick@gmail.com>
2016-08-25 08:03:18 +0100

    2016-08-25's javascript workout

---
+++ contributions/patrickbr/javascript/Core/2016-08-25.js
@@ -0,0 +1,5 @@
+Keywords in hash keys
+Hoisting applies only to variable declarations, not initializations
+Inaccuracy of binary floating-point format
+Round numbers to `N` decimals 
+Base conversion 
