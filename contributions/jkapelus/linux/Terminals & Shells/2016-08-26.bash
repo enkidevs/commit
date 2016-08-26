4147efef4f60d5ff149285ddc8a5ff0a9db2a1a9
jkapelus <lusvirkap@gmail.com>
2016-08-26 16:01:42 +0100

    2016-08-26's linux workout

---
+++ contributions/jkapelus/linux/Terminals & Shells/2016-08-26.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Adding directories to your `$PATH`
