692e11e99f863c3a762a6ff88023ebd164804f7c
divayprakash <divayprakash3@gmail.com>
2016-08-26 15:53:25 +0100

    2016-08-25's linux workout

---
+++ contributions/divayprakash/linux/System Management/2016-08-25.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
+System uptime
