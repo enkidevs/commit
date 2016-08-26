3f3066d9fffedf11bdaffe1a82d5f73cf95009bb
damay <rothbacherc@gmail.com>
2016-08-26 10:00:12 +0100

    2016-08-25's java workout

---
+++ contributions/damay/java/Fundamentals/2016-08-25.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+How to play a sound file in Java
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Using an interface as a parameter
