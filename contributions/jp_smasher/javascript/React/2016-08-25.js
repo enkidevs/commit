1e121b07e69f4b2ac4d3a39fc0db4dad38f2a6e2
jp_smasher <wongjingping@gmail.com>
2016-08-26 01:56:43 +0100

    2016-08-25's javascript workout

---
+++ contributions/jp_smasher/javascript/React/2016-08-25.js
@@ -0,0 +1,6 @@
+`null` value for *Controlled Components* in **React**
+Props in getInitialState Is an        
+Anti-Pattern
+**React** `this.props.children`
+Dangerously set `innerHTML`
+Shallow compare in **React**
