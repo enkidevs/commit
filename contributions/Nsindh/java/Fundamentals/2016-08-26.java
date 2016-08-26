8d965147a2c6b41bc821aea8a2e9542a0e5d691d
Nsindh <nikhilsindhwani@gmail.com>
2016-08-26 08:29:39 +0100

    2016-08-26's java workout

---
+++ contributions/Nsindh/java/Fundamentals/2016-08-26.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
