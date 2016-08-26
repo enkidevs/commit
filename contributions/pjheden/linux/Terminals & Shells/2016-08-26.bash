794642d8e59cf0db55329d3abbc5a3b24c08289d
pjheden <julianheden@gmail.com>
2016-08-26 18:05:49 +0100

    2016-08-26's linux workout

---
+++ contributions/pjheden/linux/Terminals & Shells/2016-08-26.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Terminal based browser
+Keep useful commands in your shell history with tags
