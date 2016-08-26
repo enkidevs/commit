64e1e1d34d5d49ec894d183fc262346bfcec37ca
matt.carron <matt.carron@gmail.com>
2016-08-26 08:54:52 +0100

    2016-08-26's java workout

---
+++ contributions/matt.carron/java/Fundamentals/2016-08-26.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
+Using an interface as a parameter
