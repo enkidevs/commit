1fa982625b48c5839423df903c6d0514ed2bc163
jmdelange <jasper@de-lange.net>
2016-08-26 12:24:21 +0100

    2016-08-26's linux workout

---
+++ contributions/jmdelange/linux/File Management/2016-08-26.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Find the unknown process preventing deleting of files
+Delete commands aliases
