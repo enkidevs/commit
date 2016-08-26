ba8e67d421a729599298bb01ce3b676beefa57a6
CarlyLilwall <carlylilwall@gmail.com>
2016-08-26 20:53:21 +0100

    2016-08-26's java workout

---
+++ contributions/CarlyLilwall/java/Error Handling/2016-08-26.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Using exceptions in Java
+Throwing an `exception`
+Careful Numeric Data Conversions
