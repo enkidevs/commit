1cd485959b9f00a5147dcec7102f8a904ac2f688
MEGHNASREDDY <meghnasreddy@gmail.com>
2016-08-26 16:46:58 +0100

    2016-08-25's linux workout

---
+++ contributions/MEGHNASREDDY/linux/File Management/2016-08-25.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
