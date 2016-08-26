8e63d53220186b1c0fabb6a49f632f5724a591c5
sandokan <luca.faggianelli@gmail.com>
2016-08-26 23:44:29 +0100

    2016-08-26's linux workout

---
+++ contributions/sandokan/linux/System Management/2016-08-26.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Basic service management with `systemd`
+Finding Open Files With `lsof`
