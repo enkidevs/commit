c5f8543d07b890550bb75611a2562db740ecee43
charles_gueunet <charles.gueunet@gmail.com>
2016-08-26 21:31:52 +0100

    2016-08-26's linux workout

---
+++ contributions/charles_gueunet/linux/File Management/2016-08-26.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Easy way of sharing files
