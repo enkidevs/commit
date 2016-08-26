8bd6aaf45f70138ff4ca9b25b489651c5bc7ef6a
jjmiranda <jjmiranda@magia.digital>
2016-08-26 17:30:27 +0100

    2016-08-26's linux workout

---
+++ contributions/jjmiranda/linux/System Management/2016-08-26.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
