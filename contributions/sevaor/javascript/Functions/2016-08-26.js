8039ef1bde71368cdbb9c6af168182c8a4f313bd
sevaor <sevaor@gmail.com>
2016-08-26 16:57:41 +0100

    2016-08-26's javascript workout

---
+++ contributions/sevaor/javascript/Functions/2016-08-26.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Method chaining
+Easily generate a random `HEX` color
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
