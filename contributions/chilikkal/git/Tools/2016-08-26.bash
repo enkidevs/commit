48164bacbd314f3bf7b4e7f5ed28070691bf4bed
chilikkal <chilikkal@gmail.com>
2016-08-26 13:03:02 +0100

    2016-08-26's git workout

---
+++ contributions/chilikkal/git/Tools/2016-08-26.bash
@@ -0,0 +1,5 @@
+Specify a range of commits using double dot syntax
+Interactively stage patches
+Create a new branch from a stash
+Remember the results of previous hunk conflicts
+Sharing data by bundling
