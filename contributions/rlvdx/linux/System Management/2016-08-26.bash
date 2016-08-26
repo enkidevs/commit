bfa0f257d3c139c0884e33805ebbe739d339bb0d
rlvdx <rlvdxslate@gmail.com>
2016-08-26 20:29:46 +0100

    2016-08-26's linux workout

---
+++ contributions/rlvdx/linux/System Management/2016-08-26.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
