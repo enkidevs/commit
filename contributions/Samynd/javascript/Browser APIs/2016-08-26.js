f75be236637ca473bd61000fe7bad4e8142a9e5c
Samynd <samynakayamadriss@gmail.com>
2016-08-26 13:54:34 +0100

    2016-08-26's javascript workout

---
+++ contributions/Samynd/javascript/Browser APIs/2016-08-26.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+`top.location.href`
+Form a `URL` from its parts 
+Warn user if **Back** button is pressed
+FileReader.readAsDataURL()
