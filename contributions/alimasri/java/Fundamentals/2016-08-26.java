0f134dfee008caedf7525d15f81e24fbf931b262
alimasri <alimasri1991@gmail.com>
2016-08-26 08:55:26 +0100

    2016-08-26's java workout

---
+++ contributions/alimasri/java/Fundamentals/2016-08-26.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Detect or prevent integer overflow
