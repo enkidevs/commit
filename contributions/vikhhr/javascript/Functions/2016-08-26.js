f4e340bec88bb637f721eca089aa5b1bd6d5fc25
vikhhr <vikhhr@gmail.com>
2016-08-26 20:40:59 +0100

    2016-08-26's javascript workout

---
+++ contributions/vikhhr/javascript/Functions/2016-08-26.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` with `call` and `apply` methods
+Functions can be declared after use
+Easily generate a random `HEX` color
+Changing a functions context with `fn.call(object)`
