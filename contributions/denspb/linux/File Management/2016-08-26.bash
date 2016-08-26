539474f4c363440a43dc19be482b21e0e7f73268
denspb <samo.public@gmail.com>
2016-08-26 16:57:51 +0100

    2016-08-26's linux workout

---
+++ contributions/denspb/linux/File Management/2016-08-26.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Working with `.gz` files
