db31f241a1979fe48e944098b18e05ac7adff5c6
Geroxan <alik810@seznam.cz>
2016-08-26 17:33:09 +0100

    2016-08-26's linux workout

---
+++ contributions/Geroxan/linux/System Management/2016-08-26.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
