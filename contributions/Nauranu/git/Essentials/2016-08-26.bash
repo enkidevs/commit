65298a324cbbb7560a54f04594d060e5e8a01d25
Nauranu <andrej-chopovskij@yandex.ru>
2016-08-26 11:33:13 +0100

    2016-08-26's git workout

---
+++ contributions/Nauranu/git/Essentials/2016-08-26.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+Pushing tags to a server
+How to set your username and email
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
