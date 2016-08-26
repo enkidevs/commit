227d2bf0566b48d4f68b009b76a0c7a649a80990
bubabum <andddre@bol.com.br>
2016-08-26 02:45:52 +0100

    2016-08-25's java workout

---
+++ contributions/bubabum/java/Error Handling/2016-08-25.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Careful Numeric Data Conversions
