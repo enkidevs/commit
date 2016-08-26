e996004bd9941ee7a1a5d08b69a3f85e551e6dba
danto <danielj@outlook.de>
2016-08-26 12:49:40 +0100

    2016-08-26's linux workout

---
+++ contributions/danto/linux/File Management/2016-08-26.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
