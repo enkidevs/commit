b2b8a8ac3a6f8e6f1a3db4c270806c8d64efa54f
bboulahdid <boulahdid.boubker@gmail.com>
2016-08-26 17:26:06 +0100

    2016-08-26's linux workout

---
+++ contributions/bboulahdid/linux/System Management/2016-08-26.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
