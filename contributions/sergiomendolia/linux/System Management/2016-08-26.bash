ead404510df6447c1597e06ed18e55b3d7bdc182
sergiomendolia <sergio@mendolia.ch>
2016-08-26 19:17:21 +0100

    2016-08-26's linux workout

---
+++ contributions/sergiomendolia/linux/System Management/2016-08-26.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Common log files
+Executing scripts on system boot using `systemd`
+Using `dd` To Create Disk Images
