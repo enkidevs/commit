9c2c5993a76311d92ef5ff3aba813d9a6ff7a073
tchan123 <thomas.chan811@icloud.com>
2016-08-26 03:38:20 +0100

    2016-08-25's linux workout

---
+++ contributions/tchan123/linux/System Management/2016-08-25.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
