633ccf9a4cbf7a05f963d10f37ad8d4ee19edc74
blackecho <angeletti.gabriele@gmail.com>
2016-08-26 08:08:16 +0100

    2016-08-26's linux workout

---
+++ contributions/blackecho/linux/Terminals & Shells/2016-08-26.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
