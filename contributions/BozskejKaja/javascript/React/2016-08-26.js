3f5b254cab2dbfc21833a605ccb265fa076897ad
BozskejKaja <filipdrgon@gmail.com>
2016-08-26 20:00:04 +0100

    2016-08-26's javascript workout

---
+++ contributions/BozskejKaja/javascript/React/2016-08-26.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+componentWillReceiveProps Not Triggered After Mounting
+
+Use **React** with other libraries
+Enhanced Prop Validation 
+Validate for required props
