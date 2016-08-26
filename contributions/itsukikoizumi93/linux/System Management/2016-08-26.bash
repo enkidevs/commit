1ff3a6ff0432f405ad5244de76b0b9de0f06aedf
itsukikoizumi93 <itsukikoizumi93@gmail.com>
2016-08-26 23:01:01 +0100

    2016-08-26's linux workout

---
+++ contributions/itsukikoizumi93/linux/System Management/2016-08-26.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Using `dd` To Create Disk Images
