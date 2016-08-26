4d8429e11026871e734fcf5d3aed56fceb80d2e7
annaphilips <ms.annaphilips@gmail.com>
2016-08-26 04:50:03 +0100

    2016-08-25's linux workout

---
+++ contributions/annaphilips/linux/Terminals & Shells/2016-08-25.bash
@@ -0,0 +1,5 @@
+Find any Unix / Linux command
+Terminal based browser
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
