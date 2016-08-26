cc64fbc3c656422535929c2e05c8e2ddce309c5c
qweluke <maalick.i@gmail.com>
2016-08-26 07:33:46 +0100

    2016-08-26's linux workout

---
+++ contributions/qweluke/linux/Terminals & Shells/2016-08-26.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Clear the terminal instantly
+Useful aliasing in bash
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
