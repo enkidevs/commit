48b6075ad7ffb502d5785886c2fdb82d8eee4a83
willa <willatauro@gmail.com>
2016-08-26 13:55:08 +0100

    2016-08-26's linux workout

---
+++ contributions/willa/linux/File Management/2016-08-26.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Practical `du` command
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
