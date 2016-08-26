4771b00d3bd18d6d98cf1aef398aa315c78d896f
Tanker405th <thenuttyirishman@gmail.com>
2016-08-26 03:11:10 +0100

    2016-08-25's linux workout

---
+++ contributions/Tanker405th/linux/File Management/2016-08-25.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
+Delete commands aliases
+Easy way of sharing files
