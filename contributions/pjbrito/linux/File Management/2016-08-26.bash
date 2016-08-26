6209bc0c286f5b67713a0c44849cf9e3aae3d1a6
pjbrito <pjbrito@gmail.com>
2016-08-26 07:10:01 +0100

    2016-08-26's linux workout

---
+++ contributions/pjbrito/linux/File Management/2016-08-26.bash
@@ -0,0 +1,6 @@
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Working with `.gz` files
+Find the unknown process preventing deleting of files
