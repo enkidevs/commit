75d8eba2b392f8bae232a5f04bd214e809fa7025
senfino <stefanosega89@gmail.com>
2016-08-26 07:09:14 +0100

    2016-08-26's git workout

---
+++ contributions/senfino/git/Essentials/2016-08-26.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+The three states in git
+Search for commits by author
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
