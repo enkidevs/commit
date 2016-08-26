e2474309880f09c8b3ef0ce0c17ff7c25295ac72
raderb <b.rader@gmx.at>
2016-08-26 06:17:24 +0100

    2016-08-26's linux workout

---
+++ contributions/raderb/linux/System Management/2016-08-26.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
