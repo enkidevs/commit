52577dfa179e99ac9b6cbfcb7ff4f42a1adbc470
florianriche <friche10@gmail.com>
2016-08-26 19:53:05 +0100

    2016-08-26's linux workout

---
+++ contributions/florianriche/linux/Terminals & Shells/2016-08-26.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
