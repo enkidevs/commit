130e4f8eef0486a10f9e88f9ed28ddd51983c3c6
alfredoszrz <alfredoszrz@gmail.com>
2016-08-26 15:10:55 +0100

    2016-08-26's linux workout

---
+++ contributions/alfredoszrz/linux/File Management/2016-08-26.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Working with `.gz` files
+Delete commands aliases
