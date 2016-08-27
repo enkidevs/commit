ab5b4cec640b0567d283eabdc599fc34ac3bd13f
LittleWladi <wladimirgramacho@gmail.com>
2016-08-27 16:34:09 +0100

    2016-08-27's javascript workout

---
+++ contributions/LittleWladi/javascript/Core/2016-08-27.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Using the double tilde `~~` 
+Most efficient way to build `HTML` strings
+Hoisting applies only to variable declarations, not initializations
+Inaccuracy of binary floating-point format
