be718cfd3a52f7639c19cae075b6066e26c17f07
Wrgl0 <wargloo@gmail.com>
2016-08-27 08:54:09 +0100

    2016-08-27's java workout

---
+++ contributions/Wrgl0/java/Data Types/2016-08-27.java
@@ -0,0 +1,5 @@
+`ArrayList` vs `LinkedList`
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Retrieve the component type of an array
+Numeric Conversion - Widening
