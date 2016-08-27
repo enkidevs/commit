6fb9d61d99972d132d8431b2f7d9948abf048865
papigers <papigers93@gmail.com>
2016-08-27 04:10:19 +0100

    2016-08-27's linux workout

---
+++ contributions/papigers/linux/System Management/2016-08-27.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
