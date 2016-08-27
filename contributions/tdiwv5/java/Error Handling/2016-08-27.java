a57bb2c6559dc656a8f46dff5b77ffb365315242
tdiwv5 <ingham_tyler@yahoo.com>
2016-08-27 17:46:34 +0100

    2016-08-27's java workout

---
+++ contributions/tdiwv5/java/Error Handling/2016-08-27.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Throwing an `exception`
