d1fe29b2171e175cef20e36c8843baa9812a87e2
wesleyrbk <wrensenbrink@gmail.com>
2016-08-27 11:07:12 +0100

    2016-08-27's javascript workout

---
+++ contributions/wesleyrbk/javascript/Node/2016-08-27.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Streams in **Node**
+Use conventional property names for error handling in Node.js
+**Node** `os` module
+`process.nextTick()` callback pattern
