3cefac7c0152f127182bda424dffcf4822e5afc2
Phalanger <marshall.david78@gmail.com>
2016-08-27 22:52:24 +0100

    2016-08-27's linux workout

---
+++ contributions/Phalanger/linux/Terminals & Shells/2016-08-27.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Find any Unix / Linux command
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
