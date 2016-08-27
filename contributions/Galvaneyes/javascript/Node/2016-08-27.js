376241de80aba77aba7053267ae3b5edc0114fa5
Galvaneyes <m.v.manchev@gmail.com>
2016-08-27 20:45:36 +0100

    2016-08-27's javascript workout

---
+++ contributions/Galvaneyes/javascript/Node/2016-08-27.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+First-error callbacks in **Node**
+**Node** `os` module
+Automatic compilation for Node with **Nodemon** 
+`try-catch` only for **sync** code
