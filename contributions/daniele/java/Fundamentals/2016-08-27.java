3751b77874fca68d9bb4b8915098d81832e60d51
daniele <daniele@monteleone.ml>
2016-08-27 08:06:11 +0100

    2016-08-27's java workout

---
+++ contributions/daniele/java/Fundamentals/2016-08-27.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
