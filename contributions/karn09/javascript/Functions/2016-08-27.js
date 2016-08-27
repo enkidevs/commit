569131bc321b03fc9c659b7791ce0ff62f833bd5
karn09 <karn09@gmail.com>
2016-08-27 14:20:04 +0100

    2016-08-27's javascript workout

---
+++ contributions/karn09/javascript/Functions/2016-08-27.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
