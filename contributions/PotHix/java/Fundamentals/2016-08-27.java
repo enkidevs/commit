9fded35147f2292486815f840521c6bdfaac43cb
PotHix <pothix@pothix.com>
2016-08-27 11:49:48 +0100

    2016-08-27's java workout

---
+++ contributions/PotHix/java/Fundamentals/2016-08-27.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Overloading Methods in Java
+The distinction between overloading and overriding methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
