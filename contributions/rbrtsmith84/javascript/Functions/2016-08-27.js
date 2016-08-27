2de7a2e24ebd6a4dddbb7c35294c99d2b74ab5be
rbrtsmith84 <rbrtsmith84@googlemail.com>
2016-08-27 12:00:08 +0100

    2016-08-27's javascript workout

---
+++ contributions/rbrtsmith84/javascript/Functions/2016-08-27.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Changing a functions context with `fn.call(object)`
