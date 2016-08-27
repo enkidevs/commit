5fb1c39501e6c2e9234e091c6fdbacaab78e94e2
darkarchon92 <nono_1992_@hotmail.com>
2016-08-27 12:14:05 +0100

    2016-08-27's javascript workout

---
+++ contributions/darkarchon92/javascript/Core/2016-08-27.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Use `splice()` to remove an item from an array
+Scope and strict mode
+Extra function arguments are undefined by default
+Getting the *real* dimensions of an image
