e4193d8aa90b7c43debcbc5014aecadb5a1c58e2
Eili <eilenberger.dominik@gmail.com>
2016-08-27 06:49:08 +0100

    2016-08-27's java workout

---
+++ contributions/Eili/java/Fundamentals/2016-08-27.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Java naming conventions
+Connecting to FTP using Java
+Using `Printstream.format` to format a string before printing
+Do not perform bitwise and arithmetic operations on the same data
