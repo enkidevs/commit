fc40c23a5ffda0dda15b9be4995f1a36279e63f7
ahps <hampus@ahps.se>
2016-08-27 12:56:08 +0100

    2016-08-27's linux workout

---
+++ contributions/ahps/linux/Package Management/2016-08-27.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Installing a `.deb` package from the terminal
+Removing PPAs
+Downgrade a package via `apt-get`
+Keeping repos updated with `cron`
