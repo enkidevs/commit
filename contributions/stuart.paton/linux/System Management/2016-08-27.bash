fcfc3833bf8c2c9c356069881d3f109b3336a64a
stuart.paton <stuart.john.paton@gmail.com>
2016-08-27 18:08:55 +0100

    2016-08-27's linux workout

---
+++ contributions/stuart.paton/linux/System Management/2016-08-27.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
