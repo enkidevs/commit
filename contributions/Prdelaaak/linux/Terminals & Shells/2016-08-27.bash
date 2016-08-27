0ed08c715f315892ef0dae8f928bc43d1470b2c5
Prdelaaak <prdelaaak@gmail.com>
2016-08-27 15:30:10 +0100

    2016-08-27's linux workout

---
+++ contributions/Prdelaaak/linux/Terminals & Shells/2016-08-27.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Find any Unix / Linux command
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
