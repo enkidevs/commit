033b2929a7837be836c49da367a9f0fae4a7b0f0
spaix <mateusz.anikiej@gmail.com>
2016-08-27 22:27:39 +0100

    2016-08-27's javascript workout

---
+++ contributions/spaix/javascript/React/2016-08-27.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+Pixel values in style props for **React**
+`if`-`else` statements in **JSX** and **React**
+Load Initial Data via AJAX
+Specify a single child
