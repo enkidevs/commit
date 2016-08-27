e13e95d9ca383aac396cce225afde23d7ea01a84
DacoDC <danncorbalan@gmail.com>
2016-08-27 20:26:35 +0100

    2016-08-27's java workout

---
+++ contributions/DacoDC/java/Fundamentals/2016-08-27.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Supply `toString()` in all classes
