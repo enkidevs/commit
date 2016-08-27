bce691796289168b2a651b7c4b0c8d26179ad67d
Tansito <david.valinhas@gmail.com>
2016-08-27 21:33:00 +0100

    2016-08-27's java workout

---
+++ contributions/Tansito/java/Fundamentals/2016-08-27.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
