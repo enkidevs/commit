80e48f66637f4d25013ac2397ce95777cbf1bb3a
e7krgc <enam93@gmail.com>
2016-08-27 07:19:36 +0100

    2016-08-27's java workout

---
+++ contributions/e7krgc/java/I/O/2016-08-27.java
@@ -0,0 +1,5 @@
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
+The `Console` class
