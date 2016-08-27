e8640f8b6dd8cb887d6f3ede8074b000e5d45a79
dabizk <dabizk@yahoo.com>
2016-08-28 00:50:43 +0100

    2016-08-27's java workout

---
+++ contributions/dabizk/java/Fundamentals/2016-08-27.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Java naming conventions
+Distinction between `public` and `private` methods
+Using `Printstream.format` to format a string before printing
+Implementing an `interface`
