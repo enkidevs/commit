c1ec83dcccb3fe12efa98a714b3f979b6f345355
omasana <stepanar@gmail.com>
2016-08-27 10:06:24 +0100

    2016-08-27's linux workout

---
+++ contributions/omasana/linux/System Management/2016-08-27.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
