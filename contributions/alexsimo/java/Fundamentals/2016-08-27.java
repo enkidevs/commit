d3566c79f5115e382fc030cd8137794f09278892
alexsimo <alexandru.simonescu@gmail.com>
2016-08-27 22:13:52 +0100

    2016-08-27's java workout

---
+++ contributions/alexsimo/java/Fundamentals/2016-08-27.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
