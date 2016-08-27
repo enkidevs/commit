f3b0474dba5e41d54ca2f7df3d66da209d9b3e22
massimo <massimobalestra@hotmail.com>
2016-08-27 14:32:12 +0100

    2016-08-27's linux workout

---
+++ contributions/massimo/linux/Networking/2016-08-27.bash
@@ -0,0 +1,5 @@
+SSH tunneling made easy
+Quick port test with `netcat`
+Multiple IP Address with aliasing 
+Useful ssh client optimizations 
+Staying connected remotely via SSH without an interactive shell
