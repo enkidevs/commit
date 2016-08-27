b576fd74ed8bd3ac11d4622fa7864777a8e202cd
TM1989 <torben.moeller@gmx.net>
2016-08-27 10:02:09 +0100

    2016-08-27's java workout

---
+++ contributions/TM1989/java/Fundamentals/2016-08-27.java
@@ -0,0 +1,5 @@
+Cropping images
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
