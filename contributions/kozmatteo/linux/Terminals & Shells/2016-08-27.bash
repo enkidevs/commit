c395718c7f85b5bfdfd66b0573137f5df07c0261
kozmatteo <defique@gmail.com>
2016-08-27 10:53:59 +0100

    2016-08-27's linux workout

---
+++ contributions/kozmatteo/linux/Terminals & Shells/2016-08-27.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Quick `bash` shortcuts 
+Useful aliasing in bash
+Keep useful commands in your shell history with tags
