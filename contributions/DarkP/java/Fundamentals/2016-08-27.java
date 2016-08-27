24ed8455da0ed3840186406df0f1a32e3dea9919
DarkP <petr456@seznam.cz>
2016-08-27 10:12:32 +0100

    2016-08-27's java workout

---
+++ contributions/DarkP/java/Fundamentals/2016-08-27.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
