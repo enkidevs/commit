3c17a1d8f2cf68ec62a33a336333d3853be1aa5e
IPat <muellschrotti@gmail.com>
2016-08-27 12:46:27 +0100

    2016-08-27's java workout

---
+++ contributions/IPat/java/Fundamentals/2016-08-27.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
