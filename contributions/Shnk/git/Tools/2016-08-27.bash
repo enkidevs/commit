7126cff803404589e7a20da29b3afe2f336b8280
Shnk <wavelet.alexandre@gmail.com>
2016-08-27 21:01:15 +0100

    2016-08-27's git workout

---
+++ contributions/Shnk/git/Tools/2016-08-27.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Specify a range of commits using double dot syntax
+Remember the results of previous hunk conflicts
+Debug using binary search
+Sharing data by bundling
