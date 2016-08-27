ee4fe5c34c8cf6659679a22c0bd8750722616dcc
jefrailey <jcaetof+enki@gmail.com>
2016-08-27 19:54:24 +0100

    2016-08-27's linux workout

---
+++ contributions/jefrailey/linux/System Management/2016-08-27.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Test disk speed with `dd`
+Detect your linux distribution
+Preventing a user from logging into the system
+Basic service management with `systemd`
