ae7e5981d6290682b565529b76c251927eae3f1d
Streiter <nadia.streiter@gmail.com>
2016-08-27 02:46:02 +0100

    2016-08-27's linux workout

---
+++ contributions/Streiter/linux/Terminals & Shells/2016-08-27.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Shebang `#!` explained 
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
