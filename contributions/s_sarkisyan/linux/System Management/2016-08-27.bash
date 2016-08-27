0403ffec920f8af286bf7af013676c382a305db9
s_sarkisyan <lie@europe.com>
2016-08-27 08:11:17 +0100

    2016-08-27's linux workout

---
+++ contributions/s_sarkisyan/linux/System Management/2016-08-27.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
