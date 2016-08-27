24658d06c8e3e998599a99fac7fc4187f751bb31
emilianoandre <emilianoandre@gmail.com>
2016-08-27 01:05:18 +0100

    2016-08-26's java workout

---
+++ contributions/emilianoandre/java/Fundamentals/2016-08-26.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
