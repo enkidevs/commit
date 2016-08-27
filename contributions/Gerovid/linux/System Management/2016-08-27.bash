6e68161d04c58e25a391cb8861ee2e332c806de4
Gerovid <gerovid@gmail.com>
2016-08-27 18:22:16 +0100

    2016-08-27's linux workout

---
+++ contributions/Gerovid/linux/System Management/2016-08-27.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Monitor System Memory using `vmstat`
