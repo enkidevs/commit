78fb9dca619e5e9c5c77b05db9e5937218740df1
rjtrout <kilgorejohntrout@gmail.com>
2016-08-27 23:11:52 +0100

    2016-08-27's linux workout

---
+++ contributions/rjtrout/linux/System Management/2016-08-27.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
