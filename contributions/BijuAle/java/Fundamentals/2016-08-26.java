438663332c7e50a7ac3f23dfc158f4c93533bf8b
BijuAle <bijuale@hotmail.com>
2016-08-27 18:32:50 +0100

    2016-08-26's java workout

---
+++ contributions/BijuAle/java/Fundamentals/2016-08-26.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Distinction between `public` and `private` methods
+Using `Printstream.format` to format a string before printing
+Using bounded type parameters in generic methods
+How range operations work
