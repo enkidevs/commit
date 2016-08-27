26a321a0d6b8735b1dc0d19396ebaeca65827a64
shakedl <shaked.lokits@gmail.com>
2016-08-27 15:59:57 +0100

    2016-08-27's javascript workout

---
+++ contributions/shakedl/javascript/Core/2016-08-27.js
@@ -0,0 +1,5 @@
+Multi-line string variables
+Hoisting applies only to variable declarations, not initializations
+Functions return `undefined` by default
+JavaScript compilation
+Extra function arguments are undefined by default
