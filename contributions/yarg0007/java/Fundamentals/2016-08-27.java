75fe24e67f2309d64bb06d92b7b1af5f0087383e
yarg0007 <benjamin.yarger@gmail.com>
2016-08-27 16:55:52 +0100

    2016-08-27's java workout

---
+++ contributions/yarg0007/java/Fundamentals/2016-08-27.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
