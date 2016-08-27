dc2f400d211a82f1fffab6fe78a27aaef44b312f
dmkonst <warqueq@gmail.com>
2016-08-27 18:12:34 +0100

    2016-08-27's linux workout

---
+++ contributions/dmkonst/linux/File Management/2016-08-27.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Using `touch` to alter files modification time
