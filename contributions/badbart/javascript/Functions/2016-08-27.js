15caadbbc5dcdff44b845eedd877dfb12e8b8069
badbart <tom.zille@gmail.com>
2016-08-27 07:54:20 +0100

    2016-08-27's javascript workout

---
+++ contributions/badbart/javascript/Functions/2016-08-27.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+`.bind()` 
+Changing a functions context with `fn.call(object)`
