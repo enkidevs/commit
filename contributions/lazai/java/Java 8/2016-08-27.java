efef8a4b6ebbd922ccc3e8d99414b6d2f67d47e6
lazai <mariusdotspinu@gmail.com>
2016-08-27 07:38:02 +0100

    2016-08-27's java workout

---
+++ contributions/lazai/java/Java 8/2016-08-27.java
@@ -0,0 +1,5 @@
+Java 8 Lambda expressions
+How to check for static dependencies of applications and libraries from the command line.
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
