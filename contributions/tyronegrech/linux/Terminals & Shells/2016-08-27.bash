4a7dd1b31205f23fc79a5567003f146e2f62daf9
tyronegrech <grechtyrone1989@gmail.com>
2016-08-27 07:51:06 +0100

    2016-08-27's linux workout

---
+++ contributions/tyronegrech/linux/Terminals & Shells/2016-08-27.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Find any Unix / Linux command
+Useful aliasing in bash
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
