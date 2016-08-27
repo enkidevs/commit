0e49f63cb45b4072267dba4bf4e810465e1b04e0
LoremIpsum <yannick.jadoul@gmail.com>
2016-08-27 15:42:17 +0100

    2016-08-27's java workout

---
+++ contributions/LoremIpsum/java/Classes & Objects/2016-08-27.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Using `static` fields
+Utility classes and `static` methods
+How and when to use `WeakHashMap` class
+Fields with Java **Reflection API**
