a6117e1654845906f6e23f590808780f8c4937fe
gautamc <chitnis.gautam1@gmail.com>
2016-08-27 05:55:42 +0100

    2016-08-26's java workout

---
+++ contributions/gautamc/java/Fundamentals/2016-08-26.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
+Using an interface as a parameter
