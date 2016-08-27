988971074d319b5268d78c5ed186485574929819
bence <bence@underyx.me>
2016-08-27 01:08:18 +0100

    2016-08-26's linux workout

---
+++ contributions/bence/linux/Terminals & Shells/2016-08-26.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Terminal incognito mode
+Keep useful commands in your shell history with tags
