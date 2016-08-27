511f649417b44c781ff218547413e9be12f44435
willa <willatauro@gmail.com>
2016-08-27 23:20:31 +0100

    2016-08-27's java workout

---
+++ contributions/willa/java/Fundamentals/2016-08-27.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Connecting to FTP using Java
+Using `Printstream.format` to format a string before printing
+Locks in `static synchronized` methods
+Using `synchronized` statements
