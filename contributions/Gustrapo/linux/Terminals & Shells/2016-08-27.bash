c86a26b1f3b542ff528cd4fb553ebc040017aa72
Gustrapo <gustavocaso@gmail.com>
2016-08-27 15:58:49 +0100

    2016-08-27's linux workout

---
+++ contributions/Gustrapo/linux/Terminals & Shells/2016-08-27.bash
@@ -0,0 +1,5 @@
+Quick `bash` shortcuts 
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
