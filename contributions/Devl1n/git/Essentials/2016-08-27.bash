db3724f715002919c8c7813f58e5b4ae773fef65
Devl1n <claes.lindahl@gmail.com>
2016-08-27 07:47:08 +0100

    2016-08-27's git workout

---
+++ contributions/Devl1n/git/Essentials/2016-08-27.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+The three states in git
+Check the status of your files
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
