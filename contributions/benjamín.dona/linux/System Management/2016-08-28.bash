a4fea758e92b707f8d72dfcd46e658e1cb73c463
benjamín.dona <benjamin.dona@live.cl>
2016-08-28 12:55:28 +0100

    2016-08-28's linux workout

---
+++ contributions/benjamín.dona/linux/System Management/2016-08-28.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
