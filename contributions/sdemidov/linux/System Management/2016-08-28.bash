0f4b66caaa9d6e735fa253dccddec79cf758e42d
sdemidov <demidov.sergej@gmail.com>
2016-08-28 13:13:25 +0100

    2016-08-28's linux workout

---
+++ contributions/sdemidov/linux/System Management/2016-08-28.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
