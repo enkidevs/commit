f8bf54c24adcecadbafdaf93a6c1f569a7d14986
coderguy <canpy30@gmail.com>
2016-08-28 19:37:19 +0100

    2016-08-28's java workout

---
+++ contributions/coderguy/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
