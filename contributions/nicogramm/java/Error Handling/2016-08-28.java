59b2670a71d96ac968229d0f6356749e030c4c3d
nicogramm <nico9klatt@gmail.com>
2016-08-28 09:01:13 +0100

    2016-08-28's java workout

---
+++ contributions/nicogramm/java/Error Handling/2016-08-28.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Prevent `exceptions` while logging Data
+Throwing an `exception`
+Careful Numeric Data Conversions
