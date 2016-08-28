0a7307fb8953b970cf1424f0e7358157d39d6e0a
dapedda <tschamberger@gmx.de>
2016-08-28 09:11:49 +0100

    2016-08-28's linux workout

---
+++ contributions/dapedda/linux/Terminals & Shells/2016-08-28.bash
@@ -0,0 +1,5 @@
+Random password generator
+Terminal incognito mode
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
