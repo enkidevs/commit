93d78263ba5c329569dc91cca6d7c0e4e830662b
jberry93 <jeffreyberry93@gmail.com>
2016-08-28 20:26:06 +0100

    2016-08-28's linux workout

---
+++ contributions/jberry93/linux/System Management/2016-08-28.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
