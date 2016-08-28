592c811cf31ad2671e19674611f8d7977719ae23
Somanprabhu <soman.sivam@gmail.com>
2016-08-28 13:22:36 +0100

    2016-08-27's java workout

---
+++ contributions/Somanprabhu/java/Fundamentals/2016-08-27.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
