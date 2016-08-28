f938262b266bb860e57828aab12d466f440f2b5f
chrswndrs <christoph.wanders@gmail.com>
2016-08-28 20:14:43 +0100

    2016-08-28's linux workout

---
+++ contributions/chrswndrs/linux/System Management/2016-08-28.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
