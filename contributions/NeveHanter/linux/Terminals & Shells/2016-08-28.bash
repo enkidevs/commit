9e8aab0a3a6100a42abc81e8bf045970d0593e9c
NeveHanter <nevehanter@gmail.com>
2016-08-28 08:44:29 +0100

    2016-08-28's linux workout

---
+++ contributions/NeveHanter/linux/Terminals & Shells/2016-08-28.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
