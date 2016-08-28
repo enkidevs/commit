170d932c969b8dbc2e771e59a3ef555d88e52942
simonvart <simon.vart@exigences.biz>
2016-08-28 10:46:17 +0100

    2016-08-28's javascript workout

---
+++ contributions/simonvart/javascript/Ecmascript 2015/2016-08-28.js
@@ -0,0 +1,6 @@
+Defaults values apply only to `undefined` (and not to `null`)
+Internationalization & Localization
+
+ES6 Arrow Functions
+Binary and octal notation in ES6
+Tail call optimisation in ES6
