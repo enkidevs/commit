c959f8a800390effd848df4de9f52acdf38427f4
dreszczyk <enki@plamka.net>
2016-08-28 11:44:00 +0100

    2016-08-28's git workout

---
+++ contributions/dreszczyk/git/Essentials/2016-08-28.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to unmodify a modified file
+Make your log output pretty
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
