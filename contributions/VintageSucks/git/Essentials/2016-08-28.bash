81c3d942dd9fabeaf8b77bc7ea51f1f9fd0158fe
VintageSucks <nikolas.evers@me.com>
2016-08-28 06:09:48 +0100

    2016-08-28's git workout

---
+++ contributions/VintageSucks/git/Essentials/2016-08-28.bash
@@ -0,0 +1,5 @@
+You can use git offline!
+How to set your username and email
+Check the status of your files
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
