be9ff282cf89ccb4574db57e5b0bfb789f2a56d4
Carleslc <lazaro.costa.carles@gmail.com>
2016-08-28 16:02:47 +0100

    2016-08-28's javascript workout

---
+++ contributions/Carleslc/javascript/Core/2016-08-28.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Data type comparison in `switch` statements
+The difference between null, undefined and NaN
+Inaccuracy of binary floating-point format
+Round numbers to `N` decimals 
