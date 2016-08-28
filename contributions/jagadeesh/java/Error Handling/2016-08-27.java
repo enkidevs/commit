113dbe40e794a6c1cffca6bc6cbef594a8ca85f2
jagadeesh <hellmaker001@gmail.com>
2016-08-28 02:54:32 +0100

    2016-08-27's java workout

---
+++ contributions/jagadeesh/java/Error Handling/2016-08-27.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
