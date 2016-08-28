0b409cb1ed72fdec1f9e93e4c64c452e91ad895e
byt3b0n3 <byt3b0n3@gmail.com>
2016-08-28 14:40:56 +0100

    2016-08-28's linux workout

---
+++ contributions/byt3b0n3/linux/System Management/2016-08-28.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
