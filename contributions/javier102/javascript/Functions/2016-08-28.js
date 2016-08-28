e4dbcfb89d37d87b00b7256ee43dae8820e7f7b4
javier102 <javier102@movistar.es>
2016-08-28 14:22:56 +0100

    2016-08-28's javascript workout

---
+++ contributions/javier102/javascript/Functions/2016-08-28.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Method chaining
+Easily generate a random `HEX` color
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
