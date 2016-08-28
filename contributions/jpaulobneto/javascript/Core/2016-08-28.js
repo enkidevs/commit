2ed5ecf0b321b5ec0e953547c621c06f89b642e7
jpaulobneto <jpaulobneto@gmail.com>
2016-08-28 14:18:24 +0100

    2016-08-28's javascript workout

---
+++ contributions/jpaulobneto/javascript/Core/2016-08-28.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Use `splice()` to remove an item from an array
+Using assignment operators
+The difference between null, undefined and NaN
+Detect online connection
