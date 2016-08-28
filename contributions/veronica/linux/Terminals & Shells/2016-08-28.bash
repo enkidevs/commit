4fdd36cf7e2805a942dddf2f405d4c44aab3faa0
veronica <me@veronicacannon.com>
2016-08-28 17:36:27 +0100

    2016-08-28's linux workout

---
+++ contributions/veronica/linux/Terminals & Shells/2016-08-28.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Adding directories to your `$PATH`
+Retrieve previous arguments
+The Basics of Environment Variables
