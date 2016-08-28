1582be4fa72da9257555d28d4ecd98f7c8126233
Alanbato <alanvelasco.a@gmail.com>
2016-08-28 13:12:54 +0100

    2016-08-28's python workout

---
+++ contributions/Alanbato/python/Data Structures/2016-08-28.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`bytes` type
+Best way to implement a simple `queue` 
+Get the most of `int`s
+Implementing **weak** references
