5c126a36b3f6dd72b729da675575ccc9217f32fc
PatrikLord <patrik.j.lord@gmail.com>
2016-08-28 08:45:24 +0100

    2016-08-28's java workout

---
+++ contributions/PatrikLord/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
