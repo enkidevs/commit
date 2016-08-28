b6eed98ffd80b2c484f92b72cc1f4eb6d62de413
alimasri <alimasri1991@gmail.com>
2016-08-28 08:40:36 +0100

    2016-08-28's java workout

---
+++ contributions/alimasri/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Common mistake on switch statements
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
