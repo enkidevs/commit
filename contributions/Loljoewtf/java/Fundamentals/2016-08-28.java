1cd05e9e227596331f4d8273f5e21cf04ec08775
Loljoewtf <joecarlyon@gmail.com>
2016-08-28 15:32:46 +0100

    2016-08-28's java workout

---
+++ contributions/Loljoewtf/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
