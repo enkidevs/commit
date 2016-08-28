944c9c8445deb3ac70bd1e4d618880fde7ac73fb
zacker <zlatko85@gmail.com>
2016-08-28 01:08:14 +0100

    2016-08-27's linux workout

---
+++ contributions/zacker/linux/System Management/2016-08-27.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Using `dd` To Create Disk Images
