179fe5ab90f1b2f13aba22cc8fd2769b5d752db1
Layo <thegamerult@googlemail.com>
2016-08-28 18:49:06 +0100

    2016-08-28's java workout

---
+++ contributions/Layo/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+Cropping images
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
