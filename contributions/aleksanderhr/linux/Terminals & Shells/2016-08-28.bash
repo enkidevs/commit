239e1bde6a814acfb5ca2a1e268cec2599bdee86
aleksanderhr <ahr@karemedia.dk>
2016-08-28 07:43:42 +0100

    2016-08-28's linux workout

---
+++ contributions/aleksanderhr/linux/Terminals & Shells/2016-08-28.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Useful aliasing in bash
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
