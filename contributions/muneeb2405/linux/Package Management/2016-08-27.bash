9b0903ea7c072ecf19a8135e571f0a4957a13597
muneeb2405 <smati_muneeb@hotmail.com>
2016-08-28 05:38:03 +0100

    2016-08-27's linux workout

---
+++ contributions/muneeb2405/linux/Package Management/2016-08-27.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+List installed packages
+Installing a `.deb` package from the terminal
+Get `apt` to use a mirror / faster mirror
+Keeping repos updated with `cron`
