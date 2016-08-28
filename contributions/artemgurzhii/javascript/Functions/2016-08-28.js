ff3923c6a2f524202f220923abbb66106b6796ad
artemgurzhii <artem.gurzhii@gmail.com>
2016-08-28 15:17:04 +0100

    2016-08-28's javascript workout

---
+++ contributions/artemgurzhii/javascript/Functions/2016-08-28.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Function call method
+Move cursor at the end of text input
+Check if a document is done loading
