06e5b5a267dc0eface36c8d44e42a8893fe33e0d
ahilsend <andre.hilsendeger@gmail.com>
2016-08-28 07:21:13 +0100

    2016-08-28's linux workout

---
+++ contributions/ahilsend/linux/File Management/2016-08-28.bash
@@ -0,0 +1,7 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
