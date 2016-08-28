8fbd6f32b1f7eac8edcd27cb6ac82b90fd3f419e
buts101 <ashish.batajoo@gmail.com>
2016-08-28 16:33:30 +0100

    2016-08-27's linux workout

---
+++ contributions/buts101/linux/Terminals & Shells/2016-08-27.bash
@@ -0,0 +1,7 @@
+Shebang `#!` explained 
+Clear bash history
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
