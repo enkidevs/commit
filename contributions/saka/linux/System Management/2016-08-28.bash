e150b3483d5516d45deec8dc48e17fcbd467e24c
saka <saka71996@gmail.com>
2016-08-28 09:26:03 +0100

    2016-08-28's linux workout

---
+++ contributions/saka/linux/System Management/2016-08-28.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
