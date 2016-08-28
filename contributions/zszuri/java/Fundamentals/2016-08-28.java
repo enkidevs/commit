d53b2ca65dffc073ca4d0b1a9c7ff5a17d1b2c01
zszuri <szuri90@gmail.com>
2016-08-28 19:15:23 +0100

    2016-08-28's java workout

---
+++ contributions/zszuri/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
