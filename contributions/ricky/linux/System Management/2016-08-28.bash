bb0ef05a7873071a6a8afef789f09b9e6f84d610
ricky <riccardotonini@gmail.com>
2016-08-28 15:18:36 +0100

    2016-08-28's linux workout

---
+++ contributions/ricky/linux/System Management/2016-08-28.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
