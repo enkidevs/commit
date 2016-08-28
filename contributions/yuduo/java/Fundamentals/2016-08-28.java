3ccbb2983d0fb2bc049c55e4d788b69fb44ca1a7
yuduo <yuduozhou@gmail.com>
2016-08-28 18:25:51 +0100

    2016-08-28's java workout

---
+++ contributions/yuduo/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
