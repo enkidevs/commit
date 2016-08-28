b53abd1b903cb94ae86245292d19f4d44f662d80
julianchan <mrjulianchan@gmail.com>
2016-08-28 16:36:37 +0100

    2016-08-28's java workout

---
+++ contributions/julianchan/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Connecting to FTP using Java
+Distinction between `public` and `private` methods
+Using bounded type parameters in generic methods
