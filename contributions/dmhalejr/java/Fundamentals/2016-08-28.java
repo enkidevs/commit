8166b0e5a8f494c50121867545a8466053eadafa
dmhalejr <dmhalejr@gmail.com>
2016-08-28 14:30:52 +0100

    2016-08-28's java workout

---
+++ contributions/dmhalejr/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
