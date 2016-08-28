17f5fdc3eee7b2655d6394511739291c20cb7ed3
IronBeagle <noamhofshi@gmail.com>
2016-08-28 18:09:27 +0100

    2016-08-28's linux workout

---
+++ contributions/IronBeagle/linux/File Management/2016-08-28.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
