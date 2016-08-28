ae1e658a3b6e2afb5ee97843c2b67a4d624ccde1
aquincy <brennanjl003@gmail.com>
2016-08-28 14:16:51 +0100

    2016-08-28's linux workout

---
+++ contributions/aquincy/linux/File Management/2016-08-28.bash
@@ -0,0 +1,7 @@
+Finding file with regexes
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Using `touch` to alter files modification time
