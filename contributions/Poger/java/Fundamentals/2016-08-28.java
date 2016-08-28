08a7bbd4c46355fdd24bbbdd92ed0ecb0326986d
Poger <gkonupek@gmail.com>
2016-08-28 22:09:03 +0100

    2016-08-28's java workout

---
+++ contributions/Poger/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
