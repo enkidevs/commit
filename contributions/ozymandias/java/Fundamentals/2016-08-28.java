afef7f528bb4c6a4cd733d2f342f0909326a5b6d
ozymandias <stenbackfelix@gmail.com>
2016-08-28 23:27:20 +0100

    2016-08-28's java workout

---
+++ contributions/ozymandias/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
