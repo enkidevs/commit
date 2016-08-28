4f4344fed6394b195f4fba278abb8b930317b088
owdaro <kamran@owdaro.com>
2016-08-28 10:49:34 +0100

    2016-08-27's javascript workout

---
+++ contributions/owdaro/javascript/Functions/2016-08-27.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+`.bind()` 
+Function call method
+Changing a functions context with `fn.call(object)`
