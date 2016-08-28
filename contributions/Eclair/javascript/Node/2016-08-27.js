8b2c036713b05a54a198c08402f768b38384213d
Eclair <jenny.filcheck@gmail.com>
2016-08-28 06:39:23 +0100

    2016-08-27's javascript workout

---
+++ contributions/Eclair/javascript/Node/2016-08-27.js
@@ -0,0 +1,5 @@
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+Handle `JSON.parse` error in Node.js
+Manage child processes in **Node**
+Middleware/pipeline design pattern
