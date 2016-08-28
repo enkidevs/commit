aacd5e9a264794687961d75d21625844dd92bf37
lexf <aleksf87@gmail.com>
2016-08-28 21:12:07 +0100

    2016-08-28's linux workout

---
+++ contributions/lexf/linux/Terminals & Shells/2016-08-28.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Random password generator
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
