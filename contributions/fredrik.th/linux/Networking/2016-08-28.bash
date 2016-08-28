15db7e92bb3c5cab8da0393106a35768a707bf24
fredrik.th <fredrik.thunberg@gmail.com>
2016-08-28 18:52:46 +0100

    2016-08-28's linux workout

---
+++ contributions/fredrik.th/linux/Networking/2016-08-28.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+How to clear `iptables` rules
+List your IPv6 configuration
+Sending emails from terminal / scripts
+Staying connected remotely via SSH without an interactive shell
