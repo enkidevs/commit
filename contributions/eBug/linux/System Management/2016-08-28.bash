fe18d18b7ac69e522fcb00733dd889b64b39ded6
eBug <ericvb69@gmail.com>
2016-08-28 17:56:56 +0100

    2016-08-28's linux workout

---
+++ contributions/eBug/linux/System Management/2016-08-28.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Basic service management with `systemd`
