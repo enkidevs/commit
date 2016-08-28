8a5a080954ba2cc914861d0d8bbc69563746dd5f
mwender <michael@michaelwender.com>
2016-08-28 17:29:15 +0100

    2016-08-28's linux workout

---
+++ contributions/mwender/linux/System Management/2016-08-28.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
