eb7d8982366a892ffb2ab6265c339b0ac327225d
LukeC <luke.costi@gmail.com>
2016-08-28 09:31:59 +0100

    2016-08-28's javascript workout

---
+++ contributions/LukeC/javascript/Core/2016-08-28.js
@@ -0,0 +1,5 @@
+Hoisting applies only to variable declarations, not initializations
+Avoid using `with`
+Prototype methods
+Extra function arguments are undefined by default
+Detect online connection
