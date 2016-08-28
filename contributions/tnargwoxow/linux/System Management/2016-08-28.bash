4874f80b207084703b4b7e20adb77f8bbbaa873a
tnargwoxow <tnargwoxow@gmail.com>
2016-08-29 00:26:51 +0100

    2016-08-28's linux workout

---
+++ contributions/tnargwoxow/linux/System Management/2016-08-28.bash
@@ -0,0 +1,7 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
