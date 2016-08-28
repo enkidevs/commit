da3cca09aabe4b62f20d45184cb41f44d563c3c1
xazax <johan.daniel.nystrom@gmail.com>
2016-08-28 09:04:46 +0100

    2016-08-28's linux workout

---
+++ contributions/xazax/linux/File Management/2016-08-28.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Deleting non-empty directories
