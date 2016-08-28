5c91991a0ec1600d0af076cbfc83e2d012579d35
k4rtik <kartiksinghal@gmail.com>
2016-08-28 21:11:13 +0100

    2016-08-28's java workout

---
+++ contributions/k4rtik/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
