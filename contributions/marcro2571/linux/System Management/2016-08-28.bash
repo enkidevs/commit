ea9f1168e222504e4de92f997d73bff1813fc28c
marcro2571 <marc@robards.org>
2016-08-28 17:04:49 +0100

    2016-08-28's linux workout

---
+++ contributions/marcro2571/linux/System Management/2016-08-28.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Cancel a system shutdown
