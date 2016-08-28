d3441fffb97f7dc4c3d0b6ee42be01c6164dfadf
barisozcelik <barisozcelikmail@gmail.com>
2016-08-28 09:08:37 +0100

    2016-08-28's java workout

---
+++ contributions/barisozcelik/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Cropping images
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
