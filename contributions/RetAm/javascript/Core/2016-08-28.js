243ada5d8755cb68e43a3180e1ec8f8db8a8dd67
RetAm <zaitsev_art@inbox.ru>
2016-08-28 16:15:35 +0100

    2016-08-28's javascript workout

---
+++ contributions/RetAm/javascript/Core/2016-08-28.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Hoisting applies only to variable declarations, not initializations
+Prototype methods
+Getting the *real* dimensions of an image
+Round numbers to `N` decimals 
