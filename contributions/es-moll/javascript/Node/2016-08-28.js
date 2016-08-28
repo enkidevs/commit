e35e2306ec577254d5d3e5af8c6e5dda9b3b50d3
es-moll <es-moll@inbox.ru>
2016-08-28 18:05:40 +0100

    2016-08-28's javascript workout

---
+++ contributions/es-moll/javascript/Node/2016-08-28.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+`process.nextTick()` callback pattern
+Automatic compilation for Node with **Nodemon** 
+`try-catch` only for **sync** code
