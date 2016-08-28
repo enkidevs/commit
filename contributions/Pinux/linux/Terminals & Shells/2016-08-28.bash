f73df855165d087933258158bdd28ee680401384
Pinux <peppe98.marino@gmail.com>
2016-08-28 10:52:53 +0100

    2016-08-28's linux workout

---
+++ contributions/Pinux/linux/Terminals & Shells/2016-08-28.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
