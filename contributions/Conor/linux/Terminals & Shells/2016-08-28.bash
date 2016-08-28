8b3cb688e86df796e2df264ff17167b41a517e35
Conor <zeus112233@gmail.com>
2016-08-28 10:43:27 +0100

    2016-08-28's linux workout

---
+++ contributions/Conor/linux/Terminals & Shells/2016-08-28.bash
@@ -0,0 +1,5 @@
+Terminal based browser
+Random password generator
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
