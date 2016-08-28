74ecc0db56844d56e4b47244468f474f53dfc47e
cpthappy <jgvogelhuber@gmail.com>
2016-08-28 15:35:40 +0100

    2016-08-28's java workout

---
+++ contributions/cpthappy/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
