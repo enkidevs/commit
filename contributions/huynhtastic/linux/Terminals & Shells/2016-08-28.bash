f2879130fae05670efc121c936459944b3dcf10c
huynhtastic <huynhtastic@gmail.com>
2016-08-28 17:39:07 +0100

    2016-08-28's linux workout

---
+++ contributions/huynhtastic/linux/Terminals & Shells/2016-08-28.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Terminal incognito mode
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
