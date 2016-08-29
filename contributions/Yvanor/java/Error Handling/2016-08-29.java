92baf584c009c97f1ecf876cc54bd96c50fe9fbc
Yvanor <maxime.yusei@orange.fr>
2016-08-29 11:33:33 +0100

    2016-08-29's java workout

---
+++ contributions/Yvanor/java/Error Handling/2016-08-29.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Careful Numeric Data Conversions
