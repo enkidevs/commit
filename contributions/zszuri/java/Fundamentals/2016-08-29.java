e8cddcb8007fc0efb424bdc78e9d4f5e22495bef
zszuri <szuri90@gmail.com>
2016-08-29 08:35:03 +0100

    2016-08-29's java workout

---
+++ contributions/zszuri/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+Cropping images
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
