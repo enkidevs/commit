5d781d1b4fee8be3b2945cc81e5333cf25aa8674
dmhalejr <dmhalejr@gmail.com>
2016-08-29 14:13:21 +0100

    2016-08-29's java workout

---
+++ contributions/dmhalejr/java/Java 8/2016-08-29.java
@@ -0,0 +1,5 @@
+Java 8 Lambda expressions
+Using `Arrays.parallelSort` 
+Use `Optional(s)` type in order to avoid `NullPointerException(s)`
+Avoid numerical overflows with *Java8* `exact` methods.
+`JJS` - Oracle Nashorn, newest JavaScript Engine
