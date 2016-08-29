89090525e5c448de602f0383f0a2f9323ddcdabb
Partyk_96 <kyrtapanjub@gmail.com>
2016-08-29 14:13:23 +0100

    2016-08-29's javascript workout

---
+++ contributions/Partyk_96/javascript/Core/2016-08-29.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Performing a function at timed intervals
+Use `splice()` to remove an item from an array
+Prevent background image flicker in **IE**
+Detect online connection
