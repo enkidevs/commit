0a19dbbe7bbc9cb6bc6f27d32149341a6d5fae77
TM1989 <torben.moeller@gmx.net>
2016-08-29 08:06:20 +0100

    2016-08-29's java workout

---
+++ contributions/TM1989/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+The distinction between overloading and overriding methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
