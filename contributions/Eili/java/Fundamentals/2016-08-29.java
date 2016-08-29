776a1c16cf080b0b52115e27e3390599d4a8bcff
Eili <eilenberger.dominik@gmail.com>
2016-08-29 05:43:27 +0100

    2016-08-29's java workout

---
+++ contributions/Eili/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Using the `Console`
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
