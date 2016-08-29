4b2f36d2c912f4b0eb420e7128c42b6a3bdf4855
Rocket3G <saverkuil@gmail.com>
2016-08-29 11:24:30 +0100

    2016-08-29's linux workout

---
+++ contributions/Rocket3G/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Sequential execution using the `;` statement separator
