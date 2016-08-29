1f36b022202a93a153050d4e4cae26c008ea188c
ritwickdsouza <ritwick.dsouza@outlook.com>
2016-08-29 22:09:48 +0100

    2016-08-28's linux workout

---
+++ contributions/ritwickdsouza/linux/Terminals & Shells/2016-08-28.bash
@@ -0,0 +1,7 @@
+Quick `bash` shortcuts 
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
