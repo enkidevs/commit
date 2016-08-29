da219d3bdb8752c412f3c138970175d11ac6807f
samircaus <samir.caus@gmail.com>
2016-08-29 07:58:18 +0100

    2016-08-29's linux workout

---
+++ contributions/samircaus/linux/File Management/2016-08-29.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
