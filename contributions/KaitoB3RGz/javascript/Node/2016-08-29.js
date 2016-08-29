00c10179e54be2fe12c1ce5c3c21d2282478cf78
KaitoB3RGz <leandro.bergmann@hotmail.com>
2016-08-29 23:51:06 +0100

    2016-08-29's javascript workout

---
+++ contributions/KaitoB3RGz/javascript/Node/2016-08-29.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+First-error callbacks in **Node**
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
