cae518cdd763231b4b006a6a0a45fc030e0ff541
ivannieto <ivan.n.s@tuta.io>
2016-08-29 11:05:36 +0100

    2016-08-29's java workout

---
+++ contributions/ivannieto/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Distinction between `public` and `private` methods
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
