3eabd3570072a3484852076ca1d3937bf06df379
kheort <khemzys@gmail.com>
2016-08-29 19:58:16 +0100

    2016-08-29's linux workout

---
+++ contributions/kheort/linux/System Management/2016-08-29.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
