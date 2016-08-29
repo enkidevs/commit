625fc3f5f249055b64ce97f824deaa2970dd6ecb
fikolo <filipdalorenzo@gmail.com>
2016-08-29 07:56:27 +0100

    2016-08-29's java workout

---
+++ contributions/fikolo/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
