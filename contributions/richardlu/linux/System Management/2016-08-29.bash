1a72255518cbaadf358c0f9b417ef37464dc5d48
richardlu <richard.wh.lu@gmail.com>
2016-08-29 17:00:25 +0100

    2016-08-29's linux workout

---
+++ contributions/richardlu/linux/System Management/2016-08-29.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
+System uptime
