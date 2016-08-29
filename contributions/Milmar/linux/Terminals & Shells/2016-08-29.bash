3540326acd96c7633818d88e09137031afab3615
Milmar <milmarwoot@gmail.com>
2016-08-29 03:32:16 +0100

    2016-08-29's linux workout

---
+++ contributions/Milmar/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
