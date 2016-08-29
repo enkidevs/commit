99c9c659aabe92ef1abea16f88ae41aa17d8b5b1
chrisdarden <c.darden@me.com>
2016-08-29 04:49:37 +0100

    2016-08-28's java workout

---
+++ contributions/chrisdarden/java/Error Handling/2016-08-28.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Use try-with-resources instead of `finally`
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Checking a double for `NaN` value
