f758e5c3be6df577dedbe50ee2b04c6999a92d74
ditzdeagos <ditzdragos@gmail.com>
2016-08-29 08:20:55 +0100

    2016-08-29's java workout

---
+++ contributions/ditzdeagos/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Supply `toString()` in all classes
