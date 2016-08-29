f93255b70815f6a8b7a55bbc9e45b47a892d3f56
VeloX <a.freris@gmail.com>
2016-08-29 23:07:18 +0100

    2016-08-29's java workout

---
+++ contributions/VeloX/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
