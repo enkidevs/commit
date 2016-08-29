6df5f426bff1ed81774b7a665d51edae18a54bfb
Chomsky <omca16@gmail.com>
2016-08-29 08:18:32 +0100

    2016-08-29's java workout

---
+++ contributions/Chomsky/java/Data Types/2016-08-29.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Collections vs arrays
+Do not attempt comparisons with NaN
