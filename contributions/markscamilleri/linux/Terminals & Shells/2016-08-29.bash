6ecc5d9c27b5ae531b1362acdacf54154f0c41ae
markscamilleri <markscamilleri@gmail.com>
2016-08-30 00:50:16 +0100

    2016-08-29's linux workout

---
+++ contributions/markscamilleri/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Terminal based browser
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
