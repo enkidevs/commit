581ffd2a764318b3b11b27acbe13362f3ff0e934
nitr0 <federico.hendrix@gmail.com>
2016-08-29 08:47:38 +0100

    2016-08-29's linux workout

---
+++ contributions/nitr0/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Shebang `#!` explained 
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
