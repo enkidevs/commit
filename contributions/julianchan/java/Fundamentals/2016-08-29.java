fa29db2a150e676e066ab7600384eb0b2a6d144c
julianchan <mrjulianchan@gmail.com>
2016-08-29 13:58:13 +0100

    2016-08-29's java workout

---
+++ contributions/julianchan/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use meaningful names
+The trade-off between short and descriptive names
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
