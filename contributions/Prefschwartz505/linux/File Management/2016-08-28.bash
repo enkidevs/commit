fedffeab62259b390280b2283cf587d119c0eb4d
Prefschwartz505 <fx_schwartz505@yahoo.com>
2016-08-29 03:03:49 +0100

    2016-08-28's linux workout

---
+++ contributions/Prefschwartz505/linux/File Management/2016-08-28.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
