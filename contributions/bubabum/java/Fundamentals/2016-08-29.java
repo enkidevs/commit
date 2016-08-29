e298271afce8e6c27d4facc4233c186b850ed21e
bubabum <andddre@bol.com.br>
2016-08-29 18:06:59 +0100

    2016-08-29's java workout

---
+++ contributions/bubabum/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Altering format string output by changing a format specifier's `argument_index`
+Distinction between `public` and `private` methods
+Use `DecimalFormat` class to format numbers
+Limit Accessibility of `Fields`
