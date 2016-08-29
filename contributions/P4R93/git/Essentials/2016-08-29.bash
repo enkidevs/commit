a89474e5080c20b751b02a47d904a7a479855212
P4R93 <p4r.esp@gmail.com>
2016-08-29 21:10:36 +0100

    2016-08-29's git workout

---
+++ contributions/P4R93/git/Essentials/2016-08-29.bash
@@ -0,0 +1,5 @@
+Finding a tag
+Search by commit message keyword
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
