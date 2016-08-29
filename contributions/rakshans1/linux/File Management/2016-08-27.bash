161eaf70883f0bb1e34f37c20124ec85ba6690b7
rakshans1 <shetty.raxx555@gmail.com>
2016-08-29 12:39:17 +0100

    2016-08-27's linux workout

---
+++ contributions/rakshans1/linux/File Management/2016-08-27.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
