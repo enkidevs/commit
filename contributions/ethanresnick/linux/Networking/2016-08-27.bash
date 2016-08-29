f0e6e470ffc87c911d9ff4a96920c7e3a687dc31
ethanresnick <ethan.resnick@gmail.com>
2016-08-29 06:18:27 +0100

    2016-08-27's linux workout

---
+++ contributions/ethanresnick/linux/Networking/2016-08-27.bash
@@ -0,0 +1,5 @@
+Find services running on your host
+Useful ssh client optimizations 
+List your IPv6 configuration
+Setting up password-free authentication
+Getting the connection speed from the terminal
