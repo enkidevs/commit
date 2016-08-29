609698b67a1b3ff1ba6d3e0d96b370bd3ca1e1cb
vseehua <vseehua@hotmail.com>
2016-08-29 04:22:40 +0100

    2016-08-28's linux workout

---
+++ contributions/vseehua/linux/System Management/2016-08-28.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Cancel a system shutdown
