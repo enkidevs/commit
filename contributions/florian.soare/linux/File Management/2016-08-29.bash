7f1f961528dc517038d8ad10e784ac15e9d4f3f1
florian.soare <florian.soare@takmate.com>
2016-08-29 07:47:29 +0100

    2016-08-29's linux workout

---
+++ contributions/florian.soare/linux/File Management/2016-08-29.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
