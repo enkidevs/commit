f74830426abd702f35d792668328b5da050e692b
lukas_hudec <l.hudec75@gmail.com>
2016-08-29 23:12:13 +0100

    2016-08-29's linux workout

---
+++ contributions/lukas_hudec/linux/File Management/2016-08-29.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
