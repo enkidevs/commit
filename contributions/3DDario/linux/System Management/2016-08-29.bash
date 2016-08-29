4640c8942e4382a4986274a6c4c2f2934cc1a14e
3DDario <delledonnedario@gmail.com>
2016-08-29 09:14:48 +0100

    2016-08-29's linux workout

---
+++ contributions/3DDario/linux/System Management/2016-08-29.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
