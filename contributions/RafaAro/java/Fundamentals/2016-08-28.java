035e4d405bb73c11a9a676c2ff1c1e98f00b4498
RafaAro <rafaaro@arobytestudios.com>
2016-08-29 03:20:20 +0100

    2016-08-28's java workout

---
+++ contributions/RafaAro/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
