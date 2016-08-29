478de2e6bf0d33ad998116040180f6f92411fa92
tal Jacobson <tal1989@gmail.com>
2016-08-29 17:58:31 +0100

    2016-08-29's javascript workout

---
+++ contributions/tal Jacobson/javascript/React/2016-08-29.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Clone Elements in **React**
+`ReactDOM.render` ref
+Default values for props
