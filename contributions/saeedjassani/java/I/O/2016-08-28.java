8499f1002880b5d74e8d74d4607ed2cbfb3d528b
saeedjassani <saeedjassani@gmail.com>
2016-08-29 11:45:26 +0100

    2016-08-28's java workout

---
+++ contributions/saeedjassani/java/I/O/2016-08-28.java
@@ -0,0 +1,5 @@
+Using buffered streams
+Checking for the existence of files and directories
+Reading file attributes
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
