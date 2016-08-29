34d49b5c3896d65460e26cc03d1c593f91785bac
raghu <raghu.9208@gmail.com>
2016-08-29 05:43:22 +0100

    2016-08-28's java workout

---
+++ contributions/raghu/java/Fundamentals/2016-08-28.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Using the `Console`
+Distinction between `public` and `private` methods
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
