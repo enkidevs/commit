45daf6975962a2f438e52261ba8d9744aeb80e64
s2mike <sai.sasidhar92@gmail.com>
2016-08-29 22:46:05 +0100

    2016-08-29's linux workout

---
+++ contributions/s2mike/linux/System Management/2016-08-29.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Cancel a system shutdown
