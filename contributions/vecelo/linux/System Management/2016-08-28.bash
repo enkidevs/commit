ceff26a4dabb5ef747fed7f5accc57bfe3bf913a
vecelo <hungmt@outlook.com>
2016-08-29 04:58:08 +0100

    2016-08-28's linux workout

---
+++ contributions/vecelo/linux/System Management/2016-08-28.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
