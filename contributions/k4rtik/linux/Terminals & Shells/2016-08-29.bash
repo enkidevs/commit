bc8f510d74619afb32de78bead5ab8799c5924bd
k4rtik <kartiksinghal@gmail.com>
2016-08-29 21:22:54 +0100

    2016-08-29's linux workout

---
+++ contributions/k4rtik/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Aliasing ssh connections
+Quick `bash` shortcuts 
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
