16ac8af05eb952a8cfecf00c8d9dde584b3334c9
nedel <nedel@gmx.com>
2016-08-29 05:37:55 +0100

    2016-08-29's python workout

---
+++ contributions/nedel/python/Data Structures/2016-08-29.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Immutable sets with `frozenset`
+Special `queue` methods
+`weakref` proxies
+Binary record packing with `bytearray`
