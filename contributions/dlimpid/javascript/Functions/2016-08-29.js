ff8a1cff3e11fc5b5af9eacd936f463020c9bfbf
dlimpid <dlimpid@gmail.com>
2016-08-30 00:43:52 +0100

    2016-08-29's javascript workout

---
+++ contributions/dlimpid/javascript/Functions/2016-08-29.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Get query/url variables
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
