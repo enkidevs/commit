7b29585adb27cb9bbac041f9e15898ca2681d91e
jcarneiro <jvmcarneiro@openmailbox.org>
2016-08-29 14:22:12 +0100

    2016-08-29's linux workout

---
+++ contributions/jcarneiro/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Shebang `#!` explained 
+Execute a command without saving it in history
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
