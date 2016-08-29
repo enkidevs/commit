4c741286378abd824a8bb4c7561f38901e7b326e
_l0stfake7 <bartek.poniatyszyn@gmail.com>
2016-08-29 13:01:10 +0100

    2016-08-29's java workout

---
+++ contributions/_l0stfake7/java/Error Handling/2016-08-29.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Creating new Exceptions
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
