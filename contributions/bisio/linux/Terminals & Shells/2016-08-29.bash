a62b77564ca708f7221ad50aeac3a7eba9878149
bisio <andrea.bisognin@gmail.com>
2016-08-29 06:31:36 +0100

    2016-08-29's linux workout

---
+++ contributions/bisio/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Terminal based browser
+Random password generator
+Get to know  your commands with `type`
