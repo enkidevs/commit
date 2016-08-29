f080854b57dcdd83ef3ce7746761151eed7b6dfa
aprilfool369 <aprilfool369@gmail.com>
2016-08-29 12:18:24 +0100

    2016-08-28's linux workout

---
+++ contributions/aprilfool369/linux/Terminals & Shells/2016-08-28.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
