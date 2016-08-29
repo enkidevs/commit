3793c5700bef360775a6ae755de70a7129657fe3
juduth <juduth@gmail.com>
2016-08-29 01:42:14 +0100

    2016-08-28's linux workout

---
+++ contributions/juduth/linux/System Management/2016-08-28.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Cancel a system shutdown
