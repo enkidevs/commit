716235771da844f653bd96187b26f16931de2512
zflopez <zaida.flopez@gmail.com>
2016-08-29 10:00:08 +0100

    2016-08-29's javascript workout

---
+++ contributions/zflopez/javascript/Functions/2016-08-29.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Anonymous functions
+IIFE pattern
