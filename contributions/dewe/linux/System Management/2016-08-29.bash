1a5d4abe015f7853d90f09cf0bf72374cd9c6171
dewe <johan@dewe.net>
2016-08-29 06:44:23 +0100

    2016-08-29's linux workout

---
+++ contributions/dewe/linux/System Management/2016-08-29.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Removing old kernels in Debian based systems
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
