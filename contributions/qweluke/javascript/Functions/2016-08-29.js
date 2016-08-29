f20923bd32cd5e391239dd7fd417341df31d7ee3
qweluke <maalick.i@gmail.com>
2016-08-29 08:58:41 +0100

    2016-08-29's javascript workout

---
+++ contributions/qweluke/javascript/Functions/2016-08-29.js
@@ -0,0 +1,5 @@
+Hoisting
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Function call method
+Changing a functions context with `fn.call(object)`
