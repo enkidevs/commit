e6de3c2d1ae24b5ba71d15dc8e345e5bd67205c6
cryptichorizon <cryptichorizon@gmail.com>
2016-08-29 06:56:46 +0100

    2016-08-29's java workout

---
+++ contributions/cryptichorizon/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
