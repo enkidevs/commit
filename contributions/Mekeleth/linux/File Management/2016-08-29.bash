9dd774c410c73b2e06fed8b35373f99c8bd507a2
Mekeleth <garadim12@gmail.com>
2016-08-29 09:30:05 +0100

    2016-08-29's linux workout

---
+++ contributions/Mekeleth/linux/File Management/2016-08-29.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
