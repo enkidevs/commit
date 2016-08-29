fb4d43b2f75197d3bf4dcf5adb54a17597899b68
vano <boris.avney@imperva.com>
2016-08-29 07:35:35 +0100

    2016-08-29's java workout

---
+++ contributions/vano/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Ternary operator
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
