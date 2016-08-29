6b1fcabc02ac129f0ca1c21ae1357c725da07d5f
mat777 <mat.wie.7@gmail.com>
2016-08-29 18:19:07 +0100

    2016-08-29's java workout

---
+++ contributions/mat777/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
