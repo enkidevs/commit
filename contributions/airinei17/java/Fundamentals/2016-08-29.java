c3cc37eab8dd5ebbcdf27a41f9374c79de5782c5
airinei17 <airinei17@gmail.com>
2016-08-29 11:53:40 +0100

    2016-08-29's java workout

---
+++ contributions/airinei17/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
