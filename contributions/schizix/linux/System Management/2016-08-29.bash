b6fc56eba256152db2dfacca26fa566a562af723
schizix <atodorovic@gmail.com>
2016-08-29 17:57:02 +0100

    2016-08-29's linux workout

---
+++ contributions/schizix/linux/System Management/2016-08-29.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
