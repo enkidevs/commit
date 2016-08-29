675307d29a0b33c26a28f56a782f73236db405c5
zr2d2 <zrowe007@gmail.com>
2016-08-29 12:58:29 +0100

    2016-08-29's javascript workout

---
+++ contributions/zr2d2/javascript/Npm/2016-08-29.js
@@ -0,0 +1,5 @@
+Manipulate npm packages cache
+Calling remote scripts with npm
+npm can handle multiple versions of a dependency
+`optionalDependencies` in npm
+Lock down dependency versions by shrinkwrapping
