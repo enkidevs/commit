628dea531460fa9d7074681b0362db9f1c55a277
mgarrett <melissa_garrett@hotmail.com>
2016-08-29 14:51:02 +0100

    2016-08-29's javascript workout

---
+++ contributions/mgarrett/javascript/React/2016-08-29.js
@@ -0,0 +1,6 @@
+Pixel values in style props for **React**
+Props in getInitialState Is an        
+Anti-Pattern
+Load Initial Data via AJAX
+Clone Elements in **React**
+Shortcut for transferring props
