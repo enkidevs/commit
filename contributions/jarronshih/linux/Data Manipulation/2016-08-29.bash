bf5128e68f386816a85197a2e0c8757d08444885
jarronshih <jarronshih@gmail.com>
2016-08-29 22:16:04 +0100

    2016-08-29's linux workout

---
+++ contributions/jarronshih/linux/Data Manipulation/2016-08-29.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+How to analyse a server's log files
+Using `lynx` to convert HTML to text
+Image manipulation  using `convert`
+Wrap text with `fold`
