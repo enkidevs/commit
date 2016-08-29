3abe3a8e9145578d8b95ba39d4f15672da0f7cf5
mattspaeth <matt.spaeth@gmail.com>
2016-08-29 15:36:18 +0100

    2016-08-29's linux workout

---
+++ contributions/mattspaeth/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Aliasing ssh connections
+Get to know  your commands with `type`
