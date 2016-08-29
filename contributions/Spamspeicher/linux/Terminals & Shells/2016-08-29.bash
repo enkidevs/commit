9af2bb35940a45863e279ca1a0c603e5470dfc79
Spamspeicher <drinsane90@gmail.com>
2016-08-29 14:51:09 +0100

    2016-08-29's linux workout

---
+++ contributions/Spamspeicher/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,7 @@
+Find any Unix / Linux command
+Terminal incognito mode
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
