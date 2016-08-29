f617695d3dd582aecba8683f3dcf52a4535bde18
Zbysek <zbysek.vacek@gmail.com>
2016-08-29 06:55:24 +0100

    2016-08-29's linux workout

---
+++ contributions/Zbysek/linux/File Management/2016-08-29.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
