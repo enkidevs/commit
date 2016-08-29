7e82c01bfc6b954b15f2fb49ec8ea8daac6c6da5
olappi <oj.lappi@gmail.com>
2016-08-29 05:16:03 +0100

    2016-08-29's javascript workout

---
+++ contributions/olappi/javascript/React/2016-08-29.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+Type of the Children props
+componentWillReceiveProps Not Triggered After Mounting
+
+Expose Component Functions in **React**
+Use **React** with other libraries
