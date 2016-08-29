d2eded3a1f3e800ce5368ab7bbb1b461bd5c1ac3
flanags5 <stephen.flanagan5@gmail.com>
2016-08-29 09:57:09 +0100

    2016-08-29's linux workout

---
+++ contributions/flanags5/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Clear the terminal instantly
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
