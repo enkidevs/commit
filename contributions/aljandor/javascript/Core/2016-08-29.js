86d09d1cf0d8f6622d13d02dd3f4176d8dd51fce
aljandor <radziejewiczmichal@gmail.com>
2016-08-29 13:48:41 +0100

    2016-08-29's javascript workout

---
+++ contributions/aljandor/javascript/Core/2016-08-29.js
@@ -0,0 +1,5 @@
+Data type comparison in `switch` statements
+Infix operators are left-associative
+Functions return `undefined` by default
+Prevent background image flicker in **IE**
+Detect **DO NOT TRACK** status
