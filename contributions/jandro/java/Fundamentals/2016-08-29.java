381eb349ff7b353a61b12f266bd47c7cf3e5cdba
jandro <malejandrofc@gmail.com>
2016-08-29 22:06:30 +0100

    2016-08-29's java workout

---
+++ contributions/jandro/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
+Using an interface as a parameter
