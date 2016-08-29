ee62eca145ace4e49ee78dc3f360a6474cbb6f7d
VegOtter <andriesphone@gmail.com>
2016-08-29 09:07:55 +0100

    2016-08-29's git workout

---
+++ contributions/VegOtter/git/Tools/2016-08-29.bash
@@ -0,0 +1,5 @@
+Let's play the blame game
+Cache your authentication details to save time
+Specify a range of commits using double dot syntax
+Create a new branch from a stash
+Show history of a function
