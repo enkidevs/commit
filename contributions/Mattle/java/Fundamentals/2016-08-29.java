c5091ffb4c7fa1cc70b8a474fbca716821f11538
Mattle <mcallier@gmail.com>
2016-08-29 16:58:46 +0100

    2016-08-29's java workout

---
+++ contributions/Mattle/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The trade-off between short and descriptive names
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
