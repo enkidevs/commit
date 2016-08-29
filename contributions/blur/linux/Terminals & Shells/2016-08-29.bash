197c680f9daadd4fe1b2a4339929f1a376cf6745
blur <filip@munk.pro>
2016-08-29 08:10:10 +0100

    2016-08-29's linux workout

---
+++ contributions/blur/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
