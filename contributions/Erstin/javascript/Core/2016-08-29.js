c7695e507d23cf0d99f834bbc38c1fadd94aa718
Erstin <ced.magno@gmail.com>
2016-08-29 07:14:17 +0100

    2016-08-29's javascript workout

---
+++ contributions/Erstin/javascript/Core/2016-08-29.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Using the double tilde `~~` 
+Most efficient way to build `HTML` strings
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
