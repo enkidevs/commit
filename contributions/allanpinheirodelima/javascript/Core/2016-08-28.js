240af14fda3c3d889bf3eac268255be369fcd5d5
allanpinheirodelima <allanpinheirodelima@gmail.com>
2016-08-29 03:37:10 +0100

    2016-08-28's javascript workout

---
+++ contributions/allanpinheirodelima/javascript/Core/2016-08-28.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+Extra function arguments are undefined by default
+Detect **DO NOT TRACK** status
+Inaccuracy of binary floating-point format
+Base conversion 
