a806231c2107d09ec42fca946eea70e39ef6fa21
Slyh <rudy.canesse@insa-cvl.fr>
2016-08-29 17:34:49 +0100

    2016-08-29's linux workout

---
+++ contributions/Slyh/linux/System Management/2016-08-29.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Executing scripts on system boot using `systemd`
+Finding Open Files With `lsof`
