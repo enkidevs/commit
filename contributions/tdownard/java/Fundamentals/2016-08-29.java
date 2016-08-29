5181a818d286141c0220b0a530091f8a35ac6676
tdownard <tom.downard.2011@bristol.ac.uk>
2016-08-29 09:42:10 +0100

    2016-08-29's java workout

---
+++ contributions/tdownard/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
+Using an interface as a parameter
