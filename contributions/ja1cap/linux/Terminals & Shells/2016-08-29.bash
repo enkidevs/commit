687c32444d5a8a89d4f3b1f5a5494d7de9f0bb5f
ja1cap <ja1cao@weasty.com>
2016-08-29 21:52:59 +0100

    2016-08-29's linux workout

---
+++ contributions/ja1cap/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
