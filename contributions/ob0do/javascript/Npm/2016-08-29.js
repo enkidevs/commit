5334a938986f639d002bec76a604d00d1c140e21
ob0do <fr3drik.l@gmail.com>
2016-08-29 07:23:50 +0100

    2016-08-29's javascript workout

---
+++ contributions/ob0do/javascript/Npm/2016-08-29.js
@@ -0,0 +1,5 @@
+Bump package version in npm
+npm can handle multiple versions of a dependency
+Manage local node modules with `npm link`
+Environmental variables and flags for `npm`
+Current Lifecycle Event
