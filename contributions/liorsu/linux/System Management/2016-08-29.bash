5e99aa7a5f9c8438d08b2b1096a6af2155d33a63
liorsu <liorsu@gmail.com>
2016-08-29 18:07:12 +0100

    2016-08-29's linux workout

---
+++ contributions/liorsu/linux/System Management/2016-08-29.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Common log files
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
