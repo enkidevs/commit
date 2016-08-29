d926ca5e33317388c224e41921bcccc1ccf59d35
DacoDC <danncorbalan@gmail.com>
2016-08-29 14:36:25 +0100

    2016-08-29's linux workout

---
+++ contributions/DacoDC/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Clear the terminal instantly
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
