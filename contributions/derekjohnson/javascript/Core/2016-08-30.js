1c91ad733d1a65de89c9d7920bdd38db1b4b7f91
derekjohnson <derek@nostrongbeliefs.com>
2016-08-30 10:06:15 +0100

    2016-08-30's javascript workout

---
+++ contributions/derekjohnson/javascript/Core/2016-08-30.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Infinity
+Hoisting applies only to variable declarations, not initializations
+`catch` is block scoped
+Extra function arguments are undefined by default
