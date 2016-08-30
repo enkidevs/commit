e5662a7f31b3742a1236e3fbc03046562291fc3e
david.midkiff <david.andrew.midkiff@gmail.com>
2016-08-31 00:25:15 +0100

    2016-08-30's linux workout

---
+++ contributions/david.midkiff/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Clear bash history
+Terminal based browser
+Keep useful commands in your shell history with tags
