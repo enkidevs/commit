f8136ede43d24c26f0764d1cf1f988b1297c665b
le_idiot <siddhesh.mhadnak@gmail.com>
2016-08-30 12:02:14 +0100

    2016-08-29's linux workout

---
+++ contributions/le_idiot/linux/System Management/2016-08-29.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
