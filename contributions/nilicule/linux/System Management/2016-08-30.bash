c779a76ae6956890885d632976c4a242a2c0f069
nilicule <nilicule@gmail.com>
2016-08-30 07:04:25 +0100

    2016-08-30's linux workout

---
+++ contributions/nilicule/linux/System Management/2016-08-30.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Common log files
+Incorrect time on dual boot systems
+Basic service management with `systemd`
