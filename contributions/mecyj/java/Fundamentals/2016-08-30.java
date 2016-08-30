84b6d19b033c004f2b5294209976b4b051461f69
mecyj <dominik.sze@gmail.com>
2016-08-30 07:40:20 +0100

    2016-08-30's java workout

---
+++ contributions/mecyj/java/Fundamentals/2016-08-30.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+How to play a sound file in Java
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Increase `PermGen` space as to avoid `OutOfMemory` errors
