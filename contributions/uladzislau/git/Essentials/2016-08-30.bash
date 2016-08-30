b8fa06b65b1ffcccb00d7004ac0de60441926bf2
uladzislau <vlad.vinnikov4@gmail.com>
2016-08-30 19:04:41 +0100

    2016-08-30's git workout

---
+++ contributions/uladzislau/git/Essentials/2016-08-30.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+The three states in git
+How to write a git commit message
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
