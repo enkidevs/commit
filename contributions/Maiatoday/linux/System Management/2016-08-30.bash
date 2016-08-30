16f3277b1e3821bb07773d1e6b5bf5e3677b408c
Maiatoday <maiatoday@gmail.com>
2016-08-30 20:56:32 +0100

    2016-08-30's linux workout

---
+++ contributions/Maiatoday/linux/System Management/2016-08-30.bash
@@ -0,0 +1,5 @@
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Cancel a system shutdown
