a75cf502c177d56ad8493ffa9f638af3dd3cdcf1
e7krgc <enam93@gmail.com>
2016-08-30 07:05:23 +0100

    2016-08-30's java workout

---
+++ contributions/e7krgc/java/Fundamentals/2016-08-30.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+How to play a sound file in Java
+Uses of the `final` keyword
+The trade-off between short and descriptive names
+Common mistake on switch statements
