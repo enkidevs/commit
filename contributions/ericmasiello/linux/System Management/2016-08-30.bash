344a7a8d1d9185e51c1c3232778caa7e77beedd8
ericmasiello <ericjmemail@gmail.com>
2016-08-30 12:19:26 +0100

    2016-08-30's linux workout

---
+++ contributions/ericmasiello/linux/System Management/2016-08-30.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
