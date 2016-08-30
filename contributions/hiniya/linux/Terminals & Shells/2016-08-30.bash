b8922d23bd1e90a67cef752d0d9b70a1c41bbebb
hiniya <hiniya@votbs.com>
2016-08-30 17:52:23 +0100

    2016-08-30's linux workout

---
+++ contributions/hiniya/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Find any Unix / Linux command
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
