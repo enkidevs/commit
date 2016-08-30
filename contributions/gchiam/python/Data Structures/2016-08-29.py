82b638da9dd303e4d7817c8cb1013ce0f49c9e48
gchiam <gordon.chiam@gmail.com>
2016-08-30 01:54:18 +0100

    2016-08-29's python workout

---
+++ contributions/gchiam/python/Data Structures/2016-08-29.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Double ended queues with `deque`
+Get the most of `float`s
+Operations with `bytes` and `bytearray`
+Implementing **weak** references
