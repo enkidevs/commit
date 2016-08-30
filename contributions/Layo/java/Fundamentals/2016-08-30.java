debe3548f3b0f4005061f046645212a31d6b4f55
Layo <thegamerult@googlemail.com>
2016-08-30 16:41:44 +0100

    2016-08-30's java workout

---
+++ contributions/Layo/java/Fundamentals/2016-08-30.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+There is no such thing as *pass-by-reference* in Java
