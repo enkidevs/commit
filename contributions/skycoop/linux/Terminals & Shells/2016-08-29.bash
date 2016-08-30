04b7993e7e613046d0244c251d277fbc96686164
skycoop <skycoop@gmail.com>
2016-08-30 02:45:57 +0100

    2016-08-29's linux workout

---
+++ contributions/skycoop/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Terminal based browser
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
