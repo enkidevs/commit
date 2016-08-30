e5df2703bb2eb80f1c02a554f2d127629935989b
eudgee <eudgee@gmail.com>
2016-08-30 13:28:48 +0100

    2016-08-30's javascript workout

---
+++ contributions/eudgee/javascript/Npm/2016-08-30.js
@@ -0,0 +1,5 @@
+npm can handle multiple versions of a dependency
+Manage local node modules with `npm link`
+Reduce package duplication 
+Scoped packages
+Lock down dependency versions by shrinkwrapping
