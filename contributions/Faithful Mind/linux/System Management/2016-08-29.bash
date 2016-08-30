5ff2a94196d5fa6c84396f29652d63b1d5dd279a
Faithful Mind <lpyy15@gmail.com>
2016-08-30 10:01:46 +0100

    2016-08-29's linux workout

---
+++ contributions/Faithful Mind/linux/System Management/2016-08-29.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
