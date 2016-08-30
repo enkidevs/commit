fca36768c2010da7c34e9b184ec02d3a24c4c9b2
mustak <mhyaqub@yahoo.com>
2016-08-30 15:24:08 +0100

    2016-08-30's javascript workout

---
+++ contributions/mustak/javascript/Functions/2016-08-30.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Function call method
+Social sharing without widgets
+Check if a document is done loading
