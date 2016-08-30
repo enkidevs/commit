f3db7e54bf63caf535f8929fd72fd11b2a63ac15
everton-agner <ton.agner@gmail.com>
2016-08-30 16:26:44 +0100

    2016-08-30's linux workout

---
+++ contributions/everton-agner/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear the terminal instantly
+Find any Unix / Linux command
+Sequential execution using the `;` statement separator
