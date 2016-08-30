95f3637b1bb96a09b047860b986a2fe433ce67ae
Granra <granra@gmail.com>
2016-08-30 12:05:34 +0100

    2016-08-30's linux workout

---
+++ contributions/Granra/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
