c08e681755f7544906d96df9210f0b3580b67b10
CorruptComputer <ngupton98@gmail.com>
2016-08-30 22:57:38 +0100

    2016-08-30's linux workout

---
+++ contributions/CorruptComputer/linux/System Management/2016-08-30.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
