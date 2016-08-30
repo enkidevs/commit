49b0e0793c15c42ad7d9c5079879ec4c06fcaea6
zszuri <szuri90@gmail.com>
2016-08-30 11:58:16 +0100

    2016-08-30's java workout

---
+++ contributions/zszuri/java/Fundamentals/2016-08-30.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Using `synchronized` statements
