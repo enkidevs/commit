4c22e8330b240d536c51372dcb9b9ac28ba41a42
antoligy <a@ax.gy>
2016-08-30 11:12:56 +0100

    2016-08-30's linux workout

---
+++ contributions/antoligy/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
