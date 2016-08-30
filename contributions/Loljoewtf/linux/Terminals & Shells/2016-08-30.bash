a4ea048add60b163293a163a840e947f8ea6f6ed
Loljoewtf <joecarlyon@gmail.com>
2016-08-30 14:22:35 +0100

    2016-08-30's linux workout

---
+++ contributions/Loljoewtf/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
