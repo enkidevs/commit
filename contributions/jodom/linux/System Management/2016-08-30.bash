0c983ae3c71f0f0f4da7bf09e9db29a3d813a01c
jodom <jodomoumak.jo@gmail.com>
2016-08-30 21:25:36 +0100

    2016-08-30's linux workout

---
+++ contributions/jodom/linux/System Management/2016-08-30.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Monitor System Memory using `vmstat`
