1a4e9b7e612504d1b2f632c68e562875f939ab74
Enterpaper <byebye99@qq.com>
2016-08-30 04:20:42 +0100

    2016-08-29's java workout

---
+++ contributions/Enterpaper/java/Fundamentals/2016-08-29.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Using bounded type parameters in generic methods
+How range operations work
