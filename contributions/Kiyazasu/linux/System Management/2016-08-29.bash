48bf54f7c564cb82087c7cb564e57028f06ddfe4
Kiyazasu <kenpo1469@gmail.com>
2016-08-30 02:36:03 +0100

    2016-08-29's linux workout

---
+++ contributions/Kiyazasu/linux/System Management/2016-08-29.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Commands to shutdown or restart the system
