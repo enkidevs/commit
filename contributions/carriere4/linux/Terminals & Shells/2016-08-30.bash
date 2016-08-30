1dd41d1820c302ad16b26e1f2b881ef013ea5317
carriere4 <carriere4@gmail.com>
2016-08-30 19:20:54 +0100

    2016-08-30's linux workout

---
+++ contributions/carriere4/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear bash history
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
