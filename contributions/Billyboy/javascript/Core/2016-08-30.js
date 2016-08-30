e7013646ac704800130cfd7cf104e1a2d4536afc
Billyboy <billybellchambers@yahoo.com>
2016-08-30 12:26:26 +0100

    2016-08-30's javascript workout

---
+++ contributions/Billyboy/javascript/Core/2016-08-30.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Performing a function at timed intervals
+Data type comparison in `switch` statements
+Scope and strict mode
+Round numbers to `N` decimals 
