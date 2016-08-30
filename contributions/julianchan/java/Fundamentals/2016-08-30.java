46383f7a89c0221b11d47e95974281b8018249cd
julianchan <mrjulianchan@gmail.com>
2016-08-30 14:28:54 +0100

    2016-08-30's java workout

---
+++ contributions/julianchan/java/Fundamentals/2016-08-30.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+How to play a sound file in Java
+Using Inheritance to reduce code repetition
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
