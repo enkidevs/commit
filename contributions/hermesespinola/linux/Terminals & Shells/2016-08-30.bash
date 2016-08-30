aa118a5facf9cbecc4ee7a38cc29bd97f98feb94
hermesespinola <hermesespinola@gmail.com>
2016-08-30 15:17:43 +0100

    2016-08-30's linux workout

---
+++ contributions/hermesespinola/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,5 @@
+Quick `bash` shortcuts 
+Random password generator
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
