22a729ad0c227aae6ba399e9abdc640bb9b99a79
Tals <talschuster@gmail.com>
2016-08-30 12:57:34 +0100

    2016-08-30's linux workout

---
+++ contributions/Tals/linux/File Management/2016-08-30.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
