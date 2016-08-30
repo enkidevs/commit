19229e3d9d75a91b59b5c270d3de5346cde5d6af
IamJeevan <jeevan.basavanna@gmail.com>
2016-08-30 12:56:37 +0100

    2016-08-29's linux workout

---
+++ contributions/IamJeevan/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
