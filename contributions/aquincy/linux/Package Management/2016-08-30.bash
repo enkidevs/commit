1b801d08d85cd07e26633399aa48884c0cd02826
aquincy <brennanjl003@gmail.com>
2016-08-30 19:09:51 +0100

    2016-08-30's linux workout

---
+++ contributions/aquincy/linux/Package Management/2016-08-30.bash
@@ -0,0 +1,5 @@
+Install specific version of packages
+List installed packages
+Installing a `.deb` package from the terminal
+Prevent updating a specific package in Debian systems
+Solving `BADSIG GPG` errors
