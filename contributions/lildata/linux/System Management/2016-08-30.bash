ed5c1800e4990a2ad40721af9307f3859c9f1250
lildata <jfo.bent@gmail.com>
2016-08-30 09:41:29 +0100

    2016-08-30's linux workout

---
+++ contributions/lildata/linux/System Management/2016-08-30.bash
@@ -0,0 +1,7 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
