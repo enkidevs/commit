ed3a69dc23088f369ab6160bbd2110cb3b74d075
dogen <samuel.sh79@gmail.com>
2016-08-30 15:34:07 +0100

    2016-08-30's python workout

---
+++ contributions/dogen/python/Data Structures/2016-08-30.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Immutable sets with `frozenset`
+Looping techniques
+Operations with `bytes` and `bytearray`
+Binary record packing with `bytearray`
