159f06aa11ce5058d3f5fbb890eeec83ea848889
iamjoel <carranzajoel@gmail.com>
2016-08-30 02:54:21 +0100

    2016-08-29's linux workout

---
+++ contributions/iamjoel/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
