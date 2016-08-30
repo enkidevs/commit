c7a776a411248d6eef827a573e4cf70a04be3afc
wild <georgedeheer6919@gmail.com>
2016-08-30 23:52:20 +0100

    2016-08-30's linux workout

---
+++ contributions/wild/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
