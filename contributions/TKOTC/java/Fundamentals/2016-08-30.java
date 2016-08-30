3f9c5d19fe0ed464afa33097ecb36283331c2545
TKOTC <steven.j.mckeown@gmail.com>
2016-08-30 18:46:26 +0100

    2016-08-30's java workout

---
+++ contributions/TKOTC/java/Fundamentals/2016-08-30.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Java naming conventions
+Using bounded type parameters in generic methods
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
