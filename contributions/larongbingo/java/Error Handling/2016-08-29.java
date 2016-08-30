3799896850553412eff81a4a4d84fa2e77a9dd0b
larongbingo <larongbingo@gmail.com>
2016-08-30 11:39:56 +0100

    2016-08-29's java workout

---
+++ contributions/larongbingo/java/Error Handling/2016-08-29.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
