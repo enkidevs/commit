6937707e925cc8d10d76d8c7476138186d6f354f
joakimnystrom <jnystromdesign@gmail.com>
2016-08-30 11:39:47 +0100

    2016-08-30's javascript workout

---
+++ contributions/joakimnystrom/javascript/Functions/2016-08-30.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` with `call` and `apply` methods
+Method chaining
+Easily generate a random `HEX` color
+Function call method
