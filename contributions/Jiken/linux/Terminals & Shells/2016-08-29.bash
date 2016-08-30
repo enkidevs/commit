80811bdf4938f3c81a9c03cf8609e3c015c6fe7b
Jiken <jeremyiken@gmail.com>
2016-08-30 03:09:12 +0100

    2016-08-29's linux workout

---
+++ contributions/Jiken/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Terminal based browser
+Useful aliasing in bash
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
