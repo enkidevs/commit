841f47c0df3069dabe027ebfa6859fb5e8baedc5
floppycatfish <legarland@gmail.com>
2016-08-30 21:18:39 +0100

    2016-08-30's javascript workout

---
+++ contributions/floppycatfish/javascript/React/2016-08-30.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+componentWillReceiveProps Not Triggered After Mounting
+
+`PureRenderMixin` in **React**
+Custom `propType`'s to be required
+Default values for props
