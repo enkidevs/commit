52c0fb80999bd69aac1f2bff052de51e84d6a08e
Zbysek <zbysek.vacek@gmail.com>
2016-08-30 13:53:16 +0100

    2016-08-30's javascript workout

---
+++ contributions/Zbysek/javascript/Core/2016-08-30.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+Avoid using `with`
+Detect an error type
