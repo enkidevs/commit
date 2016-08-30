caaf7824ec0105d9a0702988a0febfa458f663c6
ecomaikgolf <iecomaikgolf@gmail.com>
2016-08-30 23:36:03 +0100

    2016-08-30's linux workout

---
+++ contributions/ecomaikgolf/linux/System Management/2016-08-30.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
