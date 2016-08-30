d11ff7253054aa0c25811dd0f888df74f4b66c32
tibi00 <hidi.tibi@gmail.com>
2016-08-30 09:29:58 +0100

    2016-08-30's linux workout

---
+++ contributions/tibi00/linux/System Management/2016-08-30.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
