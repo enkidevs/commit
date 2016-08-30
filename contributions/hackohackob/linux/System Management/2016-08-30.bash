921760ffc556694d14d7bf5b7f3de1fa4b881452
hackohackob <hackohackob@gmail.com>
2016-08-30 17:13:27 +0100

    2016-08-30's linux workout

---
+++ contributions/hackohackob/linux/System Management/2016-08-30.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
