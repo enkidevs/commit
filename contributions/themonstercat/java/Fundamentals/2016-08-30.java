7f7403a14073cf2e0cad78dd7c06199e68cdd7c8
themonstercat <gbhamra2@gmail.com>
2016-08-30 19:30:07 +0100

    2016-08-30's java workout

---
+++ contributions/themonstercat/java/Fundamentals/2016-08-30.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
