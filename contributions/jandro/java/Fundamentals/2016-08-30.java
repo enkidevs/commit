edfeda0977ba28f4f1232dcf476ad43eac9ae93d
jandro <malejandrofc@gmail.com>
2016-08-30 22:19:27 +0100

    2016-08-30's java workout

---
+++ contributions/jandro/java/Fundamentals/2016-08-30.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
