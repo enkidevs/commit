55e91cf371494ba6a9a49a76ab774b52565d6131
xtrimsky <andrei.pervychine@gmail.com>
2016-08-30 02:48:13 +0100

    2016-08-29's javascript workout

---
+++ contributions/xtrimsky/javascript/Functions/2016-08-29.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Function call method
+Changing a functions context with `fn.call(object)`
