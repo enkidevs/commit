c0ab27cbe88e0589437fafd7084f77d7f41ac719
lunamystry <lm.mbuli+enki@gmail.com>
2016-08-30 07:26:00 +0100

    2016-08-30's linux workout

---
+++ contributions/lunamystry/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
