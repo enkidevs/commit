01649f44f1bb8faf21f1a4e109881d76120c3d38
adracea <adracea@gmail.com>
2016-08-30 10:53:26 +0100

    2016-08-30's linux workout

---
+++ contributions/adracea/linux/System Management/2016-08-30.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
