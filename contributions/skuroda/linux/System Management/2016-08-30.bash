96a88cd174827ece8bc1e2f9bf71fe78ba2781e4
skuroda <sk@mieco.org>
2016-08-31 00:56:40 +0100

    2016-08-30's linux workout

---
+++ contributions/skuroda/linux/System Management/2016-08-30.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
