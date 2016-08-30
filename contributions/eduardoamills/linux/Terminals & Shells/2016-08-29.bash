f6807251e86fc5e0dc59257acb27dc91016673b0
eduardoamills <eduardoamills@gmail.com>
2016-08-30 04:40:39 +0100

    2016-08-29's linux workout

---
+++ contributions/eduardoamills/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+`else` statements using the `||` operator 
+Retrieve previous arguments
+The Basics of Environment Variables
