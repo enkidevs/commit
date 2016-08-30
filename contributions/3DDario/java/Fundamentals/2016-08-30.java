54ce175f4d1db93cf1b7029f76e9ef6bfb5eff1f
3DDario <delledonnedario@gmail.com>
2016-08-30 08:15:21 +0100

    2016-08-30's java workout

---
+++ contributions/3DDario/java/Fundamentals/2016-08-30.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use meaningful names
+The trade-off between short and descriptive names
+Using Inheritance to reduce code repetition
+Supply `toString()` in all classes
