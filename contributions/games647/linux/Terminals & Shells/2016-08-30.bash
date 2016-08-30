0441e7825b810959fb8397cbfd5a97853027ac2c
games647 <games@tsprivat.de>
2016-08-30 08:22:43 +0100

    2016-08-30's linux workout

---
+++ contributions/games647/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Clear bash history
+Conditional command execution
+
+(`&&` operator)
+Retrieve previous arguments
+The Basics of Environment Variables
