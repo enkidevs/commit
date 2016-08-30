558128c3dbf482336d66a9919b92616f8a9d208b
davidfm <david@davidfm.es>
2016-08-30 20:06:48 +0100

    2016-08-30's linux workout

---
+++ contributions/davidfm/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
