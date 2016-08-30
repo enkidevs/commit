3f0846c3e3f6dd1c77d12a130e70a1576dbcb812
endertux <endertux@gmail.com>
2016-08-30 03:12:14 +0100

    2016-08-29's linux workout

---
+++ contributions/endertux/linux/System Management/2016-08-29.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Common log files
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
