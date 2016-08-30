17677240f0450fe71be448c34f5b1a69a658b8b2
joelandry <joelandry80@gmail.com>
2016-08-30 20:12:28 +0100

    2016-08-30's linux workout

---
+++ contributions/joelandry/linux/System Management/2016-08-30.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+System uptime
