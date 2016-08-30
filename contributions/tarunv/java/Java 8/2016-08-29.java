f4304de9573a946bd3cacee791b19ef489694098
tarunv <tarunv1997@gmail.com>
2016-08-30 13:44:43 +0100

    2016-08-29's java workout

---
+++ contributions/tarunv/java/Java 8/2016-08-29.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Use `Optional(s)` type in order to avoid `NullPointerException(s)`
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
