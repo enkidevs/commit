ff8a4f175bb8a5273bb419f493313c76b8ceca86
richardlu <richard.wh.lu@gmail.com>
2016-08-30 17:32:26 +0100

    2016-08-30's linux workout

---
+++ contributions/richardlu/linux/File Management/2016-08-30.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
+Delete commands aliases
