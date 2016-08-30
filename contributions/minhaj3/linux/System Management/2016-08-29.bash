6280d744b5c866f2d722929919f70e0760e30e01
minhaj3 <zackcool321@gmail.com>
2016-08-30 16:30:41 +0100

    2016-08-29's linux workout

---
+++ contributions/minhaj3/linux/System Management/2016-08-29.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
