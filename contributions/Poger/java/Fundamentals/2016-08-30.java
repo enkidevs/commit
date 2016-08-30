2fcca0f0fabc17755d84597f437a6f3098a54c04
Poger <gkonupek@gmail.com>
2016-08-30 15:00:13 +0100

    2016-08-30's java workout

---
+++ contributions/Poger/java/Fundamentals/2016-08-30.java
@@ -0,0 +1,5 @@
+Cropping images
+How to play a sound file in Java
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
