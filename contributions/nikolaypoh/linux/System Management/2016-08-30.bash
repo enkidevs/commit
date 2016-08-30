d56c728d8c17359a1256b23c99e692f3930822e1
nikolaypoh <nikolaypoh@gmail.com>
2016-08-30 21:13:53 +0100

    2016-08-30's linux workout

---
+++ contributions/nikolaypoh/linux/System Management/2016-08-30.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
