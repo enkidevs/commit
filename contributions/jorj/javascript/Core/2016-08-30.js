9962e87d14b88140fe8eab7378470022b2d028d5
jorj <jorj.ives@gmail.com>
2016-08-30 16:47:01 +0100

    2016-08-30's javascript workout

---
+++ contributions/jorj/javascript/Core/2016-08-30.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Data type comparison in `switch` statements
+The difference between null, undefined and NaN
+Detect **DO NOT TRACK** status
+Getting the *real* dimensions of an image
