fa3afd44e7e0baf6340a16a68ec1d3868b45370f
lorenzoi <lorenzoiannuzzi@gmail.com>
2016-08-30 15:28:27 +0100

    2016-08-30's linux workout

---
+++ contributions/lorenzoi/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
