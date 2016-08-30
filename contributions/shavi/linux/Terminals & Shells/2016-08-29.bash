f08b2114ec1b25c632a82ab99611bc7c0f63a2fb
shavi <shivinder@gmail.com>
2016-08-30 13:53:04 +0100

    2016-08-29's linux workout

---
+++ contributions/shavi/linux/Terminals & Shells/2016-08-29.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
