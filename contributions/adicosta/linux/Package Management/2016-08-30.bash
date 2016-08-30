8cc6e5b870c4cbb7749c6ab3ca1b952ca99e1f81
adicosta <alex@thepieuvre.com>
2016-08-30 06:49:34 +0100

    2016-08-30's linux workout

---
+++ contributions/adicosta/linux/Package Management/2016-08-30.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+List installed packages
+Prevent updating a specific package in Debian systems
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
