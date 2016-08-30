f7003e65e5964ca64efaba8e089f33da0ec5288a
unholycorndog <enki@unholycorndog.neomailbox.ch>
2016-08-30 01:53:16 +0100

    2016-08-29's linux workout

---
+++ contributions/unholycorndog/linux/System Management/2016-08-29.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Commands to shutdown or restart the system
