21fc92a9ea6100e184288099715079c4e1b27e3e
jttwnsnd <jt.a.townsend@gmail.com>
2016-08-30 15:36:49 +0100

    2016-08-30's linux workout

---
+++ contributions/jttwnsnd/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,5 @@
+Terminal based browser
+Random password generator
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
