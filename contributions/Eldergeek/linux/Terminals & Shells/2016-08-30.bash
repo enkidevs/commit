1aba97e01507e1dcdd997278c72a49739c3a2e8a
Eldergeek <david@geekguytech.com>
2016-08-30 15:10:53 +0100

    2016-08-30's linux workout

---
+++ contributions/Eldergeek/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Clear bash history
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
