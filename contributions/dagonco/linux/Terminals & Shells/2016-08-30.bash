a06a4868702204ca199fc94742446e014d7a2ba4
dagonco <dagonco.dev@gmail.com>
2016-08-30 08:08:23 +0100

    2016-08-30's linux workout

---
+++ contributions/dagonco/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Random password generator
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
