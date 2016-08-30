14f5524d73c4a341855ddd7c6bcc085a1ecde455
mkg20001 <mkg20001@gmail.com>
2016-08-30 15:21:00 +0100

    2016-08-30's linux workout

---
+++ contributions/mkg20001/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
