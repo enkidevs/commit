6a4d2913b45a07fbf870d3e2b9d09afa2f9d54fe
alexraby <araby424@gmail.com>
2016-08-30 16:09:00 +0100

    2016-08-30's javascript workout

---
+++ contributions/alexraby/javascript/Functions/2016-08-30.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Get query/url variables
+Move cursor at the end of text input
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
