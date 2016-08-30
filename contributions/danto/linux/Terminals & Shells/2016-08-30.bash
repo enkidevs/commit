26a724ab649219fcff2f419814d361eddf1cf076
danto <danielj@outlook.de>
2016-08-30 13:57:37 +0100

    2016-08-30's linux workout

---
+++ contributions/danto/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
