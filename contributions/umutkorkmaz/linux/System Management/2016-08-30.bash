c3d2b9f5a6a6bcdf96b953876d4b7dba4e4b4518
umutkorkmaz <info@umutkorkmaz.net>
2016-08-30 21:24:15 +0100

    2016-08-30's linux workout

---
+++ contributions/umutkorkmaz/linux/System Management/2016-08-30.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
