730d834882459f440c47ac7b912439f6de538959
lpgonzalez <lisardo.prieto.gonzalez@gmail.com>
2016-08-30 04:25:36 +0100

    2016-08-30's java workout

---
+++ contributions/lpgonzalez/java/Classes & Objects/2016-08-30.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Using `static` fields
+Utility classes and `static` methods
+Compare classes and not class names
