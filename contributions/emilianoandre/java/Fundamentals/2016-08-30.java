a165e224904171d0be58ca26dc2c16794bc7dc91
emilianoandre <emilianoandre@gmail.com>
2016-08-30 21:28:06 +0100

    2016-08-30's java workout

---
+++ contributions/emilianoandre/java/Fundamentals/2016-08-30.java
@@ -0,0 +1,4 @@
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
