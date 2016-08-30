3bc58425754b333f26c2b98e16e573681529cdc8
troyvoy88 <raelis.nexus@gmail.com>
2016-08-30 13:57:47 +0100

    2016-08-30's linux workout

---
+++ contributions/troyvoy88/linux/System Management/2016-08-30.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Common log files
+Changing the `/tmp` cleanup frequency
