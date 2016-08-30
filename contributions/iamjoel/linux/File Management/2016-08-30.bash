e3ca34785d4f5727e238bfe47a5b75fc64732db9
iamjoel <carranzajoel@gmail.com>
2016-08-30 23:15:09 +0100

    2016-08-30's linux workout

---
+++ contributions/iamjoel/linux/File Management/2016-08-30.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Delete commands aliases
