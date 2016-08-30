24a027a6fccbfd76589ad57818c4203f7d185a8e
TheBimboBread <raulgf92@gmail.com>
2016-08-30 21:49:08 +0100

    2016-08-30's linux workout

---
+++ contributions/TheBimboBread/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Useful aliasing in bash
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
