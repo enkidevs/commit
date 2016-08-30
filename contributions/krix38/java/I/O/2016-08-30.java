0bfc970928bb0610099fe158bcf079e18e354758
krix38 <krzysztof.kakol89@gmail.com>
2016-08-30 05:47:05 +0100

    2016-08-30's java workout

---
+++ contributions/krix38/java/I/O/2016-08-30.java
@@ -0,0 +1,5 @@
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
