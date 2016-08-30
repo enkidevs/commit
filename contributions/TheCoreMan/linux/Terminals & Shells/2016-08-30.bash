7a844e8bde60827120d8de1a9d92ed3f4aedccfc
TheCoreMan <dude500@gmail.com>
2016-08-30 07:04:19 +0100

    2016-08-30's linux workout

---
+++ contributions/TheCoreMan/linux/Terminals & Shells/2016-08-30.bash
@@ -0,0 +1,7 @@
+Clear the terminal instantly
+Find any Unix / Linux command
+Terminal based browser
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
