15c336622676a211b0c3592f70624ebb5a3fc69b
makroma <markoma@iki.fi>
2016-08-31 17:40:27 +0100

    2016-08-31's linux workout

---
+++ contributions/makroma/linux/System Management/2016-08-31.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Common log files
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
