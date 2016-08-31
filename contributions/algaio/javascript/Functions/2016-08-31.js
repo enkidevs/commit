658f902d1c5eac8bed9b8d032bbc65e7bc95bfe9
algaio <eckstrm@gmail.com>
2016-08-31 07:57:37 +0100

    2016-08-31's javascript workout

---
+++ contributions/algaio/javascript/Functions/2016-08-31.js
@@ -0,0 +1,5 @@
+Functions can be declared after use
+Anonymous functions
+Move cursor at the end of text input
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
