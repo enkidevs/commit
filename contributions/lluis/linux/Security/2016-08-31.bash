c52686d02d7e5144158b834239025e7be3b5e468
lluis <lluismontero@gmail.com>
2016-08-31 22:03:16 +0100

    2016-08-31's linux workout

---
+++ contributions/lluis/linux/Security/2016-08-31.bash
@@ -0,0 +1,5 @@
+Extracting the `public key` from the `private key`
+Disable SSH password authentication 
+Block IPs using `Fail2ban`
+Keeping `/boot` read-only
+Private key encryption with `OpenSSL`
