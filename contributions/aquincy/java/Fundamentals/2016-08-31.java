032c04ebb0d533962244bb594b02e8394dca2bd4
aquincy <brennanjl003@gmail.com>
2016-08-31 16:00:10 +0100

    2016-08-31's java workout

---
+++ contributions/aquincy/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Metadata: creating a user-defined file attribute
+Always specify type parameter
