9c5b97e8d287bcecd2bc04e1dc4d6b12a371841c
khalilayache <khalilayache@hotmail.com>
2016-08-31 03:16:23 +0100

    2016-08-30's linux workout

---
+++ contributions/khalilayache/linux/Package Management/2016-08-30.bash
@@ -0,0 +1,6 @@
+Backporting on Debian
+
+Installing a `.deb` package from the terminal
+Get `apt` to use a mirror / faster mirror
+Using `Tasksel` for software installation
+Keeping repos updated with `cron`
