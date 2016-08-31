37cf9a08f6f6df674fdcd5937c78bf51033a6fa3
forty2nd <mdyomin@gmail.com>
2016-08-31 19:39:10 +0100

    2016-08-31's java workout

---
+++ contributions/forty2nd/java/I/O/2016-08-31.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
