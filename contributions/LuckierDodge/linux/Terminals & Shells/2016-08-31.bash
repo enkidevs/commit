d3d7fe987ec8cc962bca59e6217ded35c5644fca
LuckierDodge <ryandlewis.rl@gmail.com>
2016-08-31 23:31:37 +0100

    2016-08-31's linux workout

---
+++ contributions/LuckierDodge/linux/Terminals & Shells/2016-08-31.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
