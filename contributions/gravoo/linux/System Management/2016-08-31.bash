8a636376ee71cb1438bdb6075a259415f2331d26
gravoo <gravoo@yahoo.com>
2016-08-31 22:34:27 +0100

    2016-08-31's linux workout

---
+++ contributions/gravoo/linux/System Management/2016-08-31.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Finding Open Files With `lsof`
