0116897c246494264d98911d9a60ca9d2b169158
Shadowpryo <terry.dewayne.hicks@gmail.com>
2016-08-31 20:39:50 +0100

    2016-08-31's linux workout

---
+++ contributions/Shadowpryo/linux/System Management/2016-08-31.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Commands to shutdown or restart the system
+Finding Open Files With `lsof`
