e253b182e1f039015d17f4beef36b273b8628505
mconiglio <mconiglio.782@gmail.com>
2016-09-01 00:00:21 +0100

    2016-08-31's javascript workout

---
+++ contributions/mconiglio/javascript/React/2016-08-31.js
@@ -0,0 +1,7 @@
+Functional Stateless Components in React
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Dangerously set `innerHTML`
+Server-side rendering 
