6e2b4a932549803f2aaa5845ff512df92fb87251
barteb <bartosz.boryn@gmail.com>
2016-08-31 16:14:58 +0100

    2016-08-31's linux workout

---
+++ contributions/barteb/linux/Terminals & Shells/2016-08-31.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Clear bash history
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
