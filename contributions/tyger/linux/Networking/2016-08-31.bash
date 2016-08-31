043d6f4f4724bedd8b42fc3517ac8e86b5f2b9e5
tyger <tyger@gmx.de>
2016-08-31 18:26:33 +0100

    2016-08-31's linux workout

---
+++ contributions/tyger/linux/Networking/2016-08-31.bash
@@ -0,0 +1,5 @@
+SSH tunneling made easy
+Proxifying `ssh` connections
+Useful ssh client optimizations 
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
