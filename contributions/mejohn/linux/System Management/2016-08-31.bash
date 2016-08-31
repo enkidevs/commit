7833c1ab7dce7227aee5097bb14e2d6c96ff0f7c
mejohn <mejohn10@ncsu.edu>
2016-08-31 18:09:25 +0100

    2016-08-31's linux workout

---
+++ contributions/mejohn/linux/System Management/2016-08-31.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
