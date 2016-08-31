243504909d0b91fa270de87e7bd34f51d98ff29b
pablofabianbm <pablofabianbm@gmail.com>
2016-08-31 04:35:25 +0100

    2016-08-30's linux workout

---
+++ contributions/pablofabianbm/linux/System Management/2016-08-30.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
