18e33a7c4509151eca3d7f1db77823de80cf60a0
araghunde <paybol.21@gmail.com>
2016-08-31 14:43:01 +0100

    2016-08-31's java workout

---
+++ contributions/araghunde/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Metadata: creating a user-defined file attribute
+Supply `toString()` in all classes
+Using an interface as a parameter
