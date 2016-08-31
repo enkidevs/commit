68b5ea343453c3e77d8bb70e1dc1173f6391d588
gromchen <gromchen@outlook.com>
2016-08-31 11:52:53 +0100

    2016-08-31's linux workout

---
+++ contributions/gromchen/linux/System Management/2016-08-31.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
