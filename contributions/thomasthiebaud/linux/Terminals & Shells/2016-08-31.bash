1360642fe71f90c4947e3f2b3789e93e52cef000
thomasthiebaud <thiebaud.tom@gmail.com>
2016-08-31 16:19:41 +0100

    2016-08-31's linux workout

---
+++ contributions/thomasthiebaud/linux/Terminals & Shells/2016-08-31.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Shebang `#!` explained 
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
