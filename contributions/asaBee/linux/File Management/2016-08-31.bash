6abc44eb10a1ad69b02ce38005e33a358af6f654
asaBee <hlovers.forever@gmail.com>
2016-08-31 22:31:31 +0100

    2016-08-31's linux workout

---
+++ contributions/asaBee/linux/File Management/2016-08-31.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Deleting non-empty directories
+Working with `.gz` files
