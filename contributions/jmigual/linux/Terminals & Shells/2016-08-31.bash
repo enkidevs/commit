eb34df40df5e0cfdb1754c3f7e8016f56050c0c8
jmigual <jmigual0@gmail.com>
2016-08-31 20:37:06 +0100

    2016-08-31's linux workout

---
+++ contributions/jmigual/linux/Terminals & Shells/2016-08-31.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
