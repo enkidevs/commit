061af2ee03819e1d2b8b6e6e6246088d376f74b2
SanzaBlancoAkA2C <sanzablancoaka2c@gmail.com>
2016-08-31 03:42:11 +0100

    2016-08-30's java workout

---
+++ contributions/SanzaBlancoAkA2C/java/Fundamentals/2016-08-30.java
@@ -0,0 +1,5 @@
+Distinction between `public` and `private` methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Supply `toString()` in all classes
