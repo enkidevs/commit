5193d88cee57aaac00bd62358433129c8b0bdeb3
podoi <jose0797@gmail.com>
2016-08-31 17:29:08 +0100

    2016-08-31's java workout

---
+++ contributions/podoi/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+Supply `toString()` in all classes
+Using an interface as a parameter
