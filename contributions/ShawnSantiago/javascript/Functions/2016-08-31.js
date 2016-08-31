e1397b5dbb616f3521f15213a5c12c8074084f20
ShawnSantiago <santiagoshawn@gmail.com>
2016-08-31 17:27:45 +0100

    2016-08-31's javascript workout

---
+++ contributions/ShawnSantiago/javascript/Functions/2016-08-31.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+IIFE pattern
+Function call method
+Social sharing without widgets
