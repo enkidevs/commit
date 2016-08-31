76eb229555d902b9a358f5f1f091cc1aa22d4aa0
ple103 <peterhuyle@outlook.com>
2016-09-01 00:31:35 +0100

    2016-08-31's javascript workout

---
+++ contributions/ple103/javascript/Browser APIs/2016-08-31.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+Navigation Timing API
+Blobs
+Window.sessionStorage
