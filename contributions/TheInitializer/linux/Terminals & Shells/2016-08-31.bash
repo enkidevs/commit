4aa2fa80ddd9b15031a72a8df6b12e67e5a84777
TheInitializer <dkteresi@gmail.com>
2016-09-01 00:55:31 +0100

    2016-08-31's linux workout

---
+++ contributions/TheInitializer/linux/Terminals & Shells/2016-08-31.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
