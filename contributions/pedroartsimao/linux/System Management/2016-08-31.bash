bc879b3328eac5337c0536d06fd1c4acf57d97f1
pedroartsimao <pedroarthursimao@gmail.com>
2016-08-31 19:23:30 +0100

    2016-08-31's linux workout

---
+++ contributions/pedroartsimao/linux/System Management/2016-08-31.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
