8bd68eccb52f7cf2e9266ed257568a20456505ab
fr3mling <svadilfar+enki@gmail.com>
2016-08-31 10:00:50 +0100

    2016-08-31's linux workout

---
+++ contributions/fr3mling/linux/Terminals & Shells/2016-08-31.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Aliasing ssh connections
+Find any Unix / Linux command
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
