eab6835805d0f79170470f4038b8648fed463b9e
diplodocus <psaulic@gmail.com>
2016-08-31 16:22:39 +0100

    2016-08-31's java workout

---
+++ contributions/diplodocus/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
