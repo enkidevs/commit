3a03bc5b7e795e56d1251b43023423ef97549aba
gogamwar <gogamwar@gmail.com>
2016-08-31 11:51:26 +0100

    2016-08-31's linux workout

---
+++ contributions/gogamwar/linux/Terminals & Shells/2016-08-31.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Find any Unix / Linux command
+Terminal based browser
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
