5b976696e0576c4e97986dd4091facf28f0614b9
aselims <selim.2k@gmail.com>
2016-08-31 21:56:22 +0100

    2016-08-31's java workout

---
+++ contributions/aselims/java/Error Handling/2016-08-31.java
@@ -0,0 +1,5 @@
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Using exceptions in Java
+Most common reason behind **stack overflow** error
+Throwing an `exception`
