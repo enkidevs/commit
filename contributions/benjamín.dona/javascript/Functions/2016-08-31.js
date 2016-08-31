c8aefd1e4ca17f06c74d95d0a71d4a579c425ffb
benjamín.dona <benjamin.dona@live.cl>
2016-09-01 00:38:45 +0100

    2016-08-31's javascript workout

---
+++ contributions/benjamín.dona/javascript/Functions/2016-08-31.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+IIFE pattern
+Easily generate a random `HEX` color
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
