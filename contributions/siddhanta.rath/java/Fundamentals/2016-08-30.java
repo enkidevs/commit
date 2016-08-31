d7c01451cb424c3872cbeabdf47ad68a0a909079
siddhanta.rath <siddhanta.rath@gmail.com>
2016-08-31 18:10:53 +0100

    2016-08-30's java workout

---
+++ contributions/siddhanta.rath/java/Fundamentals/2016-08-30.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Distinction between `public` and `private` methods
+Always specify type parameter
+Limit Accessibility of `Fields`
