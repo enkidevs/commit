1795637ac271bb2dfc07d78306d7488fa89d89dd
sisko59 <6sko59@gmail.com>
2016-08-31 18:53:57 +0100

    2016-08-31's linux workout

---
+++ contributions/sisko59/linux/Terminals & Shells/2016-08-31.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
