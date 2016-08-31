0a21af1673902e65b85d0e7ea1338f38364f3592
matensegiles <matensegiles@gmail.com>
2016-08-31 13:35:12 +0100

    2016-08-31's java workout

---
+++ contributions/matensegiles/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
