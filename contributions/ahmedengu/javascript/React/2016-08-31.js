857f2f6435bc4a328a78dfbd8b9a6ea32ef4ad09
ahmedengu <ahmedengu@gmail.com>
2016-08-31 12:45:13 +0100

    2016-08-31's javascript workout

---
+++ contributions/ahmedengu/javascript/React/2016-08-31.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Keyed fragments in **React**
+Prop Validation
+Validate for required props
