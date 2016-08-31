4ddea4e9145ee3a42e6411fca1fdebb74054e49e
florian.soare <florian.soare@takmate.com>
2016-08-31 07:14:31 +0100

    2016-08-31's linux workout

---
+++ contributions/florian.soare/linux/System Management/2016-08-31.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
