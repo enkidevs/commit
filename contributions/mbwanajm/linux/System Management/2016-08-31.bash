8a0c073bf1472dede3a77620c819a20a6f2c8534
mbwanajm <mbwanajm@gmail.com>
2016-08-31 05:49:54 +0100

    2016-08-31's linux workout

---
+++ contributions/mbwanajm/linux/System Management/2016-08-31.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Cancel a system shutdown
