bcd5a145c7f5b1e546714fc2feb4a7feb5e605bd
behe <johan.behe.lind@gmail.com>
2016-08-31 06:28:42 +0100

    2016-08-31's git workout

---
+++ contributions/behe/git/Essentials/2016-08-31.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to unmodify a modified file
+How to set your username and email
+Limiting log output by time
+Use `short` status to make output more compact
