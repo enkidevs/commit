e64d88c0bb539143ab1648c04742f27686090a1c
jamesthomasonjr <james.thomason4@gmail.com>
2016-08-31 17:53:09 +0100

    2016-08-31's java workout

---
+++ contributions/jamesthomasonjr/java/Error Handling/2016-08-31.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Using exceptions in Java
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
