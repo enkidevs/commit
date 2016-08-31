b29d96d2fb05d2cd68d4f91c3d15d27714bacd91
temirbek <temirbek@gmail.com>
2016-08-31 13:49:05 +0100

    2016-08-30's java workout

---
+++ contributions/temirbek/java/I/O/2016-08-30.java
@@ -0,0 +1,5 @@
+Create system file paths using the `Path` class
+Reading and writing text files
+Using buffered streams
+Checking for the existence of files and directories
+Deleting files and directories
