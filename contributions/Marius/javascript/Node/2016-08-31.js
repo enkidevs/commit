ae2af394540078b4fe4de7c1f75503b01cd38e47
Marius <marius.mez@gmail.com>
2016-08-31 13:20:58 +0100

    2016-08-31's javascript workout

---
+++ contributions/Marius/javascript/Node/2016-08-31.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+`process.nextTick()` callback pattern
+Automatic compilation for Node with **Nodemon** 
+Manage child processes in **Node**
