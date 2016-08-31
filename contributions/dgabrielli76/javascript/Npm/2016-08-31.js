bb10eea7abdd7fc25ffb71ce2e80d85c332f5597
dgabrielli76 <dgabrielli76@gmail.com>
2016-08-31 07:48:41 +0100

    2016-08-31's javascript workout

---
+++ contributions/dgabrielli76/javascript/Npm/2016-08-31.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Manipulate npm packages cache
+npm can handle multiple versions of a dependency
+`npm` verifies `sha1` hashes of packages
+Reduce package duplication 
