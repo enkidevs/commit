adf1a4c020eff45de16b983cb98a4602bcf5a9a4
slawikus <svyatoslav.gladkov@gmail.com>
2016-08-31 15:38:14 +0100

    2016-08-31's linux workout

---
+++ contributions/slawikus/linux/System Management/2016-08-31.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Cancel a system shutdown
