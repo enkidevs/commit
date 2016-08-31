6e58eb5fdcbda5c8cbb51b2ad02c8161426924ae
TheCoreMan <dude500@gmail.com>
2016-08-31 11:49:14 +0100

    2016-08-31's linux workout

---
+++ contributions/TheCoreMan/linux/File Management/2016-08-31.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Working with `.gz` files
+Delete commands aliases
