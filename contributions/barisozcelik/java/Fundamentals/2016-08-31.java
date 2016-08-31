c6b030553f640c4606c3308c5531ecb4feb6abcb
barisozcelik <barisozcelikmail@gmail.com>
2016-08-31 11:04:18 +0100

    2016-08-31's java workout

---
+++ contributions/barisozcelik/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+Cropping images
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
