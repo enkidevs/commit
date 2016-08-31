9188410b9eb143aec6965a886b764cfb77741398
daisy new <banyue1192@163.com>
2016-08-31 04:45:08 +0100

    2016-08-30's git workout

---
+++ contributions/daisy new/git/Essentials/2016-08-30.bash
@@ -0,0 +1,5 @@
+The three states in git
+How to set your username and email
+Use `short` status to make output more compact
+How to write a git commit message
+Remote repositories: fetching and pushing
