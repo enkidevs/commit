a3cfd1431d06402d46f5a255b6f98d7d3c02e71f
aleksander.sciubisz <aleksander.sciubisz@gmail.com>
2016-08-31 11:52:59 +0100

    2016-08-31's linux workout

---
+++ contributions/aleksander.sciubisz/linux/System Management/2016-08-31.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+List current logged on users with `w`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
