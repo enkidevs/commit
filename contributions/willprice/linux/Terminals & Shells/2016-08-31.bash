9731c26a053e56ed20e53dffde4a4fa59386c450
willprice <will.price94@gmail.com>
2016-08-31 10:11:08 +0100

    2016-08-31's linux workout

---
+++ contributions/willprice/linux/Terminals & Shells/2016-08-31.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
