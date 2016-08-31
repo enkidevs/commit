ad162efcb61a13dbd13dab28cc57737d10a6b6da
kazzz <krt.ekzzz@gmail.com>
2016-08-31 12:55:06 +0100

    2016-08-31's linux workout

---
+++ contributions/kazzz/linux/System Management/2016-08-31.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Commands to shutdown or restart the system
