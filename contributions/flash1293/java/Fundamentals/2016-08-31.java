e59ed128f40ce910e3a301418c2d4719775c024e
flash1293 <flash1293@gmail.com>
2016-08-31 06:01:46 +0100

    2016-08-31's java workout

---
+++ contributions/flash1293/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Using bounded type parameters in generic methods
+Measuring time
+Using `synchronized` statements
