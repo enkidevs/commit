782e2e115fc88e1af1aeb2058fb9f2f9e8447c6b
christopheradr <christopher.diaz.riv@gmail.com>
2016-08-31 05:46:18 +0100

    2016-08-30's linux workout

---
+++ contributions/christopheradr/linux/System Management/2016-08-30.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
