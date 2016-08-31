95545f41222f8bf4537d130dc80a30773d2dfc86
Xiris_42 <jbidault@datasolution.fr>
2016-08-31 10:05:34 +0100

    2016-08-31's git workout

---
+++ contributions/Xiris_42/git/Essentials/2016-08-31.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+How to set your username and email
+Search by commit message keyword
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
