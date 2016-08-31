3c79010042b607c3bbb7f6a0daa26ae3acb778d8
jvansan <jeff.van.santen56@gmail.com>
2016-08-31 15:31:07 +0100

    2016-08-31's java workout

---
+++ contributions/jvansan/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Inheriting a constructor from a superclass
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
