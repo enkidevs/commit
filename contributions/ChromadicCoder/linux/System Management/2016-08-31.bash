290753898d4b4fd47196e11ec1711393cf372339
ChromadicCoder <petersearby8@gmail.com>
2016-08-31 08:44:45 +0100

    2016-08-31's linux workout

---
+++ contributions/ChromadicCoder/linux/System Management/2016-08-31.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
