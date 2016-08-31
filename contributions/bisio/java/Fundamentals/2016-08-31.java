bc0c4962c3e63e37d8dbb415253ea635b8ff3bf4
bisio <andrea.bisognin@gmail.com>
2016-08-31 06:14:07 +0100

    2016-08-31's java workout

---
+++ contributions/bisio/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Using `synchronized` statements
+Detect or prevent integer overflow
+Supply `toString()` in all classes
