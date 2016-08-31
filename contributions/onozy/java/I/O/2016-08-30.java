74121802393eee9063a692ec68ce484631cc26be
onozy <onozawa2106@gmail.com>
2016-08-31 02:00:42 +0100

    2016-08-30's java workout

---
+++ contributions/onozy/java/I/O/2016-08-30.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Checking for the existence of files and directories
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
