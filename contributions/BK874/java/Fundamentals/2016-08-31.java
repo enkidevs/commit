2cf04f4ac72c438b2de98776bfd745ee81d9fd94
BK874 <bjk69@pitt.edu>
2016-08-31 14:10:48 +0100

    2016-08-31's java workout

---
+++ contributions/BK874/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
