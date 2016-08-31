4b98985edcc060fdd1d9b4832b14fd516ce53328
augustochaves <chaves.augusto@gmail.com>
2016-08-31 10:11:19 +0100

    2016-08-31's linux workout

---
+++ contributions/augustochaves/linux/Terminals & Shells/2016-08-31.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Clear bash history
+Terminal based browser
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
