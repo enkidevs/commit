c59f3bd53af0b6536de17c138960d85c6f06b213
JMarques <silvermarkes@gmail.com>
2016-08-31 08:49:39 +0100

    2016-08-31's javascript workout

---
+++ contributions/JMarques/javascript/Core/2016-08-31.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Keywords in hash keys
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Inaccuracy of binary floating-point format
