bfc9788e8626aefc4f73ea7f0ceaa55169fbbbe1
folasm87 <ayodeji.folashade@gmail.com>
2016-08-31 12:09:49 +0100

    2016-08-31's java workout

---
+++ contributions/folasm87/java/Classes & Objects/2016-08-31.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Using `static` fields
+Compare classes and not class names
+Fields with Java **Reflection API**
