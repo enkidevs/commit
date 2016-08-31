461474e9f044832dd571bb0a1d4156fa4c990dbf
Morion <morion.self@gmail.com>
2016-08-31 06:52:49 +0100

    2016-08-30's java workout

---
+++ contributions/Morion/java/Fundamentals/2016-08-30.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Inheriting a constructor from a superclass
+Metadata: setting a file's owner
+Using an interface as a parameter
