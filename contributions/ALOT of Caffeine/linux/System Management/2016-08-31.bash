5bff973e24ae79c350b318bf3a31af733b0e4363
ALOT of Caffeine <sebastiancgoeb@gmail.com>
2016-08-31 11:17:14 +0100

    2016-08-31's linux workout

---
+++ contributions/ALOT of Caffeine/linux/System Management/2016-08-31.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Cancel a system shutdown
