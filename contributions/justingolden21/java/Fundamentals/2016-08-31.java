ad63eb19345f3036c1149d1ea9c2e3568fcf619e
justingolden21 <justingolden21@gmail.com>
2016-08-31 20:24:44 +0100

    2016-08-31's java workout

---
+++ contributions/justingolden21/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
