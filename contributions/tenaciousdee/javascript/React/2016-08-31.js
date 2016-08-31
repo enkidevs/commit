ee054e45a41ab8b5f54070d8d572b22f5dd8f1ef
tenaciousdee <dennisepnd@gmail.com>
2016-08-31 23:16:17 +0100

    2016-08-31's javascript workout

---
+++ contributions/tenaciousdee/javascript/React/2016-08-31.js
@@ -0,0 +1,6 @@
+Pixel values in style props for **React**
+Props in getInitialState Is an        
+Anti-Pattern
+Expose Component Functions in **React**
+Shortcut for transferring props
+Spread attributes
