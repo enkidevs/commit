31990027255a729a422a37f5a8494eebf0bd7714
Magduska <magda6.06@wp.pl>
2016-08-31 06:51:55 +0100

    2016-08-31's java workout

---
+++ contributions/Magduska/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
+Using an interface as a parameter
