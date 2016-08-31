e2b213337da29a2c533c2eb35aa2881fdaaa54be
Kiyazasu <kenpo1469@gmail.com>
2016-08-31 09:57:41 +0100

    2016-08-30's java workout

---
+++ contributions/Kiyazasu/java/Fundamentals/2016-08-30.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Inheriting a constructor from a superclass
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
