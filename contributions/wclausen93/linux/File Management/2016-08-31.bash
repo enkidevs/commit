8889d4beda00b7ab8ae2c3307ea487ea1da7028e
wclausen93 <william.h.f.clausen@gmail.com>
2016-08-31 17:08:29 +0100

    2016-08-31's linux workout

---
+++ contributions/wclausen93/linux/File Management/2016-08-31.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Delete commands aliases
