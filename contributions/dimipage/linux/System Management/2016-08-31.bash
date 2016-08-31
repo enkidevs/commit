e5d2cb25fdf45be68b77aa2fc5aeef96b5abd1e6
dimipage <dimi.page@gmail.com>
2016-08-31 15:38:19 +0100

    2016-08-31's linux workout

---
+++ contributions/dimipage/linux/System Management/2016-08-31.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
