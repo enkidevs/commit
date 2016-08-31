6ef898e70f0cb96a3b8e60eb37658b85667d13d2
vikhhr <vikhhr@gmail.com>
2016-08-31 18:58:55 +0100

    2016-08-31's linux workout

---
+++ contributions/vikhhr/linux/System Management/2016-08-31.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Cancel a system shutdown
