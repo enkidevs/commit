206479f980c4a26d76ce95b80b30ffd173d19729
mfrazie2 <michael.s.frazier@gmail.com>
2016-09-01 05:36:00 +0100

    2016-08-31's linux workout

---
+++ contributions/mfrazie2/linux/Terminals & Shells/2016-08-31.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear bash history
+Get to know  your commands with `type`
