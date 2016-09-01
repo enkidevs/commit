fbad1f66c7bf2c910573c2db2be4ef8cfae94a7f
braggae <bregman.y@gmail.com>
2016-09-01 07:37:27 +0100

    2016-09-01's linux workout

---
+++ contributions/braggae/linux/Terminals & Shells/2016-09-01.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Random password generator
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
