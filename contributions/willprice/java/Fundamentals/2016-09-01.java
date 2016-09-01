0a84aebdf2587359752435b464ab939a6007abbc
willprice <will.price94@gmail.com>
2016-09-01 17:48:03 +0100

    2016-09-01's java workout

---
+++ contributions/willprice/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
