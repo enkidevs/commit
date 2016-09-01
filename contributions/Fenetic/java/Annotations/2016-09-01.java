e38b589cfe22de0d171371fdbb1ce074c5bd07f0
Fenetic <esyouit@gmail.com>
2016-09-01 20:23:07 +0100

    2016-09-01's java workout

---
+++ contributions/Fenetic/java/Annotations/2016-09-01.java
@@ -0,0 +1,5 @@
+`@Test` parameters
+Use `@Ignore` to disable JUnit tests
+Using the `@Deprecated` annotation
+Suppressing warnings using `@SuppressWarnings`
+Restricting an annotation's application by using `@Target`
