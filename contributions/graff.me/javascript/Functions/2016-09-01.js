6bae96b18763f6a255e638a2ff461ae8003f2f4d
graff.me <daria.rucka@gmail.com>
2016-09-01 07:57:17 +0100

    2016-09-01's javascript workout

---
+++ contributions/graff.me/javascript/Functions/2016-09-01.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Get query/url variables
+Function call method
