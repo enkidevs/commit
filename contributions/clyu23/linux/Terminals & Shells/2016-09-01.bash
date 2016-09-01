ca0507ca08b4403918f18f95fc46591af3c50dca
clyu23 <claudio.yuri@gmail.com>
2016-09-01 10:46:56 +0100

    2016-09-01's linux workout

---
+++ contributions/clyu23/linux/Terminals & Shells/2016-09-01.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Find any Unix / Linux command
+Quick `bash` shortcuts 
+Keep useful commands in your shell history with tags
