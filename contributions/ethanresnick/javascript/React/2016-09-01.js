34145e3a9abe26bcda297344882319863b4fd852
ethanresnick <ethan.resnick@gmail.com>
2016-09-01 15:31:32 +0100

    2016-09-01's javascript workout

---
+++ contributions/ethanresnick/javascript/React/2016-09-01.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Props in getInitialState Is an        
+Anti-Pattern
+Use **React** with other libraries
+Immutability helpers in **React**
+Keyed fragments in **React**
