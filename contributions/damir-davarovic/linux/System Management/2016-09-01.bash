323a680694d4be4730fb57a5ca53a1c927b1240e
damir-davarovic <damir.davarovic@gmail.com>
2016-09-01 21:32:35 +0100

    2016-09-01's linux workout

---
+++ contributions/damir-davarovic/linux/System Management/2016-09-01.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
