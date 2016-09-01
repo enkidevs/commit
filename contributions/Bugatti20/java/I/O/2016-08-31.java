af395383cd74371e3017a1ca14e84a19755236fa
Bugatti20 <1443686073@qq.com>
2016-09-01 16:19:51 +0100

    2016-08-31's java workout

---
+++ contributions/Bugatti20/java/I/O/2016-08-31.java
@@ -0,0 +1,5 @@
+How to list all files in a directory that match a filename extension in Java
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Reading file attributes
+Using the `OpenOption...` vararg to define file writing behavior
