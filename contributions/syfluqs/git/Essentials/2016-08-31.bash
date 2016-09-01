04513690c6f479a267685d8eb7c870e8740bbca0
syfluqs <syfluqs@gmail.com>
2016-09-01 16:29:42 +0100

    2016-08-31's git workout

---
+++ contributions/syfluqs/git/Essentials/2016-08-31.bash
@@ -0,0 +1,5 @@
+How to set your username and email
+Limiting log output by time
+Search by commit message keyword
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
