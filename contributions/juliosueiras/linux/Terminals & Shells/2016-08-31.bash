3da94da36a84d68229f7ee6159b88b570e69652d
juliosueiras <juliosueiras@gmail.com>
2016-09-01 01:51:44 +0100

    2016-08-31's linux workout

---
+++ contributions/juliosueiras/linux/Terminals & Shells/2016-08-31.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Random password generator
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
