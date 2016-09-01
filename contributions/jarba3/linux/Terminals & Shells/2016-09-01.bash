65d6e7df60417b5afff08143e21aaa08b67fe0e6
jarba3 <bjobard@gmail.com>
2016-09-01 12:09:52 +0100

    2016-09-01's linux workout

---
+++ contributions/jarba3/linux/Terminals & Shells/2016-09-01.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
