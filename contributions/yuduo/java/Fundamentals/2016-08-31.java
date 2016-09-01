5cc52919daf1639e8c090100b251c10e43fc7b33
yuduo <yuduozhou@gmail.com>
2016-09-01 06:15:14 +0100

    2016-08-31's java workout

---
+++ contributions/yuduo/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
