cb05f4399720fe2ed5ee2ad15f194feca6f2c08c
jakew <jcbod101@gmail.com>
2016-09-01 19:28:28 +0100

    2016-09-01's linux workout

---
+++ contributions/jakew/linux/Terminals & Shells/2016-09-01.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Terminal incognito mode
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
