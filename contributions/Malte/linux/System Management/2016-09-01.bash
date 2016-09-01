d3f2ab4e448adfa4d8a05d42dcfdfb61f03a73eb
Malte <skyguardian42@gmail.com>
2016-09-01 07:40:03 +0100

    2016-09-01's linux workout

---
+++ contributions/Malte/linux/System Management/2016-09-01.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Commands to shutdown or restart the system
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
