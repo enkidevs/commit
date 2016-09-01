320959dbb884fb65e50c0f94225e7c31054c37c9
oroszgy <oroszgy@gmail.com>
2016-09-01 14:40:28 +0100

    2016-09-01's java workout

---
+++ contributions/oroszgy/java/I/O/2016-09-01.java
@@ -0,0 +1,5 @@
+Create system file paths using the `Path` class
+Checking for the existence of files and directories
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
