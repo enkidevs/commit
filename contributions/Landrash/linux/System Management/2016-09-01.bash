dcd3ba8a8c0be17fb651fcb71ca288ecb46b7c3d
Landrash <landrash@gmail.com>
2016-09-01 08:12:58 +0100

    2016-09-01's linux workout

---
+++ contributions/Landrash/linux/System Management/2016-09-01.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Get hardware stack details with `lspci`
