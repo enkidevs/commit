c16bbeac6ad158fdafcf091e1bb4f5d537f73d39
ThiagoAnjos <tafas.anjos@gmail.com>
2016-09-01 11:09:50 +0100

    2016-09-01's linux workout

---
+++ contributions/ThiagoAnjos/linux/System Management/2016-09-01.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Finding Open Files With `lsof`
