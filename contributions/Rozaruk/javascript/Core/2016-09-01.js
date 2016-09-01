27617392c04318da750994dc53f8e9338d67f843
Rozaruk <josm.presley@gmail.com>
2016-09-01 17:53:21 +0100

    2016-09-01's javascript workout

---
+++ contributions/Rozaruk/javascript/Core/2016-09-01.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Data type comparison in `switch` statements
+Avoid using `with`
+Prevent background image flicker in **IE**
+Getting the *real* dimensions of an image
