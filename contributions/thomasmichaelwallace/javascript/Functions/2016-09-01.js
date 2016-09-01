96fb2f4496f1847fbb4361e38a7f080f41960993
thomasmichaelwallace <thomasmichaelwallace@gmail.com>
2016-09-01 09:21:38 +0100

    2016-09-01's javascript workout

---
+++ contributions/thomasmichaelwallace/javascript/Functions/2016-09-01.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Functions can be declared after use
+IIFE pattern
+Check if a document is done loading
