82bf69dc8dfd3e8f7c0c0402ce772514ce644042
crsanti <santi.camargo.rodriguez@gmail.com>
2016-09-01 11:44:00 +0100

    2016-09-01's linux workout

---
+++ contributions/crsanti/linux/System Management/2016-09-01.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
