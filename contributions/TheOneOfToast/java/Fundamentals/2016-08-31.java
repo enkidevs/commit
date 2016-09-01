bb1a3c1e0579b845713d359de42880420fb132e5
TheOneOfToast <jjbdlsantos@gmail.com>
2016-09-01 13:08:33 +0100

    2016-08-31's java workout

---
+++ contributions/TheOneOfToast/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
+Using an interface as a parameter
