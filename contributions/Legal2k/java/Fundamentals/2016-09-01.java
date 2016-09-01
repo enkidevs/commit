762cfb9a3a5b3366891e1f89a0d209a0215cd0a3
Legal2k <nikolai.pulman@npu.ee>
2016-09-01 16:38:12 +0100

    2016-09-01's java workout

---
+++ contributions/Legal2k/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
