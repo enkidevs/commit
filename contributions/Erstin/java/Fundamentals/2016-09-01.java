c54dae0b9f8dbfbd1a4e0edd8bbd0aaded4e4147
Erstin <ced.magno@gmail.com>
2016-09-01 06:55:11 +0100

    2016-09-01's java workout

---
+++ contributions/Erstin/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Connecting to FTP using Java
+Catch multiple exceptions in a single `catch` block
+Distinction between `public` and `private` methods
+Using `synchronized` statements
