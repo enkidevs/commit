fe2812c3666e682612e4728af18824bee2ec7c02
ExcaliburZero <cwellsny@nycap.rr.com>
2016-09-01 13:10:44 +0100

    2016-09-01's java workout

---
+++ contributions/ExcaliburZero/java/I/O/2016-09-01.java
@@ -0,0 +1,5 @@
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Using the `OpenOption...` vararg to define file writing behavior
+List contents of a directory with `newDirectoryStream`
