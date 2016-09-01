df5bbc9fc32badda93ffbce66521f34bdc640c45
rivergillis <jrivergillis@gmail.com>
2016-09-01 03:49:20 +0100

    2016-08-31's linux workout

---
+++ contributions/rivergillis/linux/Terminals & Shells/2016-08-31.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Find any Unix / Linux command
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
