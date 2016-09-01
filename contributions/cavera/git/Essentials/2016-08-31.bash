0025ad7529bfa8b7ee0d58184665089fd8c0a466
cavera <cavera.de@gmail.com>
2016-09-01 04:10:50 +0100

    2016-08-31's git workout

---
+++ contributions/cavera/git/Essentials/2016-08-31.bash
@@ -0,0 +1,5 @@
+Using aliases for git commands
+Make your log output pretty
+Use `short` status to make output more compact
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
