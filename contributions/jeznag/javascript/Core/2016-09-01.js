0f23f2361766a3d75c1e807848018ce2d80e09f8
jeznag <jeremy.nagel@learnosity.com>
2016-09-02 00:54:04 +0100

    2016-09-01's javascript workout

---
+++ contributions/jeznag/javascript/Core/2016-09-01.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Format currency in JavaScript
+Infix operators are left-associative
+Functions return `undefined` by default
+Prevent background image flicker in **IE**
