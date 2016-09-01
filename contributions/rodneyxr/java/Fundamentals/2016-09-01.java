49e84addf5d1632ba5b0fd7824a4298b9f30c14a
rodneyxr <rodneyxr@gmail.com>
2016-09-01 14:40:42 +0100

    2016-09-01's java workout

---
+++ contributions/rodneyxr/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
