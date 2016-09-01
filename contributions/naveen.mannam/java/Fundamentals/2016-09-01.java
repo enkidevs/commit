6495f856f66ba35e0365534519c1385cf3eb131d
naveen.mannam <naveen.mannam@gmail.com>
2016-09-01 11:38:13 +0100

    2016-09-01's java workout

---
+++ contributions/naveen.mannam/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between ```HashMap``` and ```Hashtable```
+Use strings in a `switch` statement
+Always specify type parameter
+Detect or prevent integer overflow
