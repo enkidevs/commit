19c66eac951139c3efdd17d2b9847c2a3a5dcc0e
pjanik <paweljnk@gmail.com>
2016-09-01 21:46:04 +0100

    2016-09-01's javascript workout

---
+++ contributions/pjanik/javascript/Functions/2016-09-01.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+`.bind()` 
+Easily generate a random `HEX` color
+Function call method
