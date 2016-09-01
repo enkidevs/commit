e70eb0b30b6e1fad4c05dabe43aab57760e16d3c
maxbaluev <maxbaluev@outlook.com>
2016-09-01 07:06:23 +0100

    2016-08-31's javascript workout

---
+++ contributions/maxbaluev/javascript/Functions/2016-08-31.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Check if a document is done loading
