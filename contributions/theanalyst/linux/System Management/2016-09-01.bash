16873e1c248081df4a575ad3529156dbc4c2e6fa
theanalyst <abhishek.lekshmanan@gmail.com>
2016-09-01 13:26:13 +0100

    2016-09-01's linux workout

---
+++ contributions/theanalyst/linux/System Management/2016-09-01.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Common log files
+Preventing a user from logging into the system
