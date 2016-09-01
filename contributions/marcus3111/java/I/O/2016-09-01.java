eec516cc55364565e79c0f0d5633821af3676c88
marcus3111 <marcus3111@gmail.com>
2016-09-01 12:46:41 +0100

    2016-09-01's java workout

---
+++ contributions/marcus3111/java/I/O/2016-09-01.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+How to list all files in a directory that match a filename extension in Java
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
