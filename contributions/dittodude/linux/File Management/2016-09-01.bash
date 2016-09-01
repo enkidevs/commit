82ab5dd82d7395d8b8ec97d2392c32dca5cc1b3b
dittodude <gkdragondude@gmail.com>
2016-09-01 20:01:52 +0100

    2016-09-01's linux workout

---
+++ contributions/dittodude/linux/File Management/2016-09-01.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Delete commands aliases
