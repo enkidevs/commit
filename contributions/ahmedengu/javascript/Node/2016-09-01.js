75e873219502dd5cb453cb841436d964555c4721
ahmedengu <ahmedengu@gmail.com>
2016-09-01 20:23:17 +0100

    2016-09-01's javascript workout

---
+++ contributions/ahmedengu/javascript/Node/2016-09-01.js
@@ -0,0 +1,5 @@
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+Manage child processes in **Node**
+Bulk-Write in **Node** with `cork()`
+`try-catch` only for **sync** code
