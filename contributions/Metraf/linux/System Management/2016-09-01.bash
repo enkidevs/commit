bbbeb26a1769b799391e5019e80baa1536a7fd41
Metraf <spertin94@gmail.com>
2016-09-01 19:47:56 +0100

    2016-09-01's linux workout

---
+++ contributions/Metraf/linux/System Management/2016-09-01.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Get hardware stack details with `lspci`
