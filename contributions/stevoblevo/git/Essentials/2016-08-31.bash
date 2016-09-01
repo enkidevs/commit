7774b5408e2d7da96175b9d61026e27250e87886
stevoblevo <stevoblevo@gmail.com>
2016-09-01 10:26:20 +0100

    2016-08-31's git workout

---
+++ contributions/stevoblevo/git/Essentials/2016-08-31.bash
@@ -0,0 +1,5 @@
+You can use git offline!
+Make your log output pretty
+Search for commits by author
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
