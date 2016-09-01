f5147a3cb06c9f4f9af27356a5e9d41ef5d48601
ghanto <kamil.rokosz@outlook.com>
2016-09-01 07:49:02 +0100

    2016-09-01's linux workout

---
+++ contributions/ghanto/linux/File Management/2016-09-01.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Delete commands aliases
