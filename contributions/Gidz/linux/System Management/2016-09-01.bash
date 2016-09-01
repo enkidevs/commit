760eae4310dea47aed977c4d89a4543f15c35fb9
Gidz <gidzpaul95@gmail.com>
2016-09-01 14:20:31 +0100

    2016-09-01's linux workout

---
+++ contributions/Gidz/linux/System Management/2016-09-01.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
