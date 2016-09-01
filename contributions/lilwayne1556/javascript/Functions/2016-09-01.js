cf15e80d2ec7146d8635969063f4da03310f068e
lilwayne1556 <lilwayne1556@gmail.com>
2016-09-01 15:35:45 +0100

    2016-09-01's javascript workout

---
+++ contributions/lilwayne1556/javascript/Functions/2016-09-01.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+`.bind()` 
+Easily generate a random `HEX` color
+Social sharing without widgets
