af762fe956d71193c766be58e0ff5764e2984728
haddag.abdelkader <haddag.abdelkader@gmail.com>
2016-09-01 09:39:36 +0100

    2016-09-01's linux workout

---
+++ contributions/haddag.abdelkader/linux/System Management/2016-09-01.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
