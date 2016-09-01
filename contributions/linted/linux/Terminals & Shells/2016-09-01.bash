c87a32b946cf0f33dcf522ec631bc1c0f5653970
linted <linted90@hotmail.com>
2016-09-01 21:46:52 +0100

    2016-09-01's linux workout

---
+++ contributions/linted/linux/Terminals & Shells/2016-09-01.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
