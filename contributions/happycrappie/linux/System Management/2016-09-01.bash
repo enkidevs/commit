7fe6acc9c3336271b9eba60c232258c30f2838b7
happycrappie <imerycdasilva@gmail.com>
2016-09-01 16:33:24 +0100

    2016-09-01's linux workout

---
+++ contributions/happycrappie/linux/System Management/2016-09-01.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
