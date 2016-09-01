06581ccbb50254fe191f5f88af3167d7dddcb2f7
edm00se <edm00se@gmail.com>
2016-09-02 00:41:50 +0100

    2016-09-01's javascript workout

---
+++ contributions/edm00se/javascript/Functions/2016-09-01.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Function call method
+Move cursor at the end of text input
+Social sharing without widgets
