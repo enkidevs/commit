e0db7e06c101a7782392c3f1f324f26be878bbc2
sagiben <ilnextbus@gmail.com>
2016-09-01 05:03:09 +0100

    2016-09-01's python workout

---
+++ contributions/sagiben/python/Data Structures/2016-09-01.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Built-in `list` methods
+There is more to copying
+`weakref` callbacks
+Binary record packing with `bytearray`
