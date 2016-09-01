51d6b8711df18f49fffaf7e7cbc4ad57bb335470
badbart <tom.zille@gmail.com>
2016-09-01 04:15:42 +0100

    2016-09-01's linux workout

---
+++ contributions/badbart/linux/File Management/2016-09-01.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Using `touch` to alter files modification time
