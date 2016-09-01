2f1dadec58f752687fc25d943c05781f54d35bb2
tenaciousdee <dennisepnd@gmail.com>
2016-09-01 09:22:47 +0100

    2016-09-01's javascript workout

---
+++ contributions/tenaciousdee/javascript/Functions/2016-09-01.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Anonymous functions
