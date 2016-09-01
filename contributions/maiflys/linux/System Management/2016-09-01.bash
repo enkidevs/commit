fcffc97099ea5abe0e8f75a98e9dc199bd7dce29
maiflys <mai_media@outlook.com>
2016-09-01 18:13:32 +0100

    2016-09-01's linux workout

---
+++ contributions/maiflys/linux/System Management/2016-09-01.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
