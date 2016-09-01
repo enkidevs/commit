2832be388e065ae29e36477f3da6da568f0c3025
Billyboy <billybellchambers@yahoo.com>
2016-09-01 15:21:03 +0100

    2016-09-01's javascript workout

---
+++ contributions/Billyboy/javascript/Functions/2016-09-01.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function constructor vs. function declaration vs. function expression
+IIFE pattern
+Move cursor at the end of text input
+Check if a document is done loading
