fdbda4cd0a2a4d908828d959d67e17e47ead737c
RareEvent <ham@perfect-kreim.de>
2016-09-01 12:21:31 +0100

    2016-09-01's linux workout

---
+++ contributions/RareEvent/linux/System Management/2016-09-01.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
