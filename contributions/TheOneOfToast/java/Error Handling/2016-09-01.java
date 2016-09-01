53418a3b32d9a39346bfdc607364350e78a3f33d
TheOneOfToast <jjbdlsantos@gmail.com>
2016-09-01 23:55:34 +0100

    2016-09-01's java workout

---
+++ contributions/TheOneOfToast/java/Error Handling/2016-09-01.java
@@ -0,0 +1,5 @@
+Converting stack trace to a string
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Careful Numeric Data Conversions
