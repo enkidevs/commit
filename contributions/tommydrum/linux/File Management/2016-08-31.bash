be41478d45687c573c0eb054f12590ed65429327
tommydrum <tommydrum777@gmail.com>
2016-09-01 06:12:41 +0100

    2016-08-31's linux workout

---
+++ contributions/tommydrum/linux/File Management/2016-08-31.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
