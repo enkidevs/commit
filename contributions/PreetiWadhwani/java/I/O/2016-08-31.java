f2c237a16a839971d0717e21a0d6a5c5b899d8b3
PreetiWadhwani <pwadhwani2592@gmail.com>
2016-09-01 07:00:23 +0100

    2016-08-31's java workout

---
+++ contributions/PreetiWadhwani/java/I/O/2016-08-31.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Create system file paths using the `Path` class
+Retrieving file store attributes
+Deleting files and directories
+The `Console` class
