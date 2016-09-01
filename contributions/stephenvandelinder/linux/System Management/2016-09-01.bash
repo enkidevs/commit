def0e49f5db8dcd76c521cc1a8ac508b0c0c0f99
stephenvandelinder <stephen.vandelinder@gmail.com>
2016-09-01 19:12:31 +0100

    2016-09-01's linux workout

---
+++ contributions/stephenvandelinder/linux/System Management/2016-09-01.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
