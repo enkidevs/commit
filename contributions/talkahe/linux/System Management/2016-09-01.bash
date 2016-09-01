6152438dda16dc76c84c3cfee13d737810de6ee6
talkahe <jorges996@gmail.com>
2016-09-01 03:20:45 +0100

    2016-09-01's linux workout

---
+++ contributions/talkahe/linux/System Management/2016-09-01.bash
@@ -0,0 +1,7 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Preventing a user from logging into the system
+Commands to shutdown or restart the system
