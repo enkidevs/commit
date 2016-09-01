f65ff44f071357dff44ded22380b5a781fa6c289
andrsussa <andrsussa@gmail.com>
2016-09-01 08:58:35 +0100

    2016-09-01's linux workout

---
+++ contributions/andrsussa/linux/System Management/2016-09-01.bash
@@ -0,0 +1,7 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Commands to shutdown or restart the system
