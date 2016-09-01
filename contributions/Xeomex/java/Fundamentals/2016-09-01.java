774d0e44147b2a4aca3ea70a13402747b295f9d7
Xeomex <quentinrouviere@gmail.com>
2016-09-01 16:37:59 +0100

    2016-09-01's java workout

---
+++ contributions/Xeomex/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Cropping images
+Uses of the `final` keyword
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
