5ddbfe4f8ef3970a12b19656107209410963da1b
ACarlsson2 <acarlsson1992@gmail.com>
2016-09-01 09:36:15 +0100

    2016-09-01's java workout

---
+++ contributions/ACarlsson2/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
