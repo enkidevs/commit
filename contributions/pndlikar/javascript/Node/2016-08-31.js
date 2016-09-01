2b43ff6436b67c25be42daca02dc835fc9b6305e
pndlikar <pndlikar@gmail.com>
2016-09-01 06:35:01 +0100

    2016-08-31's javascript workout

---
+++ contributions/pndlikar/javascript/Node/2016-08-31.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+First-error callbacks in **Node**
+Wrapping errors in Node.js using _node-verror_
+Handle `JSON.parse` error in Node.js
+`process.nextTick()` callback pattern
