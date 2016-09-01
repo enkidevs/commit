8dd942c0866e02dbf6eb5fbb72d7b932a0ea83b7
edvidarez <edvidarez@gmail.com>
2016-09-01 18:06:12 +0100

    2016-09-01's java workout

---
+++ contributions/edvidarez/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Inheriting a constructor from a superclass
+Using `Printstream.format` to format a string before printing
+Constant notation
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
