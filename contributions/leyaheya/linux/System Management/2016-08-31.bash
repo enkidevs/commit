7c09a0dc11776dea25401f56b89903a6e49fcae6
leyaheya <lea.dorado@gmail.com>
2016-09-01 01:24:41 +0100

    2016-08-31's linux workout

---
+++ contributions/leyaheya/linux/System Management/2016-08-31.bash
@@ -0,0 +1,7 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
