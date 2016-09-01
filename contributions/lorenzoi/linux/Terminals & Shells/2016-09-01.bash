650b4c21bc840e31a23e7cbcdde2a10a0de5cff8
lorenzoi <lorenzoiannuzzi@gmail.com>
2016-09-01 05:42:24 +0100

    2016-09-01's linux workout

---
+++ contributions/lorenzoi/linux/Terminals & Shells/2016-09-01.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Aliasing ssh connections
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
