e6f50e00ed780602f975e3e0537b2346c88662f4
Poger <gkonupek@gmail.com>
2016-09-01 13:23:55 +0100

    2016-09-01's java workout

---
+++ contributions/Poger/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Connecting to FTP using Java
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
