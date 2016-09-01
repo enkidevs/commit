e438c1cc71cc7fa966254ebb068db66d049d443c
alexraby <araby424@gmail.com>
2016-09-01 03:03:11 +0100

    2016-08-31's javascript workout

---
+++ contributions/alexraby/javascript/Core/2016-08-31.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Precision
+Multi-line string variables
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
