a922f7cc40bb956c614d00ac84759968ed2536f9
Intersys <int3rsys@live.com>
2016-09-01 11:02:21 +0100

    2016-09-01's javascript workout

---
+++ contributions/Intersys/javascript/Core/2016-09-01.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Using the double tilde `~~` 
+Hoisting applies only to variable declarations, not initializations
+Detect **DO NOT TRACK** status
+`console.*` in JavaScript
