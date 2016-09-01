d638f8fba2478ead2f29a9694de87d9a6e25357c
Stedob <steinzi.dominik@gmail.com>
2016-09-01 12:49:54 +0100

    2016-09-01's java workout

---
+++ contributions/Stedob/java/Data Types/2016-09-01.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Converting a string to upper or lower case
+Double brace initialization
+Retrieve the component type of an array
+String concatenation in Java
