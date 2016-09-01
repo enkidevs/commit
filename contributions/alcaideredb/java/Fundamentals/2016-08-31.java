b968acf7d2a0805e70bc144122c5648165a0b4e6
alcaideredb <alcaideredb@gmail.com>
2016-09-01 13:33:51 +0100

    2016-08-31's java workout

---
+++ contributions/alcaideredb/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
