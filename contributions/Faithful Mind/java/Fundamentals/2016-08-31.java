8aebac19de66bf276553a1b369373d71a35ccc4c
Faithful Mind <lpyy15@gmail.com>
2016-09-01 10:01:06 +0100

    2016-08-31's java workout

---
+++ contributions/Faithful Mind/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,4 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
