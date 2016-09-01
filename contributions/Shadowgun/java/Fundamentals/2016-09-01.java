21f8cb866f1a48c83dae68017ed621480a16994f
Shadowgun <shadowgun@i.ua>
2016-09-01 20:58:51 +0100

    2016-09-01's java workout

---
+++ contributions/Shadowgun/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Cropping images
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
