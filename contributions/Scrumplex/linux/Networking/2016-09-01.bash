c2e2678dfa462b22a70c87da0d7fdb313d8425d0
Scrumplex <sefa6727@gmail.com>
2016-09-01 19:40:10 +0100

    2016-09-01's linux workout

---
+++ contributions/Scrumplex/linux/Networking/2016-09-01.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Multiple IP Address with aliasing 
+Useful ssh client optimizations 
+List your IPv6 configuration
+Staying connected remotely via SSH without an interactive shell
