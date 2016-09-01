64647b0d372876b130d48c5914efd7e7de2413d1
Geroxan <alik810@seznam.cz>
2016-09-01 15:11:54 +0100

    2016-09-01's javascript workout

---
+++ contributions/Geroxan/javascript/Async Patterns/2016-09-01.js
@@ -0,0 +1,5 @@
+Name your callback functions to avoid 'callback hell'
+Transform values into promises
+Flow control using _co_
+The difference between _parallel_, _asynchronous_ and _concurrent_
+Inversion of Control
