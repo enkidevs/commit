3f06209111bbdc6d82b2be1295b046ec07d1af4f
pamigomp <pamigomp@op.pl>
2016-09-01 06:57:23 +0100

    2016-09-01's java workout

---
+++ contributions/pamigomp/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Using Inheritance to reduce code repetition
+Using bounded type parameters in generic methods
+Calling one constructor from another
+Always specify type parameter
+Using an interface as a parameter
