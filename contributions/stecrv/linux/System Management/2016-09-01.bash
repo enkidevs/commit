5ef57c7aa1178779f703eca250cb1ba820133906
stecrv <stefano.caravana@gmail.com>
2016-09-01 11:58:19 +0100

    2016-09-01's linux workout

---
+++ contributions/stecrv/linux/System Management/2016-09-01.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Cancel a system shutdown
