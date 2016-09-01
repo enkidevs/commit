015b323fcee4463c7e9d6474ace55a9e7e6f9dae
paraneus <aviki.friedli@gmail.com>
2016-09-01 16:19:18 +0100

    2016-09-01's linux workout

---
+++ contributions/paraneus/linux/File Management/2016-09-01.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
+Delete commands aliases
