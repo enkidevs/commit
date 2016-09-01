cb2218b867820f233dc0025e9c89d37c2baa5070
vishalsharma <vishalsharma0704@gmail.com>
2016-09-01 15:56:53 +0100

    2016-08-31's java workout

---
+++ contributions/vishalsharma/java/Fundamentals/2016-08-31.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
