7afa2ef4ff98d6cfe799af0b532d1561e1d31acd
peeyush <peeyush.pathak18@gmail.com>
2016-09-01 06:46:49 +0100

    2016-08-31's linux workout

---
+++ contributions/peeyush/linux/System Management/2016-08-31.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
