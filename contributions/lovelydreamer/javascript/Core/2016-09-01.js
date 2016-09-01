a0e639bcc1dc061c1a4d3d33759fa2a8d7d859f3
lovelydreamer <christinamitchell08@gmail.com>
2016-09-01 14:12:43 +0100

    2016-09-01's javascript workout

---
+++ contributions/lovelydreamer/javascript/Core/2016-09-01.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Use `splice()` to remove an item from an array
+Using assignment operators
+Hoisting applies only to variable declarations, not initializations
+How to merge two arrays
