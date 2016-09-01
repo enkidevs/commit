08f98d6acc7ae8c5ba6591555a71666756abb5c1
Tasqa <me@tako.mx>
2016-09-01 09:14:58 +0100

    2016-09-01's linux workout

---
+++ contributions/Tasqa/linux/System Management/2016-09-01.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
