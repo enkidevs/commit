671c0ca2da22d2148bdbdd1b7d1b95c96f9fdf82
sharef88 <gamerzi@gmail.com>
2016-09-01 15:09:23 +0100

    2016-09-01's linux workout

---
+++ contributions/sharef88/linux/System Management/2016-09-01.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
