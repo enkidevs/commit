c05631572fa49b455fc7e817c2830266159fdc04
geludancel <dancel.gilo@gmail.com>
2016-09-01 16:23:54 +0100

    2016-08-31's linux workout

---
+++ contributions/geludancel/linux/Terminals & Shells/2016-08-31.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Quick `bash` shortcuts 
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
