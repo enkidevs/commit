256cb830701c94d6d3c9a6cc1b6cc08631c9fb34
k.matusiewicz <k.matusiewicz@gmail.com>
2016-09-01 12:01:22 +0100

    2016-09-01's java workout

---
+++ contributions/k.matusiewicz/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
