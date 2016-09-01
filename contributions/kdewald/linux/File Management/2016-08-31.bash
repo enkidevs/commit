ddb037b695eb4f948616abdaee00e3c5bd7bad82
kdewald <kevin.dewald@ieee.org>
2016-09-01 02:05:09 +0100

    2016-08-31's linux workout

---
+++ contributions/kdewald/linux/File Management/2016-08-31.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Working with `.gz` files
