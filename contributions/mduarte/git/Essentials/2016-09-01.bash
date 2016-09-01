cb2627b62cb9c9e382aaf81bd02784c9b0b7fb4f
mduarte <matiasduarte1@hotmail.com>
2016-09-01 15:08:13 +0100

    2016-09-01's git workout

---
+++ contributions/mduarte/git/Essentials/2016-09-01.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+The three states in git
+Search for commits by author
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
