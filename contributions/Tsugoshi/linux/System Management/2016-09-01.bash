b7457aa0618f9609a6171916a690dd3a22989103
Tsugoshi <marciniakmateusz5@gmail.com>
2016-09-01 21:40:41 +0100

    2016-09-01's linux workout

---
+++ contributions/Tsugoshi/linux/System Management/2016-09-01.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
