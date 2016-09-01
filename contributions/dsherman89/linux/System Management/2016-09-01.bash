09d1ce139ae46c7cb723fc49a90e46d11e6f2d42
dsherman89 <dansherman89@googlemail.com>
2016-09-01 10:53:20 +0100

    2016-09-01's linux workout

---
+++ contributions/dsherman89/linux/System Management/2016-09-01.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Detect your linux distribution
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
