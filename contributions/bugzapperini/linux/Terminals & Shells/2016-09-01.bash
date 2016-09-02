567cd8f056eff5ce1330c04b1decc7f08888e6cb
bugzapperini <shashank.gosavi@niiconsulting.com>
2016-09-02 17:04:42 +0100

    2016-09-01's linux workout

---
+++ contributions/bugzapperini/linux/Terminals & Shells/2016-09-01.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
