79f9cdbedf6e8cf409d8f8bdc168f89a622c4d24
EduFdezSoy <eduardofernandezsoy@gmail.com>
2016-09-02 17:05:06 +0100

    2016-09-02's linux workout

---
+++ contributions/EduFdezSoy/linux/System Management/2016-09-02.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
