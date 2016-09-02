176bf0371d19c6d264bbfe39ce944d212097914b
rhaynel <me@rhynl.io>
2016-09-03 00:52:09 +0100

    2016-09-02's javascript workout

---
+++ contributions/rhaynel/javascript/Ecmascript 2015/2016-09-02.js
@@ -0,0 +1,5 @@
+ES6 dynamic property keys in mongo-like set clauses
+Computed property names in object literals
+Defaults values apply only to `undefined` (and not to `null`)
+Rest parameters
+Strings in ES6
