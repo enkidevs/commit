abf178dfba242e93b90235114d599fc0ccc53a66
supernacho2011 <supernacho2011@gmail.com>
2016-09-02 06:45:47 +0100

    2016-09-01's java workout

---
+++ contributions/supernacho2011/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
