d3715d05b3b746a8d5c37e7ac619f5ab578692dd
lazai <mariusdotspinu@gmail.com>
2016-09-02 07:41:10 +0100

    2016-09-02's java workout

---
+++ contributions/lazai/java/Fundamentals/2016-09-02.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Connecting to FTP using Java
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Using `Printstream.format` to format a string before printing
