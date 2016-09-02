74684a39978dcfce7ed0f8a31d9d11add30cfbc4
belgradeCode <bojanzarkovic@gmail.com>
2016-09-02 20:32:09 +0100

    2016-09-02's javascript workout

---
+++ contributions/belgradeCode/javascript/Core/2016-09-02.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Data type comparison in `switch` statements
+Extra function arguments are undefined by default
+Detect online connection
+Inaccuracy of binary floating-point format
