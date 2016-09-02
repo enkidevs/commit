5a95f5964d9a7118cc14d8a9a31360a834124e6f
hsxpunisher <gavin.sortor@gmail.com>
2016-09-02 18:52:42 +0100

    2016-09-02's linux workout

---
+++ contributions/hsxpunisher/linux/Jobs & Processes/2016-09-02.bash
@@ -0,0 +1,5 @@
+Backgrounding tasks 
+Keeping processes running after you log out
+Fixing the shell in cron
+Common issue with scripts in `cron`
+How to pause a process and run others in the background
