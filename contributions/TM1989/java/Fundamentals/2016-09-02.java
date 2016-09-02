d0079f8e553ac39e27da347024b39f4c598bd406
TM1989 <torben.moeller@gmx.net>
2016-09-02 20:11:29 +0100

    2016-09-02's java workout

---
+++ contributions/TM1989/java/Fundamentals/2016-09-02.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
