7093c187be765794b8488d96e24d79bd2b9cf729
Rocket3G <saverkuil@gmail.com>
2016-09-02 19:35:41 +0100

    2016-09-02's linux workout

---
+++ contributions/Rocket3G/linux/System Management/2016-09-02.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
