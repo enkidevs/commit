0b704702388ec1acca68bd3decccdaa68b5589a2
jcarneiro <jvmcarneiro@openmailbox.org>
2016-09-02 02:43:31 +0100

    2016-09-01's linux workout

---
+++ contributions/jcarneiro/linux/File Management/2016-09-01.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
