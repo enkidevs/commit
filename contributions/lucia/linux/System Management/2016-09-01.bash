b6825911172df248aace3e90f141b0557a638988
lucia <mikoto.yu51@gmail.com>
2016-09-02 17:19:55 +0100

    2016-09-01's linux workout

---
+++ contributions/lucia/linux/System Management/2016-09-01.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
