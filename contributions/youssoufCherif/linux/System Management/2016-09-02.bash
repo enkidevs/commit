ae87adafe092017490c15c2f016257572af4d055
youssoufCherif <youssouf.cherif.hamed@gmail.com>
2016-09-02 15:15:03 +0100

    2016-09-02's linux workout

---
+++ contributions/youssoufCherif/linux/System Management/2016-09-02.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
