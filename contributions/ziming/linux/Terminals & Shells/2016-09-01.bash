f1dee8065c4d6f1ff4b5be3ff955ab33073bfd19
ziming <ziming.lan@gmail.com>
2016-09-02 02:58:36 +0100

    2016-09-01's linux workout

---
+++ contributions/ziming/linux/Terminals & Shells/2016-09-01.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Clear the terminal instantly
+Random password generator
+Conditional command execution
+
+(`&&` operator)
