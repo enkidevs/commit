72fe452e0b69608d5fcd5c159304d5775708c1ca
ple103 <peterhuyle@outlook.com>
2016-09-02 23:54:49 +0100

    2016-09-02's linux workout

---
+++ contributions/ple103/linux/Terminals & Shells/2016-09-02.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
