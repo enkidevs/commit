0ed8ac0ae51b27f3422e6044b2c17ea644606c62
larongbingo <larongbingo@gmail.com>
2016-09-02 12:54:34 +0100

    2016-09-01's java workout

---
+++ contributions/larongbingo/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
