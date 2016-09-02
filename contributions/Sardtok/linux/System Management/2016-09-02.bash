87b65282368205e74270cb30e6371091ac54310e
Sardtok <sha@computas.com>
2016-09-02 07:43:56 +0100

    2016-09-02's linux workout

---
+++ contributions/Sardtok/linux/System Management/2016-09-02.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Finding Open Files With `lsof`
