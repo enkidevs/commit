aa898bbc8d56fe83349c5f2cd8bc953b0373d4aa
naodha <nanozpoid@gmail.com>
2016-09-02 16:35:10 +0100

    2016-09-02's java workout

---
+++ contributions/naodha/java/I/O/2016-09-02.java
@@ -0,0 +1,5 @@
+Checking for the existence of files and directories
+Reading file attributes
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
