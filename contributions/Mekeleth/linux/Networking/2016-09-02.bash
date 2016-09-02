c5355b065a4371d5dcef8dacc5cc923768eadbfb
Mekeleth <garadim12@gmail.com>
2016-09-02 22:28:11 +0100

    2016-09-02's linux workout

---
+++ contributions/Mekeleth/linux/Networking/2016-09-02.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Check `iptables` firewall status
+Find services running on your host
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
