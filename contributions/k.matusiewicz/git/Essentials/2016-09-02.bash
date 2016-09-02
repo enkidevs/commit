fe5743492b71940ce974027b59cc621f272f264b
k.matusiewicz <k.matusiewicz@gmail.com>
2016-09-02 16:04:23 +0100

    2016-09-02's git workout

---
+++ contributions/k.matusiewicz/git/Essentials/2016-09-02.bash
@@ -0,0 +1,5 @@
+Search by commit message keyword
+Search for commits by author
+How to write a git commit message
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
