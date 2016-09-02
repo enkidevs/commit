8f3cd32f82d7ec1600b9657cf432b1893d55e260
Emin <m@memin.tk>
2016-09-02 06:44:06 +0100

    2016-09-01's linux workout

---
+++ contributions/Emin/linux/System Management/2016-09-01.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Get hardware stack details with `lspci`
