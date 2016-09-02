0bcee5c71132435bcb30355a3c466122aed05cad
Frankenstein <lars.ch.franke@gmail.com>
2016-09-02 08:34:48 +0100

    2016-09-02's python workout

---
+++ contributions/Frankenstein/python/Data Structures/2016-09-02.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Best way to implement a simple `queue` 
+Special attributes of objects and classes
+`queue`s and threads
+Keep things in order with `OrderedDict`
