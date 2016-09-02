e3f7d18d587a7244b41fff97c64615ead8277f68
jacobvarner <jacob.varner14@gmail.com>
2016-09-02 13:36:07 +0100

    2016-09-02's linux workout

---
+++ contributions/jacobvarner/linux/Terminals & Shells/2016-09-02.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
