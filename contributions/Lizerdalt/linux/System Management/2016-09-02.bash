c92e81466ac4ed03f7a6bc03bd36cf1abb3a8738
Lizerdalt <chulip@protonmail.ch>
2016-09-02 11:07:26 +0100

    2016-09-02's linux workout

---
+++ contributions/Lizerdalt/linux/System Management/2016-09-02.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
