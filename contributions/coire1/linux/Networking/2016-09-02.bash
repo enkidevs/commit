e8d41686dccd7973462baae023f8f4b794ebb4c9
coire1 <coire1@gmail.com>
2016-09-02 22:19:33 +0100

    2016-09-02's linux workout

---
+++ contributions/coire1/linux/Networking/2016-09-02.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Disable `IPv6`
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Staying connected remotely via SSH without an interactive shell
