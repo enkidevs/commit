0586ef777be102589d5c3f39cad2ad51e0ec720c
Wazowski <ipodaidar@gmail.com>
2016-09-02 06:03:02 +0100

    2016-09-01's linux workout

---
+++ contributions/Wazowski/linux/System Management/2016-09-01.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
