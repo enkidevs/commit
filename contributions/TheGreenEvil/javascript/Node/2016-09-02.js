488508f8a46cc1a550fbdaa385b5ae6581e1aa86
TheGreenEvil <kirill.matsaberydze@gmail.com>
2016-09-02 20:19:07 +0100

    2016-09-02's javascript workout

---
+++ contributions/TheGreenEvil/javascript/Node/2016-09-02.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Asynchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+The built-in Node debugger
+Automatic compilation for Node with **Nodemon** 
