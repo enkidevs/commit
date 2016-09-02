0f0d0096c7aba7d99a58a2327b3e1d6d54093a72
noroziali <noroziali.a@gmail.com>
2016-09-02 11:29:05 +0100

    2016-09-01's linux workout

---
+++ contributions/noroziali/linux/System Management/2016-09-01.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
