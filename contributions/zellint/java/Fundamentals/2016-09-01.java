d02e723e548a08a63d21890c57cd0b4e6b30c163
zellint <zellint@gmail.com>
2016-09-02 04:37:39 +0100

    2016-09-01's java workout

---
+++ contributions/zellint/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Always specify type parameter
+Supply `toString()` in all classes
