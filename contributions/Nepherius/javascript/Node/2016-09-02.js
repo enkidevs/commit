9c458b330e625158a44e5d7b80ee3e7ef3632d5b
Nepherius <b3st88@gmail.com>
2016-09-02 08:11:19 +0100

    2016-09-02's javascript workout

---
+++ contributions/Nepherius/javascript/Node/2016-09-02.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+Wrapping errors in Node.js using _node-verror_
+`exports` vs. `module.exports` in **Node**
+Middleware/pipeline design pattern
