07647dc63406530bf9d5116668813f895de2bc87
Granra <granra@gmail.com>
2016-09-02 10:34:33 +0100

    2016-09-02's linux workout

---
+++ contributions/Granra/linux/System Management/2016-09-02.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Preventing a user from logging into the system
