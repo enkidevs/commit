2141d1afeb38bb7b5e97b3afd3bc8f03c74cc3b7
Animuslupus <elias-wolf@t-online.de>
2016-09-02 08:19:20 +0100

    2016-09-02's java workout

---
+++ contributions/Animuslupus/java/I/O/2016-09-02.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Reading and writing text files
+Using buffered streams
+Checking for the existence of files and directories
