e35b4c107031d3ef5f4a2cbea7665916138f06b8
BlueInked <stephaniebarnes417@hotmail.com>
2016-09-02 17:51:08 +0100

    2016-09-02's linux workout

---
+++ contributions/BlueInked/linux/Terminals & Shells/2016-09-02.bash
@@ -0,0 +1,7 @@
+Clear the terminal instantly
+Find any Unix / Linux command
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
