3b2f8ec1b77e276b509d73e9d600c1d732700474
xetra11 <xetra11@outlook.com>
2016-09-02 07:26:30 +0100

    2016-09-02's java workout

---
+++ contributions/xetra11/java/Fundamentals/2016-09-02.java
@@ -0,0 +1,5 @@
+Cropping images
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
