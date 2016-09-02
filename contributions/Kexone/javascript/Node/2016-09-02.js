64bbc781e84e4a3424a6c1749c87175a9a6a45d5
Kexone <kami.extreme@gmail.com>
2016-09-02 09:43:30 +0100

    2016-09-02's javascript workout

---
+++ contributions/Kexone/javascript/Node/2016-09-02.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+`uncaughtException` listener in Node.js
+Automatic compilation for Node with **Nodemon** 
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
