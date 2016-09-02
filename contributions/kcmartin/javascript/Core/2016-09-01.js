3985f1b094cf54682ff7a354d75079c3466ddd48
kcmartin <kcmartin2@mac.com>
2016-09-02 06:58:02 +0100

    2016-09-01's javascript workout

---
+++ contributions/kcmartin/javascript/Core/2016-09-01.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Double and single quotes
+Hoisting applies only to variable declarations, not initializations
+Extra function arguments are undefined by default
+Detect online connection
