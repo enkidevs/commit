3cbff9db90b749e56fcedac3a345027b0417fb73
frug <frugen@gmail.com>
2016-09-02 14:40:26 +0100

    2016-09-02's linux workout

---
+++ contributions/frug/linux/File Management/2016-09-02.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Working with `.gz` files
+Find the unknown process preventing deleting of files
