4842f223e34ca9d3c90dc2f0c06a1684fef41ecb
jvansan <jeff.van.santen56@gmail.com>
2016-09-02 16:50:26 +0100

    2016-09-02's java workout

---
+++ contributions/jvansan/java/Error Handling/2016-09-02.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Using exceptions in Java
+Throwing an `exception`
+Careful Numeric Data Conversions
