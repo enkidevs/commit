8a1d84cb40f73f1cf0ec0193e629b98177ca359a
shaynekellyii <skshayne@gmail.com>
2016-09-02 09:25:30 +0100

    2016-09-01's java workout

---
+++ contributions/shaynekellyii/java/Data Types/2016-09-01.java
@@ -0,0 +1,5 @@
+`ArrayList` vs `LinkedList`
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Converting numbers to strings
+Do not attempt comparisons with NaN
