739674947c7adc4c8893e0578352082bd894581c
Aasheesh29 <svaasheesh.sharma@gmail.com>
2016-09-02 19:48:30 +0100

    2016-09-01's linux workout

---
+++ contributions/Aasheesh29/linux/Terminals & Shells/2016-09-01.bash
@@ -0,0 +1,5 @@
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
