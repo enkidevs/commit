bd9af4427e03a6b2db61a92f7fe17153da289620
illbexyz <albyx.n@gmail.com>
2016-09-02 13:24:26 +0100

    2016-09-02's linux workout

---
+++ contributions/illbexyz/linux/System Management/2016-09-02.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
