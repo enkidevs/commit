65a9ec323ea642f8ac3747217074df1e7b08288b
benjamín.dona <benjamin.dona@live.cl>
2016-09-02 12:19:09 +0100

    2016-09-02's linux workout

---
+++ contributions/benjamín.dona/linux/Terminals & Shells/2016-09-02.bash
@@ -0,0 +1,5 @@
+Useful aliasing in bash
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
