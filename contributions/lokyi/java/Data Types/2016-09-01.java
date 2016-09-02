dc3a0d17cf2bc1a5cde76c1bd1151a44d8df4e2f
lokyi <roy.yi.yue@gmail.com>
2016-09-02 05:07:22 +0100

    2016-09-01's java workout

---
+++ contributions/lokyi/java/Data Types/2016-09-01.java
@@ -0,0 +1,5 @@
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Converting numbers to strings
+Converting a string to upper or lower case
+Finding a substring in a string
