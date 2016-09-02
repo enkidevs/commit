528e91fd4823a00fe69d83e6e56aac56e1c64d3c
Wolf <ya.172009@yandex.ru>
2016-09-02 21:38:17 +0100

    2016-09-02's java workout

---
+++ contributions/Wolf/java/Fundamentals/2016-09-02.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Detect or prevent integer overflow
+How range operations work
