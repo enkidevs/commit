d6997ba7a0ae687f874c06492f81c8122d544b99
x4fyr <benedikt_v@web.de>
2016-09-02 20:37:05 +0100

    2016-09-02's linux workout

---
+++ contributions/x4fyr/linux/System Management/2016-09-02.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
