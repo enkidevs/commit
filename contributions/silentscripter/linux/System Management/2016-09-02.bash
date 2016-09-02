bfccec2eeeb6a5a4f2d546fb5a857987ed85db71
silentscripter <vito.laera@protechstudio.it>
2016-09-02 21:38:24 +0100

    2016-09-02's linux workout

---
+++ contributions/silentscripter/linux/System Management/2016-09-02.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Finding Open Files With `lsof`
