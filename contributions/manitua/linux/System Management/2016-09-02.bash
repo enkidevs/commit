cf487328f22b794c39ecd9791b3ab5ba9b3c8c8e
manitua <jasmin.beharic@gmail.com>
2016-09-02 06:22:54 +0100

    2016-09-02's linux workout

---
+++ contributions/manitua/linux/System Management/2016-09-02.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+List current logged on users with `w`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
