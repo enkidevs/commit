18d7c045e2b3ab2d555dc56acf8ab4fe3ffe2d70
codedbyandrew <andrewatmadison@gmail.com>
2016-09-02 15:28:03 +0100

    2016-09-02's linux workout

---
+++ contributions/codedbyandrew/linux/Terminals & Shells/2016-09-02.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Useful aliasing in bash
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
