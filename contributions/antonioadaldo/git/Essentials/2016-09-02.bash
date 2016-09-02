b89750f7b8b13fd9df0b9d95c1747f65ee718718
antonioadaldo <antonio.adaldo.89@gmail.com>
2016-09-02 17:34:47 +0100

    2016-09-02's git workout

---
+++ contributions/antonioadaldo/git/Essentials/2016-09-02.bash
@@ -0,0 +1,5 @@
+Using tags for version control
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
+Ignore files in git
