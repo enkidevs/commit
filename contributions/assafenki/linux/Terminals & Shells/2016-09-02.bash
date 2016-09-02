c00cdc247e914f26f0345fd2b5b82b2c96fdd51a
assafenki <assaf@shiftz.com>
2016-09-02 12:46:39 +0100

    2016-09-02's linux workout

---
+++ contributions/assafenki/linux/Terminals & Shells/2016-09-02.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Random password generator
+Terminal incognito mode
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
