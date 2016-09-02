0fdf82bdc716198d2f2d0a0eabac283d4b287dba
pvyParts <aaronkable@gmail.com>
2016-09-02 09:42:03 +0100

    2016-09-01's java workout

---
+++ contributions/pvyParts/java/Fundamentals/2016-09-01.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
