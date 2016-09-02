a366d1c8c350104dd9513283fab70918bec8970f
jpclair <jpierre.clair@gmail.com>
2016-09-02 20:32:53 +0100

    2016-09-02's javascript workout

---
+++ contributions/jpclair/javascript/React/2016-09-02.js
@@ -0,0 +1,5 @@
+Functional Stateless Components in React
+`null` value for *Controlled Components* in **React**
+Using `false` in **JSX**
+Dealing with `this.props.children`
+Shallow compare in **React**
