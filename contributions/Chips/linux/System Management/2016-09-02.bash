10ced1ed5c894e637b0af9d638f2a386f08030d4
Chips <ciprianv96@hotmail.it>
2016-09-02 21:44:39 +0100

    2016-09-02's linux workout

---
+++ contributions/Chips/linux/System Management/2016-09-02.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Check the HDD with `badblocks`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
