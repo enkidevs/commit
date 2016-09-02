f07b6a9b5da2b0fd2aa64c87bc8be192b5734a3d
bleestein <bleestein@gmail.com>
2016-09-02 13:41:12 +0100

    2016-09-02's linux workout

---
+++ contributions/bleestein/linux/File Management/2016-09-02.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
+Easy way of sharing files
