e69696dc1642c63aa9d895e292cf2466f6d1be40
rav1603 <rav.lukowski@gmail.com>
2016-09-02 17:22:08 +0100

    2016-09-02's java workout

---
+++ contributions/rav1603/java/Fundamentals/2016-09-02.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+Using an interface as a parameter
