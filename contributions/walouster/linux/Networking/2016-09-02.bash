9f50eb332dadaf96d57587383ba7444271f04dc6
walouster <walouster@gmail.com>
2016-09-02 08:08:41 +0100

    2016-09-02's linux workout

---
+++ contributions/walouster/linux/Networking/2016-09-02.bash
@@ -0,0 +1,5 @@
+How to clear `iptables` rules
+Useful ssh client optimizations 
+List your IPv6 configuration
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
