c56eddcfa02700545a59b27bd1fc8d0e3b297a2f
Phackbarth <tauren321@gmail.com>
2016-09-02 13:49:01 +0100

    2016-09-02's javascript workout

---
+++ contributions/Phackbarth/javascript/Node/2016-09-02.js
@@ -0,0 +1,5 @@
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+Automatic compilation for Node with **Nodemon** 
+Manage child processes in **Node**
+Middleware/pipeline design pattern
