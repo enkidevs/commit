cf82731a3d9bd0c3da5d88982a37f480ecfad395
SeifMostafa <seifmostafa235@gmail.com>
2016-09-03 14:59:22 +0100

    2016-09-03's java workout

---
+++ contributions/SeifMostafa/java/Fundamentals/2016-09-03.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
