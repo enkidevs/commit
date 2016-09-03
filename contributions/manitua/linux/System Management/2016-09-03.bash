9c8c43963c6adf0c404b2a13ea7d4cb6929ed7fc
manitua <jasmin.beharic@gmail.com>
2016-09-03 08:00:44 +0100

    2016-09-03's linux workout

---
+++ contributions/manitua/linux/System Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
