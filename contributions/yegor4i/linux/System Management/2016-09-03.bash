6bb16396999a302b2683c095358a44857e8b4c56
yegor4i <aki@bmail.ru>
2016-09-03 17:48:36 +0100

    2016-09-03's linux workout

---
+++ contributions/yegor4i/linux/System Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
