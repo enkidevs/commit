b2e26a9a2ff115adb26f0bd8fae881f8bdb842d1
lai-nam <lai.nam0210@gmail.com>
2016-09-03 04:24:34 +0100

    2016-09-02's git workout

---
+++ contributions/lai-nam/git/Essentials/2016-09-02.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to unstage a staged file
+Using tags for version control
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
