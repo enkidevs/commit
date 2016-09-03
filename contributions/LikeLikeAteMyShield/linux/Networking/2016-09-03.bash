0268ef2ea416ede7387af33a55078e0d261f7d6c
LikeLikeAteMyShield <jonnycameron17@gmail.com>
2016-09-03 09:11:46 +0100

    2016-09-03's linux workout

---
+++ contributions/LikeLikeAteMyShield/linux/Networking/2016-09-03.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+List your IPv6 configuration
+Sending emails from terminal / scripts
+Setting up password-free authentication
+Getting the connection speed from the terminal
