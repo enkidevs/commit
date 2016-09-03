0cd271e7ffe4723bb12c83c3ec2af84ac855eff8
gravoo <gravoo@yahoo.com>
2016-09-03 12:37:20 +0100

    2016-09-03's linux workout

---
+++ contributions/gravoo/linux/File Management/2016-09-03.bash
@@ -0,0 +1,5 @@
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
