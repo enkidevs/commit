15c3b5797e16ba58825340ce90c3e6508a4c8079
neverything <silvan@required.ch>
2016-09-03 10:54:55 +0100

    2016-09-03's javascript workout

---
+++ contributions/neverything/javascript/Npm/2016-09-03.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Deprecate npm packages
+Passing through command line arguments in npm scripts
+Manage local node modules with `npm link`
+`optionalDependencies` in npm
