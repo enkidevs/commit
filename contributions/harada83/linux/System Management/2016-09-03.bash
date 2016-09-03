5409e0b6a8b553132850db326105aa10c7e3248b
harada83 <mario.cimini@gmail.com>
2016-09-03 16:36:50 +0100

    2016-09-03's linux workout

---
+++ contributions/harada83/linux/System Management/2016-09-03.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Common log files
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Finding Open Files With `lsof`
