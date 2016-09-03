efbeca8585dc445b629a36b6a394ae4f3516ab7f
Usko_Detra <mr.matthewcfrench@gmail.com>
2016-09-03 04:59:03 +0100

    2016-09-02's java workout

---
+++ contributions/Usko_Detra/java/Fundamentals/2016-09-02.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Calling one constructor from another
