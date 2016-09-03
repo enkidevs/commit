8ff0e26fbc751ea8e982e36ff1ff3b526eb23193
mlaythe <mrlaythe24@aol.com>
2016-09-03 17:51:40 +0100

    2016-09-03's linux workout

---
+++ contributions/mlaythe/linux/Terminals & Shells/2016-09-03.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear bash history
+Get to know  your commands with `type`
+Retrieve previous arguments
