e4622709fb93ed03a2ac48736c21db1dd908ab73
EniasCailliau <enias@techinbits.com>
2016-09-03 10:45:26 +0100

    2016-09-03's linux workout

---
+++ contributions/EniasCailliau/linux/System Management/2016-09-03.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Finding Open Files With `lsof`
