3d9465a9bffc698b072ee25d0099d5ff5c74047b
sHilgert <hilgertfacul@gmail.com>
2016-09-03 11:22:06 +0100

    2016-09-03's linux workout

---
+++ contributions/sHilgert/linux/Terminals & Shells/2016-09-03.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Terminal based browser
+Useful aliasing in bash
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
