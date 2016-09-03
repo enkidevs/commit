0f361fcf51936a5b969144fb075f7e0ca49169a9
jfsebastian <ferrer.jorge@gmail.com>
2016-09-03 16:58:24 +0100

    2016-09-03's linux workout

---
+++ contributions/jfsebastian/linux/Terminals & Shells/2016-09-03.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Terminal based browser
+Terminal incognito mode
+Keep useful commands in your shell history with tags
