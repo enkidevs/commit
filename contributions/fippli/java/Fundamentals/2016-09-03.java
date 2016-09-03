7dd6f96e62952d12eab6561fee9b120e2ce0b310
fippli <filip.carl.johansson@gmail.com>
2016-09-03 23:35:13 +0100

    2016-09-03's java workout

---
+++ contributions/fippli/java/Fundamentals/2016-09-03.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Detect or prevent integer overflow
+Supply `toString()` in all classes
