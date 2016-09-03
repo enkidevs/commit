c03dfe572a605c6c26116b176c11ea92cff65bb7
DarkP <petr456@seznam.cz>
2016-09-03 20:06:17 +0100

    2016-09-03's java workout

---
+++ contributions/DarkP/java/Java 8/2016-09-03.java
@@ -0,0 +1,4 @@
+Java 8 Lambda expressions
+How to check for static dependencies of applications and libraries from the command line.
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
