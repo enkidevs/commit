9fae308b2b4d2d5c27f5df8e1a8351a4d164fc17
sujal99 <sujal.ghosh@gmail.com>
2016-09-03 02:16:45 +0100

    2016-09-02's linux workout

---
+++ contributions/sujal99/linux/System Management/2016-09-02.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
