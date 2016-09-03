fd91489516a3b594f565eb8f09fc789bc3721231
happycrappie <imerycdasilva@gmail.com>
2016-09-03 01:54:17 +0100

    2016-09-02's linux workout

---
+++ contributions/happycrappie/linux/Terminals & Shells/2016-09-02.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
