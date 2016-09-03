a008a259dcf67570fb4b098622a33fa37bdcf1d7
Vaintti <antti.jk.vainikka@gmail.com>
2016-09-03 15:12:52 +0100

    2016-09-03's git workout

---
+++ contributions/Vaintti/git/Essentials/2016-09-03.bash
@@ -0,0 +1,5 @@
+Adding a remote repository
+Using aliases for git commands
+How to set your username and email
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
