e4d6d1205b284fd560fe97fc45f6945238b923ae
tikiki <t.goudine@gmail.com>
2016-09-03 21:00:41 +0100

    2016-09-03's linux workout

---
+++ contributions/tikiki/linux/Networking/2016-09-03.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Check `iptables` firewall status
+How to clear `iptables` rules
+Sending emails from terminal / scripts
+Use `tcpdump` to listen to network interface traffic
