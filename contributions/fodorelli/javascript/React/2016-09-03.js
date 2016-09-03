c5153b461cde0aece978a54c9bcbf9d97419d80b
fodorelli <fodorelli@yahoo.com>
2016-09-03 10:36:54 +0100

    2016-09-03's javascript workout

---
+++ contributions/fodorelli/javascript/React/2016-09-03.js
@@ -0,0 +1,6 @@
+componentWillReceiveProps Not Triggered After Mounting
+
+**React** `this.props.children`
+Dealing with `this.props.children`
+`PureRenderMixin` in **React**
+Clone Elements in **React**
