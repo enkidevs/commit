4d7772a666b9e33ef3aeb6c60a0f5e2f213a4066
michaelb1993 <michealbnd1993@gmail.com>
2016-09-03 12:54:05 +0100

    2016-09-03's linux workout

---
+++ contributions/michaelb1993/linux/File Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
