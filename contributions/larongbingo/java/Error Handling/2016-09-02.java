b11a1f45b09091a285f66f3ebb7147ea480eba8d
larongbingo <larongbingo@gmail.com>
2016-09-03 14:25:07 +0100

    2016-09-02's java workout

---
+++ contributions/larongbingo/java/Error Handling/2016-09-02.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Creating new Exceptions
+Using exceptions in Java
+Careful Numeric Data Conversions
