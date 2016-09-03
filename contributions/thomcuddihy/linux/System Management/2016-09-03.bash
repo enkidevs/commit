870f98c4ada39c1458daafd4d08cc022e5cc69e0
thomcuddihy <thom@hometreelab.com>
2016-09-04 00:12:08 +0100

    2016-09-03's linux workout

---
+++ contributions/thomcuddihy/linux/System Management/2016-09-03.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
