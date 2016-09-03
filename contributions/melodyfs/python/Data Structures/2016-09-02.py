eccaec4262c84419e3ac27428ed22e4c85c66e9d
melodyfs <melody870116@gmail.com>
2016-09-03 13:50:11 +0100

    2016-09-02's python workout

---
+++ contributions/melodyfs/python/Data Structures/2016-09-02.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`Dictionary` - standard mapping type
+The fundamental `tuples` 
+When `range` comes in handy
+Get more with `collections`!
