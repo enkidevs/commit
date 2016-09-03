d9c15d59bfa6ed0478cdf88ede3fed19a35620c8
gerdtorok <gerdtorok@gmail.com>
2016-09-03 15:45:33 +0100

    2016-09-03's linux workout

---
+++ contributions/gerdtorok/linux/System Management/2016-09-03.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
