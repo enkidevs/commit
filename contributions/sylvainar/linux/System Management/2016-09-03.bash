a08ba3775e7510b7fb411f68487100b90563f49c
sylvainar <sylvain.ar@hotmail.fr>
2016-09-03 17:44:43 +0100

    2016-09-03's linux workout

---
+++ contributions/sylvainar/linux/System Management/2016-09-03.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
