07891ee50537196feb219ef7e13fad5dd070ae41
perry83 <johnperry8338@gmail.com>
2016-09-03 07:49:56 +0100

    2016-09-03's javascript workout

---
+++ contributions/perry83/javascript/Functions/2016-09-03.js
@@ -0,0 +1,5 @@
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Social sharing without widgets
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
