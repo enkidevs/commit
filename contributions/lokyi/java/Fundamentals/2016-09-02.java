32ab25df2b71c3afb60a23d004b959f882bf14e7
lokyi <roy.yi.yue@gmail.com>
2016-09-03 07:09:23 +0100

    2016-09-02's java workout

---
+++ contributions/lokyi/java/Fundamentals/2016-09-02.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Difference between JRE and JDK?
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
