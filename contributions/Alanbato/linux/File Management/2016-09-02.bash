8e7b8726c786af14b54069bc32463afa41aa2c98
Alanbato <alanvelasco.a@gmail.com>
2016-09-03 07:21:44 +0100

    2016-09-02's linux workout

---
+++ contributions/Alanbato/linux/File Management/2016-09-02.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Easy way of sharing files
