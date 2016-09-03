8c43f9435446c5bf311fc5c679cd0d83f6bc6dc4
Sai Rama Krishna <gottumukala.sai@iitgn.ac.in>
2016-09-03 16:13:03 +0100

    2016-09-02's linux workout

---
+++ contributions/Sai Rama Krishna/linux/Networking/2016-09-02.bash
@@ -0,0 +1,5 @@
+How to clear `iptables` rules
+Useful ssh client optimizations 
+Get real network statistics with `slurm`
+List your IPv6 configuration
+Getting the connection speed from the terminal
