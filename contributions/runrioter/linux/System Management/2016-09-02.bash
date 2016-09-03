16436796c2deb89546d13da0fa29819d85e85d26
runrioter <runrioter@gmail.com>
2016-09-03 02:14:29 +0100

    2016-09-02's linux workout

---
+++ contributions/runrioter/linux/System Management/2016-09-02.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Basic service management with `systemd`
+Finding Open Files With `lsof`
