3661f0e697f085cce940d626e8e7cc525aacbde9
williebabzan <jakobwilliamaberg@gmail.com>
2016-09-03 15:16:43 +0100

    2016-09-03's javascript workout

---
+++ contributions/williebabzan/javascript/React/2016-09-03.js
@@ -0,0 +1,6 @@
+**React** in-line styles
+Props in getInitialState Is an        
+Anti-Pattern
+Expose Component Functions in **React**
+Dealing with `this.props.children`
+Validate for required props
