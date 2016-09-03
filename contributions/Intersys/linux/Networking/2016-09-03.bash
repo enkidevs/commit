6b8b368da58fce0951b286db095617db74b0cdce
Intersys <int3rsys@live.com>
2016-09-03 10:40:36 +0100

    2016-09-03's linux workout

---
+++ contributions/Intersys/linux/Networking/2016-09-03.bash
@@ -0,0 +1,5 @@
+Debugging `ssh` client issues
+How to clear `iptables` rules
+Sending emails from terminal / scripts
+Setting up password-free authentication
+Getting the connection speed from the terminal
