629701a401d70669a5f84c22bc7be4219eaaa503
layornos <layornos@gmail.com>
2016-09-03 13:54:32 +0100

    2016-09-03's java workout

---
+++ contributions/layornos/java/Fundamentals/2016-09-03.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
