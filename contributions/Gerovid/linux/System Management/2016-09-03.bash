2d9b4e940f9c1350a4ce7d9c8d28a9aad1cafbc3
Gerovid <gerovid@gmail.com>
2016-09-03 20:26:20 +0100

    2016-09-03's linux workout

---
+++ contributions/Gerovid/linux/System Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
