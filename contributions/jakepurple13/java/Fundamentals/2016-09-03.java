c1609075b9082b299072c11febad8ee72a1fc567
jakepurple13 <jakerein@gmail.com>
2016-09-03 19:51:17 +0100

    2016-09-03's java workout

---
+++ contributions/jakepurple13/java/Fundamentals/2016-09-03.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
