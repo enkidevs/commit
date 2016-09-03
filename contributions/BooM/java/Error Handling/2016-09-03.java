d883a60280c7bf5564cfa76ee71f6b19055ca804
BooM <tararuj.michal@gmail.com>
2016-09-03 16:32:37 +0100

    2016-09-03's java workout

---
+++ contributions/BooM/java/Error Handling/2016-09-03.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
