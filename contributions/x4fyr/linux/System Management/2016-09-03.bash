cedd92fbb4910c631e659129dded8b7bd3888a52
x4fyr <benedikt_v@web.de>
2016-09-03 18:10:08 +0100

    2016-09-03's linux workout

---
+++ contributions/x4fyr/linux/System Management/2016-09-03.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Common log files
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
