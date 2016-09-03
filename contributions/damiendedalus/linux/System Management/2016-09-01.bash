6782d6189e7579a26365a9412e54bcf2d5a24534
damiendedalus <abhid2sik@gmail.com>
2016-09-03 09:00:59 +0100

    2016-09-01's linux workout

---
+++ contributions/damiendedalus/linux/System Management/2016-09-01.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
