a774e272b6b19ec06d694e0e9ff0fcb539af59ba
martinph <martinph01@gmail.com>
2016-09-03 09:23:16 +0100

    2016-09-03's linux workout

---
+++ contributions/martinph/linux/File Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
