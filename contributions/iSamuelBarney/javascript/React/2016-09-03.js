400598309e33b67a20c513313a7aca042070796f
iSamuelBarney <samuel.barney@icloud.com>
2016-09-03 16:13:12 +0100

    2016-09-03's javascript workout

---
+++ contributions/iSamuelBarney/javascript/React/2016-09-03.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Enhanced Prop Validation 
+Custom `propType`'s to be required
+Shortcut for transferring props
