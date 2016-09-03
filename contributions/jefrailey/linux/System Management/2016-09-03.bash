03e3452abdf0cceb63bb5707584e5fc4f67cf291
jefrailey <jcaetof+enki@gmail.com>
2016-09-03 15:14:50 +0100

    2016-09-03's linux workout

---
+++ contributions/jefrailey/linux/System Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
