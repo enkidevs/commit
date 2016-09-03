5f55fd6b88c58d9f3eb37998474a104de93607cb
Ecleptic <cameron@camerongreens.com>
2016-09-03 16:22:53 +0100

    2016-09-03's java workout

---
+++ contributions/Ecleptic/java/I/O/2016-09-03.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to create directories in Java
+Create system file paths using the `Path` class
+Checking for the existence of files and directories
+List contents of a directory with `newDirectoryStream`
