3d6d723be057ff86e616c15f8b5c57512f607858
leinax <leinax@gmail.com>
2016-09-03 01:19:18 +0100

    2016-09-02's javascript workout

---
+++ contributions/leinax/javascript/Functions/2016-09-02.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` with `call` and `apply` methods
+IIFE pattern
+Get query/url variables
+Move cursor at the end of text input
