1c4e7e94643c965ed3944f1f703cd752d60e3cff
SchubertCh <schubertch@gmx.de>
2016-09-03 23:24:13 +0100

    2016-09-03's java workout

---
+++ contributions/SchubertCh/java/Fundamentals/2016-09-03.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Measuring time
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
