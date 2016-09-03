a8c0d9bf51c1332fb40adaadf8ea42805913ed88
syfluqs <syfluqs@gmail.com>
2016-09-03 15:32:41 +0100

    2016-09-02's linux workout

---
+++ contributions/syfluqs/linux/Jobs & Processes/2016-09-02.bash
@@ -0,0 +1,5 @@
+Keeping processes running after you log out
+`Crontab` newline issue
+`Cron` dot-in-filename issues when using `run-parts`
+Fixing the shell in cron
+Common issue with scripts in `cron`
