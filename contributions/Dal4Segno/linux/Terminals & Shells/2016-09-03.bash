c33d5d1b52f40a5a5ede436f3aa7c5741de51ef5
Dal4Segno <dal4segno@gmail.com>
2016-09-03 20:03:43 +0100

    2016-09-03's linux workout

---
+++ contributions/Dal4Segno/linux/Terminals & Shells/2016-09-03.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
