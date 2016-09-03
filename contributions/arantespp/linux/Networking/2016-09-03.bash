5f3762c5449a88385721a35776b069826ac9cfef
arantespp <arantespp@gmail.com>
2016-09-03 17:22:15 +0100

    2016-09-03's linux workout

---
+++ contributions/arantespp/linux/Networking/2016-09-03.bash
@@ -0,0 +1,5 @@
+Disable `IPv6`
+Sending emails from terminal / scripts
+Setting up password-free authentication
+Use `tcpdump` to listen to network interface traffic
+Staying connected remotely via SSH without an interactive shell
