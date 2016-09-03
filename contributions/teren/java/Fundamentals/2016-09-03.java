bc14b01f70b87b13776c305941981b53135bd994
teren <terenn@gmail.com>
2016-09-03 12:44:15 +0100

    2016-09-03's java workout

---
+++ contributions/teren/java/Fundamentals/2016-09-03.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
+Using an interface as a parameter
