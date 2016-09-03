e0216eca7a3c7a5180f2ebe9f20f0b84cea183b0
jjcarstens <jjcarstens@me.com>
2016-09-03 16:37:41 +0100

    2016-09-03's linux workout

---
+++ contributions/jjcarstens/linux/System Management/2016-09-03.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
