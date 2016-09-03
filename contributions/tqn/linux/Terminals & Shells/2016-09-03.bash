a5aeb3f9a29ec6879984c4d391d08e305f993249
tqn <tej.q.nair@gmail.com>
2016-09-03 18:56:09 +0100

    2016-09-03's linux workout

---
+++ contributions/tqn/linux/Terminals & Shells/2016-09-03.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
