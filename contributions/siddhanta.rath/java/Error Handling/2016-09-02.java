c96eda0069372514abac5cde2f52319833155030
siddhanta.rath <siddhanta.rath@gmail.com>
2016-09-03 10:17:05 +0100

    2016-09-02's java workout

---
+++ contributions/siddhanta.rath/java/Error Handling/2016-09-02.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Prevent `exceptions` while logging Data
+Throwing an `exception`
+Careful Numeric Data Conversions
