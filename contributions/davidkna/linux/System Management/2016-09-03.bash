adf3fc7a7bb19914a62688b056e998c614058701
davidkna <david@knaack.me>
2016-09-03 11:56:34 +0100

    2016-09-03's linux workout

---
+++ contributions/davidkna/linux/System Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
