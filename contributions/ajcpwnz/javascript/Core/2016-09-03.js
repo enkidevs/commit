60ec77b4669d55960c7c5486dd58b276978c532b
ajcpwnz <fentg1@hotmail.com>
2016-09-03 12:45:39 +0100

    2016-09-03's javascript workout

---
+++ contributions/ajcpwnz/javascript/Core/2016-09-03.js
@@ -0,0 +1,5 @@
+Keywords in hash keys
+Format currency in JavaScript
+Hoisting applies only to variable declarations, not initializations
+Check if a function exists
+Getting the *real* dimensions of an image
