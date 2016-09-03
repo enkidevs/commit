f70c8e9a7607e626d94fa7bb5010a78925604d3f
walberth <w.felipe.gutierrez@gmail.com>
2016-09-03 16:51:28 +0100

    2016-09-03's java workout

---
+++ contributions/walberth/java/Fundamentals/2016-09-03.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
