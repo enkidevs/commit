64dd4804c1d33f55fca15fd9084523e241944ee3
adracea <adracea@gmail.com>
2016-09-03 09:54:42 +0100

    2016-09-03's linux workout

---
+++ contributions/adracea/linux/Terminals & Shells/2016-09-03.bash
@@ -0,0 +1,5 @@
+Quick `bash` shortcuts 
+Useful aliasing in bash
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
