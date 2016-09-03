54ee53dff14dcfe3bd6159a5fa66393d8f320e6f
gustavkarlsson <gustav.karlsson@gmail.com>
2016-09-03 08:48:16 +0100

    2016-09-03's linux workout

---
+++ contributions/gustavkarlsson/linux/File Management/2016-09-03.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
