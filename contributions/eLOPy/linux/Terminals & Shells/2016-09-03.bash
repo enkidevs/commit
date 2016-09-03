7cbdbbccce16c9d60c40e0c4e949994f1818076c
eLOPy <m.fergiani.m@gmail.com>
2016-09-03 14:19:10 +0100

    2016-09-03's linux workout

---
+++ contributions/eLOPy/linux/Terminals & Shells/2016-09-03.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
