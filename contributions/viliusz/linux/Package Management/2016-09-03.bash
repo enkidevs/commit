9fcf66162d092eb8ef54b289cb8adc98c19e63d5
viliusz <enki@gmn.lt>
2016-09-03 21:15:32 +0100

    2016-09-03's linux workout

---
+++ contributions/viliusz/linux/Package Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+Install specific version of packages
+Backporting on Debian
+
+Get `apt` to use a mirror / faster mirror
+Using `Tasksel` for software installation
+Keeping repos updated with `cron`
