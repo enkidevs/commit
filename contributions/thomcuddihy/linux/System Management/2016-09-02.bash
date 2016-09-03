b8fab9b0ae721eb87e07afbf2f1f641e6c24ed49
thomcuddihy <thom@hometreelab.com>
2016-09-03 07:25:02 +0100

    2016-09-02's linux workout

---
+++ contributions/thomcuddihy/linux/System Management/2016-09-02.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
