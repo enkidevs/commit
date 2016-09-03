309f242fafd8fb499de7c79ec7329d653e7c3a48
pjbrito <pjbrito@gmail.com>
2016-09-03 11:30:01 +0100

    2016-09-03's linux workout

---
+++ contributions/pjbrito/linux/System Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
