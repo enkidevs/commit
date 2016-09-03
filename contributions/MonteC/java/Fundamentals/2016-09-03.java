30a260328b898c907aada7b87f698f991988cd31
MonteC <jarekucinski@gmail.com>
2016-09-03 22:16:44 +0100

    2016-09-03's java workout

---
+++ contributions/MonteC/java/Fundamentals/2016-09-03.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between ```HashMap``` and ```Hashtable```
+Common mistake on switch statements
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
