788eb24544bc71e1ce5b9ac0d4a360de1fcd8556
vsvx <911kids@gmail.com>
2016-09-03 23:36:39 +0100

    2016-09-03's javascript workout

---
+++ contributions/vsvx/javascript/React/2016-09-03.js
@@ -0,0 +1,7 @@
+Use `propTypes` on stateless components
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+`PureRenderMixin` in **React**
+Validate for required props
