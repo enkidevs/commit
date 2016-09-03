c4c27782642b893a0cf8242636bc9150807df1a9
Aiki34 <aiki_34@hotmail.com>
2016-09-03 23:04:15 +0100

    2016-09-03's java workout

---
+++ contributions/Aiki34/java/Error Handling/2016-09-03.java
@@ -0,0 +1,5 @@
+Use try-with-resources instead of `finally`
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Throwing an `exception`
