d04753146ba1f664f82674ad6a33e3adf5e5a512
adewaal824 <adewaal824@gmail.com>
2016-09-03 08:40:14 +0100

    2016-09-03's linux workout

---
+++ contributions/adewaal824/linux/System Management/2016-09-03.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Finding Open Files With `lsof`
