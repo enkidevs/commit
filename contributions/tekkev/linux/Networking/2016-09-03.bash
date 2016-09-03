bf4701196c24ce323cda85ad5873e5b7012cb434
tekkev <kuglerkev@gmail.com>
2016-09-03 14:39:25 +0100

    2016-09-03's linux workout

---
+++ contributions/tekkev/linux/Networking/2016-09-03.bash
@@ -0,0 +1,5 @@
+How to clear `iptables` rules
+Get real network statistics with `slurm`
+Limit the `wget` download rate
+Setting up password-free authentication
+Getting the connection speed from the terminal
