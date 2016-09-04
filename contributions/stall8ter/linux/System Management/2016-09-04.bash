508c69d77d4d0b5d8b5759009082cc4bcefeebb0
stall8ter <hordiyevych.danny@gmail.com>
2016-09-04 17:32:15 +0100

    2016-09-04's linux workout

---
+++ contributions/stall8ter/linux/System Management/2016-09-04.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
