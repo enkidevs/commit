613c2897c0b7a2659e093c3b6132187038b07482
AbhiramE <abhis3798@gmail.com>
2016-09-04 03:44:23 +0100

    2016-09-03's linux workout

---
+++ contributions/AbhiramE/linux/System Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
