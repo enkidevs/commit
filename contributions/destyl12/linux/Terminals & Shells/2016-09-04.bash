cfb3257cd2b178e13715804d3d319184626bdbf1
destyl12 <bzajacc@gmail.com>
2016-09-04 07:55:41 +0100

    2016-09-04's linux workout

---
+++ contributions/destyl12/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
