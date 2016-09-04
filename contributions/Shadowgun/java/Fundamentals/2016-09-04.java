692a7410ec85ff3280f013ed45993ac53b6828c1
Shadowgun <shadowgun@i.ua>
2016-09-04 07:33:52 +0100

    2016-09-04's java workout

---
+++ contributions/Shadowgun/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Cropping images
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
