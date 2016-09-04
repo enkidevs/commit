e8472551ed8fc20be73bdf709d80b9f844276ee9
osmarks <osmarks@icloud.com>
2016-09-04 20:52:14 +0100

    2016-09-04's linux workout

---
+++ contributions/osmarks/linux/System Management/2016-09-04.bash
@@ -0,0 +1,7 @@
+Revision control of configuration files with git
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Check the HDD with `badblocks`
