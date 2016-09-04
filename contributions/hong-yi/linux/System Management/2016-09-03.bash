d79730a6095e39984bc908596af01700258e6763
hong-yi <im@hongyi.io>
2016-09-04 06:52:01 +0100

    2016-09-03's linux workout

---
+++ contributions/hong-yi/linux/System Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Force a time update with `ntp`
+
+Basic service management with `systemd`
+Cancel a system shutdown
