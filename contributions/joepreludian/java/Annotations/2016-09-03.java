c242af5340f8717daf3ce7eabfbe99b4ebd7a871
joepreludian <joepreludian@gmail.com>
2016-09-04 04:50:09 +0100

    2016-09-03's java workout

---
+++ contributions/joepreludian/java/Annotations/2016-09-03.java
@@ -0,0 +1,5 @@
+`@Test` parameters
+Use `@Ignore` to disable JUnit tests
+Including annotations in Javadoc
+Restricting an annotation's application by using `@Target`
+Declaring `@Repeatable` annotation types
