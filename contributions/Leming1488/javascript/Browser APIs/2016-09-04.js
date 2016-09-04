6c44b2abcae8b030a7617b15a7572ddff450b79d
Leming1488 <borovweb@gmail.com>
2016-09-04 09:25:51 +0100

    2016-09-04's javascript workout

---
+++ contributions/Leming1488/javascript/Browser APIs/2016-09-04.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Async and defer scripts
+Use hosted scripts to increase performance
+CSS for when JavaScript is enabled
+Drag and Drop API
