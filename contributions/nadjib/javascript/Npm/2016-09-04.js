ed009e3aa93e07371ce12e483cdbb9b400e155f1
nadjib <hello@ebabel.eu>
2016-09-04 14:21:07 +0100

    2016-09-04's javascript workout

---
+++ contributions/nadjib/javascript/Npm/2016-09-04.js
@@ -0,0 +1,5 @@
+npm can handle multiple versions of a dependency
+List binaries for scripting in npm
+`npm`  as an alternative to Gulp
+Reduce package duplication 
+Lock down dependency versions by shrinkwrapping
