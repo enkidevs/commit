214f0ac5c29949a483ab12243685fac2d431aa3f
dmkonst <warqueq@gmail.com>
2016-09-04 08:26:23 +0100

    2016-09-04's linux workout

---
+++ contributions/dmkonst/linux/System Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
