eacff822ca76fbf50913085b33b659c31ff0881f
Andee <andytruong94@gmail.com>
2016-09-04 09:25:34 +0100

    2016-09-04's java workout

---
+++ contributions/Andee/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using bounded type parameters in generic methods
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
