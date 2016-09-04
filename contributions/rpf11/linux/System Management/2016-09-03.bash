a5198487ff4b1072b841e889df8914dcf7e867c1
rpf11 <raminponambalom-franco@outlook.fr>
2016-09-04 05:20:42 +0100

    2016-09-03's linux workout

---
+++ contributions/rpf11/linux/System Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
