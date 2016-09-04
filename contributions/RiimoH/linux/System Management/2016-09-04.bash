1f648291839d89c9d0eefc93d5b24896bf46d252
RiimoH <remo.herzog@hotmail.com>
2016-09-04 17:57:47 +0100

    2016-09-04's linux workout

---
+++ contributions/RiimoH/linux/System Management/2016-09-04.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Commands to shutdown or restart the system
