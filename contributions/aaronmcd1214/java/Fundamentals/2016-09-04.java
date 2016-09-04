ad8bb348c09e9d426fc181b7887464d7422220fc
aaronmcd1214 <aaronmcd1214@gmail.com>
2016-09-04 17:32:55 +0100

    2016-09-04's java workout

---
+++ contributions/aaronmcd1214/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
