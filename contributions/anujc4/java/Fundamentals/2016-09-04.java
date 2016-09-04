0be2c8adeb429f765e47fb14b1b84e65cd4d7954
anujc4 <anujc4@gmail.com>
2016-09-05 00:01:53 +0100

    2016-09-04's java workout

---
+++ contributions/anujc4/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Measuring time
+Always specify type parameter
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
