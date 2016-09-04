8becfa8dcaab6c08bd6d69c2ccc9774f8be3300b
ReyCobra <sgonato@gmail.com>
2016-09-04 14:40:38 +0100

    2016-09-04's linux workout

---
+++ contributions/ReyCobra/linux/System Management/2016-09-04.bash
@@ -0,0 +1,5 @@
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
