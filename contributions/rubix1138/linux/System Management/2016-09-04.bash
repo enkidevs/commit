2ea164b98862a268c524ea29e05d3983c730bba5
rubix1138 <xavier@ashe.com>
2016-09-04 18:39:57 +0100

    2016-09-04's linux workout

---
+++ contributions/rubix1138/linux/System Management/2016-09-04.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
