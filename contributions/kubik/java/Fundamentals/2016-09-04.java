239aa8483bd7ba0cce9be8c29c337f5e603de4dc
kubik <lkubic@poczta.onet.pl>
2016-09-04 08:51:35 +0100

    2016-09-04's java workout

---
+++ contributions/kubik/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
+Measuring time
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
