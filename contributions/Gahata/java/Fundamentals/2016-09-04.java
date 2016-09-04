7668cee3e7fb1783d6ad2dc020e2e644fed58f58
Gahata <p.jezierski999@gmail.com>
2016-09-04 02:19:10 +0100

    2016-09-04's java workout

---
+++ contributions/Gahata/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Cropping images
+Java naming conventions
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Always specify type parameter
