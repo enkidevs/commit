d1b142d08254a687d8e901d6bf835f6a0e7f22df
axin <axinmayatra@gmail.com>
2016-09-04 13:21:16 +0100

    2016-09-03's linux workout

---
+++ contributions/axin/linux/System Management/2016-09-03.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
