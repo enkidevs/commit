fe07eb394a60481550d4536248c29c3a77fc693a
Hadesy2k <kelvinkstz@gmail.com>
2016-09-04 08:54:26 +0100

    2016-09-03's linux workout

---
+++ contributions/Hadesy2k/linux/System Management/2016-09-03.bash
@@ -0,0 +1,7 @@
+Test disk speed with `dd`
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Basic service management with `systemd`
