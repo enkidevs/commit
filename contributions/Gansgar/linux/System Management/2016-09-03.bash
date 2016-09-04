dc45de33565b7f43dfe86b9824f22eb0acb9faaf
Gansgar <ansgar.f26@gmail.com>
2016-09-04 07:38:25 +0100

    2016-09-03's linux workout

---
+++ contributions/Gansgar/linux/System Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
