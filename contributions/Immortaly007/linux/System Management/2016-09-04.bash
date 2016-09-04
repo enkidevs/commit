b66d8cf0778ce07b6916de88654ae827cb471280
Immortaly007 <immortaly007@gmail.com>
2016-09-04 09:12:29 +0100

    2016-09-04's linux workout

---
+++ contributions/Immortaly007/linux/System Management/2016-09-04.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Incorrect time on dual boot systems
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
