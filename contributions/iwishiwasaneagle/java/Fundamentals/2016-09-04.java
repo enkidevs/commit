6abe719ec6b51192ceb618b912e1446b6dc19e54
iwishiwasaneagle <jh.ewers@gmail.com>
2016-09-04 09:02:58 +0100

    2016-09-04's java workout

---
+++ contributions/iwishiwasaneagle/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
