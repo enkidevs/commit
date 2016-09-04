da56f01d3ec9a7893aa770938380bcca29f3d687
stockcubus <jackstockbridgem@gmail.com>
2016-09-04 21:38:54 +0100

    2016-09-04's linux workout

---
+++ contributions/stockcubus/linux/System Management/2016-09-04.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
