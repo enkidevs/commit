6508e493c33b570993fc535563c81644e00e6ea7
jpclair <jpierre.clair@gmail.com>
2016-09-04 08:04:48 +0100

    2016-09-04's javascript workout

---
+++ contributions/jpclair/javascript/Npm/2016-09-04.js
@@ -0,0 +1,5 @@
+npm can handle multiple versions of a dependency
+Manage local node modules with `npm link`
+Environmental variables and flags for `npm`
+`npm` verifies `sha1` hashes of packages
+Scoped packages
