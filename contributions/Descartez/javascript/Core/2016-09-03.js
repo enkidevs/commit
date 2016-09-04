46785a0b032355046a54b1871f2e40c39e6d1566
Descartez <rvanstong@gmail.com>
2016-09-04 01:03:43 +0100

    2016-09-03's javascript workout

---
+++ contributions/Descartez/javascript/Core/2016-09-03.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Most efficient way to build `HTML` strings
+Use `splice()` to remove an item from an array
+Avoid using `with`
+Extra function arguments are undefined by default
