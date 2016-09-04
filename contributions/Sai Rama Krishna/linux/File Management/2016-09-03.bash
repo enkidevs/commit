f703216834d931c9c83efe213f5ce4ffc2e58d25
Sai Rama Krishna <gottumukala.sai@iitgn.ac.in>
2016-09-04 19:52:02 +0100

    2016-09-03's linux workout

---
+++ contributions/Sai Rama Krishna/linux/File Management/2016-09-03.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
+Easy way of sharing files
