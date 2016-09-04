0b4e7f076492a482e91132d84132186b756c4fbf
Eewaltsi <jonathan.ewalds@gmail.com>
2016-09-04 17:13:23 +0100

    2016-09-04's linux workout

---
+++ contributions/Eewaltsi/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
