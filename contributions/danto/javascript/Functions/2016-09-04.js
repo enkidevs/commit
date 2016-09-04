791bf20412e5c1bb9523200016bc99f0c36a6a63
danto <danielj@outlook.de>
2016-09-04 07:58:27 +0100

    2016-09-04's javascript workout

---
+++ contributions/danto/javascript/Functions/2016-09-04.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Functions can be declared after use
+`.bind()` 
+Easily generate a random `HEX` color
+Changing a functions context with `fn.call(object)`
