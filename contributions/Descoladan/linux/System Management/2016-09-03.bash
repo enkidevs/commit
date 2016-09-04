34ec0a6fcbde1553ef47a55d5af9fc510690cbd9
Descoladan <injpk23@gmail.com>
2016-09-04 02:54:41 +0100

    2016-09-03's linux workout

---
+++ contributions/Descoladan/linux/System Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Executing scripts on system boot using `systemd`
