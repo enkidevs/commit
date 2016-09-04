158b7356410fa23c770191bee5cfad6c4c7d9bac
vonpeterer <peterer87@gmail.com>
2016-09-04 11:58:23 +0100

    2016-09-04's linux workout

---
+++ contributions/vonpeterer/linux/System Management/2016-09-04.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Finding Open Files With `lsof`
