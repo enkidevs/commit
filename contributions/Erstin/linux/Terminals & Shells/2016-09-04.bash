c6d12863a685a4bb7126995cea9386c086d465f0
Erstin <ced.magno@gmail.com>
2016-09-04 07:40:54 +0100

    2016-09-04's linux workout

---
+++ contributions/Erstin/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
