5ea9c354764c48b38f43e9088d51799ddb0bf3c4
wild <georgedeheer6919@gmail.com>
2016-09-04 14:57:37 +0100

    2016-09-04's java workout

---
+++ contributions/wild/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
+Measuring time
+Increase `PermGen` space as to avoid `OutOfMemory` errors
