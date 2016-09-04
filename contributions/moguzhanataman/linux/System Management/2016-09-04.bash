199d84a18fb7c9202ba3c5351a84c855fe888f3b
moguzhanataman <thekhug@hotmail.com>
2016-09-04 18:56:54 +0100

    2016-09-04's linux workout

---
+++ contributions/moguzhanataman/linux/System Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
