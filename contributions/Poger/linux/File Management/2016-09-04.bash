b6fe8c950550ba053efd9218946ae19023139bfd
Poger <gkonupek@gmail.com>
2016-09-04 20:33:16 +0100

    2016-09-04's linux workout

---
+++ contributions/Poger/linux/File Management/2016-09-04.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
