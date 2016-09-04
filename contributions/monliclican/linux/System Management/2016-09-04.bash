1d9794f5b9ceaaf5c055c19e9e0b94ac3eee31ea
monliclican <mon.liclican@gmail.com>
2016-09-04 23:17:46 +0100

    2016-09-04's linux workout

---
+++ contributions/monliclican/linux/System Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Incorrect time on dual boot systems
+Commands to shutdown or restart the system
