c0bc76daf77a2b010068f8c20730cc4ab997231b
arnzzz <matthew.arnold28@gmail.com>
2016-09-04 20:37:02 +0100

    2016-09-04's git workout

---
+++ contributions/arnzzz/git/Essentials/2016-09-04.bash
@@ -0,0 +1,5 @@
+What is stored in a commit?
+Use `short` status to make output more compact
+How to write a git commit message
+`master` and `origin` aren't special
+Remote repositories: fetching and pushing
