dbad3dc443df98651e2684bfed7be3cacf125efc
mcheli <mpcheli7@gmail.com>
2016-09-04 18:33:09 +0100

    2016-09-04's javascript workout

---
+++ contributions/mcheli/javascript/Npm/2016-09-04.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Publishing with npm
+npm can handle multiple versions of a dependency
+Reduce package duplication 
+Lock down dependency versions by shrinkwrapping
