6572d58cc6ff31b699a6d5237697d93e5b070180
Mainvoid <visser.bouke@gmail.com>
2016-09-04 10:13:50 +0100

    2016-09-04's linux workout

---
+++ contributions/Mainvoid/linux/System Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
