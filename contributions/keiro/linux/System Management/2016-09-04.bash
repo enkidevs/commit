99dea481a49706db0e189dcdd60a430769bec0a9
keiro <keiro.dev+enki@gmail.com>
2016-09-04 14:35:14 +0100

    2016-09-04's linux workout

---
+++ contributions/keiro/linux/System Management/2016-09-04.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Cancel a system shutdown
