7417b407a320bb857fe9ef9e95dbe8b0b96d1747
mMetalStorm22 <parthgupta2008@gmail.com>
2016-09-04 21:25:26 +0100

    2016-09-03's java workout

---
+++ contributions/mMetalStorm22/java/Data Types/2016-09-03.java
@@ -0,0 +1,5 @@
+`ArrayList` vs `LinkedList`
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Converting a string to upper or lower case
