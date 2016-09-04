56ecac68a8360183044063d81ee5dc5c1f340476
i0stream <paul.iacomi@univ-amu.fr>
2016-09-04 11:14:51 +0100

    2016-09-04's linux workout

---
+++ contributions/i0stream/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
