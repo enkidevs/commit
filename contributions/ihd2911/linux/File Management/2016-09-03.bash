eb5ca3c95204cd4f1959ecbcbdfb639522a4e7b5
ihd2911 <ihd2911@gmail.com>
2016-09-04 17:07:37 +0100

    2016-09-03's linux workout

---
+++ contributions/ihd2911/linux/File Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Working with `.gz` files
