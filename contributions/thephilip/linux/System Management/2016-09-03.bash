3b88d9fbcdb80c5dc20400ee1f09b6b4fb6e9290
thephilip <thephilipsmith@gmail.com>
2016-09-04 02:26:33 +0100

    2016-09-03's linux workout

---
+++ contributions/thephilip/linux/System Management/2016-09-03.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
