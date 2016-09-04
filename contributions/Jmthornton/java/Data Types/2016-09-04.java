7d0a0254300b4db30441f9d519b39a0f371b3df5
Jmthornton <jmthornton4@gmail.com>
2016-09-04 17:11:08 +0100

    2016-09-04's java workout

---
+++ contributions/Jmthornton/java/Data Types/2016-09-04.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Converting a string to upper or lower case
+Retrieve the component type of an array
+Numeric Conversion - Widening
