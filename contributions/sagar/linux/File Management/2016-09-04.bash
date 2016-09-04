a54ae5b6d402e6795149487ecc6766bf93a33262
sagar <sadubey@syr.edu>
2016-09-05 00:02:13 +0100

    2016-09-04's linux workout

---
+++ contributions/sagar/linux/File Management/2016-09-04.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
