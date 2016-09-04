a83d46f01843271cccc33013e7312cc52ceb1a62
cmbpgh <cmbpgh@gmail.com>
2016-09-04 13:53:43 +0100

    2016-09-04's linux workout

---
+++ contributions/cmbpgh/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Terminal incognito mode
+Adding directories to your `$PATH`
