d5acc0cc4c536f4919f1c585e6930a9e1bf5ef94
remoteVoyager <mlukaszewicz2@gmail.com>
2016-09-04 16:49:17 +0100

    2016-09-04's linux workout

---
+++ contributions/remoteVoyager/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Useful aliasing in bash
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
