68c61ec0ce0e0f67c8e6a528bc2fbba89d4ecbcd
TMarmol <alv_tinoco_97@hotmail.com>
2016-09-04 16:59:54 +0100

    2016-09-04's linux workout

---
+++ contributions/TMarmol/linux/System Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+System uptime
