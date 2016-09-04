dacc65ea241f5be4466cea4d31dcb164d0e9178a
CallMeFabioo <callmefabioo@gmail.com>
2016-09-04 23:44:40 +0100

    2016-09-04's linux workout

---
+++ contributions/CallMeFabioo/linux/System Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
