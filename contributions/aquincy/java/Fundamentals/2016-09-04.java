deae472fbdfdcef5a37135dbb55b94d3aed2e2ec
aquincy <brennanjl003@gmail.com>
2016-09-04 18:59:10 +0100

    2016-09-04's java workout

---
+++ contributions/aquincy/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
+Implementing an `interface`
