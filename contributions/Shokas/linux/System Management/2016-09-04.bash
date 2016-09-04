ba0326980e610e3c4ab52ff01b6c5781417d9a9e
Shokas <shokutis@gmail.com>
2016-09-04 20:18:02 +0100

    2016-09-04's linux workout

---
+++ contributions/Shokas/linux/System Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
