01ed86a41866aab4dfdcc73da373d02d4cf5cd94
benjenkinsv95 <benjenkinsv95@gmail.com>
2016-09-04 17:35:03 +0100

    2016-09-04's java workout

---
+++ contributions/benjenkinsv95/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
