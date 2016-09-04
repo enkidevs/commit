5de4ddd47cbb9147afedb10339968ec451a67ab0
Syair <yangshi93@gmail.com>
2016-09-04 05:17:36 +0100

    2016-09-03's java workout

---
+++ contributions/Syair/java/Error Handling/2016-09-03.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Most common reason behind **stack overflow** error
+Careful Numeric Data Conversions
