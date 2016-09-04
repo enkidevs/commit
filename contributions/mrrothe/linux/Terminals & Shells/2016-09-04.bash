9705ea7f07965a511bbc11b5fa85a12b41e381e9
mrrothe <martin@mrothe.co.uk>
2016-09-04 09:34:34 +0100

    2016-09-04's linux workout

---
+++ contributions/mrrothe/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
