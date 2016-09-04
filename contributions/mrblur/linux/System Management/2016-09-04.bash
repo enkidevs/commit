0cdd576909aff878b313b9cc06880b3550e87673
mrblur <kamil+enki@mrblur.net>
2016-09-04 07:57:36 +0100

    2016-09-04's linux workout

---
+++ contributions/mrblur/linux/System Management/2016-09-04.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
