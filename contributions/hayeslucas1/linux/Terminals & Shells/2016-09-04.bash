87ee1bd2825e31880bfca775659a860baa132aab
hayeslucas1 <music.is.mii.life@gmail.com>
2016-09-04 21:26:02 +0100

    2016-09-04's linux workout

---
+++ contributions/hayeslucas1/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Quick `bash` shortcuts 
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
