c78f17cdf57298d170ee9b0b549d3b661fdc5820
KazRodgers <nohitokaze@gmail.com>
2016-09-04 18:31:37 +0100

    2016-09-04's java workout

---
+++ contributions/KazRodgers/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Ternary operator
+Using Inheritance to reduce code repetition
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
