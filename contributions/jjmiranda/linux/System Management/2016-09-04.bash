40f054428c31e4db929afac646938a5eaca747ce
jjmiranda <jjmiranda@magia.digital>
2016-09-04 13:47:47 +0100

    2016-09-04's linux workout

---
+++ contributions/jjmiranda/linux/System Management/2016-09-04.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Detect your linux distribution
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
