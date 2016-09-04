abbad0da1558d5dd7a335ebb52b3750b6889d688
jimschuh <jim.schuh@gmail.com>
2016-09-04 15:05:04 +0100

    2016-09-04's linux workout

---
+++ contributions/jimschuh/linux/System Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Check the HDD with `badblocks`
+Basic service management with `systemd`
+Commands to shutdown or restart the system
