7e19b53994ac1f27fc7e9c11510d486943293c1d
zszuri <szuri90@gmail.com>
2016-09-04 15:29:30 +0100

    2016-09-04's java workout

---
+++ contributions/zszuri/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,4 @@
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
