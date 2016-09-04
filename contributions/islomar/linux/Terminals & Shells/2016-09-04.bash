39d49a96df47156a5fce9033b8e6ad70728d683d
islomar <islomar@gmail.com>
2016-09-04 17:27:14 +0100

    2016-09-04's linux workout

---
+++ contributions/islomar/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Clear bash history
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
