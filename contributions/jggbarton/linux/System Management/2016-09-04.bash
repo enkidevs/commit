4c6bdcd2c9e386a3735d7967b989b1331a58bc8d
jggbarton <jggbarton@gmail.com>
2016-09-04 09:27:55 +0100

    2016-09-04's linux workout

---
+++ contributions/jggbarton/linux/System Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
