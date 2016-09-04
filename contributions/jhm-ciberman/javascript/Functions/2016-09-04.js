9d6e4a7f5e966e7128f2dd13575c6aaa817a5542
jhm-ciberman <jhm.ciberman@gmail.com>
2016-09-04 19:28:59 +0100

    2016-09-04's javascript workout

---
+++ contributions/jhm-ciberman/javascript/Functions/2016-09-04.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Method chaining
+Changing a functions context with `fn.call(object)`
