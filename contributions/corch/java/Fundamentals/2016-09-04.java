6badb6b39973b244e6f995ff7f0ec51226a9780d
corch <dczempisz@gmail.com>
2016-09-04 22:26:46 +0100

    2016-09-04's java workout

---
+++ contributions/corch/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Measuring time
+Using `synchronized` statements
