6f1827db8e6bf9fc9b4b9f8d0f6fb102e153990b
brumeregan <brumeregan@gmail.com>
2016-09-04 07:27:59 +0100

    2016-09-04's linux workout

---
+++ contributions/brumeregan/linux/System Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Executing scripts on system boot using `systemd`
+System uptime
