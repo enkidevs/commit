dce41d7a390afb2dc918f1afa0cc4cae26d7b98d
alexinman <alexinmann@gmail.com>
2016-09-04 15:51:42 +0100

    2016-09-04's javascript workout

---
+++ contributions/alexinman/javascript/Functions/2016-09-04.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Anonymous functions
+IIFE pattern
+Get query/url variables
+Changing a functions context with `fn.call(object)`
