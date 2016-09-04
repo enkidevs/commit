32ab40c887b33fa568367b85460f73bbc6f75ba2
millondas28 <millond705@gmail.com>
2016-09-04 19:26:11 +0100

    2016-09-03's java workout

---
+++ contributions/millondas28/java/I/O/2016-09-03.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to create directories in Java
+Create system file paths using the `Path` class
+Using buffered streams
+Checking for the existence of files and directories
