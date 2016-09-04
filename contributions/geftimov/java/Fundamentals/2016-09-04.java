50f7610299150e16c37a071b5b51892458236454
geftimov <jokatavr@gmail.com>
2016-09-04 10:14:29 +0100

    2016-09-04's java workout

---
+++ contributions/geftimov/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
