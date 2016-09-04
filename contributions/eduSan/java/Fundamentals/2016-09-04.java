e354da220f7eaa3f20930e4c7e5449eeaa3d9635
eduSan <flegias87@gmail.com>
2016-09-04 14:45:14 +0100

    2016-09-04's java workout

---
+++ contributions/eduSan/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Measuring time
+Detect or prevent integer overflow
+Supply `toString()` in all classes
