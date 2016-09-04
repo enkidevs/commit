4598bb90a40bcd516664fb77c9653839f4a28601
recontroop <rusbeh.turki@gmail.com>
2016-09-04 09:43:53 +0100

    2016-09-04's linux workout

---
+++ contributions/recontroop/linux/System Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
