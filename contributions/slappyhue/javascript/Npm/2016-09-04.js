3333042ed31a91a3eb491c38893c363d575d7914
slappyhue <slappyhue@gmail.com>
2016-09-04 08:12:53 +0100

    2016-09-04's javascript workout

---
+++ contributions/slappyhue/javascript/Npm/2016-09-04.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Manipulate npm packages cache
+npm can handle multiple versions of a dependency
+`config` object in `package.json`
+Lock down dependency versions by shrinkwrapping
