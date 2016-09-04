b7c16fee6bd9c288c047f81c238df3e38842d531
Nearein <yuan@neuf.fr>
2016-09-04 10:12:48 +0100

    2016-09-04's java workout

---
+++ contributions/Nearein/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Writing generic methods
+Inheriting a constructor from a superclass
+Using `Printstream.format` to format a string before printing
+Using `synchronized` statements
+`Autoboxing` and `Unboxing`
