3639f6b9713aff73532f6172ddc2da0e2ef355a0
mlrdk <mlrd@tilda.center>
2016-09-04 04:58:27 +0100

    2016-09-04's linux workout

---
+++ contributions/mlrdk/linux/Networking/2016-09-04.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Stress testing with `ab`
+Useful ssh client optimizations 
+List your IPv6 configuration
+Staying connected remotely via SSH without an interactive shell
