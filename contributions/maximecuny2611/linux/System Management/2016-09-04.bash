95419f2225b5a63b3d9435cccda2d3b2901babe2
maximecuny2611 <maxime.cuny@epita.fr>
2016-09-04 20:27:54 +0100

    2016-09-04's linux workout

---
+++ contributions/maximecuny2611/linux/System Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
