32ece358ebdb43e887cd5fea8cc8addcf50d8f49
Alanbato <alanvelasco.a@gmail.com>
2016-09-04 15:23:39 +0100

    2016-09-04's linux workout

---
+++ contributions/Alanbato/linux/File Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
