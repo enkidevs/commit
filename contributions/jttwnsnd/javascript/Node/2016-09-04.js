de03889ad38f99fb333bc2cef283f245ba7adda0
jttwnsnd <jt.a.townsend@gmail.com>
2016-09-04 22:54:59 +0100

    2016-09-04's javascript workout

---
+++ contributions/jttwnsnd/javascript/Node/2016-09-04.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+The built-in Node debugger
+Wrapping errors in Node.js using _node-verror_
+Handle `JSON.parse` error in Node.js
+`process.nextTick()` callback pattern
