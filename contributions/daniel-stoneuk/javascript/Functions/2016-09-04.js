5cb5a3295d05c301607e44369a3b500ea6bb466c
daniel-stoneuk <danielstone411@gmail.com>
2016-09-04 16:47:41 +0100

    2016-09-04's javascript workout

---
+++ contributions/daniel-stoneuk/javascript/Functions/2016-09-04.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Anonymous functions
