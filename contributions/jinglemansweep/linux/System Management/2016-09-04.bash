2f4fc076a9d47c50b984f196673d8e36b07ac8a7
jinglemansweep <jinglemansweep@gmail.com>
2016-09-04 10:14:37 +0100

    2016-09-04's linux workout

---
+++ contributions/jinglemansweep/linux/System Management/2016-09-04.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Test disk speed with `dd`
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
