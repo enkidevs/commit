abce8c6a3a65c607d70c97068016e06e932ce199
chausen <clayhausen@gmail.com>
2016-09-05 00:00:48 +0100

    2016-09-04's linux workout

---
+++ contributions/chausen/linux/System Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
