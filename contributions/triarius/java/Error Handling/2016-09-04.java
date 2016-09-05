15df8428c9962424e05d9c76c42bd8a3a9e093f7
triarius <narthana.epa+trigtriarius@gmail.com>
2016-09-05 11:36:57 +0100

    2016-09-04's java workout

---
+++ contributions/triarius/java/Error Handling/2016-09-04.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Using exceptions in Java
+Most common reason behind **stack overflow** error
