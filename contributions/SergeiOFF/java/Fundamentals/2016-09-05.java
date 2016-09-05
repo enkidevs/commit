84380f5cb700490208fc4d4ef7c6ce094ee73120
SergeiOFF <sergeioff57@gmail.com>
2016-09-05 14:27:47 +0100

    2016-09-05's java workout

---
+++ contributions/SergeiOFF/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
