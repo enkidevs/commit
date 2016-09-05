1b89eab1a17852ccd11801320bdc9abc37c7965c
mayermic <mayermic@pdx.edu>
2016-09-05 07:02:20 +0100

    2016-09-04's linux workout

---
+++ contributions/mayermic/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
