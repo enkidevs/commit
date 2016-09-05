7a44c863afa5b3cfa3c43f6c49b9b99248c9507b
rjtrout <kilgorejohntrout@gmail.com>
2016-09-05 02:02:36 +0100

    2016-09-04's linux workout

---
+++ contributions/rjtrout/linux/System Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+System uptime
