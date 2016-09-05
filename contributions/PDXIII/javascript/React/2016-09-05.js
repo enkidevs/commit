ea04b4adb7f99c8a85faef5f07e0a9c332332bc6
PDXIII <sekan.pete@gmail.com>
2016-09-05 11:53:32 +0100

    2016-09-05's javascript workout

---
+++ contributions/PDXIII/javascript/React/2016-09-05.js
@@ -0,0 +1,6 @@
+`if`-`else` statements in **JSX** and **React**
+Props in getInitialState Is an        
+Anti-Pattern
+**React** `this.props.children`
+Keyed fragments in **React**
+Specify a single child
