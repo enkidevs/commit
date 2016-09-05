5a89aa751a5e408f91f26b3f7ed721b4bbaabd7c
ritwikraghav14 <ritwikraghav14@hotmail.com>
2016-09-05 13:54:32 +0100

    2016-09-04's linux workout

---
+++ contributions/ritwikraghav14/linux/System Management/2016-09-04.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
