c15452c2f10775c5cb1cfefdb98d921812bbd042
ossi_jr <peter.osburg+enki@gmail.com>
2016-09-05 08:42:23 +0100

    2016-09-05's javascript workout

---
+++ contributions/ossi_jr/javascript/React/2016-09-05.js
@@ -0,0 +1,6 @@
+Pixel values in style props for **React**
+Props in getInitialState Is an        
+Anti-Pattern
+Dangerously set `innerHTML`
+Immutability helpers in **React**
+`ref` callback attribute
