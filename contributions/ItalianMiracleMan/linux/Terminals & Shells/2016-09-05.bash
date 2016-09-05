7fb7ffe14cf78f06e9377d12836b8597790240a4
ItalianMiracleMan <riccardo.spinato@gmail.com>
2016-09-05 19:25:47 +0100

    2016-09-05's linux workout

---
+++ contributions/ItalianMiracleMan/linux/Terminals & Shells/2016-09-05.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
