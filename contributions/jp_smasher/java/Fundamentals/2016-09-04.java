58deb9037716e7ec98c4d225cb094174e8df3c59
jp_smasher <wongjingping@gmail.com>
2016-09-05 02:32:19 +0100

    2016-09-04's java workout

---
+++ contributions/jp_smasher/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Calling one constructor from another
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
