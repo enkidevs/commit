ad67fdeebb17dbb059f80479283808583e96bae2
jdavidpeter <jdavidpeter@gmail.com>
2016-09-05 07:57:23 +0100

    2016-09-05's java workout

---
+++ contributions/jdavidpeter/java/Classes & Objects/2016-09-05.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Using `static` fields
+Utility classes and `static` methods
+Compare classes and not class names
+Fields with Java **Reflection API**
