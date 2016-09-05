3e281b6cf78f253e6b9de23321f3caf027dcff17
PatrikLord <patrik.j.lord@gmail.com>
2016-09-05 09:36:16 +0100

    2016-09-05's java workout

---
+++ contributions/PatrikLord/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Measuring time
+Always specify type parameter
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
