4e6dfe002645359bc8d628b29b7808201da2d903
PandaProgrammer <kaythepandaprogrammer@gmail.com>
2016-09-05 12:31:48 +0100

    2016-09-05's javascript workout

---
+++ contributions/PandaProgrammer/javascript/Core/2016-09-05.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Setting default values with `short circuiting`
+Using the double tilde `~~` 
+Apply `map` function to array items
+The difference between null, undefined and NaN
