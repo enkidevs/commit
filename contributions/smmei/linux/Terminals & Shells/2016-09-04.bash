056fbd779109cf2e2af7a3499a43ff324a1ba977
smmei <smmei@foxmail.com>
2016-09-05 18:00:43 +0100

    2016-09-04's linux workout

---
+++ contributions/smmei/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Repeating commands with `watch`
+Find any Unix / Linux command
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
