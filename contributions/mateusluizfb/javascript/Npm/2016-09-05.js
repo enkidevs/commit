9f0f6f23fdcfed23d1d07f6b54cae4c1490a54df
mateusluizfb <mateuslfreitasb@gmail.com>
2016-09-06 00:13:12 +0100

    2016-09-05's javascript workout

---
+++ contributions/mateusluizfb/javascript/Npm/2016-09-05.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Bump package version in npm
+Calling remote scripts with npm
+Passing through command line arguments in npm scripts
+`config` object in `package.json`
