2141d919ad3d11a97b0d85b3cd2821789a810566
mistyfiky <janmalisz@windowslive.com>
2016-09-05 23:40:57 +0100

    2016-09-05's linux workout

---
+++ contributions/mistyfiky/linux/Terminals & Shells/2016-09-05.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
