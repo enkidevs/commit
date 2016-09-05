649a3c25add7ff6cde4c50c436a3a5b7606c15c6
jcarneiro <jvmcarneiro@openmailbox.org>
2016-09-05 22:27:15 +0100

    2016-09-05's linux workout

---
+++ contributions/jcarneiro/linux/Package Management/2016-09-05.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Backporting on Debian
+
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
+Keeping repos updated with `cron`
