2ce73568ca3af38136ce1267f68f2b4f36c3f34f
allanlewis <allanlewis99@gmail.com>
2016-09-05 08:58:15 +0100

    2016-09-05's javascript workout

---
+++ contributions/allanlewis/javascript/Browser APIs/2016-09-05.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Use hosted scripts to increase performance
+Warn user if **Back** button is pressed
+User Timing API
+Drag and Drop API
