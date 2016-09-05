849fcebf2607a7fdbfe78adf3061f02b8ca21d37
yusuu <yusuf.kalkan@gmx.at>
2016-09-05 20:35:56 +0100

    2016-09-05's java workout

---
+++ contributions/yusuu/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
+Using an interface as a parameter
