048ffc98b1698d5c4e8bb71ee0eaffb185d587ed
litorers <alexandrejoseph.tessier@gmail.com>
2016-09-05 14:58:04 +0100

    2016-09-05's linux workout

---
+++ contributions/litorers/linux/File Management/2016-09-05.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
