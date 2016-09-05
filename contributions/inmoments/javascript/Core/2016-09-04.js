3786ce7008c7fcb12713ddd9f127c7ea6cb2cca8
inmoments <thai_hacker@hotmail.com>
2016-09-05 02:55:14 +0100

    2016-09-04's javascript workout

---
+++ contributions/inmoments/javascript/Core/2016-09-04.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Hoisting applies only to variable declarations, not initializations
+Avoid using `with`
+`console.*` in JavaScript
+Inaccuracy of binary floating-point format
