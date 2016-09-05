ace322c97f413d5d101a5b696fd7a5b697106720
iwishiwasaneagle <jh.ewers@gmail.com>
2016-09-05 10:25:00 +0100

    2016-09-05's linux workout

---
+++ contributions/iwishiwasaneagle/linux/System Management/2016-09-05.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
