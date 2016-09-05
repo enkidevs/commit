0ed6c14c5a49f9c121c558310121b96974f9f03b
Vandalis <marangon.claudio@gmail.com>
2016-09-05 20:05:24 +0100

    2016-09-05's linux workout

---
+++ contributions/Vandalis/linux/System Management/2016-09-05.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
