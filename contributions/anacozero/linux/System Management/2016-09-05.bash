851a3818a660728b0b3b29ad67a128d63858e243
anacozero <anaco7@hotmail.com>
2016-09-05 20:18:42 +0100

    2016-09-05's linux workout

---
+++ contributions/anacozero/linux/System Management/2016-09-05.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Get hardware stack details with `lspci`
+Cancel a system shutdown
