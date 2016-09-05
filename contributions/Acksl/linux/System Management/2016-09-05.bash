bca643e52d07e77bb48e7f4b2eb4cc1026220f39
Acksl <axel.dahlberg12@gmail.com>
2016-09-05 21:47:33 +0100

    2016-09-05's linux workout

---
+++ contributions/Acksl/linux/System Management/2016-09-05.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
