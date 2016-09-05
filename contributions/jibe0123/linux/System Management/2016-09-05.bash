97f65898a842606172f0a5256407ce0ce27320c0
jibe0123 <jbagostin@gmail.com>
2016-09-05 13:57:09 +0100

    2016-09-05's linux workout

---
+++ contributions/jibe0123/linux/System Management/2016-09-05.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Finding Open Files With `lsof`
