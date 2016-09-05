7b6804ff45e26debfceae77ae25e5701766e3629
Tresdon <tresdonj@gmail.com>
2016-09-05 04:56:11 +0100

    2016-09-04's javascript workout

---
+++ contributions/Tresdon/javascript/Core/2016-09-04.js
@@ -0,0 +1,5 @@
+Data type comparison in `switch` statements
+Infinity
+Hoisting applies only to variable declarations, not initializations
+`catch` is block scoped
+Getting the *real* dimensions of an image
