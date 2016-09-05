929175899a8fbc1d273d44801b94690fc9a27f0b
liahim <c.costras@gmail.com>
2016-09-05 19:31:24 +0100

    2016-09-05's linux workout

---
+++ contributions/liahim/linux/Networking/2016-09-05.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Logging dropped firewall packets
+Sending emails from terminal / scripts
+Use `tcpdump` to listen to network interface traffic
+Staying connected remotely via SSH without an interactive shell
