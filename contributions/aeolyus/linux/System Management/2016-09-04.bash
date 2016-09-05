162ab05f6d1fefc5b99229ac87c64630747b671d
aeolyus <richardhuang.huang@gmail.com>
2016-09-05 01:45:06 +0100

    2016-09-04's linux workout

---
+++ contributions/aeolyus/linux/System Management/2016-09-04.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
