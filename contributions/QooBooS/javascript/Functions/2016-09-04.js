381174b9d6f1ddbc914dd6c7b85ed0a835bce6da
QooBooS <qooboos@gmail.com>
2016-09-05 01:06:53 +0100

    2016-09-04's javascript workout

---
+++ contributions/QooBooS/javascript/Functions/2016-09-04.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+Easily generate a random `HEX` color
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
