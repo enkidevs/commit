4fedf69fd87a2b6be1a556c146d17f3b6da66dc7
merlin <dominic.schaff@gmail.com>
2016-09-05 06:47:56 +0100

    2016-09-05's java workout

---
+++ contributions/merlin/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
