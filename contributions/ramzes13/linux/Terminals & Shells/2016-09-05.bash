cd83f8192c0ed987c2a120783953de61075cab50
ramzes13 <darii.petru@gmail.com>
2016-09-05 09:25:19 +0100

    2016-09-05's linux workout

---
+++ contributions/ramzes13/linux/Terminals & Shells/2016-09-05.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Aliasing ssh connections
+Find any Unix / Linux command
+Keep useful commands in your shell history with tags
