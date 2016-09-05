2c93b057ea2b6e6c6bec19d1767931e429dbb429
namhyun-gu <mnhan0403@gmail.com>
2016-09-05 09:08:14 +0100

    2016-09-04's java workout

---
+++ contributions/namhyun-gu/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Using `synchronized` statements
+Supply `toString()` in all classes
+Using an interface as a parameter
