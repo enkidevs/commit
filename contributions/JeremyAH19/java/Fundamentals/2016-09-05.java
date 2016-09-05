725e2d91aaba8200e5e1b4a293f057a5695354e7
JeremyAH19 <jerahertz@gmail.com>
2016-09-05 14:32:39 +0100

    2016-09-05's java workout

---
+++ contributions/JeremyAH19/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Cropping images
+Using bounded type parameters in generic methods
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
