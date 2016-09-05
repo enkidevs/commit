fcdc1caa48757f204e580588a27c51c0ead3d39e
Pabloku <paloga15@hotmail.com>
2016-09-05 21:21:20 +0100

    2016-09-05's linux workout

---
+++ contributions/Pabloku/linux/System Management/2016-09-05.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+System uptime
