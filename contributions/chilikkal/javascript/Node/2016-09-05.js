bbc7f360bbd16e73e34a5cc1903fec1fcdb7254c
chilikkal <chilikkal@gmail.com>
2016-09-05 21:05:40 +0100

    2016-09-05's javascript workout

---
+++ contributions/chilikkal/javascript/Node/2016-09-05.js
@@ -0,0 +1,5 @@
+Use conventional property names for error handling in Node.js
+**Node** `os` module
+Handle `JSON.parse` error in Node.js
+`process.nextTick()` callback pattern
+`try-catch` only for **sync** code
