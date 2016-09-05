91f576329d61cb29b4c6072d93cbb523bc44dca9
alex-pl <alex@neatnet.de>
2016-09-05 15:25:27 +0100

    2016-09-05's linux workout

---
+++ contributions/alex-pl/linux/System Management/2016-09-05.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
