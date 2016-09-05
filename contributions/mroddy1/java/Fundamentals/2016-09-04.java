9c110b4a8a886920b3ee9f4ad1148a2c4c42ec14
mroddy1 <mroddy1@msudenver.edu>
2016-09-05 04:46:58 +0100

    2016-09-04's java workout

---
+++ contributions/mroddy1/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Using Inheritance to reduce code repetition
+Do not perform bitwise and arithmetic operations on the same data
