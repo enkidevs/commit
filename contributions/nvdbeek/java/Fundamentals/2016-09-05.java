a6a2da21f49da3aa2e186ffeb29e8f06a2c34b6d
nvdbeek <nielsvdbeek@gmail.com>
2016-09-05 11:41:08 +0100

    2016-09-05's java workout

---
+++ contributions/nvdbeek/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Measuring time
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
