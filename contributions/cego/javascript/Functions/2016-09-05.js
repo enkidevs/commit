6343cd0d9c1c6e359403e7427bc751f5569b3ecf
cego <ivomonteiro@sigma9.pt>
2016-09-05 22:15:05 +0100

    2016-09-05's javascript workout

---
+++ contributions/cego/javascript/Functions/2016-09-05.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Function call method
+Move cursor at the end of text input
