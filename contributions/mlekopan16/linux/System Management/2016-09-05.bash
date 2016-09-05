4c347e3ca05b1d61d053e103f6242d15053d41ae
mlekopan16 <mlekopan17@gmail.com>
2016-09-05 12:05:02 +0100

    2016-09-05's linux workout

---
+++ contributions/mlekopan16/linux/System Management/2016-09-05.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Detect your linux distribution
+List current logged on users with `w`
+Preventing a user from logging into the system
+Basic service management with `systemd`
