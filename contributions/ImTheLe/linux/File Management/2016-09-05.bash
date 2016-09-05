53c1480ead5035204eb6fca8faf5755c49d98eb2
ImTheLe <leongrdic@gmail.com>
2016-09-05 23:01:12 +0100

    2016-09-05's linux workout

---
+++ contributions/ImTheLe/linux/File Management/2016-09-05.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
+Easy way of sharing files
