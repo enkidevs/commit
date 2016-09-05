938a0634874518c4ed2ed913550d1f0aafa9d0cf
viliusz <enki@gmn.lt>
2016-09-05 19:33:39 +0100

    2016-09-05's linux workout

---
+++ contributions/viliusz/linux/Networking/2016-09-05.bash
@@ -0,0 +1,5 @@
+Useful ssh client optimizations 
+Limit the `wget` download rate
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Staying connected remotely via SSH without an interactive shell
