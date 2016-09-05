ca852aa4fa83d9d65ba1d95d1652db103314dc56
zsolt.lovasz <zsolt.lovasz@gmail.com>
2016-09-05 11:13:18 +0100

    2016-09-05's linux workout

---
+++ contributions/zsolt.lovasz/linux/Terminals & Shells/2016-09-05.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Quick `bash` shortcuts 
+Random password generator
+Breaking out of a terminal when `ssh` locks
