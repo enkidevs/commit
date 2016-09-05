0f05380dfe4ed6e41cd039aae728104cf0a3e6e3
skuroda <sk@mieco.org>
2016-09-06 00:45:54 +0100

    2016-09-05's linux workout

---
+++ contributions/skuroda/linux/System Management/2016-09-05.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
