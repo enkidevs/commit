96aea78dfe16d7ff574ea8c23eefe4589c4b68d3
AliEsmaeili <aliesm7@gmail.com>
2016-09-05 06:43:53 +0100

    2016-09-04's linux workout

---
+++ contributions/AliEsmaeili/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+The Basics of Environment Variables
