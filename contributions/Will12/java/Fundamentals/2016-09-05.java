38292c311b622453908ce72b4f1eb24a19b7c4c5
Will12 <will12@gmail.com>
2016-09-05 16:58:31 +0100

    2016-09-05's java workout

---
+++ contributions/Will12/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Measuring time
+Using `synchronized` statements
