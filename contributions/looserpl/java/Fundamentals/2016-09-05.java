acfaaa19ce9f31139e6a570e609e9e4a1eff4e4f
looserpl <loose.marek@gmail.com>
2016-09-05 23:08:27 +0100

    2016-09-05's java workout

---
+++ contributions/looserpl/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
