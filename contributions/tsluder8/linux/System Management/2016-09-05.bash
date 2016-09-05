907216b2b4251b01d8c6ff4bf384a496911b57cc
tsluder8 <tsluder8@gmail.com>
2016-09-05 23:50:39 +0100

    2016-09-05's linux workout

---
+++ contributions/tsluder8/linux/System Management/2016-09-05.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Using `dd` To Create Disk Images
