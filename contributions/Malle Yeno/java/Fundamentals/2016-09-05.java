6b2b41b1852394494de75c9214504c424102a709
Malle Yeno <mishakuzma@gmail.com>
2016-09-05 18:10:57 +0100

    2016-09-05's java workout

---
+++ contributions/Malle Yeno/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Inheriting a constructor from a superclass
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Using an interface as a parameter
