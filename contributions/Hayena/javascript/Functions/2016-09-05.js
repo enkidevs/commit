d81887ef1627afe74d2eb5945596885789ffffec
Hayena <donovan.dekuiper@gmail.com>
2016-09-05 20:01:38 +0100

    2016-09-05's javascript workout

---
+++ contributions/Hayena/javascript/Functions/2016-09-05.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+IIFE pattern
+Easily generate a random `HEX` color
+Social sharing without widgets
