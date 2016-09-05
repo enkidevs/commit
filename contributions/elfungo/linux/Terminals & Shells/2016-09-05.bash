f3bc7d3373c10b20e4094c48141b5e6109c40fd6
elfungo <papafingoss@gmail.com>
2016-09-05 02:51:15 +0100

    2016-09-05's linux workout

---
+++ contributions/elfungo/linux/Terminals & Shells/2016-09-05.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Useful aliasing in bash
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+The Basics of Environment Variables
