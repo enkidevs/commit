28e24cde4f21231004fa764ebae2f01afdc45868
colinular <colinular@gmail.com>
2016-09-05 10:45:46 +0100

    2016-09-04's linux workout

---
+++ contributions/colinular/linux/File Management/2016-09-04.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
