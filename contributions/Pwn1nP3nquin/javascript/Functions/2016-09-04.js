ea0ab604795463c7d741b55f1c4b74d5bb23b231
Pwn1nP3nquin <lipetravis@gmail.com>
2016-09-05 04:10:28 +0100

    2016-09-04's javascript workout

---
+++ contributions/Pwn1nP3nquin/javascript/Functions/2016-09-04.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Method chaining
+Function call method
+Check if a document is done loading
