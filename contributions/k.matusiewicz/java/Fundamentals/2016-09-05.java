7fb96217708d1b6d9c832a82722669bb68866675
k.matusiewicz <k.matusiewicz@gmail.com>
2016-09-05 06:44:26 +0100

    2016-09-05's java workout

---
+++ contributions/k.matusiewicz/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Inheriting a constructor from a superclass
+Using `Printstream.format` to format a string before printing
+Calling one constructor from another
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
