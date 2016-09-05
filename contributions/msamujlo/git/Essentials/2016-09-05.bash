01f8909ca0e0c9c14c37ac369ee6967cf82b8344
msamujlo <michal.samujlo@gmail.com>
2016-09-05 23:06:56 +0100

    2016-09-05's git workout

---
+++ contributions/msamujlo/git/Essentials/2016-09-05.bash
@@ -0,0 +1,5 @@
+Using tags for version control
+Search for commits by author
+How to write a git commit message
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
