46d903200efd025d0a41b214ec38a54ecfc236be
Capocaccia <carter.capocaccia@gmail.com>
2016-09-05 06:22:14 +0100

    2016-09-04's javascript workout

---
+++ contributions/Capocaccia/javascript/Core/2016-09-04.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Precision
+Treating a boolean as number
+Hoisting applies only to variable declarations, not initializations
+Getting the *real* dimensions of an image
