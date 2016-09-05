457eee2309b202d1ee55f2d01a1f46342682a8c1
Eldergeek <david@geekguytech.com>
2016-09-05 14:55:08 +0100

    2016-09-05's linux workout

---
+++ contributions/Eldergeek/linux/System Management/2016-09-05.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Basic service management with `systemd`
