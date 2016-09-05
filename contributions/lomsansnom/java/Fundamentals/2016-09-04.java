4a3c937a8ddd79759a9f2aead088baa03d671563
lomsansnom <alexandre.cathalan@gmail.com>
2016-09-05 11:20:52 +0100

    2016-09-04's java workout

---
+++ contributions/lomsansnom/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
