255fa6f14129bf0733b53edd01f03f62e4e789a0
dpmidd <dmidd87@gmail.com>
2016-09-05 03:42:55 +0100

    2016-09-04's javascript workout

---
+++ contributions/dpmidd/javascript/Functions/2016-09-04.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Get query/url variables
+Easily generate a random `HEX` color
+Function call method
