21724588df8a319d7b0a003eeeaff0ce2e2fd2d6
ToxiCore <thegpubuilder@gmail.com>
2016-09-05 12:02:35 +0100

    2016-09-05's python workout

---
+++ contributions/ToxiCore/python/Data Structures/2016-09-05.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Operations with `bytes` and `bytearray`
+Special `queue` methods
+`weakref` proxies
+Binary record packing with `bytearray`
