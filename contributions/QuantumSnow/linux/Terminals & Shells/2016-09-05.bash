5ac85ecb47fdb17a97c4d4788eb2bd3b498bdf28
QuantumSnow <jakobtheai@gmail.com>
2016-09-05 20:59:34 +0100

    2016-09-05's linux workout

---
+++ contributions/QuantumSnow/linux/Terminals & Shells/2016-09-05.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Random password generator
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
