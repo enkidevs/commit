bdac6afe2f4f61b62476dabdb2a06953d1c540e6
dschonholtz <schonholtz.d@husky.neu.edu>
2016-09-05 21:14:30 +0100

    2016-09-05's linux workout

---
+++ contributions/dschonholtz/linux/Terminals & Shells/2016-09-05.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Random password generator
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
