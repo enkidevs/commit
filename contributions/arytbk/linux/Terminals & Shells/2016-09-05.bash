d484a8872ac9666988a16f29292dd917e054a93b
arytbk <arytebeka@mac.com>
2016-09-05 14:09:21 +0100

    2016-09-05's linux workout

---
+++ contributions/arytbk/linux/Terminals & Shells/2016-09-05.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Random password generator
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
