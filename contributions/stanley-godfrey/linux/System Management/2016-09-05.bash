53e343f6069ca3bb660d594afc4626aa1ac7fe37
stanley-godfrey <stanley.godfrey@bourne-grammar.lincs.sch.uk>
2016-09-05 21:44:44 +0100

    2016-09-05's linux workout

---
+++ contributions/stanley-godfrey/linux/System Management/2016-09-05.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
