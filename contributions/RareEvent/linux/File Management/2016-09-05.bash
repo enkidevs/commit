aa7a2585dbbaa0358c7c0ce830379bbf753b1967
RareEvent <ham@perfect-kreim.de>
2016-09-05 20:08:37 +0100

    2016-09-05's linux workout

---
+++ contributions/RareEvent/linux/File Management/2016-09-05.bash
@@ -0,0 +1,7 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
