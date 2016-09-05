1db4a8ba6c35a79d4d7f4c38efb0d36237da8cc4
SCoureFoure <scoraccioiv@gmail.com>
2016-09-05 15:41:03 +0100

    2016-09-05's javascript workout

---
+++ contributions/SCoureFoure/javascript/Core/2016-09-05.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Performing a function at timed intervals
+Extra function arguments are undefined by default
+Round numbers to `N` decimals 
+Base conversion 
