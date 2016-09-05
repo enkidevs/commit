4bbc3a4ddb5248f19da8d9f7dd7c273ab6f9d434
dwhitz <dwhitz.mo@gmail.com>
2016-09-05 07:20:58 +0100

    2016-09-05's linux workout

---
+++ contributions/dwhitz/linux/System Management/2016-09-05.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
