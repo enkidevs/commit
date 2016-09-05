bc55416587719ee612a8ed0cc9a1a89350990dcb
Uz78 <gualdi.mauro@gmail.com>
2016-09-05 13:10:23 +0100

    2016-09-05's linux workout

---
+++ contributions/Uz78/linux/System Management/2016-09-05.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
