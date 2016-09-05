8a8a0320f337d92d278e248ba59368bd15a76f16
rsuda <rsuda9@gmail.com>
2016-09-05 04:22:05 +0100

    2016-09-04's linux workout

---
+++ contributions/rsuda/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Aliasing ssh connections
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
