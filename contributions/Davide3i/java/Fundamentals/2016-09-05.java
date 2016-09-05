d546763126186eb2bb54251414d85d140c00fbd4
Davide3i <davide.ciocca@alice.it>
2016-09-05 08:01:28 +0100

    2016-09-05's java workout

---
+++ contributions/Davide3i/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
