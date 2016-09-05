3575eb443d1f6bf6492499490601efb2fb1f9a16
Vitalko <vit.romaniv@gmail.com>
2016-09-05 11:08:53 +0100

    2016-09-05's linux workout

---
+++ contributions/Vitalko/linux/Terminals & Shells/2016-09-05.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
