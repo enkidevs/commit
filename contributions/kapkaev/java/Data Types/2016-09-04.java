21a21cd478c9918292bfb5f03d73ca6255549763
kapkaev <kirs.box@gmail.com>
2016-09-05 05:54:17 +0100

    2016-09-04's java workout

---
+++ contributions/kapkaev/java/Data Types/2016-09-04.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Equals operation on different data types
+Numeric Conversion - Widening
