5d8a8e361136d4da01396bf37efcca8194e8919a
henriquetinti <henrique.tinti@inatel.br>
2016-09-05 02:23:16 +0100

    2016-09-04's linux workout

---
+++ contributions/henriquetinti/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Aliasing ssh connections
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
