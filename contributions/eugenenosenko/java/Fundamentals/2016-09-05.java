7c00df9d6360f1057530396602926c12d4bf2f28
eugenenosenko <eugenenosenko@gmail.com>
2016-09-05 12:49:49 +0100

    2016-09-05's java workout

---
+++ contributions/eugenenosenko/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
