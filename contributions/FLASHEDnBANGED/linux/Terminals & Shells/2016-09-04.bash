06c297be879f296cfd4968c61f1854e5d0888bdd
FLASHEDnBANGED <slitay32@gmail.com>
2016-09-05 15:56:47 +0100

    2016-09-04's linux workout

---
+++ contributions/FLASHEDnBANGED/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Terminal based browser
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
