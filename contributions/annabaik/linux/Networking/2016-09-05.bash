7ed45c4a4d0966f197f693271f13393a06f5adf0
annabaik <annaheyonbaik@gmail.com>
2016-09-05 15:16:12 +0100

    2016-09-05's linux workout

---
+++ contributions/annabaik/linux/Networking/2016-09-05.bash
@@ -0,0 +1,5 @@
+Useful ssh client optimizations 
+Limit the `wget` download rate
+List your IPv6 configuration
+Bandwidth monitoring tools 
+Staying connected remotely via SSH without an interactive shell
