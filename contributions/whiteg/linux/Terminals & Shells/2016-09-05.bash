be252ae7e47d80748c276a2424fc27a48555f612
whiteg <white9g@gmail.com>
2016-09-05 10:27:59 +0100

    2016-09-05's linux workout

---
+++ contributions/whiteg/linux/Terminals & Shells/2016-09-05.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Find any Unix / Linux command
+Terminal based browser
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
