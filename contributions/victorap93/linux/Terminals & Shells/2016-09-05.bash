3da63457c88e3a8fc46484a386e10ebee9dd42a4
victorap93 <victorap93@gmail.com>
2016-09-05 10:45:51 +0100

    2016-09-05's linux workout

---
+++ contributions/victorap93/linux/Terminals & Shells/2016-09-05.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
