013bc47dcf7f418f7996aea74a0d2cd17f770193
DavidLevi <david.levi999@gmail.com>
2016-09-05 06:39:52 +0100

    2016-09-05's python workout

---
+++ contributions/DavidLevi/python/Data Structures/2016-09-05.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Immutable sets with `frozenset`
+Get more with `collections`!
+There is more to copying
+Prioritize your `queue` 
