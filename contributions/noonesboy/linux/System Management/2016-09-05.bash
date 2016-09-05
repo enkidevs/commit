37ab9d5da688416f00bb53494201db124ad4e6c6
noonesboy <hello@skylundy.com>
2016-09-05 21:23:39 +0100

    2016-09-05's linux workout

---
+++ contributions/noonesboy/linux/System Management/2016-09-05.bash
@@ -0,0 +1,5 @@
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
