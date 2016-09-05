a4ac77bec54ee67b9791e27cc9cc589b214172aa
mhatretush <mhatretush@gmail.com>
2016-09-05 10:33:20 +0100

    2016-09-04's linux workout

---
+++ contributions/mhatretush/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Find any Unix / Linux command
+Terminal based browser
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
