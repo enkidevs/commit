05311824ef99394f6d14c4c5985f4d45d256b459
Kotolin <kotolin3000@yandex.ru>
2016-09-05 11:16:43 +0100

    2016-09-05's linux workout

---
+++ contributions/Kotolin/linux/Terminals & Shells/2016-09-05.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Find any Unix / Linux command
+Quick `bash` shortcuts 
+Terminal incognito mode
+Sequential execution using the `;` statement separator
