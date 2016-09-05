43041ca1b1c985feebfbd75b6a53c48e25466cb3
amharis <abdullah.m.haris@gmail.com>
2016-09-05 19:23:44 +0100

    2016-09-05's linux workout

---
+++ contributions/amharis/linux/File Management/2016-09-05.bash
@@ -0,0 +1,5 @@
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Working with `.gz` files
+Delete commands aliases
