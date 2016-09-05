39338c3c3a675a9179a1d1ad7f6e955ba7cef880
tarunv <tarunv1997@gmail.com>
2016-09-05 07:46:36 +0100

    2016-09-04's java workout

---
+++ contributions/tarunv/java/I/O/2016-09-04.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Checking for the existence of files and directories
+Reading file attributes
+List contents of a directory with `newDirectoryStream`
