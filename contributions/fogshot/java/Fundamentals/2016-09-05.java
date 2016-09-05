9ce954b657604d2547ea31f2549c362895b492cb
fogshot <fogshot@gmail.com>
2016-09-05 08:09:21 +0100

    2016-09-05's java workout

---
+++ contributions/fogshot/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
