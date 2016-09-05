e8d4411287d9458023e93e6597dfc366f6bc4d0e
djnixy <nikiakbar@gmail.com>
2016-09-05 06:19:49 +0100

    2016-09-04's java workout

---
+++ contributions/djnixy/java/Fundamentals/2016-09-04.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
