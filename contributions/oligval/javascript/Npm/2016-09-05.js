8c743991cf12852e1bf103b4cca8dcafc3fbcf31
oligval <oligval@gmail.com>
2016-09-05 21:14:47 +0100

    2016-09-05's javascript workout

---
+++ contributions/oligval/javascript/Npm/2016-09-05.js
@@ -0,0 +1,5 @@
+Create projects with `npm init`
+Manipulate npm packages cache
+npm can handle multiple versions of a dependency
+Passing through command line arguments in npm scripts
+Tab completion in `npm`
