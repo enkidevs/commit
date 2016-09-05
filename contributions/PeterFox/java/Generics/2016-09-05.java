25e2d1a843c62f3bafd5e8dd01b3803fda358685
PeterFox <slyfoxy@gmail.com>
2016-09-05 17:54:33 +0100

    2016-09-05's java workout

---
+++ contributions/PeterFox/java/Generics/2016-09-05.java
@@ -0,0 +1,5 @@
+Using initializer blocks to minimize code repetition in constructors
+Upper bounded wildcards
+Using unbounded wildcards
+Use lazy initialization to defer creating an object until it's used
+Avoid using `static` variables
