adf78b527cc104cebf5e438635b3d79bfcdde5ba
Morion <morion.self@gmail.com>
2016-09-05 09:45:19 +0100

    2016-09-04's java workout

---
+++ contributions/Morion/java/Data Types/2016-09-04.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Multidimensional array declaration
+Converting a string to upper or lower case
+`StringBuffer` vs `StringBuilder`
+String concatenation in Java
