eac34d2ca710b19535b52c02b9786f348ee727da
wampiedriessen <wampiedriessen@gmail.com>
2016-09-05 13:07:57 +0100

    2016-09-05's linux workout

---
+++ contributions/wampiedriessen/linux/File Management/2016-09-05.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Using `touch` to alter files modification time
+Easy way of sharing files
