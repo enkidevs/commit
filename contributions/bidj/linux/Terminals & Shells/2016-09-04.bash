4ef5e182f7e5f8b349d7f664d14fe9287b515c75
bidj <enki@papito.33mail.com>
2016-09-05 02:34:56 +0100

    2016-09-04's linux workout

---
+++ contributions/bidj/linux/Terminals & Shells/2016-09-04.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Clear bash history
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
