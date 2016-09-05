88a3867bc970880f021399c3cae04e2307df2333
samuel-cooper <samuel.cooper03@gmail.com>
2016-09-05 21:09:29 +0100

    2016-09-05's git workout

---
+++ contributions/samuel-cooper/git/Essentials/2016-09-05.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Make your log output pretty
+Search by commit message keyword
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
