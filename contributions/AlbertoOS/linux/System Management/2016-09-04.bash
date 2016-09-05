17614757f86d933af67da0b1548f9e15b37195fd
AlbertoOS <orkan.aos@gmail.com>
2016-09-05 04:30:05 +0100

    2016-09-04's linux workout

---
+++ contributions/AlbertoOS/linux/System Management/2016-09-04.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
