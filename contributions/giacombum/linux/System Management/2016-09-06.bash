e700df793b60e33cbe386f1efd900d2b7a348c88
giacombum <giacombum@gmail.com>
2016-09-06 18:23:10 +0100

    2016-09-06's linux workout

---
+++ contributions/giacombum/linux/System Management/2016-09-06.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Basic service management with `systemd`
+Cancel a system shutdown
