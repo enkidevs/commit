3cb4364aa8ffefcbaf94d036d240100ac0a360c2
78juli <j78juli@gmail.com>
2016-09-06 19:18:08 +0100

    2016-09-06's linux workout

---
+++ contributions/78juli/linux/Terminals & Shells/2016-09-06.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Useful aliasing in bash
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
