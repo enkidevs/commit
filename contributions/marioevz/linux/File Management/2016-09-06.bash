4a793099b043bd15e389168f847b6095f46477a6
marioevz <marioevz@gmail.com>
2016-09-06 18:09:02 +0100

    2016-09-06's linux workout

---
+++ contributions/marioevz/linux/File Management/2016-09-06.bash
@@ -0,0 +1,6 @@
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
