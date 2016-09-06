c9db117641cd74baaa60ed75414f04ffc4c26b5b
imih <ivamiho@gmail.com>
2016-09-06 09:09:00 +0100

    2016-09-06's linux workout

---
+++ contributions/imih/linux/System Management/2016-09-06.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
