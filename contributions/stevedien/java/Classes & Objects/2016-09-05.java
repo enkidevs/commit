9c2e1f9ef52e69bab908cd2563d4d650832f841c
stevedien <lulzsecd@gmail.com>
2016-09-06 14:51:07 +0100

    2016-09-05's java workout

---
+++ contributions/stevedien/java/Classes & Objects/2016-09-05.java
@@ -0,0 +1,5 @@
+Java object references
+Compare classes and not class names
+Double Brace Initialization
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
