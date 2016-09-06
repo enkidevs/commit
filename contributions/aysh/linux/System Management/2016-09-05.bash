f04ec9b26bfee19f29ff1d59e24aa5e47ecb6d26
aysh <ajay.shibu.ben@gmail.com>
2016-09-06 16:48:21 +0100

    2016-09-05's linux workout

---
+++ contributions/aysh/linux/System Management/2016-09-05.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
