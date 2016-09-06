6d610abfbf5ae0be412f9297f9f6ffc604d6fc00
jjmiranda <jjmiranda@magia.digital>
2016-09-06 18:13:45 +0100

    2016-09-06's linux workout

---
+++ contributions/jjmiranda/linux/System Management/2016-09-06.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
