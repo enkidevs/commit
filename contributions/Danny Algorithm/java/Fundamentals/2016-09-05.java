02c38038ec1113efcc5ff6a9ccdef91046db84e4
Danny Algorithm <mr.dannydesignx@gmail.com>
2016-09-06 05:48:49 +0100

    2016-09-05's java workout

---
+++ contributions/Danny Algorithm/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Use strings in a `switch` statement
+Inheriting a constructor from a superclass
+Always specify type parameter
+Limit Accessibility of `Fields`
