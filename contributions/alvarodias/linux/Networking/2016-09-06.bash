01b93390edf084e9f533f9b844d7b6928a216f00
alvarodias <alvaro.j.dias@gmail.com>
2016-09-06 21:53:26 +0100

    2016-09-06's linux workout

---
+++ contributions/alvarodias/linux/Networking/2016-09-06.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+Find services running on your host
+List your IPv6 configuration
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
