7ebeaf1b9159f827332a39e1b7dc1f51b5283953
okashirin <lokishadow@mail.ru>
2016-09-06 11:15:22 +0100

    2016-09-06's java workout

---
+++ contributions/okashirin/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
