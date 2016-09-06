0ea214a63abc3fdbccc42e49694a82853e8b8f85
Zynbot <barnes.c96@gmail.com>
2016-09-06 14:30:03 +0100

    2016-09-06's linux workout

---
+++ contributions/Zynbot/linux/System Management/2016-09-06.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+List current logged on users with `w`
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
