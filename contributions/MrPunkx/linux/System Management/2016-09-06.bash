0a7545f5e59c00801e349736bd4d225bbec5356c
MrPunkx <elfassiyarin@gmail.com>
2016-09-06 17:16:57 +0100

    2016-09-06's linux workout

---
+++ contributions/MrPunkx/linux/System Management/2016-09-06.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
