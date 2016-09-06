6f806677d3a1b38cfb1b60907ee4477cd9ab7fa6
Shadowgun <shadowgun@i.ua>
2016-09-06 20:05:30 +0100

    2016-09-06's java workout

---
+++ contributions/Shadowgun/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
