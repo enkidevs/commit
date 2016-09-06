4650960c2609fee622d192f6198541069ef0be5b
jonezine <jonezine@msn.com>
2016-09-06 16:01:25 +0100

    2016-09-06's linux workout

---
+++ contributions/jonezine/linux/System Management/2016-09-06.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
