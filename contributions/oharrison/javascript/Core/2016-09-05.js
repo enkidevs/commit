6c7d5bd4e648dbeb70519ccccd24121f5209f197
oharrison <onel.harrison@gmail.com>
2016-09-06 05:12:27 +0100

    2016-09-05's javascript workout

---
+++ contributions/oharrison/javascript/Core/2016-09-05.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Most efficient way to build `HTML` strings
+Hoisting applies only to variable declarations, not initializations
+Apply `map` function to array items
+Closures
