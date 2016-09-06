2419af3caedd79305bff25dcd6f2594f1018cd6a
cfouda <cfouda@smart421.com>
2016-09-07 00:31:19 +0100

    2016-09-06's java workout

---
+++ contributions/cfouda/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
+Supply `toString()` in all classes
