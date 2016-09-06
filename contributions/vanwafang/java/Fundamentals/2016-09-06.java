182f95d193fc6b8a0a11a8fb2d3a280ed04000f3
vanwafang <vanwafang@hotmail.com>
2016-09-06 15:15:18 +0100

    2016-09-06's java workout

---
+++ contributions/vanwafang/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Connecting to FTP using Java
+Metadata: setting a file's owner
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
