f9c8c4f7d31cfd173e9fb7a3896405514421da12
Jorgep <jorgelpaez19@gmail.com>
2016-09-06 17:20:55 +0100

    2016-09-06's java workout

---
+++ contributions/Jorgep/java/I/O/2016-09-06.java
@@ -0,0 +1,5 @@
+Create system file paths using the `Path` class
+Reading file attributes
+Retrieving file store attributes
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
