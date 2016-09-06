d32ec514c5d79c9f2ca2ce95bb6100c5007fca81
pendyala <sreedhar@stratus5.com>
2016-09-06 07:05:57 +0100

    2016-09-06's java workout

---
+++ contributions/pendyala/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Limit Accessibility of `Fields`
