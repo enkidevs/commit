5e18e92dfea67bf59511ec237a27a063415a9503
daniegarcia254 <daniegarcia254@gmail.com>
2016-09-06 18:38:17 +0100

    2016-09-06's linux workout

---
+++ contributions/daniegarcia254/linux/System Management/2016-09-06.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
