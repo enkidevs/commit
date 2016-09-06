a7578e2781e8760e18dcba1a4a364384c5befb31
iceblurr <thehuy.phan92@gmail.com>
2016-09-06 07:06:44 +0100

    2016-09-06's linux workout

---
+++ contributions/iceblurr/linux/Terminals & Shells/2016-09-06.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Aliasing ssh connections
+Conditional command execution
+
+(`&&` operator)
+The Basics of Environment Variables
