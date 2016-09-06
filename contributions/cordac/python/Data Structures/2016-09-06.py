43a372467581bb85501b2ec170719c38a955d921
cordac <zal3wski.piotr@gmail.com>
2016-09-06 07:36:08 +0100

    2016-09-06's python workout

---
+++ contributions/cordac/python/Data Structures/2016-09-06.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`set` operations
+Enhance your `tuple`s
+Implementing **weak** references
+Binary record packing with `bytearray`
