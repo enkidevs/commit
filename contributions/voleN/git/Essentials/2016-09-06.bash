48f47501419ff8f195604dee8b9d20ac1db82d0f
voleN <planbshredder@gmail.com>
2016-09-06 15:59:11 +0100

    2016-09-06's git workout

---
+++ contributions/voleN/git/Essentials/2016-09-06.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+How to set your username and email
+Search by commit message keyword
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
