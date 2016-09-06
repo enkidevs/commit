9d1e8d7f60056de6163d80f4f3e1587892b75885
yahiaos <yahia.rooney@gmail.com>
2016-09-06 19:44:28 +0100

    2016-09-06's java workout

---
+++ contributions/yahiaos/java/Error Handling/2016-09-06.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Most common reason behind **stack overflow** error
+Throwing an `exception`
