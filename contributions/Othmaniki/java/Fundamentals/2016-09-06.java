92a7b1c77ce02ef7ae40b87f2fc54b499cc580dc
Othmaniki <azzaothman1@gmail.com>
2016-09-06 19:18:34 +0100

    2016-09-06's java workout

---
+++ contributions/Othmaniki/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
