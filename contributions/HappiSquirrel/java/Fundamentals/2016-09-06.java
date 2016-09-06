22f479f3aad6aea940deac26281ec90aa2e70a1b
HappiSquirrel <ludo0302@hotmail.fr>
2016-09-06 09:05:27 +0100

    2016-09-06's java workout

---
+++ contributions/HappiSquirrel/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
