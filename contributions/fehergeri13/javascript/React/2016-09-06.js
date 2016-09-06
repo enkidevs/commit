273356a5bbde36cb1e94a10faa69e1bd59b50fc7
fehergeri13 <fehergeri13@gmail.com>
2016-09-06 22:20:46 +0100

    2016-09-06's javascript workout

---
+++ contributions/fehergeri13/javascript/React/2016-09-06.js
@@ -0,0 +1,6 @@
+componentWillReceiveProps Not Triggered After Mounting
+
+`PureRenderMixin` in **React**
+Shallow compare in **React**
+Validate for required props
+Custom `propType`'s to be required
