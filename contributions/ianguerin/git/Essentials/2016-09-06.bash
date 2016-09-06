f84995ec99140b48f72c953bc1f32b0f1e2e5850
ianguerin <img236@nyu.edu>
2016-09-06 22:31:00 +0100

    2016-09-06's git workout

---
+++ contributions/ianguerin/git/Essentials/2016-09-06.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+Search for commits by author
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
