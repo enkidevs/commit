5c2e044c18928b9566875d96ccb049d5345cd57c
Tansito <david.valinhas@gmail.com>
2016-09-06 23:00:35 +0100

    2016-09-06's java workout

---
+++ contributions/Tansito/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
