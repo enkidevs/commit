da62420720bedb9c5928c19445a3790fa4a656f2
curmy <ccurran985@icloud.com>
2016-09-06 09:53:06 +0100

    2016-09-06's java workout

---
+++ contributions/curmy/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Using bounded type parameters in generic methods
+Do not perform bitwise and arithmetic operations on the same data
