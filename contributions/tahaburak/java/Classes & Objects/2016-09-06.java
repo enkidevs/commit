8633269ea9c22d6513c28f412493a41f82362b99
tahaburak <tahaburak.koc@gmail.com>
2016-09-06 23:11:01 +0100

    2016-09-06's java workout

---
+++ contributions/tahaburak/java/Classes & Objects/2016-09-06.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Use a copy constructor for non-immutable objects
+Keeping fields `private`
+Compare classes and not class names
+String formatting and parsing with `SimpleDateFormat`
