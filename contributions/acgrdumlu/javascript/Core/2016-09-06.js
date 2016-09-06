b0a834bc815d34b7a9988c679218c74ccec4a2c4
acgrdumlu <cgrdumlu@gmail.com>
2016-09-06 08:50:56 +0100

    2016-09-06's javascript workout

---
+++ contributions/acgrdumlu/javascript/Core/2016-09-06.js
@@ -0,0 +1,5 @@
+Keywords in hash keys
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Detect online connection
+Inaccuracy of binary floating-point format
