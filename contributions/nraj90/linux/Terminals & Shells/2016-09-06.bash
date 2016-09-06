4ce4419e66650fb9f05c4a48341ac772bcfdcf6e
nraj90 <nimesh90@gmail.com>
2016-09-06 20:12:18 +0100

    2016-09-06's linux workout

---
+++ contributions/nraj90/linux/Terminals & Shells/2016-09-06.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Useful aliasing in bash
+Get to know  your commands with `type`
+The Basics of Environment Variables
