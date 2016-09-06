090fc6d31a1d2fd5ce461ea9fc537cbfc3577e76
kevenYannic <keven.iwansky@gmail.com>
2016-09-06 08:12:23 +0100

    2016-09-06's java workout

---
+++ contributions/kevenYannic/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
