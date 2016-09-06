cff192d0d96203ba17674eb0ac2ae8b78e357b9e
bstruthers <bstruthers@gmail.com>
2016-09-06 18:31:17 +0100

    2016-09-06's linux workout

---
+++ contributions/bstruthers/linux/System Management/2016-09-06.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
