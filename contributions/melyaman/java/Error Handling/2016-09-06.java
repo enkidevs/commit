c0cf6b00a7db2ab13041db79a7c8298b94195fdf
melyaman <mohamed.elyamani@telecom-bretagne.eu>
2016-09-06 16:40:47 +0100

    2016-09-06's java workout

---
+++ contributions/melyaman/java/Error Handling/2016-09-06.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Checking a double for `NaN` value
