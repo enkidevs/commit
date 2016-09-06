e93facf399b390d7d3093700881a77ca122912b7
Edvinas <edvinas.reg@outlook.com>
2016-09-06 15:05:37 +0100

    2016-09-06's java workout

---
+++ contributions/Edvinas /java/Data Types/2016-09-06.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Equals operation on different data types
