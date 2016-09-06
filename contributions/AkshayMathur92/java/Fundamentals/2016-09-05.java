a726556804488b610ab145a0c2284f74e590c1be
AkshayMathur92 <avi.mathur.engg@gmail.com>
2016-09-06 19:05:36 +0100

    2016-09-05's java workout

---
+++ contributions/AkshayMathur92/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Metadata: creating a user-defined file attribute
+Measuring time
+Always specify type parameter
+Increase `PermGen` space as to avoid `OutOfMemory` errors
