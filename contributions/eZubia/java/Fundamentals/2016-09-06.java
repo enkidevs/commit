e09a94ead5bac85abde6693f9a54b9511207742a
eZubia <edzh18@gmail.com>
2016-09-06 18:02:45 +0100

    2016-09-06's java workout

---
+++ contributions/eZubia/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
