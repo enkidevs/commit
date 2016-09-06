335eb5b6e3a7ef44ef866f51d2faa1b841c0304a
bbialek <bialcus@gmail.com>
2016-09-06 10:53:01 +0100

    2016-09-06's linux workout

---
+++ contributions/bbialek/linux/Security/2016-09-06.bash
@@ -0,0 +1,5 @@
+Extracting the `public key` from the `private key`
+Disable SSH password authentication 
+Track SSH log-in attempts
+Disabling **USB** storage devices
+Private key encryption with `OpenSSL`
