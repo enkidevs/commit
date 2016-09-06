813ad659a27bd805dfc72815ad3edc65de43fbe1
k3ypad <ben@tfa.ie>
2016-09-06 11:36:05 +0100

    2016-09-06's linux workout

---
+++ contributions/k3ypad/linux/System Management/2016-09-06.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
