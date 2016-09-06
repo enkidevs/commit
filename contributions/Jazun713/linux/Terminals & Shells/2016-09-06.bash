887a225df185de3444b7de7d3131625e7c7ee0fa
Jazun713 <siros.work@gmail.com>
2016-09-06 15:17:10 +0100

    2016-09-06's linux workout

---
+++ contributions/Jazun713/linux/Terminals & Shells/2016-09-06.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
