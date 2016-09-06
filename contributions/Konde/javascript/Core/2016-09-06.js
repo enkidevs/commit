0296dddd279df08fa23664a2c274fb4401ac5ff7
Konde <adrconde@gmail.com>
2016-09-07 00:44:22 +0100

    2016-09-06's javascript workout

---
+++ contributions/Konde/javascript/Core/2016-09-06.js
@@ -0,0 +1,5 @@
+`hasOwnProperty` method
+Most efficient way to build `HTML` strings
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+`catch` is block scoped
