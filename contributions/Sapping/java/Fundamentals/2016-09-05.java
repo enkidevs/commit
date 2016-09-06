9c42acced91d2770a6ad91f20fe1461252d79647
Sapping <lucas.melin@gmail.com>
2016-09-06 01:49:25 +0100

    2016-09-05's java workout

---
+++ contributions/Sapping/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
