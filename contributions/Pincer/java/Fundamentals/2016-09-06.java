936460d7bde8c58e1b37b666252bebe930b2d0af
Pincer <jan.jauslin@gmail.com>
2016-09-06 06:56:08 +0100

    2016-09-06's java workout

---
+++ contributions/Pincer/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
