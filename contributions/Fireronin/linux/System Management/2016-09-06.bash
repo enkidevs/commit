30a169d098be72caf945348ec32cbb87ea774a19
Fireronin <nudy148@gmail.com>
2016-09-06 06:59:36 +0100

    2016-09-06's linux workout

---
+++ contributions/Fireronin/linux/System Management/2016-09-06.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
