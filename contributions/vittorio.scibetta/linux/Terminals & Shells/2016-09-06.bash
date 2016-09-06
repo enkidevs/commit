e782ba649d52368f4cd89d2471d9299f7dbdf66a
vittorio.scibetta <vittorio.scibetta@virgilio.it>
2016-09-06 08:37:51 +0100

    2016-09-06's linux workout

---
+++ contributions/vittorio.scibetta/linux/Terminals & Shells/2016-09-06.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Quick `bash` shortcuts 
+Conditional command execution
+
+(`&&` operator)
