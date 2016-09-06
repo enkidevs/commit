900fbaf1e3cb34cebb008affead71c5407f0716a
ak4t0sh <ak4t0sh@free.fr>
2016-09-06 06:36:39 +0100

    2016-09-06's linux workout

---
+++ contributions/ak4t0sh/linux/Pipeline/2016-09-06.bash
@@ -0,0 +1,5 @@
+Difference between `redirection` and `pipe`
+Monitor the progress of data through a pipe with `pv`
+Using `xargs` to build commands.
+Run local scripts remotely
+Play sounds remotely
