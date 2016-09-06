b4a4cdf14e83506e8694455e9f6d4fd6012c2e8b
Mr. G <gregor.hildebrand@gmail.com>
2016-09-06 08:00:32 +0100

    2016-09-06's java workout

---
+++ contributions/Mr. G/java/I/O/2016-09-06.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
