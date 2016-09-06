1f70390f067f681ce6a19ef9e245932bedcad2bd
chillichief <adrian.hofmann90@gmail.com>
2016-09-06 19:09:14 +0100

    2016-09-06's linux workout

---
+++ contributions/chillichief/linux/System Management/2016-09-06.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
+Cancel a system shutdown
