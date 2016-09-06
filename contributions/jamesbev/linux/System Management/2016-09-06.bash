5f594047a48e82de228c492967567111291b055b
jamesbev <jamesbev@gmail.com>
2016-09-07 00:56:39 +0100

    2016-09-06's linux workout

---
+++ contributions/jamesbev/linux/System Management/2016-09-06.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Detect your linux distribution
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Finding Open Files With `lsof`
