36a6dd306bfe8f54bbde9863cc23e372771a1694
jogli5er <roman.brunner94@gmail.com>
2016-09-06 06:28:02 +0100

    2016-09-06's java workout

---
+++ contributions/jogli5er/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
