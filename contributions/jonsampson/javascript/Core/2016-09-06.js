f8f9f3318ca098049df8de07a6b8afa8b44c5d42
jonsampson <jon.sampson@gmail.com>
2016-09-06 23:18:14 +0100

    2016-09-06's javascript workout

---
+++ contributions/jonsampson/javascript/Core/2016-09-06.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Counting the months from zero
+Filtering items out of an array
+Prevent background image flicker in **IE**
+Getting the *real* dimensions of an image
