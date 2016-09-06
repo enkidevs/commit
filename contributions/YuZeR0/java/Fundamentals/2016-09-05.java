06a48d1a59cdd451521b67a65a3154b5d6b58d1b
YuZeR0 <gunjan.kharel31@gmail.com>
2016-09-06 04:04:07 +0100

    2016-09-05's java workout

---
+++ contributions/YuZeR0/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use meaningful names
+The trade-off between short and descriptive names
+Using `Printstream.format` to format a string before printing
+Using bounded type parameters in generic methods
