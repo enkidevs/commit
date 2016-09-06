fd560a80ae49cb79df338013c209d5ed3e1788ca
LunarEclipse120 <noahajac@gmail.com>
2016-09-06 01:23:08 +0100

    2016-09-05's java workout

---
+++ contributions/LunarEclipse120/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Inheriting a constructor from a superclass
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
