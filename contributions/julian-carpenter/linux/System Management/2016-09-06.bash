b37c4495ae5cef2ec118bbf0ad7ec447404c321a
julian-carpenter <julianzimmermann@gmx.net>
2016-09-06 11:11:12 +0100

    2016-09-06's linux workout

---
+++ contributions/julian-carpenter/linux/System Management/2016-09-06.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
