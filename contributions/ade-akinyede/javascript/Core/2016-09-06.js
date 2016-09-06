1f8f44b0c938962d3f0355a49177bc54420851f9
ade-akinyede <t.akinyede@gmail.com>
2016-09-06 09:56:46 +0100

    2016-09-06's javascript workout

---
+++ contributions/ade-akinyede/javascript/Core/2016-09-06.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Types of numbers
+Use `splice()` to remove an item from an array
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
