8dcd1e0a0e5606caf127c9cf36628633215003ae
jk5605012 <jk5605012@gmail.com>
2016-09-06 14:48:28 +0100

    2016-09-05's java workout

---
+++ contributions/jk5605012/java/Error Handling/2016-09-05.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Using exceptions in Java
+Throwing an `exception`
