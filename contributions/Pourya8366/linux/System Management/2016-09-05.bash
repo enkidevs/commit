ec47e23ff4eee7e667858dcfd42c83bfa80f860b
Pourya8366 <pvalizadeh1374@gmail.com>
2016-09-06 18:55:18 +0100

    2016-09-05's linux workout

---
+++ contributions/Pourya8366/linux/System Management/2016-09-05.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
