e4f922d568903ac2e852f183e41911880e5e4285
AlanZhuTheAsian <alanyzhu@gmail.com>
2016-09-06 01:32:50 +0100

    2016-09-05's javascript workout

---
+++ contributions/AlanZhuTheAsian/javascript/Core/2016-09-05.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Data type comparison in `switch` statements
+Counting the months from zero
+Extra function arguments are undefined by default
+Round numbers to `N` decimals 
