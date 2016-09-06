ff303f7dd26911c5a83f26aa2be4eea8c4fdeda8
escorponox <escorponox@gmail.com>
2016-09-06 22:35:12 +0100

    2016-09-06's javascript workout

---
+++ contributions/escorponox/javascript/Browser APIs/2016-09-06.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+`top.location.href`
+Notifications API
+Vibration API
