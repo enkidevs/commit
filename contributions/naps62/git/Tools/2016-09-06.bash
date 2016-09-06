0d8d9f15cf1b4620bd01a5305da10c85cd1176bc
naps62 <miguel@subvisual.co>
2016-09-06 09:14:36 +0100

    2016-09-06's git workout

---
+++ contributions/naps62/git/Tools/2016-09-06.bash
@@ -0,0 +1,5 @@
+Interactively unstage changes
+Specify a range of commits using double dot syntax
+Sign your work (using GPG)
+Remember the results of previous hunk conflicts
+Debug using binary search
