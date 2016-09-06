994d8341122510327752c3addb950d047375aa0e
Sergobot <sergobot@protonmail.com>
2016-09-06 14:53:01 +0100

    2016-09-06's linux workout

---
+++ contributions/Sergobot/linux/System Management/2016-09-06.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
