68ac0ca0cd7bdffef5023a75874fec316649daee
Kezarious <kieran_linnie@hotmail.com>
2016-09-06 14:40:14 +0100

    2016-09-06's linux workout

---
+++ contributions/Kezarious/linux/Terminals & Shells/2016-09-06.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
