7edf5484054a206df2b368aff05755bdacc95914
AlbertoSH <alberto.sanz.herrero@gmail.com>
2016-09-06 20:31:58 +0100

    2016-09-06's java workout

---
+++ contributions/AlbertoSH/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
