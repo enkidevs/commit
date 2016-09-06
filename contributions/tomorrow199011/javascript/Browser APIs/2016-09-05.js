5f662fe21d498c26d1924354c375326dcaa4621c
tomorrow199011 <974683559@qq.com>
2016-09-06 08:53:34 +0100

    2016-09-05's javascript workout

---
+++ contributions/tomorrow199011/javascript/Browser APIs/2016-09-05.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+CSS for when JavaScript is enabled
+Allow an element to go fullscreen
+Warn user if **Back** button is pressed
+Window.sessionStorage
