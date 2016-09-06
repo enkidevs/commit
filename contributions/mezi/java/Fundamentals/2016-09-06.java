f3525514d092246bd3bf402cc0dee9129d607922
mezi <mezoistvan1@gmail.com>
2016-09-06 08:40:10 +0100

    2016-09-06's java workout

---
+++ contributions/mezi/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Supply `toString()` in all classes
