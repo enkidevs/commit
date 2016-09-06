de0d5eeabf5d1a3eb9fdb87b3917a59fb0c83942
Danacus <vanoverloopdaan@gmail.com>
2016-09-06 19:33:29 +0100

    2016-09-06's linux workout

---
+++ contributions/Danacus/linux/System Management/2016-09-06.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
