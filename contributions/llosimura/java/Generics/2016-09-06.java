ade4be09b8e8da63b0f11b062e73c9c682084633
llosimura <alfonsoglezluis@gmail.com>
2016-09-06 18:25:56 +0100

    2016-09-06's java workout

---
+++ contributions/llosimura/java/Generics/2016-09-06.java
@@ -0,0 +1,5 @@
+Wildcards vs Objects
+Using initializer blocks to minimize code repetition in constructors
+Upper bounded wildcards
+Using unbounded wildcards
+Use lazy initialization to defer creating an object until it's used
