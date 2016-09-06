1dd7f4ea47017f30888ba2ce05ea6d7e1924fce3
RiMillo <ricc.milani@gmail.com>
2016-09-06 19:24:32 +0100

    2016-09-06's linux workout

---
+++ contributions/RiMillo/linux/Terminals & Shells/2016-09-06.bash
@@ -0,0 +1,7 @@
+Find any Unix / Linux command
+Quick `bash` shortcuts 
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
