905ef58c2c2c5f5c3489b69f8eafb33f728e4a18
oanabogos <catalina.bogos@gmail.com>
2016-09-06 20:09:02 +0100

    2016-09-06's java workout

---
+++ contributions/oanabogos/java/I/O/2016-09-06.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
