c8e5c82776234b04e1b46ecd5c00d9e6cac2335c
karolb <karol.bojski@gmail.com>
2016-09-06 23:05:36 +0100

    2016-09-06's java workout

---
+++ contributions/karolb/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
