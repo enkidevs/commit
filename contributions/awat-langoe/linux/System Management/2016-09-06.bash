fc4bafe1148c0a88969b97b2768fed9c90e56ecc
awat-langoe <awat.langoe@gmail.com>
2016-09-06 09:23:54 +0100

    2016-09-06's linux workout

---
+++ contributions/awat-langoe/linux/System Management/2016-09-06.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
