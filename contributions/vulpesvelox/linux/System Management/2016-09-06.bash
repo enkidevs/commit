e68d1c72b25f6fd70ec6a50b758a89bd8fbe7610
vulpesvelox <nedim.lisica@gmail.com>
2016-09-06 09:42:16 +0100

    2016-09-06's linux workout

---
+++ contributions/vulpesvelox/linux/System Management/2016-09-06.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
