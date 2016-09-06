32391d5653a8025b2b6a52b13bcd08b279624565
nadavten <nadavten@gmail.com>
2016-09-06 10:38:51 +0100

    2016-09-06's javascript workout

---
+++ contributions/nadavten/javascript/Functions/2016-09-06.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Social sharing without widgets
+Check if a document is done loading
