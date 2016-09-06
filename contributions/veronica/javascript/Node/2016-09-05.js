43b3d7b59020c63a94e1e9d504d4f9e9119c9eb2
veronica <me@veronicacannon.com>
2016-09-06 05:28:34 +0100

    2016-09-05's javascript workout

---
+++ contributions/veronica/javascript/Node/2016-09-05.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Asynchronous File Write/Read in Node.js
+First-error callbacks in **Node**
+Use conventional property names for error handling in Node.js
+**Node** `os` module
