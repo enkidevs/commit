acd9627876b4c01c4873ee111e4e9d0fd19f7a85
alebaffa <alessandro.baffa@gmail.com>
2016-09-06 06:54:40 +0100

    2016-09-06's java workout

---
+++ contributions/alebaffa/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Measuring time
+Always specify type parameter
