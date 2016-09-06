34a920ef3a943474bef4e794c1cbfb4726f90b0c
euwbah <euwbah@gmail.com>
2016-09-06 07:19:23 +0100

    2016-09-05's linux workout

---
+++ contributions/euwbah/linux/System Management/2016-09-05.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
