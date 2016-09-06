e1bd8d0eaba9138f503fffc169aa68b6c8db00dc
dabizk <dabizk@yahoo.com>
2016-09-06 10:54:25 +0100

    2016-09-06's java workout

---
+++ contributions/dabizk/java/Data Types/2016-09-06.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Finding a substring in a string
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
+Retrieve the component type of an array
