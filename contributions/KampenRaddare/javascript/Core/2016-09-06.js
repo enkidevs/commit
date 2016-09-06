1e0a0b3f713f56b29a63c510ad708262749e198b
KampenRaddare <jamdycus@live.com>
2016-09-06 13:11:09 +0100

    2016-09-06's javascript workout

---
+++ contributions/KampenRaddare/javascript/Core/2016-09-06.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Using the double tilde `~~` 
+Hoisting applies only to variable declarations, not initializations
+Extra function arguments are undefined by default
+Detect online connection
