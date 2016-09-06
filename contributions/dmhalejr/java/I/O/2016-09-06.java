c50a883fe3b6b7ad785426f581997202443911b2
dmhalejr <dmhalejr@gmail.com>
2016-09-06 14:45:07 +0100

    2016-09-06's java workout

---
+++ contributions/dmhalejr/java/I/O/2016-09-06.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to create directories in Java
+How to list all files in a directory that match a filename extension in Java
+Using buffered streams
+Checking for the existence of files and directories
