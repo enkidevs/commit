d1833530cf0d095002fc621ee2c5216d04ef7bfa
rodneyxr <rodneyxr@gmail.com>
2016-09-06 06:14:54 +0100

    2016-09-05's java workout

---
+++ contributions/rodneyxr/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
