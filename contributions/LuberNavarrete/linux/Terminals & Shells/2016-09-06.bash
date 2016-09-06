c6576cf06167b3024230b0d0a36b573e45bfc2a8
LuberNavarrete <navarreteluber@gmail.com>
2016-09-06 11:24:41 +0100

    2016-09-06's linux workout

---
+++ contributions/LuberNavarrete/linux/Terminals & Shells/2016-09-06.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
