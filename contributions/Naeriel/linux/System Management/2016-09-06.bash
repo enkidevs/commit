0b759503c4c1c318ea36338dd8ad6e60bc4aa132
Naeriel <serranog.cristina@gmail.com>
2016-09-06 13:20:31 +0100

    2016-09-06's linux workout

---
+++ contributions/Naeriel/linux/System Management/2016-09-06.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
