6b883a757316e2462a0d03d96854a3c4f3ba115f
muneeb2405 <smati_muneeb@hotmail.com>
2016-09-06 03:31:32 +0100

    2016-09-05's java workout

---
+++ contributions/muneeb2405/java/Error Handling/2016-09-05.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Throwing an `exception`
+Checking a double for `NaN` value
