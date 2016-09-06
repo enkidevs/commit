2b4eacb6be18648a8e6b1e61ca48bda9eb289400
remaster <daniel.neuman@gmail.com>
2016-09-06 22:28:58 +0100

    2016-09-06's java workout

---
+++ contributions/remaster/java/I/O/2016-09-06.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Create system file paths using the `Path` class
+Reading and writing text files
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
