37c72149c7a0acabb4ce30e42a3c51abda5706cd
stitch <thomas.dickinson@gmx.com>
2016-09-06 19:02:51 +0100

    2016-09-06's git workout

---
+++ contributions/stitch/git/Tools/2016-09-06.bash
@@ -0,0 +1,5 @@
+Specify a range of commits using double dot syntax
+Create a new branch from a stash
+Sign your work (using GPG)
+Remember the results of previous hunk conflicts
+Debug using binary search
