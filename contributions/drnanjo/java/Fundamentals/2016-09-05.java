0a0638b5ecf27a024eabb0781f39ff82cd951e22
drnanjo <drnanjo@aol.com>
2016-09-06 01:50:03 +0100

    2016-09-05's java workout

---
+++ contributions/drnanjo/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Use strings in a `switch` statement
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
