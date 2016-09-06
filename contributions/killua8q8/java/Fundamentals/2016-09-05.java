43bce4dd4b934c66219621ac7b78c73cea53ad2b
killua8q8 <killua8q8@gmail.com>
2016-09-06 02:02:42 +0100

    2016-09-05's java workout

---
+++ contributions/killua8q8/java/Fundamentals/2016-09-05.java
@@ -0,0 +1,5 @@
+Ternary operator
+Using `Printstream.format` to format a string before printing
+Using bounded type parameters in generic methods
+Always specify type parameter
+Supply `toString()` in all classes
