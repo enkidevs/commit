6e44a5cb1f01758f8c62b0091c42329620955f5c
dogen <samuel.sh79@gmail.com>
2016-09-06 10:23:57 +0100

    2016-09-06's python workout

---
+++ contributions/dogen/python/Data Structures/2016-09-06.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+A thread-safe `Queue`
+Prioritize your `queue` 
+Implementing **weak** references
+Binary record packing with `bytearray`
