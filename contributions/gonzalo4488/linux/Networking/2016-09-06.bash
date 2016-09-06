ae36ae21a5991d18dbc70593175c47f4be8e74cd
gonzalo4488 <gonzalocaballero@me.com>
2016-09-06 11:49:32 +0100

    2016-09-06's linux workout

---
+++ contributions/gonzalo4488/linux/Networking/2016-09-06.bash
@@ -0,0 +1,5 @@
+Disable `IPv6`
+List your IPv6 configuration
+Getting the connection speed from the terminal
+Staying connected remotely via SSH without an interactive shell
+Short Introduction to `Wget` 
