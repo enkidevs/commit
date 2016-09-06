0d0707aec9ff128211fe0814709c22f15d2102de
atoddj <atoddj@gmail.com>
2016-09-06 02:45:33 +0100

    2016-09-05's javascript workout

---
+++ contributions/atoddj/javascript/Functions/2016-09-05.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+IIFE pattern
+Easily generate a random `HEX` color
+Social sharing without widgets
