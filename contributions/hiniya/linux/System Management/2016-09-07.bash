a2ae77eff509dac2c7f628daf3a1cf594d87284e
hiniya <hiniya@votbs.com>
2016-09-07 17:51:06 +0100

    2016-09-07's linux workout

---
+++ contributions/hiniya/linux/System Management/2016-09-07.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
