40b6cd8e3b6029e07c276fd0f44313ad588e0ad5
Kirill.S <saragiu.kirill@gmail.com>
2016-09-07 08:36:17 +0100

    2016-09-07's java workout

---
+++ contributions/Kirill.S/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Detect or prevent integer overflow
+Supply `toString()` in all classes
