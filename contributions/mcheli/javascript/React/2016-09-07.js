9058187951102c061350daad66ceb8208f768f1c
mcheli <mpcheli7@gmail.com>
2016-09-07 20:21:31 +0100

    2016-09-07's javascript workout

---
+++ contributions/mcheli/javascript/React/2016-09-07.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Self-closing tags in **JSX**
+Props in getInitialState Is an        
+Anti-Pattern
+Use **React** with other libraries
+Immutability helpers in **React**
