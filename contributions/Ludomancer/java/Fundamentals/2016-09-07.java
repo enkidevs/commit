bad18ea610769ac3ab69e41f05497ef9806431c4
Ludomancer <unique.ek@gmail.com>
2016-09-07 08:24:59 +0100

    2016-09-07's java workout

---
+++ contributions/Ludomancer/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Common mistake on switch statements
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
