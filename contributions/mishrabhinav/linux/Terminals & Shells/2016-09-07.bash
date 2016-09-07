a2844738fb7f475044e77a38c0d0a7563fb1f0cc
mishrabhinav <mishrabhinav96@gmail.com>
2016-09-07 10:23:40 +0100

    2016-09-07's linux workout

---
+++ contributions/mishrabhinav/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Aliasing ssh connections
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
