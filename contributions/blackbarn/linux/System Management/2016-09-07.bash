a9bfcdd0ba720193367cf57826f71802a732359b
blackbarn <blackbarn@gmail.com>
2016-09-07 17:43:04 +0100

    2016-09-07's linux workout

---
+++ contributions/blackbarn/linux/System Management/2016-09-07.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
