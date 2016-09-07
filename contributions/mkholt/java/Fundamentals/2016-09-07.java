6d623c7b228789645bf8753afedde0c1df699b99
mkholt <the.thawk@gmail.com>
2016-09-07 11:12:13 +0100

    2016-09-07's java workout

---
+++ contributions/mkholt/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
