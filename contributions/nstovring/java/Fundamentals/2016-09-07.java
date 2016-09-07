873ec22010da9a486e3f0827f71db2d6ccc25491
nstovring <nstovring@live.dk>
2016-09-07 15:29:28 +0100

    2016-09-07's java workout

---
+++ contributions/nstovring/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
