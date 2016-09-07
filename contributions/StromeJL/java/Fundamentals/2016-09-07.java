3fdfcd8011929d3f8ff18b5ab63309b22a5342d5
StromeJL <stromejl@gmail.com>
2016-09-07 21:40:16 +0100

    2016-09-07's java workout

---
+++ contributions/StromeJL/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
