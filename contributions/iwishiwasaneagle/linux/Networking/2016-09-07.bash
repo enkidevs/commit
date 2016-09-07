b053a0c27afdc2abcbebf57e438fac0c98c9cfe9
iwishiwasaneagle <jh.ewers@gmail.com>
2016-09-07 08:54:51 +0100

    2016-09-07's linux workout

---
+++ contributions/iwishiwasaneagle/linux/Networking/2016-09-07.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Find services running on your host
+Useful ssh client optimizations 
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
