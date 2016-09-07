a208c629ad354f37f62b743c2579a5e8bba65785
NeveHanter <nevehanter@gmail.com>
2016-09-07 07:21:27 +0100

    2016-09-07's java workout

---
+++ contributions/NeveHanter/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Increase `PermGen` space as to avoid `OutOfMemory` errors
