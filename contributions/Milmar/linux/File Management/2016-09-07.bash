3c3d2c071924d19eeaa95a50b8f32355a7a68921
Milmar <milmarwoot@gmail.com>
2016-09-07 20:20:18 +0100

    2016-09-07's linux workout

---
+++ contributions/Milmar/linux/File Management/2016-09-07.bash
@@ -0,0 +1,6 @@
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
