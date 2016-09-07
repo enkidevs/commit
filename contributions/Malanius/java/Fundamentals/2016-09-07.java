8ba6daa250432af067eae3faf4189f525ca350b4
Malanius <malanius@seznam.cz>
2016-09-07 07:21:06 +0100

    2016-09-07's java workout

---
+++ contributions/Malanius/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Catch multiple exceptions in a single `catch` block
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+Supply `toString()` in all classes
