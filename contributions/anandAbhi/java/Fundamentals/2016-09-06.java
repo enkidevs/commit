8783b5b69f9b080f0c5709290430db6a7f0e7d32
anandAbhi <oct94abhi@gmail.com>
2016-09-07 18:24:30 +0100

    2016-09-06's java workout

---
+++ contributions/anandAbhi/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
