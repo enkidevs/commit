6af9d30113e53a76d337bd9874bb999d8e56cc01
AavinashhK <atechkid2011@gmail.com>
2016-09-07 20:32:00 +0100

    2016-09-06's linux workout

---
+++ contributions/AavinashhK/linux/Terminals & Shells/2016-09-06.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Useful aliasing in bash
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
