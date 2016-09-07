246c9a96ce40fcc4a2cbf16ebd9e7e2f1f458bdc
imdyske <michal.pavuk@protonmail.com>
2016-09-07 07:49:09 +0100

    2016-09-07's java workout

---
+++ contributions/imdyske/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
