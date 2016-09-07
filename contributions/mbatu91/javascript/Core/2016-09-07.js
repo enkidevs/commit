4139cc31f4b7c32bd0745d7f1b08c722afff783e
mbatu91 <andrea.tamburini.91@gmail.com>
2016-09-07 12:25:28 +0100

    2016-09-07's javascript workout

---
+++ contributions/mbatu91/javascript/Core/2016-09-07.js
@@ -0,0 +1,5 @@
+Treating a boolean as number
+Setting the length of an array 
+`String.replace`
+Hoisting applies only to variable declarations, not initializations
+Apply `map` function to array items
