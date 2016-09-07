8ebeb74e3d36229f4034891431077e8a00b699a3
rDacted <enki@eddiecornejo.com>
2016-09-07 07:45:50 +0100

    2016-09-06's linux workout

---
+++ contributions/rDacted/linux/File Management/2016-09-06.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
