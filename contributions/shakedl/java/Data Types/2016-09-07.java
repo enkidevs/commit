e69fffd0b628a63d563808ce41b171936ac2e1cd
shakedl <shaked.lokits@gmail.com>
2016-09-07 13:42:33 +0100

    2016-09-07's java workout

---
+++ contributions/shakedl/java/Data Types/2016-09-07.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Converting numbers to strings
+`StringBuffer` vs `StringBuilder`
+Do not attempt comparisons with NaN
