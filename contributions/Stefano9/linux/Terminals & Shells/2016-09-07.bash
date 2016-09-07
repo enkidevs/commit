be10c9ca6696e0f5920c5721cc099ab5be321722
Stefano9 <toby.tallis@gmail.com>
2016-09-07 19:37:33 +0100

    2016-09-07's linux workout

---
+++ contributions/Stefano9/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Find any Unix / Linux command
+Keep useful commands in your shell history with tags
