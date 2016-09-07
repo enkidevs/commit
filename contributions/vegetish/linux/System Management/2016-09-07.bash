f218c947a73bd044125e8daf5718a499362ee755
vegetish <martin.ardal@gmail.com>
2016-09-07 18:01:47 +0100

    2016-09-07's linux workout

---
+++ contributions/vegetish/linux/System Management/2016-09-07.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Cancel a system shutdown
