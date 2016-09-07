67c0df6dbf9af1cab72e4db96f1dac8850182481
Randomguy01 <ianhwhite2001@gmail.com>
2016-09-07 12:17:05 +0100

    2016-09-07's java workout

---
+++ contributions/Randomguy01/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Measuring time
+Do not return *references* to private *mutable* class members
