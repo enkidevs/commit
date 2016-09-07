8039ec36a8e386fd212cd3f43109865ae213fc11
jjmiranda <jjmiranda@magia.digital>
2016-09-07 15:38:24 +0100

    2016-09-07's linux workout

---
+++ contributions/jjmiranda/linux/File Management/2016-09-07.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
+Delete commands aliases
+Easy way of sharing files
