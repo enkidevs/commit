e4d93013934011587bd4504ae78364f9fefb63dd
rombevillard <rombevillard@gmail.com>
2016-09-07 11:41:21 +0100

    2016-09-07's java workout

---
+++ contributions/rombevillard/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Metadata: setting a file's owner
+Measuring time
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
