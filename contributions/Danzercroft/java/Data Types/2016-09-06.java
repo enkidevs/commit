7875132f910124a3a121c83aa19e8e4c77f6ebcc
Danzercroft <digerfight@gmail.com>
2016-09-07 19:56:17 +0100

    2016-09-06's java workout

---
+++ contributions/Danzercroft/java/Data Types/2016-09-06.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Converting numbers to strings
+Converting a string to upper or lower case
+`StringBuffer` vs `StringBuilder`
