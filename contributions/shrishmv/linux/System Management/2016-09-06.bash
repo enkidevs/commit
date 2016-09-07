0a70b34354722edfcb936a6029ff5e2ead86e932
shrishmv <shrish.mv@gmail.com>
2016-09-07 07:03:31 +0100

    2016-09-06's linux workout

---
+++ contributions/shrishmv/linux/System Management/2016-09-06.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
