fed53713ee27633eca666f2e50710072485a8a43
jpabeem <jpa.beemsterboer@gmail.com>
2016-09-07 13:53:50 +0100

    2016-09-07's java workout

---
+++ contributions/jpabeem/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Updating interfaces by using `default` methods
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
