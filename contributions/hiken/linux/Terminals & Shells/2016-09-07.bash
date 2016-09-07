050828f0489c8f25ca401847f947db48b27283f8
hiken <paulngumii@gmail.com>
2016-09-07 15:01:58 +0100

    2016-09-07's linux workout

---
+++ contributions/hiken/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Clear the terminal instantly
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
