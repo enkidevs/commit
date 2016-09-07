c916964909502e93d32cb46801042ccdb11021a4
Wolf <ya.172009@yandex.ru>
2016-09-07 20:42:55 +0100

    2016-09-07's java workout

---
+++ contributions/Wolf/java/Data Types/2016-09-07.java
@@ -0,0 +1,5 @@
+Use `Arrays.asList()` to initialise lists
+Converting numbers to strings
+Converting a string to upper or lower case
+Retrieve the component type of an array
+Numeric Conversion - Widening
