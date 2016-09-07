af71ccbeea846f10536813cd1d885631170f671d
vasart <vasart@gmail.com>
2016-09-07 08:00:15 +0100

    2016-09-07's git workout

---
+++ contributions/vasart/git/Essentials/2016-09-07.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Check the status of your files
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
