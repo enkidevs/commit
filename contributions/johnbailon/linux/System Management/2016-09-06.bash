69fd5d6a371a72e6e7afff2f1f7b414a0de11c7a
johnbailon <john@sci.ph>
2016-09-07 03:08:49 +0100

    2016-09-06's linux workout

---
+++ contributions/johnbailon/linux/System Management/2016-09-06.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
