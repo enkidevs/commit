377fd824d6dba593edd8aa967044b8bd45880e9b
verdecken <subobscure@yandex.com>
2016-09-07 15:39:03 +0100

    2016-09-06's linux workout

---
+++ contributions/verdecken/linux/Terminals & Shells/2016-09-06.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
