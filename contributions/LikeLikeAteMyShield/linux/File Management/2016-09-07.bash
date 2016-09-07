a59149578ea8894d0377b589fbd68f83e7ddce1e
LikeLikeAteMyShield <jonnycameron17@gmail.com>
2016-09-07 11:37:13 +0100

    2016-09-07's linux workout

---
+++ contributions/LikeLikeAteMyShield/linux/File Management/2016-09-07.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Find the unknown process preventing deleting of files
