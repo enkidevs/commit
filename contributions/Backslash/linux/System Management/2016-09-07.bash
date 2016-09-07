887ffe3d47a8ded77ad3e4545b263ba4e4e607c2
Backslash <lukas.stana@gmail.com>
2016-09-07 14:08:13 +0100

    2016-09-07's linux workout

---
+++ contributions/Backslash/linux/System Management/2016-09-07.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
