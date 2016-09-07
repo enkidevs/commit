186f104a288dbfc6821e20d489dcf2da31f9343a
pontneddfechan <dk@danielkelly.co.uk>
2016-09-07 22:36:33 +0100

    2016-09-07's java workout

---
+++ contributions/pontneddfechan/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Floating garbage and how to deal with it
+Difference between JRE and JDK?
+Distinction between `public` and `private` methods
+Metadata: setting a file's owner
