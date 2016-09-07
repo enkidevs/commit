f01c779e0b990f45464d6827c1dd06cb8793ce15
sekayasin <sekayasin@gmail.com>
2016-09-07 20:52:15 +0100

    2016-09-07's java workout

---
+++ contributions/sekayasin/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use meaningful names
+The trade-off between short and descriptive names
+Inheriting a constructor from a superclass
+Using `Printstream.format` to format a string before printing
