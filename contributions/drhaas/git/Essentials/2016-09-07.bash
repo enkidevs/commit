ad822c9187dd9914cb963276f1fa387d802a9b09
drhaas <diogohaas@gmail.com>
2016-09-07 15:08:10 +0100

    2016-09-07's git workout

---
+++ contributions/drhaas/git/Essentials/2016-09-07.bash
@@ -0,0 +1,5 @@
+Using tags for version control
+You can use git offline!
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
