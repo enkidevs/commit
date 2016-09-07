4a5e641bc9b6f08ddafae6c86b6e44b5aed66a0f
lberrocal <luisberrocal@outlook.com>
2016-09-07 03:28:34 +0100

    2016-09-06's javascript workout

---
+++ contributions/lberrocal/javascript/Node/2016-09-06.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
+`exports` vs. `module.exports` in **Node**
+`try-catch` only for **sync** code
