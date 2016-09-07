d51ec508662e4ad89e00f3a6509fa4384f785eea
MinusKube <minuskube@gmail.com>
2016-09-07 09:04:11 +0100

    2016-09-07's java workout

---
+++ contributions/MinusKube/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
