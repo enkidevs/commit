b68f07bbf8e3cbe4b296b4131fb1cc411e39db30
JMarques <silvermarkes@gmail.com>
2016-09-07 08:19:20 +0100

    2016-09-07's java workout

---
+++ contributions/JMarques/java/I/O/2016-09-07.java
@@ -0,0 +1,5 @@
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
