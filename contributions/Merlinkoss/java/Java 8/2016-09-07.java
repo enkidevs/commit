517c94dd749644d12b67e1e2796103f9c67b3231
Merlinkoss <kondratyuk.anton.ig@gmail.com>
2016-09-07 22:53:34 +0100

    2016-09-07's java workout

---
+++ contributions/Merlinkoss/java/Java 8/2016-09-07.java
@@ -0,0 +1,5 @@
+Using `Arrays.parallelSort` 
+How to check for static dependencies of applications and libraries from the command line.
+Use `Optional(s)` type in order to avoid `NullPointerException(s)`
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
