311c084237b7e6ca639ac7497a6a56484397f484
shwalabaka <shwalabaka@gmail.com>
2016-09-07 09:45:11 +0100

    2016-09-07's java workout

---
+++ contributions/shwalabaka/java/Data Types/2016-09-07.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Double brace initialization
+`StringBuffer` vs `StringBuilder`
+Retrieve the component type of an array
