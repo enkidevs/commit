3a3b2a9fbbb44139c3415f30dbfa11679af33d85
reel <dmitryshevliakov@gmail.com>
2016-09-07 11:50:50 +0100

    2016-09-07's java workout

---
+++ contributions/reel/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
