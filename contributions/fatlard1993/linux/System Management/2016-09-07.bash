ce644d05f0841ca482e9b3d5e999baaa06117dd5
fatlard1993 <chase.whitfield@me.com>
2016-09-07 22:39:29 +0100

    2016-09-07's linux workout

---
+++ contributions/fatlard1993/linux/System Management/2016-09-07.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Cancel a system shutdown
