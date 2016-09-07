fea99e5509fa7ddcf43837dcf4056a633f22f1dd
domcull3n <dom@laundrapp.com>
2016-09-07 08:40:20 +0100

    2016-09-07's linux workout

---
+++ contributions/domcull3n/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,7 @@
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
