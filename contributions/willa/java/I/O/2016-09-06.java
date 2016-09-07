7cda72b70443803c38566d3e4c63db2a4c75f51b
willa <willatauro@gmail.com>
2016-09-07 01:22:06 +0100

    2016-09-06's java workout

---
+++ contributions/willa/java/I/O/2016-09-06.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Deleting files and directories
