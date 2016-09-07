09e27d051e31c30b944f2d5d16b1df717d243981
jjaracz <j.jaracz@poczta.pl>
2016-09-07 11:52:09 +0100

    2016-09-07's git workout

---
+++ contributions/jjaracz/git/Essentials/2016-09-07.bash
@@ -0,0 +1,5 @@
+Make your log output pretty
+Limiting log output by time
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
