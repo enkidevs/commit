f9d58443979d416ff992a7a385fb05eb5c89c52a
viplav <viplavtuladhar@gmail.com>
2016-09-07 15:59:44 +0100

    2016-09-06's java workout

---
+++ contributions/viplav/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
