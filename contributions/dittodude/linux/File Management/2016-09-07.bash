e2e35621c5bb9d03999924261d8197e9e66ad597
dittodude <gkdragondude@gmail.com>
2016-09-07 20:42:06 +0100

    2016-09-07's linux workout

---
+++ contributions/dittodude/linux/File Management/2016-09-07.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Converting between Unix and Windows text files
+Working with `.gz` files
+Find the unknown process preventing deleting of files
