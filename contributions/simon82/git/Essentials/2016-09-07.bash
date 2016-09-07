d5651d3cb046e3993e2063e4e9a2461b2aef5805
simon82 <simon@koop-bremen.de>
2016-09-07 18:05:30 +0100

    2016-09-07's git workout

---
+++ contributions/simon82/git/Essentials/2016-09-07.bash
@@ -0,0 +1,5 @@
+Using tags for version control
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Recover lost code
