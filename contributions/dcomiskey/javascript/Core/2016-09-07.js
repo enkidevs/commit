41c551640b7aff8749f1a8dcd56e786334df1f59
dcomiskey <damienacomiskey@gmail.com>
2016-09-07 14:34:08 +0100

    2016-09-07's javascript workout

---
+++ contributions/dcomiskey/javascript/Core/2016-09-07.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Types of numbers
+Counting the months from zero
+The difference between null, undefined and NaN
+Extra function arguments are undefined by default
