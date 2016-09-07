9376edcfd5bd094f278f64e26530488b8ad9e23e
nshyam97 <nishus97@yahoo.co.uk>
2016-09-07 19:53:21 +0100

    2016-09-07's java workout

---
+++ contributions/nshyam97/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Supply `toString()` in all classes
+Using an interface as a parameter
