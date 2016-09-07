27172157ac10f9170a89d0e0ea43591842331686
mukeshkannaya <mukeshkannaya@gmail.com>
2016-09-07 19:45:14 +0100

    2016-09-06's javascript workout

---
+++ contributions/mukeshkannaya/javascript/Core/2016-09-06.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+Inaccuracy of binary floating-point format
