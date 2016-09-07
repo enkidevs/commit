a5ad41c6b9dd4aae9ec7fac678a6e6a9eade5090
mfrazie2 <michael.s.frazier@gmail.com>
2016-09-07 05:20:58 +0100

    2016-09-06's linux workout

---
+++ contributions/mfrazie2/linux/Terminals & Shells/2016-09-06.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Terminal incognito mode
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
