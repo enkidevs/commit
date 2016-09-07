224159b7f638900f08ccc26e78363642dc03ec4c
sidmouthwebdesign <joseph@sidmouthwebdesign.co.uk>
2016-09-07 08:57:56 +0100

    2016-09-07's git workout

---
+++ contributions/sidmouthwebdesign/git/Essentials/2016-09-07.bash
@@ -0,0 +1,5 @@
+Using tags for version control
+Check the status of your files
+How to write a git commit message
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
