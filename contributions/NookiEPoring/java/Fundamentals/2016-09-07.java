dd359c2f26a27ddcc83ce58eec09b7a5420d2c02
NookiEPoring <nookie.poring@gmail.com>
2016-09-07 16:53:46 +0100

    2016-09-07's java workout

---
+++ contributions/NookiEPoring/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
