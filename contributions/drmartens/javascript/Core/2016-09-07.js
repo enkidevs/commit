2864c99275b2ae7dedcb0a5273344fea5bc6fc88
drmartens <martd371@newschool.edu>
2016-09-07 20:01:16 +0100

    2016-09-07's javascript workout

---
+++ contributions/drmartens/javascript/Core/2016-09-07.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Using the double tilde `~~` 
+Most efficient way to build `HTML` strings
+Prevent background image flicker in **IE**
+Detect **DO NOT TRACK** status
