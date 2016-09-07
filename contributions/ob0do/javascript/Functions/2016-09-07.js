40e05969628f2e68f422cee11a9a119dfaa50ebb
ob0do <fr3drik.l@gmail.com>
2016-09-07 07:54:16 +0100

    2016-09-07's javascript workout

---
+++ contributions/ob0do/javascript/Functions/2016-09-07.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function declarations vs function expressions
+IIFE pattern
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
