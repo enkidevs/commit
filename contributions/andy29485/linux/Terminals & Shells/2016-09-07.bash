83ce6419148c2fd19318d4c0bbfab55b5c77971d
andy29485 <andriy0126@gmail.com>
2016-09-07 12:38:44 +0100

    2016-09-07's linux workout

---
+++ contributions/andy29485/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Clear the terminal instantly
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
