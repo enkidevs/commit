d3f19dc95835f3e49ea0d22d70658ebe0ea7b1e9
games647 <games@tsprivat.de>
2016-09-07 07:58:17 +0100

    2016-09-07's git workout

---
+++ contributions/games647/git/Tools/2016-09-07.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Specify a range of commits using double dot syntax
+Interactive staging
+Useful stashing options
+Remember the results of previous hunk conflicts
