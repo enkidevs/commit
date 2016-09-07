88578e947af6f165e521c69d1a6c5e283b1280e0
TKOTC <steven.j.mckeown@gmail.com>
2016-09-07 14:11:48 +0100

    2016-09-07's java workout

---
+++ contributions/TKOTC/java/I/O/2016-09-07.java
@@ -0,0 +1,5 @@
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Retrieving file store attributes
+Deleting files and directories
+The `Console` class
