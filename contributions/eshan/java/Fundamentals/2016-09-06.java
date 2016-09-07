83f29fba40608e1e9bb20052cfd0190a328524f2
eshan <eshan.1105@gmail.com>
2016-09-07 09:13:35 +0100

    2016-09-06's java workout

---
+++ contributions/eshan/java/Fundamentals/2016-09-06.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Inheriting a constructor from a superclass
+Constant notation
+Always specify type parameter
