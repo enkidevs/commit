4c8f7d3e3644f87e8a307192f5b0b3c4fd139c22
badbart <tom.zille@gmail.com>
2016-09-07 03:30:50 +0100

    2016-09-07's linux workout

---
+++ contributions/badbart/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Terminal incognito mode
+`else` statements using the `||` operator 
