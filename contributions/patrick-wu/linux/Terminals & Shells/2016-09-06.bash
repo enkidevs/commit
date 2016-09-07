225c486ad52576bf7eb06f95e091e669c6502200
patrick-wu <patrick@5crowd.com>
2016-09-07 06:03:04 +0100

    2016-09-06's linux workout

---
+++ contributions/patrick-wu/linux/Terminals & Shells/2016-09-06.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Terminal based browser
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
