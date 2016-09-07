e7dcac68e2271f082bec41f615f7e255492a6e4c
ct2034 <post@henkelchristian.de>
2016-09-07 06:26:54 +0100

    2016-09-07's java workout

---
+++ contributions/ct2034/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
