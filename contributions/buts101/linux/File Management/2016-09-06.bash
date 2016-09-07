bd821a7d3768382653e571a3b51f27476d743dac
buts101 <ashish.batajoo@gmail.com>
2016-09-07 17:30:54 +0100

    2016-09-06's linux workout

---
+++ contributions/buts101/linux/File Management/2016-09-06.bash
@@ -0,0 +1,5 @@
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
+Delete commands aliases
+Easy way of sharing files
