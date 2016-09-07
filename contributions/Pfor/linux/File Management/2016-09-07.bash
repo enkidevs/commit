80b71f4bbdea606bafaf23e4f3063b983a961e22
Pfor <iah@protonmail.com>
2016-09-07 19:52:23 +0100

    2016-09-07's linux workout

---
+++ contributions/Pfor/linux/File Management/2016-09-07.bash
@@ -0,0 +1,5 @@
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Working with `.gz` files
+Find the unknown process preventing deleting of files
+Delete commands aliases
