8fc70da0bd39856c7e318105426e903f090df8bc
pazer <pazer@gmx.com>
2016-09-07 20:58:54 +0100

    2016-09-07's linux workout

---
+++ contributions/pazer/linux/System Management/2016-09-07.bash
@@ -0,0 +1,7 @@
+Revision control of configuration files with git
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Check the HDD with `badblocks`
+Monitor System Memory using `vmstat`
