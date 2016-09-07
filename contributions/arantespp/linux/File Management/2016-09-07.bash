20929649be8257c389cbfd0df06d7214bdcf591b
arantespp <arantespp@gmail.com>
2016-09-07 15:51:35 +0100

    2016-09-07's linux workout

---
+++ contributions/arantespp/linux/File Management/2016-09-07.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
