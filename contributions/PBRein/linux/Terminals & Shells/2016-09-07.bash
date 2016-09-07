a06853991779b18f2350baaf83a1d07a5baf3f67
PBRein <cript_lord@abv.bg>
2016-09-07 23:06:37 +0100

    2016-09-07's linux workout

---
+++ contributions/PBRein/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
