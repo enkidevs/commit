f78c5fc4ab1767253d4a0b0627f2b9efe6785bc9
beshur <a@shu.bz>
2016-09-07 09:46:59 +0100

    2016-09-07's javascript workout

---
+++ contributions/beshur/javascript/Functions/2016-09-07.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
