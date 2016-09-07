aa554c94efb28f6952621cf1f6a6712b5457c2b8
Perevedko <alex45510@ya.ru>
2016-09-07 18:06:09 +0100

    2016-09-07's linux workout

---
+++ contributions/Perevedko/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,5 @@
+Random password generator
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
