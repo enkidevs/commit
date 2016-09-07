ba16f50d4367820e441dfd296201aeed822f8c60
kenrao <rao.kenneth@gmail.com>
2016-09-07 15:08:13 +0100

    2016-09-06's linux workout

---
+++ contributions/kenrao/linux/System Management/2016-09-06.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Commands to shutdown or restart the system
