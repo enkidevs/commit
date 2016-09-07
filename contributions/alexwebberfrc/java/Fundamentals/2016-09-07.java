d0650abf9c10cde00679957ba1e1e06307e6c26b
alexwebberfrc <alexwebberfrc@gmail.com>
2016-09-07 23:17:20 +0100

    2016-09-07's java workout

---
+++ contributions/alexwebberfrc/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
