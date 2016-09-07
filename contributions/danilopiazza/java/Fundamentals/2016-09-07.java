d8c48b484e7a7174c3173822c8f4e084d6104a79
danilopiazza <danilopiazza@gmail.com>
2016-09-07 08:33:59 +0100

    2016-09-07's java workout

---
+++ contributions/danilopiazza/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
