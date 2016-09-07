c33a7dfc5c16331ca14d63a51d393d1560dc4d6d
csnxs <sean@csnxs.uk>
2016-09-07 22:09:04 +0100

    2016-09-07's java workout

---
+++ contributions/csnxs/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
