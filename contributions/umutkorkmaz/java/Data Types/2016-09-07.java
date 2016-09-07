5421492c34f90533d9529739e549d3415255b823
umutkorkmaz <info@umutkorkmaz.net>
2016-09-07 17:34:52 +0100

    2016-09-07's java workout

---
+++ contributions/umutkorkmaz/java/Data Types/2016-09-07.java
@@ -0,0 +1,5 @@
+Two ways to use an `Iterator`
+Use `Arrays.asList()` to initialise lists
+Converting a string to upper or lower case
+Retrieve the component type of an array
+String concatenation in Java
