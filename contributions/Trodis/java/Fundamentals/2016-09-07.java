ba7038bc50f632a39b60293d88fc99a50b714bd6
Trodis <sreungbrmzra@gmail.com>
2016-09-07 17:13:56 +0100

    2016-09-07's java workout

---
+++ contributions/Trodis/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
