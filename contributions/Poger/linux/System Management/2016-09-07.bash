ca0c85dda0e01b2b62dd5f557c54e4d384a5d747
Poger <gkonupek@gmail.com>
2016-09-07 11:34:51 +0100

    2016-09-07's linux workout

---
+++ contributions/Poger/linux/System Management/2016-09-07.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Detect your linux distribution
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Cancel a system shutdown
