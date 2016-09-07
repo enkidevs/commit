298d2b7c84a22af9aba266626f0f9b5f07bff9a6
BrunoAlves83 <brunoalves.alves@gmail.com>
2016-09-08 00:10:30 +0100

    2016-09-07's java workout

---
+++ contributions/BrunoAlves83/java/Error Handling/2016-09-07.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Checking a double for `NaN` value
