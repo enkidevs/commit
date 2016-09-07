4fa926e1ea7124e1917dc8a6fd8e946efb7f7a9f
Stribianese91 <matteovaralta91@gmail.com>
2016-09-07 17:11:06 +0100

    2016-09-07's linux workout

---
+++ contributions/Stribianese91/linux/System Management/2016-09-07.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Executing scripts on system boot using `systemd`
+Using `dd` To Create Disk Images
