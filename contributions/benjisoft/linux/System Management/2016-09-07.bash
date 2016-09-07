e635b1836e431390cf1653398c3952c10068c8e9
benjisoft <benji@benjicraft.info>
2016-09-07 22:05:20 +0100

    2016-09-07's linux workout

---
+++ contributions/benjisoft/linux/System Management/2016-09-07.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
