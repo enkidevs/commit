fc6528ed08cbe83b97a4b8a1cbf6bc41b853989e
Alanbato <alanvelasco.a@gmail.com>
2016-09-07 14:17:48 +0100

    2016-09-07's linux workout

---
+++ contributions/Alanbato/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
