f3fe0f904c4074d41bd912f6ea7402aa67543eb1
nporto <portokallidis@gmail.com>
2016-09-07 07:47:34 +0100

    2016-09-07's linux workout

---
+++ contributions/nporto/linux/System Management/2016-09-07.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
