54b80d9f674e9a6452aafe94063a69b44fbc4dec
sigma-z <steff.zeidler@googlemail.com>
2016-09-07 07:47:22 +0100

    2016-09-07's linux workout

---
+++ contributions/sigma-z/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
