04a54c613e155e232c7ea1b459369b0788078daa
nitzpo <pomerantz@mail.tau.ac.il>
2016-09-07 19:53:50 +0100

    2016-09-07's javascript workout

---
+++ contributions/nitzpo/javascript/Core/2016-09-07.js
@@ -0,0 +1,5 @@
+Keywords in hash keys
+Using the double tilde `~~` 
+Hoisting applies only to variable declarations, not initializations
+Prevent background image flicker in **IE**
+Detect **DO NOT TRACK** status
