6d717117b2e5502790a151d4cbcbaeac90f28743
Eldergeek <david@geekguytech.com>
2016-09-07 16:19:56 +0100

    2016-09-07's linux workout

---
+++ contributions/Eldergeek/linux/System Management/2016-09-07.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Preventing a user from logging into the system
+Monitor System Memory using `vmstat`
