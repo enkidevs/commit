42fcd8dc4dfcc17309b7a544f431eb99c32ab695
Trvsco <andrea.sassi@gmail.com>
2016-09-07 20:20:40 +0100

    2016-09-07's linux workout

---
+++ contributions/Trvsco/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Useful aliasing in bash
+Conditional command execution
+
+(`&&` operator)
