b10551120cfd20e53f2418a07e409b908ae8cf1e
vituxre <vita.reizupa@gmail.com>
2016-09-07 16:37:10 +0100

    2016-09-07's linux workout

---
+++ contributions/vituxre/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Clear the terminal instantly
+`else` statements using the `||` operator 
+The Basics of Environment Variables
