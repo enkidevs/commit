bd421c9d11667992dd6dd2e360f4f77f1ad9d5ab
raffing <raffaelequatraro@gmail.com>
2016-09-08 14:39:52 +0100

    2016-09-08's linux workout

---
+++ contributions/raffing/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
