b0ea5df7b301ad0cc2edd83b3566163591a41499
olley102 <olley102@gmail.com>
2016-09-08 22:01:51 +0100

    2016-09-08's linux workout

---
+++ contributions/olley102/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
