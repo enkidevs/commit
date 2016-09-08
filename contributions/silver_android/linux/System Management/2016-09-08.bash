9ab8456b9dbc5ca5d4c5fae67154dee4d660857e
silver_android <pererarushil1940@gmail.com>
2016-09-08 18:47:24 +0100

    2016-09-08's linux workout

---
+++ contributions/silver_android/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
