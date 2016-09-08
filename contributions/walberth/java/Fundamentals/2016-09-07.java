55a2cdd211c7e2f4ed01cd549d968cece4b59bff
walberth <w.felipe.gutierrez@gmail.com>
2016-09-08 06:37:50 +0100

    2016-09-07's java workout

---
+++ contributions/walberth/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Using the `Console`
+Updating interfaces by using `default` methods
