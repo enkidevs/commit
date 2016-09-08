a8ffdb7dbc59d368f1487449daaacef6d2e0a578
Yvanor <maxime.yusei@orange.fr>
2016-09-08 06:22:57 +0100

    2016-09-08's java workout

---
+++ contributions/Yvanor/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Use meaningful names
+Use strings in a `switch` statement
+Implementing an `interface`
