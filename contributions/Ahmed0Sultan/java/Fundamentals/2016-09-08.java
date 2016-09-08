7bc0e4727f7e5f62f86975c361a58b7454393709
Ahmed0Sultan <ahmed.khaled4941@gmail.com>
2016-09-09 00:12:38 +0100

    2016-09-08's java workout

---
+++ contributions/Ahmed0Sultan/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Connecting to FTP using Java
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Supply `toString()` in all classes
