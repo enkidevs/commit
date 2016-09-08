23e3b805df6be5a6c5ef9153094c2037a1eb82e3
Evilfury <expanders187@gmail.com>
2016-09-08 23:32:24 +0100

    2016-09-08's linux workout

---
+++ contributions/Evilfury/linux/Terminals & Shells/2016-09-08.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear the terminal instantly
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
