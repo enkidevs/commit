8cce2cde2d12effff00bd4cce94d434619d04bd7
veritimus <veritimus@o2.pl>
2016-09-08 14:17:53 +0100

    2016-09-08's javascript workout

---
+++ contributions/veritimus/javascript/Functions/2016-09-08.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Method chaining
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
