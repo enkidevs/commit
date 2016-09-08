cd382015d491fffa2aa26317d9be90bb3ca48961
alch3m1st <alch3m1st.gr@gmail.com>
2016-09-08 17:54:40 +0100

    2016-09-08's java workout

---
+++ contributions/alch3m1st/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Using an interface as a parameter
