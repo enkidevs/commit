f6cea03b94a4eba9b07d617a2e6aecac492e7a72
yaro <rojaabc@yahoo.pl>
2016-09-08 19:50:36 +0100

    2016-09-08's java workout

---
+++ contributions/yaro/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Cropping images
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+How range operations work
