2b57f0aa37070d8361769d82c5d0a54e9ee6947d
dabizk <dabizk@yahoo.com>
2016-09-08 17:16:05 +0100

    2016-09-08's java workout

---
+++ contributions/dabizk/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Measuring time
