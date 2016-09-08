f936a36cc495fdbc404ce1b72dfb4d1efdc4e582
mhoogkamer <michael.hoogkamer@gmail.com>
2016-09-08 08:30:24 +0100

    2016-09-08's linux workout

---
+++ contributions/mhoogkamer/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
