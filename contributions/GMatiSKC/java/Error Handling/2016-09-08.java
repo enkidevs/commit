3d301a22d86b3912bcc9ba098bb9088a42ca61dc
GMatiSKC <mat.guzewski@gmail.com>
2016-09-08 14:57:26 +0100

    2016-09-08's java workout

---
+++ contributions/GMatiSKC/java/Error Handling/2016-09-08.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Prevent `exceptions` while logging Data
+Throwing an `exception`
+Checking a double for `NaN` value
