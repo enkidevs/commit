530981985cc55c21de7c456222d4f4b0c9be4f97
salvivikash5 <salvivikash5@gmail.com>
2016-09-08 08:17:06 +0100

    2016-09-07's java workout

---
+++ contributions/salvivikash5/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
