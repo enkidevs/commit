adf1705c08334713b7c0dfe56de5c354a5976d67
JrobT <jack@trute.co.uk>
2016-09-08 12:19:24 +0100

    2016-09-08's java workout

---
+++ contributions/JrobT/java/Classes & Objects/2016-09-08.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Compare classes and not class names
+Fields with Java **Reflection API**
