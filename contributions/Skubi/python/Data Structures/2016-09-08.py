11b78d3b97d5e05c7f8c4399d86f89fddfaa0c35
Skubi <skubibox@gmail.com>
2016-09-08 21:40:32 +0100

    2016-09-08's python workout

---
+++ contributions/Skubi/python/Data Structures/2016-09-08.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`Dictionary` - standard mapping type
+`Dictionary` view objects
+The fundamental `tuples` 
+Enhance your `tuple`s
