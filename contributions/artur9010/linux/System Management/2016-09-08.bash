038da2a2852d2129b81783a8d97e616482d43dec
artur9010 <arturmotyka99@gmail.com>
2016-09-08 11:10:24 +0100

    2016-09-08's linux workout

---
+++ contributions/artur9010/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
