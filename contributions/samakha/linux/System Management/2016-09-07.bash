771a39fdd7906a60201cc72f0a18103d17d0d83f
samakha <samakha.chugh@gmail.com>
2016-09-08 05:13:00 +0100

    2016-09-07's linux workout

---
+++ contributions/samakha/linux/System Management/2016-09-07.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Cancel a system shutdown
