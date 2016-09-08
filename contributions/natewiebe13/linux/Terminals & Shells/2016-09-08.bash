a043b344f423e3467aff36e8b61daca801f01d6e
natewiebe13 <nate@northern.co>
2016-09-08 13:47:59 +0100

    2016-09-08's linux workout

---
+++ contributions/natewiebe13/linux/Terminals & Shells/2016-09-08.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Aliasing ssh connections
+Find any Unix / Linux command
+Terminal incognito mode
