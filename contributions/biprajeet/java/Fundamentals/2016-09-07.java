8bc1bbdfe89fd1d5953cb892d315c8192f76424e
biprajeet <biprajeet.pal@gmail.com>
2016-09-08 19:16:29 +0100

    2016-09-07's java workout

---
+++ contributions/biprajeet/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Measuring time
+There is no such thing as *pass-by-reference* in Java
