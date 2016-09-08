e2db881052a4ffdb2e6096148fe1784848b9534f
Limero <limero1337@gmail.com>
2016-09-08 22:34:52 +0100

    2016-09-08's linux workout

---
+++ contributions/Limero/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
