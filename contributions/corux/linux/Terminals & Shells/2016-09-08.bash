b29f97a6fa008036c5263d163acf7408c09bd21c
corux <tobias@wallura.org>
2016-09-08 06:43:42 +0100

    2016-09-08's linux workout

---
+++ contributions/corux/linux/Terminals & Shells/2016-09-08.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Random password generator
+Adding directories to your `$PATH`
