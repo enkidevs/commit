0f68fb9b0707a9c757a0f63ae475327b108bdd11
dead-coder <sarfrazbaig.ece2018@gmail.com>
2016-09-08 07:27:16 +0100

    2016-09-07's linux workout

---
+++ contributions/dead-coder/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
