ad5946184192a44a89d2b725e1b26af9ddadf24e
Layo <thegamerult@googlemail.com>
2016-09-08 06:02:19 +0100

    2016-09-08's java workout

---
+++ contributions/Layo/java/I/O/2016-09-08.java
@@ -0,0 +1,5 @@
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Reading file attributes
+List contents of a directory with `newDirectoryStream`
