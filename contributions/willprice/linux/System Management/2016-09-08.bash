31a678af56d288e349eb9acf6fe7ef9d0e78d274
willprice <will.price94@gmail.com>
2016-09-08 09:56:18 +0100

    2016-09-08's linux workout

---
+++ contributions/willprice/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
