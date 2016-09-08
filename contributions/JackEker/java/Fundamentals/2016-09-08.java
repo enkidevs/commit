b00efc8923558db235a4598ac2551575a8407508
JackEker <jackeker@gmail.com>
2016-09-08 06:49:42 +0100

    2016-09-08's java workout

---
+++ contributions/JackEker/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Measuring time
+Detect or prevent integer overflow
+Using an interface as a parameter
