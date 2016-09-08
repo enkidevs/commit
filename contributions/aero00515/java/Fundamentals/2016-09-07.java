5a57ba6a83ae6cc914b0cd04b8a8417fd86e28a6
aero00515 <aero00515@gmail.com>
2016-09-08 15:04:21 +0100

    2016-09-07's java workout

---
+++ contributions/aero00515/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
