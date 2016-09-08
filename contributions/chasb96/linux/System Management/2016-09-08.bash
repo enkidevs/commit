3c0935138b3c0acb68375892f8e862b2370603bc
chasb96 <chasb96@gmail.com>
2016-09-08 15:14:57 +0100

    2016-09-08's linux workout

---
+++ contributions/chasb96/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Finding Open Files With `lsof`
