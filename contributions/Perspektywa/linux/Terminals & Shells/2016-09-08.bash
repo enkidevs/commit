3ac76c9af4532bd35d1d958a5bb2e53985cf45c8
Perspektywa <perspektywa.tk@gmail.com>
2016-09-08 13:23:50 +0100

    2016-09-08's linux workout

---
+++ contributions/Perspektywa/linux/Terminals & Shells/2016-09-08.bash
@@ -0,0 +1,5 @@
+Terminal based browser
+Random password generator
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
