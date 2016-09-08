dc01a9bd523e81406bfdf02e210da7616899f852
Marius <marius.mez@gmail.com>
2016-09-08 14:03:56 +0100

    2016-09-08's linux workout

---
+++ contributions/Marius/linux/Terminals & Shells/2016-09-08.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
