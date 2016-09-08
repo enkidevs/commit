27ab35799df46cd34fb4d8b15585dbf46afc5738
justintv90 <justin.tv90@gmail.com>
2016-09-08 16:27:01 +0100

    2016-09-07's linux workout

---
+++ contributions/justintv90/linux/System Management/2016-09-07.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
