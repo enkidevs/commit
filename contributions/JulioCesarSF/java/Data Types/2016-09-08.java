ea9477e30ef0408315472bdece39764c1f79be06
JulioCesarSF <julio.com@uol.com.br>
2016-09-09 00:40:46 +0100

    2016-09-08's java workout

---
+++ contributions/JulioCesarSF/java/Data Types/2016-09-08.java
@@ -0,0 +1,5 @@
+Multidimensional array declaration
+Finding a substring in a string
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
+Retrieve the component type of an array
