0b4aab873b5bd5d5bcbb392d1b35950d8406727a
GoryMoon <gurreja@gmail.com>
2016-09-08 22:50:46 +0100

    2016-09-08's javascript workout

---
+++ contributions/GoryMoon/javascript/React/2016-09-08.js
@@ -0,0 +1,6 @@
+`null` value for *Controlled Components* in **React**
+Props in getInitialState Is an        
+Anti-Pattern
+Use **React** with other libraries
+`ref` callback attribute
+Default values for props
