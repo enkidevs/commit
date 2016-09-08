f73ab50c16f23bb936e3a220871d23c665aa944c
Mansweet <mnmansour91@gmail.com>
2016-09-08 19:32:37 +0100

    2016-09-08's linux workout

---
+++ contributions/Mansweet/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Test disk speed with `dd`
+Check the HDD with `badblocks`
+Commands to shutdown or restart the system
