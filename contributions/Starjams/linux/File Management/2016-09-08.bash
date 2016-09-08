7b382cd872285199b656e29e75d997b77b2b92c3
Starjams <robertbmerriman@gmail.com>
2016-09-08 09:38:23 +0100

    2016-09-08's linux workout

---
+++ contributions/Starjams/linux/File Management/2016-09-08.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Deleting non-empty directories
