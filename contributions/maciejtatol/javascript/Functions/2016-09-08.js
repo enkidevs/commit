1d9c312a1bbeb38b59531563a3f78c36f5ef2c04
maciejtatol <maciejtatol@gmail.com>
2016-09-08 19:13:26 +0100

    2016-09-08's javascript workout

---
+++ contributions/maciejtatol/javascript/Functions/2016-09-08.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Method chaining
+Easily generate a random `HEX` color
