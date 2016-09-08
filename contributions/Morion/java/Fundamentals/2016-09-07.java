544caa6a00255e87db66e9e7c73390c8fc57bbb4
Morion <morion.self@gmail.com>
2016-09-08 05:00:40 +0100

    2016-09-07's java workout

---
+++ contributions/Morion/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Locks in `static synchronized` methods
+`Autoboxing` and `Unboxing`
