4c0485e4be27ec51c583466b9f4c947edf4bc099
lunamystry <lm.mbuli+enki@gmail.com>
2016-09-08 05:31:27 +0100

    2016-09-08's linux workout

---
+++ contributions/lunamystry/linux/File Management/2016-09-08.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
