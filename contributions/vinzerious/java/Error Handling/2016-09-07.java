f95343a14fd584106b05144966c52f957a3dec38
vinzerious <mavinmail@gmail.com>
2016-09-08 13:49:58 +0100

    2016-09-07's java workout

---
+++ contributions/vinzerious/java/Error Handling/2016-09-07.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Creating new Exceptions
+Careful Numeric Data Conversions
