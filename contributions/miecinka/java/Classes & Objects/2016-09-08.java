712e278d9acd67fc15fe6ad905afe76a73a0110c
miecinka <mietinhovanish@gmail.com>
2016-09-08 17:51:03 +0100

    2016-09-08's java workout

---
+++ contributions/miecinka/java/Classes & Objects/2016-09-08.java
@@ -0,0 +1,5 @@
+Template methods in abstract classes
+Java object references
+Compare classes and not class names
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
