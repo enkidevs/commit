f376841e1d8f7859d8b34b931a803db6e23387f7
PredatorFeesh <zatbuilder@gmail.com>
2016-09-08 03:42:59 +0100

    2016-09-07's linux workout

---
+++ contributions/PredatorFeesh/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Terminal incognito mode
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
