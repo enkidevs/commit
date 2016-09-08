0ea90b16be78887be260ae434fd882bff0ecf8fa
Faithful Mind <lpyy15@gmail.com>
2016-09-08 16:53:20 +0100

    2016-09-07's java workout

---
+++ contributions/Faithful Mind/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Using `Printstream.format` to format a string before printing
+Always specify type parameter
