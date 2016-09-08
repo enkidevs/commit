f7c9ff47fdec98f0014a7e435707f2e6f61e88c6
pontneddfechan <dk@danielkelly.co.uk>
2016-09-08 20:01:25 +0100

    2016-09-08's java workout

---
+++ contributions/pontneddfechan/java/I/O/2016-09-08.java
@@ -0,0 +1,5 @@
+Reading and writing text files
+Checking for the existence of files and directories
+Reading file attributes
+Retrieving file store attributes
+Listing a file system's root directories
