1361a4fd0068755934f031a09074abc4be1f6c0b
LikeLikeAteMyShield <jonnycameron17@gmail.com>
2016-09-08 09:19:30 +0100

    2016-09-08's linux workout

---
+++ contributions/LikeLikeAteMyShield/linux/File Management/2016-09-08.bash
@@ -0,0 +1,6 @@
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
