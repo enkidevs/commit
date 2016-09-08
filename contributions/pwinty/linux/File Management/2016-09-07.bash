4b016ed754d671f42d93d028f9a38b8b0bfa6cb4
pwinty <peterwinteriii@gmail.com>
2016-09-08 04:32:51 +0100

    2016-09-07's linux workout

---
+++ contributions/pwinty/linux/File Management/2016-09-07.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Easy way of sharing files
