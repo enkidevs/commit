b3bd5e6dc2995df34574c59c82360fd3293ef890
LuberNavarrete <navarreteluber@gmail.com>
2016-09-08 11:17:53 +0100

    2016-09-08's linux workout

---
+++ contributions/LuberNavarrete/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
