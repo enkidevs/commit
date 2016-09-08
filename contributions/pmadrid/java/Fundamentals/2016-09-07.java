cbee949f02a995c73d86c09cb78895b8a5411b9e
pmadrid <pmadrid@gmail.com>
2016-09-08 03:45:34 +0100

    2016-09-07's java workout

---
+++ contributions/pmadrid/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
