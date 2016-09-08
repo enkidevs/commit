ae069aba6b4ead22920177a70fe58e1d22564774
nabil <nblbmr@gmail.com>
2016-09-08 12:06:54 +0100

    2016-09-08's linux workout

---
+++ contributions/nabil/linux/System Management/2016-09-08.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
