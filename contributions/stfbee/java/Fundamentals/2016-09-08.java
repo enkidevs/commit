9f9140976006db9ea6c90075dd87ebba30be4350
stfbee <pinkey.950@gmail.com>
2016-09-08 08:58:38 +0100

    2016-09-08's java workout

---
+++ contributions/stfbee/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
