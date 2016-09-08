e507ca7061d214fb02cb1739a9e9f339840ad1a1
Corey88 <lucascavalcante707@gmail.com>
2016-09-08 23:42:32 +0100

    2016-09-08's java workout

---
+++ contributions/Corey88/java/I/O/2016-09-08.java
@@ -0,0 +1,4 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+How to list all files in a directory that match a filename extension in Java
+List contents of a directory with `newDirectoryStream`
