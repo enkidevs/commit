845d240fe5e5470cee3d35551901d0f5cdd5e2c9
christinalcole <christinalcole@gmail.com>
2016-09-08 03:27:43 +0100

    2016-09-07's linux workout

---
+++ contributions/christinalcole/linux/System Management/2016-09-07.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
