319b7fa25ce01aaa9c597b3d52269b9ce6f83731
clausewtz45 <clausewitz45@gmail.com>
2016-09-08 07:56:23 +0100

    2016-09-08's linux workout

---
+++ contributions/clausewtz45/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Common log files
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
