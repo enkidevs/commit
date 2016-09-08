7b2f5568cbf5ee25da888f495a34588f4cdbccb5
GuillaumeSeg <guillaume.segado@gmail.com>
2016-09-08 18:05:32 +0100

    2016-09-08's java workout

---
+++ contributions/GuillaumeSeg/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
