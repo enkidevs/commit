bad35d581b0ed14a98cfa8d419b60109756edcf2
GNi33 <theill182@gmail.com>
2016-09-08 23:53:13 +0100

    2016-09-08's java workout

---
+++ contributions/GNi33/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Metadata: creating a user-defined file attribute
+Using an interface as a parameter
