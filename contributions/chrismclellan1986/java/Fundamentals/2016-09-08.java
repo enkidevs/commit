92dc2bf2f7bbb95c3432902b4cd45f17c24b4564
chrismclellan1986 <cmclellan@live.co.uk>
2016-09-08 11:27:11 +0100

    2016-09-08's java workout

---
+++ contributions/chrismclellan1986/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Using `synchronized` statements
+Constant notation
