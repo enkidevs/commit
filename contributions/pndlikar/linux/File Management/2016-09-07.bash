9eeac8f15374108e656a7c0e2a5372128df3bedd
pndlikar <pndlikar@gmail.com>
2016-09-08 07:22:06 +0100

    2016-09-07's linux workout

---
+++ contributions/pndlikar/linux/File Management/2016-09-07.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
