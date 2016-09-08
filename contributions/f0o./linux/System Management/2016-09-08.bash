2892b46cb690cb245878f9f46de015f9f35da1e9
f0o <jshshwusksjg@devilcode.org>
2016-09-08 06:41:21 +0100

    2016-09-08's linux workout

---
+++ contributions/f0o./linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Basic service management with `systemd`
