f795fa72482367a10e275f0b8bced93be3d1ad53
harinsa <harin.sanghirun@gmail.com>
2016-09-08 06:44:01 +0100

    2016-09-07's linux workout

---
+++ contributions/harinsa/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
