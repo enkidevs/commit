ee8794793c7c3f864bbae9b609188c54e6ccfe7d
Pindagus <agusnobel@gmail.com>
2016-09-08 10:19:55 +0100

    2016-09-08's linux workout

---
+++ contributions/Pindagus/linux/Terminals & Shells/2016-09-08.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Quick `bash` shortcuts 
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
