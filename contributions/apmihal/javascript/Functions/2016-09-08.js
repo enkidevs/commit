d98f9d01af9dc0954a0b3e82d122e418ed73cc5c
apmihal <apmihal@gmail.com>
2016-09-08 21:02:27 +0100

    2016-09-08's javascript workout

---
+++ contributions/apmihal/javascript/Functions/2016-09-08.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` with `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+`.bind()` 
+Method chaining
