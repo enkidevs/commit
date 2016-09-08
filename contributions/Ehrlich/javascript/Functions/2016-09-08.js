97aab4ca699272d0698e2a7debb8bd12220f7bad
Ehrlich <anissa.d.alexandercs@gmail.com>
2016-09-08 15:32:25 +0100

    2016-09-08's javascript workout

---
+++ contributions/Ehrlich/javascript/Functions/2016-09-08.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Get query/url variables
+Easily generate a random `HEX` color
+Social sharing without widgets
