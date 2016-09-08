cae1ec237e7fcc028f8b5bff5be964838fed3ead
theswolf <chris.geymo@gmail.com>
2016-09-08 15:48:43 +0100

    2016-09-08's java workout

---
+++ contributions/theswolf/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
