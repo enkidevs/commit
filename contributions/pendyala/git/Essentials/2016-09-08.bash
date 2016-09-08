79cd4cd000cbe95ef502907e3a70b9449bbdb15f
pendyala <sreedhar@stratus5.com>
2016-09-08 07:01:31 +0100

    2016-09-08's git workout

---
+++ contributions/pendyala/git/Essentials/2016-09-08.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+How to unmodify a modified file
+Use `short` status to make output more compact
+How to write a git commit message
+`master` and `origin` aren't special
