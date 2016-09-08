16fb8b9c1f34bde728146376ac311f8f9a817a68
scott-walker <scott-walker@inbox.com>
2016-09-09 00:20:24 +0100

    2016-09-08's java workout

---
+++ contributions/scott-walker/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Cropping images
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
