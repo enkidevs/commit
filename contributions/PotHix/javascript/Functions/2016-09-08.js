1d82bfb3ed3f499b2ed21b9916801b19d1448002
PotHix <pothix@pothix.com>
2016-09-08 13:15:17 +0100

    2016-09-08's javascript workout

---
+++ contributions/PotHix/javascript/Functions/2016-09-08.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Method chaining
+Easily generate a random `HEX` color
+Social sharing without widgets
