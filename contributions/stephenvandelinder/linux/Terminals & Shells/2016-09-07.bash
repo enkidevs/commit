4f139ac562c2ddd4aa15bb4617a5bdb3015f6b62
stephenvandelinder <stephen.vandelinder@gmail.com>
2016-09-08 01:23:47 +0100

    2016-09-07's linux workout

---
+++ contributions/stephenvandelinder/linux/Terminals & Shells/2016-09-07.bash
@@ -0,0 +1,7 @@
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
