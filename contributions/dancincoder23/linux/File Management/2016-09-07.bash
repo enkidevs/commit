5cd05e0a38e81f81270c856f484e230678b46ca2
dancincoder23 <dancingal212@gmail.com>
2016-09-08 06:24:45 +0100

    2016-09-07's linux workout

---
+++ contributions/dancincoder23/linux/File Management/2016-09-07.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Working with `.gz` files
+Find the unknown process preventing deleting of files
