278eecd48b6ee5c06bfe09f526c0da84cf60e946
esensar <es.ensar@gmail.com>
2016-09-08 07:55:35 +0100

    2016-09-08's java workout

---
+++ contributions/esensar/java/Error Handling/2016-09-08.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Converting stack trace to a string
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
