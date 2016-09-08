f4bc129bb858cf0082f5265b7a12da4e14e4c858
RoxSyf <sancho.roxane@gmail.com>
2016-09-08 20:10:44 +0100

    2016-09-08's java workout

---
+++ contributions/RoxSyf/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
