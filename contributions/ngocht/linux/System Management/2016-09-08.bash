3ee5f4d64f4c87e36ec1d016bafb6250b93b0e83
ngocht <nicolas@gocht.at>
2016-09-08 07:13:34 +0100

    2016-09-08's linux workout

---
+++ contributions/ngocht/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
