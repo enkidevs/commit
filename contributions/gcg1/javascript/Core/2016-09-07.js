3f0389d5dd376e0e8614b83678eff34df503345e
gcg1 <george@seevantage.com>
2016-09-08 09:14:27 +0100

    2016-09-07's javascript workout

---
+++ contributions/gcg1/javascript/Core/2016-09-07.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Use `splice()` to remove an item from an array
+Hoisting applies only to variable declarations, not initializations
+The difference between null, undefined and NaN
+`console.*` in JavaScript
