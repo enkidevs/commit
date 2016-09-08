1bbf51c6fa63c7859c3a2134873407b2b1e08bf6
pankee <physicplay@gmail.com>
2016-09-08 06:36:28 +0100

    2016-09-07's java workout

---
+++ contributions/pankee/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Supply `toString()` in all classes
+Using an interface as a parameter
