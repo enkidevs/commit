f268cd0530caa1352390b0842452b7cef83547cb
eduSan <flegias87@gmail.com>
2016-09-08 12:10:21 +0100

    2016-09-08's java workout

---
+++ contributions/eduSan/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
