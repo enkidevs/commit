1b92e9eb863dff963e3f82e93afa691b7f150fd7
road2015 <road_2015@hotmail.com>
2016-09-08 22:49:56 +0100

    2016-09-08's javascript workout

---
+++ contributions/road2015/javascript/Core/2016-09-08.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Keywords in hash keys
+Setting the length of an array 
+Data type comparison in `switch` statements
+Extra function arguments are undefined by default
