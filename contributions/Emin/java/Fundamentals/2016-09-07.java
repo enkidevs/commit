c8c3a4b255efe344319e6388766e70f53b97f355
Emin <m@memin.tk>
2016-09-08 02:25:12 +0100

    2016-09-07's java workout

---
+++ contributions/Emin/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Cropping images
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
