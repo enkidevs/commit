297079de0b55af130e376e404e415d3f9bda5998
ekoj <as.esu.eko@gmail.com>
2016-09-08 11:14:22 +0100

    2016-09-08's linux workout

---
+++ contributions/ekoj/linux/System Management/2016-09-08.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
