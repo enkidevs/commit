a9fbd07c94738c366b550b8d5b8bd440b78d3787
mcheli <mpcheli7@gmail.com>
2016-09-08 17:16:19 +0100

    2016-09-08's javascript workout

---
+++ contributions/mcheli/javascript/Express/2016-09-08.js
@@ -0,0 +1,5 @@
+`response` object additional properties in **Express**
+Easy way to deliver **HTML** pages with **Express**
+`cookie-session` in **Express**
+Log with `morgan` in **Express**
+Error handler in **Express**
