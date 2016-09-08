6dd2ab5a8c0deeb8d49b26f2e6ee9e85c17f7e34
esoxjem <arunsasidharan121@gmail.com>
2016-09-08 10:58:04 +0100

    2016-09-07's java workout

---
+++ contributions/esoxjem/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Always specify type parameter
+Do not return *references* to private *mutable* class members
