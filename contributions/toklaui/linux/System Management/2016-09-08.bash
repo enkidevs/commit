7c475e0f590f95d1e3c2686b5a3721e185e3a267
toklaui <toklaui@live.de>
2016-09-08 07:35:36 +0100

    2016-09-08's linux workout

---
+++ contributions/toklaui/linux/System Management/2016-09-08.bash
@@ -0,0 +1,7 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
