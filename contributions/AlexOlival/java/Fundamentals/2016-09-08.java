074bf2143e255785f06063faa36e3f14e93b2e19
AlexOlival <lex.olival@gmail.com>
2016-09-08 18:44:09 +0100

    2016-09-08's java workout

---
+++ contributions/AlexOlival/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Overloading Methods in Java
+Uses of the `final` keyword
+Catch multiple exceptions in a single `catch` block
+Using `Printstream.format` to format a string before printing
