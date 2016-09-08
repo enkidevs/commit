be2877d0a4c2d7a9242e21fdca876bd56dce9e27
Dardy87 <dardus87@gmail.com>
2016-09-08 22:59:37 +0100

    2016-09-08's linux workout

---
+++ contributions/Dardy87/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
