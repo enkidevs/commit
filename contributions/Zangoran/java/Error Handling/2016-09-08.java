aae1013cc9f7ff07d76111ffa84932415c95b2b5
Zangoran <zach@landquist.net>
2016-09-08 21:51:02 +0100

    2016-09-08's java workout

---
+++ contributions/Zangoran/java/Error Handling/2016-09-08.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
