21de7ac7c6af4d1e5cde8598340bc317b381fb73
guiguidu31300 <guiguidu31300@gmail.com>
2016-09-08 07:37:17 +0100

    2016-09-08's java workout

---
+++ contributions/guiguidu31300/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
