96203902905df4f5fb95a9de1011e5eb3e4e9469
josephdviviano <joseph@viviano.ca>
2016-09-08 14:35:37 +0100

    2016-09-08's linux workout

---
+++ contributions/josephdviviano/linux/Terminals & Shells/2016-09-08.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Terminal incognito mode
+Keep useful commands in your shell history with tags
