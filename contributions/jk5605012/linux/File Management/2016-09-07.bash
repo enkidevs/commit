9c606634b45a523e611563be6b8ea7ff9cda644b
jk5605012 <jk5605012@gmail.com>
2016-09-08 10:57:10 +0100

    2016-09-07's linux workout

---
+++ contributions/jk5605012/linux/File Management/2016-09-07.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Delete commands aliases
