a89f11d0aff955b1496ff533a8ad29203469e5e5
mustafaismail22 <mustafa.ismail22@gmail.com>
2016-09-08 22:33:29 +0100

    2016-09-08's linux workout

---
+++ contributions/mustafaismail22/linux/Package Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+List installed packages
+Installing a `.deb` package from the terminal
+Downgrade a package via `apt-get`
+Keeping repos updated with `cron`
