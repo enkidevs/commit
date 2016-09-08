8427488f3b8fee633316da0df8bf8a04972863f6
yeshanqi <ysq1989@gmail.com>
2016-09-08 02:01:59 +0100

    2016-09-07's javascript workout

---
+++ contributions/yeshanqi/javascript/Browser APIs/2016-09-07.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+CSS for when JavaScript is enabled
+High Resolution Time API
+Navigation Timing API
+FileReader.readAsArrayBuffer()
