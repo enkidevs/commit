4168f2c1878aa191c56bd7210d0e78271cb4e15d
theMadness <daniele@l33t.it>
2016-09-08 17:23:56 +0100

    2016-09-08's javascript workout

---
+++ contributions/theMadness/javascript/Functions/2016-09-08.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Functions can be declared after use
+Method chaining
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
