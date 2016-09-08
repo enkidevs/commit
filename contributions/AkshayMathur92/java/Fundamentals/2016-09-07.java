a51084e2df86ff06c39c9a90277b6add03dd2a5b
AkshayMathur92 <avi.mathur.engg@gmail.com>
2016-09-08 21:00:38 +0100

    2016-09-07's java workout

---
+++ contributions/AkshayMathur92/java/Fundamentals/2016-09-07.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Supply `toString()` in all classes
