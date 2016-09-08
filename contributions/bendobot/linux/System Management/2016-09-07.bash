c93bd62bda5646276ce6110d177fde03d784629c
bendobot <whatsbending@gmail.com>
2016-09-08 04:42:34 +0100

    2016-09-07's linux workout

---
+++ contributions/bendobot/linux/System Management/2016-09-07.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
