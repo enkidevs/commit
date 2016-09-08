efa061c13144e1ab1903a93fdd6e7109b59d6d9c
thomcuddihy <thom@hometreelab.com>
2016-09-08 19:54:59 +0100

    2016-09-08's linux workout

---
+++ contributions/thomcuddihy/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
