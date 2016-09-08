97ba0f7cc1b5180e3aeb34a11a290df9f9186b40
huppys <huppys55@gmail.com>
2016-09-08 19:08:39 +0100

    2016-09-08's linux workout

---
+++ contributions/huppys/linux/Terminals & Shells/2016-09-08.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Terminal based browser
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
