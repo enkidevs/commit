33cca8f57412e6461f0d1483192edcba5b81b8bb
GevaSk8 <manuelf.mola@gmail.com>
2016-09-08 18:49:37 +0100

    2016-09-08's java workout

---
+++ contributions/GevaSk8/java/Annotations/2016-09-08.java
@@ -0,0 +1,5 @@
+Using the `@Deprecated` annotation
+Suppressing warnings using `@SuppressWarnings`
+Including annotations in Javadoc
+Restricting an annotation's application by using `@Target`
+Declaring `@Repeatable` annotation types
