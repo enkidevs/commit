2ca16a8608dd536c0f6eb8909aab00bfd81762e0
somsak <somsaks@gmail.com>
2016-09-08 07:34:48 +0100

    2016-09-07's linux workout

---
+++ contributions/somsak/linux/Data Manipulation/2016-09-07.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+How to analyse a server's log files
+Getting rid of duplicate lines
+Using `lynx` to convert HTML to text
+Image manipulation  using `convert`
