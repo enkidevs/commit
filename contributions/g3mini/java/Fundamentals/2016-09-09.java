93f2fa27798d98056dbf23496c1c2f1ab4d782b0
g3mini <mcjanz@gmail.com>
2016-09-09 07:27:44 +0100

    2016-09-09's java workout

---
+++ contributions/g3mini/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Overloading Methods in Java
+Distinction between `public` and `private` methods
+Constant notation
+Limit Accessibility of `Fields`
