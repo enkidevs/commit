6034d14e88f429fe304b8ff59d32923160e12dba
iceblurr <thehuy.phan92@gmail.com>
2016-09-09 07:06:00 +0100

    2016-09-09's linux workout

---
+++ contributions/iceblurr/linux/File Management/2016-09-09.bash
@@ -0,0 +1,5 @@
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Easy way of sharing files
