e9856ca14deece116519f78c05d2dd99538ce37a
asaBee <hlovers.forever@gmail.com>
2016-09-09 22:15:30 +0100

    2016-09-09's java workout

---
+++ contributions/asaBee/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
