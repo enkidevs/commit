90695ceb23b5294823ee2a44b159e2c71ac2b8ae
joseagon <joseagon@cisco.com>
2016-09-09 20:47:15 +0100

    2016-09-09's linux workout

---
+++ contributions/joseagon/linux/System Management/2016-09-09.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Finding Open Files With `lsof`
