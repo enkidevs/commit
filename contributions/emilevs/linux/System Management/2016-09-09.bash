0c55ded5a1caf2f42b23340e468da4be710d04a2
emilevs <emailemile@xs4all.nl>
2016-09-09 07:59:50 +0100

    2016-09-09's linux workout

---
+++ contributions/emilevs/linux/System Management/2016-09-09.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
