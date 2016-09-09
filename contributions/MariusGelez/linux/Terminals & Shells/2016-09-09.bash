48eb42abec2dcc5477e6d13ef6710ae9eb991d76
MariusGelez <marius.gelez@gmail.com>
2016-09-09 05:59:23 +0100

    2016-09-09's linux workout

---
+++ contributions/MariusGelez/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
