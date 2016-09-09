ee39ad5d0c22382b3824df1d07ba3002c19047ac
vfi <davion.groovy@gmail.com>
2016-09-10 00:53:01 +0100

    2016-09-09's linux workout

---
+++ contributions/vfi/linux/System Management/2016-09-09.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
