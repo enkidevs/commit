ba921a4f6c481f1c7c3198518af7763a181fb851
eamerial <joao.nascimento.viegas@gmail.com>
2016-09-09 13:02:02 +0100

    2016-09-09's linux workout

---
+++ contributions/eamerial/linux/System Management/2016-09-09.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+System uptime
