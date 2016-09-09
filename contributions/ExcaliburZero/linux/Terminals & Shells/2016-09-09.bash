2ddcd23901f9b13772ef9466ddddeb0d92e4d5d9
ExcaliburZero <cwellsny@nycap.rr.com>
2016-09-09 13:24:09 +0100

    2016-09-09's linux workout

---
+++ contributions/ExcaliburZero/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Keep useful commands in your shell history with tags
