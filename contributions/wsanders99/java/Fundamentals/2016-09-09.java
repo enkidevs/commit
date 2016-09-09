bcac09ddabeb4da203becddd22faca21d8847f14
wsanders99 <william.sanders.99@live.com>
2016-09-09 21:19:09 +0100

    2016-09-09's java workout

---
+++ contributions/wsanders99/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Use meaningful names
+The trade-off between short and descriptive names
+Altering format string output by changing a format specifier's `argument_index`
+Do not perform bitwise and arithmetic operations on the same data
