97e9924fb36e33e2cfde4cc148dab521d1983874
Adyel <adyel@live.com>
2016-09-09 23:57:20 +0100

    2016-09-09's linux workout

---
+++ contributions/Adyel/linux/System Management/2016-09-09.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
