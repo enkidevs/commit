93065118e52d1e161c7ea19512316e0a9dbf9092
fusu <solaris1995@gmail.com>
2016-09-09 07:44:36 +0100

    2016-09-09's javascript workout

---
+++ contributions/fusu/javascript/Functions/2016-09-09.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Anonymous functions
+Function call method
+Check if a document is done loading
