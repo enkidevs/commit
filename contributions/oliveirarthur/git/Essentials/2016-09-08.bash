e1a4138a7469a3645d4487dfb23e490dd2e19f61
oliveirarthur <arthuroliveira799@gmail.com>
2016-09-09 04:22:58 +0100

    2016-09-08's git workout

---
+++ contributions/oliveirarthur/git/Essentials/2016-09-08.bash
@@ -0,0 +1,5 @@
+Adding a remote repository
+Using tags for version control
+Search by commit message keyword
+Use `short` status to make output more compact
+Remote repositories: fetching and pushing
