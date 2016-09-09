a469ab53510f401c123a710061e8c08c44e1a8e1
Animuslupus <elias-wolf@t-online.de>
2016-09-09 06:02:24 +0100

    2016-09-09's java workout

---
+++ contributions/Animuslupus/java/I/O/2016-09-09.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to list all files in a directory that match a filename extension in Java
+Reading file attributes
+Listing a file system's root directories
+The `Console` class
