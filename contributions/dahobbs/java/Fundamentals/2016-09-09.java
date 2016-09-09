fc45ffb5f8dcfb78efeb9a1f669a140e83d50794
dahobbs <dah3@hw.ac.uk>
2016-09-09 12:25:41 +0100

    2016-09-09's java workout

---
+++ contributions/dahobbs/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
