89186233c98f01fbd9da96a9a3b1eb8e928789a8
Diogo95 <dmfpinheiro@gmail.com>
2016-09-09 01:40:30 +0100

    2016-09-08's linux workout

---
+++ contributions/Diogo95/linux/Terminals & Shells/2016-09-08.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
