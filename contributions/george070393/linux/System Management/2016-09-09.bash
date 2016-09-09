a462f29ee312e7f8d578f6ee641167ad51e08ae8
george070393 <george070393@gmail.com>
2016-09-09 20:36:22 +0100

    2016-09-09's linux workout

---
+++ contributions/george070393/linux/System Management/2016-09-09.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
