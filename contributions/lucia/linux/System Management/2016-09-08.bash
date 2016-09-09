a7383eef2f95084d12f540d1f4873f3fd5a8d9e2
lucia <mikoto.yu51@gmail.com>
2016-09-09 10:55:04 +0100

    2016-09-08's linux workout

---
+++ contributions/lucia/linux/System Management/2016-09-08.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Monitor System Memory using `vmstat`
