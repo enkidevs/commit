3ed7ae05c79813c91468956c07611f945f919832
colecathey <cole.cathey@outlook.com>
2016-09-09 10:59:12 +0100

    2016-09-09's linux workout

---
+++ contributions/colecathey/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
