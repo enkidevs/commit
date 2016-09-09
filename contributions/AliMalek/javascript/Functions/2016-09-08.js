77681ab57a441306b31d4551c7fd7d2582fdc573
AliMalek <ali.malek.71@gmail.com>
2016-09-09 21:29:20 +0100

    2016-09-08's javascript workout

---
+++ contributions/AliMalek/javascript/Functions/2016-09-08.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function call method
+Social sharing without widgets
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
