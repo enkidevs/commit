93645ad3bcdab232246953a0face186441159a53
_____ <mishak253@gmail.com>
2016-09-09 01:52:56 +0100

    2016-09-08's java workout

---
+++ contributions/_____/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Metadata: setting a file's owner
+Detect or prevent integer overflow
