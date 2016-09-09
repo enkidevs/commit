c37cbc7ba8ea224681d2f5a6e22216df87875bec
ciaomandi <giacomograssi95@gmail.com>
2016-09-09 17:02:08 +0100

    2016-09-09's linux workout

---
+++ contributions/ciaomandi/linux/Networking/2016-09-09.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+List your IPv6 configuration
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
+Short Introduction to `Wget` 
