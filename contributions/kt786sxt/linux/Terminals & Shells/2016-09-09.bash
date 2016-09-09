d553c6493c67690047bbff65d1b21849f8415064
kt786sxt <simon.pitel@gmail.com>
2016-09-09 04:50:49 +0100

    2016-09-09's linux workout

---
+++ contributions/kt786sxt/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Clear bash history
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
