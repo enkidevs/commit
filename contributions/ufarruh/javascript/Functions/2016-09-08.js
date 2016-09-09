f5c8b864a983a7782b42e1a4705d78a0be90e58c
ufarruh <ufarruh@gmail.com>
2016-09-09 03:33:56 +0100

    2016-09-08's javascript workout

---
+++ contributions/ufarruh/javascript/Functions/2016-09-08.js
@@ -0,0 +1,5 @@
+Difference between **.call** and **.apply** methods
+Get query/url variables
+Easily generate a random `HEX` color
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
