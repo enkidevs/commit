71e66ff61ac87e521597de41e63225784de9d5d0
Wazowski <ipodaidar@gmail.com>
2016-09-09 06:27:19 +0100

    2016-09-09's linux workout

---
+++ contributions/Wazowski/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear the terminal instantly
+Adding directories to your `$PATH`
