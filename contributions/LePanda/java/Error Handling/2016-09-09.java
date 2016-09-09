ce85c322e3b7171a55a682e6a787d4506cd6f2b9
LePanda <aviv02020@gmail.com>
2016-09-09 15:02:39 +0100

    2016-09-09's java workout

---
+++ contributions/LePanda/java/Error Handling/2016-09-09.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Most common reason behind **stack overflow** error
+Careful Numeric Data Conversions
