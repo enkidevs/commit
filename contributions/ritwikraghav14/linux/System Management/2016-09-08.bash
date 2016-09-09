cb73ed062cfb3945d05b92170bff05e664cfd2eb
ritwikraghav14 <ritwikraghav14@hotmail.com>
2016-09-09 13:15:10 +0100

    2016-09-08's linux workout

---
+++ contributions/ritwikraghav14/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
