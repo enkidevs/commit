0cc8062ac7608562781b60733e786ade14a3f253
ericlethier <e.lethier@gmail.com>
2016-09-09 17:11:14 +0100

    2016-09-09's javascript workout

---
+++ contributions/ericlethier/javascript/Functions/2016-09-09.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Move cursor at the end of text input
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
