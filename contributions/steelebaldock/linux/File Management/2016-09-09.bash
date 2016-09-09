099a663194121e9cb89c203bf5223130a9499985
steelebaldock <steele.baldock@gmail.com>
2016-09-09 22:31:05 +0100

    2016-09-09's linux workout

---
+++ contributions/steelebaldock/linux/File Management/2016-09-09.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
