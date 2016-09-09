5ea8cb4f4f752e971e720b70602e2362a5c18f8f
steelebaldock <steele.baldock@gmail.com>
2016-09-09 07:44:24 +0100

    2016-09-08's linux workout

---
+++ contributions/steelebaldock/linux/Terminals & Shells/2016-09-08.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
