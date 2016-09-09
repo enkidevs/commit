e86f5eea08f7122760d1d2e15289454c3bc44d4a
sbalint98 <sbsbalint14@gmail.com>
2016-09-09 10:51:07 +0100

    2016-09-09's linux workout

---
+++ contributions/sbalint98/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
