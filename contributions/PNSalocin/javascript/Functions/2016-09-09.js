c7eabb4eddad26a52954846744d01f5e462ad726
PNSalocin <nicolas.merelli@gmail.com>
2016-09-09 23:47:40 +0100

    2016-09-09's javascript workout

---
+++ contributions/PNSalocin/javascript/Functions/2016-09-09.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+IIFE pattern
+Easily generate a random `HEX` color
+Check if a document is done loading
