4f1423d5e2f18a9e89000f1dd261792df17b5768
DavidLevi <david.levi999@gmail.com>
2016-09-09 20:14:13 +0100

    2016-09-09's linux workout

---
+++ contributions/DavidLevi/linux/Networking/2016-09-09.bash
@@ -0,0 +1,5 @@
+SSH tunneling made easy
+Logging dropped firewall packets
+List your IPv6 configuration
+Sending emails from terminal / scripts
+Staying connected remotely via SSH without an interactive shell
