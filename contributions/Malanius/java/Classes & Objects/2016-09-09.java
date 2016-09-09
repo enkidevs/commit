d5b23e44a9ee08b7fb20446d8677a1f0d623e6df
Malanius <malanius@seznam.cz>
2016-09-09 08:27:40 +0100

    2016-09-09's java workout

---
+++ contributions/Malanius/java/Classes & Objects/2016-09-09.java
@@ -0,0 +1,5 @@
+Sibling Classes
+Use a copy constructor for non-immutable objects
+Utility classes and `static` methods
+Java object references
+String formatting and parsing with `SimpleDateFormat`
