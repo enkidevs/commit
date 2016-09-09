933f3b3fef780eda8ba5bd31a494b70608129c38
Fre_d <frogkr@gmail.com>
2016-09-09 23:02:48 +0100

    2016-09-09's linux workout

---
+++ contributions/Fre_d/linux/Jobs & Processes/2016-09-09.bash
@@ -0,0 +1,5 @@
+Backgrounding tasks 
+`Cron` dot-in-filename issues when using `run-parts`
+Fixing the shell in cron
+Common issue with scripts in `cron`
+How to pause a process and run others in the background
