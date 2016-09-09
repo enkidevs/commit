9eeb5d86b6889aad13e97c2348f6c3b90bdb90e6
corux <tobias@wallura.org>
2016-09-09 06:42:53 +0100

    2016-09-09's java workout

---
+++ contributions/corux/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Avoid using `static` variables
