4a181b977546e8d828aed3476933bc1c53d0a899
Mavwreck <mavwreck@hotmail.com>
2016-09-09 02:16:21 +0100

    2016-09-08's linux workout

---
+++ contributions/Mavwreck/linux/Terminals & Shells/2016-09-08.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Terminal based browser
+Terminal incognito mode
+Keep useful commands in your shell history with tags
