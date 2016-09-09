884b2f6ead89e32329b2709de72e3f806cef3927
JoshuaCrocker <joshua.crocker@hotmail.co.uk>
2016-09-09 06:41:44 +0100

    2016-09-09's java workout

---
+++ contributions/JoshuaCrocker/java/I/O/2016-09-09.java
@@ -0,0 +1,5 @@
+Create system file paths using the `Path` class
+Checking for the existence of files and directories
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
+The `Console` class
