25261246f7a1ff28fa9052a311bb67c1582007f6
lazai <mariusdotspinu@gmail.com>
2016-09-09 11:21:41 +0100

    2016-09-09's java workout

---
+++ contributions/lazai/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use meaningful names
+Java naming conventions
+Altering format string output by changing a format specifier's `argument_index`
+How range operations work
