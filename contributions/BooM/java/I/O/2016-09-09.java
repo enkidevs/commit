0d4b03a6d24b873037a0754cd4cd479b773bb3c4
BooM <tararuj.michal@gmail.com>
2016-09-09 07:36:52 +0100

    2016-09-09's java workout

---
+++ contributions/BooM/java/I/O/2016-09-09.java
@@ -0,0 +1,5 @@
+How to list all files in a directory that match a filename extension in Java
+Create system file paths using the `Path` class
+Using buffered streams
+Deleting files and directories
+The `Console` class
