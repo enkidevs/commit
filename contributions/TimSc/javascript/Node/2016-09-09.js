b6bf3c2cdbdeee580a148ed08cc0f23cd0487eb8
TimSc <scht.sc@gmail.com>
2016-09-09 11:12:47 +0100

    2016-09-09's javascript workout

---
+++ contributions/TimSc/javascript/Node/2016-09-09.js
@@ -0,0 +1,5 @@
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+Clustering
+Automatic compilation for Node with **Nodemon** 
+`try-catch` only for **sync** code
