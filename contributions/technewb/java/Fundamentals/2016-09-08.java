fe8e8392974c711dc356feee4ee8f4f962dfdd47
technewb <lauramneal3@hotmail.com>
2016-09-09 05:00:09 +0100

    2016-09-08's java workout

---
+++ contributions/technewb/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+Overloading Methods in Java
+Using Inheritance to reduce code repetition
+Using `Printstream.format` to format a string before printing
