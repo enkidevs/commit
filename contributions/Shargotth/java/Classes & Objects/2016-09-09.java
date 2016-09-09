b63b5573d5b44fb67f7939a314117abd46b7ee90
Shargotth <martin.hogge@gmail.com>
2016-09-09 15:40:29 +0100

    2016-09-09's java workout

---
+++ contributions/Shargotth/java/Classes & Objects/2016-09-09.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Use a copy constructor for non-immutable objects
+Utility classes and `static` methods
+How and when to use `WeakHashMap` class
+Fields with Java **Reflection API**
