d9fdc52d9dd55fb7f993c4dcb1f6d9d55f4027e2
jacobdanner <jacob.danner@gmail.com>
2016-09-09 19:12:40 +0100

    2016-09-09's java workout

---
+++ contributions/jacobdanner/java/I/O/2016-09-09.java
@@ -0,0 +1,5 @@
+Reading and writing text files
+Using buffered streams
+Checking for the existence of files and directories
+List contents of a directory with `newDirectoryStream`
+The `Console` class
