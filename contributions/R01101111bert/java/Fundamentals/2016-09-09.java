5df5ceb5b56c57825f07cb58f96cdb808c516fdc
R01101111bert <robert.obkircher@gmail.com>
2016-09-09 16:30:41 +0100

    2016-09-09's java workout

---
+++ contributions/R01101111bert/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,4 @@
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
