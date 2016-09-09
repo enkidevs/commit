87f29f280d5152203ba69bf2dbfbbe070a16fc84
Satnam14 <satnamxv63@gmail.com>
2016-09-09 16:23:27 +0100

    2016-09-09's linux workout

---
+++ contributions/Satnam14/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Random password generator
