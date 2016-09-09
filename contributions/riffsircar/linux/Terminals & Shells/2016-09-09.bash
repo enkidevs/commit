129a0ee73e9f7882a3090b22d30385a810898e62
riffsircar <andrewsmith_rusmag@yahoo.co.in>
2016-09-09 15:02:26 +0100

    2016-09-09's linux workout

---
+++ contributions/riffsircar/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Terminal incognito mode
+Sequential execution using the `;` statement separator
