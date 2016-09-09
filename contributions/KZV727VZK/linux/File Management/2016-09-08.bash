ed10f38498fe155c9933c83efb589a049b78067c
KZV727VZK <kzv727vzk@kzv727vzk.com>
2016-09-09 01:42:46 +0100

    2016-09-08's linux workout

---
+++ contributions/KZV727VZK/linux/File Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Find the unknown process preventing deleting of files
+Delete commands aliases
