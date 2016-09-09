d3cb211c9924cedc5b4cee395377d2a917765796
jaysonthepirate <jayson.the.pirate@gmail.com>
2016-09-09 21:02:16 +0100

    2016-09-09's java workout

---
+++ contributions/jaysonthepirate/java/Annotations/2016-09-09.java
@@ -0,0 +1,5 @@
+Use `@Ignore` to disable JUnit tests
+Using the `@Deprecated` annotation
+Suppressing warnings using `@SuppressWarnings`
+Restricting an annotation's application by using `@Target`
+Declaring `@Repeatable` annotation types
