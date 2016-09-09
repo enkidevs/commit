4a5535055f7f5d98ce654f2f9f794606f52fa11a
rychan14 <rychan14@gmail.com>
2016-09-09 23:24:47 +0100

    2016-09-09's javascript workout

---
+++ contributions/rychan14/javascript/React/2016-09-09.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+componentWillReceiveProps Not Triggered After Mounting
+
+Immutability helpers in **React**
+Enhanced Prop Validation 
+Custom `propType`'s to be required
