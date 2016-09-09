02fbfe2f8a7ee58f417cf132749a9274a1b7f5fe
siberianmario <siberian.mario@gmail.com>
2016-09-09 15:24:36 +0100

    2016-09-08's java workout

---
+++ contributions/siberianmario/java/I/O/2016-09-08.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Reading and writing text files
+Retrieving file store attributes
+Listing a file system's root directories
