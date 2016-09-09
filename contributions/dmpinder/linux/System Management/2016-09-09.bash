5fdddac50b978876988b71f46e6dfebccba01db9
dmpinder <dp-signup@vatu.co.uk>
2016-09-09 14:46:55 +0100

    2016-09-09's linux workout

---
+++ contributions/dmpinder/linux/System Management/2016-09-09.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
