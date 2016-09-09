3dba958dfe184f3d899f333a99639ba6acb57d43
telly23 <guillaume.ducrocq0616@gmail.com>
2016-09-09 10:06:03 +0100

    2016-09-09's linux workout

---
+++ contributions/telly23/linux/Package Management/2016-09-09.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Installing a `.deb` package from the terminal
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
+Keeping repos updated with `cron`
