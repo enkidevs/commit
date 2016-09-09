92b5cd339a7c34fb12d357246a248f87927c0f22
gpoxy <ganeshprasad227@gmail.com>
2016-09-09 06:04:11 +0100

    2016-09-08's linux workout

---
+++ contributions/gpoxy/linux/Terminals & Shells/2016-09-08.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Clear the terminal instantly
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
