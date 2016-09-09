a515d85ca8cc643eb12dd2a95ace3618a426e4c4
aseychell <aldrinseychell@gmail.com>
2016-09-09 11:11:39 +0100

    2016-09-09's linux workout

---
+++ contributions/aseychell/linux/File Management/2016-09-09.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
