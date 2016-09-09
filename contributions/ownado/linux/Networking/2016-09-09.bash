ccc074a2b0242542a4f1e298908cba07419443ef
ownado <ruben_l-10@hotmail.com>
2016-09-09 09:05:44 +0100

    2016-09-09's linux workout

---
+++ contributions/ownado/linux/Networking/2016-09-09.bash
@@ -0,0 +1,5 @@
+Debugging `ssh` client issues
+How to clear `iptables` rules
+Useful ssh client optimizations 
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
