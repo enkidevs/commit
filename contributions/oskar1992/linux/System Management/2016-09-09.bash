779818611c24a60950124694a218ab59f3ccfe33
oskar1992 <oskar.koelbl@gmx.at>
2016-09-09 21:42:21 +0100

    2016-09-09's linux workout

---
+++ contributions/oskar1992/linux/System Management/2016-09-09.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
+System uptime
