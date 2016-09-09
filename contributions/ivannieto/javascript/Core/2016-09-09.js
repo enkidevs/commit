92ccdaf7ecb4ce5f28914909315590eb72da49bd
ivannieto <ivan.n.s@tuta.io>
2016-09-09 15:53:37 +0100

    2016-09-09's javascript workout

---
+++ contributions/ivannieto/javascript/Core/2016-09-09.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Truthiness
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+Prevent background image flicker in **IE**
