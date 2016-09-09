ac22fd8b7f9df11ba9ccf53aa5bdba74f908f81f
stevew20 <stevewells20@gmail.com>
2016-09-09 11:49:38 +0100

    2016-09-09's linux workout

---
+++ contributions/stevew20/linux/File Management/2016-09-09.bash
@@ -0,0 +1,7 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
+Easy way of sharing files
