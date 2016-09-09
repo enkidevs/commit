1e142437c9672bedf21d5af84bf063ae8befa323
antoligy <a@ax.gy>
2016-09-09 19:17:09 +0100

    2016-09-09's javascript workout

---
+++ contributions/antoligy/javascript/React/2016-09-09.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+componentWillReceiveProps Not Triggered After Mounting
+
+Use **React** with other libraries
+Dangerously set `innerHTML`
+Immutability helpers in **React**
