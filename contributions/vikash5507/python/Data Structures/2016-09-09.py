2faed79398ccd1bdb3e505b2f383f5aa195122e3
vikash5507 <vikash4466kumar@gmail.com>
2016-09-09 23:10:05 +0100

    2016-09-09's python workout

---
+++ contributions/vikash5507/python/Data Structures/2016-09-09.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Best way to implement a simple `queue` 
+`Module`s everywhere!
+Special attributes of objects and classes
+`queue`s and threads
