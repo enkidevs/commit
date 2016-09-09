1c89806de305a84a3ce20f7074df6c176ac034cd
ekoj <as.esu.eko@gmail.com>
2016-09-09 19:45:34 +0100

    2016-09-09's linux workout

---
+++ contributions/ekoj/linux/System Management/2016-09-09.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Common log files
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
