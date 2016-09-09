89e0ca911c3a7ac3f86121284c6ba5d341a2563f
vteoh <icloud08@gmail.com>
2016-09-09 09:31:37 +0100

    2016-09-08's linux workout

---
+++ contributions/vteoh/linux/Terminals & Shells/2016-09-08.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Useful aliasing in bash
+Terminal incognito mode
+Keep useful commands in your shell history with tags
