a6620950509b1fefb890c92eac3c3ea6c3189f75
bruxbs <brusaler27@gmail.com>
2016-09-09 17:30:05 +0100

    2016-09-09's linux workout

---
+++ contributions/bruxbs/linux/System Management/2016-09-09.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
