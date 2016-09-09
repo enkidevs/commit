18185ab6c9e388eb0f336b3603273c163ce77653
Backfighter <wpw.peter@gmail.com>
2016-09-09 09:43:20 +0100

    2016-09-09's java workout

---
+++ contributions/Backfighter/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Supply `toString()` in all classes
