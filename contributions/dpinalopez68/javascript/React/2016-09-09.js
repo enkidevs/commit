25cbc4ce44f6ebec4f81ea6e38725122c30fc0bb
dpinalopez68 <dpinalopez68@gmail.com>
2016-09-09 18:06:13 +0100

    2016-09-09's javascript workout

---
+++ contributions/dpinalopez68/javascript/React/2016-09-09.js
@@ -0,0 +1,5 @@
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+Dangerously set `innerHTML`
+Shallow compare in **React**
+Immutability helpers in **React**
