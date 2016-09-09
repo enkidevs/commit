e4b90c94d7e3586335c4667095c6f0dd6bd8296d
luiz3107 <lh.santos2008@gmail.com>
2016-09-09 05:11:14 +0100

    2016-09-08's java workout

---
+++ contributions/luiz3107/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Cropping images
+Using bounded type parameters in generic methods
+Measuring time
+Increase `PermGen` space as to avoid `OutOfMemory` errors
