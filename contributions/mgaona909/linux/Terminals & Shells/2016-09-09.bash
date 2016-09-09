7304d789a02467f5f9a683be811ea8635a4df518
mgaona909 <mgaona909@gmail.com>
2016-09-09 17:29:08 +0100

    2016-09-09's linux workout

---
+++ contributions/mgaona909/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,7 @@
+Find any Unix / Linux command
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
