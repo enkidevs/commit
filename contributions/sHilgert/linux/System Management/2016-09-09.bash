c08b123553c30c8587095d4433934a345a808fa3
sHilgert <hilgertfacul@gmail.com>
2016-09-09 22:45:57 +0100

    2016-09-09's linux workout

---
+++ contributions/sHilgert/linux/System Management/2016-09-09.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Commands to shutdown or restart the system
