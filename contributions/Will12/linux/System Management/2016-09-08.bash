2a5774942f09ceaec4554b9849804a49edc30274
Will12 <will12@gmail.com>
2016-09-09 06:12:50 +0100

    2016-09-08's linux workout

---
+++ contributions/Will12/linux/System Management/2016-09-08.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
