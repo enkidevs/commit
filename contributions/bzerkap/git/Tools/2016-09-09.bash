c8bbf988899f7fde254aa94ed41aee47f10982f9
bzerkap <bankspaulw@gmail.com>
2016-09-09 17:46:35 +0100

    2016-09-09's git workout

---
+++ contributions/bzerkap/git/Tools/2016-09-09.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Locate a commit by its hash
+Specify a commit by its ancestry
+Specify a range of commits using double dot syntax
+Create a new branch from a stash
