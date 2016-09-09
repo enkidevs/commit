c398d1417736bc675175f5d901c76e1c964b083f
Beingbook <beingbook@gmail.com>
2016-09-09 02:49:47 +0100

    2016-09-08's javascript workout

---
+++ contributions/Beingbook/javascript/Ecmascript 2015/2016-09-08.js
@@ -0,0 +1,6 @@
+ES6 dynamic property keys in mongo-like set clauses
+Defaults values apply only to `undefined` (and not to `null`)
+Internationalization & Localization
+
+Generators as observers in ES6
+ES6 Generator Transpiler
