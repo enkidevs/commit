5d9b7b81f9fca361780b5b124e04daa3b3121c2c
lmgginspace <lmgginspace@gmail.com>
2016-09-09 23:22:20 +0100

    2016-09-09's python workout

---
+++ contributions/lmgginspace/python/Data Structures/2016-09-09.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Using a `list` as a `stack`
+Special attributes of objects and classes
+Immutable sets with `frozenset`
+Get the most of `float`s
