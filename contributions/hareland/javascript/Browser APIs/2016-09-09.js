8e767c566546b780d3be6bfe3f6cef76739e2686
hareland <kristian@hareland.eu>
2016-09-09 23:19:24 +0100

    2016-09-09's javascript workout

---
+++ contributions/hareland/javascript/Browser APIs/2016-09-09.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use hosted scripts to increase performance
+Drawing with HTML5 `Canvas` API
+Navigating the browser history
+High Resolution Time API
