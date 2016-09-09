7330e5ca9f9cbc15f95a912e36a6cf40a969c38f
NateGlazier <ng230@sussex.ac.uk>
2016-09-09 09:30:25 +0100

    2016-09-09's linux workout

---
+++ contributions/NateGlazier/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Useful aliasing in bash
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
