1f8c5abe0cd333a19ee6cc54c46667621799c919
tnargwoxow <tnargwoxow@gmail.com>
2016-09-09 10:29:26 +0100

    2016-09-09's linux workout

---
+++ contributions/tnargwoxow/linux/File Management/2016-09-09.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Get disk space usage with `df`
+
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
