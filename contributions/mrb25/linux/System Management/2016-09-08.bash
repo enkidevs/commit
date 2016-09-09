1fdbd3ab0343fc051fd2653a2fe422dc0b861b30
mrb25 <mitchben15@gmail.com>
2016-09-09 05:23:14 +0100

    2016-09-08's linux workout

---
+++ contributions/mrb25/linux/System Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
