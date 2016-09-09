382eda0d7c3feb11c7d6a049949792b6376d35ce
pharlan1 <pharlan42@gmail.com>
2016-09-09 22:40:27 +0100

    2016-09-09's linux workout

---
+++ contributions/pharlan1/linux/File Management/2016-09-09.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Easy way of sharing files
