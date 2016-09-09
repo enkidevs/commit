c6fcf8af68fb9d30e6f383a33cf1313f52a920a3
sagiben <ilnextbus@gmail.com>
2016-09-09 05:34:57 +0100

    2016-09-09's java workout

---
+++ contributions/sagiben/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Calling one constructor from another
+Measuring time
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
