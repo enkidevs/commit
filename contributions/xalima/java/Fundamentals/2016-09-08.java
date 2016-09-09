6198b1bd13e7e04b6a8bb586b8128a016b62ee0d
xalima <ajefferson1111@gmail.com>
2016-09-09 03:02:11 +0100

    2016-09-08's java workout

---
+++ contributions/xalima/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
