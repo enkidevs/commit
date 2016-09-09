46961ced49dc8b75164ba7b4934f6308d74fe34d
tenaciousdee <dennisepnd@gmail.com>
2016-09-09 18:01:42 +0100

    2016-09-09's javascript workout

---
+++ contributions/tenaciousdee/javascript/Functions/2016-09-09.js
@@ -0,0 +1,5 @@
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Easily generate a random `HEX` color
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
