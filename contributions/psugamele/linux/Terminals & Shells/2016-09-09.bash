59be9d3265a7a3b8e3edca248482caf886f23322
psugamele <psugamele@gisitalia.it>
2016-09-09 21:00:34 +0100

    2016-09-09's linux workout

---
+++ contributions/psugamele/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Aliasing ssh connections
+Useful aliasing in bash
+Keep useful commands in your shell history with tags
