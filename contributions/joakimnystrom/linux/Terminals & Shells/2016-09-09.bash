d08a9fc01f36ae83e1c0e22c040bd26b5d20d1f9
joakimnystrom <jnystromdesign@gmail.com>
2016-09-09 08:42:12 +0100

    2016-09-09's linux workout

---
+++ contributions/joakimnystrom/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
