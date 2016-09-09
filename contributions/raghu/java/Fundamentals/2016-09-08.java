cd21a901932fd6645c584d04d4dc46570b374627
raghu <raghu.9208@gmail.com>
2016-09-09 04:37:09 +0100

    2016-09-08's java workout

---
+++ contributions/raghu/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Inheriting a constructor from a superclass
+Using `Printstream.format` to format a string before printing
+Always specify type parameter
+Avoid using `static` variables
