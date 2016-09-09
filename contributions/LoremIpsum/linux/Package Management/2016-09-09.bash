99fe84e5271d7eb76541978c52b984a62e3ce347
LoremIpsum <yannick.jadoul@gmail.com>
2016-09-09 16:50:26 +0100

    2016-09-09's linux workout

---
+++ contributions/LoremIpsum/linux/Package Management/2016-09-09.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Backporting on Debian
+
+Get `apt` to use a mirror / faster mirror
+Downgrade a package via `apt-get`
+Keeping repos updated with `cron`
