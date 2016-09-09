489e5918466826ec9abf9bac8ab273f2d9a3c219
aaronmcd1214 <aaronmcd1214@gmail.com>
2016-09-09 18:32:50 +0100

    2016-09-09's java workout

---
+++ contributions/aaronmcd1214/java/Patterns/2016-09-09.java
@@ -0,0 +1,5 @@
+Canceling an individual task using `java.util.Timer` class
+Private constructors
+Static factory methods
+Using initializer blocks to minimize code repetition in constructors
+Package naming conventions
