a213747220da91a9fe137037d410b81c880bcca3
lilwayne1556 <lilwayne1556@gmail.com>
2016-09-09 01:38:45 +0100

    2016-09-08's java workout

---
+++ contributions/lilwayne1556/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Use strings in a `switch` statement
+Use `DecimalFormat` class to format numbers
+Always specify type parameter
+There is no such thing as *pass-by-reference* in Java
