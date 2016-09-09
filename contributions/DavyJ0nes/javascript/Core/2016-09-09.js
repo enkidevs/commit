9b50c0018c6cf8de5ffddab48102cc722e512171
DavyJ0nes <davy.jones@me.com>
2016-09-09 21:24:40 +0100

    2016-09-09's javascript workout

---
+++ contributions/DavyJ0nes/javascript/Core/2016-09-09.js
@@ -0,0 +1,5 @@
+Using assignment operators
+Hoisting applies only to variable declarations, not initializations
+The difference between null, undefined and NaN
+Extra function arguments are undefined by default
+`console.*` in JavaScript
