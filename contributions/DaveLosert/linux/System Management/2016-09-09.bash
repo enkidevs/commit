b137ad0cb5a9bde4afd174338a6272154759842f
DaveLosert <david.losert@gmx.de>
2016-09-09 20:08:09 +0100

    2016-09-09's linux workout

---
+++ contributions/DaveLosert/linux/System Management/2016-09-09.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
