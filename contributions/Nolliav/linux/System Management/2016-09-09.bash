57b0b32975f4f63fa26666d5d50b28d3c104f950
Nolliav <matthieu.viallon@gmail.com>
2016-09-09 22:56:21 +0100

    2016-09-09's linux workout

---
+++ contributions/Nolliav/linux/System Management/2016-09-09.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
