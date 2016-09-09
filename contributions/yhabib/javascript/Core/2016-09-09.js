f43fd8d7cb18b57d9ac2f2f39d39e356f56a2ef5
yhabib <habibfernandez@gmail.com>
2016-09-09 12:48:24 +0100

    2016-09-09's javascript workout

---
+++ contributions/yhabib/javascript/Core/2016-09-09.js
@@ -0,0 +1,5 @@
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Scope and strict mode
+Avoid using `with`
+Prevent background image flicker in **IE**
