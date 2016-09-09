a3bb633b6a8b36ba9a0d9ef14321e9d8dcff539e
francescolucchi <francescolucchi@gmail.com>
2016-09-09 17:34:27 +0100

    2016-09-09's java workout

---
+++ contributions/francescolucchi/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
