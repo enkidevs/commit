9e27ca7989cbd57943fe37a34affd92618e6cf26
pazer <pazer@gmx.com>
2016-09-09 22:13:13 +0100

    2016-09-09's java workout

---
+++ contributions/pazer/java/Error Handling/2016-09-09.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
