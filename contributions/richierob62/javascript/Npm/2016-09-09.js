03634e5a60cb697757573cfa8d8724f6df3b0b3d
richierob62 <richierob62@gmail.com>
2016-09-10 00:09:09 +0100

    2016-09-09's javascript workout

---
+++ contributions/richierob62/javascript/Npm/2016-09-09.js
@@ -0,0 +1,5 @@
+npm can handle multiple versions of a dependency
+Manage local node modules with `npm link`
+`npm`  as an alternative to Gulp
+devDependencies in `npm`
+`optionalDependencies` in npm
