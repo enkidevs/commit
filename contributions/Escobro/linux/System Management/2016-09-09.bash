a21d3f23f753fac0b451e36d411c34e72716c78c
Escobro <escobar.michael2@gmail.com>
2016-09-09 21:14:26 +0100

    2016-09-09's linux workout

---
+++ contributions/Escobro/linux/System Management/2016-09-09.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+List current logged on users with `w`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
