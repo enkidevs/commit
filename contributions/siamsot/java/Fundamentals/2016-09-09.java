d76abd5289c907c5acf9629bd314f6e89cee6616
siamsot <siamsot@gmail.com>
2016-09-09 11:52:05 +0100

    2016-09-09's java workout

---
+++ contributions/siamsot/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Writing generic methods
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+Limit Accessibility of `Fields`
+Avoid using `static` variables
