0e324a553f04ba6b089a56d9960883565274e070
bidj <enki@papito.33mail.com>
2016-09-09 02:04:01 +0100

    2016-09-08's linux workout

---
+++ contributions/bidj/linux/File Management/2016-09-08.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
