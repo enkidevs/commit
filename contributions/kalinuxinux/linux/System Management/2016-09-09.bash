98f699db146737d6bdbd8faecd51614a9328c3d5
kalinuxinux <khg207@gmail.com>
2016-09-09 19:18:00 +0100

    2016-09-09's linux workout

---
+++ contributions/kalinuxinux/linux/System Management/2016-09-09.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
