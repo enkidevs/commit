df6b2c421a07d95ca5516be6a1ba60ad596f6a12
ahmetilgin <279967@ogr.ktu.edu.tr>
2016-09-09 08:01:09 +0100

    2016-09-09's linux workout

---
+++ contributions/ahmetilgin/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Execute a command without saving it in history
+Aliasing ssh connections
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
