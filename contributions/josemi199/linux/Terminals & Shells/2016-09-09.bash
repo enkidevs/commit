5ee816f9647a41988f645079c04d00d8d2e272dd
josemi199 <josemicaso@gmail.com>
2016-09-10 00:08:21 +0100

    2016-09-09's linux workout

---
+++ contributions/josemi199/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
