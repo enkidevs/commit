02392cc8d6b7adbaff079e99d354d7a6bc743d8e
ajoshi <ajoshi@financialengines.com>
2016-09-09 02:50:48 +0100

    2016-09-08's java workout

---
+++ contributions/ajoshi/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
