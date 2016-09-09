d98a740cef5408f7e97717a7c15a9196cc4a4782
destan <destan@dorukdestan.com>
2016-09-09 16:01:11 +0100

    2016-09-09's java workout

---
+++ contributions/destan/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
