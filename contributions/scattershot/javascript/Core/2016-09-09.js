03a7973d021c52809ddf209a5473f1d182c10b16
scattershot <colin@scattershotcreative.com>
2016-09-09 10:59:10 +0100

    2016-09-09's javascript workout

---
+++ contributions/scattershot/javascript/Core/2016-09-09.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Performing a function at timed intervals
+Prevent background image flicker in **IE**
+Getting the *real* dimensions of an image
+Round numbers to `N` decimals 
