c960abc9c6a74c6d2f2272c4c806157fe84d3373
Jako81624 <jacobwlane@gmail.com>
2016-09-09 11:41:32 +0100

    2016-09-09's java workout

---
+++ contributions/Jako81624/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Altering format string output by changing a format specifier's `argument_index`
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
+Using an interface as a parameter
