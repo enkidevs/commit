545782223a85e33365c7a81f0ed97684596ad56c
thePier <arrigo.pierotti@gmail.com>
2016-09-09 01:32:37 +0100

    2016-09-08's java workout

---
+++ contributions/thePier/java/Fundamentals/2016-09-08.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Using an interface as a parameter
