568a4a9bc46e54b8a471e9d1f14532c0ae78f4eb
samhamnam <samuel.hammersberg@gmail.com>
2016-09-10 09:01:33 +0100

    2016-09-10's java workout

---
+++ contributions/samhamnam/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Measuring time
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
