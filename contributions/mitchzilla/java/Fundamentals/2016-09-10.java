a4d808294ebbad50ff36223b3969ce4abea0c5ad
mitchzilla <mitchissweet@gmail.com>
2016-09-10 19:25:14 +0100

    2016-09-10's java workout

---
+++ contributions/mitchzilla/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
