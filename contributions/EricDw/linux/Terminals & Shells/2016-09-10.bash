3df8b31b0afa13e77ee31160861f5c72207e50b2
EricDw <dewildte@gmail.com>
2016-09-10 21:22:03 +0100

    2016-09-10's linux workout

---
+++ contributions/EricDw/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Terminal incognito mode
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
