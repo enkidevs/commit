24b7a80daec185f59eafc3b78c5e21bcb950322e
reel <dmitryshevliakov@gmail.com>
2016-09-10 10:26:30 +0100

    2016-09-10's java workout

---
+++ contributions/reel/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Using an interface as a parameter
