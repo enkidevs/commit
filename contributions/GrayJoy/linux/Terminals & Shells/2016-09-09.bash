971348e51eb298e2c057bf0895a9bc3721938e00
GrayJoy <daorenmc@gmail.com>
2016-09-10 17:26:40 +0100

    2016-09-09's linux workout

---
+++ contributions/GrayJoy/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
