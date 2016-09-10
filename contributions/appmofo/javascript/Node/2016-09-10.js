1902b101a39558d41f1182a29b7b7bd437357e4c
appmofo <ivan@appmofo.com>
2016-09-11 00:12:55 +0100

    2016-09-10's javascript workout

---
+++ contributions/appmofo/javascript/Node/2016-09-10.js
@@ -0,0 +1,5 @@
+Http Server in **Node**
+Global Objects and Environment Variables in **Node**
+Use conventional property names for error handling in Node.js
+**Node** `os` module
+`try-catch` only for **sync** code
