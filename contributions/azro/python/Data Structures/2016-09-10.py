a4e45bfaf0d2fb233f73dd073a4a5a316cf2c74c
azro <akramdaadaa@gmail.com>
2016-09-10 07:54:17 +0100

    2016-09-10's python workout

---
+++ contributions/azro/python/Data Structures/2016-09-10.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`Dictionary` view objects
+Double ended queues with `deque`
+Immutable sets with `frozenset`
+Get more with `collections`!
