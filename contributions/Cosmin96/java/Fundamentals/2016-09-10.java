88fe39d0ebd553ee00a0251f356a9727d83d901f
Cosmin96 <zcabcnb@ucl.ac.uk>
2016-09-10 10:02:55 +0100

    2016-09-10's java workout

---
+++ contributions/Cosmin96/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Inheriting a constructor from a superclass
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Using an interface as a parameter
