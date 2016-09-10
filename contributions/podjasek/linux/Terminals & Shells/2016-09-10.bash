f24ff655ddf1b840d46f82b473e5ceddbb77f992
podjasek <sebastian@podjasek.pl>
2016-09-10 11:54:44 +0100

    2016-09-10's linux workout

---
+++ contributions/podjasek/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal incognito mode
+Keep useful commands in your shell history with tags
