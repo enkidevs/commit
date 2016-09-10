264ee3ffb358584bee32c4b5bc616847f83fb9aa
lanark <danielcontarelli@gmail.com>
2016-09-10 09:05:12 +0100

    2016-09-10's linux workout

---
+++ contributions/lanark/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
