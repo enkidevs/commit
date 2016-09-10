5b73fd4818ad021159305c4ac13b2ae6d23dcc30
SirViente <sirviente@gmail.com>
2016-09-10 13:12:27 +0100

    2016-09-10's linux workout

---
+++ contributions/SirViente/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear the terminal instantly
+Useful aliasing in bash
+Sequential execution using the `;` statement separator
