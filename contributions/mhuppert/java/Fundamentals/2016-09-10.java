86d6954ead3cac77409033034bd37fcb0f7b6785
mhuppert <info@marcel-huppert.de>
2016-09-10 11:37:03 +0100

    2016-09-10's java workout

---
+++ contributions/mhuppert/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Measuring time
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
