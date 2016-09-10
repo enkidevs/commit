36bba8532eacfca8003975efbece76e431413e7f
Prominence <prominence96@gmail.com>
2016-09-10 10:25:04 +0100

    2016-09-10's java workout

---
+++ contributions/Prominence/java/Java 8/2016-09-10.java
@@ -0,0 +1,5 @@
+Java 8 Lambda expressions
+Using `Arrays.parallelSort` 
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
