73eeed47976281bfba6f7ec1b4795fc207c8c165
leomcchon <alphatesteando@gmail.com>
2016-09-10 08:41:47 +0100

    2016-09-10's linux workout

---
+++ contributions/leomcchon/linux/System Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+List current logged on users with `w`
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
