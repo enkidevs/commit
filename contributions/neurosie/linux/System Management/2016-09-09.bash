18ad2692aa99dcbd2ab16e39bf6c0317213499fd
neurosie <neurosie@gmail.com>
2016-09-10 05:53:20 +0100

    2016-09-09's linux workout

---
+++ contributions/neurosie/linux/System Management/2016-09-09.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
