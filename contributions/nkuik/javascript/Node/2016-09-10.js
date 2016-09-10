c87a24fe28d807a175ac232333967facfc2978ef
nkuik <nathan.kuik@gmail.com>
2016-09-10 13:35:31 +0100

    2016-09-10's javascript workout

---
+++ contributions/nkuik/javascript/Node/2016-09-10.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+`exports` vs. `module.exports` in **Node**
+`try-catch` only for **sync** code
