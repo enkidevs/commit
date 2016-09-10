b4b1b36fb69f4dfc11f2d1b0d60dad7fc3db5b36
Emin <m@memin.tk>
2016-09-10 11:07:30 +0100

    2016-09-09's java workout

---
+++ contributions/Emin/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
