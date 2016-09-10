bc34ee1b15af679b81f134fd601c50e9f9b571c5
heimdall <hemidal@gmail.com>
2016-09-10 15:03:36 +0100

    2016-09-10's javascript workout

---
+++ contributions/heimdall/javascript/Functions/2016-09-10.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Anonymous functions
+Function call method
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
