a09a5a3153a802aae8959c5cce52fc9798e71cf4
therealshabi <shahbaz.h96@gmail.com>
2016-09-10 06:42:11 +0100

    2016-09-09's java workout

---
+++ contributions/therealshabi/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use meaningful names
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Always specify type parameter
