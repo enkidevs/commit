195168e48c51946fc72e8208e9155b47a3b81e74
lucia <mikoto.yu51@gmail.com>
2016-09-10 03:19:12 +0100

    2016-09-09's linux workout

---
+++ contributions/lucia/linux/System Management/2016-09-09.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Preventing a user from logging into the system
+Commands to shutdown or restart the system
+System uptime
