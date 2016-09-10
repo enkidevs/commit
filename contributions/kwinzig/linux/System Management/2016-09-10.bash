aa2989780ba58603c0e33a2c62399b6bd914d8ba
kwinzig <klaus.winzig@googlemail.com>
2016-09-10 16:28:42 +0100

    2016-09-10's linux workout

---
+++ contributions/kwinzig/linux/System Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Commands to shutdown or restart the system
+Finding Open Files With `lsof`
