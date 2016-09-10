fb461da7782eba3ebaaaf0424a7fe55595d307a3
ClarkMitchell <clarkcsmitchell@gmail.com>
2016-09-10 22:27:36 +0100

    2016-09-10's linux workout

---
+++ contributions/ClarkMitchell/linux/System Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
