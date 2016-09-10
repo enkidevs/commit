612d4c1072f1656cbd948dda7a598a4c3540f34e
hsxpunisher <gavin.sortor@gmail.com>
2016-09-10 19:22:48 +0100

    2016-09-10's git workout

---
+++ contributions/hsxpunisher/git/Essentials/2016-09-10.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to unstage a staged file
+What is a checksum?
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
