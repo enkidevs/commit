ca5059ab38b7e0d06222fb6b2ed9a0cc6fd541fd
xazax <johan.daniel.nystrom@gmail.com>
2016-09-10 09:06:11 +0100

    2016-09-10's linux workout

---
+++ contributions/xazax/linux/System Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
+Finding Open Files With `lsof`
