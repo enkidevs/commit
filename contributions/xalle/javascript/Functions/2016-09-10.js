b845ded061d22860db5369c5de2257b9663e1b66
xalle <n.malmqvist@gmail.com>
2016-09-10 11:07:53 +0100

    2016-09-10's javascript workout

---
+++ contributions/xalle/javascript/Functions/2016-09-10.js
@@ -0,0 +1,5 @@
+Hoisting
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Changing a functions context with `fn.call(object)`
