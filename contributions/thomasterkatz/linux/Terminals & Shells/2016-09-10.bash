0b015e0cb6ea35ad3f6d5ebab72dd216b87323e2
thomasterkatz <thomasterkatz@gmail.com>
2016-09-10 11:57:27 +0100

    2016-09-10's linux workout

---
+++ contributions/thomasterkatz/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
