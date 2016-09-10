06ff118e0af0c28e9b200581775f981889972779
hsjolin <hsjolin@outlook.com>
2016-09-10 07:19:46 +0100

    2016-09-10's java workout

---
+++ contributions/hsjolin/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Measuring time
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
