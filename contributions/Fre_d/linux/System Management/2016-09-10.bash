3e5c65e3acff07a1d02af08b8328e1dc22626844
Fre_d <frogkr@gmail.com>
2016-09-10 13:06:57 +0100

    2016-09-10's linux workout

---
+++ contributions/Fre_d/linux/System Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
