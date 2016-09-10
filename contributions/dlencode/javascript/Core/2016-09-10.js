9a6430131105cdd07f45388d9fafdc7b395f4fa0
dlencode <mail@dlencode.com>
2016-09-10 21:31:07 +0100

    2016-09-10's javascript workout

---
+++ contributions/dlencode/javascript/Core/2016-09-10.js
@@ -0,0 +1,5 @@
+Setting default values with `short circuiting`
+`catch` is block scoped
+Extra function arguments are undefined by default
+Prevent background image flicker in **IE**
+Detect **DO NOT TRACK** status
