9ef02dfa52acebbc4309869d12a3878266de11d3
donnici <yep.dias@gmail.com>
2016-09-10 18:28:28 +0100

    2016-09-10's linux workout

---
+++ contributions/donnici/linux/System Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
