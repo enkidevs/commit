5e8ab93b621027a696dca9053a9a493480178070
syfo <lalo.garcia.2@gmail.com>
2016-09-10 03:09:30 +0100

    2016-09-09's javascript workout

---
+++ contributions/syfo/javascript/React/2016-09-09.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Functional Stateless Components in React
+Props in getInitialState Is an        
+Anti-Pattern
+Shallow compare in **React**
+`ReactDOM.render` ref
