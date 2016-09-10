8cc3c2024498d826d5a6fe6608bdcb65639945c0
Will12 <will12@gmail.com>
2016-09-10 16:14:28 +0100

    2016-09-10's java workout

---
+++ contributions/Will12/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Java naming conventions
+Connecting to FTP using Java
+Do not perform bitwise and arithmetic operations on the same data
