40fa90655b65bc299335b8ed36698db3b6ae8126
manuxviii <emmanuel.mesnige@protonmail.com>
2016-09-10 22:37:54 +0100

    2016-09-10's linux workout

---
+++ contributions/manuxviii/linux/Networking/2016-09-10.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Sending emails from terminal / scripts
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Staying connected remotely via SSH without an interactive shell
