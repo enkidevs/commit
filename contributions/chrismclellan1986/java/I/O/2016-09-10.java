49d3da82cde680f6e1e2fe52b71440fcd63fdaf4
chrismclellan1986 <cmclellan@live.co.uk>
2016-09-10 08:30:47 +0100

    2016-09-10's java workout

---
+++ contributions/chrismclellan1986/java/I/O/2016-09-10.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to list all files in a directory that match a filename extension in Java
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
+Deleting files and directories
