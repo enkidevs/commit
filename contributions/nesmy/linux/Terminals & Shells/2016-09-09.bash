2737627b99a5074ecbab93c0804aea6e17aa60fc
nesmy <pnesmy@gmail.com>
2016-09-10 04:22:21 +0100

    2016-09-09's linux workout

---
+++ contributions/nesmy/linux/Terminals & Shells/2016-09-09.bash
@@ -0,0 +1,5 @@
+Clear bash history
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
