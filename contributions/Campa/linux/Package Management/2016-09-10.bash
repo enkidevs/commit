3ec5fd231747cb146b570e1df718e3338f5a3f4a
Campa <euller_sl@hotmail.com>
2016-09-10 19:23:35 +0100

    2016-09-10's linux workout

---
+++ contributions/Campa/linux/Package Management/2016-09-10.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+Backporting on Debian
+
+Downgrade a package via `apt-get`
+Keeping repos updated with `cron`
