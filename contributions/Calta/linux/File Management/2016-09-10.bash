8fa9789036378507b1c0f3f199eff18ffd8f1121
Calta <marco.caltabellotta@gmail.com>
2016-09-10 21:40:25 +0100

    2016-09-10's linux workout

---
+++ contributions/Calta/linux/File Management/2016-09-10.bash
@@ -0,0 +1,7 @@
+Finding file with regexes
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
