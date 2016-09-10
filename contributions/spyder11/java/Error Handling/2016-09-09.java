1901340622fabe157029efdb54077b863c7dff75
spyder11 <spyder11@me.com>
2016-09-10 01:34:41 +0100

    2016-09-09's java workout

---
+++ contributions/spyder11/java/Error Handling/2016-09-09.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
