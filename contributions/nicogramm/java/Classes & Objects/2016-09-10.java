87ac4a97194c115e51a76377d67af489c60b0887
nicogramm <nico9klatt@gmail.com>
2016-09-10 13:09:48 +0100

    2016-09-10's java workout

---
+++ contributions/nicogramm/java/Classes & Objects/2016-09-10.java
@@ -0,0 +1,5 @@
+Utility classes and `static` methods
+The `abstract` keyword
+Double Brace Initialization
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
