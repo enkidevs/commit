b4922f9d4136e2e7bcdab25a1d6aa851d91d003e
JrobT <jack@trute.co.uk>
2016-09-10 11:26:13 +0100

    2016-09-10's java workout

---
+++ contributions/JrobT/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
