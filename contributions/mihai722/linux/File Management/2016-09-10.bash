350fddaf0832d63c8f9cea87c36a5d9e8b4f00fd
mihai722 <mihai7221@gmail.com>
2016-09-10 10:57:44 +0100

    2016-09-10's linux workout

---
+++ contributions/mihai722/linux/File Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Deleting non-empty directories
