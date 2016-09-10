10eb0f2228bc7582d37e032afae2454fef3ae20b
romajmg <romajmg@gmail.com>
2016-09-11 00:58:27 +0100

    2016-09-10's linux workout

---
+++ contributions/romajmg/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
