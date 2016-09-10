129bf9eeda54f9568c4e475161afa62928b0c1cc
dnrost <dnrost@gmail.com>
2016-09-10 22:33:44 +0100

    2016-09-10's java workout

---
+++ contributions/dnrost/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
