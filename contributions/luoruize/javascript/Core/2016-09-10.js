2e134da247c563db3321073292a4dd381fd5072f
luoruize <luoruize@gmail.com>
2016-09-10 17:40:24 +0100

    2016-09-10's javascript workout

---
+++ contributions/luoruize/javascript/Core/2016-09-10.js
@@ -0,0 +1,5 @@
+Labeling your loops
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Prevent background image flicker in **IE**
+Getting the *real* dimensions of an image
