249c07369ad6e2a3d2eea2578a0d80aa7e341853
P4R93 <p4r.esp@gmail.com>
2016-09-10 22:10:55 +0100

    2016-09-10's linux workout

---
+++ contributions/P4R93/linux/System Management/2016-09-10.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
