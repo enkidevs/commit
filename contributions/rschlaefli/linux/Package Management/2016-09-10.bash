f7eb75ebc55a2a72f04c302448dbdf5fd01a95cb
rschlaefli <rolandschlaefli@gmail.com>
2016-09-10 08:16:17 +0100

    2016-09-10's linux workout

---
+++ contributions/rschlaefli/linux/Package Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Removing PPAs
+Prevent updating a specific package in Debian systems
+Using `Tasksel` for software installation
+Using `PPAs`
