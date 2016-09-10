795afc999db43d7d1c5d83d89d11dc162f0c297b
solohtml <abdonjesusbp@gmail.com>
2016-09-10 10:47:41 +0100

    2016-09-10's javascript workout

---
+++ contributions/solohtml/javascript/Core/2016-09-10.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Hoisting applies only to variable declarations, not initializations
+Avoid using `with`
+Inaccuracy of binary floating-point format
+Base conversion 
