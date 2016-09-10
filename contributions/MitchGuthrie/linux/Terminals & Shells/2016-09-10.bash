f8acdd3b0b9de934730e2acbd669b0e242cdc632
MitchGuthrie <mitchell.guthrie13@gmail.com>
2016-09-10 19:26:17 +0100

    2016-09-10's linux workout

---
+++ contributions/MitchGuthrie/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Clear the terminal instantly
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
