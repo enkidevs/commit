bc02a32f4cbf1a13f6d30b00b68b94c707d18fa2
isurunix <isurunix@gmail.com>
2016-09-10 06:53:35 +0100

    2016-09-09's javascript workout

---
+++ contributions/isurunix/javascript/Node/2016-09-09.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Synchronous File Write/Read in Node.js
+First-error callbacks in **Node**
+Use conventional property names for error handling in Node.js
+**Node** `os` module
