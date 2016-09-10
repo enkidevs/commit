42c7f3f319a60993e733b3115247c8643867f1c3
himynameisbeni <benmoore422@gmail.com>
2016-09-10 01:32:23 +0100

    2016-09-09's git workout

---
+++ contributions/himynameisbeni/git/Essentials/2016-09-09.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+You can use git offline!
+How to set your username and email
+Use `short` status to make output more compact
+`master` and `origin` aren't special
