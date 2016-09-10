76136b7b113d35d60040c11d9c0ae7e6d926cf58
lohikansun <pincus.d@husky.neu.edu>
2016-09-11 00:57:59 +0100

    2016-09-10's java workout

---
+++ contributions/lohikansun/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Updating interfaces by using `default` methods
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
