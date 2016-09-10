a10b619b9e312d7a3e24c4281ad6d11825b8b49c
gkhnoisgtht <m81a12@gmail.com>
2016-09-10 16:08:20 +0100

    2016-09-10's java workout

---
+++ contributions/gkhnoisgtht/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
