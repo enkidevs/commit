be22b84d964928946c638c1fd47dbcdc5d417aae
laurfx <laurflesh@gmail.com>
2016-09-11 00:25:50 +0100

    2016-09-10's linux workout

---
+++ contributions/laurfx/linux/System Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Detect your linux distribution
+List current logged on users with `w`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
