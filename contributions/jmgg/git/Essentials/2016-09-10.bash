f6f5b441da17324105b08409127892eda03016bf
jmgg <guigom9@gmail.com>
2016-09-11 00:43:41 +0100

    2016-09-10's git workout

---
+++ contributions/jmgg/git/Essentials/2016-09-10.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+What is stored in a commit?
+Search by commit message keyword
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
