e786d2da4af0d2b93ab1de0bf74aec9a982dfa09
anthonyh29 <anthon.hill@outlook.com>
2016-09-10 12:17:42 +0100

    2016-09-10's linux workout

---
+++ contributions/anthonyh29/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
