5515ae56067a17f3c25653798907b7f66e2f7c94
Micha-kun <michakun@gmail.com>
2016-09-10 18:25:16 +0100

    2016-09-10's linux workout

---
+++ contributions/Micha-kun/linux/System Management/2016-09-10.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
