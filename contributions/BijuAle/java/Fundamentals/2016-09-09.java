4cf096af170402b79848c0e3d1fe295bfad884fe
BijuAle <bijuale@hotmail.com>
2016-09-10 11:21:06 +0100

    2016-09-09's java workout

---
+++ contributions/BijuAle/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Using Inheritance to reduce code repetition
+Inheriting a constructor from a superclass
+Metadata: setting a file's owner
