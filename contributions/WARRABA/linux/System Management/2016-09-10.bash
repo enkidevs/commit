6594322e3bbd1e1d4e1d16ddd87364676827101f
WARRABA <quateba@gmail.com>
2016-09-10 08:52:32 +0100

    2016-09-10's linux workout

---
+++ contributions/WARRABA/linux/System Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Detect your linux distribution
+Changing the timezone on deb based systems
+Monitor System Memory using `vmstat`
