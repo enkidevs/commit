f8f0a42cb0c68bdc4b02a48221c1ff4f19200b26
Qiik <qiik@live.ru>
2016-09-10 06:08:12 +0100

    2016-09-10's java workout

---
+++ contributions/Qiik/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
