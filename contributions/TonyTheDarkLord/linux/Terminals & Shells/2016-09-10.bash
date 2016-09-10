1c3a2f6f57b3db786d5e50d0faa4700c0288c8f8
TonyTheDarkLord <jua.log@outlook.com>
2016-09-10 15:25:40 +0100

    2016-09-10's linux workout

---
+++ contributions/TonyTheDarkLord/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
