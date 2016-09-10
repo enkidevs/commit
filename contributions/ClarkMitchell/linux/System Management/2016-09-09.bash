f99a108e976dfe037e8b3ce76b288a5020e81494
ClarkMitchell <clarkcsmitchell@gmail.com>
2016-09-10 01:30:52 +0100

    2016-09-09's linux workout

---
+++ contributions/ClarkMitchell/linux/System Management/2016-09-09.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Using `dd` To Create Disk Images
