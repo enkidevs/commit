81f43fdd6c32d8e3b112e1ab60c553469d0793ea
Descoladan <injpk23@gmail.com>
2016-09-10 02:51:39 +0100

    2016-09-09's java workout

---
+++ contributions/Descoladan/java/I/O/2016-09-09.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Using buffered streams
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
