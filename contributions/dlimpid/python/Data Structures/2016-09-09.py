6fd30512346eebd79a4d877a07d0fced5d1138e2
dlimpid <dlimpid@gmail.com>
2016-09-10 01:15:27 +0100

    2016-09-09's python workout

---
+++ contributions/dlimpid/python/Data Structures/2016-09-09.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Double ended queues with `deque`
+Best way to implement a simple `queue` 
+A thread-safe `Queue`
+Binary record packing with `bytearray`
