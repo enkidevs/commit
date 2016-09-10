6cf0ed9abac9103ea02af83925c1ef69f9d0908a
Farkh <farkhutdinov1398@gmail.com>
2016-09-10 08:36:50 +0100

    2016-09-10's git workout

---
+++ contributions/Farkh/git/Essentials/2016-09-10.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+What is stored in a commit?
+Search for commits by author
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
