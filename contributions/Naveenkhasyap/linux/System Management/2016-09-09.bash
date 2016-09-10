d2c128fec8d56d07c56ac455ce4ae3d577aceeeb
Naveenkhasyap <naveen.maltesh@gmail.com>
2016-09-10 18:34:49 +0100

    2016-09-09's linux workout

---
+++ contributions/Naveenkhasyap/linux/System Management/2016-09-09.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
