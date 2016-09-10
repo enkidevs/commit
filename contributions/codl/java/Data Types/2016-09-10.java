bf328582a6b20957af0dc816d5fb6c5084f031ad
codl <codl@codl.fr>
2016-09-10 14:04:37 +0100

    2016-09-10's java workout

---
+++ contributions/codl/java/Data Types/2016-09-10.java
@@ -0,0 +1,5 @@
+Two ways to use an `Iterator`
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Finding a substring in a string
+Retrieve the component type of an array
