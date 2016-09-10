56c4fd8d827e94cc766ae2650d05d1e656e7e6b5
Jebediah <jefwillems100@gmail.com>
2016-09-10 10:27:49 +0100

    2016-09-10's linux workout

---
+++ contributions/Jebediah/linux/File Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Working with `.gz` files
