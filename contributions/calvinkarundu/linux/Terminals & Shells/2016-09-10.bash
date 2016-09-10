257671f662d09fd1af001f17b9a1a8fc6006b23c
calvinkarundu <calvin.mwenda@gmail.com>
2016-09-10 22:18:14 +0100

    2016-09-10's linux workout

---
+++ contributions/calvinkarundu/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Find any Unix / Linux command
+Useful aliasing in bash
