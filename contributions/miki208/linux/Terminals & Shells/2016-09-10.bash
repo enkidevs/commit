2a8e337232227e31e211107a0d3a5b22894293b5
miki208 <miloss208@gmail.com>
2016-09-10 23:53:18 +0100

    2016-09-10's linux workout

---
+++ contributions/miki208/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,4 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
