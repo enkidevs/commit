4716d366547d0532d16c9a73df8e9f95e0267e47
Totof <christophe.fargeix@gmail.com>
2016-09-10 16:08:04 +0100

    2016-09-10's java workout

---
+++ contributions/Totof/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Always specify type parameter
+Do not return *references* to private *mutable* class members
