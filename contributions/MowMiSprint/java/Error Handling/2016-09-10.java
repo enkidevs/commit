d186ffd983b24c4ec5460240316f23688b1d07ce
MowMiSprint <lolface1998@gmail.com>
2016-09-10 14:49:24 +0100

    2016-09-10's java workout

---
+++ contributions/MowMiSprint/java/Error Handling/2016-09-10.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Use try-with-resources instead of `finally`
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
