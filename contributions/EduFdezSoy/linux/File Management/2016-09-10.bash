4bfd71b37c41d497fcb12aa835412a0eed8415b7
EduFdezSoy <eduardofernandezsoy@gmail.com>
2016-09-10 21:45:54 +0100

    2016-09-10's linux workout

---
+++ contributions/EduFdezSoy/linux/File Management/2016-09-10.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Easy way of sharing files
