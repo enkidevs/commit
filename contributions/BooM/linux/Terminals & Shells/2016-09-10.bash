e853c9d02fa11b119e637e583d56e5d61c9ec9b5
BooM <tararuj.michal@gmail.com>
2016-09-10 16:43:57 +0100

    2016-09-10's linux workout

---
+++ contributions/BooM/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
