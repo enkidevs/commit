bc3027cadc899cbb28d84f8d1a13de2b9e59f52d
Nearein <yuan@neuf.fr>
2016-09-10 11:33:22 +0100

    2016-09-10's java workout

---
+++ contributions/Nearein/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The trade-off between short and descriptive names
+Common mistake on switch statements
+Using Inheritance to reduce code repetition
+Do not perform bitwise and arithmetic operations on the same data
