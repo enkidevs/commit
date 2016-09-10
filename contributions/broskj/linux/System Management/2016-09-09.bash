c3c05f97069008c0e51418c6cb3340c0a6f2d718
broskj <kjbrost@gmail.com>
2016-09-10 03:28:50 +0100

    2016-09-09's linux workout

---
+++ contributions/broskj/linux/System Management/2016-09-09.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
