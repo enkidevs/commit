f117fe250e35d8d30ca1044249df108dac65bc5a
yuri-val <yuri.valigursky@gmail.com>
2016-09-10 20:53:28 +0100

    2016-09-10's linux workout

---
+++ contributions/yuri-val/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Clear bash history
+Random password generator
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
