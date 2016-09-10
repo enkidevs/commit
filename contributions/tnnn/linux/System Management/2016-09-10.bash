48b050e180c13fdc88e62715dbb0ffc3b6703dc2
tnnn <github@tnnn.eu>
2016-09-10 14:08:59 +0100

    2016-09-10's linux workout

---
+++ contributions/tnnn/linux/System Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
