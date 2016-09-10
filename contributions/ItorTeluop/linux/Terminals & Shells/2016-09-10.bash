b03b2f32472bdccc0b24754e5e9f2e55a9893410
ItorTeluop <gabstlaurent@gmail.com>
2016-09-10 23:53:25 +0100

    2016-09-10's linux workout

---
+++ contributions/ItorTeluop/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Terminal based browser
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
