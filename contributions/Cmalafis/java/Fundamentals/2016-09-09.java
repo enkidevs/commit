dcf78d1e0032140535d20d8f631bf6dff5b872ac
Cmalafis <cmalafis@gmail.com>
2016-09-10 05:21:18 +0100

    2016-09-09's java workout

---
+++ contributions/Cmalafis/java/Fundamentals/2016-09-09.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
