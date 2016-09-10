a25351752b251ce648548a88f6857e6d2ca5c2c3
luandaja <luandaja94@hotmail.com>
2016-09-10 02:18:36 +0100

    2016-09-09's javascript workout

---
+++ contributions/luandaja/javascript/Functions/2016-09-09.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function constructor vs. function declaration vs. function expression
+IIFE pattern
+Function call method
+Changing a functions context with `fn.call(object)`
