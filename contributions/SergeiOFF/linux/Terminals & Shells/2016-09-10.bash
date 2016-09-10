4632dc5a9e430937788aadbb8723488f9b96a45b
SergeiOFF <sergeioff57@gmail.com>
2016-09-10 07:21:56 +0100

    2016-09-10's linux workout

---
+++ contributions/SergeiOFF/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
+The Basics of Environment Variables
