8895a65bdd1bba7538b5909845e172b17d37b344
staralfur <hiyuga@gmail.com>
2016-09-10 12:46:36 +0100

    2016-09-10's java workout

---
+++ contributions/staralfur/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Java naming conventions
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Limit Accessibility of `Fields`
