cbd528842f7455e676d24048205206ad43a54a12
IceHusky <icehusky1337@yahoo.com>
2016-09-10 23:20:19 +0100

    2016-09-10's linux workout

---
+++ contributions/IceHusky/linux/System Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
