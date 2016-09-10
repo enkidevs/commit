15531d7658c409db61dd83d53b4d6f1ac0327047
maurom88 <mauro.meden@gmail.com>
2016-09-10 08:52:09 +0100

    2016-09-10's javascript workout

---
+++ contributions/maurom88/javascript/Core/2016-09-10.js
@@ -0,0 +1,5 @@
+Precision
+Using assignment operators
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Prevent background image flicker in **IE**
