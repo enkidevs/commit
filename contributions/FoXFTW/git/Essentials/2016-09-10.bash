3d58d16762ea003d0155e993a99d919a7d2520f8
FoXFTW <hannes.christian@gmail.com>
2016-09-11 00:50:20 +0100

    2016-09-10's git workout

---
+++ contributions/FoXFTW/git/Essentials/2016-09-10.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+What is stored in a commit?
+Make your log output pretty
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
