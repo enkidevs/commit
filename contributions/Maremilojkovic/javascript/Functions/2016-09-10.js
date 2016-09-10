34899efe3b966564538582bb894e3e81137a66f6
Maremilojkovic <maremilojkovic@gmail.com>
2016-09-10 08:51:23 +0100

    2016-09-10's javascript workout

---
+++ contributions/Maremilojkovic/javascript/Functions/2016-09-10.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
