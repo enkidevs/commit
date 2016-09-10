7d30863bc57d8d34f8e11010c38689dcf098cea7
MonteC <jarekucinski@gmail.com>
2016-09-10 23:18:16 +0100

    2016-09-10's java workout

---
+++ contributions/MonteC/java/Error Handling/2016-09-10.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Careful Numeric Data Conversions
