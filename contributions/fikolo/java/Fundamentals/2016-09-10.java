fb661d47c9980b77253304723928e57a79dff17b
fikolo <filipdalorenzo@gmail.com>
2016-09-10 08:37:46 +0100

    2016-09-10's java workout

---
+++ contributions/fikolo/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Always specify type parameter
+Increase `PermGen` space as to avoid `OutOfMemory` errors
