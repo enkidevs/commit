f04155985923259e0527e8b5f4e06fde722fb7ae
xbilek18 <honza.bilek4@gmail.com>
2016-09-10 17:56:34 +0100

    2016-09-10's linux workout

---
+++ contributions/xbilek18/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
