b70d77fcee83f0dafbb560dbe8ad8153654fdd8a
chrswndrs <christoph.wanders@gmail.com>
2016-09-10 12:32:10 +0100

    2016-09-10's linux workout

---
+++ contributions/chrswndrs/linux/System Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
