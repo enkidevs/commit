968003f4c17e14e82dccbf9f3603871c9b7f8fda
bofh <bofh-reg-enki@diegeekdie.com>
2016-09-10 13:13:26 +0100

    2016-09-10's java workout

---
+++ contributions/bofh/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
