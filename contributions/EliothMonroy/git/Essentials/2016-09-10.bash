b6f9ac97ed2b76f4546f884efd4b68c7e09e93bc
EliothMonroy <eliothmonroy@yahoo.com.mx>
2016-09-10 18:59:00 +0100

    2016-09-10's git workout

---
+++ contributions/EliothMonroy/git/Essentials/2016-09-10.bash
@@ -0,0 +1,5 @@
+Adding a remote repository
+How to set your username and email
+Search by commit message keyword
+Use `short` status to make output more compact
+Remote repositories: fetching and pushing
