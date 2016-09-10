8c8a6f36613c452459211cf16727c07e30327158
Marciyo <marcelmierzejewski@gmail.com>
2016-09-10 14:37:37 +0100

    2016-09-10's java workout

---
+++ contributions/Marciyo/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
