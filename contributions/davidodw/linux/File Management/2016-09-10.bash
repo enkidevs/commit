a8d1d388f08a2d91d8862347cf690ac6249ca27a
davidodw <dodwei.dev@gmail.com>
2016-09-11 17:24:25 +0100

    2016-09-10's linux workout

---
+++ contributions/davidodw/linux/File Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
+Delete commands aliases
+Easy way of sharing files
