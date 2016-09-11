d607a038dec0d76f75dd2240c8e56e4770c291f7
greenik <needik@gmail.com>
2016-09-11 21:11:04 +0100

    2016-09-11's linux workout

---
+++ contributions/greenik/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
