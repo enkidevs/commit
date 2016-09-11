8a676991f21112f9726c5a4a9eb70754c0d01b16
hdubel94 <dubelh1@tcnj.edu>
2016-09-11 16:49:42 +0100

    2016-09-11's linux workout

---
+++ contributions/hdubel94/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Execute a command without saving it in history
+Clear the terminal instantly
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
