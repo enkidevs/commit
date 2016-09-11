e59870cb237dd6a151727f1e0ff63ef5a41172c8
gjp523 <gjp523@gmail.com>
2016-09-11 22:46:38 +0100

    2016-09-11's linux workout

---
+++ contributions/gjp523/linux/System Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Check the HDD with `badblocks`
+Finding Open Files With `lsof`
