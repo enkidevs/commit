fe64494413b582e67240ea15e079dc76f64ab9c5
MrDziobak <wkrzychuw@gmail.com>
2016-09-11 09:17:35 +0100

    2016-09-11's javascript workout

---
+++ contributions/MrDziobak/javascript/Node/2016-09-11.js
@@ -0,0 +1,5 @@
+`uncaughtException` listener in Node.js
+Manage child processes in **Node**
+Bulk-Write in **Node** with `cork()`
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
