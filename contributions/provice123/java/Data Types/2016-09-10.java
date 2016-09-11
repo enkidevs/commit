20e8e5a1dab5fff4440813124664dd5f2c5a3c8a
provice123 <cancerean.pranav@gmail.com>
2016-09-11 21:57:12 +0100

    2016-09-10's java workout

---
+++ contributions/provice123/java/Data Types/2016-09-10.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Multidimensional array declaration
+Do not attempt comparisons with NaN
