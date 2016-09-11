e09746bb9cb1303244650a65950bf0a42f0e9d36
mbode <maxbode@gmail.com>
2016-09-11 09:50:49 +0100

    2016-09-11's java workout

---
+++ contributions/mbode/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
