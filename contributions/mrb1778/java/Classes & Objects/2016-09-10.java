4867953e1a867b85b74c60eaf261b135669878df
mrb1778 <mrb1778@hotmail.com>
2016-09-11 09:31:08 +0100

    2016-09-10's java workout

---
+++ contributions/mrb1778/java/Classes & Objects/2016-09-10.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Template methods in abstract classes
+Compare classes and not class names
+How and when to use `WeakHashMap` class
+Fields with Java **Reflection API**
