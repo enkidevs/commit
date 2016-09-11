d03edc78c59e85d6f4e462017975ee8b4781deb4
Yvanor <maxime.yusei@orange.fr>
2016-09-11 09:39:12 +0100

    2016-09-11's java workout

---
+++ contributions/Yvanor/java/Error Handling/2016-09-11.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Using exceptions in Java
