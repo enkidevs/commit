0c3765d70fd209415fdc1112b7af834661f2817a
psychorobotninja <tefuller@gmail.com>
2016-09-11 13:23:17 +0100

    2016-09-11's javascript workout

---
+++ contributions/psychorobotninja/javascript/Core/2016-09-11.js
@@ -0,0 +1,5 @@
+Performing a function at timed intervals
+Use `splice()` to remove an item from an array
+No block scope
+Extra function arguments are undefined by default
+Prevent background image flicker in **IE**
