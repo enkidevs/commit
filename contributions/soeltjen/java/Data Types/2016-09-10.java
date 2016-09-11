6e254b4c595c2305fbb220743b75eda7896a259a
soeltjen <dtinytop@hotmail.com>
2016-09-11 07:18:07 +0100

    2016-09-10's java workout

---
+++ contributions/soeltjen/java/Data Types/2016-09-10.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Converting a string to upper or lower case
+`StringBuffer` vs `StringBuilder`
