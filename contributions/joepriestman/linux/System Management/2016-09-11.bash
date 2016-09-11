91df5ca17a98c869a5ef5de53886d608ea8867a9
joepriestman <joe.priestman@googlemail.com>
2016-09-11 15:21:38 +0100

    2016-09-11's linux workout

---
+++ contributions/joepriestman/linux/System Management/2016-09-11.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
