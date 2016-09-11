b26be3b948619ddd5e5bb6a7565ef63b5ec9682a
w3bMaster <at.w3bmaster@gmail.com>
2016-09-11 07:12:12 +0100

    2016-09-11's linux workout

---
+++ contributions/w3bMaster/linux/System Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
