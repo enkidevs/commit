fa7801e42c06d3d7d3a200e00ba2f79f1012dd8b
timvojicic <tim.vojicic@gmail.com>
2016-09-11 13:10:02 +0100

    2016-09-11's linux workout

---
+++ contributions/timvojicic/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
