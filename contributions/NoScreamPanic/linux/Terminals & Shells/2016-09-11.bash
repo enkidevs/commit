af1e3d5dae56c46b218ba42d288117e1c5ef4c8f
NoScreamPanic <sow.comp@gmail.com>
2016-09-11 12:44:43 +0100

    2016-09-11's linux workout

---
+++ contributions/NoScreamPanic/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Random password generator
+Adding directories to your `$PATH`
+Retrieve previous arguments
