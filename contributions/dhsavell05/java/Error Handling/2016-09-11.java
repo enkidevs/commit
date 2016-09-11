239543de2c0a6cc7b305c7a928d90d8aefbedc52
dhsavell05 <dhsavell05@gmail.com>
2016-09-11 14:22:40 +0100

    2016-09-11's java workout

---
+++ contributions/dhsavell05/java/Error Handling/2016-09-11.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Most common reason behind **stack overflow** error
