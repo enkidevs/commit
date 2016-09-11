3a2f490d6fc821fa2a13b359d33fd662ad0ebfdf
Sanyo <micmark6@gmail.com>
2016-09-11 19:53:01 +0100

    2016-09-11's linux workout

---
+++ contributions/Sanyo/linux/System Management/2016-09-11.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
