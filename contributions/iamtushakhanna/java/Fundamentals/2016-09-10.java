18fc757985b7cc70d59197bd0704d3f627ef286a
iamtushakhanna <iamtusharkhanna@gmail.com>
2016-09-11 11:14:15 +0100

    2016-09-10's java workout

---
+++ contributions/iamtushakhanna/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Measuring time
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
