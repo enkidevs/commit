1b2f155e464bb68035503415c5d1b8f9ed3b90ba
Fre_d <frogkr@gmail.com>
2016-09-11 20:38:28 +0100

    2016-09-11's linux workout

---
+++ contributions/Fre_d/linux/File Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
