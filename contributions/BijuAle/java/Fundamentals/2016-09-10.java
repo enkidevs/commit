f2184b476fabc35b19170716f859fdead05089ae
BijuAle <bijuale@hotmail.com>
2016-09-11 03:46:00 +0100

    2016-09-10's java workout

---
+++ contributions/BijuAle/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Using `synchronized` statements
+How range operations work
