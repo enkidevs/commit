c76f919922254ff8575addef9c8ba4700716dba3
NiklasMerz <niklasmerz@gmx.net>
2016-09-11 11:13:18 +0100

    2016-09-11's linux workout

---
+++ contributions/NiklasMerz/linux/System Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Monitor System Memory using `vmstat`
+System uptime
