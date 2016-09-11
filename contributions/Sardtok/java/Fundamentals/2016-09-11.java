5c1b4f72d2079fb93cb5f8ff6979367768022c37
Sardtok <sha@computas.com>
2016-09-11 08:32:57 +0100

    2016-09-11's java workout

---
+++ contributions/Sardtok/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
