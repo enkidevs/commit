758733f93471ba2a8dfccbf4bb97f49bec4b214e
durandj <james.durand@alumni.msoe.edu>
2016-09-11 12:53:21 +0100

    2016-09-11's java workout

---
+++ contributions/durandj/java/Error Handling/2016-09-11.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Careful Numeric Data Conversions
