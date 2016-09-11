69e4bbfd7fa07645f88a806f9294ec658da11400
tcase360 <tmcase16@gmail.com>
2016-09-11 08:02:43 +0100

    2016-09-10's linux workout

---
+++ contributions/tcase360/linux/System Management/2016-09-10.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
