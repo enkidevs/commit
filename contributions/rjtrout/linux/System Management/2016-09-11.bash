921fe5bd5a5486db8fea380706d15fcaf18c69f6
rjtrout <kilgorejohntrout@gmail.com>
2016-09-11 20:28:01 +0100

    2016-09-11's linux workout

---
+++ contributions/rjtrout/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Commands to shutdown or restart the system
