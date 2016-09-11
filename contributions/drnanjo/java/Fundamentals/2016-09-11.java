e96f5b14397e9ab82ee856cda249eed184d3e9fe
drnanjo <drnanjo@aol.com>
2016-09-11 16:53:39 +0100

    2016-09-11's java workout

---
+++ contributions/drnanjo/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Inheriting a constructor from a superclass
+Calling one constructor from another
+Using `synchronized` statements
+Avoid using `static` variables
