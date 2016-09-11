53af49588576f1c0f9d64529894b39e668ebc7dc
amoghbanta <amoghbanta@gmail.com>
2016-09-11 10:21:29 +0100

    2016-09-10's linux workout

---
+++ contributions/amoghbanta/linux/System Management/2016-09-10.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
