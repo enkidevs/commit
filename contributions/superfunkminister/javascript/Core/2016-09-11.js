0876a5e8abdf6a068e85a5ff48c1740e62d67873
superfunkminister <hello@piers.io>
2016-09-11 21:46:23 +0100

    2016-09-11's javascript workout

---
+++ contributions/superfunkminister/javascript/Core/2016-09-11.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Performing a function at timed intervals
+Use `splice()` to remove an item from an array
+The difference between null, undefined and NaN
+Detect **DO NOT TRACK** status
