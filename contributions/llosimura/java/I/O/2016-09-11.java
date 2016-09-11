ae12e465e69bef7f24af9de61c022cb7f3a3547e
llosimura <alfonsoglezluis@gmail.com>
2016-09-11 19:47:42 +0100

    2016-09-11's java workout

---
+++ contributions/llosimura/java/I/O/2016-09-11.java
@@ -0,0 +1,5 @@
+Create system file paths using the `Path` class
+Using buffered streams
+Checking for the existence of files and directories
+Reading file attributes
+Listing a file system's root directories
