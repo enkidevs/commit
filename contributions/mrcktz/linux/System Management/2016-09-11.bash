3e693aa721904fc6cb9b8acad245c7e9f819a236
mrcktz <marco.kotz@googlemail.com>
2016-09-11 14:21:00 +0100

    2016-09-11's linux workout

---
+++ contributions/mrcktz/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
