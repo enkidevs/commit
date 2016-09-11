d0b2f1d808e68f0c4a6b3f6415992d545ec851b0
xazax <johan.daniel.nystrom@gmail.com>
2016-09-11 10:11:41 +0100

    2016-09-11's linux workout

---
+++ contributions/xazax/linux/File Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
