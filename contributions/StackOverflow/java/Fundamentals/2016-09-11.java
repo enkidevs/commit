e6855d2deaa7ee572df3777dbe6c6668a026ebf4
StackOverflow <nuti1996@gmail.com>
2016-09-11 22:20:22 +0100

    2016-09-11's java workout

---
+++ contributions/StackOverflow/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
