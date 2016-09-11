858e5d6c7a02619bfbb2efb8b1e6d627e22fbde3
ritwikraghav14 <ritwikraghav14@hotmail.com>
2016-09-11 04:21:16 +0100

    2016-09-10's java workout

---
+++ contributions/ritwikraghav14/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
