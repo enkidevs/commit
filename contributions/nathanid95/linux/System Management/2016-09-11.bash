b13a1de55a5025437a695ae1aee253936e150c47
nathanid95 <nathanid95@gmail.com>
2016-09-11 19:01:33 +0100

    2016-09-11's linux workout

---
+++ contributions/nathanid95/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
