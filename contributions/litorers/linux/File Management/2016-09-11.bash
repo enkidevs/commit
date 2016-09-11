6965c5fe68dd6c48ad70268af69bc7a89a7875fe
litorers <alexandrejoseph.tessier@gmail.com>
2016-09-11 20:48:01 +0100

    2016-09-11's linux workout

---
+++ contributions/litorers/linux/File Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Delete commands aliases
