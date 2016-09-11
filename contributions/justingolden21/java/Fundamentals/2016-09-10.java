fd5350356f79b49de1b3f20845edaeba0fd196b7
justingolden21 <justingolden21@gmail.com>
2016-09-11 01:10:22 +0100

    2016-09-10's java workout

---
+++ contributions/justingolden21/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+Avoid using `static` variables
