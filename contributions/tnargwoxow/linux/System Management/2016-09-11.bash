0858def1d8301d330854818b8c899b1f92cc6d66
tnargwoxow <tnargwoxow@gmail.com>
2016-09-11 08:12:49 +0100

    2016-09-11's linux workout

---
+++ contributions/tnargwoxow/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+System uptime
