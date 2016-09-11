3d7ca96269799097deca817b3cfc6c74d4a546c2
gerthe <julyasik.007@gmail.com>
2016-09-11 21:38:59 +0100

    2016-09-11's javascript workout

---
+++ contributions/gerthe/javascript/Functions/2016-09-11.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Anonymous functions
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
