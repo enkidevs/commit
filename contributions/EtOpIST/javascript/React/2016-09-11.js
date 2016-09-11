66da1b85c38620af20d8f4f3dbed151f5aa55ba7
EtOpIST <etopist@outlook.com>
2016-09-11 21:59:54 +0100

    2016-09-11's javascript workout

---
+++ contributions/EtOpIST/javascript/React/2016-09-11.js
@@ -0,0 +1,6 @@
+componentWillReceiveProps Not Triggered After Mounting
+
+Dangerously set `innerHTML`
+Immutability helpers in **React**
+Validate for required props
+Shortcut for transferring props
