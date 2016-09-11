d49f169bf020c0b4827bb2bb9e6dff0abee005bb
sandor.dargo <sandor.dargo@gmail.com>
2016-09-11 06:22:14 +0100

    2016-09-11's java workout

---
+++ contributions/sandor.dargo/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Difference between JRE and JDK?
+Inheriting a constructor from a superclass
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
