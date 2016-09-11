f0f27720cff315a1a8fdfa13dcc00b662ae10995
ipar9702 <ipar9702@uni.sydney.edu.au>
2016-09-11 13:21:57 +0100

    2016-09-10's java workout

---
+++ contributions/ipar9702/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use meaningful names
+Use strings in a `switch` statement
+Using Inheritance to reduce code repetition
+Calling one constructor from another
