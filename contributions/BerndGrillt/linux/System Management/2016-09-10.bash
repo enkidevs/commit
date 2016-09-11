d2f6687d5f0bf5bd6f63df8ed17d9b2bbabb280a
BerndGrillt <lennart.putz@gmail.com>
2016-09-11 01:27:25 +0100

    2016-09-10's linux workout

---
+++ contributions/BerndGrillt/linux/System Management/2016-09-10.bash
@@ -0,0 +1,7 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
