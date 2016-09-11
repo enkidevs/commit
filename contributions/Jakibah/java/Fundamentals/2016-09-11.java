962e05f73d0e97e25124c6a67f1c6bbf23c3021b
Jakibah <iakovospetrouentertainment@gmail.com>
2016-09-11 18:52:15 +0100

    2016-09-11's java workout

---
+++ contributions/Jakibah/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
