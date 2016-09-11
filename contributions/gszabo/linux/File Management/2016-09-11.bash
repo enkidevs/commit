388f77ca76894b2040510417b31163b64143cc72
gszabo <harry.bme@gmail.com>
2016-09-11 07:51:22 +0100

    2016-09-11's linux workout

---
+++ contributions/gszabo/linux/File Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Easy way of sharing files
