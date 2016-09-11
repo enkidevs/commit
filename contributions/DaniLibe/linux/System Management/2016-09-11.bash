83917172962349ced395fb70a421d54c1fe252e6
DaniLibe <danylibedev@gmail.com>
2016-09-11 11:08:12 +0100

    2016-09-11's linux workout

---
+++ contributions/DaniLibe/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Finding Open Files With `lsof`
