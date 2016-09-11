ea4e9742119473fab887bebad55e265982c4cc5c
neevalex <neevalex@gmail.com>
2016-09-11 09:38:09 +0100

    2016-09-11's linux workout

---
+++ contributions/neevalex/linux/File Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
