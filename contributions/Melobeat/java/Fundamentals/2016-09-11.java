f476f8dea18c5254159da67368b3829ed06c156b
Melobeat <kaikronenberg25@gmail.com>
2016-09-11 18:33:00 +0100

    2016-09-11's java workout

---
+++ contributions/Melobeat/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Using Inheritance to reduce code repetition
+Inheriting a constructor from a superclass
+There is no such thing as *pass-by-reference* in Java
+How range operations work
+Using an interface as a parameter
