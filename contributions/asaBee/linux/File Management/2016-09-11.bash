cb542abc5505b9b296e56c56493f43be6e80f308
asaBee <hlovers.forever@gmail.com>
2016-09-11 23:25:16 +0100

    2016-09-11's linux workout

---
+++ contributions/asaBee/linux/File Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
