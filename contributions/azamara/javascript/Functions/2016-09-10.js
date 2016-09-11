ab6a2afae6fe065b1affc4cae693ecc1d05df001
azamara <azamara@me.com>
2016-09-11 18:15:05 +0100

    2016-09-10's javascript workout

---
+++ contributions/azamara/javascript/Functions/2016-09-10.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Easily generate a random `HEX` color
