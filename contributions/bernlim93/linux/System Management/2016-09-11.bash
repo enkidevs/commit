c222632d49b32f9ebf9484d97f8d9d6fb80759cc
bernlim93 <me@bernlim.com>
2016-09-11 22:40:49 +0100

    2016-09-11's linux workout

---
+++ contributions/bernlim93/linux/System Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Commands to shutdown or restart the system
