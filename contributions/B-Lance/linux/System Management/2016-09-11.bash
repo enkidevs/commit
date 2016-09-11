cccf1cf4ebf3386e960d5311f8663d85f5789629
B-Lance <grzegorztukin@gmail.com>
2016-09-12 00:28:01 +0100

    2016-09-11's linux workout

---
+++ contributions/B-Lance/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
