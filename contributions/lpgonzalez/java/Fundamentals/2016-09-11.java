11a7912742e6f92bf92d44e7821e987414b2e88d
lpgonzalez <lisardo.prieto.gonzalez@gmail.com>
2016-09-11 04:03:24 +0100

    2016-09-11's java workout

---
+++ contributions/lpgonzalez/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Common mistake on switch statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
