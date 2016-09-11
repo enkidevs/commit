cc049e2f507f7220b4753401551a3f812589064f
afduarte <a.fduarte1@gmail.com>
2016-09-11 09:43:59 +0100

    2016-09-11's linux workout

---
+++ contributions/afduarte/linux/System Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
