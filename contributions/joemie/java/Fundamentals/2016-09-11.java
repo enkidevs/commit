b63fe1a9f85262f4578c54322ce67c59dc4aaf15
joemie <joemie92@gmail.com>
2016-09-11 13:20:52 +0100

    2016-09-11's java workout

---
+++ contributions/joemie/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Using bounded type parameters in generic methods
+Avoid using `static` variables
+Supply `toString()` in all classes
