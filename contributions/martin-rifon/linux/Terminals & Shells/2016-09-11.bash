523f914113c012a2e6ba489e95fa3b2c9dfd5575
martin-rifon <mrifonelizalde@gmail.com>
2016-09-11 14:37:48 +0100

    2016-09-11's linux workout

---
+++ contributions/martin-rifon/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
