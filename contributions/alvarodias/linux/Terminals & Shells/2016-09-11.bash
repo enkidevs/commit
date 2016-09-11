b2a12d912584b3046a856346846f745e96ae412b
alvarodias <alvaro.j.dias@gmail.com>
2016-09-11 22:08:52 +0100

    2016-09-11's linux workout

---
+++ contributions/alvarodias/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Clear the terminal instantly
+Find any Unix / Linux command
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
