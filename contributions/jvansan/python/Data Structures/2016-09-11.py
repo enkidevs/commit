5e528bcbcbc5440acdd5f7e9b89e1f48d1616bba
jvansan <jeff.van.santen56@gmail.com>
2016-09-11 18:09:11 +0100

    2016-09-11's python workout

---
+++ contributions/jvansan/python/Data Structures/2016-09-11.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`Dictionary` - standard mapping type
+The fundamental `tuples` 
+Working with `set`s
+Operations with `bytes` and `bytearray`
