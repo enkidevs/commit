4e074de197e0c074cbec1e558b18cf5bd4b8616e
AavinashhK <atechkid2011@gmail.com>
2016-09-11 02:31:00 +0100

    2016-09-10's linux workout

---
+++ contributions/AavinashhK/linux/File Management/2016-09-10.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Deleting non-empty directories
+Working with `.gz` files
