812a2b951696b59417a4a978d91f6f5a16d05726
Hellonearthis <hellonearthisman@gmail.com>
2016-09-11 09:47:13 +0100

    2016-09-10's javascript workout

---
+++ contributions/Hellonearthis/javascript/Node/2016-09-10.js
@@ -0,0 +1,5 @@
+Wrapping errors in Node.js using _node-verror_
+`process.nextTick()` callback pattern
+Bulk-Write in **Node** with `cork()`
+Avoid command injection 
+Avoid using `sudo` when running Node
