a19c5d126b3c9167423380c2f2e2f6f6e09e68a6
oliveirapaulo <paulo.oliveira@gmail.com>
2016-09-11 06:22:53 +0100

    2016-09-10's linux workout

---
+++ contributions/oliveirapaulo/linux/System Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Basic service management with `systemd`
