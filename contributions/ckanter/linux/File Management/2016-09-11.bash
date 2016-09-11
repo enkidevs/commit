2dbf34894760a1369c1782e4855f893383f496f8
ckanter <ckanter@wisc.edu>
2016-09-11 18:07:31 +0100

    2016-09-11's linux workout

---
+++ contributions/ckanter/linux/File Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
