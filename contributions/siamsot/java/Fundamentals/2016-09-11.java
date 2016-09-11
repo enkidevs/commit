0ca34b940c46a08beb68a93df6aa376cbc551488
siamsot <siamsot@gmail.com>
2016-09-11 09:23:21 +0100

    2016-09-11's java workout

---
+++ contributions/siamsot/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Common mistake on switch statements
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
