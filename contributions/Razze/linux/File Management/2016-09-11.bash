2756109dbfa1c8384120cbd9d8e83b0c6ffaa71d
Razze <razzeee@gmail.com>
2016-09-11 08:11:56 +0100

    2016-09-11's linux workout

---
+++ contributions/Razze/linux/File Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
