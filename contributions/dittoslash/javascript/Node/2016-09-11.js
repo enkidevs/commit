8d795b43885a8055bbf79f6b91a43fc56aa59a4e
dittoslash <dittoslash@gmail.com>
2016-09-11 07:34:16 +0100

    2016-09-11's javascript workout

---
+++ contributions/dittoslash/javascript/Node/2016-09-11.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+How to debug Node.js
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
+Automatic compilation for Node with **Nodemon** 
