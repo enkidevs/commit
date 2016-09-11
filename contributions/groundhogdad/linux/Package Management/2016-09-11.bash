2b5d596b5616dffb3a69109677f184f3a52a5708
groundhogdad <groundhogdad@gmail.com>
2016-09-11 06:30:30 +0100

    2016-09-11's linux workout

---
+++ contributions/groundhogdad/linux/Package Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Installing a `.deb` package from the terminal
+Using `Tasksel` for software installation
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
