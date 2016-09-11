6fa7b7e2fb754d75a54cf79eb8f49b733a8d5cd3
ericnerdo <erdonsama@gmail.com>
2016-09-11 12:17:01 +0100

    2016-09-11's java workout

---
+++ contributions/ericnerdo/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
