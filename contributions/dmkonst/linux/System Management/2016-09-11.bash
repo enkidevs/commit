db213abe992dedea7d40426631d8c3e9401a1a6a
dmkonst <warqueq@gmail.com>
2016-09-11 05:51:17 +0100

    2016-09-11's linux workout

---
+++ contributions/dmkonst/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Check the HDD with `badblocks`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
