ca6c2ed7a69bef44949ca5b221fb4da2a9aafc49
tomaspinho <me@tomaspinho.com>
2016-09-11 14:05:02 +0100

    2016-09-11's java workout

---
+++ contributions/tomaspinho/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Measuring time
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
