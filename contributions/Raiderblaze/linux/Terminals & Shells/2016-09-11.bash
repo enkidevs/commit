704864003ea2d50e05401312b69ff29738414284
Raiderblaze <raiderblaze@gmail.com>
2016-09-11 16:21:41 +0100

    2016-09-11's linux workout

---
+++ contributions/Raiderblaze/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Find any Unix / Linux command
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
