6d5c8117dbfd4c54bee418b6f046b30fb2475169
Danidin <danny.shefer@gmail.com>
2016-09-11 20:53:33 +0100

    2016-09-11's java workout

---
+++ contributions/Danidin/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Measuring time
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
