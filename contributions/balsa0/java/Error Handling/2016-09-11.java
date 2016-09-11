9559ae9ee84be64495032e41622001e371d785e9
balsa0 <ktibalazs@gmail.com>
2016-09-11 21:47:08 +0100

    2016-09-11's java workout

---
+++ contributions/balsa0/java/Error Handling/2016-09-11.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Creating new Exceptions
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
