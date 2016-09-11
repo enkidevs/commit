c7aaebeb9576285bccf7b3c9b3a1e9edccfc9f48
WandersonAlves <wandersonalvesferreira2012@gmail.com>
2016-09-11 05:56:46 +0100

    2016-09-10's linux workout

---
+++ contributions/WandersonAlves/linux/Terminals & Shells/2016-09-10.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Terminal based browser
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
