21b3e225fb23d97ca64d981cd14460d08f4a34bc
Themacprod <mathieu.albrespy@gmail.com>
2016-09-11 04:33:51 +0100

    2016-09-10's linux workout

---
+++ contributions/Themacprod/linux/System Management/2016-09-10.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Using `dd` To Create Disk Images
