b4bfef3c087867dec15b7f6d232e9233a342edd0
b0uill0n <ilia.kerzhner@gmail.com>
2016-09-11 09:43:39 +0100

    2016-09-11's linux workout

---
+++ contributions/b0uill0n/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
