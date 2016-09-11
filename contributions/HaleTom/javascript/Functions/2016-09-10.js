34abbba192814789f8eb22d875609e6ed041ba97
HaleTom <tom@hale.ee>
2016-09-11 17:33:28 +0100

    2016-09-10's javascript workout

---
+++ contributions/HaleTom/javascript/Functions/2016-09-10.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Get query/url variables
+Move cursor at the end of text input
+Check if a document is done loading
