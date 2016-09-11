7354553ef6ffba7ae7aa7c718d9a8d1ecf0266b8
Aerial94 <alekb1@gmail.com>
2016-09-11 19:17:00 +0100

    2016-09-11's javascript workout

---
+++ contributions/Aerial94/javascript/Core/2016-09-11.js
@@ -0,0 +1,5 @@
+Keywords in hash keys
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+Extra function arguments are undefined by default
