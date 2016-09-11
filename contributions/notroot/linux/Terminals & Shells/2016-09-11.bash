e7a76436a3b9081b40ee05fddf3f498cf14100fb
notroot <notroot@exler.info>
2016-09-11 12:12:27 +0100

    2016-09-11's linux workout

---
+++ contributions/notroot/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Quick `bash` shortcuts 
+Useful aliasing in bash
