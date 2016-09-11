f86a6546406df1b257e9403d085e49802c1fd01a
Awlex <wieseralex580@gmail.com>
2016-09-11 02:04:09 +0100

    2016-09-10's java workout

---
+++ contributions/Awlex/java/I/O/2016-09-10.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Using buffered streams
+Checking for the existence of files and directories
+Using the `OpenOption...` vararg to define file writing behavior
