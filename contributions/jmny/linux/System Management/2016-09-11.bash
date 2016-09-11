7660ff927bd380270a79b3f70f1b679353c93701
jmny <louis@jmny.eu>
2016-09-11 20:00:14 +0100

    2016-09-11's linux workout

---
+++ contributions/jmny/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
