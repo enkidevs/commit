4475980a7dcb1f6e32070260bae0a02eb3bf9aea
LuisPalomo <luispalomosiv@gmail.com>
2016-09-11 11:32:46 +0100

    2016-09-11's javascript workout

---
+++ contributions/LuisPalomo/javascript/Npm/2016-09-11.js
@@ -0,0 +1,5 @@
+Passing through command line arguments in npm scripts
+List binaries for scripting in npm
+Tab completion in `npm`
+Package distribution tags
+Lock down dependency versions by shrinkwrapping
