c1db04c39573a1859f7d41a373ae607163e58c7e
e7krgc <enam93@gmail.com>
2016-09-11 10:16:50 +0100

    2016-09-11's linux workout

---
+++ contributions/e7krgc/linux/File Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Working with `.gz` files
+Find the unknown process preventing deleting of files
