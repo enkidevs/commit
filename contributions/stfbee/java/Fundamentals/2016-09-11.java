6ab767b534be05973afb7d0b7f790ac9ac54d70f
stfbee <pinkey.950@gmail.com>
2016-09-11 20:59:22 +0100

    2016-09-11's java workout

---
+++ contributions/stfbee/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
