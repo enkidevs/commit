a74a7a51f4dc205eabd6b1de5e2206d9d8957abe
pini <pinhasph@gmail.com>
2016-09-11 08:08:30 +0100

    2016-09-11's linux workout

---
+++ contributions/pini/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Execute a command without saving it in history
+Terminal based browser
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
