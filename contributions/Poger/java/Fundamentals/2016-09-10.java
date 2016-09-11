449d54e6c849daebbffdb3bd812392983c7189b0
Poger <gkonupek@gmail.com>
2016-09-11 04:15:11 +0100

    2016-09-10's java workout

---
+++ contributions/Poger/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Always specify type parameter
+Avoid using `static` variables
