38d68351b606fbd27ece1bbdfa5a126c4018ac9b
Tusk <tuskone16@gmail.com>
2016-09-11 22:12:40 +0100

    2016-09-11's javascript workout

---
+++ contributions/Tusk/javascript/Core/2016-09-11.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Treating a boolean as number
+`String.replace`
+Infix operators are left-associative
+Hoisting applies only to variable declarations, not initializations
