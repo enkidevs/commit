000db21d1e4c3f73e1a35697932d2dd4a5acf597
stan.cdenisa <stan.cdenisa@gmail.com>
2016-09-11 11:14:41 +0100

    2016-09-11's java workout

---
+++ contributions/stan.cdenisa/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
