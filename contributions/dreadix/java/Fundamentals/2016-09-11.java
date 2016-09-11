45c54d2316f1b3f1b867eeb7ff83443ac39001b9
dreadix <evanspe@gmail.com>
2016-09-11 14:49:58 +0100

    2016-09-11's java workout

---
+++ contributions/dreadix/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
