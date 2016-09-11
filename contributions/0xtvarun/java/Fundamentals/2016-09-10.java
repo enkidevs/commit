1c9fa70063dacead1ae16d2b1ef15eea41c97a92
0xtvarun <vtewari26@gmail.com>
2016-09-11 09:13:47 +0100

    2016-09-10's java workout

---
+++ contributions/0xtvarun/java/Fundamentals/2016-09-10.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use `DecimalFormat` class to format numbers
+Always specify type parameter
+Avoid using `static` variables
+Supply `toString()` in all classes
