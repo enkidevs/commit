00d175770ca0a7b2c6bf24df1c691b4e17959002
Noahas <noahas111@gmail.com>
2016-09-11 19:45:03 +0100

    2016-09-11's java workout

---
+++ contributions/Noahas/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Cropping images
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Increase `PermGen` space as to avoid `OutOfMemory` errors
