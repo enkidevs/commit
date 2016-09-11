309b70caf3cd15d58991ddf6eb2d7696e89b18e5
sassy_samurai <srikant2014@gmail.com>
2016-09-11 18:56:00 +0100

    2016-09-10's linux workout

---
+++ contributions/sassy_samurai/linux/Package Management/2016-09-10.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+List installed packages
+Installing a `.deb` package from the terminal
+Solving `BADSIG GPG` errors
