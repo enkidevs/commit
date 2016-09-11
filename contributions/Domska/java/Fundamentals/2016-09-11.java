ab023a6af3488de00992ef7907994ad8ad1b2144
Domska <mrwheezkas@gmail.com>
2016-09-11 23:05:38 +0100

    2016-09-11's java workout

---
+++ contributions/Domska/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Implementing an `interface`
