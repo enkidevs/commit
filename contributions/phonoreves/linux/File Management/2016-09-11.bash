9f3aef94be76bd2b5a5cff70a3d314414b753341
phonoreves <joseph.verron@gmail.com>
2016-09-11 22:00:19 +0100

    2016-09-11's linux workout

---
+++ contributions/phonoreves/linux/File Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Deleting non-empty directories
