5e9419a07161b2923085b90d3daf6876e9fb5218
samcarlin <sservicebot@gmail.com>
2016-09-12 00:48:57 +0100

    2016-09-11's java workout

---
+++ contributions/samcarlin/java/Data Types/2016-09-11.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Finding a substring in a string
+Numeric Conversion - Widening
