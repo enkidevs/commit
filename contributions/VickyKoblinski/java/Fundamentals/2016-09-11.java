4cb9cc2d59040e5f2106263d8f418d4908be94fe
VickyKoblinski <safimoyo@gmail.com>
2016-09-11 16:49:45 +0100

    2016-09-11's java workout

---
+++ contributions/VickyKoblinski/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Cropping images
+Altering format string output by changing a format specifier's `argument_index`
+Do not return *references* to private *mutable* class members
