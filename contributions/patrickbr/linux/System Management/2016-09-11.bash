3706e3f2feb747cef02804d3cf8b86a2407c33a2
patrickbr <spelbrekerpatrick@gmail.com>
2016-09-11 08:14:32 +0100

    2016-09-11's linux workout

---
+++ contributions/patrickbr/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
