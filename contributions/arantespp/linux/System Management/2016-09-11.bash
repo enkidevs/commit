774187aa7f270537349049af5c85b30220620117
arantespp <arantespp@gmail.com>
2016-09-11 14:40:13 +0100

    2016-09-11's linux workout

---
+++ contributions/arantespp/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Using `dd` To Create Disk Images
