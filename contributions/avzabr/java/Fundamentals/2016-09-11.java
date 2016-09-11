f813332634678b52b9190936de0073658fc27684
avzabr <avzabr@gmail.com>
2016-09-11 19:08:13 +0100

    2016-09-11's java workout

---
+++ contributions/avzabr/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: setting a file's owner
+Measuring time
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
