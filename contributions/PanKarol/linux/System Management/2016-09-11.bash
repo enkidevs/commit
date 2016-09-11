ee64826e2571ac483f0236a576dce075f6e1c271
PanKarol <kamusur@gmail.com>
2016-09-11 17:45:12 +0100

    2016-09-11's linux workout

---
+++ contributions/PanKarol/linux/System Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Common log files
+Preventing a user from logging into the system
+Basic service management with `systemd`
