95d03dec792205dc4e46c0a3b5bfba1a9d646f8d
MrCrowned <dom.brause@gmail.com>
2016-09-11 13:57:02 +0100

    2016-09-11's linux workout

---
+++ contributions/MrCrowned/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,7 @@
+Terminal based browser
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
