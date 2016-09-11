2686e77be66eadc44341cffd380bf02925ea0084
john_li <john_li_20001216@yahoo.com>
2016-09-11 21:25:34 +0100

    2016-09-11's linux workout

---
+++ contributions/john_li/linux/System Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
