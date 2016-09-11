e94468ffdddc1be088060e31b4967c929dcc0436
luxar <lucasmecanicapando@gmail.com>
2016-09-11 20:02:54 +0100

    2016-09-11's git workout

---
+++ contributions/luxar/git/Essentials/2016-09-11.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+Adding a remote repository
+What is stored in a commit?
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
