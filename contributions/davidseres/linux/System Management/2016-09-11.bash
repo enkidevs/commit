c4cf18aeeacc5d154514ceae70a08792b2b22fa2
davidseres <davidseres@gmail.com>
2016-09-11 17:51:28 +0100

    2016-09-11's linux workout

---
+++ contributions/davidseres/linux/System Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
