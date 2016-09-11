e056fe4a5c1aca78f3d2ce289d61fc0fbd8b34f9
elvispt <elvispt@gmail.com>
2016-09-11 01:54:27 +0100

    2016-09-10's linux workout

---
+++ contributions/elvispt/linux/System Management/2016-09-10.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
