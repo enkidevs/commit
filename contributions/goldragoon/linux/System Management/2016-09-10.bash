d2b41de764fb2dcde7e35e72e4c3bc5aead65aee
goldragoon <paganinist@gmail.com>
2016-09-11 03:42:48 +0100

    2016-09-10's linux workout

---
+++ contributions/goldragoon/linux/System Management/2016-09-10.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
