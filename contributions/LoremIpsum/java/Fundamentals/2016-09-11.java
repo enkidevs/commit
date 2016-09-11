10b5ae4de20e2fca912df95ca5e726784560c3ef
LoremIpsum <yannick.jadoul@gmail.com>
2016-09-11 16:12:34 +0100

    2016-09-11's java workout

---
+++ contributions/LoremIpsum/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Cropping images
+Uses of the `final` keyword
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
