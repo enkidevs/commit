51d62d965aa3693c2f9d5e2842db324e8069c245
AlanZhuTheAsian <alanyzhu@gmail.com>
2016-09-12 00:51:18 +0100

    2016-09-11's java workout

---
+++ contributions/AlanZhuTheAsian/java/Data Types/2016-09-11.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Multidimensional array declaration
+Double brace initialization
+Retrieve the component type of an array
