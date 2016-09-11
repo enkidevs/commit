6c9a684856b3ea641ee286b56d71470815454390
T-bond <tbondvagyok@gmail.com>
2016-09-11 21:49:34 +0100

    2016-09-11's java workout

---
+++ contributions/T-bond/java/Error Handling/2016-09-11.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
