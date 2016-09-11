560c37b5c73948cef655c065be1ebb512269ce95
Zanex <alessio.zanella88@gmail.com>
2016-09-11 12:10:30 +0100

    2016-09-11's javascript workout

---
+++ contributions/Zanex/javascript/Npm/2016-09-11.js
@@ -0,0 +1,5 @@
+Manipulate npm packages cache
+npm can handle multiple versions of a dependency
+Manage local node modules with `npm link`
+`npm`  as an alternative to Gulp
+devDependencies in `npm`
