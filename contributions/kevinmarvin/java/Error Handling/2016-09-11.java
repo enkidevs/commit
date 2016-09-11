30f433ebb508650f5520ef1301e567df5f99a07c
kevinmarvin <kevin.marvin@gmail.com>
2016-09-11 13:35:33 +0100

    2016-09-11's java workout

---
+++ contributions/kevinmarvin/java/Error Handling/2016-09-11.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Using exceptions in Java
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
