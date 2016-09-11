48f6b13018646c2c74cff2ea58effe26cf8d813a
brerx <th.mueller87@gmail.com>
2016-09-11 17:52:02 +0100

    2016-09-11's linux workout

---
+++ contributions/brerx/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
