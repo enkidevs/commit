952fe8629c0185fde7699144d995b64900289073
eoin-obrien <eoinobrien910@gmail.com>
2016-09-11 13:19:54 +0100

    2016-09-11's linux workout

---
+++ contributions/eoin-obrien/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,5 @@
+Quick `bash` shortcuts 
+Random password generator
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
