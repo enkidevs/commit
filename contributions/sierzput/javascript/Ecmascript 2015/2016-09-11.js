88d1dff2209b96780524bb4a0a30bc9c0718c271
sierzput <sierzput@gmail.com>
2016-09-11 06:15:04 +0100

    2016-09-11's javascript workout

---
+++ contributions/sierzput/javascript/Ecmascript 2015/2016-09-11.js
@@ -0,0 +1,6 @@
+Computed property names in object literals
+Defaults values apply only to `undefined` (and not to `null`)
+Internationalization & Localization
+
+ES6 `Number` methods
+New methods in `Math`
