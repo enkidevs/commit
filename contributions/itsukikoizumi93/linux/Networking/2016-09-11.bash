69e15fe60b0b0d2dccbbb2618ed47cd80bc7abbd
itsukikoizumi93 <itsukikoizumi93@gmail.com>
2016-09-11 22:17:52 +0100

    2016-09-11's linux workout

---
+++ contributions/itsukikoizumi93/linux/Networking/2016-09-11.bash
@@ -0,0 +1,5 @@
+Multiple IP Address with aliasing 
+Useful ssh client optimizations 
+Getting the connection speed from the terminal
+Bandwidth monitoring tools 
+Staying connected remotely via SSH without an interactive shell
