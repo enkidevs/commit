c46e06248142950ea3ebe1856064261dd01c6352
keicus <charlestdjames1@gmail.com>
2016-09-11 17:21:44 +0100

    2016-09-11's java workout

---
+++ contributions/keicus/java/Error Handling/2016-09-11.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
