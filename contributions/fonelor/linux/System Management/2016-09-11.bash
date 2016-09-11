0eb888127246477ce00ba943f4ea6fd2e9f7a870
fonelor <firstrolenof@yandex.ru>
2016-09-11 21:20:29 +0100

    2016-09-11's linux workout

---
+++ contributions/fonelor/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
