e774b3d474ba43882b95eadbf7bd8bcc05cc9ab7
yjose <yjosejose6@gmail.com>
2016-09-11 16:55:01 +0100

    2016-09-11's javascript workout

---
+++ contributions/yjose/javascript/Functions/2016-09-11.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Easily generate a random `HEX` color
+Check if a document is done loading
