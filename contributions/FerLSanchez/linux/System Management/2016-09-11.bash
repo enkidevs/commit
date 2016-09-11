c3ba344412fbffcf53de27dc8f29d9b120266fe1
FerLSanchez <fernando.l.sanchez@gmail.com>
2016-09-11 14:23:31 +0100

    2016-09-11's linux workout

---
+++ contributions/FerLSanchez/linux/System Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
