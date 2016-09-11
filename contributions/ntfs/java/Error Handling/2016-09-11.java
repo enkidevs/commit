7c6ef0e7404064f3a17f8f0f8f539883324e8fb5
ntfs <ntfs2002@gmail.com>
2016-09-11 20:12:11 +0100

    2016-09-11's java workout

---
+++ contributions/ntfs/java/Error Handling/2016-09-11.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Checking a double for `NaN` value
