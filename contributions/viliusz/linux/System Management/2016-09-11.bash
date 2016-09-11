3469325ee0f8e5c58e5315a8289479e6532aff0f
viliusz <enki@gmn.lt>
2016-09-11 22:15:05 +0100

    2016-09-11's linux workout

---
+++ contributions/viliusz/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
