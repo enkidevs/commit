b20b80e7449bdcf370de5da43a93a6c21de1af4f
Eowyn <ariel.bridgeman@gmail.com>
2016-09-11 22:41:46 +0100

    2016-09-11's linux workout

---
+++ contributions/Eowyn/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+The Basics of Environment Variables
