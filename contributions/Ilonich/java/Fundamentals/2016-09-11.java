5260f013e158ade181376c02924eb88f6f4e6ca6
Ilonich <niko-ikko@yandex.ru>
2016-09-12 00:38:03 +0100

    2016-09-11's java workout

---
+++ contributions/Ilonich/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Avoid using `static` variables
