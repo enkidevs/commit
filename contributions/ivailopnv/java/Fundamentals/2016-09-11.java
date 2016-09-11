f08dc7a20073e460b6c2cf333b79413657a14ad3
ivailopnv <ivailopnv@yahoo.com>
2016-09-11 13:16:24 +0100

    2016-09-11's java workout

---
+++ contributions/ivailopnv/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Measuring time
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
