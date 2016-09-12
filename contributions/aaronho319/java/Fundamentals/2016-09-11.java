eff06b47f5b946ccfe5a2ff6422417bba6b8ca1e
aaronho319 <aaronstevenson.ho@gmail.com>
2016-09-12 04:24:49 +0100

    2016-09-11's java workout

---
+++ contributions/aaronho319/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
