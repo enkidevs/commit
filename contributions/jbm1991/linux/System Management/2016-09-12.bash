aadb30f31ea756a12b58e4702c175c053d2000cc
jbm1991 <jbm1991@gmail.com>
2016-09-12 21:26:15 +0100

    2016-09-12's linux workout

---
+++ contributions/jbm1991/linux/System Management/2016-09-12.bash
@@ -0,0 +1,7 @@
+Revision control of configuration files with git
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
