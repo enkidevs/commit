5ca1dfbc09ebb719e1e7771dc89544d4d91cef28
r-hryniewski <racibor@gmail.com>
2016-09-12 14:51:11 +0100

    2016-09-12's javascript workout

---
+++ contributions/r-hryniewski/javascript/Core/2016-09-12.js
@@ -0,0 +1,5 @@
+Setting default values with `short circuiting`
+Using assignment operators
+Hoisting applies only to variable declarations, not initializations
+Prototype methods
+How to merge two arrays
