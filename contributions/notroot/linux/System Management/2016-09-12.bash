b5e6ce7f5bd3dc44e2eb549ec3b174155ea55d5e
notroot <notroot@exler.info>
2016-09-12 19:26:10 +0100

    2016-09-12's linux workout

---
+++ contributions/notroot/linux/System Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Detect your linux distribution
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
