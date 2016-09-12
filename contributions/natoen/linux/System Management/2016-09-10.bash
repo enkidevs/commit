a872781c31c54ffe7fefae2712032313785ef134
natoen <hgmatsuda@gmail.com>
2016-09-12 17:06:54 +0100

    2016-09-10's linux workout

---
+++ contributions/natoen/linux/System Management/2016-09-10.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
