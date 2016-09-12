edbb00f9e21705d2e3187b530c4ff18972dd5218
Fenetic <esyouit@gmail.com>
2016-09-12 19:05:28 +0100

    2016-09-12's java workout

---
+++ contributions/Fenetic/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Common mistake on switch statements
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
