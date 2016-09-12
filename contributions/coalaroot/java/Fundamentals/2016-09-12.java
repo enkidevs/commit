f50080f7d0569db91f8508ceed56a7abb25f355f
coalaroot <jakubkrolak94@gmail.com>
2016-09-12 21:57:04 +0100

    2016-09-12's java workout

---
+++ contributions/coalaroot/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
+Using an interface as a parameter
