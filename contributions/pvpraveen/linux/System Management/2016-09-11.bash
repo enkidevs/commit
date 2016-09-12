682f009543babec68547d9743f98da2bdfee78cc
pvpraveen <venkatapraveen13@gmail.com>
2016-09-12 14:22:11 +0100

    2016-09-11's linux workout

---
+++ contributions/pvpraveen/linux/System Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Commands to shutdown or restart the system
+Finding Open Files With `lsof`
