37930f1c8cb6d117baa64b3031e99a928127ef07
CapnSammeh <samherring111@gmail.com>
2016-09-12 13:07:12 +0100

    2016-09-11's linux workout

---
+++ contributions/CapnSammeh/linux/System Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
