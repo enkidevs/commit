fa1c18504bf3454fba616cba85ebf8bde46344bd
chrishopkins53 <chrishopkins53@gmail.com>
2016-09-12 07:10:06 +0100

    2016-09-12's linux workout

---
+++ contributions/chrishopkins53/linux/System Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
