6dc39d8f23c238bf83f69b4d6a93bd6cae67dc38
bdflemin <bryanfleming@gmail.com>
2016-09-12 23:42:56 +0100

    2016-09-12's linux workout

---
+++ contributions/bdflemin/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
