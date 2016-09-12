86a2744b3662bf83cedeb30695bcf479b1ac465c
gstavrinos <stavrinosgeo@gmail.com>
2016-09-12 17:57:41 +0100

    2016-09-12's linux workout

---
+++ contributions/gstavrinos/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Finding Open Files With `lsof`
