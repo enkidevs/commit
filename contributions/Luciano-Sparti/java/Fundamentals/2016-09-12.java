b3021a042389e78d404682273e25c1d1445b4305
Luciano-Sparti <luciano.sparti@usal.edu.ar>
2016-09-12 16:56:33 +0100

    2016-09-12's java workout

---
+++ contributions/Luciano-Sparti/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Limit Accessibility of `Fields`
+Using an interface as a parameter
