1a5a1452c2d0c5e9912540c28a60fd4a8dc1c4a5
tonysusi <junk@unclestu.com>
2016-09-12 08:07:22 +0100

    2016-09-11's linux workout

---
+++ contributions/tonysusi/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Terminal based browser
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
