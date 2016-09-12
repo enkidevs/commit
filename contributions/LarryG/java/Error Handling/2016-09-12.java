0284a0480f64b0b34941fa37d88f9a5e667143b8
LarryG <laurynasgiriunas@gmail.com>
2016-09-12 22:31:54 +0100

    2016-09-12's java workout

---
+++ contributions/LarryG/java/Error Handling/2016-09-12.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
