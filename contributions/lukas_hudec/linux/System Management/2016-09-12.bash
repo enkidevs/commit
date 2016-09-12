d662e8593d69264e11ce11e5943386be61e4582a
lukas_hudec <l.hudec75@gmail.com>
2016-09-12 14:45:12 +0100

    2016-09-12's linux workout

---
+++ contributions/lukas_hudec/linux/System Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
