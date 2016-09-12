e55363ddaf9aee72c405a28ed4bdc0c7cc70d71a
keko950 <gilberto.plaza@udc.es>
2016-09-12 09:40:16 +0100

    2016-09-12's linux workout

---
+++ contributions/keko950/linux/System Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Check the HDD with `badblocks`
