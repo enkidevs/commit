0782ee6c5a37e3dbe5c0bcff77eb1c71c600e972
zeroFruit <14wnrkim@gmail.com>
2016-09-12 03:08:53 +0100

    2016-09-11's linux workout

---
+++ contributions/zeroFruit/linux/System Management/2016-09-11.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
