23dab026d51d7bd5fbe3e6b8fea1200150e1d3ed
thomasthiebaud <thiebaud.tom@gmail.com>
2016-09-12 07:21:51 +0100

    2016-09-12's javascript workout

---
+++ contributions/thomasthiebaud/javascript/Npm/2016-09-12.js
@@ -0,0 +1,5 @@
+List binaries for scripting in npm
+Environmental variables and flags for `npm`
+`npm`  as an alternative to Gulp
+`npm` verifies `sha1` hashes of packages
+`optionalDependencies` in npm
