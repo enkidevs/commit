3f998c13c25d7871936e98bd0f62d9f9c63003b5
Creeperfriend <clemenstiedt@gmx.de>
2016-09-12 21:24:21 +0100

    2016-09-12's linux workout

---
+++ contributions/Creeperfriend/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Detect your linux distribution
+Change your `hostname` on systems using `systemd`
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
