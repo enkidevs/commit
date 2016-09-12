0caab8b0a07dc53a1c402ebb7c8173f6e5ddd839
moguzhanataman <thekhug@hotmail.com>
2016-09-12 08:12:17 +0100

    2016-09-12's linux workout

---
+++ contributions/moguzhanataman/linux/File Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Working with `.gz` files
