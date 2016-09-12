00f124cf67c7de55c7663b55576f71319820716e
Ellis6578 <ellis6578@gmail.com>
2016-09-12 17:38:46 +0100

    2016-09-12's git workout

---
+++ contributions/Ellis6578/git/Essentials/2016-09-12.bash
@@ -0,0 +1,5 @@
+Using tags for version control
+Using aliases for git commands
+Search by commit message keyword
+Use `short` status to make output more compact
+Remote repositories: fetching and pushing
