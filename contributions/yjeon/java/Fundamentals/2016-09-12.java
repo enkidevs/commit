6fc200c1b631a5c126816aca13eb157dcdf1970f
yjeon <yoon3971@gmail.com>
2016-09-12 20:04:15 +0100

    2016-09-12's java workout

---
+++ contributions/yjeon/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Supply `toString()` in all classes
+Using an interface as a parameter
