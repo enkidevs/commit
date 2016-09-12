8301798232d4be7afda9fa9c35d354b7c12c46f4
MTomczynski <mj.tomczynski@gmail.com>
2016-09-12 06:21:45 +0100

    2016-09-12's java workout

---
+++ contributions/MTomczynski/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
