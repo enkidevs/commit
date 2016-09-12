cdbd8f3df26d1d103d3e8686f3f4e02656a6b8e7
reaper057 <martin.leydier@gmail.com>
2016-09-12 20:21:18 +0100

    2016-09-12's linux workout

---
+++ contributions/reaper057/linux/File Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
