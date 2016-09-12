cc604198012ce470ba34f410a0ca755523779b47
k3ypad <ben@tfa.ie>
2016-09-12 08:51:29 +0100

    2016-09-12's linux workout

---
+++ contributions/k3ypad/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Using `dd` To Create Disk Images
