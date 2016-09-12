667e510bcfb7d0dea4bfb77994c3869817328897
alvarodias <alvaro.j.dias@gmail.com>
2016-09-12 21:51:00 +0100

    2016-09-12's linux workout

---
+++ contributions/alvarodias/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
