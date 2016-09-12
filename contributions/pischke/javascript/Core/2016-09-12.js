1d7622d9e2b3124e603eefd80504b3c1d8e997bd
pischke <pischke_t@gmx.net>
2016-09-12 13:34:07 +0100

    2016-09-12's javascript workout

---
+++ contributions/pischke/javascript/Core/2016-09-12.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Most efficient way to build `HTML` strings
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+Closures
