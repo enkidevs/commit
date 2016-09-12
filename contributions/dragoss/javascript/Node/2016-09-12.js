143b01487aeda4399c2de91b800373370fe4f17c
dragoss <dragos.stefanache@gmail.com>
2016-09-12 07:28:01 +0100

    2016-09-12's javascript workout

---
+++ contributions/dragoss/javascript/Node/2016-09-12.js
@@ -0,0 +1,5 @@
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+Middleware/pipeline design pattern
