34ba74adf3890c45803c52b32fcd75853721b301
a-porsia <antonio.p.00ar@gmail.com>
2016-09-12 12:46:23 +0100

    2016-09-12's linux workout

---
+++ contributions/a-porsia/linux/System Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
