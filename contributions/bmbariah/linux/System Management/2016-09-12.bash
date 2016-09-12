550b364288f6971d2feb781f8a448ba44a43611a
bmbariah <bndibx@gmail.com>
2016-09-12 07:28:15 +0100

    2016-09-12's linux workout

---
+++ contributions/bmbariah/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
