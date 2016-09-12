11c49d0f9a42d9f8e72a0230c56dc1199cebb253
elvispt <elvispt@gmail.com>
2016-09-13 00:07:59 +0100

    2016-09-12's javascript workout

---
+++ contributions/elvispt/javascript/Functions/2016-09-12.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
