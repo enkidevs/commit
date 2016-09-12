7dce2881928f2d358f8a8f236962007eeedce6c8
Azteca <eugenio.mg@gmail.com>
2016-09-12 04:00:19 +0100

    2016-09-11's linux workout

---
+++ contributions/Azteca/linux/System Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
