bddac0f15fa7cb015e7ca481f1a604dc15445afe
AlexLionne <alextitou3@gmail.com>
2016-09-12 11:22:24 +0100

    2016-09-12's java workout

---
+++ contributions/AlexLionne/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
