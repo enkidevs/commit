08d4eab5e0001e0b8cdb3ee01416cc1391b5c82e
corderophilosophy <rich@corderophilosophy.com>
2016-09-12 20:39:14 +0100

    2016-09-12's linux workout

---
+++ contributions/corderophilosophy/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,5 @@
+Terminal incognito mode
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
