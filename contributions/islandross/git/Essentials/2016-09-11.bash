1665cccc79337d0078ec5eea43924e2065600872
islandross <ross@islandecc.hk>
2016-09-12 14:44:52 +0100

    2016-09-11's git workout

---
+++ contributions/islandross/git/Essentials/2016-09-11.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to unstage a staged file
+The three states in git
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
