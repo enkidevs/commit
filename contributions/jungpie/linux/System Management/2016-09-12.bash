5931bb625f8da0e762de7cc4d90ccc237c43d11b
jungpie <4straws@gmail.com>
2016-09-12 21:04:14 +0100

    2016-09-12's linux workout

---
+++ contributions/jungpie/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
+Finding Open Files With `lsof`
