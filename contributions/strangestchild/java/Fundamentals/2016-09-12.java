05576424cf20a0f08e6c49ec27840b8a068c81a9
strangestchild <stefankopieczek@gmail.com>
2016-09-12 07:36:32 +0100

    2016-09-12's java workout

---
+++ contributions/strangestchild/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Always specify type parameter
+Increase `PermGen` space as to avoid `OutOfMemory` errors
