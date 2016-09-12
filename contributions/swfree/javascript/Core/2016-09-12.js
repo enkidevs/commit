937d27f9a21ceb175358b021d8a21c00122ed7f6
swfree <skye.free@gmail.com>
2016-09-12 17:30:19 +0100

    2016-09-12's javascript workout

---
+++ contributions/swfree/javascript/Core/2016-09-12.js
@@ -0,0 +1,5 @@
+`hasOwnProperty` method
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Scope and strict mode
+Getting the *real* dimensions of an image
