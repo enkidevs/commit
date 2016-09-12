195d173cfa280d3a3baa092130ea3c58f9757a4e
P4R93 <p4r.esp@gmail.com>
2016-09-12 22:48:54 +0100

    2016-09-12's linux workout

---
+++ contributions/P4R93/linux/File Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
