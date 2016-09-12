ae3c77e4c0e70eca348b8817a86db7b9a98fdf3c
Dirtheev <martinezmolinspablo@gmail.com>
2016-09-12 15:18:34 +0100

    2016-09-12's linux workout

---
+++ contributions/Dirtheev/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
