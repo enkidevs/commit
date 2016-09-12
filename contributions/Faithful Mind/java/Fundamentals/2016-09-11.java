ed72ecbb5e10e7780d483b7aa4f200276cb5236a
Faithful Mind <lpyy15@gmail.com>
2016-09-12 16:46:35 +0100

    2016-09-11's java workout

---
+++ contributions/Faithful Mind/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Implementing an `interface`
