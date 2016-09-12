b7886a24c2b67b8c8b60b2ca8e6f44f8c7c2cc55
Cube <cubejg@outlook.com>
2016-09-12 21:47:48 +0100

    2016-09-12's linux workout

---
+++ contributions/Cube/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear bash history
+Find any Unix / Linux command
+Terminal based browser
