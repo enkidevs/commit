e612ad15aa39151e249cb4a661f6125f65c741b8
luxar <lucasmecanicapando@gmail.com>
2016-09-12 07:44:48 +0100

    2016-09-12's java workout

---
+++ contributions/luxar/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Detect or prevent integer overflow
+Supply `toString()` in all classes
