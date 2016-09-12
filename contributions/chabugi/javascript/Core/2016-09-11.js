1decae7a9dddb4e4e2570fc74b082024a86feaca
chabugi <cbk4092@gmail.com>
2016-09-12 08:23:23 +0100

    2016-09-11's javascript workout

---
+++ contributions/chabugi/javascript/Core/2016-09-11.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Using assignment operators
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Detect an error type
