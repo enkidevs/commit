1660fc9d70ed58b1dd12e6847dfad9f239eb36e2
sreemanrao <srmnrao98@gmail.com>
2016-09-12 03:42:55 +0100

    2016-09-11's linux workout

---
+++ contributions/sreemanrao/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Useful aliasing in bash
+Random password generator
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
