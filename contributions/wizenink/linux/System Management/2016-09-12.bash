451ab7b6f2bbae0862b2e8545f7e293f428595cc
wizenink <david.maseda@hotmail.com>
2016-09-12 21:36:53 +0100

    2016-09-12's linux workout

---
+++ contributions/wizenink/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Common log files
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
