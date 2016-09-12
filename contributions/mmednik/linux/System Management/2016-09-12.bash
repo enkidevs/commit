64ca1ac2f9a451fae8d765de4786baac332cc544
mmednik <mmednik@gmail.com>
2016-09-12 19:41:15 +0100

    2016-09-12's linux workout

---
+++ contributions/mmednik/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Finding Open Files With `lsof`
