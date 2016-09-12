66b7f575c384cf94b53e09f2051676da6dbd90fc
papigers <papigers93@gmail.com>
2016-09-12 17:02:44 +0100

    2016-09-12's linux workout

---
+++ contributions/papigers/linux/File Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
