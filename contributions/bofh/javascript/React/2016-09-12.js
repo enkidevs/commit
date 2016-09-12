0e06c29b9736437adbedfad40dbe5cf59bd4991e
bofh <bofh-reg-enki@diegeekdie.com>
2016-09-12 19:14:29 +0100

    2016-09-12's javascript workout

---
+++ contributions/bofh/javascript/React/2016-09-12.js
@@ -0,0 +1,6 @@
+`null` value for *Controlled Components* in **React**
+Props in getInitialState Is an        
+Anti-Pattern
+Clone Elements in **React**
+Shortcut for transferring props
+Spread attributes
