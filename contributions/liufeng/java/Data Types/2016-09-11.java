fb2accf4a95a3fa9fd2807088255613a952e817a
liufeng <liufeng5460@163.com>
2016-09-12 03:07:54 +0100

    2016-09-11's java workout

---
+++ contributions/liufeng/java/Data Types/2016-09-11.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Finding a substring in a string
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
