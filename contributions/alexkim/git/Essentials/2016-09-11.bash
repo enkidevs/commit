dce081c514ec59f5aad954e93baaa7b7e64c0990
alexkim <hyoungseok.k@gmail.com>
2016-09-12 02:35:23 +0100

    2016-09-11's git workout

---
+++ contributions/alexkim/git/Essentials/2016-09-11.bash
@@ -0,0 +1,5 @@
+Using tags for version control
+The three states in git
+Search by commit message keyword
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
