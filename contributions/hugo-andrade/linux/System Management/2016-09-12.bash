b1160f920afb29e5f127fbdcafbfb21bb60bd947
hugo-andrade <hugo.slip@gmail.com>
2016-09-12 13:21:14 +0100

    2016-09-12's linux workout

---
+++ contributions/hugo-andrade/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
