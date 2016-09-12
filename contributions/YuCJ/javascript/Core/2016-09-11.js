9064f5c0bae1d276fd9eb4af93e6bbe015d19545
YuCJ <nakts0123@gmail.com>
2016-09-12 12:42:51 +0100

    2016-09-11's javascript workout

---
+++ contributions/YuCJ/javascript/Core/2016-09-11.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+JavaScript compilation
+Extra function arguments are undefined by default
+Prevent background image flicker in **IE**
+How to merge two arrays
