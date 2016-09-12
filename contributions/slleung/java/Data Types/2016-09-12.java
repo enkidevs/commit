2bc0f88ecbc30cdb0666c53e20a0f8c9184b06e4
slleung <siulungleung@gmail.com>
2016-09-12 14:27:33 +0100

    2016-09-12's java workout

---
+++ contributions/slleung/java/Data Types/2016-09-12.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Finding a substring in a string
+Equals operation on different data types
