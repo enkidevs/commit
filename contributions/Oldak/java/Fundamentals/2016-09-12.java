5befa51f1d457ba7dcbf1d03357ebb6127e9ea51
Oldak <olda.kopp@gmail.com>
2016-09-12 18:44:26 +0100

    2016-09-12's java workout

---
+++ contributions/Oldak/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+Using an interface as a parameter
