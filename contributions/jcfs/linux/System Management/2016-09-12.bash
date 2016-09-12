ef762b360ec884ad0ea6ec2b83d91b8964ddfa2a
jcfs <joao.salavisa@gmail.com>
2016-09-12 23:47:43 +0100

    2016-09-12's linux workout

---
+++ contributions/jcfs/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
