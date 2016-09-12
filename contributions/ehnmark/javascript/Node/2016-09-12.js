d01f31b2eff63dd902f315f7c0bfdf86e2bd6931
ehnmark <github@ehnmark.com>
2016-09-13 00:15:40 +0100

    2016-09-12's javascript workout

---
+++ contributions/ehnmark/javascript/Node/2016-09-12.js
@@ -0,0 +1,5 @@
+Use conventional property names for error handling in Node.js
+**Node** `os` module
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
+`try-catch` only for **sync** code
