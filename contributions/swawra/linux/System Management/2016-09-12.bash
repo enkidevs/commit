76f27b0d0a143480c8a56b0ba62cfcad98646f63
swawra <swawra@gmail.com>
2016-09-12 21:48:09 +0100

    2016-09-12's linux workout

---
+++ contributions/swawra/linux/System Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
