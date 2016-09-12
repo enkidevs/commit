bda66e2bbfc9477366494ff1344350651a05dbb7
xCloudx8 <danielestrife@gmail.com>
2016-09-12 11:51:08 +0100

    2016-09-12's linux workout

---
+++ contributions/xCloudx8/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
