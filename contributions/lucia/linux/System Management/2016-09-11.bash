ef63eb7dfa8572fc2ae62a74ec54495779659f78
lucia <mikoto.yu51@gmail.com>
2016-09-12 15:39:56 +0100

    2016-09-11's linux workout

---
+++ contributions/lucia/linux/System Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
