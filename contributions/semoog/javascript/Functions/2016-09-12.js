1889252b3ffaea7f319acbca5560bcf9f73d7a66
semoog <sebmernst@gmail.com>
2016-09-12 18:33:41 +0100

    2016-09-12's javascript workout

---
+++ contributions/semoog/javascript/Functions/2016-09-12.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Method chaining
+Social sharing without widgets
