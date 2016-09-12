afa168812c44b5c15a2f72a1aed6afcbaa08c879
Moritz155 <moritz1296@gmx.de>
2016-09-12 22:28:29 +0100

    2016-09-12's linux workout

---
+++ contributions/Moritz155/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
