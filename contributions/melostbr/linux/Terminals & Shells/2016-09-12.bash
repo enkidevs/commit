78d7314b1de45f12d1d951ba4b8858ab1c74abe5
melostbr <stefanietmelo@gmail.com>
2016-09-12 19:54:24 +0100

    2016-09-12's linux workout

---
+++ contributions/melostbr/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Find any Unix / Linux command
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
