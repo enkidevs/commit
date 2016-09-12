649814765055d1689ed0c965e872ffd33960ce20
filbertphang <fillberth@gmail.com>
2016-09-12 16:14:14 +0100

    2016-09-11's linux workout

---
+++ contributions/filbertphang/linux/System Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
