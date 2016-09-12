35fed794d752b700aa92198ccc4c5419c333e589
Nockiro <robinfreund99@gmx.de>
2016-09-12 22:07:00 +0100

    2016-09-12's linux workout

---
+++ contributions/Nockiro/linux/System Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Preventing a user from logging into the system
