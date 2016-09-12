bcbedfb524f8cb2955aa77c990369b2ab2fa5385
saka <saka71996@gmail.com>
2016-09-12 05:11:36 +0100

    2016-09-12's java workout

---
+++ contributions/saka/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Measuring time
+Always specify type parameter
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
