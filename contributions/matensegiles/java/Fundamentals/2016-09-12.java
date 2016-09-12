2a083e95ceab7797ea73d2beb2688d2431700598
matensegiles <matensegiles@gmail.com>
2016-09-12 13:41:20 +0100

    2016-09-12's java workout

---
+++ contributions/matensegiles/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+`Autoboxing` and `Unboxing`
+Avoid using `static` variables
