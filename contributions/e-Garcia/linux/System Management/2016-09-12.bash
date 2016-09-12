9a2b64d414ec5a3a79dd5083af555c6a63f97549
e-Garcia <erick.garcia.garcia@gmail.com>
2016-09-12 18:20:17 +0100

    2016-09-12's linux workout

---
+++ contributions/e-Garcia/linux/System Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
