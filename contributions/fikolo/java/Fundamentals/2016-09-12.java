455bac7c7f9c1678596ecce7a89dd00f217b8ccc
fikolo <filipdalorenzo@gmail.com>
2016-09-12 07:04:00 +0100

    2016-09-12's java workout

---
+++ contributions/fikolo/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
