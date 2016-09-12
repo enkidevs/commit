2a14d003926da0596408ca89675a37946cc7835a
OfekWeinberger <ofekwa@gmail.com>
2016-09-12 13:01:24 +0100

    2016-09-12's java workout

---
+++ contributions/OfekWeinberger/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Common mistake on switch statements
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
