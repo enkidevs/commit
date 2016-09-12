a8e9268c44018ed040a2755039228710f0571490
tommbarton <tommbarton@gmail.com>
2016-09-13 00:39:06 +0100

    2016-09-12's linux workout

---
+++ contributions/tommbarton/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
