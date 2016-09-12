4c12dc10f2ed39c373f699fddce30a0c3f52b239
zmisson424 <zmisson424@gmail.com>
2016-09-12 14:06:55 +0100

    2016-09-12's linux workout

---
+++ contributions/zmisson424/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Useful aliasing in bash
+Random password generator
+`else` statements using the `||` operator 
