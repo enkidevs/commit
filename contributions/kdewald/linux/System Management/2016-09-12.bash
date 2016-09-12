b7bfcc2ba866f2faf067485493ca528ae96c5dbe
kdewald <kevin.dewald@ieee.org>
2016-09-12 11:05:04 +0100

    2016-09-12's linux workout

---
+++ contributions/kdewald/linux/System Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
