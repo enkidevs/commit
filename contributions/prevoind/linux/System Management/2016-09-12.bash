abd062725bfe69de6bfae6fe2c5d5df8a0aa75df
prevoind <reganprevost@gmail.com>
2016-09-12 22:19:15 +0100

    2016-09-12's linux workout

---
+++ contributions/prevoind/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
+Finding Open Files With `lsof`
