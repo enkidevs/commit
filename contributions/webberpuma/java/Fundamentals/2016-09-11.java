6e3fa97982ecdbda5f5f92ed7878877a54cc1c6a
webberpuma <webberpuma@gmail.com>
2016-09-12 06:08:40 +0100

    2016-09-11's java workout

---
+++ contributions/webberpuma/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Avoid using `static` variables
