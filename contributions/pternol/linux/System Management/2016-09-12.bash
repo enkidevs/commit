e01564343344a60a1247fb5927d5d0fa7fdba2a6
pternol <pet.hassler+github@gmail.com>
2016-09-12 18:22:27 +0100

    2016-09-12's linux workout

---
+++ contributions/pternol/linux/System Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
