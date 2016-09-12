f0cb0b8a8229febf916ba7bbb72498b71788ffff
shortsamshort <shortsamshort@gmail.com>
2016-09-12 11:04:04 +0100

    2016-09-12's linux workout

---
+++ contributions/shortsamshort/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Common log files
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Finding Open Files With `lsof`
