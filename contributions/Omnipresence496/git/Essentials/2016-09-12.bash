b271b5bdb3b93f1a3d6e0b82c2a74a3c538bd65d
Omnipresence496 <troybondswell496@gmail.com>
2016-09-12 11:07:56 +0100

    2016-09-12's git workout

---
+++ contributions/Omnipresence496/git/Essentials/2016-09-12.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Using aliases for git commands
+Limiting log output by time
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
