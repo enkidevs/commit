d379e78c66cc81cd900e85981b7f7330721db3af
mcheli <mpcheli7@gmail.com>
2016-09-12 18:05:15 +0100

    2016-09-12's javascript workout

---
+++ contributions/mcheli/javascript/Functions/2016-09-12.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Easily generate a random `HEX` color
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
