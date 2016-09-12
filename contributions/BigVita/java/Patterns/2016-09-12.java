737aa705ccd06744ffedb8f9a5c516c99c2494d0
BigVita <vitalijkretz@outlook.de>
2016-09-12 20:50:00 +0100

    2016-09-12's java workout

---
+++ contributions/BigVita/java/Patterns/2016-09-12.java
@@ -0,0 +1,5 @@
+Canceling an individual task using `java.util.Timer` class
+Singleton Pattern
+Private constructors
+Static factory methods
+Using initializer blocks to minimize code repetition in constructors
