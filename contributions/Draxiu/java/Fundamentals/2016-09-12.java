36d92706df75b38ea08eb05dba886114e459d935
Draxiu <drax98@gmail.com>
2016-09-12 19:39:56 +0100

    2016-09-12's java workout

---
+++ contributions/Draxiu/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
