5cdc2cdaddd67614910a5b414d326e1b95212b92
bmbeverst <bmbeverst@gmail.com>
2016-09-13 00:57:49 +0100

    2016-09-12's java workout

---
+++ contributions/bmbeverst/java/Annotations/2016-09-12.java
@@ -0,0 +1,5 @@
+`@Test` parameters
+Using the `@Deprecated` annotation
+Suppressing warnings using `@SuppressWarnings`
+Restricting an annotation's application by using `@Target`
+Declaring `@Repeatable` annotation types
