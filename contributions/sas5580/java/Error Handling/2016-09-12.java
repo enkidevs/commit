988ea331b5c6f86050ab3c53c8afeb1eb1b70eda
sas5580 <gupta.saswata@gmail.com>
2016-09-12 19:12:13 +0100

    2016-09-12's java workout

---
+++ contributions/sas5580/java/Error Handling/2016-09-12.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Using exceptions in Java
+Most common reason behind **stack overflow** error
+Throwing an `exception`
