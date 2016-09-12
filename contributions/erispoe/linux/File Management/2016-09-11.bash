bd10ffb8712af61cd17344f79a18320273534eae
erispoe <favrebulle@gmail.com>
2016-09-12 01:15:05 +0100

    2016-09-11's linux workout

---
+++ contributions/erispoe/linux/File Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Converting between Unix and Windows text files
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Working with `.gz` files
+Delete commands aliases
