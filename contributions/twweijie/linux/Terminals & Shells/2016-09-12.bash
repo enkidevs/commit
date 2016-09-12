5d04706fcd7ff5845d7aea2c03cbe2bbdfb9bec3
twweijie <tanweijie_94@hotmail.com>
2016-09-12 18:56:02 +0100

    2016-09-12's linux workout

---
+++ contributions/twweijie/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Aliasing ssh connections
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+The Basics of Environment Variables
