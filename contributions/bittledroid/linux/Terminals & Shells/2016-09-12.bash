803b82f3f975849dba63047b324b153685d97eab
bittledroid <bittledroid@gmail.com>
2016-09-12 22:45:12 +0100

    2016-09-12's linux workout

---
+++ contributions/bittledroid/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
