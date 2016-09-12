6b8d135e1a942458c403ebe7daa9e634dcecfeb8
fatlard1993 <chase.whitfield@me.com>
2016-09-12 09:04:27 +0100

    2016-09-11's linux workout

---
+++ contributions/fatlard1993/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear bash history
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
