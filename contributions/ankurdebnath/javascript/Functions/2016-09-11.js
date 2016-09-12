6d0ae647aba55acd84c63dd1c7d6da6689e5ad51
ankurdebnath <ankurdebnath1994@gmail.com>
2016-09-12 21:32:00 +0100

    2016-09-11's javascript workout

---
+++ contributions/ankurdebnath/javascript/Functions/2016-09-11.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Get query/url variables
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
