5fc7752d678f0816c1c752b0541b4f2d5112919f
Alekzanderz <anthony.kissling@gmail.com>
2016-09-12 07:33:34 +0100

    2016-09-12's javascript workout

---
+++ contributions/Alekzanderz/javascript/Core/2016-09-12.js
@@ -0,0 +1,5 @@
+Labeling your loops
+Hoisting applies only to variable declarations, not initializations
+`catch` is block scoped
+Prevent background image flicker in **IE**
+Detect online connection
