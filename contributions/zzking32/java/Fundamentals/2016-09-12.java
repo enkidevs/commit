7a9f9b2a6936398936cadb4b580314d3f192dd4c
zzking32 <ulusalz@gmail.com>
2016-09-12 11:37:38 +0100

    2016-09-12's java workout

---
+++ contributions/zzking32/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+Use strings in a `switch` statement
+Using Inheritance to reduce code repetition
+Calling one constructor from another
+Supply `toString()` in all classes
