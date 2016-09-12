0dd4c982cc1102a7641e8cbd96abd9f8c8c16961
symach1ne <trygve.nordtveit@gmail.com>
2016-09-12 21:06:35 +0100

    2016-09-12's javascript workout

---
+++ contributions/symach1ne/javascript/Functions/2016-09-12.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Get query/url variables
+Easily generate a random `HEX` color
+Function call method
