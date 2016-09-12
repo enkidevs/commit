80890e6a4366b72c14b4a337eda220b5bb7ce425
greg-solomon <greg.solomon95@gmail.com>
2016-09-12 07:34:15 +0100

    2016-09-11's java workout

---
+++ contributions/greg-solomon/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,4 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
