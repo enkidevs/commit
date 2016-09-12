5c61ac121af54b559bbfe56e340215ab0b72fe7c
BrianHannon <brianhannon73@gmail.com>
2016-09-12 14:53:57 +0100

    2016-09-12's linux workout

---
+++ contributions/BrianHannon/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Terminal incognito mode
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
