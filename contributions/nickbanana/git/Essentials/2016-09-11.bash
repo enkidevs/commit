4a9cc27a153a3158686d80912daebc6f33781bd8
nickbanana <nick.chi@gmail.com>
2016-09-12 07:19:50 +0100

    2016-09-11's git workout

---
+++ contributions/nickbanana/git/Essentials/2016-09-11.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+You can use git offline!
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
