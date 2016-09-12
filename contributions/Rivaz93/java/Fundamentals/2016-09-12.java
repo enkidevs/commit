96ed1ac6f176142920512fd830a99ebbe68cc394
Rivaz93 <alin.ionut.cojocaru@gmail.com>
2016-09-12 20:22:15 +0100

    2016-09-12's java workout

---
+++ contributions/Rivaz93/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
