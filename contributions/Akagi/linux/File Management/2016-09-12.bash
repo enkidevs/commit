350a0e3c11fe2125bc7b66d5eec8bcbbc7d6041e
Akagi <stringtheory@gmx.net>
2016-09-12 06:44:59 +0100

    2016-09-12's linux workout

---
+++ contributions/Akagi/linux/File Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
