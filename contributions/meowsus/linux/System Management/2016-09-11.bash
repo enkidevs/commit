7a9524bac0a56966f3e667224aad797dc3edd6c4
meowsus <curt@meows.us>
2016-09-12 11:35:12 +0100

    2016-09-11's linux workout

---
+++ contributions/meowsus/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Common log files
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
