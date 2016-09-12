fbc885a08b31dd4b17541be2ea03b8663807d0a6
kiwiapple87 <mrorange4287@gmail.com>
2016-09-12 16:53:51 +0100

    2016-09-12's linux workout

---
+++ contributions/kiwiapple87/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Preventing a user from logging into the system
+Monitor System Memory using `vmstat`
+System uptime
