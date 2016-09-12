68dc20ac4fae7da7a8feb4be168df02c82f6b7f6
itsukikoizumi93 <itsukikoizumi93@gmail.com>
2016-09-12 23:33:14 +0100

    2016-09-12's git workout

---
+++ contributions/itsukikoizumi93/git/Essentials/2016-09-12.bash
@@ -0,0 +1,5 @@
+Make your log output pretty
+Search for commits by author
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
