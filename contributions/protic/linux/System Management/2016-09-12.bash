521981d36e1e1336d139b0fee06910e46fe9e300
protic <adispapazian@gmail.com>
2016-09-12 14:47:30 +0100

    2016-09-12's linux workout

---
+++ contributions/protic/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
