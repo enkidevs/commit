76c783c533a9ca9273be091c4c0f12a98fc235c6
marcelgrilo <marcel.grilo.dev@gmail.com>
2016-09-13 00:25:56 +0100

    2016-09-12's git workout

---
+++ contributions/marcelgrilo/git/Essentials/2016-09-12.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+You can use git offline!
+How to set your username and email
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
