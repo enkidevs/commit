91f120ba7d9f6f5a47fb92bfc0af8af54e944ff9
MSFJarvis <msfjarvis@gmail.com>
2016-09-12 18:31:42 +0100

    2016-09-11's linux workout

---
+++ contributions/MSFJarvis/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
