10da9c42847fb9794c20d7eae7eca35d03ae003a
aqws3 <aqwssama@gmail.com>
2016-09-12 19:04:44 +0100

    2016-09-12's linux workout

---
+++ contributions/aqws3/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
