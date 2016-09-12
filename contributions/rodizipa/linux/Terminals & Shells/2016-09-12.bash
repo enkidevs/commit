c6b75b679b0bf00cecce2c7778bf77520b01385b
rodizipa <rodizipa2007@gmail.com>
2016-09-13 00:12:21 +0100

    2016-09-12's linux workout

---
+++ contributions/rodizipa/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
