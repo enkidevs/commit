c61790b6be75821ac3e8eb710432dadf4b3e1a8d
ccp1992 <cpustejovsky@rocketmail.com>
2016-09-12 02:52:33 +0100

    2016-09-11's linux workout

---
+++ contributions/ccp1992/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,7 @@
+Find any Unix / Linux command
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+The Basics of Environment Variables
