fcc6ba2a62135e49995c8ac2517e41e1a5aa6f95
bennettj1087 <bennettj1087@gmail.com>
2016-09-12 03:58:20 +0100

    2016-09-11's linux workout

---
+++ contributions/bennettj1087/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Test disk speed with `dd`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
