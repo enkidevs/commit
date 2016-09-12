b704c31ff39dbd9fb170c5f80d0b9df4e8eb40fa
baldrick <paianes@yahoo.gr>
2016-09-12 06:19:11 +0100

    2016-09-12's linux workout

---
+++ contributions/baldrick/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Cancel a system shutdown
