194eb7d5640261a06255604cf667f2dcad878d06
khalidhamdi7 <khalidhamdi7@gmail.com>
2016-09-12 11:23:03 +0100

    2016-09-12's linux workout

---
+++ contributions/khalidhamdi7/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
