933424be90765b30713e8d005e9ee7f41c29b8c2
kitzudoubletail <nico.kuemmel@gmail.com>
2016-09-12 12:30:21 +0100

    2016-09-12's linux workout

---
+++ contributions/kitzudoubletail/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Finding Open Files With `lsof`
