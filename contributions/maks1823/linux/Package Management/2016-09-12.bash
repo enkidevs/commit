6233313c4a12f55853c32fd95cfdb858659bf4e2
maks1823 <maks1823@gmail.com>
2016-09-12 07:47:44 +0100

    2016-09-12's linux workout

---
+++ contributions/maks1823/linux/Package Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Install specific version of packages
+List installed packages
+Installing a `.deb` package from the terminal
+Using `Tasksel` for software installation
+Keeping repos updated with `cron`
