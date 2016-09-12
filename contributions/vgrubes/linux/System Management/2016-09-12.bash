a31dbdb75a8c02d21471a0461cc815ada3f24b63
vgrubes <vladan@fwd.hr>
2016-09-12 07:09:24 +0100

    2016-09-12's linux workout

---
+++ contributions/vgrubes/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
