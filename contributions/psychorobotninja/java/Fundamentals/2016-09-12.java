d23d7ab79c71cef24fb19b13b847ac6d6b8debd6
psychorobotninja <tefuller@gmail.com>
2016-09-12 16:17:52 +0100

    2016-09-12's java workout

---
+++ contributions/psychorobotninja/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Using an interface as a parameter
