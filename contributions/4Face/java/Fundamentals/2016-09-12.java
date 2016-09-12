a41e27824986df4a3c783165098f34e96c67c1c5
4Face <fardavide@gmail.com>
2016-09-12 17:30:29 +0100

    2016-09-12's java workout

---
+++ contributions/4Face/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+`Autoboxing` and `Unboxing`
+Supply `toString()` in all classes
