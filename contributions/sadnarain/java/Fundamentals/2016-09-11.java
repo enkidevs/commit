901a99951fc12e202c8fed9a3bc9740552192cdc
sadnarain <sadnarain@gmail.com>
2016-09-12 01:47:13 +0100

    2016-09-11's java workout

---
+++ contributions/sadnarain/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
