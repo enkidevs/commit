c2ad1ff3a5c7c27bf9393a7ae95738845a32703a
sagiben <ilnextbus@gmail.com>
2016-09-12 04:33:43 +0100

    2016-09-12's python workout

---
+++ contributions/sagiben/python/Data Structures/2016-09-12.py
@@ -0,0 +1,5 @@
+Following PEP 8 styling guideline.
+A simple way to select a random item from a `list/tuple` data stucture
+`bytearray` objects
+`queue`s and threads
+Keep things in order with `OrderedDict`
