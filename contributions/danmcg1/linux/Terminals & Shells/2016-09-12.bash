0c4f4e2afb8ff84c6ba33824da1f433adb3a33aa
danmcg1 <danmcgarvie@gmail.com>
2016-09-12 23:01:50 +0100

    2016-09-12's linux workout

---
+++ contributions/danmcg1/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
