e3b553392bcbef13891af7b73716116062fb6672
mustieles <eric.mustieles@hotmail.com>
2016-09-12 15:20:20 +0100

    2016-09-12's javascript workout

---
+++ contributions/mustieles/javascript/Functions/2016-09-12.js
@@ -0,0 +1,5 @@
+Hoisting
+Function constructor vs. function declaration vs. function expression
+Method chaining
+Easily generate a random `HEX` color
+Changing a functions context with `fn.call(object)`
