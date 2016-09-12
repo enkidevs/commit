2947ec4df50dc8084e6d06cc072676b6390f975a
crispar <jwcris.park@gmail.com>
2016-09-12 03:53:46 +0100

    2016-09-11's linux workout

---
+++ contributions/crispar/linux/Terminals & Shells/2016-09-11.bash
@@ -0,0 +1,7 @@
+Clear the terminal instantly
+Find any Unix / Linux command
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
