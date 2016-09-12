8f839b03e822214d7659781bfd2eb4ded1f54bd0
aostrun <andrijan.ostrun@gmail.com>
2016-09-13 00:50:59 +0100

    2016-09-12's linux workout

---
+++ contributions/aostrun/linux/System Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
