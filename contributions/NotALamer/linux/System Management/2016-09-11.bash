009e31001926c5147d5d7372094a8ea811d8ea25
NotALamer <alvaro.movil.ms@gmail.com>
2016-09-12 01:50:01 +0100

    2016-09-11's linux workout

---
+++ contributions/NotALamer/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
