efe7c7362a74a92ed34b30fe330110332cc9e868
Stevelicious <husteven.91@gmail.com>
2016-09-12 23:31:40 +0100

    2016-09-12's java workout

---
+++ contributions/Stevelicious/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Always specify type parameter
+Do not return *references* to private *mutable* class members
