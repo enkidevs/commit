f18b286bd244168ea36f7be04590e0e54051ab25
Axiome <n.libermann@gmail.com>
2016-09-12 21:32:36 +0100

    2016-09-12's javascript workout

---
+++ contributions/Axiome/javascript/Core/2016-09-12.js
@@ -0,0 +1,5 @@
+Multi-line string variables
+Infix operators are left-associative
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+Scope and strict mode
