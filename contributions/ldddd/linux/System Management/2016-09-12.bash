15024b70e7bdcdfe9baeec0d44d7f7ac597bbcb4
ldddd <lodphp@gmail.com>
2016-09-12 15:28:48 +0100

    2016-09-12's linux workout

---
+++ contributions/ldddd/linux/System Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
