52b0c6dbf4e53c1c72753f233458da4525d03e1b
eduardm1 <modreanu.eduard@gmail.com>
2016-09-12 14:36:22 +0100

    2016-09-12's java workout

---
+++ contributions/eduardm1/java/Error Handling/2016-09-12.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
