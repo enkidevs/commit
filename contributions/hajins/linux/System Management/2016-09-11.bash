0af2ca5b24cdc8f7b36cf5421cca78767d9dd0ab
hajins <hajins@gmail.com>
2016-09-12 02:49:08 +0100

    2016-09-11's linux workout

---
+++ contributions/hajins/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
