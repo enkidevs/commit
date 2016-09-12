17b6084d6766227cefbdd0a482af2102e3278db4
tchan123 <thomas.chan811@icloud.com>
2016-09-12 02:30:14 +0100

    2016-09-11's linux workout

---
+++ contributions/tchan123/linux/File Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Deleting non-empty directories
