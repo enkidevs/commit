262adaacd5e14d7654834df2fbb681ce7ee3cd60
nickdab <nickdab@gmail.com>
2016-09-12 21:27:07 +0100

    2016-09-12's linux workout

---
+++ contributions/nickdab/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Retrieve previous arguments
