3a34925f828a12e766a575b5ce55dcd60a47865c
tuxiedev <techie.anish@gmail.com>
2016-09-12 07:38:36 +0100

    2016-09-11's javascript workout

---
+++ contributions/tuxiedev/javascript/Core/2016-09-11.js
@@ -0,0 +1,5 @@
+Data type comparison in `switch` statements
+Counting the months from zero
+The difference between null, undefined and NaN
+Detect **DO NOT TRACK** status
+Getting the *real* dimensions of an image
