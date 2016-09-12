c012abba5cdc3b0c065425913635e5280f875f57
Marciyo <marcelmierzejewski@gmail.com>
2016-09-12 21:42:06 +0100

    2016-09-12's java workout

---
+++ contributions/Marciyo/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Ternary operator
+Using Inheritance to reduce code repetition
+Using bounded type parameters in generic methods
+Calling one constructor from another
