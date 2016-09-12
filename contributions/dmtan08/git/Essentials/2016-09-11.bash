2f1e66b0b187d2eacd532f1e3168e4f0fa6f2c7a
dmtan08 <dawntan08@gmail.com>
2016-09-12 11:42:35 +0100

    2016-09-11's git workout

---
+++ contributions/dmtan08/git/Essentials/2016-09-11.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Use `short` status to make output more compact
+How to write a git commit message
+Remote repositories: fetching and pushing
+Ignore files in git
