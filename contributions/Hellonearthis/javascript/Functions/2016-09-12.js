2127784ebd0f744cee101a81b9466b0bd5326984
Hellonearthis <hellonearthisman@gmail.com>
2016-09-12 19:22:57 +0100

    2016-09-12's javascript workout

---
+++ contributions/Hellonearthis/javascript/Functions/2016-09-12.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+IIFE pattern
