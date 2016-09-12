3f8fd44d65af14080e4c6155238b3cc7e92ceff1
Dvalar <x3lt0x@gmail.com>
2016-09-12 07:09:46 +0100

    2016-09-12's linux workout

---
+++ contributions/Dvalar/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
