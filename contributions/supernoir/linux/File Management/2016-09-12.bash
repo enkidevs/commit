d996becc1068fce60c8cfa1fa1146821c620c3ea
supernoir <kawohl.bentodesign@gmail.com>
2016-09-12 13:36:28 +0100

    2016-09-12's linux workout

---
+++ contributions/supernoir/linux/File Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
