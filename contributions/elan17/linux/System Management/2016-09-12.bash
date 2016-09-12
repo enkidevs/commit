6157c69c4631fa9581673bc960e49fc91669b8b6
elan17 <elan17.programacion@gmail.com>
2016-09-12 22:25:14 +0100

    2016-09-12's linux workout

---
+++ contributions/elan17/linux/System Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
