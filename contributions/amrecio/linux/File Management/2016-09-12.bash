2a58fe37a8fb5be9d765b7c4d82552e04c3584a0
amrecio <amrecio@gmail.com>
2016-09-12 07:20:31 +0100

    2016-09-12's linux workout

---
+++ contributions/amrecio/linux/File Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Easy way of sharing files
