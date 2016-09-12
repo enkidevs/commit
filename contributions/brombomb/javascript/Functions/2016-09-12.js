4bf30e1356d40f4e78e8acd765fd1cf853357f61
brombomb <brombomb@gmail.com>
2016-09-12 20:38:26 +0100

    2016-09-12's javascript workout

---
+++ contributions/brombomb/javascript/Functions/2016-09-12.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Functions can be declared after use
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
