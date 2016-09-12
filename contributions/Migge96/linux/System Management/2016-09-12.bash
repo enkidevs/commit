266f08836d4f54f90797010b62d66c1b3383cb3c
Migge96 <lukasmicke@ymail.com>
2016-09-12 13:14:51 +0100

    2016-09-12's linux workout

---
+++ contributions/Migge96/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
