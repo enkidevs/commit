77a09ea019a1dc58888d8d08f654ef73123db838
zanesalvatore <zanesalvatore@gmail.com>
2016-09-12 14:48:21 +0100

    2016-09-12's linux workout

---
+++ contributions/zanesalvatore/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
