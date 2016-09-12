6aafd86b3270d473e5f08e7b075c6468fb1a9027
deekayen <deekayen@deekayen.net>
2016-09-12 18:04:54 +0100

    2016-09-12's linux workout

---
+++ contributions/deekayen/linux/Networking/2016-09-12.bash
@@ -0,0 +1,5 @@
+ Intro to `iptables`
+Useful ssh client optimizations 
+Limit the `wget` download rate
+Staying connected remotely via SSH without an interactive shell
+Short Introduction to `Wget` 
