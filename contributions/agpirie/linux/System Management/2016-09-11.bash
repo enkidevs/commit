53b8c05b94db32da588381208e94c0c74c309af8
agpirie <agpiriemobile@gmail.com>
2016-09-12 02:11:24 +0100

    2016-09-11's linux workout

---
+++ contributions/agpirie/linux/System Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
