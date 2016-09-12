99ecf4869406ad3bdbabcd3491153c2c4ebb3c61
Deathnerd <wes.gilleland@gmail.com>
2016-09-12 01:34:13 +0100

    2016-09-11's linux workout

---
+++ contributions/Deathnerd/linux/System Management/2016-09-11.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
