cbb9c6f459877f185fc6b28d80f273a506020fd0
88roy88 <88roy88@gmail.com>
2016-09-12 18:39:11 +0100

    2016-09-12's linux workout

---
+++ contributions/88roy88/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Terminal incognito mode
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
