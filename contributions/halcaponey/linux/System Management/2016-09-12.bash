dc44f0fd5a4b07d1f5705a412ff76fc26d28616b
halcaponey <eric.peresse@gmail.com>
2016-09-12 11:37:56 +0100

    2016-09-12's linux workout

---
+++ contributions/halcaponey/linux/System Management/2016-09-12.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
