30b50b6b3e17018a41ca8b77a9ad651249df90ec
denemmy <denemmy@gmail.com>
2016-09-12 09:45:25 +0100

    2016-09-12's linux workout

---
+++ contributions/denemmy/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Retrieve previous arguments
