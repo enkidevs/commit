fb799b6f24d9584ab0460d6322a5acb883cc83e7
MissValeska <missvaleskavv@gmail.com>
2016-09-12 17:45:29 +0100

    2016-09-12's java workout

---
+++ contributions/MissValeska/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Inheriting a constructor from a superclass
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Supply `toString()` in all classes
