98afb43c2e99c605cd918515d77d68ff9a623f2b
thejakeofink <thejakeofink@gmail.com>
2016-09-12 07:41:58 +0100

    2016-09-11's java workout

---
+++ contributions/thejakeofink/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Always specify type parameter
+Do not return *references* to private *mutable* class members
