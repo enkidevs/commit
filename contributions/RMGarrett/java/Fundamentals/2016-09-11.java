0ab9b995f574f9b95ef209883f1508db8d7c3d4f
RMGarrett <rossgarrett1@hotmail.co.uk>
2016-09-12 01:45:30 +0100

    2016-09-11's java workout

---
+++ contributions/RMGarrett/java/Fundamentals/2016-09-11.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
