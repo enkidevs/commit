14d4e897df6eca91b66d879d72fdeb8e6a108c1b
NoName64Bits <ciobanulaur@gmail.com>
2016-09-12 19:21:45 +0100

    2016-09-12's linux workout

---
+++ contributions/NoName64Bits/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Retrieve previous arguments
