5d2ce2242167cd9d5e0a6f16bb55c94b4e6b7f29
orientis <jazztuck@aol.com>
2016-09-12 01:53:59 +0100

    2016-09-11's linux workout

---
+++ contributions/orientis/linux/File Management/2016-09-11.bash
@@ -0,0 +1,5 @@
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Working with `.gz` files
+Find the unknown process preventing deleting of files
