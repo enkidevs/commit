d3ceb54cfc27085201c571dee6ea3008b28c555b
cynthisl <cynthia@cynthialee.net>
2016-09-13 06:18:07 +0100

    2016-09-12's linux workout

---
+++ contributions/cynthisl/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
