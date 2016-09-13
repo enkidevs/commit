18c9db4cc82a2a910d9498da460ab518df25b53d
bodo <bodoeichstaedt@gmail.com>
2016-09-13 14:39:52 +0100

    2016-09-13's javascript workout

---
+++ contributions/bodo/javascript/Core/2016-09-13.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Weak vs Strict equality operator
+Most efficient way to build `HTML` strings
+Use `splice()` to remove an item from an array
+Round numbers to `N` decimals 
