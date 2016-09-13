cefd8f055bf89b6fb8b39a4a1c6b21a5defe044f
yucenli <yucenli@gmail.com>
2016-09-13 18:27:55 +0100

    2016-09-13's java workout

---
+++ contributions/yucenli/java/Java 8/2016-09-13.java
@@ -0,0 +1,5 @@
+Use `Optional(s)` type in order to avoid `NullPointerException(s)`
+Control OS `Process`es in `Java8`
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
