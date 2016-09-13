dfb34f8e4899345d851cc26f4f6834c2300b0c2e
adrianek <a.nadratowski@gmail.com>
2016-09-13 07:43:30 +0100

    2016-09-13's linux workout

---
+++ contributions/adrianek/linux/Terminals & Shells/2016-09-13.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
