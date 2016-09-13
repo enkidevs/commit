78d4c99e42f052540963360b676f1300885c438c
shaileshsutar88 <shaileshsutar88@gmail.com>
2016-09-13 21:00:18 +0100

    2016-09-12's linux workout

---
+++ contributions/shaileshsutar88/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Useful aliasing in bash
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
