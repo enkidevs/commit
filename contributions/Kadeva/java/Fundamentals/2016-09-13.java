e718b0113d16c851c0c000d592c9eb0bfe7cd8aa
Kadeva <kalumniatoris@gmail.com>
2016-09-13 19:59:41 +0100

    2016-09-13's java workout

---
+++ contributions/Kadeva/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
