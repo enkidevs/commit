35305be3437373053144b40fff0b0c13c9b19cbc
dpmidd <dmidd87@gmail.com>
2016-09-13 04:10:32 +0100

    2016-09-12's javascript workout

---
+++ contributions/dpmidd/javascript/Node/2016-09-12.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+The built-in Node debugger
+Wrapping errors in Node.js using _node-verror_
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
