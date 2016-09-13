e159c52131e09731975b0dacb8436cf218e3e79a
Omnipresence496 <troybondswell496@gmail.com>
2016-09-13 11:50:32 +0100

    2016-09-13's linux workout

---
+++ contributions/Omnipresence496/linux/System Management/2016-09-13.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
