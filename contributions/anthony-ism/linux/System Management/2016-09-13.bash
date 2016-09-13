3d98e8d77fb4deb3ce0d0d3e2ae1479bf8fcbb40
anthony-ism <rizzo0917@gmail.com>
2016-09-13 21:39:10 +0100

    2016-09-13's linux workout

---
+++ contributions/anthony-ism/linux/System Management/2016-09-13.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Basic service management with `systemd`
