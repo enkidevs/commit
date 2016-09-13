bd138f0d3421b44cbd4379cbc623cbd245b93016
pineoc <pineoc@naver.com>
2016-09-13 15:16:35 +0100

    2016-09-12's linux workout

---
+++ contributions/pineoc/linux/System Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
