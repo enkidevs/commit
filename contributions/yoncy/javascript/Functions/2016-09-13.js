8b5ef7eda26c7cc29ab86ecb59b05820b87b9b1e
yoncy <ionut.tudorel@gmail.com>
2016-09-13 23:31:46 +0100

    2016-09-13's javascript workout

---
+++ contributions/yoncy/javascript/Functions/2016-09-13.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Function call method
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
