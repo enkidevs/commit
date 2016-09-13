640c8cf65e411cb6468368f9210e74231407146c
alkom1 <alkom1programmer@gmail.com>
2016-09-13 14:08:32 +0100

    2016-09-13's java workout

---
+++ contributions/alkom1/java/Error Handling/2016-09-13.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Creating new Exceptions
+Most common reason behind **stack overflow** error
+Careful Numeric Data Conversions
