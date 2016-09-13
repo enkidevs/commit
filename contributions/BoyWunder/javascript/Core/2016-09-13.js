cdc9af7e3f2f4a950ec1c95f5a4d9ab0de9bec78
BoyWunder <monkeywunder@gmail.com>
2016-09-13 21:04:28 +0100

    2016-09-13's javascript workout

---
+++ contributions/BoyWunder/javascript/Core/2016-09-13.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Setting default values with `short circuiting`
+Use `splice()` to remove an item from an array
+Data type comparison in `switch` statements
+Extra function arguments are undefined by default
