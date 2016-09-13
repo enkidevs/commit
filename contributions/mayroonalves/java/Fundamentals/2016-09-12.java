098966fb03d01cad036be6181f2bf9fe05d4be30
mayroonalves <mayroonalves@gmail.com>
2016-09-13 04:17:51 +0100

    2016-09-12's java workout

---
+++ contributions/mayroonalves/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Distinction between `public` and `private` methods
+Metadata: setting a file's owner
+Constant notation
+Limit Accessibility of `Fields`
