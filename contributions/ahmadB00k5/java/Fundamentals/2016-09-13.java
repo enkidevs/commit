f98b43ae75f6947c1b6342b9c9674c1f7b1a2e81
ahmadB00k5 <book578@gmail.com>
2016-09-13 18:02:11 +0100

    2016-09-13's java workout

---
+++ contributions/ahmadB00k5/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Measuring time
+Using an interface as a parameter
