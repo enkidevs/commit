bebabacaff8254350186eb61d60dc7d24a1f0149
sky2826 <aakashtiwari03@gmail.com>
2016-09-13 13:13:48 +0100

    2016-09-12's linux workout

---
+++ contributions/sky2826/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
