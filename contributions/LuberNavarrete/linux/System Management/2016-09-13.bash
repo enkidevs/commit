64bc37aa60250e835a59b0958198b0d40bc3bcf4
LuberNavarrete <navarreteluber@gmail.com>
2016-09-13 18:01:15 +0100

    2016-09-13's linux workout

---
+++ contributions/LuberNavarrete/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
