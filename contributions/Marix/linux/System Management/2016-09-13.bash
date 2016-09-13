1d89a4bf7de9f9607e2244c42a4b4a1ec656a425
Marix <marix@marix.org>
2016-09-13 19:18:46 +0100

    2016-09-13's linux workout

---
+++ contributions/Marix/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
