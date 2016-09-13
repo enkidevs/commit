31a0604a80daa23375bde53ecb908b99af94476a
bertalanp99 <peter.bertalan.root@gmail.com>
2016-09-13 22:14:33 +0100

    2016-09-13's linux workout

---
+++ contributions/bertalanp99/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
