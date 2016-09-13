4f9785f97ffecffea08386619565bcea558a2d90
TheWebTech <jonmclaren1@gmail.com>
2016-09-13 13:48:48 +0100

    2016-09-13's javascript workout

---
+++ contributions/TheWebTech/javascript/Npm/2016-09-13.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Registry user accounts for npm
+`npm` verifies `sha1` hashes of packages
+`optionalDependencies` in npm
+Lock down dependency versions by shrinkwrapping
