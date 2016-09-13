9dee384a67ffb3299013c5cfb4d8611bbc7f07d3
mcaulay <mark@shed.io>
2016-09-13 22:29:00 +0100

    2016-09-13's javascript workout

---
+++ contributions/mcaulay/javascript/Node/2016-09-13.js
@@ -0,0 +1,5 @@
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+`exports` vs. `module.exports` in **Node**
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
