66c7f4b5507a6515e10c9e343543f75889a61b5b
silid <simon+github@liddicott.com>
2016-09-13 08:25:46 +0100

    2016-09-13's linux workout

---
+++ contributions/silid/linux/Terminals & Shells/2016-09-13.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Execute a command without saving it in history
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
