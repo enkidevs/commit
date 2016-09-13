d50dc48f5567bb23e09c38bba91e80b585286dc2
muhammadaljunied <muhammadaljunied@gmail.com>
2016-09-13 08:08:57 +0100

    2016-09-12's java workout

---
+++ contributions/muhammadaljunied/java/Data Types/2016-09-12.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Finding a substring in a string
+Numeric Conversion - Widening
