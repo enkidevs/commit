acf9c8c6fce06117eb444c6d346eec8d1046338e
RaphaelRaja <jrrphonix@gmail.com>
2016-09-13 19:16:13 +0100

    2016-09-12's java workout

---
+++ contributions/RaphaelRaja/java/Data Types/2016-09-12.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Double brace initialization
+Numeric Conversion - Widening
