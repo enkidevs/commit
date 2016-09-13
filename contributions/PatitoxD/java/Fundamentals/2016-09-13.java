06c5fdced117322c8c71c0fc989add32aaedaa7b
PatitoxD <mail2patrickengel@gmail.com>
2016-09-13 10:28:31 +0100

    2016-09-13's java workout

---
+++ contributions/PatitoxD/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
