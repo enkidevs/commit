495e2fe77b8705ac281b1a33f175939390876565
kelstar95 <kelstar95@gmail.com>
2016-09-13 16:44:16 +0100

    2016-09-13's java workout

---
+++ contributions/kelstar95/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
