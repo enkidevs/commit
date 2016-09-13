a458de87a3699de2b84aa5195b0a754995d89098
tazr42 <enki@reghif.com>
2016-09-13 23:24:11 +0100

    2016-09-13's python workout

---
+++ contributions/tazr42/python/Data Structures/2016-09-13.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Double ended queues with `deque`
+Special attributes of objects and classes
+Immutable sets with `frozenset`
+Operations with `bytes` and `bytearray`
