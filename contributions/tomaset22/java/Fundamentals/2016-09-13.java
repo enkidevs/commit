69bbdd10c14a116af3b1b749149528ac0cb829b1
tomaset22 <tomasengel7@gmail.com>
2016-09-13 14:28:44 +0100

    2016-09-13's java workout

---
+++ contributions/tomaset22/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Supply `toString()` in all classes
