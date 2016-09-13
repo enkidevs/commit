eefc5ddca0d1c823d98ef9c294666740bd7318ce
csyouk <csyouk@gmail.com>
2016-09-13 04:54:41 +0100

    2016-09-12's linux workout

---
+++ contributions/csyouk/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Basic service management with `systemd`
