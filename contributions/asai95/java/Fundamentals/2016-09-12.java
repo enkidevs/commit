cd714651a5b1050324b597d5d0b88e7a5996c403
asai95 <viktr5909@mail.ru>
2016-09-13 05:53:55 +0100

    2016-09-12's java workout

---
+++ contributions/asai95/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The trade-off between short and descriptive names
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+`Autoboxing` and `Unboxing`
