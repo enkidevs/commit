f25968f0ab00b11552cdc2aadeff5c88c32b1ad3
CentOS <kylejohnproctor@gmail.com>
2016-09-13 10:37:07 +0100

    2016-09-13's linux workout

---
+++ contributions/CentOS/linux/File Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
+Find the unknown process preventing deleting of files
