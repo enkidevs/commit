22b4efabbc08078518203bfbd62525f285d38c54
Szkaplerny <tsado.in@gmail.com>
2016-09-13 18:44:18 +0100

    2016-09-13's linux workout

---
+++ contributions/Szkaplerny/linux/System Management/2016-09-13.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
