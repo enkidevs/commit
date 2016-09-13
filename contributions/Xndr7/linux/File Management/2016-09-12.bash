61c182cbff21e438856e8d3c4097ebf96c22ead7
Xndr7 <sldesalobo@gmail.com>
2016-09-13 02:39:20 +0100

    2016-09-12's linux workout

---
+++ contributions/Xndr7/linux/File Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
