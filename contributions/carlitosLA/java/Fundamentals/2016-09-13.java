7bb70983d91ac259178046929c16c5e1d465933b
carlitosLA <carlos_rdv@hotmail.com>
2016-09-13 13:34:24 +0100

    2016-09-13's java workout

---
+++ contributions/carlitosLA/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Avoid using `static` variables
