d60b9648299c747e983607de1502b953ec2a2703
le_idiot <siddhesh.mhadnak@gmail.com>
2016-09-13 13:52:58 +0100

    2016-09-12's linux workout

---
+++ contributions/le_idiot/linux/File Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Get disk space usage with `df`
+
+Find the unknown process preventing deleting of files
+Easy way of sharing files
