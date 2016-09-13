5a2ee8a8c0141304024c3e4df5958642c82461d7
ulfandreas <me@andreasekholm.com>
2016-09-13 19:17:45 +0100

    2016-09-13's linux workout

---
+++ contributions/ulfandreas/linux/System Management/2016-09-13.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
