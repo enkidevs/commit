ec6ad2d660fbf227e329e0e214b8bfb3a21c85fb
timothynsheehan <timothynsheehan@gmail.com>
2016-09-13 05:10:32 +0100

    2016-09-12's java workout

---
+++ contributions/timothynsheehan/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Altering format string output by changing a format specifier's `argument_index`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
+Using an interface as a parameter
