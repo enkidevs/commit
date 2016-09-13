720c102351a209b56ef1d7a09324a3c874a6d0af
bender87 <cicciogae@gmail.com>
2016-09-13 23:23:33 +0100

    2016-09-13's linux workout

---
+++ contributions/bender87/linux/Terminals & Shells/2016-09-13.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
