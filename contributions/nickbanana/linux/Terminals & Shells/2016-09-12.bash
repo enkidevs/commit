f298167778f4f17e887948d282827212706c9707
nickbanana <nick.chi@gmail.com>
2016-09-13 06:43:35 +0100

    2016-09-12's linux workout

---
+++ contributions/nickbanana/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
