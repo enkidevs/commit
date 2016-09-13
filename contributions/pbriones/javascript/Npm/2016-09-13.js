3eade23684402aed80d79d8821c8fda7ace7d0f9
pbriones <p.briones@tcs.com>
2016-09-13 12:24:20 +0100

    2016-09-13's javascript workout

---
+++ contributions/pbriones/javascript/Npm/2016-09-13.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Manage local node modules with `npm link`
+`npm` verifies `sha1` hashes of packages
+Package distribution tags
+Lock down dependency versions by shrinkwrapping
