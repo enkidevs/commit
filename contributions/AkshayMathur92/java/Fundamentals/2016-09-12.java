510a9dd78b31111dc446bf112d114f2e0130a32e
AkshayMathur92 <avi.mathur.engg@gmail.com>
2016-09-13 20:18:57 +0100

    2016-09-12's java workout

---
+++ contributions/AkshayMathur92/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Cropping images
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
