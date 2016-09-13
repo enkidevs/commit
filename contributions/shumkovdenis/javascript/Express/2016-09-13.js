db44618061162ed2e9db2c896ec8bc89d176ee62
shumkovdenis <shumkovdenis@gmail.com>
2016-09-13 05:56:36 +0100

    2016-09-13's javascript workout

---
+++ contributions/shumkovdenis/javascript/Express/2016-09-13.js
@@ -0,0 +1,5 @@
+Easy way to deliver **HTML** pages with **Express**
+**GZIP** compression for **Express**
+Using **Express 4.0** `router`
+Log with `morgan` in **Express**
+Error handler in **Express**
