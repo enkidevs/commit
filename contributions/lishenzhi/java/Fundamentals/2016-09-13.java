4c010760e395c4e724e71c2b1d69c7519d715aad
lishenzhi <lishenzhi@gmail.com>
2016-09-13 13:35:40 +0100

    2016-09-13's java workout

---
+++ contributions/lishenzhi/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
