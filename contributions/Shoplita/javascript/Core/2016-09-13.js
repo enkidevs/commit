3916d0337e0e1cc790fe9bcf77dce775297fe2b3
Shoplita <florespryscila@gmail.com>
2016-09-13 13:06:46 +0100

    2016-09-13's javascript workout

---
+++ contributions/Shoplita/javascript/Core/2016-09-13.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Setting the length of an array 
+Counting the months from zero
+Getting the *real* dimensions of an image
+Inaccuracy of binary floating-point format
