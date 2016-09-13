dbc2e21f021ab9d6e40b8edb5113516d61607991
ple103 <peterhuyle@outlook.com>
2016-09-13 22:18:45 +0100

    2016-09-13's linux workout

---
+++ contributions/ple103/linux/File Management/2016-09-13.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
