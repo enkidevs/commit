50a55021d0b4ea0692c3dcb9467b1d8eb1e49b6c
mjnitz02 <mjnitz02@gmail.com>
2016-09-13 13:54:39 +0100

    2016-09-13's java workout

---
+++ contributions/mjnitz02/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
