cf6de0c700ee91a72280a010a54845b300166c33
tiagoamaro <tiagopadrela@gmail.com>
2016-09-13 07:20:19 +0100

    2016-09-13's javascript workout

---
+++ contributions/tiagoamaro/javascript/Functions/2016-09-13.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+IIFE pattern
+Easily generate a random `HEX` color
+Check if a document is done loading
