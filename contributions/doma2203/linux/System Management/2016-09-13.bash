d010df9775612e34997400ee4a80fbf8a9d44573
doma2203 <dominika.kalafut@onet.eu>
2016-09-13 21:52:41 +0100

    2016-09-13's linux workout

---
+++ contributions/doma2203/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
