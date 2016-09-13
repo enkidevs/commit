74cad32d5d7c66576045174e2285b86a7db5c474
psi1717 <ephesians01@gmail.com>
2016-09-13 05:05:32 +0100

    2016-09-12's javascript workout

---
+++ contributions/psi1717/javascript/React/2016-09-12.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+**React** in-line styles
+Type of the Children props
+componentWillReceiveProps Not Triggered After Mounting
+
+Dealing with `this.props.children`
