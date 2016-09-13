a2b41a499dd9ded8ca6dfa77df4c9d9e8bb7a59a
destan <destan@dorukdestan.com>
2016-09-13 08:01:25 +0100

    2016-09-13's java workout

---
+++ contributions/destan/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Cropping images
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
