86d196de47b3266361ff1a5911510d88d36879df
Tomek263 <tom.hejra@gmail.com>
2016-09-13 16:49:05 +0100

    2016-09-13's java workout

---
+++ contributions/Tomek263/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Supply `toString()` in all classes
