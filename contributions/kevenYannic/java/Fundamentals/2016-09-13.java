fe90b381e59be188425a70347eeb3b06f6f0483a
kevenYannic <keven.iwansky@gmail.com>
2016-09-13 08:19:02 +0100

    2016-09-13's java workout

---
+++ contributions/kevenYannic/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
