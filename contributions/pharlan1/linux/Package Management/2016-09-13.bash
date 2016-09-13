49fc24c737eb86aa1a1dd5f7b62cff521bbef886
pharlan1 <pharlan42@gmail.com>
2016-09-13 10:40:35 +0100

    2016-09-13's linux workout

---
+++ contributions/pharlan1/linux/Package Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+List installed packages
+Using `Tasksel` for software installation
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
