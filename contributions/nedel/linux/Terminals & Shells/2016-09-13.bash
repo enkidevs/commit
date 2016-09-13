6b7310a0c7b47069ab46128f11569b0349060b3a
nedel <nedel@gmx.com>
2016-09-13 05:39:43 +0100

    2016-09-13's linux workout

---
+++ contributions/nedel/linux/Terminals & Shells/2016-09-13.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Aliasing ssh connections
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
