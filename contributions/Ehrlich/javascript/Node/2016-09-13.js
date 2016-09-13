2c3f7d089d23e6e39a84c3031da715c8faa432a4
Ehrlich <anissa.d.alexandercs@gmail.com>
2016-09-13 14:53:03 +0100

    2016-09-13's javascript workout

---
+++ contributions/Ehrlich/javascript/Node/2016-09-13.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Global Objects and Environment Variables in **Node**
+Use conventional property names for error handling in Node.js
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
