d065f64cac5b44b45514fa47f7157e42479ea344
bendobot <whatsbending@gmail.com>
2016-09-13 18:56:08 +0100

    2016-09-13's linux workout

---
+++ contributions/bendobot/linux/Terminals & Shells/2016-09-13.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
