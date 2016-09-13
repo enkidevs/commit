b6bf7515b2d34b640ac52931a108dc73075fb962
chishum <chishum9@gmail.com>
2016-09-13 06:31:41 +0100

    2016-09-12's java workout

---
+++ contributions/chishum/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
