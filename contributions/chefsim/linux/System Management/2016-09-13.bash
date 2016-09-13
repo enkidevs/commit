8e75972f92f551480447eb07be45235241714290
chefsim <chefsim@hotmail.com>
2016-09-13 16:59:48 +0100

    2016-09-13's linux workout

---
+++ contributions/chefsim/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
