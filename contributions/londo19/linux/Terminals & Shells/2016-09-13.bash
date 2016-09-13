8438fd790a66445523d8265f58163ae86986fdb3
londo19 <londo2@gmx.de>
2016-09-13 19:43:26 +0100

    2016-09-13's linux workout

---
+++ contributions/londo19/linux/Terminals & Shells/2016-09-13.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal incognito mode
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
