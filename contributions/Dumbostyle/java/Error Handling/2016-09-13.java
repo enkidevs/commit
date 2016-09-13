e5c80516af0b2c4f089cfe86f86c7b3e698b380a
Dumbostyle <worecraft@gmail.com>
2016-09-13 21:10:35 +0100

    2016-09-13's java workout

---
+++ contributions/Dumbostyle/java/Error Handling/2016-09-13.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Using exceptions in Java
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Careful Numeric Data Conversions
