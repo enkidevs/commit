ae7a42824012cb8bbada07654377347412556912
Mexeaki <joaquim.ramos@outlook.fr>
2016-09-13 17:07:50 +0100

    2016-09-13's linux workout

---
+++ contributions/Mexeaki/linux/System Management/2016-09-13.bash
@@ -0,0 +1,7 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
