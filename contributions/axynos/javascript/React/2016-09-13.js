c59bea22e3f5280b5709d80da578eeff9c945c1b
axynos <silverkruus@gmail.com>
2016-09-13 17:25:14 +0100

    2016-09-13's javascript workout

---
+++ contributions/axynos/javascript/React/2016-09-13.js
@@ -0,0 +1,6 @@
+`null` value for *Controlled Components* in **React**
+Props in getInitialState Is an        
+Anti-Pattern
+`PureRenderMixin` in **React**
+Shallow compare in **React**
+Clone Elements in **React**
