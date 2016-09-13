b89334a5dda4b95d49588d8e2c06c6539725e1d5
fisent <lukzieniewicz@gmail.com>
2016-09-13 10:04:22 +0100

    2016-09-13's java workout

---
+++ contributions/fisent/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Always specify type parameter
