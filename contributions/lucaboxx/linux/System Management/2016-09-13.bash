e60e99240ecfacc410023a78ab0380b2771e4918
lucaboxx <lucabosso96@gmail.com>
2016-09-13 09:21:44 +0100

    2016-09-13's linux workout

---
+++ contributions/lucaboxx/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
