030d6a5c6478799a22f7839d194b0d3ff4a884d4
drumor <ludovic.taffin@gmail.com>
2016-09-13 09:14:54 +0100

    2016-09-13's linux workout

---
+++ contributions/drumor/linux/File Management/2016-09-13.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
+Easy way of sharing files
