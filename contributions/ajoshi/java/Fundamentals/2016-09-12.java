4dcf100a4fd039b396b252af215f7dc33d03a8b3
ajoshi <ajoshi@financialengines.com>
2016-09-13 02:28:35 +0100

    2016-09-12's java workout

---
+++ contributions/ajoshi/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,4 @@
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Increase `PermGen` space as to avoid `OutOfMemory` errors
