78c6867b37b8131e49a6cc4f5b8a3a2d12f1979e
corux <tobias@wallura.org>
2016-09-13 07:11:35 +0100

    2016-09-13's linux workout

---
+++ contributions/corux/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Using `dd` To Create Disk Images
