2d8db79af87dd36e365f284f0539eed401b84b2f
ankman <thomas.ankerl@gmail.com>
2016-09-13 07:35:22 +0100

    2016-09-13's java workout

---
+++ contributions/ankman/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Using an interface as a parameter
