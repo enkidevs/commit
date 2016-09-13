461cb00aad8ff020f878d4e3c27ff335825f3525
naXa777 <alarmo.lord@gmail.com>
2016-09-13 21:46:08 +0100

    2016-09-13's java workout

---
+++ contributions/naXa777/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Measuring time
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
