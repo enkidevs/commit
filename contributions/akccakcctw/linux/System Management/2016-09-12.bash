d7e13cd85ac96b11d7192c57a89e3622a1f1ba96
akccakcctw <akccakccwww@gmail.com>
2016-09-13 14:29:32 +0100

    2016-09-12's linux workout

---
+++ contributions/akccakcctw/linux/System Management/2016-09-12.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Preventing a user from logging into the system
+Monitor System Memory using `vmstat`
+System uptime
