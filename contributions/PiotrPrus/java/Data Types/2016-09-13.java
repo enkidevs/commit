6371cf1c7680860b25d010a0e41ba05b056e783a
PiotrPrus <prus.piotr@gmail.com>
2016-09-13 08:43:44 +0100

    2016-09-13's java workout

---
+++ contributions/PiotrPrus/java/Data Types/2016-09-13.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Collections vs arrays
+Use `Arrays.asList()` to initialise lists
+Converting a string to upper or lower case
