5532fdc2ce4202ab2163798175878b0960e6d3d7
Mattle <mcallier@gmail.com>
2016-09-13 21:51:49 +0100

    2016-09-13's java workout

---
+++ contributions/Mattle/java/Classes & Objects/2016-09-13.java
@@ -0,0 +1,5 @@
+Template methods in abstract classes
+Utility classes and `static` methods
+Keeping fields `private`
+Compare classes and not class names
+String formatting and parsing with `SimpleDateFormat`
