cd1e0947b6dab76fe647f15c83103cfe26cdb0cb
luci7fer <linxiasheng@gmail.com>
2016-09-13 10:02:29 +0100

    2016-09-12's java workout

---
+++ contributions/luci7fer/java/I/O/2016-09-12.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+Deleting files and directories
