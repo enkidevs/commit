fdd7daef9093fe2233be9be4bc354f0eb771338b
zelsazgh <zhugehonghn@gmail.com>
2016-09-13 01:00:42 +0100

    2016-09-12's linux workout

---
+++ contributions/zelsazgh/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
