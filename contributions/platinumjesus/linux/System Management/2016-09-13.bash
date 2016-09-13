79f4b3e08b627ba7adbcb1eabe9e12988d9dd7c1
platinumjesus <cipettaro@gmail.com>
2016-09-13 06:54:31 +0100

    2016-09-13's linux workout

---
+++ contributions/platinumjesus/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Basic service management with `systemd`
+Cancel a system shutdown
