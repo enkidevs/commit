e10449caf1b6386ad38a696ad5c6818209bb9464
thefirstequinox <quino.gamer@gmail.com>
2016-09-13 07:52:09 +0100

    2016-09-12's java workout

---
+++ contributions/thefirstequinox/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Common mistake on switch statements
+Locks in `static synchronized` methods
+Always specify type parameter
+Using an interface as a parameter
