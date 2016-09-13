b7af194f7e234b4122a54585ebdfd65f15d6560a
james-gibson <james@aptotude.com>
2016-09-13 05:58:43 +0100

    2016-09-12's linux workout

---
+++ contributions/james-gibson/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
