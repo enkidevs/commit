0ac2a7e079dd39d6c3dcbccaaeeb3783dd0a8168
jonathanabennett <doulos05@gmail.com>
2016-09-13 05:27:31 +0100

    2016-09-12's python workout

---
+++ contributions/jonathanabennett/python/Data Structures/2016-09-12.py
@@ -0,0 +1,5 @@
+Double ended queues with `deque`
+Special attributes of objects and classes
+Operations with `bytes` and `bytearray`
+Prioritize your `queue` 
+Binary record packing with `bytearray`
