f1a0488a54f5840de43c089f746b84baf68a9a95
Faithful Mind <lpyy15@gmail.com>
2016-09-13 10:43:01 +0100

    2016-09-12's java workout

---
+++ contributions/Faithful Mind/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
