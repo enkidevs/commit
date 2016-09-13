db93afe094dc7916736dff536d861a89ea7b185e
remipou <remi.puig@gmail.com>
2016-09-13 06:12:51 +0100

    2016-09-13's git workout

---
+++ contributions/remipou/git/Essentials/2016-09-13.bash
@@ -0,0 +1,5 @@
+Using aliases for git commands
+Make your log output pretty
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
