eba84f769495399cdc8c2a13d43faa2414982d3f
jannis4112 <jannis.kirschner@gmail.com>
2016-09-13 09:12:56 +0100

    2016-09-13's linux workout

---
+++ contributions/jannis4112/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
