551ebcce3cabedcb9486067b1607cbf90dc59bbc
raghu <raghu.9208@gmail.com>
2016-09-13 04:54:00 +0100

    2016-09-12's git workout

---
+++ contributions/raghu/git/Essentials/2016-09-12.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Using tags for version control
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
