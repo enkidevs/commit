cd938ecd64cb004ab218a896769eb907841d8b30
sansagara <leo.atencio@gmail.com>
2016-09-13 18:42:06 +0100

    2016-09-13's java workout

---
+++ contributions/sansagara/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
