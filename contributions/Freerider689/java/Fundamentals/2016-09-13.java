2ac12362d0da98fb0c924a12c9b514bf8f613a71
Freerider689 <cyril.grossenbacher@gmx.ch>
2016-09-13 04:48:14 +0100

    2016-09-13's java workout

---
+++ contributions/Freerider689/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
