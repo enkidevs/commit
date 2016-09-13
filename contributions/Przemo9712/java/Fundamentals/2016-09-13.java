abafbfa89109a0083dd7a94c69d1ea1df15735e4
Przemo9712 <przemo9712@gmail.com>
2016-09-13 06:13:43 +0100

    2016-09-13's java workout

---
+++ contributions/Przemo9712/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
