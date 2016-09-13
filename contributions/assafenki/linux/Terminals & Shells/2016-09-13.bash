4fdb421f251564095851c085360f8e2fcbcacbf6
assafenki <assaf@shiftz.com>
2016-09-13 05:11:08 +0100

    2016-09-13's linux workout

---
+++ contributions/assafenki/linux/Terminals & Shells/2016-09-13.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Shebang `#!` explained 
+Execute a command without saving it in history
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
