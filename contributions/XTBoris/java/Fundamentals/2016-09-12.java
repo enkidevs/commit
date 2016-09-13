5b9363e96f2cf5f969572fb64c1b36227844d80a
XTBoris <borisgoullet@gmail.com>
2016-09-13 01:05:05 +0100

    2016-09-12's java workout

---
+++ contributions/XTBoris/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Measuring time
+Supply `toString()` in all classes
