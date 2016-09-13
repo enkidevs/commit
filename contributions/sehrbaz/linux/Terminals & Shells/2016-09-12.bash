e40a23df7b07e67172f5d27bad639df8009f2bfb
sehrbaz <faridz13@gmail.com>
2016-09-13 21:41:30 +0100

    2016-09-12's linux workout

---
+++ contributions/sehrbaz/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
