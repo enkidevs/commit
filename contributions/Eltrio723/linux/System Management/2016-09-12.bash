6cc12b8be7d8f54c22cd28401f865ce0e6ec25b5
Eltrio723 <anto96nio@gmail.com>
2016-09-13 01:11:17 +0100

    2016-09-12's linux workout

---
+++ contributions/Eltrio723/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Cancel a system shutdown
