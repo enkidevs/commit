6b0ae55c2633e09d9f723c2700cb2300d39853c2
DIrvin <drk.irvin90@gmail.com>
2016-09-13 15:45:02 +0100

    2016-09-13's java workout

---
+++ contributions/DIrvin/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
