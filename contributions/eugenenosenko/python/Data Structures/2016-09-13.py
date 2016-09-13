418e2b36aad7bcad9c6b5c89ea0e1962fe71e6e5
eugenenosenko <eugenenosenko@gmail.com>
2016-09-13 09:24:25 +0100

    2016-09-13's python workout

---
+++ contributions/eugenenosenko/python/Data Structures/2016-09-13.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`Dictionary` view objects
+Your own Python `calendar` 
+Immutable sets with `frozenset`
+Get the most of `int`s
