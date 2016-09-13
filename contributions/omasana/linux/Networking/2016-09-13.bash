4fe1d38321f422fed997d597f806717f47bc44db
omasana <stepanar@gmail.com>
2016-09-13 05:25:40 +0100

    2016-09-13's linux workout

---
+++ contributions/omasana/linux/Networking/2016-09-13.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Proxifying `ssh` connections
+Check `iptables` firewall status
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
