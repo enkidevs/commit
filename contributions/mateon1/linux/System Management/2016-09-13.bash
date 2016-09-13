5d2d8c2e67cd024800a8cbde4f9616320fdd87d4
mateon1 <matin1111@wp.pl>
2016-09-13 07:41:58 +0100

    2016-09-13's linux workout

---
+++ contributions/mateon1/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
