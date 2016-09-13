1591ff26b039ffb422beda7c3a0790a027e69557
Mainachtsmann <strass.fred@gmail.com>
2016-09-13 22:07:31 +0100

    2016-09-13's java workout

---
+++ contributions/Mainachtsmann/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Connecting to FTP using Java
+Catch multiple exceptions in a single `catch` block
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
