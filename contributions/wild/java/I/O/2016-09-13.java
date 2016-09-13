e844ffd79fa08ce49a2a212ce440d9eae7518b8d
wild <georgedeheer6919@gmail.com>
2016-09-13 17:24:17 +0100

    2016-09-13's java workout

---
+++ contributions/wild/java/I/O/2016-09-13.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Using buffered streams
+Reading file attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
