1a8889f69e663bcb1c28e4b2dc913a4c5b27a6a4
stfbee <pinkey.950@gmail.com>
2016-09-13 08:04:45 +0100

    2016-09-13's java workout

---
+++ contributions/stfbee/java/I/O/2016-09-13.java
@@ -0,0 +1,5 @@
+How to list all files in a directory that match a filename extension in Java
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Using the `OpenOption...` vararg to define file writing behavior
+List contents of a directory with `newDirectoryStream`
