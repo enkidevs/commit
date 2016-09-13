bcdc906c91b0ef2421ee7bed85fc850b535a2f17
DavidKwon <hyuk7410@gmail.com>
2016-09-13 02:40:38 +0100

    2016-09-12's java workout

---
+++ contributions/DavidKwon/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Using an interface as a parameter
