6048689229c088dcb028f184322bf40f0883d8e7
miblanchard <michael.james.blanchard@gmail.com>
2016-09-14 00:40:45 +0100

    2016-09-13's javascript workout

---
+++ contributions/miblanchard/javascript/Functions/2016-09-13.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Function call method
