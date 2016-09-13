c6d8a53d756fe0aea0a2dcc597df92456348bcac
kaosmos <matteo@kaosmos.it>
2016-09-13 20:23:58 +0100

    2016-09-13's linux workout

---
+++ contributions/kaosmos/linux/Terminals & Shells/2016-09-13.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
