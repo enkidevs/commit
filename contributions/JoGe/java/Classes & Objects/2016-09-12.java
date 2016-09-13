7e76dc05d4cf8795e6f904f85d0d25b3fc1300a2
JoGe <jonasgeuens@gmail.com>
2016-09-13 03:28:13 +0100

    2016-09-12's java workout

---
+++ contributions/JoGe/java/Classes & Objects/2016-09-12.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+How and when to use `WeakHashMap` class
+Fields with Java **Reflection API**
