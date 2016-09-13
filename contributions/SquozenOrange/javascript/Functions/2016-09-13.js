0e12d06219e1b17819323d8090f4f2f97d7e624b
SquozenOrange <panomosh@gmail.com>
2016-09-13 09:04:33 +0100

    2016-09-13's javascript workout

---
+++ contributions/SquozenOrange/javascript/Functions/2016-09-13.js
@@ -0,0 +1,5 @@
+Hoisting
+Function declarations vs function expressions
+Functions can be declared after use
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
