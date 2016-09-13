27dad8cd7c8344859fc32dac5e20002dc4afcbdf
luminarious <hardikovamees@gmail.com>
2016-09-13 18:31:20 +0100

    2016-09-13's git workout

---
+++ contributions/luminarious/git/Essentials/2016-09-13.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+Using aliases for git commands
+How to set your username and email
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
