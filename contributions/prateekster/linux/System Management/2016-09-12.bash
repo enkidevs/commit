dea09d7639dae77e47dde6f8bbbd679ff9d31ac3
prateekster <ppagarawal@gmail.com>
2016-09-13 16:01:29 +0100

    2016-09-12's linux workout

---
+++ contributions/prateekster/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
