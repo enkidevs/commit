603f1e5e2bf30560c1b6f321e69f1ddaa9d18e11
aaronbiller <aaronbiller@gmail.com>
2016-09-13 05:16:10 +0100

    2016-09-12's linux workout

---
+++ contributions/aaronbiller/linux/System Management/2016-09-12.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
