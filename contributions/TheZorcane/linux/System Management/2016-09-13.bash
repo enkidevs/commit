87562c092651b1c74d5926f8c425bc186e467f38
TheZorcane <ro2363@gmail.com>
2016-09-13 05:20:13 +0100

    2016-09-13's linux workout

---
+++ contributions/TheZorcane/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
