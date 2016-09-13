3ba80137da962d5afe34ae268c7fb3bde9d5c7dd
MonteC <jarekucinski@gmail.com>
2016-09-13 22:27:49 +0100

    2016-09-13's java workout

---
+++ contributions/MonteC/java/Classes & Objects/2016-09-13.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Compare classes and not class names
+Diamond Operator
+String formatting and parsing with `SimpleDateFormat`
