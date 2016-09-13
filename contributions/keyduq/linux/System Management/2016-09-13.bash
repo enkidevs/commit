02a280f70b99f4c63a1aa87f3d7856cfd8ee7d05
keyduq <thkeyduq@gmail.com>
2016-09-13 23:48:19 +0100

    2016-09-13's linux workout

---
+++ contributions/keyduq/linux/System Management/2016-09-13.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Using `dd` To Create Disk Images
