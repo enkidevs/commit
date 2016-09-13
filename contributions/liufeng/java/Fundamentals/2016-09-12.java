c0902ce165b0759f32894d8ccf4936f8a1dd5cbc
liufeng <liufeng5460@163.com>
2016-09-13 01:03:40 +0100

    2016-09-12's java workout

---
+++ contributions/liufeng/java/Fundamentals/2016-09-12.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+Distinction between `public` and `private` methods
+How range operations work
