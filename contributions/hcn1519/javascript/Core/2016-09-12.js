fac42ebca7d1dfeb3bc71d87b885c6bcedfb19b5
hcn1519 <hcn1519@gmail.com>
2016-09-13 02:47:38 +0100

    2016-09-12's javascript workout

---
+++ contributions/hcn1519/javascript/Core/2016-09-12.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+Truthiness
+Hoisting applies only to variable declarations, not initializations
+Avoid using `with`
+Prevent background image flicker in **IE**
