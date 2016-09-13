cb958ccbf79ecc423537cf630f4fb8ca8d7cc0b0
ssolecki <sebastian.solecki@gmail.com>
2016-09-13 23:05:28 +0100

    2016-09-13's javascript workout

---
+++ contributions/ssolecki/javascript/Functions/2016-09-13.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
