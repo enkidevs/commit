29f4686e7a0995fee3ec9e0de8089b26ea81cd6a
Limero <limero1337@gmail.com>
2016-09-13 18:11:50 +0100

    2016-09-13's linux workout

---
+++ contributions/Limero/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
