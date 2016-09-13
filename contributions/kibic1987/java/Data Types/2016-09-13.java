cb4376817f317ede34418e03ef031c2106d4d878
kibic1987 <looqash1987@gmail.com>
2016-09-13 19:53:09 +0100

    2016-09-13's java workout

---
+++ contributions/kibic1987/java/Data Types/2016-09-13.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Converting numbers to strings
+`StringBuffer` vs `StringBuilder`
+String concatenation in Java
