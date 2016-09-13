3166613ab75712d49ea4e9eee55441724ad04c17
KidAmnesiac1 <conor.cadogan@gmail.com>
2016-09-13 04:32:20 +0100

    2016-09-12's linux workout

---
+++ contributions/KidAmnesiac1/linux/Terminals & Shells/2016-09-12.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Random password generator
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
