792824b0cd238ce81cfbaaec575342f6b903fded
stefano-brambilla-853558 <s.brambilla93@gmail.com>
2016-09-13 10:04:54 +0100

    2016-09-13's linux workout

---
+++ contributions/stefano-brambilla-853558/linux/System Management/2016-09-13.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
