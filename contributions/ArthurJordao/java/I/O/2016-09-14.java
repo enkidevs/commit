00f844fa12be02c13b1c95f3578e35a5cbdd4d15
ArthurJordao <arthurbernardijordao@gmail.com>
2016-09-14 17:04:34 +0100

    2016-09-14's java workout

---
+++ contributions/ArthurJordao/java/I/O/2016-09-14.java
@@ -0,0 +1,5 @@
+Checking for the existence of files and directories
+Reading file attributes
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+List contents of a directory with `newDirectoryStream`
