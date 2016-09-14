e991c9e42e0fdd88ef73cb10da35f08cc14204b9
zamzami <ziad.zamzami@gmail.com>
2016-09-14 17:53:12 +0100

    2016-09-14's linux workout

---
+++ contributions/zamzami/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Terminal based browser
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
