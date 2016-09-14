9e645b83f562b1017ec25acfba9b45c494c179f7
tyronegrech <grechtyrone1989@gmail.com>
2016-09-14 18:14:19 +0100

    2016-09-14's git workout

---
+++ contributions/tyronegrech/git/Tools/2016-09-14.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Interactively unstage changes
+Useful stashing options
+Create a new branch from a stash
+Remember the results of previous hunk conflicts
