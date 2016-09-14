10a6edf94078487b5fbb7db0d36f8411ca9c7af8
IceFoxX <icefoxx@aikq.de>
2016-09-14 11:24:25 +0100

    2016-09-14's linux workout

---
+++ contributions/IceFoxX/linux/System Management/2016-09-14.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Test disk speed with `dd`
+Executing scripts on system boot using `systemd`
+Using `dd` To Create Disk Images
