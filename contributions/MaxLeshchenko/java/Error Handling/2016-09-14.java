a6a091b009f3936384f9761d93e1f2f7666e05d9
MaxLeshchenko <maksim.lesh@gmail.com>
2016-09-14 17:14:21 +0100

    2016-09-14's java workout

---
+++ contributions/MaxLeshchenko/java/Error Handling/2016-09-14.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Using exceptions in Java
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
