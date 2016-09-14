d0c550e1b7b4505f0e776403726b9a2c87fc2695
g3mini <mcjanz@gmail.com>
2016-09-14 09:32:19 +0100

    2016-09-14's java workout

---
+++ contributions/g3mini/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between JRE and JDK?
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
