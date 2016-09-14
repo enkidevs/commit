96723bc24e4ccc94b2105b8828249d4c14036042
alter2000 <altnate2000@gmail.com>
2016-09-14 20:44:04 +0100

    2016-09-14's linux workout

---
+++ contributions/alter2000/linux/File Management/2016-09-14.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
