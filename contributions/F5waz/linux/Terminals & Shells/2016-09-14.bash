7a043ec477d84969c632080d43119a979e33665c
F5waz <fawaz.saumtally@gmail.com>
2016-09-14 08:32:16 +0100

    2016-09-14's linux workout

---
+++ contributions/F5waz/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Find any Unix / Linux command
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
