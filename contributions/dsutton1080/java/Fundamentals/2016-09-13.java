5fe9fd911d5e1e850bf536eca0d6b03cbb6971ae
dsutton1080 <dsutton1080@gmail.com>
2016-09-14 04:13:15 +0100

    2016-09-13's java workout

---
+++ contributions/dsutton1080/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Ternary operator
+Catch multiple exceptions in a single `catch` block
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
