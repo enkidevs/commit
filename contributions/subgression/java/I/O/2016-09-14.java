6043c51d8c1a5040f4d96cc50eaf38033a18fb53
subgression <vaila94.imac@gmail.com>
2016-09-14 07:46:00 +0100

    2016-09-14's java workout

---
+++ contributions/subgression/java/I/O/2016-09-14.java
@@ -0,0 +1,5 @@
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Using the `OpenOption...` vararg to define file writing behavior
+List contents of a directory with `newDirectoryStream`
