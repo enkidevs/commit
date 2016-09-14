efc1e7e01e7159950721486b3e26808b5603a8aa
Campa <euller_sl@hotmail.com>
2016-09-14 12:00:02 +0100

    2016-09-14's linux workout

---
+++ contributions/Campa/linux/Networking/2016-09-14.bash
@@ -0,0 +1,5 @@
+Debugging `ssh` client issues
+Logging dropped firewall packets
+Find services running on your host
+Limit the `wget` download rate
+Use `tcpdump` to listen to network interface traffic
