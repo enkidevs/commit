106ef55d1d6e41412663cb4cfac8e711562bf690
mihaiberq <mihaiberq@gmail.com>
2016-09-14 08:53:41 +0100

    2016-09-14's java workout

---
+++ contributions/mihaiberq/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Common mistake on switch statements
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
