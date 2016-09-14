aabd1ebdbd70ff7e8398bcdb29027b259d6e8b10
linusyoung <linusyoungrice@gmail.com>
2016-09-14 13:17:03 +0100

    2016-09-13's linux workout

---
+++ contributions/linusyoung/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Commands to shutdown or restart the system
+Finding Open Files With `lsof`
