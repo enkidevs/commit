5500b012ef62eba4cf1cb1ced596f25a704e9787
laffont.sylvain <laffont.sylvain@gmail.com>
2016-09-14 14:32:25 +0100

    2016-09-14's linux workout

---
+++ contributions/laffont.sylvain/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
