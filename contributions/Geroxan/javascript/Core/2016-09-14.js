1eaf0de7946928e1b70ffc89fdeb5d55e07f8937
Geroxan <alik810@seznam.cz>
2016-09-14 19:29:33 +0100

    2016-09-14's javascript workout

---
+++ contributions/Geroxan/javascript/Core/2016-09-14.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Use `splice()` to remove an item from an array
+Infix operators are left-associative
+The difference between null, undefined and NaN
+JavaScript compilation
