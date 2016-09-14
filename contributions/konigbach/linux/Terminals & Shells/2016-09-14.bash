5319ca5660b0e345e15c663175f9918794c07e3d
konigbach <jsbachrocks@gmail.com>
2016-09-14 23:55:22 +0100

    2016-09-14's linux workout

---
+++ contributions/konigbach/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Useful aliasing in bash
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
