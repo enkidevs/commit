a7fc40ba7bbe2f2ff50d8b85ad2efca171063f85
gridkid98 <rnhoward51@gmail.com>
2016-09-14 09:39:28 +0100

    2016-09-13's linux workout

---
+++ contributions/gridkid98/linux/Terminals & Shells/2016-09-13.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
