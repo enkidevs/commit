6ec9c3640666bcfc9d59d8c5ddca5bfcdc303bed
yarg0007 <benjamin.yarger@gmail.com>
2016-09-14 12:46:59 +0100

    2016-09-14's java workout

---
+++ contributions/yarg0007/java/Java 8/2016-09-14.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Using `Arrays.parallelSort` 
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
