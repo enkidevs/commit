996fe354808847da43e62f635409165bc17a9fb0
keks411 <osterwald@posteo.de>
2016-09-14 05:04:24 +0100

    2016-09-13's linux workout

---
+++ contributions/keks411/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
