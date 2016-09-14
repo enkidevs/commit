d5dcad9ac13ac5e26426fae4aa0ad95805259d5e
martin-rifon <mrifonelizalde@gmail.com>
2016-09-14 02:22:48 +0100

    2016-09-13's javascript workout

---
+++ contributions/martin-rifon/javascript/Node/2016-09-13.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+**Node** `os` module
+Wrapping errors in Node.js using _node-verror_
+`exports` vs. `module.exports` in **Node**
+`try-catch` only for **sync** code
