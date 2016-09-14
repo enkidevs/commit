5c7319d05f609959ec75c38b6c3c53bfc2ddbd84
rokadias <saqib@saqibr.com>
2016-09-14 02:34:47 +0100

    2016-09-13's linux workout

---
+++ contributions/rokadias/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
