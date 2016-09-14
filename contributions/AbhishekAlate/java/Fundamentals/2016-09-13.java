2969d16bc05b81b2f4847882f6843c6c3364b9c8
AbhishekAlate <abhishek.alate@hotmail.com>
2016-09-14 21:07:39 +0100

    2016-09-13's java workout

---
+++ contributions/AbhishekAlate/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
