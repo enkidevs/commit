3cc31ea5bb85784add3f6957218ffce794abcdd2
vfi <davion.groovy@gmail.com>
2016-09-14 15:03:26 +0100

    2016-09-14's java workout

---
+++ contributions/vfi/java/Classes & Objects/2016-09-14.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Use a copy constructor for non-immutable objects
+Utility classes and `static` methods
+Compare classes and not class names
+Fields with Java **Reflection API**
