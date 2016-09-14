1292d6788a0f5d68ba99be35ae5dec6ec1218067
marcin.binczyk <marcin.binczyk@gmail.com>
2016-09-14 15:56:43 +0100

    2016-09-14's java workout

---
+++ contributions/marcin.binczyk/java/Java 8/2016-09-14.java
@@ -0,0 +1,5 @@
+Java 8 Lambda expressions
+How to check for static dependencies of applications and libraries from the command line.
+Control OS `Process`es in `Java8`
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
