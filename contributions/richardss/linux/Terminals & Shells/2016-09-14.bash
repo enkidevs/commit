2dd32bed143db3d5c95837e75893fb77f4bcf2a4
richardss <rolandka88@gmail.com>
2016-09-14 10:45:28 +0100

    2016-09-14's linux workout

---
+++ contributions/richardss/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Useful aliasing in bash
+Random password generator
+Breaking out of a terminal when `ssh` locks
