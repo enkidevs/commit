783ae6b95518ff2f25f7f50a16fa0c9966d134db
araghunde <paybol.21@gmail.com>
2016-09-14 16:22:06 +0100

    2016-09-14's java workout

---
+++ contributions/araghunde/java/Data Types/2016-09-14.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Converting a string to upper or lower case
+Retrieve the component type of an array
