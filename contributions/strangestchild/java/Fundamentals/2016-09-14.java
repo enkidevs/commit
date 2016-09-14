da0e6a6c90f6846604d351c717c619c0ad5ec29a
strangestchild <stefankopieczek@gmail.com>
2016-09-14 06:39:33 +0100

    2016-09-14's java workout

---
+++ contributions/strangestchild/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
