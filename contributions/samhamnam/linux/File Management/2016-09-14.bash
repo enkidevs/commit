2bc665362e1373c26c15effc61c961a2358f86be
samhamnam <samuel.hammersberg@gmail.com>
2016-09-14 06:48:58 +0100

    2016-09-14's linux workout

---
+++ contributions/samhamnam/linux/File Management/2016-09-14.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
