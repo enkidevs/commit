72131b2ce18f2a5c9c0f2e1b77143515539d2b6e
DavideRoss <davide.ross93@gmail.com>
2016-09-14 08:04:04 +0100

    2016-09-14's linux workout

---
+++ contributions/DavideRoss/linux/System Management/2016-09-14.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
