98e6786209c27c1223c84b09d3130d6766057965
Lagaffe212 <robapaulhenry@gmail.com>
2016-09-14 17:30:06 +0100

    2016-09-08's java workout

---
+++ contributions/Lagaffe212/java/I/O/2016-09-08.java
@@ -0,0 +1,5 @@
+Using buffered streams
+Reading file attributes
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+List contents of a directory with `newDirectoryStream`
