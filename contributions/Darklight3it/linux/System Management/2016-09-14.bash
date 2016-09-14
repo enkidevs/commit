93210d73aef7420590b62635602887381620ba32
Darklight3it <darklight3it@gmail.com>
2016-09-14 19:56:22 +0100

    2016-09-14's linux workout

---
+++ contributions/Darklight3it/linux/System Management/2016-09-14.bash
@@ -0,0 +1,7 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
