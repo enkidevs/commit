88e3d1f2ae0ec309f86125370bf71738169c5018
jaylenw <mr2cleanj@live.com>
2016-09-15 00:18:49 +0100

    2016-09-14's java workout

---
+++ contributions/jaylenw/java/I/O/2016-09-14.java
@@ -0,0 +1,5 @@
+Create system file paths using the `Path` class
+Using buffered streams
+Checking for the existence of files and directories
+Reading file attributes
+Using the `OpenOption...` vararg to define file writing behavior
