0b6f71ad720c3a844daa590670098617822e7d09
Albin <albin.theander@gmail.com>
2016-09-14 21:02:31 +0100

    2016-09-14's linux workout

---
+++ contributions/Albin/linux/File Management/2016-09-14.bash
@@ -0,0 +1,5 @@
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Using `touch` to alter files modification time
+Delete commands aliases
+Easy way of sharing files
