57ca3a101622400bd8e6f53faad15e93b802a309
omata <ctxo19@gmail.com>
2016-09-14 11:35:40 +0100

    2016-09-13's javascript workout

---
+++ contributions/omata/javascript/Core/2016-09-13.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Using `.test()` with RegExp
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+How to merge two arrays
