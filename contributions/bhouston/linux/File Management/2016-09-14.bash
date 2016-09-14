9d91bd551b41bfaf7b7c9200cd75c20db3d9784a
bhouston <enki@thehouston.space>
2016-09-14 15:53:41 +0100

    2016-09-14's linux workout

---
+++ contributions/bhouston/linux/File Management/2016-09-14.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
