0b66654879320681f678ce4ef9369683352dd330
ANyanCatFan <saitam.spatz@gmail.com>
2016-09-14 16:45:32 +0100

    2016-09-14's linux workout

---
+++ contributions/ANyanCatFan/linux/System Management/2016-09-14.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
