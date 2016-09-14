e79f62b965f9e303d8c6bc324a3027f22cba62dd
goethes <martial.goehry@gmail.com>
2016-09-14 21:12:35 +0100

    2016-09-14's linux workout

---
+++ contributions/goethes/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
