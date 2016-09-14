7c18ad46a0575a8c80941d89cd001501b9d74f24
JimS <james.solum@gmail.com>
2016-09-14 02:43:23 +0100

    2016-09-13's linux workout

---
+++ contributions/JimS/linux/Terminals & Shells/2016-09-13.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Find any Unix / Linux command
+Random password generator
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
