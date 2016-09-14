7048a9dd8e77ee1225fcda2cce7d483f5b10c9e7
asai95 <viktr5909@mail.ru>
2016-09-14 10:54:10 +0100

    2016-09-13's java workout

---
+++ contributions/asai95/java/I/O/2016-09-13.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Reading file attributes
