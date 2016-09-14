cfeb42ba77cad68b1b62a90f3cfe03b081f3ea3d
manitua <jasmin.beharic@gmail.com>
2016-09-14 06:09:29 +0100

    2016-09-14's linux workout

---
+++ contributions/manitua/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
