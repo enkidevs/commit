fbe6aee70137b40f3a61cf65a8f129cd14ea46a4
jarronshih <jarronshih@gmail.com>
2016-09-14 17:54:41 +0100

    2016-09-14's linux workout

---
+++ contributions/jarronshih/linux/System Management/2016-09-14.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Basic service management with `systemd`
