71331f0bb37869359411ff56a2c267c2f93962ac
renegat59 <bananq@gmail.com>
2016-09-14 18:21:50 +0100

    2016-09-14's linux workout

---
+++ contributions/renegat59/linux/Security/2016-09-14.bash
@@ -0,0 +1,5 @@
+Extracting the `public key` from the `private key`
+Block IPs using `Fail2ban`
+Best password generation utilities
+Disabling **USB** storage devices
+Private key encryption with `OpenSSL`
