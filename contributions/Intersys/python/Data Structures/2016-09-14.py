9ab7c9d3f2d60658c9318d3f775e14d48683f93e
Intersys <int3rsys@live.com>
2016-09-14 14:40:23 +0100

    2016-09-14's python workout

---
+++ contributions/Intersys/python/Data Structures/2016-09-14.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Immutable sets with `frozenset`
+Operations with `bytes` and `bytearray`
+A thread-safe `Queue`
+Special `queue` methods
