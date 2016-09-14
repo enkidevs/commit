a26bc58cde56c4990127852bc743c712a292b5c2
rasmuspedersen1992 <rasmuspedersen1992@gmail.com>
2016-09-14 21:38:02 +0100

    2016-09-14's java workout

---
+++ contributions/rasmuspedersen1992/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Inheriting a constructor from a superclass
+Implementing an `interface`
