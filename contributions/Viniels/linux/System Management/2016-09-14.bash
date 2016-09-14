ef383ac43768e9dc196c2665fe28b7abf92573e5
Viniels <vincent.geers@ziggo.nl>
2016-09-14 21:33:32 +0100

    2016-09-14's linux workout

---
+++ contributions/Viniels/linux/System Management/2016-09-14.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
