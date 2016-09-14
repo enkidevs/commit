a8e6c7ca28e2a12711c759b043f77106cb71b0d0
StackOverflow <nuti1996@gmail.com>
2016-09-14 15:51:43 +0100

    2016-09-14's java workout

---
+++ contributions/StackOverflow/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
