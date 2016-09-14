284e975f34949ac35e1391fa05c5caaf9b18b493
kevguy <kevlai22@uw.edu>
2016-09-14 15:49:00 +0100

    2016-09-13's javascript workout

---
+++ contributions/kevguy/javascript/React/2016-09-13.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Server-side rendering 
+Clone Elements in **React**
+Specify a single child
