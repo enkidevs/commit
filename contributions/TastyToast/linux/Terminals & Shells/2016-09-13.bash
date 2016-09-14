9d591bcb65e8f6a628cdfee67b2311322a8d9260
TastyToast <doniconner@gmail.com>
2016-09-14 06:19:56 +0100

    2016-09-13's linux workout

---
+++ contributions/TastyToast/linux/Terminals & Shells/2016-09-13.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
