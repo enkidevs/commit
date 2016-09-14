b21f32b27bc371384ac3b42712706cf13ceb50ad
HuShifang <stephen.p.ford@gmail.com>
2016-09-14 03:52:08 +0100

    2016-09-13's linux workout

---
+++ contributions/HuShifang/linux/System Management/2016-09-13.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
