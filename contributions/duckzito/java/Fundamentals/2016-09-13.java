35d7e81b09726c1b66dfbeff24dad95c0e70c80f
duckzito <duckzito@gmail.com>
2016-09-14 03:43:20 +0100

    2016-09-13's java workout

---
+++ contributions/duckzito/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Cropping images
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
