c3f3f98bbd367bbc69c0d2e5a135557330977624
emyarod <allllaboutyou@gmail.com>
2016-09-14 22:00:32 +0100

    2016-09-14's javascript workout

---
+++ contributions/emyarod/javascript/Core/2016-09-14.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Setting the length of an array 
+Hoisting applies only to variable declarations, not initializations
+Apply `map` function to array items
+Inaccuracy of binary floating-point format
