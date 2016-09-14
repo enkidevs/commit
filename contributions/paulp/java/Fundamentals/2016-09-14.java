a75d12808c19325088894227dcb0b960255599a2
paulp <paulp@hotmail.sk>
2016-09-14 05:26:39 +0100

    2016-09-14's java workout

---
+++ contributions/paulp/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
