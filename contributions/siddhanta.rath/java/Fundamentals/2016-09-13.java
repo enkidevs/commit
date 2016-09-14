209e66ddeedc01b1764df60acfbe5edd89d354cc
siddhanta.rath <siddhanta.rath@gmail.com>
2016-09-14 15:08:21 +0100

    2016-09-13's java workout

---
+++ contributions/siddhanta.rath/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Ternary operator
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+How range operations work
