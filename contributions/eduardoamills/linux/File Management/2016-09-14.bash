def60b8950f02cb0df734bdd28ac480fd0e0cbf6
eduardoamills <eduardoamills@gmail.com>
2016-09-15 00:57:24 +0100

    2016-09-14's linux workout

---
+++ contributions/eduardoamills/linux/File Management/2016-09-14.bash
@@ -0,0 +1,6 @@
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Easy way of sharing files
