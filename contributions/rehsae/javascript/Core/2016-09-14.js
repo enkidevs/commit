76821b142767635dd15ab38c39c7744672b27e41
rehsae <rehsae@gmail.com>
2016-09-14 14:57:58 +0100

    2016-09-14's javascript workout

---
+++ contributions/rehsae/javascript/Core/2016-09-14.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Setting the length of an array 
+Double and single quotes
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
