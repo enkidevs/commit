171b6156e22bf25606462926f947306dcd76ea89
SitiBanc <ci84n01dy@gmail.com>
2016-09-14 13:20:58 +0100

    2016-09-13's linux workout

---
+++ contributions/SitiBanc/linux/Terminals & Shells/2016-09-13.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Terminal incognito mode
+Sequential execution using the `;` statement separator
