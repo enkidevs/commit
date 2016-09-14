f9e2d59741057ad91d04e0a85f945e3fbbcdaede
philaser <philemonjo@gmail.com>
2016-09-14 10:26:42 +0100

    2016-09-14's java workout

---
+++ contributions/philaser/java/Error Handling/2016-09-14.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Creating new Exceptions
+Checking a double for `NaN` value
