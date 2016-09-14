b7269627f7eaeb866986778b05be76f454ab7528
lkjnym <lkjnym@gmail.com>
2016-09-14 09:39:06 +0100

    2016-09-13's javascript workout

---
+++ contributions/lkjnym/javascript/Core/2016-09-13.js
@@ -0,0 +1,5 @@
+Performing a function at timed intervals
+Data type comparison in `switch` statements
+No block scope
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
