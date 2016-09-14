26b1c08d0d6e3abb4a37b8bfee6eadbd22d4627a
aaronbiller <aaronbiller@gmail.com>
2016-09-14 13:40:52 +0100

    2016-09-14's linux workout

---
+++ contributions/aaronbiller/linux/System Management/2016-09-14.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Incorrect time on dual boot systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
