b07c7b755812f7d3703789e63d8fabd34fbfc8fc
dannyboyer <dannyboyer49@gmail.com>
2016-09-14 02:09:32 +0100

    2016-09-13's java workout

---
+++ contributions/dannyboyer/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
