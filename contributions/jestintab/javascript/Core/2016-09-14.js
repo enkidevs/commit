93c6b44b7ace73c2f2fa48922e85ebc86c24d053
jestintab <jestintab@gmail.com>
2016-09-14 11:57:25 +0100

    2016-09-14's javascript workout

---
+++ contributions/jestintab/javascript/Core/2016-09-14.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Performing a function at timed intervals
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Getting the *real* dimensions of an image
