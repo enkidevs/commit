db507ea0fa2ac1ca6f2b510cf3fc2d11c674f197
kossuth <steve.earl.choe@gmail.com>
2016-09-14 17:17:38 +0100

    2016-09-14's linux workout

---
+++ contributions/kossuth/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Execute a command without saving it in history
+Find any Unix / Linux command
+Random password generator
+Keep useful commands in your shell history with tags
