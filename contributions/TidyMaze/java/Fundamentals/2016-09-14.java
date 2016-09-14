8a10a1f0ad9987c3a94db06acb0e0da44c2cbbba
TidyMaze <rolland_yann@orange.fr>
2016-09-14 07:18:29 +0100

    2016-09-14's java workout

---
+++ contributions/TidyMaze/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Cropping images
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
