7595f5b1645861bff3d9b6294e73fe4224c82a30
replicat <replikay@gmail.com>
2016-09-14 10:01:59 +0100

    2016-09-13's javascript workout

---
+++ contributions/replicat/javascript/Functions/2016-09-13.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Functions can be declared after use
+`.bind()` 
+Anonymous functions
+Changing a functions context with `fn.call(object)`
