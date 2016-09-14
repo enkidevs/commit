e3341b328922fa764eae69db47db9a1739145bb9
Roanapur <alexis.aki@live.fr>
2016-09-14 21:27:20 +0100

    2016-09-14's linux workout

---
+++ contributions/Roanapur/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
