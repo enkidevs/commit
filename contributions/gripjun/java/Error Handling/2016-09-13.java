d37ecb4bc3d0034967abf6c64b896df6fe6de9d5
gripjun <gripjun@gmail.com>
2016-09-14 01:49:31 +0100

    2016-09-13's java workout

---
+++ contributions/gripjun/java/Error Handling/2016-09-13.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
