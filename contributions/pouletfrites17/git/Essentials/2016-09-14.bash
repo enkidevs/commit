52cd6987d713aa9c0253f929953d032e6aba2b4f
pouletfrites17 <alexandrapetitjean@hotmail.fr>
2016-09-14 22:27:26 +0100

    2016-09-14's git workout

---
+++ contributions/pouletfrites17/git/Essentials/2016-09-14.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+You can use git offline!
+Check the status of your files
+Use `short` status to make output more compact
+`master` and `origin` aren't special
