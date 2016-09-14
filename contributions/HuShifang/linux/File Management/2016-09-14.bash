703d66640dc5f1b82fa1aa33a4982c79a76c4062
HuShifang <stephen.p.ford@gmail.com>
2016-09-14 22:58:53 +0100

    2016-09-14's linux workout

---
+++ contributions/HuShifang/linux/File Management/2016-09-14.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Deleting non-empty directories
