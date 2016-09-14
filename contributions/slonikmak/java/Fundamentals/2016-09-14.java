eac518485151f6c03784857ed8c050098eaf3c68
slonikmak <slonikmak@gmail.com>
2016-09-14 08:56:45 +0100

    2016-09-14's java workout

---
+++ contributions/slonikmak/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Locks in `static synchronized` methods
+Always specify type parameter
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
