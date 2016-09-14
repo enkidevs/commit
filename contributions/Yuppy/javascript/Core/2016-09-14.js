aa72bf99c526df1915e8d65e77ef2068a50a87f9
Yuppy <yuppy@avd.pl>
2016-09-14 16:16:20 +0100

    2016-09-14's javascript workout

---
+++ contributions/Yuppy/javascript/Core/2016-09-14.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Use `splice()` to remove an item from an array
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Closures
