5f9764cd6b345e305dc39b2857b19f05587e60c9
kabelo2ka <kabelotooka@gmail.com>
2016-09-14 19:34:34 +0100

    2016-09-14's java workout

---
+++ contributions/kabelo2ka/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Detect or prevent integer overflow
