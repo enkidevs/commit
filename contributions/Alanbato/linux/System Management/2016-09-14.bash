884895e5a9edf5e90259f8b976f6a34995b6b9d9
Alanbato <alanvelasco.a@gmail.com>
2016-09-14 18:08:29 +0100

    2016-09-14's linux workout

---
+++ contributions/Alanbato/linux/System Management/2016-09-14.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
