c057e765d0059798fed98d9d6c607579e010cea7
CLA-C <brando_cla@hotmail.com>
2016-09-14 13:42:21 +0100

    2016-09-14's linux workout

---
+++ contributions/CLA-C/linux/System Management/2016-09-14.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Basic service management with `systemd`
