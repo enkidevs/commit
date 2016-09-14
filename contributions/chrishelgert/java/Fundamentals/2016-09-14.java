8b741a51a6ccc404d14327eff8caa0ef84d48979
chrishelgert <chris.helgert@yahoo.com>
2016-09-14 10:05:15 +0100

    2016-09-14's java workout

---
+++ contributions/chrishelgert/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
