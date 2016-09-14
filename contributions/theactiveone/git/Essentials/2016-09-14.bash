b4b3d37e43477eb01a550161b1d6d61bc0c180e3
theactiveone <maximilian.reimer.42@gmail.com>
2016-09-14 21:58:28 +0100

    2016-09-14's git workout

---
+++ contributions/theactiveone/git/Essentials/2016-09-14.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Using tags for version control
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
