22157e2c64dc38690332a030e558432a08503428
FloodResistantMirrorDrill <ballard37@gmail.com>
2016-09-14 13:07:26 +0100

    2016-09-14's linux workout

---
+++ contributions/FloodResistantMirrorDrill/linux/System Management/2016-09-14.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
