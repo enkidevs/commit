4b835100a0d77dcc9e6bc7985f0a5d0c4ddc58b8
mszmit <mszmitek@gmail.com>
2016-09-14 09:56:45 +0100

    2016-09-14's java workout

---
+++ contributions/mszmit/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
