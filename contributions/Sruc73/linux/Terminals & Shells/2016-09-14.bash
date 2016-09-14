223cd65a8099f354f41c7e12990add95a62f97f6
Sruc73 <mika73@laposte.net>
2016-09-14 12:53:55 +0100

    2016-09-14's linux workout

---
+++ contributions/Sruc73/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,5 @@
+Terminal based browser
+Useful aliasing in bash
+Random password generator
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
