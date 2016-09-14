5abaa8a33f0216951d8c3f429e998e1cf14c44a7
bbialek <bialcus@gmail.com>
2016-09-14 22:11:17 +0100

    2016-09-14's python workout

---
+++ contributions/bbialek/python/Data Structures/2016-09-14.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+The fundamental `tuples` 
+Best way to implement a simple `queue` 
+`set` operations
+Get the most of `float`s
