f28c0e02cec6cb31234e604cb3d777994982f669
Przemo9712 <przemo9712@gmail.com>
2016-09-14 07:18:18 +0100

    2016-09-14's linux workout

---
+++ contributions/Przemo9712/linux/System Management/2016-09-14.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
