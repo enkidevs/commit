18b613d62123c7985c49a845cad3ebbbb8ca7afc
robbieaverill <robbie@averill.co.nz>
2016-09-14 03:05:59 +0100

    2016-09-13's linux workout

---
+++ contributions/robbieaverill/linux/System Management/2016-09-13.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
