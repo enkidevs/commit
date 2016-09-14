ae20017530d09617b4d50c68171cfbfa6b45111c
sheryiu <yiu.cheuk.lok@gmail.com>
2016-09-14 04:30:57 +0100

    2016-09-13's java workout

---
+++ contributions/sheryiu/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
