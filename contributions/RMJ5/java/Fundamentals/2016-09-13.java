e9e717434d6a92cc37bcca872e7682155edcab8e
RMJ5 <rmj_8@hotmail.com>
2016-09-14 14:51:32 +0100

    2016-09-13's java workout

---
+++ contributions/RMJ5/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+Detect or prevent integer overflow
+Supply `toString()` in all classes
