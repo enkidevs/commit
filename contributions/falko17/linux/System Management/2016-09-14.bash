573e4ea4f1167639e9f71340ad3b92a47322a34d
falko17 <falko@plfisiagency.de.vu>
2016-09-14 17:52:08 +0100

    2016-09-14's linux workout

---
+++ contributions/falko17/linux/System Management/2016-09-14.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
