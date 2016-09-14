d351cf019a79bb78aee322a249f3522c18ce7d5f
spottus <lucabrandimarti@live.it>
2016-09-14 21:09:39 +0100

    2016-09-14's linux workout

---
+++ contributions/spottus/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Repeating commands with `watch`
+Execute a command without saving it in history
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
