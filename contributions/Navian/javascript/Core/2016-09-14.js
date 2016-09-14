580a48770defe6b2de13d1e09db97bd574d17046
Navian <lukasz.sadurski@gmail.com>
2016-09-14 18:33:28 +0100

    2016-09-14's javascript workout

---
+++ contributions/Navian/javascript/Core/2016-09-14.js
@@ -0,0 +1,5 @@
+Performing a function at timed intervals
+Filtering items out of an array
+The difference between null, undefined and NaN
+Extra function arguments are undefined by default
+Round numbers to `N` decimals 
