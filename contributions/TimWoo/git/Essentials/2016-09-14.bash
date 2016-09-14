8b22a38c6cf2f5e27626b6fd289f13b8359183e2
TimWoo <tim.wostemeier@gmail.com>
2016-09-14 19:58:05 +0100

    2016-09-14's git workout

---
+++ contributions/TimWoo/git/Essentials/2016-09-14.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Limiting log output by time
+How to write a git commit message
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
