30b3c10dde2783a1f5016aade22f6d87ae6bc872
jvasallo <joelvasallo@gmail.com>
2016-09-14 14:39:53 +0100

    2016-09-14's java workout

---
+++ contributions/jvasallo/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Supply `toString()` in all classes
+Using an interface as a parameter
