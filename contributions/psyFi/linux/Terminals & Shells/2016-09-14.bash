33be03d48e5d39b37b9c75edcfaeab4688ca5baa
psyFi <william@hatcher.work>
2016-09-14 17:25:53 +0100

    2016-09-14's linux workout

---
+++ contributions/psyFi/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
