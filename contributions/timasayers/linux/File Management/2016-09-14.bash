9f16385a1f95a42827a00d4c03a0bddbb1445cfd
timasayers <tim@timasayers.com>
2016-09-14 23:19:32 +0100

    2016-09-14's linux workout

---
+++ contributions/timasayers/linux/File Management/2016-09-14.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
