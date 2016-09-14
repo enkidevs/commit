b89953a8549f4df492b5acce3843f52696fa8753
fbjerggaard <mail@prooi.dk>
2016-09-14 09:49:38 +0100

    2016-09-14's java workout

---
+++ contributions/fbjerggaard/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
