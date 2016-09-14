7113b061ca025786f407e14c3d06216234783bc9
Brylex <brylexpl@gmail.com>
2016-09-14 23:20:42 +0100

    2016-09-14's javascript workout

---
+++ contributions/Brylex/javascript/Functions/2016-09-14.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Functions can be declared after use
+`.bind()` 
+Social sharing without widgets
