4bb34d9e27e820987ec168302c11a0270d35c24a
ipopov13 <thema3xman@gmail.com>
2016-09-14 08:06:01 +0100

    2016-09-14's python workout

---
+++ contributions/ipopov13/python/Data Structures/2016-09-14.py
@@ -0,0 +1,5 @@
+Following PEP 8 styling guideline.
+A simple way to select a random item from a `list/tuple` data stucture
+`Module`s everywhere!
+Special attributes of objects and classes
+Keep things in order with `OrderedDict`
