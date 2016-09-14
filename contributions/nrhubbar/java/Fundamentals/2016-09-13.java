136086d5fd6fe24094ce5cabf6dfe86d9c3fff8d
nrhubbar <14hubbardn@gmail.com>
2016-09-14 04:32:08 +0100

    2016-09-13's java workout

---
+++ contributions/nrhubbar/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
