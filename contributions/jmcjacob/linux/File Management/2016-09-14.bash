4d54499b5c054c5291953cd08fbb656c0b937b84
jmcjacob <jacob.carse@gmail.com>
2016-09-14 22:13:46 +0100

    2016-09-14's linux workout

---
+++ contributions/jmcjacob/linux/File Management/2016-09-14.bash
@@ -0,0 +1,5 @@
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Working with `.gz` files
+Delete commands aliases
