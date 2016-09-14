9755ba99b66dba943df0431552fb0793b1e5d399
Eidar <eidarare@gmail.com>
2016-09-14 08:11:15 +0100

    2016-09-14's javascript workout

---
+++ contributions/Eidar/javascript/Node/2016-09-14.js
@@ -0,0 +1,5 @@
+First-error callbacks in **Node**
+Wrapping errors in Node.js using _node-verror_
+Handle `JSON.parse` error in Node.js
+`process.nextTick()` callback pattern
+Automatic compilation for Node with **Nodemon** 
