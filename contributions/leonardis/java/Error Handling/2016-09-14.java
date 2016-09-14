3e0aae984f7cec9663f11d58069b97ac29b9f139
leonardis <leonardisrojas@gmail.com>
2016-09-14 13:15:11 +0100

    2016-09-14's java workout

---
+++ contributions/leonardis/java/Error Handling/2016-09-14.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Use try-with-resources instead of `finally`
+Prevent `exceptions` while logging Data
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
