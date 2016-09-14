f61b06a12a911a2b586069f69f77734159368e2d
eplooo <explo211@gmail.com>
2016-09-14 19:22:53 +0100

    2016-09-14's linux workout

---
+++ contributions/eplooo/linux/System Management/2016-09-14.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
