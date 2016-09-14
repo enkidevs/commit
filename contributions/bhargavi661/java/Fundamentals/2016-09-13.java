60f8823f4cf96f01ad31b4dd72ee4638a561ae70
bhargavi661 <bhargavi661@gmail.com>
2016-09-14 07:27:52 +0100

    2016-09-13's java workout

---
+++ contributions/bhargavi661/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+Always specify type parameter
