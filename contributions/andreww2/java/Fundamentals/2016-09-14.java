e8b31461699d58172d105384e99e671775060560
andreww2 <andreww21@gmail.com>
2016-09-14 20:27:11 +0100

    2016-09-14's java workout

---
+++ contributions/andreww2/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Using an interface as a parameter
