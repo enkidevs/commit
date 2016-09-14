456c6170f8cd4e25070244ce425d7424bb76b839
irvingaxelb <irvingb232@gmail.com>
2016-09-14 07:26:12 +0100

    2016-09-13's javascript workout

---
+++ contributions/irvingaxelb/javascript/Functions/2016-09-13.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Method chaining
+Easily generate a random `HEX` color
+Social sharing without widgets
+Check if a document is done loading
