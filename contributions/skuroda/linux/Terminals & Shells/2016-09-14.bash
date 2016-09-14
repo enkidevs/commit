ce6fdffbcd0d53d756cefdf798d215aeb0de7e3e
skuroda <sk@mieco.org>
2016-09-15 00:41:21 +0100

    2016-09-14's linux workout

---
+++ contributions/skuroda/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Quick `bash` shortcuts 
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
