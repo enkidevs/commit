ab870d293f1a3bfade41f4381be690df56a28a84
iMinion <k.srirammanoj@gmail.com>
2016-09-14 04:57:37 +0100

    2016-09-13's javascript workout

---
+++ contributions/iMinion/javascript/Core/2016-09-13.js
@@ -0,0 +1,5 @@
+Get a random item from an Array
+Keywords in hash keys
+Most efficient way to build `HTML` strings
+Double and single quotes
+Hoisting applies only to variable declarations, not initializations
