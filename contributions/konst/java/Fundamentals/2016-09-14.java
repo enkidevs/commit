2fdd5d2ebae6c528b6c229f2840233cf356a7041
konst <kosnar@gmail.com>
2016-09-14 12:57:56 +0100

    2016-09-14's java workout

---
+++ contributions/konst/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Cropping images
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
