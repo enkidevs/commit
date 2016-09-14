82643b590ad0d56d8ba9dfad6b46b6910a4d4937
DIrvin <drk.irvin90@gmail.com>
2016-09-14 20:42:31 +0100

    2016-09-14's java workout

---
+++ contributions/DIrvin/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
