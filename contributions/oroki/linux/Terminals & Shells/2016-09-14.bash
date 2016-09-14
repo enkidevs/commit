3af62f8a2ca3376baedc9123ec55346bbbe63a14
oroki <antonio.fracasso@gmail.com>
2016-09-14 07:28:10 +0100

    2016-09-14's linux workout

---
+++ contributions/oroki/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
