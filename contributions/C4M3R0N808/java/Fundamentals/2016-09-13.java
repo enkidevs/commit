6cb906e618d7028d2b1338bb45eb7fa1430e5ae7
C4M3R0N808 <css0012@uah.edu>
2016-09-14 03:49:19 +0100

    2016-09-13's java workout

---
+++ contributions/C4M3R0N808/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+Measuring time
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
