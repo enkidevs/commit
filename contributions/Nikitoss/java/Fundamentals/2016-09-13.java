6274a6f1118ddbe56d245a81e8211d315bab7ec6
Nikitoss <krutikov.nikita7@gmail.com>
2016-09-14 04:56:45 +0100

    2016-09-13's java workout

---
+++ contributions/Nikitoss/java/Fundamentals/2016-09-13.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
+Using an interface as a parameter
