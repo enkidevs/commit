1a3ffa8cb8a81de76f33c2971dd352fb94810a8a
ankman <thomas.ankerl@gmail.com>
2016-09-14 15:39:26 +0100

    2016-09-14's linux workout

---
+++ contributions/ankman/linux/System Management/2016-09-14.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Basic service management with `systemd`
