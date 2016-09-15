9aa48171e10fabf5c7f72ce837ab592277fdcda6
oldteacher <drsantos@gmail.com>
2016-09-15 19:17:54 +0100

    2016-09-15's linux workout

---
+++ contributions/oldteacher/linux/System Management/2016-09-15.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
