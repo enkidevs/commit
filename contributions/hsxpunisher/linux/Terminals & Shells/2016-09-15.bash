6ff22ac7dff5ae4167955eda1f18dc1d20ea371a
hsxpunisher <gavin.sortor@gmail.com>
2016-09-15 18:56:22 +0100

    2016-09-15's linux workout

---
+++ contributions/hsxpunisher/linux/Terminals & Shells/2016-09-15.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
