e87fca5630d3d640cdeae256c0bdbb01964c85a9
durandj <james.durand@alumni.msoe.edu>
2016-09-15 12:56:13 +0100

    2016-09-15's linux workout

---
+++ contributions/durandj/linux/System Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
