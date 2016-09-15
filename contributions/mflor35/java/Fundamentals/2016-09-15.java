208288b7fb1b5e9281bec1b3f8a68b4a70148405
mflor35 <floresmigu3l@gmail.com>
2016-09-15 20:19:02 +0100

    2016-09-15's java workout

---
+++ contributions/mflor35/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
