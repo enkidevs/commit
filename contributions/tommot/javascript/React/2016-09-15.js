843023ef4ab75a77e58982ed26f1cf21913b2df5
tommot <thomas.prebeck@web.de>
2016-09-15 09:47:36 +0100

    2016-09-15's javascript workout

---
+++ contributions/tommot/javascript/React/2016-09-15.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+`ref` callback attribute
+Custom validations for props
+Shortcut for transferring props
