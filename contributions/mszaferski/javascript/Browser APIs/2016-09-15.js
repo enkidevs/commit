8f4983217d376aa57bd68db271effe140e4015db
mszaferski <mszaferski@gmail.com>
2016-09-15 21:16:38 +0100

    2016-09-15's javascript workout

---
+++ contributions/mszaferski/javascript/Browser APIs/2016-09-15.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Use hosted scripts to increase performance
+Form a `URL` from its parts 
+Warn user if **Back** button is pressed
+Fetch API
