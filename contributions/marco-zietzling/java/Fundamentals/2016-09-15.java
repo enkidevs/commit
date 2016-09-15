757327e9d191add3c1adce7a8fd08b6c10bd5880
marco-zietzling <marco.zietzling@gmail.com>
2016-09-15 09:12:06 +0100

    2016-09-15's java workout

---
+++ contributions/marco-zietzling/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Connecting to FTP using Java
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
