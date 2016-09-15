c71423586d2c78db755775d10cc2405dd86d4c75
emreaktrk <emreaktrk@hotmail.com>
2016-09-15 15:48:10 +0100

    2016-09-15's java workout

---
+++ contributions/emreaktrk/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
