73fc2de2ebe5725583baec02ebbe41c453591507
dzienish <kamil.dzieniszewski@gmail.com>
2016-09-15 15:20:19 +0100

    2016-09-15's javascript workout

---
+++ contributions/dzienish/javascript/Browser APIs/2016-09-15.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use hosted scripts to increase performance
+Form a `URL` from its parts 
+FileReader.readAsDataURL()
+Page Visibility API
