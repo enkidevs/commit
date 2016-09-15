3bb1fa2c13ee535be2109b00a789f24037b6fd00
notroot <notroot@exler.info>
2016-09-15 21:00:46 +0100

    2016-09-15's java workout

---
+++ contributions/notroot/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
