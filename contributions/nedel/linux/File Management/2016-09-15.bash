ad7eec33a0cb72a06717a2a7120bd3e521c24b4d
nedel <nedel@gmx.com>
2016-09-15 05:43:35 +0100

    2016-09-15's linux workout

---
+++ contributions/nedel/linux/File Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Converting between Unix and Windows text files
+Using `touch` to alter files modification time
+Working with `.gz` files
