606559b07d0870d24eaf9d51301492a723f2f887
cryptichorizon <cryptichorizon@gmail.com>
2016-09-15 17:48:13 +0100

    2016-09-15's javascript workout

---
+++ contributions/cryptichorizon/javascript/Core/2016-09-15.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Hoisting applies only to variable declarations, not initializations
+Prevent background image flicker in **IE**
+Detect **DO NOT TRACK** status
+Getting the *real* dimensions of an image
