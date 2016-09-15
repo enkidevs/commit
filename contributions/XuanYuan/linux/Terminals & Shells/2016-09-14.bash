03869d6ca941ea60909e6fea5cd16958bb38f1ea
XuanYuan <ookk8282@gmail.com>
2016-09-15 16:25:09 +0100

    2016-09-14's linux workout

---
+++ contributions/XuanYuan/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+`else` statements using the `||` operator 
+Retrieve previous arguments
