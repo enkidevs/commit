4dca7448466208a3d6ec4bb56d57601ad3c68a5c
RMcNeely <rmcneely23@gmail.com>
2016-09-15 17:23:44 +0100

    2016-09-15's linux workout

---
+++ contributions/RMcNeely/linux/System Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
