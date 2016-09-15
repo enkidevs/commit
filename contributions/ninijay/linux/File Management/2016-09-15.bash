7d13d964574fb515ae73445600ed892edb0e80be
ninijay <ninijay93@gmail.com>
2016-09-15 15:48:24 +0100

    2016-09-15's linux workout

---
+++ contributions/ninijay/linux/File Management/2016-09-15.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
