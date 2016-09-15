2155f8774d29798f6d64937061db0dc770417980
pritansh <pritansh.chaudhary@gmail.com>
2016-09-15 10:39:05 +0100

    2016-09-14's java workout

---
+++ contributions/pritansh/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
