10e8d523023c8c6e3a5f704472544ccafe2e598e
Robyv2 <mail@roberthollander.nl>
2016-09-15 21:40:11 +0100

    2016-09-15's linux workout

---
+++ contributions/Robyv2/linux/System Management/2016-09-15.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
