94ef974b435f6da5979072e0a7dc1ed130d0f983
diego123 <bartosz.szymanski1@gmail.com>
2016-09-15 21:48:41 +0100

    2016-09-15's java workout

---
+++ contributions/diego123/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The trade-off between short and descriptive names
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
