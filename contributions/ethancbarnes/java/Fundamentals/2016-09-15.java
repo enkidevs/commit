dbce4ed450d9088901307d24e0bde38078929b8e
ethancbarnes <ethancbarnes@uga.edu>
2016-09-15 21:45:50 +0100

    2016-09-15's java workout

---
+++ contributions/ethancbarnes/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
+Using an interface as a parameter
