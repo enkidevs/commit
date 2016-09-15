5d39d763a54c07589ecaedf5ec99e2cb002b8498
Necrolisk <scoble.mr@gmail.com>
2016-09-15 09:43:44 +0100

    2016-09-14's linux workout

---
+++ contributions/Necrolisk/linux/System Management/2016-09-14.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
