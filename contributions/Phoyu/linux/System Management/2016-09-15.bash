54ecddc4043f7928f4a6d3955f80ae5961833003
Phoyu <mathias.phoyu@gmail.com>
2016-09-15 12:01:52 +0100

    2016-09-15's linux workout

---
+++ contributions/Phoyu/linux/System Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Get hardware stack details with `lspci`
