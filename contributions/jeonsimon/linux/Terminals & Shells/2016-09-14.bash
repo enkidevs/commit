6d424b3a01c757337de06e8105052f47d730ea93
jeonsimon <jeonsimon@gmail.com>
2016-09-15 01:39:27 +0100

    2016-09-14's linux workout

---
+++ contributions/jeonsimon/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,7 @@
+Find any Unix / Linux command
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
