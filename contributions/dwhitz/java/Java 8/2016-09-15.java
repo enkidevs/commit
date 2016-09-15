a0c9333fcb71b871a9d0d80660b16cae2c68f34c
dwhitz <dwhitz.mo@gmail.com>
2016-09-15 07:28:52 +0100

    2016-09-15's java workout

---
+++ contributions/dwhitz/java/Java 8/2016-09-15.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Using `Arrays.parallelSort` 
+Control OS `Process`es in `Java8`
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
