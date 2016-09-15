cd780503e1bb63e1d6dc667f30e56120fd5b9048
Xpdmk <roy.jarvinen@gmail.com>
2016-09-15 18:05:10 +0100

    2016-09-15's java workout

---
+++ contributions/Xpdmk/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Connecting to FTP using Java
+Catch multiple exceptions in a single `catch` block
+Distinction between `public` and `private` methods
+Measuring time
