22c8cafd779858344c1080f078619c835c4f8b49
kapcsi84 <jeno.kapcsandi@gmail.com>
2016-09-15 08:39:14 +0100

    2016-09-15's linux workout

---
+++ contributions/kapcsi84/linux/Terminals & Shells/2016-09-15.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Bash history reverse search
+Execute a command without saving it in history
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
