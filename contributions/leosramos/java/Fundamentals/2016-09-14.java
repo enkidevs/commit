4f00097bbe357786f33ec14e08a8863f35eff47e
leosramos <leosr89@gmail.com>
2016-09-15 04:20:34 +0100

    2016-09-14's java workout

---
+++ contributions/leosramos/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
