1b81caf10555a70d181343b06521a1779586e6b1
davidapye <davidasherpye@gmail.com>
2016-09-15 18:35:25 +0100

    2016-09-15's java workout

---
+++ contributions/davidapye/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Measuring time
+Always specify type parameter
+Do not return *references* to private *mutable* class members
