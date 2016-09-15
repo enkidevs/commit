bd1e9ce3bc12a4456f223d72837fb8c3feb8528c
psyFi <william@hatcher.work>
2016-09-15 14:25:37 +0100

    2016-09-15's linux workout

---
+++ contributions/psyFi/linux/Networking/2016-09-15.bash
@@ -0,0 +1,5 @@
+Debugging `ssh` client issues
+Logging dropped firewall packets
+Useful ssh client optimizations 
+Setting up password-free authentication
+Use `tcpdump` to listen to network interface traffic
