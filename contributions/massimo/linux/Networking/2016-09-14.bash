b66c1b85416ac475fab1839a1f26ff52dab74d03
massimo <massimobalestra@hotmail.com>
2016-09-15 04:59:51 +0100

    2016-09-14's linux workout

---
+++ contributions/massimo/linux/Networking/2016-09-14.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+Get real network statistics with `slurm`
+List your IPv6 configuration
+Make use of `netstat`
+Staying connected remotely via SSH without an interactive shell
