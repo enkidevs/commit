583140d4670cb166a950d282a3da33dba20241e9
Corey88 <lucascavalcante707@gmail.com>
2016-09-15 06:37:33 +0100

    2016-09-14's java workout

---
+++ contributions/Corey88/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Difference between `==` and `equals()`
+Distinction between `public` and `private` methods
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
