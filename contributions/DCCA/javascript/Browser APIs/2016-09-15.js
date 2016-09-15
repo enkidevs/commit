90de039e209e9d1c9e1531becd53efddb512564b
DCCA <dcca12@gmail.com>
2016-09-15 12:50:10 +0100

    2016-09-15's javascript workout

---
+++ contributions/DCCA/javascript/Browser APIs/2016-09-15.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+Modify a website's URL
+Navigating the browser history
+ProgressEvent
