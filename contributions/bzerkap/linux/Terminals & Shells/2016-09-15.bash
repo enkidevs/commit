a2f3172c76184aaa2acbe88abb02c4df155f375a
bzerkap <bankspaulw@gmail.com>
2016-09-15 18:40:10 +0100

    2016-09-15's linux workout

---
+++ contributions/bzerkap/linux/Terminals & Shells/2016-09-15.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
