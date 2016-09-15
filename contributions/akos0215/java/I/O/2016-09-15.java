82016d71ceaed465e22b16f98c9f9de159f79c50
akos0215 <akos0215@gmail.com>
2016-09-15 17:19:39 +0100

    2016-09-15's java workout

---
+++ contributions/akos0215/java/I/O/2016-09-15.java
@@ -0,0 +1,5 @@
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
