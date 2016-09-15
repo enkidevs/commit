e813593d01efabcd38a40a10b3f65569c3ffa6ce
zero9 <c.lobrano@gmail.com>
2016-09-15 12:45:18 +0100

    2016-09-15's linux workout

---
+++ contributions/zero9/linux/File Management/2016-09-15.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
