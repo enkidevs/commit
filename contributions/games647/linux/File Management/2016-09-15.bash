7710490e9d67dfafe8f678f070b2bb50e8bf3e78
games647 <games@tsprivat.de>
2016-09-15 07:53:58 +0100

    2016-09-15's linux workout

---
+++ contributions/games647/linux/File Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
+Delete commands aliases
