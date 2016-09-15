189a309ae797fbe1e3251cb6e9a45a597c3355e9
olman74 <ray.alexandre74@gmail.com>
2016-09-15 10:08:59 +0100

    2016-09-15's java workout

---
+++ contributions/olman74/java/Data Types/2016-09-15.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Converting numbers to strings
+Equals operation on different data types
+Retrieve the component type of an array
+String concatenation in Java
