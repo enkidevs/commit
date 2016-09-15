0149bdf7627c5bf78c74b98f587e4ff47d2820c5
bmcch <bruno.chegancas@gmail.com>
2016-09-15 08:36:23 +0100

    2016-09-15's linux workout

---
+++ contributions/bmcch/linux/File Management/2016-09-15.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
