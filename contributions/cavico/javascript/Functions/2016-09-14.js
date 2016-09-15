ad5d4207e9f80f816df3d35ad903f867974d7636
cavico <camilo@cavico.com.br>
2016-09-15 01:56:24 +0100

    2016-09-14's javascript workout

---
+++ contributions/cavico/javascript/Functions/2016-09-14.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Move cursor at the end of text input
+Social sharing without widgets
