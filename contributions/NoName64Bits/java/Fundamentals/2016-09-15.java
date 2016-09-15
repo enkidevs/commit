4b7ad3bcac0200b5eea12aa41e8beecb5bff2c4e
NoName64Bits <ciobanulaur@gmail.com>
2016-09-15 10:41:24 +0100

    2016-09-15's java workout

---
+++ contributions/NoName64Bits/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
