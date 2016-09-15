631d2d5102ac4c097309bd7b5444c267bad5c590
Eldergeek <david@geekguytech.com>
2016-09-15 17:11:14 +0100

    2016-09-15's linux workout

---
+++ contributions/Eldergeek/linux/Terminals & Shells/2016-09-15.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
