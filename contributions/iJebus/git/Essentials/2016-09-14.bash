90df439a04942aa7e10edeb9c7ffc6b9e6ee9e66
iJebus <liam.jones.aus@gmail.com>
2016-09-15 17:03:33 +0100

    2016-09-14's git workout

---
+++ contributions/iJebus/git/Essentials/2016-09-14.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to unmodify a modified file
+What is a checksum?
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
