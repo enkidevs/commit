e834e0b277413e7cb400c770f5408da77e794c5e
yasushi <yasushi.ohtsuka@gmail.com>
2016-09-16 00:54:27 +0100

    2016-09-15's java workout

---
+++ contributions/yasushi/java/I/O/2016-09-15.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+List contents of a directory with `newDirectoryStream`
