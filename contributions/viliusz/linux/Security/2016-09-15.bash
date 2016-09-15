941a99935f738eeb71382d6133ef92ab2d43ce6d
viliusz <enki@gmn.lt>
2016-09-15 18:55:30 +0100

    2016-09-15's linux workout

---
+++ contributions/viliusz/linux/Security/2016-09-15.bash
@@ -0,0 +1,5 @@
+Extracting the `public key` from the `private key`
+Disable SSH password authentication 
+Disabling **USB** storage devices
+Keeping `/boot` read-only
+Private key encryption with `OpenSSL`
