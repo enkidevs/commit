807788da3f4b760aa501428d26141501cc68728e
carpenma <carpenma@gmail.com>
2016-09-15 04:42:18 +0100

    2016-09-14's linux workout

---
+++ contributions/carpenma/linux/System Management/2016-09-14.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
