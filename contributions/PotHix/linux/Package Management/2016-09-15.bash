987376c4ff2809ba7bb5ee30b4013e422b0dac1c
PotHix <pothix@pothix.com>
2016-09-15 12:36:14 +0100

    2016-09-15's linux workout

---
+++ contributions/PotHix/linux/Package Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+List installed packages
+Prevent updating a specific package in Debian systems
+Using `PPAs`
