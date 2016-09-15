027226f0cfd2e755260d1591061309648f3fc23c
Logan <logan@commonbond.co>
2016-09-15 10:30:41 +0100

    2016-09-15's java workout

---
+++ contributions/Logan/java/Data Types/2016-09-15.java
@@ -0,0 +1,5 @@
+Converting numbers to strings
+Converting a string to upper or lower case
+Finding a substring in a string
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
