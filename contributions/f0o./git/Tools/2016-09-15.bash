a02f7a48199226cdc6d35781f431518e2b5439ff
f0o <jshshwusksjg@devilcode.org>
2016-09-15 18:40:40 +0100

    2016-09-15's git workout

---
+++ contributions/f0o./git/Tools/2016-09-15.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Specify a commit by its ancestry
+Specify a range of commits using double dot syntax
+Revision tools
+Remember the results of previous hunk conflicts
