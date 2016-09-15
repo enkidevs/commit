154b933f0d5cb27ebcb33433d6c24d4f95e24402
Xopoc <vasilij.zhukov@gmail.com>
2016-09-15 21:45:28 +0100

    2016-09-15's javascript workout

---
+++ contributions/Xopoc/javascript/Functions/2016-09-15.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Functions can be declared after use
+IIFE pattern
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
