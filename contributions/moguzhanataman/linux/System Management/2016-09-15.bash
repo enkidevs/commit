3388f367769b2fd8f6737f0935b4d1b58e030eec
moguzhanataman <thekhug@hotmail.com>
2016-09-15 16:58:35 +0100

    2016-09-15's linux workout

---
+++ contributions/moguzhanataman/linux/System Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+System uptime
