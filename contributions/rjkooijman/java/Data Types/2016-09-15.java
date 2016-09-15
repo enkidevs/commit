1126565cf47cfd133856e5cbb6286ed50b3fe956
rjkooijman <rjkooijman1995@gmail.com>
2016-09-15 15:05:49 +0100

    2016-09-15's java workout

---
+++ contributions/rjkooijman/java/Data Types/2016-09-15.java
@@ -0,0 +1,5 @@
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Finding a substring in a string
+Equals operation on different data types
+Do not attempt comparisons with NaN
