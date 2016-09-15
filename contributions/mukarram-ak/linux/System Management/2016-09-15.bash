94ba8cbf66ba1e3236db2a30f7a7022409e81000
mukarram-ak <ak.mukarram@yahoo.com>
2016-09-15 17:36:23 +0100

    2016-09-15's linux workout

---
+++ contributions/mukarram-ak/linux/System Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
