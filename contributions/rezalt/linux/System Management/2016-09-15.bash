3ce9fd8211565e325748048372a1e401782d253c
rezalt <ollobrains@gmail.com>
2016-09-15 20:09:12 +0100

    2016-09-15's linux workout

---
+++ contributions/rezalt/linux/System Management/2016-09-15.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Commands to shutdown or restart the system
+Cancel a system shutdown
