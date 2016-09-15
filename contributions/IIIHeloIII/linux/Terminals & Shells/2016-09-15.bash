43ec5ebff6179cdf9ffcf9d58b40a8a49f8479c0
IIIHeloIII <kristiandahl@gmail.com>
2016-09-15 16:34:06 +0100

    2016-09-15's linux workout

---
+++ contributions/IIIHeloIII/linux/Terminals & Shells/2016-09-15.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Useful aliasing in bash
+Get to know  your commands with `type`
