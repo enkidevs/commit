8de648c63b4dbd1b0f6ec8e0c15a75ceab8c31c0
AlexOlival <lex.olival@gmail.com>
2016-09-15 19:21:02 +0100

    2016-09-15's java workout

---
+++ contributions/AlexOlival/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Altering format string output by changing a format specifier's `argument_index`
+Constant notation
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
