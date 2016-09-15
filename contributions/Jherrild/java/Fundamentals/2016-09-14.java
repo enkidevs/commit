3394bdc6e2bd9d69f3c28280b9b2451c74154103
Jherrild <jestenh@gmail.com>
2016-09-15 07:32:51 +0100

    2016-09-14's java workout

---
+++ contributions/Jherrild/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
