b96f46f3aa45b131308f9849fe6880b15c2e45ab
stain88 <marcbaghdadi@gmail.com>
2016-09-15 09:05:40 +0100

    2016-09-15's git workout

---
+++ contributions/stain88/git/Tools/2016-09-15.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Specify a commit by its ancestry
+Useful stashing options
+Create a new branch from a stash
+Remember the results of previous hunk conflicts
