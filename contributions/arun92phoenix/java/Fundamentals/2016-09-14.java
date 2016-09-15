300c64b8d1337e7e7eb0785b3eba94a5f567bde9
arun92phoenix <arun92phoenix@gmail.com>
2016-09-15 16:52:39 +0100

    2016-09-14's java workout

---
+++ contributions/arun92phoenix/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+Supply `toString()` in all classes
