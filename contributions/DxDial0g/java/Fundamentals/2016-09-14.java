a66507b8bee39931c6110d199b57b110f7c2ca54
DxDial0g <drestrepohinc@gmail.com>
2016-09-15 04:29:02 +0100

    2016-09-14's java workout

---
+++ contributions/DxDial0g/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
