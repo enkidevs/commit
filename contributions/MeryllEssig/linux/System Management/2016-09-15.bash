db503ce95b59e41c934c7d054c9d27daea2c28a4
MeryllEssig <meryll.essig@free.fr>
2016-09-15 15:39:04 +0100

    2016-09-15's linux workout

---
+++ contributions/MeryllEssig/linux/System Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
