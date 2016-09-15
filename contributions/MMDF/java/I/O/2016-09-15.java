9da55b75b95df5d079083dd035e0f5b5665f6df2
MMDF <efeakca@gmail.com>
2016-09-15 21:36:15 +0100

    2016-09-15's java workout

---
+++ contributions/MMDF/java/I/O/2016-09-15.java
@@ -0,0 +1,5 @@
+How to list all files in a directory that match a filename extension in Java
+Using buffered streams
+Checking for the existence of files and directories
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
