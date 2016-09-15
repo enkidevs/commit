5008f4ce38e8fda86966feeefe08de62e99da1c2
jar7nt <jar7nt@gmail.com>
2016-09-15 14:45:54 +0100

    2016-09-14's linux workout

---
+++ contributions/jar7nt/linux/System Management/2016-09-14.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Cancel a system shutdown
