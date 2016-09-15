ae4b3f7390a1ece810aca4e1a8f11a677a68ee2a
danleyb2 <ndieksman@gmail.com>
2016-09-15 16:22:46 +0100

    2016-09-15's linux workout

---
+++ contributions/danleyb2/linux/System Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
