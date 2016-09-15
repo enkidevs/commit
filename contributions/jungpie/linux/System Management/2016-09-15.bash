8a68b4b59cf8a38b4ebb8926b87d0e5f924c2b29
jungpie <4straws@gmail.com>
2016-09-15 19:30:59 +0100

    2016-09-15's linux workout

---
+++ contributions/jungpie/linux/System Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
