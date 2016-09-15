b9c909276b8dccf05b2724f9b645f879016a12c2
balloman <benard.allotey4@yahoo.com>
2016-09-15 13:00:41 +0100

    2016-09-15's java workout

---
+++ contributions/balloman/java/Data Types/2016-09-15.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Converting numbers to strings
+Retrieve the component type of an array
+Numeric Conversion - Widening
