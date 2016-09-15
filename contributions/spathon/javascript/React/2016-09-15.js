0bbb72f3bc9d589fdd5c477dc42135cc48a8c568
spathon <enki@spathon.com>
2016-09-15 07:18:50 +0100

    2016-09-15's javascript workout

---
+++ contributions/spathon/javascript/React/2016-09-15.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+componentWillReceiveProps Not Triggered After Mounting
+
+`PureRenderMixin` in **React**
+`ref` callback attribute
+Shortcut for transferring props
