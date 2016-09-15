91b6e37e72e83737c0ec54ce2a1499fa22290800
Alioshel <cvl.escu@gmail.com>
2016-09-15 09:51:01 +0100

    2016-09-15's linux workout

---
+++ contributions/Alioshel/linux/System Management/2016-09-15.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
