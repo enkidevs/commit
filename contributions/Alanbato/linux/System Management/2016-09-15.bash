22c1680e03c44cc689681f46567b27cd5abecfb4
Alanbato <alanvelasco.a@gmail.com>
2016-09-15 14:41:52 +0100

    2016-09-15's linux workout

---
+++ contributions/Alanbato/linux/System Management/2016-09-15.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
