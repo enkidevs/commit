7c43c08df13b387c2afe0802012371e3d647d2d7
agr2003 <agr2003@gmail.com>
2016-09-15 11:47:29 +0100

    2016-09-15's java workout

---
+++ contributions/agr2003/java/I/O/2016-09-15.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Create system file paths using the `Path` class
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
