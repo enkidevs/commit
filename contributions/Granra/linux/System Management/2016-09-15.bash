80c802785a5aad887b23f4e005f69dd27d5fce59
Granra <granra@gmail.com>
2016-09-15 13:32:42 +0100

    2016-09-15's linux workout

---
+++ contributions/Granra/linux/System Management/2016-09-15.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
