eab020dca6c25c48e95ac7dbfc5a06ad0542bd41
ErikPohle <erikpohle@hotmail.com>
2016-09-15 07:33:20 +0100

    2016-09-14's java workout

---
+++ contributions/ErikPohle/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
