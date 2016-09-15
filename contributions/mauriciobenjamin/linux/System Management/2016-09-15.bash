fe79f7bc231c44ac6caedfa5c705009aa7814a66
mauriciobenjamin <mauriciobenjamin@gmail.com>
2016-09-15 13:56:51 +0100

    2016-09-15's linux workout

---
+++ contributions/mauriciobenjamin/linux/System Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
