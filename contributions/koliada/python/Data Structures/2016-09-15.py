b5ce5a1127cc07a764ae9bf05fc805fefae7d0e4
koliada <alex.fiator@gmail.com>
2016-09-15 07:39:44 +0100

    2016-09-15's python workout

---
+++ contributions/koliada/python/Data Structures/2016-09-15.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`del` statement for  lists
+`Dictionary` - standard mapping type
+Special attributes of objects and classes
+Looping techniques
