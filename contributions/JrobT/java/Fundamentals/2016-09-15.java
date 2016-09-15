45b6e5ac6555e82a2eac61a7b962a6fc8142a0df
JrobT <jack@trute.co.uk>
2016-09-15 11:47:14 +0100

    2016-09-15's java workout

---
+++ contributions/JrobT/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
