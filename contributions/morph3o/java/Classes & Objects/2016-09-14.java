4be42dfccf9db5f07c45424e02c2cf545d47c666
morph3o <pdivasto@gmail.com>
2016-09-15 01:21:02 +0100

    2016-09-14's java workout

---
+++ contributions/morph3o/java/Classes & Objects/2016-09-14.java
@@ -0,0 +1,5 @@
+Template methods in abstract classes
+Keeping fields `private`
+Compare classes and not class names
+Double Brace Initialization
+String formatting and parsing with `SimpleDateFormat`
