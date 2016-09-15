4af10b3e262f35d320bc323f6e304e7207709df5
code_geek <asparsh29kumar@gmail.com>
2016-09-15 03:10:43 +0100

    2016-09-14's linux workout

---
+++ contributions/code_geek/linux/System Management/2016-09-14.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
