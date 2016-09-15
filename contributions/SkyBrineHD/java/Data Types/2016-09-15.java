371261f0bd3f43afbd7a8cf6e957c08a5b0ff188
SkyBrineHD <felixlkunz@gmail.com>
2016-09-15 21:32:44 +0100

    2016-09-15's java workout

---
+++ contributions/SkyBrineHD/java/Data Types/2016-09-15.java
@@ -0,0 +1,5 @@
+Two ways to use an `Iterator`
+Multidimensional array declaration
+Converting a string to upper or lower case
+Equals operation on different data types
+Retrieve the component type of an array
