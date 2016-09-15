b5e253303376d92c13b68cd9e3647eed30c9f39e
robsrac <robs.rac@gmail.com>
2016-09-15 18:59:39 +0100

    2016-09-15's javascript workout

---
+++ contributions/robsrac/javascript/Node/2016-09-15.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Synchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+Use conventional property names for error handling in Node.js
+Automatic compilation for Node with **Nodemon** 
