fd0e43dabdd751b998c117c1c1fa7562fc770d72
avborhanian <dobbybabee@gmail.com>
2016-09-15 12:52:39 +0100

    2016-09-15's python workout

---
+++ contributions/avborhanian/python/Data Structures/2016-09-15.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Prioritize your `queue` 
+Special `queue` methods
+`weakref` callbacks
+Binary record packing with `bytearray`
