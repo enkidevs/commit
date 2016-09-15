d542377bc637ea16a16e64b6008938db32e1d3ff
AnOliver <oliverjohnyoung@gmail.com>
2016-09-15 03:32:47 +0100

    2016-09-14's java workout

---
+++ contributions/AnOliver/java/Error Handling/2016-09-14.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Throwing an `exception`
