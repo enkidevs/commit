d3dcbee16db63a2d6a355a13b07f928c7666e5f4
chrussellwalker <c.russell.walker@gmail.com>
2016-09-15 15:03:31 +0100

    2016-09-15's linux workout

---
+++ contributions/chrussellwalker/linux/Terminals & Shells/2016-09-15.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
+The Basics of Environment Variables
