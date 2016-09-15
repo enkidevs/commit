ec9175f48b902e00f4b5c212f78fca4439eb530c
bartlex <artiom.budnikoff@yandex.ru>
2016-09-15 05:19:14 +0100

    2016-09-15's javascript workout

---
+++ contributions/bartlex/javascript/Functions/2016-09-15.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+IIFE pattern
+Changing a functions context with `fn.call(object)`
