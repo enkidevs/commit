3a3ec1ceb3fb7e774bfa5f3d84fc3315757f2777
nsieber <nsieber@gmail.com>
2016-09-15 17:22:42 +0100

    2016-09-15's linux workout

---
+++ contributions/nsieber/linux/System Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Find out if the system's architecture is 32 or 64 bit
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
