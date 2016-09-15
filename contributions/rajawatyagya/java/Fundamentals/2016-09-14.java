ae061eff4a56027b3bdbba137649ae3a1501017c
rajawatyagya <rajawatyagya@gmail.com>
2016-09-15 21:29:40 +0100

    2016-09-14's java workout

---
+++ contributions/rajawatyagya/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
