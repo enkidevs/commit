c9f64a86a0647f8574c7e5af15d1aee3eac7a477
Gakin <gakin@usc.edu>
2016-09-16 00:02:26 +0100

    2016-09-15's linux workout

---
+++ contributions/Gakin/linux/Terminals & Shells/2016-09-15.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
