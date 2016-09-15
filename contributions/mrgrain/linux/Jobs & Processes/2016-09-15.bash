7ff43badf371e09417feedb3345f2150ae152cb8
mrgrain <me@momo.kiwi>
2016-09-15 21:55:15 +0100

    2016-09-15's linux workout

---
+++ contributions/mrgrain/linux/Jobs & Processes/2016-09-15.bash
@@ -0,0 +1,5 @@
+Keeping processes running after you log out
+`Cron` dot-in-filename issues when using `run-parts`
+Fixing the shell in cron
+Common issue with scripts in `cron`
+Set a process priority
