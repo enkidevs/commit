f20420c38983b112bf28dcc96a150950f851bb65
rayjee <formergod@gmail.com>
2016-09-15 04:37:00 +0100

    2016-09-15's java workout

---
+++ contributions/rayjee/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Ternary operator
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
