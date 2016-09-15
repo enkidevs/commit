f19fce0297fd839f1ff997933dd9e2d782dbe155
liko28s <liko28s@gmail.com>
2016-09-15 12:05:04 +0100

    2016-09-15's java workout

---
+++ contributions/liko28s/java/Data Types/2016-09-15.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Converting numbers to strings
+Converting a string to upper or lower case
+`StringBuffer` vs `StringBuilder`
