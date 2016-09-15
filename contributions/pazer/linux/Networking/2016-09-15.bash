800aa5b5cb6931924e21aac4c55635812db52479
pazer <pazer@gmx.com>
2016-09-15 19:56:01 +0100

    2016-09-15's linux workout

---
+++ contributions/pazer/linux/Networking/2016-09-15.bash
@@ -0,0 +1,5 @@
+Logging dropped firewall packets
+Useful ssh client optimizations 
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Short Introduction to `Wget` 
