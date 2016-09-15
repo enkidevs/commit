174eb51dd2761132f2f39e08fa47c4051d635974
gkhnoisgtht <m81a12@gmail.com>
2016-09-15 19:39:32 +0100

    2016-09-15's linux workout

---
+++ contributions/gkhnoisgtht/linux/Networking/2016-09-15.bash
@@ -0,0 +1,5 @@
+Disable `IPv6`
+Useful ssh client optimizations 
+Sending emails from terminal / scripts
+Bandwidth monitoring tools 
+Staying connected remotely via SSH without an interactive shell
