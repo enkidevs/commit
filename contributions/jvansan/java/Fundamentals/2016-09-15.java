0413bc81894a881b3721c7b19356fdd6d1b58916
jvansan <jeff.van.santen56@gmail.com>
2016-09-15 17:07:31 +0100

    2016-09-15's java workout

---
+++ contributions/jvansan/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Ternary operator
+Using the `Console`
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
