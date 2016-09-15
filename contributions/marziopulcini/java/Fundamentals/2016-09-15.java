177cb0e7c16d98bbae1f3fff2cb26fe14eaf2572
marziopulcini <marzio.pulcini@at-shopnet.com>
2016-09-15 09:33:13 +0100

    2016-09-15's java workout

---
+++ contributions/marziopulcini/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Difference between `==` and `equals()`
+Overloading Methods in Java
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
