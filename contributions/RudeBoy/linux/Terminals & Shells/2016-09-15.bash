2163d4898b331aca047b474ab4ad8c22aec40eb9
RudeBoy <romanisman@gmail.com>
2016-09-15 18:43:17 +0100

    2016-09-15's linux workout

---
+++ contributions/RudeBoy/linux/Terminals & Shells/2016-09-15.bash
@@ -0,0 +1,5 @@
+Clear bash history
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
