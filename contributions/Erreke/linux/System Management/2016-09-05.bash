9633138c6bc82d3ec697d0ef670449f72e1442e5
Erreke <ereke@list.ru>
2016-09-15 10:01:28 +0100

    2016-09-05's linux workout

---
+++ contributions/Erreke/linux/System Management/2016-09-05.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
