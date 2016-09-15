f71718c8a3ca783f9ff8ea5c1fe1ee737855e6ed
gridkid98 <rnhoward51@gmail.com>
2016-09-16 00:25:45 +0100

    2016-09-15's linux workout

---
+++ contributions/gridkid98/linux/File Management/2016-09-15.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Find the unknown process preventing deleting of files
+Easy way of sharing files
