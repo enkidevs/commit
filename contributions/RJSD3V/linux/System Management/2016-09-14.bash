e524103d667fea01330e5569b1e2434ffb706635
RJSD3V <rjsode25@gmail.com>
2016-09-15 07:47:14 +0100

    2016-09-14's linux workout

---
+++ contributions/RJSD3V/linux/System Management/2016-09-14.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
