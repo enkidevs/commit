51865d61304ef7983af0da608ec2a49a591e2972
carlosA <area.rua@gmail.com>
2016-09-15 11:59:37 +0100

    2016-09-15's linux workout

---
+++ contributions/carlosA/linux/System Management/2016-09-15.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
