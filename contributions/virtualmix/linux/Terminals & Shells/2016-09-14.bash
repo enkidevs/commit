4aef994785edf7724591bd71f08f12e0d14f8a7e
virtualmix <basiledeplante@gmail.com>
2016-09-15 04:16:13 +0100

    2016-09-14's linux workout

---
+++ contributions/virtualmix/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Quick `bash` shortcuts 
+Random password generator
+Keep useful commands in your shell history with tags
