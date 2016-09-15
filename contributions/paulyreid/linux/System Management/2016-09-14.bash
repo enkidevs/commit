b3721dcd8c482f9cfdde298922b8efb95e97d9c1
paulyreid <paulyreid@gmail.com>
2016-09-15 10:45:27 +0100

    2016-09-14's linux workout

---
+++ contributions/paulyreid/linux/System Management/2016-09-14.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Finding Open Files With `lsof`
