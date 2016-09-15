9283ae1113ce45c70427189d8362fbfc7c27e5dd
Gentu <ghager93@gmail.com>
2016-09-15 22:33:11 +0100

    2016-09-15's java workout

---
+++ contributions/Gentu/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
