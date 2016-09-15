058fe01d246970e608074f7f2b6030d34d279f9b
GeneralBison <general.bison@gmail.com>
2016-09-15 14:38:56 +0100

    2016-09-15's linux workout

---
+++ contributions/GeneralBison/linux/System Management/2016-09-15.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
