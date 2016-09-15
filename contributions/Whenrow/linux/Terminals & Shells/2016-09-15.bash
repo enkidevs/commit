2046056763e9856d24b6c46f15d2092206ca290e
Whenrow <william.henrotin@gmail.com>
2016-09-15 10:43:11 +0100

    2016-09-15's linux workout

---
+++ contributions/Whenrow/linux/Terminals & Shells/2016-09-15.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
