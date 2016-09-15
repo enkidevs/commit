d368a0fb0502b10d98e3cb33c4989b3e8e4b81fc
philrogersuk <philrogersuk@gmail.com>
2016-09-16 00:22:45 +0100

    2016-09-15's java workout

---
+++ contributions/philrogersuk/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
