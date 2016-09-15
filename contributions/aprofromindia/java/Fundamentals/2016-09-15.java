d11d4d79c4d4636b1e15c4c60e4f1f15ed47f459
aprofromindia <aprofromindia@hotmail.com>
2016-09-15 12:20:22 +0100

    2016-09-15's java workout

---
+++ contributions/aprofromindia/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
