d56141d03d799db8ee5faa35e88b4e6afeac91c9
george070393 <george070393@gmail.com>
2016-09-15 18:51:36 +0100

    2016-09-15's linux workout

---
+++ contributions/george070393/linux/File Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
