0b2a6e430c974ff869a4c212e57a76d821fa7497
jonathanabennett <doulos05@gmail.com>
2016-09-15 03:40:45 +0100

    2016-09-14's java workout

---
+++ contributions/jonathanabennett/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Detect or prevent integer overflow
+Supply `toString()` in all classes
+Using an interface as a parameter
