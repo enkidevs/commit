04064245ba0eadf9532a4efe883cd73e1103f1c6
ayanokouji <dithicus@gmail.com>
2016-09-15 17:08:15 +0100

    2016-09-15's linux workout

---
+++ contributions/ayanokouji/linux/Terminals & Shells/2016-09-15.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
