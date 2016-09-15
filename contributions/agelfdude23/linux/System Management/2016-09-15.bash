441efbbabf56782a3468a0984ac4329ba7803169
agelfdude23 <agelfdude23@gmail.com>
2016-09-15 19:35:19 +0100

    2016-09-15's linux workout

---
+++ contributions/agelfdude23/linux/System Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
