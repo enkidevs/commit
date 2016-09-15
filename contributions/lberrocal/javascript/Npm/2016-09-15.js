0659e2ffdc46df49789a5618e954ef8b7abb58c1
lberrocal <luisberrocal@outlook.com>
2016-09-15 16:15:40 +0100

    2016-09-15's javascript workout

---
+++ contributions/lberrocal/javascript/Npm/2016-09-15.js
@@ -0,0 +1,5 @@
+Abbreviate commands in npm
+Passing through command line arguments in npm scripts
+`optionalDependencies` in npm
+Current Lifecycle Event
+Lock down dependency versions by shrinkwrapping
