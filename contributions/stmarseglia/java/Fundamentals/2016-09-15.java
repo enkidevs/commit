966626430704b5e99cd32e127cdee76b8f5b6d92
stmarseglia <st.marseglia@gmail.com>
2016-09-15 14:10:34 +0100

    2016-09-15's java workout

---
+++ contributions/stmarseglia/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Supply `toString()` in all classes
