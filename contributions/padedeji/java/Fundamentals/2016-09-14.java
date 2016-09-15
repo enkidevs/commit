ec96729086efe10884ea311da81f9fb801c0ac4c
padedeji <adedejipaul97@gmail.com>
2016-09-15 05:24:16 +0100

    2016-09-14's java workout

---
+++ contributions/padedeji/java/Fundamentals/2016-09-14.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Distinction between `public` and `private` methods
+Using bounded type parameters in generic methods
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
