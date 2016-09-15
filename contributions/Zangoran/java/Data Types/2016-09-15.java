c2ba3461d30b199b4df6820f41e73290f67412ae
Zangoran <zach@landquist.net>
2016-09-15 13:14:04 +0100

    2016-09-15's java workout

---
+++ contributions/Zangoran/java/Data Types/2016-09-15.java
@@ -0,0 +1,5 @@
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Converting a string to upper or lower case
+`StringBuffer` vs `StringBuilder`
+Do not attempt comparisons with NaN
