c88b7ae18fb3a43f4b9f16d21c8f857752992fe3
papollo <apollo.piotrr@gmail.com>
2016-09-16 00:18:49 +0100

    2016-09-15's java workout

---
+++ contributions/papollo/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
