7dccd94737aadbc5d94d2d732cf6296d367ffffb
steved <stephendoyle75@gmail.com>
2016-09-15 09:39:40 +0100

    2016-09-15's linux workout

---
+++ contributions/steved/linux/File Management/2016-09-15.bash
@@ -0,0 +1,7 @@
+Practical `du` command
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
