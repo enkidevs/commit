1ba9189f64108aea6d0eea222843284229dd4a8a
LetishaB <leticia.baranyai@gmail.com>
2016-09-15 20:44:20 +0100

    2016-09-15's javascript workout

---
+++ contributions/LetishaB/javascript/Functions/2016-09-15.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Function call method
+Social sharing without widgets
+Check if a document is done loading
