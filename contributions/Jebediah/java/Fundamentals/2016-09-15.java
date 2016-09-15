754bf516a59af3905d96329cf124eeeba79f99a9
Jebediah <jefwillems100@gmail.com>
2016-09-15 08:15:04 +0100

    2016-09-15's java workout

---
+++ contributions/Jebediah/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Always specify type parameter
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
