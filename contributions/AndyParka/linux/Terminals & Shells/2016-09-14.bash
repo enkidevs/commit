74ae0be1a85f1d307c15126dc140b21d0a9eca42
AndyParka <andy_parka@hotmail.com>
2016-09-15 05:39:25 +0100

    2016-09-14's linux workout

---
+++ contributions/AndyParka/linux/Terminals & Shells/2016-09-14.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Terminal incognito mode
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
