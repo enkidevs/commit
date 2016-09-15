97c8c6741109c549a3ce3a7b2f8ea748d92a772a
willfri <github@willy-tech.de>
2016-09-15 22:20:59 +0100

    2016-09-15's git workout

---
+++ contributions/willfri/git/Essentials/2016-09-15.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to set your username and email
+Check the status of your files
+Use `short` status to make output more compact
+Remote repositories: fetching and pushing
