9f363381959360d3085a62482caa806def1a4670
fntc <fntc@list.ru>
2016-09-16 16:22:27 +0100

    2016-09-15's java workout

---
+++ contributions/fntc/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
