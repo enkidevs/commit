ad26381a3c5c8386de30c64ed1a70cb99f3d5cb9
voreny <voreny.gelio@gmail.com>
2016-09-16 15:44:32 +0100

    2016-09-16's javascript workout

---
+++ contributions/voreny/javascript/Functions/2016-09-16.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
