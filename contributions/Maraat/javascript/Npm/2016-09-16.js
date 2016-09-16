0a6204159d20c5e1d002fa4436cfc9f339b7ce21
Maraat <maraatto@gmail.com>
2016-09-16 09:38:36 +0100

    2016-09-16's javascript workout

---
+++ contributions/Maraat/javascript/Npm/2016-09-16.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+npm can handle multiple versions of a dependency
+Combining script commands in npm
+`npm` verifies `sha1` hashes of packages
+Tab completion in `npm`
