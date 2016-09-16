ec6b64c444f5a63e1d930cf1e92a796ea13ac3db
Milaine <milanweygandt@live.de>
2016-09-16 06:24:49 +0100

    2016-09-16's java workout

---
+++ contributions/Milaine/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+Measuring time
+Increase `PermGen` space as to avoid `OutOfMemory` errors
