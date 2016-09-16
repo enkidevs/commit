48e6bd2ad990665158c5e1a4a91d9bf6085d91e3
groundhogdad <groundhogdad@gmail.com>
2016-09-16 06:03:42 +0100

    2016-09-16's linux workout

---
+++ contributions/groundhogdad/linux/Networking/2016-09-16.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Logging dropped firewall packets
+Sending emails from terminal / scripts
+Setting up password-free authentication
+Use `tcpdump` to listen to network interface traffic
