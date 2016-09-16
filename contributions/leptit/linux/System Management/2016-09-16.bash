eff936190aefbf193d10de6f904a8d3feed46259
leptit <hugo.cosset@free.fr>
2016-09-16 19:58:36 +0100

    2016-09-16's linux workout

---
+++ contributions/leptit/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
