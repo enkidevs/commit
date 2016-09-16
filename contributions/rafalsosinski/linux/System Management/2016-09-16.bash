e5faed5b80d88dd76f21d9c30bb42c1124a3c23b
rafalsosinski <rafal.sosinski+github@gmail.com>
2016-09-16 06:20:22 +0100

    2016-09-16's linux workout

---
+++ contributions/rafalsosinski/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Get hardware stack details with `lspci`
