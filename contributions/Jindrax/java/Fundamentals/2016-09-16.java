34b016655da80ae9e7749e62c8227d5d7519edc1
Jindrax <jhvangar@gmail.com>
2016-09-16 11:13:17 +0100

    2016-09-16's java workout

---
+++ contributions/Jindrax/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Always specify type parameter
+Using an interface as a parameter
