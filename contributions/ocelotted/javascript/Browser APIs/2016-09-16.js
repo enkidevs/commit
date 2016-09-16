11c1318d079e9199885b563cd021a5dbc0ab6c9f
ocelotted <ocelotted@gmail.com>
2016-09-16 13:18:06 +0100

    2016-09-16's javascript workout

---
+++ contributions/ocelotted/javascript/Browser APIs/2016-09-16.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Async and defer scripts
+Use hosted scripts to increase performance
+Modify a website's URL
+Warn user if **Back** button is pressed
