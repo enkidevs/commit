3493b6c68ab88b325f79d5603b14b967ac7694a7
RodrigoAre <raksismundoputrido@gmail.com>
2016-09-16 18:52:18 +0100

    2016-09-16's javascript workout

---
+++ contributions/RodrigoAre/javascript/Browser APIs/2016-09-16.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Interaction with the user
+Drawing with HTML5 `Canvas` API
+Allow an element to go fullscreen
+FileReader.readAsDataURL()
