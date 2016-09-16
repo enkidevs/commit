266f96d949f07add8c34083ac02fcdcacbd8a465
agelfdude23 <agelfdude23@gmail.com>
2016-09-16 19:31:23 +0100

    2016-09-16's linux workout

---
+++ contributions/agelfdude23/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Using `dd` To Create Disk Images
