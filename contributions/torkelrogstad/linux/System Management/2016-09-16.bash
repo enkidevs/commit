9686117cc9723ce9fb5feea947a65ebeec2c1ef8
torkelrogstad <torkelrog@gmail.com>
2016-09-16 16:48:02 +0100

    2016-09-16's linux workout

---
+++ contributions/torkelrogstad/linux/System Management/2016-09-16.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Basic service management with `systemd`
