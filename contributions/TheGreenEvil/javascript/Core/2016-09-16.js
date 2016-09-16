4b0895f8de960a8cf1a10c01ddd48e39d640c5c1
TheGreenEvil <kirill.matsaberydze@gmail.com>
2016-09-16 17:53:21 +0100

    2016-09-16's javascript workout

---
+++ contributions/TheGreenEvil/javascript/Core/2016-09-16.js
@@ -0,0 +1,5 @@
+Treating a boolean as number
+Hoisting applies only to variable declarations, not initializations
+`console.*` in JavaScript
+Inaccuracy of binary floating-point format
+Base conversion 
