dee04dd4e1cf8dee565c745c78bedd826c0d4609
DrFox0 <samuele.pretto@gmail.com>
2016-09-16 06:46:45 +0100

    2016-09-16's linux workout

---
+++ contributions/DrFox0/linux/System Management/2016-09-16.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
