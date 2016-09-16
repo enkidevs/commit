bcdd95ae9f9edbb088105632e7efbad8d44692b1
abdelrhman <abdelrhmanwalid1@gmail.com>
2016-09-17 00:04:44 +0100

    2016-09-16's java workout

---
+++ contributions/abdelrhman/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Metadata: creating a user-defined file attribute
+Always specify type parameter
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
