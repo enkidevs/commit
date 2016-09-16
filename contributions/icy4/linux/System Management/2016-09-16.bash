cd3fe9c7b5e66fa96207ba80dac818cfaae82ddf
icy4 <krzysztof.jan.baj+enki@gmail.com>
2016-09-16 08:43:26 +0100

    2016-09-16's linux workout

---
+++ contributions/icy4/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Common log files
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
