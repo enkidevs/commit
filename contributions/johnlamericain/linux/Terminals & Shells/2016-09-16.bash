80c485783428434613d746e8a67328f196c2e2b0
johnlamericain <jonathan.courtois@gmail.com>
2016-09-16 08:50:00 +0100

    2016-09-16's linux workout

---
+++ contributions/johnlamericain/linux/Terminals & Shells/2016-09-16.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Find any Unix / Linux command
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
