4a265a73cfaaca9480aaff45e65226b32243a397
egoisticalgoat <pascal.pirschel@gmail.com>
2016-09-16 22:39:15 +0100

    2016-09-16's linux workout

---
+++ contributions/egoisticalgoat/linux/System Management/2016-09-16.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Basic service management with `systemd`
