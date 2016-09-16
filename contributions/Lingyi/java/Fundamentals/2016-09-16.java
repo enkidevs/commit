7b2c6e6da749c3f66313c388a5e61b282966364e
Lingyi <coerryb@gmail.com>
2016-09-16 10:37:51 +0100

    2016-09-16's java workout

---
+++ contributions/Lingyi/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
