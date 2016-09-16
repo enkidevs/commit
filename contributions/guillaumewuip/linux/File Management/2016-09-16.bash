9ba680f3d4abacf998e0e6f75bc4d097bb8b3cb6
guillaumewuip <clochard.guillaume@gmail.com>
2016-09-16 06:07:31 +0100

    2016-09-16's linux workout

---
+++ contributions/guillaumewuip/linux/File Management/2016-09-16.bash
@@ -0,0 +1,7 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
