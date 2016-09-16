bf9e642788b05b0208abefd6df20cdd95c1e1391
marcin.binczyk <marcin.binczyk@gmail.com>
2016-09-16 09:50:47 +0100

    2016-09-16's java workout

---
+++ contributions/marcin.binczyk/java/Data Types/2016-09-16.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
+String concatenation in Java
