33fc4f10dbc658ecf357da1f4577eb9974b4387a
Robilemb <robin.lembach@gmail.com>
2016-09-16 08:22:23 +0100

    2016-09-16's linux workout

---
+++ contributions/Robilemb/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
