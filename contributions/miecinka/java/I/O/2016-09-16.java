4ca669d19dbe5441db164dca30cf24ab7b36dbc4
miecinka <mietinhovanish@gmail.com>
2016-09-16 17:07:17 +0100

    2016-09-16's java workout

---
+++ contributions/miecinka/java/I/O/2016-09-16.java
@@ -0,0 +1,5 @@
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Reading file attributes
+Deleting files and directories
