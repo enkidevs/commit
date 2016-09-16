91c64f4d03b459835a9491fbb071e98633edf791
EclectickMedia <ariana.giroux@gmail.com>
2016-09-16 06:42:19 +0100

    2016-09-15's java workout

---
+++ contributions/EclectickMedia/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
