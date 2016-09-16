fc3639851f23c686291642a6b1a4edd412339313
francesco123 <fra.lombardo92@gmail.com>
2016-09-16 09:37:40 +0100

    2016-09-16's linux workout

---
+++ contributions/francesco123/linux/Terminals & Shells/2016-09-16.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Find any Unix / Linux command
+Terminal based browser
+Conditional command execution
+
+(`&&` operator)
