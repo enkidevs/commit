932a4d938876f262f930f1c9d31f50cc17a7a2d8
CarlyLilwall <carlylilwall@gmail.com>
2016-09-16 14:41:24 +0100

    2016-09-16's java workout

---
+++ contributions/CarlyLilwall/java/I/O/2016-09-16.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to list all files in a directory that match a filename extension in Java
+Create system file paths using the `Path` class
+Checking for the existence of files and directories
+List contents of a directory with `newDirectoryStream`
