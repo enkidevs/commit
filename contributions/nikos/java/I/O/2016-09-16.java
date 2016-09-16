92b76cecddc89fc3923c817a15eb17b3c0e32ce8
nikos <triantafyllou.ni@gmail.com>
2016-09-16 05:30:47 +0100

    2016-09-16's java workout

---
+++ contributions/nikos/java/I/O/2016-09-16.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
