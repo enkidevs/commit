dd4e29ecf6ffd3834368c1a98e13488d4aa80d51
manitua <jasmin.beharic@gmail.com>
2016-09-16 10:10:57 +0100

    2016-09-16's linux workout

---
+++ contributions/manitua/linux/Networking/2016-09-16.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+Disable `IPv6`
+Limit the `wget` download rate
+Use `tcpdump` to listen to network interface traffic
+Staying connected remotely via SSH without an interactive shell
