e998b83d6d83e8e40acb287920209a346f227e5c
tnm717 <thomas.masterson@gmail.com>
2016-09-16 16:16:29 +0100

    2016-09-16's linux workout

---
+++ contributions/tnm717/linux/Package Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Installing a `.deb` package from the terminal
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
