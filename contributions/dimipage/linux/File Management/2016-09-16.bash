052f816f165dab732891db645420b658a36de2fd
dimipage <dimi.page@gmail.com>
2016-09-16 11:17:22 +0100

    2016-09-16's linux workout

---
+++ contributions/dimipage/linux/File Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Delete commands aliases
