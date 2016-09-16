e8e864ed18239af8c96487043d69050d1298ec07
Malanius <malanius@seznam.cz>
2016-09-16 09:20:59 +0100

    2016-09-16's java workout

---
+++ contributions/Malanius/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+The distinction between overloading and overriding methods
+Inheriting a constructor from a superclass
+There is no such thing as *pass-by-reference* in Java
+`Autoboxing` and `Unboxing`
