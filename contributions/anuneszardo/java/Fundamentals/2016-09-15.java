ac0c34c712496091a04e5ee9a10b9a33858a8516
anuneszardo <anuneszardo@gmail.com>
2016-09-16 04:13:30 +0100

    2016-09-15's java workout

---
+++ contributions/anuneszardo/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Using `Printstream.format` to format a string before printing
+Always specify type parameter
