4c2394e201713db16e6219d76174180a9fcf81c0
BK874 <bjk69@pitt.edu>
2016-09-16 14:49:51 +0100

    2016-09-16's linux workout

---
+++ contributions/BK874/linux/File Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
+Easy way of sharing files
