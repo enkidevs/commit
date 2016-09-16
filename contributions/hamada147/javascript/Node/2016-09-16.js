ecb98cd6865e4ff97ab34ff0e64e75bf4f8c5863
hamada147 <moussa.ahmed95@gmail.com>
2016-09-16 09:34:58 +0100

    2016-09-16's javascript workout

---
+++ contributions/hamada147/javascript/Node/2016-09-16.js
@@ -0,0 +1,5 @@
+Wrapping errors in Node.js using _node-verror_
+`process.nextTick()` callback pattern
+Manage child processes in **Node**
+Factories design pattern
+Avoid using `sudo` when running Node
