2693500406e2a65af8963d777aec5dbc647a319d
fthebaud <fthebaud@gmail.com>
2016-09-16 07:19:12 +0100

    2016-09-16's java workout

---
+++ contributions/fthebaud/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
