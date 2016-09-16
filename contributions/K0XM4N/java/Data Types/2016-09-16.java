fa0c430ac2fbad379eb015b2907be4237396cc55
K0XM4N <radwan.krzysztof.1993@gmail.com>
2016-09-16 23:37:53 +0100

    2016-09-16's java workout

---
+++ contributions/K0XM4N/java/Data Types/2016-09-16.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Converting a string to upper or lower case
+Retrieve the component type of an array
