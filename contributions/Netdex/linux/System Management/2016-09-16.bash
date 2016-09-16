806e1c134ca096d9063570f03bfaaf34a2f0eaa7
Netdex <netdex593@gmail.com>
2016-09-16 18:29:19 +0100

    2016-09-16's linux workout

---
+++ contributions/Netdex/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
