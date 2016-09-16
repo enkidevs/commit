b2743ceb47cc448c62c3101c1066e407423f4049
tnargwoxow <tnargwoxow@gmail.com>
2016-09-16 08:36:12 +0100

    2016-09-16's linux workout

---
+++ contributions/tnargwoxow/linux/File Management/2016-09-16.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Delete commands aliases
