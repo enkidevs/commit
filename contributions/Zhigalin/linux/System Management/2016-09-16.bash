23a09718c55cb45a3040dade49f8713bb8818dc2
Zhigalin <alexander@zhigalin.tk>
2016-09-16 22:38:44 +0100

    2016-09-16's linux workout

---
+++ contributions/Zhigalin/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Detect your linux distribution
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
