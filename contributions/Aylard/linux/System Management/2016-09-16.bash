61f2784334575982fddedcfc1fdda73d1ae432be
Aylard <aylard7@gmail.com>
2016-09-16 21:50:03 +0100

    2016-09-16's linux workout

---
+++ contributions/Aylard/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
