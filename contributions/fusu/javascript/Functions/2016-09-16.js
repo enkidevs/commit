c47745599cb9a8bb5ba63cbf2800a3c03346304c
fusu <solaris1995@gmail.com>
2016-09-16 13:52:59 +0100

    2016-09-16's javascript workout

---
+++ contributions/fusu/javascript/Functions/2016-09-16.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Functions can be declared after use
+Get query/url variables
+Easily generate a random `HEX` color
+Changing a functions context with `fn.call(object)`
