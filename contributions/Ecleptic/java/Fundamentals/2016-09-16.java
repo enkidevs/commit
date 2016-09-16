6000d533ff938ac7068a5dee9396d6a24db1e400
Ecleptic <cameron@camerongreens.com>
2016-09-16 14:13:59 +0100

    2016-09-16's java workout

---
+++ contributions/Ecleptic/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+The trade-off between short and descriptive names
+Ternary operator
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
