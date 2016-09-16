ab8fee3916ec2f716e4dcef6413937fe9f603b05
pischke <pischke_t@gmx.net>
2016-09-16 20:16:15 +0100

    2016-09-16's linux workout

---
+++ contributions/pischke/linux/System Management/2016-09-16.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
