7e0e6f2bbda7fa6e857847af97761775103609bf
Marckendal <marckendal@gmail.com>
2016-09-16 09:55:23 +0100

    2016-09-16's linux workout

---
+++ contributions/Marckendal/linux/System Management/2016-09-16.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Basic service management with `systemd`
+Commands to shutdown or restart the system
