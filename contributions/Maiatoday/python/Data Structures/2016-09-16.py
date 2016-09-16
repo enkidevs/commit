61756e2643b34d8c9b1be51c3794f846c6bc999e
Maiatoday <maiatoday@gmail.com>
2016-09-16 20:35:54 +0100

    2016-09-16's python workout

---
+++ contributions/Maiatoday/python/Data Structures/2016-09-16.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Best way to implement a simple `queue` 
+Special attributes of objects and classes
+Get the most of `float`s
+There is more to copying
