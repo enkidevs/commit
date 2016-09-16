75a002f75e6c6cd81a9d0617f625f9f9a59c790d
ct2034 <post@henkelchristian.de>
2016-09-16 21:22:49 +0100

    2016-09-16's java workout

---
+++ contributions/ct2034/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Writing generic methods
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
