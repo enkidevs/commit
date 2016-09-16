161dcb20c9eb56e8828dcf89ba617b13b65088ee
vifon <findhill@gmail.com>
2016-09-16 08:37:51 +0100

    2016-09-15's java workout

---
+++ contributions/vifon/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
