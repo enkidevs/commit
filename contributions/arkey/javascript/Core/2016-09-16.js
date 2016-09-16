71571bf065076bb943d1e7844e9540a8a381524b
arkey <ilordarkey@gmail.com>
2016-09-16 07:59:02 +0100

    2016-09-16's javascript workout

---
+++ contributions/arkey/javascript/Core/2016-09-16.js
@@ -0,0 +1,5 @@
+Infix operators are left-associative
+Filtering items out of an array
+Apply `map` function to array items
+Functions return `undefined` by default
+Prevent background image flicker in **IE**
