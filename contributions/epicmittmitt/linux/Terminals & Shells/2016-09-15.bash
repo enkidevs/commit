be82314dbb220cc7809b1c75cb867307ef9140af
epicmittmitt <epicmittmitt@yahoo.com>
2016-09-16 03:15:14 +0100

    2016-09-15's linux workout

---
+++ contributions/epicmittmitt/linux/Terminals & Shells/2016-09-15.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Useful aliasing in bash
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
