30efe23d5781d0ab2dfdb25a6da82e211c78f78a
msamujlo <michal.samujlo@gmail.com>
2016-09-16 19:33:31 +0100

    2016-09-16's java workout

---
+++ contributions/msamujlo/java/Java 8/2016-09-16.java
@@ -0,0 +1,4 @@
+Defining `static` methods within an `interface`
+Java 8 Lambda expressions
+How to check for static dependencies of applications and libraries from the command line.
+`JJS` - Oracle Nashorn, newest JavaScript Engine
