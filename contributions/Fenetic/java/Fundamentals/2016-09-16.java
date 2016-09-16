bfa9d0b410147382b62f988244e1d5d6ff42614e
Fenetic <esyouit@gmail.com>
2016-09-16 19:52:57 +0100

    2016-09-16's java workout

---
+++ contributions/Fenetic/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
