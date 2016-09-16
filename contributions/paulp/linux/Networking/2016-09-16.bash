2592bc05780f13a5407dc2f6fc89666728665a91
paulp <paulp@hotmail.sk>
2016-09-16 09:44:22 +0100

    2016-09-16's linux workout

---
+++ contributions/paulp/linux/Networking/2016-09-16.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Useful ssh client optimizations 
+List your IPv6 configuration
+Setting up password-free authentication
+Use `tcpdump` to listen to network interface traffic
