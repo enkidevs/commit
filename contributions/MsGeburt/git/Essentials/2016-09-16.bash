a39f8be3965184378907c2c99e885ac548cbb699
MsGeburt <admin@marco-hajek.me>
2016-09-16 12:55:32 +0100

    2016-09-16's git workout

---
+++ contributions/MsGeburt/git/Essentials/2016-09-16.bash
@@ -0,0 +1,5 @@
+Pushing tags to a server
+Search by commit message keyword
+Use `short` status to make output more compact
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
