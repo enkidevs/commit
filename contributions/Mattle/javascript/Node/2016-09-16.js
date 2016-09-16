53ca0865bf71038e0d298c7a9dd79209fe6cd131
Mattle <mcallier@gmail.com>
2016-09-16 19:57:46 +0100

    2016-09-16's javascript workout

---
+++ contributions/Mattle/javascript/Node/2016-09-16.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+Manage child processes in **Node**
