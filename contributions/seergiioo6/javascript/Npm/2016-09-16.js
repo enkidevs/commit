070e0ed71c70a79ccf5f0ec39f7784a766c19023
seergiioo6 <hello@sergioandres.io>
2016-09-16 13:13:08 +0100

    2016-09-16's javascript workout

---
+++ contributions/seergiioo6/javascript/Npm/2016-09-16.js
@@ -0,0 +1,5 @@
+Manipulate npm packages cache
+Manage local node modules with `npm link`
+Environmental variables and flags for `npm`
+`npm` verifies `sha1` hashes of packages
+Reduce package duplication 
