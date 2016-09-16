3869ce2c14fc31f24bf6e7f5bd26f91842868ad5
mattia.penati <mattia.penati@gmail.com>
2016-09-16 14:09:34 +0100

    2016-09-16's linux workout

---
+++ contributions/mattia.penati/linux/Terminals & Shells/2016-09-16.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Retrieve previous arguments
