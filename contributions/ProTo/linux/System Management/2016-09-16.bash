0c2e9d47c7038a4814c2af5e0f874e66a8b91fc5
ProTo <stefan.oberdoerfer@hotmail.de>
2016-09-16 22:40:42 +0100

    2016-09-16's linux workout

---
+++ contributions/ProTo/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Test disk speed with `dd`
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Basic service management with `systemd`
