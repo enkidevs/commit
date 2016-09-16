2ab58b65a917c9b934a3412c707497934a4e3abf
psbella <pablo.s.bella@gmail.com>
2016-09-16 02:49:13 +0100

    2016-09-15's linux workout

---
+++ contributions/psbella/linux/Networking/2016-09-15.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+How to clear `iptables` rules
+Get real network statistics with `slurm`
+Limit the `wget` download rate
+Getting the connection speed from the terminal
