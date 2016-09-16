6e0f85176480e76b7072380e5d461c8ec26e89b4
jwilspnw <justin.m.wilson@hotmail.com>
2016-09-16 19:01:35 +0100

    2016-09-16's java workout

---
+++ contributions/jwilspnw/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Using an interface as a parameter
