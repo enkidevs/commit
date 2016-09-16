de258a2e9b863ff6c028f0ab01dae6d90c223e44
plutap <pluta.p@gmail.com>
2016-09-16 10:30:30 +0100

    2016-09-16's java workout

---
+++ contributions/plutap/java/I/O/2016-09-16.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+How to list all files in a directory that match a filename extension in Java
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+List contents of a directory with `newDirectoryStream`
