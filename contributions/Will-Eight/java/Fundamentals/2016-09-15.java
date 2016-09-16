3ea0da2bc6d7c4d4f2d0646228ac2de93e5a99db
Will-Eight <wmccabe8@gmail.com>
2016-09-16 03:10:09 +0100

    2016-09-15's java workout

---
+++ contributions/Will-Eight/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Measuring time
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
