bba75dce2b81edaf784e4e15530f57570758343f
juduth <juduth@gmail.com>
2016-09-16 13:07:03 +0100

    2016-09-16's linux workout

---
+++ contributions/juduth/linux/File Management/2016-09-16.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
