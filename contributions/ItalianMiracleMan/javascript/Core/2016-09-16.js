474a9fa6f42a1ea8a3131b369f49708f280a28b6
ItalianMiracleMan <riccardo.spinato@gmail.com>
2016-09-16 08:33:24 +0100

    2016-09-16's javascript workout

---
+++ contributions/ItalianMiracleMan/javascript/Core/2016-09-16.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Performing a function at timed intervals
+The distinction between `==` and `===`
+NaN === NaN?
+The difference between null, undefined and NaN
