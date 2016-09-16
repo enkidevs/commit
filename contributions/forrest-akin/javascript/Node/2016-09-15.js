bb6ded4625093a8aba46950dfcc579222cd444b8
forrest-akin <forrest.akin@gmail.com>
2016-09-16 04:50:15 +0100

    2016-09-15's javascript workout

---
+++ contributions/forrest-akin/javascript/Node/2016-09-15.js
@@ -0,0 +1,5 @@
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+`try-catch` only for **sync** code
+Factories design pattern
+Avoid using `sudo` when running Node
