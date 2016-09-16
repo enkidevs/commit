398a14968b316bb9242883409cf24ffb00e610f9
quarckster <quarckster@gmail.com>
2016-09-16 19:12:06 +0100

    2016-09-16's linux workout

---
+++ contributions/quarckster/linux/File Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
