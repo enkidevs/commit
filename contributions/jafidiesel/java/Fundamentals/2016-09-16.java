4bae7b38eb4c74bcf3ee8109ea5ccde25b0d5dc6
jafidiesel <jafibravin@outlook.com>
2016-09-16 20:17:45 +0100

    2016-09-16's java workout

---
+++ contributions/jafidiesel/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Supply `toString()` in all classes
