8387743321e122f418b2b3171b65783f04fad412
rcleozier <rcleozier@gmail.com>
2016-09-16 15:56:26 +0100

    2016-09-16's javascript workout

---
+++ contributions/rcleozier/javascript/Browser APIs/2016-09-16.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+Navigating the browser history
+ProgressEvent
+Geolocation
