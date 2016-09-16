eec8090a68c2ba36ee0239b689b06a9f557b3a9d
rasheem <alef.herrera@gmail.com>
2016-09-16 16:18:35 +0100

    2016-09-16's javascript workout

---
+++ contributions/rasheem/javascript/Functions/2016-09-16.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Move cursor at the end of text input
+Check if a document is done loading
