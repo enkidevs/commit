d3263d68e5fcebc2e3beb61d3de32797c3e76b3c
ak4t0sh <ak4t0sh@free.fr>
2016-09-16 13:19:24 +0100

    2016-09-16's java workout

---
+++ contributions/ak4t0sh/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
