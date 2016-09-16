6537fb1a4d89f28209da8bf437d44f09f55f17a6
mehmetnuriyaman <mnyaman@piernet.com.tr>
2016-09-16 09:40:40 +0100

    2016-09-16's linux workout

---
+++ contributions/mehmetnuriyaman/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
