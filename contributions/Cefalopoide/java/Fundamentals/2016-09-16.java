149b42e67825b33ec2b59d4eb6866c537f8d4538
Cefalopoide <luca.filipponi89@gmail.com>
2016-09-16 21:19:42 +0100

    2016-09-16's java workout

---
+++ contributions/Cefalopoide/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Connecting to FTP using Java
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
