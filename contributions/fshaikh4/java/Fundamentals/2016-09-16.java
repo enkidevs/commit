01014849253d56299b27e558d44ed8291421560d
fshaikh4 <fshaikh4@gmu.edu>
2016-09-16 10:56:09 +0100

    2016-09-16's java workout

---
+++ contributions/fshaikh4/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
