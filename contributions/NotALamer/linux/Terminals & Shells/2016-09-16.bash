7da0fe6cb3822e88788d43cf01779cc3429bb68a
NotALamer <alvaro.movil.ms@gmail.com>
2016-09-16 11:01:12 +0100

    2016-09-16's linux workout

---
+++ contributions/NotALamer/linux/Terminals & Shells/2016-09-16.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear bash history
+`else` statements using the `||` operator 
+Retrieve previous arguments
