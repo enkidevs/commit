a9ecdd9f5c85321f99fab3421a60839b7fb1ff04
ovlb <o@ovlb.net>
2016-09-16 07:46:15 +0100

    2016-09-16's javascript workout

---
+++ contributions/ovlb/javascript/Node/2016-09-16.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Wrapping errors in Node.js using _node-verror_
+`process.nextTick()` callback pattern
+Bulk-Write in **Node** with `cork()`
+Avoid using `sudo` when running Node
