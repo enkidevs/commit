271500d15173a67c1d0c0ad006e1cc97063445b4
keiner <keiner.v.ihnen@gmail.com>
2016-09-16 07:05:43 +0100

    2016-09-16's javascript workout

---
+++ contributions/keiner/javascript/Core/2016-09-16.js
@@ -0,0 +1,5 @@
+Get a random item from an Array
+Hoisting applies only to variable declarations, not initializations
+Avoid using `with`
+Extra function arguments are undefined by default
+How to merge two arrays
