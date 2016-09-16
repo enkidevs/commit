b7750d83002bea8886639c1158b1d1d75777959a
AwaNoodle <mark.nick.turner@gmail.com>
2016-09-17 00:54:24 +0100

    2016-09-16's linux workout

---
+++ contributions/AwaNoodle/linux/System Management/2016-09-16.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
