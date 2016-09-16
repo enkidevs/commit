ae79d3a82a6484317aa425fc0e5a49c99d4e481b
oroki <antonio.fracasso@gmail.com>
2016-09-16 06:26:24 +0100

    2016-09-16's linux workout

---
+++ contributions/oroki/linux/System Management/2016-09-16.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
