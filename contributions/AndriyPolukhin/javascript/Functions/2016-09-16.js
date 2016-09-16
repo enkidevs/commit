3aca0b8d1f7e08e0a50c5087422a6f54e36f2ec2
AndriyPolukhin <andriy.polukhin@gmail.com>
2016-09-16 18:50:56 +0100

    2016-09-16's javascript workout

---
+++ contributions/AndriyPolukhin/javascript/Functions/2016-09-16.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Anonymous functions
