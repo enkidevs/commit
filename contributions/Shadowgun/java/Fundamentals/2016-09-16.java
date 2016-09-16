81f3f1bcc36b24c397cc496974ed955411d6fa86
Shadowgun <shadowgun@i.ua>
2016-09-16 09:32:27 +0100

    2016-09-16's java workout

---
+++ contributions/Shadowgun/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
