39b9ca08dab808612957d17b1bab3aa635dd00b2
Luis1988 <luisillosevillano1988@gmail.com>
2016-09-16 06:15:05 +0100

    2016-09-16's linux workout

---
+++ contributions/Luis1988/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Finding Open Files With `lsof`
