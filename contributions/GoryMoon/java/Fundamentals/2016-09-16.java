6abd5dcf139fc177821040f2417d016ecca55826
GoryMoon <gurreja@gmail.com>
2016-09-16 22:50:48 +0100

    2016-09-16's java workout

---
+++ contributions/GoryMoon/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
