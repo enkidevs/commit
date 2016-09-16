cafa7a47647055b4f689d005eaa58f552a57a21f
jnighlight <jlites@stetson.edu>
2016-09-16 22:28:41 +0100

    2016-09-16's linux workout

---
+++ contributions/jnighlight/linux/Terminals & Shells/2016-09-16.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
