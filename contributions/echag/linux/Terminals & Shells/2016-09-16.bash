7a644f0bb6fcd5e9ceb638c72216b0aa154cf242
echag <emmanuelchagnas@gmail.com>
2016-09-16 20:38:45 +0100

    2016-09-16's linux workout

---
+++ contributions/echag/linux/Terminals & Shells/2016-09-16.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
