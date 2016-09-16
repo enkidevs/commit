b19f4122d2fd74b5484f1faf3cec003558641b4e
Shargotth <martin.hogge@gmail.com>
2016-09-16 06:56:17 +0100

    2016-09-16's java workout

---
+++ contributions/Shargotth/java/I/O/2016-09-16.java
@@ -0,0 +1,5 @@
+How to list all files in a directory that match a filename extension in Java
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
