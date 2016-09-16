5a298615db7734b8c241cb99a3bf3bfe6efec063
rohindaswani <rohindaswani@gmail.com>
2016-09-16 15:07:18 +0100

    2016-09-16's javascript workout

---
+++ contributions/rohindaswani/javascript/Functions/2016-09-16.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+IIFE pattern
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
