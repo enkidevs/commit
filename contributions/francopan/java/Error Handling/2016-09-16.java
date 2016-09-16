1251ed6ed8397292bf28469aeb0a1e5acea56b12
francopan <francobpan@outlook.com>
2016-09-16 16:44:24 +0100

    2016-09-16's java workout

---
+++ contributions/francopan/java/Error Handling/2016-09-16.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Throwing an `exception`
+Careful Numeric Data Conversions
