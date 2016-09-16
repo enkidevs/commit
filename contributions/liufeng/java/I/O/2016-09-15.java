ef25e341849add7560ed6d117793f2141133b42b
liufeng <liufeng5460@163.com>
2016-09-16 01:19:41 +0100

    2016-09-15's java workout

---
+++ contributions/liufeng/java/I/O/2016-09-15.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Retrieving file store attributes
