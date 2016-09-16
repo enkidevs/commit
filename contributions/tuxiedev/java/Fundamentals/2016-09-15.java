d85e040390761476204eaca11dff74d58a7ac2a8
tuxiedev <techie.anish@gmail.com>
2016-09-16 10:15:28 +0100

    2016-09-15's java workout

---
+++ contributions/tuxiedev/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Updating interfaces by using `default` methods
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
