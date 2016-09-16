3d1a070f44379786e0f3aeaf27af93d78a7ae133
adamgyulavari <azeroths@gmail.com>
2016-09-16 15:40:24 +0100

    2016-09-16's java workout

---
+++ contributions/adamgyulavari/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
