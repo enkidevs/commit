73f62e969fb38e1bab46783e41420cda4c684c5d
AkhilSinghania <akhiltws@gmail.com>
2016-09-16 14:57:26 +0100

    2016-09-15's linux workout

---
+++ contributions/AkhilSinghania/linux/Terminals & Shells/2016-09-15.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Aliasing ssh connections
+Find any Unix / Linux command
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
