3e0009b574c7aed14fc88874cf99ebc48213c2d8
hsingh2 <hsingh2@lamar.edu>
2016-09-16 06:45:08 +0100

    2016-09-15's java workout

---
+++ contributions/hsingh2/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
