5708ecfa01a43028616bb3c31a91e365b9089b5d
victordev <victorfrontend@gmail.com>
2016-09-16 17:57:31 +0100

    2016-09-16's linux workout

---
+++ contributions/victordev/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Cancel a system shutdown
+Finding Open Files With `lsof`
