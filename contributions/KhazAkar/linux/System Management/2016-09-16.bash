ed3065e7614ce5a8d1f2c47dc20972c3e38a299b
KhazAkar <damianzrb@gmail.com>
2016-09-16 19:54:51 +0100

    2016-09-16's linux workout

---
+++ contributions/KhazAkar/linux/System Management/2016-09-16.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
