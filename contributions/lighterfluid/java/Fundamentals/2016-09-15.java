5b2ff8343cdde684004b39c1d9053d37967b66a3
lighterfluid <2yusaku@gmail.com>
2016-09-16 06:33:48 +0100

    2016-09-15's java workout

---
+++ contributions/lighterfluid/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
