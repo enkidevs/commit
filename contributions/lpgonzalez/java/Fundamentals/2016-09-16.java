54f8842913c3d80b08885fb00bc2a49e851b6215
lpgonzalez <lisardo.prieto.gonzalez@gmail.com>
2016-09-16 04:39:08 +0100

    2016-09-16's java workout

---
+++ contributions/lpgonzalez/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Always specify type parameter
+Do not return *references* to private *mutable* class members
