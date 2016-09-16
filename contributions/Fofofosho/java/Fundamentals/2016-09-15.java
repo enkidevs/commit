099ec0e8caea8eaf63ec018e09adbaeb9277858a
Fofofosho <forrest.town18@gmail.com>
2016-09-16 04:10:16 +0100

    2016-09-15's java workout

---
+++ contributions/Fofofosho/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Measuring time
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
