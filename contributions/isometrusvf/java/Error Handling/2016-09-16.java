850d19c009cbcb419daa35c6d749e5589c45917f
isometrusvf <theepicgabriel0@gmail.com>
2016-09-16 15:26:06 +0100

    2016-09-16's java workout

---
+++ contributions/isometrusvf/java/Error Handling/2016-09-16.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Converting stack trace to a string
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
