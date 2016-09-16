ac215ae63dd2836563fc5467ef8b0d73f039d97b
hoplita112358 <hoplita112358@gmail.com>
2016-09-16 11:22:07 +0100

    2016-09-16's linux workout

---
+++ contributions/hoplita112358/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Basic service management with `systemd`
+Cancel a system shutdown
