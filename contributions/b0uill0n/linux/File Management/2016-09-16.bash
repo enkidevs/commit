678de12ee39edacbd9cc76719c1beb894a41ddca
b0uill0n <ilia.kerzhner@gmail.com>
2016-09-16 16:21:29 +0100

    2016-09-16's linux workout

---
+++ contributions/b0uill0n/linux/File Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Using `touch` to alter files modification time
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
