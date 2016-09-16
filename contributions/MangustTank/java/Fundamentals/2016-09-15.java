e7728803cef0f640c7d49e8fed20dfb28e4d2d4c
MangustTank <a.poludov@gmail.com>
2016-09-16 12:36:38 +0100

    2016-09-15's java workout

---
+++ contributions/MangustTank/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
