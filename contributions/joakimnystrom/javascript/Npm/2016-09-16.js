fd7fd77283f1ea8d97052513bf10be954c925c61
joakimnystrom <jnystromdesign@gmail.com>
2016-09-16 19:44:22 +0100

    2016-09-16's javascript workout

---
+++ contributions/joakimnystrom/javascript/Npm/2016-09-16.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Combining script commands in npm
+List binaries for scripting in npm
+`npm` verifies `sha1` hashes of packages
+`config` object in `package.json`
