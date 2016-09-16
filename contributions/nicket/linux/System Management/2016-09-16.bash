24a328dfb81f80a08f567605522575acc5082611
nicket <nick@weap.nl>
2016-09-16 06:47:18 +0100

    2016-09-16's linux workout

---
+++ contributions/nicket/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
