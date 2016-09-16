79ba6a8ebd406be2c206701dbe729429ae0a6645
hong7463 <hison7463@gmail.com>
2016-09-16 16:14:14 +0100

    2016-09-16's linux workout

---
+++ contributions/hong7463/linux/System Management/2016-09-16.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
