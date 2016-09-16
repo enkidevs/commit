89d0f6febb5025852452c2d1a3ffa12c4cdf2d5a
nikooiko <nikooiko@inf.uth.gr>
2016-09-16 08:52:39 +0100

    2016-09-16's linux workout

---
+++ contributions/nikooiko/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
