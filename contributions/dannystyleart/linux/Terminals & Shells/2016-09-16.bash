5a64df9b5c4e19bd9df2a752d03819e299339150
dannystyleart <dannystyleart@gmail.com>
2016-09-16 08:15:38 +0100

    2016-09-16's linux workout

---
+++ contributions/dannystyleart/linux/Terminals & Shells/2016-09-16.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
