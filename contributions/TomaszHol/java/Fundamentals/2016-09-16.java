ad19c4e6f2b20237757ef64b87e94a624fe714c3
TomaszHol <holeksa.tomasz@gmail.com>
2016-09-16 13:27:46 +0100

    2016-09-16's java workout

---
+++ contributions/TomaszHol/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
