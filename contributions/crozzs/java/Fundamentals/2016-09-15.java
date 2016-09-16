350c7923ffd0ab13b04b7335467a40538e6abeb0
crozzs <rodrigolosik@gmail.com>
2016-09-16 02:49:15 +0100

    2016-09-15's java workout

---
+++ contributions/crozzs/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+The distinction between overloading and overriding methods
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Implementing an `interface`
