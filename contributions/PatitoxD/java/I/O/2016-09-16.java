d3a0a62798c6427331f65a6daf91bd010b384632
PatitoxD <mail2patrickengel@gmail.com>
2016-09-16 12:14:36 +0100

    2016-09-16's java workout

---
+++ contributions/PatitoxD/java/I/O/2016-09-16.java
@@ -0,0 +1,5 @@
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
