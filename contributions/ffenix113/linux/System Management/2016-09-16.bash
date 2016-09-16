dad80ab1f14bd638ccf0b00e3f92b9c6b09e8189
ffenix113 <razerer@bigmir.net>
2016-09-16 18:36:21 +0100

    2016-09-16's linux workout

---
+++ contributions/ffenix113/linux/System Management/2016-09-16.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
