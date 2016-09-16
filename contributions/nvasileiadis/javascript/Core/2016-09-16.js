58535edfae290cdf39de6d74d33ca679ec33ab07
nvasileiadis <vasileiadis.nikos@gmail.com>
2016-09-16 10:34:12 +0100

    2016-09-16's javascript workout

---
+++ contributions/nvasileiadis/javascript/Core/2016-09-16.js
@@ -0,0 +1,5 @@
+Deleting properties
+Hoisting applies only to variable declarations, not initializations
+Apply `map` function to array items
+Avoid using `with`
+Getting the *real* dimensions of an image
