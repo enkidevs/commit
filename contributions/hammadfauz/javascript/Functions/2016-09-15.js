60753db416f29f10c801ff11550a1fc03013eb69
hammadfauz <hammad.fauz@gmail.com>
2016-09-16 23:31:02 +0100

    2016-09-15's javascript workout

---
+++ contributions/hammadfauz/javascript/Functions/2016-09-15.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` using `call` and `apply` methods
+Easily generate a random `HEX` color
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
