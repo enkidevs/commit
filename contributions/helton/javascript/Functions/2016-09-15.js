841bb33d421b037f3051d2db6f80cdd789af0817
helton <heltoncarlossouza@gmail.com>
2016-09-16 03:24:50 +0100

    2016-09-15's javascript workout

---
+++ contributions/helton/javascript/Functions/2016-09-15.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Easily generate a random `HEX` color
+Move cursor at the end of text input
+Social sharing without widgets
