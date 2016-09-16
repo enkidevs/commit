bd1963c40aaf2ba3eea6c30d3407e926aa85a7ce
MufloNerd <l.montunato@gmail.com>
2016-09-16 07:36:15 +0100

    2016-09-16's linux workout

---
+++ contributions/MufloNerd/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
