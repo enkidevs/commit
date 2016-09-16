f87d4743cb6fe3bdea7c904e2479ec3d6c092e65
buts101 <ashish.batajoo@gmail.com>
2016-09-16 15:12:56 +0100

    2016-09-15's linux workout

---
+++ contributions/buts101/linux/System Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
+Cancel a system shutdown
