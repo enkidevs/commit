26fe02f891fbd6151611492c9a9a46f803fe5a81
tereska <janecki@gmail.com>
2016-09-16 21:24:38 +0100

    2016-09-16's linux workout

---
+++ contributions/tereska/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
