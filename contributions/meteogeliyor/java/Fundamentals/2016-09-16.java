3bc2b02a461fb331a4df00f03d50828459462dd6
meteogeliyor <mertcan.alici18@gmail.com>
2016-09-16 22:49:55 +0100

    2016-09-16's java workout

---
+++ contributions/meteogeliyor/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Use meaningful names
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
