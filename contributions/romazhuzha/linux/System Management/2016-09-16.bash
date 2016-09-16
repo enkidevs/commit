2d6c81d0eab73d1954bacbdd5d6afc4b3c3baafb
romazhuzha <romazhuzha@gmail.com>
2016-09-16 17:27:56 +0100

    2016-09-16's linux workout

---
+++ contributions/romazhuzha/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Get hardware stack details with `lspci`
