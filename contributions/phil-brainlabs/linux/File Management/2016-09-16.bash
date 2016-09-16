e5c19ccc4e229e6251dbeeb9956e263090a7eb69
phil-brainlabs <phil@brainlabsdigital.com>
2016-09-16 16:25:16 +0100

    2016-09-16's linux workout

---
+++ contributions/phil-brainlabs/linux/File Management/2016-09-16.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
