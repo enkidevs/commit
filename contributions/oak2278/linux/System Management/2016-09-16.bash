97742b7dfc3ab579d550cf19bf9c9a380c72582c
oak2278 <oak2278@gmail.com>
2016-09-16 22:02:46 +0100

    2016-09-16's linux workout

---
+++ contributions/oak2278/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
