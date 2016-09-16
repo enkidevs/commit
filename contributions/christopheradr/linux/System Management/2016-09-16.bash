7cfbadb4ba97130faea07cdc2bfa7454f31fe97f
christopheradr <christopher.diaz.riv@gmail.com>
2016-09-16 15:20:04 +0100

    2016-09-16's linux workout

---
+++ contributions/christopheradr/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+System uptime
