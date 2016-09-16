9a3a4354799ed0a3d39456503ad8dd5f700ec998
dev_hernandez2 <william.warewoolf343@gmail.com>
2016-09-16 18:00:41 +0100

    2016-09-16's javascript workout

---
+++ contributions/dev_hernandez2/javascript/Node/2016-09-16.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+First-error callbacks in **Node**
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+`exports` vs. `module.exports` in **Node**
