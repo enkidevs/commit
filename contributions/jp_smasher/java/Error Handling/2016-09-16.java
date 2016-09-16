548999d1958108932a43030527b41786c28a0991
jp_smasher <wongjingping@gmail.com>
2016-09-17 00:23:54 +0100

    2016-09-16's java workout

---
+++ contributions/jp_smasher/java/Error Handling/2016-09-16.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Prevent `exceptions` while logging Data
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
