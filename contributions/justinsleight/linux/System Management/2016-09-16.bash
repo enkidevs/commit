cf024682675f70bcd32de43bc6d5f89994bc0d37
justinsleight <sleightjustin@gmail.com>
2016-09-16 23:24:31 +0100

    2016-09-16's linux workout

---
+++ contributions/justinsleight/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Finding Open Files With `lsof`
