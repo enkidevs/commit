a8e9d69175a9a27fa1a9c30b5215b8e5b0d68665
danto <danielj@outlook.de>
2016-09-16 17:26:12 +0100

    2016-09-16's linux workout

---
+++ contributions/danto/linux/Networking/2016-09-16.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Useful ssh client optimizations 
+Getting the connection speed from the terminal
+Bandwidth monitoring tools 
+Staying connected remotely via SSH without an interactive shell
