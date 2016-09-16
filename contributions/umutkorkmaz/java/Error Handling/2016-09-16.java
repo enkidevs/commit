afe43d4855b6aec87f8f733b4e83a3af9ba660fd
umutkorkmaz <info@umutkorkmaz.net>
2016-09-16 21:10:08 +0100

    2016-09-16's java workout

---
+++ contributions/umutkorkmaz/java/Error Handling/2016-09-16.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Using exceptions in Java
+Most common reason behind **stack overflow** error
