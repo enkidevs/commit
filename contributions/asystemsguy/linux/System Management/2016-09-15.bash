16acb30cfe34845e01acf382731d375354e19ca9
asystemsguy <devkhv129@gmail.com>
2016-09-16 15:16:31 +0100

    2016-09-15's linux workout

---
+++ contributions/asystemsguy/linux/System Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Finding Open Files With `lsof`
