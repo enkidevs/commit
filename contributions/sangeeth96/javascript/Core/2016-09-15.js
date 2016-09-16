733f1018ef0e7dbe25c6ea2e25ff7b64b06370d6
sangeeth96 <sangeethmanayil@outlook.com>
2016-09-16 20:09:41 +0100

    2016-09-15's javascript workout

---
+++ contributions/sangeeth96/javascript/Core/2016-09-15.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Types of numbers
+No block scope
+Hoisting applies only to variable declarations, not initializations
+Extra function arguments are undefined by default
