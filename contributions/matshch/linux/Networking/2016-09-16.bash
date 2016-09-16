1e9812f1ece89458f59f936dd150e4445e578c5f
matshch <matshch@gmail.com>
2016-09-16 06:31:29 +0100

    2016-09-16's linux workout

---
+++ contributions/matshch/linux/Networking/2016-09-16.bash
@@ -0,0 +1,5 @@
+Find services running on your host
+Multiple IP Address with aliasing 
+Get real network statistics with `slurm`
+List your IPv6 configuration
+Sending emails from terminal / scripts
