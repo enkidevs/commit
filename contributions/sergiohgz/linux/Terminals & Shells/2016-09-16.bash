0d05634a34fa27a0b8f1c5c928e19bf617550b35
sergiohgz <sergioherreraguz@gmail.com>
2016-09-16 14:08:05 +0100

    2016-09-16's linux workout

---
+++ contributions/sergiohgz/linux/Terminals & Shells/2016-09-16.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Aliasing ssh connections
+Terminal based browser
+Random password generator
