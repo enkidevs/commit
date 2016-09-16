53f23ac85a0386c98e81845679553e81fe55c1c8
Sangsom <rpudulis@gmail.com>
2016-09-16 11:47:04 +0100

    2016-09-16's java workout

---
+++ contributions/Sangsom/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+Limit Accessibility of `Fields`
