eaa69bb1da1db07053fb23fa7eb66931d6766125
tomaszkrzyzek <tomasz.krzyzek@gmail.com>
2016-09-16 15:19:18 +0100

    2016-09-16's java workout

---
+++ contributions/tomaszkrzyzek/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Supply `toString()` in all classes
+Using an interface as a parameter
