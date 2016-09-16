404839d86304c761db287d1ec623277a61448313
laurybueno <laury.bueno@gmail.com>
2016-09-16 13:40:21 +0100

    2016-09-15's linux workout

---
+++ contributions/laurybueno/linux/System Management/2016-09-15.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
