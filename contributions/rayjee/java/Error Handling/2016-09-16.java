9b26dff3b73fea1a41757b4a1a33055a8f6260c7
rayjee <formergod@gmail.com>
2016-09-16 11:08:49 +0100

    2016-09-16's java workout

---
+++ contributions/rayjee/java/Error Handling/2016-09-16.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Using exceptions in Java
+Most common reason behind **stack overflow** error
+Throwing an `exception`
