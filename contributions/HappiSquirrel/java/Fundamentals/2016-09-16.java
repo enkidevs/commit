51675d05952dd9d09f4ca988a84ec47b2b971168
HappiSquirrel <ludo0302@hotmail.fr>
2016-09-16 11:02:08 +0100

    2016-09-16's java workout

---
+++ contributions/HappiSquirrel/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Supply `toString()` in all classes
