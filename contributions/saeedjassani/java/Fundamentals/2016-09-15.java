fb572b324190aca0e421d7f94a0281a91128c408
saeedjassani <saeedjassani@gmail.com>
2016-09-16 18:26:38 +0100

    2016-09-15's java workout

---
+++ contributions/saeedjassani/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
