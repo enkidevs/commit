f9c2a3707fa23a724d23ce234d967a6d986dd988
joepreludian <joepreludian@gmail.com>
2016-09-16 21:15:12 +0100

    2016-09-16's java workout

---
+++ contributions/joepreludian/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
