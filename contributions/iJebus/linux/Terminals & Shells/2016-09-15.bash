c2ca4d9bbb152a851f7158f0b646e92c5b68c1a6
iJebus <liam.jones.aus@gmail.com>
2016-09-16 07:09:03 +0100

    2016-09-15's linux workout

---
+++ contributions/iJebus/linux/Terminals & Shells/2016-09-15.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Retrieve previous arguments
+The Basics of Environment Variables
