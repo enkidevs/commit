840003885d8da70fb713888961a6012a40062458
pirj <pirjsuka@gmail.com>
2016-09-16 19:56:20 +0100

    2016-09-16's linux workout

---
+++ contributions/pirj/linux/File Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
