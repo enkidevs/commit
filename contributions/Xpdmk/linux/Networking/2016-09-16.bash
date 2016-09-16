4f7d22a437765a9fa2d18beaa9b6213ba946cbaf
Xpdmk <roy.jarvinen@gmail.com>
2016-09-16 15:51:35 +0100

    2016-09-16's linux workout

---
+++ contributions/Xpdmk/linux/Networking/2016-09-16.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+How to clear `iptables` rules
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
+Short Introduction to `Wget` 
