4504a53582200dbf0fec3e3b0ad67b58262905a8
dzienish <kamil.dzieniszewski@gmail.com>
2016-09-16 21:48:10 +0100

    2016-09-16's javascript workout

---
+++ contributions/dzienish/javascript/Functions/2016-09-16.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Method chaining
+Easily generate a random `HEX` color
+Function call method
