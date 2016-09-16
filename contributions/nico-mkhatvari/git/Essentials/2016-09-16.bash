225aadef95d649a2d3a3e05e95ea1edbc06a793a
nico-mkhatvari <nico.mkhatvari@gmail.com>
2016-09-16 13:06:12 +0100

    2016-09-16's git workout

---
+++ contributions/nico-mkhatvari/git/Essentials/2016-09-16.bash
@@ -0,0 +1,5 @@
+The three states in git
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
+Perform a dry run
