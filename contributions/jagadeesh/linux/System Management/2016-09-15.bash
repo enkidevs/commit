7de4fb98986f9d4fbee5404055a7f598da7921dc
jagadeesh <hellmaker001@gmail.com>
2016-09-16 03:12:31 +0100

    2016-09-15's linux workout

---
+++ contributions/jagadeesh/linux/System Management/2016-09-15.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
