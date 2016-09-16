13424e2e9bd09b741be09578f6e6c4733bee4d2b
mageru <jmiller9@gmail.com>
2016-09-16 14:59:38 +0100

    2016-09-16's java workout

---
+++ contributions/mageru/java/Error Handling/2016-09-16.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Using exceptions in Java
+Throwing an `exception`
+Checking a double for `NaN` value
