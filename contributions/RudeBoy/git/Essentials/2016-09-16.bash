01a2203872cc901bcebede21bce88ce738cd41dc
RudeBoy <romanisman@gmail.com>
2016-09-16 17:00:36 +0100

    2016-09-16's git workout

---
+++ contributions/RudeBoy/git/Essentials/2016-09-16.bash
@@ -0,0 +1,5 @@
+You can use git offline!
+Search by commit message keyword
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: fetching and pushing
