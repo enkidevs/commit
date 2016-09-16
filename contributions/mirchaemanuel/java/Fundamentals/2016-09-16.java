9804df5320e8ca2c836cfdfc15499d7889b0b24e
mirchaemanuel <mirchaemanuel@gmail.com>
2016-09-16 05:24:56 +0100

    2016-09-16's java workout

---
+++ contributions/mirchaemanuel/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
