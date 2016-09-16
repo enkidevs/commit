2ff64b2bc4b7bf5a781f0aff55bfeecccde35046
ludoows <theartist768@gmail.com>
2016-09-16 06:21:29 +0100

    2016-09-16's linux workout

---
+++ contributions/ludoows/linux/Terminals & Shells/2016-09-16.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
