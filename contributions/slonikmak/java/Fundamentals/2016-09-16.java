a96e80a23d6c4f530ac840c6032591a10141a6f6
slonikmak <slonikmak@gmail.com>
2016-09-16 10:39:56 +0100

    2016-09-16's java workout

---
+++ contributions/slonikmak/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+The distinction between overloading and overriding methods
+Use meaningful names
+Common mistake on switch statements
+Do not return *references* to private *mutable* class members
