6888a0a981773ca4f43cb796cbdaf27cd90c03e6
boutchaz <zakaria.boutchamir@gmail.com>
2016-09-16 11:43:01 +0100

    2016-09-16's linux workout

---
+++ contributions/boutchaz/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
