313bf2b28ef48d3b0d0a1c1a6594c8af85dcb366
MeryllEssig <meryll.essig@free.fr>
2016-09-17 12:21:42 +0100

    2016-09-17's linux workout

---
+++ contributions/MeryllEssig/linux/File Management/2016-09-17.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
