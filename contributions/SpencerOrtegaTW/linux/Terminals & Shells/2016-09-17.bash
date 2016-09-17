676c05021a589dee5ae49255dc47b05c76378c40
SpencerOrtegaTW <spencerortega1996@gmail.com>
2016-09-17 13:21:51 +0100

    2016-09-17's linux workout

---
+++ contributions/SpencerOrtegaTW/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Terminal incognito mode
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
