42e71d2bb25145c7f1cde9f3e521f9468e0e851a
mbwanajm <mbwanajm@gmail.com>
2016-09-17 10:29:49 +0100

    2016-09-16's javascript workout

---
+++ contributions/mbwanajm/javascript/Core/2016-09-16.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Use `splice()` to remove an item from an array
+Data type comparison in `switch` statements
+Getting the *real* dimensions of an image
+Inaccuracy of binary floating-point format
