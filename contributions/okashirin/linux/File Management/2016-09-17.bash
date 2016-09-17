aefd48ca5c2e48f5162f4f2d9385ad9437ba30b6
okashirin <lokishadow@mail.ru>
2016-09-17 23:45:29 +0100

    2016-09-17's linux workout

---
+++ contributions/okashirin/linux/File Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
