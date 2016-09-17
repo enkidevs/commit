cde2efbc0328a53b64885606af26f53c2b440bbe
pgromano <pablo.romano42@gmail.com>
2016-09-17 20:24:12 +0100

    2016-09-17's python workout

---
+++ contributions/pgromano/python/Data Structures/2016-09-17.py
@@ -0,0 +1,5 @@
+Following PEP 8 styling guideline.
+A simple way to select a random item from a `list/tuple` data stucture
+Immutable sets with `frozenset`
+Get more with `collections`!
+How to count
