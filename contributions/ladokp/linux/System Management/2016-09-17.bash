57bca2306a406cd5e5e5ed30f7fc0ff944a0f04c
ladokp <patrick@ks.co.at>
2016-09-17 06:30:04 +0100

    2016-09-17's linux workout

---
+++ contributions/ladokp/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
