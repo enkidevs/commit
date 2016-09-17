562656b50427145c409d5971af06f4a98fca7aac
TheNewEra <bastian.gee@gmail.com>
2016-09-17 17:05:22 +0100

    2016-09-17's java workout

---
+++ contributions/TheNewEra/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Use strings in a `switch` statement
+Use `DecimalFormat` class to format numbers
+`Autoboxing` and `Unboxing`
