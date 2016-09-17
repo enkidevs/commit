8246fdc49f436a68d1d4ccdc6be8d82a5bb2b62c
SAM4191 <sam4191@gmail.com>
2016-09-17 10:20:46 +0100

    2016-09-17's java workout

---
+++ contributions/SAM4191/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
+Using an interface as a parameter
