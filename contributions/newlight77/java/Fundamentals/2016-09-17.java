f2f43b08b072a966d871bbec9aac93c916d3454f
newlight77 <newlight77@gmail.com>
2016-09-17 08:24:49 +0100

    2016-09-17's java workout

---
+++ contributions/newlight77/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
