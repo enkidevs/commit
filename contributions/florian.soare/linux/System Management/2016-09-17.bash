920ba4a89d4423fdb8b4c2d4d5995166be672d6a
florian.soare <florian.soare@takmate.com>
2016-09-17 06:51:42 +0100

    2016-09-17's linux workout

---
+++ contributions/florian.soare/linux/System Management/2016-09-17.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Find the `MAC` address of all network interfaces
+
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+System uptime
