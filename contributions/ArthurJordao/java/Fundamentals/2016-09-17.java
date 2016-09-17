a4c10c5f73872574d58b7040e1d6e23df81ab60e
ArthurJordao <arthurbernardijordao@gmail.com>
2016-09-17 18:02:39 +0100

    2016-09-17's java workout

---
+++ contributions/ArthurJordao/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use `DecimalFormat` class to format numbers
+Measuring time
+Always specify type parameter
+There is no such thing as *pass-by-reference* in Java
