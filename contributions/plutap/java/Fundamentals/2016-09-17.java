6d43be91494baf99d63e5960e7d00da66355291e
plutap <pluta.p@gmail.com>
2016-09-17 06:16:57 +0100

    2016-09-17's java workout

---
+++ contributions/plutap/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Always specify type parameter
+Do not return *references* to private *mutable* class members
