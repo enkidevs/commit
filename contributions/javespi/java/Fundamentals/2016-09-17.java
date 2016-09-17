a3d871518784c8092182b33b27a8954d97d69841
javespi <javespalf@gmail.com>
2016-09-17 09:19:43 +0100

    2016-09-17's java workout

---
+++ contributions/javespi/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Common mistake on switch statements
+Inheriting a constructor from a superclass
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
