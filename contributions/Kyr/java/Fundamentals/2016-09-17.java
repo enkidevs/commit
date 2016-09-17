41d4be567561478750286b148677ccad51bb0bb3
Kyr <kyrylofedorov@gmail.com>
2016-09-17 18:05:44 +0100

    2016-09-17's java workout

---
+++ contributions/Kyr/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Inheriting a constructor from a superclass
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
