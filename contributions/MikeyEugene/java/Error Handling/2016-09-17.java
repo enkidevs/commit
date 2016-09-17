2b426004d6fda4d686958473ae23f0bab4e4ef9d
MikeyEugene <mikeseffinemail@gmail.com>
2016-09-17 23:20:35 +0100

    2016-09-17's java workout

---
+++ contributions/MikeyEugene/java/Error Handling/2016-09-17.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Most common reason behind **stack overflow** error
+Throwing an `exception`
