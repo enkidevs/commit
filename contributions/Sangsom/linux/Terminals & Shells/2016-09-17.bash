36bc7f4b80659c246b5b3587ed458eed91b50d74
Sangsom <rpudulis@gmail.com>
2016-09-17 08:26:56 +0100

    2016-09-17's linux workout

---
+++ contributions/Sangsom/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
