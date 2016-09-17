ebe4bc62908a8da23a731a716582f81d924d6e81
gszabo <harry.bme@gmail.com>
2016-09-17 21:16:57 +0100

    2016-09-17's linux workout

---
+++ contributions/gszabo/linux/File Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
