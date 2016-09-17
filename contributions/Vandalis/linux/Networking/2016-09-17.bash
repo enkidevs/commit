50c14b2409da011ac03e1338a4e71a039dcde134
Vandalis <marangon.claudio@gmail.com>
2016-09-17 19:21:58 +0100

    2016-09-17's linux workout

---
+++ contributions/Vandalis/linux/Networking/2016-09-17.bash
@@ -0,0 +1,5 @@
+Find services running on your host
+List your IPv6 configuration
+Getting the connection speed from the terminal
+Make use of `netstat`
+Staying connected remotely via SSH without an interactive shell
