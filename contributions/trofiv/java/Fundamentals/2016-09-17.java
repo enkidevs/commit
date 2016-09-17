c0d9ae72dcb0954d8e829049c3b836a23dc160b0
trofiv <stranger.65536@gmail.com>
2016-09-17 16:34:25 +0100

    2016-09-17's java workout

---
+++ contributions/trofiv/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
