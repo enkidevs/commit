b789237c62ce283dabd4feddfa1c85db315dc241
Leerro <gabriel.f.bernard@gmail.com>
2016-09-17 16:31:11 +0100

    2016-09-17's linux workout

---
+++ contributions/Leerro/linux/File Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Find the unknown process preventing deleting of files
+Easy way of sharing files
