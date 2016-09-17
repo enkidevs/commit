b694839f501f392c43516e7338d530426598f411
sebwieland <wielandseb@googlemail.com>
2016-09-17 20:16:44 +0100

    2016-09-17's git workout

---
+++ contributions/sebwieland/git/Essentials/2016-09-17.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+Using aliases for git commands
+Check the status of your files
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
