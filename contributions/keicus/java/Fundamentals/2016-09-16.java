e1da2e333bf88c3b6772259f8a055b6050cf1856
keicus <charlestdjames1@gmail.com>
2016-09-17 02:02:53 +0100

    2016-09-16's java workout

---
+++ contributions/keicus/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Using Inheritance to reduce code repetition
+Metadata: creating a user-defined file attribute
+Calling one constructor from another
+Avoid using `static` variables
