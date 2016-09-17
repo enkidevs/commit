5cbc1a14f1d5601a267b6882dc6114eeffb8a4ed
Pratt0923 <imapratt@nc.rr.com>
2016-09-17 15:04:09 +0100

    2016-09-17's javascript workout

---
+++ contributions/Pratt0923/javascript/Npm/2016-09-17.js
@@ -0,0 +1,5 @@
+Manipulate npm packages cache
+npm can handle multiple versions of a dependency
+Package distribution tags
+Reduce package duplication 
+Lock down dependency versions by shrinkwrapping
