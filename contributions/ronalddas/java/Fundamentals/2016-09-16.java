3a803e19a7fe7c27d0dead33670008dfac68f0a2
ronalddas <ronald1das@gmail.com>
2016-09-17 13:11:13 +0100

    2016-09-16's java workout

---
+++ contributions/ronalddas/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Inheriting a constructor from a superclass
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
