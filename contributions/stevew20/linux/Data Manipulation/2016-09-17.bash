d97b914ad5eaa9e69bf438c9c5ddaf3e0521e3ff
stevew20 <stevewells20@gmail.com>
2016-09-17 19:53:35 +0100

    2016-09-17's linux workout

---
+++ contributions/stevew20/linux/Data Manipulation/2016-09-17.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+Short intro to `grep`
+Getting rid of duplicate lines
+Using `lynx` to convert HTML to text
+Count non-empty lines with `nl` utility
