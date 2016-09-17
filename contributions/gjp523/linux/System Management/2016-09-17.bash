79bc54cc1880cfd9245177e55b30a4d7d073a510
gjp523 <gjp523@gmail.com>
2016-09-18 00:42:32 +0100

    2016-09-17's linux workout

---
+++ contributions/gjp523/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
