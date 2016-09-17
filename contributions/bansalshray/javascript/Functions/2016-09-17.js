18e9f6f66208d3564303a7593ec75fb1398dfdee
bansalshray <bansalshray@gmail.com>
2016-09-17 17:56:32 +0100

    2016-09-17's javascript workout

---
+++ contributions/bansalshray/javascript/Functions/2016-09-17.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Function call method
+Social sharing without widgets
