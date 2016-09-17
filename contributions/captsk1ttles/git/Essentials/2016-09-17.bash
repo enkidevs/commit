ae36e1f9c00ac72590dfcfc1d3c4f4679ae9cf1d
captsk1ttles <cptsk1ttles1@gmail.com>
2016-09-18 00:36:54 +0100

    2016-09-17's git workout

---
+++ contributions/captsk1ttles/git/Essentials/2016-09-17.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Using tags for version control
+Make your log output pretty
+Search by commit message keyword
+Remote repositories: viewing, editing and deleting
