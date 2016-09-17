18df9e2d579da4a534ffc00d57f0ecad8d32aabc
K0XM4N <radwan.krzysztof.1993@gmail.com>
2016-09-17 11:07:41 +0100

    2016-09-17's java workout

---
+++ contributions/K0XM4N/java/Error Handling/2016-09-17.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Checking a double for `NaN` value
