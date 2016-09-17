1555164a59c9c66ddc2fd9e9593e19d8b5ff71ea
ckmah <clarence.k.mah@gmail.com>
2016-09-17 19:56:50 +0100

    2016-09-17's linux workout

---
+++ contributions/ckmah/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Useful aliasing in bash
+Terminal incognito mode
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
