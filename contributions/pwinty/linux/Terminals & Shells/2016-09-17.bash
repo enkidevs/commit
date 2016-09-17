0946dbf9777e3e94ea9bfbb78187bbfcce8ce7eb
pwinty <peterwinteriii@gmail.com>
2016-09-17 14:39:14 +0100

    2016-09-17's linux workout

---
+++ contributions/pwinty/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Terminal incognito mode
+Adding directories to your `$PATH`
