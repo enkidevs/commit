e4fc12cf5a3cf10daac993b327241e3b488b4dac
Alanbato <alanvelasco.a@gmail.com>
2016-09-17 22:45:27 +0100

    2016-09-17's linux workout

---
+++ contributions/Alanbato/linux/Networking/2016-09-17.bash
@@ -0,0 +1,5 @@
+ Intro to `iptables`
+Quick port test with `netcat`
+Get real network statistics with `slurm`
+Sending emails from terminal / scripts
+Use `tcpdump` to listen to network interface traffic
