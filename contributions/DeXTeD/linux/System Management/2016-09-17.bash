94eacdc2be16c4f9441e441a088cdd72ba370079
DeXTeD <dexted.xt@gmail.com>
2016-09-17 16:06:54 +0100

    2016-09-17's linux workout

---
+++ contributions/DeXTeD/linux/System Management/2016-09-17.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
