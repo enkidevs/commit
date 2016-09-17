bbfc4060cb148a2057aaa60c4d36b3e018245610
rasheem <alef.herrera@gmail.com>
2016-09-18 00:20:26 +0100

    2016-09-17's javascript workout

---
+++ contributions/rasheem/javascript/Functions/2016-09-17.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function declarations vs function expressions
+Functions can be declared after use
+Function call method
+Social sharing without widgets
