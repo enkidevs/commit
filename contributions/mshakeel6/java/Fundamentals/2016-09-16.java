bed1598785335026d42061edea180cac72dc51e5
mshakeel6 <mshakeelibrahim@gmail.com>
2016-09-17 03:56:01 +0100

    2016-09-16's java workout

---
+++ contributions/mshakeel6/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using bounded type parameters in generic methods
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
