64839302720bf27d2a5a86d44aaecf9af942bf4a
Ruud <ruud@arfie.nl>
2016-09-17 11:20:22 +0100

    2016-09-17's javascript workout

---
+++ contributions/Ruud/javascript/React/2016-09-17.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Keyed fragments in **React**
+Prop Validation
+Shortcut for transferring props
