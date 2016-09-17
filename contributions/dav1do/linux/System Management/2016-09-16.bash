85049998b4edf476779c71bf196d55d70c1966b6
dav1do <david.estes01@gmail.com>
2016-09-17 01:04:32 +0100

    2016-09-16's linux workout

---
+++ contributions/dav1do/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
