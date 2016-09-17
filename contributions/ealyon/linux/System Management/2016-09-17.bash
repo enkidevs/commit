60fc8c0e6e739e6c3cf96a6708c83677e44c9372
ealyon <jeremy.junqua@gmail.com>
2016-09-17 13:43:19 +0100

    2016-09-17's linux workout

---
+++ contributions/ealyon/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
