e3e5fa7f3ccdfc40f748065eddd49d07eed7449a
RaphaelRaja <jrrphonix@gmail.com>
2016-09-17 14:13:52 +0100

    2016-09-16's java workout

---
+++ contributions/RaphaelRaja/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Supply `toString()` in all classes
+Using an interface as a parameter
