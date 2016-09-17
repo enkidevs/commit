624b391c767e52fd6d4a3f7329a05cfb33e7b197
AnatolyRugalev <anatoly.rugalev@gmail.com>
2016-09-17 09:16:05 +0100

    2016-09-16's linux workout

---
+++ contributions/AnatolyRugalev/linux/Terminals & Shells/2016-09-16.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Find any Unix / Linux command
+Useful aliasing in bash
+Random password generator
