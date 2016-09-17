1e19a8b63dcc06772a58c3f70745ab579f723acc
code_geek <asparsh29kumar@gmail.com>
2016-09-17 03:08:46 +0100

    2016-09-16's linux workout

---
+++ contributions/code_geek/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
