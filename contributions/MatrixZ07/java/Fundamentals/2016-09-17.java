bac77d3c68a1aa46c847246668a81c620a7461e5
MatrixZ07 <tobi_1907@web.de>
2016-09-17 22:28:06 +0100

    2016-09-17's java workout

---
+++ contributions/MatrixZ07/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Inheriting a constructor from a superclass
+Measuring time
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
