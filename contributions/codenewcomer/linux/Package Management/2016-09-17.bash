20370c0d30d0926297a90b2b8e96ac84b7931837
codenewcomer <codenewcomer@gmail.com>
2016-09-17 11:16:24 +0100

    2016-09-17's linux workout

---
+++ contributions/codenewcomer/linux/Package Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+List installed packages
+Prevent updating a specific package in Debian systems
+Get `apt` to use a mirror / faster mirror
+Solving `BADSIG GPG` errors
