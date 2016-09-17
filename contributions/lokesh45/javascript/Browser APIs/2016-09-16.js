16d797520e050f739e551db048156d401a881146
lokesh45 <police.lokesh23@gmail.com>
2016-09-17 19:38:28 +0100

    2016-09-16's javascript workout

---
+++ contributions/lokesh45/javascript/Browser APIs/2016-09-16.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Form a `URL` from its parts 
+Warn user if **Back** button is pressed
+Window.sessionStorage
+MediaDevices.getUserMedia()
