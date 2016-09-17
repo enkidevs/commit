2458a8750a7cec93fbd752b33b3f3833875156ec
jjsamudio <jjspanda@hotmail.com>
2016-09-17 23:22:00 +0100

    2016-09-17's javascript workout

---
+++ contributions/jjsamudio/javascript/Browser APIs/2016-09-17.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+Use hosted scripts to increase performance
+Warn user if **Back** button is pressed
+User Timing API
