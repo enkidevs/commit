ff62cc809f36b885248418efe7c2fe63ba82a00f
tyronegrech <grechtyrone1989@gmail.com>
2016-09-17 09:31:28 +0100

    2016-09-17's linux workout

---
+++ contributions/tyronegrech/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,7 @@
+Quick `bash` shortcuts 
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
