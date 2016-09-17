2567b54a8c983d774e2e80360834f9425e7d9324
metsuri34 <metsaranta.mikko@gmail.com>
2016-09-17 20:36:17 +0100

    2016-09-17's java workout

---
+++ contributions/metsuri34/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Common mistake on switch statements
+Distinction between `public` and `private` methods
+`Autoboxing` and `Unboxing`
+Using an interface as a parameter
