98c9c99dd42dce7c62333a1fcc6288b6b98775c1
sagiben <ilnextbus@gmail.com>
2016-09-17 06:17:35 +0100

    2016-09-17's java workout

---
+++ contributions/sagiben/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+The distinction between overloading and overriding methods
+Use strings in a `switch` statement
+Avoid using `static` variables
+Implementing an `interface`
