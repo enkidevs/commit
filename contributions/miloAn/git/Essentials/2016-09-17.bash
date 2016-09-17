d4e2131840374d59afa9c93a1e36c168943a26fb
miloAn <campero189@gmail.com>
2016-09-17 08:09:01 +0100

    2016-09-17's git workout

---
+++ contributions/miloAn/git/Essentials/2016-09-17.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+Search by commit message keyword
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
