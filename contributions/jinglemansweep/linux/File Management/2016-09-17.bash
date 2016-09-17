ef0b2357759d9934b3bf64d582606129904d370e
jinglemansweep <jinglemansweep@gmail.com>
2016-09-17 15:15:16 +0100

    2016-09-17's linux workout

---
+++ contributions/jinglemansweep/linux/File Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
