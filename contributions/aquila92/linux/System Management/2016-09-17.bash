7b60ef15eb956320f025937d60377a66df233369
aquila92 <giovanniaquila92@gmail.com>
2016-09-17 17:07:07 +0100

    2016-09-17's linux workout

---
+++ contributions/aquila92/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
