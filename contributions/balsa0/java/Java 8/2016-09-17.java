1942b58706a0c85458926221cfab103d369810c8
balsa0 <ktibalazs@gmail.com>
2016-09-17 19:53:03 +0100

    2016-09-17's java workout

---
+++ contributions/balsa0/java/Java 8/2016-09-17.java
@@ -0,0 +1,5 @@
+Java 8 Lambda expressions
+Using `Arrays.parallelSort` 
+How to check for static dependencies of applications and libraries from the command line.
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
