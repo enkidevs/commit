cb49f7e4609aa1ff20b1f29a56b01a346b55bea5
filiprund <filiprundberg@gmail.com>
2016-09-17 11:41:19 +0100

    2016-09-17's javascript workout

---
+++ contributions/filiprund/javascript/Node/2016-09-17.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Global Objects and Environment Variables in **Node**
+Handle `JSON.parse` error in Node.js
+`process.nextTick()` callback pattern
+Automatic compilation for Node with **Nodemon** 
