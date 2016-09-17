e81afdca2cbb439c78594839d03ff867b8549536
araghunde <paybol.21@gmail.com>
2016-09-17 15:19:03 +0100

    2016-09-17's linux workout

---
+++ contributions/araghunde/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
+The Basics of Environment Variables
