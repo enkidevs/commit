96c6625dd90f05ee860d8cf31f515a13311d8674
nodh <c.kollmann@gmail.com>
2016-09-17 20:14:37 +0100

    2016-09-17's java workout

---
+++ contributions/nodh/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Connecting to FTP using Java
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Supply `toString()` in all classes
