08a3d4c5166f46867365c7f6890fbaaf3c05b9a6
r1si <elia.baragiola@gmail.com>
2016-09-17 23:51:05 +0100

    2016-09-17's linux workout

---
+++ contributions/r1si/linux/System Management/2016-09-17.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Basic service management with `systemd`
