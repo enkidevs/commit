a07090a42a71ba449fca09ac95ac46bd00738e3f
naXa777 <alarmo.lord@gmail.com>
2016-09-17 22:31:08 +0100

    2016-09-17's linux workout

---
+++ contributions/naXa777/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
