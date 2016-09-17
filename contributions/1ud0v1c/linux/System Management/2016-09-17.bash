fec4caa30f2bf90b5ee0f2accef29d210ed2cd8d
1ud0v1c <ludovic.vimont@gmail.com>
2016-09-17 20:55:42 +0100

    2016-09-17's linux workout

---
+++ contributions/1ud0v1c/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
