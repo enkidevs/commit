f8fd4db6016c2e96f6185e6a3bb0d5132f772ef9
f0o <jshshwusksjg@devilcode.org>
2016-09-17 19:09:25 +0100

    2016-09-17's linux workout

---
+++ contributions/f0o./linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Finding Open Files With `lsof`
