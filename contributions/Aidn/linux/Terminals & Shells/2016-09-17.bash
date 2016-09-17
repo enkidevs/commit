9ea8b6cfe6e59b636a89f07fa73341d6bb085afe
Aidn <homuhe@gmail.com>
2016-09-17 17:34:02 +0100

    2016-09-17's linux workout

---
+++ contributions/Aidn/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
