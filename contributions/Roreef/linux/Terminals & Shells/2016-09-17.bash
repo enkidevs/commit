d4c0efe0d13c2c7b165d25963efb992a3d4dd2fd
Roreef <rowan05@gmail.com>
2016-09-17 23:48:19 +0100

    2016-09-17's linux workout

---
+++ contributions/Roreef/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Clear the terminal instantly
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
