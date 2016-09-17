70eeaa34ba97ddafdf274f0502deb25152fd9488
AlbertoOS <orkan.aos@gmail.com>
2016-09-18 00:05:15 +0100

    2016-09-17's linux workout

---
+++ contributions/AlbertoOS/linux/File Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
