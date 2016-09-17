04f3a7617b127b0cee0edcea4c25ae38cb63f4c4
oldteacher <drsantos@gmail.com>
2016-09-17 12:21:18 +0100

    2016-09-17's linux workout

---
+++ contributions/oldteacher/linux/Networking/2016-09-17.bash
@@ -0,0 +1,5 @@
+Useful ssh client optimizations 
+Bandwidth monitoring tools 
+Use `tcpdump` to listen to network interface traffic
+Staying connected remotely via SSH without an interactive shell
+Short Introduction to `Wget` 
