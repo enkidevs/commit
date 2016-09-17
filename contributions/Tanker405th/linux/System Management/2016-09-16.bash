3f71ed969dcbbfa07cf3f68a16c92a732a48ec5d
Tanker405th <thenuttyirishman@gmail.com>
2016-09-17 04:23:55 +0100

    2016-09-16's linux workout

---
+++ contributions/Tanker405th/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
