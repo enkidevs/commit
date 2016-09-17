12944c287f9c56feae64e3e6a04650a09e1bd38f
mohinishjoshi <mohinish@razoo.com>
2016-09-17 17:26:17 +0100

    2016-09-16's javascript workout

---
+++ contributions/mohinishjoshi/javascript/Browser APIs/2016-09-16.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+Form a `URL` from its parts 
+`Window.localStorage`
+Fetch API
