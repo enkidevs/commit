18c9e61d83ea4831b577de4760fe06f54dbd313c
JoshuaMorris <me@joshuamorris.info>
2016-09-17 05:52:46 +0100

    2016-09-16's linux workout

---
+++ contributions/JoshuaMorris/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Basic service management with `systemd`
+Cancel a system shutdown
