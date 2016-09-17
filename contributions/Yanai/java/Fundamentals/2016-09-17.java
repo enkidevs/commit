0389e6315c74aa53efb1a8f514e5b40ff953115a
Yanai <yanai.sachs@gmail.com>
2016-09-17 19:47:51 +0100

    2016-09-17's java workout

---
+++ contributions/Yanai/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
