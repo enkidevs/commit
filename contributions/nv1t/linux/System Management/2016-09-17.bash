acaaf448ddc964325c829575277b73cb51439c15
nv1t <nuit@hoeja.de>
2016-09-17 08:38:01 +0100

    2016-09-17's linux workout

---
+++ contributions/nv1t/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
