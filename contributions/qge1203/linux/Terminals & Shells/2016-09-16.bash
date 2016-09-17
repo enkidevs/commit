6580e688770a498b2f206590a9c2e35a404fe3f9
qge1203 <qge1203@gmail.com>
2016-09-17 01:47:57 +0100

    2016-09-16's linux workout

---
+++ contributions/qge1203/linux/Terminals & Shells/2016-09-16.bash
@@ -0,0 +1,5 @@
+Quick `bash` shortcuts 
+Random password generator
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
