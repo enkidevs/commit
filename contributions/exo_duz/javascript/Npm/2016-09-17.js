fe63b61a4d3bbb596f8b57cb492be6fa3c6aae91
exo_duz <exo_duz@yahoo.com>
2016-09-17 14:49:24 +0100

    2016-09-17's javascript workout

---
+++ contributions/exo_duz/javascript/Npm/2016-09-17.js
@@ -0,0 +1,5 @@
+Registry user accounts for npm
+Manipulate npm packages cache
+Calling remote scripts with npm
+Passing through command line arguments in npm scripts
+`npm`  as an alternative to Gulp
