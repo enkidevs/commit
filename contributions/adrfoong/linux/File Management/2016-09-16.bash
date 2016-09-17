07022cc0b9ee99bd9414219e359f4cbe80d860ea
adrfoong <eidorian.foong@gmail.com>
2016-09-17 05:31:22 +0100

    2016-09-16's linux workout

---
+++ contributions/adrfoong/linux/File Management/2016-09-16.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
