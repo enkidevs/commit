8cc52d9c89485e96937ce54e0f30c9fb4b53a8d3
MsGeburt <admin@marco-hajek.me>
2016-09-17 09:21:13 +0100

    2016-09-17's linux workout

---
+++ contributions/MsGeburt/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Finding Open Files With `lsof`
