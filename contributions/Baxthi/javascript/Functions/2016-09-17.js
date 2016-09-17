245170b57fc14fbff0628f2961d3b6518549ba2d
Baxthi <thi.kolmann@gmail.com>
2016-09-17 19:26:00 +0100

    2016-09-17's javascript workout

---
+++ contributions/Baxthi/javascript/Functions/2016-09-17.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Easily generate a random `HEX` color
+Social sharing without widgets
