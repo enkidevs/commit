31e53b5dc3a8923f6e469ba8c302181921c7e896
saka <saka71996@gmail.com>
2016-09-17 05:42:20 +0100

    2016-09-17's linux workout

---
+++ contributions/saka/linux/Networking/2016-09-17.bash
@@ -0,0 +1,5 @@
+Logging dropped firewall packets
+Check `iptables` firewall status
+Useful ssh client optimizations 
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
