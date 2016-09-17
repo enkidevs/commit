2aac1882279cd6c94e2ad162c5c12c20002ccd8d
roncapat <ronca.pat@gmail.com>
2016-09-17 09:02:52 +0100

    2016-09-17's linux workout

---
+++ contributions/roncapat/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
