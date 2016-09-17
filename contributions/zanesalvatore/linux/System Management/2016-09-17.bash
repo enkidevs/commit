0e5af73120f1ff8db0f3bb3075bebde32cfaf535
zanesalvatore <zanesalvatore@gmail.com>
2016-09-17 18:38:39 +0100

    2016-09-17's linux workout

---
+++ contributions/zanesalvatore/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
