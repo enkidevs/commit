e827ec3bae82d8d35fa12689ec87b5c4a58803eb
yasada <y.asada216@gmail.com>
2016-09-17 05:04:22 +0100

    2016-09-15's java workout

---
+++ contributions/yasada/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Locks in `static synchronized` methods
+Always specify type parameter
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
