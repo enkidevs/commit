9c6994814b5c18f37b40b25bd59f08d6ee1e8e0e
BoseSean <xinyesg@gamil.com>
2016-09-17 06:52:11 +0100

    2016-09-16's linux workout

---
+++ contributions/BoseSean/linux/Terminals & Shells/2016-09-16.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
