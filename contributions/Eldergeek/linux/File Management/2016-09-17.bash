6236775f8d5b12cbb4e20913c7350f9be5f895f0
Eldergeek <david@geekguytech.com>
2016-09-17 16:29:43 +0100

    2016-09-17's linux workout

---
+++ contributions/Eldergeek/linux/File Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Deleting non-empty directories
+Working with `.gz` files
