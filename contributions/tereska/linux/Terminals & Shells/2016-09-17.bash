fe5fd9825afb4bcc44f58675a83929fa0cd57835
tereska <janecki@gmail.com>
2016-09-17 19:00:24 +0100

    2016-09-17's linux workout

---
+++ contributions/tereska/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Terminal based browser
+Useful aliasing in bash
+Random password generator
