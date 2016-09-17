8c85c5ffb1db0b491f72671ea13b8eb92816b83c
tnargwoxow <tnargwoxow@gmail.com>
2016-09-17 09:13:29 +0100

    2016-09-17's linux workout

---
+++ contributions/tnargwoxow/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
