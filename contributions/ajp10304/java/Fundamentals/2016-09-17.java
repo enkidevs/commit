9b995f3177d4ad4669b212bf22aa5c4f8054bd59
ajp10304 <a.pocklington@gmail.com>
2016-09-17 21:33:48 +0100

    2016-09-17's java workout

---
+++ contributions/ajp10304/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
