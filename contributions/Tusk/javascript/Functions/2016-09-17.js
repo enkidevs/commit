3a14bf7f8f7898adce2c9d97e7f64c2b46fd59e1
Tusk <tuskone16@gmail.com>
2016-09-17 13:36:53 +0100

    2016-09-17's javascript workout

---
+++ contributions/Tusk/javascript/Functions/2016-09-17.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
