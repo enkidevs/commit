4b3458ad6dfea08e81fb37748f71abab1eb74382
jvasallo <joelvasallo@gmail.com>
2016-09-17 19:39:33 +0100

    2016-09-17's linux workout

---
+++ contributions/jvasallo/linux/Package Management/2016-09-17.bash
@@ -0,0 +1,6 @@
+Install specific version of packages
+Backporting on Debian
+
+List installed packages
+Prevent updating a specific package in Debian systems
+Using `Tasksel` for software installation
