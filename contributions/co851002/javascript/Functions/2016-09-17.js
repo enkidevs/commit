aa568017c8e7abb872450a297d0e63563cdcb8b4
co851002 <co851002@gmail.com>
2016-09-17 15:39:20 +0100

    2016-09-17's javascript workout

---
+++ contributions/co851002/javascript/Functions/2016-09-17.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
