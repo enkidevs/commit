865632c2be88cb449204250352e7c10ca1795ea1
infiniteshroom <infiniteshroom@gmail.com>
2016-09-17 15:30:19 +0100

    2016-09-17's linux workout

---
+++ contributions/infiniteshroom/linux/Networking/2016-09-17.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Check `iptables` firewall status
+Multiple IP Address with aliasing 
+Sending emails from terminal / scripts
+Staying connected remotely via SSH without an interactive shell
