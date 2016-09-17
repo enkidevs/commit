db2473485c54ce3bc35188b7cc6d719e758540c2
games647 <games@tsprivat.de>
2016-09-17 09:14:19 +0100

    2016-09-17's linux workout

---
+++ contributions/games647/linux/File Management/2016-09-17.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
