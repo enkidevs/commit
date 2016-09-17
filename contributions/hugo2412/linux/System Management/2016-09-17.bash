646eb9cf125da5a1e65a5f42065de3e8b0d8ec48
hugo2412 <hb.fontvieille@gmail.com>
2016-09-17 09:58:59 +0100

    2016-09-17's linux workout

---
+++ contributions/hugo2412/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
