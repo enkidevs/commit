817bbca13d8cce2a75551bd7da3c2bdc4a2ecad9
annabaik <annaheyonbaik@gmail.com>
2016-09-17 09:37:47 +0100

    2016-09-17's linux workout

---
+++ contributions/annabaik/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
