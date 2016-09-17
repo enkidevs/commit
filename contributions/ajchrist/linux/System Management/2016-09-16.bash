daa0b88bc0e98f9e4257dca9102cbb12889393ab
ajchrist <ajchristiano91@gmail.com>
2016-09-17 01:52:53 +0100

    2016-09-16's linux workout

---
+++ contributions/ajchrist/linux/System Management/2016-09-16.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Cancel a system shutdown
