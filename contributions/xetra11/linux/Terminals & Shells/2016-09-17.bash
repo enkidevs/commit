46820bda15fa414d50fa8b68ffade73f7477891e
xetra11 <xetra11@outlook.com>
2016-09-17 07:25:25 +0100

    2016-09-17's linux workout

---
+++ contributions/xetra11/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+Retrieve previous arguments
