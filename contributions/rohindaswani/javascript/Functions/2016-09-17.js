a322d0a73784f3cc05dc8c65a1c60feb32a23654
rohindaswani <rohindaswani@gmail.com>
2016-09-17 19:00:20 +0100

    2016-09-17's javascript workout

---
+++ contributions/rohindaswani/javascript/Functions/2016-09-17.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Method chaining
+Easily generate a random `HEX` color
