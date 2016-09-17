435f7fa118c19eb5fa304a297052e8f05ed228b5
cecco <francescousseglio@gmail.com>
2016-09-17 09:24:33 +0100

    2016-09-17's linux workout

---
+++ contributions/cecco/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Finding Open Files With `lsof`
