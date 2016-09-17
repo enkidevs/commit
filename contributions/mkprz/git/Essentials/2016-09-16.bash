6e747b574cb4ed0dd48edef4f06e0f44e97402f5
mkprz <meekprize@gmail.com>
2016-09-17 07:30:34 +0100

    2016-09-16's git workout

---
+++ contributions/mkprz/git/Essentials/2016-09-16.bash
@@ -0,0 +1,5 @@
+Make your log output pretty
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Ignore files in git
