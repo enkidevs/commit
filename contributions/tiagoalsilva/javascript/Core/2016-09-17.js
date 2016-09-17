be09ca9993239193521f05e08237ec4b1c1a8fc9
tiagoalsilva <tiagoalsilva@gmail.com>
2016-09-17 11:39:54 +0100

    2016-09-17's javascript workout

---
+++ contributions/tiagoalsilva/javascript/Core/2016-09-17.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Setting default values with `short circuiting`
+Using the double tilde `~~` 
+Most efficient way to build `HTML` strings
+Format currency in JavaScript
