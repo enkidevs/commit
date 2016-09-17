2a3335ca7877d8d17428572f514b3813ab944e2a
Milmar <milmarwoot@gmail.com>
2016-09-17 19:27:12 +0100

    2016-09-17's linux workout

---
+++ contributions/Milmar/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
