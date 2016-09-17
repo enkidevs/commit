ef8177146c50eacfd7ff4c2dc967e77ccdca087e
mrgrain <me@momo.kiwi>
2016-09-18 00:58:07 +0100

    2016-09-17's linux workout

---
+++ contributions/mrgrain/linux/Package Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Get `apt` to use a mirror / faster mirror
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
