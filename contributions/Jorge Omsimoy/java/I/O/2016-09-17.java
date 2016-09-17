58031e5a2e03d49a5f8df92ec66c50bd2a2eaf5c
Jorge Omsimoy <jorgeafs99@gmail.com>
2016-09-17 19:54:18 +0100

    2016-09-17's java workout

---
+++ contributions/Jorge Omsimoy/java/I/O/2016-09-17.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Checking for the existence of files and directories
+Reading file attributes
+List contents of a directory with `newDirectoryStream`
+Deleting files and directories
