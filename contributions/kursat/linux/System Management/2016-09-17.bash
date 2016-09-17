aafe328b94a7a7ae423580c5904c78628714e9ea
kursat <kursat.yigitoglu@gmail.com>
2016-09-18 00:02:08 +0100

    2016-09-17's linux workout

---
+++ contributions/kursat/linux/System Management/2016-09-17.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
