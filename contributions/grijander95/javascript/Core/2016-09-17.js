a7e2abfadd66efc0b4a75eb7b908a93a0a5d3aa5
grijander95 <correoparacacas2@gmail.com>
2016-09-17 23:56:42 +0100

    2016-09-17's javascript workout

---
+++ contributions/grijander95/javascript/Core/2016-09-17.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Using the double tilde `~~` 
+Hoisting applies only to variable declarations, not initializations
+The difference between null, undefined and NaN
+Avoid using `with`
