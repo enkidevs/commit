7d249a5fbd24db7049858664344a120a9cc6f6d4
martin-rifon <mrifonelizalde@gmail.com>
2016-09-17 01:55:31 +0100

    2016-09-16's linux workout

---
+++ contributions/martin-rifon/linux/Terminals & Shells/2016-09-16.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Terminal incognito mode
+`else` statements using the `||` operator 
