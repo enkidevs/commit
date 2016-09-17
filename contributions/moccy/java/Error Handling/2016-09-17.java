20723cc5fdd4966b96b2c804cf1f5bffbcf442de
moccy <elliot+github@dewhurst.me>
2016-09-17 19:45:22 +0100

    2016-09-17's java workout

---
+++ contributions/moccy/java/Error Handling/2016-09-17.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Throwing an `exception`
