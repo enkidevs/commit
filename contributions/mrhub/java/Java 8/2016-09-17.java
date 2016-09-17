25d1a364ee096009df918ef8a9416a0f83dd4a2e
mrhub <niklas.huss@gmail.com>
2016-09-17 07:37:12 +0100

    2016-09-17's java workout

---
+++ contributions/mrhub/java/Java 8/2016-09-17.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Using `Arrays.parallelSort` 
+How to check for static dependencies of applications and libraries from the command line.
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
