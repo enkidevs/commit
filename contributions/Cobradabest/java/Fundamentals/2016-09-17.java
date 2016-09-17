2f30cad9457c8794a29126fe4e831ad034df74b7
Cobradabest <cobradabest@aol.com>
2016-09-17 14:24:43 +0100

    2016-09-17's java workout

---
+++ contributions/Cobradabest/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Limit Accessibility of `Fields`
