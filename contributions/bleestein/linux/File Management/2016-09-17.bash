f32c5527e4932903e326241d1f061ef00a06345d
bleestein <bleestein@gmail.com>
2016-09-17 21:31:20 +0100

    2016-09-17's linux workout

---
+++ contributions/bleestein/linux/File Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
