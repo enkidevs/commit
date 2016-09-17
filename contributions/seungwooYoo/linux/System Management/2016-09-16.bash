d55c82a04eaea4b7e0f9409e4553dc8ca0d04687
seungwooYoo <seungwoo.yoo@gmail.com>
2016-09-17 08:34:10 +0100

    2016-09-16's linux workout

---
+++ contributions/seungwooYoo/linux/System Management/2016-09-16.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
