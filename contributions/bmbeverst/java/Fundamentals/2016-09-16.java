9fa63cc82e3460639b800fc69da54be47b00318c
bmbeverst <bmbeverst@gmail.com>
2016-09-17 04:14:40 +0100

    2016-09-16's java workout

---
+++ contributions/bmbeverst/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
+Using an interface as a parameter
