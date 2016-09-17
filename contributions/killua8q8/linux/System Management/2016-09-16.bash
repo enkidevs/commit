336e1331298bfbec577f93908f5a4abf49f738e0
killua8q8 <killua8q8@gmail.com>
2016-09-17 01:08:05 +0100

    2016-09-16's linux workout

---
+++ contributions/killua8q8/linux/System Management/2016-09-16.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Cancel a system shutdown
