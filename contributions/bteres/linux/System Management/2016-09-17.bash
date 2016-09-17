6f3a5460048cd77897e36cc6fbb79c40205ca7ce
bteres <bteres@gmail.com>
2016-09-17 08:33:38 +0100

    2016-09-17's linux workout

---
+++ contributions/bteres/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
