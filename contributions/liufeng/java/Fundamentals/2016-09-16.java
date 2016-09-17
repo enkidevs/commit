b93e0d89894b118243a56dbb43d50222c7a6c1fc
liufeng <liufeng5460@163.com>
2016-09-17 04:46:02 +0100

    2016-09-16's java workout

---
+++ contributions/liufeng/java/Fundamentals/2016-09-16.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+The trade-off between short and descriptive names
+Connecting to FTP using Java
+Avoid using `static` variables
