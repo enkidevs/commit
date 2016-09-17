8e51a2c61e192c9f21afe42d349fbfffa31e61de
dorharr <dorharr@gmail.com>
2016-09-17 10:19:24 +0100

    2016-09-17's linux workout

---
+++ contributions/dorharr/linux/File Management/2016-09-17.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
+Easy way of sharing files
