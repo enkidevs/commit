c1f7922f473b6ea9b8edd64fbdd665b06c03671b
Swooky <swooky@outlook.com>
2016-09-17 15:26:49 +0100

    2016-09-17's java workout

---
+++ contributions/Swooky/java/Error Handling/2016-09-17.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Most common reason behind **stack overflow** error
