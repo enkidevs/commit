1e93ba33a4f72019c0794419bddcd22af5df87c0
wacuuu <jwalecki1996@gmail.com>
2016-09-17 10:25:29 +0100

    2016-09-17's java workout

---
+++ contributions/wacuuu/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Using an interface as a parameter
