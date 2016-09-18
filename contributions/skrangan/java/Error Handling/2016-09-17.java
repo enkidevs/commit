07ff9b1ee2cd6db8475bb9a711bb2305cdd8116c
skrangan <skrangan@ymail.com>
2016-09-18 12:55:48 +0100

    2016-09-17's java workout

---
+++ contributions/skrangan/java/Error Handling/2016-09-17.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
