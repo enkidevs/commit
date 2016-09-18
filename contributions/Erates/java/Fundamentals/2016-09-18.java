d805e47efe75058fe935d7fc1a6f6853849d1e68
Erates <reasno@hotmail.com>
2016-09-18 19:04:24 +0100

    2016-09-18's java workout

---
+++ contributions/Erates/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
+Using an interface as a parameter
