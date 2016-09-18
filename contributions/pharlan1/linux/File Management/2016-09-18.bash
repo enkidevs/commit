44748afb9eaba4888771992ca037fcae130fea4f
pharlan1 <pharlan42@gmail.com>
2016-09-18 19:43:05 +0100

    2016-09-18's linux workout

---
+++ contributions/pharlan1/linux/File Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
+Delete commands aliases
