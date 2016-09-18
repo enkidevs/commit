569060b6c4dc14ce45ca9c03e14e772a3e9a6b6a
P1lgrim <mateuszzagorski1@gmail.com>
2016-09-18 17:37:57 +0100

    2016-09-18's javascript workout

---
+++ contributions/P1lgrim/javascript/Core/2016-09-18.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Use `splice()` to remove an item from an array
+Using assignment operators
+Inaccuracy of binary floating-point format
+Round numbers to `N` decimals 
