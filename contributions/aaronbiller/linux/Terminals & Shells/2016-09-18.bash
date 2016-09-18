74c865780eb6adbaa111112f0074467d96648dec
aaronbiller <aaronbiller@gmail.com>
2016-09-18 09:53:13 +0100

    2016-09-18's linux workout

---
+++ contributions/aaronbiller/linux/Terminals & Shells/2016-09-18.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Random password generator
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
