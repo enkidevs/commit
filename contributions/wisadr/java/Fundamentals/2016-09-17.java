3623fdb7537b600508a8830e991a8097f63a3a10
wisadr <ardianows@gmail.com>
2016-09-18 01:36:31 +0100

    2016-09-17's java workout

---
+++ contributions/wisadr/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Inheriting a constructor from a superclass
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
