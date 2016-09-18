a1cce49900e7e0ed237dfe4d9be461db7f8fa6c8
PatitoxD <mail2patrickengel@gmail.com>
2016-09-18 11:37:43 +0100

    2016-09-18's javascript workout

---
+++ contributions/PatitoxD/javascript/Core/2016-09-18.js
@@ -0,0 +1,5 @@
+Precision
+Labeling your loops
+Hoisting applies only to variable declarations, not initializations
+Extra function arguments are undefined by default
+Detect **DO NOT TRACK** status
