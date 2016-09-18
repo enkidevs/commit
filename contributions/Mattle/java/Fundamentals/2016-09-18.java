1903a0d8962ea852ccff8e44aac522fe7c7478e0
Mattle <mcallier@gmail.com>
2016-09-18 17:07:24 +0100

    2016-09-18's java workout

---
+++ contributions/Mattle/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+The trade-off between short and descriptive names
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
