7924b206b4f6226dbe4cbd8cee99f992adb653d5
greg-solomon <greg.solomon95@gmail.com>
2016-09-18 14:32:38 +0100

    2016-09-18's linux workout

---
+++ contributions/greg-solomon/linux/System Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
