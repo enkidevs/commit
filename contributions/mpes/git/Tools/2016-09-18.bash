d042001c81549576bdbad9f0a1a388e61767a100
mpes <michal.pesicka@gmail.com>
2016-09-18 20:32:56 +0100

    2016-09-18's git workout

---
+++ contributions/mpes/git/Tools/2016-09-18.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Interactively unstage changes
+Specify a commit by its ancestry
+Specify a range of commits using double dot syntax
+Show history of a function
