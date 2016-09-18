08b092f4b216b7f220439cb00ebd16782b59b9d8
kiki407 <kiki407@hotmail.com>
2016-09-18 09:59:53 +0100

    2016-09-18's javascript workout

---
+++ contributions/kiki407/javascript/Functions/2016-09-18.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Method chaining
+Easily generate a random `HEX` color
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
