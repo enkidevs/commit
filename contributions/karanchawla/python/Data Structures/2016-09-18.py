f2751c61700dfa1af7b9448c7dd352ea107a47cd
karanchawla <karanchawla.ck@gmail.com>
2016-09-19 00:17:13 +0100

    2016-09-18's python workout

---
+++ contributions/karanchawla/python/Data Structures/2016-09-18.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+When `range` comes in handy
+Double ended queues with `deque`
+Special attributes of objects and classes
+Operations with `bytes` and `bytearray`
