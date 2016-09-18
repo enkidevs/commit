5e5af29a602cfdf41c943b3f2a259b6fa76d3c15
kortatu <kortatu@gmail.com>
2016-09-18 20:46:30 +0100

    2016-09-18's linux workout

---
+++ contributions/kortatu/linux/Terminals & Shells/2016-09-18.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Find any Unix / Linux command
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
