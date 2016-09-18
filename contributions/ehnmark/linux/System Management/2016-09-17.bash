4a9839bf83d128bf559d28e7e0a74e141d806f14
ehnmark <github@ehnmark.com>
2016-09-18 15:13:47 +0100

    2016-09-17's linux workout

---
+++ contributions/ehnmark/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Finding Open Files With `lsof`
