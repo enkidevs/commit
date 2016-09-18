ca5a53bde400f68473bc8a99cbc27041ce9bcefd
GoDodgers <alberto.m.esquivias@gmail.com>
2016-09-19 00:56:34 +0100

    2016-09-18's git workout

---
+++ contributions/GoDodgers/git/Essentials/2016-09-18.bash
@@ -0,0 +1,5 @@
+Using tags for version control
+What is stored in a commit?
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
