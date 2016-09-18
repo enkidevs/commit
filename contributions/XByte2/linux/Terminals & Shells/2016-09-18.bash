0075614f5a6041859a194aaf7359d6c58ba91ea7
XByte2 <xbyte80@gmail.com>
2016-09-18 18:05:52 +0100

    2016-09-18's linux workout

---
+++ contributions/XByte2/linux/Terminals & Shells/2016-09-18.bash
@@ -0,0 +1,7 @@
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
