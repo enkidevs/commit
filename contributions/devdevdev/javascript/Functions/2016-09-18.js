aa17344ef2b379b1f8bb59a32a08488e530480a2
devdevdev <whiskeyrockaroller@gmail.com>
2016-09-18 17:47:34 +0100

    2016-09-18's javascript workout

---
+++ contributions/devdevdev/javascript/Functions/2016-09-18.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Method chaining
+Move cursor at the end of text input
+Social sharing without widgets
