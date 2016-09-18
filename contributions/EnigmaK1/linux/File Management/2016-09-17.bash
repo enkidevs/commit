8dd8018082cfd542e71105be887fe61a34fc1a9d
EnigmaK1 <singhkanishka1@gmail.com>
2016-09-18 06:54:32 +0100

    2016-09-17's linux workout

---
+++ contributions/EnigmaK1/linux/File Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Using `touch` to alter files modification time
+Easy way of sharing files
