ed5973e16dce53b9ae547526635aa2ee86b66a05
MrCrowned <dom.brause@gmail.com>
2016-09-18 09:56:05 +0100

    2016-09-18's linux workout

---
+++ contributions/MrCrowned/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
