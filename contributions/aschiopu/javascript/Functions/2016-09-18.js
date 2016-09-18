46011c0d5c824155ce4619d6849248de4257ad8b
aschiopu <alexxschiopu@gmail.com>
2016-09-18 15:57:37 +0100

    2016-09-18's javascript workout

---
+++ contributions/aschiopu/javascript/Functions/2016-09-18.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Function call method
+Check if a document is done loading
