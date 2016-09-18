51df11e4510a2cc6858f2bf117d100fb4814e3bc
rakshans1 <shetty.raxx555@gmail.com>
2016-09-18 04:39:48 +0100

    2016-09-17's javascript workout

---
+++ contributions/rakshans1/javascript/React/2016-09-17.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Functional Stateless Components in React
+Self-closing tags in **JSX**
+Props in getInitialState Is an        
+Anti-Pattern
+Dangerously set `innerHTML`
