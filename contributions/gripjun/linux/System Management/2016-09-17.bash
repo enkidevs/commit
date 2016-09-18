858bf2e04f1fd86af148aa78dc4283aab2e051d6
gripjun <gripjun@gmail.com>
2016-09-18 02:50:21 +0100

    2016-09-17's linux workout

---
+++ contributions/gripjun/linux/System Management/2016-09-17.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
