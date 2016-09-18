6880823b6780da224a2e912ae6615a4478ff2755
oscp <adamrodacki@gmail.com>
2016-09-18 22:00:15 +0100

    2016-09-18's linux workout

---
+++ contributions/oscp/linux/System Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Force a time update with `ntp`
+
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
