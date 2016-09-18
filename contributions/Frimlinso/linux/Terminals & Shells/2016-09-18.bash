433d2fe6ff711b1f8739ff391818fb00c04ca440
Frimlinso <roysiu@outlook.com>
2016-09-18 20:48:15 +0100

    2016-09-18's linux workout

---
+++ contributions/Frimlinso/linux/Terminals & Shells/2016-09-18.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
