b158d861cb4b4d71bdd224d30a0815e3bb451da9
GoryMoon <gurreja@gmail.com>
2016-09-18 12:35:48 +0100

    2016-09-18's java workout

---
+++ contributions/GoryMoon/java/I/O/2016-09-18.java
@@ -0,0 +1,5 @@
+Using buffered streams
+Checking for the existence of files and directories
+Reading file attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
