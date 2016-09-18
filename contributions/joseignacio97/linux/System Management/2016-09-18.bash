cfb9504f96f749884e5b0c2906a9b51f023f54cc
joseignacio97 <joseignacioescudero97@hotmail.com>
2016-09-18 23:12:05 +0100

    2016-09-18's linux workout

---
+++ contributions/joseignacio97/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Using `dd` To Create Disk Images
