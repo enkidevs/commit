ff4d156644f77c1638e3c4c7c2309c0930d64e0c
Sawel <danielsolomon94.ds@gmail.com>
2016-09-18 11:46:16 +0100

    2016-09-18's linux workout

---
+++ contributions/Sawel/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
