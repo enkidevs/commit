9a3ad2b77572ad0b86d88687c08d43a1be86d7d2
zackespinoza <zackespinoza@gmail.com>
2016-09-18 01:16:25 +0100

    2016-09-17's java workout

---
+++ contributions/zackespinoza/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
