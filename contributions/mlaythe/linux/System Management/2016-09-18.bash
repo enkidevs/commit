66cbfe7207917f7950c0061097156a9bcb6cd3b8
mlaythe <mrlaythe24@aol.com>
2016-09-18 17:38:50 +0100

    2016-09-18's linux workout

---
+++ contributions/mlaythe/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Test disk speed with `dd`
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Monitor System Memory using `vmstat`
