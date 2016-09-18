550f5857dbe8ede4af845cef29ab9a89e20e07a5
rbaileycs <rbailey.cs@gmail.com>
2016-09-19 00:29:06 +0100

    2016-09-18's java workout

---
+++ contributions/rbaileycs/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Always specify type parameter
+Avoid using `static` variables
