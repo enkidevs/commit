6b505653ccdeaed9c0de1fbbb52bf35d36900b86
tsdesigns <nickedwards69@googlemail.com>
2016-09-18 14:43:32 +0100

    2016-09-18's linux workout

---
+++ contributions/tsdesigns/linux/Terminals & Shells/2016-09-18.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Terminal incognito mode
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
