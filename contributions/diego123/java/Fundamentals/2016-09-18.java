ffc86ff64f3e6a903508b61b0a76707d743ee075
diego123 <bartosz.szymanski1@gmail.com>
2016-09-18 09:08:31 +0100

    2016-09-18's java workout

---
+++ contributions/diego123/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Use meaningful names
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
