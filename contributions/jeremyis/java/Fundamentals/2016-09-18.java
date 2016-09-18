aa0aea025092e3c3684454f012106ba89bd0fd6d
jeremyis <jeremyisatmit+enki@gmail.com>
2016-09-18 18:31:37 +0100

    2016-09-18's java workout

---
+++ contributions/jeremyis/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
