1bd14a560356b8bed1fd8415c5c6976992876a64
JCHHeilmann <johannes_heilmann@t-online.de>
2016-09-18 19:53:05 +0100

    2016-09-18's java workout

---
+++ contributions/JCHHeilmann/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
