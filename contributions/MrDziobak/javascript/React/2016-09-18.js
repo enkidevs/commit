e787514c4c9844471e94650a072870b0b6b9021a
MrDziobak <wkrzychuw@gmail.com>
2016-09-18 09:59:05 +0100

    2016-09-18's javascript workout

---
+++ contributions/MrDziobak/javascript/React/2016-09-18.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+componentWillReceiveProps Not Triggered After Mounting
+
+Dangerously set `innerHTML`
+Clone Elements in **React**
+Default values for props
