834996b660a8971e47a3cbc50e1ccfe649fc74a2
manitua <jasmin.beharic@gmail.com>
2016-09-18 10:38:49 +0100

    2016-09-18's linux workout

---
+++ contributions/manitua/linux/File Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
