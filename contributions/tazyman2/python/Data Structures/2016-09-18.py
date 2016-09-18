ac3fdecc64a32c987a4fb436f9bdc47c8219288c
tazyman2 <dlees094@uottawa.ca>
2016-09-18 15:09:35 +0100

    2016-09-18's python workout

---
+++ contributions/tazyman2/python/Data Structures/2016-09-18.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Best way to implement a simple `queue` 
+How to count
+Implementing **weak** references
+Binary record packing with `bytearray`
