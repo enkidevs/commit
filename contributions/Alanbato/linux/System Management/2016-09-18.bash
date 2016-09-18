43860758950b5f31e943ae7eee11d66d9419ab94
Alanbato <alanvelasco.a@gmail.com>
2016-09-18 15:48:13 +0100

    2016-09-18's linux workout

---
+++ contributions/Alanbato/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Detect your linux distribution
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
