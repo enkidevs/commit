28fb298d65691acb5566365d092add309a9e3134
inorishizuka <nekos@kyu.moe>
2016-09-18 09:51:37 +0100

    2016-09-18's java workout

---
+++ contributions/inorishizuka/java/Data Types/2016-09-18.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Converting a string to upper or lower case
+Retrieve the component type of an array
+String concatenation in Java
+Do not attempt comparisons with NaN
