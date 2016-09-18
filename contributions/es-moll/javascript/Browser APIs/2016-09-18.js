1a1a6a95eda1ee214fd3eedc3d4aa058f0face1a
es-moll <es-moll@inbox.ru>
2016-09-18 10:29:55 +0100

    2016-09-18's javascript workout

---
+++ contributions/es-moll/javascript/Browser APIs/2016-09-18.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+CSS for when JavaScript is enabled
+Window.sessionStorage
+Fetch API
