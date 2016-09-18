71848ae23d75dca626b00c74db47529ca2ff1c9e
corpulentcoffee <dave@corpulentcoffee.com>
2016-09-19 00:04:30 +0100

    2016-09-18's linux workout

---
+++ contributions/corpulentcoffee/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
