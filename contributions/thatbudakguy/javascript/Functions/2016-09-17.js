010a2b796156b1ac416a8e441cbf254061f56193
thatbudakguy <thatbudakguy@gmail.com>
2016-09-18 01:41:25 +0100

    2016-09-17's javascript workout

---
+++ contributions/thatbudakguy/javascript/Functions/2016-09-17.js
@@ -0,0 +1,5 @@
+Hoisting
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
