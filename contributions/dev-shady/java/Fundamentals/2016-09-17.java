372179c8c0aa898677cf5b850e5f319f6fa8faf7
dev-shady <itsdevshady@gmail.com>
2016-09-18 12:30:14 +0100

    2016-09-17's java workout

---
+++ contributions/dev-shady/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between `==` and `equals()`
+Catch multiple exceptions in a single `catch` block
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
