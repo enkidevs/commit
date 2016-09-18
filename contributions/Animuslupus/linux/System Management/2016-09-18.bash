248715c27c303f35291879dac7979f2c1c8f41ef
Animuslupus <elias-wolf@t-online.de>
2016-09-18 10:01:04 +0100

    2016-09-18's linux workout

---
+++ contributions/Animuslupus/linux/System Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
