3aa29640fc2c3cd62a09cace6caaf05d20bbd785
dimipage <dimi.page@gmail.com>
2016-09-18 23:33:01 +0100

    2016-09-18's linux workout

---
+++ contributions/dimipage/linux/System Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Monitor System Memory using `vmstat`
