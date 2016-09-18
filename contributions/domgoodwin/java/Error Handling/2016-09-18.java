f8da03ef93a1c225f04a8aed174e2d53a3835d50
domgoodwin <d0m182goodwin@gmail.com>
2016-09-18 08:59:31 +0100

    2016-09-18's java workout

---
+++ contributions/domgoodwin/java/Error Handling/2016-09-18.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Most common reason behind **stack overflow** error
+Throwing an `exception`
