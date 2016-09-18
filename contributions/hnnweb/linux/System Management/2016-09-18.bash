d3185b63ea2e8dba4c02c0c01fc5980034fc8103
hnnweb <henrik@b6.se>
2016-09-18 21:22:01 +0100

    2016-09-18's linux workout

---
+++ contributions/hnnweb/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
