eec950a67fd8d87162838a5195f2939f1b7bc393
mjnitz02 <mjnitz02@gmail.com>
2016-09-18 15:45:03 +0100

    2016-09-18's linux workout

---
+++ contributions/mjnitz02/linux/Terminals & Shells/2016-09-18.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+The Basics of Environment Variables
