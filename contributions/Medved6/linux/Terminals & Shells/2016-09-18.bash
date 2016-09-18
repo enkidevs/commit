f2cb171cfa5a01776523b92180983fc98bb73ec1
Medved6 <swsevolojsky@gmail.com>
2016-09-19 00:17:38 +0100

    2016-09-18's linux workout

---
+++ contributions/Medved6/linux/Terminals & Shells/2016-09-18.bash
@@ -0,0 +1,7 @@
+Find any Unix / Linux command
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
