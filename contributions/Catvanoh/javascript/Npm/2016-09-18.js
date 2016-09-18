5d7fa524fd894409f2710501a3c8a15ff72028c2
Catvanoh <pavelpie@gmail.com>
2016-09-18 09:27:13 +0100

    2016-09-18's javascript workout

---
+++ contributions/Catvanoh/javascript/Npm/2016-09-18.js
@@ -0,0 +1,5 @@
+Registry user accounts for npm
+npm can handle multiple versions of a dependency
+`npm` verifies `sha1` hashes of packages
+`optionalDependencies` in npm
+Lock down dependency versions by shrinkwrapping
