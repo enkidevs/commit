142aa988eabab6d74a1c9ccdc73aa67875892b82
lilykissme <459811276@qq.com>
2016-09-18 03:58:08 +0100

    2016-09-17's linux workout

---
+++ contributions/lilykissme/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Find any Unix / Linux command
+Quick `bash` shortcuts 
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
