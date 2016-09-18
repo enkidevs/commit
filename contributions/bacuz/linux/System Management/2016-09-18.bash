29a29adb698385385ddbd1bf12da8bf12d278fdc
bacuz <jbetances07@gmail.com>
2016-09-18 09:37:20 +0100

    2016-09-18's linux workout

---
+++ contributions/bacuz/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
