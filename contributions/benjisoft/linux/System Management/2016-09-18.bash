3edb044eac8b0da241899277addfdf18fa19cd2d
benjisoft <benji@benjicraft.info>
2016-09-18 22:05:39 +0100

    2016-09-18's linux workout

---
+++ contributions/benjisoft/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
