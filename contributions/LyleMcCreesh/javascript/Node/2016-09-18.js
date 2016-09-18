2f82cc14612f54b1d01671ece644753258dadac5
LyleMcCreesh <lyle.mccreesh@gmail.com>
2016-09-18 10:28:35 +0100

    2016-09-18's javascript workout

---
+++ contributions/LyleMcCreesh/javascript/Node/2016-09-18.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Streams in **Node**
+`uncaughtException` listener in Node.js
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
