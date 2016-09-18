cb8bb7711ce66e279f6a47568811305d9ab29d1a
pedrosfa <sfa.pedro@gmail.com>
2016-09-18 19:19:31 +0100

    2016-09-18's linux workout

---
+++ contributions/pedrosfa/linux/File Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Delete commands aliases
