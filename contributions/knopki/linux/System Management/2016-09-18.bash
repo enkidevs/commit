8ee9fc6b3b3a4de1039c54b37641f8b63f393f77
knopki <korolev.srg@gmail.com>
2016-09-18 23:41:15 +0100

    2016-09-18's linux workout

---
+++ contributions/knopki/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
