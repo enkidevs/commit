4d92271e25bf2cae64a218f8b1849c9e9404821b
zeroFruit <14wnrkim@gmail.com>
2016-09-18 11:37:46 +0100

    2016-09-17's linux workout

---
+++ contributions/zeroFruit/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
