5ff8c751855a272cbb8d5df46e12e255ac1572ca
darwin_nacionales <darwin_nacionales@yahoo.com>
2016-09-18 06:53:57 +0100

    2016-09-17's linux workout

---
+++ contributions/darwin_nacionales/linux/System Management/2016-09-17.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
