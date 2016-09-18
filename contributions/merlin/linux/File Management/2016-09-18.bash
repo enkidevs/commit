78919cdb58997afba60ac737fb9fd1e6a5a992f8
merlin <dominic.schaff@gmail.com>
2016-09-18 09:14:11 +0100

    2016-09-18's linux workout

---
+++ contributions/merlin/linux/File Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
