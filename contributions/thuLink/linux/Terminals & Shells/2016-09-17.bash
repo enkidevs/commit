2657f676398f2d7ae5a4a50d511f1def6c533729
thuLink <xazrael@hotmail.com>
2016-09-18 01:22:04 +0100

    2016-09-17's linux workout

---
+++ contributions/thuLink/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Quick `bash` shortcuts 
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
