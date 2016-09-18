2036f4e36b627b0f74039f4a7f22ca71baf976d9
Milmar <milmarwoot@gmail.com>
2016-09-18 16:01:33 +0100

    2016-09-18's linux workout

---
+++ contributions/Milmar/linux/File Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Easy way of sharing files
