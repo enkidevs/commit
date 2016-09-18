32b6417584c17b1419adc667aef9a7f08a9f56c1
morowinger <morowinger@gmail.com>
2016-09-18 11:46:14 +0100

    2016-09-18's linux workout

---
+++ contributions/morowinger/linux/System Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
