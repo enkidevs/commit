59e248db1020e01cf87cc7a0d4897e04b65e2820
aymeric-giraudet <neadj59@gmail.com>
2016-09-18 15:39:19 +0100

    2016-09-18's java workout

---
+++ contributions/aymeric-giraudet/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
