9b454d3d828036967e5fdc45c37647137a020ccc
dancincoder23 <dancingal212@gmail.com>
2016-09-18 09:19:08 +0100

    2016-09-17's linux workout

---
+++ contributions/dancincoder23/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Quick `bash` shortcuts 
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
