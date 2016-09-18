c0515e456c636fc43c6985e2e9ff13f046e6eeab
harsh_j4 <harshal.jagtap4@gmail.com>
2016-09-18 06:42:22 +0100

    2016-09-17's linux workout

---
+++ contributions/harsh_j4/linux/System Management/2016-09-17.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Cancel a system shutdown
