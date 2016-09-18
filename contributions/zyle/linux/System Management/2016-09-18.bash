38d6cd6cacd3e88a8974cf8f89c2cdb4c24655ac
zyle <inu99scar@gmail.com>
2016-09-18 23:47:59 +0100

    2016-09-18's linux workout

---
+++ contributions/zyle/linux/System Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Finding Open Files With `lsof`
