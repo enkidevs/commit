d1c597abb939acf35d349e9972a56b11b46ee454
Shiinzy8 <shiinzy8@gmail.com>
2016-09-18 22:44:08 +0100

    2016-09-18's java workout

---
+++ contributions/Shiinzy8/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Using bounded type parameters in generic methods
+Measuring time
+Using `synchronized` statements
