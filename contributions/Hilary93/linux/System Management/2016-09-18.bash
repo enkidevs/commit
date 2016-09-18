2e730e1d5fe43744b7b15663b8ee552a00934e26
Hilary93 <artur.pi.laskowski@gmail.com>
2016-09-18 23:06:11 +0100

    2016-09-18's linux workout

---
+++ contributions/Hilary93/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
+System uptime
