46efc89355a89771de1aa08852a471080304ff3f
Morion <morion.self@gmail.com>
2016-09-18 08:00:52 +0100

    2016-09-17's java workout

---
+++ contributions/Morion/java/Error Handling/2016-09-17.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Throwing an `exception`
