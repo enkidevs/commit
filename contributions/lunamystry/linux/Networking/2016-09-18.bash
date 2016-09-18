19103ca2c1ce0a346c88696579f962561296625a
lunamystry <lm.mbuli+enki@gmail.com>
2016-09-18 04:52:56 +0100

    2016-09-18's linux workout

---
+++ contributions/lunamystry/linux/Networking/2016-09-18.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Disable `IPv6`
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
+Staying connected remotely via SSH without an interactive shell
