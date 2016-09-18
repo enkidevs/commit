05fe37d8f940f47c6832c6deec677e5961549c76
brerx <th.mueller87@gmail.com>
2016-09-18 18:01:46 +0100

    2016-09-18's java workout

---
+++ contributions/brerx/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+Always specify type parameter
+Do not return *references* to private *mutable* class members
