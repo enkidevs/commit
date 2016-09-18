f512e48b68a40d9e55732d4b48af1376e7a2ad64
destan <destan@dorukdestan.com>
2016-09-18 12:26:22 +0100

    2016-09-18's linux workout

---
+++ contributions/destan/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
