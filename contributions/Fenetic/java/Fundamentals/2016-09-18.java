0f3a6c34f4fe0222761477693c429cb67adc486a
Fenetic <esyouit@gmail.com>
2016-09-18 20:58:32 +0100

    2016-09-18's java workout

---
+++ contributions/Fenetic/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Measuring time
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
