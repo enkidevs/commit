d6d953f4e1f8dd8b5b00398312766236056c209c
Frankenstein <lars.ch.franke@gmail.com>
2016-09-18 09:59:39 +0100

    2016-09-18's linux workout

---
+++ contributions/Frankenstein/linux/Networking/2016-09-18.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Get real network statistics with `slurm`
+List your IPv6 configuration
+Staying connected remotely via SSH without an interactive shell
+Short Introduction to `Wget` 
