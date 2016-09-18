31fac6dc8685158d8c8a87fb9e82cb84e3851bb0
tomaset22 <tomasengel7@gmail.com>
2016-09-18 15:13:01 +0100

    2016-09-18's java workout

---
+++ contributions/tomaset22/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
