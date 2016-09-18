f6c98887c282553b4099955211a85eb1ae8abcc9
bubbaJackson <david.relan@gmail.com>
2016-09-18 12:50:20 +0100

    2016-09-18's linux workout

---
+++ contributions/bubbaJackson/linux/Terminals & Shells/2016-09-18.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
