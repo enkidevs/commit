e348e3294ac6ea3c3a7129524e6aceabe6ee8b95
quylaa <quylaa@gmail.com>
2016-09-18 18:38:31 +0100

    2016-09-18's linux workout

---
+++ contributions/quylaa/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
