52edb4769fcbd548a54cde8139bf2f243b49766b
okashirin <lokishadow@mail.ru>
2016-09-18 23:58:10 +0100

    2016-09-18's linux workout

---
+++ contributions/okashirin/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Detect your linux distribution
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
