233f843e8c829266f9cf050f3cee08e1afa03eca
il-katta <ilkatta88@gmail.com>
2016-09-18 17:53:10 +0100

    2016-09-18's linux workout

---
+++ contributions/il-katta/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Detect your linux distribution
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
