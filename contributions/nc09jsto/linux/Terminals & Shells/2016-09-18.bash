85d0814debc8c3c527adaed9e444f4a5202687f3
nc09jsto <nc09jsto@gmail.com>
2016-09-18 21:49:02 +0100

    2016-09-18's linux workout

---
+++ contributions/nc09jsto/linux/Terminals & Shells/2016-09-18.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
