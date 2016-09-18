70e894ef2091267339469bf38d3a5acd8dd87cfd
quarckster <quarckster@gmail.com>
2016-09-18 16:05:08 +0100

    2016-09-18's linux workout

---
+++ contributions/quarckster/linux/Networking/2016-09-18.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Useful ssh client optimizations 
+Get real network statistics with `slurm`
+Setting up password-free authentication
+Getting the connection speed from the terminal
