c71927bb1073eee108c382c164e1cf366487de58
ShadowHv <shadow.exnw@gmail.com>
2016-09-18 18:52:33 +0100

    2016-09-18's javascript workout

---
+++ contributions/ShadowHv/javascript/Functions/2016-09-18.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Functions can be declared after use
+Easily generate a random `HEX` color
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
