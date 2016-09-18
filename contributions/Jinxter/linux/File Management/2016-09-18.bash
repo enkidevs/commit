01f5304dc2272abd860facce66dcdd4c81968b42
Jinxter <propwe@gmail.com>
2016-09-18 11:20:37 +0100

    2016-09-18's linux workout

---
+++ contributions/Jinxter/linux/File Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
