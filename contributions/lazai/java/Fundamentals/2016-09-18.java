9cf04664f3a07c092213cf8da665786411847f75
lazai <mariusdotspinu@gmail.com>
2016-09-18 07:37:34 +0100

    2016-09-18's java workout

---
+++ contributions/lazai/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+The trade-off between short and descriptive names
+Supply `toString()` in all classes
+Using an interface as a parameter
