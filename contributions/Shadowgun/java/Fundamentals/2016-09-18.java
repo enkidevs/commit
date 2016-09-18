44b1f8f078c4ad40d75795695cc0c55e5313dd7f
Shadowgun <shadowgun@i.ua>
2016-09-18 09:29:01 +0100

    2016-09-18's java workout

---
+++ contributions/Shadowgun/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
