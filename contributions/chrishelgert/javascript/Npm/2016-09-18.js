aa00f05480902c4d542189a8aca05e917ba0f5ef
chrishelgert <chris.helgert@yahoo.com>
2016-09-18 10:26:26 +0100

    2016-09-18's javascript workout

---
+++ contributions/chrishelgert/javascript/Npm/2016-09-18.js
@@ -0,0 +1,5 @@
+Manipulate npm packages cache
+npm can handle multiple versions of a dependency
+List binaries for scripting in npm
+Manage local node modules with `npm link`
+Reduce package duplication 
