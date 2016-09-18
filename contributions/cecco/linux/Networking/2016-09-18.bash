d01eff533ea11256b820d591d16eb394ee8c49e5
cecco <francescousseglio@gmail.com>
2016-09-18 22:43:50 +0100

    2016-09-18's linux workout

---
+++ contributions/cecco/linux/Networking/2016-09-18.bash
@@ -0,0 +1,5 @@
+Stress testing with `ab`
+Logging dropped firewall packets
+List your IPv6 configuration
+Sending emails from terminal / scripts
+Use `tcpdump` to listen to network interface traffic
