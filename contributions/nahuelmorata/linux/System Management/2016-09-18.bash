029c2c9e00a7c07adf35f45950d9e695e44a3f37
nahuelmorata <nahuelmorata@gmail.com>
2016-09-18 14:15:13 +0100

    2016-09-18's linux workout

---
+++ contributions/nahuelmorata/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
