cdaaee2d5c1093e74954b2113a71050a18c754c9
nikooiko <nikooiko@inf.uth.gr>
2016-09-18 13:43:38 +0100

    2016-09-18's linux workout

---
+++ contributions/nikooiko/linux/Terminals & Shells/2016-09-18.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Retrieve previous arguments
