3938813f44cbd135b59b4fb7f9f5da0b90390b71
berryzed <kckc963@naver.com>
2016-09-18 08:47:23 +0100

    2016-09-17's javascript workout

---
+++ contributions/berryzed/javascript/Core/2016-09-17.js
@@ -0,0 +1,5 @@
+Get a random item from an Array
+Performing a function at timed intervals
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+Scope and strict mode
