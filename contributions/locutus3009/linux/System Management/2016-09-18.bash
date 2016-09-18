b33004400536db692773664204b57681ab098b18
locutus3009 <nikolay.nerovniy@gmail.com>
2016-09-18 10:22:21 +0100

    2016-09-18's linux workout

---
+++ contributions/locutus3009/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
