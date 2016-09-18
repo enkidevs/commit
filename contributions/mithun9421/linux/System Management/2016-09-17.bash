72f2f5b31f36b5bac5bca4ce41b860922e5b5b4e
mithun9421 <mithunmuralee94@gmail.com>
2016-09-18 20:09:04 +0100

    2016-09-17's linux workout

---
+++ contributions/mithun9421/linux/System Management/2016-09-17.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
