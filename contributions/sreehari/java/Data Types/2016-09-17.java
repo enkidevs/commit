7fc3cea1bad63be72c73a9444ae676f03c269f54
sreehari <sreeh710@gmail.com>
2016-09-18 17:19:23 +0100

    2016-09-17's java workout

---
+++ contributions/sreehari/java/Data Types/2016-09-17.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Converting a string to upper or lower case
+Equals operation on different data types
+Numeric Conversion - Widening
