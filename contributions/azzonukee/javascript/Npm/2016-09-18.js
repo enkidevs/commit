c0420ce41037bed39bb7c73d7a1c173c9b79433a
azzonukee <anuar.suarez@hotmail.com>
2016-09-18 15:19:32 +0100

    2016-09-18's javascript workout

---
+++ contributions/azzonukee/javascript/Npm/2016-09-18.js
@@ -0,0 +1,5 @@
+Manipulate npm packages cache
+npm can handle multiple versions of a dependency
+Environmental variables and flags for `npm`
+`npm` verifies `sha1` hashes of packages
+Lock down dependency versions by shrinkwrapping
