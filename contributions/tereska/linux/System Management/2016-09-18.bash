24cafac0d62caa8cb2e0ff7ceff3941f536532dc
tereska <janecki@gmail.com>
2016-09-18 18:58:33 +0100

    2016-09-18's linux workout

---
+++ contributions/tereska/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
