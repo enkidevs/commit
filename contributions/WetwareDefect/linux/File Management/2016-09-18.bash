c2904bb0601772bad9f572cdba31bbd620a5959f
WetwareDefect <john.piatras@gmail.com>
2016-09-18 16:15:39 +0100

    2016-09-18's linux workout

---
+++ contributions/WetwareDefect/linux/File Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Delete commands aliases
