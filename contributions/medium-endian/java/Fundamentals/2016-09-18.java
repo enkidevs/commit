24d4e6710c01b435fe6dad6f3c419c786759a407
medium-endian <mediumendian@gmail.com>
2016-09-18 07:08:42 +0100

    2016-09-18's java workout

---
+++ contributions/medium-endian/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
