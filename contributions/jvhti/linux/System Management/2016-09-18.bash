a56d3e773feae60d0c202899d6db470d067b194e
jvhti <jvhti@hotmail.com>
2016-09-18 15:40:04 +0100

    2016-09-18's linux workout

---
+++ contributions/jvhti/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
