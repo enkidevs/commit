35502c044d272445041965d53605e3ce12a606cb
ttp2113 <ttp2113@columbia.edu>
2016-09-18 04:42:39 +0100

    2016-09-17's linux workout

---
+++ contributions/ttp2113/linux/Terminals & Shells/2016-09-17.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Execute a command without saving it in history
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
