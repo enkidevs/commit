550132c9913429fb12cc67dca729bd1f55627e77
Lexcrd1337 <lexcrd@yahoo.com>
2016-09-18 16:58:11 +0100

    2016-09-18's java workout

---
+++ contributions/Lexcrd1337/java/I/O/2016-09-18.java
@@ -0,0 +1,5 @@
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+List contents of a directory with `newDirectoryStream`
