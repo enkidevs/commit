b9acc660fa29013a7e53422156dbb42ec53c5262
MeesMaas <meesmaas999@gmail.com>
2016-09-18 17:18:05 +0100

    2016-09-18's linux workout

---
+++ contributions/MeesMaas/linux/Terminals & Shells/2016-09-18.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal incognito mode
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
