e0159cafb4d0cba7df36d1e03c3bad47b4e3db22
Sanyo <micmark6@gmail.com>
2016-09-18 11:31:15 +0100

    2016-09-18's java workout

---
+++ contributions/Sanyo/java/Patterns/2016-09-18.java
@@ -0,0 +1,5 @@
+Canceling an individual task using `java.util.Timer` class
+Singleton Pattern
+Private constructors
+Using initializer blocks to minimize code repetition in constructors
+Package naming conventions
