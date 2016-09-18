8b2a68c2de4b51e34dc34dcf7570c885bd4badad
Smeh <sgarcia030@gmail.com>
2016-09-18 13:43:50 +0100

    2016-09-18's linux workout

---
+++ contributions/Smeh/linux/System Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
