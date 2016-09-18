2e6065039bd5a6c34639e9e891aa464d4398886a
lokyi <roy.yi.yue@gmail.com>
2016-09-18 04:53:08 +0100

    2016-09-17's java workout

---
+++ contributions/lokyi/java/I/O/2016-09-17.java
@@ -0,0 +1,5 @@
+Create system file paths using the `Path` class
+Reading and writing text files
+Checking for the existence of files and directories
+List contents of a directory with `newDirectoryStream`
+The `Console` class
