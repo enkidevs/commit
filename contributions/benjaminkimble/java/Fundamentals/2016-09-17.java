9df39372cc23b5cf09826a18d775094332fcfa8c
benjaminkimble <benjaminroykimble@gmail.com>
2016-09-18 03:13:57 +0100

    2016-09-17's java workout

---
+++ contributions/benjaminkimble/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
