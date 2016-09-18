77a5f84a37fdd7ece1dbfbf0648218405f5b2b2f
marekgrygorczuk <marek@grygorczuk.pl>
2016-09-18 18:49:56 +0100

    2016-09-18's linux workout

---
+++ contributions/marekgrygorczuk/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Basic service management with `systemd`
