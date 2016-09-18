ca741f95a16a82c143a92be867cb616a3d9febcf
TheCoreMan <dude500@gmail.com>
2016-09-18 06:11:18 +0100

    2016-09-18's linux workout

---
+++ contributions/TheCoreMan/linux/File Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
