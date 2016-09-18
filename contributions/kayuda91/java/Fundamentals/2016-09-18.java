e9da114a0afbf97661d53bf7ec94ffd41c3869ab
kayuda91 <kayuda91@gmail.com>
2016-09-18 21:34:53 +0100

    2016-09-18's java workout

---
+++ contributions/kayuda91/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
