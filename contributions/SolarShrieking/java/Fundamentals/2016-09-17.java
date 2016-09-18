92fbe11ff9150df392bb9e4949e0ef66ba524b69
SolarShrieking <michael.anible@gmail.com>
2016-09-18 01:42:08 +0100

    2016-09-17's java workout

---
+++ contributions/SolarShrieking/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use meaningful names
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Using `synchronized` statements
