39e29cec99d88390ea5cc6cc5fcb85acb221c0ae
95lazyass19 <kulshreshtha.naman2@gmail.com>
2016-09-18 22:13:45 +0100

    2016-09-17's linux workout

---
+++ contributions/95lazyass19/linux/System Management/2016-09-17.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
