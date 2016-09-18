75b243a80fb3919bef54d8ec38a57afbbfb22f4f
racchio <racchio@gmail.com>
2016-09-18 22:41:02 +0100

    2016-09-18's javascript workout

---
+++ contributions/racchio/javascript/Core/2016-09-18.js
@@ -0,0 +1,5 @@
+Data type comparison in `switch` statements
+Filtering items out of an array
+Getting the *real* dimensions of an image
+`console.*` in JavaScript
+Inaccuracy of binary floating-point format
