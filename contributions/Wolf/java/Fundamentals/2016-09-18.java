1d171af8ede8d2a9c5e5a5ca156366ff9c2841e3
Wolf <ya.172009@yandex.ru>
2016-09-18 21:29:32 +0100

    2016-09-18's java workout

---
+++ contributions/Wolf/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Calling one constructor from another
+Using `synchronized` statements
