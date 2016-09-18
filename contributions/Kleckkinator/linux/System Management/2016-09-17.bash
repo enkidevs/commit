2519c0d52e98339e3144426819395d8ad5f72d4c
Kleckkinator <makamakamaka5@gmail.com>
2016-09-18 02:56:59 +0100

    2016-09-17's linux workout

---
+++ contributions/Kleckkinator/linux/System Management/2016-09-17.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
