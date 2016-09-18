89aca380b854159290fe2053a91dcc19e9ea70ae
thiwanon1525 <gmgame.1525@gmail.com>
2016-09-18 13:47:25 +0100

    2016-09-17's java workout

---
+++ contributions/thiwanon1525/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
