57a0a77a943705f8249284047cbc9a4ccc728255
HuShifang <stephen.p.ford@gmail.com>
2016-09-18 07:07:06 +0100

    2016-09-17's linux workout

---
+++ contributions/HuShifang/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
