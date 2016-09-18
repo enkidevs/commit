bc16ed51237e7df146d60a2b029abcebfe00614f
cordac <zal3wski.piotr@gmail.com>
2016-09-18 10:41:18 +0100

    2016-09-18's javascript workout

---
+++ contributions/cordac/javascript/Express/2016-09-18.js
@@ -0,0 +1,5 @@
+`response` object additional properties in **Express**
+Easy way to deliver **HTML** pages with **Express**
+**GZIP** compression for **Express**
+`body-parser` in **Express**
+Log with `morgan` in **Express**
