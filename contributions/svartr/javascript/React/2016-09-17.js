1432c4e320bb233fb2ee7a225b3f88da6c9d39c8
svartr <the.trash.collector@yandex.com>
2016-09-18 14:07:51 +0100

    2016-09-17's javascript workout

---
+++ contributions/svartr/javascript/React/2016-09-17.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Functional Stateless Components in React
+Props in getInitialState Is an        
+Anti-Pattern
+Dangerously set `innerHTML`
+`ref` callback attribute
