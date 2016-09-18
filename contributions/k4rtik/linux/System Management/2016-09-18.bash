9fcba793d9cfe44e25cf7e963feeac8589e52a2d
k4rtik <kartiksinghal@gmail.com>
2016-09-18 19:06:45 +0100

    2016-09-18's linux workout

---
+++ contributions/k4rtik/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Common log files
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Finding Open Files With `lsof`
