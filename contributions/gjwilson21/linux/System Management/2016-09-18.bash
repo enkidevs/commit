c128665fddf8d2627b8a5b0bfc1bef2741e70312
gjwilson21 <gjwilson21@gmail.com>
2016-09-18 23:25:39 +0100

    2016-09-18's linux workout

---
+++ contributions/gjwilson21/linux/System Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Common log files
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
