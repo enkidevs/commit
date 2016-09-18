fa3cfa830e8804d52606ddd91be6fadf167ec52b
Descoladan <injpk23@gmail.com>
2016-09-18 05:16:23 +0100

    2016-09-17's java workout

---
+++ contributions/Descoladan/java/Java 8/2016-09-17.java
@@ -0,0 +1,5 @@
+Java 8 Lambda expressions
+Using `Arrays.parallelSort` 
+Control OS `Process`es in `Java8`
+Avoid numerical overflows with *Java8* `exact` methods.
+`JJS` - Oracle Nashorn, newest JavaScript Engine
