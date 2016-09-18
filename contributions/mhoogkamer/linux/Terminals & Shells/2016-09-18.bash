6effd35e46535c18a6383d5dad610d771e5a423b
mhoogkamer <michael.hoogkamer@gmail.com>
2016-09-18 12:59:09 +0100

    2016-09-18's linux workout

---
+++ contributions/mhoogkamer/linux/Terminals & Shells/2016-09-18.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
