7c4734cfeb2ea55a93d031737544110de10ca6b4
arnaudoisel <arnaud.oisel@gmail.com>
2016-09-18 14:49:42 +0100

    2016-09-18's javascript workout

---
+++ contributions/arnaudoisel/javascript/Core/2016-09-18.js
@@ -0,0 +1,5 @@
+Types of numbers
+Most efficient way to build `HTML` strings
+Setting the length of an array 
+Hoisting applies only to variable declarations, not initializations
+`catch` is block scoped
