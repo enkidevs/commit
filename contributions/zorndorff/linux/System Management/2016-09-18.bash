b3b4adce2a8ac54fbf043ed7548007ba5cec0bb4
zorndorff <zorndorff@gmail.com>
2016-09-18 20:51:49 +0100

    2016-09-18's linux workout

---
+++ contributions/zorndorff/linux/System Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Finding Open Files With `lsof`
