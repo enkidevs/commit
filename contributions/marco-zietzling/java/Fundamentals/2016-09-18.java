9c4afd22884994ba1d3fc05f862243fcb270c32c
marco-zietzling <marco.zietzling@gmail.com>
2016-09-18 11:29:58 +0100

    2016-09-18's java workout

---
+++ contributions/marco-zietzling/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Supply `toString()` in all classes
+Using an interface as a parameter
