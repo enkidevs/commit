3ab70ee70cd46025292d39502093d71d4a48e2ce
indoriMonk <amanshulvyas@gmail.com>
2016-09-18 05:53:34 +0100

    2016-09-17's python workout

---
+++ contributions/indoriMonk/python/Data Structures/2016-09-17.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`Dictionary` view objects
+Best way to implement a simple `queue` 
+Special attributes of objects and classes
+`queue`s and threads
