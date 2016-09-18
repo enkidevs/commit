3fa19e7ecea3a53f47f908fd983d91a92142d032
stullageek <akalx44@gmail.com>
2016-09-18 16:51:00 +0100

    2016-09-18's java workout

---
+++ contributions/stullageek/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
