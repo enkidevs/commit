a8d270c05283503b3bf508660d90d015860feb64
Darklight3it <darklight3it@gmail.com>
2016-09-18 08:42:41 +0100

    2016-09-18's linux workout

---
+++ contributions/Darklight3it/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Find out if the system's architecture is 32 or 64 bit
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
