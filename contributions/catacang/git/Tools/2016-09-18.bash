5f1fd23eaa066dcbd70ac63dd89da45accc29c38
catacang <jocaquintas@yahoo.com>
2016-09-18 10:30:23 +0100

    2016-09-18's git workout

---
+++ contributions/catacang/git/Tools/2016-09-18.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Specify a range of commits using double dot syntax
+Create a new branch from a stash
+Sign your work (using GPG)
+Debug using binary search
