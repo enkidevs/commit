5ecbe967c78b9d4f22b65c9a445950bdd0f97c0f
peterleung89 <peterleung89@gmail.com>
2016-09-18 21:04:20 +0100

    2016-09-18's java workout

---
+++ contributions/peterleung89/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Using `synchronized` statements
+Avoid using `static` variables
