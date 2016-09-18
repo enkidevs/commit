b8f8f0a65b633f1f80233632b8d490b54730abb5
Cefalopoide <luca.filipponi89@gmail.com>
2016-09-18 15:26:06 +0100

    2016-09-18's java workout

---
+++ contributions/Cefalopoide/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Avoid using `static` variables
