9f4c29aa8f38b2e04cb763d790da6ff9d0379ffc
CarlosTuring-94 <charlierp94@gmail.com>
2016-09-18 09:02:44 +0100

    2016-09-18's linux workout

---
+++ contributions/CarlosTuring-94/linux/Terminals & Shells/2016-09-18.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Useful aliasing in bash
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
