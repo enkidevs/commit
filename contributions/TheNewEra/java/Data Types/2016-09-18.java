729430e1790e8e4287dafc56fefc397c5d68d410
TheNewEra <bastian.gee@gmail.com>
2016-09-18 20:52:25 +0100

    2016-09-18's java workout

---
+++ contributions/TheNewEra/java/Data Types/2016-09-18.java
@@ -0,0 +1,5 @@
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Finding a substring in a string
+Retrieve the component type of an array
+Numeric Conversion - Widening
