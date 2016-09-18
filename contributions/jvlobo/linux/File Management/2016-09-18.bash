a08ac46ce0881b0bf7b1f9090fe03d3a30bd4add
jvlobo <jvlg2002@hotmail.com>
2016-09-18 11:50:00 +0100

    2016-09-18's linux workout

---
+++ contributions/jvlobo/linux/File Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Converting between Unix and Windows text files
+Using `touch` to alter files modification time
+Working with `.gz` files
+Find the unknown process preventing deleting of files
