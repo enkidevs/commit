71e4983d09135ba14b20fce86f5d3fff6ffe65b7
toklaui <toklaui@live.de>
2016-09-18 19:07:17 +0100

    2016-09-18's javascript workout

---
+++ contributions/toklaui/javascript/Functions/2016-09-18.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Function call method
+Move cursor at the end of text input
