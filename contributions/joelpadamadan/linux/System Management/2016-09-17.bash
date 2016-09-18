02ac6ffb57a48f5aba329447e1aaa7e2fe6a536e
joelpadamadan <joelpadamadan@gmail.com>
2016-09-18 07:46:21 +0100

    2016-09-17's linux workout

---
+++ contributions/joelpadamadan/linux/System Management/2016-09-17.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
