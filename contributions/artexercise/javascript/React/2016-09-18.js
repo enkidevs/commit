986576e2e25305f09ce4758a5f4ba902c9bf4aa5
artexercise <artexercise@yahoo.com>
2016-09-18 21:00:58 +0100

    2016-09-18's javascript workout

---
+++ contributions/artexercise/javascript/React/2016-09-18.js
@@ -0,0 +1,5 @@
+Functional Stateless Components in React
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+Using `false` in **JSX**
+**React** `this.props.children`
