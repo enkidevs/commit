75e09bb14f796ea564279724eeadaa4c7df9aa0c
ancom22 <ancom22@protonmail.com>
2016-09-18 21:27:29 +0100

    2016-09-18's linux workout

---
+++ contributions/ancom22/linux/System Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
