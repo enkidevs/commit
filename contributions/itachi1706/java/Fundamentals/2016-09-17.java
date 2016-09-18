d7de7e506ee626b646c79849a93af5b6de1a791b
itachi1706 <kennethsohyq@hotmail.com>
2016-09-18 09:58:53 +0100

    2016-09-17's java workout

---
+++ contributions/itachi1706/java/Fundamentals/2016-09-17.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+Always specify type parameter
+Increase `PermGen` space as to avoid `OutOfMemory` errors
