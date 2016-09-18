bf0800b8671ac138ece38cabf8eef20fc718c04b
mshakeel6 <mshakeelibrahim@gmail.com>
2016-09-18 09:41:47 +0100

    2016-09-17's linux workout

---
+++ contributions/mshakeel6/linux/System Management/2016-09-17.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
+Cancel a system shutdown
