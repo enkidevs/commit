c6fe76cc9e72bd8190a970fedd83e0beb9826be7
jnighlight <jlites@stetson.edu>
2016-09-19 13:51:31 +0100

    2016-09-19's linux workout

---
+++ contributions/jnighlight/linux/Package Management/2016-09-19.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Backporting on Debian
+
+List installed packages
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
