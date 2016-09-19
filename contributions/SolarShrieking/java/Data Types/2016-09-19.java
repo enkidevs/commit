36a3a8bf4c3c0d4c6e4b36d769368a5128cf376e
SolarShrieking <michael.anible@gmail.com>
2016-09-19 19:25:21 +0100

    2016-09-19's java workout

---
+++ contributions/SolarShrieking/java/Data Types/2016-09-19.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Double brace initialization
+`StringBuffer` vs `StringBuilder`
+String concatenation in Java
