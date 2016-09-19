24d7176a33e1b7054def3bb8ed659bf181a939a5
Marius <marius.mez@gmail.com>
2016-09-19 21:37:38 +0100

    2016-09-19's git workout

---
+++ contributions/Marius/git/Tools/2016-09-19.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Specify a range of commits using double dot syntax
+Interactively stage patches
+Useful stashing options
+Create a new branch from a stash
