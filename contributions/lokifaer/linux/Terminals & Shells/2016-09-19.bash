56675f9c313e49052b05e70ed602082a30a28b46
lokifaer <lokifaer@gmail.com>
2016-09-19 09:56:44 +0100

    2016-09-19's linux workout

---
+++ contributions/lokifaer/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Random password generator
+Adding directories to your `$PATH`
