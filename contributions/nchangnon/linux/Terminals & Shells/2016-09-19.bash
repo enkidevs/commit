d65d75e5c8976302c2372a3b523417b4f9445f47
nchangnon <nick.changnon@gmail.com>
2016-09-19 23:29:19 +0100

    2016-09-19's linux workout

---
+++ contributions/nchangnon/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Aliasing ssh connections
+Get to know  your commands with `type`
