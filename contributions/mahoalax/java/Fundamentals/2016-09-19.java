5ddd200014ab5f8b81172c779577dbe61b236c35
mahoalax <marie.laxenaire@webedia-group.com>
2016-09-19 22:54:58 +0100

    2016-09-19's java workout

---
+++ contributions/mahoalax/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Implementing an `interface`
