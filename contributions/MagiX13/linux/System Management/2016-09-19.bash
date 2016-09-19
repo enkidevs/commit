024102c18e04d8da3e250eb969467d68c4c0aa38
MagiX13 <github@scheins.info>
2016-09-19 09:53:52 +0100

    2016-09-19's linux workout

---
+++ contributions/MagiX13/linux/System Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
