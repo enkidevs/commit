8effcd6724d9e29ab49aa5cc6cd15c88923241de
ALKashout <alkashout@gmail.com>
2016-09-19 18:40:10 +0100

    2016-09-19's java workout

---
+++ contributions/ALKashout/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
