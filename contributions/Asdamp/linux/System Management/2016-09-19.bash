c0c70ccda8f44b206f77681f1862f12b5ce0165e
Asdamp <antonio.danilo92@gmail.com>
2016-09-19 23:23:53 +0100

    2016-09-19's linux workout

---
+++ contributions/Asdamp/linux/System Management/2016-09-19.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Cancel a system shutdown
