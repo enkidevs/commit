09e4c4984a59594f197782731ec261fda6658d15
SkyBrineHD <felixlkunz@gmail.com>
2016-09-19 21:38:54 +0100

    2016-09-19's java workout

---
+++ contributions/SkyBrineHD/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
+Calling one constructor from another
