9a6f78f7eef25bb13963e121a83c56223339c874
zainzin <zainelerris@yahoo.com>
2016-09-19 11:37:33 +0100

    2016-09-19's java workout

---
+++ contributions/zainzin/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
