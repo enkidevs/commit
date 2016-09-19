616d3189cfe2d03393649b7393918cda7695ae18
romaincausse <romain.causse@gmail.com>
2016-09-19 21:10:31 +0100

    2016-09-19's javascript workout

---
+++ contributions/romaincausse/javascript/React/2016-09-19.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Pixel values in style props for **React**
+Props in getInitialState Is an        
+Anti-Pattern
+**React** `this.props.children`
+Dangerously set `innerHTML`
