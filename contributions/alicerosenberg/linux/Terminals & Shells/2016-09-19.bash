3fda8789557392d3779876075b5112a45b7a5f04
alicerosenberg <aj@aliceinwonder.land>
2016-09-19 17:49:23 +0100

    2016-09-19's linux workout

---
+++ contributions/alicerosenberg/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
