0e3bb67934345367ba4b2a0f022445252675c477
oldteacher <drsantos@gmail.com>
2016-09-19 10:07:13 +0100

    2016-09-18's linux workout

---
+++ contributions/oldteacher/linux/Terminals & Shells/2016-09-18.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Find any Unix / Linux command
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
