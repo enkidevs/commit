fa7268d1f9fe687d9b82d00248d7248ba40da4c4
nokados <nokados@yandex.ru>
2016-09-19 21:20:11 +0100

    2016-09-19's linux workout

---
+++ contributions/nokados/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Clear the terminal instantly
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
