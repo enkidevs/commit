dc9fd897f73855dddf2dcd83b9764aceb7178456
pjheden <julianheden@gmail.com>
2016-09-19 09:09:24 +0100

    2016-09-19's linux workout

---
+++ contributions/pjheden/linux/System Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Using `dd` To Create Disk Images
