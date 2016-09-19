fe0fced935a1dc01137de0939852885a60d7051c
Scrumplex <sefa6727@gmail.com>
2016-09-19 14:24:04 +0100

    2016-09-19's java workout

---
+++ contributions/Scrumplex/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
