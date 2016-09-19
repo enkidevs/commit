18098b0d941f38c361dc74aba7d2e2856f2d0354
oldteacher <drsantos@gmail.com>
2016-09-19 10:15:42 +0100

    2016-09-19's linux workout

---
+++ contributions/oldteacher/linux/Data Manipulation/2016-09-19.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+How to analyse a server's log files
+Getting rid of duplicate lines
+Count non-empty lines with `nl` utility
+Wrap text with `fold`
