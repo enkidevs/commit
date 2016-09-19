fb1c64040eaf263a9b4e466611c9e60ccee67f96
coalaroot <jakubkrolak94@gmail.com>
2016-09-19 10:02:05 +0100

    2016-09-19's java workout

---
+++ contributions/coalaroot/java/I/O/2016-09-19.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to create directories in Java
+Reading and writing text files
+List contents of a directory with `newDirectoryStream`
+Deleting files and directories
