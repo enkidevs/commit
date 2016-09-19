a05821acadabfcd65378fe45b311586cba75444d
sebgc <sebastiangcooper@gmail.com>
2016-09-19 17:31:13 +0100

    2016-09-19's linux workout

---
+++ contributions/sebgc/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Random password generator
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
