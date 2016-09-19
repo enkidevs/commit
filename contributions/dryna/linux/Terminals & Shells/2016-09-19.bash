cefaddbe222d4cc42757a01959055c74c7dd90cf
dryna <kubadrynkowski@gmail.com>
2016-09-19 21:36:58 +0100

    2016-09-19's linux workout

---
+++ contributions/dryna/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Clear the terminal instantly
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
