7d04012b1fb0e3b8897160639cbaa3c138d10f24
killua8q8 <killua8q8@gmail.com>
2016-09-19 06:26:44 +0100

    2016-09-18's java workout

---
+++ contributions/killua8q8/java/I/O/2016-09-18.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
