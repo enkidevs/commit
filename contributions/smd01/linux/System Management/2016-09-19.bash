0c7b2003837688cb5f6f6bbd54af518f39337a63
smd01 <saldazzo@gmail.com>
2016-09-19 15:59:58 +0100

    2016-09-19's linux workout

---
+++ contributions/smd01/linux/System Management/2016-09-19.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
