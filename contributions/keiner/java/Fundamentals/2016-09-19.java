feeb4e09dc89651befce13e53bca01e2aeef3c05
keiner <keiner.v.ihnen@gmail.com>
2016-09-19 07:27:42 +0100

    2016-09-19's java workout

---
+++ contributions/keiner/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Using an interface as a parameter
