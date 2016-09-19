c90188638c05e38865c0bb932ef4c7f39d601e04
Razvan33 <rzvncc33@gmail.com>
2016-09-19 05:39:32 +0100

    2016-09-17's javascript workout

---
+++ contributions/Razvan33/javascript/Npm/2016-09-17.js
@@ -0,0 +1,5 @@
+npm can handle multiple versions of a dependency
+Combining script commands in npm
+devDependencies in `npm`
+Tab completion in `npm`
+Lock down dependency versions by shrinkwrapping
