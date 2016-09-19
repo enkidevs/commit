a8db2a048bba2dc5bfebb7615bd1c57301db4642
JulienBon <julien.bonvillain@free.fr>
2016-09-19 11:58:11 +0100

    2016-09-19's linux workout

---
+++ contributions/JulienBon/linux/System Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
