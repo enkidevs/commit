11fcee082a0b7e53f7868a139b20a54308e24a04
dukman <dukman.box@gmail.com>
2016-09-19 08:20:15 +0100

    2016-09-19's java workout

---
+++ contributions/dukman/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Constant notation
+Using an interface as a parameter
