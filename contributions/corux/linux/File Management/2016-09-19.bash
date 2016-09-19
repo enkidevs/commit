d418b16a0655daf4ae1fa9984e5505aafbd82b50
corux <tobias@wallura.org>
2016-09-19 06:52:19 +0100

    2016-09-19's linux workout

---
+++ contributions/corux/linux/File Management/2016-09-19.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
