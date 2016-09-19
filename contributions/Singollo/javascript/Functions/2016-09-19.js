016ab1f2a2bb66d2fd745f65bc57fb3924573ae4
Singollo <jedrzej.danko@gmail.com>
2016-09-19 16:37:56 +0100

    2016-09-19's javascript workout

---
+++ contributions/Singollo/javascript/Functions/2016-09-19.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Anonymous functions
+Get query/url variables
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
