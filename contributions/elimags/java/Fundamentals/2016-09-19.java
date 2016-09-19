6be4776531a78fb0b6c7ae8e16e92208511d8b9e
elimags <esm6117@rit.edu>
2016-09-19 19:05:14 +0100

    2016-09-19's java workout

---
+++ contributions/elimags/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Implementing an `interface`
+Using an interface as a parameter
