861e21d29f609cfbfa7b067f89eff4f6315bfe7c
stevedien <lulzsecd@gmail.com>
2016-09-19 21:05:05 +0100

    2016-09-19's linux workout

---
+++ contributions/stevedien/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Find any Unix / Linux command
+Random password generator
+Conditional command execution
+
+(`&&` operator)
