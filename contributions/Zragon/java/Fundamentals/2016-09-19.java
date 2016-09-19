198a917623311790df3276e2dc1872b8c2ab098c
Zragon <szymon.chaber@o2.pl>
2016-09-19 09:17:06 +0100

    2016-09-19's java workout

---
+++ contributions/Zragon/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Metadata: creating a user-defined file attribute
+Measuring time
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
