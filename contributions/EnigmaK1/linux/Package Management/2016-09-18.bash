a524bd066133aa8b8d4ec570836bcbd0bdf3604e
EnigmaK1 <singhkanishka1@gmail.com>
2016-09-19 02:07:42 +0100

    2016-09-18's linux workout

---
+++ contributions/EnigmaK1/linux/Package Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Install specific version of packages
+List installed packages
+Installing a `.deb` package from the terminal
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
