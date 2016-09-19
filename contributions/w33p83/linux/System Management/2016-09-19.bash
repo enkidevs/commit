3f9f768f076a107bf2fcd18f26df848088445b03
w33p83 <pithoud@intechinfo.fr>
2016-09-19 13:10:46 +0100

    2016-09-19's linux workout

---
+++ contributions/w33p83/linux/System Management/2016-09-19.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
