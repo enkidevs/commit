1743d74b6e1babb16fb89d43b1bfa03b4e8189ed
bboulahdid <boulahdid.boubker@gmail.com>
2016-09-19 11:42:02 +0100

    2016-09-19's java workout

---
+++ contributions/bboulahdid/java/Data Types/2016-09-19.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Converting a string to upper or lower case
+Double brace initialization
