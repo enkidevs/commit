432fddbaff0b1ec7fdcbfe02da6fcf3a37781ffb
diego123 <bartosz.szymanski1@gmail.com>
2016-09-19 07:25:29 +0100

    2016-09-19's java workout

---
+++ contributions/diego123/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between JRE and JDK?
+Common mistake on switch statements
+Do not perform bitwise and arithmetic operations on the same data
+How range operations work
