79dad054b093ba64e973c0c30c43f37acb870188
sebnilsson <seb.nilsson@gmail.com>
2016-09-19 06:10:05 +0100

    2016-09-19's javascript workout

---
+++ contributions/sebnilsson/javascript/Core/2016-09-19.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Using the double tilde `~~` 
+Setting the length of an array 
+Labeling your loops
+Hoisting applies only to variable declarations, not initializations
