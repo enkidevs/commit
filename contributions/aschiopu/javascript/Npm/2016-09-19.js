09ad7ebddc8c722a5dd82886582e2e7246ac271b
aschiopu <alexxschiopu@gmail.com>
2016-09-19 15:23:23 +0100

    2016-09-19's javascript workout

---
+++ contributions/aschiopu/javascript/Npm/2016-09-19.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+npm can handle multiple versions of a dependency
+Manage local node modules with `npm link`
+Package distribution tags
+Lock down dependency versions by shrinkwrapping
