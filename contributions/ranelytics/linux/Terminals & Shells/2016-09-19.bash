ef9ebe1a8b780dc1ee8217e1d378443038606c88
ranelytics <ranelytics@gmail.com>
2016-09-19 06:29:30 +0100

    2016-09-19's linux workout

---
+++ contributions/ranelytics/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
