eef7146105e829d92eb8da0145fa38e45bc44202
evanliomain <evan.liomain@gmail.com>
2016-09-19 08:27:45 +0100

    2016-09-19's javascript workout

---
+++ contributions/evanliomain/javascript/React/2016-09-19.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+componentWillReceiveProps Not Triggered After Mounting
+
+Load Initial Data via AJAX
+Server-side rendering 
+Shortcut for transferring props
