ab59770d1a031f14c761dac87576c4d5c1a216a2
yjeon <yoon3971@gmail.com>
2016-09-19 16:16:12 +0100

    2016-09-19's linux workout

---
+++ contributions/yjeon/linux/System Management/2016-09-19.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
