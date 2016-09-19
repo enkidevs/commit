b2bcd3e03e2615055f376417b50404354c568e1a
washingt0 <washingtonstk@gmail.com>
2016-09-19 16:58:14 +0100

    2016-09-19's linux workout

---
+++ contributions/washingt0/linux/System Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
