0b9b490e065f720ea37fb227628d83dace3f8a0d
Smeh <sgarcia030@gmail.com>
2016-09-19 14:42:57 +0100

    2016-09-19's linux workout

---
+++ contributions/Smeh/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Terminal based browser
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
