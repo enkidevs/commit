119a23aafac1df9c14140165183a4a3991d668c7
Andee <andytruong94@gmail.com>
2016-09-19 11:51:59 +0100

    2016-09-19's java workout

---
+++ contributions/Andee/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Always specify type parameter
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
