1966d3d4bc359dc596167e1e25d499476bc46a02
jameshlewis <jamesjagalin@virginmedia.com>
2016-09-19 22:16:14 +0100

    2016-09-19's linux workout

---
+++ contributions/jameshlewis/linux/System Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+List current logged on users with `w`
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
