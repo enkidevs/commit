bb02d4e20c20505855edbb093f126017074c2aa3
games647 <games@tsprivat.de>
2016-09-19 07:00:36 +0100

    2016-09-19's linux workout

---
+++ contributions/games647/linux/System Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Find out if the system's architecture is 32 or 64 bit
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
