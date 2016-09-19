228003d748a694cd6131df7f1e263d9bd3393bc7
vifon <findhill@gmail.com>
2016-09-19 13:39:57 +0100

    2016-09-18's java workout

---
+++ contributions/vifon/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The trade-off between short and descriptive names
+Metadata: creating a user-defined file attribute
+How range operations work
+Avoid using `static` variables
