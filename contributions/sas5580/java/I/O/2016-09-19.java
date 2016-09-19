03f129a13a390f1810f13687f16b8a4dfbba66b6
sas5580 <gupta.saswata@gmail.com>
2016-09-19 13:32:24 +0100

    2016-09-19's java workout

---
+++ contributions/sas5580/java/I/O/2016-09-19.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+Create system file paths using the `Path` class
+Using buffered streams
+Checking for the existence of files and directories
+Listing a file system's root directories
