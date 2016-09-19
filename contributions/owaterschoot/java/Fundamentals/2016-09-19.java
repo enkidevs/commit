5f5851ac6389eec77108ed573920622bdf997596
owaterschoot <owaterschoot@gmail.com>
2016-09-19 16:22:43 +0100

    2016-09-19's java workout

---
+++ contributions/owaterschoot/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
