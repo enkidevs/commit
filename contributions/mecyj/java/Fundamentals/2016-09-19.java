8c9e3cb1fefb3e27a8fad68b4fb306ace5371445
mecyj <dominik.sze@gmail.com>
2016-09-19 14:10:51 +0100

    2016-09-19's java workout

---
+++ contributions/mecyj/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Always specify type parameter
+Avoid using `static` variables
