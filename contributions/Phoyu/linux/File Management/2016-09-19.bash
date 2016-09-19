c743e0ea5edaee0f6ddd4bd35f5b67d074a8bbab
Phoyu <mathias.phoyu@gmail.com>
2016-09-19 13:43:24 +0100

    2016-09-19's linux workout

---
+++ contributions/Phoyu/linux/File Management/2016-09-19.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
