1177281c111f859fb063a979a18bf769c71d5d32
moyvera <moyvera@geekalt42.net>
2016-09-19 16:09:29 +0100

    2016-09-19's linux workout

---
+++ contributions/moyvera/linux/Networking/2016-09-19.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Logging dropped firewall packets
+Check `iptables` firewall status
+How to clear `iptables` rules
+Use `tcpdump` to listen to network interface traffic
