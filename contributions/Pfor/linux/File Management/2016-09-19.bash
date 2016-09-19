d21fcfd9bf7682544715abf538f2324d3e1591ff
Pfor <iah@protonmail.com>
2016-09-19 21:22:17 +0100

    2016-09-19's linux workout

---
+++ contributions/Pfor/linux/File Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Practical `du` command
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
