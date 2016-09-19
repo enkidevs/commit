eaa4c8213560efba95018c6626eaf02444c35447
pini <pinhasph@gmail.com>
2016-09-19 07:53:18 +0100

    2016-09-19's javascript workout

---
+++ contributions/pini/javascript/Functions/2016-09-19.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+`.bind()` 
+Easily generate a random `HEX` color
+Social sharing without widgets
