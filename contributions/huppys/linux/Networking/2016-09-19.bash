aff7e6f94d3f988aceb15b1ea6c55250be15ef6d
huppys <huppys55@gmail.com>
2016-09-19 22:14:28 +0100

    2016-09-19's linux workout

---
+++ contributions/huppys/linux/Networking/2016-09-19.bash
@@ -0,0 +1,5 @@
+Debugging `ssh` client issues
+Sending emails from terminal / scripts
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Short Introduction to `Wget` 
