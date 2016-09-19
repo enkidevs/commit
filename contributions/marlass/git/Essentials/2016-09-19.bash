7d0e46a343ff07dfbe87731d7adcd7f4764e52a5
marlass <lasakmarcin90@gmail.com>
2016-09-19 18:49:30 +0100

    2016-09-19's git workout

---
+++ contributions/marlass/git/Essentials/2016-09-19.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+How to set your username and email
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
+Ignore files in git
