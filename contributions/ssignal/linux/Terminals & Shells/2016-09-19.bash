25b561573db559c56af513e3d7ec2702f56d81f6
ssignal <kiho.choi@lge.com>
2016-09-20 00:09:10 +0100

    2016-09-19's linux workout

---
+++ contributions/ssignal/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,4 @@
+Repeating commands with `watch`
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
