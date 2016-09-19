39a7c98905bf35eba26cb42f3656f6346d8cc482
Miterion <heiko.carrasco@yahoo.com>
2016-09-19 10:58:41 +0100

    2016-09-19's linux workout

---
+++ contributions/Miterion/linux/File Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
