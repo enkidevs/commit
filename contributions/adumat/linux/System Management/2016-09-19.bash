baafcabc212fc5581649f69396b79be09884daad
adumat <matteo.a91@gmail.com>
2016-09-19 12:25:54 +0100

    2016-09-19's linux workout

---
+++ contributions/adumat/linux/System Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
