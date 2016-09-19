1bc39050ab1ee9acf12a8166064d745947fa3d4d
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2016-09-20 00:09:45 +0100

    2016-09-19's java workout

---
+++ contributions/ehdrjsdlzzzz/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Supply `toString()` in all classes
