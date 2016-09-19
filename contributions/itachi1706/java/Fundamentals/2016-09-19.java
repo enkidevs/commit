8832b43e1e29d3fac5d8aa203e9ecb9689b50837
itachi1706 <kennethsohyq@hotmail.com>
2016-09-19 22:26:23 +0100

    2016-09-19's java workout

---
+++ contributions/itachi1706/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
