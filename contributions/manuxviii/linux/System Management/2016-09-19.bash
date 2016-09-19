b2a32434b54fea12e88fe78a4277cde058394e9b
manuxviii <emmanuel.mesnige@protonmail.com>
2016-09-19 21:05:01 +0100

    2016-09-19's linux workout

---
+++ contributions/manuxviii/linux/System Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
