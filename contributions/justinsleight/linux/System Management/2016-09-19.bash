1d4fc36abf958caeef79ddc9cb14b45f2b1010b0
justinsleight <sleightjustin@gmail.com>
2016-09-19 21:39:11 +0100

    2016-09-19's linux workout

---
+++ contributions/justinsleight/linux/System Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
+System uptime
