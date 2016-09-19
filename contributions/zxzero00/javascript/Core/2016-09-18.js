0bda43ca28e6b61fe028e011444cdeb25a1d2b04
zxzero00 <ernesto.soltero11@gmail.com>
2016-09-19 07:38:54 +0100

    2016-09-18's javascript workout

---
+++ contributions/zxzero00/javascript/Core/2016-09-18.js
@@ -0,0 +1,5 @@
+Types of numbers
+Use `splice()` to remove an item from an array
+Using assignment operators
+The difference between null, undefined and NaN
+Prevent background image flicker in **IE**
