c41e1aa9882c8e33fc823c9281668a90ca13ee46
rengare <gorecki.gracjan@gmail.com>
2016-09-19 04:51:31 +0100

    2016-09-18's java workout

---
+++ contributions/rengare/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
