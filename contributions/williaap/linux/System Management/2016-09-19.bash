d2e5eef90a2028a1e5bdfa5ca9e928fa8d84a355
williaap <williaap@gmail.com>
2016-09-19 21:19:54 +0100

    2016-09-19's linux workout

---
+++ contributions/williaap/linux/System Management/2016-09-19.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Find the `MAC` address of all network interfaces
+
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
