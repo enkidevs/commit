d7cfb6bf9a90e77ffaedeb754477092270f366b8
Faithful Mind <lpyy15@gmail.com>
2016-09-19 04:29:04 +0100

    2016-09-18's java workout

---
+++ contributions/Faithful Mind/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use strings in a `switch` statement
+Detect or prevent integer overflow
+How range operations work
+Supply `toString()` in all classes
