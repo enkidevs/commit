38384a04b4f9126201aeb3361ddfaa675bf3cdb3
VickyKoblinski <safimoyo@gmail.com>
2016-09-19 02:13:29 +0100

    2016-09-18's java workout

---
+++ contributions/VickyKoblinski/java/Java 8/2016-09-18.java
@@ -0,0 +1,5 @@
+Java 8 Lambda expressions
+Control OS `Process`es in `Java8`
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
