bd96d263df04acd15364f383ffd75db88ab4674e
sassy_samurai <srikant2014@gmail.com>
2016-09-19 18:07:59 +0100

    2016-09-18's linux workout

---
+++ contributions/sassy_samurai/linux/System Management/2016-09-18.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
