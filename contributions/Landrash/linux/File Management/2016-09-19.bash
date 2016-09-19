05168752d23d41846b2bb38ab322ecb7175ad449
Landrash <landrash@gmail.com>
2016-09-19 16:15:55 +0100

    2016-09-19's linux workout

---
+++ contributions/Landrash/linux/File Management/2016-09-19.bash
@@ -0,0 +1,4 @@
+Fast file indexing with `updatedb` and `locate`
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
