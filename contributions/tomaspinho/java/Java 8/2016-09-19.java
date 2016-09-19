c29b84118c2994b5a896a78c9ab9ea2b00ed1e01
tomaspinho <me@tomaspinho.com>
2016-09-19 19:16:05 +0100

    2016-09-19's java workout

---
+++ contributions/tomaspinho/java/Java 8/2016-09-19.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+How to check for static dependencies of applications and libraries from the command line.
+Use `Optional(s)` type in order to avoid `NullPointerException(s)`
+Control OS `Process`es in `Java8`
+Java8's new `StampedLock`
