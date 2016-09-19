5f00fc38da505487f334d4a559815529d421617b
mihai722 <mihai7221@gmail.com>
2016-09-19 21:31:42 +0100

    2016-09-19's linux workout

---
+++ contributions/mihai722/linux/File Management/2016-09-19.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Delete commands aliases
