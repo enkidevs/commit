5882b0156a5d0f3a1a66cb2aa445f494700114b0
dimipage <dimi.page@gmail.com>
2016-09-19 10:18:41 +0100

    2016-09-19's linux workout

---
+++ contributions/dimipage/linux/Networking/2016-09-19.bash
@@ -0,0 +1,5 @@
+Logging dropped firewall packets
+Get real network statistics with `slurm`
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Short Introduction to `Wget` 
