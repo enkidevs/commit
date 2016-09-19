378abb143bd050644d566962cbe2febf4a7b38e5
Krapsus <fernandomartinez0295@gmail.com>
2016-09-19 03:55:42 +0100

    2016-09-18's java workout

---
+++ contributions/Krapsus/java/I/O/2016-09-18.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+Create system file paths using the `Path` class
+Reading and writing text files
+Using buffered streams
+Checking for the existence of files and directories
