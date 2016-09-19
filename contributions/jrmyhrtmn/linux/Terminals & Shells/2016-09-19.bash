74c45733c809465701b2c1aab0d57b27bcd47283
jrmyhrtmn <jmaize91@gmail.com>
2016-09-19 14:14:51 +0100

    2016-09-19's linux workout

---
+++ contributions/jrmyhrtmn/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Useful aliasing in bash
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
