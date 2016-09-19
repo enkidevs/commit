7b81ec763ea6c3d66bd2d620e643f8e5dcc1074f
saffad <michal_kolsut@o2.pl>
2016-09-19 13:28:04 +0100

    2016-09-19's java workout

---
+++ contributions/saffad/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Updating interfaces by using `default` methods
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
