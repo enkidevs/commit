0f49c394dc04de4757dd6c7538d643987a3674cb
Llambi <hugopf.mail@gmail.com>
2016-09-19 07:16:08 +0100

    2016-09-19's linux workout

---
+++ contributions/Llambi/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Terminal based browser
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
