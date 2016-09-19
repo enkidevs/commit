3307354fda745a61d4e44de9567dd1380a4ee3f7
Campa <euller_sl@hotmail.com>
2016-09-19 16:33:55 +0100

    2016-09-19's linux workout

---
+++ contributions/Campa/linux/System Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
