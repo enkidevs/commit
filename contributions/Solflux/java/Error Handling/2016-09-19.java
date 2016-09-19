ae3d73d722d08a8862f89f139947c347aed55100
Solflux <enki@solflux.co.uk>
2016-09-19 18:02:57 +0100

    2016-09-19's java workout

---
+++ contributions/Solflux/java/Error Handling/2016-09-19.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Using exceptions in Java
+Throwing an `exception`
+Careful Numeric Data Conversions
