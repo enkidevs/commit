8537530ffc7138c7424ec7be09d2b992fe0ad395
scw1022 <sauchi1022@gmail.com>
2016-09-19 03:16:01 +0100

    2016-09-18's linux workout

---
+++ contributions/scw1022/linux/System Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
