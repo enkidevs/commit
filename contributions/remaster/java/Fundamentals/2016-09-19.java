9ecc118aa92df4c6470bf09c7954cbf9fc6c026f
remaster <daniel.neuman@gmail.com>
2016-09-19 19:11:36 +0100

    2016-09-19's java workout

---
+++ contributions/remaster/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Calling one constructor from another
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+`Autoboxing` and `Unboxing`
