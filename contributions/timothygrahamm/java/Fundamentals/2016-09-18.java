5ebd236aaa9a9c915aedb5302f5188bd8f0b3bfe
timothygrahamm <timothygrahamm@gmail.com>
2016-09-19 01:52:58 +0100

    2016-09-18's java workout

---
+++ contributions/timothygrahamm/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
