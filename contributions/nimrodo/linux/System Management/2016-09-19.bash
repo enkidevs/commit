8fe40603b18618b317fae99d8ce7d069b0cb471b
nimrodo <nimrodo@gmail.com>
2016-09-19 14:38:13 +0100

    2016-09-19's linux workout

---
+++ contributions/nimrodo/linux/System Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
