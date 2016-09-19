ae47b994bcf5ebe4a9379ae36c5cef5dc552acae
ricrogz <ricrogz@gmail.com>
2016-09-19 08:04:28 +0100

    2016-09-19's git workout

---
+++ contributions/ricrogz/git/Essentials/2016-09-19.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+Using tags for version control
+Search by commit message keyword
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
