78e5c2c93aa48ed7c75a595b2d0f0602e06e2ec0
garvamel <garvamel@gmail.com>
2016-09-19 16:33:30 +0100

    2016-09-19's python workout

---
+++ contributions/garvamel/python/Data Structures/2016-09-19.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`del` statement for  lists
+`Dictionary` view objects
+Best way to implement a simple `queue` 
+`Module`s everywhere!
