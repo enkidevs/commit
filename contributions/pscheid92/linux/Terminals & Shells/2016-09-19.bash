6f0926e97076192958a8c93671951affa9d08ddc
pscheid92 <p.scheid92@gmail.com>
2016-09-19 07:11:18 +0100

    2016-09-19's linux workout

---
+++ contributions/pscheid92/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Bash history reverse search
+Execute a command without saving it in history
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
