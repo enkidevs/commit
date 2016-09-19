96fd4824299d5d81096225286f3e8962cdd0a643
QuantumSnow <jakobtheai@gmail.com>
2016-09-19 21:21:07 +0100

    2016-09-19's linux workout

---
+++ contributions/QuantumSnow/linux/File Management/2016-09-19.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
