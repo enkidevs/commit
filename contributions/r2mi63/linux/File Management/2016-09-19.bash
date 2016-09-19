3fff437833fbb4088fcfea0166c66c29897b58a5
r2mi63 <remi.charbo@gmail.com>
2016-09-19 12:40:39 +0100

    2016-09-19's linux workout

---
+++ contributions/r2mi63/linux/File Management/2016-09-19.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
+Easy way of sharing files
