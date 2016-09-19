5de0fb6b801b6c312deca448dd7fad79226bccbb
JrobT <jack@trute.co.uk>
2016-09-19 11:24:02 +0100

    2016-09-19's linux workout

---
+++ contributions/JrobT/linux/File Management/2016-09-19.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Practical `du` command
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
