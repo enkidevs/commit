608f7d3d4445cd657e6733bb505ac4627c940e76
bleestein <bleestein@gmail.com>
2016-09-19 01:44:21 +0100

    2016-09-18's javascript workout

---
+++ contributions/bleestein/javascript/Node/2016-09-18.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Global Objects and Environment Variables in **Node**
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
+`try-catch` only for **sync** code
