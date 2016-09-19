55ad3654d218d09bbff20885b535590c246050de
ehnmark <github@ehnmark.com>
2016-09-19 23:16:25 +0100

    2016-09-19's linux workout

---
+++ contributions/ehnmark/linux/System Management/2016-09-19.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
