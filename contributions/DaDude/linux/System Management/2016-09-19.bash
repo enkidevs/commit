433b407eaf78427cf8a37bdf75571650a9be9949
DaDude <marc.luther91@googlemail.com>
2016-09-19 15:00:27 +0100

    2016-09-19's linux workout

---
+++ contributions/DaDude/linux/System Management/2016-09-19.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
