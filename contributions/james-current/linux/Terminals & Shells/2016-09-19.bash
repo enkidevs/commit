c7752f447a5c0159278752404e04ce82f838b267
james-current <jcurrent01@gmail.com>
2016-09-19 15:07:41 +0100

    2016-09-19's linux workout

---
+++ contributions/james-current/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Aliasing ssh connections
+Random password generator
+Sequential execution using the `;` statement separator
