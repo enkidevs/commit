7e8fad0487eb3f377bd4269ffcae6339e7746b94
le_idiot <siddhesh.mhadnak@gmail.com>
2016-09-19 16:23:10 +0100

    2016-09-18's linux workout

---
+++ contributions/le_idiot/linux/File Management/2016-09-18.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
