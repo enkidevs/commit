ac22e12571b68ab4bee75b9b203280c44f4c8cd4
moyosore <mohyour01@gmail.com>
2016-09-19 12:49:41 +0100

    2016-09-19's linux workout

---
+++ contributions/moyosore/linux/System Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Detect your linux distribution
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
