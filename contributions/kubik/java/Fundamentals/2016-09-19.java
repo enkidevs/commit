cd7d85a0c044268a6a29b2c25ec5e4821ab9a5ef
kubik <lkubic@poczta.onet.pl>
2016-09-19 07:31:23 +0100

    2016-09-19's java workout

---
+++ contributions/kubik/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
