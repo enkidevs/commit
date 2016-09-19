2d4d8814a5900469c9e0342e21af1cbe27d7886b
marcin.binczyk <marcin.binczyk@gmail.com>
2016-09-19 12:17:13 +0100

    2016-09-19's java workout

---
+++ contributions/marcin.binczyk/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+The distinction between overloading and overriding methods
+Use meaningful names
+Catch multiple exceptions in a single `catch` block
+Using bounded type parameters in generic methods
