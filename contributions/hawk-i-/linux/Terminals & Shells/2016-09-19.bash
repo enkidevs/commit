183343dd434d78a75eb3904037513ff0e3924e94
hawk-i- <gurbakhshish.s@gmail.com>
2016-09-19 23:04:16 +0100

    2016-09-19's linux workout

---
+++ contributions/hawk-i-/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
