f8ec6eb92a6c4e6f66711363c8bcfa7619bd43d5
derektolliver <derek.tolliver@utexas.edu>
2016-09-20 00:35:07 +0100

    2016-09-19's java workout

---
+++ contributions/derektolliver/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
