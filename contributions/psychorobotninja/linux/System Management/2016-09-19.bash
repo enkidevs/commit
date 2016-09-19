005e1e4adf95f9fd61ecbb487610868e2b94d957
psychorobotninja <tefuller@gmail.com>
2016-09-19 16:27:27 +0100

    2016-09-19's linux workout

---
+++ contributions/psychorobotninja/linux/System Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
