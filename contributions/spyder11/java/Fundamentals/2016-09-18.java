c596986a4e98840209c8d9ea9242c49ad0da8b5f
spyder11 <spyder11@me.com>
2016-09-19 03:49:47 +0100

    2016-09-18's java workout

---
+++ contributions/spyder11/java/Fundamentals/2016-09-18.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Ternary operator
+Use strings in a `switch` statement
+Using Inheritance to reduce code repetition
+Using bounded type parameters in generic methods
