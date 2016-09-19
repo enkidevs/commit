e362874534a7d8c95c45409fdef519fe20207631
dan3k <eldan3k@gmail.com>
2016-09-19 12:37:35 +0100

    2016-09-19's java workout

---
+++ contributions/dan3k/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+The distinction between overloading and overriding methods
+Java naming conventions
+Catch multiple exceptions in a single `catch` block
+Using `synchronized` statements
