74dabc0100a8f81bdbd5b9c689f02fdb13a4617e
recycli <wnsdh354@naver.com>
2016-09-20 04:25:09 +0100

    2016-09-19's linux workout

---
+++ contributions/recycli/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
