f13c854b9cb5a537ec1281b8fe8a0f7d296da689
crakoucas <crakoucas@gmail.com>
2016-09-20 23:15:51 +0100

    2016-09-20's linux workout

---
+++ contributions/crakoucas/linux/System Management/2016-09-20.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Basic service management with `systemd`
