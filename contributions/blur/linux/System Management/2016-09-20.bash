e83ad71f8141ff28ae29d14651560a31dbecc6f7
blur <filip@munk.pro>
2016-09-20 08:26:24 +0100

    2016-09-20's linux workout

---
+++ contributions/blur/linux/System Management/2016-09-20.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
