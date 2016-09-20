7c1ee9e9a2bea650128aea9eaca990e5ddf173a2
jvasallo <joelvasallo@gmail.com>
2016-09-20 22:29:31 +0100

    2016-09-20's linux workout

---
+++ contributions/jvasallo/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
