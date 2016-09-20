88f0cd37982669f97c34d49759b372da95145f45
Krynio <krynio7@gmail.com>
2016-09-20 20:24:34 +0100

    2016-09-20's java workout

---
+++ contributions/Krynio/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
