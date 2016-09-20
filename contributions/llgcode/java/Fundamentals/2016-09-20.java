cfa835d80c8d849a218943b04bcae8c18f37e5ba
llgcode <legoff.laurent@gmail.com>
2016-09-20 10:14:22 +0100

    2016-09-20's java workout

---
+++ contributions/llgcode/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Always specify type parameter
+Supply `toString()` in all classes
