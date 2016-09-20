88638edb18e0d7912c9ce6943d19171eca580616
dukman <dukman.box@gmail.com>
2016-09-20 19:29:37 +0100

    2016-09-20's java workout

---
+++ contributions/dukman/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
