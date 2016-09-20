d3b5f73c07b46ec7fe7f6efb2f98610399f54e2a
nahuelmorata <nahuelmorata@gmail.com>
2016-09-20 10:40:17 +0100

    2016-09-20's javascript workout

---
+++ contributions/nahuelmorata/javascript/Core/2016-09-20.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Most efficient way to build `HTML` strings
+Data type comparison in `switch` statements
+Prevent background image flicker in **IE**
+Detect online connection
