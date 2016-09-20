cd8acfeb23874518dd49d3bb306cc9c23039fb2d
egoisticalgoat <pascal.pirschel@gmail.com>
2016-09-20 09:33:06 +0100

    2016-09-20's linux workout

---
+++ contributions/egoisticalgoat/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Clear the terminal instantly
+Terminal incognito mode
+`else` statements using the `||` operator 
