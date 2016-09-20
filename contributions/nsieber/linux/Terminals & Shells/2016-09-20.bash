b7caa80fb796fa02ab4086b17f6db0ecbd1f6561
nsieber <nsieber@gmail.com>
2016-09-20 17:12:39 +0100

    2016-09-20's linux workout

---
+++ contributions/nsieber/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,5 @@
+Useful aliasing in bash
+Terminal incognito mode
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
