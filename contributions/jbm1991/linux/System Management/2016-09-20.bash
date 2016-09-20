2ebf31f651ebd140f9aaed1431c5a364c51b3cdc
jbm1991 <jbm1991@gmail.com>
2016-09-20 23:22:30 +0100

    2016-09-20's linux workout

---
+++ contributions/jbm1991/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
