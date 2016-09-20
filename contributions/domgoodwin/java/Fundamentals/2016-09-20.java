78e0585bd62f7995a6f82524eb5adb292629685d
domgoodwin <d0m182goodwin@gmail.com>
2016-09-20 13:26:40 +0100

    2016-09-20's java workout

---
+++ contributions/domgoodwin/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
