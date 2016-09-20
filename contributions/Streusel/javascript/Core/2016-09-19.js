88fa8ef677931a079ac9f3519d3c53347effdf2d
Streusel <m.me1@live.de>
2016-09-20 03:04:02 +0100

    2016-09-19's javascript workout

---
+++ contributions/Streusel/javascript/Core/2016-09-19.js
@@ -0,0 +1,5 @@
+Deleting properties
+Data type comparison in `switch` statements
+Infinity
+Hoisting applies only to variable declarations, not initializations
+Getting the *real* dimensions of an image
