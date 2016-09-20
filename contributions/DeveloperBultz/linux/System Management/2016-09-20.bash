ead1aa5ae91b76cc965f4ada753b0a0384911a57
DeveloperBultz <bultz@csgopurse.com>
2016-09-20 13:41:28 +0100

    2016-09-20's linux workout

---
+++ contributions/DeveloperBultz/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
