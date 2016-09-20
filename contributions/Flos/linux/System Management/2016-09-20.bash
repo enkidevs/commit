4911237df29b77ec68d45b80379fce0dca9fc0ab
Flos <fn187-github@yahoo.de>
2016-09-20 11:43:37 +0100

    2016-09-20's linux workout

---
+++ contributions/Flos/linux/System Management/2016-09-20.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
