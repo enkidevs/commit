c082332e774bdda2580a8910efe9c0d845045870
cybersmurf <megamrsk@gmail.com>
2016-09-20 01:41:13 +0100

    2016-09-19's linux workout

---
+++ contributions/cybersmurf/linux/System Management/2016-09-19.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Check the HDD with `badblocks`
