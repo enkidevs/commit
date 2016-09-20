c59d5029d2d667624693091c87bebfa24ac57483
aayvazyan-tgm <ariay@live.at>
2016-09-20 10:19:44 +0100

    2016-09-20's java workout

---
+++ contributions/aayvazyan-tgm/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Avoid using `static` variables
