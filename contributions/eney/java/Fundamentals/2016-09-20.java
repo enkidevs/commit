a17e55165c71ebbe4ff74603a46a6f85648b4d6e
eney <jzabrodin@gmail.com>
2016-09-20 13:47:29 +0100

    2016-09-20's java workout

---
+++ contributions/eney/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,4 @@
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Limit Accessibility of `Fields`
