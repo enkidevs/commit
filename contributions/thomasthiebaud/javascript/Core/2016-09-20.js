e120560ebd5086aaf34e86bf437505e4a6e9d35f
thomasthiebaud <thiebaud.tom@gmail.com>
2016-09-20 18:29:39 +0100

    2016-09-20's javascript workout

---
+++ contributions/thomasthiebaud/javascript/Core/2016-09-20.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Data type comparison in `switch` statements
+Extra function arguments are undefined by default
+Detect **DO NOT TRACK** status
+Getting the *real* dimensions of an image
