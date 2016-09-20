ec28ac0582e831a669157799fade3beed2bbbefa
XuanYuan <ookk8282@gmail.com>
2016-09-20 05:23:50 +0100

    2016-09-19's linux workout

---
+++ contributions/XuanYuan/linux/Terminals & Shells/2016-09-19.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
