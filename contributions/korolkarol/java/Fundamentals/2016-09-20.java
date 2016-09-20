543b6b71a4e4d829c0015ceb70d393c63d2a36dc
korolkarol <egzepl@gmail.com>
2016-09-20 20:28:40 +0100

    2016-09-20's java workout

---
+++ contributions/korolkarol/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
