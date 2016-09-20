8ace66b3a70d9458665fe727c490d6359feb39c6
mhfayssal <mhfayssal@gmail.com>
2016-09-20 09:00:39 +0100

    2016-09-20's linux workout

---
+++ contributions/mhfayssal/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Repeating commands with `watch`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
