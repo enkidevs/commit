465a95ebbef63ce8a4f7416692bb2c322af05979
steved <stephendoyle75@gmail.com>
2016-09-20 10:42:07 +0100

    2016-09-20's linux workout

---
+++ contributions/steved/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Terminal based browser
+Useful aliasing in bash
+`else` statements using the `||` operator 
