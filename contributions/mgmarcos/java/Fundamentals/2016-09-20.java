7c91c2e6cf504041b8b0a26311990fe00929c51d
mgmarcos <mgmarcos@gmail.com>
2016-09-20 08:55:41 +0100

    2016-09-20's java workout

---
+++ contributions/mgmarcos/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+Connecting to FTP using Java
+Distinction between `public` and `private` methods
+Use `DecimalFormat` class to format numbers
+Supply `toString()` in all classes
