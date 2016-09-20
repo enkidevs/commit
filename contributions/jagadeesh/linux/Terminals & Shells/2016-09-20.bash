b44a6b1629f1dca9100f9d28368961fc7b4d8e97
jagadeesh <hellmaker001@gmail.com>
2016-09-20 17:30:57 +0100

    2016-09-20's linux workout

---
+++ contributions/jagadeesh/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
