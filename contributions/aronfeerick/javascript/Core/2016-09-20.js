55a30680251b150eeef1655788c8e4113996d1db
aronfeerick <aronfeerick@gmail.com>
2016-09-20 23:04:57 +0100

    2016-09-20's javascript workout

---
+++ contributions/aronfeerick/javascript/Core/2016-09-20.js
@@ -0,0 +1,5 @@
+Keywords in hash keys
+Most efficient way to build `HTML` strings
+Extra function arguments are undefined by default
+Prevent background image flicker in **IE**
+Detect an error type
