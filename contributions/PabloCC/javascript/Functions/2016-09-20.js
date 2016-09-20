dc4ce8598da71309ba438c2e19e8af5514db56a9
PabloCC <pablocc1996@gmail.com>
2016-09-20 17:32:36 +0100

    2016-09-20's javascript workout

---
+++ contributions/PabloCC/javascript/Functions/2016-09-20.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Hoisting
+Difference between **.call** and **.apply** methods
+Anonymous functions
+Check if a document is done loading
