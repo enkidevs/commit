865b7dcb0a703ff63d4d71d01d590c75fd8335d1
krazhug <leclercq.pl@gmail.com>
2016-09-20 06:51:23 +0100

    2016-09-20's linux workout

---
+++ contributions/krazhug/linux/System Management/2016-09-20.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
