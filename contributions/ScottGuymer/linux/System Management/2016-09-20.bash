c8469432c72cf93efd2423da1228bbeff58a83b0
ScottGuymer <scott@scottguymer.co.uk>
2016-09-20 07:54:47 +0100

    2016-09-20's linux workout

---
+++ contributions/ScottGuymer/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
