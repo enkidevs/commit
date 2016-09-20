c039b007684b5bfb18cf05c5e609c0230db26c33
hugo2412 <hb.fontvieille@gmail.com>
2016-09-20 10:44:48 +0100

    2016-09-20's linux workout

---
+++ contributions/hugo2412/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Get to know  your commands with `type`
