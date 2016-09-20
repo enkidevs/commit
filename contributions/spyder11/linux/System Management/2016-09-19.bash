a79f0650c8c71a0de2dc60a4bf5de6cf140ce38e
spyder11 <spyder11@me.com>
2016-09-20 03:17:31 +0100

    2016-09-19's linux workout

---
+++ contributions/spyder11/linux/System Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Test disk speed with `dd`
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
