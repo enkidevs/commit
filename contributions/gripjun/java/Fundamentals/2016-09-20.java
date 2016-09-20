3c4b46d23d3e8208e15ef863c63e61eab5268f56
gripjun <gripjun@gmail.com>
2016-09-20 23:12:24 +0100

    2016-09-20's java workout

---
+++ contributions/gripjun/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Metadata: creating a user-defined file attribute
+Supply `toString()` in all classes
