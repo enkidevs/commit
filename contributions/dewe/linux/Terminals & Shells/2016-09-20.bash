7cafb5bcbb0d4ca9941a2bbc2dab9e6846e3162a
dewe <johan@dewe.net>
2016-09-20 07:58:57 +0100

    2016-09-20's linux workout

---
+++ contributions/dewe/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Useful aliasing in bash
+Terminal incognito mode
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
