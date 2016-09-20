3cf3986e9affeb7a30ae9b69bb69ce173f755882
symplekaos <symplekaos@gmail.com>
2016-09-20 21:19:04 +0100

    2016-09-20's linux workout

---
+++ contributions/symplekaos/linux/System Management/2016-09-20.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
