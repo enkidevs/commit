67cf74fa5d1045429c76b5ac8f833bf405fc923a
Thommi <thomasandre84@gmail.com>
2016-09-20 18:03:10 +0100

    2016-09-20's java workout

---
+++ contributions/Thommi/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
