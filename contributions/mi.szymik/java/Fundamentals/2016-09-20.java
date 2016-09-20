0db12e4ae7e42b594b9fcdd5f515840add08a41d
mi.szymik <maykl.szymik@gmail.com>
2016-09-20 21:48:21 +0100

    2016-09-20's java workout

---
+++ contributions/mi.szymik/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
