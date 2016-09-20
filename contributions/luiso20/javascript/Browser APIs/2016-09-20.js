531f96102166a57f7394224fefb93b7ae3d80413
luiso20 <luiso20@gmail.com>
2016-09-20 17:36:40 +0100

    2016-09-20's javascript workout

---
+++ contributions/luiso20/javascript/Browser APIs/2016-09-20.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Allow an element to go fullscreen
+Warn user if **Back** button is pressed
+Network Information API
+MediaDevices.getUserMedia()
