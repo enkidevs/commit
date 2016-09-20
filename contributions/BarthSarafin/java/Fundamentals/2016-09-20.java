379cdc992b72a1c34e98152058e2b202e443c4c8
BarthSarafin <stefan.r.bachmann@gmail.com>
2016-09-20 19:43:30 +0100

    2016-09-20's java workout

---
+++ contributions/BarthSarafin/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
