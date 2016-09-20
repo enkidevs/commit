8a93d6327673d8bb66be76dc8f0891e07900e943
eddimas <eddimas@gmail.com>
2016-09-20 23:18:24 +0100

    2016-09-20's linux workout

---
+++ contributions/eddimas/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
