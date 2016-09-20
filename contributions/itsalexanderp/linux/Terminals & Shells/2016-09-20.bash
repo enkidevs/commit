262a265d32393163114e3dc5e71a7057265b23db
itsalexanderp <itsalexanderp@gmail.com>
2016-09-20 12:23:39 +0100

    2016-09-20's linux workout

---
+++ contributions/itsalexanderp/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Useful aliasing in bash
+Random password generator
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
