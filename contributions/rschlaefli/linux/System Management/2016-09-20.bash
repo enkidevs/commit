2b7104478444afbcd576e27f1b486a01f063929a
rschlaefli <rolandschlaefli@gmail.com>
2016-09-20 19:28:22 +0100

    2016-09-20's linux workout

---
+++ contributions/rschlaefli/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
