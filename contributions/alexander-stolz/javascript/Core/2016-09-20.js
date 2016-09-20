92a2775710a3ed35845df1da1cb165653a40eedd
alexander-stolz <amstolz@gmail.com>
2016-09-20 06:53:59 +0100

    2016-09-20's javascript workout

---
+++ contributions/alexander-stolz/javascript/Core/2016-09-20.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Weak vs Strict equality operator
+Use `splice()` to remove an item from an array
+Hoisting applies only to variable declarations, not initializations
+Getting the *real* dimensions of an image
