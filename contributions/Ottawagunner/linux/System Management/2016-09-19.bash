b56512fd1071ff41b72194d593b51a46f881e849
Ottawagunner <matthew.lewis.taylor@gmail.com>
2016-09-20 03:33:46 +0100

    2016-09-19's linux workout

---
+++ contributions/Ottawagunner/linux/System Management/2016-09-19.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Commands to shutdown or restart the system
