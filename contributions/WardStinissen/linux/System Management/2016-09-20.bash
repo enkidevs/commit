2e46e89a5f3207162795e66d24f5125ac21a5b9a
WardStinissen <ward.stinissen@gmail.com>
2016-09-20 21:14:49 +0100

    2016-09-20's linux workout

---
+++ contributions/WardStinissen/linux/System Management/2016-09-20.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
