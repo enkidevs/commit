7bad5a5679ebe8a21552ed169d957ecb85a7f774
Vovke <vova.lando@gmail.com>
2016-09-20 14:06:27 +0100

    2016-09-20's linux workout

---
+++ contributions/Vovke/linux/System Management/2016-09-20.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
