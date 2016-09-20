b89d50b335a3c8f96112f3b72e4c09b60cfd7574
happycrappie <imerycdasilva@gmail.com>
2016-09-20 14:18:38 +0100

    2016-09-20's javascript workout

---
+++ contributions/happycrappie/javascript/Core/2016-09-20.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+No block scope
+Hoisting applies only to variable declarations, not initializations
+The difference between null, undefined and NaN
+`console.*` in JavaScript
