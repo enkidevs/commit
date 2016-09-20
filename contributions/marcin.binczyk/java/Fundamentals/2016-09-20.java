93284056ea4be45a1bbfb9382b647f9de450ed72
marcin.binczyk <marcin.binczyk@gmail.com>
2016-09-20 09:21:32 +0100

    2016-09-20's java workout

---
+++ contributions/marcin.binczyk/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
