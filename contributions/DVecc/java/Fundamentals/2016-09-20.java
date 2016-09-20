ee146b40ae5342033ca525eb75b7f2f2521e327f
DVecc <n00787794@students.ncc.edu>
2016-09-20 17:06:24 +0100

    2016-09-20's java workout

---
+++ contributions/DVecc/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Supply `toString()` in all classes
