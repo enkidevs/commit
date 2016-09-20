37bc096342451388251f59f125fd8fa8a36940c4
stullageek <akalx44@gmail.com>
2016-09-20 19:29:25 +0100

    2016-09-20's java workout

---
+++ contributions/stullageek/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Using `synchronized` statements
+Detect or prevent integer overflow
+Using an interface as a parameter
