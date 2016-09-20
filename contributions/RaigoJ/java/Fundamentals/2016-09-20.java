285e4228a291b7f105dfdb49fc7c633aaf9f716a
RaigoJ <raigo.jerva@gmail.com>
2016-09-20 13:40:48 +0100

    2016-09-20's java workout

---
+++ contributions/RaigoJ/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
