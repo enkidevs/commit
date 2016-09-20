b88ff752d6d4a9fb4e359abb69aff40da4669f34
Alphatroya <alphatroya@gmail.com>
2016-09-20 10:31:44 +0100

    2016-09-20's linux workout

---
+++ contributions/Alphatroya/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
