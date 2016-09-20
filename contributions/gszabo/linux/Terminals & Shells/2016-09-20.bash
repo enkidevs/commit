67cbea7c88a91b0ac944cafe5321e7271e7407e3
gszabo <harry.bme@gmail.com>
2016-09-20 07:01:01 +0100

    2016-09-20's linux workout

---
+++ contributions/gszabo/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
