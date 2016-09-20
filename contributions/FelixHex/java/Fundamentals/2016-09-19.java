64fa30c791c09f0329b7547fff45d64332d2ddcb
FelixHex <maximcove@gmail.com>
2016-09-20 04:57:23 +0100

    2016-09-19's java workout

---
+++ contributions/FelixHex/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Inheriting a constructor from a superclass
+Measuring time
+Supply `toString()` in all classes
+Using an interface as a parameter
