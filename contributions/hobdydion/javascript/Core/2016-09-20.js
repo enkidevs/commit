f007eba8d15d2e1e3eba09cd38efdf227e0fe8e1
hobdydion <hobdydion@gmail.com>
2016-09-20 22:03:47 +0100

    2016-09-20's javascript workout

---
+++ contributions/hobdydion/javascript/Core/2016-09-20.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Using the double tilde `~~` 
+Data type comparison in `switch` statements
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
