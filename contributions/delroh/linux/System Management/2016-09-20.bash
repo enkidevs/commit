9a271355032d71885b0e7f4e0c2891246354bf5c
delroh <henrilinc45@hotmail.com>
2016-09-20 11:19:10 +0100

    2016-09-20's linux workout

---
+++ contributions/delroh/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
