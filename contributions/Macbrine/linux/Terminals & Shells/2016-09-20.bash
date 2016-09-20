1eeb8697007760662674e7c07b9ef90207da2501
Macbrine <mmacneil@vols.utk.edu>
2016-09-20 21:56:53 +0100

    2016-09-20's linux workout

---
+++ contributions/Macbrine/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,7 @@
+Find any Unix / Linux command
+Random password generator
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
