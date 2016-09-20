1fbcdbb7294221d2a7bdd55510bc51c753df92b3
FredTuna <fredd.g@gmail.com>
2016-09-20 18:47:04 +0100

    2016-09-20's java workout

---
+++ contributions/FredTuna/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Always specify type parameter
+Increase `PermGen` space as to avoid `OutOfMemory` errors
