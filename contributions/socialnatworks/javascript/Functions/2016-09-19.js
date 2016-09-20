97c45d5b008160ab3934acbe550b1be926fe558b
socialnatworks <nataliewy.ip@gmail.com>
2016-09-20 16:08:16 +0100

    2016-09-19's javascript workout

---
+++ contributions/socialnatworks/javascript/Functions/2016-09-19.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
