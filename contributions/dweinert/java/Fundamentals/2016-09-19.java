03f6f6e9bc54ed054fd5745f9b9facddf2a46ead
dweinert <d.weinert@live.com>
2016-09-20 06:15:42 +0100

    2016-09-19's java workout

---
+++ contributions/dweinert/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Constant notation
+Implementing an `interface`
+Using an interface as a parameter
