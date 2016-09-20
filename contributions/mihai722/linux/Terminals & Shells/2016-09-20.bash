b935a986c8c11e8cdeb3df90bbc0047c18fc2887
mihai722 <mihai7221@gmail.com>
2016-09-20 11:55:59 +0100

    2016-09-20's linux workout

---
+++ contributions/mihai722/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Clear the terminal instantly
+Find any Unix / Linux command
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
