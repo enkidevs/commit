92ca9001ea379dfc7cac812c9e8a93f3d984a645
dantavo <darkmind88@gmail.com>
2016-09-20 08:19:35 +0100

    2016-09-20's java workout

---
+++ contributions/dantavo/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
