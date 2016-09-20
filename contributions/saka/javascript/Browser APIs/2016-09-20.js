d4e224b6387ca89b6ee6d5fb597b0577e2806f01
saka <saka71996@gmail.com>
2016-09-20 16:58:30 +0100

    2016-09-20's javascript workout

---
+++ contributions/saka/javascript/Browser APIs/2016-09-20.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+Blobs
+Window.sessionStorage
+Network Information API
