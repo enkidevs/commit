4846fcb3251396309bb9ea67397c3c9950a0d86f
george070393 <george070393@gmail.com>
2016-09-20 07:21:16 +0100

    2016-09-20's linux workout

---
+++ contributions/george070393/linux/File Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+Converting between Unix and Windows text files
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Delete commands aliases
+Easy way of sharing files
