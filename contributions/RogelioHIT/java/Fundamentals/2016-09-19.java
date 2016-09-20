df298279ea67180396b4e391fd6caa7c6eb00200
RogelioHIT <red1hhh@gmail.com>
2016-09-20 03:19:28 +0100

    2016-09-19's java workout

---
+++ contributions/RogelioHIT/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Always specify type parameter
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
