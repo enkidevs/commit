ed6ddd5d792384a547c697a6bbfdbe61ed972ba2
arnis71 <arnis71@gmail.com>
2016-09-20 13:09:35 +0100

    2016-09-19's java workout

---
+++ contributions/arnis71/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
