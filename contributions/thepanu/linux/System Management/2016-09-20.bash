a3672f25e6e4447c6aa96d3257dbc02ea65a9ba4
thepanu <p.m.kinnari@gmail.com>
2016-09-20 22:21:46 +0100

    2016-09-20's linux workout

---
+++ contributions/thepanu/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
