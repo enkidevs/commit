cb6190e95a1436f56ad82d69a6201f400c68dab1
spudnica <amonowy@gmail.com>
2016-09-20 22:51:25 +0100

    2016-09-20's linux workout

---
+++ contributions/spudnica/linux/System Management/2016-09-20.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Monitor System Memory using `vmstat`
