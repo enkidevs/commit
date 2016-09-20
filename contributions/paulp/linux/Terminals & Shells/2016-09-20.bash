b23902aa3b14e96a7732e5b52713f1679441ad98
paulp <paulp@hotmail.sk>
2016-09-20 09:36:17 +0100

    2016-09-20's linux workout

---
+++ contributions/paulp/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,7 @@
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
