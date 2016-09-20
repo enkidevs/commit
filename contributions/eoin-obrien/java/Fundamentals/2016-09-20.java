9456bdd8b16c55dc35605c12810c2628c683677e
eoin-obrien <eoinobrien910@gmail.com>
2016-09-20 10:13:36 +0100

    2016-09-20's java workout

---
+++ contributions/eoin-obrien/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
+Using an interface as a parameter
