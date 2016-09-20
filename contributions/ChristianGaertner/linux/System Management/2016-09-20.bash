4ceccbb259b3016dc18d90db7d7c5be6b13bdce9
ChristianGaertner <chrisdagardner@gmail.com>
2016-09-20 16:34:30 +0100

    2016-09-20's linux workout

---
+++ contributions/ChristianGaertner/linux/System Management/2016-09-20.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
