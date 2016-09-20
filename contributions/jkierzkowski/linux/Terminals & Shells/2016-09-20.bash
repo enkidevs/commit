2980baf09e786e95795bdf1a4f5aac40e12fc5e5
jkierzkowski <jakubkierzkowski@gmail.com>
2016-09-20 20:02:47 +0100

    2016-09-20's linux workout

---
+++ contributions/jkierzkowski/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
