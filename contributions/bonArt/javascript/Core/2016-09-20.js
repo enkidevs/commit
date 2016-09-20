9ed2878ef6b00cfc3e7c6f2023b1ed610404e569
bonArt <alien2711@yandex.ru>
2016-09-20 13:27:55 +0100

    2016-09-20's javascript workout

---
+++ contributions/bonArt/javascript/Core/2016-09-20.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Hoisting applies only to variable declarations, not initializations
+Closures
+Extra function arguments are undefined by default
+Detect an error type
