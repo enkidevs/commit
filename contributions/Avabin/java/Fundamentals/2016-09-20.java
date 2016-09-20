50de459682c7c34a5f9f9b7de4b1345c9a939d59
Avabin <andrzej.piotrowski.76@gmail.com>
2016-09-20 16:36:49 +0100

    2016-09-20's java workout

---
+++ contributions/Avabin/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
