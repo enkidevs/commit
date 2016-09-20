20e2787ed1d63be8f1af024062458c8459bc194c
kevenYannic <keven.iwansky@gmail.com>
2016-09-20 11:28:58 +0100

    2016-09-20's linux workout

---
+++ contributions/kevenYannic/linux/System Management/2016-09-20.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
