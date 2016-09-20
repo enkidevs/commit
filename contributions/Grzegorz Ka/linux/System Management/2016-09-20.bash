9199f722a9cac4f94c33ec30f1712948bb642cb1
Grzegorz Ka <g.komajda@gmail.com>
2016-09-20 16:35:46 +0100

    2016-09-20's linux workout

---
+++ contributions/Grzegorz Ka/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
