c26136cd9344d7d5d44fa9008313519b9492036b
RaphaelRaja <jrrphonix@gmail.com>
2016-09-20 19:04:07 +0100

    2016-09-19's java workout

---
+++ contributions/RaphaelRaja/java/Fundamentals/2016-09-19.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+Limit Accessibility of `Fields`
