12a32773774a82dbe2896b76b203d60c12ad84a3
samconnor <samuelboss360@gmail.com>
2016-09-20 20:54:50 +0100

    2016-09-20's java workout

---
+++ contributions/samconnor/java/Data Types/2016-09-20.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use `Arrays.asList()` to initialise lists
+Converting a string to upper or lower case
+Finding a substring in a string
+Retrieve the component type of an array
