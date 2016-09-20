96075c43b6eaa770707dcef65b726e467437d832
hassehulabeck <hans@hulabeck.se>
2016-09-20 16:15:10 +0100

    2016-09-20's javascript workout

---
+++ contributions/hassehulabeck/javascript/Core/2016-09-20.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Filtering items out of an array
+Detect an error type
+Getting the *real* dimensions of an image
+Inaccuracy of binary floating-point format
