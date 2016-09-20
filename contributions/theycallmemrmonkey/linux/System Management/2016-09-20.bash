739e164324befa59827d5be0ce3eab4555944f65
theycallmemrmonkey <christopher.kaschny@gmail.com>
2016-09-20 23:58:32 +0100

    2016-09-20's linux workout

---
+++ contributions/theycallmemrmonkey/linux/System Management/2016-09-20.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
