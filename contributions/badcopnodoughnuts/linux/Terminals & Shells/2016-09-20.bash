a1bae019140371f1e385a75ac55585497714c257
badcopnodoughnuts <jay@phoenixclothingco.com>
2016-09-20 16:54:44 +0100

    2016-09-20's linux workout

---
+++ contributions/badcopnodoughnuts/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
