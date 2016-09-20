3d2994e95b0c2ee099b5af9bf43ff970036c22c8
tyronegrech <grechtyrone1989@gmail.com>
2016-09-20 20:42:50 +0100

    2016-09-20's linux workout

---
+++ contributions/tyronegrech/linux/System Management/2016-09-20.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Cancel a system shutdown
