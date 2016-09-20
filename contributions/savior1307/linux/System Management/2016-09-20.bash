d3d2277519467fca5cad53822902660d550d92fc
savior1307 <savior1307@gmail.com>
2016-09-20 06:08:59 +0100

    2016-09-20's linux workout

---
+++ contributions/savior1307/linux/System Management/2016-09-20.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
