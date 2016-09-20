b5240bb6b525a3b0f35935eb71314354a34156c8
LarryG <laurynasgiriunas@gmail.com>
2016-09-20 16:33:58 +0100

    2016-09-20's java workout

---
+++ contributions/LarryG/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
