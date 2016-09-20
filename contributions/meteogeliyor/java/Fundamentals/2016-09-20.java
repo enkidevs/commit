5d745ad5f74b0e31075fccf6d54f5687f1a78675
meteogeliyor <mertcan.alici18@gmail.com>
2016-09-20 20:44:58 +0100

    2016-09-20's java workout

---
+++ contributions/meteogeliyor/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
+Using an interface as a parameter
