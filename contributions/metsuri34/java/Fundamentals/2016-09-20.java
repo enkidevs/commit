01e366c45eca893bfd686e25a0a22ad57d67c260
metsuri34 <metsaranta.mikko@gmail.com>
2016-09-20 16:30:36 +0100

    2016-09-20's java workout

---
+++ contributions/metsuri34/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
