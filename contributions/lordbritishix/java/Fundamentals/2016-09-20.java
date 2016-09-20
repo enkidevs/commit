63c95622a96d7e2b5a3c955989fa15d8c390b23e
lordbritishix <jim.quitevis@gmail.com>
2016-09-20 17:21:54 +0100

    2016-09-20's java workout

---
+++ contributions/lordbritishix/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
