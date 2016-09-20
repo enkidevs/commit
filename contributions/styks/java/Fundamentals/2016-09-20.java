25233710f75a58d71a46f5f2cb3372d5e2364cf6
styks <simon31313@gmail.com>
2016-09-20 21:42:26 +0100

    2016-09-20's java workout

---
+++ contributions/styks/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
+Using an interface as a parameter
