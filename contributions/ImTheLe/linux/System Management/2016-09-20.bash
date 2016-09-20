9f59511cbd2f0b9bd9841e6f61c4fa423dd5639d
ImTheLe <leongrdic@gmail.com>
2016-09-20 19:34:57 +0100

    2016-09-20's linux workout

---
+++ contributions/ImTheLe/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
