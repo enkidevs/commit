e2f14c9ae73ad31656b2380185623f3a9c7d2432
manitua <jasmin.beharic@gmail.com>
2016-09-20 06:01:28 +0100

    2016-09-20's linux workout

---
+++ contributions/manitua/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Finding Open Files With `lsof`
