7d664edd08a5d50c0254aab4e6eb15fd93c6eaff
ProperBritish <daniellawrencehilton@hotmail.co.uk>
2016-09-20 18:36:56 +0100

    2016-09-20's linux workout

---
+++ contributions/ProperBritish/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
