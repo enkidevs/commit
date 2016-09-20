be45319296a796cc91d1afff33513d20158e2e47
mrsandman326 <adriobkork@gmail.com>
2016-09-20 22:10:33 +0100

    2016-09-20's linux workout

---
+++ contributions/mrsandman326/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
