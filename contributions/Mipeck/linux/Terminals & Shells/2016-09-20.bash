ed940b02a3f7a2bf65d59e5af024bda441d3ce7a
Mipeck <michal.zakrzewski88@gmail.com>
2016-09-20 20:58:36 +0100

    2016-09-20's linux workout

---
+++ contributions/Mipeck/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Random password generator
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
