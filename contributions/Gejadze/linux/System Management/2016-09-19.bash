7f19556c47eda3314cdd4c3a43c4f7080091cbf8
Gejadze <gejadze@gmail.com>
2016-09-20 03:44:04 +0100

    2016-09-19's linux workout

---
+++ contributions/Gejadze/linux/System Management/2016-09-19.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
