5be4b94767f5b1ee9b05dc92a772a0954f0ae177
Gpef <oleg.baslak@gmail.com>
2016-09-21 19:19:39 +0100

    2016-09-21's java workout

---
+++ contributions/Gpef/java/Data Types/2016-09-21.java
@@ -0,0 +1,5 @@
+Use `Arrays.asList()` to initialise lists
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
+Do not attempt comparisons with NaN
+Numeric Conversion - Widening
