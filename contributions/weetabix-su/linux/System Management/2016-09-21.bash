8a2d2336ee8381248a8673b8d802583c6a0a49b4
weetabix-su <vovo27_miranemiko@yahoo.co.jp>
2016-09-21 23:17:17 +0100

    2016-09-21's linux workout

---
+++ contributions/weetabix-su/linux/System Management/2016-09-21.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
