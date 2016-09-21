6b27f09986d78ef091925ab7701a732b7a1010bb
canemacchina <canemacchina@gmail.com>
2016-09-21 07:20:55 +0100

    2016-09-21's java workout

---
+++ contributions/canemacchina/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
