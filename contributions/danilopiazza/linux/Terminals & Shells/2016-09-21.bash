d9169c9d5a69263c6726b481011aced5d1445404
danilopiazza <danilopiazza@gmail.com>
2016-09-21 06:35:54 +0100

    2016-09-21's linux workout

---
+++ contributions/danilopiazza/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
