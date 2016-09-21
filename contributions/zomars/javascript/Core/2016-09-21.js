c303a9f421441e54878343f2d2ff533b4e2cfab2
zomars <zomars@me.com>
2016-09-21 20:31:19 +0100

    2016-09-21's javascript workout

---
+++ contributions/zomars/javascript/Core/2016-09-21.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Weak vs Strict equality operator
+Hoisting applies only to variable declarations, not initializations
+Closures
+Getting the *real* dimensions of an image
