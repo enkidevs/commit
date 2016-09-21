9fbcc6bc3d46d7e6baaf53d3f0cd6d037b34580b
Pankraz01 <mine6626@gmail.com>
2016-09-21 21:38:55 +0100

    2016-09-21's java workout

---
+++ contributions/Pankraz01/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
