cd89a7d24f863b690b3eb980499edc418e3ac0c4
Prominence <prominence96@gmail.com>
2016-09-21 04:57:16 +0100

    2016-09-21's linux workout

---
+++ contributions/Prominence/linux/System Management/2016-09-21.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
