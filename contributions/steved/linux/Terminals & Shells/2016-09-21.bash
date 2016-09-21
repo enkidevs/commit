988ea0f3eacd435242df46438ed40fdb3b2ecd26
steved <stephendoyle75@gmail.com>
2016-09-21 09:54:30 +0100

    2016-09-21's linux workout

---
+++ contributions/steved/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Aliasing ssh connections
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
