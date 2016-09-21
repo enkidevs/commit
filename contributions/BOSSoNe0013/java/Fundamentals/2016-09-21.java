f0382f1731588fa66c6a79f8cd527cb768df5bda
BOSSoNe0013 <bossone0013@gmail.com>
2016-09-21 12:15:46 +0100

    2016-09-21's java workout

---
+++ contributions/BOSSoNe0013/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
