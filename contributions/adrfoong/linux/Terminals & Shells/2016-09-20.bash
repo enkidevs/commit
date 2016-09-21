4cd82a3715417f4fb1ae6be76c16a08a4dcf9e7a
adrfoong <eidorian.foong@gmail.com>
2016-09-21 07:16:07 +0100

    2016-09-20's linux workout

---
+++ contributions/adrfoong/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Terminal based browser
+Useful aliasing in bash
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
