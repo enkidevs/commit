47cda1dbe35f8c22c59f36594018dd408e73787e
anubis342 <anubis34234@gmail.com>
2016-09-22 00:20:25 +0100

    2016-09-21's linux workout

---
+++ contributions/anubis342/linux/File Management/2016-09-21.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
