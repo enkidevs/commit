c98363e70e7200e8a015fa7d319941b8fca8a3e5
dominikus1993 <dominikus1910@gmail.com>
2016-09-21 15:38:15 +0100

    2016-09-21's javascript workout

---
+++ contributions/dominikus1993/javascript/Core/2016-09-21.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Use `splice()` to remove an item from an array
+Filtering items out of an array
+The difference between null, undefined and NaN
+Getting the *real* dimensions of an image
