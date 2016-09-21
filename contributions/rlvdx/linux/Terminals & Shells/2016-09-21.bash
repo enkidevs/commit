4737a97c005cd50572c23e186408cd524f93dd70
rlvdx <rlvdxslate@gmail.com>
2016-09-21 17:54:21 +0100

    2016-09-21's linux workout

---
+++ contributions/rlvdx/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Clear the terminal instantly
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
