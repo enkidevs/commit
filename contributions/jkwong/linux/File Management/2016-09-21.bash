14e42707b6707307ecfd14f94a7b1f367f4dc918
jkwong <jeffrey.kwong@gmail.com>
2016-09-21 16:30:03 +0100

    2016-09-21's linux workout

---
+++ contributions/jkwong/linux/File Management/2016-09-21.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
