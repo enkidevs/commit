ef74dd2036e8d9fe3a6193b600b3f542d985fa9a
PembertonRJ <rj.pemberton@apx-labs.com>
2016-09-21 16:34:21 +0100

    2016-09-21's linux workout

---
+++ contributions/PembertonRJ/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Useful aliasing in bash
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
