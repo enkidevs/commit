34b8b82a12c7bb35c31ad83313a2b0d8be72f066
shivdrgn <tdice.noob@gmail.com>
2016-09-21 11:28:14 +0100

    2016-09-21's linux workout

---
+++ contributions/shivdrgn/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Find any Unix / Linux command
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
