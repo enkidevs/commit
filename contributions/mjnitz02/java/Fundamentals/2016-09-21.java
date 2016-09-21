5e56f4196df72f6fb841518e8c0754e00f5a3e0d
mjnitz02 <mjnitz02@gmail.com>
2016-09-21 12:22:59 +0100

    2016-09-21's java workout

---
+++ contributions/mjnitz02/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
