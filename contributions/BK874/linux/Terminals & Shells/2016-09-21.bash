02c948bcca9166feefbe020e62e476925a73190c
BK874 <bjk69@pitt.edu>
2016-09-21 13:52:17 +0100

    2016-09-21's linux workout

---
+++ contributions/BK874/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,7 @@
+Terminal based browser
+Useful aliasing in bash
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
