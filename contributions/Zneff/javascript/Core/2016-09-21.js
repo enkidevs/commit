4fc5d21b79d6b768978c06b4f7f02969856b14c6
Zneff <zneff@mail.ru>
2016-09-21 10:43:12 +0100

    2016-09-21's javascript workout

---
+++ contributions/Zneff/javascript/Core/2016-09-21.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Data type comparison in `switch` statements
+Truthiness
+Hoisting applies only to variable declarations, not initializations
+Getting the *real* dimensions of an image
