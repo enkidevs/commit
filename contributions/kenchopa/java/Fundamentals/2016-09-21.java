cd63100a5e47b1f84692970a761ff137949ed836
kenchopa <webdesign.kenchopa@gmail.com>
2016-09-21 23:12:24 +0100

    2016-09-21's java workout

---
+++ contributions/kenchopa/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Always specify type parameter
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
