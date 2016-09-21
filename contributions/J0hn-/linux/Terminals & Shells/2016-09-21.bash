ac92087a72e3fb59cb215df3a5f60d910d9eae97
J0hn- <jonathan.platteau@yahoo.fr>
2016-09-21 10:17:02 +0100

    2016-09-21's linux workout

---
+++ contributions/J0hn-/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Useful aliasing in bash
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
