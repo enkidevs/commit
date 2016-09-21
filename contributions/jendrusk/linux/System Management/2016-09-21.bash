83b8de13e4bf1cd7ebb99ae6b9d5e88571d3929f
jendrusk <andrzej@abakus.net.pl>
2016-09-21 14:50:27 +0100

    2016-09-21's linux workout

---
+++ contributions/jendrusk/linux/System Management/2016-09-21.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
