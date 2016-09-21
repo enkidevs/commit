c6b4dc97b968f5d9e8ae540c684b79bb27627b41
lkh4768 <lkh5510@gmail.com>
2016-09-21 23:46:57 +0100

    2016-09-21's linux workout

---
+++ contributions/lkh4768/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
