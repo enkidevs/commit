edb75ebbfb50b7b619d782a427e0e4178ccba82d
zymtom <lolipopzere@gmail.com>
2016-09-21 10:05:28 +0100

    2016-09-21's linux workout

---
+++ contributions/zymtom/linux/System Management/2016-09-21.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
