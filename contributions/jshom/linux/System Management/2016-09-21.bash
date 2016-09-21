0e3252220aef94d4de3a2f5c1f1629bd897a8beb
jshom <jacob@shomstein.com>
2016-09-21 06:36:31 +0100

    2016-09-21's linux workout

---
+++ contributions/jshom/linux/System Management/2016-09-21.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
