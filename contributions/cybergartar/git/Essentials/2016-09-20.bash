620d6811bab5be357fcf01cf635951963347baee
cybergartar <kamonnop.a@gmail.com>
2016-09-21 03:09:59 +0100

    2016-09-20's git workout

---
+++ contributions/cybergartar/git/Essentials/2016-09-20.bash
@@ -0,0 +1,5 @@
+How to set your username and email
+Limiting log output by time
+Search by commit message keyword
+Use `short` status to make output more compact
+Remote repositories: fetching and pushing
