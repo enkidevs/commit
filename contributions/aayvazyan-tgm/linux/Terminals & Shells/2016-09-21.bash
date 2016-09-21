60f7301d9cf381ef48212b342d7906bcd6aa3ade
aayvazyan-tgm <ariay@live.at>
2016-09-21 07:16:06 +0100

    2016-09-21's linux workout

---
+++ contributions/aayvazyan-tgm/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Repeating commands with `watch`
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Conditional command execution
+
+(`&&` operator)
