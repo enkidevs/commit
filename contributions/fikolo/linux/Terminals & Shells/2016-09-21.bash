97308df5758b03202d8d0adfad485e42c0065251
fikolo <filipdalorenzo@gmail.com>
2016-09-21 11:04:32 +0100

    2016-09-21's linux workout

---
+++ contributions/fikolo/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Keep useful commands in your shell history with tags
