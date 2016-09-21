9efe9d24e8553417bfc03093bab6de16f76023e2
Grzegorz Ka <g.komajda@gmail.com>
2016-09-21 06:28:46 +0100

    2016-09-21's linux workout

---
+++ contributions/Grzegorz Ka/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Terminal based browser
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
