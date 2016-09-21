9439d7f61c7204f7ca2b7adddb81678b4f484014
DxDial0g <drestrepohinc@gmail.com>
2016-09-21 20:49:21 +0100

    2016-09-21's linux workout

---
+++ contributions/DxDial0g/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Terminal based browser
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
