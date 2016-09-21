c19db6772defe6190c6cffa41465d1d2b83c92ba
SamDudley <dudley.co.uk@gmail.com>
2016-09-21 01:57:53 +0100

    2016-09-20's javascript workout

---
+++ contributions/SamDudley/javascript/Core/2016-09-20.js
@@ -0,0 +1,5 @@
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Scope and strict mode
+Extra function arguments are undefined by default
+Prevent background image flicker in **IE**
