3c78cfe06671b8c9756025970d3e3e60a5bcf84b
TheMicroGirl <e.grabowiecka@outlook.com>
2016-09-21 23:12:08 +0100

    2016-09-21's java workout

---
+++ contributions/TheMicroGirl/java/I/O/2016-09-21.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to create directories in Java
+Create system file paths using the `Path` class
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
