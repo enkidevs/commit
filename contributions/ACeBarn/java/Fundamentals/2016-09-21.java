a8314a4399a5a2b7df8408a9a1e1c5441ea13a04
ACeBarn <alessio.bisgen@gmail.com>
2016-09-21 11:12:40 +0100

    2016-09-21's java workout

---
+++ contributions/ACeBarn/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
+Using an interface as a parameter
