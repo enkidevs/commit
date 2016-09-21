4d2b7af411f5450f091a62c5458c156f2df696f6
ellisandy <andrew@andrewryanellis.com>
2016-09-21 16:46:39 +0100

    2016-09-21's linux workout

---
+++ contributions/ellisandy/linux/System Management/2016-09-21.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Basic service management with `systemd`
