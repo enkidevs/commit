1d0acf173dcbcf25dd188a9c50e8f3afc42957a6
doronda <romadoronda@gmail.com>
2016-09-21 23:07:26 +0100

    2016-09-21's java workout

---
+++ contributions/doronda/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Measuring time
+Limit Accessibility of `Fields`
+Using an interface as a parameter
