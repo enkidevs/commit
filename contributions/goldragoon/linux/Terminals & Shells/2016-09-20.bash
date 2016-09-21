87b29b0bb79908ca073648ec4025996a2897e936
goldragoon <paganinist@gmail.com>
2016-09-21 18:53:42 +0100

    2016-09-20's linux workout

---
+++ contributions/goldragoon/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
