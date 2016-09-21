282d666865654e1fb2d7ef3cf2867d06347352fd
MinusKube <minuskube@gmail.com>
2016-09-21 11:08:30 +0100

    2016-09-21's linux workout

---
+++ contributions/MinusKube/linux/System Management/2016-09-21.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
