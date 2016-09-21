10f98aaa441aa690d5ae444b10a2db729c1ff2a5
Sanyo <micmark6@gmail.com>
2016-09-21 15:25:41 +0100

    2016-09-21's linux workout

---
+++ contributions/Sanyo/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
