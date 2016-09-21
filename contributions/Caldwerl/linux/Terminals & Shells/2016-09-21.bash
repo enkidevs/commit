cd3eff8956c01d77ca8e13addf9b463c913f6e52
Caldwerl <caldwerl@gmail.com>
2016-09-21 22:56:17 +0100

    2016-09-21's linux workout

---
+++ contributions/Caldwerl/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
