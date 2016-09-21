e1f83fb9132376b386760d05d902a5d79f68cea6
Mayhem50 <regnier.ben@gmail.com>
2016-09-21 10:08:30 +0100

    2016-09-21's javascript workout

---
+++ contributions/Mayhem50/javascript/Core/2016-09-21.js
@@ -0,0 +1,5 @@
+Treating a boolean as number
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Apply `map` function to array items
+Closures
