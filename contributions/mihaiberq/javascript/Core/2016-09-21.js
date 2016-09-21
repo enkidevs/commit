38bdbf9fbf2aec6e071ed2522dffb4ddd19b2700
mihaiberq <mihaiberq@gmail.com>
2016-09-21 14:24:04 +0100

    2016-09-21's javascript workout

---
+++ contributions/mihaiberq/javascript/Core/2016-09-21.js
@@ -0,0 +1,5 @@
+`hasOwnProperty` method
+Labeling your loops
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Getting the *real* dimensions of an image
