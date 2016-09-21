0777642e59bc99c211e1363074fbe314c5053c5b
aleksander.sciubisz <aleksander.sciubisz@gmail.com>
2016-09-21 11:17:15 +0100

    2016-09-21's javascript workout

---
+++ contributions/aleksander.sciubisz/javascript/Core/2016-09-21.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Types of numbers
+Precision
+Most efficient way to build `HTML` strings
+Hoisting applies only to variable declarations, not initializations
