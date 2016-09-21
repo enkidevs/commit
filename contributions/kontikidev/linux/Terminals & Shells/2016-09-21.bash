4cd1404e943545de75879fa2375d0391d199971b
kontikidev <dominic.seifert.dev@gmail.com>
2016-09-21 06:10:52 +0100

    2016-09-21's linux workout

---
+++ contributions/kontikidev/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Random password generator
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
