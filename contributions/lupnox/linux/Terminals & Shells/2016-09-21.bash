98454d94ed9bcf9a2d94f4e97447912eeb0147f3
lupnox <lupnox@gmail.com>
2016-09-21 22:10:50 +0100

    2016-09-21's linux workout

---
+++ contributions/lupnox/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Find any Unix / Linux command
+Random password generator
+Keep useful commands in your shell history with tags
