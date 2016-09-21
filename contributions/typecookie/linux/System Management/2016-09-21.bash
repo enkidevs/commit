41cad4cfbcbbc8fcb6ea54679604d8b9228b1046
typecookie <typecookie@gmail.com>
2016-09-21 14:28:38 +0100

    2016-09-21's linux workout

---
+++ contributions/typecookie/linux/System Management/2016-09-21.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
