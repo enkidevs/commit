4c20ab788c2f74c58e2b2a0057d28bf214c486d4
nuxero <inge.zelaya@gmail.com>
2016-09-21 13:42:24 +0100

    2016-09-21's java workout

---
+++ contributions/nuxero/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
