ff60347d129469b34d3d0f53cdff5fe7a3710393
nedel <nedel@gmx.com>
2016-09-21 05:45:32 +0100

    2016-09-21's linux workout

---
+++ contributions/nedel/linux/System Management/2016-09-21.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Cancel a system shutdown
