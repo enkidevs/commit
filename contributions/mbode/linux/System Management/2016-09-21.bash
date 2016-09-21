fc8fbf5e157f0748f0ad62435e62a8c9e04329fe
mbode <maxbode@gmail.com>
2016-09-21 08:42:49 +0100

    2016-09-21's linux workout

---
+++ contributions/mbode/linux/System Management/2016-09-21.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
