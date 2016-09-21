5c9aed685acec9a9a8a57cb6c20f245d11732b04
walberth <w.felipe.gutierrez@gmail.com>
2016-09-21 05:08:23 +0100

    2016-09-20's java workout

---
+++ contributions/walberth/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
