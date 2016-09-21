f269ff898ac949c4c501db76375dff4d19a45fe6
andikn <andrea.poll93@gmail.com>
2016-09-21 22:49:50 +0100

    2016-09-21's java workout

---
+++ contributions/andikn/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+Locks in `static synchronized` methods
