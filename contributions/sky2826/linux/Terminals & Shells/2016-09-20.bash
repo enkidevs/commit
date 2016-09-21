fe476aa9ff24c03898643329314c9b0878b0fcd4
sky2826 <aakashtiwari03@gmail.com>
2016-09-21 06:01:08 +0100

    2016-09-20's linux workout

---
+++ contributions/sky2826/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
