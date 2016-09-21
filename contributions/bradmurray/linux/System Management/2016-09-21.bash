4cca4e9a499989764b0c751567baec21c1c99239
bradmurray <brad@bradmurray.com>
2016-09-21 17:48:07 +0100

    2016-09-21's linux workout

---
+++ contributions/bradmurray/linux/System Management/2016-09-21.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Basic service management with `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
