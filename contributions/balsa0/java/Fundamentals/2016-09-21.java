20e40d03d3ddeecbf02316ded1edc6e0cee37cfb
balsa0 <ktibalazs@gmail.com>
2016-09-21 19:58:28 +0100

    2016-09-21's java workout

---
+++ contributions/balsa0/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Supply `toString()` in all classes
+Using an interface as a parameter
