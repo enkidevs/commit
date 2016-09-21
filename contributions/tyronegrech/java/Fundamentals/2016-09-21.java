5c42492f23eaad7d9e9b3eaafd65bc097b0235fb
tyronegrech <grechtyrone1989@gmail.com>
2016-09-21 19:15:14 +0100

    2016-09-21's java workout

---
+++ contributions/tyronegrech/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Measuring time
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
