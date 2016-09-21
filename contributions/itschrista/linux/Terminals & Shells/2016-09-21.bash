6e864bbb7c594a7a34b7cc604b9799f1431d44f1
itschrista <christa-l@hotmail.co.uk>
2016-09-21 11:21:49 +0100

    2016-09-21's linux workout

---
+++ contributions/itschrista/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
