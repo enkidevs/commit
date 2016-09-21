90dc4aed11452fa8b96c045620ec36bbb52464b6
amyleesada <gabyessence@gmail.com>
2016-09-21 06:22:50 +0100

    2016-09-20's linux workout

---
+++ contributions/amyleesada/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Commands to shutdown or restart the system
+Finding Open Files With `lsof`
