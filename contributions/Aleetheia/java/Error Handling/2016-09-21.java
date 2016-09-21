a66d7b144075f5b3720bd31b694ab7372dfd7734
Aleetheia <dounia.saez@gmail.com>
2016-09-21 13:04:19 +0100

    2016-09-21's java workout

---
+++ contributions/Aleetheia/java/Error Handling/2016-09-21.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Prevent `exceptions` while logging Data
+Careful Numeric Data Conversions
