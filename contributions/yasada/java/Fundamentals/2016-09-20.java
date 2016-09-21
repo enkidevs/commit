5200becdc1a87e4cd0843d4499e9263fae954ec2
yasada <y.asada216@gmail.com>
2016-09-21 12:02:54 +0100

    2016-09-20's java workout

---
+++ contributions/yasada/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
