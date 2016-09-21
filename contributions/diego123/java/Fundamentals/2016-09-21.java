27c3d7829fb2648b6123e45906895877ed61b51c
diego123 <bartosz.szymanski1@gmail.com>
2016-09-21 23:00:03 +0100

    2016-09-21's java workout

---
+++ contributions/diego123/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
+Using an interface as a parameter
