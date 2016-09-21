f0f11db5a6f7a31dd57697e4d992a355c2fba08e
Landrash <landrash@gmail.com>
2016-09-21 08:33:50 +0100

    2016-09-21's linux workout

---
+++ contributions/Landrash/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
