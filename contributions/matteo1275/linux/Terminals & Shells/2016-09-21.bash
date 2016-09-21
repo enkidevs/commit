69bce89673fc974ae4fd6ad738bea11f5f48f365
matteo1275 <matteofumagalli1275@gmail.com>
2016-09-21 13:09:52 +0100

    2016-09-21's linux workout

---
+++ contributions/matteo1275/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
