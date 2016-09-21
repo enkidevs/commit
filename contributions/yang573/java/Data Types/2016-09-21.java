06b252b265c136d3305ae721f4c04afa01b3d28b
yang573 <yangyang1998@gmail.com>
2016-09-21 14:57:32 +0100

    2016-09-21's java workout

---
+++ contributions/yang573/java/Data Types/2016-09-21.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Converting a string to upper or lower case
+Do not attempt comparisons with NaN
