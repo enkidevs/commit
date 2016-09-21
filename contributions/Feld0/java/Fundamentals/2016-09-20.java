dc137f552b6cfe1a824d9caccd6c2dd48e8af569
Feld0 <enki@peter.deltchev.com>
2016-09-21 01:43:49 +0100

    2016-09-20's java workout

---
+++ contributions/Feld0/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Measuring time
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
