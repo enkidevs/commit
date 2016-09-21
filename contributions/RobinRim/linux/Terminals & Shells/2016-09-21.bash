b06ce052540f7c27f09bfe5db6265d3cb8110fb1
RobinRim <contact@robnono.fr>
2016-09-21 09:26:10 +0100

    2016-09-21's linux workout

---
+++ contributions/RobinRim/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Terminal based browser
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
