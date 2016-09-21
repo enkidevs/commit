547cfebd00035ee60b968967ebb80ed2bd98e1f7
colinular <colinular@gmail.com>
2016-09-21 13:24:44 +0100

    2016-09-20's linux workout

---
+++ contributions/colinular/linux/System Management/2016-09-20.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
