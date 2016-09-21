4e7a5d2c63ab5477fa117fbd5e5985cd970bbdcc
pldiiw <pldiiw@gmail.com>
2016-09-21 19:28:37 +0100

    2016-09-21's git workout

---
+++ contributions/pldiiw/git/Essentials/2016-09-21.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+Pushing tags to a server
+How to set your username and email
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
