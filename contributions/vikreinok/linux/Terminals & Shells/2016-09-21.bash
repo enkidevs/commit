24c6f491e18df12903b37e11d2352b6be3e22584
vikreinok <vikreinok@gmail.com>
2016-09-21 14:30:59 +0100

    2016-09-21's linux workout

---
+++ contributions/vikreinok/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
