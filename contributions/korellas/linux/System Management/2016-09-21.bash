4264952a6b744d2bedc3bc607e41a2a1b804bcb2
korellas <korellas@gmail.com>
2016-09-22 00:03:53 +0100

    2016-09-21's linux workout

---
+++ contributions/korellas/linux/System Management/2016-09-21.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
