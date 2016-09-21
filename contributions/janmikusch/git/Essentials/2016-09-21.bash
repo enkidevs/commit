2e68989d892d5bd5c29b96a95d506047f43db08a
janmikusch <jan.mikusch@gmail.com>
2016-09-21 13:46:57 +0100

    2016-09-21's git workout

---
+++ contributions/janmikusch/git/Essentials/2016-09-21.bash
@@ -0,0 +1,5 @@
+Adding a remote repository
+You can use git offline!
+Check the status of your files
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
