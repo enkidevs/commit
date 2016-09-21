abe1932666532360e416c6b0d1967de93ed9171c
shortsamshort <shortsamshort@gmail.com>
2016-09-21 14:37:29 +0100

    2016-09-21's linux workout

---
+++ contributions/shortsamshort/linux/System Management/2016-09-21.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
