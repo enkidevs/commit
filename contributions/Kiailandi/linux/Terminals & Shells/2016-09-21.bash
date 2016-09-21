e586e1ee532279f4ceeb84043bc9b394bc910e2b
Kiailandi <jock3r@hotmail.it>
2016-09-21 12:41:15 +0100

    2016-09-21's linux workout

---
+++ contributions/Kiailandi/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Clear the terminal instantly
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
