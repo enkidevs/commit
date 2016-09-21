373c46e230ac7e7eb497f08dd2258169ea69beaf
toshkuuu <toshko.grigorov95@gmail.com>
2016-09-21 19:47:25 +0100

    2016-09-21's java workout

---
+++ contributions/toshkuuu/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+Calling one constructor from another
+Locks in `static synchronized` methods
