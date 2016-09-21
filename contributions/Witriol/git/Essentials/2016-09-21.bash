89dcd04e8a0b0a24285bbfedb373e4a72735e040
Witriol <tomas.simicek@outlook.com>
2016-09-21 09:22:44 +0100

    2016-09-21's git workout

---
+++ contributions/Witriol/git/Essentials/2016-09-21.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+Using aliases for git commands
+Search by commit message keyword
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
