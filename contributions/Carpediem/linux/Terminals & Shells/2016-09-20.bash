6eca08577b14627ed8153735396816062f7e3b8c
Carpediem <1185329223@qq.com>
2016-09-21 13:42:17 +0100

    2016-09-20's linux workout

---
+++ contributions/Carpediem/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
