3280fd0a7b444ae8fed2267dd5258291745aff17
SoNiKBooM <sonik.td@gmail.com>
2016-09-21 20:48:06 +0100

    2016-09-21's javascript workout

---
+++ contributions/SoNiKBooM/javascript/Core/2016-09-21.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Hoisting applies only to variable declarations, not initializations
+Extra function arguments are undefined by default
+Detect online connection
+`console.*` in JavaScript
