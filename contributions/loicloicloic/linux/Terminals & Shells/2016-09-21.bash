74f23aa56c994bad57470caff62774b54963aaa7
loicloicloic <loic.car@hotmail.fr>
2016-09-21 14:40:41 +0100

    2016-09-21's linux workout

---
+++ contributions/loicloicloic/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Quick `bash` shortcuts 
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
