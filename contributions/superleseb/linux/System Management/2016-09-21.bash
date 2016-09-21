b6473e24c88028562301cafa1efb1e1fa28b8e4e
superleseb <superleseb@neuf.fr>
2016-09-21 23:37:37 +0100

    2016-09-21's linux workout

---
+++ contributions/superleseb/linux/System Management/2016-09-21.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Get hardware stack details with `lspci`
