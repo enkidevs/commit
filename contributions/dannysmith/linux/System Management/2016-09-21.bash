b5e3ead6aa218a47b1221e86092acf94cff2a9ca
dannysmith <hi@danny.is>
2016-09-21 21:58:10 +0100

    2016-09-21's linux workout

---
+++ contributions/dannysmith/linux/System Management/2016-09-21.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
