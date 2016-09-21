bcc4d64cd953c62591f5d10c1c68e63dfa4becfe
Asdamp <antonio.danilo92@gmail.com>
2016-09-21 12:39:21 +0100

    2016-09-21's git workout

---
+++ contributions/Asdamp/git/Essentials/2016-09-21.bash
@@ -0,0 +1,5 @@
+How to set your username and email
+Search by commit message keyword
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
+Perform a dry run
