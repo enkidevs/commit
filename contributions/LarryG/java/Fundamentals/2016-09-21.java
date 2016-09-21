414361025cff90268a9339bf923e0e29471c31aa
LarryG <laurynasgiriunas@gmail.com>
2016-09-21 22:46:06 +0100

    2016-09-21's java workout

---
+++ contributions/LarryG/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Using bounded type parameters in generic methods
+Measuring time
+Using `synchronized` statements
+Using an interface as a parameter
