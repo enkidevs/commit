1438ec66b0f7d1cabef5d4cdcda109abc08cb93f
koliada <alex.fiator@gmail.com>
2016-09-21 23:05:35 +0100

    2016-09-21's java workout

---
+++ contributions/koliada/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Measuring time
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Using an interface as a parameter
