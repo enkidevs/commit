dbfe0e7fe5b484d7c33d4185da73556682bb8110
olman74 <ray.alexandre74@gmail.com>
2016-09-21 20:05:06 +0100

    2016-09-21's java workout

---
+++ contributions/olman74/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Inheriting a constructor from a superclass
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
+Using an interface as a parameter
