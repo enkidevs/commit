911c4f669d492686297fb441635f5799ce20b393
NoName64Bits <ciobanulaur@gmail.com>
2016-09-21 23:55:48 +0100

    2016-09-21's java workout

---
+++ contributions/NoName64Bits/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
