28b55bc4fbd2b934f7858199c26391722a255622
euwbah <euwbah@gmail.com>
2016-09-21 10:50:58 +0100

    2016-09-20's java workout

---
+++ contributions/euwbah/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,4 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
