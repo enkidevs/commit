9c578311aba38eb050638b4756cf71ddbd55f763
Psilo <kwoodul1990@gmail.com>
2016-09-21 22:48:59 +0100

    2016-09-21's linux workout

---
+++ contributions/Psilo/linux/System Management/2016-09-21.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
