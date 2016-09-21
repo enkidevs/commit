c8b9b0eb58133baf7615f578560f468719f004ca
_tomm7 <srcthomas@gmail.com>
2016-09-21 04:23:25 +0100

    2016-09-20's linux workout

---
+++ contributions/_tomm7/linux/Terminals & Shells/2016-09-20.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
