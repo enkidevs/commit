dcd032920f95315051f9c07af06f81a1cfbe7b66
mrewers <marekrewers@gmail.com>
2016-09-21 15:20:59 +0100

    2016-09-21's git workout

---
+++ contributions/mrewers/git/Essentials/2016-09-21.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+What is stored in a commit?
+Make your log output pretty
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
