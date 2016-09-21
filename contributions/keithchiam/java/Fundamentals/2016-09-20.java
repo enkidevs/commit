a225fca20340bb31b26ab4b330da1321afc2fbb2
keithchiam <keithchiam1@gmail.com>
2016-09-21 01:41:02 +0100

    2016-09-20's java workout

---
+++ contributions/keithchiam/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Using an interface as a parameter
