b25bd24bb3e65a0acb9d511e3bc6a7e89e87bfdb
SergeiOFF <sergeioff57@gmail.com>
2016-09-21 10:08:54 +0100

    2016-09-21's linux workout

---
+++ contributions/SergeiOFF/linux/System Management/2016-09-21.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
