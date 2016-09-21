68536f5b9a308f958d1310fbc08b39527a3bda4c
Vira <thomas.ruehrig@studierende.htl-donaustadt.at>
2016-09-21 05:52:40 +0100

    2016-09-21's java workout

---
+++ contributions/Vira/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
