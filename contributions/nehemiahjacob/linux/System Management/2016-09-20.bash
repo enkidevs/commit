b068654cb1fc9159f9271c27ef42b035cda4b817
nehemiahjacob <nehemiah.jacob@gmail.com>
2016-09-21 07:01:44 +0100

    2016-09-20's linux workout

---
+++ contributions/nehemiahjacob/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Executing scripts on system boot using `systemd`
+Finding Open Files With `lsof`
