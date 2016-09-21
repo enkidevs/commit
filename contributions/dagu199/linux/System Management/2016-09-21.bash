19bede8a2124851d267baf1c826930dfbd3ef137
dagu199 <d_garciau@hotmail.com>
2016-09-21 16:51:13 +0100

    2016-09-21's linux workout

---
+++ contributions/dagu199/linux/System Management/2016-09-21.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Find the `MAC` address of all network interfaces
+
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
