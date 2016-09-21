7f19e04bbd396287416f5d2e6002368584805c63
hyftar <simon_landry@ymail.com>
2016-09-21 21:07:23 +0100

    2016-09-21's linux workout

---
+++ contributions/hyftar/linux/System Management/2016-09-21.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Basic service management with `systemd`
+Commands to shutdown or restart the system
