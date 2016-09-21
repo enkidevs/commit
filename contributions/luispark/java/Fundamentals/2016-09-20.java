29961100213fad8b05f53a51fb293de0ac8903db
luispark <luis.gc.park@gmail.com>
2016-09-21 01:29:57 +0100

    2016-09-20's java workout

---
+++ contributions/luispark/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
