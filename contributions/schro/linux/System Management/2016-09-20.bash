ffffc00f938a3ef8bc8c05e5e8394fb2c9e5cb9b
schro <schro.htc@gmail.com>
2016-09-21 05:56:57 +0100

    2016-09-20's linux workout

---
+++ contributions/schro/linux/System Management/2016-09-20.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
