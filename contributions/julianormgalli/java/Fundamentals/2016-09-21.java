5e9c314c998c7c3f7a92691f56b93f7f6503f162
julianormgalli <julianormgalli@gmail.com>
2016-09-22 00:11:54 +0100

    2016-09-21's java workout

---
+++ contributions/julianormgalli/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Measuring time
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
