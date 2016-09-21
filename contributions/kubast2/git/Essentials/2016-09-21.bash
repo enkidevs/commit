92e8e42c327028ff2a0e0b36775dd3f9879b97e5
kubast2 <kubast2@gmail.com>
2016-09-21 10:47:42 +0100

    2016-09-21's git workout

---
+++ contributions/kubast2/git/Essentials/2016-09-21.bash
@@ -0,0 +1,5 @@
+Using aliases for git commands
+What is stored in a commit?
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
