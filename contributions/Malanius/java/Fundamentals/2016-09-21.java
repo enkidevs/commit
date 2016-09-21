f9b700cf5a41dfc756dbfa76a7e0ffc4edf461ac
Malanius <malanius@seznam.cz>
2016-09-21 08:14:35 +0100

    2016-09-21's java workout

---
+++ contributions/Malanius/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Using an interface as a parameter
