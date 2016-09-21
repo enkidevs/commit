0e3f0113f6a9f3afe091f950da77e615cfbb580f
bbialek <bialcus@gmail.com>
2016-09-21 12:20:33 +0100

    2016-09-21's linux workout

---
+++ contributions/bbialek/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
