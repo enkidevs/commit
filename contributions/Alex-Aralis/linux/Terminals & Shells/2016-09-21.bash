3d21b7b39cedc740932b41e2867b174caf33ed42
Alex-Aralis <alex.aralis@gmail.com>
2016-09-21 14:16:55 +0100

    2016-09-21's linux workout

---
+++ contributions/Alex-Aralis/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Shebang `#!` explained 
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Keep useful commands in your shell history with tags
