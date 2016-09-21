4ff39198868754f40cc6397ce0a77f3bd736de93
pedperpat <pedro-aperez1@hotmail.com>
2016-09-21 14:56:40 +0100

    2016-09-21's linux workout

---
+++ contributions/pedperpat/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
