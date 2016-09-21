03d2d30964c4e465830e635d8618069611eadd6a
Emrehan1995 <emrehanfirtin@hotmail.de>
2016-09-21 06:36:32 +0100

    2016-09-21's linux workout

---
+++ contributions/Emrehan1995/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Quick `bash` shortcuts 
+Terminal based browser
+Adding directories to your `$PATH`
