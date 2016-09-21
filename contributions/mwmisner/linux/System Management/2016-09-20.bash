2fccd9dc38496171baddfeb42805e87055f022ed
mwmisner <mwmisner@gmail.com>
2016-09-21 02:54:11 +0100

    2016-09-20's linux workout

---
+++ contributions/mwmisner/linux/System Management/2016-09-20.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
