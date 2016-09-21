2056c768757182c7f2c198c0e095a4a9f56c42e7
Tipertuba <tipertuba@gmail.com>
2016-09-21 13:05:52 +0100

    2016-09-21's linux workout

---
+++ contributions/Tipertuba/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Terminal based browser
+Sequential execution using the `;` statement separator
