bd7c2029df54e6dab332842a142b8f109a2d4ba0
Kishore <kishorepremier@gmail.com>
2016-09-21 02:40:21 +0100

    2016-09-20's java workout

---
+++ contributions/Kishore/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
