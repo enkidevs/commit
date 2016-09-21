8ac73930671a9c6e2ae93a76d66261bf38cf93d5
eddimas <eddimas@gmail.com>
2016-09-21 23:16:17 +0100

    2016-09-21's java workout

---
+++ contributions/eddimas/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
