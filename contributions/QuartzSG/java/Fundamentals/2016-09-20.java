b62d689860f67d62c2b0c1477dd6b89745db98d2
QuartzSG <adrianquartx@gmail.com>
2016-09-21 09:30:49 +0100

    2016-09-20's java workout

---
+++ contributions/QuartzSG/java/Fundamentals/2016-09-20.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Inheriting a constructor from a superclass
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
