2874b1b84788d26b9b040c0c47f95d60e6dae226
jagi_babu <alek.golaszewski@gmail.com>
2016-09-21 15:51:24 +0100

    2016-09-21's java workout

---
+++ contributions/jagi_babu/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Measuring time
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
