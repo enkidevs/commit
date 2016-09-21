f3873d29d3c392965379aed590e21d7f74f451cf
thomcuddihy <thom@hometreelab.com>
2016-09-21 19:27:26 +0100

    2016-09-21's java workout

---
+++ contributions/thomcuddihy/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Measuring time
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
