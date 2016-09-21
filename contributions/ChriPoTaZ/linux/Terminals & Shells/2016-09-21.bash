9ae9d69be8f65833dc78c36a897094ff3fe0cedf
ChriPoTaZ <chripotaz@gmail.com>
2016-09-21 17:54:08 +0100

    2016-09-21's linux workout

---
+++ contributions/ChriPoTaZ/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Find any Unix / Linux command
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
