23fdc8c18890265d174ef5f77e10424b023dc86b
josecrmrz <josecrmrz@gmail.com>
2016-09-21 04:06:20 +0100

    2016-09-20's linux workout

---
+++ contributions/josecrmrz/linux/Networking/2016-09-20.bash
@@ -0,0 +1,5 @@
+Logging dropped firewall packets
+Useful ssh client optimizations 
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
+Make use of `netstat`
