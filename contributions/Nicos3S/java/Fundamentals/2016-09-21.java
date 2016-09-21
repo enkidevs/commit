ad211d2d940198384fc6b7ca47366d8ead611db0
Nicos3S <boeckh.nicolas@gmail.com>
2016-09-21 13:08:02 +0100

    2016-09-21's java workout

---
+++ contributions/Nicos3S/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
+Using an interface as a parameter
