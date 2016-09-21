9de6fd417785b0bdf3caf617f046a710e65c3332
fcafra <fcafra@gmail.com>
2016-09-21 19:29:50 +0100

    2016-09-21's linux workout

---
+++ contributions/fcafra/linux/System Management/2016-09-21.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
