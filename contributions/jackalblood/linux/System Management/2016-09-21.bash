8fcb4f04477be50cb6f4aa388f53982756b395e1
jackalblood <joemannix8@gmail.com>
2016-09-21 15:14:16 +0100

    2016-09-21's linux workout

---
+++ contributions/jackalblood/linux/System Management/2016-09-21.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
