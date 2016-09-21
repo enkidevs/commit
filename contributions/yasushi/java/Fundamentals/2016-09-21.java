9cbc8bd5f63568fd6c2ec2b913729475db155db4
yasushi <yasushi.ohtsuka@gmail.com>
2016-09-22 00:09:06 +0100

    2016-09-21's java workout

---
+++ contributions/yasushi/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Java naming conventions
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Implementing an `interface`
