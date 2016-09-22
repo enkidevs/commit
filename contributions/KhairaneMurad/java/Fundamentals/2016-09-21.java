ff00f6486f1d2588b5c1592047e10755ebd8103b
KhairaneMurad <khairane.mourad@gmail.com>
2016-09-22 02:32:03 +0100

    2016-09-21's java workout

---
+++ contributions/KhairaneMurad/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
