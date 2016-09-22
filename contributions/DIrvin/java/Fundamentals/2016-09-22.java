371af82c28c79ab9721871ca89664e7665af0399
DIrvin <drk.irvin90@gmail.com>
2016-09-22 22:15:13 +0100

    2016-09-22's java workout

---
+++ contributions/DIrvin/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
