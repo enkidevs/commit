b8c9fbe7d114f412e8c7e8eb1329be6cda8f08f6
Contumil <mjaimeog@gmail.com>
2016-09-22 22:28:49 +0100

    2016-09-22's java workout

---
+++ contributions/Contumil/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Measuring time
+Using `synchronized` statements
