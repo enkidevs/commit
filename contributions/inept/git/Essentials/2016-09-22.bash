381fa363dcd9ffce6a7e7180fb47c496a85f28ba
inept <tim.holmes.mitra@gmail.com>
2016-09-22 21:22:20 +0100

    2016-09-22's git workout

---
+++ contributions/inept/git/Essentials/2016-09-22.bash
@@ -0,0 +1,5 @@
+Using aliases for git commands
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Dates in git
