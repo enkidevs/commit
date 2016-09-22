8ea6c16dd34951a59ad6f1d8af4aa788b7bcd02c
jackuss <jackuss@gmail.com>
2016-09-22 20:24:08 +0100

    2016-09-22's javascript workout

---
+++ contributions/jackuss/javascript/Core/2016-09-22.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Data type comparison in `switch` statements
+The difference between null, undefined and NaN
+Prevent background image flicker in **IE**
+Detect **DO NOT TRACK** status
