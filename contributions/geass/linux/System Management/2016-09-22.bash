51b8083325c5fd84f3d29cc7d97217d7dae70c80
geass <muhammet.arsln@gmail.com>
2016-09-22 18:59:04 +0100

    2016-09-22's linux workout

---
+++ contributions/geass/linux/System Management/2016-09-22.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Fixing `locale` issues in Debian systems
