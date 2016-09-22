cabff94aaae07d9cfb12e7b90db2b1574fe820e3
machinetalk <andy.rudnitsky@gmail.com>
2016-09-22 20:09:04 +0100

    2016-09-22's javascript workout

---
+++ contributions/machinetalk/javascript/Node/2016-09-22.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Global Objects and Environment Variables in **Node**
+First-error callbacks in **Node**
+How to debug Node.js
+Use conventional property names for error handling in Node.js
