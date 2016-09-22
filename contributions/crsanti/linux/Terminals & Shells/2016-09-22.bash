7469900244784e62a9897fa7b091d587df0b2613
crsanti <santi.camargo.rodriguez@gmail.com>
2016-09-22 13:02:37 +0100

    2016-09-22's linux workout

---
+++ contributions/crsanti/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
