22e8b857b8c1027cd175ae38ad687fe861cd6b93
ddagsan <emrefatih47@gmail.com>
2016-09-22 16:11:44 +0100

    2016-09-22's git workout

---
+++ contributions/ddagsan/git/Essentials/2016-09-22.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+Limiting log output by time
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
