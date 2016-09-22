aae36f0c2ebdc1fd57b778d791b645021ff8aa54
garytxo <garytxo@gmail.com>
2016-09-22 06:39:13 +0100

    2016-09-22's javascript workout

---
+++ contributions/garytxo/javascript/Core/2016-09-22.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+Data type comparison in `switch` statements
+Extra function arguments are undefined by default
+Detect an error type
+Round numbers to `N` decimals 
