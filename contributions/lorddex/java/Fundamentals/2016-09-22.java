ef9029b499d31808cfbb10718a9eadda67730919
lorddex <f.apollonio@ldlabs.org>
2016-09-22 07:26:40 +0100

    2016-09-22's java workout

---
+++ contributions/lorddex/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
