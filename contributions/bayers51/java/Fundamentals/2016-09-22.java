0b79f5ed0318f2f7b275c5a980b129daa81e54d5
bayers51 <bayers@tds.net>
2016-09-22 17:13:38 +0100

    2016-09-22's java workout

---
+++ contributions/bayers51/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Metadata: setting a file's owner
