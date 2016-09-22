48c4e57391bfdefdc4b17d0e13e2b8adb53ef76c
nikos <triantafyllou.ni@gmail.com>
2016-09-22 05:20:49 +0100

    2016-09-22's java workout

---
+++ contributions/nikos/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Using an interface as a parameter
