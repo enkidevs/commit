3b288514d54bcb6fc6f4aab93c7f902bc9437cc9
DarkGenius <darkgenius@nextmail.ru>
2016-09-22 05:05:49 +0100

    2016-09-22's linux workout

---
+++ contributions/DarkGenius/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Find any Unix / Linux command
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
