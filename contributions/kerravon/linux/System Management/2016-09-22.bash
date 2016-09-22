8405f9f35f2404a7b105f5f25d52a0817339d16d
kerravon <tom@jynold.com>
2016-09-22 18:57:09 +0100

    2016-09-22's linux workout

---
+++ contributions/kerravon/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
