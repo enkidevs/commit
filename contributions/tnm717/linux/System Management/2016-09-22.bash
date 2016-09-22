b3f6acd9c97450c7a7e73829e69acc1d61d19bc0
tnm717 <thomas.masterson@gmail.com>
2016-09-22 16:50:52 +0100

    2016-09-22's linux workout

---
+++ contributions/tnm717/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Find out if the system's architecture is 32 or 64 bit
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
