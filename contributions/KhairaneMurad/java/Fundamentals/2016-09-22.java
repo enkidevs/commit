65ab4f062f53a0c613ae0e1c3315425385478db1
KhairaneMurad <khairane.mourad@gmail.com>
2016-09-22 19:46:22 +0100

    2016-09-22's java workout

---
+++ contributions/KhairaneMurad/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Measuring time
+Using an interface as a parameter
