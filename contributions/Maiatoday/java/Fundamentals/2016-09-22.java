1b19fde59b3f4a62bd8f5496500a14caa6207616
Maiatoday <maiatoday@gmail.com>
2016-09-22 06:52:35 +0100

    2016-09-22's java workout

---
+++ contributions/Maiatoday/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Using the `Console`
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
