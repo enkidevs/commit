b4a4844b6da444f5f11bc74d5ecf821c79d46aa9
venugv <venugv@gmail.com>
2016-09-22 16:56:30 +0100

    2016-09-22's java workout

---
+++ contributions/venugv/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
