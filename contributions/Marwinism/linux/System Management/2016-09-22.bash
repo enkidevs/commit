7e3547a425f950a1339ac42c7f471899d56c161c
Marwinism <marvin.wiik@gmail.com>
2016-09-22 22:52:18 +0100

    2016-09-22's linux workout

---
+++ contributions/Marwinism/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
