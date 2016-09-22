ad93c2b0890ac8bd3b216d3d58241226fbd28448
presidentarrow <piyush.galphat@gmail.com>
2016-09-22 17:01:56 +0100

    2016-09-21's linux workout

---
+++ contributions/presidentarrow/linux/System Management/2016-09-21.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
