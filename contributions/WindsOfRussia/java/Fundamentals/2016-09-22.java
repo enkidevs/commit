6e54639387fa808b6fa86886b9036444ca1cb4a9
WindsOfRussia <andr920jhckrs@gmail.com>
2016-09-23 00:25:41 +0100

    2016-09-22's java workout

---
+++ contributions/WindsOfRussia/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Measuring time
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
