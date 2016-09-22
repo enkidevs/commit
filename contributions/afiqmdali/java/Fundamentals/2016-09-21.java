e4fd5ab0539964d194a323a81d7236d28af8bca2
afiqmdali <afiqmdali@gmail.com>
2016-09-22 11:24:32 +0100

    2016-09-21's java workout

---
+++ contributions/afiqmdali/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Supply `toString()` in all classes
+Using an interface as a parameter
