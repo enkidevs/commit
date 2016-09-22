342701f40ab2fa82d0994b80e7b54024bd30c5fa
noideas4thisname <noideas4thisname@hotmail.com>
2016-09-22 10:53:24 +0100

    2016-09-21's linux workout

---
+++ contributions/noideas4thisname/linux/System Management/2016-09-21.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Detect your linux distribution
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
