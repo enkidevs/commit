8bd61b5a4df2ae335176fe994a7cecac185650b6
lokifaer <lokifaer@gmail.com>
2016-09-22 22:49:02 +0100

    2016-09-22's java workout

---
+++ contributions/lokifaer/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Using an interface as a parameter
