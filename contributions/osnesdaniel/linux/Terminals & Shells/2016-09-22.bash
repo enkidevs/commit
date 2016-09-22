970264e1c4a85cd30e677ffe4b0203e6884b37ff
osnesdaniel <osnesdaniel@gmail.com>
2016-09-22 18:00:07 +0100

    2016-09-22's linux workout

---
+++ contributions/osnesdaniel/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Useful aliasing in bash
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
