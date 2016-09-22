0edce21017e0cbbf6a4b6b0fd1a7654328022d23
hvdv8 <fx772@hotmail.com>
2016-09-22 22:15:13 +0100

    2016-09-22's linux workout

---
+++ contributions/hvdv8/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
