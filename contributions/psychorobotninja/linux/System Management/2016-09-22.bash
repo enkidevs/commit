5e1dcc6d15d16f509274c4be76fb3e4bc1508620
psychorobotninja <tefuller@gmail.com>
2016-09-22 18:51:57 +0100

    2016-09-22's linux workout

---
+++ contributions/psychorobotninja/linux/System Management/2016-09-22.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
