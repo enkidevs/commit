c5d6a8b903f1a575552dfadf2d33e9cd55478826
nickkoro02 <nickkoro02@gmail.com>
2016-09-22 14:11:09 +0100

    2016-09-22's linux workout

---
+++ contributions/nickkoro02/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
