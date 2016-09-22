36f649ec741ddd13e7cdab9573cf063d5a9f2ad9
Pi143 <1337@gmx-topmail.de>
2016-09-22 23:50:19 +0100

    2016-09-22's linux workout

---
+++ contributions/Pi143/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Aliasing ssh connections
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
