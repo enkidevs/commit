715eef2e7a193523acb5660576392e3af96dcdcf
XotoX1337 <frederic.leist@gmail.com>
2016-09-22 09:31:28 +0100

    2016-09-22's linux workout

---
+++ contributions/XotoX1337/linux/System Management/2016-09-22.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
