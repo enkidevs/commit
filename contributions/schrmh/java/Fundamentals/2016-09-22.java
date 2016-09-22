c7f945a0cef830f219d3b7f4ddb5f0addfa624ac
schrmh <schrmhmobil@gmail.com>
2016-09-22 22:20:31 +0100

    2016-09-22's java workout

---
+++ contributions/schrmh/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Inheriting a constructor from a superclass
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
