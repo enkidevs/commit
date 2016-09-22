35dbeacc1723a2307d83e505ae9135656c07aa60
nud3l <dominik.harz@gmail.com>
2016-09-22 22:14:19 +0100

    2016-09-22's java workout

---
+++ contributions/nud3l/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Using an interface as a parameter
