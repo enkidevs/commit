538c1e5bd7c3e5f42c918c0d80c1c393c3a987a0
jessanhq <jessan@gmail.com>
2016-09-22 16:16:06 +0100

    2016-09-22's java workout

---
+++ contributions/jessanhq/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Measuring time
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
