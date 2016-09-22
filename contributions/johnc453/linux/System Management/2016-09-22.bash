b0925e7ecec794d9e9d0b86ad893a5b71cb29a17
johnc453 <john.clark@cantab.net>
2016-09-22 09:13:44 +0100

    2016-09-22's linux workout

---
+++ contributions/johnc453/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
