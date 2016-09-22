ad9eaf4e87642ce274a015ac1035693bd3a0c8dd
sergiandy <sergiandy54@gmail.com>
2016-09-22 17:33:26 +0100

    2016-09-22's javascript workout

---
+++ contributions/sergiandy/javascript/Core/2016-09-22.js
@@ -0,0 +1,5 @@
+Deleting properties
+Using the double tilde `~~` 
+Most efficient way to build `HTML` strings
+Extra function arguments are undefined by default
+Getting the *real* dimensions of an image
