eb592317747709cef2d04458a6e652b46a1edbde
bhouston <enki@thehouston.space>
2016-09-22 22:15:03 +0100

    2016-09-22's linux workout

---
+++ contributions/bhouston/linux/System Management/2016-09-22.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Preventing a user from logging into the system
+Finding Open Files With `lsof`
