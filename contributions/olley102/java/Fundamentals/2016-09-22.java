0920a5a14decef5a414f4a280ba511c63057ef88
olley102 <olley102@gmail.com>
2016-09-22 21:01:15 +0100

    2016-09-22's java workout

---
+++ contributions/olley102/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Inheriting a constructor from a superclass
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
