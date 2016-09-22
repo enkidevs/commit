9b9dc1f5d66c1ec0ad39b985d67b4446ec99055b
Xoting <alex.palacioslopez@gmail.com>
2016-09-23 00:03:31 +0100

    2016-09-22's linux workout

---
+++ contributions/Xoting/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Common log files
+Changing the timezone on deb based systems
