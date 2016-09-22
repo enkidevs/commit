1bc5e660182352d57de468926a4123b56614c3d6
aldum <spongyabob@gmail.com>
2016-09-22 09:05:46 +0100

    2016-09-22's linux workout

---
+++ contributions/aldum/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
