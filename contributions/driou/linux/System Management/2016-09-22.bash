476fd8f7bfe6d05bd9b0995d39f73fbcb7ab75ac
driou <admin@driou.tech>
2016-09-22 08:23:25 +0100

    2016-09-22's linux workout

---
+++ contributions/driou/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
