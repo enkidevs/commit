d2d6ab43e280690596737e172856f4dc0ec8b276
knopki <korolev.srg@gmail.com>
2016-09-22 17:59:39 +0100

    2016-09-22's linux workout

---
+++ contributions/knopki/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
