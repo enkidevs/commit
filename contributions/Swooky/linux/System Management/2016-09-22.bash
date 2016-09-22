7ee3663bee361579f3b5adcb0f8f477fedb8c718
Swooky <swooky@outlook.com>
2016-09-22 23:12:59 +0100

    2016-09-22's linux workout

---
+++ contributions/Swooky/linux/System Management/2016-09-22.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
