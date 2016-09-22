d414a2ad2b0812059ceb99031198d0e8642490c9
Psynbiotik <psynbiotik@gmail.com>
2016-09-22 03:57:09 +0100

    2016-09-21's git workout

---
+++ contributions/Psynbiotik/git/Essentials/2016-09-21.bash
@@ -0,0 +1,5 @@
+You can use git offline!
+Search by commit message keyword
+Check the status of your files
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
