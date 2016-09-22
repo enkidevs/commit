f316e4c135839563c8d38a049ce596614f33ac75
nodh <c.kollmann@gmail.com>
2016-09-22 11:26:41 +0100

    2016-09-22's linux workout

---
+++ contributions/nodh/linux/System Management/2016-09-22.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
