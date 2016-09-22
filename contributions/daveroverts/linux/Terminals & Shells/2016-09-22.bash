14e3a064316fe8b50eb1368938aa6bd35678396b
daveroverts <me@daveroverts.nl>
2016-09-22 07:24:00 +0100

    2016-09-22's linux workout

---
+++ contributions/daveroverts/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
