e9b04de943cd6e1f1cc52dc1f6e2b94eeb4843cb
Gerovid <gerovid@gmail.com>
2016-09-22 22:07:41 +0100

    2016-09-22's linux workout

---
+++ contributions/Gerovid/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Cancel a system shutdown
