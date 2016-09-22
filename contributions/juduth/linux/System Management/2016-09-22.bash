908d4d28512e0dade82683353e3581d0dcab0fb5
juduth <juduth@gmail.com>
2016-09-22 14:34:34 +0100

    2016-09-22's linux workout

---
+++ contributions/juduth/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
