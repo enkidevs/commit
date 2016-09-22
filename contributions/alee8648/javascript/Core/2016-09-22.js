81875fbb1208aced459368c8c0b7e79de45ae0ca
alee8648 <alee8648@gmail.com>
2016-09-22 08:27:45 +0100

    2016-09-22's javascript workout

---
+++ contributions/alee8648/javascript/Core/2016-09-22.js
@@ -0,0 +1,5 @@
+`hasOwnProperty` method
+Weak vs Strict equality operator
+Hoisting applies only to variable declarations, not initializations
+Detect online connection
+Getting the *real* dimensions of an image
