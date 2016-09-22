86ac40e86753dd403924e2e7d4440933d00c8521
abisz <reinsperger.smn@gmail.com>
2016-09-22 22:00:32 +0100

    2016-09-22's javascript workout

---
+++ contributions/abisz/javascript/React/2016-09-22.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+componentWillReceiveProps Not Triggered After Mounting
+
+`ReactDOM.render` ref
+Custom validations for props
+Shortcut for transferring props
