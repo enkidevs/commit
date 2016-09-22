d0e8b97fbfbcaa9a58845347ee04ca78e522813d
Awlex <wieseralex580@gmail.com>
2016-09-22 19:16:19 +0100

    2016-09-22's java workout

---
+++ contributions/Awlex/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
