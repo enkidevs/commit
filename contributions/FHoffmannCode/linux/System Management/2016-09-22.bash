6a57094b131a10365259d13c75c8b930bb1b8382
FHoffmannCode <f.higuain@gmail.com>
2016-09-22 07:20:25 +0100

    2016-09-22's linux workout

---
+++ contributions/FHoffmannCode/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Detect your linux distribution
+Changing the `/tmp` cleanup frequency
+Finding Open Files With `lsof`
