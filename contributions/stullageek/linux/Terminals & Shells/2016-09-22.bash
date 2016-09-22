9fe41901be9c7cf7f20b903e274771aad8ae54f9
stullageek <akalx44@gmail.com>
2016-09-22 16:14:48 +0100

    2016-09-22's linux workout

---
+++ contributions/stullageek/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Aliasing ssh connections
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
