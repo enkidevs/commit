c76dc9c5186e91489e6465057b8e4b4c6c84f5da
lucaslizama <lucaslizama3@hotmail.com>
2016-09-22 05:06:37 +0100

    2016-09-21's linux workout

---
+++ contributions/lucaslizama/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
