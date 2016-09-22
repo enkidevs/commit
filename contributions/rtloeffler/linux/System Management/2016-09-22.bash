0026e90a6b997515e687a8ead8eb261de8747d2a
rtloeffler <rloeffler@dynamic.vision>
2016-09-22 21:06:45 +0100

    2016-09-22's linux workout

---
+++ contributions/rtloeffler/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Monitor System Memory using `vmstat`
