b6358318c8412f04bbd6ec7fc01d2f9500a89be7
alain21218 <alain21218@hotmail.com>
2016-09-22 11:42:34 +0100

    2016-09-21's linux workout

---
+++ contributions/alain21218/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Useful aliasing in bash
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
