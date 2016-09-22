b33aa584c6460ea12d6ec76025dcb16eb0d9f67c
FootballFan141 <xbox360141@gmail.com>
2016-09-22 14:55:26 +0100

    2016-09-22's java workout

---
+++ contributions/FootballFan141/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
