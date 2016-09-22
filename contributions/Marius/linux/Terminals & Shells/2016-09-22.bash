69f047e69bb2611f7d032d929278d2f3c4463556
Marius <marius.mez@gmail.com>
2016-09-22 13:26:27 +0100

    2016-09-22's linux workout

---
+++ contributions/Marius/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
