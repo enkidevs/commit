a55aa17638246457c0b519738083318efa4d9973
rapholtz <raphaellmattozo@gmail.com>
2016-09-22 03:17:11 +0100

    2016-09-21's linux workout

---
+++ contributions/rapholtz/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Find any Unix / Linux command
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
