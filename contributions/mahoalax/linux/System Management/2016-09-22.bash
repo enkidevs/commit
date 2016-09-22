5dbb1fc91cf8a63693d90a2ffe857e50636f16dd
mahoalax <marie.laxenaire@webedia-group.com>
2016-09-22 20:30:03 +0100

    2016-09-22's linux workout

---
+++ contributions/mahoalax/linux/System Management/2016-09-22.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
