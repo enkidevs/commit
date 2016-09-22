5e651c88b29a88a701525c62c10008ca2b1764a2
Denpeer <d.j.p.vanpeer@student.tudelft.nl>
2016-09-22 08:29:37 +0100

    2016-09-22's linux workout

---
+++ contributions/Denpeer/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
