9ff713efc649096cf7b20628f8fea4b6a743a8f6
Fenetic <esyouit@gmail.com>
2016-09-22 20:42:11 +0100

    2016-09-22's javascript workout

---
+++ contributions/Fenetic/javascript/Express/2016-09-22.js
@@ -0,0 +1,5 @@
+`response` object additional properties in **Express**
+Easy way to deliver **HTML** pages with **Express**
+`cookie-session` in **Express**
+Error handler in **Express**
+`vhost` in **Express**
