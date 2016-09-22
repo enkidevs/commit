a339951702528cf1bbe003cc990ccd41635b3651
ZhSh <jenyashulman@gmail.com>
2016-09-22 13:41:19 +0100

    2016-09-22's java workout

---
+++ contributions/ZhSh/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
