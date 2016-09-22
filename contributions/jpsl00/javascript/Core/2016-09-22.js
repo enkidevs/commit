ecf06513dcb54ce4d493ae5b5f261547663085ad
jpsl00 <joaopedroluciano@gmail.com>
2016-09-22 12:09:01 +0100

    2016-09-22's javascript workout

---
+++ contributions/jpsl00/javascript/Core/2016-09-22.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+`NaN` is a number
+Extra function arguments are undefined by default
+Getting the *real* dimensions of an image
+Inaccuracy of binary floating-point format
