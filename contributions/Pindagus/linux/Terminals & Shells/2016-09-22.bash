d1af403ff7ea3ea7ec63892f6eb560639cf4955c
Pindagus <agusnobel@gmail.com>
2016-09-22 06:44:17 +0100

    2016-09-22's linux workout

---
+++ contributions/Pindagus/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,7 @@
+Useful aliasing in bash
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
