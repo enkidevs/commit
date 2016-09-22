6c1e5fa1e973eacda227b3717bec68a9314aab42
christopheradr <christopher.diaz.riv@gmail.com>
2016-09-23 00:48:47 +0100

    2016-09-22's linux workout

---
+++ contributions/christopheradr/linux/File Management/2016-09-22.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
+Delete commands aliases
