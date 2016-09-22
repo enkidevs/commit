5aa13d67633b3eb39ecf24a6e6f56942be5c60d3
tsluder8 <tsluder8@gmail.com>
2016-09-22 21:39:38 +0100

    2016-09-22's linux workout

---
+++ contributions/tsluder8/linux/System Management/2016-09-22.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
