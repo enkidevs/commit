0c2c89eab15435826f7fc98331988253702741e2
kiwiapple87 <mrorange4287@gmail.com>
2016-09-22 01:23:54 +0100

    2016-09-21's linux workout

---
+++ contributions/kiwiapple87/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Clear the terminal instantly
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
