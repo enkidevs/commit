4ef1c3dcbe9ca3cd35eb2f7ffe20a3eabdfcab59
karlm <karl.missiaen@sfpd.fgov.be>
2016-09-22 06:56:32 +0100

    2016-09-22's java workout

---
+++ contributions/karlm/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
