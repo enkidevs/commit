d61ee3350b5c4edcfbc7ccb6bfe5f021759ff7fb
themattkellyshow <themattkellyshow@gmail.com>
2016-09-22 05:14:34 +0100

    2016-09-21's linux workout

---
+++ contributions/themattkellyshow/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Terminal based browser
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
