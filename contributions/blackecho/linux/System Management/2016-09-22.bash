61b500f5b8d130bb2ae63b3374d919f06235c9e5
blackecho <angeletti.gabriele@gmail.com>
2016-09-22 08:56:28 +0100

    2016-09-22's linux workout

---
+++ contributions/blackecho/linux/System Management/2016-09-22.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
