fa3f7f02f3d3889812b4142dcc4dcdf8b968941e
Well97 <gabriel.well@hotmail.com>
2016-09-22 23:23:16 +0100

    2016-09-22's linux workout

---
+++ contributions/Well97/linux/System Management/2016-09-22.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
