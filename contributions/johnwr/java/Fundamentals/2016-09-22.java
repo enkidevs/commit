d976d558067a0f1b615b0b05b9bf8aa243d35637
johnwr <johnwr_enki@wraalsen.no>
2016-09-22 08:46:52 +0100

    2016-09-22's java workout

---
+++ contributions/johnwr/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+Supply `toString()` in all classes
