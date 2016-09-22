8754787a9963f4ef8711d9604b5b8554144c6398
krakensham <smolinoleksandr@gmail.com>
2016-09-22 06:09:25 +0100

    2016-09-21's java workout

---
+++ contributions/krakensham/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
