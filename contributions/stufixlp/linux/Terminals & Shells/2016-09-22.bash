aed4f83aaa54b4809004e8c1f5d575f7fa30a5e6
stufixlp <stufixlp@gmail.com>
2016-09-22 20:54:41 +0100

    2016-09-22's linux workout

---
+++ contributions/stufixlp/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Repeating commands with `watch`
+Quick `bash` shortcuts 
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
