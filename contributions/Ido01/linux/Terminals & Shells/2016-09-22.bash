cf606686a03e4fb677ca35c32e96ff4a93c7aeb3
Ido01 <idodon001@gmail.com>
2016-09-22 14:12:08 +0100

    2016-09-22's linux workout

---
+++ contributions/Ido01/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Terminal incognito mode
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
