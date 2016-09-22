8def4b9c7264834583d8bb06ab6bed80fe23b2fc
inandrea <inandrea82@gmail.com>
2016-09-22 17:18:38 +0100

    2016-09-22's linux workout

---
+++ contributions/inandrea/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Useful aliasing in bash
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
