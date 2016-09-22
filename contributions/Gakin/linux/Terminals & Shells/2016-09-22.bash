a7824e143e29c3414b90a8c61dd0126c8ae90225
Gakin <gakin@usc.edu>
2016-09-22 14:19:13 +0100

    2016-09-22's linux workout

---
+++ contributions/Gakin/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Terminal based browser
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
