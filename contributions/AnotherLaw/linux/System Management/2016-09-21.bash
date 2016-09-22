2ca12c87ab24cd0290d81921dcd1a4b7c31b0d7e
AnotherLaw <law.and.contradiction@gmail.com>
2016-09-22 12:07:54 +0100

    2016-09-21's linux workout

---
+++ contributions/AnotherLaw/linux/System Management/2016-09-21.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
