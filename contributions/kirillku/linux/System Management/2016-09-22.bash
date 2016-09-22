5a4f16ffe3f904357e02fe71e903a4ef3e87523d
kirillku <kubryakov@gmail.com>
2016-09-22 18:56:49 +0100

    2016-09-22's linux workout

---
+++ contributions/kirillku/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
