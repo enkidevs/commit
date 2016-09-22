f427ebafec448bea4eb02c97d0fdb1888b6763c9
PizzaFreak <nico.greve89@gmail.com>
2016-09-22 09:07:02 +0100

    2016-09-22's linux workout

---
+++ contributions/PizzaFreak/linux/System Management/2016-09-22.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
