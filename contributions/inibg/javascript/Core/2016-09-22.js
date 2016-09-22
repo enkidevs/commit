2c754efde14d75fc925b9a7871b8e0e4f6d08edb
inibg <inibg2@gmail.com>
2016-09-22 12:34:33 +0100

    2016-09-22's javascript workout

---
+++ contributions/inibg/javascript/Core/2016-09-22.js
@@ -0,0 +1,5 @@
+Hoisting applies only to variable declarations, not initializations
+The difference between null, undefined and NaN
+`catch` is block scoped
+`console.*` in JavaScript
+Inaccuracy of binary floating-point format
