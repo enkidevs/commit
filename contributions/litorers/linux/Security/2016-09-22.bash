c979f12c8911ff0940a4299a295e101673e2728f
litorers <alexandrejoseph.tessier@gmail.com>
2016-09-22 12:00:09 +0100

    2016-09-22's linux workout

---
+++ contributions/litorers/linux/Security/2016-09-22.bash
@@ -0,0 +1,5 @@
+Security news delivered via DNS
+Extracting the `public key` from the `private key`
+Block IPs using `Fail2ban`
+Disabling **USB** storage devices
+Private key encryption with `OpenSSL`
