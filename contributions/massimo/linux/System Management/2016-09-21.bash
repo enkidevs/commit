ab3d1d03bdb818178b44ced6b21d3db22bdae42c
massimo <massimobalestra@hotmail.com>
2016-09-22 04:52:21 +0100

    2016-09-21's linux workout

---
+++ contributions/massimo/linux/System Management/2016-09-21.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
