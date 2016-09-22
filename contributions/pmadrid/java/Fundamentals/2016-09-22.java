132a832ed4d3041a218a8f56e066a1aa668887f4
pmadrid <pmadrid@gmail.com>
2016-09-22 21:55:29 +0100

    2016-09-22's java workout

---
+++ contributions/pmadrid/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
