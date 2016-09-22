7c960a14336214338788c5f3f9752465b8fd6f33
ashwinnairenki <ashwinnair1992@gmail.com>
2016-09-22 06:04:59 +0100

    2016-09-21's linux workout

---
+++ contributions/ashwinnairenki/linux/System Management/2016-09-21.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
