d42b56feec1da9ae1f0802e22262f76467c45663
koru <glassonyou@gmail.com>
2016-09-22 14:09:30 +0100

    2016-09-21's linux workout

---
+++ contributions/koru/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
