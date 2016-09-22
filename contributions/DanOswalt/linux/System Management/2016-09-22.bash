811e7141164fb7e6e53057fee904e63f7d24c153
DanOswalt <do189705@gmail.com>
2016-09-22 21:15:06 +0100

    2016-09-22's linux workout

---
+++ contributions/DanOswalt/linux/System Management/2016-09-22.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
