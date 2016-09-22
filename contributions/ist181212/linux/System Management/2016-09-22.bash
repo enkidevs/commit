ca0ef6cb0bea6e394ff8a635f8b6b9317a01d3b3
ist181212 <joaocarlospalma.999@gmail.com>
2016-09-22 15:32:36 +0100

    2016-09-22's linux workout

---
+++ contributions/ist181212/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Detect your linux distribution
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Monitor System Memory using `vmstat`
