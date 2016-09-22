89f8e82510a1dbbc6a8bb7df1a847f50c86d8542
kurosaki15 <mairkwow@gmail.com>
2016-09-22 13:13:01 +0100

    2016-09-22's java workout

---
+++ contributions/kurosaki15/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
