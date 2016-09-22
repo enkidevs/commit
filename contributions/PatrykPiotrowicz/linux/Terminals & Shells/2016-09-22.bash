18edbadce1086f6d97d8d43a0a569157ed050a3e
PatrykPiotrowicz <patrykpiotrowicz666@gmail.com>
2016-09-22 16:09:53 +0100

    2016-09-22's linux workout

---
+++ contributions/PatrykPiotrowicz/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
