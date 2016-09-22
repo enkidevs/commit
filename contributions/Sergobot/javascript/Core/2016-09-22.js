a0c49422339eeed63821ed56699955f57edbe0c7
Sergobot <sergobot@protonmail.com>
2016-09-22 09:35:16 +0100

    2016-09-22's javascript workout

---
+++ contributions/Sergobot/javascript/Core/2016-09-22.js
@@ -0,0 +1,5 @@
+Keywords in hash keys
+Data type comparison in `switch` statements
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+`catch` is block scoped
