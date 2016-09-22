13309ba9f449d266e66b6d336e5ee0cf90236dcf
RareEvent <ham@perfect-kreim.de>
2016-09-22 06:44:58 +0100

    2016-09-22's linux workout

---
+++ contributions/RareEvent/linux/Networking/2016-09-22.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Logging dropped firewall packets
+List your IPv6 configuration
+Bandwidth monitoring tools 
+Staying connected remotely via SSH without an interactive shell
