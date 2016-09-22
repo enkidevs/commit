cef604a5ada721271a3cd09bc86e80f492d6911a
jukka2003 <jukka2003@gmail.com>
2016-09-22 16:43:30 +0100

    2016-09-22's java workout

---
+++ contributions/jukka2003/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
