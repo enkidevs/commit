3ba7223d621b4388714ee0510753f2098cafd8f1
DrFox0 <samuele.pretto@gmail.com>
2016-09-22 08:07:21 +0100

    2016-09-22's java workout

---
+++ contributions/DrFox0/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
