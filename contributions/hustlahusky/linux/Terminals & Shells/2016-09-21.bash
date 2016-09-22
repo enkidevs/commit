dc3b954edb81361e737eae739578984470d93e22
hustlahusky <genteelknight@gmail.com>
2016-09-22 18:10:38 +0100

    2016-09-21's linux workout

---
+++ contributions/hustlahusky/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Keep useful commands in your shell history with tags
