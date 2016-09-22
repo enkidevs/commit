66ee9b06ee7e3985724e734504f9366299dfbe83
hyftar <simon_landry@ymail.com>
2016-09-22 14:05:57 +0100

    2016-09-22's linux workout

---
+++ contributions/hyftar/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Random password generator
+Terminal incognito mode
+Get to know  your commands with `type`
