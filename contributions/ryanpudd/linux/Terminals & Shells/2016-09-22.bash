66d692738f6a421212f09023cb9db6b2e403b33a
ryanpudd <ryanpudd@gmail.com>
2016-09-22 08:38:35 +0100

    2016-09-22's linux workout

---
+++ contributions/ryanpudd/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Random password generator
+Terminal incognito mode
+Keep useful commands in your shell history with tags
