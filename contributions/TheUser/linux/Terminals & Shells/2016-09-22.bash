be0ef5fa2dbe13e6cfeaefb9aee8896cfff67f83
TheUser <erlendahall@gmail.com>
2016-09-22 10:43:10 +0100

    2016-09-22's linux workout

---
+++ contributions/TheUser/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Random password generator
+Terminal incognito mode
+Keep useful commands in your shell history with tags
