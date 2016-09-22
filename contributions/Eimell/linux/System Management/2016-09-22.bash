2f8ad9ac6b616070b647c1534359d51d2ef85b2f
Eimell <eimell@gmail.com>
2016-09-22 08:17:26 +0100

    2016-09-22's linux workout

---
+++ contributions/Eimell/linux/System Management/2016-09-22.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
+Basic service management with `systemd`
+Monitor System Memory using `vmstat`
