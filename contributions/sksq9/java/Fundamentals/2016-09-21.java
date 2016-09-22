65df4d1c61024ae56d916ef90a55655676d27ae6
sksq9 <shubham.zeez@gmail.com>
2016-09-22 17:27:35 +0100

    2016-09-21's java workout

---
+++ contributions/sksq9/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
