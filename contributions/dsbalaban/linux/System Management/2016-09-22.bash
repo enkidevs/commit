965aef9f66626cee82f4fd709bfd49e19262136e
dsbalaban <dan.stefan.balaban@gmail.com>
2016-09-22 21:37:00 +0100

    2016-09-22's linux workout

---
+++ contributions/dsbalaban/linux/System Management/2016-09-22.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
