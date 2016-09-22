5f40d978a0905d8009418183add21dc474cbd7aa
gogobebe2 <gogobebe2@gmail.com>
2016-09-22 01:41:03 +0100

    2016-09-21's linux workout

---
+++ contributions/gogobebe2/linux/System Management/2016-09-21.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
