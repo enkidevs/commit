e0c5ab1dca06b9ed8abfd7aa0840976378565e32
Jiuchu <le9227034470@yahoo.fr>
2016-09-22 12:31:31 +0100

    2016-09-22's java workout

---
+++ contributions/Jiuchu/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
