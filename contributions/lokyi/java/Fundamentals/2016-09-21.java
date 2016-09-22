78a2fdbb0a8557f43beef7f5c27d970a87d763eb
lokyi <roy.yi.yue@gmail.com>
2016-09-22 04:52:47 +0100

    2016-09-21's java workout

---
+++ contributions/lokyi/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The trade-off between short and descriptive names
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Detect or prevent integer overflow
