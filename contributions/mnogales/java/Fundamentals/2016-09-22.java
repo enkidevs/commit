5d599320a01bf227508ddcec3928a5829dde73e4
mnogales <nogales.manuel@gmail.com>
2016-09-22 16:52:48 +0100

    2016-09-22's java workout

---
+++ contributions/mnogales/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
