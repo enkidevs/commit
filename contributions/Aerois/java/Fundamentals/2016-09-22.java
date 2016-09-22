5268c0860c291845173c1cffcc2d59903d08b27e
Aerois <alain.rarman@hotmail.fr>
2016-09-22 10:52:44 +0100

    2016-09-22's java workout

---
+++ contributions/Aerois/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Altering format string output by changing a format specifier's `argument_index`
+Limit Accessibility of `Fields`
