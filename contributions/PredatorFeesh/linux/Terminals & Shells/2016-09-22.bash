d2b6d656b1dfbd4120761c11b3455ff9b95af9e6
PredatorFeesh <zatbuilder@gmail.com>
2016-09-22 22:45:01 +0100

    2016-09-22's linux workout

---
+++ contributions/PredatorFeesh/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
