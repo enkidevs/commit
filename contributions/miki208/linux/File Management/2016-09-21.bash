f82db40789b02f3c8b849546e7d852c0b66f3eca
miki208 <miloss208@gmail.com>
2016-09-22 01:53:46 +0100

    2016-09-21's linux workout

---
+++ contributions/miki208/linux/File Management/2016-09-21.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
