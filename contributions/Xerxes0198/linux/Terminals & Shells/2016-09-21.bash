bba11791a241be651e0c35aad771fb577b5cd067
Xerxes0198 <greg.scott@y7mail.com>
2016-09-22 04:44:12 +0100

    2016-09-21's linux workout

---
+++ contributions/Xerxes0198/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
