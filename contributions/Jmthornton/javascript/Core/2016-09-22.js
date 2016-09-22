423c3393e8bd817b033191c745fea242b4fac71f
Jmthornton <jmthornton4@gmail.com>
2016-09-22 18:48:14 +0100

    2016-09-22's javascript workout

---
+++ contributions/Jmthornton/javascript/Core/2016-09-22.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Precision
+Most efficient way to build `HTML` strings
+Hoisting applies only to variable declarations, not initializations
+Detect **DO NOT TRACK** status
