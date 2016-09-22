b75f9128edb25abb4db66cc763d52415e8771981
zszuri <szuri90@gmail.com>
2016-09-22 10:45:56 +0100

    2016-09-22's java workout

---
+++ contributions/zszuri/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Connecting to FTP using Java
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
