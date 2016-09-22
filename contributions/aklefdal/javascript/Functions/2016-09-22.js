79bf6d0c4b2729e53bd046d58443b6cd53e2e3d6
aklefdal <alf.kare@lefdal.cc>
2016-09-22 22:52:18 +0100

    2016-09-22's javascript workout

---
+++ contributions/aklefdal/javascript/Functions/2016-09-22.js
@@ -0,0 +1,5 @@
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Function call method
+Move cursor at the end of text input
+Check if a document is done loading
