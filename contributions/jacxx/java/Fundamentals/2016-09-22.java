c02225a9f00dbdd2196d77ed008c389553aea533
jacxx <jacxx.gaming@gmail.com>
2016-09-22 20:36:40 +0100

    2016-09-22's java workout

---
+++ contributions/jacxx/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
