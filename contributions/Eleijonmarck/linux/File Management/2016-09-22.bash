368081d87e890febf0498cdcb53fd58ea15b2cca
Eleijonmarck <eric.leijonmarck@gmail.com>
2016-09-22 07:56:37 +0100

    2016-09-22's linux workout

---
+++ contributions/Eleijonmarck/linux/File Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
+Easy way of sharing files
