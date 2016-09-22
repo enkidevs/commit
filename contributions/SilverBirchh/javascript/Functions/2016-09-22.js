6c278d1e8062560c934b9e5227a01f7937956813
SilverBirchh <bradleybirch09@yahoo.co.uk>
2016-09-22 20:56:53 +0100

    2016-09-22's javascript workout

---
+++ contributions/SilverBirchh/javascript/Functions/2016-09-22.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+IIFE pattern
+Social sharing without widgets
