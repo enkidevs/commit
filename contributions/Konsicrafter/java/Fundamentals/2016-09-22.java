6e1d4b4b7794c3407513a06576017f5acbdf42da
Konsicrafter <konsicrafter@web.de>
2016-09-22 21:53:48 +0100

    2016-09-22's java workout

---
+++ contributions/Konsicrafter/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
