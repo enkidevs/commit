8c42f917b3483e2dafd3de2712f248b06a794c73
lavinoys <grmjkzz@gmail.com>
2016-09-22 04:40:10 +0100

    2016-09-21's javascript workout

---
+++ contributions/lavinoys/javascript/Core/2016-09-21.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Counting the months from zero
+The difference between null, undefined and NaN
+Extra function arguments are undefined by default
+Detect online connection
