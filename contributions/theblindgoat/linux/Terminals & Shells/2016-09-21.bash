105f2d806c01efda00c3490a8f9f92762e409595
theblindgoat <mancha.anthony.j@gmail.com>
2016-09-22 02:33:36 +0100

    2016-09-21's linux workout

---
+++ contributions/theblindgoat/linux/Terminals & Shells/2016-09-21.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Useful aliasing in bash
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
