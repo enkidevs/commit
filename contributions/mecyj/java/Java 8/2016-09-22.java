669476e552c81dd6d44b8a76182ecd736a03a562
mecyj <dominik.sze@gmail.com>
2016-09-22 14:07:19 +0100

    2016-09-22's java workout

---
+++ contributions/mecyj/java/Java 8/2016-09-22.java
@@ -0,0 +1,5 @@
+How to check for static dependencies of applications and libraries from the command line.
+Use `Optional(s)` type in order to avoid `NullPointerException(s)`
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
