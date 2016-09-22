fce759c4e542a4ef41c79981159fb95dbe723dd7
mroddy1 <mroddy1@msudenver.edu>
2016-09-22 07:30:06 +0100

    2016-09-21's java workout

---
+++ contributions/mroddy1/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
