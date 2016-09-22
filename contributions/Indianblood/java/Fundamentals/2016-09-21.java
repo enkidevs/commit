b978ec36e45a15576044898f992c541998dd87f6
Indianblood <indianblood@gmail.com>
2016-09-22 06:39:05 +0100

    2016-09-21's java workout

---
+++ contributions/Indianblood/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
