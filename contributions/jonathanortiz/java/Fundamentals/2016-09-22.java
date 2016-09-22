147282e34265951a88518568b9700d72fee09bab
jonathanortiz <jorgejov@gmail.com>
2016-09-22 13:24:56 +0100

    2016-09-22's java workout

---
+++ contributions/jonathanortiz/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
