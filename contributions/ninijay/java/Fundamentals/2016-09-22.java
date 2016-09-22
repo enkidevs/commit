8a490676f190dcfd8bb52d483e00d9630932a7fd
ninijay <ninijay93@gmail.com>
2016-09-22 19:46:31 +0100

    2016-09-22's java workout

---
+++ contributions/ninijay/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
+Using an interface as a parameter
