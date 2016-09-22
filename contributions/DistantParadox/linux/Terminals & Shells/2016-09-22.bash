2251a08bb4baea05782b4d531b4f5d65cb12e845
DistantParadox <craigtski47@gmail.com>
2016-09-22 10:00:01 +0100

    2016-09-22's linux workout

---
+++ contributions/DistantParadox/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Terminal based browser
+Random password generator
+Adding directories to your `$PATH`
