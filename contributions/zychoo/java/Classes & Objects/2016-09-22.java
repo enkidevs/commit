2a2292d7df336f5ad136fd4575d6f628b42c5a43
zychoo <zychster@gmail.com>
2016-09-22 07:34:00 +0100

    2016-09-22's java workout

---
+++ contributions/zychoo/java/Classes & Objects/2016-09-22.java
@@ -0,0 +1,5 @@
+Template methods in abstract classes
+Keeping fields `private`
+Double Brace Initialization
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
