65d521fbd9176c0c5c8092efab587e7ff75ff3fb
keicus <charlestdjames1@gmail.com>
2016-09-22 03:15:19 +0100

    2016-09-21's java workout

---
+++ contributions/keicus/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Inheriting a constructor from a superclass
+Using `synchronized` statements
+Supply `toString()` in all classes
+Using an interface as a parameter
