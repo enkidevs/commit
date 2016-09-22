a757d0a98fb3dd7c2c00216c90100d9127a6aece
behey <misha.behey@gmail.com>
2016-09-22 05:24:08 +0100

    2016-09-21's java workout

---
+++ contributions/behey/java/Fundamentals/2016-09-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
