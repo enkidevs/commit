36fdf3dbfffe0ba9f667109aa9caa2cb50080ba7
jakepurple13 <jakerein@gmail.com>
2016-09-22 15:18:45 +0100

    2016-09-22's java workout

---
+++ contributions/jakepurple13/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
