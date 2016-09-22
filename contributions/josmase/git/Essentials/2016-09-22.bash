20f4a80765722cbd02f833a01189c23e45e724c6
josmase <josnase@gmail.com>
2016-09-22 18:24:20 +0100

    2016-09-22's git workout

---
+++ contributions/josmase/git/Essentials/2016-09-22.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Adding a remote repository
+You can use git offline!
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
