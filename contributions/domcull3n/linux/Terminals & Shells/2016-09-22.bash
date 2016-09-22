7a246ffe212612b4be4624ed5efe17678825e1e0
domcull3n <dom@laundrapp.com>
2016-09-22 08:59:43 +0100

    2016-09-22's linux workout

---
+++ contributions/domcull3n/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Terminal incognito mode
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
