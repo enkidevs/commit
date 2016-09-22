fa045298d106168f6096321f013270532024b6c4
PierreBesson <pbesson@ippon.fr>
2016-09-22 21:23:46 +0100

    2016-09-22's java workout

---
+++ contributions/PierreBesson/java/Data Types/2016-09-22.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Finding a substring in a string
+String concatenation in Java
+Do not attempt comparisons with NaN
