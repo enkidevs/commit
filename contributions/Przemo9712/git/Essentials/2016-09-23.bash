cd42bd1790291f194c6da96c7f1ba4baecf1d38e
Przemo9712 <przemo9712@gmail.com>
2016-09-23 07:07:05 +0100

    2016-09-23's git workout

---
+++ contributions/Przemo9712/git/Essentials/2016-09-23.bash
@@ -0,0 +1,5 @@
+How to set your username and email
+Make your log output pretty
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
+Ignore files in git
