70927a61e23de1beaa56bc16cd04e706d00189b6
HappiSquirrel <ludo0302@hotmail.fr>
2016-09-23 15:01:12 +0100

    2016-09-23's java workout

---
+++ contributions/HappiSquirrel/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Using bounded type parameters in generic methods
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
