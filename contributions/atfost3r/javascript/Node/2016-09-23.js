b7dd8261c4f24694e05d2fe0d85261c02ac3f74c
atfost3r <atfost3r@gmail.com>
2016-09-23 14:52:40 +0100

    2016-09-23's javascript workout

---
+++ contributions/atfost3r/javascript/Node/2016-09-23.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Custom Node REPL Server
+First-error callbacks in **Node**
+`uncaughtException` listener in Node.js
+Automatic compilation for Node with **Nodemon** 
