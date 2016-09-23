802d8294f450caccb3d63b2385b79010739c42a4
chrispolley <chris.polley@gmail.com>
2016-09-23 17:56:40 +0100

    2016-09-23's javascript workout

---
+++ contributions/chrispolley/javascript/Core/2016-09-23.js
@@ -0,0 +1,5 @@
+Deleting properties
+Performing a function at timed intervals
+Hoisting applies only to variable declarations, not initializations
+NaN === NaN?
+Extra function arguments are undefined by default
