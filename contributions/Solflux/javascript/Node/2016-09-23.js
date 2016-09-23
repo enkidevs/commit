42352b6addcc280214a6a52f0aec74e3860c656d
Solflux <enki@solflux.co.uk>
2016-09-23 13:43:38 +0100

    2016-09-23's javascript workout

---
+++ contributions/Solflux/javascript/Node/2016-09-23.js
@@ -0,0 +1,5 @@
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+`process.nextTick()` callback pattern
+`try-catch` only for **sync** code
