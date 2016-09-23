30952a621fc5ace170cf47c5762816df44005d0d
viralwarrior012 <viralwarrior012@gmail.com>
2016-09-23 19:50:50 +0100

    2016-09-23's javascript workout

---
+++ contributions/viralwarrior012/javascript/Functions/2016-09-23.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+IIFE pattern
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
