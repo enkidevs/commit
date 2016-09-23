f82997d6b7e897bc3b7450952f334ce4ba55d361
massimo <massimobalestra@hotmail.com>
2016-09-23 05:09:28 +0100

    2016-09-22's javascript workout

---
+++ contributions/massimo/javascript/Browser APIs/2016-09-22.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Async and defer scripts
+Form a `URL` from its parts 
+Warn user if **Back** button is pressed
+Network Information API
