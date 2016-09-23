1d7c79f13f3669327c81a75beadf34c8a79a0694
threedaysmore <charles.kiser@careerbuilder.com>
2016-09-23 22:49:42 +0100

    2016-09-23's linux workout

---
+++ contributions/threedaysmore/linux/System Management/2016-09-23.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
