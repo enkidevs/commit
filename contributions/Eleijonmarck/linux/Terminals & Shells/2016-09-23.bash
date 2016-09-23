c012b428ec5dc2ade5bce75f20a0a16c7c24dc2d
Eleijonmarck <eric.leijonmarck@gmail.com>
2016-09-23 08:28:47 +0100

    2016-09-23's linux workout

---
+++ contributions/Eleijonmarck/linux/Terminals & Shells/2016-09-23.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Clear the terminal instantly
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
