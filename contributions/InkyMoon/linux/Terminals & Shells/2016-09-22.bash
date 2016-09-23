0e43ecfbbdb2db42b1f2f68921a7fb83adc01097
InkyMoon <patrick.kastelic@gmail.com>
2016-09-23 05:57:41 +0100

    2016-09-22's linux workout

---
+++ contributions/InkyMoon/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Useful aliasing in bash
+Conditional command execution
+
+(`&&` operator)
