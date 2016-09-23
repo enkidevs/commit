0438483733bd857aba923c162df594fe709df13c
Ilonich <niko-ikko@yandex.ru>
2016-09-23 02:44:44 +0100

    2016-09-23's java workout

---
+++ contributions/Ilonich/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
