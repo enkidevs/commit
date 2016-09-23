7e9704a152a573ffcf14ad9e68844def21d7694b
kardudu <kevin.donaldy@gmail.com>
2016-09-23 23:53:20 +0100

    2016-09-23's linux workout

---
+++ contributions/kardudu/linux/System Management/2016-09-23.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Test disk speed with `dd`
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
