0af591852f6e05ba5df702cd186315759dbccbde
yyadavalli <yashwanth.yadavalli@gmail.com>
2016-09-23 19:14:32 +0100

    2016-09-23's java workout

---
+++ contributions/yyadavalli/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
