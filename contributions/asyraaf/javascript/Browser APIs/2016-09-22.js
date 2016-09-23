2d7d613b8a5ca837d90acc88a50bedf9b607ef14
asyraaf <asyraaf.azahari@gmail.com>
2016-09-23 02:06:50 +0100

    2016-09-22's javascript workout

---
+++ contributions/asyraaf/javascript/Browser APIs/2016-09-22.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+Use hosted scripts to increase performance
+Drawing with HTML5 `Canvas` API
+Page Visibility API
