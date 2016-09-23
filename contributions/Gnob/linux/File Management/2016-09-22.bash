d85a61bbbb591a5a52ef485b574e10fc51f49776
Gnob <gnob244@gmail.com>
2016-09-23 08:53:47 +0100

    2016-09-22's linux workout

---
+++ contributions/Gnob/linux/File Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Delete commands aliases
