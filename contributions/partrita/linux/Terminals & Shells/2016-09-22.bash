caf34eba5122248b57d3e03e8d039b00f9eafadb
partrita <partrita@gmail.com>
2016-09-23 13:22:26 +0100

    2016-09-22's linux workout

---
+++ contributions/partrita/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
