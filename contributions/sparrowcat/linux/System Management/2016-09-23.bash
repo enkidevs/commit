b15ddde0a55be5d1bbb695f619dda34e56ce7ec7
sparrowcat <silvia.knappe725@gmail.com>
2016-09-23 15:43:29 +0100

    2016-09-23's linux workout

---
+++ contributions/sparrowcat/linux/System Management/2016-09-23.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
