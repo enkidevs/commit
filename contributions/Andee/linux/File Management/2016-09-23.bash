2cbf267e2770d159f3a8afff88e285aa72bcbd6d
Andee <andytruong94@gmail.com>
2016-09-23 22:32:50 +0100

    2016-09-23's linux workout

---
+++ contributions/Andee/linux/File Management/2016-09-23.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Working with `.gz` files
+Easy way of sharing files
