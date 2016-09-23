dba925c02eb546a812cebf65d5024581ec3adab2
factoryEquipment <cs.serg@gmail.com>
2016-09-23 02:26:53 +0100

    2016-09-22's java workout

---
+++ contributions/factoryEquipment/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Using an interface as a parameter
