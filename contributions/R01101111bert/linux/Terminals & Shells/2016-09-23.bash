afde9d51b877c6271d904d4a0001732ebd373882
R01101111bert <robert.obkircher@gmail.com>
2016-09-23 09:49:02 +0100

    2016-09-23's linux workout

---
+++ contributions/R01101111bert/linux/Terminals & Shells/2016-09-23.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
