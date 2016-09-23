96a65e5dba92f939c0129ea2b16c6cc9a64cae0c
RogelioHIT <red1hhh@gmail.com>
2016-09-23 07:33:10 +0100

    2016-09-22's javascript workout

---
+++ contributions/RogelioHIT/javascript/Core/2016-09-22.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Undefined / Empty / Null Checks
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+Avoid using `with`
