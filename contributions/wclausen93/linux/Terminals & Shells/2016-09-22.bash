adb06d0c5fa04169a302d7b3e456fb637fb544d3
wclausen93 <william.h.f.clausen@gmail.com>
2016-09-23 07:24:03 +0100

    2016-09-22's linux workout

---
+++ contributions/wclausen93/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
