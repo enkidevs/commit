88c61dd3d726f6e835f70a4d4d349416487be1ba
Akipe <milletre.julien@gmail.com>
2016-09-23 22:41:34 +0100

    2016-09-23's linux workout

---
+++ contributions/Akipe/linux/Terminals & Shells/2016-09-23.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
