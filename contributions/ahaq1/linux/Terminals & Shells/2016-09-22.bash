c8adc7e6a416f3c51908830fb3c011124d5075da
ahaq1 <desisyntax@gmail.com>
2016-09-23 01:35:22 +0100

    2016-09-22's linux workout

---
+++ contributions/ahaq1/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal incognito mode
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
