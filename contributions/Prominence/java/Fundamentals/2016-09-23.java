1efcb809365d28ae3f27f3d851fc4bf2a58f5c07
Prominence <prominence96@gmail.com>
2016-09-23 05:03:55 +0100

    2016-09-23's java workout

---
+++ contributions/Prominence/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Altering format string output by changing a format specifier's `argument_index`
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
