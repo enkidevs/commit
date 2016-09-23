84d6e2779f37e7b3a6d7586f23ba1c29097c5b99
Antonio-Smirnov <toxa.inc@mail.ru>
2016-09-23 14:52:09 +0100

    2016-09-23's java workout

---
+++ contributions/Antonio-Smirnov/java/I/O/2016-09-23.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Using the `OpenOption...` vararg to define file writing behavior
+List contents of a directory with `newDirectoryStream`
