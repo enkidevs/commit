41b04b77ee932b8fb49f1654b091eb94f4be14ad
benalisalmane <benalisalmane@gmail.com>
2016-09-23 20:08:47 +0100

    2016-09-23's javascript workout

---
+++ contributions/benalisalmane/javascript/Functions/2016-09-23.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Anonymous functions
+IIFE pattern
+Function call method
