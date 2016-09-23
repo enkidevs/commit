ab3949079f68fbd321076f6958a9ef95d1cc3277
RatRiot <asidemamonomas@gmail.com>
2016-09-23 02:41:49 +0100

    2016-09-22's java workout

---
+++ contributions/RatRiot/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Overloading Methods in Java
+Using Inheritance to reduce code repetition
+Using `Printstream.format` to format a string before printing
+Locks in `static synchronized` methods
