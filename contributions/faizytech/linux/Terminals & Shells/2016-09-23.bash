d8628a6a271ee97a2fb715e7a87166e36c67088e
faizytech <faizytech.php@gmail.com>
2016-09-24 00:11:38 +0100

    2016-09-23's linux workout

---
+++ contributions/faizytech/linux/Terminals & Shells/2016-09-23.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
