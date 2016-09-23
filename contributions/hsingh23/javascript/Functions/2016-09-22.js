a81f1baef873d062b0a98dbbaec10d292870497d
hsingh23 <hisingh1@gmail.com>
2016-09-23 01:20:26 +0100

    2016-09-22's javascript workout

---
+++ contributions/hsingh23/javascript/Functions/2016-09-22.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Method chaining
+Move cursor at the end of text input
+Social sharing without widgets
