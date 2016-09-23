49ff656719dc967ecc00e4df0b23328b38798ebe
adam.pap4 <adam.pap4@gmail.com>
2016-09-23 02:32:31 +0100

    2016-09-21's linux workout

---
+++ contributions/adam.pap4/linux/System Management/2016-09-21.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
