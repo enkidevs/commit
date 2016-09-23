369084df86115591a1a33aaede66228aab489107
gavrielrh <gxr2329@rit.edu>
2016-09-23 04:53:03 +0100

    2016-09-22's java workout

---
+++ contributions/gavrielrh/java/Data Types/2016-09-22.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Finding a substring in a string
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
