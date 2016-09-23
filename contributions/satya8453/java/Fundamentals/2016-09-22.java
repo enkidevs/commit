ee43f708efa3ce7031f4c1c1132ef5e675da5ca5
satya8453 <das.satyaprakash@gmail.com>
2016-09-23 22:11:09 +0100

    2016-09-22's java workout

---
+++ contributions/satya8453/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Always specify type parameter
