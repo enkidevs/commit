2a97de1209fd4bac3d2c8bea25ba7997417db8f9
ZohaibMKhan <zohaib.m.khan96@gmail.com>
2016-09-23 01:41:32 +0100

    2016-09-22's java workout

---
+++ contributions/ZohaibMKhan/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Supply `toString()` in all classes
+Using an interface as a parameter
