f633ba5ddd8c07e84d29743a961cd7615013e8ff
Werner <werner.foss@gmail.com>
2016-09-23 19:04:59 +0100

    2016-09-23's linux workout

---
+++ contributions/Werner/linux/Terminals & Shells/2016-09-23.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Find any Unix / Linux command
+Random password generator
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
