2b40343d5d82329601d0c2ec890bc5df402e265c
mukarram-ak <ak.mukarram@yahoo.com>
2016-09-23 17:49:45 +0100

    2016-09-23's linux workout

---
+++ contributions/mukarram-ak/linux/Terminals & Shells/2016-09-23.bash
@@ -0,0 +1,5 @@
+Quick `bash` shortcuts 
+Terminal incognito mode
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
