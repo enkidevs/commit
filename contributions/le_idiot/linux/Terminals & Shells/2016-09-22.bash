9ac19fcf612379d922db7a754c1cea94e6192e55
le_idiot <siddhesh.mhadnak@gmail.com>
2016-09-23 13:42:50 +0100

    2016-09-22's linux workout

---
+++ contributions/le_idiot/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
