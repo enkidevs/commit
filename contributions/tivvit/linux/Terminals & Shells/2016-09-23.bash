b43e95fc3d27b948a2bf77e2731ea7e0b5731491
tivvit <tivvit@seznam.cz>
2016-09-23 18:37:39 +0100

    2016-09-23's linux workout

---
+++ contributions/tivvit/linux/Terminals & Shells/2016-09-23.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Quick `bash` shortcuts 
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
