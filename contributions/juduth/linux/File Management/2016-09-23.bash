f6d8b65867cbe312ed7d4cf5a55382d8607de8fe
juduth <juduth@gmail.com>
2016-09-23 13:49:34 +0100

    2016-09-23's linux workout

---
+++ contributions/juduth/linux/File Management/2016-09-23.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Deleting files in a secure manner
+Deleting non-empty directories
+Easy way of sharing files
