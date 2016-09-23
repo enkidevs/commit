8ce2f08eee0a4979702bd84a628924979ca090eb
Alex-Aralis <alex.aralis@gmail.com>
2016-09-23 17:44:35 +0100

    2016-09-23's java workout

---
+++ contributions/Alex-Aralis/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
