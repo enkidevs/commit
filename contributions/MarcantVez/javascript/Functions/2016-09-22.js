a99b17b2a60126d2cf487ad2abd424346f237099
MarcantVez <marcant.vezina@gmail.com>
2016-09-23 03:50:01 +0100

    2016-09-22's javascript workout

---
+++ contributions/MarcantVez/javascript/Functions/2016-09-22.js
@@ -0,0 +1,5 @@
+Hoisting
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Move cursor at the end of text input
+Social sharing without widgets
