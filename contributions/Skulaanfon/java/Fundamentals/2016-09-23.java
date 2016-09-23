d1abd0c802b5c711825e1669ffafffa0bcf2a58c
Skulaanfon <skulaanfon@gmail.com>
2016-09-23 18:13:15 +0100

    2016-09-23's java workout

---
+++ contributions/Skulaanfon/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
