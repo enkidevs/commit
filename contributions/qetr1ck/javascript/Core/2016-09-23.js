08d3ba04b59fdd4c0299dd8ab2d00fbc19ca15f7
qetr1ck <warlok000@rambler.ru>
2016-09-23 23:05:38 +0100

    2016-09-23's javascript workout

---
+++ contributions/qetr1ck/javascript/Core/2016-09-23.js
@@ -0,0 +1,5 @@
+Deleting properties
+Hoisting applies only to variable declarations, not initializations
+Scope and strict mode
+Extra function arguments are undefined by default
+Detect an error type
