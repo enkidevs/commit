fb509b87d2d5b86003bef44e5649906204d13b60
Awlex <wieseralex580@gmail.com>
2016-09-23 07:49:46 +0100

    2016-09-23's java workout

---
+++ contributions/Awlex/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
+Using an interface as a parameter
