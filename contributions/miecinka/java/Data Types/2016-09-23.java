ba8f5b9a00a5f5bae20633e4a8cd97238ea86938
miecinka <mietinhovanish@gmail.com>
2016-09-23 18:49:11 +0100

    2016-09-23's java workout

---
+++ contributions/miecinka/java/Data Types/2016-09-23.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
