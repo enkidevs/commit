418d8419f3f2c75f0946496ba8da3beaa3a5d295
aeolyus <richardhuang.huang@gmail.com>
2016-09-23 03:31:24 +0100

    2016-09-22's linux workout

---
+++ contributions/aeolyus/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Useful aliasing in bash
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
