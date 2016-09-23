596c473587b03049ad19c44e9e17ea0b6938d6af
Matpir <mafiu2@o2.pl>
2016-09-23 23:12:52 +0100

    2016-09-22's java workout

---
+++ contributions/Matpir/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
