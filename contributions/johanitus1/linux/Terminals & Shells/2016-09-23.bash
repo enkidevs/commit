8e2d4c2e852c6caf33a4cbee5746a34e4cc4406c
johanitus1 <joan.vinyals@est.fib.upc.edu>
2016-09-23 18:24:29 +0100

    2016-09-23's linux workout

---
+++ contributions/johanitus1/linux/Terminals & Shells/2016-09-23.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Find any Unix / Linux command
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
