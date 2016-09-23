3e3f07b2ce09fa6039ee88e2edbac0e30cdc1c4e
Freerider689 <cyril.grossenbacher@gmx.ch>
2016-09-23 09:04:59 +0100

    2016-09-23's java workout

---
+++ contributions/Freerider689/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Writing generic methods
+Using the `Console`
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
