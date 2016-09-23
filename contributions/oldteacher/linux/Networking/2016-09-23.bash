a309806438ff2b342faaf52555f59a41a07520e2
oldteacher <drsantos@gmail.com>
2016-09-23 22:03:50 +0100

    2016-09-23's linux workout

---
+++ contributions/oldteacher/linux/Networking/2016-09-23.bash
@@ -0,0 +1,5 @@
+SSH tunneling made easy
+Find services running on your host
+Useful ssh client optimizations 
+Setting up password-free authentication
+Use `tcpdump` to listen to network interface traffic
