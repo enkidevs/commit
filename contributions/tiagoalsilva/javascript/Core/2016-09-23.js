e1f1b63aa0d08be4b362c04887d8923a373a609e
tiagoalsilva <tiagoalsilva@gmail.com>
2016-09-23 13:21:56 +0100

    2016-09-23's javascript workout

---
+++ contributions/tiagoalsilva/javascript/Core/2016-09-23.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Use `splice()` to remove an item from an array
+Hoisting applies only to variable declarations, not initializations
+Extra function arguments are undefined by default
+Detect online connection
