819b85d2ae0d56182bde415cc95e9605adc4c2f7
NiklasMerz <niklasmerz@gmx.net>
2016-09-23 11:16:02 +0100

    2016-09-23's java workout

---
+++ contributions/NiklasMerz/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Using Inheritance to reduce code repetition
+Always specify type parameter
