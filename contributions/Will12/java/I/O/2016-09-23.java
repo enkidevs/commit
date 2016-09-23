cf136bbc863b2707579983eae451b8649a34e21f
Will12 <will12@gmail.com>
2016-09-23 17:17:02 +0100

    2016-09-23's java workout

---
+++ contributions/Will12/java/I/O/2016-09-23.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+Create system file paths using the `Path` class
+Listing a file system's root directories
+The `Console` class
