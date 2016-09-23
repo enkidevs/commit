4eef6a2c8bbb054d3fd56eeb2341033e03571278
GTAVbeastya <sastvns@gmail.com>
2016-09-23 03:27:49 +0100

    2016-09-22's java workout

---
+++ contributions/GTAVbeastya/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using an interface as a parameter
