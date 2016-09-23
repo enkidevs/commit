5499ccdac78a2ecc2cf4cfb2763af38e7f384392
artexercise <artexercise@yahoo.com>
2016-09-23 15:05:14 +0100

    2016-09-23's java workout

---
+++ contributions/artexercise/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
