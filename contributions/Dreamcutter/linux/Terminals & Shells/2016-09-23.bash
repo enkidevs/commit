9794319ae74298d99ed45f08fd39ab5254e6632e
Dreamcutter <ron323@web.de>
2016-09-23 15:23:48 +0100

    2016-09-23's linux workout

---
+++ contributions/Dreamcutter/linux/Terminals & Shells/2016-09-23.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
