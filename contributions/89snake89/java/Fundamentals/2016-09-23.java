a32e63d659e84de093fb8511682666bbb009e283
89snake89 <bottanfabio@hotmail.it>
2016-09-23 14:45:25 +0100

    2016-09-23's java workout

---
+++ contributions/89snake89/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Measuring time
+Using `synchronized` statements
+Using an interface as a parameter
