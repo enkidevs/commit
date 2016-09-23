2cb364cb006297e7f5a62807ca8591aed0645cc9
sandeshkaranam <sandeshkaranam1@gmail.com>
2016-09-23 03:22:32 +0100

    2016-09-22's java workout

---
+++ contributions/sandeshkaranam/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
