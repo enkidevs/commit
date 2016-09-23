3784d88a20764b5ca662128d1489a91d1ceed1d2
jaraksus <oleg.truhan789@gmail.com>
2016-09-23 13:54:15 +0100

    2016-09-23's linux workout

---
+++ contributions/jaraksus/linux/Terminals & Shells/2016-09-23.bash
@@ -0,0 +1,5 @@
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
