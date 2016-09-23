f2785fc5c4378786de95be7db79510b6492c9a82
aiduryagin <aiduryagih@gmail.com>
2016-09-23 19:58:57 +0100

    2016-09-23's javascript workout

---
+++ contributions/aiduryagin/javascript/Core/2016-09-23.js
@@ -0,0 +1,5 @@
+Types of numbers
+Using the double tilde `~~` 
+Functions return `undefined` by default
+Extra function arguments are undefined by default
+Prevent background image flicker in **IE**
