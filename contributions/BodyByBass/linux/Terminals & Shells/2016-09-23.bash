1d1f56e28691a108841abf63d4aa15cb426f39ca
BodyByBass <jrijo135@gmail.com>
2016-09-23 15:21:43 +0100

    2016-09-23's linux workout

---
+++ contributions/BodyByBass/linux/Terminals & Shells/2016-09-23.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Quick `bash` shortcuts 
+Conditional command execution
+
+(`&&` operator)
