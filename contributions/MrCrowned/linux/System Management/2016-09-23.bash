924fc416af0ba65ef4678a90dcf7789f29483477
MrCrowned <dom.brause@gmail.com>
2016-09-23 13:42:16 +0100

    2016-09-23's linux workout

---
+++ contributions/MrCrowned/linux/System Management/2016-09-23.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Commands to shutdown or restart the system
