55dc31dc3d5ce3f1e2642d2fe65f28d0c8fa08f6
chefsim <chefsim@hotmail.com>
2016-09-23 16:39:05 +0100

    2016-09-23's linux workout

---
+++ contributions/chefsim/linux/Terminals & Shells/2016-09-23.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
