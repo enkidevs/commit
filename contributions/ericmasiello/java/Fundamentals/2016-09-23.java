c5fb00007ec57290d027150d11051db30ad1957e
ericmasiello <ericjmemail@gmail.com>
2016-09-23 23:31:55 +0100

    2016-09-23's java workout

---
+++ contributions/ericmasiello/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Using an interface as a parameter
