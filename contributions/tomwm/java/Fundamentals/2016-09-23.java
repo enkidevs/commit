4094b899f106357b2d8d1e07880802f1f7efd115
tomwm <tom.marshall@hotmail.co.uk>
2016-09-23 14:16:45 +0100

    2016-09-23's java workout

---
+++ contributions/tomwm/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Locks in `static synchronized` methods
+Limit Accessibility of `Fields`
+Using an interface as a parameter
