d27ef4114f396fb61eb943957bebf361a7d10354
jareddecker1002 <jareddecker1002@gmail.com>
2016-09-23 03:36:28 +0100

    2016-09-22's linux workout

---
+++ contributions/jareddecker1002/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Cancel a system shutdown
