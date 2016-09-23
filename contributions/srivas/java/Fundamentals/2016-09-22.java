5232b689597aee4f57849300ed91e220b17f992b
srivas <sebastian.rivas.ch@gmail.com>
2016-09-23 03:43:51 +0100

    2016-09-22's java workout

---
+++ contributions/srivas/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
