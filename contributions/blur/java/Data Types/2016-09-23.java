32b754915c340cfd1621184a747cddcc7354a823
blur <filip@munk.pro>
2016-09-23 13:21:18 +0100

    2016-09-23's java workout

---
+++ contributions/blur/java/Data Types/2016-09-23.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Equals operation on different data types
+Retrieve the component type of an array
