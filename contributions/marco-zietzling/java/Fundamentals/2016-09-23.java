e6451b214fab49b87d3548ec0fc20878097295b2
marco-zietzling <marco.zietzling@gmail.com>
2016-09-23 08:17:28 +0100

    2016-09-23's java workout

---
+++ contributions/marco-zietzling/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Measuring time
+Increase `PermGen` space as to avoid `OutOfMemory` errors
