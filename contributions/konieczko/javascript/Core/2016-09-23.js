7847d580355f6c73c8c8bd3fda71fa9ea901e177
konieczko <pkonieczny91@gmail.com>
2016-09-23 16:43:45 +0100

    2016-09-23's javascript workout

---
+++ contributions/konieczko/javascript/Core/2016-09-23.js
@@ -0,0 +1,5 @@
+Keywords in hash keys
+Using the double tilde `~~` 
+Most efficient way to build `HTML` strings
+Data type comparison in `switch` statements
+Getting the *real* dimensions of an image
