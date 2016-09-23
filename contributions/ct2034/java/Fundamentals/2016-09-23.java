6904b2b2f56eb91fc1bf1ccfce14e311f2f0adc9
ct2034 <post@henkelchristian.de>
2016-09-23 07:01:17 +0100

    2016-09-23's java workout

---
+++ contributions/ct2034/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Using an interface as a parameter
