e3d324d450525b91b5f1e18259e2ba76c429a3b4
pedperpat <pedro-aperez1@hotmail.com>
2016-09-23 10:40:24 +0100

    2016-09-23's java workout

---
+++ contributions/pedperpat/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
