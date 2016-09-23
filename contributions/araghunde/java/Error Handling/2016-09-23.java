c1203a704faefb49d25eff3e7580d97c264d1744
araghunde <paybol.21@gmail.com>
2016-09-23 22:06:48 +0100

    2016-09-23's java workout

---
+++ contributions/araghunde/java/Error Handling/2016-09-23.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Prevent `exceptions` while logging Data
+Throwing an `exception`
+Careful Numeric Data Conversions
