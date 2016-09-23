7c52af583962c6c7ea75085616032cac2ff8fef3
robindevouge <robin.devouge@gmail.com>
2016-09-23 13:07:45 +0100

    2016-09-23's javascript workout

---
+++ contributions/robindevouge/javascript/Browser APIs/2016-09-23.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+Network Information API
+Fetch API
+MediaDevices.getUserMedia()
