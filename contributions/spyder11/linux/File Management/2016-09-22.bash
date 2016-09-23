163aa1e2d7f25528b9d0f585e130ba72c755f53f
spyder11 <spyder11@me.com>
2016-09-23 03:25:20 +0100

    2016-09-22's linux workout

---
+++ contributions/spyder11/linux/File Management/2016-09-22.bash
@@ -0,0 +1,7 @@
+Practical `du` command
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Deleting files in a secure manner
