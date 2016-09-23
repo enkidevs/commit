83248729eb2bf33378f042bf794a027f025013f4
bonArt <alien2711@yandex.ru>
2016-09-23 14:53:50 +0100

    2016-09-23's javascript workout

---
+++ contributions/bonArt/javascript/React/2016-09-23.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+Type of the Children props
+componentWillReceiveProps Not Triggered After Mounting
+
+Specify a single child
+Shortcut for transferring props
