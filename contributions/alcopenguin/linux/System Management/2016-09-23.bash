e71bf993c4c94799c5c2c3864b94a48dfd950eae
alcopenguin <morggoth70@gmail.com>
2016-09-23 20:25:31 +0100

    2016-09-23's linux workout

---
+++ contributions/alcopenguin/linux/System Management/2016-09-23.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
