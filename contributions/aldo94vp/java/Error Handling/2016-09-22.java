cb698022a3208b29fa099cf304753e498521ba50
aldo94vp <aldo94vp@gmail.com>
2016-09-23 03:23:40 +0100

    2016-09-22's java workout

---
+++ contributions/aldo94vp/java/Error Handling/2016-09-22.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Checking a double for `NaN` value
