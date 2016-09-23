9f236f90e5fd9be75f8a3124060415e2999c4462
marioso <hello@mariosommer.at>
2016-09-23 07:16:04 +0100

    2016-09-23's javascript workout

---
+++ contributions/marioso/javascript/Functions/2016-09-23.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Hoisting
+Functions can be declared after use
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
