a6f478dcbe33a3a94a0afb6e9ebf644190c35b4c
NotALamer <alvaro.movil.ms@gmail.com>
2016-09-23 10:55:13 +0100

    2016-09-23's linux workout

---
+++ contributions/NotALamer/linux/File Management/2016-09-23.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Preserving permissions and structure with `rsync`
+Working with `.gz` files
+Find the unknown process preventing deleting of files
+Easy way of sharing files
