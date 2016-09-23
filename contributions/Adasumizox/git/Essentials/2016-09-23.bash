7283a9a3e483f7210e0093a359067aae5d593041
Adasumizox <darkdan099@gmail.com>
2016-09-23 22:12:27 +0100

    2016-09-23's git workout

---
+++ contributions/Adasumizox/git/Essentials/2016-09-23.bash
@@ -0,0 +1,5 @@
+How to set your username and email
+Check the status of your files
+Use `short` status to make output more compact
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
