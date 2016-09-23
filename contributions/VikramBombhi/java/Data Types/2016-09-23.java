50cf485df21c95b078362613404a8a069892cd53
VikramBombhi <vbombhi@gmail.com>
2016-09-23 14:29:00 +0100

    2016-09-23's java workout

---
+++ contributions/VikramBombhi/java/Data Types/2016-09-23.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Use `Arrays.asList()` to initialise lists
+`StringBuffer` vs `StringBuilder`
+Do not attempt comparisons with NaN
