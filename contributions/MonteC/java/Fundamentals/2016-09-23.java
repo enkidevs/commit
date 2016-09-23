adcd488197ede12d70ae12bba19a69292b9186f6
MonteC <jarekucinski@gmail.com>
2016-09-23 21:06:23 +0100

    2016-09-23's java workout

---
+++ contributions/MonteC/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Using an interface as a parameter
