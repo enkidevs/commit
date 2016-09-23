7150d3829bca2ecfc84bb35b770ce4d05700bb6b
nimrodo <nimrodo@gmail.com>
2016-09-23 18:10:31 +0100

    2016-09-23's linux workout

---
+++ contributions/nimrodo/linux/System Management/2016-09-23.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Common log files
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
