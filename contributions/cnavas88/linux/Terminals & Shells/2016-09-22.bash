b3ed0f1419df29e9a1f32f875cc04807291c9ce3
cnavas88 <dante.maniacs@gmail.com>
2016-09-23 09:21:12 +0100

    2016-09-22's linux workout

---
+++ contributions/cnavas88/linux/Terminals & Shells/2016-09-22.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Breaking out of a terminal when `ssh` locks
+Retrieve previous arguments
+The Basics of Environment Variables
