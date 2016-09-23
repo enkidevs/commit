f20af2b509596d3fdec24399b141ff4b5a001acb
EnzDev <enzo@mallard.ml>
2016-09-23 08:16:21 +0100

    2016-09-23's linux workout

---
+++ contributions/EnzDev/linux/System Management/2016-09-23.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Fixing `locale` issues in Debian systems
