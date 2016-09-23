f5cdc90c8dda7b63f6885c3376b160674072e18a
zsolt.lovasz <zsolt.lovasz@gmail.com>
2016-09-23 15:25:55 +0100

    2016-09-23's linux workout

---
+++ contributions/zsolt.lovasz/linux/System Management/2016-09-23.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
