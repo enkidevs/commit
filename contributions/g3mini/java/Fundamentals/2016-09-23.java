8ed71ae09ba364ab4c5e85090faf3f6e10ca1067
g3mini <mcjanz@gmail.com>
2016-09-23 07:21:08 +0100

    2016-09-23's java workout

---
+++ contributions/g3mini/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
