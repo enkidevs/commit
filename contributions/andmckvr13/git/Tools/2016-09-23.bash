818c710872431d7ef279da9ae4cb277131065163
andmckvr13 <andmckvr@gmail.com>
2016-09-23 13:23:06 +0100

    2016-09-23's git workout

---
+++ contributions/andmckvr13/git/Tools/2016-09-23.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Interactively unstage changes
+Specify a range of commits using double dot syntax
+Sign your work (using GPG)
+Debug using binary search
