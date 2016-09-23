62e5d9f059d03f416d004fe7db2247df0d500773
sagiben <ilnextbus@gmail.com>
2016-09-23 05:12:38 +0100

    2016-09-23's linux workout

---
+++ contributions/sagiben/linux/System Management/2016-09-23.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
