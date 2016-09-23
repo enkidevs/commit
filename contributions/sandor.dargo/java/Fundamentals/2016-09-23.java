4fd18e0c167442f7ec370e5d265a866c6ff365a1
sandor.dargo <sandor.dargo@gmail.com>
2016-09-23 05:11:56 +0100

    2016-09-23's java workout

---
+++ contributions/sandor.dargo/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use strings in a `switch` statement
+Distinction between `public` and `private` methods
+Using `Printstream.format` to format a string before printing
+Constant notation
