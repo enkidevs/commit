5c1b864aee00a82e374f6825e233eba86e4e4894
claudiosegala <claudiosegalafilho@gmail.com>
2016-09-23 20:16:33 +0100

    2016-09-23's linux workout

---
+++ contributions/claudiosegala/linux/Terminals & Shells/2016-09-23.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
