8954d996a3c02e99b9ad27c38a04112147e99fa1
nether <kthhdz@gmail.com>
2016-09-23 02:34:33 +0100

    2016-09-22's linux workout

---
+++ contributions/nether/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
