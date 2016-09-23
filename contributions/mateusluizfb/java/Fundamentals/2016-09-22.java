5880097497aab3bb467077923ff10ed2f0e8eaf2
mateusluizfb <mateuslfreitasb@gmail.com>
2016-09-23 02:18:45 +0100

    2016-09-22's java workout

---
+++ contributions/mateusluizfb/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
