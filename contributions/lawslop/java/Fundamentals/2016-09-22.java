6f455ec9729e52f8b6867a8005a79edc95f03bd7
lawslop <lawslop@gmail.com>
2016-09-23 01:42:56 +0100

    2016-09-22's java workout

---
+++ contributions/lawslop/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Altering format string output by changing a format specifier's `argument_index`
+Using `Printstream.format` to format a string before printing
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
