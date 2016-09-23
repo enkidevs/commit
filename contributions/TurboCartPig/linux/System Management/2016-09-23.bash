b096391ae410d20d803be9495df431e9f0df3622
TurboCartPig <dennkris@live.no>
2016-09-23 21:51:44 +0100

    2016-09-23's linux workout

---
+++ contributions/TurboCartPig/linux/System Management/2016-09-23.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
