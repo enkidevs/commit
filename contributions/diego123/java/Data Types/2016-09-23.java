bc9fe0629b9906cc072d2c15c5467aee8af55117
diego123 <bartosz.szymanski1@gmail.com>
2016-09-23 08:09:48 +0100

    2016-09-23's java workout

---
+++ contributions/diego123/java/Data Types/2016-09-23.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Use `Arrays.asList()` to initialise lists
+String concatenation in Java
