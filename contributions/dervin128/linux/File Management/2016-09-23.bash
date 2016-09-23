c9db1e46c6706abc13e790a2974cec044d098a7c
dervin128 <trueegsegger@hotmail.com>
2016-09-23 10:41:11 +0100

    2016-09-23's linux workout

---
+++ contributions/dervin128/linux/File Management/2016-09-23.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Delete commands aliases
