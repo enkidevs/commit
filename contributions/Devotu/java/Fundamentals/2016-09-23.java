e09eed8226ad60a97c25d43d975a010b277c941f
Devotu <otuvesson@gmail.com>
2016-09-23 15:08:55 +0100

    2016-09-23's java workout

---
+++ contributions/Devotu/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
