c61abbe8ab25d6036eec9e5ac5f08838ec0262f3
binaryplease <binaryplease@gmail.com>
2016-09-23 09:06:27 +0100

    2016-09-23's linux workout

---
+++ contributions/binaryplease/linux/System Management/2016-09-23.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Using `dd` To Create Disk Images
