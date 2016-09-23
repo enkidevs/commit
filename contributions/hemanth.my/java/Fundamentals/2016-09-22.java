510a4413eea0c3d5dcb9bdd2e1c0776f33f65596
hemanth.my <hemanth.022@gmail.com>
2016-09-23 07:16:38 +0100

    2016-09-22's java workout

---
+++ contributions/hemanth.my/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Avoid using `static` variables
