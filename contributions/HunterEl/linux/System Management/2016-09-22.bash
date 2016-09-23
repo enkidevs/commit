4984ddcf66c2aaa492163b653dda3b85a7c88397
HunterEl <hunterelbourn@gmail.com>
2016-09-23 03:14:20 +0100

    2016-09-22's linux workout

---
+++ contributions/HunterEl/linux/System Management/2016-09-22.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+List current logged on users with `w`
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
