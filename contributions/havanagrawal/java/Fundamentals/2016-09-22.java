46f4810f73fa04a9db3db8df2df30387e5247529
havanagrawal <havanagrawal@gmail.com>
2016-09-23 02:29:16 +0100

    2016-09-22's java workout

---
+++ contributions/havanagrawal/java/Fundamentals/2016-09-22.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
