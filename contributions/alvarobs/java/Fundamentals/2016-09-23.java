87fd5698075be0ed7008ffff093f15a0669e2588
alvarobs <alvarobs@gmail.com>
2016-09-23 13:07:01 +0100

    2016-09-23's java workout

---
+++ contributions/alvarobs/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Calling one constructor from another
+Constant notation
