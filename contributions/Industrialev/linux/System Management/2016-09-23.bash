53eb512f9fbdadaf778757815019fec76ee905d7
Industrialev <industrialev@gmail.com>
2016-09-23 20:04:28 +0100

    2016-09-23's linux workout

---
+++ contributions/Industrialev/linux/System Management/2016-09-23.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
