4a7b3f06197e696876d29607698f617e808efb13
GoosvandenBekerom <goos.bekerom@gmail.com>
2016-09-23 15:15:07 +0100

    2016-09-23's java workout

---
+++ contributions/GoosvandenBekerom/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Measuring time
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
