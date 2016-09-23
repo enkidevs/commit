dac075cd84893fc84e350fd28052673401f04ee0
MosanZH <mosan_zh@163.com>
2016-09-23 01:54:34 +0100

    2016-09-22's java workout

---
+++ contributions/MosanZH/java/Error Handling/2016-09-22.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Creating new Exceptions
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
