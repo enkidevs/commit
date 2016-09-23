21cf1ff2c6a072966c6b304a53dd05140e372bc3
yang573 <yangyang1998@gmail.com>
2016-09-23 17:13:00 +0100

    2016-09-23's java workout

---
+++ contributions/yang573/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
