0686c9e0f99682225e036b76d7717248721e5e69
dan3k <eldan3k@gmail.com>
2016-09-23 11:31:50 +0100

    2016-09-23's java workout

---
+++ contributions/dan3k/java/Java 8/2016-09-23.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Using `Arrays.parallelSort` 
+How to check for static dependencies of applications and libraries from the command line.
+Control OS `Process`es in `Java8`
+Avoid numerical overflows with *Java8* `exact` methods.
