521ed8cb301ddecca07eefeb558c5760d6a14f7b
mads79 <mads@mo2tion.com>
2016-09-23 06:54:22 +0100

    2016-09-23's linux workout

---
+++ contributions/mads79/linux/Terminals & Shells/2016-09-23.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Random password generator
