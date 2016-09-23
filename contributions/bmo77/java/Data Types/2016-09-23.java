de299ccedca72c51b55beeb5520c397488e440a4
bmo77 <basha.others@gmail.com>
2016-09-23 11:45:59 +0100

    2016-09-23's java workout

---
+++ contributions/bmo77/java/Data Types/2016-09-23.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Retrieve the component type of an array
+Numeric Conversion - Widening
