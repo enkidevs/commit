f4b4dfb0ff95c7c02776d1b4b497ac14dc8d6a25
peleccom <peleccom@mail.ru>
2016-09-23 18:59:19 +0100

    2016-09-23's linux workout

---
+++ contributions/peleccom/linux/System Management/2016-09-23.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Check the HDD with `badblocks`
