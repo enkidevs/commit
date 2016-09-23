c614a3a1a0ca5fafd6cc64befa44fcecf58b2102
Kishore <kishorepremier@gmail.com>
2016-09-23 05:59:30 +0100

    2016-09-22's java workout

---
+++ contributions/Kishore/java/I/O/2016-09-22.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Checking for the existence of files and directories
+Reading file attributes
+Deleting files and directories
