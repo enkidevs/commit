636cf876309348016e5af6d67daed2d17112138b
wasteland540 <m_m_elz@web.de>
2016-09-24 19:33:21 +0100

    2016-09-24's java workout

---
+++ contributions/wasteland540/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
