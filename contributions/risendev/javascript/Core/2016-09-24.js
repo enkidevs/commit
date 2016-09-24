5170ea2e265136d226815dafb6b5dd6d6b0cb9ae
risendev <risendev@gmail.com>
2016-09-24 23:17:53 +0100

    2016-09-24's javascript workout

---
+++ contributions/risendev/javascript/Core/2016-09-24.js
@@ -0,0 +1,5 @@
+Format currency in JavaScript
+The difference between null, undefined and NaN
+Extra function arguments are undefined by default
+Detect online connection
+Detect **DO NOT TRACK** status
