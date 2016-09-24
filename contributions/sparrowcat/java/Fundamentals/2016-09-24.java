7a74f50b562936ffdbd6c2998834218c5295c4af
sparrowcat <silvia.knappe725@gmail.com>
2016-09-24 22:02:01 +0100

    2016-09-24's java workout

---
+++ contributions/sparrowcat/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Metadata: creating a user-defined file attribute
+Supply `toString()` in all classes
