bed9135ee5cbb0b20cb42a286142532f5337a73c
Asdamp <antonio.danilo92@gmail.com>
2016-09-24 23:15:54 +0100

    2016-09-24's java workout

---
+++ contributions/Asdamp/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
