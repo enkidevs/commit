df516458a2152234929cbe08891aec07f0c9f9ef
Vebble <serafin21@gmail.com>
2016-09-24 20:22:52 +0100

    2016-09-24's java workout

---
+++ contributions/Vebble/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Limit Accessibility of `Fields`
+Using an interface as a parameter
