cf51cd4f3a0de53e51c4a3f0fc19c460157de543
hyftar <simon_landry@ymail.com>
2016-09-24 05:31:29 +0100

    2016-09-23's linux workout

---
+++ contributions/hyftar/linux/File Management/2016-09-23.bash
@@ -0,0 +1,6 @@
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Deleting non-empty directories
