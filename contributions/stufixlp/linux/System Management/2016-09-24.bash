c2fc62550784e9104aaa31a0443b71b72e4a5650
stufixlp <stufixlp@gmail.com>
2016-09-24 20:57:45 +0100

    2016-09-24's linux workout

---
+++ contributions/stufixlp/linux/System Management/2016-09-24.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
