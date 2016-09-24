acdba4c533b4d59b63aac3001b48b8bb03c4dbed
miky90 <miky9090@gmail.com>
2016-09-24 07:07:06 +0100

    2016-09-24's git workout

---
+++ contributions/miky90/git/Essentials/2016-09-24.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to unmodify a modified file
+Pushing tags to a server
+How to set your username and email
+Remote repositories: viewing, editing and deleting
