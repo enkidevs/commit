9d820755d8d986f13b3bb8ebb17aa2ffbcf85a30
mahoalax <marie.laxenaire@webedia-group.com>
2016-09-25 00:28:47 +0100

    2016-09-24's java workout

---
+++ contributions/mahoalax/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
