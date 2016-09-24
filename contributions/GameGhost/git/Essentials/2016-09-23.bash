ec8d4dbcaee416601bd5234ff765495d8bcac29f
GameGhost <gameghost@bigfoot.com>
2016-09-24 02:25:47 +0100

    2016-09-23's git workout

---
+++ contributions/GameGhost/git/Essentials/2016-09-23.bash
@@ -0,0 +1,5 @@
+Adding a remote repository
+How to set your username and email
+Make your log output pretty
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
