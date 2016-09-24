5b544ea73a96329ba16c893d91e943e41d08f230
gadzhibala <gadzhi@yandex.ru>
2016-09-25 00:06:33 +0100

    2016-09-24's linux workout

---
+++ contributions/gadzhibala/linux/System Management/2016-09-24.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
