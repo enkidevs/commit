73ecc352b103ba6c8ed069921da70be5ac934f37
Intersys <int3rsys@live.com>
2016-09-24 07:44:54 +0100

    2016-09-24's linux workout

---
+++ contributions/Intersys/linux/Networking/2016-09-24.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Check `iptables` firewall status
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Short Introduction to `Wget` 
