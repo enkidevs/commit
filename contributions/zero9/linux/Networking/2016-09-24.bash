5149f76f3cf956e01fa95f17d79bef2880cf2a2d
zero9 <c.lobrano@gmail.com>
2016-09-24 19:48:12 +0100

    2016-09-24's linux workout

---
+++ contributions/zero9/linux/Networking/2016-09-24.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Logging dropped firewall packets
+Find services running on your host
+Bandwidth monitoring tools 
+Use `tcpdump` to listen to network interface traffic
