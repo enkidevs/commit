037da1f089dc0c39effdb84f0260c1a27ec9b336
fredng92 <ngjq2@hotmail.com>
2016-09-24 09:09:20 +0100

    2016-09-23's python workout

---
+++ contributions/fredng92/python/Data Structures/2016-09-23.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`Dictionary` view objects
+Double ended queues with `deque`
+Best way to implement a simple `queue` 
+Immutable sets with `frozenset`
