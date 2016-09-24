72a5b8cddbee4c1a4f3d720334383e65cfcafe01
inibg <inibg2@gmail.com>
2016-09-24 22:58:58 +0100

    2016-09-24's java workout

---
+++ contributions/inibg/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
