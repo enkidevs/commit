4a9516a3a7ef9751b03f0ef67cd7bd6601d454bc
rockingshubh1994 <rockingshubh1994@gmail.com>
2016-09-24 10:30:40 +0100

    2016-09-23's java workout

---
+++ contributions/rockingshubh1994/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Supply `toString()` in all classes
+Using an interface as a parameter
