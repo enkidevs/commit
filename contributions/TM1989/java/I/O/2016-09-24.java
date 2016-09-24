c6341d2b25900c12628751de333be309ce63d4d9
TM1989 <torben.moeller@gmx.net>
2016-09-25 00:48:56 +0100

    2016-09-24's java workout

---
+++ contributions/TM1989/java/I/O/2016-09-24.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Checking for the existence of files and directories
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
