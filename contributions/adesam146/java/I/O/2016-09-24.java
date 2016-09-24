207ee58147d962251a76a442be419c530287db5d
adesam146 <a.samuel.o@hotmail.co.uk>
2016-09-24 14:51:43 +0100

    2016-09-24's java workout

---
+++ contributions/adesam146/java/I/O/2016-09-24.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+How to create directories in Java
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
