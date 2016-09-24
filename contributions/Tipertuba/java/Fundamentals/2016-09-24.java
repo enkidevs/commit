34b795e9cae5d8f61ea20d519b9c31510c2536e5
Tipertuba <tipertuba@gmail.com>
2016-09-24 14:28:39 +0100

    2016-09-24's java workout

---
+++ contributions/Tipertuba/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
