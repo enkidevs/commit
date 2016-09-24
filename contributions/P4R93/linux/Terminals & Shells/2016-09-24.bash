45bddd59a6fd68ce82f6467032883ae8a362e8b8
P4R93 <p4r.esp@gmail.com>
2016-09-24 22:07:25 +0100

    2016-09-24's linux workout

---
+++ contributions/P4R93/linux/Terminals & Shells/2016-09-24.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+The Basics of Environment Variables
