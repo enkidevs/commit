297be225d331e38876c236857b65d7860de5dcb4
zanesalvatore <zanesalvatore@gmail.com>
2016-09-25 00:45:57 +0100

    2016-09-24's linux workout

---
+++ contributions/zanesalvatore/linux/Networking/2016-09-24.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Limit the `wget` download rate
+List your IPv6 configuration
+Bandwidth monitoring tools 
+Staying connected remotely via SSH without an interactive shell
