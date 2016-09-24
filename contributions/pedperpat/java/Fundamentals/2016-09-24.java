0955041eb08d7826a00077d9b5f68c5e00737ef1
pedperpat <pedro-aperez1@hotmail.com>
2016-09-24 03:53:57 +0100

    2016-09-24's java workout

---
+++ contributions/pedperpat/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Metadata: setting a file's owner
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
