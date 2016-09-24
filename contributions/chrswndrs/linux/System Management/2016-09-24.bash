300c2b10a3e51e0d6597bc592d833b3ee1feaf49
chrswndrs <christoph.wanders@gmail.com>
2016-09-24 23:40:28 +0100

    2016-09-24's linux workout

---
+++ contributions/chrswndrs/linux/System Management/2016-09-24.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
