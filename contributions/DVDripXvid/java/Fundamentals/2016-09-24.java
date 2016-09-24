78abd40c10bb28d49d10c93d9833d7d479cd4a3e
DVDripXvid <dvdrip.xvid.avi@gmail.com>
2016-09-24 10:09:08 +0100

    2016-09-24's java workout

---
+++ contributions/DVDripXvid/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+Using an interface as a parameter
