8ab73876fb05e55767aceb408768fedad61151b9
aklefdal <alf.kare@lefdal.cc>
2016-09-24 08:29:17 +0100

    2016-09-24's javascript workout

---
+++ contributions/aklefdal/javascript/Functions/2016-09-24.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function constructor vs. function declaration vs. function expression
+Method chaining
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
