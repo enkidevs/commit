2850b4e1d389894a71da1271ccb97bb02321a3a7
sky2826 <aakashtiwari03@gmail.com>
2016-09-24 20:15:09 +0100

    2016-09-23's linux workout

---
+++ contributions/sky2826/linux/File Management/2016-09-23.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
+Find the unknown process preventing deleting of files
+Easy way of sharing files
