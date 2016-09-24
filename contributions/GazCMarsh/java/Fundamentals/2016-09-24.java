902cef0937a812273cf27f727e32f78509fa2799
GazCMarsh <gazmarsh@meaningfulname.net>
2016-09-24 09:07:16 +0100

    2016-09-24's java workout

---
+++ contributions/GazCMarsh/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Measuring time
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
