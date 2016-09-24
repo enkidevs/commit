60a16b25c11d677b7edbe0d5996b803156f894ff
liko28s <liko28s@gmail.com>
2016-09-24 15:09:53 +0100

    2016-09-24's java workout

---
+++ contributions/liko28s/java/Error Handling/2016-09-24.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Prevent `exceptions` while logging Data
+Careful Numeric Data Conversions
