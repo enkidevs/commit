abdb4ae149f40cc5ceafbd1f0dfa5a69ef5f6abb
AndriyPolukhin <andriy.polukhin@gmail.com>
2016-09-24 19:57:59 +0100

    2016-09-24's javascript workout

---
+++ contributions/AndriyPolukhin/javascript/React/2016-09-24.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+componentWillReceiveProps Not Triggered After Mounting
+
+Dangerously set `innerHTML`
+Keyed fragments in **React**
+Validate for required props
