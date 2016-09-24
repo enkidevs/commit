b0cb9e9017938b48a0807a2bc183f1dde97f0ced
pigion138138 <kylemoran06@gmail.com>
2016-09-24 21:47:56 +0100

    2016-09-24's linux workout

---
+++ contributions/pigion138138/linux/System Management/2016-09-24.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Common log files
