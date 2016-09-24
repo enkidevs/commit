59122e061a82607cdd06f29b19fd804450f160dd
stufixlp <stufixlp@gmail.com>
2016-09-24 02:09:51 +0100

    2016-09-23's java workout

---
+++ contributions/stufixlp/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Distinction between `public` and `private` methods
+How range operations work
