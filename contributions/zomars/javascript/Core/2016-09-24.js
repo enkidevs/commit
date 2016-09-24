b912ebe0f30fdf4b5ffe901c8fdd40345dc867ad
zomars <zomars@me.com>
2016-09-24 20:49:18 +0100

    2016-09-24's javascript workout

---
+++ contributions/zomars/javascript/Core/2016-09-24.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Weak vs Strict equality operator
+Most efficient way to build `HTML` strings
+Meaning of polyfill
+Getting the *real* dimensions of an image
