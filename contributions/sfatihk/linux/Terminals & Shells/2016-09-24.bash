6c5d3e997d85f6f3c72914aa2b6c490ec0172d99
sfatihk <sakkarmen@gmail.com>
2016-09-24 23:03:48 +0100

    2016-09-24's linux workout

---
+++ contributions/sfatihk/linux/Terminals & Shells/2016-09-24.bash
@@ -0,0 +1,7 @@
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
