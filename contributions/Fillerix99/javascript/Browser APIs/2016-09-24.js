d4fef9d216493c55eaf037f61464b924de979e32
Fillerix99 <fillerix@fillerix.se>
2016-09-24 20:37:51 +0100

    2016-09-24's javascript workout

---
+++ contributions/Fillerix99/javascript/Browser APIs/2016-09-24.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+Form a `URL` from its parts 
+Navigating the browser history
+Navigation Timing API
