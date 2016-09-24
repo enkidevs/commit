79e9a8e38b73688de060fe4aab312092dc54af72
HoodieCollin <collinkokotas@gmail.com>
2016-09-24 05:40:37 +0100

    2016-09-23's javascript workout

---
+++ contributions/HoodieCollin/javascript/Core/2016-09-23.js
@@ -0,0 +1,5 @@
+Types of numbers
+Most efficient way to build `HTML` strings
+Hoisting applies only to variable declarations, not initializations
+`catch` is block scoped
+Detect **DO NOT TRACK** status
