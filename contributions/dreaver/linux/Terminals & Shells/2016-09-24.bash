6786141e7ba9287185f33e4071b463a02a03b64d
dreaver <jmribeiro77209@gmail.com>
2016-09-24 16:53:58 +0100

    2016-09-24's linux workout

---
+++ contributions/dreaver/linux/Terminals & Shells/2016-09-24.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Quick `bash` shortcuts 
+Terminal based browser
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
