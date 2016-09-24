25b9ea7516b54ec013bab5e425986f80cba3816d
Karthikraja34 <rajakarthik131@gmail.com>
2016-09-24 14:04:00 +0100

    2016-09-23's javascript workout

---
+++ contributions/Karthikraja34/javascript/Functions/2016-09-23.js
@@ -0,0 +1,5 @@
+Hoisting
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Easily generate a random `HEX` color
+Changing a functions context with `fn.call(object)`
