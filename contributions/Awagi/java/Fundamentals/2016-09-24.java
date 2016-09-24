530fa272a5b68a431e72889e41291d73dab74f8f
Awagi <scott.awagi@gmail.com>
2016-09-24 07:37:44 +0100

    2016-09-24's java workout

---
+++ contributions/Awagi/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
