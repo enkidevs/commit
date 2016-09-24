9b18c4a76abf18e22ab227c2e59f8ef962afcca3
MrCrowned <dom.brause@gmail.com>
2016-09-24 14:53:12 +0100

    2016-09-24's linux workout

---
+++ contributions/MrCrowned/linux/File Management/2016-09-24.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Deleting files in a secure manner
+Deleting non-empty directories
+Delete commands aliases
