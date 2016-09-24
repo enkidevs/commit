988156378e54e70c42ffa0157481c0c8dc650435
tawe <munn.jon@gmail.com>
2016-09-24 02:29:45 +0100

    2016-09-23's javascript workout

---
+++ contributions/tawe/javascript/React/2016-09-23.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+componentWillReceiveProps Not Triggered After Mounting
+
+Keyed fragments in **React**
+Enhanced Prop Validation 
+Shortcut for transferring props
