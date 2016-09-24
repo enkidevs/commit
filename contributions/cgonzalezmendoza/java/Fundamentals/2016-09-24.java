80c35525bbba6c333d088f5759ddb8f1bc7465b5
cgonzalezmendoza <gzz.carlos.e@gmail.com>
2016-09-24 15:38:04 +0100

    2016-09-24's java workout

---
+++ contributions/cgonzalezmendoza/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
