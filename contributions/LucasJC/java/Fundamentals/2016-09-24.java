ebf1c536b9e222a800db1ddf02f291d4aa4daa9b
LucasJC <lucas.couchot@gmail.com>
2016-09-24 23:04:32 +0100

    2016-09-24's java workout

---
+++ contributions/LucasJC/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Avoid using `static` variables
