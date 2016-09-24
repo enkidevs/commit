a346c74119c55793e38cea2d787db005f270104c
aufbakanleitung <hvdveer@gmail.com>
2016-09-24 08:14:39 +0100

    2016-09-24's java workout

---
+++ contributions/aufbakanleitung/java/Data Types/2016-09-24.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Converting a string to upper or lower case
+Finding a substring in a string
+Do not attempt comparisons with NaN
