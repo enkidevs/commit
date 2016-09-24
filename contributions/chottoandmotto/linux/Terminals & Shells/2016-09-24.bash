5605e3c9479947cd90dc446528d07ac9341e799c
chottoandmotto <info@chottoandmotto.com>
2016-09-25 00:17:35 +0100

    2016-09-24's linux workout

---
+++ contributions/chottoandmotto/linux/Terminals & Shells/2016-09-24.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Find any Unix / Linux command
+Random password generator
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
