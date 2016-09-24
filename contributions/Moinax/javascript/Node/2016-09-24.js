ae893d9b3d2cb9e4b757a29a0358167c2450de30
Moinax <jerome@moinax.com>
2016-09-24 19:37:19 +0100

    2016-09-24's javascript workout

---
+++ contributions/Moinax/javascript/Node/2016-09-24.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Wrapping errors in Node.js using _node-verror_
+`process.nextTick()` callback pattern
+Bulk-Write in **Node** with `cork()`
+`try-catch` only for **sync** code
