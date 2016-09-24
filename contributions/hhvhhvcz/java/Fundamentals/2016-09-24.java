69560bc8ea7e9f574673cbaacc9fda8beffb6497
hhvhhvcz <hhvhhvcz@gmail.com>
2016-09-24 20:09:30 +0100

    2016-09-24's java workout

---
+++ contributions/hhvhhvcz/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
