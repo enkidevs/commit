fd4dcc3f5d0fa14e84d922705d61e97bb88514c6
StackOverflow <nuti1996@gmail.com>
2016-09-24 11:10:17 +0100

    2016-09-24's java workout

---
+++ contributions/StackOverflow/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+Supply `toString()` in all classes
