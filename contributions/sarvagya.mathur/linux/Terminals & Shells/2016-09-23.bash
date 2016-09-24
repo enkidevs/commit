be78b2457f107fe77fc3da99dfccf8700fd220a5
sarvagya.mathur <mathur.sarvagya@gmail.com>
2016-09-24 05:21:00 +0100

    2016-09-23's linux workout

---
+++ contributions/sarvagya.mathur/linux/Terminals & Shells/2016-09-23.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal incognito mode
+Sequential execution using the `;` statement separator
