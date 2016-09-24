3ada4d9f6adb0191168b6aec7087dd43ceccd415
himanshurawlani <himanshurawlani@hotmail.com>
2016-09-24 22:11:32 +0100

    2016-09-23's java workout

---
+++ contributions/himanshurawlani/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
