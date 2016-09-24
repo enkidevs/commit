b4a5507af416bc727596bb28032ffb4129e3fca3
TomRaven <16lazart@elmbrookstudents.org>
2016-09-24 11:41:06 +0100

    2016-09-24's java workout

---
+++ contributions/TomRaven/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
