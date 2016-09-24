b867f404a638de079bab20cb4550e2a60085ff5e
francescolucchi <francescolucchi@gmail.com>
2016-09-24 22:30:12 +0100

    2016-09-24's java workout

---
+++ contributions/francescolucchi/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
