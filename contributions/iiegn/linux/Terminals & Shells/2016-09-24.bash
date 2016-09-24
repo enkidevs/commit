be9a32c9ffb49cc6bfd090f1855531744e435ced
iiegn <github@iiegn.de>
2016-09-24 08:04:16 +0100

    2016-09-24's linux workout

---
+++ contributions/iiegn/linux/Terminals & Shells/2016-09-24.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Useful aliasing in bash
+Random password generator
+Adding directories to your `$PATH`
