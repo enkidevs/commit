9190adfb0de6c3a1f67f172d000e06cf5492f2b6
PredatorFeesh <zatbuilder@gmail.com>
2016-09-24 17:31:01 +0100

    2016-09-24's linux workout

---
+++ contributions/PredatorFeesh/linux/System Management/2016-09-24.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Get hardware stack details with `lspci`
