1ed68b09a6a7b0d304f5014fc0f95f51e9cabd25
codeinvain <daniel@dnastudio.co.il>
2016-09-24 06:31:20 +0100

    2016-09-24's linux workout

---
+++ contributions/codeinvain/linux/Terminals & Shells/2016-09-24.bash
@@ -0,0 +1,5 @@
+Aliasing ssh connections
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
