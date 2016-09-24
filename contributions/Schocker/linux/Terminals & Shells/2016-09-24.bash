6c06bfbd08c1a005b44f6aed6ae83a681e282f54
Schocker <adikorzeniowski@gmail.com>
2016-09-25 00:32:50 +0100

    2016-09-24's linux workout

---
+++ contributions/Schocker/linux/Terminals & Shells/2016-09-24.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
