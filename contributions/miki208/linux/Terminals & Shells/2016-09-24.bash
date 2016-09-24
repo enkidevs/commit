8217afa637f5d3b5dad3c985a82edfdc707f3fee
miki208 <miloss208@gmail.com>
2016-09-24 22:42:27 +0100

    2016-09-24's linux workout

---
+++ contributions/miki208/linux/Terminals & Shells/2016-09-24.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Aliasing ssh connections
+Useful aliasing in bash
