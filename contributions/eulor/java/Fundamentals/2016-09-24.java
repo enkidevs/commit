5d20005ebd1ddcfb99422cc5bd6cb0ad292508cc
eulor <yanik.wechsler@gmail.com>
2016-09-24 11:46:10 +0100

    2016-09-24's java workout

---
+++ contributions/eulor/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
