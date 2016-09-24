e9c76bff94f62c22afcad3c382460a844793b3bf
ClarkMitchell <clarkcsmitchell@gmail.com>
2016-09-24 03:20:41 +0100

    2016-09-23's linux workout

---
+++ contributions/ClarkMitchell/linux/System Management/2016-09-23.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Detect your linux distribution
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
