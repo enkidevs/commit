e491da4c224d731508d24261a9adcc6ed4922587
Sanyo <micmark6@gmail.com>
2016-09-24 05:44:11 +0100

    2016-09-24's java workout

---
+++ contributions/Sanyo/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Overloading Methods in Java
+Catch multiple exceptions in a single `catch` block
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
