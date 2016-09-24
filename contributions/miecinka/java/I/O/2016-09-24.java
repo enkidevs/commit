daecf211c3203fe1099a7922ec8154e737046dab
miecinka <mietinhovanish@gmail.com>
2016-09-24 17:51:09 +0100

    2016-09-24's java workout

---
+++ contributions/miecinka/java/I/O/2016-09-24.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Reading and writing text files
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
+The `Console` class
