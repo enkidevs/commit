a4f774266e3d759b33e7e0ca684946193ce69c4b
bashtovoy <topflasher@gmail.com>
2016-09-24 13:50:01 +0100

    2016-09-24's javascript workout

---
+++ contributions/bashtovoy/javascript/Node/2016-09-24.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+First-error callbacks in **Node**
+The built-in Node debugger
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
