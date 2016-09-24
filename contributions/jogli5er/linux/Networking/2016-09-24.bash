b3463093c89b44435037d4edc65849d411ce6961
jogli5er <roman.brunner94@gmail.com>
2016-09-24 14:29:51 +0100

    2016-09-24's linux workout

---
+++ contributions/jogli5er/linux/Networking/2016-09-24.bash
@@ -0,0 +1,5 @@
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
+Bandwidth monitoring tools 
+Make use of `netstat`
+Staying connected remotely via SSH without an interactive shell
