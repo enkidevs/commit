275dd8f270032695608b38b54156b45526531272
agrimsachdeva <as2936@njit.edu>
2016-09-24 02:15:51 +0100

    2016-09-23's java workout

---
+++ contributions/agrimsachdeva/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Metadata: setting a file's owner
+Measuring time
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
