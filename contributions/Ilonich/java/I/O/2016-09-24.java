95f6892c81f74013f338d9ac551674b599e00237
Ilonich <niko-ikko@yandex.ru>
2016-09-24 10:37:07 +0100

    2016-09-24's java workout

---
+++ contributions/Ilonich/java/I/O/2016-09-24.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+List contents of a directory with `newDirectoryStream`
