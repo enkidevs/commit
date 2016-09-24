356f4800f9c852b5c2e2ae222106505c07604606
Lizander <kevin9075@gmail.com>
2016-09-24 13:21:29 +0100

    2016-09-24's linux workout

---
+++ contributions/Lizander/linux/Terminals & Shells/2016-09-24.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Random password generator
+`else` statements using the `||` operator 
