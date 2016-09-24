20c6ae9c78cb633643f0e66c57457be667e0d957
dantasse <dan.tasse@gmail.com>
2016-09-24 19:49:43 +0100

    2016-09-24's javascript workout

---
+++ contributions/dantasse/javascript/Node/2016-09-24.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Wrapping errors in Node.js using _node-verror_
+Handle `JSON.parse` error in Node.js
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
