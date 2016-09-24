0158f488eb3c02be0fa0c444d5c065ef2dfa923a
vpinna80 <valentino.pinna@bancaditalia.it>
2016-09-24 08:37:08 +0100

    2016-09-24's linux workout

---
+++ contributions/vpinna80/linux/Package Management/2016-09-24.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+Installing a `.deb` package from the terminal
+Using `Tasksel` for software installation
+Keeping repos updated with `cron`
