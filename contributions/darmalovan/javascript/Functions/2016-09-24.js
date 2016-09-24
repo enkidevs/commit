b44df004f4488667ae4d895e81421f27b8b127e4
darmalovan <darmalovan@gmail.com>
2016-09-24 16:44:04 +0100

    2016-09-24's javascript workout

---
+++ contributions/darmalovan/javascript/Functions/2016-09-24.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Function call method
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
