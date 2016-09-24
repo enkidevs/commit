13cd456003566456b91d3188473bd82a29158352
james-current <jcurrent01@gmail.com>
2016-09-24 16:04:38 +0100

    2016-09-24's linux workout

---
+++ contributions/james-current/linux/System Management/2016-09-24.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
