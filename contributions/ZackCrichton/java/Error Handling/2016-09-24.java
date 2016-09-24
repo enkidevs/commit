46e552bad347c26037df75bab745b1299d181e10
ZackCrichton <zackcrichton@hotmail.co.uk>
2016-09-24 16:13:19 +0100

    2016-09-24's java workout

---
+++ contributions/ZackCrichton/java/Error Handling/2016-09-24.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Throwing an `exception`
