8bf413665695f47bfb4ddf7ee706b7d248debc18
Kurre <baijeri@gmail.com>
2016-09-24 21:37:15 +0100

    2016-09-24's linux workout

---
+++ contributions/Kurre/linux/Networking/2016-09-24.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Check `iptables` firewall status
+Get real network statistics with `slurm`
+Limit the `wget` download rate
+Staying connected remotely via SSH without an interactive shell
