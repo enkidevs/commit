a07418d1d629c9170d493411031291189d46886a
BitPhinix <meier.eric2@t-online.de>
2016-09-24 10:30:58 +0100

    2016-09-24's java workout

---
+++ contributions/BitPhinix/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
