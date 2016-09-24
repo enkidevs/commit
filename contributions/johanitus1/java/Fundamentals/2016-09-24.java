019d6c33ef4769be5262c7a982b63a134728276e
johanitus1 <joan.vinyals@est.fib.upc.edu>
2016-09-24 18:56:44 +0100

    2016-09-24's java workout

---
+++ contributions/johanitus1/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
