6253f0b469c19e71ec4ff09c7b32334199778511
matheusmco <matheus0mco@gmail.com>
2016-09-24 12:06:11 +0100

    2016-09-24's java workout

---
+++ contributions/matheusmco/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Measuring time
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
