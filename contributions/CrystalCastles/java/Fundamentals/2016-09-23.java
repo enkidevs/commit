1b66e864987a4e358d1f3d7a643136da594a8610
CrystalCastles <danielpichardo716@gmail.com>
2016-09-24 02:40:49 +0100

    2016-09-23's java workout

---
+++ contributions/CrystalCastles/java/Fundamentals/2016-09-23.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
