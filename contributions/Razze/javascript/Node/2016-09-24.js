a6ec55de8d77dabe61b768e5bd1b14e13f33e24c
Razze <razzeee@gmail.com>
2016-09-24 23:03:06 +0100

    2016-09-24's javascript workout

---
+++ contributions/Razze/javascript/Node/2016-09-24.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
+`exports` vs. `module.exports` in **Node**
