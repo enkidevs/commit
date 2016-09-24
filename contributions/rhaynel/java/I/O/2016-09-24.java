d0b6873cd7cd9d5754a729b24c45c802ef7044a2
rhaynel <me@rhynl.io>
2016-09-24 19:29:49 +0100

    2016-09-24's java workout

---
+++ contributions/rhaynel/java/I/O/2016-09-24.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Reading file attributes
