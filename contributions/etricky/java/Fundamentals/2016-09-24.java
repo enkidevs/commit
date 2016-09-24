cbb7ff27c2b29304a34bd5add3e65183ff5c66b4
etricky <etricky@gmail.com>
2016-09-24 13:01:57 +0100

    2016-09-24's java workout

---
+++ contributions/etricky/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
