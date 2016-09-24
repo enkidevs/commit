e0ba135dce4dcdd2f6817650b44817f6dc9fb9aa
lilwayne1556 <lilwayne1556@gmail.com>
2016-09-24 03:07:40 +0100

    2016-09-23's linux workout

---
+++ contributions/lilwayne1556/linux/File Management/2016-09-23.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Working with `.gz` files
+Find the unknown process preventing deleting of files
