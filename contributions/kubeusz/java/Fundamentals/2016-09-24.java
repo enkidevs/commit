1ad5b99db1928ecf273879ed9f5c445c68db2ebf
kubeusz <jakubtrzcinka@gmail.com>
2016-09-24 11:21:45 +0100

    2016-09-24's java workout

---
+++ contributions/kubeusz/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Measuring time
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
