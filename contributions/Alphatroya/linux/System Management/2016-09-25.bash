6ad48222365ee33a24d969a7f83402c6437978a6
Alphatroya <alphatroya@gmail.com>
2016-09-25 10:04:42 +0100

    2016-09-25's linux workout

---
+++ contributions/Alphatroya/linux/System Management/2016-09-25.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Detect your linux distribution
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
