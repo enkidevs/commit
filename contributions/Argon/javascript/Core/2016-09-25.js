8e9950f971250368806503273425cd6261b9bd1b
Argon <radek.ermel@gmail.com>
2016-09-25 14:02:24 +0100

    2016-09-25's javascript workout

---
+++ contributions/Argon/javascript/Core/2016-09-25.js
@@ -0,0 +1,5 @@
+Deleting properties
+Treating a boolean as number
+Hoisting applies only to variable declarations, not initializations
+Extra function arguments are undefined by default
+Getting the *real* dimensions of an image
