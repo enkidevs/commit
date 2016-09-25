a76b84ae48cc1e01e6cdeda2ea751f99b90129fc
eduardopeixe <walthfish@yahoo.com>
2016-09-25 18:09:32 +0100

    2016-09-25's javascript workout

---
+++ contributions/eduardopeixe/javascript/Functions/2016-09-25.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Get query/url variables
+Easily generate a random `HEX` color
+Function call method
+Changing a functions context with `fn.call(object)`
