352397b91e5bf9de6a216fa0cd23ce2692e00a0c
alakhame <ndiaye.khadim.inpt@gmail.com>
2016-09-25 22:49:02 +0100

    2016-09-25's java workout

---
+++ contributions/alakhame/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
