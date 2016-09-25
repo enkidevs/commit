3d0fbe1bc913cc38a9298537261e519308a293d7
grabowskipawel <grabowskipawel91@gmail.com>
2016-09-25 17:21:04 +0100

    2016-09-25's javascript workout

---
+++ contributions/grabowskipawel/javascript/Core/2016-09-25.js
@@ -0,0 +1,5 @@
+Keywords in hash keys
+Most efficient way to build `HTML` strings
+Hoisting applies only to variable declarations, not initializations
+Prevent background image flicker in **IE**
+Detect an error type
