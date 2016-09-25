63a08abfb32a83b0d9e45bcc2b2938d6849b43b7
JordanDuncan <jrdndncn@gmail.com>
2016-09-25 11:03:51 +0100

    2016-09-25's java workout

---
+++ contributions/JordanDuncan/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
