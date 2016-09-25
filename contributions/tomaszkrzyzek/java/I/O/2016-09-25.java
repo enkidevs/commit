5e65ecbedcfa340d17d729ae32bb69770fb82402
tomaszkrzyzek <tomasz.krzyzek@gmail.com>
2016-09-25 02:53:12 +0100

    2016-09-25's java workout

---
+++ contributions/tomaszkrzyzek/java/I/O/2016-09-25.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+How to list all files in a directory that match a filename extension in Java
+Using buffered streams
+Translating `Scanner` tokens into primitive types
