35d4ed300c20bb22347c887dbaaf7e275309f8c6
bzpuerta <beatriz.puerta.hoyas@gmail.com>
2016-09-25 01:02:15 +0100

    2016-09-24's java workout

---
+++ contributions/bzpuerta/java/I/O/2016-09-24.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Create system file paths using the `Path` class
+Reading and writing text files
+Checking for the existence of files and directories
+Retrieving file store attributes
