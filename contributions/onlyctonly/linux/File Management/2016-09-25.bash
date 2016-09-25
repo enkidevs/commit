55beaea2891aa8b707e6d6cbb89c095d1bf66ccf
onlyctonly <onlyctonly@icloud.com>
2016-09-25 16:22:10 +0100

    2016-09-25's linux workout

---
+++ contributions/onlyctonly/linux/File Management/2016-09-25.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
