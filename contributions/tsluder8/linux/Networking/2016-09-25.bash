545126f30cdbffd3afa3203046ff9d928367266e
tsluder8 <tsluder8@gmail.com>
2016-09-25 20:28:12 +0100

    2016-09-25's linux workout

---
+++ contributions/tsluder8/linux/Networking/2016-09-25.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+Multiple IP Address with aliasing 
+Disable `IPv6`
+Get real network statistics with `slurm`
+Staying connected remotely via SSH without an interactive shell
