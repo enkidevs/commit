ad00a77f72c0b9109914ad2218d11c4222417abb
tamchow <chowtam8991@gmail.com>
2016-09-25 14:21:22 +0100

    2016-09-24's java workout

---
+++ contributions/tamchow/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
