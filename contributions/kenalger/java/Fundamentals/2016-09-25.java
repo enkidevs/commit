412da40eb0f3b368979f144ef4a68a7828fcbfaf
kenalger <kenalger@comcast.net>
2016-09-25 16:51:05 +0100

    2016-09-25's java workout

---
+++ contributions/kenalger/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The trade-off between short and descriptive names
+Using the `Console`
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
