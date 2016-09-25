18c88f94e6dc0d4ded63fc016425d7a3bc166d2c
gardir <gardiz@gmail.com>
2016-09-25 18:14:28 +0100

    2016-09-25's java workout

---
+++ contributions/gardir/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
