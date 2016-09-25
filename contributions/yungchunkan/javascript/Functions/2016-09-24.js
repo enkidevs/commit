c324edb6d2bf0d556fe66a47fb9babcbe5a58f9a
yungchunkan <yungchunkan@gmail.com>
2016-09-25 06:32:27 +0100

    2016-09-24's javascript workout

---
+++ contributions/yungchunkan/javascript/Functions/2016-09-24.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Function call method
+Check if a document is done loading
