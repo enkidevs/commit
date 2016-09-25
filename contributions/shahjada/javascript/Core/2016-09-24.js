290e7c2655ed7cd3021633c129d1ac44619f6cb0
shahjada <shahjada085@gmail.com>
2016-09-25 09:11:15 +0100

    2016-09-24's javascript workout

---
+++ contributions/shahjada/javascript/Core/2016-09-24.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Most efficient way to build `HTML` strings
+Setting the length of an array 
+The difference between null, undefined and NaN
+`console.*` in JavaScript
