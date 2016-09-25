e05bf6459fd6e7ee18f634ab1390123c6d04465d
veronica <me@veronicacannon.com>
2016-09-25 12:37:27 +0100

    2016-09-25's javascript workout

---
+++ contributions/veronica/javascript/Functions/2016-09-25.js
@@ -0,0 +1,5 @@
+Hoisting
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Easily generate a random `HEX` color
