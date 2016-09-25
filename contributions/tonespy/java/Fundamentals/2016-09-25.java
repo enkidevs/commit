9cc1698aefe8e04352074006a3cccd87fcfe549c
tonespy <terrycents@gmail.com>
2016-09-25 20:48:25 +0100

    2016-09-25's java workout

---
+++ contributions/tonespy/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
