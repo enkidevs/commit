17c8186b03befd37f865a39dfd4ea2f8113beffa
carriersandra <carriersandra@live.ca>
2016-09-25 02:59:32 +0100

    2016-09-24's git workout

---
+++ contributions/carriersandra/git/Essentials/2016-09-24.bash
@@ -0,0 +1,5 @@
+Finding a tag
+Use `short` status to make output more compact
+How to write a git commit message
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
