e7f3f87c82c05d5729ab4697341bc9f653227189
jjcarstens <jjcarstens@me.com>
2016-09-25 18:22:18 +0100

    2016-09-25's git workout

---
+++ contributions/jjcarstens/git/Tools/2016-09-25.bash
@@ -0,0 +1,5 @@
+Specify a commit by its ancestry
+Specify a range of commits using double dot syntax
+Useful stashing options
+Remember the results of previous hunk conflicts
+Debug using binary search
