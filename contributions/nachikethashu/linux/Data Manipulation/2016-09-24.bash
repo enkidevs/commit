ccfdcf168a7da15ad0e9f39b639ebb15c88c6241
nachikethashu <nachikethashu@gmail.com>
2016-09-25 04:28:34 +0100

    2016-09-24's linux workout

---
+++ contributions/nachikethashu/linux/Data Manipulation/2016-09-24.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+Using `lynx` to convert HTML to text
+Image manipulation  using `convert`
+`sort` lines in a text file
+Math in the terminal with `bc`
