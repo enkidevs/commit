030c055faaf60226043e3083087ef6b229a4e190
CycleOfTheAbsurd <alex7up42@gmail.com>
2016-09-25 16:30:20 +0100

    2016-09-25's java workout

---
+++ contributions/CycleOfTheAbsurd/java/Data Types/2016-09-25.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
