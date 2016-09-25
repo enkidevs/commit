a02900b428776909a3838ea670a6e9915c7c2acc
niharika <nihapinkey@gmail.com>
2016-09-25 04:32:24 +0100

    2016-09-24's java workout

---
+++ contributions/niharika/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
