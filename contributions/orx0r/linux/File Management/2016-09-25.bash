4d9050a37354f56890718276f9ed76e77b737b10
orx0r <norbitronik@gmail.com>
2016-09-25 20:47:36 +0100

    2016-09-25's linux workout

---
+++ contributions/orx0r/linux/File Management/2016-09-25.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
