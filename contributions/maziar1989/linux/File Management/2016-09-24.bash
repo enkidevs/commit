97fe681b7a898c898393e94c95e2c69aa29c83a5
maziar1989 <chrono_i0nz@yahoo.com>
2016-09-25 05:42:35 +0100

    2016-09-24's linux workout

---
+++ contributions/maziar1989/linux/File Management/2016-09-24.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Working with `.gz` files
+Find the unknown process preventing deleting of files
