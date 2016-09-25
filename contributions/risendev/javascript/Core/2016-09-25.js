20ec00619383191a59625ebd9f242ab9e2c399c9
risendev <risendev@gmail.com>
2016-09-25 12:16:29 +0100

    2016-09-25's javascript workout

---
+++ contributions/risendev/javascript/Core/2016-09-25.js
@@ -0,0 +1,5 @@
+Performing a function at timed intervals
+Data type comparison in `switch` statements
+Filtering items out of an array
+Scope and strict mode
+Prevent background image flicker in **IE**
