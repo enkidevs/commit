afefaa2e48c6a342c2715024b2b92801421752ae
ozanust <ozanust9@gmail.com>
2016-09-25 11:51:44 +0100

    2016-09-25's java workout

---
+++ contributions/ozanust/java/I/O/2016-09-25.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
+Deleting files and directories
