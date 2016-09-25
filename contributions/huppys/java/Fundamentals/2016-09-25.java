d0d4d84ea5d2320072315abeee6b463dfb813efd
huppys <huppys55@gmail.com>
2016-09-25 13:24:41 +0100

    2016-09-25's java workout

---
+++ contributions/huppys/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
