b9808a87be0759f131c032ad89ac43b3bd56442f
Ahyari <ahmad.alhyari@gmail.com>
2016-09-25 10:20:15 +0100

    2016-09-24's java workout

---
+++ contributions/Ahyari/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
