242139cd31d7eac17fc7a51fad16b72e70d65bce
JanDC <jandecavele@gmail.com>
2016-09-25 19:24:56 +0100

    2016-09-25's linux workout

---
+++ contributions/JanDC/linux/System Management/2016-09-25.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
