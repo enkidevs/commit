17ebdcaa70a680a795ac9195f61c52e36c469209
mels065 <mellusbrandon@gmail.com>
2016-09-25 02:10:37 +0100

    2016-09-24's java workout

---
+++ contributions/mels065/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
