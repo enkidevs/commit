9dfc90c2d4848e1e49e350dba168add6862d9823
psychorobotninja <tefuller@gmail.com>
2016-09-25 04:44:32 +0100

    2016-09-24's java workout

---
+++ contributions/psychorobotninja/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
