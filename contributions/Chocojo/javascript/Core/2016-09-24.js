b2b189b2b11e6d67973be9710b901d57bb4ef404
Chocojo <chocojosue99@gmail.com>
2016-09-25 02:58:05 +0100

    2016-09-24's javascript workout

---
+++ contributions/Chocojo/javascript/Core/2016-09-24.js
@@ -0,0 +1,5 @@
+Performing a function at timed intervals
+Data type comparison in `switch` statements
+Extra function arguments are undefined by default
+Inaccuracy of binary floating-point format
+Base conversion 
