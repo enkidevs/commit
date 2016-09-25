273e7039e66654b32ef316c1e3ba56fcbc05d3db
agauniyal <dev.agauniyal@outlook.com>
2016-09-25 16:52:08 +0100

    2016-09-24's linux workout

---
+++ contributions/agauniyal/linux/File Management/2016-09-24.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
