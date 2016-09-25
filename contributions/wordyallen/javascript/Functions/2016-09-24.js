8003ab9214023c77cbee4e511283f954557fae40
wordyallen <joe.narvaez@gmail.com>
2016-09-25 16:01:13 +0100

    2016-09-24's javascript workout

---
+++ contributions/wordyallen/javascript/Functions/2016-09-24.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+`.bind()` 
+IIFE pattern
+Check if a document is done loading
