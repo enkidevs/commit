48336c8845ccd46cbf09c7711a8f9676ec56b25c
S3R4F <skittas@gmail.com>
2016-09-25 09:02:57 +0100

    2016-09-25's linux workout

---
+++ contributions/S3R4F/linux/Terminals & Shells/2016-09-25.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Aliasing ssh connections
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
