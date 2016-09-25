6beb637d05d122f8e443d31727bd768b697068c7
AlexeiTipenko <atipenko2@gmail.com>
2016-09-25 14:29:03 +0100

    2016-09-24's java workout

---
+++ contributions/AlexeiTipenko/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Using bounded type parameters in generic methods
+Always specify type parameter
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
