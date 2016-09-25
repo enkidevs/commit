348b70b18924c10bd09df7447125553570523981
ouoam <poompathai.cha@pccpl.ac.th>
2016-09-25 02:00:31 +0100

    2016-09-24's linux workout

---
+++ contributions/ouoam/linux/System Management/2016-09-24.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Find out if the system's architecture is 32 or 64 bit
+Common log files
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
