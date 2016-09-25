68b5b6cecb896797d48d11545ca20d2ce476736b
rasialdev <rasialdev@gmail.com>
2016-09-25 22:20:21 +0100

    2016-09-25's java workout

---
+++ contributions/rasialdev/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
