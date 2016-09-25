afc26a7da51de08b166edd277d2b985153f3e853
ViniMoraes <moraes.vinicius@hotmail.com>
2016-09-25 17:10:26 +0100

    2016-09-25's linux workout

---
+++ contributions/ViniMoraes/linux/System Management/2016-09-25.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
