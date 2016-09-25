81f104195088d6588f22b7d7fcceb06e8295673d
AlphaAtlanta <hubertdolny@gmail.com>
2016-09-25 05:15:46 +0100

    2016-09-25's java workout

---
+++ contributions/AlphaAtlanta/java/Java 8/2016-09-25.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Using `Arrays.parallelSort` 
+Use `Optional(s)` type in order to avoid `NullPointerException(s)`
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
