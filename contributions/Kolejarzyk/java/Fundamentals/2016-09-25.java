c0202af38b0e9a60e1a789ceb1218a8d0d67be34
Kolejarzyk <pawel.dee@live.com>
2016-09-25 14:38:32 +0100

    2016-09-25's java workout

---
+++ contributions/Kolejarzyk/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
