ff300365fe4e68672213991106935659975e59f4
RhysAit <matteo1919.mdg@gmail.com>
2016-09-25 19:35:43 +0100

    2016-09-25's java workout

---
+++ contributions/RhysAit/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
