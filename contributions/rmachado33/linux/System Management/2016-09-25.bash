85eadc2634a27d6d658c97df13f1bb1ca0b49cf3
rmachado33 <mchdr19@gmail.com>
2016-09-25 22:16:19 +0100

    2016-09-25's linux workout

---
+++ contributions/rmachado33/linux/System Management/2016-09-25.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
