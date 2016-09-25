e7028f0f5558f08f4148895d6f22a163f0a22e78
tbruun <tbruun@gmail.com>
2016-09-25 09:45:29 +0100

    2016-09-25's java workout

---
+++ contributions/tbruun/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
+Using an interface as a parameter
