9555702bc7e8933c3f6a7bde9faf70a707459ff1
lpgonzalez <lisardo.prieto.gonzalez@gmail.com>
2016-09-25 14:03:57 +0100

    2016-09-25's java workout

---
+++ contributions/lpgonzalez/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Avoid using `static` variables
+Supply `toString()` in all classes
