01e0c170c39c3b6163cfaf7e70c32f113d102492
pedperpat <pedro-aperez1@hotmail.com>
2016-09-25 15:09:06 +0100

    2016-09-25's java workout

---
+++ contributions/pedperpat/java/Data Types/2016-09-25.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+String concatenation in Java
+Numeric Conversion - Widening
