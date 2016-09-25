22f10577f34ef077a969289c59fe450fe8ab10db
sithadmin <jeffmille@gmail.com>
2016-09-25 01:08:21 +0100

    2016-09-24's git workout

---
+++ contributions/sithadmin/git/Essentials/2016-09-24.bash
@@ -0,0 +1,5 @@
+Adding a remote repository
+Using aliases for git commands
+Use `short` status to make output more compact
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
