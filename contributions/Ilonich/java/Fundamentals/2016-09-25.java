3d9646447db86dca0803db4b0c18fd1f692ef672
Ilonich <niko-ikko@yandex.ru>
2016-09-25 08:10:17 +0100

    2016-09-25's java workout

---
+++ contributions/Ilonich/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
