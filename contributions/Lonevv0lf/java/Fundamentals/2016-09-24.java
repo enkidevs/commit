e2cefdb1d266ed2807f2fbe2cc708f18e9c90f0a
Lonevv0lf <yourongl@hotmail.com>
2016-09-25 15:30:15 +0100

    2016-09-24's java workout

---
+++ contributions/Lonevv0lf/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
