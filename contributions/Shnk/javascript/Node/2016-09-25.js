94b807e9015efe8c5f3e14e221743c88a508686d
Shnk <wavelet.alexandre@gmail.com>
2016-09-25 15:15:00 +0100

    2016-09-25's javascript workout

---
+++ contributions/Shnk/javascript/Node/2016-09-25.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+`uncaughtException` listener in Node.js
+Automatic compilation for Node with **Nodemon** 
+Manage child processes in **Node**
+`try-catch` only for **sync** code
