546f13094216e09bad7b5d38f5cea7e9882c2c18
FreeRadicand <conner_mcgrath@yahoo.com>
2016-09-25 15:55:49 +0100

    2016-09-25's javascript workout

---
+++ contributions/FreeRadicand/javascript/Node/2016-09-25.js
@@ -0,0 +1,5 @@
+How to debug Node.js
+Wrapping errors in Node.js using _node-verror_
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
+Middleware/pipeline design pattern
