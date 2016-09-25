40998e50148078575da39e0646df27dce0a26e57
mihai722 <mihai7221@gmail.com>
2016-09-25 09:41:34 +0100

    2016-09-25's linux workout

---
+++ contributions/mihai722/linux/Terminals & Shells/2016-09-25.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Useful aliasing in bash
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
