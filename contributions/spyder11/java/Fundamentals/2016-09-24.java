d58f833b0165f3b39a0f1669dbccae870f9bc5f8
spyder11 <spyder11@me.com>
2016-09-25 01:34:16 +0100

    2016-09-24's java workout

---
+++ contributions/spyder11/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Calling one constructor from another
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
