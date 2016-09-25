d27f87d2fcea24083ac4393369ae547243db4d05
dimipage <dimi.page@gmail.com>
2016-09-25 15:04:20 +0100

    2016-09-25's linux workout

---
+++ contributions/dimipage/linux/Networking/2016-09-25.bash
@@ -0,0 +1,5 @@
+ Intro to `iptables`
+Find services running on your host
+Get real network statistics with `slurm`
+Setting up password-free authentication
+Getting the connection speed from the terminal
