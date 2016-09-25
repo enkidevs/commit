f29cfb9848e01b167b8a5e7608b962197846aa36
george070393 <george070393@gmail.com>
2016-09-25 08:09:52 +0100

    2016-09-25's linux workout

---
+++ contributions/george070393/linux/File Management/2016-09-25.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
