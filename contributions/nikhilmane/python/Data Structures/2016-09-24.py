ab2d97380bd30b057dd9c7f423c4d9d1526e55a1
nikhilmane <nikhilcm42@gmail.com>
2016-09-25 05:31:07 +0100

    2016-09-24's python workout

---
+++ contributions/nikhilmane/python/Data Structures/2016-09-24.py
@@ -0,0 +1,5 @@
+Following PEP 8 styling guideline.
+A simple way to select a random item from a `list/tuple` data stucture
+Double ended queues with `deque`
+`Module`s everywhere!
+Special attributes of objects and classes
