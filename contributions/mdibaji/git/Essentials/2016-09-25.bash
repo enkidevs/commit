3ef7abd7ea3b00ac5702e6625a16a67ed986c748
mdibaji <dibajim@gmail.com>
2016-09-25 14:03:20 +0100

    2016-09-25's git workout

---
+++ contributions/mdibaji/git/Essentials/2016-09-25.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Adding a remote repository
+Using tags for version control
+How to set your username and email
+Remote repositories: viewing, editing and deleting
