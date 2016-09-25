832b69eb9b46127d51d80927e6a222716fe0513e
eddimas <eddimas@gmail.com>
2016-09-25 05:53:56 +0100

    2016-09-24's java workout

---
+++ contributions/eddimas/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
