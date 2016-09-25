25f84c55fc0d9edcf8adb318c8a24dc4c33204a1
M4STR0W0 <m4st3r0w@gmail.com>
2016-09-25 18:15:38 +0100

    2016-09-25's java workout

---
+++ contributions/M4STR0W0/java/Data Types/2016-09-25.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Converting a string to upper or lower case
+Finding a substring in a string
+Numeric Conversion - Widening
