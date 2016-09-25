02a58254bc8b8c7ca7845e50127301fab311fa8b
karlm <karl.missiaen@sfpd.fgov.be>
2016-09-25 06:43:42 +0100

    2016-09-25's java workout

---
+++ contributions/karlm/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Avoid using `static` variables
