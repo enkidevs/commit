64329181d3d587cf6ff2d0a136408c562488db4b
blacktelescope <chris@blacktelescope.co.uk>
2016-09-26 00:08:28 +0100

    2016-09-25's javascript workout

---
+++ contributions/blacktelescope/javascript/Core/2016-09-25.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+Data type comparison in `switch` statements
+`NaN` is a number
+Prevent background image flicker in **IE**
+Detect **DO NOT TRACK** status
