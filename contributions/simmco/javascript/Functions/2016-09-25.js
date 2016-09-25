4768ec1f08c6b1fdae66a8698ff9173a4a377ca8
simmco <m.simmelbauer@gmail.com>
2016-09-25 13:35:26 +0100

    2016-09-25's javascript workout

---
+++ contributions/simmco/javascript/Functions/2016-09-25.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Anonymous functions
+Easily generate a random `HEX` color
+Changing a functions context with `fn.call(object)`
