46a06bee967a5e1a30000d5524a2a510dd2b05f7
Aagitated7 <apro7@outlook.com>
2016-09-25 05:07:03 +0100

    2016-09-24's java workout

---
+++ contributions/Aagitated7/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Altering format string output by changing a format specifier's `argument_index`
+Calling one constructor from another
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
