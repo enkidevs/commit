53eb413f742d174dc4557767e950bb1060cbd08d
BarthSarafin <stefan.r.bachmann@gmail.com>
2016-09-25 08:54:11 +0100

    2016-09-25's linux workout

---
+++ contributions/BarthSarafin/linux/System Management/2016-09-25.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
