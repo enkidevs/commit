3db767716fc6f590ea7bf6d2e5c9b3d36d052aed
Angelmenam <angelmenam@hotmail.com>
2016-09-25 17:27:58 +0100

    2016-09-25's java workout

---
+++ contributions/Angelmenam/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
