eed6f2835ee0cd8126564831c49dc826e0a779b8
xSooDx <soodsaurabh95@gmail.com>
2016-09-25 09:39:31 +0100

    2016-09-24's java workout

---
+++ contributions/xSooDx/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
