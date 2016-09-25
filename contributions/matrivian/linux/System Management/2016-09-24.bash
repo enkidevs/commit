f1e9cc33029f63adfba6c020addb4c20751fb15d
matrivian <junguo@protonmail.com>
2016-09-25 06:17:21 +0100

    2016-09-24's linux workout

---
+++ contributions/matrivian/linux/System Management/2016-09-24.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
