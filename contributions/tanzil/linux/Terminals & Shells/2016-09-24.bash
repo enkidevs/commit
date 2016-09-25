b6f32bc591eb390a0400448f707984780e037a29
tanzil <ponkhi403@gmail.com>
2016-09-25 19:35:09 +0100

    2016-09-24's linux workout

---
+++ contributions/tanzil/linux/Terminals & Shells/2016-09-24.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Terminal incognito mode
+Keep useful commands in your shell history with tags
