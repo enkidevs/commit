4832388618a23a897d10f1d25d722c0e4fc9cfbc
bhillard <benhillard1@gmail.com>
2016-09-25 22:58:24 +0100

    2016-09-25's linux workout

---
+++ contributions/bhillard/linux/System Management/2016-09-25.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+System uptime
