5e69409670100b47b1556e820841c0c37cc29236
dsbalaban <dan.stefan.balaban@gmail.com>
2016-09-25 23:02:06 +0100

    2016-09-25's javascript workout

---
+++ contributions/dsbalaban/javascript/Browser APIs/2016-09-25.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Async and defer scripts
+CSS for when JavaScript is enabled
+Modify a website's URL
+Navigating the browser history
