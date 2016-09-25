01829f8dc488d1413b16408aecc0970f6f3fc5ea
nuxero <inge.zelaya@gmail.com>
2016-09-25 21:56:45 +0100

    2016-09-25's java workout

---
+++ contributions/nuxero/java/Data Types/2016-09-25.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+`StringBuffer` vs `StringBuilder`
+Retrieve the component type of an array
+String concatenation in Java
