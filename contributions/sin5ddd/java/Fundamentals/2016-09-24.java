2183af65613386e15bba263985fbd121fcaa9a60
sin5ddd <shingo.kitayama@gmail.com>
2016-09-25 08:29:21 +0100

    2016-09-24's java workout

---
+++ contributions/sin5ddd/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
