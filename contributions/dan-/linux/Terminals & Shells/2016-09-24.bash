2b8ad3d32628fa27c9bfe49083c0341630d6d1cf
dan- <njskater1490@gmail.com>
2016-09-25 01:31:39 +0100

    2016-09-24's linux workout

---
+++ contributions/dan-/linux/Terminals & Shells/2016-09-24.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
