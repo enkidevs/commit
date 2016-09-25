17886b9bc80c500bb57af6c4223d3dbaad3bb62d
critesjosh <critesjosh@gmail.com>
2016-09-25 18:23:59 +0100

    2016-09-25's javascript workout

---
+++ contributions/critesjosh/javascript/Functions/2016-09-25.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Method chaining
+Check if a document is done loading
