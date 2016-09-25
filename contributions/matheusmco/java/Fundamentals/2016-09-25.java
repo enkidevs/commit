147cc838f6b04650f39fa294e3489b5d736adac3
matheusmco <matheus0mco@gmail.com>
2016-09-25 13:03:26 +0100

    2016-09-25's java workout

---
+++ contributions/matheusmco/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Supply `toString()` in all classes
