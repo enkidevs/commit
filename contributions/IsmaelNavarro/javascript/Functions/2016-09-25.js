5efe93f3e8f020f0dc329428def77fae99e26efb
IsmaelNavarro <ismanapa@gmail.com>
2016-09-25 15:49:33 +0100

    2016-09-25's javascript workout

---
+++ contributions/IsmaelNavarro/javascript/Functions/2016-09-25.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Get query/url variables
+Changing a functions context with `fn.call(object)`
