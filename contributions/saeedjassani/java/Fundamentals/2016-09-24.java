8e235d75fbedba21532c77c5c8937848402caff4
saeedjassani <saeedjassani@gmail.com>
2016-09-25 15:28:47 +0100

    2016-09-24's java workout

---
+++ contributions/saeedjassani/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
