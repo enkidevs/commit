a7f3df7c08b83803655fbfb034aae6a0c0c8acf0
Karthikraja34 <rajakarthik131@gmail.com>
2016-09-25 03:26:33 +0100

    2016-09-24's javascript workout

---
+++ contributions/Karthikraja34/javascript/Core/2016-09-24.js
@@ -0,0 +1,5 @@
+Format currency in JavaScript
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Closures
+Getting the *real* dimensions of an image
