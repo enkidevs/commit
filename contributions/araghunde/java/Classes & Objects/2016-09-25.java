5d9375bfaae0d4cbdd0470942d381a7f357769ba
araghunde <paybol.21@gmail.com>
2016-09-25 12:19:02 +0100

    2016-09-25's java workout

---
+++ contributions/araghunde/java/Classes & Objects/2016-09-25.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Using `static` fields
+Utility classes and `static` methods
+Keeping fields `private`
+String formatting and parsing with `SimpleDateFormat`
