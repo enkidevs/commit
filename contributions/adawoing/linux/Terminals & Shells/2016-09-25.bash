10fa788d50d1f4ec6adee60cfea86cba620a388e
adawoing <eakey200@gmail.com>
2016-09-25 11:52:09 +0100

    2016-09-25's linux workout

---
+++ contributions/adawoing/linux/Terminals & Shells/2016-09-25.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
