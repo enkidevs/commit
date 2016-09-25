308846b4a4d9e29dc9334661e1b53c9f7706c4af
Awlex <wieseralex580@gmail.com>
2016-09-25 13:29:16 +0100

    2016-09-25's java workout

---
+++ contributions/Awlex/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Altering format string output by changing a format specifier's `argument_index`
+Always specify type parameter
+Detect or prevent integer overflow
