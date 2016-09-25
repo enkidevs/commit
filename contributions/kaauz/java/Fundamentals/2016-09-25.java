999c41e0bc9ae076bf36c32c6a3c0a959e1d29ec
kaauz <dgsted@gmail.com>
2016-09-25 19:36:24 +0100

    2016-09-25's java workout

---
+++ contributions/kaauz/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Measuring time
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
