e4ffbe26416750ea008b11722f0771408b7bdfbc
litorers <alexandrejoseph.tessier@gmail.com>
2016-09-25 15:05:25 +0100

    2016-09-25's linux workout

---
+++ contributions/litorers/linux/File Management/2016-09-25.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Using `touch` to alter files modification time
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
+Easy way of sharing files
