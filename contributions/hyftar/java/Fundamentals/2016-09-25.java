11dc2c181973c8d7f4550d8396a75a076bc073ad
hyftar <simon_landry@ymail.com>
2016-09-25 14:45:35 +0100

    2016-09-25's java workout

---
+++ contributions/hyftar/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
