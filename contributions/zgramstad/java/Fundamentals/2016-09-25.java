c6f2c99a3b96fbf7f8f72af44327849900b13c94
zgramstad <zeg2@rice.edu>
2016-09-25 19:56:20 +0100

    2016-09-25's java workout

---
+++ contributions/zgramstad/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
