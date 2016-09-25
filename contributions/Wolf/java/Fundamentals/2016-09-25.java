3de7bcfbe478698062383e8deb34238c0153d7d8
Wolf <ya.172009@yandex.ru>
2016-09-25 19:40:00 +0100

    2016-09-25's java workout

---
+++ contributions/Wolf/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Always specify type parameter
+Do not return *references* to private *mutable* class members
