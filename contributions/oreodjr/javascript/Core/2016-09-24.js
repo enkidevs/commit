d33233860b50200d0d8c864c0cb61d44c03b926f
oreodjr <otakudjr@gmail.com>
2016-09-25 07:02:17 +0100

    2016-09-24's javascript workout

---
+++ contributions/oreodjr/javascript/Core/2016-09-24.js
@@ -0,0 +1,5 @@
+Deleting properties
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Inaccuracy of binary floating-point format
