a67fb9334556ad88e77d8b68d119429ce457f8fb
Bcdo <bjornar.otterlei@gmail.com>
2016-09-25 23:25:04 +0100

    2016-09-25's linux workout

---
+++ contributions/Bcdo/linux/System Management/2016-09-25.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
