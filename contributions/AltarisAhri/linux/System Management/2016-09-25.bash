14004cd522afabc2426c199e119d052b8f9178b9
AltarisAhri <jordpjones@gmail.com>
2016-09-25 14:59:06 +0100

    2016-09-25's linux workout

---
+++ contributions/AltarisAhri/linux/System Management/2016-09-25.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Cancel a system shutdown
