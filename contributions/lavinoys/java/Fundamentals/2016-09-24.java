d85c4842687d7a4bba1c5d1cb4e4c32cb7c901e8
lavinoys <grmjkzz@gmail.com>
2016-09-25 03:19:18 +0100

    2016-09-24's java workout

---
+++ contributions/lavinoys/java/Fundamentals/2016-09-24.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
