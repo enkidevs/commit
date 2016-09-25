13ee191922c610a104736685476f4f52e694fc38
dckendrick <dckendrick24@gmail.com>
2016-09-25 02:09:23 +0100

    2016-09-24's javascript workout

---
+++ contributions/dckendrick/javascript/Npm/2016-09-24.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Bump package version in npm
+Passing through command line arguments in npm scripts
+`npm` verifies `sha1` hashes of packages
+`peerDependencies`
