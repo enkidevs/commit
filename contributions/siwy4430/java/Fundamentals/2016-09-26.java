8de4892dc8fb205032bc06a2b696b4fb9828b2a0
siwy4430 <siwinski.piotr@outlook.com>
2016-09-26 06:40:23 +0100

    2016-09-26's java workout

---
+++ contributions/siwy4430/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
