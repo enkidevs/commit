8ff98f8742a2913a97d25f8131d5a01fb3d2efa9
henkehagfeldt <henke9606@hotmail.com>
2016-09-26 06:21:52 +0100

    2016-09-26's git workout

---
+++ contributions/henkehagfeldt/git/Essentials/2016-09-26.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+What is stored in a commit?
+Search by commit message keyword
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
