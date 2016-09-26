74bf9baca4f38c696c13e801ed23675321580c29
joelandry <joelandry80@gmail.com>
2016-09-26 18:43:02 +0100

    2016-09-26's javascript workout

---
+++ contributions/joelandry/javascript/Core/2016-09-26.js
@@ -0,0 +1,5 @@
+Setting default values with `short circuiting`
+Using the double tilde `~~` 
+Most efficient way to build `HTML` strings
+The difference between null, undefined and NaN
+`catch` is block scoped
