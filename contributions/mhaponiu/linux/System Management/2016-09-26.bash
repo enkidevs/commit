b62463caa83494a939c36341b3b63a799f21c06c
mhaponiu <m.haponiuk@stud.elka.pw.edu.pl>
2016-09-26 09:50:49 +0100

    2016-09-26's linux workout

---
+++ contributions/mhaponiu/linux/System Management/2016-09-26.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
