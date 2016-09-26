b9fc3454a50b184e3564481fcefb73c21b78eabc
ChowchPowChow <juevch97@gmail.com>
2016-09-26 15:51:32 +0100

    2016-09-26's java workout

---
+++ contributions/ChowchPowChow/java/Error Handling/2016-09-26.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Using exceptions in Java
+Throwing an `exception`
