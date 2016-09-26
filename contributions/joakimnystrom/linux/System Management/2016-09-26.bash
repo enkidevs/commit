a66e2184197e24bf024a21cfed6cf164bb5ddd40
joakimnystrom <jnystromdesign@gmail.com>
2016-09-26 11:35:04 +0100

    2016-09-26's linux workout

---
+++ contributions/joakimnystrom/linux/System Management/2016-09-26.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
