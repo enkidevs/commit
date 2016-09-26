1ca81360e320aec649a5f36b2d4a9dd63fd20597
levinus <levin@straubis.org>
2016-09-26 20:12:17 +0100

    2016-09-26's linux workout

---
+++ contributions/levinus/linux/System Management/2016-09-26.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
