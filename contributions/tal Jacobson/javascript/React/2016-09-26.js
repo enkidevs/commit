54dc69d7c12579509c69b315017042df6a53ca76
tal Jacobson <tal1989@gmail.com>
2016-09-26 17:27:01 +0100

    2016-09-26's javascript workout

---
+++ contributions/tal Jacobson/javascript/React/2016-09-26.js
@@ -0,0 +1,5 @@
+Functional Stateless Components in React
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+Clone Elements in **React**
+`ReactDOM.render` ref
