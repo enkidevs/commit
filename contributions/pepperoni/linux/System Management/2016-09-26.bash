9507f10edcbcf644f4bdc54e0b0564c1b1ac7135
pepperoni <ometaxas@hotmail.com>
2016-09-26 13:27:42 +0100

    2016-09-26's linux workout

---
+++ contributions/pepperoni/linux/System Management/2016-09-26.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
