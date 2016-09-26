5f7f7a3f90a171fa5a78d549c6a72ed7b812cd53
Jebediah <jefwillems100@gmail.com>
2016-09-26 08:21:42 +0100

    2016-09-26's java workout

---
+++ contributions/Jebediah/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Avoid using `static` variables
