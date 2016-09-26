a9c0b6089051094693a4616bffb537c6e566f9ee
huppys <huppys55@gmail.com>
2016-09-26 22:02:34 +0100

    2016-09-26's linux workout

---
+++ contributions/huppys/linux/System Management/2016-09-26.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+System uptime
+Finding Open Files With `lsof`
