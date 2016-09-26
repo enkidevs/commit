98b4a2c740a1881e110d1debde161b032ba4950a
kerravon <tom@jynold.com>
2016-09-26 22:58:37 +0100

    2016-09-26's linux workout

---
+++ contributions/kerravon/linux/File Management/2016-09-26.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
