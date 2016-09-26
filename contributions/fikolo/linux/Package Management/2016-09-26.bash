cde71c6fbd80dd83a9e3c90189812a4be22950fd
fikolo <filipdalorenzo@gmail.com>
2016-09-26 06:54:06 +0100

    2016-09-26's linux workout

---
+++ contributions/fikolo/linux/Package Management/2016-09-26.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Removing PPAs
+Prevent updating a specific package in Debian systems
+Get `apt` to use a mirror / faster mirror
+Keeping repos updated with `cron`
