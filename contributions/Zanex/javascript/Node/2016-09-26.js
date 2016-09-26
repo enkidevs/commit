3000d3cb3a65f3fea3676e1098614f057d41be69
Zanex <alessio.zanella88@gmail.com>
2016-09-26 11:43:57 +0100

    2016-09-26's javascript workout

---
+++ contributions/Zanex/javascript/Node/2016-09-26.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Asynchronous File Write/Read in Node.js
+Wrapping errors in Node.js using _node-verror_
+Manage child processes in **Node**
+Middleware/pipeline design pattern
