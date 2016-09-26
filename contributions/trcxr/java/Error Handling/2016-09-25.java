fd6a2f76ce3b31c67c870555ee9feab55444631a
trcxr <amanahuja16@hotmail.com>
2016-09-26 10:12:08 +0100

    2016-09-25's java workout

---
+++ contributions/trcxr/java/Error Handling/2016-09-25.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Throwing an `exception`
