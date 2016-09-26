0c85f918b9071dc6d3ccab96b95881b805ede65f
GoDodgers <alberto.m.esquivias@gmail.com>
2016-09-26 04:51:39 +0100

    2016-09-25's linux workout

---
+++ contributions/GoDodgers/linux/Terminals & Shells/2016-09-25.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Find any Unix / Linux command
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
