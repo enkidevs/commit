2b27c19aa0050e2ee49433ac1c33050114785951
tiagoamaro <tiagopadrela@gmail.com>
2016-09-26 04:42:51 +0100

    2016-09-26's linux workout

---
+++ contributions/tiagoamaro/linux/System Management/2016-09-26.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Using `dd` To Create Disk Images
