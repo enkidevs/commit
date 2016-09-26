25834a16a41c6cb32bd2a4efdee9b16df3d2f4d2
Neil-Liang <xl712@nyu.edu>
2016-09-26 23:31:14 +0100

    2016-09-26's java workout

---
+++ contributions/Neil-Liang/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
