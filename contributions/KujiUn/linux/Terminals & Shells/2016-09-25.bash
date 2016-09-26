2cbceddbfff0b000854f087ac0ef141b090125d9
KujiUn <toneotom@gmail.com>
2016-09-26 04:16:01 +0100

    2016-09-25's linux workout

---
+++ contributions/KujiUn/linux/Terminals & Shells/2016-09-25.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Useful aliasing in bash
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
