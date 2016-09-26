fc023b660a7852fa5b6577bdcdc501a0dab39188
Andee <andytruong94@gmail.com>
2016-09-26 18:27:29 +0100

    2016-09-26's java workout

---
+++ contributions/Andee/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
