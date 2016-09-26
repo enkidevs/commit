74a27dd1634686f703dfbab6c79c7f252d0c01f8
EricM <eric.meallier@gmail.com>
2016-09-26 06:47:33 +0100

    2016-09-15's java workout

---
+++ contributions/EricM/java/Fundamentals/2016-09-15.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Altering format string output by changing a format specifier's `argument_index`
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
