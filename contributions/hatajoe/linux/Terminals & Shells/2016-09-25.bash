aa2c93666b191ec8e4381c8707f151048d98a34e
hatajoe <hatanaka@cloverlab.jp>
2016-09-26 05:12:59 +0100

    2016-09-25's linux workout

---
+++ contributions/hatajoe/linux/Terminals & Shells/2016-09-25.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
