39a99f3b7486a4db96c5e7608fac00346ed4a2fe
matandobr <matandobr@gmail.com>
2016-09-26 16:55:43 +0100

    2016-09-26's java workout

---
+++ contributions/matandobr/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Writing generic methods
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+How range operations work
