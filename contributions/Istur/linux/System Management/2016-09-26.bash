3aef80483d8b42c3f6ec0d6033a514c64f7caaa6
Istur <istur922@gmail.com>
2016-09-26 07:12:44 +0100

    2016-09-26's linux workout

---
+++ contributions/Istur/linux/System Management/2016-09-26.bash
@@ -0,0 +1,5 @@
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
