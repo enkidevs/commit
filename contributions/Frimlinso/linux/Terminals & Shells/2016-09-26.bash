fdeb8e0fe27df97a02d624ea715c3c9462d6c25e
Frimlinso <roysiu@outlook.com>
2016-09-26 09:00:04 +0100

    2016-09-26's linux workout

---
+++ contributions/Frimlinso/linux/Terminals & Shells/2016-09-26.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Terminal incognito mode
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
