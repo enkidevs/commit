c956e757bfe8f103507671d1914a589906ad0b4f
benkenawell <benkenawell@gmail.com>
2016-09-26 03:24:57 +0100

    2016-09-25's javascript workout

---
+++ contributions/benkenawell/javascript/Browser APIs/2016-09-25.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Async and defer scripts
+CSS for when JavaScript is enabled
+Warn user if **Back** button is pressed
+FileReader.readAsArrayBuffer()
