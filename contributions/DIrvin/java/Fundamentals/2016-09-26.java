6122ab5a54289ff2fe0b076683672230ba345961
DIrvin <drk.irvin90@gmail.com>
2016-09-26 18:00:58 +0100

    2016-09-26's java workout

---
+++ contributions/DIrvin/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
+Using an interface as a parameter
