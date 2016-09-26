db52c9061cf23b7d1db981cd1ce338ddfe7e944c
Tolumbas <tzat.anton@gmail.com>
2016-09-26 17:49:05 +0100

    2016-09-26's javascript workout

---
+++ contributions/Tolumbas/javascript/Npm/2016-09-26.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Manage local node modules with `npm link`
+Environmental variables and flags for `npm`
+`npm` verifies `sha1` hashes of packages
+`optionalDependencies` in npm
