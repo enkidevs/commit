55008befa5eeca8bf4e41aed205f7eecf32ea094
R0flcopt3r <eiriklavik@gmail.com>
2016-09-26 10:40:32 +0100

    2016-09-26's linux workout

---
+++ contributions/R0flcopt3r/linux/System Management/2016-09-26.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Changing the timezone on deb based systems
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
