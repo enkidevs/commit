9c19916628e529d8f7de89a8cbb35877221b88a7
rschlaefli <rolandschlaefli@gmail.com>
2016-09-26 08:54:38 +0100

    2016-09-26's linux workout

---
+++ contributions/rschlaefli/linux/File Management/2016-09-26.bash
@@ -0,0 +1,7 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
