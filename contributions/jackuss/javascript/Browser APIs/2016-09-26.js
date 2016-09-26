66d8c0edc8820ecf8ebe2f5f14a764716c4fce07
jackuss <jackuss@gmail.com>
2016-09-26 21:40:43 +0100

    2016-09-26's javascript workout

---
+++ contributions/jackuss/javascript/Browser APIs/2016-09-26.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Use hosted scripts to increase performance
+High Resolution Time API
+Navigation Timing API
+`Window.localStorage`
