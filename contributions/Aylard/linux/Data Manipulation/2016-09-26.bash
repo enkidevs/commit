2ce6b6af3c75adc79a6b77aa6c59c44ae3ca736f
Aylard <aylard7@gmail.com>
2016-09-26 14:51:46 +0100

    2016-09-26's linux workout

---
+++ contributions/Aylard/linux/Data Manipulation/2016-09-26.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+Getting rid of duplicate lines
+Using `lynx` to convert HTML to text
+Image manipulation  using `convert`
+Clean configuration files using `sed`
