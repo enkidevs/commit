bdf6c8c5f085ebb2faf63bf04db583adf1879407
stevew20 <stevewells20@gmail.com>
2016-09-26 03:23:05 +0100

    2016-09-25's java workout

---
+++ contributions/stevew20/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Using an interface as a parameter
