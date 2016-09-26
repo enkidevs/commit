f99f29bf924e67f949c1ce8772ccbe33be4a5c2d
nodh <c.kollmann@gmail.com>
2016-09-26 21:21:30 +0100

    2016-09-26's java workout

---
+++ contributions/nodh/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Always specify type parameter
