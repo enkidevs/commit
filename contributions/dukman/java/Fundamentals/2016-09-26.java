ddc25ded8b186793cbb2dbc7260b263b4ab532b2
dukman <dukman.box@gmail.com>
2016-09-26 19:54:37 +0100

    2016-09-26's java workout

---
+++ contributions/dukman/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+`Autoboxing` and `Unboxing`
+Using an interface as a parameter
