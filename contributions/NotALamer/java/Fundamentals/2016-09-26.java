8276c5413a70081fe135bdb175a2f881bc4e7e73
NotALamer <alvaro.movil.ms@gmail.com>
2016-09-26 23:17:28 +0100

    2016-09-26's java workout

---
+++ contributions/NotALamer/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,4 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Supply `toString()` in all classes
+Using an interface as a parameter
