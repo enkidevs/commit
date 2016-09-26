07dc5140559ff215bf3739fc3416e59cdb8beb02
ymahajan456 <y.mahajan456@gmail.com>
2016-09-26 14:35:50 +0100

    2016-09-25's linux workout

---
+++ contributions/ymahajan456/linux/Terminals & Shells/2016-09-25.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Terminal incognito mode
+Adding directories to your `$PATH`
+The Basics of Environment Variables
