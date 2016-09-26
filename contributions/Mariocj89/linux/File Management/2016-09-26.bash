05224133d96561993f6a8985fd0c420676cfdbc1
Mariocj89 <mariocj89@gmail.com>
2016-09-26 20:47:00 +0100

    2016-09-26's linux workout

---
+++ contributions/Mariocj89/linux/File Management/2016-09-26.bash
@@ -0,0 +1,5 @@
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
+Delete commands aliases
+Easy way of sharing files
