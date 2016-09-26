f4ba70a20a86c97f226c15a04e9b0d9fceac5c57
okashirin <lokishadow@mail.ru>
2016-09-26 09:16:02 +0100

    2016-09-26's java workout

---
+++ contributions/okashirin/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
