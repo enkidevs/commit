ad29db510efc89d72f93b488274ceb5c23c1252c
emptymalei <emptymalei@qq.com>
2016-09-26 08:01:04 +0100

    2016-09-25's javascript workout

---
+++ contributions/emptymalei/javascript/Node/2016-09-25.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
+Automatic compilation for Node with **Nodemon** 
