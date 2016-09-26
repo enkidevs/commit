2bf27092c77703d2e08cc851659f0eee0f7b6a08
sagiben <ilnextbus@gmail.com>
2016-09-26 04:55:14 +0100

    2016-09-26's java workout

---
+++ contributions/sagiben/java/Data Types/2016-09-26.java
@@ -0,0 +1,5 @@
+Converting numbers to strings
+Converting a string to upper or lower case
+Finding a substring in a string
+`StringBuffer` vs `StringBuilder`
+Retrieve the component type of an array
