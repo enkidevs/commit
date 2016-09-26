1d1964913ec4b0b4b34bf19199f47f75cf0f082d
reggiecollier <reggie@colliergroupllc.com>
2016-09-26 22:54:29 +0100

    2016-09-26's linux workout

---
+++ contributions/reggiecollier/linux/System Management/2016-09-26.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Monitor System Memory using `vmstat`
