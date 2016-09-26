e4799adeb96ddd219d943bb784a44c6ae9f83b2d
TahsinGokalp <tahsinsaan@gmail.com>
2016-09-26 21:14:54 +0100

    2016-09-26's javascript workout

---
+++ contributions/TahsinGokalp/javascript/Functions/2016-09-26.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Anonymous functions
+Easily generate a random `HEX` color
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
