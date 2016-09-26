3cb68746d90be0628525dd21d26e995fb0f011c1
Lichthagel <lichthagel@gmail.com>
2016-09-26 13:13:49 +0100

    2016-09-26's java workout

---
+++ contributions/Lichthagel/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Using `synchronized` statements
+Avoid using `static` variables
