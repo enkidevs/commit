679ba2948f45974d31f4b27ee70264fc9f9bc532
AntonBliznets <antonbl@mail.ru>
2016-09-26 15:40:40 +0100

    2016-09-25's javascript workout

---
+++ contributions/AntonBliznets/javascript/Core/2016-09-25.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Keywords in hash keys
+Use `splice()` to remove an item from an array
+Data type comparison in `switch` statements
+Prevent background image flicker in **IE**
