c60daa967acbe7d424c45c55b1ef5e2c9acb8b27
Przemo9712 <przemo9712@gmail.com>
2016-09-26 09:52:54 +0100

    2016-09-26's linux workout

---
+++ contributions/Przemo9712/linux/Terminals & Shells/2016-09-26.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
