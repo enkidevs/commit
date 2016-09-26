fcd651d5b46f64910be4886c4502bb996e23c755
karunsingh <karun.singh17@gmail.com>
2016-09-26 23:28:47 +0100

    2016-09-26's javascript workout

---
+++ contributions/karunsingh/javascript/Browser APIs/2016-09-26.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Async and defer scripts
+Use hosted scripts to increase performance
+Modify a website's URL
+Allow an element to go fullscreen
