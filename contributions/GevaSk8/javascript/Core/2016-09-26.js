ab4a5713ae0d77d1d752cee5d843140275acd406
GevaSk8 <manuelf.mola@gmail.com>
2016-09-26 06:57:38 +0100

    2016-09-26's javascript workout

---
+++ contributions/GevaSk8/javascript/Core/2016-09-26.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Data type comparison in `switch` statements
+`NaN` is a number
+Extra function arguments are undefined by default
+Detect online connection
