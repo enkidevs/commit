722f11156b71f84251a28a59ba97c190a49d2035
SB-Jr <shrijitbasak@gmail.com>
2016-09-26 22:18:17 +0100

    2016-09-25's java workout

---
+++ contributions/SB-Jr/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Measuring time
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
