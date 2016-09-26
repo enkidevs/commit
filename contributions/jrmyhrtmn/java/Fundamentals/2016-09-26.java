92b19d3f2163a11a666d3328e992a6158545ebed
jrmyhrtmn <jmaize91@gmail.com>
2016-09-26 17:37:42 +0100

    2016-09-26's java workout

---
+++ contributions/jrmyhrtmn/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
