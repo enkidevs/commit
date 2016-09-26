3234119a17cbab7f8ec89e664b608f1c002a51f2
Shadowgun <shadowgun@i.ua>
2016-09-26 07:08:17 +0100

    2016-09-26's java workout

---
+++ contributions/Shadowgun/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
