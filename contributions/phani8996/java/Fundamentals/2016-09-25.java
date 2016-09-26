d15d2a2b318c06a8886cd79f61eabf7eb5e10a35
phani8996 <phani8996@gmail.com>
2016-09-26 16:26:06 +0100

    2016-09-25's java workout

---
+++ contributions/phani8996/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use strings in a `switch` statement
+Use `DecimalFormat` class to format numbers
+Calling one constructor from another
+Measuring time
