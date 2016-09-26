8d7a4640b479b7333070a44c48480521e13a6db3
Ecleptic <cameron@camerongreens.com>
2016-09-26 14:51:54 +0100

    2016-09-26's javascript workout

---
+++ contributions/Ecleptic/javascript/React/2016-09-26.js
@@ -0,0 +1,6 @@
+componentWillReceiveProps Not Triggered After Mounting
+
+Use **React** with other libraries
+`PureRenderMixin` in **React**
+`ref` callback attribute
+Custom `propType`'s to be required
