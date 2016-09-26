7e08796831e73ffc83f66aecb0d527627e57a983
kevintigelaar <kevin@tigel-net.nl>
2016-09-26 06:46:53 +0100

    2016-09-26's linux workout

---
+++ contributions/kevintigelaar/linux/System Management/2016-09-26.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
