cb16bdf063d7af9fe1ef9d0dbd7a0dd45e4c51ba
harrybarnes <harrybarnes13@gmail.com>
2016-09-26 14:53:49 +0100

    2016-09-26's java workout

---
+++ contributions/harrybarnes/java/Data Types/2016-09-26.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Converting numbers to strings
+Finding a substring in a string
+`StringBuffer` vs `StringBuilder`
+String concatenation in Java
