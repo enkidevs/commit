b7693e37552421c151ec565652af10341ae29eb6
nikkop <nphemma@yahoo.se>
2016-09-26 13:54:37 +0100

    2016-09-26's linux workout

---
+++ contributions/nikkop/linux/Terminals & Shells/2016-09-26.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
