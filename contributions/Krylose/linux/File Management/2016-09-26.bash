a46a33dd5015032de971ad5dc6e2038e732ac6cc
Krylose <jtrenchard@outlook.com>
2016-09-26 23:15:44 +0100

    2016-09-26's linux workout

---
+++ contributions/Krylose/linux/File Management/2016-09-26.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
