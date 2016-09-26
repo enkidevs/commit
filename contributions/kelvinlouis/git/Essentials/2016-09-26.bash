13832f004ad16c4e5b4b46c4902c9f03e3da6887
kelvinlouis <kelvin.trash@gmail.com>
2016-09-26 16:14:00 +0100

    2016-09-26's git workout

---
+++ contributions/kelvinlouis/git/Essentials/2016-09-26.bash
@@ -0,0 +1,5 @@
+What is stored in a commit?
+You can use git offline!
+Search by commit message keyword
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
