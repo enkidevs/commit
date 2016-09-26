a590195c0d4965c24f71d738bc644176a8780491
gogi <janik.w777@gmail.com>
2016-09-26 18:02:09 +0100

    2016-09-26's java workout

---
+++ contributions/gogi/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Inheriting a constructor from a superclass
+Metadata: setting a file's owner
+Using `synchronized` statements
+Detect or prevent integer overflow
