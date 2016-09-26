d67afbbd78a1dc8ded0a631f67e1cacb0a08d89f
kossuth <steve.earl.choe@gmail.com>
2016-09-26 15:46:50 +0100

    2016-09-26's linux workout

---
+++ contributions/kossuth/linux/Terminals & Shells/2016-09-26.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Useful aliasing in bash
+Adding directories to your `$PATH`
+Retrieve previous arguments
