9ed483001bd27f12c10783ee19448c19be1dfcf1
silverbulletzjc <hhyyww123@foxmail.com>
2016-09-26 16:11:51 +0100

    2016-09-25's java workout

---
+++ contributions/silverbulletzjc/java/Fundamentals/2016-09-25.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Difference between JRE and JDK?
+The distinction between overloading and overriding methods
+Using `synchronized` statements
+Supply `toString()` in all classes
