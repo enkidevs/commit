7d0ee669e12827a139e5d517a51c2aa8ddc6827a
jjfs85 <john@scimone.net>
2016-09-26 03:23:57 +0100

    2016-09-25's linux workout

---
+++ contributions/jjfs85/linux/System Management/2016-09-25.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
