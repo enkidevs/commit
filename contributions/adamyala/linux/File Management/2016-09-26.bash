5793b074287123fd3ebed3696734fb27c4abcc4a
adamyala <adamkyala@gmail.com>
2016-09-26 14:34:38 +0100

    2016-09-26's linux workout

---
+++ contributions/adamyala/linux/File Management/2016-09-26.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
