a5c382d855d6bdff76c9d57e5396e4ebe0db2f81
orx0r <norbitronik@gmail.com>
2016-09-26 07:33:49 +0100

    2016-09-26's linux workout

---
+++ contributions/orx0r/linux/Networking/2016-09-26.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Logging dropped firewall packets
+Getting the connection speed from the terminal
+Make use of `netstat`
+Staying connected remotely via SSH without an interactive shell
