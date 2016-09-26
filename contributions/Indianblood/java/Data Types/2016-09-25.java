53c9091a91851cb2e748145baa091053f5eb8200
Indianblood <indianblood@gmail.com>
2016-09-26 04:50:17 +0100

    2016-09-25's java workout

---
+++ contributions/Indianblood/java/Data Types/2016-09-25.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Equals operation on different data types
+Numeric Conversion - Widening
