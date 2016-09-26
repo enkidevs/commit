8d4b74e53f4c5fd90755a809139531196b7e8ab1
rasialdev <rasialdev@gmail.com>
2016-09-26 22:36:02 +0100

    2016-09-26's java workout

---
+++ contributions/rasialdev/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
