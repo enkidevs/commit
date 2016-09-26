02c54807fc2a386a548c488f9bf568daf3bc7b7e
SamuelPinho <samuel.pinho@fatec.sp.gov.br>
2016-09-26 04:24:16 +0100

    2016-09-25's linux workout

---
+++ contributions/SamuelPinho/linux/Terminals & Shells/2016-09-25.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Random password generator
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
