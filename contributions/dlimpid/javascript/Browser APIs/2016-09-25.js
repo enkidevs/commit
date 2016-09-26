39bfa5a09e271bf018b9397b422c5e90f6afc4ae
dlimpid <dlimpid@gmail.com>
2016-09-26 07:37:59 +0100

    2016-09-25's javascript workout

---
+++ contributions/dlimpid/javascript/Browser APIs/2016-09-25.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+Use `String.link` to create `<a>` tags without messy concatenation
+Allow an element to go fullscreen
+Blobs
+Network Information API
