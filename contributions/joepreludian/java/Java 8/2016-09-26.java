ac8462f7771f7ed1fc05cf6833e098c6e9a77f1e
joepreludian <joepreludian@gmail.com>
2016-09-26 19:16:54 +0100

    2016-09-26's java workout

---
+++ contributions/joepreludian/java/Java 8/2016-09-26.java
@@ -0,0 +1,5 @@
+Java 8 Lambda expressions
+Using `Arrays.parallelSort` 
+How to check for static dependencies of applications and libraries from the command line.
+Use `Optional(s)` type in order to avoid `NullPointerException(s)`
+Java8's new `StampedLock`
