0954a68121788a19951fef109bd4e1fa0a19d04f
Yvanor <maxime.yusei@orange.fr>
2016-09-26 18:08:18 +0100

    2016-09-26's java workout

---
+++ contributions/Yvanor/java/Data Types/2016-09-26.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Converting a string to upper or lower case
+Finding a substring in a string
+`StringBuffer` vs `StringBuilder`
