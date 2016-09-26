55e9fe81d7740805fc870a654b078b71a5b7ddf7
longp <lphan1989@gmail.com>
2016-09-26 04:37:39 +0100

    2016-09-25's javascript workout

---
+++ contributions/longp/javascript/React/2016-09-25.js
@@ -0,0 +1,5 @@
+Functional Stateless Components in React
+`null` value for *Controlled Components* in **React**
+Use **React** with other libraries
+`ReactDOM.render` ref
+Shortcut for transferring props
