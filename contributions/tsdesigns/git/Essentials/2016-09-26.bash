74ab4d248c73fd72bf73197fec3bcefab94dd3b0
tsdesigns <nickedwards69@googlemail.com>
2016-09-26 15:36:46 +0100

    2016-09-26's git workout

---
+++ contributions/tsdesigns/git/Essentials/2016-09-26.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to unmodify a modified file
+Using aliases for git commands
+Check the status of your files
+Use `short` status to make output more compact
