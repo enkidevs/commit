057799568ae04dc1c735d3583f4a442068a3830d
vmann <vmann@vmann.net>
2016-09-26 05:32:28 +0100

    2016-09-25's linux workout

---
+++ contributions/vmann/linux/System Management/2016-09-25.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Commands to shutdown or restart the system
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
