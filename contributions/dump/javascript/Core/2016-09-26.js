d468c3aa43e5ffb1e734d5d340af840b2d34c5bb
dump <info@lucatestoni.it>
2016-09-26 10:39:35 +0100

    2016-09-26's javascript workout

---
+++ contributions/dump/javascript/Core/2016-09-26.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Detect **DO NOT TRACK** status
+Getting the *real* dimensions of an image
