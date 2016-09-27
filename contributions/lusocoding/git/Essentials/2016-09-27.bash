b12d06577ab0e2cbee3bda502b65345243116aa0
lusocoding <nuno.barreiro@outlook.com>
2016-09-27 21:51:57 +0100

    2016-09-27's git workout

---
+++ contributions/lusocoding/git/Essentials/2016-09-27.bash
@@ -0,0 +1,5 @@
+Limiting log output by time
+Use `short` status to make output more compact
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
