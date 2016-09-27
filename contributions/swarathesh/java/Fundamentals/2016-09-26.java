19f2797805b3d14c050407adade534f2ab09389e
swarathesh <swarathesh60@gmail.com>
2016-09-27 15:30:56 +0100

    2016-09-26's java workout

---
+++ contributions/swarathesh/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Connecting to FTP using Java
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
