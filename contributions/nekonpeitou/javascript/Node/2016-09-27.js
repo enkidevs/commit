07391a68dc9f2bf3150f517261833170db47ad58
nekonpeitou <cathy.chi.chan@gmail.com>
2016-09-27 08:33:47 +0100

    2016-09-27's javascript workout

---
+++ contributions/nekonpeitou/javascript/Node/2016-09-27.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+**Node** `os` module
+`process.nextTick()` callback pattern
+Automatic compilation for Node with **Nodemon** 
+`try-catch` only for **sync** code
