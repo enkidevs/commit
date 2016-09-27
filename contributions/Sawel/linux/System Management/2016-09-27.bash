e29cead9becb0c52e22dfb29d030020735ec1ae6
Sawel <danielsolomon94.ds@gmail.com>
2016-09-27 10:56:21 +0100

    2016-09-27's linux workout

---
+++ contributions/Sawel/linux/System Management/2016-09-27.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
