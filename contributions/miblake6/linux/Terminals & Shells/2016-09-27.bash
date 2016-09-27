ddc56a62691f724d491439c02b3652449687e736
miblake6 <miblake6@gmail.com>
2016-09-27 17:59:36 +0100

    2016-09-27's linux workout

---
+++ contributions/miblake6/linux/Terminals & Shells/2016-09-27.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
