250733766a85ef7386a154ffbf98230b79e281a0
sandeshkaranam <sandeshkaranam1@gmail.com>
2016-09-27 18:08:06 +0100

    2016-09-26's java workout

---
+++ contributions/sandeshkaranam/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Increase `PermGen` space as to avoid `OutOfMemory` errors
