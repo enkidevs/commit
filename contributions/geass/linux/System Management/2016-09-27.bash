b42b25267be7aa46a004aa4cd6d1e75b3543c4ef
geass <muhammet.arsln@gmail.com>
2016-09-27 18:59:31 +0100

    2016-09-27's linux workout

---
+++ contributions/geass/linux/System Management/2016-09-27.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
