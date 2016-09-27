f7cf03a68520c5f52123d63509a3d3be008f0ac4
clayboy <lebrunoantoni@gmail.com>
2016-09-27 11:30:59 +0100

    2016-09-27's javascript workout

---
+++ contributions/clayboy/javascript/Core/2016-09-27.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Using the double tilde `~~` 
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Getting the *real* dimensions of an image
