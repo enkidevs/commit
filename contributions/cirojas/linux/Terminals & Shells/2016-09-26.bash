1cf65336a654056a1dd25f1e9aa1ec437e992178
cirojas <c.rojasvictoriano@gmail.com>
2016-09-27 05:41:23 +0100

    2016-09-26's linux workout

---
+++ contributions/cirojas/linux/Terminals & Shells/2016-09-26.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear the terminal instantly
+Terminal based browser
+Keep useful commands in your shell history with tags
