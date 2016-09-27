b4513b67a76c9a9f239889817678aa145949e565
Razze <razzeee@gmail.com>
2016-09-27 16:42:41 +0100

    2016-09-27's javascript workout

---
+++ contributions/Razze/javascript/Core/2016-09-27.js
@@ -0,0 +1,5 @@
+Deleting properties
+The distinction between `==` and `===`
+The difference between null, undefined and NaN
+Extra function arguments are undefined by default
+Detect online connection
