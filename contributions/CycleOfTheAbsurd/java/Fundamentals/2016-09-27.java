b223e225a8ab375b7f56b26d264e6d72da64d3b3
CycleOfTheAbsurd <alex7up42@gmail.com>
2016-09-27 16:00:23 +0100

    2016-09-27's java workout

---
+++ contributions/CycleOfTheAbsurd/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Use strings in a `switch` statement
+Do not perform bitwise and arithmetic operations on the same data
+`Autoboxing` and `Unboxing`
+How range operations work
