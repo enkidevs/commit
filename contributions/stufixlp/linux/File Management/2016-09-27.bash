4eea0ac2c67e35cba506558da5d86648621a92ea
stufixlp <stufixlp@gmail.com>
2016-09-27 05:09:33 +0100

    2016-09-27's linux workout

---
+++ contributions/stufixlp/linux/File Management/2016-09-27.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Working with `.gz` files
+Find the unknown process preventing deleting of files
+Delete commands aliases
