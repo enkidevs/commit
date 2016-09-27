49162c5056c89bfc7da067e9b49b4592f6c103c4
towfurious <towfurious@gmail.com>
2016-09-27 11:45:59 +0100

    2016-09-27's java workout

---
+++ contributions/towfurious/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+Java naming conventions
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
