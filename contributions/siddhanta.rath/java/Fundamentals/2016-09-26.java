988bebd5ab4192e429c5268439678cf3423b1782
siddhanta.rath <siddhanta.rath@gmail.com>
2016-09-27 16:29:19 +0100

    2016-09-26's java workout

---
+++ contributions/siddhanta.rath/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Use meaningful names
+Altering format string output by changing a format specifier's `argument_index`
+Using `Printstream.format` to format a string before printing
+Using bounded type parameters in generic methods
