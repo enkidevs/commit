83d5267bdaa9d15d32d49110826e8f2b5b8e0a29
tivvit <tivvit@seznam.cz>
2016-09-27 08:41:23 +0100

    2016-09-27's linux workout

---
+++ contributions/tivvit/linux/System Management/2016-09-27.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Common log files
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
