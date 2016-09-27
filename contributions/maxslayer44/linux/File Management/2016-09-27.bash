72def897e0ca3a505185f648a6591835c39ff589
maxslayer44 <maxime.baumann.pro@gmail.com>
2016-09-27 22:16:41 +0100

    2016-09-27's linux workout

---
+++ contributions/maxslayer44/linux/File Management/2016-09-27.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
