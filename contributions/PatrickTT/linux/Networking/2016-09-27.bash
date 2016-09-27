967440f1cc064354205349ad1cfb2ed73eabcf90
PatrickTT <patrick@tunkl.de>
2016-09-27 19:29:35 +0100

    2016-09-27's linux workout

---
+++ contributions/PatrickTT/linux/Networking/2016-09-27.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Limit the `wget` download rate
+Sending emails from terminal / scripts
+Make use of `netstat`
+Staying connected remotely via SSH without an interactive shell
