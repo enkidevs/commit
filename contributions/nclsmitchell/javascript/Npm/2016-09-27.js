90299bec032cc9a4214cf760b3d45176bff74e77
nclsmitchell <ncls.mitchell@gmail.com>
2016-09-27 08:09:44 +0100

    2016-09-27's javascript workout

---
+++ contributions/nclsmitchell/javascript/Npm/2016-09-27.js
@@ -0,0 +1,5 @@
+Bump package version in npm
+npm can handle multiple versions of a dependency
+`optionalDependencies` in npm
+Reduce package duplication 
+Lock down dependency versions by shrinkwrapping
