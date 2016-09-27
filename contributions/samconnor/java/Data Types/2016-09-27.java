33371244d73d63c0defc853ce7139028092438df
samconnor <samuelboss360@gmail.com>
2016-09-27 23:49:06 +0100

    2016-09-27's java workout

---
+++ contributions/samconnor/java/Data Types/2016-09-27.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
+Numeric Conversion - Widening
