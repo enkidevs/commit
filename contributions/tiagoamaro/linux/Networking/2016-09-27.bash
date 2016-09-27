d7a77744a09ffcbe6d0650e4cc0156378ea8e08b
tiagoamaro <tiagopadrela@gmail.com>
2016-09-27 07:23:16 +0100

    2016-09-27's linux workout

---
+++ contributions/tiagoamaro/linux/Networking/2016-09-27.bash
@@ -0,0 +1,5 @@
+SSH tunneling made easy
+Multiple IP Address with aliasing 
+Useful ssh client optimizations 
+Sending emails from terminal / scripts
+Use `tcpdump` to listen to network interface traffic
