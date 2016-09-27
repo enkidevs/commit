690fe0067cbd5b6362190aa507ad5525885f8c3b
kevdokimov <kirillprof@ya.ru>
2016-09-27 19:08:12 +0100

    2016-09-27's linux workout

---
+++ contributions/kevdokimov/linux/Terminals & Shells/2016-09-27.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
