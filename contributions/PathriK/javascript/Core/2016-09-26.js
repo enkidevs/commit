706400e53b70168d117ebd553db01a5bf6734e0b
PathriK <pathrikumark@gmail.com>
2016-09-27 03:21:37 +0100

    2016-09-26's javascript workout

---
+++ contributions/PathriK/javascript/Core/2016-09-26.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Scope and strict mode
+Avoid using `with`
