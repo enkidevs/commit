c7e02401114f9c58d42dec69602140bbeddcab99
keicus <charlestdjames1@gmail.com>
2016-09-27 14:41:01 +0100

    2016-09-27's java workout

---
+++ contributions/keicus/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using Inheritance to reduce code repetition
+Metadata: creating a user-defined file attribute
+How range operations work
+Supply `toString()` in all classes
