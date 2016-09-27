3d52a355335f016f0b38cc7ebb0ec2ba1a633a7e
2DemiGods <zumer.vlad@yahoo.ro>
2016-09-27 10:52:57 +0100

    2016-09-27's git workout

---
+++ contributions/2DemiGods/git/Tools/2016-09-27.bash
@@ -0,0 +1,5 @@
+Let's play the blame game
+Cache your authentication details to save time
+Interactively unstage changes
+Locate a commit by its hash
+Specify a range of commits using double dot syntax
