585b4bfac19cfec114dd5e6042e8542bf51f85cf
AndersB <anders.barth@gmail.com>
2016-09-27 17:37:19 +0100

    2016-09-27's python workout

---
+++ contributions/AndersB/python/Data Structures/2016-09-27.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Your own Python `calendar` 
+Double ended queues with `deque`
+Get the most of `int`s
+Get the most of `float`s
