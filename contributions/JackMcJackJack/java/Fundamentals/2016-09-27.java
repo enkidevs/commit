bda5bd142264a01dbd6fd32f0f42f50797ee8284
JackMcJackJack <jackkennedy98@gmail.com>
2016-09-27 18:51:29 +0100

    2016-09-27's java workout

---
+++ contributions/JackMcJackJack/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The trade-off between short and descriptive names
+Measuring time
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
