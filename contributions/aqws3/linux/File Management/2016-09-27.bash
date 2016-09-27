9abfa486fd726d6ca87bff00da52ca393bd0e08d
aqws3 <aqwssama@gmail.com>
2016-09-27 16:23:03 +0100

    2016-09-27's linux workout

---
+++ contributions/aqws3/linux/File Management/2016-09-27.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
+Easy way of sharing files
