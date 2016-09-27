ea26f66e4830a98230f8dd7e6c36d51e9d61504e
mpes <michal.pesicka@gmail.com>
2016-09-27 08:11:15 +0100

    2016-09-27's git workout

---
+++ contributions/mpes/git/Tools/2016-09-27.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Interactively unstage changes
+Diff images using *ExifTool*
+Specify a range of commits using double dot syntax
+Debug using binary search
