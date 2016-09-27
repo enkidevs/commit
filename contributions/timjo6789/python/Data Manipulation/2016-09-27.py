fbc43e717478bd6a23bb6495f00328488dd3b560
timjo6789 <tgj2u2@gmail.com>
2016-09-27 13:48:50 +0100

    2016-09-27's python workout

---
+++ contributions/timjo6789/python/Data Manipulation/2016-09-27.py
@@ -0,0 +1,5 @@
+Three ways to substitute a substring of a string
+Use `struct` to work with Binary Data
+Compare sequences with `difflib`
+Get the similarity ratio of two sequences
+Working with junk data
