31ae4468468e9edc1a117a0603d3f0143d6635f9
go4taj <go4taj@gmail.com>
2016-09-27 17:32:35 +0100

    2016-09-26's linux workout

---
+++ contributions/go4taj/linux/Terminals & Shells/2016-09-26.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Bash history reverse search
+Execute a command without saving it in history
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
