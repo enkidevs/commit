121d331f517bb241c0fabdcf04fa9b1cd1e28cc4
dosrmals1 <songhyuk@gmail.com>
2016-09-27 14:51:08 +0100

    2016-09-27's linux workout

---
+++ contributions/dosrmals1/linux/Terminals & Shells/2016-09-27.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
