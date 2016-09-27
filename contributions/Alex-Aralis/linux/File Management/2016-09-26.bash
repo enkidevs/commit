d17e1d97839f92dd8c7c217c8ff0fcbaada886c8
Alex-Aralis <alex.aralis@gmail.com>
2016-09-27 02:07:33 +0100

    2016-09-26's linux workout

---
+++ contributions/Alex-Aralis/linux/File Management/2016-09-26.bash
@@ -0,0 +1,7 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
