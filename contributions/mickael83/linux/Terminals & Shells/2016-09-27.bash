51ee370ca55eed9f70f2d364b4ae57862083acd5
mickael83 <mickael.pinheiro@gmail.com>
2016-09-27 18:44:11 +0100

    2016-09-27's linux workout

---
+++ contributions/mickael83/linux/Terminals & Shells/2016-09-27.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Terminal based browser
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
