68b36efbc9491ad7549fff582a118b93c735545b
jakeshi <jakehku@gmail.com>
2016-09-27 16:41:14 +0100

    2016-09-27's git workout

---
+++ contributions/jakeshi/git/Essentials/2016-09-27.bash
@@ -0,0 +1,5 @@
+The three states in git
+Make your log output pretty
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
