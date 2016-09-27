952c547396c9e15e4e9b186acfb8b94280d03f36
lokyi <roy.yi.yue@gmail.com>
2016-09-27 05:04:02 +0100

    2016-09-26's javascript workout

---
+++ contributions/lokyi/javascript/Browser APIs/2016-09-26.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Async and defer scripts
+Use hosted scripts to increase performance
+Form a `URL` from its parts 
+Drag and Drop API
