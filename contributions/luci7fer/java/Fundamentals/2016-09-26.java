8ed2b712debf4991a416219d02a427ffb98dfc2f
luci7fer <linxiasheng@gmail.com>
2016-09-27 08:35:28 +0100

    2016-09-26's java workout

---
+++ contributions/luci7fer/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Difference between `==` and `equals()`
+Using Inheritance to reduce code repetition
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
