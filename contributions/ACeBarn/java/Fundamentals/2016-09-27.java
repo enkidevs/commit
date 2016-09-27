bf492078912b6932c37e2e174d7929ae0488e870
ACeBarn <alessio.bisgen@gmail.com>
2016-09-27 13:27:58 +0100

    2016-09-27's java workout

---
+++ contributions/ACeBarn/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
