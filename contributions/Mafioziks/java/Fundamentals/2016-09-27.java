4bee92e7a9f5162fca7bf158e32c1c2aa38d2d41
Mafioziks <tt00792@gmail.com>
2016-09-27 10:41:36 +0100

    2016-09-27's java workout

---
+++ contributions/Mafioziks/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
