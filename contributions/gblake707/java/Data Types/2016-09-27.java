0d9609b4eb581a1c6bf8d32d98874a037a0f04fa
gblake707 <gblake707@yahoo.com>
2016-09-27 18:56:28 +0100

    2016-09-27's java workout

---
+++ contributions/gblake707/java/Data Types/2016-09-27.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Multidimensional array declaration
+Finding a substring in a string
+Numeric Conversion - Widening
