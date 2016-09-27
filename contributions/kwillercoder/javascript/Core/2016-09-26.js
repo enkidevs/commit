87066dd61537756d57672d07b1b14a8f238ab6a1
kwillercoder <6522kaivalya@gmail.com>
2016-09-27 13:16:19 +0100

    2016-09-26's javascript workout

---
+++ contributions/kwillercoder/javascript/Core/2016-09-26.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Weak vs Strict equality operator
+Types of numbers
+Using assignment operators
+Hoisting applies only to variable declarations, not initializations
