8c5993c2798e5809ce0e77d7da24857488323fd6
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-09-27 02:24:01 +0100

    2016-09-26's linux workout

---
+++ contributions/Jameskmonger/linux/System Management/2016-09-26.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
