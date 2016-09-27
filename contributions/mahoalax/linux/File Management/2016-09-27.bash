ac9d9e5e89c016de1fc682852233762e0c3b9cbe
mahoalax <marie.laxenaire@webedia-group.com>
2016-09-27 22:24:00 +0100

    2016-09-27's linux workout

---
+++ contributions/mahoalax/linux/File Management/2016-09-27.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
