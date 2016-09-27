959884373759908e6ff31a161ab92122de4d79c8
visama <visama396@gmail.com>
2016-09-27 04:48:08 +0100

    2016-09-27's javascript workout

---
+++ contributions/visama/javascript/Core/2016-09-27.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Performing a function at timed intervals
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+Scope and strict mode
