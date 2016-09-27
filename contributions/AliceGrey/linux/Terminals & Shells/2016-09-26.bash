acc37c144e3d627111c408fbce6adbf25cff29e4
AliceGrey <aliceweigt@gmail.com>
2016-09-27 08:53:38 +0100

    2016-09-26's linux workout

---
+++ contributions/AliceGrey/linux/Terminals & Shells/2016-09-26.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Conditional command execution
+
+(`&&` operator)
