b84fb80a70846ce6d8086b1d020b3be4ca470909
pock2793 <pock27932@gmail.com>
2016-09-27 19:49:37 +0100

    2016-09-27's java workout

---
+++ contributions/pock2793/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Inheriting a constructor from a superclass
+Metadata: setting a file's owner
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
