c1ea572c94200e4238f80ed8cbdec9fe56e112f3
flolight <florianclanet@gmail.com>
2016-09-27 19:28:08 +0100

    2016-09-27's java workout

---
+++ contributions/flolight/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use strings in a `switch` statement
+Distinction between `public` and `private` methods
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
