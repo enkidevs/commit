b6dc1d0dec6a38d3fb6a4ff556b005d6ee7899b0
snowpl <kacper.werema@gmail.com>
2016-09-27 14:47:43 +0100

    2016-09-27's javascript workout

---
+++ contributions/snowpl/javascript/Core/2016-09-27.js
@@ -0,0 +1,5 @@
+Setting default values with `short circuiting`
+Precision
+Setting the length of an array 
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
