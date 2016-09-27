1db808f5adf790b50bc7df1dc337f9bf0cf21442
AnthonyFusco <anthony.fusco42@gmail.com>
2016-09-27 09:07:02 +0100

    2016-09-27's java workout

---
+++ contributions/AnthonyFusco/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
+Using `synchronized` statements
