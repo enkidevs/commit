8aa561a25df8953ed335546fc976880291610dd7
Mariocj89 <mariocj89@gmail.com>
2016-09-27 20:26:09 +0100

    2016-09-27's linux workout

---
+++ contributions/Mariocj89/linux/Terminals & Shells/2016-09-27.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Repeating commands with `watch`
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
