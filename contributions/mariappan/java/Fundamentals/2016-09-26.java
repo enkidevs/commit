65fdf8721424097db1aad8c1508e977bcdb71049
mariappan <mariappanourindia@gmail.com>
2016-09-27 17:25:04 +0100

    2016-09-26's java workout

---
+++ contributions/mariappan/java/Fundamentals/2016-09-26.java
@@ -0,0 +1,5 @@
+Inheriting a constructor from a superclass
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
