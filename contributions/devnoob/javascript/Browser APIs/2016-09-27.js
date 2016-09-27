4a60f4a4de7763212466dcf481bfe78fdb5b4e9d
devnoob <joaoecoceanus@gmail.com>
2016-09-28 00:47:39 +0100

    2016-09-27's javascript workout

---
+++ contributions/devnoob/javascript/Browser APIs/2016-09-27.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Drawing with HTML5 `Canvas` API
+High Resolution Time API
+FileReader.readAsArrayBuffer()
+MediaDevices.getUserMedia()
