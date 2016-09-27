ede52c6e817f7a339efbe3f21337fc83b4544358
cico <radualexandrucosmin@gmail.com>
2016-09-27 16:30:39 +0100

    2016-09-27's java workout

---
+++ contributions/cico/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Always specify type parameter
