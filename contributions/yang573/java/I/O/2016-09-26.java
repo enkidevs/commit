91886342d262fc7471c55ed2b80a8a447aa859b6
yang573 <yangyang1998@gmail.com>
2016-09-27 04:10:21 +0100

    2016-09-26's java workout

---
+++ contributions/yang573/java/I/O/2016-09-26.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+Using buffered streams
+Checking for the existence of files and directories
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
