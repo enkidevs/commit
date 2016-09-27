24bfb6f5620c97579192de76edb9056ae8e7fe01
Systemdir <systemdir@hotmail.de>
2016-09-28 00:45:38 +0100

    2016-09-27's java workout

---
+++ contributions/Systemdir/java/Error Handling/2016-09-27.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Using exceptions in Java
+Throwing an `exception`
+Careful Numeric Data Conversions
