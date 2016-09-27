e0fc69db72f5a2f73c5e62d6b5b977e1428dd9a5
tnargwoxow <tnargwoxow@gmail.com>
2016-09-27 08:00:44 +0100

    2016-09-27's linux workout

---
+++ contributions/tnargwoxow/linux/Terminals & Shells/2016-09-27.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Retrieve previous arguments
