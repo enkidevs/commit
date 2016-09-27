a7c09a355aff674ab25840b28ed98551370b1d00
squablyScientist <squablesquable@gmail.com>
2016-09-27 14:25:46 +0100

    2016-09-27's linux workout

---
+++ contributions/squablyScientist/linux/Terminals & Shells/2016-09-27.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Terminal incognito mode
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
