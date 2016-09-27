3fd7bbf77e44d06b5c64cff6c5c8c4b4eb0d2f24
jneilmbs <jneilmbs@gmail.com>
2016-09-27 18:44:00 +0100

    2016-09-27's linux workout

---
+++ contributions/jneilmbs/linux/Networking/2016-09-27.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Check `iptables` firewall status
+Get real network statistics with `slurm`
+Limit the `wget` download rate
+Use `tcpdump` to listen to network interface traffic
