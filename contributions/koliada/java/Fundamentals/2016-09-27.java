932b86713da69a014c9b3924998ff76f720e2693
koliada <alex.fiator@gmail.com>
2016-09-27 08:18:54 +0100

    2016-09-27's java workout

---
+++ contributions/koliada/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Use meaningful names
+Java naming conventions
+Using `Printstream.format` to format a string before printing
+There is no such thing as *pass-by-reference* in Java
