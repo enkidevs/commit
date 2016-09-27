48484eb71902a50b10311d3db6606a7562db8ad0
Tomaz123 <tomas.jacobsen@hotmail.com>
2016-09-27 13:43:25 +0100

    2016-09-27's java workout

---
+++ contributions/Tomaz123/java/Error Handling/2016-09-27.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Checking a double for `NaN` value
