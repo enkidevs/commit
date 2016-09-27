19dfb38904a01465ea0be1b0cde756b000bc6926
steved <stephendoyle75@gmail.com>
2016-09-27 09:47:01 +0100

    2016-09-27's linux workout

---
+++ contributions/steved/linux/Terminals & Shells/2016-09-27.bash
@@ -0,0 +1,5 @@
+Find any Unix / Linux command
+Quick `bash` shortcuts 
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
