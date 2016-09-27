ec8dc213613d105b47d00aff457755827a596389
Vira <thomas.ruehrig@studierende.htl-donaustadt.at>
2016-09-27 07:01:44 +0100

    2016-09-27's java workout

---
+++ contributions/Vira/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Locks in `static synchronized` methods
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
