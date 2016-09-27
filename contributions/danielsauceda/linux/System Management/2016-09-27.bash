5b339ce49685992d9d6061b0687c13fad2a3101f
danielsauceda <danielsauceda34@gmail.com>
2016-09-27 19:23:59 +0100

    2016-09-27's linux workout

---
+++ contributions/danielsauceda/linux/System Management/2016-09-27.bash
@@ -0,0 +1,5 @@
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
