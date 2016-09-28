f1d8041ce4b118b09b0a1eb730eef0309a08845c
ramandeep89 <ramandeep89@gmail.com>
2016-09-28 15:24:44 +0100

    2016-09-27's java workout

---
+++ contributions/ramandeep89/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
