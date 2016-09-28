fbe0bad9d83ac61b2ace3716f3c4d2d29d87101b
e7krgc <enam93@gmail.com>
2016-09-28 07:19:56 +0100

    2016-09-28's linux workout

---
+++ contributions/e7krgc/linux/System Management/2016-09-28.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
