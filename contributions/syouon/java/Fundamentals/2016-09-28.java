69158daa9dabac4b9bcf81db1f45ae2fda14f588
syouon <syouon.do@gmail.com>
2016-09-28 23:43:53 +0100

    2016-09-28's java workout

---
+++ contributions/syouon/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
