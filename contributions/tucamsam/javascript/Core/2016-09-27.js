c2028aceca3253f300095f4883f5f662c215e770
tucamsam <tucamsam@gmail.com>
2016-09-28 04:36:16 +0100

    2016-09-27's javascript workout

---
+++ contributions/tucamsam/javascript/Core/2016-09-27.js
@@ -0,0 +1,5 @@
+Data type comparison in `switch` statements
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Prevent background image flicker in **IE**
