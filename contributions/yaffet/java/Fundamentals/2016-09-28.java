fa8403e504059748cd28055a8e41e804cbc30fb1
yaffet <uniac@web.de>
2016-09-28 22:23:51 +0100

    2016-09-28's java workout

---
+++ contributions/yaffet/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
