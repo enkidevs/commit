eada90311791ce6d1f0db624a7ce33dbda929a1b
myang310 <mgy209@nyu.edu>
2016-09-28 20:30:32 +0100

    2016-09-28's linux workout

---
+++ contributions/myang310/linux/System Management/2016-09-28.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+List current logged on users with `w`
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
