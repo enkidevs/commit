f57dbbd86bf8558e538f72dd916cc2452c7b6644
RMJ5 <rmj_8@hotmail.com>
2016-09-28 08:31:54 +0100

    2016-09-27's javascript workout

---
+++ contributions/RMJ5/javascript/React/2016-09-27.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+componentWillReceiveProps Not Triggered After Mounting
+
+Immutability helpers in **React**
+`ref` callback attribute
+Enhanced Prop Validation 
