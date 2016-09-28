bfd7af5546d0ad475cf4a477eb288c1790f2d3ff
nromanzin <nicolas.romanzin@gmail.com>
2016-09-28 22:07:21 +0100

    2016-09-28's java workout

---
+++ contributions/nromanzin/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
