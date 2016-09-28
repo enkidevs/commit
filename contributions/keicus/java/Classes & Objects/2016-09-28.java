b9b98a127df8b815549418ba855647efeed37331
keicus <charlestdjames1@gmail.com>
2016-09-28 19:17:29 +0100

    2016-09-28's java workout

---
+++ contributions/keicus/java/Classes & Objects/2016-09-28.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Utility classes and `static` methods
+Keeping fields `private`
+Double Brace Initialization
+How and when to use `WeakHashMap` class
