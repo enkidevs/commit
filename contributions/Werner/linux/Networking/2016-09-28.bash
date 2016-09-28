1cf05d691459f3169039df364b6173aac8e6dd49
Werner <werner.foss@gmail.com>
2016-09-28 07:58:29 +0100

    2016-09-28's linux workout

---
+++ contributions/Werner/linux/Networking/2016-09-28.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+List your IPv6 configuration
+Getting the connection speed from the terminal
+Bandwidth monitoring tools 
+Use `tcpdump` to listen to network interface traffic
