2901ca7de326a1cc1411fba51607f3651fa9f14b
ItsMichal <michaljbodzianowski@gmail.com>
2016-09-28 04:12:10 +0100

    2016-09-27's java workout

---
+++ contributions/ItsMichal/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
