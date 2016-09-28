7bcd83c48a5c1e38a70bc95f34c1bea1c446c09c
xLillium <nicolas.motillon@gmail.com>
2016-09-28 09:32:49 +0100

    2016-09-28's java workout

---
+++ contributions/xLillium/java/Data Types/2016-09-28.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Two ways to use an `Iterator`
+Finding a substring in a string
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
