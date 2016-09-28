35b289d1501143842e09b47cee644e6ea19bf3d0
drugarce <drugarce@mt.net.mk>
2016-09-28 18:23:02 +0100

    2016-09-28's javascript workout

---
+++ contributions/drugarce/javascript/Core/2016-09-28.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Format currency in JavaScript
+Apply `map` function to array items
+Extra function arguments are undefined by default
+Prevent background image flicker in **IE**
