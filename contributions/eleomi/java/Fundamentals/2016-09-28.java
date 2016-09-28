0d0dd757686a7b780bf1232d6ce97554d62bc37c
eleomi <nerd.foo@ya.ru>
2016-09-28 15:25:29 +0100

    2016-09-28's java workout

---
+++ contributions/eleomi/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Implementing an `interface`
