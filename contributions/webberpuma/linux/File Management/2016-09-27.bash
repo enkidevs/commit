41e46d13b2539e45bf26969746cc2a9e0c186555
webberpuma <webberpuma@gmail.com>
2016-09-28 02:13:34 +0100

    2016-09-27's linux workout

---
+++ contributions/webberpuma/linux/File Management/2016-09-27.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Deleting files in a secure manner
+Deleting non-empty directories
+Easy way of sharing files
