8926102628b41772f2c8acd936de90590c954a59
mhaponiu <m.haponiuk@stud.elka.pw.edu.pl>
2016-09-28 23:55:16 +0100

    2016-09-28's linux workout

---
+++ contributions/mhaponiu/linux/File Management/2016-09-28.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Practical `du` command
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
