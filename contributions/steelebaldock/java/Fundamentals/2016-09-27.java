30a7abb27e0e3bea62369f74501df748f493d014
steelebaldock <steele.baldock@gmail.com>
2016-09-28 10:39:44 +0100

    2016-09-27's java workout

---
+++ contributions/steelebaldock/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Using an interface as a parameter
