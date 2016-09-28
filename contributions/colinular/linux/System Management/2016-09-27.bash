22d8e415279e09e69371361e536e10069f58eda5
colinular <colinular@gmail.com>
2016-09-28 14:11:44 +0100

    2016-09-27's linux workout

---
+++ contributions/colinular/linux/System Management/2016-09-27.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+System uptime
+Finding Open Files With `lsof`
