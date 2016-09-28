dbe45efa546446c0fca92e47c350000faa4f514c
JoshuaMorris <me@joshuamorris.info>
2016-09-28 04:19:15 +0100

    2016-09-27's javascript workout

---
+++ contributions/JoshuaMorris/javascript/Node/2016-09-27.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Streams in **Node**
+Handle `JSON.parse` error in Node.js
+`process.nextTick()` callback pattern
+Automatic compilation for Node with **Nodemon** 
