0552785e9bb8a8e26351cfe4035637003f9cef77
Robrob <robrobply99@gmail.com>
2016-09-28 04:31:36 +0100

    2016-09-27's java workout

---
+++ contributions/Robrob/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Using an interface as a parameter
