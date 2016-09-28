b72934e322da1a8f218b0517a409e869589ba221
jrmyhrtmn <jmaize91@gmail.com>
2016-09-28 20:53:12 +0100

    2016-09-28's javascript workout

---
+++ contributions/jrmyhrtmn/javascript/React/2016-09-28.js
@@ -0,0 +1,6 @@
+`null` value for *Controlled Components* in **React**
+Type of the Children props
+Props in getInitialState Is an        
+Anti-Pattern
+Using `false` in **JSX**
+**React** `this.props.children`
