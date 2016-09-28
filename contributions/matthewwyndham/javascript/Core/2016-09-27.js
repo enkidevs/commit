f08a4e2b16f691da56f97a8a2596178a3dcf9108
matthewwyndham <matthewwyndham@gmail.com>
2016-09-28 02:44:05 +0100

    2016-09-27's javascript workout

---
+++ contributions/matthewwyndham/javascript/Core/2016-09-27.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Detect an error type
+Getting the *real* dimensions of an image
+Inaccuracy of binary floating-point format
+Round numbers to `N` decimals 
