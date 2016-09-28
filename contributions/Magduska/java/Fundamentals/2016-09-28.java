ec92098a2196c231e21977dadf0a2758ba94063a
Magduska <magda6.06@wp.pl>
2016-09-28 06:38:33 +0100

    2016-09-28's java workout

---
+++ contributions/Magduska/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
