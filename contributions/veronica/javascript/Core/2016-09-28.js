12db175080dd9f006960b3495ad5c368c9cded84
veronica <me@veronicacannon.com>
2016-09-28 14:03:12 +0100

    2016-09-28's javascript workout

---
+++ contributions/veronica/javascript/Core/2016-09-28.js
@@ -0,0 +1,5 @@
+Setting default values with `short circuiting`
+Multi-line string variables
+Infix operators are left-associative
+Apply `map` function to array items
+Detect **DO NOT TRACK** status
