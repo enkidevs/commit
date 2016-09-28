3e7db96c83234ef1ac43b880784623fc954c0b72
santi8194 <alvarezsoto755@hotmail.com>
2016-09-28 16:51:35 +0100

    2016-09-28's java workout

---
+++ contributions/santi8194/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Distinction between `public` and `private` methods
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+`Autoboxing` and `Unboxing`
