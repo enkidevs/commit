e89e3565879f0a4d3fcda1cb2418cea07c57c868
JoaoFranciscoNeto <joao.neto.feup@gmail.com>
2016-09-28 16:26:34 +0100

    2016-09-28's git workout

---
+++ contributions/JoaoFranciscoNeto/git/Essentials/2016-09-28.bash
@@ -0,0 +1,5 @@
+The three states in git
+Make your log output pretty
+Check the status of your files
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
