dd76b7be0aff2be1c4a9ab335967628a6bd8f7de
Leerro <gabriel.f.bernard@gmail.com>
2016-09-28 17:44:50 +0100

    2016-09-28's java workout

---
+++ contributions/Leerro/java/Annotations/2016-09-28.java
@@ -0,0 +1,5 @@
+`@Test` parameters
+Use `@Ignore` to disable JUnit tests
+Using the `@Deprecated` annotation
+Suppressing warnings using `@SuppressWarnings`
+Declaring `@Repeatable` annotation types
