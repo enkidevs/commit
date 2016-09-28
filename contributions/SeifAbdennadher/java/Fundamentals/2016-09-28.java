13a3c67babea74fe3ab260d51a53a73bc13a4987
SeifAbdennadher <m.abdennadher.seif@gmail.com>
2016-09-28 21:15:17 +0100

    2016-09-28's java workout

---
+++ contributions/SeifAbdennadher/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Measuring time
+Increase `PermGen` space as to avoid `OutOfMemory` errors
