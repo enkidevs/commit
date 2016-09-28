b16e1b13d142affa0a459c3d4167c464cfaf560f
IceFoxX <icefoxx@aikq.de>
2016-09-28 18:38:36 +0100

    2016-09-28's linux workout

---
+++ contributions/IceFoxX/linux/Security/2016-09-28.bash
@@ -0,0 +1,5 @@
+Extracting the `public key` from the `private key`
+Disable SSH password authentication 
+Track SSH log-in attempts
+Best password generation utilities
+Private key encryption with `OpenSSL`
