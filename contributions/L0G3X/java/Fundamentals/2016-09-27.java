8c9e675b873a5f8cc29eb5259f9140041d78e16f
L0G3X <wt2080@gmail.com>
2016-09-28 01:46:09 +0100

    2016-09-27's java workout

---
+++ contributions/L0G3X/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Use `DecimalFormat` class to format numbers
+Calling one constructor from another
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
