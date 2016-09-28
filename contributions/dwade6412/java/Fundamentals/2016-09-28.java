30b47fcc50683945dd8b4dff3aa1f609206989a2
dwade6412 <dwade6412@gmail.com>
2016-09-28 12:26:09 +0100

    2016-09-28's java workout

---
+++ contributions/dwade6412/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Inheriting a constructor from a superclass
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
