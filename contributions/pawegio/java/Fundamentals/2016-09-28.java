c65300186a17ac29bf75807dd67fe610b966cb9a
pawegio <pawegio@gmail.com>
2016-09-28 08:40:47 +0100

    2016-09-28's java workout

---
+++ contributions/pawegio/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Measuring time
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
