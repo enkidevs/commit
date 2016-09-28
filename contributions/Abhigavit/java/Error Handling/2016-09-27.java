a14dfa76a8de690a3173f19181c616d7fd120c82
Abhigavit <abhishekgavit32@gmail.com>
2016-09-28 18:30:55 +0100

    2016-09-27's java workout

---
+++ contributions/Abhigavit/java/Error Handling/2016-09-27.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Prevent `exceptions` while logging Data
+Careful Numeric Data Conversions
