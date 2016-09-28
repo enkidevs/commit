732645d5c45756d751fe1e89da5e625f32171a62
kubik <lkubic@poczta.onet.pl>
2016-09-28 07:37:18 +0100

    2016-09-28's javascript workout

---
+++ contributions/kubik/javascript/Npm/2016-09-28.js
@@ -0,0 +1,5 @@
+npm can handle multiple versions of a dependency
+Combining script commands in npm
+Manage local node modules with `npm link`
+`npm` verifies `sha1` hashes of packages
+devDependencies in `npm`
