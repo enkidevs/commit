73ae1ea5ad6fdbe6e1b483b47ce5492aef8ade93
padawan4330 <padawan4330@gmail.com>
2016-09-28 16:23:35 +0100

    2016-09-28's linux workout

---
+++ contributions/padawan4330/linux/System Management/2016-09-28.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
