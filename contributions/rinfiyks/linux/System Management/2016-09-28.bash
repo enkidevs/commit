19244c1d1de4dfdc8d8d902a9ef89b0c167cbcc2
rinfiyks <tom.gould.92@gmail.com>
2016-09-28 20:29:12 +0100

    2016-09-28's linux workout

---
+++ contributions/rinfiyks/linux/System Management/2016-09-28.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
