e42d6b1250bd6c027e094a7fdccda83a352dea7e
Toroto008 <toroto006@hotmail.de>
2016-09-28 06:12:25 +0100

    2016-09-28's java workout

---
+++ contributions/Toroto008/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
