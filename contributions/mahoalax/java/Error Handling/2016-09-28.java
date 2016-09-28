789ec5cf1b42f97f787876117eb77108c15c8aa7
mahoalax <marie.laxenaire@webedia-group.com>
2016-09-28 22:07:58 +0100

    2016-09-28's java workout

---
+++ contributions/mahoalax/java/Error Handling/2016-09-28.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Using exceptions in Java
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Careful Numeric Data Conversions
