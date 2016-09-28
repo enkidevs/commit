e8ad43868d79c7f116d7a1b7e9065940d5ff8e04
ianjuma <ian.juma@yahoo.com>
2016-09-28 21:42:52 +0100

    2016-09-28's linux workout

---
+++ contributions/ianjuma/linux/Networking/2016-09-28.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Find services running on your host
+Multiple IP Address with aliasing 
+Use `tcpdump` to listen to network interface traffic
+Short Introduction to `Wget` 
