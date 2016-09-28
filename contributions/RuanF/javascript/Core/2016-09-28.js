77db0af8616d32349d78315a5977385b8eca0df6
RuanF <bassruan@gmail.com>
2016-09-28 20:35:24 +0100

    2016-09-28's javascript workout

---
+++ contributions/RuanF/javascript/Core/2016-09-28.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Weak vs Strict equality operator
+Hoisting applies only to variable declarations, not initializations
+`catch` is block scoped
+Extra function arguments are undefined by default
