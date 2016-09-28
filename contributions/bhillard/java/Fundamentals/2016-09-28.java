116835d9f3229f4dca1cdc6dc796570377312891
bhillard <benhillard1@gmail.com>
2016-09-28 12:05:11 +0100

    2016-09-28's java workout

---
+++ contributions/bhillard/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Using bounded type parameters in generic methods
+Measuring time
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
