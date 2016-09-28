eeb17d1ad689f01da1b40db3cb8ee9dada6932a0
gustavopaes <gustavo.paes@gmail.com>
2016-09-28 13:02:54 +0100

    2016-09-28's linux workout

---
+++ contributions/gustavopaes/linux/Networking/2016-09-28.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Staying connected remotely via SSH without an interactive shell
+Short Introduction to `Wget` 
