24c912c1b2e88c593df2cd4f0bed6740edf1feb5
Ilonich <niko-ikko@yandex.ru>
2016-09-28 02:30:16 +0100

    2016-09-28's java workout

---
+++ contributions/Ilonich/java/I/O/2016-09-28.java
@@ -0,0 +1,5 @@
+How to list all files in a directory that match a filename extension in Java
+Using buffered streams
+Reading file attributes
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
