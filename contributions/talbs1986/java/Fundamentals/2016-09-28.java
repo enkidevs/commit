78ecd6070093d2dae7e402dbba57043ea4c41160
talbs1986 <talbs1986@gmail.com>
2016-09-28 09:44:23 +0100

    2016-09-28's java workout

---
+++ contributions/talbs1986/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
+Using an interface as a parameter
