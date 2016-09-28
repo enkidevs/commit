e79f0d2e4438969a1ca2760bab5ef4baae82268b
gmoshkin <louielouie314@gmail.com>
2016-09-28 11:57:52 +0100

    2016-09-28's linux workout

---
+++ contributions/gmoshkin/linux/System Management/2016-09-28.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
