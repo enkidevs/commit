b838c86f6ed4806a22953b97372c443a91536da6
randhir051 <randhirsingh051@gmail.com>
2016-09-28 08:52:41 +0100

    2016-09-27's java workout

---
+++ contributions/randhir051/java/Fundamentals/2016-09-27.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Supply `toString()` in all classes
