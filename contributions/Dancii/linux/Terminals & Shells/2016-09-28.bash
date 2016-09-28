a589ac43a2d88e3d84ee6a9c7023ff183af5fcca
Dancii <danijel.kljakic@gmail.com>
2016-09-28 22:32:41 +0100

    2016-09-28's linux workout

---
+++ contributions/Dancii/linux/Terminals & Shells/2016-09-28.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
