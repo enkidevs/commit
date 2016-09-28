6c887ed5c5abdfba76e57333a8926d34d595caeb
jukka2003 <jukka2003@gmail.com>
2016-09-28 21:44:40 +0100

    2016-09-28's java workout

---
+++ contributions/jukka2003/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Using bounded type parameters in generic methods
+Avoid using `static` variables
+Using an interface as a parameter
