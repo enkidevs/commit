3302c55b38599c77376ecadb8ca39062c52c13e3
swettw <swettw@gmail.com>
2016-09-28 15:32:18 +0100

    2016-09-28's linux workout

---
+++ contributions/swettw/linux/System Management/2016-09-28.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
