2b43eeda56d3c8244239bb90a9ccb2a54090dd65
arthurvasconcelos <vasconcelos.arthur@gmail.com>
2016-09-28 16:54:15 +0100

    2016-09-28's javascript workout

---
+++ contributions/arthurvasconcelos/javascript/React/2016-09-28.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Props in getInitialState Is an        
+Anti-Pattern
+Enhanced Prop Validation 
+Custom `propType`'s to be required
+Default values for props
