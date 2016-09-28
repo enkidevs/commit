6a6f8ff0e2c1f3e30aa5af97289d32f23a87b907
glandalf <sjehann@gmail.com>
2016-09-28 08:10:54 +0100

    2016-09-28's linux workout

---
+++ contributions/glandalf/linux/File Management/2016-09-28.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
