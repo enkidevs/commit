5e90df3d9561a47fbba7d2d50c1c7b173fe34fa9
samarhxc <samarhxc@gmail.com>
2016-09-29 00:37:35 +0100

    2016-09-28's javascript workout

---
+++ contributions/samarhxc/javascript/Functions/2016-09-28.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function constructor vs. function declaration vs. function expression
+`.bind()` 
+Easily generate a random `HEX` color
+Social sharing without widgets
