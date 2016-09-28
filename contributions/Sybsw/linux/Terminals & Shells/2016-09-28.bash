b95ddd6d9552f22b0bdd39770e96b470320e0fdd
Sybsw <sybsward@gmail.com>
2016-09-28 07:19:58 +0100

    2016-09-28's linux workout

---
+++ contributions/Sybsw/linux/Terminals & Shells/2016-09-28.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
