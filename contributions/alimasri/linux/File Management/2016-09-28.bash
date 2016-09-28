c9f1fae47202c43f3077ceff2e300ab7438a498b
alimasri <alimasri1991@gmail.com>
2016-09-28 08:19:54 +0100

    2016-09-28's linux workout

---
+++ contributions/alimasri/linux/File Management/2016-09-28.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Deleting non-empty directories
