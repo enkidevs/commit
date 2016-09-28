60024dd719dcf4fd87acb5ccc6f54bbe1b3de842
dakk <gessadavide@gmail.com>
2016-09-28 10:38:31 +0100

    2016-09-28's linux workout

---
+++ contributions/dakk/linux/Jobs & Processes/2016-09-28.bash
@@ -0,0 +1,5 @@
+Backgrounding tasks 
+Keeping processes running after you log out
+`Cron` dot-in-filename issues when using `run-parts`
+Fixing the shell in cron
+Common issue with scripts in `cron`
