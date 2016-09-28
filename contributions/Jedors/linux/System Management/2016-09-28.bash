8c07a96400f5fd3b194b9f6544f69aaa7d4dfa62
Jedors <mael7@free.fr>
2016-09-28 23:47:23 +0100

    2016-09-28's linux workout

---
+++ contributions/Jedors/linux/System Management/2016-09-28.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+List current logged on users with `w`
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
