79296a0d1b3808b2cae2c39be9e85cd5109cd59b
RudeBoy <romanisman@gmail.com>
2016-09-28 08:48:14 +0100

    2016-09-28's linux workout

---
+++ contributions/RudeBoy/linux/File Management/2016-09-28.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
+Delete commands aliases
