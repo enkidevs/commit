1d88028dfd74f015eecda9d9ffb403e93dc47476
Jok3r <danielxd74@gmail.com>
2016-09-28 16:21:01 +0100

    2016-09-28's java workout

---
+++ contributions/Jok3r/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Inheriting a constructor from a superclass
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
+Using an interface as a parameter
