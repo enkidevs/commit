a2cd9c0976c16a7e0996a9e3430de4b59e1171dd
Jack-BP <carlos.xavier.rj@gmail.com>
2016-09-28 14:09:13 +0100

    2016-09-28's javascript workout

---
+++ contributions/Jack-BP/javascript/Browser APIs/2016-09-28.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Navigating the browser history
+Allow an element to go fullscreen
+FileReader.readAsArrayBuffer()
+MediaDevices.getUserMedia()
