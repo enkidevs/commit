ed8fce30fad82e7950dc0588fd6544c882d8bd7c
afc11hn <afc11hn@hotmail.de>
2016-09-28 06:43:15 +0100

    2016-09-28's javascript workout

---
+++ contributions/afc11hn/javascript/Npm/2016-09-28.js
@@ -0,0 +1,5 @@
+Registry user accounts for npm
+Manipulate npm packages cache
+npm can handle multiple versions of a dependency
+Passing through command line arguments in npm scripts
+`npm`  as an alternative to Gulp
