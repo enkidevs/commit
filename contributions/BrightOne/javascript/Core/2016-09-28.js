c7483288582489f395ad342b6445682357158125
BrightOne <filalex77@gmail.com>
2016-09-28 06:07:06 +0100

    2016-09-28's javascript workout

---
+++ contributions/BrightOne/javascript/Core/2016-09-28.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Setting default values with `short circuiting`
+No block scope
+Hoisting applies only to variable declarations, not initializations
+NaN === NaN?
