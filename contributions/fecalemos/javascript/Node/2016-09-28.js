4bfd61ab02711972e5f5797f3f9ebf399e41e019
fecalemos <fecalemos@gmail.com>
2016-09-28 21:08:47 +0100

    2016-09-28's javascript workout

---
+++ contributions/fecalemos/javascript/Node/2016-09-28.js
@@ -0,0 +1,5 @@
+First-error callbacks in **Node**
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+**Node** `os` module
+`uncaughtException` listener in Node.js
