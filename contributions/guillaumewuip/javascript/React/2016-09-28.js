0114f1fe168844d35691348de75d76b6c5ad616c
guillaumewuip <clochard.guillaume@gmail.com>
2016-09-28 06:09:46 +0100

    2016-09-28's javascript workout

---
+++ contributions/guillaumewuip/javascript/React/2016-09-28.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Props in getInitialState Is an        
+Anti-Pattern
+Server-side rendering 
+Keyed fragments in **React**
+Custom `propType`'s to be required
