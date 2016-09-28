6c2f250216c30f5b384c1b1c7d383b1f136697cf
Sensei <przemyslaw.cabaj@gmail.com>
2016-09-28 10:01:12 +0100

    2016-09-28's java workout

---
+++ contributions/Sensei/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Updating interfaces by using `default` methods
+Limit Accessibility of `Fields`
+Using an interface as a parameter
