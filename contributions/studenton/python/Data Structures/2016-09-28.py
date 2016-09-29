e5971b69a3bf89dc22ee4283b803ce68175181c0
studenton <studenton.com@gmail.com>
2016-09-29 01:01:47 +0100

    2016-09-28's python workout

---
+++ contributions/studenton/python/Data Structures/2016-09-28.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`Dictionary` view objects
+When `range` comes in handy
+Best way to implement a simple `queue` 
+Get more with `collections`!
