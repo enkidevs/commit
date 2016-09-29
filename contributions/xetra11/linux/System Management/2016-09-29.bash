5e0003d7e942f250f55642db2f7971f0c6f2684d
xetra11 <xetra11@outlook.com>
2016-09-29 21:47:23 +0100

    2016-09-29's linux workout

---
+++ contributions/xetra11/linux/System Management/2016-09-29.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Monitor System Memory using `vmstat`
