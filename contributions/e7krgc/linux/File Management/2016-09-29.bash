c34e143ae1d587bbde9f135939edc8d71d9007c3
e7krgc <enam93@gmail.com>
2016-09-29 07:08:52 +0100

    2016-09-29's linux workout

---
+++ contributions/e7krgc/linux/File Management/2016-09-29.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
