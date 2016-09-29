4da849e849cdf1eb84e2a88cbfeb922018ad6049
zanesalvatore <zanesalvatore@gmail.com>
2016-09-29 17:27:18 +0100

    2016-09-29's linux workout

---
+++ contributions/zanesalvatore/linux/File Management/2016-09-29.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
