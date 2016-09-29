f5ad0a6429a227ec6554bedda23ed94f8e5766cb
rapholtz <raphaellmattozo@gmail.com>
2016-09-29 14:47:37 +0100

    2016-09-29's linux workout

---
+++ contributions/rapholtz/linux/System Management/2016-09-29.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Common log files
+Incorrect time on dual boot systems
