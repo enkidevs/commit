91889e5ceee3bf6630f4859afb7989a2fb6f21ab
marcosmesser <marcosmesser@gmail.com>
2016-09-29 13:07:02 +0100

    2016-09-29's javascript workout

---
+++ contributions/marcosmesser/javascript/Functions/2016-09-29.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Method chaining
+Changing a functions context with `fn.call(object)`
