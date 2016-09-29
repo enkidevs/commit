1429d00348300d31123f302be30bb880b40531c6
tolsen <tolsen@southern.edu>
2016-09-29 14:36:16 +0100

    2016-09-29's java workout

---
+++ contributions/tolsen/java/I/O/2016-09-29.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Using buffered streams
+Checking for the existence of files and directories
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
