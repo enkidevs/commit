abea60317fd874ad4a43bbc5469f0a44e27f4af7
Reemahs <shameeromar@gmail.com>
2016-09-30 00:31:01 +0100

    2016-09-29's java workout

---
+++ contributions/Reemahs/java/Fundamentals/2016-09-29.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Supply `toString()` in all classes
