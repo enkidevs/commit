f09a7fb1e19020f2b4aa7b6169a3b852c8b756c0
medium-endian <mediumendian@gmail.com>
2016-09-29 20:08:42 +0100

    2016-09-29's linux workout

---
+++ contributions/medium-endian/linux/File Management/2016-09-29.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
