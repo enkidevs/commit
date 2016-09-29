c641c43637dba448a21653af8eb21e2d63a7c661
derrg <kenvandalsum@gmail.com>
2016-09-29 19:16:30 +0100

    2016-09-29's javascript workout

---
+++ contributions/derrg/javascript/React/2016-09-29.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+Using `false` in **JSX**
+Use **React** with other libraries
