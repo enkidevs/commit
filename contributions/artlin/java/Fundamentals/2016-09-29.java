09746b93acb709f32b67236c3d30320d60c41e20
artlin <kselos@gmail.com>
2016-09-29 12:14:30 +0100

    2016-09-29's java workout

---
+++ contributions/artlin/java/Fundamentals/2016-09-29.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Calling one constructor from another
+Detect or prevent integer overflow
