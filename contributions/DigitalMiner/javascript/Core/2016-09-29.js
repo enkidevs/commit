beafc8df614c81077efa110a3b605e7687c93c31
DigitalMiner <awkwardnuts@gmail.com>
2016-09-29 17:47:10 +0100

    2016-09-29's javascript workout

---
+++ contributions/DigitalMiner/javascript/Core/2016-09-29.js
@@ -0,0 +1,5 @@
+Multi-line string variables
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+JavaScript compilation
+Getting the *real* dimensions of an image
