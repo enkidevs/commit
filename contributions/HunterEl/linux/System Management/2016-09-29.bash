336d84ed562c99d3922221996fae5a4c7fd01c9e
HunterEl <hunterelbourn@gmail.com>
2016-09-29 20:12:08 +0100

    2016-09-29's linux workout

---
+++ contributions/HunterEl/linux/System Management/2016-09-29.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
