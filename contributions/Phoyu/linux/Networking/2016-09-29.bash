f6f5e6d68d721be9e338aaf39421856cdbdc7e6d
Phoyu <mathias.phoyu@gmail.com>
2016-09-29 12:35:48 +0100

    2016-09-29's linux workout

---
+++ contributions/Phoyu/linux/Networking/2016-09-29.bash
@@ -0,0 +1,5 @@
+Get real network statistics with `slurm`
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
+Make use of `netstat`
