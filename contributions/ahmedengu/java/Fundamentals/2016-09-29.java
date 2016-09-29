b1637d59e3345d37bd6040ee6ad364ad57bf2be0
ahmedengu <ahmedengu@gmail.com>
2016-09-29 11:21:04 +0100

    2016-09-29's java workout

---
+++ contributions/ahmedengu/java/Fundamentals/2016-09-29.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
