f5b7b5e9b58b021d574372d51bd6a2dac78dab75
bartlex <artiom.budnikoff@yandex.ru>
2016-09-29 04:12:06 +0100

    2016-09-28's linux workout

---
+++ contributions/bartlex/linux/Terminals & Shells/2016-09-28.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Repeating commands with `watch`
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
