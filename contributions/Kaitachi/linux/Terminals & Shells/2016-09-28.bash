a93884965a28fcbe07d303157db5dd09b390c3de
Kaitachi <kaitachi@me.com>
2016-09-29 02:09:09 +0100

    2016-09-28's linux workout

---
+++ contributions/Kaitachi/linux/Terminals & Shells/2016-09-28.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
