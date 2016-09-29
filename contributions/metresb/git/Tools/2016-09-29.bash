a384f6c90eb9847d32a25553c39d76ae6043863b
metresb <metresb@gmail.com>
2016-09-29 23:55:31 +0100

    2016-09-29's git workout

---
+++ contributions/metresb/git/Tools/2016-09-29.bash
@@ -0,0 +1,5 @@
+Interactively unstage changes
+Specify a range of commits using double dot syntax
+Interactively stage patches
+Sign your work (using GPG)
+Remember the results of previous hunk conflicts
