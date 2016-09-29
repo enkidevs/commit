d0881c166804d861e2e5b3731082ecfc556b6f5a
Sybsw <sybsward@gmail.com>
2016-09-29 14:49:52 +0100

    2016-09-29's linux workout

---
+++ contributions/Sybsw/linux/Terminals & Shells/2016-09-29.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
