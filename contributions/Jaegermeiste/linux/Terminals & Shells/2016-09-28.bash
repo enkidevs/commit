e77b3a5d8f2d0e686549da15266445ec613fa04b
Jaegermeiste <jason.grant.george@gmail.com>
2016-09-29 05:30:03 +0100

    2016-09-28's linux workout

---
+++ contributions/Jaegermeiste/linux/Terminals & Shells/2016-09-28.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Quick `bash` shortcuts 
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
