345630b3aed2028f6dc6654c6f2339c26f4ac7e8
webberpuma <webberpuma@gmail.com>
2016-09-29 01:50:40 +0100

    2016-09-28's java workout

---
+++ contributions/webberpuma/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+The trade-off between short and descriptive names
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+`Autoboxing` and `Unboxing`
