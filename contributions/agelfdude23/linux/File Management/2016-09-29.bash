22f2f05973682c0453a4637c2af35f347b161c3f
agelfdude23 <agelfdude23@gmail.com>
2016-09-29 16:45:29 +0100

    2016-09-29's linux workout

---
+++ contributions/agelfdude23/linux/File Management/2016-09-29.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Find the unknown process preventing deleting of files
+Easy way of sharing files
