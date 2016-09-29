2360f4849cf23c42d41be392c21e52a1a672525d
blackecho <angeletti.gabriele@gmail.com>
2016-09-29 21:55:50 +0100

    2016-09-29's linux workout

---
+++ contributions/blackecho/linux/System Management/2016-09-29.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Commands to shutdown or restart the system
+Finding Open Files With `lsof`
