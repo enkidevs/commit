5d35c053384f8806fe227a1b921d3f975dc45876
erviker <erviker@gmail.com>
2016-09-29 14:40:26 +0100

    2016-09-29's git workout

---
+++ contributions/erviker/git/Essentials/2016-09-29.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to unmodify a modified file
+What is stored in a commit?
+Search by commit message keyword
+Remote repositories: viewing, editing and deleting
