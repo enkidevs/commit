36c3113c809b170493ea58a23072f69a70254caf
Toriath <developer@tobiaskloss.de>
2016-09-29 05:53:56 +0100

    2016-09-29's linux workout

---
+++ contributions/Toriath/linux/System Management/2016-09-29.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
