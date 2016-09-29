a199f378ae716f962d8597b2064287da38c55faf
mbode <maxbode@gmail.com>
2016-09-29 07:04:43 +0100

    2016-09-29's java workout

---
+++ contributions/mbode/java/Fundamentals/2016-09-29.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
