379883e16c16f61f587b1cd68bcd3d6b284cf5e9
LuckierDodge <ryandlewis.rl@gmail.com>
2016-09-29 17:18:55 +0100

    2016-09-29's java workout

---
+++ contributions/LuckierDodge/java/Fundamentals/2016-09-29.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+Limit Accessibility of `Fields`
