7e42a04d8267e450a0de455fa25ef4576b10facd
HappiSquirrel <ludo0302@hotmail.fr>
2016-09-29 07:50:59 +0100

    2016-09-29's java workout

---
+++ contributions/HappiSquirrel/java/Fundamentals/2016-09-29.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
