5173cbb6a3a530047444b83e7d6c330656b0506a
w3bMaster <at.w3bmaster@gmail.com>
2016-09-29 19:33:14 +0100

    2016-09-29's linux workout

---
+++ contributions/w3bMaster/linux/Terminals & Shells/2016-09-29.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Terminal based browser
+Random password generator
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
