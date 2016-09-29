8be6fa9a56126edb1b9c7101a8f43d6df3c341cd
marcuslopes <marcusporteira@gmail.com>
2016-09-29 12:50:31 +0100

    2016-09-29's linux workout

---
+++ contributions/marcuslopes/linux/Permissions In Linux/2016-09-29.bash
@@ -0,0 +1,6 @@
+Users And Groups On Linux
+Changing File Ownership With `chown`
+Changing File Permissions With `chmod`
+Adding New Users With `useradd`
+Changing Group Membership With `usermod`
+Changing User Passwords With `passwd`
