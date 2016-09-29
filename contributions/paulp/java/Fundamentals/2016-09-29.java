04f54b983a6c5c9501d13efdb1bdec6f962ac2e4
paulp <paulp@hotmail.sk>
2016-09-29 08:47:18 +0100

    2016-09-29's java workout

---
+++ contributions/paulp/java/Fundamentals/2016-09-29.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
+Using an interface as a parameter
