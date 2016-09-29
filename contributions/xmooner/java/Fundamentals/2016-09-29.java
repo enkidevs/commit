57a39d0bddc4c26e7d3a783db9a7f224f3b3b223
xmooner <gioruffa@gmail.com>
2016-09-29 18:05:06 +0100

    2016-09-29's java workout

---
+++ contributions/xmooner/java/Fundamentals/2016-09-29.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Detect or prevent integer overflow
