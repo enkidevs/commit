07b94eb0e354923b3437e1c37babd3a96fde9391
JIFT <lucasecabrera@gmail.com>
2016-09-29 01:12:39 +0100

    2016-09-28's java workout

---
+++ contributions/JIFT/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Inheriting a constructor from a superclass
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
