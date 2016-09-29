85641b228871e44154fe9c1372531a424a6f6f3a
DIrvin <drk.irvin90@gmail.com>
2016-09-29 04:59:20 +0100

    2016-09-28's java workout

---
+++ contributions/DIrvin/java/Testing/2016-09-28.java
@@ -0,0 +1,5 @@
+JUnit: prefer `@Test` over `extends TestCase`
+Focus testing on key areas
+JUnit: Specifying file locations
+The Robot Utility
+Replacements for `setUp()` and `tearDown()` for **JUnit**
