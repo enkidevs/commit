755ec550e7ab59742517e2a77966d54fc98092e0
stufixlp <stufixlp@gmail.com>
2016-09-29 22:45:51 +0100

    2016-09-29's java workout

---
+++ contributions/stufixlp/java/Error Handling/2016-09-29.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Using exceptions in Java
