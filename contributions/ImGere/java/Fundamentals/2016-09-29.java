d773c62e3570f906f37a8b0d164541281279c620
ImGere <ale22.gerevini@gmail.com>
2016-09-29 18:40:52 +0100

    2016-09-29's java workout

---
+++ contributions/ImGere/java/Fundamentals/2016-09-29.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The trade-off between short and descriptive names
+Java naming conventions
+Updating interfaces by using `default` methods
+Implementing an `interface`
