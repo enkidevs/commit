a91b65548a77a12826e67427de01b9295450b8e0
xabi <ale23@hotmail.fr>
2016-09-29 17:25:19 +0100

    2016-09-29's linux workout

---
+++ contributions/xabi/linux/Terminals & Shells/2016-09-29.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Terminal based browser
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
