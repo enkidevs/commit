69b93c9aacb62b27266c75b36bad956623c9dd74
fthebaud <fthebaud@gmail.com>
2016-09-29 07:21:27 +0100

    2016-09-29's java workout

---
+++ contributions/fthebaud/java/Fundamentals/2016-09-29.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
+Always specify type parameter
+Avoid using `static` variables
