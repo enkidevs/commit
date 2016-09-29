c435221453a82b20089affbcfe3407277e787e06
WetwareDefect <john.piatras@gmail.com>
2016-09-29 18:44:04 +0100

    2016-09-29's java workout

---
+++ contributions/WetwareDefect/java/Fundamentals/2016-09-29.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Calling one constructor from another
+Constant notation
+Always specify type parameter
