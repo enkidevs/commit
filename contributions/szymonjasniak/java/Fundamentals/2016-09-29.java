2171ea41e69a89c9868b1d2343cb2e0b85416e60
szymonjasniak <szymonjasniak@gmail.com>
2016-09-29 21:34:23 +0100

    2016-09-29's java workout

---
+++ contributions/szymonjasniak/java/Fundamentals/2016-09-29.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Measuring time
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
