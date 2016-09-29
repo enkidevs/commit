7939085d9d6db035f7b369d25c0bfa17cce28aa8
IKawaiiCupcake <t.eltrop@web.de>
2016-09-29 06:00:13 +0100

    2016-09-29's java workout

---
+++ contributions/IKawaiiCupcake/java/Fundamentals/2016-09-29.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Difference between JRE and JDK?
+Using Inheritance to reduce code repetition
+Locks in `static synchronized` methods
+Always specify type parameter
