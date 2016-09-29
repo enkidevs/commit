92aaf366820b114bdd9ef77a4208543d22cfbf11
elShambler <tacastill@gmail.com>
2016-09-29 17:59:06 +0100

    2016-09-29's linux workout

---
+++ contributions/elShambler/linux/Terminals & Shells/2016-09-29.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Clear the terminal instantly
+Terminal based browser
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
