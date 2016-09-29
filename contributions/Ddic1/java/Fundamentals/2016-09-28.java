9c95f2d3571581851f974fef5cd183a6bc709079
Ddic1 <ecarb@uci.edu>
2016-09-29 08:31:48 +0100

    2016-09-28's java workout

---
+++ contributions/Ddic1/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
