f7d009ba0e19249498a41a86524c712f8ad127d6
WeLikeAich <jaichler13@apu.edu>
2016-09-29 05:53:24 +0100

    2016-09-28's linux workout

---
+++ contributions/WeLikeAich/linux/System Management/2016-09-28.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
