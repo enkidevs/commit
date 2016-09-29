ed9bb5870054077a16b3224f400a32866ded5f70
towdie <towdie13@gmail.com>
2016-09-29 10:04:04 +0100

    2016-09-29's linux workout

---
+++ contributions/towdie/linux/Terminals & Shells/2016-09-29.bash
@@ -0,0 +1,7 @@
+Quick `bash` shortcuts 
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
