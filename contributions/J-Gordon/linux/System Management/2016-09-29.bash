983f5784ba295d309bc9cb4205227810f57d1f58
J-Gordon <jg.wurian@gmail.com>
2016-09-29 08:22:33 +0100

    2016-09-29's linux workout

---
+++ contributions/J-Gordon/linux/System Management/2016-09-29.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
