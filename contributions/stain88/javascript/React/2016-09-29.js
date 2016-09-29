336cea1442da90eef33942def320684a928079fa
stain88 <marcbaghdadi@gmail.com>
2016-09-29 08:52:31 +0100

    2016-09-29's javascript workout

---
+++ contributions/stain88/javascript/React/2016-09-29.js
@@ -0,0 +1,6 @@
+componentWillReceiveProps Not Triggered After Mounting
+
+Use **React** with other libraries
+Clone Elements in **React**
+Custom `propType`'s to be required
+Default values for props
