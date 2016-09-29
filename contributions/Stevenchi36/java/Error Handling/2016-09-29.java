f3a58535fb046d875ea3a44299a94d80a534d0a5
Stevenchi36 <steven.childrey36@yahoo.com>
2016-09-29 19:57:21 +0100

    2016-09-29's java workout

---
+++ contributions/Stevenchi36/java/Error Handling/2016-09-29.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Prevent `exceptions` while logging Data
