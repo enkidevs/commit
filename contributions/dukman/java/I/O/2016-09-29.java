782e4fc68c765244cfec601e6c61c4ac721bd707
dukman <dukman.box@gmail.com>
2016-09-29 15:42:16 +0100

    2016-09-29's java workout

---
+++ contributions/dukman/java/I/O/2016-09-29.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+Checking for the existence of files and directories
+List contents of a directory with `newDirectoryStream`
+The `Console` class
