173e882edc2ee5d27af5b79a144887727b40cc21
leandro-alv <lacsdev@gmail.com>
2016-09-29 22:50:03 +0100

    2016-09-29's linux workout

---
+++ contributions/leandro-alv/linux/File Management/2016-09-29.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Easy way of sharing files
