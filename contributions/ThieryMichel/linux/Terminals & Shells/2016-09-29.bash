4387e4019182706abc4f849704e04def1e5ff09a
ThieryMichel <thiery@marmelab.com>
2016-09-29 18:46:09 +0100

    2016-09-29's linux workout

---
+++ contributions/ThieryMichel/linux/Terminals & Shells/2016-09-29.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Find any Unix / Linux command
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
