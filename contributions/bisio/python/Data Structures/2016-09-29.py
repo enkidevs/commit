7a581f56b41db816f1ac1e4f89fa52f0b77ae0ee
bisio <andrea.bisognin@gmail.com>
2016-09-29 06:18:44 +0100

    2016-09-29's python workout

---
+++ contributions/bisio/python/Data Structures/2016-09-29.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Built-in `list` methods
+Keep things in order with `OrderedDict`
+Prioritize your `queue` 
+Implementing **weak** references
