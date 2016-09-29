03d82831fa5da53a5e373802c98286022929add4
AkshayMathur92 <avi.mathur.engg@gmail.com>
2016-09-29 06:11:25 +0100

    2016-09-28's java workout

---
+++ contributions/AkshayMathur92/java/Fundamentals/2016-09-28.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
