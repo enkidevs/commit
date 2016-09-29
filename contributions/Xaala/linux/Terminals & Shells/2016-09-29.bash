85821ff2bfcdf8547f674db5963eb2f3debf7d0e
Xaala <ditchendorfc@yahoo.com>
2016-09-29 18:52:08 +0100

    2016-09-29's linux workout

---
+++ contributions/Xaala/linux/Terminals & Shells/2016-09-29.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Find any Unix / Linux command
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
