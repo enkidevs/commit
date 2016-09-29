458b2546a5e7e7bc48097adcd2d851c8c3d927a2
ealyon <jeremy.junqua@gmail.com>
2016-09-29 10:57:26 +0100

    2016-09-29's linux workout

---
+++ contributions/ealyon/linux/Terminals & Shells/2016-09-29.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Clear bash history
+Quick `bash` shortcuts 
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
