bd0bfe53619c61a0226eec08444f606ae8658131
damb <oumarsharif@gmail.com>
2016-09-29 17:43:30 +0100

    2016-09-29's linux workout

---
+++ contributions/damb/linux/Terminals & Shells/2016-09-29.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Clear the terminal instantly
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
