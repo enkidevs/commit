fc588586515353d78e880b58063b935fc20261a8
ilbonzo <matteo@magni.me>
2016-09-29 17:23:57 +0100

    2016-09-29's linux workout

---
+++ contributions/ilbonzo/linux/System Management/2016-09-29.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Common log files
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
