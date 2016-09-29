73156ec980df26bed04a3b6d36ee3aa16a656951
HunterEl <hunterelbourn@gmail.com>
2016-09-29 07:55:43 +0100

    2016-09-28's linux workout

---
+++ contributions/HunterEl/linux/Permissions & Users/2016-09-28.bash
@@ -0,0 +1,5 @@
+The `setuid` permission
+Executing commands with `sudo` without password
+Granting `root` access to a user
+Making a file unalterable with `chattr`
+Get file permissions in octal form
