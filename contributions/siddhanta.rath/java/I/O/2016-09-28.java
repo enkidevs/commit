56f65717a325e480c7326d85bc2a12359ead117e
siddhanta.rath <siddhanta.rath@gmail.com>
2016-09-29 15:35:31 +0100

    2016-09-28's java workout

---
+++ contributions/siddhanta.rath/java/I/O/2016-09-28.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+Deleting files and directories
+The `Console` class
