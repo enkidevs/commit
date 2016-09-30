93d2357ea618b30b7bf68f6164b0af2da72ad6c6
straubt1 <straubt1@mymail.nku.edu>
2016-09-30 01:46:13 +0100

    2016-09-29's linux workout

---
+++ contributions/straubt1/linux/Terminals & Shells/2016-09-29.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Random password generator
+Adding directories to your `$PATH`
