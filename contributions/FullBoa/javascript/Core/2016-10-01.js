3562948dc6cd29272e26721b68e0c7ace5c3718e
FullBoa <kafka.fb@gmail.com>
2016-10-01 16:47:59 +0100

    2016-10-01's javascript workout

---
+++ contributions/FullBoa/javascript/Core/2016-10-01.js
@@ -0,0 +1,5 @@
+Deleting properties
+Most efficient way to build `HTML` strings
+Performing a function at timed intervals
+Use `splice()` to remove an item from an array
+Data type comparison in `switch` statements
