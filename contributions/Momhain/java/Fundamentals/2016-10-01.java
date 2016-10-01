6d8e2c13264dd740e3a3e009092ff77021f0e166
Momhain <romain.r.gautier@gmail.com>
2016-10-01 12:38:14 +0100

    2016-10-01's java workout

---
+++ contributions/Momhain/java/Fundamentals/2016-10-01.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
