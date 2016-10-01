962471025aac3021d1d60532ca7528b6155bdb32
JackForbes <jackforbes24@gmail.com>
2016-10-01 22:09:29 +0100

    2016-10-01's javascript workout

---
+++ contributions/JackForbes/javascript/Core/2016-10-01.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Using the double tilde `~~` 
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Extra function arguments are undefined by default
