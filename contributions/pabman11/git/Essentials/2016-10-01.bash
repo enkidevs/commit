2d3ab97f9e0168d73e6ee6b617ebf074c151e9a5
pabman11 <pablofornalimanzanas@gmail.com>
2016-10-01 14:01:17 +0100

    2016-10-01's git workout

---
+++ contributions/pabman11/git/Essentials/2016-10-01.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+You can use git offline!
+Make your log output pretty
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
