7bc6a7131d988c2031503e1c5e3d3df0a266f6cc
LarryG <laurynasgiriunas@gmail.com>
2016-10-01 17:42:15 +0100

    2016-10-01's java workout

---
+++ contributions/LarryG/java/Fundamentals/2016-10-01.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between ```HashMap``` and ```Hashtable```
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
