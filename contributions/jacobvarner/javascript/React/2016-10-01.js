82056228500fa805166addb929972a48d4ee3389
jacobvarner <jacob.varner14@gmail.com>
2016-10-02 00:30:34 +0100

    2016-10-01's javascript workout

---
+++ contributions/jacobvarner/javascript/React/2016-10-01.js
@@ -0,0 +1,6 @@
+**React** in-line styles
+`if`-`else` statements in **JSX** and **React**
+componentWillReceiveProps Not Triggered After Mounting
+
+`ref` callback attribute
+Enhanced Prop Validation 
