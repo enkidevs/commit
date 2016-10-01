ccdc9310794e0ee3e0f2e65d7ff9a44df8efe36e
Metalhearf <dasilvaadrien@hotmail.fr>
2016-10-01 15:13:06 +0100

    2016-10-01's linux workout

---
+++ contributions/Metalhearf/linux/System Management/2016-10-01.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
