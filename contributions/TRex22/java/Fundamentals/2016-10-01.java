ec4d10bed61c7df38d37a3644f3230fbdc56e1ed
TRex22 <contact@jasonchalom.com>
2016-10-01 15:00:02 +0100

    2016-10-01's java workout

---
+++ contributions/TRex22/java/Fundamentals/2016-10-01.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Measuring time
+Using `synchronized` statements
+Using an interface as a parameter
