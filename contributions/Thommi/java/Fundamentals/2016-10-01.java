e9fe142b9d024dfce96a8539aa26909eea7af38e
Thommi <thomasandre84@gmail.com>
2016-10-01 19:11:29 +0100

    2016-10-01's java workout

---
+++ contributions/Thommi/java/Fundamentals/2016-10-01.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Using bounded type parameters in generic methods
+Always specify type parameter
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
