9cbfdea8de836359a42a0bc5d28df1be628c8822
SondaP <paxxa.kontakt@gmail.com>
2016-10-01 15:26:45 +0100

    2016-09-30's linux workout

---
+++ contributions/SondaP/linux/System Management/2016-09-30.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Incorrect time on dual boot systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
