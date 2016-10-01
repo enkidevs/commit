e15e824b76d3482fbf83e5f88bb5b17635daf930
Sensei <przemyslaw.cabaj@gmail.com>
2016-10-01 20:11:24 +0100

    2016-10-01's java workout

---
+++ contributions/Sensei/java/Fundamentals/2016-10-01.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Metadata: creating a user-defined file attribute
+Measuring time
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
