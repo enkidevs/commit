24d4be7ab119b6289bda10d16dbd9f26ebd52624
DIrvin <drk.irvin90@gmail.com>
2016-10-01 20:26:50 +0100

    2016-10-01's java workout

---
+++ contributions/DIrvin/java/I/O/2016-10-01.java
@@ -0,0 +1,5 @@
+How to list all files in a directory that match a filename extension in Java
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Using the `OpenOption...` vararg to define file writing behavior
