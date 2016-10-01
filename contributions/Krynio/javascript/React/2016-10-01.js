36da00c2c31fed3116a291ccf906d042cd10294c
Krynio <krynio7@gmail.com>
2016-10-01 12:36:55 +0100

    2016-10-01's javascript workout

---
+++ contributions/Krynio/javascript/React/2016-10-01.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+`if`-`else` statements in **JSX** and **React**
+Load Initial Data via AJAX
+Use **React** with other libraries
+Shortcut for transferring props
