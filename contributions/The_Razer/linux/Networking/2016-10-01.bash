825b2ca40718a6706614e8fd15f30e94caaeb70c
The_Razer <crazyrazer@inbox.ru>
2016-10-01 15:53:05 +0100

    2016-10-01's linux workout

---
+++ contributions/The_Razer/linux/Networking/2016-10-01.bash
@@ -0,0 +1,5 @@
+Useful ssh client optimizations 
+Get real network statistics with `slurm`
+List your IPv6 configuration
+Bandwidth monitoring tools 
+Use `tcpdump` to listen to network interface traffic
