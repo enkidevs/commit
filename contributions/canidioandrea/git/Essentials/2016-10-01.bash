9ac21c7dbcaa38672158a1397936f34daee6366f
canidioandrea <canidio.a@gmail.com>
2016-10-01 15:07:30 +0100

    2016-10-01's git workout

---
+++ contributions/canidioandrea/git/Essentials/2016-10-01.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Using aliases for git commands
+What is stored in a commit?
+Use `short` status to make output more compact
+`master` and `origin` aren't special
