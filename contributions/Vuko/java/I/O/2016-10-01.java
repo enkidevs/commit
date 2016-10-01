4ee7a7261987e65f092be457a53986d1300b60a7
Vuko <dawidwdvd@yahoo.com>
2016-10-01 15:28:49 +0100

    2016-10-01's java workout

---
+++ contributions/Vuko/java/I/O/2016-10-01.java
@@ -0,0 +1,5 @@
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
