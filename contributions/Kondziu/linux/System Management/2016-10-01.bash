b049e85a0539a6d009388290c5cfef90437f8bd9
Kondziu <kondziu1988@gmail.com>
2016-10-01 20:21:23 +0100

    2016-10-01's linux workout

---
+++ contributions/Kondziu/linux/System Management/2016-10-01.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
