0919cec4fb67ba61e908547b20111eb323f7e3ef
JohnnyN <johnnytn.mail@gmail.com>
2016-10-01 22:21:54 +0100

    2016-10-01's java workout

---
+++ contributions/JohnnyN/java/Fundamentals/2016-10-01.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
