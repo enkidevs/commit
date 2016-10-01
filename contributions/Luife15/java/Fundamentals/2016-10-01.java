1fec8db4103a6a3321efc7ff9fed34f086f66810
Luife15 <lfpineros@hotmail.com>
2016-10-01 18:21:05 +0100

    2016-10-01's java workout

---
+++ contributions/Luife15/java/Fundamentals/2016-10-01.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
