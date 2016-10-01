8c4d11c42e7da4e6dc5d87a4fc1f70c84a1f2d73
DVDripXvid <dvdrip.xvid.avi@gmail.com>
2016-10-01 14:10:01 +0100

    2016-10-01's javascript workout

---
+++ contributions/DVDripXvid/javascript/Browser APIs/2016-10-01.js
@@ -0,0 +1,5 @@
+Don't assume that HTML script tags are always run sequentially
+CSS for when JavaScript is enabled
+Form a `URL` from its parts 
+FileReader.readAsArrayBuffer()
+Window.sessionStorage
