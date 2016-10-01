c04698e990c503944ba4f2a5ac08205f37fde5b4
winklerrr <enki@sandrowinkler.com>
2016-10-01 13:01:14 +0100

    2016-10-01's java workout

---
+++ contributions/winklerrr/java/Fundamentals/2016-10-01.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
