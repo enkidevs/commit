233dd39af74b678f3b3b74cc583ab465af64771d
casdidier <cheung.didier@gmail.com>
2016-10-02 00:53:49 +0100

    2016-10-01's java workout

---
+++ contributions/casdidier/java/Data Types/2016-10-01.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Converting numbers to strings
+Converting a string to upper or lower case
+String concatenation in Java
+Do not attempt comparisons with NaN
