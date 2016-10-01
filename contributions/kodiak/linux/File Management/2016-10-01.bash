aeb24425f0bab4b9f2f1436d46f478e1f61f7424
kodiak <mgoyanes@gmail.com>
2016-10-02 00:02:26 +0100

    2016-10-01's linux workout

---
+++ contributions/kodiak/linux/File Management/2016-10-01.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
