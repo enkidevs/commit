5d99fc5219434ef4790f2d568f6536fd18c59208
shark09 <simon.dalvy@gmail.com>
2016-10-01 23:41:44 +0100

    2016-10-01's linux workout

---
+++ contributions/shark09/linux/System Management/2016-10-01.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
