efdb2eeec78a02902dfb38a8b87cdf80927c8c29
mjbozzo <mjbozzo@gmail.com>
2016-10-01 18:29:03 +0100

    2016-10-01's javascript workout

---
+++ contributions/mjbozzo/javascript/React/2016-10-01.js
@@ -0,0 +1,7 @@
+Type of the Children props
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Immutability helpers in **React**
+Validate for required props
