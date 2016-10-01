86b7fd0234c3d2f21a288804e212a3edf2d6f0f6
jabisu <yosoyjavier@gmail.com>
2016-10-01 16:34:26 +0100

    2016-10-01's linux workout

---
+++ contributions/jabisu/linux/System Management/2016-10-01.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
