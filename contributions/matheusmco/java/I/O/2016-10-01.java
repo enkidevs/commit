e77507d329a8bd21670610afa18b434a36d68e5d
matheusmco <matheus0mco@gmail.com>
2016-10-01 12:38:45 +0100

    2016-10-01's java workout

---
+++ contributions/matheusmco/java/I/O/2016-10-01.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Checking for the existence of files and directories
+Reading file attributes
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
