9acc3a7bd1342b44cd264f44e56c91977d67a7bc
padawan4330 <padawan4330@gmail.com>
2016-10-01 13:57:36 +0100

    2016-10-01's java workout

---
+++ contributions/padawan4330/java/Fundamentals/2016-10-01.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
