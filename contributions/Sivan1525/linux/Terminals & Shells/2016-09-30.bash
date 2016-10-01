a65bdb80e49a81c0a8f1d7344d912a0dd7ad6b75
Sivan1525 <siva20078@gmail.com>
2016-10-01 15:47:30 +0100

    2016-09-30's linux workout

---
+++ contributions/Sivan1525/linux/Terminals & Shells/2016-09-30.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
