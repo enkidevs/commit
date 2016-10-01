5b43ef8f24d38e61a91c466f26ec8123fba5c165
srbaxter <srosebaxter@gmail.com>
2016-10-01 15:25:49 +0100

    2016-10-01's javascript workout

---
+++ contributions/srbaxter/javascript/Core/2016-10-01.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Setting default values with `short circuiting`
+Meaning of polyfill
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
