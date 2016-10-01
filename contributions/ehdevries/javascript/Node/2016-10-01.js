9ae90b7c0552a9f485611165f2d7a1291afee83c
ehdevries <ehdevries@gmail.com>
2016-10-01 21:50:22 +0100

    2016-10-01's javascript workout

---
+++ contributions/ehdevries/javascript/Node/2016-10-01.js
@@ -0,0 +1,5 @@
+First-error callbacks in **Node**
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
+Manage child processes in **Node**
+Middleware/pipeline design pattern
