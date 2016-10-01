6a578971c3eea48b72eb451daf9b2185b60671dc
DanielBertoldi <danielbertoldi@msn.com>
2016-10-02 00:18:17 +0100

    2016-10-01's linux workout

---
+++ contributions/DanielBertoldi/linux/File Management/2016-10-01.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Practical `du` command
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
