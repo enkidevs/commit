6b9411a831ce9b2628c037516b37559b3aaa3aee
apopiidoru <apopiidoru@yahoo.com>
2016-10-01 18:26:42 +0100

    2016-10-01's linux workout

---
+++ contributions/apopiidoru/linux/System Management/2016-10-01.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
