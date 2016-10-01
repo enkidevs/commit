ab23980734c3e1d1fb108f92b7454e324e84098e
r2mi63 <remi.charbo@gmail.com>
2016-10-01 21:12:04 +0100

    2016-10-01's linux workout

---
+++ contributions/r2mi63/linux/File Management/2016-10-01.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
