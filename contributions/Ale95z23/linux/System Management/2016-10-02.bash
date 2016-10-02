b9d4f1f9949cc079aa6be360078da814657d1565
Ale95z23 <ale95z23@gmail.com>
2016-10-02 17:39:57 +0100

    2016-10-02's linux workout

---
+++ contributions/Ale95z23/linux/System Management/2016-10-02.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Common log files
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
