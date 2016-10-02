16d92560d101ef861c276d559d0f200e01916749
Grorel <grorel.kazui@gmail.com>
2016-10-02 08:27:42 +0100

    2016-10-02's javascript workout

---
+++ contributions/Grorel/javascript/Functions/2016-10-02.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+`.bind()` 
+Method chaining
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
