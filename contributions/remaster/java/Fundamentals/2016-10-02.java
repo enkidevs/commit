3ea8d56d2f0dc55d57fd585be1d34f4146bad242
remaster <daniel.neuman@gmail.com>
2016-10-02 18:54:32 +0100

    2016-10-02's java workout

---
+++ contributions/remaster/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Ternary operator
+Writing generic methods
+Distinction between `public` and `private` methods
+Updating interfaces by using `default` methods
