75d2e8d85d6ecf92b11562a0b41acd4240cc6ee1
Fman1257 <feliceman@gmail.com>
2016-10-02 20:28:54 +0100

    2016-10-02's git workout

---
+++ contributions/Fman1257/git/Essentials/2016-10-02.bash
@@ -0,0 +1,5 @@
+Search by commit message keyword
+How to write a git commit message
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
