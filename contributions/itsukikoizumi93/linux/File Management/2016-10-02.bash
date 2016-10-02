ea4c4a0fddccf8560572c390202cf15080d79806
itsukikoizumi93 <itsukikoizumi93@gmail.com>
2016-10-02 22:11:33 +0100

    2016-10-02's linux workout

---
+++ contributions/itsukikoizumi93/linux/File Management/2016-10-02.bash
@@ -0,0 +1,7 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
