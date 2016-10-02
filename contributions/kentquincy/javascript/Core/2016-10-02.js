f6024d817714ea40e8061a08a49078fb6d23d920
kentquincy <lui89lizama@gmail.com>
2016-10-02 20:47:50 +0100

    2016-10-02's javascript workout

---
+++ contributions/kentquincy/javascript/Core/2016-10-02.js
@@ -0,0 +1,5 @@
+Data type comparison in `switch` statements
+Counting the months from zero
+Avoid using `with`
+Extra function arguments are undefined by default
+Prevent background image flicker in **IE**
