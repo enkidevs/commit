69394dfd5af93dd3bc79c2382d49c3cfdb357b91
AndersB <anders.barth@gmail.com>
2016-10-02 09:18:13 +0100

    2016-10-02's linux workout

---
+++ contributions/AndersB/linux/Terminals & Shells/2016-10-02.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
