872234c8d85fd8a911b7d5aec28d80ff5ba41cd4
bongy <a.bongy93@gmail.com>
2016-10-02 13:33:30 +0100

    2016-10-02's linux workout

---
+++ contributions/bongy/linux/System Management/2016-10-02.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Finding Open Files With `lsof`
