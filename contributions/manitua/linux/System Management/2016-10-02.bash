9ea12834dbf81df816cee5e74e4ec6d29f161e14
manitua <jasmin.beharic@gmail.com>
2016-10-02 10:03:44 +0100

    2016-10-02's linux workout

---
+++ contributions/manitua/linux/System Management/2016-10-02.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Detect your linux distribution
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Finding Open Files With `lsof`
