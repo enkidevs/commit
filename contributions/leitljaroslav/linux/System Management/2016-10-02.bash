47a90117a8dc54c73375087012ba166471d864d9
leitljaroslav <leitl.jaroslav@gmail.com>
2016-10-02 22:40:29 +0100

    2016-10-02's linux workout

---
+++ contributions/leitljaroslav/linux/System Management/2016-10-02.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Incorrect time on dual boot systems
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
