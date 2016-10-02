8b3594898ed2c3c5fd8ea9396f57e078af8d80cb
superfunkminister <hello@piers.io>
2016-10-02 19:16:26 +0100

    2016-10-02's javascript workout

---
+++ contributions/superfunkminister/javascript/Node/2016-10-02.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+`process.nextTick()` callback pattern
+`exports` vs. `module.exports` in **Node**
