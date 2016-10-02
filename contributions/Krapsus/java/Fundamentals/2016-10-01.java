70ff5e9113646f9cfed0f5e07aa59c345b35ad8d
Krapsus <fernandomartinez0295@gmail.com>
2016-10-02 02:51:11 +0100

    2016-10-01's java workout

---
+++ contributions/Krapsus/java/Fundamentals/2016-10-01.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Difference between JRE and JDK?
+The trade-off between short and descriptive names
+Use strings in a `switch` statement
+How range operations work
