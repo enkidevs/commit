a5f688dbdbcd218f42b59054465da20694fe9540
CubeAuCarre <william.t.briant@gmail.com>
2016-10-02 15:38:41 +0100

    2016-10-02's java workout

---
+++ contributions/CubeAuCarre/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
