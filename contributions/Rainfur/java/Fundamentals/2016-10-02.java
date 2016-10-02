53c3b5e6346687576184624aa7ab6e6ba79c03c9
Rainfur <meltonaerik@gmail.com>
2016-10-02 21:52:46 +0100

    2016-10-02's java workout

---
+++ contributions/Rainfur/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
