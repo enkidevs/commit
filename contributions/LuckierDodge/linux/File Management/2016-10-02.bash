641fc23a433650e3baf83231d0f08d28496fe5d5
LuckierDodge <ryandlewis.rl@gmail.com>
2016-10-02 19:10:52 +0100

    2016-10-02's linux workout

---
+++ contributions/LuckierDodge/linux/File Management/2016-10-02.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
+Delete commands aliases
