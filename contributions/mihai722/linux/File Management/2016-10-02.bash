4db55c5003bc4104a698981d7c9561f95cc1c4cc
mihai722 <mihai7221@gmail.com>
2016-10-02 07:59:25 +0100

    2016-10-02's linux workout

---
+++ contributions/mihai722/linux/File Management/2016-10-02.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Delete commands aliases
