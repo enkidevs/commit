3de47583a4e1eaa2df3ef1509e2640641858fda4
gblake707 <gblake707@yahoo.com>
2016-10-02 18:21:01 +0100

    2016-10-02's java workout

---
+++ contributions/gblake707/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
