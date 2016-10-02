a1c76c19c0b81b1ab134cd4707d6580e103a4d30
choolwe <nakalonga@gmail.com>
2016-10-02 08:45:05 +0100

    2016-10-02's javascript workout

---
+++ contributions/choolwe/javascript/Functions/2016-10-02.js
@@ -0,0 +1,4 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Changing a functions context with `fn.call(object)`
