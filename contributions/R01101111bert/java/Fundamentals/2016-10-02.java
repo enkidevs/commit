f2aae6decddf60831da9ea089ecc204c91e6d61f
R01101111bert <robert.obkircher@gmail.com>
2016-10-02 20:45:38 +0100

    2016-10-02's java workout

---
+++ contributions/R01101111bert/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
