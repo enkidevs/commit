6ad7f40fbbd39b4e1cd344142f36556d25afb34a
Azteca <eugenio.mg@gmail.com>
2016-10-02 21:47:44 +0100

    2016-10-02's linux workout

---
+++ contributions/Azteca/linux/System Management/2016-10-02.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Changing the `/tmp` cleanup frequency
+Commands to shutdown or restart the system
+Cancel a system shutdown
