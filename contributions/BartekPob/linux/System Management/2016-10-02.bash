6d279036f8217456281a9c215178d4afdf2c4057
BartekPob <bartosz@pobocha.pl>
2016-10-02 15:24:18 +0100

    2016-10-02's linux workout

---
+++ contributions/BartekPob/linux/System Management/2016-10-02.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
