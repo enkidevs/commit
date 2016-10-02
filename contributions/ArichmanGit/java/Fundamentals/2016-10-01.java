b6e09018787565b9caaa9100baf5e24559773ea6
ArichmanGit <alexdrichman@gmail.com>
2016-10-02 06:28:01 +0100

    2016-10-01's java workout

---
+++ contributions/ArichmanGit/java/Fundamentals/2016-10-01.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Using an interface as a parameter
