3f8afa6d2852cc68fe4df39d87253eb77b4c8a65
adesam146 <a.samuel.o@hotmail.co.uk>
2016-10-02 22:14:41 +0100

    2016-10-02's java workout

---
+++ contributions/adesam146/java/Data Types/2016-10-02.java
@@ -0,0 +1,5 @@
+Two ways to use an `Iterator`
+Use `Arrays.asList()` to initialise lists
+Converting a string to upper or lower case
+Finding a substring in a string
+`StringBuffer` vs `StringBuilder`
