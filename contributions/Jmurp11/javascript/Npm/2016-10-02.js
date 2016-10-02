aafc1079bffbf2e85a36ab9cf476bbed5f2f6381
Jmurp11 <murphyj1011@gmail.com>
2016-10-02 16:54:50 +0100

    2016-10-02's javascript workout

---
+++ contributions/Jmurp11/javascript/Npm/2016-10-02.js
@@ -0,0 +1,5 @@
+Environmental variables and flags for `npm`
+`npm` verifies `sha1` hashes of packages
+Tab completion in `npm`
+Current Lifecycle Event
+Lock down dependency versions by shrinkwrapping
