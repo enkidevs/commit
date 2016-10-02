701d4a59c1ff40fcf93dfd11e82d90f656713b06
AlphaAtlanta <hubertdolny@gmail.com>
2016-10-02 20:52:10 +0100

    2016-10-02's java workout

---
+++ contributions/AlphaAtlanta/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
