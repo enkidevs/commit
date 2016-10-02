f3751fac3f57cf083d8e24a26569ec2fa242f213
petervasil1234 <petervasil1234@gmail.com>
2016-10-02 11:56:59 +0100

    2016-10-02's java workout

---
+++ contributions/petervasil1234/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
