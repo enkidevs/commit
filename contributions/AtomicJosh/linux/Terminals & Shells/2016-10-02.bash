72bd9b3f643a96e99205ca0ab9f5d901e44bb105
AtomicJosh <jtillett86@gmail.com>
2016-10-02 11:17:23 +0100

    2016-10-02's linux workout

---
+++ contributions/AtomicJosh/linux/Terminals & Shells/2016-10-02.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Clear the terminal instantly
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
