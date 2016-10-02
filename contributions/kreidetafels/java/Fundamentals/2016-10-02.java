f6c12f4b17c09d894aa35889c7551de8e9963860
kreidetafels <kreidetafels@gmail.com>
2016-10-02 15:25:06 +0100

    2016-10-02's java workout

---
+++ contributions/kreidetafels/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
