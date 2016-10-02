fac59396a29eaa9e4a8bf52fe5e646b9dafc1b50
azhall <hall@afraidofheights.net>
2016-10-02 01:05:44 +0100

    2016-10-01's java workout

---
+++ contributions/azhall/java/Fundamentals/2016-10-01.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
