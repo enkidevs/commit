499d750c734cb15a8f581d8b18f4e1a20b5a5e25
wdwtlwnd <wdwtlwnd@gmail.com>
2016-10-02 07:23:08 +0100

    2016-10-01's javascript workout

---
+++ contributions/wdwtlwnd/javascript/Core/2016-10-01.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Setting default values with `short circuiting`
+Infix operators are left-associative
+JavaScript compilation
+Getting the *real* dimensions of an image
