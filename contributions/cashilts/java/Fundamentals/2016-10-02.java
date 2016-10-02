1bebef309845c9f81795c7c7dd439801c2b6adee
cashilts <christopherhilts@gmail.com>
2016-10-02 20:34:57 +0100

    2016-10-02's java workout

---
+++ contributions/cashilts/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
