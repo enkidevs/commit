46ad72f679923f1219efc067b9f00fc12d90a6ff
aono <poprosturonin@gmail.com>
2016-10-02 01:33:45 +0100

    2016-10-01's java workout

---
+++ contributions/aono/java/Fundamentals/2016-10-01.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
