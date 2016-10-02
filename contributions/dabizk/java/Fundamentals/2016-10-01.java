a4f74591ad422ac89f2cca84045d8cd1b068f812
dabizk <dabizk@yahoo.com>
2016-10-02 02:10:55 +0100

    2016-10-01's java workout

---
+++ contributions/dabizk/java/Fundamentals/2016-10-01.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+`Autoboxing` and `Unboxing`
+Using an interface as a parameter
