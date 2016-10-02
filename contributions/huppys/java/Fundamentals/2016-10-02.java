68fac9e11268e4b254f5b5a0fb7f2e4a10b7f2ea
huppys <huppys55@gmail.com>
2016-10-02 19:43:56 +0100

    2016-10-02's java workout

---
+++ contributions/huppys/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+The distinction between overloading and overriding methods
+Using the `Console`
+Using `Printstream.format` to format a string before printing
+Limit Accessibility of `Fields`
