76a175e7076991d77a2fc5131d6b1b225b0c4a0e
ghanto <kamil.rokosz@outlook.com>
2016-10-02 17:22:59 +0100

    2016-10-02's linux workout

---
+++ contributions/ghanto/linux/Data Manipulation/2016-10-02.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+Getting rid of duplicate lines
+Using `lynx` to convert HTML to text
+Count non-empty lines with `nl` utility
+`sort` lines in a text file
