960cc846fd13ae8ee86a7a9c73883068f15fd134
mrkimo1998 <mrkimo1998@protonmail.com>
2016-10-02 09:35:51 +0100

    2016-10-02's java workout

---
+++ contributions/mrkimo1998/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
