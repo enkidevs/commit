1302ec1fc113a4a14f4d659ffefaafdda70d69e5
Serverfrog <bastian-venz@t-online.de>
2016-10-02 14:49:13 +0100

    2016-10-02's java workout

---
+++ contributions/Serverfrog/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
