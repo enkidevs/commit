7f750868e34ca768eda63f2d4a7aeec0bb75a385
klingan <andy@klingeleers.com>
2016-10-02 18:18:58 +0100

    2016-10-02's java workout

---
+++ contributions/klingan/java/Error Handling/2016-10-02.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Creating new Exceptions
+Checking a double for `NaN` value
