ffac74095055799e22634e23d8586afecd7baa0a
ktrio3 <kevin.dennis1995@yahoo.com>
2016-10-02 15:14:49 +0100

    2016-10-02's linux workout

---
+++ contributions/ktrio3/linux/System Management/2016-10-02.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
