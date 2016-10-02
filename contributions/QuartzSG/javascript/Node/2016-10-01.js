0ee14f45333b6bae6a571284bf972ccf7fbb924d
QuartzSG <adrianquartx@gmail.com>
2016-10-02 04:56:53 +0100

    2016-10-01's javascript workout

---
+++ contributions/QuartzSG/javascript/Node/2016-10-01.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+**Node** `os` module
+`exports` vs. `module.exports` in **Node**
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
