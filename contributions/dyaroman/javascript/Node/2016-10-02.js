219df81d6da2ca2d3c03bdb21d962d0810a91b15
dyaroman <dyaroman@gmail.com>
2016-10-02 17:11:25 +0100

    2016-10-02's javascript workout

---
+++ contributions/dyaroman/javascript/Node/2016-10-02.js
@@ -0,0 +1,5 @@
+The built-in Node debugger
+`uncaughtException` listener in Node.js
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
+Middleware/pipeline design pattern
