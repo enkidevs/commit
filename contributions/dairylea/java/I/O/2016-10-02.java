3303185da0d4f4242120e9e572e1d7d1e2b8eb2e
dairylea <dannylcranmer@gmail.com>
2016-10-02 19:42:20 +0100

    2016-10-02's java workout

---
+++ contributions/dairylea/java/I/O/2016-10-02.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+List contents of a directory with `newDirectoryStream`
