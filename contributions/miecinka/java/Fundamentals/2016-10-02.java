732a918dc2b825dfef1ae3ba059f704982108189
miecinka <mietinhovanish@gmail.com>
2016-10-02 08:53:27 +0100

    2016-10-02's java workout

---
+++ contributions/miecinka/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Using the `Console`
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
