b7596a98b9e55c83a1ccd52b7fe91ebd47567025
nmarshall <marshall.nicolas@gmail.com>
2016-10-02 13:25:13 +0100

    2016-10-02's linux workout

---
+++ contributions/nmarshall/linux/Terminals & Shells/2016-10-02.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Terminal based browser
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
