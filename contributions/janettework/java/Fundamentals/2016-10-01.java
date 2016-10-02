4f59bdc1a9f5231bdda08563bfb1e87c88bcd380
janettework <janettechiu@gmail.com>
2016-10-02 08:33:15 +0100

    2016-10-01's java workout

---
+++ contributions/janettework/java/Fundamentals/2016-10-01.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Measuring time
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
