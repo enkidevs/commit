750f925f3089c3df73d2faffd4799c8fc841177a
landkommunenhippie <hahn.tobias66@gmail.com>
2016-10-02 23:21:16 +0100

    2016-10-02's javascript workout

---
+++ contributions/landkommunenhippie/javascript/Node/2016-10-02.js
@@ -0,0 +1,5 @@
+Wrapping errors in Node.js using _node-verror_
+Manage child processes in **Node**
+Bulk-Write in **Node** with `cork()`
+Middleware/pipeline design pattern
+Avoid using `sudo` when running Node
