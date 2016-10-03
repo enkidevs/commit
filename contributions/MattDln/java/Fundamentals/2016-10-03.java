b237539d32cb6c78d9440b2e72f5b3438a63b30d
MattDln <matthieu.deillon@gmail.com>
2016-10-03 17:47:48 +0100

    2016-10-03's java workout

---
+++ contributions/MattDln/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
