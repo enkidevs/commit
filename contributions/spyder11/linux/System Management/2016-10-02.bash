20e98e6aba05370aee9ae720018f6a6cf3062c59
spyder11 <spyder11@me.com>
2016-10-03 01:09:37 +0100

    2016-10-02's linux workout

---
+++ contributions/spyder11/linux/System Management/2016-10-02.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
