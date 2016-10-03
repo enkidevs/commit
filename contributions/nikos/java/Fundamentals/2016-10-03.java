8df5d29ac7f6c55c0a7a81960a8d62880930e9df
nikos <triantafyllou.ni@gmail.com>
2016-10-03 05:18:29 +0100

    2016-10-03's java workout

---
+++ contributions/nikos/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+Calling one constructor from another
+Locks in `static synchronized` methods
