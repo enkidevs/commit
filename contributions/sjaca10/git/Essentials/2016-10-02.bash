1ec936f5ca00dcb5b82a6e8617980827f6ee34ce
sjaca10 <sjaca10@gmail.com>
2016-10-03 01:45:07 +0100

    2016-10-02's git workout

---
+++ contributions/sjaca10/git/Essentials/2016-10-02.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+Adding a remote repository
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: fetching and pushing
