54e87cac51849f4dcdb283ab7ef73a300161eeab
pewarrenau <pewarrenau@gmail.com>
2016-10-03 09:30:06 +0100

    2016-10-03's git workout

---
+++ contributions/pewarrenau/git/Tools/2016-10-03.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Specify a commit by its ancestry
+Specify a range of commits using double dot syntax
+Sign your work (using GPG)
+Debug using binary search
