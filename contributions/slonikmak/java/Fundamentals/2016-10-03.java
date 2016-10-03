c8ccc009d87e120f5c319fc81e0874ffeb911d63
slonikmak <slonikmak@gmail.com>
2016-10-03 12:59:46 +0100

    2016-10-03's java workout

---
+++ contributions/slonikmak/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use meaningful names
+Java naming conventions
+Using bounded type parameters in generic methods
+How range operations work
