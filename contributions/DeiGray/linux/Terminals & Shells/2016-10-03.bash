e05530aa45a290dbb440ad1682988878d43a247b
DeiGray <fabsider@gmail.com>
2016-10-03 06:43:05 +0100

    2016-10-03's linux workout

---
+++ contributions/DeiGray/linux/Terminals & Shells/2016-10-03.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Find any Unix / Linux command
+Terminal based browser
+Keep useful commands in your shell history with tags
