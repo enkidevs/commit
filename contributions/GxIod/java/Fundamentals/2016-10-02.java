93e614dfe0126ceefeb93a0e5039b699afe9ce43
GxIod <prabvin82@gmail.com>
2016-10-03 05:57:22 +0100

    2016-10-02's java workout

---
+++ contributions/GxIod/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
