ea2c1e46a061e1b4d2322ed979b1c5fe9addb5b7
ColtonGBouchard <cgb14d@my.fsu.edu>
2016-10-03 17:45:35 +0100

    2016-10-03's java workout

---
+++ contributions/ColtonGBouchard/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
