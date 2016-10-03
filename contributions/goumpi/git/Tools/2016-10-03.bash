35007ad30a11e339815ca435d1e9ec98fc6e4f88
goumpi <layenjulian@gmail.com>
2016-10-03 19:18:11 +0100

    2016-10-03's git workout

---
+++ contributions/goumpi/git/Tools/2016-10-03.bash
@@ -0,0 +1,5 @@
+Interactively unstage changes
+Specify a range of commits using double dot syntax
+Useful stashing options
+Remember the results of previous hunk conflicts
+Debug using binary search
