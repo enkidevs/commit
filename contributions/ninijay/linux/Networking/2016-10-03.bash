c0ee98cf14b00a82a0b3c90926013db34a10352d
ninijay <ninijay93@gmail.com>
2016-10-03 21:01:21 +0100

    2016-10-03's linux workout

---
+++ contributions/ninijay/linux/Networking/2016-10-03.bash
@@ -0,0 +1,5 @@
+Find services running on your host
+Limit the `wget` download rate
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
+Short Introduction to `Wget` 
