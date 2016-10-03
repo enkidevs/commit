64145a5911c3842eede641b82ec2151605190a3e
WetwareDefect <john.piatras@gmail.com>
2016-10-03 18:12:14 +0100

    2016-10-03's java workout

---
+++ contributions/WetwareDefect/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Difference between JRE and JDK?
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Writing generic methods
