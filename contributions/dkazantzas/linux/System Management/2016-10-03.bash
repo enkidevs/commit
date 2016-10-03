30e0c423bac0a602c6287783be7903fcede7d1cc
dkazantzas <dimitriskazantzas@hotmail.com>
2016-10-03 07:44:46 +0100

    2016-10-03's linux workout

---
+++ contributions/dkazantzas/linux/System Management/2016-10-03.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Check the HDD with `badblocks`
+Basic service management with `systemd`
+Finding Open Files With `lsof`
