3427c7a3302d972fcf74ce64e4c0482fd85e2eed
sevensilvers <jeffpaltera@outlook.com>
2016-10-04 00:49:51 +0100

    2016-10-03's java workout

---
+++ contributions/sevensilvers/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Supply `toString()` in all classes
