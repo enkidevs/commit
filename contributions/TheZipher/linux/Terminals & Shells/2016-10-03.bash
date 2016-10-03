56386cadc956f026c1e332bbf1f255e410d09a0f
TheZipher <alexmm1907@gmail.com>
2016-10-03 14:12:12 +0100

    2016-10-03's linux workout

---
+++ contributions/TheZipher/linux/Terminals & Shells/2016-10-03.bash
@@ -0,0 +1,5 @@
+Find any Unix / Linux command
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
