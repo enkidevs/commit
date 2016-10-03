06d2dc7f451d80f3b4581258905c3292d7a1dbb0
wtbenica <wtbenica@gmail.com>
2016-10-03 07:57:24 +0100

    2016-10-02's java workout

---
+++ contributions/wtbenica/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Avoid using `static` variables
