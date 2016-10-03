0f5e07a0290c8aac67ca92a0087a9b9c2e8861cc
kmlion <tchoukatte@gmail.com>
2016-10-03 17:10:33 +0100

    2016-10-03's javascript workout

---
+++ contributions/kmlion/javascript/Core/2016-10-03.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+NaN === NaN?
+The difference between null, undefined and NaN
+Prevent background image flicker in **IE**
+Detect **DO NOT TRACK** status
