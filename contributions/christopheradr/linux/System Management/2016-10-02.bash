e1023c4c4fe159b137c81e42425317a74f14b086
christopheradr <christopher.diaz.riv@gmail.com>
2016-10-03 01:22:16 +0100

    2016-10-02's linux workout

---
+++ contributions/christopheradr/linux/System Management/2016-10-02.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
