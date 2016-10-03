dfdf8eb171582d9416fc576c77a76d478d5bf1c7
JulienRepele <j.repele@gmail.com>
2016-10-03 23:19:07 +0100

    2016-10-03's java workout

---
+++ contributions/JulienRepele/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
