3d4bac6a253073ec690c1ddbd3f5e6d1555c69d3
lednhatkhanh <lednhatkhanh@gmail.com>
2016-10-03 10:06:49 +0100

    2016-10-02's javascript workout

---
+++ contributions/lednhatkhanh/javascript/Core/2016-10-02.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Most efficient way to build `HTML` strings
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
