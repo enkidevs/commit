80b6ba9e2a405695633f690e1dee0c1e6a911d9a
peterjokumsen <pjokumsen@gmail.com>
2016-10-03 11:55:08 +0100

    2016-10-03's java workout

---
+++ contributions/peterjokumsen/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Using bounded type parameters in generic methods
+Measuring time
+Using `synchronized` statements
