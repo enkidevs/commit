0eb8f2f83073b0db01e6b2400a430a64a9d3cb2c
ArnauFa <wemgar@hotmail.com>
2016-10-03 10:50:52 +0100

    2016-10-03's javascript workout

---
+++ contributions/ArnauFa/javascript/Functions/2016-10-03.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Anonymous functions
+Get query/url variables
+Function call method
+Changing a functions context with `fn.call(object)`
