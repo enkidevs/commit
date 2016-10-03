74c579200279ee1db49422f402237e35b3265423
butu <ichbingenial@gmail.com>
2016-10-03 13:54:48 +0100

    2016-10-03's linux workout

---
+++ contributions/butu/linux/Terminals & Shells/2016-10-03.bash
@@ -0,0 +1,5 @@
+Find any Unix / Linux command
+Terminal based browser
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
