d5ec210f7887eb78930d4fbff84b7324f36a1962
ghanto <kamil.rokosz@outlook.com>
2016-10-03 20:34:29 +0100

    2016-10-03's linux workout

---
+++ contributions/ghanto/linux/System Management/2016-10-03.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
