ece05353a6f1ca6dd7ecc9ea056648b6df9d1ee0
miecinka <mietinhovanish@gmail.com>
2016-10-03 12:46:50 +0100

    2016-10-03's java workout

---
+++ contributions/miecinka/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Catch multiple exceptions in a single `catch` block
+Distinction between `public` and `private` methods
+Using `Printstream.format` to format a string before printing
