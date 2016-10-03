c282d64684a91a279728085e5db8708d18539df6
jgalinha <j.b.galinha@gmail.com>
2016-10-03 23:28:03 +0100

    2016-10-03's linux workout

---
+++ contributions/jgalinha/linux/System Management/2016-10-03.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
