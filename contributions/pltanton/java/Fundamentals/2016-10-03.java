37f33b874ef4f9e67eb92d0cb42982e3d2f24cd5
pltanton <plotnikovanton@gmail.com>
2016-10-03 23:21:00 +0100

    2016-10-03's java workout

---
+++ contributions/pltanton/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
