886638d82a28594208ea5103e9fa2400709572b0
Pablo_Seguin <llosapablo@gmail.com>
2016-10-03 22:41:46 +0100

    2016-10-03's java workout

---
+++ contributions/Pablo_Seguin/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
