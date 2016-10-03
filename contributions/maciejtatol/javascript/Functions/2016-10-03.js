64db4275a292abb85dd08c634a2c36e0cc02643d
maciejtatol <maciejtatol@gmail.com>
2016-10-03 06:20:14 +0100

    2016-10-03's javascript workout

---
+++ contributions/maciejtatol/javascript/Functions/2016-10-03.js
@@ -0,0 +1,5 @@
+Hoisting
+Function constructor vs. function declaration vs. function expression
+Move cursor at the end of text input
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
