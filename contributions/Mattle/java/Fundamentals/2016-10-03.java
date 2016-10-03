35d070c904a3418391911c02c829f6139cac9bd8
Mattle <mcallier@gmail.com>
2016-10-03 16:53:11 +0100

    2016-10-03's java workout

---
+++ contributions/Mattle/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+Constant notation
+Avoid using `static` variables
