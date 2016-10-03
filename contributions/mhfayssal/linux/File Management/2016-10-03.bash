ae05b21fba00b73a03a974fa72064e42b3c8afaa
mhfayssal <mhfayssal@gmail.com>
2016-10-03 15:47:16 +0100

    2016-10-03's linux workout

---
+++ contributions/mhfayssal/linux/File Management/2016-10-03.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Deleting non-empty directories
