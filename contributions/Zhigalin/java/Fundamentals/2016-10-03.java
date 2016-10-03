ab86e660d165a73e37192ebf086a6d93ea5163ed
Zhigalin <alexander@zhigalin.tk>
2016-10-03 09:43:39 +0100

    2016-10-03's java workout

---
+++ contributions/Zhigalin/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+The distinction between overloading and overriding methods
+Ternary operator
+Calling one constructor from another
+Locks in `static synchronized` methods
