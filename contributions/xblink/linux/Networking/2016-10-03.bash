7223da406dd173f2cf83dec78dc2daec9649b553
xblink <jedeldridge@gmail.com>
2016-10-03 16:35:14 +0100

    2016-10-03's linux workout

---
+++ contributions/xblink/linux/Networking/2016-10-03.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Useful ssh client optimizations 
+List your IPv6 configuration
+Sending emails from terminal / scripts
+Use `tcpdump` to listen to network interface traffic
