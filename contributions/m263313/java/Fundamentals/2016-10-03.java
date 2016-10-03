308a8e528b7f2589687ce3cc35ae8029fc508dd7
m263313 <m263313@gmail.com>
2016-10-03 20:19:00 +0100

    2016-10-03's java workout

---
+++ contributions/m263313/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Use meaningful names
+The trade-off between short and descriptive names
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Calling one constructor from another
