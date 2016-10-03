48e8da5f257f079c5d59c6108ad37bf97f7652f2
lkh4768 <lkh5510@gmail.com>
2016-10-03 23:59:38 +0100

    2016-10-03's linux workout

---
+++ contributions/lkh4768/linux/Permissions In Linux/2016-10-03.bash
@@ -0,0 +1,7 @@
+Users And Groups On Linux
+File Ownership
+Changing File Ownership With `chown`
+Changing File Permissions With `chmod`
+Adding New Users With `useradd`
+Changing Group Membership With `usermod`
+Changing User Passwords With `passwd`
