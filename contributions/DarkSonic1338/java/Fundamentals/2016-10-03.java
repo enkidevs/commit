fe7a56c4b209d3395ac5de510c439be66b23b620
DarkSonic1338 <darksonic1338@gmail.com>
2016-10-03 18:46:44 +0100

    2016-10-03's java workout

---
+++ contributions/DarkSonic1338/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
