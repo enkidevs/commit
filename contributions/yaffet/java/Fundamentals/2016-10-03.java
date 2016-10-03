28ce6e1f3c23e906f7cf406d5928e3d3bda2c1fa
yaffet <uniac@web.de>
2016-10-03 02:48:01 +0100

    2016-10-03's java workout

---
+++ contributions/yaffet/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Avoid using `static` variables
