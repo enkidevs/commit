8b2b251eb5d91fe5f4e28b0f2ec6c10830a60d7d
PrestonMarques <preston_marques@hotmail.com>
2016-10-03 14:42:42 +0100

    2016-10-02's javascript workout

---
+++ contributions/PrestonMarques/javascript/Node/2016-10-02.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Asynchronous File Write/Read in Node.js
+Handle `JSON.parse` error in Node.js
+`process.nextTick()` callback pattern
+Manage child processes in **Node**
