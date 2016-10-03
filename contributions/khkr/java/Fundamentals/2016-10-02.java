ba33ea17228908861ac830b8ae1fb9f2a7e990a6
khkr <khkrlife@gmail.com>
2016-10-03 06:22:26 +0100

    2016-10-02's java workout

---
+++ contributions/khkr/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+Using Inheritance to reduce code repetition
+Inheriting a constructor from a superclass
+Limit Accessibility of `Fields`
