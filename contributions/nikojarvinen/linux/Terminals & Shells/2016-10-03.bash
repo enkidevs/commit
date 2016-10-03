22ea29ae1de22f695434f85e277352b0f6e591d2
nikojarvinen <niko.jarvinen@tuxie.org>
2016-10-03 05:37:42 +0100

    2016-10-03's linux workout

---
+++ contributions/nikojarvinen/linux/Terminals & Shells/2016-10-03.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Terminal incognito mode
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
