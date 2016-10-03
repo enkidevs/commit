f3da3c4162383171277fa40564fce9bfc4b6647f
AaronMorton <santasarmy@gmail.com>
2016-10-03 10:59:11 +0100

    2016-10-03's linux workout

---
+++ contributions/AaronMorton/linux/Terminals & Shells/2016-10-03.bash
@@ -0,0 +1,5 @@
+Quick `bash` shortcuts 
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
