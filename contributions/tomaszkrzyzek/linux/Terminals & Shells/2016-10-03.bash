4f83c02870feeeb6ff2100f6d6173a78554663fb
tomaszkrzyzek <tomasz.krzyzek@gmail.com>
2016-10-03 19:35:59 +0100

    2016-10-03's linux workout

---
+++ contributions/tomaszkrzyzek/linux/Terminals & Shells/2016-10-03.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
