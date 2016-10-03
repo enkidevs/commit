c410013fd16194807dd0b23122ad877f06404c73
NiklasMerz <niklasmerz@gmx.net>
2016-10-03 12:13:10 +0100

    2016-10-03's javascript workout

---
+++ contributions/NiklasMerz/javascript/Node/2016-10-03.js
@@ -0,0 +1,5 @@
+Streams in **Node**
+`uncaughtException` listener in Node.js
+Automatic compilation for Node with **Nodemon** 
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
