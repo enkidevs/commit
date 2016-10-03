d643d4bdce226478dff78473ec0895a16fbfae9f
cryptichorizon <cryptichorizon@gmail.com>
2016-10-03 16:47:31 +0100

    2016-10-03's linux workout

---
+++ contributions/cryptichorizon/linux/File Management/2016-10-03.bash
@@ -0,0 +1,7 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
