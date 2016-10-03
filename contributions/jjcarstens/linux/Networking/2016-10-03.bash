64a656ba94ec9502316c426534198959e47062e4
jjcarstens <jjcarstens@me.com>
2016-10-03 14:29:55 +0100

    2016-10-03's linux workout

---
+++ contributions/jjcarstens/linux/Networking/2016-10-03.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+Get real network statistics with `slurm`
+Limit the `wget` download rate
+Setting up password-free authentication
+Use `tcpdump` to listen to network interface traffic
