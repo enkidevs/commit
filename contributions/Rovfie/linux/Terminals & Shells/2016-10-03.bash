0097391959812d2f84ae83f98e8eb844a7199537
Rovfie <rovervandernoort@gmail.com>
2016-10-03 10:42:14 +0100

    2016-10-03's linux workout

---
+++ contributions/Rovfie/linux/Terminals & Shells/2016-10-03.bash
@@ -0,0 +1,7 @@
+Clear the terminal instantly
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
