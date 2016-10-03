e3e971710787761bdd121164bea8703850614b04
M3T3ORA <luk.macek91@gmail.com>
2016-10-03 10:22:39 +0100

    2016-10-03's javascript workout

---
+++ contributions/M3T3ORA/javascript/Core/2016-10-03.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+Infinity
+Filtering items out of an array
+The difference between null, undefined and NaN
+Extra function arguments are undefined by default
