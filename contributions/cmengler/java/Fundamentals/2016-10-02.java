2c2fe8072693aeb955f94e19e1b1fe10162bf1e6
cmengler <christian@cmengler.com>
2016-10-03 13:31:07 +0100

    2016-10-02's java workout

---
+++ contributions/cmengler/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Using an interface as a parameter
