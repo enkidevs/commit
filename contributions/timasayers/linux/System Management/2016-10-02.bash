f5e3fabc3ea60f7f6404cda26601ca84d4ef29d4
timasayers <tim@timasayers.com>
2016-10-03 10:05:15 +0100

    2016-10-02's linux workout

---
+++ contributions/timasayers/linux/System Management/2016-10-02.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
