f5ce59b91a9996a1dd64ab5f7692ba85c8b77877
Daniel-Heyde <dpheyde@yahoo.com>
2016-10-03 04:47:26 +0100

    2016-10-02's java workout

---
+++ contributions/Daniel-Heyde/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
