0e37047737b4f4f037bdb0a3b18db000865d4ecc
zero9 <c.lobrano@gmail.com>
2016-10-03 22:39:03 +0100

    2016-10-03's linux workout

---
+++ contributions/zero9/linux/System Management/2016-10-03.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Using `dd` To Create Disk Images
