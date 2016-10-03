47fd4a459a1fd6883e016ea06a6c4ddcfb559595
kubik <lkubic@poczta.onet.pl>
2016-10-03 07:46:20 +0100

    2016-10-03's java workout

---
+++ contributions/kubik/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Ternary operator
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
