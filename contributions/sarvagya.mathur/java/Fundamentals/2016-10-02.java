cc82c99f2f6d32820f92150228362376c66e8526
sarvagya.mathur <mathur.sarvagya@gmail.com>
2016-10-03 20:52:09 +0100

    2016-10-02's java workout

---
+++ contributions/sarvagya.mathur/java/Fundamentals/2016-10-02.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
+Using an interface as a parameter
