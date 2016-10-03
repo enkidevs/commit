27d4a68061a3cdd9cf2fcd6af03edc4fc23f08d7
NoName64Bits <ciobanulaur@gmail.com>
2016-10-03 15:25:43 +0100

    2016-10-03's java workout

---
+++ contributions/NoName64Bits/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Avoid using `static` variables
