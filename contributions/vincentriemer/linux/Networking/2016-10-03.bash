8a85157ef5f7a9a7133acbe97e3984ad4c88166d
vincentriemer <vincentriemer@gmail.com>
2016-10-03 13:18:45 +0100

    2016-10-03's linux workout

---
+++ contributions/vincentriemer/linux/Networking/2016-10-03.bash
@@ -0,0 +1,5 @@
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Make use of `netstat`
+Staying connected remotely via SSH without an interactive shell
+Short Introduction to `Wget` 
