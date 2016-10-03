5017a817e7d41f6db10b2b0554bcf146f2a7878e
solohtml <abdonjesusbp@gmail.com>
2016-10-03 06:55:01 +0100

    2016-10-03's linux workout

---
+++ contributions/solohtml/linux/Terminals & Shells/2016-10-03.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
