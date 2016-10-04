5accfdceb0beb27c9abc569e6a56d6f5f5163772
Krapsus <fernandomartinez0295@gmail.com>
2016-10-04 05:58:34 +0100

    2016-10-03's java workout

---
+++ contributions/Krapsus/java/Error Handling/2016-10-03.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
