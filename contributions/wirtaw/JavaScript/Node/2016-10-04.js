f7c319c8c2826b2de04f09c9fd16d78959fabd16
wirtaw <float45@gmail.com>
2016-10-04 22:00:57 +0100

    2016-10-04's JavaScript workout

---
+++ contributions/wirtaw/JavaScript/Node/2016-10-04.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
+`process.nextTick()` callback pattern
+`try-catch` only for **sync** code
