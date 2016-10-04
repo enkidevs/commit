42cc91b782f100f948b61b1d22a0a8bcb71c34be
bendobot <whatsbending@gmail.com>
2016-10-04 05:11:19 +0100

    2016-10-03's linux workout

---
+++ contributions/bendobot/linux/Networking/2016-10-03.bash
@@ -0,0 +1,5 @@
+ Intro to `iptables`
+Check `iptables` firewall status
+Find services running on your host
+Get real network statistics with `slurm`
+Use `tcpdump` to listen to network interface traffic
