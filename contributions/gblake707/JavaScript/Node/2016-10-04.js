b5d7cfafae7b3d18e7283e99f373fae5b4a00d34
gblake707 <gblake707@yahoo.com>
2016-10-04 18:45:57 +0100

    2016-10-04's JavaScript workout

---
+++ contributions/gblake707/JavaScript/Node/2016-10-04.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+Clustering
+`try-catch` only for **sync** code
