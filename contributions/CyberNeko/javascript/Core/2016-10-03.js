6235fed45345c6515ecf8d7a537c17d8fd42eac3
CyberNeko <cyberneo42@gmail.com>
2016-10-04 06:27:21 +0100

    2016-10-03's javascript workout

---
+++ contributions/CyberNeko/javascript/Core/2016-10-03.js
@@ -0,0 +1,5 @@
+Shuffle an array using `sort` and `random`.
+Setting default values with `short circuiting`
+Performing a function at timed intervals
+The difference between null, undefined and NaN
+Round numbers to `N` decimals 
