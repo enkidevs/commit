ba24e1af4cb1a69fbaf5ce350074c7cda777ad02
t1meshift <me@t1meshift.cz.cc>
2016-10-04 01:39:44 +0100

    2016-10-03's java workout

---
+++ contributions/t1meshift/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
