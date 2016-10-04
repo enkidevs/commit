a5780a58017959385a5922e17786d28f496562d7
MakeDreams <thomas-multerer@web.de>
2016-10-04 12:04:05 +0100

    2016-10-04's java workout

---
+++ contributions/MakeDreams/java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Using an interface as a parameter
