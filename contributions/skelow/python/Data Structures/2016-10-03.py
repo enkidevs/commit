1895c8c5c4e50718697004a34321865d8f3c1c7c
skelow <simon.kelow@gmail.com>
2016-10-04 03:48:11 +0100

    2016-10-03's python workout

---
+++ contributions/skelow/python/Data Structures/2016-10-03.py
@@ -0,0 +1,5 @@
+Following PEP 8 styling guideline.
+A simple way to select a random item from a `list/tuple` data stucture
+Best way to implement a simple `queue` 
+`Module`s everywhere!
+Binary record packing with `bytearray`
