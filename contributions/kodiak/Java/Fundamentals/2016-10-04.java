d112c31e19cd4839d6c9c1f6c5e26b576a4294f6
kodiak <mgoyanes@gmail.com>
2016-10-04 23:00:02 +0100

    2016-10-04's Java workout

---
+++ contributions/kodiak/Java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
