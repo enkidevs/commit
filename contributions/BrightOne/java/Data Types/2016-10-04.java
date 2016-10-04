84da12799c8b2a91dcee95402e3f3edb7993e20b
BrightOne <filalex77@gmail.com>
2016-10-04 06:41:31 +0100

    2016-10-04's java workout

---
+++ contributions/BrightOne/java/Data Types/2016-10-04.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Finding a substring in a string
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
+Numeric Conversion - Widening
