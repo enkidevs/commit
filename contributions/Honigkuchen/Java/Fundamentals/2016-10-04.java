92e384d8be6af6b5bde20d16c04204e2e9ced5f0
Honigkuchen <jonashaubold@live.de>
2016-10-04 18:23:08 +0100

    2016-10-04's Java workout

---
+++ contributions/Honigkuchen/Java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Measuring time
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
