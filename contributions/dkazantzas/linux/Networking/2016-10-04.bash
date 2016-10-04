10fdeefc999ed0f4fccca6bcae30e5885431f0c1
dkazantzas <dimitriskazantzas@hotmail.com>
2016-10-04 11:26:39 +0100

    2016-10-04's linux workout

---
+++ contributions/dkazantzas/linux/Networking/2016-10-04.bash
@@ -0,0 +1,5 @@
+Logging dropped firewall packets
+Disable `IPv6`
+Useful ssh client optimizations 
+Use `tcpdump` to listen to network interface traffic
+Staying connected remotely via SSH without an interactive shell
