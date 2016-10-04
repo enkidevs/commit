e9f07b3f3a3b32efac1b234af6be9e419e9ced49
csevillano <csevillano2@gmail.com>
2016-10-05 00:22:57 +0100

    2016-10-04's Linux workout

---
+++ contributions/csevillano/Linux/System Management/2016-10-04.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
