bdeaaca4a3d9a390491d4ef9bac87be1d27b51eb
nimrodo <nimrodo@gmail.com>
2016-10-04 13:21:08 +0100

    2016-10-04's Linux workout

---
+++ contributions/nimrodo/Linux/System Management/2016-10-04.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
