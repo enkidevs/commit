e4aa70a28fc90c99723f028ae903787637719fbb
Werner <werner.foss@gmail.com>
2016-10-04 08:14:32 +0100

    2016-10-04's linux workout

---
+++ contributions/Werner/linux/File Management/2016-10-04.bash
@@ -0,0 +1,6 @@
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
