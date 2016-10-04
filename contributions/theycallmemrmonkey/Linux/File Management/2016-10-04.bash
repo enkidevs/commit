84ba01a2fdeda59824d055a900da2a427be9a72f
theycallmemrmonkey <christopher.kaschny@gmail.com>
2016-10-04 13:48:33 +0100

    2016-10-04's Linux workout

---
+++ contributions/theycallmemrmonkey/Linux/File Management/2016-10-04.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
+Delete commands aliases
