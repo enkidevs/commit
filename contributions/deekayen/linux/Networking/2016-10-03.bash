ea60d49c85925d62bc1024cd85e6c47b60097203
deekayen <deekayen@deekayen.net>
2016-10-04 01:50:35 +0100

    2016-10-03's linux workout

---
+++ contributions/deekayen/linux/Networking/2016-10-03.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+Find services running on your host
+Multiple IP Address with aliasing 
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
