9c308303edcb7b1bfe5a11c4bbc98de029898f69
egalock <abelaranega@gmail.com>
2016-10-04 16:31:58 +0100

    2016-10-04's Linux workout

---
+++ contributions/egalock/Linux/System Management/2016-10-04.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Fixing `locale` issues in Debian systems
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
