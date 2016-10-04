3d3b763753bfb172381de99131d2d0819cf0a7cd
ajlatour <ajlatour@gmail.com>
2016-10-04 16:07:30 +0100

    2016-10-04's Java workout

---
+++ contributions/ajlatour/Java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
+Using an interface as a parameter
