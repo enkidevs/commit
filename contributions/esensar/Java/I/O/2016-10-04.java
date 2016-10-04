d84df375eb19f77d8408769beb0df2ef65e12764
esensar <es.ensar@gmail.com>
2016-10-04 22:58:51 +0100

    2016-10-04's Java workout

---
+++ contributions/esensar/Java/I/O/2016-10-04.java
@@ -0,0 +1,5 @@
+Create system file paths using the `Path` class
+Checking for the existence of files and directories
+Reading file attributes
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
