52016ef468d3f7afffa4d1eabb7f8898a7d59f1b
factoryEquipment <cs.serg@gmail.com>
2016-10-04 04:53:59 +0100

    2016-10-03's java workout

---
+++ contributions/factoryEquipment/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
