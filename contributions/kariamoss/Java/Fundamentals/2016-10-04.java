d142944635eb4fcfedfc64605f01306841c68626
kariamoss <jehanportable@gmail.com>
2016-10-04 23:39:56 +0100

    2016-10-04's Java workout

---
+++ contributions/kariamoss/Java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Measuring time
+Using an interface as a parameter
