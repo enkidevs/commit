de35e23e113eb30afc254f7505621c06e8461ef8
ratvien <ratvien@gmail.com>
2016-10-04 03:44:44 +0100

    2016-10-03's linux workout

---
+++ contributions/ratvien/linux/File Management/2016-10-03.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
