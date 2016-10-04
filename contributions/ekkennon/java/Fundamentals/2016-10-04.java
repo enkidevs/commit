2443702a9d8e89d5a5afb0b0b415fa7057be5376
ekkennon <ekkennon@gmail.com>
2016-10-04 11:36:19 +0100

    2016-10-04's java workout

---
+++ contributions/ekkennon/java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
