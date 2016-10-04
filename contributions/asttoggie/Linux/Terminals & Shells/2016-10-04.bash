420b8835740d6d3be2251daa0d17057ced050b40
asttoggie <ast.toggie@gmail.com>
2016-10-04 21:49:32 +0100

    2016-10-04's Linux workout

---
+++ contributions/asttoggie/Linux/Terminals & Shells/2016-10-04.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Terminal based browser
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
