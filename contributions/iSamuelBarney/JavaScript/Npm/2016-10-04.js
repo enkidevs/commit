17115df0a49b9e143911758a582db1c8096c54bf
iSamuelBarney <samuel.barney@icloud.com>
2016-10-04 14:47:41 +0100

    2016-10-04's JavaScript workout

---
+++ contributions/iSamuelBarney/JavaScript/Npm/2016-10-04.js
@@ -0,0 +1,5 @@
+Manipulate npm packages cache
+npm can handle multiple versions of a dependency
+`npm` verifies `sha1` hashes of packages
+`config` object in `package.json`
+Package distribution tags
