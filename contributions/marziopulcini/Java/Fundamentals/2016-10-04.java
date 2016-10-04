26a759f37bf3c214e16dc68c7b8dee2537fb17be
marziopulcini <marzio.pulcini@at-shopnet.com>
2016-10-04 23:29:46 +0100

    2016-10-04's Java workout

---
+++ contributions/marziopulcini/Java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
