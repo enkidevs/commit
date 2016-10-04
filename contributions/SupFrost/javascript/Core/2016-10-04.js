57605da5784c16df2a6b10e23b071c8a77ee37cb
SupFrost <nickjorens@gmail.com>
2016-10-04 09:06:40 +0100

    2016-10-04's javascript workout

---
+++ contributions/SupFrost/javascript/Core/2016-10-04.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Keywords in hash keys
+Hoisting applies only to variable declarations, not initializations
+The difference between null, undefined and NaN
+Getting the *real* dimensions of an image
