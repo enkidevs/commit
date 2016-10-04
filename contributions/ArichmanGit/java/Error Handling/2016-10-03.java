a622ce68f22bb3527be2c31243624ecc112b46c1
ArichmanGit <alexdrichman@gmail.com>
2016-10-04 07:29:48 +0100

    2016-10-03's java workout

---
+++ contributions/ArichmanGit/java/Error Handling/2016-10-03.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Prevent `exceptions` while logging Data
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
