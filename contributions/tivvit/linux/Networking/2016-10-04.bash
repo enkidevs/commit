c621d011b9f444718694e6ca27adde9a03e1e006
tivvit <tivvit@seznam.cz>
2016-10-04 08:46:11 +0100

    2016-10-04's linux workout

---
+++ contributions/tivvit/linux/Networking/2016-10-04.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Logging dropped firewall packets
+Useful ssh client optimizations 
+Bandwidth monitoring tools 
+Staying connected remotely via SSH without an interactive shell
