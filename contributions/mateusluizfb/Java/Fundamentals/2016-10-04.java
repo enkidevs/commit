9160ecd4d7d3bbe0f7db0b34a88869f0862f607f
mateusluizfb <mateuslfreitasb@gmail.com>
2016-10-04 23:53:18 +0100

    2016-10-04's Java workout

---
+++ contributions/mateusluizfb/Java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
