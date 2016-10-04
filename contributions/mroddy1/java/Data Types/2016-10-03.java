b152d26aee6aeaebfd7ee8a07479a03481042fe8
mroddy1 <mroddy1@msudenver.edu>
2016-10-04 05:28:53 +0100

    2016-10-03's java workout

---
+++ contributions/mroddy1/java/Data Types/2016-10-03.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Finding a substring in a string
+`StringBuffer` vs `StringBuilder`
+Numeric Conversion - Widening
