58e62e7cd47319072288ada6930df670244521de
danrivcap <danrivcap@gmail.com>
2016-10-04 19:29:30 +0100

    2016-10-04's JavaScript workout

---
+++ contributions/danrivcap/JavaScript/Core/2016-10-04.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Deleting properties
+Most efficient way to build `HTML` strings
+Hoisting applies only to variable declarations, not initializations
+Getting the *real* dimensions of an image
