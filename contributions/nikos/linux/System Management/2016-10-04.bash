e7ff5938f0772970fdf7c05a3ad678cf2f30d1c8
nikos <triantafyllou.ni@gmail.com>
2016-10-04 05:17:19 +0100

    2016-10-04's linux workout

---
+++ contributions/nikos/linux/System Management/2016-10-04.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Finding Open Files With `lsof`
