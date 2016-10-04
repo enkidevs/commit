508e6508ce06198e9e34107d0905765679942b12
DainRickards <dain329@yahoo.com>
2016-10-04 08:43:10 +0100

    2016-10-03's linux workout

---
+++ contributions/DainRickards/linux/Terminals & Shells/2016-10-03.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Repeating commands with `watch`
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
