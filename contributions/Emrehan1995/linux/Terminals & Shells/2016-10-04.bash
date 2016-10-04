7a6c3d61c6d2f4af2fcec66c10c1bb6c87c05073
Emrehan1995 <emrehanfirtin@hotmail.de>
2016-10-04 06:59:25 +0100

    2016-10-04's linux workout

---
+++ contributions/Emrehan1995/linux/Terminals & Shells/2016-10-04.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+Retrieve previous arguments
