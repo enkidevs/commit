a24360cf21e8d4297955c8c987430f0a7a0d5e52
dyaroman <dyaroman@gmail.com>
2016-10-04 06:58:35 +0100

    2016-10-04's javascript workout

---
+++ contributions/dyaroman/javascript/Node/2016-10-04.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Custom Node REPL Server
+Use conventional property names for error handling in Node.js
+`process.nextTick()` callback pattern
+`try-catch` only for **sync** code
