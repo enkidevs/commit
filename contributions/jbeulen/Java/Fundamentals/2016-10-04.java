4efe3eb48241bbdf1618a999f6aa95a7aaff3ae8
jbeulen <jeroenbeulen@gmail.com>
2016-10-04 23:32:02 +0100

    2016-10-04's Java workout

---
+++ contributions/jbeulen/Java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+Calling one constructor from another
+Constant notation
+There is no such thing as *pass-by-reference* in Java
