6e970ac05bf4b26d44c95798d9fc59b70bf6e6cb
tnargwoxow <tnargwoxow@gmail.com>
2016-10-04 06:44:47 +0100

    2016-10-04's linux workout

---
+++ contributions/tnargwoxow/linux/System Management/2016-10-04.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Detect your linux distribution
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Finding Open Files With `lsof`
