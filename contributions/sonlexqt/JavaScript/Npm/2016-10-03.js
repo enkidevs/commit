f227d03c69d1c7bc1b89aeb6e8548bfd213ca275
sonlexqt <sonlexqt@gmail.com>
2016-10-04 14:19:54 +0100

    2016-10-03's JavaScript workout

---
+++ contributions/sonlexqt/JavaScript/Npm/2016-10-03.js
@@ -0,0 +1,5 @@
+npm can handle multiple versions of a dependency
+List binaries for scripting in npm
+Manage local node modules with `npm link`
+Tab completion in `npm`
+Lock down dependency versions by shrinkwrapping
