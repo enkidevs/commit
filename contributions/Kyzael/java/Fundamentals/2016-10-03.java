8ee6fac5a2df98564d6141c56a767f963298b5e1
Kyzael <pkealley@gmail.com>
2016-10-04 01:04:52 +0100

    2016-10-03's java workout

---
+++ contributions/Kyzael/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Detect or prevent integer overflow
