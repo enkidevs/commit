30af23400ec3942ca95efe9bd520b377e9f0c160
SolviQorda <soren.goard@gmail.com>
2016-10-04 17:40:53 +0100

    2016-10-04's Java workout

---
+++ contributions/SolviQorda/Java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
