5deb5edfe6910d3f03b1af0fb4a2949be616f0cc
Vira <thomas.ruehrig@studierende.htl-donaustadt.at>
2016-10-04 06:27:28 +0100

    2016-10-04's linux workout

---
+++ contributions/Vira/linux/Terminals & Shells/2016-10-04.bash
@@ -0,0 +1,7 @@
+Clear the terminal instantly
+Useful aliasing in bash
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
