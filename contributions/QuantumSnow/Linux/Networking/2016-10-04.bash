ee983917c9df1466bf1cb2b583ea7e9d3316bc21
QuantumSnow <jakobtheai@gmail.com>
2016-10-04 20:44:17 +0100

    2016-10-04's Linux workout

---
+++ contributions/QuantumSnow/Linux/Networking/2016-10-04.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Check `iptables` firewall status
+Find services running on your host
+Get real network statistics with `slurm`
+Use `tcpdump` to listen to network interface traffic
