737a557a25f1d525e9a49c0e6e8b2795b5788616
pierrot111 <flande.pierre@gmail.com>
2016-10-04 10:12:01 +0100

    2016-10-04's linux workout

---
+++ contributions/pierrot111/linux/File Management/2016-10-04.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Converting between Unix and Windows text files
+Find the unknown process preventing deleting of files
+Easy way of sharing files
