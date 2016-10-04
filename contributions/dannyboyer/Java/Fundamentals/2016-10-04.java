1f8633e33fd059615b48f64df430604c66a95c9b
dannyboyer <dannyboyer49@gmail.com>
2016-10-04 15:10:31 +0100

    2016-10-04's Java workout

---
+++ contributions/dannyboyer/Java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Avoid using `static` variables
+Using an interface as a parameter
