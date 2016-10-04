b79e39dc0aa931a100c5fefe7e7a1db935125819
xetra11 <xetra11@outlook.com>
2016-10-04 20:15:23 +0100

    2016-10-04's Git workout

---
+++ contributions/xetra11/Git/Tools/2016-10-04.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Specify a commit by its ancestry
+Show history of a function
+Remember the results of previous hunk conflicts
+Debug using binary search
