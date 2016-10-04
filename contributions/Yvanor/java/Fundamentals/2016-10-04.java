305464df45e39ffea0c6c40e56c43708cf2a5067
Yvanor <maxime.yusei@orange.fr>
2016-10-04 06:23:08 +0100

    2016-10-04's java workout

---
+++ contributions/Yvanor/java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Connecting to FTP using Java
+Catch multiple exceptions in a single `catch` block
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
