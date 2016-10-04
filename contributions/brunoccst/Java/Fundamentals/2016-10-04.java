8ea1cee417b8ea5b588e4ae41f5badca639877c9
brunoccst <brunoccst@gmail.com>
2016-10-04 23:14:20 +0100

    2016-10-04's Java workout

---
+++ contributions/brunoccst/Java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Using bounded type parameters in generic methods
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
