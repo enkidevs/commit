4b8b2a423eec9d4cc43faebcda968ad8cb62f14f
CoderOfDreams <coderofdreams@gmail.com>
2016-10-04 05:57:17 +0100

    2016-10-03's java workout

---
+++ contributions/CoderOfDreams/java/Fundamentals/2016-10-03.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Using an interface as a parameter
