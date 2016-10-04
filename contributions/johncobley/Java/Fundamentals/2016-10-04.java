99af6fe49e6a0cec10bffe2440ae962e31adfb95
johncobley <john.cobley@comtek.co.uk>
2016-10-04 21:01:29 +0100

    2016-10-04's Java workout

---
+++ contributions/johncobley/Java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Measuring time
+Detect or prevent integer overflow
