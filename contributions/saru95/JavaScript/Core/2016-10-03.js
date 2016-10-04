08befcd88c353b4bafba5fa438223b0251e489d9
saru95 <sarthakmunshi@gmail.com>
2016-10-04 20:26:23 +0100

    2016-10-03's JavaScript workout

---
+++ contributions/saru95/JavaScript/Core/2016-10-03.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Using the double tilde `~~` 
+Most efficient way to build `HTML` strings
+The difference between null, undefined and NaN
+`console.*` in JavaScript
