c15e6633acd96f1d1f23e725c0f0c141ed899553
escherina <rhian.vanesch@gmail.com>
2016-10-04 13:09:34 +0100

    2016-10-04's JavaScript workout

---
+++ contributions/escherina/JavaScript/Node/2016-10-04.js
@@ -0,0 +1,5 @@
+Http Server in **Node**
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+Automatic compilation for Node with **Nodemon** 
+`try-catch` only for **sync** code
