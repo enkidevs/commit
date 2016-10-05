8645f769d889446f740587dec0ff92e1a2b20172
ramandeep89 <ramandeep89@gmail.com>
2016-10-05 13:43:06 +0100

    2016-10-04's JavaScript workout

---
+++ contributions/ramandeep89/JavaScript/Core/2016-10-04.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Most efficient way to build `HTML` strings
+Counting the months from zero
+Prevent background image flicker in **IE**
+Round numbers to `N` decimals 
