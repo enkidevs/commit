a0b93622b92f872a16b434070bd55260850af808
phat-sumo <ethanjpayne1@gmail.com>
2016-10-05 09:41:38 +0100

    2016-10-04's Java workout

---
+++ contributions/phat-sumo/Java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
