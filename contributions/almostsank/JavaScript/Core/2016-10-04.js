47e0b3f62b95697cc642d131a0ef2f190d94ae1f
almostsank <almostsank@gmail.com>
2016-10-05 14:20:53 +0100

    2016-10-04's JavaScript workout

---
+++ contributions/almostsank/JavaScript/Core/2016-10-04.js
@@ -0,0 +1,5 @@
+Shuffle an array using `sort` and `random`.
+Undefined / Empty / Null Checks
+Performing a function at timed intervals
+Use `splice()` to remove an item from an array
+No block scope
