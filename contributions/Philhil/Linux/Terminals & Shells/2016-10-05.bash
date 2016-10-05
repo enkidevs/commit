4b91c4405e0ea561979dd15406fcfec804b7d323
Philhil <philippe@pilarkto.net>
2016-10-05 09:15:29 +0100

    2016-10-05's Linux workout

---
+++ contributions/Philhil/Linux/Terminals & Shells/2016-10-05.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear the terminal instantly
+Conditional command execution
+
+(`&&` operator)
