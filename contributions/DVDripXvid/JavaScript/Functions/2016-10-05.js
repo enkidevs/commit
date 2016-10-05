daa453e0d514f6a2fea6bf2f616c724b433898ca
DVDripXvid <dvdrip.xvid.avi@gmail.com>
2016-10-05 11:01:10 +0100

    2016-10-05's JavaScript workout

---
+++ contributions/DVDripXvid/JavaScript/Functions/2016-10-05.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Method chaining
+Changing a functions context with `fn.call(object)`
