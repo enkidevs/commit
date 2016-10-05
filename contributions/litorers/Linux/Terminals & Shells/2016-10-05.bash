e6b3fa2e22c521ffd7d61ecb1993c17ea1e31403
litorers <alexandrejoseph.tessier@gmail.com>
2016-10-05 13:47:47 +0100

    2016-10-05's Linux workout

---
+++ contributions/litorers/Linux/Terminals & Shells/2016-10-05.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Random password generator
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
