6572a6e06ce153cbaec990382bbae14b249e7980
StilasCzech <stilas.bertelsen@seznam.cz>
2016-10-05 14:39:35 +0100

    2016-10-05's Linux workout

---
+++ contributions/StilasCzech/Linux/Terminals & Shells/2016-10-05.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Aliasing ssh connections
+Useful aliasing in bash
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
