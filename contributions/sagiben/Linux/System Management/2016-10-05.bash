3cfb7a0a93b71a0789cbab5c5b23d3d901143c23
sagiben <ilnextbus@gmail.com>
2016-10-05 04:54:59 +0100

    2016-10-05's Linux workout

---
+++ contributions/sagiben/Linux/System Management/2016-10-05.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Cancel a system shutdown
