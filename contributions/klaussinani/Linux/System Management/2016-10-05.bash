0e5a2528cd71989e2a79ebe696c905c54f5f47ee
klaussinani <klaudiosinani@protonmail.ch>
2016-10-05 20:28:29 +0100

    2016-10-05's Linux workout

---
+++ contributions/klaussinani/Linux/System Management/2016-10-05.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
