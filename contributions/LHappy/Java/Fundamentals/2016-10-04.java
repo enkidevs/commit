04954d4e0bfc0e9b34b9d4ca76b08cff7c305b1f
LHappy <ge1tianlun@163.com>
2016-10-05 15:37:43 +0100

    2016-10-04's Java workout

---
+++ contributions/LHappy/Java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
+Using `Printstream.format` to format a string before printing
