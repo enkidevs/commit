06e11a272d6a53afc6acc952a9257c02ea68d55b
escherina <rhian.vanesch@gmail.com>
2016-10-05 11:08:48 +0100

    2016-10-05's Git workout

---
+++ contributions/escherina/Git/Essentials/2016-10-05.bash
@@ -0,0 +1,5 @@
+Adding a remote repository
+How to set your username and email
+Search by commit message keyword
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
