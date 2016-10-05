ce5b8e093e1306cb55fee6c69253ce07e8859ef5
byte_evolution <cobantudor@gmail.com>
2016-10-05 07:48:40 +0100

    2016-10-05's JavaScript workout

---
+++ contributions/byte_evolution/JavaScript/Core/2016-10-05.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Closures
+Prevent background image flicker in **IE**
