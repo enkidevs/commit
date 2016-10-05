1bf4d955c89e4d9a393e17d82db7e24ec65bdb4c
kalpeshsejpal <sejpalkalpesh@gmail.com>
2016-10-05 18:15:11 +0100

    2016-10-04's Linux workout

---
+++ contributions/kalpeshsejpal/Linux/System Management/2016-10-04.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
