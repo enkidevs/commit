96e5d39f00acb0e4d55e15018c42c2ff8fca0445
mlipa <marcin.lipa@protonmail.com>
2016-10-05 08:56:05 +0100

    2016-10-05's Java workout

---
+++ contributions/mlipa/Java/Fundamentals/2016-10-05.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Inheriting a constructor from a superclass
+Metadata: creating a user-defined file attribute
