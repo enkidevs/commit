d41acf0385abe9bbc694000f3b0f860c77dbe599
djgmk <mgmt@djgmk.com.br>
2016-10-05 12:21:05 +0100

    2016-10-05's Linux workout

---
+++ contributions/djgmk/Linux/System Management/2016-10-05.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
