90a358488f0cbad3f0f2c46c808ff1c5459c47f6
JLDevOps <jldevops@gmail.com>
2016-10-05 17:21:22 +0100

    2016-10-05's Java workout

---
+++ contributions/JLDevOps/Java/Fundamentals/2016-10-05.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Measuring time
+Detect or prevent integer overflow
