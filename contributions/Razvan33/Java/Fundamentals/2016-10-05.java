a8e4246c4ddbb294abe4c35429b23c7d49c515f2
Razvan33 <rzvncc33@gmail.com>
2016-10-05 14:53:04 +0100

    2016-10-05's Java workout

---
+++ contributions/Razvan33/Java/Fundamentals/2016-10-05.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
