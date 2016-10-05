6db0a4cce4948f13c339cdd55b811db0bbf5000a
PizzaFreak <nico.greve89@gmail.com>
2016-10-05 11:01:40 +0100

    2016-10-05's Linux workout

---
+++ contributions/PizzaFreak/Linux/Terminals & Shells/2016-10-05.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
