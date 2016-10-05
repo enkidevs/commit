82050261d05c51b1160625c93d08ce1ac8724065
DaNiiL4 <adaniil4.93@gmail.com>
2016-10-05 06:45:12 +0100

    2016-10-04's Java workout

---
+++ contributions/DaNiiL4/Java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
