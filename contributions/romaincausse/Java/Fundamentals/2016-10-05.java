b726738f68f16d68151f483fdcd3d86df1d018ee
romaincausse <romain.causse@gmail.com>
2016-10-05 22:46:09 +0100

    2016-10-05's Java workout

---
+++ contributions/romaincausse/Java/Fundamentals/2016-10-05.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Supply `toString()` in all classes
