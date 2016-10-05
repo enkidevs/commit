01413f355fad1e1607194d861ad3d9aa5485a0ed
JustCallMeL <mtemple12@gmail.com>
2016-10-05 19:09:21 +0100

    2016-10-05's Java workout

---
+++ contributions/JustCallMeL/Java/Fundamentals/2016-10-05.java
@@ -0,0 +1,5 @@
+Measuring time
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
