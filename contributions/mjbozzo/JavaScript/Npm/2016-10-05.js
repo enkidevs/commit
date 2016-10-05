f869dc4619390ee4804662f3efed2b4f4bf22e88
mjbozzo <mjbozzo@gmail.com>
2016-10-05 12:14:48 +0100

    2016-10-05's JavaScript workout

---
+++ contributions/mjbozzo/JavaScript/Npm/2016-10-05.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Deprecate npm packages
+List binaries for scripting in npm
+Manage local node modules with `npm link`
+Environmental variables and flags for `npm`
