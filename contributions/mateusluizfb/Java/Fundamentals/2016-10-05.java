77658e09d42df69a4cbc6b65009ea0dcfd47c2e3
mateusluizfb <mateuslfreitasb@gmail.com>
2016-10-05 23:37:44 +0100

    2016-10-05's Java workout

---
+++ contributions/mateusluizfb/Java/Fundamentals/2016-10-05.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
