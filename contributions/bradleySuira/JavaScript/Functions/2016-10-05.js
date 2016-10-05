5ed098ed9376034cf4166ba310a05a521055574d
bradleySuira <bradleysuira@gmail.com>
2016-10-05 15:10:35 +0100

    2016-10-05's JavaScript workout

---
+++ contributions/bradleySuira/JavaScript/Functions/2016-10-05.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Function call method
+Move cursor at the end of text input
