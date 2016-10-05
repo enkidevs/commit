4e05d2caf348d60b88440785ab28c0239ccc36c1
iJebus <liam.jones.aus@gmail.com>
2016-10-05 17:42:19 +0100

    2016-10-04's Linux workout

---
+++ contributions/iJebus/Linux/File Management/2016-10-04.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Using `touch` to alter files modification time
+Deleting non-empty directories
