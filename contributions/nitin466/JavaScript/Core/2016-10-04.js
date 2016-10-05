0706d06df0b8d41c0de9a6b7934e82c7696a3d65
nitin466 <nitinkumar01@gmail.com>
2016-10-05 17:07:50 +0100

    2016-10-04's JavaScript workout

---
+++ contributions/nitin466/JavaScript/Core/2016-10-04.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Undefined / Empty / Null Checks
+Deleting properties
+Hoisting applies only to variable declarations, not initializations
+Detect **DO NOT TRACK** status
