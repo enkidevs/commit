0c23490d5aa375ef1bb1adcfd07853fe0edce892
remaster <daniel.neuman@gmail.com>
2016-10-05 19:09:26 +0100

    2016-10-05's Java workout

---
+++ contributions/remaster/Java/Fundamentals/2016-10-05.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Inheriting a constructor from a superclass
+Using `Printstream.format` to format a string before printing
+Implementing an `interface`
