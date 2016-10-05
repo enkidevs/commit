535cc2b838e4f4c4a06abbbc80f849986b083604
toXel <timo625@gmail.com>
2016-10-05 21:36:01 +0100

    2016-10-05's Linux workout

---
+++ contributions/toXel/Linux/Networking/2016-10-05.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Logging dropped firewall packets
+Multiple IP Address with aliasing 
+Limit the `wget` download rate
+Staying connected remotely via SSH without an interactive shell
