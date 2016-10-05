55c9c3afca3f0d817726e78e9b51bf375d3560e4
CMYKpixels <julien.alsina@gmail.com>
2016-10-05 11:40:08 +0100

    2016-10-05's JavaScript workout

---
+++ contributions/CMYKpixels/JavaScript/Core/2016-10-05.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+Using `.test()` with RegExp
+The difference between null, undefined and NaN
+Getting the *real* dimensions of an image
+Inaccuracy of binary floating-point format
