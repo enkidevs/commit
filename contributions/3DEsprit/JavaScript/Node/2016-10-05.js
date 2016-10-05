f65fbf194151b3bf93a2a316f9018a73fc70e283
3DEsprit <vilesyn@gmail.com>
2016-10-05 21:11:53 +0100

    2016-10-05's JavaScript workout

---
+++ contributions/3DEsprit/JavaScript/Node/2016-10-05.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Wrapping errors in Node.js using _node-verror_
+`process.nextTick()` callback pattern
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
