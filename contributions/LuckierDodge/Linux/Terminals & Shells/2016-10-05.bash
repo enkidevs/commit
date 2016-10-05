94d8eb92cddb4858713a2dc310a4cb0b4abb3ab5
LuckierDodge <ryandlewis.rl@gmail.com>
2016-10-05 18:24:47 +0100

    2016-10-05's Linux workout

---
+++ contributions/LuckierDodge/Linux/Terminals & Shells/2016-10-05.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
