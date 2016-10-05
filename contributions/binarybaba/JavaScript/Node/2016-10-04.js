3a0baf03f21d4edff8ea24f7ca09a57e5d9dab2d
binarybaba <amin.ajani17@gmail.com>
2016-10-05 19:05:52 +0100

    2016-10-04's JavaScript workout

---
+++ contributions/binarybaba/JavaScript/Node/2016-10-04.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Asynchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
+Manage child processes in **Node**
