d982242173518284cd6dc099f8aeaefcb17b1ab5
cclarke30 <chris@gradtouch.com>
2016-10-06 00:45:56 +0100

    2016-10-05's Git workout

---
+++ contributions/cclarke30/Git/Essentials/2016-10-05.bash
@@ -0,0 +1,5 @@
+Adding a remote repository
+How to set your username and email
+Check the status of your files
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
