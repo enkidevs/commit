9abb0a9e56692b899447ec165cc3d3312a81adf2
Vebble <serafin21@gmail.com>
2016-10-05 18:34:12 +0100

    2016-10-05's Java workout

---
+++ contributions/Vebble/Java/I/O/2016-10-05.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to list all files in a directory that match a filename extension in Java
+Reading and writing text files
+Retrieving file store attributes
+Deleting files and directories
