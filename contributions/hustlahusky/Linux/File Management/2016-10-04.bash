5b86b2019f53e08677f9dd2269f7e5cecbd78f82
hustlahusky <genteelknight@gmail.com>
2016-10-05 08:43:28 +0100

    2016-10-04's Linux workout

---
+++ contributions/hustlahusky/Linux/File Management/2016-10-04.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
