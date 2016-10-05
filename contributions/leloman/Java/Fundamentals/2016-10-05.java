ccc3219de95ed2930487367f9987cf31769afe69
leloman <antonio.marcoalvarez2009@gmail.com>
2016-10-05 12:19:48 +0100

    2016-10-05's Java workout

---
+++ contributions/leloman/Java/Fundamentals/2016-10-05.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Inheriting a constructor from a superclass
+Using `synchronized` statements
+Supply `toString()` in all classes
