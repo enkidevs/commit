4948943a90f54de6e7641ef9409bf1e1bad4180e
r2mi63 <remi.charbo@gmail.com>
2016-10-05 22:41:06 +0100

    2016-10-05's Linux workout

---
+++ contributions/r2mi63/Linux/Package Management/2016-10-05.bash
@@ -0,0 +1,5 @@
+Install specific version of packages
+Prevent updating a specific package in Debian systems
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
