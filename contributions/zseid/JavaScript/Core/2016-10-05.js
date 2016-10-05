c5340aff2939677ff5d2f60a380aa61c0b649094
zseid <seerofthefuture@gmail.com>
2016-10-05 18:31:17 +0100

    2016-10-05's JavaScript workout

---
+++ contributions/zseid/JavaScript/Core/2016-10-05.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+Hoisting applies only to variable declarations, not initializations
+Avoid using `with`
+How to merge two arrays
+Round numbers to `N` decimals 
