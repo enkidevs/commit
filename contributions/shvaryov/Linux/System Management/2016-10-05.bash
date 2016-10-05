1cf44eca8bfea0f94c2a5dc722ab3ad3a8de5ea5
shvaryov <shvaryov@gmail.com>
2016-10-05 17:26:09 +0100

    2016-10-05's Linux workout

---
+++ contributions/shvaryov/Linux/System Management/2016-10-05.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Detect your linux distribution
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Finding Open Files With `lsof`
