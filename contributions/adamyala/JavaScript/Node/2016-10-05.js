82701fc815b8d32d75cf643ed3371ab262721610
adamyala <adamkyala@gmail.com>
2016-10-05 18:15:28 +0100

    2016-10-05's JavaScript workout

---
+++ contributions/adamyala/JavaScript/Node/2016-10-05.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+Wrapping errors in Node.js using _node-verror_
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
+Middleware/pipeline design pattern
