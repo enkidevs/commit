8e4c3f8bb7d52629bb33e5a58dd9e6be696a87e6
seta17 <satkiewicz1+github@gmail.com>
2016-10-05 08:53:50 +0100

    2016-10-05's Linux workout

---
+++ contributions/seta17/Linux/File Management/2016-10-05.bash
@@ -0,0 +1,6 @@
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
