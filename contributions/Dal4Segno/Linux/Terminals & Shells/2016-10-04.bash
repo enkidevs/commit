0e0ae0c5fda474c18b693d94baf46926115e07b5
Dal4Segno <dal4segno@gmail.com>
2016-10-05 14:06:44 +0100

    2016-10-04's Linux workout

---
+++ contributions/Dal4Segno/Linux/Terminals & Shells/2016-10-04.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear bash history
+Quick `bash` shortcuts 
+Terminal incognito mode
+Keep useful commands in your shell history with tags
