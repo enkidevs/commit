98a3bf9bd79e7b51efe5afe98a36de6cacca141f
gigioSouza <giovane.bds@gmail.com>
2016-10-05 12:55:59 +0100

    2016-10-05's Java workout

---
+++ contributions/gigioSouza/Java/Fundamentals/2016-10-05.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
