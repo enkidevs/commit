28c6955d1b486766ae7824633fb719cf1988c77f
Manee44 <manee-50-@hotmail.com>
2016-10-05 05:02:51 +0100

    2016-10-05's Java workout

---
+++ contributions/Manee44/Java/Fundamentals/2016-10-05.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+Detect or prevent integer overflow
+Supply `toString()` in all classes
