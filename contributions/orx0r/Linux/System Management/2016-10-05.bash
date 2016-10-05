f6078cfccb365dfacc9bf510c22930622655a029
orx0r <norbitronik@gmail.com>
2016-10-05 07:30:19 +0100

    2016-10-05's Linux workout

---
+++ contributions/orx0r/Linux/System Management/2016-10-05.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Get hardware stack details with `lspci`
