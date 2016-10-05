0e8865dfa0df9bd4134d97d2c124002d16fdd163
fruitJazzy <li4drow@gmail.com>
2016-10-05 14:06:51 +0100

    2016-10-05's Java workout

---
+++ contributions/fruitJazzy/Java/Fundamentals/2016-10-05.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
