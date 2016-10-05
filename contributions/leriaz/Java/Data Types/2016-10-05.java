6962dc1c65a91b2e7f78883c8110422fc5209e7e
leriaz <leriaz17@gmail.com>
2016-10-05 09:44:40 +0100

    2016-10-05's Java workout

---
+++ contributions/leriaz/Java/Data Types/2016-10-05.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Finding a substring in a string
+`StringBuffer` vs `StringBuilder`
+Numeric Conversion - Widening
