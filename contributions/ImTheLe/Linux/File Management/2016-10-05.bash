de2d9cafb3bb654e34f32426a8401695d920ad16
ImTheLe <leongrdic@gmail.com>
2016-10-05 20:08:50 +0100

    2016-10-05's Linux workout

---
+++ contributions/ImTheLe/Linux/File Management/2016-10-05.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
