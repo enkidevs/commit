a6d80a892d000c46dc315aa8619b6701e4df7cfa
TheMicroGirl <e.grabowiecka@outlook.com>
2016-10-05 09:17:57 +0100

    2016-10-05's Java workout

---
+++ contributions/TheMicroGirl/Java/Data Types/2016-10-05.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+`StringBuffer` vs `StringBuilder`
+String concatenation in Java
+Numeric Conversion - Widening
