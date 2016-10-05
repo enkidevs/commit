8581e3b090548da455b99af17f91c21779b4fbd1
cyio <icaner@qq.com>
2016-10-05 04:13:19 +0100

    2016-10-04's JavaScript workout

---
+++ contributions/cyio/JavaScript/Functions/2016-10-04.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Function call method
+Move cursor at the end of text input
+Check if a document is done loading
