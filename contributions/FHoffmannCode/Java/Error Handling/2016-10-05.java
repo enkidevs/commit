e4f4de44a8e3b03a1602afcfc4c45c2518451112
FHoffmannCode <f.higuain@gmail.com>
2016-10-05 07:18:07 +0100

    2016-10-05's Java workout

---
+++ contributions/FHoffmannCode/Java/Error Handling/2016-10-05.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
