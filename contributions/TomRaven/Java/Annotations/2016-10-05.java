8f8eb91079df08202b9c84b72f1451b08e4841d0
TomRaven <16lazart@elmbrookstudents.org>
2016-10-05 17:49:53 +0100

    2016-10-05's Java workout

---
+++ contributions/TomRaven/Java/Annotations/2016-10-05.java
@@ -0,0 +1,5 @@
+`@Test` parameters
+Use `@Ignore` to disable JUnit tests
+Using the `@Deprecated` annotation
+Restricting an annotation's application by using `@Target`
+Declaring `@Repeatable` annotation types
