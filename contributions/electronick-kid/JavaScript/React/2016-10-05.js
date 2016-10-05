27735559d83c29de8d3bdeda97199ffc289c1819
electronick-kid <electronick.kid@gmail.com>
2016-10-05 09:09:26 +0100

    2016-10-05's JavaScript workout

---
+++ contributions/electronick-kid/JavaScript/React/2016-10-05.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+Type of the Children props
+Props in getInitialState Is an        
+Anti-Pattern
+Use **React** with other libraries
+Validate for required props
