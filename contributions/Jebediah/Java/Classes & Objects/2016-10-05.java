c7591c0a2d3b0676512bfa7e14523be7099623c6
Jebediah <jefwillems100@gmail.com>
2016-10-05 08:25:19 +0100

    2016-10-05's Java workout

---
+++ contributions/Jebediah/Java/Classes & Objects/2016-10-05.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Compare classes and not class names
+How and when to use `WeakHashMap` class
