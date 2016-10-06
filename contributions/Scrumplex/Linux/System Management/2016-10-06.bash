bf7e3d412bbd85b4ae32b16f8891303663f358a6
Scrumplex <sefa6727@gmail.com>
2016-10-06 14:06:50 +0100

    2016-10-06's Linux workout

---
+++ contributions/Scrumplex/Linux/System Management/2016-10-06.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Using `dd` To Create Disk Images
