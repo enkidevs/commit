d85189eda0165af2407f6773760634b7e2cb4f53
FinnSell <contact@finn-thorben.me>
2016-10-06 07:11:02 +0100

    2016-10-06's Linux workout

---
+++ contributions/FinnSell/Linux/System Management/2016-10-06.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
