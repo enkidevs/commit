7cbfa5849d28f422b32b657008048e5c07c12cb1
yosan998 <yosan998@gmail.com>
2016-10-06 06:52:31 +0100

    2016-10-06's Java workout

---
+++ contributions/yosan998/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
+Using an interface as a parameter
