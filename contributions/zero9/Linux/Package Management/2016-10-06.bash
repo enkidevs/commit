28fb6d7ceac16755abd7644cab6da5d4717aa909
zero9 <c.lobrano@gmail.com>
2016-10-06 21:07:22 +0100

    2016-10-06's Linux workout

---
+++ contributions/zero9/Linux/Package Management/2016-10-06.bash
@@ -0,0 +1,5 @@
+List installed packages
+Prevent updating a specific package in Debian systems
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
