22310bab849131378a06e40780b6d7064de54cd2
ninijay <ninijay93@gmail.com>
2016-10-06 16:57:45 +0100

    2016-10-06's Linux workout

---
+++ contributions/ninijay/Linux/System Management/2016-10-06.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Finding Open Files With `lsof`
