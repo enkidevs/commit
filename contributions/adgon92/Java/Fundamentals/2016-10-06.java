47a55818a6d635f90065c0f2a43c233473aa6223
adgon92 <adgon92@gmail.com>
2016-10-06 06:56:57 +0100

    2016-10-06's Java workout

---
+++ contributions/adgon92/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
