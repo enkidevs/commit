9c2a5e67b23705a29b2a75e0f59b8ee2d2f41243
javijaviii <abregomail@gmail.com>
2016-10-06 11:41:48 +0100

    2016-10-06's Java workout

---
+++ contributions/javijaviii/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Always specify type parameter
+Do not return *references* to private *mutable* class members
