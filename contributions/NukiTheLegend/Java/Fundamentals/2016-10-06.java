9619e48522069c1f91c4a55802acff976b0414d0
NukiTheLegend <rudivanbiljon@gmail.com>
2016-10-06 05:00:27 +0100

    2016-10-06's Java workout

---
+++ contributions/NukiTheLegend/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
