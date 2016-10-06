5fac16bf4c9c2cf1af138cc105a839ac508b2af3
GuilhermeG <guiguie321@gmail.com>
2016-10-06 09:16:08 +0100

    2016-10-06's JavaScript workout

---
+++ contributions/GuilhermeG/JavaScript/Npm/2016-10-06.js
@@ -0,0 +1,5 @@
+Create projects with `npm init`
+Niceties for your npm package
+npm can handle multiple versions of a dependency
+Current Lifecycle Event
+Lock down dependency versions by shrinkwrapping
