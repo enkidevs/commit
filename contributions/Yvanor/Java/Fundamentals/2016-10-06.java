e6665cdb6dc835c7456bc2f2922003162834dbb6
Yvanor <maxime.yusei@orange.fr>
2016-10-06 06:21:12 +0100

    2016-10-06's Java workout

---
+++ contributions/Yvanor/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Uses of the `final` keyword
+Using bounded type parameters in generic methods
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
