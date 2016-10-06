47c082714ea459908a44e7a49359f02ec4a090e5
Rahul91 <rahulramesh118@gmail.com>
2016-10-06 10:04:22 +0100

    2016-10-05's Java workout

---
+++ contributions/Rahul91/Java/Fundamentals/2016-10-05.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
