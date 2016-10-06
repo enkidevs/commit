39ee26b28febe4fd0234a548278ade500ad40f28
liufeng <liufeng5460@163.com>
2016-10-06 16:28:51 +0100

    2016-10-05's Java workout

---
+++ contributions/liufeng/Java/Fundamentals/2016-10-05.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Limit Accessibility of `Fields`
