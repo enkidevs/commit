0796d44903146e6c7b8a36fd729f92486655d32e
moczul <moczul@gmail.com>
2016-10-06 06:59:23 +0100

    2016-10-06's Java workout

---
+++ contributions/moczul/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
