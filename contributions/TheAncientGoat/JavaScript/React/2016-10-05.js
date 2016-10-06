cc376c207572a347d16cd9040e9da59041d89c65
TheAncientGoat <serjndestroy@gmail.com>
2016-10-06 02:58:55 +0100

    2016-10-05's JavaScript workout

---
+++ contributions/TheAncientGoat/JavaScript/React/2016-10-05.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Functional Stateless Components in React
+Props in getInitialState Is an        
+Anti-Pattern
+Server-side rendering 
+Keyed fragments in **React**
