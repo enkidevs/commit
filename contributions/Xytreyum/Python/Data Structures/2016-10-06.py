77266e225373eb306eee755d0ae8ee7b64f90f4b
Xytreyum <mathias711@gmail.com>
2016-10-06 21:55:37 +0100

    2016-10-06's Python workout

---
+++ contributions/Xytreyum/Python/Data Structures/2016-10-06.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+There is more to copying
+Keep things in order with `OrderedDict`
+A thread-safe `Queue`
+Special `queue` methods
