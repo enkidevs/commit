281249a2eea6d9db7e05e231b2d9752570ccfc4f
ramandeep89 <ramandeep89@gmail.com>
2016-10-06 04:10:08 +0100

    2016-10-05's Java workout

---
+++ contributions/ramandeep89/Java/Fundamentals/2016-10-05.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
