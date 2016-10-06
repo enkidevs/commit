ccf186e2efd99dbd5257da9fb679441da9022e3e
JessieLiao <jessie.liao.0622@gmail.com>
2016-10-06 07:54:14 +0100

    2016-10-05's Java workout

---
+++ contributions/JessieLiao/Java/Fundamentals/2016-10-05.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
