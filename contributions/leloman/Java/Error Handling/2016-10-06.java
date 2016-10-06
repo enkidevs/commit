6044e05ea0a5d3109caf04a1ec46768b7538b00b
leloman <antonio.marcoalvarez2009@gmail.com>
2016-10-06 10:57:03 +0100

    2016-10-06's Java workout

---
+++ contributions/leloman/Java/Error Handling/2016-10-06.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Converting stack trace to a string
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Throwing an `exception`
