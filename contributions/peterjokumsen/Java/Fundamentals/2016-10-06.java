29c51b0d0293dc4ab17953556a8fe9249c325aee
peterjokumsen <pjokumsen@gmail.com>
2016-10-06 21:11:45 +0100

    2016-10-06's Java workout

---
+++ contributions/peterjokumsen/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between `==` and `equals()`
+The trade-off between short and descriptive names
+Using `Printstream.format` to format a string before printing
+Limit Accessibility of `Fields`
