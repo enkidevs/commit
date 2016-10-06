b0b226ca6f81fd49806ea430c10260895fb8674a
antoined73 <dezartoine@hotmail.fr>
2016-10-06 06:44:20 +0100

    2016-10-06's Java workout

---
+++ contributions/antoined73/Java/Error Handling/2016-10-06.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Throwing an `exception`
+Checking a double for `NaN` value
