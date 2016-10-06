e272124f79fc0818aa81a6185aa92fc67235854f
fthebaud <fthebaud@gmail.com>
2016-10-06 07:23:41 +0100

    2016-10-06's Git workout

---
+++ contributions/fthebaud/Git/Essentials/2016-10-06.bash
@@ -0,0 +1,5 @@
+Using tags for version control
+What is stored in a commit?
+Use `short` status to make output more compact
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
