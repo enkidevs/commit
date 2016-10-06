5d789ad8016b764800f31f0b4cb7d8ec9b8b71db
skuroda <sk@mieco.org>
2016-10-06 01:35:11 +0100

    2016-10-05's Linux workout

---
+++ contributions/skuroda/Linux/System Management/2016-10-05.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Change your `hostname` on systems using `systemd`
