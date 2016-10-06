cbe0d856138bceac94c6e4d73034f3aaff3c5972
ahaleen <adam.haleen@gmail.com>
2016-10-06 03:08:31 +0100

    2016-10-05's Linux workout

---
+++ contributions/ahaleen/Linux/Terminals & Shells/2016-10-05.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Retrieve previous arguments
