6ba6b8f97151f056a781cc2993d0acd85e9963ff
itachi1706 <kennethsohyq@hotmail.com>
2016-10-06 02:22:12 +0100

    2016-10-05's Linux workout

---
+++ contributions/itachi1706/Linux/System Management/2016-10-05.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
