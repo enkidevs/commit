2c90a347c694476c8e78335eb8efd915b09a2db0
peterleung89 <peterleung89@gmail.com>
2016-10-06 14:58:35 +0100

    2016-10-06's Java workout

---
+++ contributions/peterleung89/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
