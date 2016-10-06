abc3d1f29d97b92e0468f1887ae9d423cded66b7
IceTimux <icetimux@gmail.com>
2016-10-06 13:47:43 +0100

    2016-10-06's JavaScript workout

---
+++ contributions/IceTimux/JavaScript/Core/2016-10-06.js
@@ -0,0 +1,5 @@
+Shuffle an array using `sort` and `random`.
+Performing a function at timed intervals
+The difference between null, undefined and NaN
+How to merge two arrays
+`console.*` in JavaScript
