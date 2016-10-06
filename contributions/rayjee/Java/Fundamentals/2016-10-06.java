4f9decdd666a3e5f24fcff843f6ddcf1b5f55078
rayjee <formergod@gmail.com>
2016-10-06 14:52:33 +0100

    2016-10-06's Java workout

---
+++ contributions/rayjee/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+Using bounded type parameters in generic methods
+How range operations work
