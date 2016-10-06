76c1c3b8abce850ae5b2c335cdd14bb1322de348
Leerro <gabriel.f.bernard@gmail.com>
2016-10-06 15:21:52 +0100

    2016-10-06's Java workout

---
+++ contributions/Leerro/Java/Error Handling/2016-10-06.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
