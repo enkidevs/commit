a9b22e591b10fb2eddcb1a63e3788757a27e17d7
saka <saka71996@gmail.com>
2016-10-06 06:08:41 +0100

    2016-10-06's Python workout

---
+++ contributions/saka/Python/Data Structures/2016-10-06.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+When `range` comes in handy
+`bytearray` objects
+Operations with `bytes` and `bytearray`
+`queue`s and threads
