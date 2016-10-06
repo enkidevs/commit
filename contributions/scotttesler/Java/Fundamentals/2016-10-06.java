f6a165a4fa16de3209711d1f87e1543a503f7a0d
scotttesler <scottiet123@gmail.com>
2016-10-06 22:10:55 +0100

    2016-10-06's Java workout

---
+++ contributions/scotttesler/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Overloading Methods in Java
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Constant notation
