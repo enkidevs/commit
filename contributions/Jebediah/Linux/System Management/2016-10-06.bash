a17627606e56ad15d4f61c22bf504cf7a1483775
Jebediah <jefwillems100@gmail.com>
2016-10-06 08:08:02 +0100

    2016-10-06's Linux workout

---
+++ contributions/Jebediah/Linux/System Management/2016-10-06.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
