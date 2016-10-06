7fa412465ba353eedf084940ae6a1661467565de
Cccompany <bertrand@cournaud.fr>
2016-10-06 06:20:26 +0100

    2016-10-06's Java workout

---
+++ contributions/Cccompany/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
