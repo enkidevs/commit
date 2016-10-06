a2402b8b5d9934c00b82033a0f5fe4abbe3765eb
RafaAguilar <rafael.aguilar.1987@gmail.com>
2016-10-06 22:51:25 +0100

    2016-10-06's Python workout

---
+++ contributions/RafaAguilar/Python/Data Structures/2016-10-06.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Operations with `bytes` and `bytearray`
+How to count
+Keep things in order with `OrderedDict`
+`weakref` callbacks
