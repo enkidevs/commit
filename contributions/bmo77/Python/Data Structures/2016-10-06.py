a6673e8b2919ec45fbe69d55f9badf08a623ef2e
bmo77 <basha.others@gmail.com>
2016-10-06 11:57:48 +0100

    2016-10-06's Python workout

---
+++ contributions/bmo77/Python/Data Structures/2016-10-06.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Immutable sets with `frozenset`
+`set` operations
+There is more to copying
+Operations with `bytes` and `bytearray`
