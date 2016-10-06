de4b60f33a24dc54ac9f16bfb2d2a720de87bf7c
doomi <kurai7hikari@gmail.com>
2016-10-06 06:00:33 +0100

    2016-10-06's Linux workout

---
+++ contributions/doomi/Linux/System Management/2016-10-06.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
