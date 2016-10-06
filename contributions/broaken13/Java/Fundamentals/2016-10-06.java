17c93050e8d30715fbe20607aa7aa85e916061dc
broaken13 <broaken13@gmail.com>
2016-10-06 16:40:12 +0100

    2016-10-06's Java workout

---
+++ contributions/broaken13/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
