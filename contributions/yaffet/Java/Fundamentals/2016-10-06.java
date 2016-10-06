4fcd378e46efd0a357101fc4f171e01f1a376070
yaffet <uniac@web.de>
2016-10-06 06:47:37 +0100

    2016-10-06's Java workout

---
+++ contributions/yaffet/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Using an interface as a parameter
