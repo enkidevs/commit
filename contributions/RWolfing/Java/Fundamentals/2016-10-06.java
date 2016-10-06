76c7b9605e8b0c7b5fae462f13b29c62101bd343
RWolfing <robert.wolfinger@gmx.net>
2016-10-06 07:36:17 +0100

    2016-10-06's Java workout

---
+++ contributions/RWolfing/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
