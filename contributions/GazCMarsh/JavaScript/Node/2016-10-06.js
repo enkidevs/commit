038a19ddb7741573dbbcbb15e81b5e9b4f1b1f76
GazCMarsh <gazmarsh@meaningfulname.net>
2016-10-06 16:58:38 +0100

    2016-10-06's JavaScript workout

---
+++ contributions/GazCMarsh/JavaScript/Node/2016-10-06.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Asynchronous File Write/Read in Node.js
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+Automatic compilation for Node with **Nodemon** 
