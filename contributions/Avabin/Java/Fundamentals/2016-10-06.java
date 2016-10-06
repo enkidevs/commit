2371151428cedfc615395cef1a40dcf9d915d2b5
Avabin <andrzej.piotrowski.76@gmail.com>
2016-10-06 10:56:26 +0100

    2016-10-06's Java workout

---
+++ contributions/Avabin/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Connecting to FTP using Java
+Do not perform bitwise and arithmetic operations on the same data
+`Autoboxing` and `Unboxing`
+Supply `toString()` in all classes
