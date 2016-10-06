ba0fcb3c22d570d68cfd5fcfc4ab2e44114e90b2
swettw <swettw@gmail.com>
2016-10-06 17:38:06 +0100

    2016-10-06's Linux workout

---
+++ contributions/swettw/Linux/Networking/2016-10-06.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Logging dropped firewall packets
+How to clear `iptables` rules
+Setting up password-free authentication
+Staying connected remotely via SSH without an interactive shell
