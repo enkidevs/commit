822dc2446adcff825b0d79e117fcd0859ebef0c3
rengare <gorecki.gracjan@gmail.com>
2016-10-06 14:40:19 +0100

    2016-10-06's Java workout

---
+++ contributions/rengare/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: setting a file's owner
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
