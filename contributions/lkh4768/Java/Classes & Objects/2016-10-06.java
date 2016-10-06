2a3fe8bf9ff67d4a337ef67901aa013980e0ae71
lkh4768 <lkh5510@gmail.com>
2016-10-06 23:47:02 +0100

    2016-10-06's Java workout

---
+++ contributions/lkh4768/Java/Classes & Objects/2016-10-06.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Template methods in abstract classes
+Utility classes and `static` methods
+Compare classes and not class names
+Fields with Java **Reflection API**
