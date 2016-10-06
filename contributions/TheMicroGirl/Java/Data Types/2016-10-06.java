8a113d128de9b2de19fd986d79bfa0b6b05e8f31
TheMicroGirl <e.grabowiecka@outlook.com>
2016-10-06 22:38:52 +0100

    2016-10-06's Java workout

---
+++ contributions/TheMicroGirl/Java/Data Types/2016-10-06.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+String concatenation in Java
+Numeric Conversion - Widening
