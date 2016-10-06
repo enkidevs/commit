6ff6e88e094574a0cfa266ba7416cbf923ec366b
bisio <andrea.bisognin@gmail.com>
2016-10-06 11:58:30 +0100

    2016-10-06's Linux workout

---
+++ contributions/bisio/Linux/Networking/2016-10-06.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Multiple IP Address with aliasing 
+Useful ssh client optimizations 
+Setting up password-free authentication
+Use `tcpdump` to listen to network interface traffic
