beff4ec433f8b39b640cd13153e2cccd65ee51ee
Hellonearthis <hellonearthisman@gmail.com>
2016-10-06 23:04:01 +0100

    2016-10-06's JavaScript workout

---
+++ contributions/Hellonearthis/JavaScript/Npm/2016-10-06.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Combining script commands in npm
+Manage local node modules with `npm link`
+`npm` verifies `sha1` hashes of packages
+Reduce package duplication 
