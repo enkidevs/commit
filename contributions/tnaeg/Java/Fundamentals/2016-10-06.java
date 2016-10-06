c54acc81d9a5d957a3114e37b316899aaf3522e3
tnaeg <c10mjn@cs.umu.se>
2016-10-06 08:22:08 +0100

    2016-10-06's Java workout

---
+++ contributions/tnaeg/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Measuring time
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
