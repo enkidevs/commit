bd3b887f63f75edf7e18de8a233f6f571287ad79
butu <ichbingenial@gmail.com>
2016-10-06 08:58:14 +0100

    2016-10-06's JavaScript workout

---
+++ contributions/butu/JavaScript/Core/2016-10-06.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Setting the length of an array 
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+Getting the *real* dimensions of an image
