dd4556305446b9cb0de27b6dab055d5637628341
Toriath <developer@tobiaskloss.de>
2016-10-06 16:30:39 +0100

    2016-10-06's JavaScript workout

---
+++ contributions/Toriath/JavaScript/Core/2016-10-06.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Using assignment operators
+Hoisting applies only to variable declarations, not initializations
+Prevent background image flicker in **IE**
+Detect **DO NOT TRACK** status
