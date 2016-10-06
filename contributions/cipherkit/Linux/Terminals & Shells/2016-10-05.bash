7332a6ee68168f0fbeb7f4b2a8d106660ba574a6
cipherkit <matthew.squire@mcnhealthcare.com>
2016-10-06 02:10:25 +0100

    2016-10-05's Linux workout

---
+++ contributions/cipherkit/Linux/Terminals & Shells/2016-10-05.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
