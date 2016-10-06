f1e8f58f1c33f88dc24ca40385f1c0b7455af551
sattel <aturubar@gmail.com>
2016-10-06 12:34:43 +0100

    2016-10-06's Linux workout

---
+++ contributions/sattel/Linux/System Management/2016-10-06.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
