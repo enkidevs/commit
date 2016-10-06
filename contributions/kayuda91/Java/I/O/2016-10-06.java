d6cc37c3c1d34e1572d663abb4bb7674c4c2d5bd
kayuda91 <kayuda91@gmail.com>
2016-10-06 12:33:09 +0100

    2016-10-06's Java workout

---
+++ contributions/kayuda91/Java/I/O/2016-10-06.java
@@ -0,0 +1,5 @@
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
