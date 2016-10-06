2b37312931c5ec2686d0a26aea27c72a6dbdb4c1
svartr <the.trash.collector@yandex.com>
2016-10-06 10:05:29 +0100

    2016-10-05's JavaScript workout

---
+++ contributions/svartr/JavaScript/React/2016-10-05.js
@@ -0,0 +1,6 @@
+componentWillReceiveProps Not Triggered After Mounting
+
+`PureRenderMixin` in **React**
+Immutability helpers in **React**
+Custom validations for props
+Custom `propType`'s to be required
