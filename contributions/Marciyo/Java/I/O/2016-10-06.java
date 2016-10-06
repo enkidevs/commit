5383772d86152ae8ea7607c4a670fe13fda3d228
Marciyo <marcelmierzejewski@gmail.com>
2016-10-06 17:05:25 +0100

    2016-10-06's Java workout

---
+++ contributions/Marciyo/Java/I/O/2016-10-06.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to create directories in Java
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
+Deleting files and directories
