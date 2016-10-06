b22d77323eeaceb3adf2bd90dab4406f18914bbc
Marius <marius.mez@gmail.com>
2016-10-06 13:41:00 +0100

    2016-10-06's Java workout

---
+++ contributions/Marius/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
+Supply `toString()` in all classes
