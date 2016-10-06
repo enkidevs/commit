63a7a03b7dafd72f394143e18697d9819e987c1d
Kailas <me@kailas.xyz>
2016-10-06 15:26:01 +0100

    2016-10-06's Java workout

---
+++ contributions/Kailas/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
+Supply `toString()` in all classes
