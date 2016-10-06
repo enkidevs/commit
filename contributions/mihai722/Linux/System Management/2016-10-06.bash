4c8064e018740257686eb06c014093a3a7a8ed47
mihai722 <mihai7221@gmail.com>
2016-10-06 05:51:44 +0100

    2016-10-06's Linux workout

---
+++ contributions/mihai722/Linux/System Management/2016-10-06.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Incorrect time on dual boot systems
