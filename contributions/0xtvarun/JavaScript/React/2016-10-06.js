346eb8ae1150bc1050cfef41280cf9ef0025251c
0xtvarun <vtewari26@gmail.com>
2016-10-07 14:08:36 +0100

    2016-10-06's JavaScript workout

---
+++ contributions/0xtvarun/JavaScript/React/2016-10-06.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+Props in getInitialState Is an        
+Anti-Pattern
+`ref` callback attribute
+Custom `propType`'s to be required
+Default values for props
