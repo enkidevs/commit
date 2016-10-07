a65196bd1b950e483242674c12f7b0a660cde346
erekhron <adamrossmusic@gmail.com>
2016-10-07 22:22:09 +0100

    2016-10-07's JavaScript workout

---
+++ contributions/erekhron/JavaScript/React/2016-10-07.js
@@ -0,0 +1,6 @@
+Pixel values in style props for **React**
+Props in getInitialState Is an        
+Anti-Pattern
+Dangerously set `innerHTML`
+Shallow compare in **React**
+Shortcut for transferring props
