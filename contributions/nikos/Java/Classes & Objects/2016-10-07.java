15ac3f0c7d3ffb22ea4edb6aef3ef95d3c1328d1
nikos <triantafyllou.ni@gmail.com>
2016-10-07 18:18:01 +0100

    2016-10-07's Java workout

---
+++ contributions/nikos/Java/Classes & Objects/2016-10-07.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Utility classes and `static` methods
+Using inner classes
+Double Brace Initialization
+String formatting and parsing with `SimpleDateFormat`
