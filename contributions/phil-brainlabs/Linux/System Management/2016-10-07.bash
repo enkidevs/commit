87b2884cd8cc727fb2768c3810c666ec4c2f1c3f
phil-brainlabs <phil@brainlabsdigital.com>
2016-10-07 13:18:12 +0100

    2016-10-07's Linux workout

---
+++ contributions/phil-brainlabs/Linux/System Management/2016-10-07.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Basic service management with `systemd`
