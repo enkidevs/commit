16717753c52ad8aa46d141017e6d82104932d5bb
shahrukhamd <shahrukh.amd@gmail.com>
2016-10-07 19:26:40 +0100

    2016-10-06's Java workout

---
+++ contributions/shahrukhamd/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
