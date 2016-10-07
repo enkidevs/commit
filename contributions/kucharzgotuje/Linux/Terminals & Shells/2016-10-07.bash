f58b93abcf1ea008ea55b7536dfe8614bde90ccc
kucharzgotuje <kucharz.gotuje@gmail.com>
2016-10-07 12:23:37 +0100

    2016-10-07's Linux workout

---
+++ contributions/kucharzgotuje/Linux/Terminals & Shells/2016-10-07.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
