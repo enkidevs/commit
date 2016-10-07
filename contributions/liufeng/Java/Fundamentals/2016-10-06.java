a62988ba33961180d5034827805f1ae5445ee02d
liufeng <liufeng5460@163.com>
2016-10-07 11:07:58 +0100

    2016-10-06's Java workout

---
+++ contributions/liufeng/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Calling one constructor from another
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
