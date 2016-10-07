8b30d74aed25c82b7b9947c6e3bafb22612f32db
RianFuro <florian.proksch@outlook.com>
2016-10-07 09:49:12 +0100

    2016-10-07's JavaScript workout

---
+++ contributions/RianFuro/JavaScript/Functions/2016-10-07.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Method chaining
+Changing a functions context with `fn.call(object)`
