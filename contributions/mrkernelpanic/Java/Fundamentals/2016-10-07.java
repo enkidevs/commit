3d0f563c0bdc7cfbfbc4b63fad1b19db2c968655
mrkernelpanic <enki@boehme.me>
2016-10-07 23:35:58 +0100

    2016-10-07's Java workout

---
+++ contributions/mrkernelpanic/Java/Fundamentals/2016-10-07.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
