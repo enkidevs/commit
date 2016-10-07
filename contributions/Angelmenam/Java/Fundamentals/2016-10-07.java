1e4cae69bc4f7b95aa1fa3a6b0c4c0d5163b683e
Angelmenam <angelmenam@hotmail.com>
2016-10-07 20:22:07 +0100

    2016-10-07's Java workout

---
+++ contributions/Angelmenam/Java/Fundamentals/2016-10-07.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
