938df314cfccbce90e00be73757fecd03dcbf798
rodbarbz <rodrigo.mesquita.barboza@gmail.com>
2016-10-07 23:45:20 +0100

    2016-10-07's Linux workout

---
+++ contributions/rodbarbz/Linux/Terminals & Shells/2016-10-07.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
