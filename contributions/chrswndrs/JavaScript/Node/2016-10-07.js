05ced149cc45f91b9b9dfd3b4b77c9ea1cb345d8
chrswndrs <christoph.wanders@gmail.com>
2016-10-07 13:22:19 +0100

    2016-10-07's JavaScript workout

---
+++ contributions/chrswndrs/JavaScript/Node/2016-10-07.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+First-error callbacks in **Node**
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
