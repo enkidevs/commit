a7aa396ef862396ecdcec20253dc1b0fb06c7935
aldum <spongyabob@gmail.com>
2016-10-07 09:48:50 +0100

    2016-10-07's Linux workout

---
+++ contributions/aldum/Linux/Networking/2016-10-07.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Sending emails from terminal / scripts
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
