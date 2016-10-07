4487ab498ab0a951e8e502a8d6ad1f3e5fa90460
es-moll <es-moll@inbox.ru>
2016-10-07 06:28:10 +0100

    2016-10-07's JavaScript workout

---
+++ contributions/es-moll/JavaScript/Core/2016-10-07.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+No block scope
+Prevent background image flicker in **IE**
+Getting the *real* dimensions of an image
+Inaccuracy of binary floating-point format
