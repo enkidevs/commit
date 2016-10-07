be57ca18808204e68cd008b82f004483f2429af2
dmitry-korolev <dmitry@korolev.dk>
2016-10-07 07:47:48 +0100

    2016-10-07's JavaScript workout

---
+++ contributions/dmitry-korolev/JavaScript/Npm/2016-10-07.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Manipulate npm packages cache
+npm can handle multiple versions of a dependency
+Environmental variables and flags for `npm`
+`npm` verifies `sha1` hashes of packages
