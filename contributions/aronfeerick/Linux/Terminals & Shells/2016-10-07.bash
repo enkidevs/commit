b36289901ca13534d512753b5de10dde7828adde
aronfeerick <aronfeerick@gmail.com>
2016-10-07 20:19:51 +0100

    2016-10-07's Linux workout

---
+++ contributions/aronfeerick/Linux/Terminals & Shells/2016-10-07.bash
@@ -0,0 +1,5 @@
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
