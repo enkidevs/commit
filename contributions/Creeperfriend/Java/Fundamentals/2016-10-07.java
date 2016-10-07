42a9bb1e8cc5e12ad370f511482060c312e724ef
Creeperfriend <clemenstiedt@gmx.de>
2016-10-07 21:26:24 +0100

    2016-10-07's Java workout

---
+++ contributions/Creeperfriend/Java/Fundamentals/2016-10-07.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Distinction between `public` and `private` methods
+Calling one constructor from another
+Locks in `static synchronized` methods
+Constant notation
