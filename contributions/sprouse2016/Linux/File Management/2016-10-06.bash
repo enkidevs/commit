5595bec7963be89fe9bc38c1a61d9f42cf6c7bc0
sprouse2016 <sprouse2016@gmail.com>
2016-10-07 01:49:18 +0100

    2016-10-06's Linux workout

---
+++ contributions/sprouse2016/Linux/File Management/2016-10-06.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
