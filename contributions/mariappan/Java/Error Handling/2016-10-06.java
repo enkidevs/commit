1df617ab6aae58f82b9a9aa402ea8b8ad926a49d
mariappan <mariappanourindia@gmail.com>
2016-10-07 16:50:24 +0100

    2016-10-06's Java workout

---
+++ contributions/mariappan/Java/Error Handling/2016-10-06.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Checking a double for `NaN` value
