d449ddff53eae0445b362c2316ab0905e7ebc2c1
symplekaos <symplekaos@gmail.com>
2016-10-07 17:11:32 +0100

    2016-10-07's JavaScript workout

---
+++ contributions/symplekaos/JavaScript/Node/2016-10-07.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+First-error callbacks in **Node**
+`uncaughtException` listener in Node.js
+`try-catch` only for **sync** code
