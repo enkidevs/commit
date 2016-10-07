baa99d4bcfe46d44a8323d50d26c9e3d49fd047c
mhfayssal <mhfayssal@gmail.com>
2016-10-07 07:55:30 +0100

    2016-10-07's Linux workout

---
+++ contributions/mhfayssal/Linux/System Management/2016-10-07.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
