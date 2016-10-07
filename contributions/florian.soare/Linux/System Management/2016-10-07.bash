6e60c7efe0543ef4d7114192ff1d950f9050e95b
florian.soare <florian.soare@takmate.com>
2016-10-07 09:41:18 +0100

    2016-10-07's Linux workout

---
+++ contributions/florian.soare/Linux/System Management/2016-10-07.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Commands to shutdown or restart the system
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
