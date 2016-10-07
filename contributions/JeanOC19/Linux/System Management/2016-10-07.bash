02f61a6ddeac654cfee1f26fd184cc93fca8806e
JeanOC19 <jean.candelaria@upr.edu>
2016-10-07 21:03:29 +0100

    2016-10-07's Linux workout

---
+++ contributions/JeanOC19/Linux/System Management/2016-10-07.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
