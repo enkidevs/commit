8b3433f6e97831d393986a0cd4704ed991503356
tommot <thomas.prebeck@web.de>
2016-10-07 10:14:43 +0100

    2016-10-07's Java workout

---
+++ contributions/tommot/Java/I/O/2016-10-07.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Reading file attributes
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
