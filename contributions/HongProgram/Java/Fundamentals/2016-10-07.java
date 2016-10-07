60fc2b27f7a83e38010cad8d0932ba0acde1755f
HongProgram <caihong098@gmail.com>
2016-10-07 18:06:12 +0100

    2016-10-07's Java workout

---
+++ contributions/HongProgram/Java/Fundamentals/2016-10-07.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Metadata: setting a file's owner
+Using `synchronized` statements
