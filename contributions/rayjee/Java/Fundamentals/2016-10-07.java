78e84b5881198dccb9f7d4386bd25d0f1c0e1223
rayjee <formergod@gmail.com>
2016-10-07 15:00:38 +0100

    2016-10-07's Java workout

---
+++ contributions/rayjee/Java/Fundamentals/2016-10-07.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Common mistake on switch statements
+Using Inheritance to reduce code repetition
+Calling one constructor from another
+Using an interface as a parameter
