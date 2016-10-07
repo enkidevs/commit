fabefcdb22cb9d63087d0447033c08bc69530bd1
Asiorek <roczniak.joanna@gmail.com>
2016-10-07 18:58:21 +0100

    2016-10-07's Java workout

---
+++ contributions/Asiorek/Java/Fundamentals/2016-10-07.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
