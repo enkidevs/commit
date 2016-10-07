582ac10c462ce6b1f516dbd84b57992c51bc155b
jneilmbs <jneilmbs@gmail.com>
2016-10-08 00:09:52 +0100

    2016-10-07's Linux workout

---
+++ contributions/jneilmbs/Linux/Networking/2016-10-07.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+List your IPv6 configuration
+Sending emails from terminal / scripts
+Setting up password-free authentication
+Getting the connection speed from the terminal
