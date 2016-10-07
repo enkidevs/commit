49008df5069db452dcfdf643e64f5dbbf7aac07b
FinnSell <contact@finn-thorben.me>
2016-10-07 08:44:38 +0100

    2016-10-07's Java workout

---
+++ contributions/FinnSell/Java/Java 8/2016-10-07.java
@@ -0,0 +1,5 @@
+Using `Arrays.parallelSort` 
+How to check for static dependencies of applications and libraries from the command line.
+Control OS `Process`es in `Java8`
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
