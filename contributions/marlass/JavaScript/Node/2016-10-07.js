1e692402e5c99f965d1783a481775e3df9625eb8
marlass <lasakmarcin90@gmail.com>
2016-10-07 04:35:53 +0100

    2016-10-07's JavaScript workout

---
+++ contributions/marlass/JavaScript/Node/2016-10-07.js
@@ -0,0 +1,5 @@
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+Handle `JSON.parse` error in Node.js
+Automatic compilation for Node with **Nodemon** 
