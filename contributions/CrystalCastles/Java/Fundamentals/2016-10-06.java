530fcb8c84ebdecd2335c10ebc2477f238bc60a7
CrystalCastles <danielpichardo716@gmail.com>
2016-10-07 01:49:21 +0100

    2016-10-06's Java workout

---
+++ contributions/CrystalCastles/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
