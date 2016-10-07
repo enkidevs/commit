d4085332dd560dbcf33411ff8ad2e2f399330b06
NiklasMerz <niklasmerz@gmx.net>
2016-10-07 11:16:34 +0100

    2016-10-07's Java workout

---
+++ contributions/NiklasMerz/Java/Data Types/2016-10-07.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Converting numbers to strings
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
