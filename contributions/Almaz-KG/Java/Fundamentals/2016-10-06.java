9914896263f517413c1d39571629696430eebbd1
Almaz-KG <muktaalmaz@yandex.ru>
2016-10-07 02:42:31 +0100

    2016-10-06's Java workout

---
+++ contributions/Almaz-KG/Java/Fundamentals/2016-10-06.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
