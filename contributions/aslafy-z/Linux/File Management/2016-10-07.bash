49800b8ade018ffef897ecc5ea953eccd3ac03d7
aslafy-z <aslafy_z@epitech.eu>
2016-10-07 19:15:36 +0100

    2016-10-07's Linux workout

---
+++ contributions/aslafy-z/Linux/File Management/2016-10-07.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
