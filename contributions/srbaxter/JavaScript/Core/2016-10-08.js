0ba3091903f260dbf63f1cbaa95ce9ebf2749e28
srbaxter <srosebaxter@gmail.com>
2016-10-08 15:37:57 +0100

    2016-10-08's JavaScript workout

---
+++ contributions/srbaxter/JavaScript/Core/2016-10-08.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Format currency in JavaScript
+Counting the months from zero
+Functions return `undefined` by default
+Extra function arguments are undefined by default
