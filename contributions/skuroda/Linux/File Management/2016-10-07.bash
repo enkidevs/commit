0d63bfdd42843c0671e693995180fb95c03f4884
skuroda <sk@mieco.org>
2016-10-08 02:56:57 +0100

    2016-10-07's Linux workout

---
+++ contributions/skuroda/Linux/File Management/2016-10-07.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Deleting non-empty directories
+Easy way of sharing files
