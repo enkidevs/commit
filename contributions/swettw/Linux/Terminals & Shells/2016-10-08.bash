563e9b46e40a1ecdffeae40d00c5b5aa859a6498
swettw <swettw@gmail.com>
2016-10-08 18:07:36 +0100

    2016-10-08's Linux workout

---
+++ contributions/swettw/Linux/Terminals & Shells/2016-10-08.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Execute a command without saving it in history
+Find any Unix / Linux command
+Terminal incognito mode
+Sequential execution using the `;` statement separator
