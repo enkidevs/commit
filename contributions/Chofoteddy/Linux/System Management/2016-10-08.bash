ec0177f5a8eefb22bdd974070c8e945d9c3f1f2c
Chofoteddy <chofoteddy@gmail.com>
2016-10-08 16:33:08 +0100

    2016-10-08's Linux workout

---
+++ contributions/Chofoteddy/Linux/System Management/2016-10-08.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
