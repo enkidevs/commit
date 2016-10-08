08ac86988c79c3748442a084f638e24e86ec513c
metresb <metresb@gmail.com>
2016-10-08 15:04:16 +0100

    2016-10-08's Java workout

---
+++ contributions/metresb/Java/Fundamentals/2016-10-08.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
