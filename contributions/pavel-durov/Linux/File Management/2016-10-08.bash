23c99881a5ebbd69642bcfc337274ef5813d243e
pavel-durov <pdurov0@gmail.com>
2016-10-08 08:46:28 +0100

    2016-10-08's Linux workout

---
+++ contributions/pavel-durov/Linux/File Management/2016-10-08.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
