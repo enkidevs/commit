d7f6c1fd52264f8d4cef3effb84f673e87b9b1bc
rajeevriitm <rajeevriitm@gmail.com>
2016-10-08 09:34:05 +0100

    2016-10-07's Linux workout

---
+++ contributions/rajeevriitm/Linux/System Management/2016-10-07.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
