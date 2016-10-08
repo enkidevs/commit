db9c3d9d2638a68397ece8aa81d9c43eefd59ec2
e7krgc <enam93@gmail.com>
2016-10-08 08:33:01 +0100

    2016-10-08's Linux workout

---
+++ contributions/e7krgc/Linux/System Management/2016-10-08.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Common log files
+Using `dd` To Create Disk Images
