1813c1d528b98148537b0d3a264c748d8e6afb42
pldiiw <pldiiw@gmail.com>
2016-10-08 15:14:13 +0100

    2016-10-08's Linux workout

---
+++ contributions/pldiiw/Linux/File Management/2016-10-08.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
