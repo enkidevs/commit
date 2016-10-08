090b843fe95d3e743909a685d16db8cfbf438eb4
g3mini <mcjanz@gmail.com>
2016-10-08 07:35:02 +0100

    2016-10-08's Java workout

---
+++ contributions/g3mini/Java/Fundamentals/2016-10-08.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Catch multiple exceptions in a single `catch` block
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+`Autoboxing` and `Unboxing`
