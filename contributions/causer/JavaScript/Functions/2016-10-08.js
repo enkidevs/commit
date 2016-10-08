03966a4ccd4d5184db2b64b18c052db4ba782e4c
causer <causer24@gmail.com>
2016-10-08 21:53:19 +0100

    2016-10-08's JavaScript workout

---
+++ contributions/causer/JavaScript/Functions/2016-10-08.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Anonymous functions
+Easily generate a random `HEX` color
+Function call method
+Changing a functions context with `fn.call(object)`
