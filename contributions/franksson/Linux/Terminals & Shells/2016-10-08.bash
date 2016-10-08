ae507e581ff71ea48d1dd737acb101475d98567d
franksson <thefranksson@gmail.com>
2016-10-08 13:23:56 +0100

    2016-10-08's Linux workout

---
+++ contributions/franksson/Linux/Terminals & Shells/2016-10-08.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Terminal based browser
+Adding directories to your `$PATH`
