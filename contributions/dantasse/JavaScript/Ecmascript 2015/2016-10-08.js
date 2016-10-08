1bcea35f371c43411878842787860105a36dac7d
dantasse <dan.tasse@gmail.com>
2016-10-08 17:23:27 +0100

    2016-10-08's JavaScript workout

---
+++ contributions/dantasse/JavaScript/Ecmascript 2015/2016-10-08.js
@@ -0,0 +1,5 @@
+Computed property names in object literals
+Default function parameters
+Defaults values apply only to `undefined` (and not to `null`)
+The `spread` operator
+Binary and octal notation in ES6
