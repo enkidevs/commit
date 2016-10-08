c8a381025508b789ac72ba358ff7a449e453a4f1
bezdak <fcarcelonista@gmail.com>
2016-10-08 10:20:53 +0100

    2016-10-08's Java workout

---
+++ contributions/bezdak/Java/Fundamentals/2016-10-08.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Do not perform bitwise and arithmetic operations on the same data
