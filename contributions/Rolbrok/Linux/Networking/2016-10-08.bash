3c230d854fb8945b4d2581f0f936d2d5ddcba3b0
Rolbrok <baptiste.defrance.dev@gmail.com>
2016-10-08 16:33:59 +0100

    2016-10-08's Linux workout

---
+++ contributions/Rolbrok/Linux/Networking/2016-10-08.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Check `iptables` firewall status
+Useful ssh client optimizations 
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
