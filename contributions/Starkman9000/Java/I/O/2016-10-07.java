729efeaa8bbf03cfbb3b5184f48fa7f4af855865
Starkman9000 <starkman9000@gmail.com>
2016-10-08 09:42:32 +0100

    2016-10-07's Java workout

---
+++ contributions/Starkman9000/Java/I/O/2016-10-07.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to list all files in a directory that match a filename extension in Java
+Create system file paths using the `Path` class
+List contents of a directory with `newDirectoryStream`
+Deleting files and directories
