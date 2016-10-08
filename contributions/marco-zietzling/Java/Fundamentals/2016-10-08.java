9939623b4e0dc79ddade233068747fd6d693dac6
marco-zietzling <marco.zietzling@gmail.com>
2016-10-08 12:45:01 +0100

    2016-10-08's Java workout

---
+++ contributions/marco-zietzling/Java/Fundamentals/2016-10-08.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Using an interface as a parameter
