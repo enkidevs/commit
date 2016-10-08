7ec19ab968df03122e238cfc5061728b722ebfa8
maverigz <maverigzdev@gmail.com>
2016-10-08 09:30:48 +0100

    2016-10-08's Java workout

---
+++ contributions/maverigz/Java/Fundamentals/2016-10-08.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Measuring time
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
