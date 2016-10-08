7bb6a3b2a4b8b552589cfdf007bb55a9f1dc7a78
andrei-pavel <andrei.pavel@cti.pub.ro>
2016-10-08 08:07:33 +0100

    2016-10-08's Linux workout

---
+++ contributions/andrei-pavel/Linux/System Management/2016-10-08.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
