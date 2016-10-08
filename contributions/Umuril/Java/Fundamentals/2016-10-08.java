01f678b0ff166115a7e30955ff3cea236fd83717
Umuril <ed.rubi.1993@hotmail.com>
2016-10-08 07:37:21 +0100

    2016-10-08's Java workout

---
+++ contributions/Umuril/Java/Fundamentals/2016-10-08.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Measuring time
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
