b3374517284f516f7abc739324119bde942d759c
Aylard <aylard7@gmail.com>
2016-10-08 07:54:23 +0100

    2016-10-08's Linux workout

---
+++ contributions/Aylard/Linux/System Management/2016-10-08.bash
@@ -0,0 +1,6 @@
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Finding Open Files With `lsof`
