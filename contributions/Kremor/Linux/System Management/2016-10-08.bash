0494216b7afddd9a73eebc6820a2a4eaf2255abc
Kremor <kremor@mail.com>
2016-10-08 16:04:07 +0100

    2016-10-08's Linux workout

---
+++ contributions/Kremor/Linux/System Management/2016-10-08.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
