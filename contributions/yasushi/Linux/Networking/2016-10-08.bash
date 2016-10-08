502ce689a573b80c60ec99bf7d4a2ffb190ee89f
yasushi <yasushi.ohtsuka@gmail.com>
2016-10-09 00:46:05 +0100

    2016-10-08's Linux workout

---
+++ contributions/yasushi/Linux/Networking/2016-10-08.bash
@@ -0,0 +1,5 @@
+Find services running on your host
+Useful ssh client optimizations 
+Get real network statistics with `slurm`
+Limit the `wget` download rate
+Getting the connection speed from the terminal
