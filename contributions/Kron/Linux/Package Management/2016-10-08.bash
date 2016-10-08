392a699f0e78b99ff40e69cfc9bce5e2c3f939b5
Kron <miguelbuzato@bol.com.br>
2016-10-09 00:04:23 +0100

    2016-10-08's Linux workout

---
+++ contributions/Kron/Linux/Package Management/2016-10-08.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+Get `apt` to use a mirror / faster mirror
+Using `Tasksel` for software installation
+Solving `BADSIG GPG` errors
