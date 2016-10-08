bbb0558e2b89f2b31b3d7261a0fe00b5f8dea5bf
edm00se <edm00se@gmail.com>
2016-10-08 21:49:37 +0100

    2016-10-08's Java workout

---
+++ contributions/edm00se/Java/Fundamentals/2016-10-08.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
