b8825e603ab5da150f45d829340ddca362d2593d
d-noble <dnoble2@myseneca.ca>
2016-10-08 17:01:23 +0100

    2016-10-08's Java workout

---
+++ contributions/d-noble/Java/Fundamentals/2016-10-08.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
