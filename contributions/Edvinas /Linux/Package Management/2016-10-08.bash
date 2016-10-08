5205d33aba8ce23137c5f2441429489d0c26e36c
Edvinas <edvinas.reg@outlook.com>
2016-10-08 13:52:11 +0100

    2016-10-08's Linux workout

---
+++ contributions/Edvinas /Linux/Package Management/2016-10-08.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+List installed packages
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
