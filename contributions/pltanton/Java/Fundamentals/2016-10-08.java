b3ce48a655d76fc999271e4517206e2ea7ed0d6d
pltanton <plotnikovanton@gmail.com>
2016-10-08 13:27:45 +0100

    2016-10-08's Java workout

---
+++ contributions/pltanton/Java/Fundamentals/2016-10-08.java
@@ -0,0 +1,5 @@
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
+Using an interface as a parameter
