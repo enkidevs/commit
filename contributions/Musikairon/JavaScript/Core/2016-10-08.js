c6011f985da6293c526fe7543b545a8ae03dcfe2
Musikairon <arieel_inside@live.com>
2016-10-08 21:18:19 +0100

    2016-10-08's JavaScript workout

---
+++ contributions/Musikairon/JavaScript/Core/2016-10-08.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Most efficient way to build `HTML` strings
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+`console.*` in JavaScript
