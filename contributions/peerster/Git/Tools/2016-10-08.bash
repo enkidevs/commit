7b248b485163c1c4ab59bdd6a547327175972768
peerster <paer.berge@gmail.com>
2016-10-08 06:09:14 +0100

    2016-10-08's Git workout

---
+++ contributions/peerster/Git/Tools/2016-10-08.bash
@@ -0,0 +1,5 @@
+Let's play the blame game
+Cache your authentication details to save time
+Interactively unstage changes
+Specify a range of commits using double dot syntax
+Debug using binary search
