c4aca9757cd5123a37f98c83316de1c22240c67f
skaflock <skaflock@gmail.com>
2016-10-08 19:43:14 +0100

    2016-10-08's JavaScript workout

---
+++ contributions/skaflock/JavaScript/Npm/2016-10-08.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Calling remote scripts with npm
+npm projects dependencies
+Passing through command line arguments in npm scripts
+`npm` verifies `sha1` hashes of packages
