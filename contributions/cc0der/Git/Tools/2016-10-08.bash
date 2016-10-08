d20459ac4fa0ea8f1908e9193fe8e81ca185cc1e
cc0der <markus.hinterseer@gmail.com>
2016-10-08 08:16:48 +0100

    2016-10-08's Git workout

---
+++ contributions/cc0der/Git/Tools/2016-10-08.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Locate a commit by its hash
+Specify a range of commits using double dot syntax
+Interactively stage patches
+Sharing data by bundling
