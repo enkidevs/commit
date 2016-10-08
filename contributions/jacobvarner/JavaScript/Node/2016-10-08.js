ada7e8c6fd9f32dd023fc08176fa5c614e2246fa
jacobvarner <jacob.varner14@gmail.com>
2016-10-08 12:41:36 +0100

    2016-10-08's JavaScript workout

---
+++ contributions/jacobvarner/JavaScript/Node/2016-10-08.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+`process.nextTick()` callback pattern
+Clustering
+`exports` vs. `module.exports` in **Node**
