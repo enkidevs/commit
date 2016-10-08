ed1a817e3c7a8f321a6a83c6ea04529dccd82e22
tarciofps <tarciot7@gmail.com>
2016-10-09 00:17:56 +0100

    2016-10-08's Java workout

---
+++ contributions/tarciofps/Java/Fundamentals/2016-10-08.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
