290e74f7d55bfba9601a45ea74736bdf8699886c
codecrap <folexiy@rocketmail.com>
2016-10-08 13:42:01 +0100

    2016-10-08's Java workout

---
+++ contributions/codecrap/Java/I/O/2016-10-08.java
@@ -0,0 +1,5 @@
+Reading and writing text files
+Retrieving file store attributes
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
+Deleting files and directories
