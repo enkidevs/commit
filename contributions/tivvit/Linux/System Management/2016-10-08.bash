ea5c2cf29d675bcd72ddcd74b51aba45a0e21512
tivvit <tivvit@seznam.cz>
2016-10-08 09:35:31 +0100

    2016-10-08's Linux workout

---
+++ contributions/tivvit/Linux/System Management/2016-10-08.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
