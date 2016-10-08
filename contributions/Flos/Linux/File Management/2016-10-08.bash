de55a19753efb1ad4a6a7c03d79a93c069232ea1
Flos <fn187-github@yahoo.de>
2016-10-08 12:16:46 +0100

    2016-10-08's Linux workout

---
+++ contributions/Flos/Linux/File Management/2016-10-08.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Easy way of sharing files
