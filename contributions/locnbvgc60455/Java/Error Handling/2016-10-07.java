2390777bfc4d11950b340fbad71b54ac8888238b
locnbvgc60455 <locnbvgc60455@fpt.edu.vn>
2016-10-08 06:51:44 +0100

    2016-10-07's Java workout

---
+++ contributions/locnbvgc60455/Java/Error Handling/2016-10-07.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Careful Numeric Data Conversions
