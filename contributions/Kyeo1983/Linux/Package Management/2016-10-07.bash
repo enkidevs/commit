c044d783f09aba090bb4315b3f87fafa40e3bb4e
Kyeo1983 <kyeo_ses@yahoo.com>
2016-10-08 04:15:51 +0100

    2016-10-07's Linux workout

---
+++ contributions/Kyeo1983/Linux/Package Management/2016-10-07.bash
@@ -0,0 +1,6 @@
+Install specific version of packages
+Backporting on Debian
+
+Installing a `.deb` package from the terminal
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
