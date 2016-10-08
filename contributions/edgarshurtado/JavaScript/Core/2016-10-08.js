c490d008cf199d70eec7d519c29d279a5430ab97
edgarshurtado <edsanhu@gmail.com>
2016-10-08 11:28:15 +0100

    2016-10-08's JavaScript workout

---
+++ contributions/edgarshurtado/JavaScript/Core/2016-10-08.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Using the double tilde `~~` 
+Data type comparison in `switch` statements
+Counting the months from zero
+The difference between null, undefined and NaN
