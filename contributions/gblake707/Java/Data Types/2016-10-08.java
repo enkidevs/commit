ff0bac0af529af4db2ec7003cffa895b8f087026
gblake707 <gblake707@yahoo.com>
2016-10-08 21:33:25 +0100

    2016-10-08's Java workout

---
+++ contributions/gblake707/Java/Data Types/2016-10-08.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
+Do not attempt comparisons with NaN
