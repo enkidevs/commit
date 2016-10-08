da9b6318019bde303131af1718bca5dce9cac0b5
DarkP <petr456@seznam.cz>
2016-10-08 10:26:52 +0100

    2016-10-08's Java workout

---
+++ contributions/DarkP/Java/I/O/2016-10-08.java
@@ -0,0 +1,5 @@
+How to list all files in a directory that match a filename extension in Java
+Create system file paths using the `Path` class
+Checking for the existence of files and directories
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
