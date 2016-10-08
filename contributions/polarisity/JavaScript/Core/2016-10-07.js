8826bfe69241a46e44a5f780c54b79e42de2c734
polarisity <lionel.lim.l@gmail.com>
2016-10-08 02:39:28 +0100

    2016-10-07's JavaScript workout

---
+++ contributions/polarisity/JavaScript/Core/2016-10-07.js
@@ -0,0 +1,5 @@
+Keywords in hash keys
+Hoisting applies only to variable declarations, not initializations
+Avoid using `with`
+Extra function arguments are undefined by default
+Getting the *real* dimensions of an image
