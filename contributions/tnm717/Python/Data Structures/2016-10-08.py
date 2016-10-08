0732682fcfd0c7cec9aaa13774c6232c0f1e73c0
tnm717 <thomas.masterson@gmail.com>
2016-10-08 16:37:11 +0100

    2016-10-08's Python workout

---
+++ contributions/tnm717/Python/Data Structures/2016-10-08.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Built-in `list` methods
+`Dictionary` - standard mapping type
+`set` operations
+Operations with `bytes` and `bytearray`
