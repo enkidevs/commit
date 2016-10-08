f6af7df76c3b0032f640d66a5cbaa5e2bc65d640
shark09 <simon.dalvy@gmail.com>
2016-10-08 08:21:16 +0100

    2016-10-08's Linux workout

---
+++ contributions/shark09/Linux/System Management/2016-10-08.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Finding Open Files With `lsof`
