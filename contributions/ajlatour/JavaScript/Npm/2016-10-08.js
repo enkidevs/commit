5ec227abd3f505f19c328af07e8623c167a536ef
ajlatour <ajlatour@gmail.com>
2016-10-08 15:57:15 +0100

    2016-10-08's JavaScript workout

---
+++ contributions/ajlatour/JavaScript/Npm/2016-10-08.js
@@ -0,0 +1,5 @@
+Deprecate npm packages
+Bump package version in npm
+npm can handle multiple versions of a dependency
+Environmental variables and flags for `npm`
+`npm` verifies `sha1` hashes of packages
