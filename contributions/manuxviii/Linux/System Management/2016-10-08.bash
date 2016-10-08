ddf4b82effd590cf171778cab131bb04dfe3e8dd
manuxviii <emmanuel.mesnige@protonmail.com>
2016-10-08 22:08:28 +0100

    2016-10-08's Linux workout

---
+++ contributions/manuxviii/Linux/System Management/2016-10-08.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Basic service management with `systemd`
+Finding Open Files With `lsof`
