5f9b13f254ff400fe605fe0f837c286dd644acef
oldteacher <drsantos@gmail.com>
2016-10-08 02:18:06 +0100

    2016-10-07's Linux workout

---
+++ contributions/oldteacher/Linux/Networking/2016-10-07.bash
@@ -0,0 +1,5 @@
+SSH tunneling made easy
+Proxifying `ssh` connections
+Useful ssh client optimizations 
+Setting up password-free authentication
+Use `tcpdump` to listen to network interface traffic
