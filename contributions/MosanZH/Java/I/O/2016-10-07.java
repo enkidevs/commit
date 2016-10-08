9842f96027f4a8c16ea80470a3305bcd1daa3987
MosanZH <mosan_zh@163.com>
2016-10-08 01:15:18 +0100

    2016-10-07's Java workout

---
+++ contributions/MosanZH/Java/I/O/2016-10-07.java
@@ -0,0 +1,4 @@
+Use `File.separator` while dealing with file paths
+Translating `Scanner` tokens into primitive types
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
