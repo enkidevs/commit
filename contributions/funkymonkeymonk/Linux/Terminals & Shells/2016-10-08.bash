fcee6bfdcd6ff68910498cb189d6ca98e03cdd6d
funkymonkeymonk <monkey@buildingbananas.com>
2016-10-08 19:57:10 +0100

    2016-10-08's Linux workout

---
+++ contributions/funkymonkeymonk/Linux/Terminals & Shells/2016-10-08.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Aliasing ssh connections
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
