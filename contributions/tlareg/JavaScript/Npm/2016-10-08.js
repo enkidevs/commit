b65beb64ef404d90d6c480ad04e08f9ff580364f
tlareg <tlaregaivir@gmail.com>
2016-10-08 18:48:29 +0100

    2016-10-08's JavaScript workout

---
+++ contributions/tlareg/JavaScript/Npm/2016-10-08.js
@@ -0,0 +1,5 @@
+npm can handle multiple versions of a dependency
+Combining script commands in npm
+Environmental variables and flags for `npm`
+`npm` verifies `sha1` hashes of packages
+Lock down dependency versions by shrinkwrapping
