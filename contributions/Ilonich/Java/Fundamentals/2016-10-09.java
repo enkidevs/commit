11cd93b2b91b221311f458889a84dd9fd8e732d7
Ilonich <niko-ikko@yandex.ru>
2016-10-09 03:45:16 +0100

    2016-10-09's Java workout

---
+++ contributions/Ilonich/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
