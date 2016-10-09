ce28bc9f6647d338aa7137579e300c8ec44eaa3f
Edvinas <edvinas.reg@outlook.com>
2016-10-09 10:40:12 +0100

    2016-10-09's Java workout

---
+++ contributions/Edvinas /Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Overloading Methods in Java
+Use strings in a `switch` statement
+Calling one constructor from another
+Locks in `static synchronized` methods
