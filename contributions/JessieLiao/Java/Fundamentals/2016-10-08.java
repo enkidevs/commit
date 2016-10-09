034d652f0839d5b61cf930b8adad370376b239a9
JessieLiao <jessie.liao.0622@gmail.com>
2016-10-09 03:50:34 +0100

    2016-10-08's Java workout

---
+++ contributions/JessieLiao/Java/Fundamentals/2016-10-08.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Constant notation
