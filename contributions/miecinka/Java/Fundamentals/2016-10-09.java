d6b2888144f0ee906821551cca4b58fb4e10d6df
miecinka <mietinhovanish@gmail.com>
2016-10-09 17:40:25 +0100

    2016-10-09's Java workout

---
+++ contributions/miecinka/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Use meaningful names
+Use strings in a `switch` statement
+Locks in `static synchronized` methods
