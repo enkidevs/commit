72aa59a3477809bb95c0764f2515c2384fd42063
Rainsho <djkyux@163.com>
2016-10-09 02:37:17 +0100

    2016-10-08's Java workout

---
+++ contributions/Rainsho/Java/Fundamentals/2016-10-08.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
