ae0faa52faaecb1e3fa9297fa4059f11f588db5f
omarsafwany <omar.safwany@gmail.com>
2016-10-09 22:35:39 +0100

    2016-10-09's Java workout

---
+++ contributions/omarsafwany/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
