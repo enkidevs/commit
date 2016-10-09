5627db0834c4dcc2dcc8b45dee407c23e8d2bfe7
jjmiranda <jjmiranda@magia.digital>
2016-10-09 23:54:12 +0100

    2016-10-09's Linux workout

---
+++ contributions/jjmiranda/Linux/File Management/2016-10-09.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Practical `du` command
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
