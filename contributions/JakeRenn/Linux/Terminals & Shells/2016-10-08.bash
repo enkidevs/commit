efe5719258d89f2c5f342003c6b9dfdcbce7fd5b
JakeRenn <rencjviei@gmail.com>
2016-10-09 07:18:13 +0100

    2016-10-08's Linux workout

---
+++ contributions/JakeRenn/Linux/Terminals & Shells/2016-10-08.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Useful aliasing in bash
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
