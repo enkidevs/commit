c10534062169c274e2e4b627238c97282bc7c668
Iprovidethepaint <iprovidethepaint@gmail.com>
2016-10-10 00:30:09 +0100

    2016-10-09's Java workout

---
+++ contributions/Iprovidethepaint/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using an interface as a parameter
