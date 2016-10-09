5f1fe6adedcfcd56aec2a7cca60ceb33081d1e71
christopheradr <christopher.diaz.riv@gmail.com>
2016-10-09 22:39:06 +0100

    2016-10-09's Linux workout

---
+++ contributions/christopheradr/Linux/Package Management/2016-10-09.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+Backporting on Debian
+
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
