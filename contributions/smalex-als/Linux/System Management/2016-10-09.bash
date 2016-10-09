bcc0cd8b6df213e4ba1fa4f479a9a1329b8eb1aa
smalex-als <smalex69@gmail.com>
2016-10-10 00:04:27 +0100

    2016-10-09's Linux workout

---
+++ contributions/smalex-als/Linux/System Management/2016-10-09.bash
@@ -0,0 +1,5 @@
+Common log files
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
