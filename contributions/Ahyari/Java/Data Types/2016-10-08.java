6278b882b30eb579351d58db622760bee6cccbb0
Ahyari <ahmad.alhyari@gmail.com>
2016-10-09 06:12:52 +0100

    2016-10-08's Java workout

---
+++ contributions/Ahyari/Java/Data Types/2016-10-08.java
@@ -0,0 +1,5 @@
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Converting a string to upper or lower case
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
