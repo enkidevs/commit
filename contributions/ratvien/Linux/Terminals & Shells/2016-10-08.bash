b7b2643702f5c97993eef0c1dfa1fb968f7dda80
ratvien <ratvien@gmail.com>
2016-10-09 10:06:05 +0100

    2016-10-08's Linux workout

---
+++ contributions/ratvien/Linux/Terminals & Shells/2016-10-08.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Clear the terminal instantly
+Conditional command execution
+
+(`&&` operator)
