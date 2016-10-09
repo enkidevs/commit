843e389c18b4ff10556b9d0fe553e437baae8e11
bstruthers <bstruthers@gmail.com>
2016-10-09 16:39:18 +0100

    2016-10-09's JavaScript workout

---
+++ contributions/bstruthers/JavaScript/Functions/2016-10-09.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Move cursor at the end of text input
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
