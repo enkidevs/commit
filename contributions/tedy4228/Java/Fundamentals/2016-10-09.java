d2639c1c0f173d5e0ff75f139aadfaf5b434394a
tedy4228 <thebilbo42@aol.com>
2016-10-09 07:54:57 +0100

    2016-10-09's Java workout

---
+++ contributions/tedy4228/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
