6071714349e94cbe104d705a4692c14c9173cd40
Alanbato <alanvelasco.a@gmail.com>
2016-10-09 15:34:42 +0100

    2016-10-09's Linux workout

---
+++ contributions/Alanbato/Linux/System Management/2016-10-09.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Find the `MAC` address of all network interfaces
+
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Executing scripts on system boot using `systemd`
