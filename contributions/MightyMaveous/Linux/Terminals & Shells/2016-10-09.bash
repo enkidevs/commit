b7cd68eee259c67b320253f28b7f203e5aaa6bef
MightyMaveous <marvin.n.evers@gmail.com>
2016-10-09 10:07:52 +0100

    2016-10-09's Linux workout

---
+++ contributions/MightyMaveous/Linux/Terminals & Shells/2016-10-09.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
