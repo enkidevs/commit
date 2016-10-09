bd06f6ac3d46ee4585c2a9939bfa6d3d81015ea9
Mattle <mcallier@gmail.com>
2016-10-09 21:21:10 +0100

    2016-10-09's Java workout

---
+++ contributions/Mattle/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use meaningful names
+Inheriting a constructor from a superclass
+Use `DecimalFormat` class to format numbers
+Always specify type parameter
