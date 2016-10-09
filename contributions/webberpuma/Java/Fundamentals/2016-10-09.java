80791bb6d5c56aaeae5653879c9b438a448f0a3d
webberpuma <webberpuma@gmail.com>
2016-10-10 00:57:03 +0100

    2016-10-09's Java workout

---
+++ contributions/webberpuma/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+The trade-off between short and descriptive names
+Ternary operator
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
