ba14164e89881f7adb7d8475d8a5ca794a6139fe
alejandrovillegas <neopilatos@gmail.com>
2016-10-09 14:25:38 +0100

    2016-10-09's Java workout

---
+++ contributions/alejandrovillegas/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
