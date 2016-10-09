948ec2dc116875a145d493a69b31c92bcc048df0
zszuri <szuri90@gmail.com>
2016-10-09 10:57:28 +0100

    2016-10-09's Java workout

---
+++ contributions/zszuri/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
