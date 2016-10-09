d835ee198f19258d42a6865af381323fef34916f
m4vy <m4vypl@gmail.com>
2016-10-09 16:07:43 +0100

    2016-10-09's Java workout

---
+++ contributions/m4vy/Java/I/O/2016-10-09.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Create system file paths using the `Path` class
+Reading and writing text files
+Reading file attributes
+Listing a file system's root directories
