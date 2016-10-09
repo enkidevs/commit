189b2a57dcb98e50795c300f9fb5fd4e251f826f
Avabin <andrzej.piotrowski.76@gmail.com>
2016-10-09 11:39:51 +0100

    2016-10-09's Linux workout

---
+++ contributions/Avabin/Linux/System Management/2016-10-09.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
