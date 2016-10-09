83f556a7e4b0226017bdd2f0ad3847a2e436158a
Yupie <yupie_123@rocketmail.com>
2016-10-09 14:05:45 +0100

    2016-10-09's Java workout

---
+++ contributions/Yupie/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
+Supply `toString()` in all classes
