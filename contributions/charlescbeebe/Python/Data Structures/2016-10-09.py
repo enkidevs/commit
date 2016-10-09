f64cf07f2b6e7fa3705e4b63069e427249be5662
charlescbeebe <charlescbeebe@gmail.com>
2016-10-09 16:05:47 +0100

    2016-10-09's Python workout

---
+++ contributions/charlescbeebe/Python/Data Structures/2016-10-09.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Working with `set`s
+Get the most of `float`s
+Operations with `bytes` and `bytearray`
+Keep things in order with `OrderedDict`
