3fb32b83ee24098bc9f16d1de0a3654852ee7d12
saeedjassani <saeedjassani@gmail.com>
2016-10-09 16:35:28 +0100

    2016-10-08's Java workout

---
+++ contributions/saeedjassani/Java/Fundamentals/2016-10-08.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
