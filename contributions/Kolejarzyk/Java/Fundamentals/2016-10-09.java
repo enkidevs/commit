bbd4d0259af09de439c57f05a63ff915b57b526f
Kolejarzyk <pawel.dee@live.com>
2016-10-09 14:13:12 +0100

    2016-10-09's Java workout

---
+++ contributions/Kolejarzyk/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
