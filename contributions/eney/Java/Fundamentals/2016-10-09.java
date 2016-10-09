f6f9a48f1ca5604bf9c3e8eb02b95cf4676bd2c2
eney <jzabrodin@gmail.com>
2016-10-09 18:07:58 +0100

    2016-10-09's Java workout

---
+++ contributions/eney/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Difference between `==` and `equals()`
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
+`Autoboxing` and `Unboxing`
