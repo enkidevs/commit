de2a7733d4272cfe7425c21c9897a269fcd75cd9
CellarD0-0r <alan.turing39@gmail.com>
2016-10-09 07:53:03 +0100

    2016-10-09's Java workout

---
+++ contributions/CellarD0-0r/Java/Error Handling/2016-10-09.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Converting stack trace to a string
+Using exceptions in Java
+Most common reason behind **stack overflow** error
+Throwing an `exception`
