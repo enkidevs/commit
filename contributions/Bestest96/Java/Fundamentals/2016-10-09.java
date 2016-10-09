e468fc6040c43d6936fbd3b053474bc3f2b2f0d9
Bestest96 <jaluki@vp.pl>
2016-10-09 03:53:54 +0100

    2016-10-09's Java workout

---
+++ contributions/Bestest96/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
