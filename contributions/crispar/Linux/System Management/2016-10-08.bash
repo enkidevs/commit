cc6ed5ab6e280332dff53685534e9eda057d82ea
crispar <jwcris.park@gmail.com>
2016-10-09 15:16:37 +0100

    2016-10-08's Linux workout

---
+++ contributions/crispar/Linux/System Management/2016-10-08.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
+Cancel a system shutdown
