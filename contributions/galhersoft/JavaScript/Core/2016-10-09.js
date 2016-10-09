4e07e0fca7e55eab3ca5de146ab7d6ec34e9a0a6
galhersoft <galhersoft@gmail.com>
2016-10-09 22:02:34 +0100

    2016-10-09's JavaScript workout

---
+++ contributions/galhersoft/JavaScript/Core/2016-10-09.js
@@ -0,0 +1,5 @@
+Treating a boolean as number
+Double and single quotes
+Hoisting applies only to variable declarations, not initializations
+Apply `map` function to array items
+Scope and strict mode
