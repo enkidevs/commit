b64d4a2341e43928917767785540eea504d0cfd2
Yadeki <ydekinder@telenet.be>
2016-10-09 18:55:05 +0100

    2016-10-09's Linux workout

---
+++ contributions/Yadeki/Linux/Package Management/2016-10-09.bash
@@ -0,0 +1,5 @@
+List installed packages
+Prevent updating a specific package in Debian systems
+Using `Tasksel` for software installation
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
