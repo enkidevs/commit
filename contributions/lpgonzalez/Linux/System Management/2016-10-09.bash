c481f7820da17d8e8c18a3bfffe61243d9e4b326
lpgonzalez <lisardo.prieto.gonzalez@gmail.com>
2016-10-09 11:54:31 +0100

    2016-10-09's Linux workout

---
+++ contributions/lpgonzalez/Linux/System Management/2016-10-09.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Removing old kernels in Debian based systems
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
