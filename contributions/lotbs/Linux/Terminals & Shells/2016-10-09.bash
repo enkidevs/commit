885b0a682110e9d73f1d4ebeb9452860d75c9b32
lotbs <nkosindlovu87@gmail.com>
2016-10-09 08:34:39 +0100

    2016-10-09's Linux workout

---
+++ contributions/lotbs/Linux/Terminals & Shells/2016-10-09.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
