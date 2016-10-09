80bd1fdba1bcdcb7289581311433216641925df0
thomasthiebaud <thiebaud.tom@gmail.com>
2016-10-09 08:05:01 +0100

    2016-10-09's Java workout

---
+++ contributions/thomasthiebaud/Java/Java 8/2016-10-09.java
@@ -0,0 +1,5 @@
+Java 8 Lambda expressions
+Use `Optional(s)` type in order to avoid `NullPointerException(s)`
+Control OS `Process`es in `Java8`
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
