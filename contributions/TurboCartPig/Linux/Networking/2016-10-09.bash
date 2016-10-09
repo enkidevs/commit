cb72edb2e862b997c0892ee178b0793b4ff28096
TurboCartPig <dennkris@live.no>
2016-10-09 13:50:55 +0100

    2016-10-09's Linux workout

---
+++ contributions/TurboCartPig/Linux/Networking/2016-10-09.bash
@@ -0,0 +1,5 @@
+How to clear `iptables` rules
+Useful ssh client optimizations 
+Sending emails from terminal / scripts
+Make use of `netstat`
+Staying connected remotely via SSH without an interactive shell
