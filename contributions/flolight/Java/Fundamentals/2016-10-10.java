d051b9678f9e4b164ca477ba7b509fc9b214035f
flolight <florianclanet@gmail.com>
2016-10-10 13:09:00 +0100

    2016-10-10's Java workout

---
+++ contributions/flolight/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Overloading Methods in Java
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+Constant notation
