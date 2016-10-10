71c2f87c6ddb3b7f4d24af49e16145493e12b9c8
TheInitializer <dkteresi@gmail.com>
2016-10-10 19:56:08 +0100

    2016-10-10's Linux workout

---
+++ contributions/TheInitializer/Linux/Terminals & Shells/2016-10-10.bash
@@ -0,0 +1,7 @@
+Find any Unix / Linux command
+Useful aliasing in bash
+Random password generator
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
