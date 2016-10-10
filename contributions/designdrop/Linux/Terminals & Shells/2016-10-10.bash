c7638765a4c598e7b25f48c669ba0b6d27ed9dc3
designdrop <abekalili@gmail.com>
2016-10-10 17:43:23 +0100

    2016-10-10's Linux workout

---
+++ contributions/designdrop/Linux/Terminals & Shells/2016-10-10.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Random password generator
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
