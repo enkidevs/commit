8ae29c3f0b2bda44c782f36b92e041eb118fb018
thrundi <thrundi@gmail.com>
2016-10-10 12:34:08 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/thrundi/JavaScript/Node/2016-10-10.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Asynchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+Automatic compilation for Node with **Nodemon** 
+Middleware/pipeline design pattern
