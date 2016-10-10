92faaec97fb04791646e5ae652270dacaa6f76f0
sierzput <sierzput@gmail.com>
2016-10-10 07:51:17 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/sierzput/JavaScript/React/2016-10-10.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Props in getInitialState Is an        
+Anti-Pattern
+Expose Component Functions in **React**
+Dealing with `this.props.children`
+Shortcut for transferring props
