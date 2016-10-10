aede137829a2586e9a09bf34309f8fae560533cc
Gimyboya <b.afatah@yahoo.com>
2016-10-10 09:26:18 +0100

    2016-10-09's Java workout

---
+++ contributions/Gimyboya/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
