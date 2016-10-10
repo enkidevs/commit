43489a3d80641631c07bfde83c4cda8e70403858
Narckcomler <218451@student.pwr.edu.pl>
2016-10-10 13:09:06 +0100

    2016-10-10's Linux workout

---
+++ contributions/Narckcomler/Linux/System Management/2016-10-10.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Common log files
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
