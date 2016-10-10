2252c8cec3853ee715cca06efb499120f511edad
gigioSouza <giovane.bds@gmail.com>
2016-10-10 12:50:28 +0100

    2016-10-10's Java workout

---
+++ contributions/gigioSouza/Java/Data Types/2016-10-10.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Use `Arrays.asList()` to initialise lists
+Do not attempt comparisons with NaN
