ebb5233ac28e2025b2c221280450d4a3768e0790
teclab85 <teclab85@gmail.com>
2016-10-10 13:11:56 +0100

    2016-10-10's Java workout

---
+++ contributions/teclab85/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Using bounded type parameters in generic methods
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
