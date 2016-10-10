bae789c6cf91f18ee07e3f666405070f216be86f
gstavrinos <stavrinosgeo@gmail.com>
2016-10-10 16:32:35 +0100

    2016-10-10's Linux workout

---
+++ contributions/gstavrinos/Linux/Networking/2016-10-10.bash
@@ -0,0 +1,5 @@
+Limit the `wget` download rate
+Getting the connection speed from the terminal
+Bandwidth monitoring tools 
+Use `tcpdump` to listen to network interface traffic
+Short Introduction to `Wget` 
