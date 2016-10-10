cf05b36fcd4be625ac1afa86ec8c7d4ed148764c
redzik <erdezet@gmail.com>
2016-10-10 12:07:48 +0100

    2016-10-10's Java workout

---
+++ contributions/redzik/Java/Data Types/2016-10-10.java
@@ -0,0 +1,5 @@
+Two ways to use an `Iterator`
+Converting a string to upper or lower case
+Finding a substring in a string
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
