9cea22c8eec4803aaa6c03693244e5791c01a9c7
thescriptking <thescriptking@gmail.com>
2016-10-10 10:12:08 +0100

    2016-10-10's Linux workout

---
+++ contributions/thescriptking/Linux/Terminals & Shells/2016-10-10.bash
@@ -0,0 +1,7 @@
+Aliasing ssh connections
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
