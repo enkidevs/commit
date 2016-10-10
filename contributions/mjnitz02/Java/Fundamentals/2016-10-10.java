4836a4b322db7a33a55b0cc4302669fb9a353ebf
mjnitz02 <mjnitz02@gmail.com>
2016-10-10 20:14:15 +0100

    2016-10-10's Java workout

---
+++ contributions/mjnitz02/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
