1d394e822831e1f6dc00d312c58ef0d0600f8071
pratikkayasths <pratik.kayastha@gmail.com>
2016-10-10 10:13:05 +0100

    2016-10-09's Java workout

---
+++ contributions/pratikkayasths/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
