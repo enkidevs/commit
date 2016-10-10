90b0851d64a8b10a581e927c1b1429b2fe3bb2ef
jecastro1 <jecastro.ie@gmail.com>
2016-10-10 07:44:07 +0100

    2016-10-10's Java workout

---
+++ contributions/jecastro1/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
