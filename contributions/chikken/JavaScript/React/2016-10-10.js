16a8d122bad7202631cf08351571db8ccf904ccd
chikken <joel.wahlund@comhem.se>
2016-10-10 21:32:14 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/chikken/JavaScript/React/2016-10-10.js
@@ -0,0 +1,5 @@
+Functional Stateless Components in React
+`if`-`else` statements in **JSX** and **React**
+**React** `this.props.children`
+Enhanced Prop Validation 
+Shortcut for transferring props
