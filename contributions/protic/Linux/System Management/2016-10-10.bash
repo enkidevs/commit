f49c9a045cffda1ac6602b6ca1a04bfb05935e24
protic <adispapazian@gmail.com>
2016-10-10 16:11:33 +0100

    2016-10-10's Linux workout

---
+++ contributions/protic/Linux/System Management/2016-10-10.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
