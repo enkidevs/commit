be65d3c7404a42bd12b046471f04c7143b5e7220
Zanex <alessio.zanella88@gmail.com>
2016-10-10 11:09:02 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/Zanex/JavaScript/Express/2016-10-10.js
@@ -0,0 +1,5 @@
+`response` object additional properties in **Express**
+**GZIP** compression for **Express**
+Using **Express 4.0** `router`
+Session handling in **Express**
+Error handler in **Express**
