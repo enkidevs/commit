f4344255b00fe6eb3ea5add85dd71113e49064be
jeffs5 <jeffs5s@gmail.com>
2016-10-10 23:18:42 +0100

    2016-10-10's Java workout

---
+++ contributions/jeffs5/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
