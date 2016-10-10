70a7d9fe7bd7067b442bdd1b51c9c9add17cd726
Creeperfriend <clemenstiedt@gmx.de>
2016-10-10 20:27:43 +0100

    2016-10-10's Linux workout

---
+++ contributions/Creeperfriend/Linux/Networking/2016-10-10.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+Get real network statistics with `slurm`
+List your IPv6 configuration
+Setting up password-free authentication
+Use `tcpdump` to listen to network interface traffic
