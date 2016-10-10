55958d99309427ef1b67035ef09780d2a62ab487
dr.virtuozov <dr.virtuozov@ya.ru>
2016-10-10 17:00:42 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/dr.virtuozov/JavaScript/Npm/2016-10-10.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+npm can handle multiple versions of a dependency
+Combining script commands in npm
+Environmental variables and flags for `npm`
+`peerDependencies`
