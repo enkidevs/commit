3fb1b033cd98886ea3286f363b3c9f15c286fc61
shumkovdenis <shumkovdenis@gmail.com>
2016-10-10 17:35:55 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/shumkovdenis/JavaScript/Node/2016-10-10.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+`uncaughtException` listener in Node.js
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
