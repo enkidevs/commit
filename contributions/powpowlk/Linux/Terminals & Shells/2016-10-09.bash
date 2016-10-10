4faedfaefabb0fcabb5bd645a1f2ab9d8371dd23
powpowlk <matiasmendoza1994@gmail.com>
2016-10-10 06:20:31 +0100

    2016-10-09's Linux workout

---
+++ contributions/powpowlk/Linux/Terminals & Shells/2016-10-09.bash
@@ -0,0 +1,7 @@
+Clear the terminal instantly
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
