fa339dc50f524fb5b95c56c625ea7b16d10c6f7c
cmbpgh <cmbpgh@gmail.com>
2016-10-10 09:39:13 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/cmbpgh/JavaScript/Core/2016-10-10.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Undefined / Empty / Null Checks
+Use `splice()` to remove an item from an array
+Hoisting applies only to variable declarations, not initializations
+`console.*` in JavaScript
