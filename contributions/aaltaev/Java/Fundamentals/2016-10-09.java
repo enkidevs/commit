cb6bda22ce634f40f697ab74a0037e935b0e51ab
aaltaev <andalt92@gmail.com>
2016-10-10 18:13:32 +0100

    2016-10-09's Java workout

---
+++ contributions/aaltaev/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
