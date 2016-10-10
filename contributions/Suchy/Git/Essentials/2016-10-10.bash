7b05cc6f6398cef9b355b3823290b37b1a6eac3e
Suchy <a.kutrasinski@gmail.com>
2016-10-10 07:47:07 +0100

    2016-10-10's Git workout

---
+++ contributions/Suchy/Git/Essentials/2016-10-10.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+Search for commits by author
+Check the status of your files
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
