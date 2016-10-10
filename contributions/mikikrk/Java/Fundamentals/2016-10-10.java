7aa95361a6d5bbe3d97133c01ddaa53a04559d30
mikikrk <miki.krk@gmail.com>
2016-10-10 07:33:47 +0100

    2016-10-10's Java workout

---
+++ contributions/mikikrk/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Measuring time
