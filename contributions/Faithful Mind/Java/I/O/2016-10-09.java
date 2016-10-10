ab660b36b2e9aadbc72a43485b8b7bb2fa0b1f12
Faithful Mind <lpyy15@gmail.com>
2016-10-10 06:50:04 +0100

    2016-10-09's Java workout

---
+++ contributions/Faithful Mind/Java/I/O/2016-10-09.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+How to list all files in a directory that match a filename extension in Java
+Create system file paths using the `Path` class
+Listing a file system's root directories
