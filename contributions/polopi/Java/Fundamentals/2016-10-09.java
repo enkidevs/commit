b87c837744723dcce7980cc965d951799af7ab71
polopi <p.popineau@gmail.com>
2016-10-10 04:19:25 +0100

    2016-10-09's Java workout

---
+++ contributions/polopi/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
