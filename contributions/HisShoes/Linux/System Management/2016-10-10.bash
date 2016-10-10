fd0f3a38a944a97a5ae753008acbb1de04c16d5a
HisShoes <ryan999000999@hotmail.co.uk>
2016-10-10 14:50:23 +0100

    2016-10-10's Linux workout

---
+++ contributions/HisShoes/Linux/System Management/2016-10-10.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Finding Open Files With `lsof`
