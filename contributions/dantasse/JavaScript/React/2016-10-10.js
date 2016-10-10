8e7a099295ed04cd95396fcaab5e5af729fd2739
dantasse <dan.tasse@gmail.com>
2016-10-10 16:26:06 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/dantasse/JavaScript/React/2016-10-10.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Use **React** with other libraries
+Keyed fragments in **React**
+Enhanced Prop Validation 
