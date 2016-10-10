d34b80cd146c0bd27d6a18300440b693385c97e7
NanoBugg <nuno.franco16@gmail.com>
2016-10-10 13:06:43 +0100

    2016-10-10's Java workout

---
+++ contributions/NanoBugg/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
