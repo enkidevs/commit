d53ea958ac9d72142e9ee211b9d542bb64408fca
alex.expertvision <alexandru.balasa@rocketmail.com>
2016-10-10 06:29:39 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/alex.expertvision/JavaScript/React/2016-10-10.js
@@ -0,0 +1,6 @@
+`null` value for *Controlled Components* in **React**
+Props in getInitialState Is an        
+Anti-Pattern
+`PureRenderMixin` in **React**
+`ref` callback attribute
+Enhanced Prop Validation 
