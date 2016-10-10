17d9c29b5e4bf6ff72aa555e29c92d069d0030c2
amro <amr.elghobary@gmail.com>
2016-10-10 09:50:33 +0100

    2016-10-10's Java workout

---
+++ contributions/amro/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+Always specify type parameter
+Implementing an `interface`
+Using an interface as a parameter
