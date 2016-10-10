d2bc88569f443b464b559ed6a2fe991ab10b52af
willcodes <wdifruscio@gmail.com>
2016-10-10 14:55:22 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/willcodes/JavaScript/Npm/2016-10-10.js
@@ -0,0 +1,5 @@
+Registry user accounts for npm
+Bump package version in npm
+npm can handle multiple versions of a dependency
+Current Lifecycle Event
+Lock down dependency versions by shrinkwrapping
