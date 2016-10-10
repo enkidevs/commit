75d8383f6bd17f68bce06ab22f61d7047829fa28
DaMo86 <moritz.daniel+git@gmail.com>
2016-10-10 07:37:48 +0100

    2016-10-10's Java workout

---
+++ contributions/DaMo86/Java/I/O/2016-10-10.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Reading file attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
