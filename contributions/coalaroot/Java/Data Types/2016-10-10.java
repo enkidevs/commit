97ad0dd48e2f97e4a5022894a8220b1a5399646a
coalaroot <jakubkrolak94@gmail.com>
2016-10-10 10:33:43 +0100

    2016-10-10's Java workout

---
+++ contributions/coalaroot/Java/Data Types/2016-10-10.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Retrieve the component type of an array
+String concatenation in Java
+Do not attempt comparisons with NaN
