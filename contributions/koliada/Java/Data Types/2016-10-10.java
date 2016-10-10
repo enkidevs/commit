3f47ee0f813d406cb36dc19a40d1bf79802e84b2
koliada <alex.fiator@gmail.com>
2016-10-10 06:22:30 +0100

    2016-10-10's Java workout

---
+++ contributions/koliada/Java/Data Types/2016-10-10.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Finding a substring in a string
+Equals operation on different data types
+Numeric Conversion - Widening
