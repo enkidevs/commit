4fec023b67eb52546b7f18e445801d519d61ce34
martin-rifon <mrifonelizalde@gmail.com>
2016-10-10 17:03:53 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/martin-rifon/JavaScript/React/2016-10-10.js
@@ -0,0 +1,6 @@
+Pixel values in style props for **React**
+Props in getInitialState Is an        
+Anti-Pattern
+Use **React** with other libraries
+Shallow compare in **React**
+Validate for required props
