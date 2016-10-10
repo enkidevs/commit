fbc80f7ce4dcea9d50c62a953972451befad1d1a
wieczorek1990 <wieczorek1990@gmail.com>
2016-10-10 17:40:07 +0100

    2016-10-10's Git workout

---
+++ contributions/wieczorek1990/Git/Tools/2016-10-10.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Interactively unstage changes
+Specify a range of commits using double dot syntax
+Create a new branch from a stash
+Show history of a function
