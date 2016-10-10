f96ea79599373743ade4780ded7b55a59f1d37ae
tiagoalsilva <tiagoalsilva@gmail.com>
2016-10-10 13:01:21 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/tiagoalsilva/JavaScript/Node/2016-10-10.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+Automatic compilation for Node with **Nodemon** 
+Middleware/pipeline design pattern
