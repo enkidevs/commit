7a7e7dedc236d5871a0c1cfdc381e1eeb8f1b764
Juice312 <juicedrink312@gmail.com>
2016-10-10 22:34:08 +0100

    2016-10-10's Linux workout

---
+++ contributions/Juice312/Linux/File Management/2016-10-10.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
