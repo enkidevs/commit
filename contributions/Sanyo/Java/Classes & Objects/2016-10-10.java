3f874c8f456514d9af77fea854764dfd5828ce9f
Sanyo <micmark6@gmail.com>
2016-10-10 23:48:16 +0100

    2016-10-10's Java workout

---
+++ contributions/Sanyo/Java/Classes & Objects/2016-10-10.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Template methods in abstract classes
+Utility classes and `static` methods
+Compare classes and not class names
+How and when to use `WeakHashMap` class
