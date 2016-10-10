36c5c169b9a6d11e5344a082ed86c609cbdf2589
kaizendad <wade@wadearmstrong.com>
2016-10-10 21:46:30 +0100

    2016-10-10's Git workout

---
+++ contributions/kaizendad/Git/Essentials/2016-10-10.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+How to set your username and email
+Use `short` status to make output more compact
+How to write a git commit message
+Remote repositories: fetching and pushing
