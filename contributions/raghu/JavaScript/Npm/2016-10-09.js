5b21c8ff0fcea761b1ac2f00c1a5a7f235b0038a
raghu <raghu.9208@gmail.com>
2016-10-10 04:53:46 +0100

    2016-10-09's JavaScript workout

---
+++ contributions/raghu/JavaScript/Npm/2016-10-09.js
@@ -0,0 +1,5 @@
+Environmental variables and flags for `npm`
+`npm` verifies `sha1` hashes of packages
+`optionalDependencies` in npm
+Scoped packages
+Lock down dependency versions by shrinkwrapping
