5aa0fb4d10064bbc865530715094ae51e07f7cc7
jessanhq <jessan@gmail.com>
2016-10-10 15:30:27 +0100

    2016-10-10's Java workout

---
+++ contributions/jessanhq/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Detect or prevent integer overflow
