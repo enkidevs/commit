1387a21c368ea5f1d544fdb7c79be9f3d4f78947
Thommi <thomasandre84@gmail.com>
2016-10-10 20:34:12 +0100

    2016-10-10's Java workout

---
+++ contributions/Thommi/Java/I/O/2016-10-10.java
@@ -0,0 +1,5 @@
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Retrieving file store attributes
+Listing a file system's root directories
