62921a94db0e33611fa218aa6e0a344611cb929d
djleox <leandrolion88@hotmail.com>
2016-10-10 19:10:49 +0100

    2016-10-10's Java workout

---
+++ contributions/djleox/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
