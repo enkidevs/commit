35043e52f35a372083a16d287ab10e715377386f
Morion <morion.self@gmail.com>
2016-10-10 06:45:14 +0100

    2016-10-09's Java workout

---
+++ contributions/Morion/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Using an interface as a parameter
