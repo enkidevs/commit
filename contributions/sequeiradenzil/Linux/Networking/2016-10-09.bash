5d2640ee4cbaff94d0bf6c2acb6ff20f3004f946
sequeiradenzil <sequeira.denzil@gmail.com>
2016-10-10 07:59:18 +0100

    2016-10-09's Linux workout

---
+++ contributions/sequeiradenzil/Linux/Networking/2016-10-09.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Useful ssh client optimizations 
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
+Short Introduction to `Wget` 
