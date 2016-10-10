b0ea8580ccd78f76bb0012173a4dc1b6cd29c5c5
Geroxan <alik810@seznam.cz>
2016-10-10 17:43:39 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/Geroxan/JavaScript/Npm/2016-10-10.js
@@ -0,0 +1,5 @@
+Deprecate npm packages
+Manipulate npm packages cache
+npm can handle multiple versions of a dependency
+Environmental variables and flags for `npm`
+Lock down dependency versions by shrinkwrapping
