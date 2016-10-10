07855e77114189efdab6b866f0064a7c20e16b7c
cschritt <christian@trademesh.de>
2016-10-10 12:56:17 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/cschritt/JavaScript/Node/2016-10-10.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Asynchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+`uncaughtException` listener in Node.js
+Automatic compilation for Node with **Nodemon** 
