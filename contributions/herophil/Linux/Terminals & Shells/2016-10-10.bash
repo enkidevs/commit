bec72c7d7ef5414a40e0f5e7d6a92828416991a9
herophil <philip.herold.01@gmail.com>
2016-10-10 14:11:04 +0100

    2016-10-10's Linux workout

---
+++ contributions/herophil/Linux/Terminals & Shells/2016-10-10.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Terminal incognito mode
+Keep useful commands in your shell history with tags
