b35111c9040cf996f9ddf8620378722f1d7189b4
knapeczadam <knapecz.adam@gmail.com>
2016-10-10 19:11:42 +0100

    2016-10-10's Java workout

---
+++ contributions/knapeczadam/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
