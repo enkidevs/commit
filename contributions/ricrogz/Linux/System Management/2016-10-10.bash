8f1554a16521f8efcff2618ed29ee727052af761
ricrogz <ricrogz@gmail.com>
2016-10-10 08:04:58 +0100

    2016-10-10's Linux workout

---
+++ contributions/ricrogz/Linux/System Management/2016-10-10.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Removing old kernels in Debian based systems
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
