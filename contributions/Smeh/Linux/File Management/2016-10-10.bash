6fc471f7d52e14228951817fab62defdecbdc73a
Smeh <sgarcia030@gmail.com>
2016-10-10 18:02:48 +0100

    2016-10-10's Linux workout

---
+++ contributions/Smeh/Linux/File Management/2016-10-10.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
+Delete commands aliases
