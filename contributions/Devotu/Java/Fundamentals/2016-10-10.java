32719402ffa40faa0032e6de52cd20a16d22510a
Devotu <otuvesson@gmail.com>
2016-10-10 15:10:06 +0100

    2016-10-10's Java workout

---
+++ contributions/Devotu/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
