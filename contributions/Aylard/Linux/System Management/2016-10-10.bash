0af608a66bcb37f338fda8169061d6cff040f7d3
Aylard <aylard7@gmail.com>
2016-10-10 07:22:23 +0100

    2016-10-10's Linux workout

---
+++ contributions/Aylard/Linux/System Management/2016-10-10.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
