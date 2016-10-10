cc3d9ac04a81d3e9f764e71981c5b2508709a60c
yungchunkan <yungchunkan@gmail.com>
2016-10-10 19:12:12 +0100

    2016-10-09's Java workout

---
+++ contributions/yungchunkan/Java/Fundamentals/2016-10-09.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
