b65ddb36c6578c5b373b80ad87fd2119d245254b
cecco <francescousseglio@gmail.com>
2016-10-10 22:49:39 +0100

    2016-10-10's Linux workout

---
+++ contributions/cecco/Linux/Networking/2016-10-10.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Multiple IP Address with aliasing 
+Useful ssh client optimizations 
+Bandwidth monitoring tools 
+Staying connected remotely via SSH without an interactive shell
