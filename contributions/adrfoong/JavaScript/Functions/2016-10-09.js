02cda2ea68b7486aab4c5ef01683620e7dd845f3
adrfoong <eidorian.foong@gmail.com>
2016-10-10 06:24:57 +0100

    2016-10-09's JavaScript workout

---
+++ contributions/adrfoong/JavaScript/Functions/2016-10-09.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Changing a functions context with `fn.call(object)`
