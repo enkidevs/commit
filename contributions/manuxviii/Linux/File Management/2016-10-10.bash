268fbd71db9370580ec288f1159996cb29996a97
manuxviii <emmanuel.mesnige@protonmail.com>
2016-10-10 21:04:46 +0100

    2016-10-10's Linux workout

---
+++ contributions/manuxviii/Linux/File Management/2016-10-10.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
