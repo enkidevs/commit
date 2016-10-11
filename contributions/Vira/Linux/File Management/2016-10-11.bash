c30aff2e0fe352a32917c55e85c754d24f146308
Vira <thomas.ruehrig@studierende.htl-donaustadt.at>
2016-10-11 06:36:30 +0100

    2016-10-11's Linux workout

---
+++ contributions/Vira/Linux/File Management/2016-10-11.bash
@@ -0,0 +1,6 @@
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
+Delete commands aliases
