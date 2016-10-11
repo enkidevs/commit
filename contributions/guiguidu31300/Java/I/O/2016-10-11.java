9722f3055e385d1976f0c1e0a74a131ef9183942
guiguidu31300 <guiguidu31300@gmail.com>
2016-10-11 19:17:04 +0100

    2016-10-11's Java workout

---
+++ contributions/guiguidu31300/Java/I/O/2016-10-11.java
@@ -0,0 +1,5 @@
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Reading file attributes
+Using the `OpenOption...` vararg to define file writing behavior
