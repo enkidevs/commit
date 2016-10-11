1bd338319a9d1a5493fa9d7091207c0e1b669880
Kertt123 <krzysztof_serkowski@interia.pl>
2016-10-11 07:48:42 +0100

    2016-10-11's Java workout

---
+++ contributions/Kertt123/Java/Data Types/2016-10-11.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Equals operation on different data types
+String concatenation in Java
