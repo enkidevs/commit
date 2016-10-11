58fd1beef953668a352fb516a1b59fb088622f7d
1ktribble <tribble.kai@gmail.com>
2016-10-11 19:59:30 +0100

    2016-10-11's Java workout

---
+++ contributions/1ktribble/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Updating interfaces by using `default` methods
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
