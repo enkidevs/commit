0b32b0ab06b3a124e1d13e3f82559536c2ff6cb4
bidj <enki@papito.33mail.com>
2016-10-11 20:13:28 +0100

    2016-10-11's Linux workout

---
+++ contributions/bidj/Linux/File Management/2016-10-11.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Get disk space usage with `df`
+
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
+Easy way of sharing files
