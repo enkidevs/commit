32057a0eaea6c508b22e2e02ef6014a807a3e0ba
fthebaud <fthebaud@gmail.com>
2016-10-11 07:02:54 +0100

    2016-10-11's Java workout

---
+++ contributions/fthebaud/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Metadata: setting a file's owner
+Supply `toString()` in all classes
