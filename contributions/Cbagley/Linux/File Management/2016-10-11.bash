420877d527a33adc9ab4915e75a80f0cfe364878
Cbagley <cpbagley@gmail.com>
2016-10-11 21:50:17 +0100

    2016-10-11's Linux workout

---
+++ contributions/Cbagley/Linux/File Management/2016-10-11.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Delete commands aliases
