42406d0e54e9b0a01198fa9b560006ea69ed21e2
mindw0rk <mindvvorc@gmail.com>
2016-10-11 02:13:30 +0100

    2016-10-10's Java workout

---
+++ contributions/mindw0rk/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Locks in `static synchronized` methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
