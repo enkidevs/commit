26a50e2a0fc5b4a79c45bb7ad2fe4258d12bafec
helldrum <jonathanchardon@hotmail.fr>
2016-10-11 17:23:28 +0100

    2016-10-11's Linux workout

---
+++ contributions/helldrum/Linux/File Management/2016-10-11.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
