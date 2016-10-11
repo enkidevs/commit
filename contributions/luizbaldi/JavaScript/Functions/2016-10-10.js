dc7c83d005828144f0f41eca093c9b0da7a46394
luizbaldi <baldilp@gmail.com>
2016-10-11 01:43:40 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/luizbaldi/JavaScript/Functions/2016-10-10.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+`.bind()` 
+Method chaining
+Changing a functions context with `fn.call(object)`
