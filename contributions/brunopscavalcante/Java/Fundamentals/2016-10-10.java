06ea2b9c59b4d6cb645fd7f2ff9f55bd09a85a6c
brunopscavalcante <brian.solidus@gmail.com>
2016-10-11 01:21:26 +0100

    2016-10-10's Java workout

---
+++ contributions/brunopscavalcante/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Measuring time
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
