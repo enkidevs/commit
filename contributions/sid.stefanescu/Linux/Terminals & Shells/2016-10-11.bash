61349b4c4526c48041c182e70da0e4bedd74e097
sid.stefanescu <sid.stefanescu@gmail.com>
2016-10-11 22:04:40 +0100

    2016-10-11's Linux workout

---
+++ contributions/sid.stefanescu/Linux/Terminals & Shells/2016-10-11.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Clear the terminal instantly
+Terminal incognito mode
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
