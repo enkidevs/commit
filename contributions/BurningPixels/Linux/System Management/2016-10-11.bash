8ed1c6c7fc214c355b45c61ba656883bbe1b5149
BurningPixels <jarodprivat@gmail.com>
2016-10-11 23:22:59 +0100

    2016-10-11's Linux workout

---
+++ contributions/BurningPixels/Linux/System Management/2016-10-11.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
