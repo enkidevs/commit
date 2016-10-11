dbf2b6dad47622f2f2b41468a544497121d2a1c6
cosmolejo <almego95@gmail.com>
2016-10-11 17:21:21 +0100

    2016-10-11's Linux workout

---
+++ contributions/cosmolejo/Linux/Terminals & Shells/2016-10-11.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Random password generator
+Breaking out of a terminal when `ssh` locks
