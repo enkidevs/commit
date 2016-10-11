3299daed55beb661da18d1abab31bb83998161a3
stevedien <lulzsecd@gmail.com>
2016-10-11 04:16:45 +0100

    2016-10-10's Java workout

---
+++ contributions/stevedien/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+Calling one constructor from another
+Constant notation
