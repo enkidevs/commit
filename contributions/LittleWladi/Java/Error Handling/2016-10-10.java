579619eadf8212bd60c2d2a752ad806cedebec90
LittleWladi <wladimirgramacho@gmail.com>
2016-10-11 02:35:42 +0100

    2016-10-10's Java workout

---
+++ contributions/LittleWladi/Java/Error Handling/2016-10-10.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Prevent `exceptions` while logging Data
+Throwing an `exception`
