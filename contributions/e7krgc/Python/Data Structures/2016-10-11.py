b5fe3a97c19c5e1c6109ceb2de18cdff4e5d12ec
e7krgc <enam93@gmail.com>
2016-10-11 07:16:01 +0100

    2016-10-11's Python workout

---
+++ contributions/e7krgc/Python/Data Structures/2016-10-11.py
@@ -0,0 +1,5 @@
+Following PEP 8 styling guideline.
+A simple way to select a random item from a `list/tuple` data stucture
+Get the most of `int`s
+Looping techniques
+Binary record packing with `bytearray`
