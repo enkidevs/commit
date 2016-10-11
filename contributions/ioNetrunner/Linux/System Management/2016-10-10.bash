93ed8506ec661e296c9e5f244463bfb63b0bd993
ioNetrunner <bradyrainey@bradydoingstuff.com>
2016-10-11 01:16:21 +0100

    2016-10-10's Linux workout

---
+++ contributions/ioNetrunner/Linux/System Management/2016-10-10.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Check the HDD with `badblocks`
+Basic service management with `systemd`
