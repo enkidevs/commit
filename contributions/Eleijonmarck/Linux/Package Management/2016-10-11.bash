d001dc01db91a1a1d35c0f83e845775a41627045
Eleijonmarck <eric.leijonmarck@gmail.com>
2016-10-11 18:53:15 +0100

    2016-10-11's Linux workout

---
+++ contributions/Eleijonmarck/Linux/Package Management/2016-10-11.bash
@@ -0,0 +1,5 @@
+Removing PPAs
+Prevent updating a specific package in Debian systems
+Get `apt` to use a mirror / faster mirror
+Using `Tasksel` for software installation
+Solving `BADSIG GPG` errors
