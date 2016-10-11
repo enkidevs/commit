d996da0d72c3459c7d3b3e403a1460a0a5f3d67e
sylcha <chambon.sylvain@gmail.com>
2016-10-11 15:33:26 +0100

    2016-10-11's Java workout

---
+++ contributions/sylcha/Java/Error Handling/2016-10-11.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using exceptions in Java
+Prevent `exceptions` while logging Data
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
