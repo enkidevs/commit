7428a1d299961ded236c3e91c80b2819750b9dcc
Evidica <evidica@gmail.com>
2016-10-11 03:07:44 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/Evidica/JavaScript/Core/2016-10-10.js
@@ -0,0 +1,5 @@
+Data type comparison in `switch` statements
+NaN === NaN?
+Extra function arguments are undefined by default
+Prevent background image flicker in **IE**
+Detect **DO NOT TRACK** status
