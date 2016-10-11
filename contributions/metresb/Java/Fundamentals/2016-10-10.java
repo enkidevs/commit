b4928f0cddaa9d6af4b7c68915fe3efe54b723b4
metresb <metresb@gmail.com>
2016-10-11 05:58:09 +0100

    2016-10-10's Java workout

---
+++ contributions/metresb/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
+Using an interface as a parameter
