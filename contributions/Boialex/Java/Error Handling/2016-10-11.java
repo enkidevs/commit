6d51f20fdc6670b968d13fb3b24e105d95778c86
Boialex <xdr007@gmail.com>
2016-10-11 18:28:18 +0100

    2016-10-11's Java workout

---
+++ contributions/Boialex/Java/Error Handling/2016-10-11.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Using exceptions in Java
+Most common reason behind **stack overflow** error
+Throwing an `exception`
