b5755d816705a2461eb1ba20a1c29a622fbf1d23
Mattle <mcallier@gmail.com>
2016-10-11 17:39:52 +0100

    2016-10-11's Java workout

---
+++ contributions/Mattle/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Using `Printstream.format` to format a string before printing
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
