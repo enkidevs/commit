aea6481bd9c6e1ea2f9eb8aae0e800c92fe8dfb9
dragnipur <ppj.vandorst@gmail.com>
2016-10-11 20:24:36 +0100

    2016-10-11's JavaScript workout

---
+++ contributions/dragnipur/JavaScript/Functions/2016-10-11.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Function call method
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
