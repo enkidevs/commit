66dc45dd71b65812078960b1bd777b2653147df7
kill129 <xxalon1996xx@gmail.com>
2016-10-11 07:22:09 +0100

    2016-10-11's Java workout

---
+++ contributions/kill129/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Connecting to FTP using Java
+Using bounded type parameters in generic methods
+Always specify type parameter
+Do not return *references* to private *mutable* class members
