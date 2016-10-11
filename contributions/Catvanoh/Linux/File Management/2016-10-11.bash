610ef9afccaf667999e91a1820640e64376f6d75
Catvanoh <pavelpie@gmail.com>
2016-10-11 08:54:29 +0100

    2016-10-11's Linux workout

---
+++ contributions/Catvanoh/Linux/File Management/2016-10-11.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Converting between Unix and Windows text files
+Monitor the progress of data through a pipe with `pv`
+Delete commands aliases
