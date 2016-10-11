fcef0ee13fc7252175c2b5ec051737de8f8dc874
BK874 <bjk69@pitt.edu>
2016-10-11 13:58:39 +0100

    2016-10-11's Java workout

---
+++ contributions/BK874/Java/Data Types/2016-10-11.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
+Do not attempt comparisons with NaN
