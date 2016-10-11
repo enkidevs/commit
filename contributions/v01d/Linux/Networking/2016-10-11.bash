4f8ea001795d3059c899f65d4e3065ba4314e510
v01d <nantog@gmail.com>
2016-10-11 07:49:02 +0100

    2016-10-11's Linux workout

---
+++ contributions/v01d/Linux/Networking/2016-10-11.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+How to clear `iptables` rules
+Useful ssh client optimizations 
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
