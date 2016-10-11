44a81c2ff7c65b5a153431da508c75d4b1f85333
Sivan1525 <siva20078@gmail.com>
2016-10-11 14:56:44 +0100

    2016-10-10's Linux workout

---
+++ contributions/Sivan1525/Linux/File Management/2016-10-10.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Delete commands aliases
+Easy way of sharing files
