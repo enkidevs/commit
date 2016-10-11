8c168487a0ea16c6b40bb7490f48c8661bcad630
HiddenHlF <manuxhlf@gmail.com>
2016-10-11 11:51:25 +0100

    2016-10-11's Java workout

---
+++ contributions/HiddenHlF/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
