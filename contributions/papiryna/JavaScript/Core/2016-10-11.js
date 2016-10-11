23dd1945c26d202209d9f182bc1eefb20f0f1110
papiryna <papiryna@gmail.com>
2016-10-11 08:00:17 +0100

    2016-10-11's JavaScript workout

---
+++ contributions/papiryna/JavaScript/Core/2016-10-11.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Performing a function at timed intervals
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+Detect **DO NOT TRACK** status
