62d2fbfefbdea97dd420648dabc971d10b19a8ca
xX_NeverDark_Xx <thermegil@orange.fr>
2016-10-11 20:40:40 +0100

    2016-10-11's Linux workout

---
+++ contributions/xX_NeverDark_Xx/Linux/File Management/2016-10-11.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Working with `.gz` files
+Easy way of sharing files
