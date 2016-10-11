7e41ad1c18f5be2d3e7bfb90b2bcc14a39135d75
vlad.bezden <vladbezden@yahoo.com>
2016-10-11 10:35:38 +0100

    2016-10-11's JavaScript workout

---
+++ contributions/vlad.bezden/JavaScript/Core/2016-10-11.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Apply `map` function to array items
+Scope and strict mode
+Extra function arguments are undefined by default
+Prevent background image flicker in **IE**
