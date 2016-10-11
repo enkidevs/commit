3f6e2adb84da52a80dc80199ac77456068fdf7e6
Scrumplex <sefa6727@gmail.com>
2016-10-11 15:30:21 +0100

    2016-10-11's Linux workout

---
+++ contributions/Scrumplex/Linux/System Management/2016-10-11.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Common log files
+Incorrect time on dual boot systems
