20815e5c46daa7b0b8bd602c29650b9c0011937c
miky90 <miky9090@gmail.com>
2016-10-11 21:45:04 +0100

    2016-10-11's JavaScript workout

---
+++ contributions/miky90/JavaScript/Node/2016-10-11.js
@@ -0,0 +1,5 @@
+Http Server in **Node**
+Node Inspector
+Use conventional property names for error handling in Node.js
+Automatic compilation for Node with **Nodemon** 
+Manage child processes in **Node**
