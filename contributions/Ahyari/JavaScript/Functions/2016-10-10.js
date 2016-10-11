79f05cf3e1f4b2e9ddc60e468108d1933cda513e
Ahyari <ahmad.alhyari@gmail.com>
2016-10-11 06:44:46 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/Ahyari/JavaScript/Functions/2016-10-10.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
