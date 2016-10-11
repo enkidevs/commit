e80a05908fb7b1a99c76612bf6cd9c452de81271
mahoalax <marie.laxenaire@webedia-group.com>
2016-10-11 13:14:21 +0100

    2016-10-11's Linux workout

---
+++ contributions/mahoalax/Linux/Terminals & Shells/2016-10-11.bash
@@ -0,0 +1,7 @@
+Repeating commands with `watch`
+Find any Unix / Linux command
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
