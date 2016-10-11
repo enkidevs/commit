0f7974aa872a28aa61a67449c20d0ff1f0199356
kubik <lkubic@poczta.onet.pl>
2016-10-11 07:12:41 +0100

    2016-10-11's Java workout

---
+++ contributions/kubik/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Ternary operator
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
