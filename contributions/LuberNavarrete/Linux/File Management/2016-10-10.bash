a8201d2203601d72a3b3640d9750f439d98500df
LuberNavarrete <navarreteluber@gmail.com>
2016-10-11 03:17:12 +0100

    2016-10-10's Linux workout

---
+++ contributions/LuberNavarrete/Linux/File Management/2016-10-10.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Extracting `tar` files to a specific directory
+Deleting non-empty directories
+Working with `.gz` files
+Find the unknown process preventing deleting of files
