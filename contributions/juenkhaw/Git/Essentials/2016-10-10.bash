a1cd85499073863a6cb6b303ea6f147ead56dcf5
juenkhaw <khawtattjuen1997@gmail.com>
2016-10-11 09:43:31 +0100

    2016-10-10's Git workout

---
+++ contributions/juenkhaw/Git/Essentials/2016-10-10.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+How to set your username and email
+Check the status of your files
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
