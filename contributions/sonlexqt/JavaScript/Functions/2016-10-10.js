3a2067eb6cb841e706f6f3baca700453750670f6
sonlexqt <sonlexqt@gmail.com>
2016-10-11 09:16:13 +0100

    2016-10-10's JavaScript workout

---
+++ contributions/sonlexqt/JavaScript/Functions/2016-10-10.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+IIFE pattern
+Method chaining
