472209e9712da0f9562c2797d054bf6389de91fc
3DEsprit <vilesyn@gmail.com>
2016-10-11 18:21:50 +0100

    2016-10-11's JavaScript workout

---
+++ contributions/3DEsprit/JavaScript/Express/2016-10-11.js
@@ -0,0 +1,5 @@
+`response` object additional properties in **Express**
+Easy way to deliver **HTML** pages with **Express**
+Using **Express 4.0** `router`
+`cookie-session` in **Express**
+Log with `morgan` in **Express**
