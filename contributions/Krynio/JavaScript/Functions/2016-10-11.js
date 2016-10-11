a99c0977664dc0b6bf33775b1e05bdaadd1db435
Krynio <krynio7@gmail.com>
2016-10-11 15:39:09 +0100

    2016-10-11's JavaScript workout

---
+++ contributions/Krynio/JavaScript/Functions/2016-10-11.js
@@ -0,0 +1,4 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Functions can be declared after use
+Changing a functions context with `fn.call(object)`
