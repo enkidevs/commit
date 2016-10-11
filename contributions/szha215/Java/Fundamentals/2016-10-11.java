8f20557581140cd78f5f506834889d652b68a12f
szha215 <szha215@aucklanduni.ac.nz>
2016-10-11 23:18:52 +0100

    2016-10-11's Java workout

---
+++ contributions/szha215/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Avoid using `static` variables
+Using an interface as a parameter
