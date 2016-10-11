e0dcea38151be9e6053e7fec385fd05a3e4f7d8e
lachlan-eagling <lochie.eagling@gmail.com>
2016-10-11 21:02:16 +0100

    2016-10-11's Java workout

---
+++ contributions/lachlan-eagling/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+How range operations work
