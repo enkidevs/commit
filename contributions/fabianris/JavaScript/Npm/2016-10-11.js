cdd2a19301029f3c67a114b2e546eda7a5ffe7cc
fabianris <fabianrislundblad@gmail.com>
2016-10-11 12:12:08 +0100

    2016-10-11's JavaScript workout

---
+++ contributions/fabianris/JavaScript/Npm/2016-10-11.js
@@ -0,0 +1,5 @@
+Deprecate npm packages
+Niceties for your npm package
+npm can handle multiple versions of a dependency
+List binaries for scripting in npm
+Manage local node modules with `npm link`
