b52901167485b8e87212cee01cbb67187d2599ee
BooM <tararuj.michal@gmail.com>
2016-10-11 06:33:56 +0100

    2016-10-11's Java workout

---
+++ contributions/BooM/Java/I/O/2016-10-11.java
@@ -0,0 +1,5 @@
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
