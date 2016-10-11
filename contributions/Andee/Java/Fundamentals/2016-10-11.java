9e0bb13deac98abd332136ebe1a5d974fbd54d01
Andee <andytruong94@gmail.com>
2016-10-11 10:38:25 +0100

    2016-10-11's Java workout

---
+++ contributions/Andee/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
