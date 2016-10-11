1f862f898f22aab1d6c1fdbf5addad096de8d2e7
padawan4330 <padawan4330@gmail.com>
2016-10-11 17:13:02 +0100

    2016-10-11's Java workout

---
+++ contributions/padawan4330/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
