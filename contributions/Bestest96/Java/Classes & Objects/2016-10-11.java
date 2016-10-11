7ec23a0d644bb3623269998da4e312aa38cdee20
Bestest96 <jaluki@vp.pl>
2016-10-11 10:09:45 +0100

    2016-10-11's Java workout

---
+++ contributions/Bestest96/Java/Classes & Objects/2016-10-11.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Using `static` fields
+How and when to use `WeakHashMap` class
