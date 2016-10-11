720615c9dd4ff7e0e7e243c81b192cd9f8a42c6f
Avabin <andrzej.piotrowski.76@gmail.com>
2016-10-11 09:28:20 +0100

    2016-10-11's Java workout

---
+++ contributions/Avabin/Java/I/O/2016-10-11.java
@@ -0,0 +1,5 @@
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Retrieving file store attributes
+Deleting files and directories
