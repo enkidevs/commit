e5ea60b2982a141e3908df4151b99061824f5576
boggie <sukmanov.b@gmail.com>
2016-10-11 20:34:22 +0100

    2016-10-11's Java workout

---
+++ contributions/boggie/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: setting a file's owner
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
