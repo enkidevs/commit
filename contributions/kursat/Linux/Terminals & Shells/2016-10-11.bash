7d4874fdf4822ff7b258ed4962b9c1919c897dd8
kursat <kursat.yigitoglu@gmail.com>
2016-10-11 16:26:37 +0100

    2016-10-11's Linux workout

---
+++ contributions/kursat/Linux/Terminals & Shells/2016-10-11.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+The Basics of Environment Variables
