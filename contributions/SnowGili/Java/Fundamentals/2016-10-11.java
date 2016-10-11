4e7c2dbad79ad002ae45f05900b912e8b7403621
SnowGili <acceleratorsnow@gmail.com>
2016-10-11 12:21:52 +0100

    2016-10-11's Java workout

---
+++ contributions/SnowGili/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
