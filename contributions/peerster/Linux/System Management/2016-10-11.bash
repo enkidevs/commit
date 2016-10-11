74513086bc912f6aa2b65e30dad980eeb5dbe2c6
peerster <paer.berge@gmail.com>
2016-10-11 06:51:19 +0100

    2016-10-11's Linux workout

---
+++ contributions/peerster/Linux/System Management/2016-10-11.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
