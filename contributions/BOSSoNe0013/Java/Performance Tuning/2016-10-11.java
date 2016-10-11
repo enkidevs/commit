695a8400d5d17aef24bddc0b2ee46235e6fcce92
BOSSoNe0013 <bossone0013@gmail.com>
2016-10-11 08:04:48 +0100

    2016-10-11's Java workout

---
+++ contributions/BOSSoNe0013/Java/Performance Tuning/2016-10-11.java
@@ -0,0 +1,5 @@
+Bitwise and Bit Shift operations in Java
+Use lazy initialization to defer creating an object until it's used
+Java TreeMap and TreeSet overview
+Java HashSet and HashMap overview
+Using `StringBuilder` class
