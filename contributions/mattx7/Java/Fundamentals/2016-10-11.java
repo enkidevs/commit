44168d13f8098bf92e6f08800d7d9118d237235b
mattx7 <lion.pierau@gmail.com>
2016-10-11 10:06:37 +0100

    2016-10-11's Java workout

---
+++ contributions/mattx7/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Supply `toString()` in all classes
+Using an interface as a parameter
