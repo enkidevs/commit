435144b16fff39871d445fb842ad814b0aafabe5
g3mini <mcjanz@gmail.com>
2016-10-11 07:19:51 +0100

    2016-10-11's Java workout

---
+++ contributions/g3mini/Java/I/O/2016-10-11.java
@@ -0,0 +1,5 @@
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
+Deleting files and directories
