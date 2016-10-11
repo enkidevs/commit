2bc33d4687b8c8c2a722f5144227b5adffca7887
FlorianGainza <florian.gainza.prof@gmail.com>
2016-10-11 07:11:56 +0100

    2016-10-11's Linux workout

---
+++ contributions/FlorianGainza/Linux/System Management/2016-10-11.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Using `dd` To Create Disk Images
