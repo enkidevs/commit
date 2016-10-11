8299a9d2f6d097ba1ced179bf6e1d161962b8fa0
Narckcomler <218451@student.pwr.edu.pl>
2016-10-11 08:18:48 +0100

    2016-10-11's Linux workout

---
+++ contributions/Narckcomler/Linux/File Management/2016-10-11.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
