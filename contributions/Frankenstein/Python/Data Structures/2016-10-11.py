e64a8e009cc47ecaf17dc4f38f3eafa13095aa38
Frankenstein <lars.ch.franke@gmail.com>
2016-10-11 07:53:35 +0100

    2016-10-11's Python workout

---
+++ contributions/Frankenstein/Python/Data Structures/2016-10-11.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`bytes` type
+Special attributes of objects and classes
+`weakref` proxies
+Binary record packing with `bytearray`
