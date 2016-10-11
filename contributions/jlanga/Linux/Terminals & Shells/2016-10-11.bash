84cd687cb9bc7d263cefb5f36685998a38e352b5
jlanga <jorge.langa.arranz@gmail.com>
2016-10-11 14:29:34 +0100

    2016-10-11's Linux workout

---
+++ contributions/jlanga/Linux/Terminals & Shells/2016-10-11.bash
@@ -0,0 +1,7 @@
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
+The Basics of Environment Variables
