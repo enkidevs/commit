d0ee501d2541c57f4db546dfa6d7b4b3a66fceb7
Jinxter <propwe@gmail.com>
2016-10-11 10:18:54 +0100

    2016-10-11's JavaScript workout

---
+++ contributions/Jinxter/JavaScript/Functions/2016-10-11.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Hoisting
+Function declarations vs function expressions
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
