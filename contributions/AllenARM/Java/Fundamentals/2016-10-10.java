1e28cfe626d100ad7e75f33c0e85807393cabbd5
AllenARM <allenwem@gmail.com>
2016-10-11 07:17:14 +0100

    2016-10-10's Java workout

---
+++ contributions/AllenARM/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
