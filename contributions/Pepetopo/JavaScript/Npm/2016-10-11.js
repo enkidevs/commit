5aacfd4659d8172f2e6f1b65eeedf9f1ac097b72
Pepetopo <elias.ylanen@gmail.com>
2016-10-11 13:22:24 +0100

    2016-10-11's JavaScript workout

---
+++ contributions/Pepetopo/JavaScript/Npm/2016-10-11.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+npm can handle multiple versions of a dependency
+`npm` verifies `sha1` hashes of packages
+`optionalDependencies` in npm
+Reduce package duplication 
