783155696c7dde1c30dc94eb33de5b93c6aabee7
Mobyh <luisgaza95sant@gmail.com>
2016-10-11 03:11:14 +0100

    2016-10-10's Java workout

---
+++ contributions/Mobyh/Java/Fundamentals/2016-10-10.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Inheriting a constructor from a superclass
+Metadata: setting a file's owner
+Using `synchronized` statements
+Supply `toString()` in all classes
