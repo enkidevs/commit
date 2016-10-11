95ddea266ecc47fc94d337ea6647be8f06ad1af2
christopheradr <christopher.diaz.riv@gmail.com>
2016-10-11 14:03:35 +0100

    2016-10-11's Linux workout

---
+++ contributions/christopheradr/Linux/File Management/2016-10-11.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
