dc8d91f006211f87a14cbf9d7fbce22833b0bb55
helldrum <jonathanchardon@hotmail.fr>
2016-10-12 13:20:16 +0100

    2016-10-12's Linux workout

---
+++ contributions/helldrum/Linux/Networking/2016-10-12.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Multiple IP Address with aliasing 
+Get real network statistics with `slurm`
+Sending emails from terminal / scripts
+Staying connected remotely via SSH without an interactive shell
