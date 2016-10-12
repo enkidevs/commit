b6be51a5cf7b8d6cc57c272562f73f3a19dad3e5
w3bMaster <at.w3bmaster@gmail.com>
2016-10-12 16:09:28 +0100

    2016-10-12's Linux workout

---
+++ contributions/w3bMaster/Linux/Networking/2016-10-12.bash
@@ -0,0 +1,5 @@
+How to clear `iptables` rules
+Disable `IPv6`
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
