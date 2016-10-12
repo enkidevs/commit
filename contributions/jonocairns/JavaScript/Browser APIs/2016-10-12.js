41fd072633ef30ccafb5ac43140a0ce346b1798e
jonocairns <jonocairns@gmail.com>
2016-10-12 19:13:45 +0100

    2016-10-12's JavaScript workout

---
+++ contributions/jonocairns/JavaScript/Browser APIs/2016-10-12.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Use hosted scripts to increase performance
+Warn user if **Back** button is pressed
+ProgressEvent
+MediaDevices.getUserMedia()
