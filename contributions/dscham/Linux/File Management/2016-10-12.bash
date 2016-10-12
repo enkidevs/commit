4f54b942e1057291d5d066472fd7baab0c49e9c0
dscham <dominik.schramm@chello.at>
2016-10-12 13:52:19 +0100

    2016-10-12's Linux workout

---
+++ contributions/dscham/Linux/File Management/2016-10-12.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
