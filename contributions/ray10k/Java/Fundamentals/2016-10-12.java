be2a23c114751832913d05d1c330168fdbb96a0a
ray10k <w.j.van.den.heuvel@xmsnet.nl>
2016-10-12 17:45:58 +0100

    2016-10-12's Java workout

---
+++ contributions/ray10k/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
