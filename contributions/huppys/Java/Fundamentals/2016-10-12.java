f229df75b1843fe12b6aef4b5060f69c8b58b6e2
huppys <huppys55@gmail.com>
2016-10-12 18:57:49 +0100

    2016-10-12's Java workout

---
+++ contributions/huppys/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The trade-off between short and descriptive names
+Using Inheritance to reduce code repetition
+Using `synchronized` statements
+`Autoboxing` and `Unboxing`
