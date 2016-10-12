52c242d3be11c887f2f2852540528c6791472171
tomoyuen <wisyue@yahoo.com>
2016-10-12 01:39:56 +0100

    2016-10-11's JavaScript workout

---
+++ contributions/tomoyuen/JavaScript/Node/2016-10-11.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+The built-in Node debugger
+`process.nextTick()` callback pattern
+`exports` vs. `module.exports` in **Node**
+`try-catch` only for **sync** code
