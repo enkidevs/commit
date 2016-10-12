9a13c5a91b8f2fcc5a609147f7757c4fb1e1099f
CycleOfTheAbsurd <alex7up42@gmail.com>
2016-10-12 19:59:03 +0100

    2016-10-12's Java workout

---
+++ contributions/CycleOfTheAbsurd/Java/I/O/2016-10-12.java
@@ -0,0 +1,5 @@
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
+The `Console` class
