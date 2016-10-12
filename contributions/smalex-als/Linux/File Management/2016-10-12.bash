827ac81f97f9de2c243ec56af0a8890319bb442f
smalex-als <smalex69@gmail.com>
2016-10-12 12:11:55 +0100

    2016-10-12's Linux workout

---
+++ contributions/smalex-als/Linux/File Management/2016-10-12.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
