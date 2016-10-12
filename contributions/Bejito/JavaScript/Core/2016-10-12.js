4d7b0a73e8bf7b60e1edf04e528f5ef06f6e5e16
Bejito <gillesiweins@gmail.com>
2016-10-12 16:45:26 +0100

    2016-10-12's JavaScript workout

---
+++ contributions/Bejito/JavaScript/Core/2016-10-12.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+The distinction between `==` and `===`
+Filtering items out of an array
+Prevent background image flicker in **IE**
+Inaccuracy of binary floating-point format
