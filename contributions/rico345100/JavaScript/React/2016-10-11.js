742bfab1c18341dc50a44667b56dd75e819b82e1
rico345100 <rico345100@gmail.com>
2016-10-12 04:02:58 +0100

    2016-10-11's JavaScript workout

---
+++ contributions/rico345100/JavaScript/React/2016-10-11.js
@@ -0,0 +1,6 @@
+Type of the Children props
+componentWillReceiveProps Not Triggered After Mounting
+
+Load Initial Data via AJAX
+Use **React** with other libraries
+Immutability helpers in **React**
