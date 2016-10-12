057918f0d62248a52e049e58b368a954db416423
archligt <frodo.archligt@gmail.com>
2016-10-12 13:01:36 +0100

    2016-10-12's Linux workout

---
+++ contributions/archligt/Linux/File Management/2016-10-12.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
+Find the unknown process preventing deleting of files
