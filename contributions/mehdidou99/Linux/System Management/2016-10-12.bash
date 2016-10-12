ca0b965776bda3f94f1c64b198acfacfb70de5d2
mehdidou99 <mehdibenharrats@yahoo.fr>
2016-10-12 16:25:25 +0100

    2016-10-12's Linux workout

---
+++ contributions/mehdidou99/Linux/System Management/2016-10-12.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
