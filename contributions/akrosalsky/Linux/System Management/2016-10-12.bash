0070be23335632e1333fb06b490d9063b476a842
akrosalsky <akrosalsky@gmail.com>
2016-10-12 15:14:28 +0100

    2016-10-12's Linux workout

---
+++ contributions/akrosalsky/Linux/System Management/2016-10-12.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Finding Open Files With `lsof`
