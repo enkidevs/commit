267d32b730f79548a0c6cdd53c985543dcbc57db
factoryEquipment <cs.serg@gmail.com>
2016-10-12 02:42:58 +0100

    2016-10-11's Java workout

---
+++ contributions/factoryEquipment/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
