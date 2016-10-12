4560c67732d6e5ce2e7ec1ffcd99b53683ee4169
Blackmoor <ccalero.itcr@gmail.com>
2016-10-12 18:47:49 +0100

    2016-10-12's Java workout

---
+++ contributions/Blackmoor/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Always specify type parameter
+Using an interface as a parameter
