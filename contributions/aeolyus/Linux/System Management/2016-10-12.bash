eefd78064a4f62d6fe311d97a2f0bf6cdc60d9d4
aeolyus <richardhuang.huang@gmail.com>
2016-10-13 00:34:12 +0100

    2016-10-12's Linux workout

---
+++ contributions/aeolyus/Linux/System Management/2016-10-12.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
