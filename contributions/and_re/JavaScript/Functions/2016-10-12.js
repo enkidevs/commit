ec2a383d54f46572dc7a7d541942eb8d3433ecce
and_re <andre2go@gmail.com>
2016-10-12 07:08:02 +0100

    2016-10-12's JavaScript workout

---
+++ contributions/and_re/JavaScript/Functions/2016-10-12.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Check if a document is done loading
