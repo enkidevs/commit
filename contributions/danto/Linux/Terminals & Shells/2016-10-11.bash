e156cef6f1ce61f445fb7f250c6cbea36ea67cf0
danto <danielj@outlook.de>
2016-10-12 07:29:04 +0100

    2016-10-11's Linux workout

---
+++ contributions/danto/Linux/Terminals & Shells/2016-10-11.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
