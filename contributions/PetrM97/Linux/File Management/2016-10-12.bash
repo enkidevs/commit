8a02ea47e73ee8e6882548baa671380a23fa33ea
PetrM97 <petrmichalik1@seznam.cz>
2016-10-12 12:44:38 +0100

    2016-10-12's Linux workout

---
+++ contributions/PetrM97/Linux/File Management/2016-10-12.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
+Easy way of sharing files
