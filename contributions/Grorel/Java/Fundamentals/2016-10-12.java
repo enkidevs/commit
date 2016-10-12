e652c159dc70ab05efd155e8b3887a7bd1a76f46
Grorel <grorel.kazui@gmail.com>
2016-10-12 07:34:37 +0100

    2016-10-12's Java workout

---
+++ contributions/Grorel/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,4 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
