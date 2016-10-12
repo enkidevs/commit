1430e176d1394acc7bd3dbb1b472f5146cf33f1c
Cobradabest <cobradabest@aol.com>
2016-10-12 18:09:32 +0100

    2016-10-12's Java workout

---
+++ contributions/Cobradabest/Java/I/O/2016-10-12.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Create system file paths using the `Path` class
+Using buffered streams
+Deleting files and directories
