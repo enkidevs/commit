814be03cee3bd221ecf2b9c297122e42c3b0b18c
BaLanZe <balanzeneto@gmail.com>
2016-10-12 13:04:05 +0100

    2016-10-12's JavaScript workout

---
+++ contributions/BaLanZe/JavaScript/React/2016-10-12.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Functional Stateless Components in React
+componentWillReceiveProps Not Triggered After Mounting
+
+`ref` callback attribute
+Validate for required props
