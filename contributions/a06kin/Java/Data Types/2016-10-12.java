0321787af7c1b68a562a1f8034aed3264d71ee09
a06kin <a06kin@gmail.com>
2016-10-12 06:57:12 +0100

    2016-10-12's Java workout

---
+++ contributions/a06kin/Java/Data Types/2016-10-12.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Use `Arrays.asList()` to initialise lists
+`StringBuffer` vs `StringBuilder`
