b0cccfc2fff4fc88451cc643a6127730803cacd8
havanagrawal <havanagrawal@gmail.com>
2016-10-12 08:56:56 +0100

    2016-10-11's Git workout

---
+++ contributions/havanagrawal/Git/Essentials/2016-10-11.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+How to unmodify a modified file
+Using tags for version control
+Use `short` status to make output more compact
+Remote repositories: fetching and pushing
