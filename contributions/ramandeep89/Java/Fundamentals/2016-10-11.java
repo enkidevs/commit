988d5e2348e83878ef763737f68c56eca086de59
ramandeep89 <ramandeep89@gmail.com>
2016-10-12 02:41:55 +0100

    2016-10-11's Java workout

---
+++ contributions/ramandeep89/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
