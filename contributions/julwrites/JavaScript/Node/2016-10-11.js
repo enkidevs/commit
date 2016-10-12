f5a71dda5d2056490510e231a173029e0f17e51a
julwrites <jump4jupiter@gmail.com>
2016-10-12 15:32:13 +0100

    2016-10-11's JavaScript workout

---
+++ contributions/julwrites/JavaScript/Node/2016-10-11.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+Manage child processes in **Node**
+Middleware/pipeline design pattern
