0ccecfe7540d10b76497a49b824ee0e5ff70d1db
Bestest96 <jaluki@vp.pl>
2016-10-12 08:46:37 +0100

    2016-10-12's Java workout

---
+++ contributions/Bestest96/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
+Using an interface as a parameter
