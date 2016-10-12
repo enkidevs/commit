5164013e69c3828c9d075bd5256ef86140e40003
Krynio <krynio7@gmail.com>
2016-10-12 16:07:28 +0100

    2016-10-12's JavaScript workout

---
+++ contributions/Krynio/JavaScript/Node/2016-10-12.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Asynchronous File Write/Read in Node.js
+`uncaughtException` listener in Node.js
+Automatic compilation for Node with **Nodemon** 
+Manage child processes in **Node**
