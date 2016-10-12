0240d35861aec90cb13247bd00eba22bcadd5b34
sjaca10 <sjaca10@gmail.com>
2016-10-12 13:04:50 +0100

    2016-10-12's Java workout

---
+++ contributions/sjaca10/Java/Error Handling/2016-10-12.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Throwing an `exception`
+Careful Numeric Data Conversions
