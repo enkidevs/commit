61365170e6b51df0cc559ad5ce6286b4016dd6b8
Maiatoday <maiatoday@gmail.com>
2016-10-12 08:42:13 +0100

    2016-10-12's Linux workout

---
+++ contributions/Maiatoday/Linux/System Management/2016-10-12.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
