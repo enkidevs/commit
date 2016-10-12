f90d0ce818731d2a46144bf8e20f43a8d9434e8d
g3mini <mcjanz@gmail.com>
2016-10-12 12:11:09 +0100

    2016-10-12's Java workout

---
+++ contributions/g3mini/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Calling one constructor from another
+Measuring time
