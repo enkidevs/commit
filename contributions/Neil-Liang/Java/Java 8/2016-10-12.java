66860d1e15bdd61e581c4ed0a0a097cbcbfd5212
Neil-Liang <xl712@nyu.edu>
2016-10-12 17:44:12 +0100

    2016-10-12's Java workout

---
+++ contributions/Neil-Liang/Java/Java 8/2016-10-12.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Using `Arrays.parallelSort` 
+How to check for static dependencies of applications and libraries from the command line.
+Use `Optional(s)` type in order to avoid `NullPointerException(s)`
+Avoid numerical overflows with *Java8* `exact` methods.
