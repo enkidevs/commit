9cf167f06d6d187961432af961e512a7b4b93fd8
yaffet <uniac@web.de>
2016-10-12 21:16:08 +0100

    2016-10-12's Java workout

---
+++ contributions/yaffet/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
