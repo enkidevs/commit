99125fee45833b99c3fa599a48a2be748cb471d3
ChristianGaertner <chrisdagardner@gmail.com>
2016-10-12 12:11:34 +0100

    2016-10-12's JavaScript workout

---
+++ contributions/ChristianGaertner/JavaScript/Core/2016-10-12.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Hoisting applies only to variable declarations, not initializations
+Scope and strict mode
+Prevent background image flicker in **IE**
+Detect **DO NOT TRACK** status
