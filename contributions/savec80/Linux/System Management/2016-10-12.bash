ff1ce70042547bfe938e0cc00f69a041ea969af4
savec80 <artem.savenkov@gmail.com>
2016-10-12 22:41:22 +0100

    2016-10-12's Linux workout

---
+++ contributions/savec80/Linux/System Management/2016-10-12.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
