3e0df8849e999375a8c26a6f9cb0fbd0d22a20c1
nelsonio <martin23haupt@gmail.com>
2016-10-12 10:04:45 +0100

    2016-10-12's JavaScript workout

---
+++ contributions/nelsonio/JavaScript/Functions/2016-10-12.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` using `call` and `apply` methods
+IIFE pattern
+Method chaining
+Check if a document is done loading
