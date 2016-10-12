3bb76fe3162d92f75ed0ce041ceaad116b409131
ZwoRmi <lemaire.lucas1@gmail.com>
2016-10-12 06:54:41 +0100

    2016-10-12's Java workout

---
+++ contributions/ZwoRmi/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
