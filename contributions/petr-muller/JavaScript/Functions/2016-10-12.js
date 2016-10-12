761187a844d6c9cdc5ad40b80237fd273a8c8fd5
petr-muller <afri@afri.cz>
2016-10-13 00:04:52 +0100

    2016-10-12's JavaScript workout

---
+++ contributions/petr-muller/JavaScript/Functions/2016-10-12.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Hoisting
+Get query/url variables
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
