bc5ffb5ae6e288bac79fde4060fc446442bf3c96
CellarD0-0r <alan.turing39@gmail.com>
2016-10-12 10:39:50 +0100

    2016-10-12's Java workout

---
+++ contributions/CellarD0-0r/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Use meaningful names
+Distinction between `public` and `private` methods
+Calling one constructor from another
+Limit Accessibility of `Fields`
