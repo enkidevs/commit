bdda4ee2032be03d02b8bbd6e621670b603b5c2d
jafidiesel <jafibravin@outlook.com>
2016-10-12 05:20:26 +0100

    2016-10-11's Java workout

---
+++ contributions/jafidiesel/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Measuring time
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
