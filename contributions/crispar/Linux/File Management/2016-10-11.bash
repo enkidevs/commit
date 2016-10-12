ea7abb1b8f6ad687cfbd3ac11e2d934f0560c8c5
crispar <jwcris.park@gmail.com>
2016-10-12 14:32:29 +0100

    2016-10-11's Linux workout

---
+++ contributions/crispar/Linux/File Management/2016-10-11.bash
@@ -0,0 +1,6 @@
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
