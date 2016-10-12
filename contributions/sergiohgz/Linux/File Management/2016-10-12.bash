38a4a7ecbdf9594b55bb4c910e14fce0bdbb6701
sergiohgz <sergioherreraguz@gmail.com>
2016-10-12 23:34:55 +0100

    2016-10-12's Linux workout

---
+++ contributions/sergiohgz/Linux/File Management/2016-10-12.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Easy way of sharing files
