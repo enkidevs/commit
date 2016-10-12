ce9a4858b6f2817ffb6709c12618921dcfdcadf4
Antonio-Smirnov <toxa.inc@mail.ru>
2016-10-12 19:22:33 +0100

    2016-10-12's Java workout

---
+++ contributions/Antonio-Smirnov/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
+Supply `toString()` in all classes
