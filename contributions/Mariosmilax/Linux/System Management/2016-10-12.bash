9d44eac2dd6bfeb9674c5356b44414346674af5a
Mariosmilax <elior.b@dynamicyield.com>
2016-10-12 17:09:07 +0100

    2016-10-12's Linux workout

---
+++ contributions/Mariosmilax/Linux/System Management/2016-10-12.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
