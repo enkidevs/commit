8889648c929d507db1e91ea501d047678f8a3b17
anema165 <justyna.skomra@gmail.com>
2016-10-12 08:22:03 +0100

    2016-10-12's JavaScript workout

---
+++ contributions/anema165/JavaScript/React/2016-10-12.js
@@ -0,0 +1,6 @@
+Props in getInitialState Is an        
+Anti-Pattern
+Use **React** with other libraries
+Custom `propType`'s to be required
+Default values for props
+Shortcut for transferring props
