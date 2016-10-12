da28b122e38aeb4f278aeb8efd7e30df533597b2
eduardopeixe <walthfish@yahoo.com>
2016-10-12 18:50:04 +0100

    2016-10-12's JavaScript workout

---
+++ contributions/eduardopeixe/JavaScript/Node/2016-10-12.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Custom Node REPL Server
+Http Server in **Node**
+Global Objects and Environment Variables in **Node**
+Use conventional property names for error handling in Node.js
