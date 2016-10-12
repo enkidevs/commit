be64d141de5382394ffa39449c9ec479610d6e5b
krishnavangala <saichaitanya6@gmail.com>
2016-10-12 09:07:21 +0100

    2016-10-12's Linux workout

---
+++ contributions/krishnavangala/Linux/System Management/2016-10-12.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Find the `MAC` address of all network interfaces
+
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
