de29b65fa407bd093701924b2e7ba8fef924e3a2
goto1134 <1134togo@gmail.com>
2016-10-12 05:43:54 +0100

    2016-10-12's Java workout

---
+++ contributions/goto1134/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
