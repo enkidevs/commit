fbd8423e968643f978a08852143343491e8b706c
vincentjeltsch <jeltsch.vincent@gmail.com>
2016-10-12 22:25:15 +0100

    2016-10-12's Linux workout

---
+++ contributions/vincentjeltsch/Linux/System Management/2016-10-12.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Find the `MAC` address of all network interfaces
+
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
