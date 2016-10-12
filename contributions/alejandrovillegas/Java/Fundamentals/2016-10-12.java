10926a5420305a31d6462971292acbea697c2f5c
alejandrovillegas <neopilatos@gmail.com>
2016-10-12 18:32:31 +0100

    2016-10-12's Java workout

---
+++ contributions/alejandrovillegas/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Using `Printstream.format` to format a string before printing
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
