7eda4b809e4800c03e7929e160c9e885db242e42
cronos2 <nacho.cronos2@gmail.com>
2016-10-12 20:09:43 +0100

    2016-10-12's Linux workout

---
+++ contributions/cronos2/Linux/Networking/2016-10-12.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Logging dropped firewall packets
+Useful ssh client optimizations 
+Limit the `wget` download rate
+Staying connected remotely via SSH without an interactive shell
