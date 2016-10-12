fd257c6d2aac85b428b1c3e6a9170559fa4af3fd
Trey133 <trey.waldrop1@student.ctuonline.edu>
2016-10-12 04:38:56 +0100

    2016-10-11's Linux workout

---
+++ contributions/Trey133/Linux/File Management/2016-10-11.bash
@@ -0,0 +1,5 @@
+Converting between Unix and Windows text files
+Using `touch` to alter files modification time
+Working with `.gz` files
+Find the unknown process preventing deleting of files
+Delete commands aliases
