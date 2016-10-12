973b4294c93a9c07314e094093824548a471edb8
dan339 <danielcampbell339@gmail.com>
2016-10-12 07:35:55 +0100

    2016-10-12's Java workout

---
+++ contributions/dan339/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Supply `toString()` in all classes
