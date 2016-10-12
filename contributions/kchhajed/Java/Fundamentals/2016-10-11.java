e3df15a931e11492d6d997eea0ed13536de1f348
kchhajed <kchhajed@gmail.com>
2016-10-12 19:30:38 +0100

    2016-10-11's Java workout

---
+++ contributions/kchhajed/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
