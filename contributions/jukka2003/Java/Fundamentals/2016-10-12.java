bf755d610a57d111541326977792f8f82c3450ef
jukka2003 <jukka2003@gmail.com>
2016-10-12 10:48:10 +0100

    2016-10-12's Java workout

---
+++ contributions/jukka2003/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Detect or prevent integer overflow
+Using an interface as a parameter
