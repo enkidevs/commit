0fb3c103a6c2f4944db2ac1d49d004b91fe930d5
Prominence <prominence96@gmail.com>
2016-10-12 07:36:33 +0100

    2016-10-12's Linux workout

---
+++ contributions/Prominence/Linux/Networking/2016-10-12.bash
@@ -0,0 +1,5 @@
+Debugging `ssh` client issues
+Proxifying `ssh` connections
+Get real network statistics with `slurm`
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
