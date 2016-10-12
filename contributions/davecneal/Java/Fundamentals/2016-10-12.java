5397006ae21ab98acfff04518351f1b3b5ab1143
davecneal <davecneal@gmail.com>
2016-10-12 20:38:05 +0100

    2016-10-12's Java workout

---
+++ contributions/davecneal/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
