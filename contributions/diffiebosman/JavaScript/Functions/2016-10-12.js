78b8046b6e1d4184ed95588dd579dffabe41a10f
diffiebosman <diffiebosman@gmail.com>
2016-10-12 12:12:59 +0100

    2016-10-12's JavaScript workout

---
+++ contributions/diffiebosman/JavaScript/Functions/2016-10-12.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Method chaining
+Changing a functions context with `fn.call(object)`
