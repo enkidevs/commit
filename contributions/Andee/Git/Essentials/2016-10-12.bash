a4909ab4d2a65e06d98d28fb90da74e37aef2854
Andee <andytruong94@gmail.com>
2016-10-12 13:40:29 +0100

    2016-10-12's Git workout

---
+++ contributions/Andee/Git/Essentials/2016-10-12.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+Adding a remote repository
+How to set your username and email
+Limiting log output by time
+Remote repositories: viewing, editing and deleting
