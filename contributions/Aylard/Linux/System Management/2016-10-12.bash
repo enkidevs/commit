97f04801b54f5b2c7780a0b23cc9e3470b294dee
Aylard <aylard7@gmail.com>
2016-10-12 21:41:58 +0100

    2016-10-12's Linux workout

---
+++ contributions/Aylard/Linux/System Management/2016-10-12.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
