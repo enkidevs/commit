b0f4cad882ffcd362cbb93c53f4c54b1257c8e30
andreyrusanov <rusanov.andrey.mail@gmail.com>
2016-10-12 17:15:23 +0100

    2016-10-12's Linux workout

---
+++ contributions/andreyrusanov/Linux/Networking/2016-10-12.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Useful ssh client optimizations 
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
