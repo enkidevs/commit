1168600f9f9d5bb7d3c5f556822e495e772ae1c6
TheInitializer <dkteresi@gmail.com>
2016-10-12 23:47:19 +0100

    2016-10-12's Linux workout

---
+++ contributions/TheInitializer/Linux/Pipeline Basics/2016-10-12.bash
@@ -0,0 +1,5 @@
+The Standard Streams: `stdin`, `stdout`, and `stderr`
+The Basics of The Pipe Operator `|`
+Input And Output Redirection With `<` and `>`
+The Basics of Command Substitution
+Appending Output With `>>`
