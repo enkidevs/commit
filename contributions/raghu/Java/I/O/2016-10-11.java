c38d8a86315426521a9654c90b2ba46c200ef280
raghu <raghu.9208@gmail.com>
2016-10-12 03:45:39 +0100

    2016-10-11's Java workout

---
+++ contributions/raghu/Java/I/O/2016-10-11.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+How to create directories in Java
+List contents of a directory with `newDirectoryStream`
+The `Console` class
