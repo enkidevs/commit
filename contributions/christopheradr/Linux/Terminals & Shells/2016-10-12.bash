8d99b97eba7ed4bd3a4e74a715c0f7618d2c603e
christopheradr <christopher.diaz.riv@gmail.com>
2016-10-12 16:12:30 +0100

    2016-10-12's Linux workout

---
+++ contributions/christopheradr/Linux/Terminals & Shells/2016-10-12.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
