a5832c18166bcca9d5962277f19549456d589111
liufeng <liufeng5460@163.com>
2016-10-12 02:40:30 +0100

    2016-10-11's Java workout

---
+++ contributions/liufeng/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use strings in a `switch` statement
+Using `Printstream.format` to format a string before printing
+Using `synchronized` statements
+Implementing an `interface`
