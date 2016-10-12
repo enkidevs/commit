89e017a49a612879f0ba04f4cd33db009b7e7b01
edonoxako <edonoxako@gmail.com>
2016-10-12 08:24:54 +0100

    2016-10-11's Java workout

---
+++ contributions/edonoxako/Java/Fundamentals/2016-10-11.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Supply `toString()` in all classes
