a13d9f3ba92cd1443b9532eb137520ba62620379
BooM <tararuj.michal@gmail.com>
2016-10-13 06:45:37 +0100

    2016-10-13's Java workout

---
+++ contributions/BooM/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Always specify type parameter
+Limit Accessibility of `Fields`
+Implementing an `interface`
