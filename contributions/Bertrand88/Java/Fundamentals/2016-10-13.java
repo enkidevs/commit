08be809e966d75c2e71807b031c69b46b69751b5
Bertrand88 <bertrand.noel.88@gmail.com>
2016-10-13 07:35:28 +0100

    2016-10-13's Java workout

---
+++ contributions/Bertrand88/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
