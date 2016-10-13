6c7a6c98ee221f6b218bc167184bcf21e78700c7
Gtosta96 <gabrieltosta3@gmail.com>
2016-10-14 00:48:29 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/Gtosta96/JavaScript/Core/2016-10-13.js
@@ -0,0 +1,5 @@
+Infinity
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Extra function arguments are undefined by default
+Detect **DO NOT TRACK** status
