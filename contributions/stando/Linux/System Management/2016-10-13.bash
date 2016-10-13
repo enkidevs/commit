39f49ac4c212bc1fd2ea7efabe1cd7403c88eca5
stando <stano.kudlac@gmail.com>
2016-10-13 23:07:01 +0100

    2016-10-13's Linux workout

---
+++ contributions/stando/Linux/System Management/2016-10-13.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
