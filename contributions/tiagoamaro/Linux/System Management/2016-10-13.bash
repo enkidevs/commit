a09e2e60d38b689f42c294fe662583d2c62995a9
tiagoamaro <tiagopadrela@gmail.com>
2016-10-13 07:49:08 +0100

    2016-10-13's Linux workout

---
+++ contributions/tiagoamaro/Linux/System Management/2016-10-13.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Basic service management with `systemd`
+Finding Open Files With `lsof`
