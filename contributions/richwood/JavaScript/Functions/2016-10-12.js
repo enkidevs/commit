7a5723f9d706b5015fd891bf30c3fa3f7b973ef1
richwood <richebois.benjamin@gmail.com>
2016-10-13 17:27:26 +0100

    2016-10-12's JavaScript workout

---
+++ contributions/richwood/JavaScript/Functions/2016-10-12.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+IIFE pattern
+Social sharing without widgets
