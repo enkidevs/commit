49d01217857751d610e16f39d5498b8584275618
k8307 <aknjnull@gmail.com>
2016-10-13 11:13:18 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/k8307/JavaScript/Core/2016-10-13.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Using the double tilde `~~` 
+Infix operators are left-associative
+Hoisting applies only to variable declarations, not initializations
+JavaScript compilation
