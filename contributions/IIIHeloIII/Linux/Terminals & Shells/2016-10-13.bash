6ae9a4d3e220050379248b3ce2fbf2d3c4f192ac
IIIHeloIII <kristiandahl@gmail.com>
2016-10-13 13:15:31 +0100

    2016-10-13's Linux workout

---
+++ contributions/IIIHeloIII/Linux/Terminals & Shells/2016-10-13.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
