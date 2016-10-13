3710db4a532c604e6584351ba8139e05e034cdcc
BrightOne <filalex77@gmail.com>
2016-10-13 14:27:24 +0100

    2016-10-13's Java workout

---
+++ contributions/BrightOne/Java/Java 8/2016-10-13.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Java 8 Lambda expressions
+Control OS `Process`es in `Java8`
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
