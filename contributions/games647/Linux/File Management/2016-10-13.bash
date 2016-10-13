0c406d664bd28ee2cc660de06a9929e89d763f3d
games647 <games@tsprivat.de>
2016-10-13 16:21:00 +0100

    2016-10-13's Linux workout

---
+++ contributions/games647/Linux/File Management/2016-10-13.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Using `touch` to alter files modification time
