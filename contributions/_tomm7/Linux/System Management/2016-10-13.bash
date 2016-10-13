a655428357b115bba9c93b132ebfbc205cccc779
_tomm7 <srcthomas@gmail.com>
2016-10-13 19:47:05 +0100

    2016-10-13's Linux workout

---
+++ contributions/_tomm7/Linux/System Management/2016-10-13.bash
@@ -0,0 +1,6 @@
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
