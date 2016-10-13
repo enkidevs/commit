e5f4b84aa5eee238f0d4880d78cf0fb187d6615a
gelmaci <gokhanelmaci26@gmail.com>
2016-10-13 19:35:38 +0100

    2016-10-13's Java workout

---
+++ contributions/gelmaci/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
