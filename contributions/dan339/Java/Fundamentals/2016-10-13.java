4481d6046e0b70f490500fedd63a89ccbf11cbe3
dan339 <danielcampbell339@gmail.com>
2016-10-13 07:27:12 +0100

    2016-10-13's Java workout

---
+++ contributions/dan339/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Difference between JRE and JDK?
+Calling one constructor from another
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
