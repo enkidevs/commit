0bcad063788df12017e647e6c3b7b3a2ffb34b5b
AltRightCodeFight <cody@cteds.com>
2016-10-14 00:24:34 +0100

    2016-10-13's Linux workout

---
+++ contributions/AltRightCodeFight/Linux/File Management/2016-10-13.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
