bda05bf2321b6b7faec73e53f8b7a52dd19890d8
adak007 <adam.ambrozek@gmail.com>
2016-10-13 19:45:31 +0100

    2016-10-13's Java workout

---
+++ contributions/adak007/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Avoid using `static` variables
+Using an interface as a parameter
