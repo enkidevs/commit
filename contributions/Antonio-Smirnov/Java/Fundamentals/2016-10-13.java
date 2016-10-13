687530603ef5f7404e9e94b3b3ae1e7b6183bc27
Antonio-Smirnov <toxa.inc@mail.ru>
2016-10-13 16:49:44 +0100

    2016-10-13's Java workout

---
+++ contributions/Antonio-Smirnov/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Connecting to FTP using Java
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Always specify type parameter
+Increase `PermGen` space as to avoid `OutOfMemory` errors
