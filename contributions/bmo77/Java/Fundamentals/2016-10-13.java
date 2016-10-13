734053028e6a861da8d837cd8c33d83ea9652965
bmo77 <basha.others@gmail.com>
2016-10-13 11:48:05 +0100

    2016-10-13's Java workout

---
+++ contributions/bmo77/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
