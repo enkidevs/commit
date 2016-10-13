df07fdf10317b95bf46b7f148c5e094725ff80ad
kenalger <kenalger@comcast.net>
2016-10-13 15:32:18 +0100

    2016-10-13's Java workout

---
+++ contributions/kenalger/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Uses of the `final` keyword
+Catch multiple exceptions in a single `catch` block
+Using `Printstream.format` to format a string before printing
+Using bounded type parameters in generic methods
