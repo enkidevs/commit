b0cf06edab3318fd341725c49a1312b99818cb6b
PotHix <pothix@pothix.com>
2016-10-13 11:11:03 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/PotHix/JavaScript/Functions/2016-10-13.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Method chaining
+Easily generate a random `HEX` color
