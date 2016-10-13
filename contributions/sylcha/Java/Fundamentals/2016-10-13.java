09fcc5059ab0b896f75ac87ab1eb79a7bea692a7
sylcha <chambon.sylvain@gmail.com>
2016-10-13 06:20:11 +0100

    2016-10-13's Java workout

---
+++ contributions/sylcha/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
+How range operations work
+Using an interface as a parameter
