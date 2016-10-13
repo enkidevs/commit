9cfa9bf6c18e7ce35ce2b34218f515ed7932382d
brigun <brigun@gmail.com>
2016-10-13 23:40:19 +0100

    2016-10-13's Java workout

---
+++ contributions/brigun/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Connecting to FTP using Java
+Writing generic methods
+Metadata: creating a user-defined file attribute
+Constant notation
