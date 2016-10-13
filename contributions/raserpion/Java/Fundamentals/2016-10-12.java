5b72a354bee4c9451c89fdc31ec23a642f64e9dc
raserpion <rodrigo.loiola@gmail.com>
2016-10-13 02:28:46 +0100

    2016-10-12's Java workout

---
+++ contributions/raserpion/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Supply `toString()` in all classes
