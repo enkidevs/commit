316c07c7f62c5adf06be3d0f8028dc39d3f0c8a0
kracke <kracke@infomax-it.de>
2016-10-13 16:07:51 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/kracke/JavaScript/Node/2016-10-13.js
@@ -0,0 +1,5 @@
+The built-in Node debugger
+Node Inspector
+Use conventional property names for error handling in Node.js
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
