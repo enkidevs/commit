bba62602cbe0cf37374cb3b5722ae4045f53aac7
Evidica <evidica@gmail.com>
2016-10-13 03:38:05 +0100

    2016-10-12's JavaScript workout

---
+++ contributions/Evidica/JavaScript/Functions/2016-10-12.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Method chaining
