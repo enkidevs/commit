eaef95beee0a9d57d26c71914206933258c6b59f
dabizk <dabizk@yahoo.com>
2016-10-14 00:54:42 +0100

    2016-10-13's Java workout

---
+++ contributions/dabizk/Java/I/O/2016-10-13.java
@@ -0,0 +1,5 @@
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Reading file attributes
+Retrieving file store attributes
+Listing a file system's root directories
