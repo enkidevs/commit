a8bd12666c6ca05ac6f5353121bbc87d2658d1f2
ccpizz <viorel.lupu@netcentric.biz>
2016-10-13 06:10:59 +0100

    2016-10-13's Python workout

---
+++ contributions/ccpizz/Python/Data Structures/2016-10-13.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Special attributes of objects and classes
+Get the most of `float`s
+Keep things in order with `OrderedDict`
+Prioritize your `queue` 
