9b1f90dfc98c4bc042dc834721631becce4ea494
kwitczak <kwitczak19@gmail.com>
2016-10-13 17:30:25 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/kwitczak/JavaScript/Npm/2016-10-13.js
@@ -0,0 +1,5 @@
+npm can handle multiple versions of a dependency
+`optionalDependencies` in npm
+Tab completion in `npm`
+Reduce package duplication 
+Lock down dependency versions by shrinkwrapping
