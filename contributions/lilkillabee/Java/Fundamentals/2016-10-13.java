9374f8eb4781936a1d57cbbce3c8182f99370ce4
lilkillabee <lilkillabee@gmail.com>
2016-10-13 16:41:07 +0100

    2016-10-13's Java workout

---
+++ contributions/lilkillabee/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
