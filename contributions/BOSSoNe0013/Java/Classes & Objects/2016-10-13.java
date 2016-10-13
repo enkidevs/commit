782bc9f83ea5078dd775bf47b6b2111a7fef1cd3
BOSSoNe0013 <bossone0013@gmail.com>
2016-10-13 20:48:00 +0100

    2016-10-13's Java workout

---
+++ contributions/BOSSoNe0013/Java/Classes & Objects/2016-10-13.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Compare classes and not class names
+Double Brace Initialization
+How and when to use `WeakHashMap` class
+Fields with Java **Reflection API**
