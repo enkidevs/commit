bccd5d2162b27d97501b737c5d4d660f54133844
BioK03 <bertrand.choubert@hotmail.fr>
2016-10-13 07:41:36 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/BioK03/JavaScript/Node/2016-10-13.js
@@ -0,0 +1,5 @@
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+**Node** `os` module
+Wrapping errors in Node.js using _node-verror_
+`try-catch` only for **sync** code
