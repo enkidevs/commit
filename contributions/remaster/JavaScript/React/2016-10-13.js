079d05a2b0c67fea0e80e2d8d274b850a7286172
remaster <daniel.neuman@gmail.com>
2016-10-13 22:32:08 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/remaster/JavaScript/React/2016-10-13.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Functional Stateless Components in React
+Props in getInitialState Is an        
+Anti-Pattern
+Keyed fragments in **React**
+Prop Validation
