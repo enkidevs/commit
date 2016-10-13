6139f7ae5c01efbfb7efa5685d8857b4a982634e
Eldergeek <david@geekguytech.com>
2016-10-13 17:17:40 +0100

    2016-10-13's Linux workout

---
+++ contributions/Eldergeek/Linux/File Management/2016-10-13.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
