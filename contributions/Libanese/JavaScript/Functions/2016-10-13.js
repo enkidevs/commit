fee0b3001936366b6b592c1eec0ed7263b79f551
Libanese <attiliomandola@gmail.com>
2016-10-13 13:02:14 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/Libanese/JavaScript/Functions/2016-10-13.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+`.bind()` 
+Easily generate a random `HEX` color
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
