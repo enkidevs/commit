5bcdecbf2327d94bad9c89bda18cedeac555399b
effigy13 <frederika13@yahoo.com>
2016-10-13 18:13:51 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/effigy13/JavaScript/React/2016-10-13.js
@@ -0,0 +1,6 @@
+`if`-`else` statements in **JSX** and **React**
+componentWillReceiveProps Not Triggered After Mounting
+
+Dealing with `this.props.children`
+Immutability helpers in **React**
+Default values for props
