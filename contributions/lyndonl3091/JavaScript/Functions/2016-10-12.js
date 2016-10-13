df0bd17950fb93c8951b192ee46c05ac302da993
lyndonl3091 <lyndonl3091@gmail.com>
2016-10-13 02:30:18 +0100

    2016-10-12's JavaScript workout

---
+++ contributions/lyndonl3091/JavaScript/Functions/2016-10-12.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Method chaining
+Easily generate a random `HEX` color
