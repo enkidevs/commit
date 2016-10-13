6046030d64acf9ea52ca29a2bc6cbbe9ed8cf6bc
bbialek <bialcus@gmail.com>
2016-10-14 00:11:42 +0100

    2016-10-13's Linux workout

---
+++ contributions/bbialek/Linux/Package Management/2016-10-13.bash
@@ -0,0 +1,5 @@
+Installing a `.deb` package from the terminal
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
