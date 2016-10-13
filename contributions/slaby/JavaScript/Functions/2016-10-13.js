bf79a0e3efb82c560fcbd2496d4806512ae40019
slaby <pufek93@gmail.com>
2016-10-13 07:28:39 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/slaby/JavaScript/Functions/2016-10-13.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Easily generate a random `HEX` color
