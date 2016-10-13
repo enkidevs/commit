51aa28de0a02441aad9c0554ab2046c1f83bb8b2
lachlan-eagling <lochie.eagling@gmail.com>
2016-10-13 21:49:17 +0100

    2016-10-13's Java workout

---
+++ contributions/lachlan-eagling/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Use meaningful names
+The trade-off between short and descriptive names
+Connecting to FTP using Java
+Metadata: creating a user-defined file attribute
+Always specify type parameter
