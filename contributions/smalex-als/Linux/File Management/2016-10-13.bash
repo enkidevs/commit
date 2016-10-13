1ab47c3d62d82a5e4d2ec177a96360450895c318
smalex-als <smalex69@gmail.com>
2016-10-13 14:25:08 +0100

    2016-10-13's Linux workout

---
+++ contributions/smalex-als/Linux/File Management/2016-10-13.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
