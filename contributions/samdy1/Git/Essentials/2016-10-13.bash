5a50e8aeb712cffefe4562a36fa91aa1c55b4066
samdy1 <samueliceblade@gmail.com>
2016-10-13 07:46:28 +0100

    2016-10-13's Git workout

---
+++ contributions/samdy1/Git/Essentials/2016-10-13.bash
@@ -0,0 +1,5 @@
+Using aliases for git commands
+How to set your username and email
+Search for commits by author
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
