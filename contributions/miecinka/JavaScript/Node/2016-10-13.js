93f04ba12fe894ec6c9bd8ca458f844e18437060
miecinka <mietinhovanish@gmail.com>
2016-10-13 17:08:15 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/miecinka/JavaScript/Node/2016-10-13.js
@@ -0,0 +1,5 @@
+The built-in Node debugger
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+Automatic compilation for Node with **Nodemon** 
+Middleware/pipeline design pattern
