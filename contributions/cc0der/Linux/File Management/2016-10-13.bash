37af2c18e65973b876277ea605655bd42fc6f553
cc0der <markus.hinterseer@gmail.com>
2016-10-13 09:32:45 +0100

    2016-10-13's Linux workout

---
+++ contributions/cc0der/Linux/File Management/2016-10-13.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
