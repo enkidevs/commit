219bb6a92b9902b23179736347d5e453a99de718
L33tCh <l33tch@gmail.com>
2016-10-13 19:53:43 +0100

    2016-10-13's Linux workout

---
+++ contributions/L33tCh/Linux/Package Management/2016-10-13.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+Prevent updating a specific package in Debian systems
+Get `apt` to use a mirror / faster mirror
+Using `PPAs`
