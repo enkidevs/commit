5d09fa53799e3f513c01b64409a3b6871d3c7199
bteres <bteres@gmail.com>
2016-10-13 10:11:02 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/bteres/JavaScript/React/2016-10-13.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Shallow compare in **React**
+Clone Elements in **React**
+Default values for props
