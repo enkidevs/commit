97a90608e03de9c9d531c532785775f06d6dd71c
jordyamc <jordyamc@hotmail.com>
2016-10-13 04:40:23 +0100

    2016-10-12's Java workout

---
+++ contributions/jordyamc/Java/Fundamentals/2016-10-12.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
