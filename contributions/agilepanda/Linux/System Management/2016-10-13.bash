0e935c0f2227def010895356613b464ca552c4ce
agilepanda <pantelei@cs.uni-bonn.de>
2016-10-13 05:12:11 +0100

    2016-10-13's Linux workout

---
+++ contributions/agilepanda/Linux/System Management/2016-10-13.bash
@@ -0,0 +1,5 @@
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Finding Open Files With `lsof`
