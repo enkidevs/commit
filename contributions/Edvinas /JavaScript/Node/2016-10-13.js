262981e7ded44ef7e18e7ea0c948692e0c5422b0
Edvinas <edvinas.reg@outlook.com>
2016-10-13 07:24:53 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/Edvinas /JavaScript/Node/2016-10-13.js
@@ -0,0 +1,5 @@
+How to debug Node.js
+Wrapping errors in Node.js using _node-verror_
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
+Middleware/pipeline design pattern
