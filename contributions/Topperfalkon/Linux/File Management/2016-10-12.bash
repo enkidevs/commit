143bb9829d814c9a1b0618668638be94631ce351
Topperfalkon <topperfalkon@gmail.com>
2016-10-13 01:17:37 +0100

    2016-10-12's Linux workout

---
+++ contributions/Topperfalkon/Linux/File Management/2016-10-12.bash
@@ -0,0 +1,6 @@
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
