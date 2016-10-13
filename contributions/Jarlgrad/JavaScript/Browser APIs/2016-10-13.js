d8d01d9bfe5ce73852673edb0168885fd9c908f5
Jarlgrad <timothy.jalgard@gmail.com>
2016-10-13 17:23:06 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/Jarlgrad/JavaScript/Browser APIs/2016-10-13.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Use hosted scripts to increase performance
+CSS for when JavaScript is enabled
+Navigation Timing API
+ProgressEvent
