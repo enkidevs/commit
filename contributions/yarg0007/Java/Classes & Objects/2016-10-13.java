9369ecae3a11e777b3189733188b4570ade29d8a
yarg0007 <benjamin.yarger@gmail.com>
2016-10-13 13:39:53 +0100

    2016-10-13's Java workout

---
+++ contributions/yarg0007/Java/Classes & Objects/2016-10-13.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Using `static` fields
+Utility classes and `static` methods
+Compare classes and not class names
+How and when to use `WeakHashMap` class
