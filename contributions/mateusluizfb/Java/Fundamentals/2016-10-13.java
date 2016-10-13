da5bb8a5082eeb205b14670f19653eb6bac50c30
mateusluizfb <mateuslfreitasb@gmail.com>
2016-10-13 23:35:57 +0100

    2016-10-13's Java workout

---
+++ contributions/mateusluizfb/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: creating a user-defined file attribute
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
