3c4a8e0c5e18ec860b21ccdbcdee3bee582dc969
okashirin <lokishadow@mail.ru>
2016-10-13 10:46:38 +0100

    2016-10-13's Java workout

---
+++ contributions/okashirin/Java/I/O/2016-10-13.java
@@ -0,0 +1,5 @@
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
