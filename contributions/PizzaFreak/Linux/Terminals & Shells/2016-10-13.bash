01bcaa00d92777d2fba4c75d77de057a0ce51ae0
PizzaFreak <nico.greve89@gmail.com>
2016-10-13 10:50:26 +0100

    2016-10-13's Linux workout

---
+++ contributions/PizzaFreak/Linux/Terminals & Shells/2016-10-13.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
