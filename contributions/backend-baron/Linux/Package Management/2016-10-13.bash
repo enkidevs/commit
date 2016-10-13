8abf34b94e7581720a35ed5d7552a9773be3b37d
backend-baron <jakubowskimarcin90@gmail.com>
2016-10-13 11:26:49 +0100

    2016-10-13's Linux workout

---
+++ contributions/backend-baron/Linux/Package Management/2016-10-13.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+List installed packages
+Installing a `.deb` package from the terminal
+Using `Tasksel` for software installation
+Solving `BADSIG GPG` errors
