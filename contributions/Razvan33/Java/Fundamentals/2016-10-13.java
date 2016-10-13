fb967065d121dba0e6fecb4bc3da0ac1aca8163a
Razvan33 <rzvncc33@gmail.com>
2016-10-13 06:14:20 +0100

    2016-10-13's Java workout

---
+++ contributions/Razvan33/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Using an interface as a parameter
