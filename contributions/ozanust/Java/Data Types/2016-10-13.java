08c6a5b54bd446b2e3ba01eb591eb814b3e41f3a
ozanust <ozanust9@gmail.com>
2016-10-13 08:26:47 +0100

    2016-10-13's Java workout

---
+++ contributions/ozanust/Java/Data Types/2016-10-13.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Using `enum` in Java
+Use `Arrays.asList()` to initialise lists
+Equals operation on different data types
+Retrieve the component type of an array
