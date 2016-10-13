503f29b192fc75f9d7930ccb1b60f27869ff8275
Boddah <spaziani.fa@gmail.com>
2016-10-13 23:53:11 +0100

    2016-10-13's Java workout

---
+++ contributions/Boddah/Java/Data Types/2016-10-13.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Using `enum` in Java
+Use `Arrays.asList()` to initialise lists
+Converting a string to upper or lower case
+Do not attempt comparisons with NaN
