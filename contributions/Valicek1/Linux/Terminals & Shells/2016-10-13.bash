1a9c9719e4b21e129ac76854d9d9e52b5cc102e1
Valicek1 <valicek1994@gmail.com>
2016-10-13 14:35:10 +0100

    2016-10-13's Linux workout

---
+++ contributions/Valicek1/Linux/Terminals & Shells/2016-10-13.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
