0b90caad2c18e3a16fb8767c04be74303353d32a
saffad <michal_kolsut@o2.pl>
2016-10-13 06:12:53 +0100

    2016-10-13's Java workout

---
+++ contributions/saffad/Java/I/O/2016-10-13.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to create directories in Java
+Create system file paths using the `Path` class
+Reading and writing text files
+Checking for the existence of files and directories
