b756aae79e20a609bd7ace47b695565e8bf2bae0
veronica <me@veronicacannon.com>
2016-10-13 14:39:30 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/veronica/JavaScript/Npm/2016-10-13.js
@@ -0,0 +1,5 @@
+Bump package version in npm
+Passing through command line arguments in npm scripts
+List binaries for scripting in npm
+Manage local node modules with `npm link`
+`npm` verifies `sha1` hashes of packages
