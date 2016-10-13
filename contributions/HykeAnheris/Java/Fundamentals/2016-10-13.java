62e3ab753d89b5b17f37c4e1b85979292922982f
HykeAnheris <alex.a@gmx.fr>
2016-10-13 23:16:12 +0100

    2016-10-13's Java workout

---
+++ contributions/HykeAnheris/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between `==` and `equals()`
+Java naming conventions
+Altering format string output by changing a format specifier's `argument_index`
+How range operations work
