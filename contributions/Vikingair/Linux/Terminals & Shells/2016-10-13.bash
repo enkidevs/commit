f7b170d9ef8458e53b8da9684c210c2dfd0c7dbc
Vikingair <luft.viktor.bms@gmail.com>
2016-10-13 07:09:17 +0100

    2016-10-13's Linux workout

---
+++ contributions/Vikingair/Linux/Terminals & Shells/2016-10-13.bash
@@ -0,0 +1,5 @@
+Quick `bash` shortcuts 
+Random password generator
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
