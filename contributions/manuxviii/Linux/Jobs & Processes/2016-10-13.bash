331152259e6237c6d45bfcdbec145bdbf5a70e63
manuxviii <emmanuel.mesnige@protonmail.com>
2016-10-13 21:53:23 +0100

    2016-10-13's Linux workout

---
+++ contributions/manuxviii/Linux/Jobs & Processes/2016-10-13.bash
@@ -0,0 +1,5 @@
+The `2>&1` redirection
+`Crontab` newline issue
+`Cron` dot-in-filename issues when using `run-parts`
+Common issue with scripts in `cron`
+How to pause a process and run others in the background
