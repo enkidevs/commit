b53abed52f321fde37bf1d3c4bd26910a4d16143
doomsterinc <doomsterinc1@gmail.com>
2016-10-13 17:37:04 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/doomsterinc/JavaScript/Core/2016-10-13.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Performing a function at timed intervals
+Hoisting applies only to variable declarations, not initializations
+`catch` is block scoped
+Detect online connection
