b71ac3f1d99ea21e3024de0c0a78105da752bcb9
padi <marcrendlignacio@gmail.com>
2016-10-13 11:30:03 +0100

    2016-10-12's Linux workout

---
+++ contributions/padi/Linux/System Management/2016-10-12.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
