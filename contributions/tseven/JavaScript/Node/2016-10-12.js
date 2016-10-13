60b08c0beacdf30f1bb67909789d4ef5722e204e
tseven <tseven@gmail.com>
2016-10-13 08:19:54 +0100

    2016-10-12's JavaScript workout

---
+++ contributions/tseven/JavaScript/Node/2016-10-12.js
@@ -0,0 +1,5 @@
+Wrapping errors in Node.js using _node-verror_
+Bulk-Write in **Node** with `cork()`
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
+Avoid using `sudo` when running Node
