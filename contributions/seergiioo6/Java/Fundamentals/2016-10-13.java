1313d81269e436d220921fd7f5f3ce39871f06ac
seergiioo6 <hello@sergioandres.io>
2016-10-13 23:11:48 +0100

    2016-10-13's Java workout

---
+++ contributions/seergiioo6/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using the `Console`
+Using Inheritance to reduce code repetition
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
