7652f339e39acb65a2064ec63691e9407dddb522
AlexxBoo <alexxboo@gmx.de>
2016-10-13 09:39:57 +0100

    2016-10-13's Java workout

---
+++ contributions/AlexxBoo/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
