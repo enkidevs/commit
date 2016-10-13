ce88f7c834f42d531443fe1be75983da5b218d1e
ghstridr <ghstridr@gmail.com>
2016-10-13 02:28:13 +0100

    2016-10-12's Python workout

---
+++ contributions/ghstridr/Python/Data Structures/2016-10-12.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`Dictionary` view objects
+Best way to implement a simple `queue` 
+Enhance your `tuple`s
+Get the most of `float`s
