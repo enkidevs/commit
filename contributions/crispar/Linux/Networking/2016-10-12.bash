b3264b8c4726f7590f843cf03567b274dfac5681
crispar <jwcris.park@gmail.com>
2016-10-13 15:10:07 +0100

    2016-10-12's Linux workout

---
+++ contributions/crispar/Linux/Networking/2016-10-12.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Useful ssh client optimizations 
+Get real network statistics with `slurm`
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
