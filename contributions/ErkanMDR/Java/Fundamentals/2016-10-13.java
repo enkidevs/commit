01ff0031433e1b34e09c072b78576a32558ac99c
ErkanMDR <erkanmdr@gmail.com>
2016-10-13 17:09:00 +0100

    2016-10-13's Java workout

---
+++ contributions/ErkanMDR/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Measuring time
+Increase `PermGen` space as to avoid `OutOfMemory` errors
