8542058f5a5ec8d512a9ff755d4833f143571076
allfanmu <allfanmu@wp.pl>
2016-10-13 06:00:52 +0100

    2016-10-13's Java workout

---
+++ contributions/allfanmu/Java/I/O/2016-10-13.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Reading and writing text files
+Checking for the existence of files and directories
+Deleting files and directories
