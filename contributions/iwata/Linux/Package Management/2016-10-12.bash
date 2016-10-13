7f7c745919b049c095dffed360444da486da006d
iwata <gootonori+enki@gmail.com>
2016-10-13 04:00:23 +0100

    2016-10-12's Linux workout

---
+++ contributions/iwata/Linux/Package Management/2016-10-12.bash
@@ -0,0 +1,5 @@
+Prevent updating a specific package in Debian systems
+Get `apt` to use a mirror / faster mirror
+Using `Tasksel` for software installation
+Using `PPAs`
+Downgrade a package via `apt-get`
