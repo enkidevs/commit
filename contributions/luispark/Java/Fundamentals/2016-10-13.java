a046a92b6873769f568697e368d6194f2edde6d6
luispark <luis.gc.park@gmail.com>
2016-10-14 00:39:36 +0100

    2016-10-13's Java workout

---
+++ contributions/luispark/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
