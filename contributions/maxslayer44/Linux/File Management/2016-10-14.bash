d3c1484b95e6459177939552a26712330473cf24
maxslayer44 <maxime.baumann.pro@gmail.com>
2016-10-14 13:54:08 +0100

    2016-10-14's Linux workout

---
+++ contributions/maxslayer44/Linux/File Management/2016-10-14.bash
@@ -0,0 +1,6 @@
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Delete commands aliases
+Easy way of sharing files
