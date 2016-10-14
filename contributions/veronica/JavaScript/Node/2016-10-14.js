63e07321a2b015a44968590eb8e09189270a519a
veronica <me@veronicacannon.com>
2016-10-14 14:24:14 +0100

    2016-10-14's JavaScript workout

---
+++ contributions/veronica/JavaScript/Node/2016-10-14.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Http Server in **Node**
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+`try-catch` only for **sync** code
