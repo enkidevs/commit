0d681ee9471faf21839a4800750f316deb798bac
inf0rm4tik3r <lupo-95@hotmail.de>
2016-10-14 18:48:06 +0100

    2016-10-14's Java workout

---
+++ contributions/inf0rm4tik3r/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Altering format string output by changing a format specifier's `argument_index`
+Detect or prevent integer overflow
+Avoid using `static` variables
