5cac37e026d25d12f4572c84b0f3b99e73aec9dd
Aylard <aylard7@gmail.com>
2016-10-14 07:16:19 +0100

    2016-10-14's Git workout

---
+++ contributions/Aylard/Git/Tools/2016-10-14.bash
@@ -0,0 +1,5 @@
+Interactively unstage changes
+Specify a range of commits using double dot syntax
+Interactively stage patches
+Remember the results of previous hunk conflicts
+Sharing data by bundling
