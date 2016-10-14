787f01726348e96efd92fc2dcc77726deaa0d6c2
JonaC22 <jonathan.corallo.utn@gmail.com>
2016-10-14 23:34:11 +0100

    2016-10-14's Linux workout

---
+++ contributions/JonaC22/Linux/Security/2016-10-14.bash
@@ -0,0 +1,5 @@
+Extracting the `public key` from the `private key`
+Best password generation utilities
+Lockdown **Cronjobs**
+Disabling **USB** storage devices
+Private key encryption with `OpenSSL`
