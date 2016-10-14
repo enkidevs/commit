d1393ec7c01cf71e8835df96d1aff0abcc96ff35
Jinxter <propwe@gmail.com>
2016-10-14 12:55:40 +0100

    2016-10-14's Linux workout

---
+++ contributions/Jinxter/Linux/File Management/2016-10-14.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Deleting files in a secure manner
