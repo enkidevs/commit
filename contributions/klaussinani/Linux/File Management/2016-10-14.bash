f963fbb7a81c6c2126101a1351b64ac96a89a775
klaussinani <klaudiosinani@protonmail.ch>
2016-10-14 15:12:05 +0100

    2016-10-14's Linux workout

---
+++ contributions/klaussinani/Linux/File Management/2016-10-14.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Delete commands aliases
