13ae53532f883dccf8a43e9934754981a3255a5f
Dalalibera <giovannedalalibera@hotmail.com>
2016-10-14 07:38:36 +0100

    2016-10-14's Java workout

---
+++ contributions/Dalalibera/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Using Inheritance to reduce code repetition
+`Autoboxing` and `Unboxing`
