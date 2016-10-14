3e4e7ecfac9e2a31aae31fc4640396a10dbe6c0a
tomaset22 <tomasengel7@gmail.com>
2016-10-14 08:38:01 +0100

    2016-10-14's Java workout

---
+++ contributions/tomaset22/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Supply `toString()` in all classes
