53b671858d1b94cc09d8ffd5b31d378b0fd033ca
Kron <miguelbuzato@bol.com.br>
2016-10-14 22:47:13 +0100

    2016-10-14's JavaScript workout

---
+++ contributions/Kron/JavaScript/Npm/2016-10-14.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Registry user accounts for npm
+npm can handle multiple versions of a dependency
+Environmental variables and flags for `npm`
+`config` object in `package.json`
