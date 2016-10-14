8e79431f72ea79bc447d57fe2a350c586ae898d0
JoshuaMorris <me@joshuamorris.info>
2016-10-14 19:00:26 +0100

    2016-10-14's JavaScript workout

---
+++ contributions/JoshuaMorris/JavaScript/Npm/2016-10-14.js
@@ -0,0 +1,5 @@
+Create projects with `npm init`
+Bump package version in npm
+Passing through command line arguments in npm scripts
+`config` object in `package.json`
+Lock down dependency versions by shrinkwrapping
