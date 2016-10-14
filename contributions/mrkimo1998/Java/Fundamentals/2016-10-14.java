571f0a8e398d271f211db42302a92079269b16a8
mrkimo1998 <mrkimo1998@protonmail.com>
2016-10-14 16:45:04 +0100

    2016-10-14's Java workout

---
+++ contributions/mrkimo1998/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Measuring time
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
