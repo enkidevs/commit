beaef4e87fd472a9eba94506970fcaf14ed4e101
Mass.sow <mass.sow@gmail.com>
2016-10-14 21:26:45 +0100

    2016-10-14's Java workout

---
+++ contributions/Mass.sow/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+The trade-off between short and descriptive names
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
