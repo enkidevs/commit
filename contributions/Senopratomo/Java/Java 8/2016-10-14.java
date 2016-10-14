bb36d951046f53396ecc44d293694a37ede97483
Senopratomo <lord.ernesto@gmail.com>
2016-10-14 18:06:57 +0100

    2016-10-14's Java workout

---
+++ contributions/Senopratomo/Java/Java 8/2016-10-14.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Java 8 Lambda expressions
+Using `Arrays.parallelSort` 
+How to check for static dependencies of applications and libraries from the command line.
+`JJS` - Oracle Nashorn, newest JavaScript Engine
