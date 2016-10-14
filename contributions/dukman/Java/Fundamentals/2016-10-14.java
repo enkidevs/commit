2821b11a3e981ecc45b8b8ed36bb05dfceb7c051
dukman <dukman.box@gmail.com>
2016-10-14 08:25:32 +0100

    2016-10-14's Java workout

---
+++ contributions/dukman/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Ternary operator
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
