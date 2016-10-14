2eb7fea48943265f652751d3b7b2ba71d4b974c0
PotHix <pothix@pothix.com>
2016-10-14 12:10:58 +0100

    2016-10-14's Linux workout

---
+++ contributions/PotHix/Linux/System Management/2016-10-14.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Changing the timezone on deb based systems
+Monitor System Memory using `vmstat`
+Using `dd` To Create Disk Images
