b1168c28b4d05f63274e86206f106105e96e4df2
PsnDth <psndth@gmail.com>
2016-10-14 03:50:35 +0100

    2016-10-13's JavaScript workout

---
+++ contributions/PsnDth/JavaScript/Npm/2016-10-13.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Manipulate npm packages cache
+npm can handle multiple versions of a dependency
+List binaries for scripting in npm
+Reduce package duplication 
