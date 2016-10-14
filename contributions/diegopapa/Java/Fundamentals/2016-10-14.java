45b25c962842f9702dc97bad6add5c2ca10749c7
diegopapa <diego.papa.job@gmail.com>
2016-10-14 07:44:50 +0100

    2016-10-14's Java workout

---
+++ contributions/diegopapa/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+Always specify type parameter
