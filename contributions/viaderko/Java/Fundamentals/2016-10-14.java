ccf83596521612fab1d7e98b708555440bb00a13
viaderko <adam.drobik@gmail.com>
2016-10-14 05:22:55 +0100

    2016-10-14's Java workout

---
+++ contributions/viaderko/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Writing generic methods
+Use `DecimalFormat` class to format numbers
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
