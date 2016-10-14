6a3b7bf58cb9962374279d83cc2af85a994a16c6
Danziger <gmzcodes@outlook.com>
2016-10-14 14:04:35 +0100

    2016-10-14's JavaScript workout

---
+++ contributions/Danziger/JavaScript/React/2016-10-14.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Use **React** with other libraries
+Immutability helpers in **React**
+Shortcut for transferring props
