c8f85fec839d02c6990ed0163989c26523b2abb6
KasteSponja <angelcastellanossegundo@gmail.com>
2016-10-14 19:54:23 +0100

    2016-10-14's Java workout

---
+++ contributions/KasteSponja/Java/Data Types/2016-10-14.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Use `Arrays.asList()` to initialise lists
+Finding a substring in a string
+Numeric Conversion - Widening
