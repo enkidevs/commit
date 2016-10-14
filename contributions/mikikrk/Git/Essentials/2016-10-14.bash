d438074b9a4c13f0151248da2cc7023f4b10c911
mikikrk <miki.krk@gmail.com>
2016-10-14 22:40:13 +0100

    2016-10-14's Git workout

---
+++ contributions/mikikrk/Git/Essentials/2016-10-14.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+Adding a remote repository
+Limiting log output by time
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
