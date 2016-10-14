9dfc51ea2d10b96809f8d6cb74f883b0d68f7b58
Seonnyn <h.roeszler@gmail.com>
2016-10-14 09:22:28 +0100

    2016-10-14's JavaScript workout

---
+++ contributions/Seonnyn/JavaScript/Node/2016-10-14.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+`process.nextTick()` callback pattern
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
+Middleware/pipeline design pattern
