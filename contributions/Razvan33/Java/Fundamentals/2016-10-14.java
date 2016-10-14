b3266befb0df2e8f53a876b424eb24d17474c69e
Razvan33 <rzvncc33@gmail.com>
2016-10-14 18:33:27 +0100

    2016-10-14's Java workout

---
+++ contributions/Razvan33/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
