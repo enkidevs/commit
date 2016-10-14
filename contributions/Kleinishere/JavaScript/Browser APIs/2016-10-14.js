5cc4303a1d933448105260c87837797578821f63
Kleinishere <notmpk@gmail.com>
2016-10-14 23:57:43 +0100

    2016-10-14's JavaScript workout

---
+++ contributions/Kleinishere/JavaScript/Browser APIs/2016-10-14.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+CSS for when JavaScript is enabled
+Allow an element to go fullscreen
+Notifications API
+FileReader.readAsArrayBuffer()
