2693c47abcbc8c29a0830aa209b9c406c4547401
toXel <timo625@gmail.com>
2016-10-14 22:10:42 +0100

    2016-10-14's JavaScript workout

---
+++ contributions/toXel/JavaScript/Functions/2016-10-14.js
@@ -0,0 +1,5 @@
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Method chaining
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
