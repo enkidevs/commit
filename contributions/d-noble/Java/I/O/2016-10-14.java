a08b5142d00bb0696bdb490a4a6518e489835657
d-noble <dnoble2@myseneca.ca>
2016-10-14 13:24:35 +0100

    2016-10-14's Java workout

---
+++ contributions/d-noble/Java/I/O/2016-10-14.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+How to list all files in a directory that match a filename extension in Java
+Checking for the existence of files and directories
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
