86fb3846e347f4925823d16c8eb6aec213c61d60
Leerro <gabriel.f.bernard@gmail.com>
2016-10-14 15:42:30 +0100

    2016-10-14's Linux workout

---
+++ contributions/Leerro/Linux/Terminals & Shells/2016-10-14.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Useful aliasing in bash
+Terminal incognito mode
