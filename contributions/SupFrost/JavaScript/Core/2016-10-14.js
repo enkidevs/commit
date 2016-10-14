2db3f1cdd6352417f1a09d8008f0b0e2d1fa321b
SupFrost <nickjorens@gmail.com>
2016-10-14 07:47:18 +0100

    2016-10-14's JavaScript workout

---
+++ contributions/SupFrost/JavaScript/Core/2016-10-14.js
@@ -0,0 +1,5 @@
+Infix operators are left-associative
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+The difference between null, undefined and NaN
+Inaccuracy of binary floating-point format
