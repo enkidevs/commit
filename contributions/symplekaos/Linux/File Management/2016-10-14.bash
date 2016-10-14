a29cdbe71642addc6f4abeba5e8964d4ada33688
symplekaos <symplekaos@gmail.com>
2016-10-14 15:58:26 +0100

    2016-10-14's Linux workout

---
+++ contributions/symplekaos/Linux/File Management/2016-10-14.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
