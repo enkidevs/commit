bd391def59cc04db0559a3bd7d97560eca004b9e
eoin-obrien <eoinobrien910@gmail.com>
2016-10-14 10:45:31 +0100

    2016-10-14's JavaScript workout

---
+++ contributions/eoin-obrien/JavaScript/Functions/2016-10-14.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Move cursor at the end of text input
+Social sharing without widgets
