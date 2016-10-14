e79065b1cb930652e3c93d1d9c9bb9b44d8c5f9e
kracke <kracke@infomax-it.de>
2016-10-14 06:50:48 +0100

    2016-10-14's JavaScript workout

---
+++ contributions/kracke/JavaScript/Node/2016-10-14.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+Streams in **Node**
+How to debug Node.js
+`uncaughtException` listener in Node.js
+Automatic compilation for Node with **Nodemon** 
