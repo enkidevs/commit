610410ec9cacbacc434eb9480c18f838f5196d92
bbohac <bastian.bohac@gmail.com>
2016-10-14 16:45:43 +0100

    2016-10-14's Java workout

---
+++ contributions/bbohac/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using Inheritance to reduce code repetition
+Using `Printstream.format` to format a string before printing
+Constant notation
+Limit Accessibility of `Fields`
