f322fccba05bdc09c3d12dd9028a8a2d36defdaa
liufeng <liufeng5460@163.com>
2016-10-14 01:13:31 +0100

    2016-10-13's Java workout

---
+++ contributions/liufeng/Java/Fundamentals/2016-10-13.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Using Inheritance to reduce code repetition
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+`Autoboxing` and `Unboxing`
