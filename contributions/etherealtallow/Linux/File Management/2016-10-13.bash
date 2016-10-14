c78853093bad2507882d61fa463c940c4f0fb8d7
etherealtallow <jlapslie@gmail.com>
2016-10-14 03:57:54 +0100

    2016-10-13's Linux workout

---
+++ contributions/etherealtallow/Linux/File Management/2016-10-13.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Working with `.gz` files
+Find the unknown process preventing deleting of files
