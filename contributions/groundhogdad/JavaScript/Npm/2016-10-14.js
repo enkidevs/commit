94525cd5ae847929d1470ed2b3af4fffcfb49928
groundhogdad <groundhogdad@gmail.com>
2016-10-14 06:37:10 +0100

    2016-10-14's JavaScript workout

---
+++ contributions/groundhogdad/JavaScript/Npm/2016-10-14.js
@@ -0,0 +1,5 @@
+npm can handle multiple versions of a dependency
+Combining script commands in npm
+`npm`  as an alternative to Gulp
+Reduce package duplication 
+Lock down dependency versions by shrinkwrapping
