c20924885887b93ecf86c5add8de3e995b53eda3
annabaik <annaheyonbaik@gmail.com>
2016-10-14 08:49:52 +0100

    2016-10-14's Linux workout

---
+++ contributions/annabaik/Linux/File Management/2016-10-14.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Deleting files in a secure manner
+Easy way of sharing files
