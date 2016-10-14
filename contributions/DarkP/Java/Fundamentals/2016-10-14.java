05723c9d5f2b58f2da3229e46d301aad8083b0e4
DarkP <petr456@seznam.cz>
2016-10-14 20:04:46 +0100

    2016-10-14's Java workout

---
+++ contributions/DarkP/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
