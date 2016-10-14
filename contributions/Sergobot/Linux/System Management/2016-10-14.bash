73dae80d4bd7c5472df438dc7b8872c1d2c8434f
Sergobot <sergobot@protonmail.com>
2016-10-14 07:47:38 +0100

    2016-10-14's Linux workout

---
+++ contributions/Sergobot/Linux/System Management/2016-10-14.bash
@@ -0,0 +1,7 @@
+Find the `MAC` address of all network interfaces
+
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
