0de4387ec0b06763f428fdc8f0a7a20787352f60
Vira <thomas.ruehrig@studierende.htl-donaustadt.at>
2016-10-14 04:22:46 +0100

    2016-10-14's Linux workout

---
+++ contributions/Vira/Linux/System Management/2016-10-14.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
