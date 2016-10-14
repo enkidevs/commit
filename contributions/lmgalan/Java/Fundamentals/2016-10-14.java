0ec4524472e59e7e0572b865ad58fc839311f8df
lmgalan <luismigalan@gmail.com>
2016-10-14 17:45:58 +0100

    2016-10-14's Java workout

---
+++ contributions/lmgalan/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Distinction between `public` and `private` methods
+Do not perform bitwise and arithmetic operations on the same data
+Implementing an `interface`
