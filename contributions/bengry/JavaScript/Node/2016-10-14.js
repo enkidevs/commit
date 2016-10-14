365331925721b9e72a4045bd89cdeb9119cc51a4
bengry <bengr6@gmail.com>
2016-10-14 09:39:18 +0100

    2016-10-14's JavaScript workout

---
+++ contributions/bengry/JavaScript/Node/2016-10-14.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+The built-in Node debugger
+**Node** `os` module
+Wrapping errors in Node.js using _node-verror_
+Automatic compilation for Node with **Nodemon** 
