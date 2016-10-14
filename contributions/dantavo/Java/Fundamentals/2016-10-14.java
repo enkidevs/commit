f343d43285aebacd31d632503bdf5e5e0af6ad35
dantavo <darkmind88@gmail.com>
2016-10-14 14:48:26 +0100

    2016-10-14's Java workout

---
+++ contributions/dantavo/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use `DecimalFormat` class to format numbers
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
