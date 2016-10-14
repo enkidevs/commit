969175e3860d2e9cb7dc6ff1f70f20a1d7da2b77
codenewcomer <codenewcomer@gmail.com>
2016-10-14 07:24:57 +0100

    2016-10-14's Linux workout

---
+++ contributions/codenewcomer/Linux/File Management/2016-10-14.bash
@@ -0,0 +1,6 @@
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
+Easy way of sharing files
