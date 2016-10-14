0534bde45c14a66f7af1fac6725bfb38c278c93f
hansonryne <hansonryne@gmail.com>
2016-10-14 11:20:58 +0100

    2016-10-14's Linux workout

---
+++ contributions/hansonryne/Linux/File Management/2016-10-14.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
