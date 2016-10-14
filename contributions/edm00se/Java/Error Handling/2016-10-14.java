9f752d2a9b774634d0506eeb891d531b5d393c8f
edm00se <edm00se@gmail.com>
2016-10-14 12:33:15 +0100

    2016-10-14's Java workout

---
+++ contributions/edm00se/Java/Error Handling/2016-10-14.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Creating new Exceptions
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Checking a double for `NaN` value
