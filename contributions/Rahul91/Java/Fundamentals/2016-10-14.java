04013cec435d81d921c51cc4f96acca5ad8d52bb
Rahul91 <rahulramesh118@gmail.com>
2016-10-15 11:04:13 +0100

    2016-10-14's Java workout

---
+++ contributions/Rahul91/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Java naming conventions
+Use strings in a `switch` statement
+Avoid using `static` variables
