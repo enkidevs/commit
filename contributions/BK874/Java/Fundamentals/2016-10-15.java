0674d33b4c5aec239202e0374bf0f3aa2abb3422
BK874 <bjk69@pitt.edu>
2016-10-16 00:04:13 +0100

    2016-10-15's Java workout

---
+++ contributions/BK874/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
+How range operations work
+Avoid using `static` variables
