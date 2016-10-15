affe2a45e868da3f886ed54f7e9a7707d11e14f5
Kvetoslav <jiri.heneberk@icompllex.com>
2016-10-15 14:32:01 +0100

    2016-10-15's JavaScript workout

---
+++ contributions/Kvetoslav/JavaScript/Core/2016-10-15.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Functions return `undefined` by default
+The difference between null, undefined and NaN
+Extra function arguments are undefined by default
+Inaccuracy of binary floating-point format
