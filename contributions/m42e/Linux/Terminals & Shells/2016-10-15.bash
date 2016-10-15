706f076a195ab7eb47653edbb94310ef7cdc008f
m42e <matthias@bilger.info>
2016-10-15 08:50:42 +0100

    2016-10-15's Linux workout

---
+++ contributions/m42e/Linux/Terminals & Shells/2016-10-15.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Useful aliasing in bash
+Random password generator
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
