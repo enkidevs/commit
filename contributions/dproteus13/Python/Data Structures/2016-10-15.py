4e2af4db8547935a67cb07e0c689464ca449f769
dproteus13 <proteus@gmail.com>
2016-10-15 10:55:19 +0100

    2016-10-15's Python workout

---
+++ contributions/dproteus13/Python/Data Structures/2016-10-15.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`del` statement for  lists
+`Dictionary` - standard mapping type
+When `range` comes in handy
+Get the most of `int`s
