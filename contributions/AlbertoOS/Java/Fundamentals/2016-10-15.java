ca9cf2eae4e17c7719a54abf7100a1a60145c883
AlbertoOS <orkan.aos@gmail.com>
2016-10-15 13:44:40 +0100

    2016-10-15's Java workout

---
+++ contributions/AlbertoOS/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
