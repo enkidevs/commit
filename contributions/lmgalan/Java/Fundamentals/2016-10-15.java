9c6d1d887d56f68dd6d8aacae9065699e294183b
lmgalan <luismigalan@gmail.com>
2016-10-15 21:45:09 +0100

    2016-10-15's Java workout

---
+++ contributions/lmgalan/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Distinction between `public` and `private` methods
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Supply `toString()` in all classes
