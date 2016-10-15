84174302d5599ab71d8c50929133083d9890854e
kSom <kgsommer@gmail.com>
2016-10-15 08:33:58 +0100

    2016-10-15's Java workout

---
+++ contributions/kSom/Java/Classes & Objects/2016-10-15.java
@@ -0,0 +1,5 @@
+Template methods in abstract classes
+The `abstract` keyword
+Double Brace Initialization
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
