b350d6c17a43150623c225fe22f9f036a4ad3f85
brigun <brigun@gmail.com>
2016-10-15 22:58:38 +0100

    2016-10-15's Java workout

---
+++ contributions/brigun/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+The trade-off between short and descriptive names
+Java naming conventions
+Using Inheritance to reduce code repetition
+Metadata: setting a file's owner
