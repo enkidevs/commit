6697063744d8631975f5a30a48e71a90854e3bb6
Kertt123 <krzysztof_serkowski@interia.pl>
2016-10-15 17:39:38 +0100

    2016-10-15's Java workout

---
+++ contributions/Kertt123/Java/Error Handling/2016-10-15.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
