767e02060be5a1ded242d1d12b4f15882822b0b0
NiklasMerz <niklasmerz@gmx.net>
2016-10-15 15:27:43 +0100

    2016-10-15's Java workout

---
+++ contributions/NiklasMerz/Java/Error Handling/2016-10-15.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Prevent `exceptions` while logging Data
+Throwing an `exception`
+Checking a double for `NaN` value
