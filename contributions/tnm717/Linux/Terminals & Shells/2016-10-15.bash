d67f79d07d04264c9560fe152013372f300047a7
tnm717 <thomas.masterson@gmail.com>
2016-10-15 19:46:45 +0100

    2016-10-15's Linux workout

---
+++ contributions/tnm717/Linux/Terminals & Shells/2016-10-15.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Find any Unix / Linux command
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
