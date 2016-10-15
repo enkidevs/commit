23de6c0d00ff29fb68affa1354729635a6c4dc80
jsheffers <shanejeffers@mac.com>
2016-10-15 22:44:26 +0100

    2016-10-15's JavaScript workout

---
+++ contributions/jsheffers/JavaScript/Core/2016-10-15.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Most efficient way to build `HTML` strings
+Data type comparison in `switch` statements
+Infinity
+Extra function arguments are undefined by default
