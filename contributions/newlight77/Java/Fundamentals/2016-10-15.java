34c03f2814b88105ed5781b1ae71d54a4de9722b
newlight77 <newlight77@gmail.com>
2016-10-15 08:57:39 +0100

    2016-10-15's Java workout

---
+++ contributions/newlight77/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
