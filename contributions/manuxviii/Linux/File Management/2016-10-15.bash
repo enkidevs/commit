00a88045d5349cdce7e3562b442dfbb9cbe57a79
manuxviii <emmanuel.mesnige@protonmail.com>
2016-10-15 22:36:29 +0100

    2016-10-15's Linux workout

---
+++ contributions/manuxviii/Linux/File Management/2016-10-15.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
