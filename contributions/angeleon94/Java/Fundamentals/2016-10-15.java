eabd1d53628ae1d7028c44071faac32aeb5ea7ba
angeleon94 <angeleon94@gmail.com>
2016-10-15 19:16:50 +0100

    2016-10-15's Java workout

---
+++ contributions/angeleon94/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use meaningful names
+The trade-off between short and descriptive names
+Using Inheritance to reduce code repetition
+There is no such thing as *pass-by-reference* in Java
