b8b93c553be045091aaa62c31e3a5443320080d5
BooM <tararuj.michal@gmail.com>
2016-10-15 07:03:28 +0100

    2016-10-15's Java workout

---
+++ contributions/BooM/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Common mistake on switch statements
+Use strings in a `switch` statement
+Use `DecimalFormat` class to format numbers
+How range operations work
