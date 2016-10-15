25f2fb67e037d4167bdee9e7b69fe39123c3ffc9
pahhh <p-a@mail.ru>
2016-10-15 04:58:44 +0100

    2016-10-15's Python workout

---
+++ contributions/pahhh/Python/Data Structures/2016-10-15.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Built-in `list` methods
+When `range` comes in handy
+`Module`s everywhere!
+Special attributes of objects and classes
