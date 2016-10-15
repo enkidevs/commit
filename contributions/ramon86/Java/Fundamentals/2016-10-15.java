e2c627e55e254d16a6bbb68601de6142e7f7102c
ramon86 <ramonvandam@gmail.com>
2016-10-15 07:53:22 +0100

    2016-10-15's Java workout

---
+++ contributions/ramon86/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The trade-off between short and descriptive names
+Inheriting a constructor from a superclass
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
