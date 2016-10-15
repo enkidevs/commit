24da0af368d6ad3a877aeb0295b37364a5c0a842
tnargwoxow <tnargwoxow@gmail.com>
2016-10-15 07:52:30 +0100

    2016-10-15's Linux workout

---
+++ contributions/tnargwoxow/Linux/File Management/2016-10-15.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
+Easy way of sharing files
