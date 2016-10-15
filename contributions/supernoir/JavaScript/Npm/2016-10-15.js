d03e73ade331bec2e27d61104edac460e715b75f
supernoir <kawohl.bentodesign@gmail.com>
2016-10-15 13:07:07 +0100

    2016-10-15's JavaScript workout

---
+++ contributions/supernoir/JavaScript/Npm/2016-10-15.js
@@ -0,0 +1,5 @@
+Manage local node modules with `npm link`
+`npm` verifies `sha1` hashes of packages
+`peerDependencies`
+`optionalDependencies` in npm
+Lock down dependency versions by shrinkwrapping
