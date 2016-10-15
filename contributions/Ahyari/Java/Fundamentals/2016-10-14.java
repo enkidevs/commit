73cf29775c28ac85e8cd129c0721d165dae3791d
Ahyari <ahmad.alhyari@gmail.com>
2016-10-15 08:44:09 +0100

    2016-10-14's Java workout

---
+++ contributions/Ahyari/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use meaningful names
+Always specify type parameter
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
