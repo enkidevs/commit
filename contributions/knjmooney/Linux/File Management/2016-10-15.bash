dc30b278c0080882df36f6850f118b56d88bad3f
knjmooney <knjmooney@gmail.com>
2016-10-15 22:59:29 +0100

    2016-10-15's Linux workout

---
+++ contributions/knjmooney/Linux/File Management/2016-10-15.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
