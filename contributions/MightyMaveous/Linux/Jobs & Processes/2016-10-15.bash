904c2181b9db9d05c99a7a78a69d000c259d2bd5
MightyMaveous <marvin.n.evers@gmail.com>
2016-10-15 15:40:41 +0100

    2016-10-15's Linux workout

---
+++ contributions/MightyMaveous/Linux/Jobs & Processes/2016-10-15.bash
@@ -0,0 +1,5 @@
+The `2>&1` redirection
+Keeping processes running after you log out
+Common issue with scripts in `cron`
+Set a process priority
+How to pause a process and run others in the background
