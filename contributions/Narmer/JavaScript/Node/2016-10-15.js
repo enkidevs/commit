679ff8dae34398a63ca1e21bde7cf97a1938fc76
Narmer <narmer.menes@icloud.com>
2016-10-15 08:51:37 +0100

    2016-10-15's JavaScript workout

---
+++ contributions/Narmer/JavaScript/Node/2016-10-15.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Custom Node REPL Server
+First-error callbacks in **Node**
+Use conventional property names for error handling in Node.js
+`uncaughtException` listener in Node.js
