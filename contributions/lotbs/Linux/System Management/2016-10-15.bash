894c5e923859d44f1366c794b70f89c7d3d3ff69
lotbs <nkosindlovu87@gmail.com>
2016-10-15 19:08:38 +0100

    2016-10-15's Linux workout

---
+++ contributions/lotbs/Linux/System Management/2016-10-15.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
