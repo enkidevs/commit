c0bd6f48b4266783d288b10285f3cce20a267b09
stevedien <lulzsecd@gmail.com>
2016-10-15 16:36:28 +0100

    2016-10-14's Git workout

---
+++ contributions/stevedien/Git/Tools/2016-10-14.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Specify a commit by its ancestry
+Interactively stage patches
+Create a new branch from a stash
+Remember the results of previous hunk conflicts
