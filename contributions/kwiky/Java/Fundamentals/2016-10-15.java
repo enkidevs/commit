af4d920385ffc82030c9bcabd44e2de7c2c20e60
kwiky <steve@grosbois.fr>
2016-10-15 08:15:10 +0100

    2016-10-15's Java workout

---
+++ contributions/kwiky/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Connecting to FTP using Java
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
+Calling one constructor from another
