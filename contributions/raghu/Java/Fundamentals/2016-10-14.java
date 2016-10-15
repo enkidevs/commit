238129caf50eb2cbbcb5475b1ef8ca608d724425
raghu <raghu.9208@gmail.com>
2016-10-15 04:25:22 +0100

    2016-10-14's Java workout

---
+++ contributions/raghu/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Common mistake on switch statements
+Connecting to FTP using Java
