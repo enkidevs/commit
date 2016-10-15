3314ddb6913a819b3b5c076ba8fb1ea2e15fe6fd
Senopratomo <lord.ernesto@gmail.com>
2016-10-15 16:24:21 +0100

    2016-10-15's Java workout

---
+++ contributions/Senopratomo/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
