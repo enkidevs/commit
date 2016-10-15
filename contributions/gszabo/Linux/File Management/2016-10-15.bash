3a0bec11fd3ad11da77567e1fd8c3c9a7f2455a9
gszabo <harry.bme@gmail.com>
2016-10-15 08:09:38 +0100

    2016-10-15's Linux workout

---
+++ contributions/gszabo/Linux/File Management/2016-10-15.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
