6b9255c2892003747bbfd686b2c1212fee3ac2f5
ggin <guillaume.a.giner@gmail.com>
2016-10-15 07:52:17 +0100

    2016-10-15's Java workout

---
+++ contributions/ggin/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
