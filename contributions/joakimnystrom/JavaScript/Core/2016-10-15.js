af2fd14eed97f0b1de1632164bb274494a1de147
joakimnystrom <jnystromdesign@gmail.com>
2016-10-15 10:47:05 +0100

    2016-10-15's JavaScript workout

---
+++ contributions/joakimnystrom/JavaScript/Core/2016-10-15.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Hoisting applies only to variable declarations, not initializations
+Apply `map` function to array items
+Functions return `undefined` by default
+Context in JavaScript
