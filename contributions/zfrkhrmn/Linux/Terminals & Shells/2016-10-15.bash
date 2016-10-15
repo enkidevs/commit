24126118539329a263955b57d028a5cd43626051
zfrkhrmn <zaferkahraman123@gmail.com>
2016-10-15 14:58:31 +0100

    2016-10-15's Linux workout

---
+++ contributions/zfrkhrmn/Linux/Terminals & Shells/2016-10-15.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Terminal based browser
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
