c52f47bda5a370c7cc3bd014de040b28bf0e4e49
anitakar <akarbow@gmail.com>
2016-10-15 22:51:49 +0100

    2016-10-15's Linux workout

---
+++ contributions/anitakar/Linux/Terminals & Shells/2016-10-15.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear the terminal instantly
+Clear bash history
+Adding directories to your `$PATH`
