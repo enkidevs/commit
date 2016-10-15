7cbc323f519806e05e2217f32165ed815732530f
GameGhost <gameghost@bigfoot.com>
2016-10-15 22:24:20 +0100

    2016-10-15's JavaScript workout

---
+++ contributions/GameGhost/JavaScript/Npm/2016-10-15.js
@@ -0,0 +1,5 @@
+Registry user accounts for npm
+Combining script commands in npm
+`npm` verifies `sha1` hashes of packages
+`config` object in `package.json`
+Lock down dependency versions by shrinkwrapping
