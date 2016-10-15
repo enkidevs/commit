752de549d9269a4b87aeab724dcdb768fcf2eac2
Sanyo <micmark6@gmail.com>
2016-10-15 11:36:41 +0100

    2016-10-15's Linux workout

---
+++ contributions/Sanyo/Linux/Terminals & Shells/2016-10-15.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Random password generator
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
