96decd0e4adedf9f6736638c81fcfe77ff17b00f
GoosvandenBekerom <goos.bekerom@gmail.com>
2016-10-15 12:22:31 +0100

    2016-10-15's JavaScript workout

---
+++ contributions/GoosvandenBekerom/JavaScript/Npm/2016-10-15.js
@@ -0,0 +1,5 @@
+npm can handle multiple versions of a dependency
+Passing through command line arguments in npm scripts
+List binaries for scripting in npm
+Manage local node modules with `npm link`
+Current Lifecycle Event
