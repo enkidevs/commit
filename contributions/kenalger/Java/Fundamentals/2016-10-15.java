693aed8f0500c9b4da1bd15fdd28a89d803f4f72
kenalger <kenalger@comcast.net>
2016-10-15 16:17:29 +0100

    2016-10-15's Java workout

---
+++ contributions/kenalger/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Floating garbage and how to deal with it
+Use meaningful names
+Inheriting a constructor from a superclass
+Metadata: creating a user-defined file attribute
