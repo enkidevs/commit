52541125b31dad0770f531ae26c48ca82b450d5e
bkrem <ben.kremer@hotmail.co.uk>
2016-10-15 17:45:31 +0100

    2016-10-14's Linux workout

---
+++ contributions/bkrem/Linux/File Management/2016-10-14.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Delete commands aliases
