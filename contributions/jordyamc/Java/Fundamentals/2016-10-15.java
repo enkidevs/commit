efa3a97e460b6c81aafab706bc9ddf8906beabcb
jordyamc <jordyamc@hotmail.com>
2016-10-15 17:24:36 +0100

    2016-10-15's Java workout

---
+++ contributions/jordyamc/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Always specify type parameter
