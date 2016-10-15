f00470f4a4656b4dd27f0eafccc5e982e939e9f3
asicoderpower <asicoderpower@gmail.com>
2016-10-15 14:46:19 +0100

    2016-10-15's Java workout

---
+++ contributions/asicoderpower/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+Writing generic methods
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
