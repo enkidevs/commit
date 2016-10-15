5e15c829d5acbe67b179fb3f0e8593a07083b0f0
e7krgc <enam93@gmail.com>
2016-10-15 08:26:09 +0100

    2016-10-15's Linux workout

---
+++ contributions/e7krgc/Linux/Terminals & Shells/2016-10-15.bash
@@ -0,0 +1,7 @@
+Find any Unix / Linux command
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+The Basics of Environment Variables
