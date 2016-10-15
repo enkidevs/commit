8fb4eb3e3de75e18f6bfe29449419189bce8e1ef
zszuri <szuri90@gmail.com>
2016-10-15 12:35:38 +0100

    2016-10-15's Java workout

---
+++ contributions/zszuri/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
