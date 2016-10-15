7162c021c9766465aa10c09c7a2a6b333cb9ed33
bbohac <bastian.bohac@gmail.com>
2016-10-15 07:09:14 +0100

    2016-10-15's Java workout

---
+++ contributions/bbohac/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Use meaningful names
+The trade-off between short and descriptive names
+Writing generic methods
+Distinction between `public` and `private` methods
+Inheriting a constructor from a superclass
