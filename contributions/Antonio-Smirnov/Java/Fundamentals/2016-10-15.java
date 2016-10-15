4f894dea9517b69beeb913eccfb234d745ee5990
Antonio-Smirnov <toxa.inc@mail.ru>
2016-10-15 08:48:58 +0100

    2016-10-15's Java workout

---
+++ contributions/Antonio-Smirnov/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Do not return *references* to private *mutable* class members
