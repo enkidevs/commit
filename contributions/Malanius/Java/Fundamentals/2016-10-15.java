06364ec1b33f3e768169f4ad6fa1765b76a1d598
Malanius <malanius@seznam.cz>
2016-10-15 07:00:31 +0100

    2016-10-15's Java workout

---
+++ contributions/Malanius/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Overloading Methods in Java
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
