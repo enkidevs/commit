0495d3ff878bb92c934f5b96b7cc80bb8ecc3d0f
zero9 <c.lobrano@gmail.com>
2016-10-15 21:12:37 +0100

    2016-10-15's Linux workout

---
+++ contributions/zero9/Linux/Jobs & Processes/2016-10-15.bash
@@ -0,0 +1,5 @@
+Backgrounding tasks 
+`Cron` dot-in-filename issues when using `run-parts`
+Common issue with scripts in `cron`
+Set a process priority
+How to pause a process and run others in the background
