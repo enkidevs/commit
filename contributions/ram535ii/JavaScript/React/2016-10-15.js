c27203dea5d43c36110fd72a6b6474606f7e597a
ram535ii <constantijnschepens@gmail.com>
2016-10-15 17:11:28 +0100

    2016-10-15's JavaScript workout

---
+++ contributions/ram535ii/JavaScript/React/2016-10-15.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+componentWillReceiveProps Not Triggered After Mounting
+
+Use **React** with other libraries
+`PureRenderMixin` in **React**
+Custom validations for props
