f76d557ae9f532d49bfcf61ee6970b47579e43e9
seergiioo6 <hello@sergioandres.io>
2016-10-15 14:29:28 +0100

    2016-10-15's JavaScript workout

---
+++ contributions/seergiioo6/JavaScript/Node/2016-10-15.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+The built-in Node debugger
+Streams in **Node**
+Use conventional property names for error handling in Node.js
+`exports` vs. `module.exports` in **Node**
