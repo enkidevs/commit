5d7185edb22e9dff5b3d427440895826ef956393
MonteC <jarekucinski@gmail.com>
2016-10-15 17:50:42 +0100

    2016-10-15's Java workout

---
+++ contributions/MonteC/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Using the `Console`
+Using `Printstream.format` to format a string before printing
+Limit Accessibility of `Fields`
+Using an interface as a parameter
