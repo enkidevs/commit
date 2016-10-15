93691b16f53e4b1cd2cbd0c04a990af48c11a410
aakarsh-gupta <aakarsh.gupta.in@gmail.com>
2016-10-15 12:44:40 +0100

    2016-10-14's Java workout

---
+++ contributions/aakarsh-gupta/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Updating interfaces by using `default` methods
+Measuring time
+Detect or prevent integer overflow
+Supply `toString()` in all classes
