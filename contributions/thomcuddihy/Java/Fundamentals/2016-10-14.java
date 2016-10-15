a1891d19a5cd3a97db4d7c080092ee5d299377c3
thomcuddihy <thom@hometreelab.com>
2016-10-15 08:53:53 +0100

    2016-10-14's Java workout

---
+++ contributions/thomcuddihy/Java/Fundamentals/2016-10-14.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
