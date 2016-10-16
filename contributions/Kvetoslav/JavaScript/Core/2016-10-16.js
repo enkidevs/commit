3daf5835a5bc5237abe1d2fba108abb761f17805
Kvetoslav <jiri.heneberk@icompllex.com>
2016-10-16 12:19:09 +0100

    2016-10-16's JavaScript workout

---
+++ contributions/Kvetoslav/JavaScript/Core/2016-10-16.js
@@ -0,0 +1,5 @@
+Get a random item from an Array
+Setting the length of an array 
+Data type comparison in `switch` statements
+Prevent background image flicker in **IE**
+Detect **DO NOT TRACK** status
