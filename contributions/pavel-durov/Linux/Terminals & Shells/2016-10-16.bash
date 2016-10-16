d9c7e7cf862fe07baa1562f23d3231ef8440151b
pavel-durov <pdurov0@gmail.com>
2016-10-16 06:44:55 +0100

    2016-10-16's Linux workout

---
+++ contributions/pavel-durov/Linux/Terminals & Shells/2016-10-16.bash
@@ -0,0 +1,7 @@
+Clear bash history
+Terminal based browser
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
