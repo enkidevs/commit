d7639925ef71a7e1848a5644d177baebc2a5afa1
doomi <kurai7hikari@gmail.com>
2016-10-16 12:31:02 +0100

    2016-10-16's Linux workout

---
+++ contributions/doomi/Linux/File Management/2016-10-16.bash
@@ -0,0 +1,7 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
