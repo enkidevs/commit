6311830e5a12889e076b65e8b5bc8002309bab3c
leriaz <leriaz17@gmail.com>
2016-10-16 19:04:34 +0100

    2016-10-16's JavaScript workout

---
+++ contributions/leriaz/JavaScript/Core/2016-10-16.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Labeling your loops
+`String.replace`
+Hoisting applies only to variable declarations, not initializations
+Extra function arguments are undefined by default
