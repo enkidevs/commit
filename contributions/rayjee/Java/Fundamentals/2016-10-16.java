b0a4d0483f664d77d59bf192f8ad87dabc4fbf1d
rayjee <formergod@gmail.com>
2016-10-16 11:49:11 +0100

    2016-10-16's Java workout

---
+++ contributions/rayjee/Java/Fundamentals/2016-10-16.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Writing generic methods
+Distinction between `public` and `private` methods
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
