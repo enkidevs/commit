b1741720c81a854130d5a7c0c806fe4acd4eeda6
kubik <lkubic@poczta.onet.pl>
2016-10-16 12:16:29 +0100

    2016-10-16's Java workout

---
+++ contributions/kubik/Java/Data Types/2016-10-16.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Do not attempt comparisons with NaN
+Numeric Conversion - Widening
