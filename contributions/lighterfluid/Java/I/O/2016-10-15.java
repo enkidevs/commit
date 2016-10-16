8853b8650f7cac93a5341dd92668d44f792ab484
lighterfluid <2yusaku@gmail.com>
2016-10-16 07:47:25 +0100

    2016-10-15's Java workout

---
+++ contributions/lighterfluid/Java/I/O/2016-10-15.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Retrieving file store attributes
+Listing a file system's root directories
+The `Console` class
