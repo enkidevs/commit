75450c905b18050639ed51da7df07ab792c7b370
phil-brainlabs <phil@brainlabsdigital.com>
2016-10-16 14:17:33 +0100

    2016-10-16's Linux workout

---
+++ contributions/phil-brainlabs/Linux/Terminals & Shells/2016-10-16.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
