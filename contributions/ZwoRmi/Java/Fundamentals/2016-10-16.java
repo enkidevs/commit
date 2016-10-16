5dddff2a1f3b0ce9c1bf609cdb1b93ac6ff4a69a
ZwoRmi <lemaire.lucas1@gmail.com>
2016-10-16 14:52:33 +0100

    2016-10-16's Java workout

---
+++ contributions/ZwoRmi/Java/Fundamentals/2016-10-16.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
