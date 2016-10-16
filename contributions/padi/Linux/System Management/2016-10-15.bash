6d0fe2210892afb5266b89f984c27397928f7f86
padi <marcrendlignacio@gmail.com>
2016-10-16 13:58:59 +0100

    2016-10-15's Linux workout

---
+++ contributions/padi/Linux/System Management/2016-10-15.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
