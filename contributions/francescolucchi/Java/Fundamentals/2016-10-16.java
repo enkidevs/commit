01f62b78f2bd690fc9990e98f61b1fcc7feb21c6
francescolucchi <francescolucchi@gmail.com>
2016-10-16 22:52:48 +0100

    2016-10-16's Java workout

---
+++ contributions/francescolucchi/Java/Fundamentals/2016-10-16.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
