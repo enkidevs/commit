9ba40393f204f223b9eb977368b506f6e1d25226
nikos <triantafyllou.ni@gmail.com>
2016-10-16 11:03:25 +0100

    2016-10-16's Java workout

---
+++ contributions/nikos/Java/Fundamentals/2016-10-16.java
@@ -0,0 +1,5 @@
+Overloading Methods in Java
+Connecting to FTP using Java
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Avoid using `static` variables
