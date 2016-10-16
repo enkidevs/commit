8891e4efbf5e0028668890a5f9ec117f5c5420e6
kimrip <kimrip@gmail.com>
2016-10-16 23:13:50 +0100

    2016-10-16's Git workout

---
+++ contributions/kimrip/Git/Essentials/2016-10-16.bash
@@ -0,0 +1,5 @@
+You can use git offline!
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Modifying a Previous Commit
