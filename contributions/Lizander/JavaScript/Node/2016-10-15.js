bf46676ff89422c471c064c209823dca3ef42111
Lizander <kevin9075@gmail.com>
2016-10-16 01:36:54 +0100

    2016-10-15's JavaScript workout

---
+++ contributions/Lizander/JavaScript/Node/2016-10-15.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Streams in **Node**
+Wrapping errors in Node.js using _node-verror_
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
