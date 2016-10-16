4daf59508a28413388a612f160cd1844eca8a82a
jvlobo <jvlg2002@hotmail.com>
2016-10-16 18:33:04 +0100

    2016-10-16's JavaScript workout

---
+++ contributions/jvlobo/JavaScript/Functions/2016-10-16.js
@@ -0,0 +1,5 @@
+Hoisting
+Function constructor vs. function declaration vs. function expression
+`.bind()` 
+Easily generate a random `HEX` color
+Changing a functions context with `fn.call(object)`
