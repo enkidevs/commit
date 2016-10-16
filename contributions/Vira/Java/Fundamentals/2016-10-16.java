b3331c21d2afbb730336ac34b066445444fdaf12
Vira <thomas.ruehrig@studierende.htl-donaustadt.at>
2016-10-16 10:56:48 +0100

    2016-10-16's Java workout

---
+++ contributions/Vira/Java/Fundamentals/2016-10-16.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
