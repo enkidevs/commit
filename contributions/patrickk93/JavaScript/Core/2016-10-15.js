962d7f21f4b0b2446880ef5f5377efc5bb426482
patrickk93 <patrickkim93@gmail.com>
2016-10-16 14:10:05 +0100

    2016-10-15's JavaScript workout

---
+++ contributions/patrickk93/JavaScript/Core/2016-10-15.js
@@ -0,0 +1,5 @@
+Labeling your loops
+Data type comparison in `switch` statements
+Counting the months from zero
+Functions return `undefined` by default
+Extra function arguments are undefined by default
