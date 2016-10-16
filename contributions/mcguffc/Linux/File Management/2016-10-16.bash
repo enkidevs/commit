61564af679ada7bbe74726371de787b22858c9ea
mcguffc <chris.mcguffin87@gmail.com>
2016-10-16 20:18:28 +0100

    2016-10-16's Linux workout

---
+++ contributions/mcguffc/Linux/File Management/2016-10-16.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
+Easy way of sharing files
