5d023c580e1c320a1428f77e07934c3a057c69dc
wes977 <wes977@gmail.com>
2016-10-16 17:52:40 +0100

    2016-10-16's Java workout

---
+++ contributions/wes977/Java/Fundamentals/2016-10-16.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Always specify type parameter
+Increase `PermGen` space as to avoid `OutOfMemory` errors
