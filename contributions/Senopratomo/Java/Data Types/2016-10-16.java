9c7523117bebea456146625c7bcd3966eb6f8029
Senopratomo <lord.ernesto@gmail.com>
2016-10-16 23:24:54 +0100

    2016-10-16's Java workout

---
+++ contributions/Senopratomo/Java/Data Types/2016-10-16.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Retrieve the component type of an array
+String concatenation in Java
+Do not attempt comparisons with NaN
