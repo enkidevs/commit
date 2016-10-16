4b1518c5013c035e164e064a46143b0839f92d8a
rblakejohnson <rblakejohnson@gmail.com>
2016-10-16 06:45:58 +0100

    2016-10-15's JavaScript workout

---
+++ contributions/rblakejohnson/JavaScript/Npm/2016-10-15.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+`npm` verifies `sha1` hashes of packages
+`optionalDependencies` in npm
+Package distribution tags
+Lock down dependency versions by shrinkwrapping
