1fe427061000bb3425dfdfba15c54a29a92f5799
shark09 <simon.dalvy@gmail.com>
2016-10-16 21:28:18 +0100

    2016-10-16's Linux workout

---
+++ contributions/shark09/Linux/File Management/2016-10-16.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
