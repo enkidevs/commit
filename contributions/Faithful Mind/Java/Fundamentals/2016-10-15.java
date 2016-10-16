8e582e4ea41ec11395bfe8097d19b9124177f235
Faithful Mind <lpyy15@gmail.com>
2016-10-16 17:09:41 +0100

    2016-10-15's Java workout

---
+++ contributions/Faithful Mind/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Do not return *references* to private *mutable* class members
