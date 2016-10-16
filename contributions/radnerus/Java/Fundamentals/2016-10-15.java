1543b5e428f530c4bb888a862be36a363ef557bc
radnerus <surendarandyou@gmail.com>
2016-10-16 06:20:04 +0100

    2016-10-15's Java workout

---
+++ contributions/radnerus/Java/Fundamentals/2016-10-15.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
