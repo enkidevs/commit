df3ab2f76c69b17a4c4342d2a7f91c9df4634b99
cmbpgh <cmbpgh@gmail.com>
2016-10-16 02:25:12 +0100

    2016-10-15's JavaScript workout

---
+++ contributions/cmbpgh/JavaScript/React/2016-10-15.js
@@ -0,0 +1,6 @@
+`null` value for *Controlled Components* in **React**
+Props in getInitialState Is an        
+Anti-Pattern
+Dealing with `this.props.children`
+`ReactDOM.render` ref
+Specify a single child
