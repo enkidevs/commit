9533babc9221432b6957b7fef3ce48b040dfc5e7
stevedien <lulzsecd@gmail.com>
2016-10-16 15:19:18 +0100

    2016-10-15's Java workout

---
+++ contributions/stevedien/Java/Data Types/2016-10-15.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Converting numbers to strings
+Equals operation on different data types
+Numeric Conversion - Widening
