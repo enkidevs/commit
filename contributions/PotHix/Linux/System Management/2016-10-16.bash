7f86221006437a7834c86b1b43ab5a84cf59ae2f
PotHix <pothix@pothix.com>
2016-10-16 13:32:57 +0100

    2016-10-16's Linux workout

---
+++ contributions/PotHix/Linux/System Management/2016-10-16.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
