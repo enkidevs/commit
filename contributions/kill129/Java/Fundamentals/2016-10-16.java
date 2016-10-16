dbaa0daf3c21db3ff0b23c8b8f5d125b46e6992d
kill129 <xxalon1996xx@gmail.com>
2016-10-16 09:23:32 +0100

    2016-10-16's Java workout

---
+++ contributions/kill129/Java/Fundamentals/2016-10-16.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
+Avoid using `static` variables
