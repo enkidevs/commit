25c59e8b00a183b0a5fcd2927bb2b2f79c9c24b0
zszuri <szuri90@gmail.com>
2016-10-16 12:12:06 +0100

    2016-10-16's Java workout

---
+++ contributions/zszuri/Java/Fundamentals/2016-10-16.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
