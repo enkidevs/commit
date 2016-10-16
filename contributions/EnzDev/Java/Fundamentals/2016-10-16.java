6f18be164ee30477d6271eb3d03d23dab6531d1a
EnzDev <enzo@mallard.ml>
2016-10-16 10:04:22 +0100

    2016-10-16's Java workout

---
+++ contributions/EnzDev/Java/Fundamentals/2016-10-16.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
