ad0d586faf683c5a7a7ba6702b50c92fd37ba44d
eney <jzabrodin@gmail.com>
2016-10-16 18:48:28 +0100

    2016-10-16's Linux workout

---
+++ contributions/eney/Linux/Networking/2016-10-16.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+Getting the connection speed from the terminal
+Bandwidth monitoring tools 
+Use `tcpdump` to listen to network interface traffic
+Short Introduction to `Wget` 
