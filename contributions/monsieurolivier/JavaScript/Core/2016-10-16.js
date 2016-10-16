a53bec789486c101880ef57235b76e4de74dab63
monsieurolivier <monsieur.olivier@gmail.com>
2016-10-16 14:00:04 +0100

    2016-10-16's JavaScript workout

---
+++ contributions/monsieurolivier/JavaScript/Core/2016-10-16.js
@@ -0,0 +1,5 @@
+Shuffle an array using `sort` and `random`.
+`if/else` shortcut `conditional operator`
+Use `splice()` to remove an item from an array
+Round numbers to `N` decimals 
+Base conversion 
