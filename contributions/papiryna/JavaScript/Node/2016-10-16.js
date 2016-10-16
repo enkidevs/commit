6bfcca3ed641e84ecd8655565effd3c8ecaa16be
papiryna <papiryna@gmail.com>
2016-10-16 16:32:33 +0100

    2016-10-16's JavaScript workout

---
+++ contributions/papiryna/JavaScript/Node/2016-10-16.js
@@ -0,0 +1,5 @@
+First-error callbacks in **Node**
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
