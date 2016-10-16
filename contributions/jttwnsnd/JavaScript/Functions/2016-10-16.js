13ac5aa25f8dbe21c7ce183416e5ab3497ca6800
jttwnsnd <jt.a.townsend@gmail.com>
2016-10-16 15:49:34 +0100

    2016-10-16's JavaScript workout

---
+++ contributions/jttwnsnd/JavaScript/Functions/2016-10-16.js
@@ -0,0 +1,5 @@
+Hoisting
+Function constructor vs. function declaration vs. function expression
+Anonymous functions
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
