4da43b061f76345bc9587496bff4268b6989d7df
psychorobotninja <tefuller@gmail.com>
2016-10-17 00:47:09 +0100

    2016-10-16's Java workout

---
+++ contributions/psychorobotninja/Java/Fundamentals/2016-10-16.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Using an interface as a parameter
