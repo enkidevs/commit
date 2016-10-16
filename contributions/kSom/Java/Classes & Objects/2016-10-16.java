9cedb44e4b5b112da1c9a3c41b6f15e1a346dd61
kSom <kgsommer@gmail.com>
2016-10-16 17:46:37 +0100

    2016-10-16's Java workout

---
+++ contributions/kSom/Java/Classes & Objects/2016-10-16.java
@@ -0,0 +1,5 @@
+Using `static` fields
+Utility classes and `static` methods
+Double Brace Initialization
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
