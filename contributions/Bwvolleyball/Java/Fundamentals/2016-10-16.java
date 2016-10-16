67b677ccde978595803f56ffa51bb902ebce0ca2
Bwvolleyball <ward486@regis.edu>
2016-10-16 16:17:52 +0100

    2016-10-16's Java workout

---
+++ contributions/Bwvolleyball/Java/Fundamentals/2016-10-16.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
