27e47a1a1c7c9ed0f6a5ea8a896f2873706454f7
Catvanoh <pavelpie@gmail.com>
2016-10-16 12:13:51 +0100

    2016-10-16's Git workout

---
+++ contributions/Catvanoh/Git/Essentials/2016-10-16.bash
@@ -0,0 +1,5 @@
+Limiting log output by time
+Check the status of your files
+Use `short` status to make output more compact
+How to write a git commit message
+Remote repositories: fetching and pushing
