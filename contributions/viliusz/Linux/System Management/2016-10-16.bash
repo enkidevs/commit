cbc28babf7182cf5218ee2d16e7cabb54f4e15cc
viliusz <enki@gmn.lt>
2016-10-16 21:47:24 +0100

    2016-10-16's Linux workout

---
+++ contributions/viliusz/Linux/System Management/2016-10-16.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
