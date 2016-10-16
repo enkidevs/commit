578217cb2f1fd0e7da3def4ab7bad7fa326686d8
samalazau <samalazau@gmail.com>
2016-10-16 22:23:51 +0100

    2016-10-16's Linux workout

---
+++ contributions/samalazau/Linux/Permissions In Linux/2016-10-16.bash
@@ -0,0 +1,6 @@
+File Ownership
+Changing File Ownership With `chown`
+Changing File Permissions With `chmod`
+Adding New Users With `useradd`
+Changing Group Membership With `usermod`
+Changing User Passwords With `passwd`
