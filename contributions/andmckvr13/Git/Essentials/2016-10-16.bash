1a68ecd41d97b669fe47bee8a03353f48d67d349
andmckvr13 <andmckvr@gmail.com>
2016-10-16 13:31:49 +0100

    2016-10-16's Git workout

---
+++ contributions/andmckvr13/Git/Essentials/2016-10-16.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+What is stored in a commit?
+Limiting log output by time
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
