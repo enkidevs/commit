471e609350c7c77cb05c08956c4516ec1c730c18
Lacuno <christoph.weiler@yahoo.de>
2016-10-16 08:33:48 +0100

    2016-10-16's JavaScript workout

---
+++ contributions/Lacuno/JavaScript/Functions/2016-10-16.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
