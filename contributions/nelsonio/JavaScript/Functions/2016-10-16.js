b0354fa030d029aaf98ce2d29c36a6d402f939ea
nelsonio <martin23haupt@gmail.com>
2016-10-16 13:34:03 +0100

    2016-10-16's JavaScript workout

---
+++ contributions/nelsonio/JavaScript/Functions/2016-10-16.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+`.bind()` 
+Get query/url variables
+Changing a functions context with `fn.call(object)`
