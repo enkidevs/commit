68c360182c3c170217296b663889b33fb9cba317
archligt <frodo.archligt@gmail.com>
2016-10-16 15:06:02 +0100

    2016-10-16's Linux workout

---
+++ contributions/archligt/Linux/File Management/2016-10-16.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Deleting files in a secure manner
