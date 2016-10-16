58cd5a0a0bb3bbb82bf404d8d80c56854fbd6b4a
alex.expertvision <alexandru.balasa@rocketmail.com>
2016-10-16 09:13:49 +0100

    2016-10-16's JavaScript workout

---
+++ contributions/alex.expertvision/JavaScript/Core/2016-10-16.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Format currency in JavaScript
+Filtering items out of an array
+Prevent background image flicker in **IE**
+Inaccuracy of binary floating-point format
