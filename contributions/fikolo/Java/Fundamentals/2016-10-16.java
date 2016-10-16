33fcf177b7e88d74f38e7cb14706ef9142060fd4
fikolo <filipdalorenzo@gmail.com>
2016-10-16 08:03:57 +0100

    2016-10-16's Java workout

---
+++ contributions/fikolo/Java/Fundamentals/2016-10-16.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Connecting to FTP using Java
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Supply `toString()` in all classes
