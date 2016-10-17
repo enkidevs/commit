48394b016732aa2f30c80f2b3c67534f451dbb11
tokibeck <tokibeck@gmail.com>
2016-10-17 22:49:46 +0100

    2016-10-17's Linux workout

---
+++ contributions/tokibeck/Linux/File Management/2016-10-17.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Easy way of sharing files
