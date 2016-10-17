9938ff2087a6699e1657eed9ae93d25b695c69a1
bendobot <whatsbending@gmail.com>
2016-10-17 19:43:58 +0100

    2016-10-17's Linux workout

---
+++ contributions/bendobot/Linux/System Management/2016-10-17.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
