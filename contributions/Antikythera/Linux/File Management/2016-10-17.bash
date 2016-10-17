2220d3327de773f29fedc787d342a74a456ca678
Antikythera <voodoomanic@gmail.com>
2016-10-17 18:50:49 +0100

    2016-10-17's Linux workout

---
+++ contributions/Antikythera/Linux/File Management/2016-10-17.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
