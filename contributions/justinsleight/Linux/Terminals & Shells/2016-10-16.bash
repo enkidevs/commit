925975ae0b044d34c98120a4cd6046671d837f80
justinsleight <sleightjustin@gmail.com>
2016-10-17 06:09:53 +0100

    2016-10-16's Linux workout

---
+++ contributions/justinsleight/Linux/Terminals & Shells/2016-10-16.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
