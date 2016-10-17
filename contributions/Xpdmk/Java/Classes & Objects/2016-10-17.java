e83a07ddeee67253d91b45626cb096043442585d
Xpdmk <roy.jarvinen@gmail.com>
2016-10-17 16:19:48 +0100

    2016-10-17's Java workout

---
+++ contributions/Xpdmk/Java/Classes & Objects/2016-10-17.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Utility classes and `static` methods
+Compare classes and not class names
+How and when to use `WeakHashMap` class
