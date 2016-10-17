5b957c82dbab397efc875b2e6c6a4262e85c0969
JakeRenn <rencjviei@gmail.com>
2016-10-17 11:24:31 +0100

    2016-10-16's Linux workout

---
+++ contributions/JakeRenn/Linux/System Management/2016-10-16.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
