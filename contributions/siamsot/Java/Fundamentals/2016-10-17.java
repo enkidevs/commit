97f124e99383cf004f7f89d95e595bde2a46648a
siamsot <siamsot@gmail.com>
2016-10-17 11:32:39 +0100

    2016-10-17's Java workout

---
+++ contributions/siamsot/Java/Fundamentals/2016-10-17.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
