a284a39be203c84f024040efd71bae4745f94bfe
DVDripXvid <dvdrip.xvid.avi@gmail.com>
2016-10-17 14:17:16 +0100

    2016-10-17's Java workout

---
+++ contributions/DVDripXvid/Java/Fundamentals/2016-10-17.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Common mistake on switch statements
+Connecting to FTP using Java
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
