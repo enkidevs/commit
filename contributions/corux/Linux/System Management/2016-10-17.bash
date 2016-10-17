22d8fb1f1c91f2b4b3f96e54cb55acdef2f7aafb
corux <tobias@wallura.org>
2016-10-17 07:12:47 +0100

    2016-10-17's Linux workout

---
+++ contributions/corux/Linux/System Management/2016-10-17.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
