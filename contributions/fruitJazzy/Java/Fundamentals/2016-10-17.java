4829ce949bf5c083a09dfff257ecf22d3be4ef9b
fruitJazzy <li4drow@gmail.com>
2016-10-17 12:08:55 +0100

    2016-10-17's Java workout

---
+++ contributions/fruitJazzy/Java/Fundamentals/2016-10-17.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Always specify type parameter
