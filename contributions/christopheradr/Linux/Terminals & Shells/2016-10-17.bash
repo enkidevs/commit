fe68bbe5aa8d0dcb4565c55fcd37a8e2f42095f8
christopheradr <christopher.diaz.riv@gmail.com>
2016-10-17 13:51:55 +0100

    2016-10-17's Linux workout

---
+++ contributions/christopheradr/Linux/Terminals & Shells/2016-10-17.bash
@@ -0,0 +1,5 @@
+Find any Unix / Linux command
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
