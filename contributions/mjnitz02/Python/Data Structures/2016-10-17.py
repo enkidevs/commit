e6f831cb6260aec98f06a94e211897e278032b67
mjnitz02 <mjnitz02@gmail.com>
2016-10-17 14:58:50 +0100

    2016-10-17's Python workout

---
+++ contributions/mjnitz02/Python/Data Structures/2016-10-17.py
@@ -0,0 +1,5 @@
+Following PEP 8 styling guideline.
+A simple way to select a random item from a `list/tuple` data stucture
+Your own Python `calendar` 
+When `range` comes in handy
+There is more to copying
