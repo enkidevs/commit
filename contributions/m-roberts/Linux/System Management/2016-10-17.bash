13b8991477cd9d05648faa90bc3625dca73afbe0
m-roberts <mike@pi-top.com>
2016-10-17 18:04:48 +0100

    2016-10-17's Linux workout

---
+++ contributions/m-roberts/Linux/System Management/2016-10-17.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
