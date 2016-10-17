1f2c698395999fa3b0c3a419d1f8a0a587581495
Morion <morion.self@gmail.com>
2016-10-17 06:50:27 +0100

    2016-10-16's Java workout

---
+++ contributions/Morion/Java/Fundamentals/2016-10-16.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between `==` and `equals()`
+Catch multiple exceptions in a single `catch` block
+Distinction between `public` and `private` methods
+Supply `toString()` in all classes
