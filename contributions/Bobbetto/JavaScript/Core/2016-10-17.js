4582441e02d5653cbaf3ecc58539a9d08d9df9d1
Bobbetto <bbobbetto@gmail.com>
2016-10-17 10:12:00 +0100

    2016-10-17's JavaScript workout

---
+++ contributions/Bobbetto/JavaScript/Core/2016-10-17.js
@@ -0,0 +1,5 @@
+Be careful when using `typeof`.
+`if/else` shortcut `conditional operator`
+Use `splice()` to remove an item from an array
+Using `.test()` with RegExp
+The difference between null, undefined and NaN
