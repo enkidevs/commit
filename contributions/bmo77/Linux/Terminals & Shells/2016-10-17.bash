a5b078e95a63fefe54a91cd343f1261c58dc228c
bmo77 <basha.others@gmail.com>
2016-10-17 12:56:10 +0100

    2016-10-17's Linux workout

---
+++ contributions/bmo77/Linux/Terminals & Shells/2016-10-17.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Clear bash history
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
