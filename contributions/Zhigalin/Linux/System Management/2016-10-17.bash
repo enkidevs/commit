c54ceabce8150cd0496ba8237b1f766c34018d56
Zhigalin <alexander@zhigalin.tk>
2016-10-17 23:10:55 +0100

    2016-10-17's Linux workout

---
+++ contributions/Zhigalin/Linux/System Management/2016-10-17.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Detect your linux distribution
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Commands to shutdown or restart the system
