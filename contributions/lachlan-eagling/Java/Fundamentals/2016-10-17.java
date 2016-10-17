533ea70164d1d56d9a147571b2ed760cc4066915
lachlan-eagling <lochie.eagling@gmail.com>
2016-10-17 21:22:59 +0100

    2016-10-17's Java workout

---
+++ contributions/lachlan-eagling/Java/Fundamentals/2016-10-17.java
@@ -0,0 +1,5 @@
+Use meaningful names
+The trade-off between short and descriptive names
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+Locks in `static synchronized` methods
