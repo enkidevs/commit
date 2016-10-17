adff110caa229705e2268308ef2afdccaf185d12
kRyM1337 <leonid.yanchis@gmail.com>
2016-10-17 14:35:03 +0100

    2016-10-17's Java workout

---
+++ contributions/kRyM1337/Java/Fundamentals/2016-10-17.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
