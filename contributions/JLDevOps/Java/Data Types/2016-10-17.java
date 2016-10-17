768453ca34ffe5b2b42fb2edbf585bea5f1f2848
JLDevOps <jldevops@gmail.com>
2016-10-17 19:10:29 +0100

    2016-10-17's Java workout

---
+++ contributions/JLDevOps/Java/Data Types/2016-10-17.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Converting numbers to strings
+Finding a substring in a string
+Retrieve the component type of an array
+String concatenation in Java
