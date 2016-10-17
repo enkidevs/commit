9553a6e969afb1a4d1958ee65a06df27f047bb0a
kkothari93 <krk3mar@gmail.com>
2016-10-17 15:32:07 +0100

    2016-10-17's Linux workout

---
+++ contributions/kkothari93/Linux/Terminals & Shells/2016-10-17.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+Retrieve previous arguments
+The Basics of Environment Variables
