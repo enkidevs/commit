922d09835af1a4b25140972c0061fb5cfc720c4f
Aylard <aylard7@gmail.com>
2016-10-17 13:53:37 +0100

    2016-10-17's Linux workout

---
+++ contributions/Aylard/Linux/System Management/2016-10-17.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+List current logged on users with `w`
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
