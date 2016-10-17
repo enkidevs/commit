acedaf00f2930c8745b08be8799803df8b5f6ed0
Drnkn <prehistoric2003@gmail.com>
2016-10-17 10:23:04 +0100

    2016-10-17's Java workout

---
+++ contributions/Drnkn/Java/Fundamentals/2016-10-17.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
