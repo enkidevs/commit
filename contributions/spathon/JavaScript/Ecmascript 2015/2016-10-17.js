574cfd40bf6e09eaba91fb2990fe83b34981cd58
spathon <enki@spathon.com>
2016-10-17 07:18:14 +0100

    2016-10-17's JavaScript workout

---
+++ contributions/spathon/JavaScript/Ecmascript 2015/2016-10-17.js
@@ -0,0 +1,5 @@
+ES6 dynamic property keys in mongo-like set clauses
+Defaults values apply only to `undefined` (and not to `null`)
+Creating promises
+Generators as observers in ES6
+Tail call optimisation in ES6
