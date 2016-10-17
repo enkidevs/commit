2e5bfa8d6a1587b3dc7986991548fa693f84b2d7
bfalala <fbenj1988@gmail.com>
2016-10-17 07:51:05 +0100

    2016-10-17's JavaScript workout

---
+++ contributions/bfalala/JavaScript/Core/2016-10-17.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Keywords in hash keys
+Use `splice()` to remove an item from an array
+The difference between null, undefined and NaN
+Detect an error type
