316ad16cce83c40b2fc6695b59725beef715c4d7
CasperJon <kjonczyk99@gmail.com>
2016-10-17 20:51:57 +0100

    2016-10-17's Java workout

---
+++ contributions/CasperJon/Java/Fundamentals/2016-10-17.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
