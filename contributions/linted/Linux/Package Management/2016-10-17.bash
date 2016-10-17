837aaae3972381384feca972bd959e5844bb494d
linted <linted90@hotmail.com>
2016-10-17 17:35:53 +0100

    2016-10-17's Linux workout

---
+++ contributions/linted/Linux/Package Management/2016-10-17.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Removing PPAs
+Prevent updating a specific package in Debian systems
+Get `apt` to use a mirror / faster mirror
+Downgrade a package via `apt-get`
