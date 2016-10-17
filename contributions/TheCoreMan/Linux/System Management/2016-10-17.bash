052b3857beb98f6bfdfc7c0f5692db1150c1ab7d
TheCoreMan <dude500@gmail.com>
2016-10-17 10:29:37 +0100

    2016-10-17's Linux workout

---
+++ contributions/TheCoreMan/Linux/System Management/2016-10-17.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
