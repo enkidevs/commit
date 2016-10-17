ffdb0a455fef1915d3cb61b73580490f98478e5e
bobahrus <bobahrus@gmail.com>
2016-10-17 09:17:31 +0100

    2016-10-17's Java workout

---
+++ contributions/bobahrus/Java/Data Types/2016-10-17.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Converting a string to upper or lower case
+Finding a substring in a string
+`StringBuffer` vs `StringBuilder`
