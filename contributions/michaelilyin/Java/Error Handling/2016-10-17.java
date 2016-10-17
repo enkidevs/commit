8462c0f51aeb9009e81866ddbcee0a53b0e11f47
michaelilyin <michael.s.ilyin@gmail.com>
2016-10-17 12:05:35 +0100

    2016-10-17's Java workout

---
+++ contributions/michaelilyin/Java/Error Handling/2016-10-17.java
@@ -0,0 +1,4 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
