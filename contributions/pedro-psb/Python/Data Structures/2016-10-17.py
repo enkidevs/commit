652e4f3821978ac1a046f75685b6cfb80559bc48
pedro-psb <pedropsb95@gmail.com>
2016-10-17 16:04:08 +0100

    2016-10-17's Python workout

---
+++ contributions/pedro-psb/Python/Data Structures/2016-10-17.py
@@ -0,0 +1,5 @@
+Following PEP 8 styling guideline.
+A simple way to select a random item from a `list/tuple` data stucture
+Your own Python `calendar` 
+Get the most of `int`s
+Get the most of `float`s
