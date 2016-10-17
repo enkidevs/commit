0e7cb56844bd52b76cdec64757504dcc6035f026
guilef <gfajula@gmail.com>
2016-10-17 23:00:07 +0100

    2016-10-17's JavaScript workout

---
+++ contributions/guilef/JavaScript/Core/2016-10-17.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Data type comparison in `switch` statements
+Double and single quotes
+The distinction between `==` and `===`
+The difference between null, undefined and NaN
