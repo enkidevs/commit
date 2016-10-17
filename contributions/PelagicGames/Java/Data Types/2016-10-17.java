d532c26677e0aa475c43ae4d47ac47c330b06ccf
PelagicGames <games.pelagic@gmail.com>
2016-10-17 18:25:59 +0100

    2016-10-17's Java workout

---
+++ contributions/PelagicGames/Java/Data Types/2016-10-17.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Converting a string to upper or lower case
+Finding a substring in a string
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
