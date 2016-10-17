00a1b8753de9c0864df1c437759aaa1e2ad66eab
Kandit <loomlisp@gmail.com>
2016-10-17 16:37:25 +0100

    2016-10-17's Linux workout

---
+++ contributions/Kandit/Linux/Terminals & Shells/2016-10-17.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Find any Unix / Linux command
+Random password generator
+Adding directories to your `$PATH`
