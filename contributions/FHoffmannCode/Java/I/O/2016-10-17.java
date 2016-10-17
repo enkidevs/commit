02628c94001cdf4a52316275233c775c04a388e4
FHoffmannCode <f.higuain@gmail.com>
2016-10-17 07:23:09 +0100

    2016-10-17's Java workout

---
+++ contributions/FHoffmannCode/Java/I/O/2016-10-17.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Checking for the existence of files and directories
+Retrieving file store attributes
+Deleting files and directories
+The `Console` class
