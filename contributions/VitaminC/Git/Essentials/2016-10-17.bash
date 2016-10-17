9aa70cbad4521f51d3d95e0656a9a57ac306f54a
VitaminC <enki@j-salzer.de>
2016-10-17 15:56:24 +0100

    2016-10-17's Git workout

---
+++ contributions/VitaminC/Git/Essentials/2016-10-17.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+You can use git offline!
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
+Modifying a Previous Commit
