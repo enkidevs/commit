737d08574d9b93c960bbffe8775438b3f19f80b9
stufixlp <stufixlp@gmail.com>
2016-10-17 22:01:13 +0100

    2016-10-17's Java workout

---
+++ contributions/stufixlp/Java/Data Types/2016-10-17.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+`StringBuffer` vs `StringBuilder`
+Do not attempt comparisons with NaN
+Numeric Conversion - Widening
