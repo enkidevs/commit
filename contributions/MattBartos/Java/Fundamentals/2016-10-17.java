a0882ae5f834e78a48cef1299479893aa6ee3ba9
MattBartos <mat.tej@seznam.cz>
2016-10-17 23:18:11 +0100

    2016-10-17's Java workout

---
+++ contributions/MattBartos/Java/Fundamentals/2016-10-17.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Writing generic methods
+Using the `Console`
+Using `Printstream.format` to format a string before printing
+There is no such thing as *pass-by-reference* in Java
