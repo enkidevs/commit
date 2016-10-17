cef6711982cf1056b2de323adca99dc10d85b086
3sp4nt4 <thiago.baloon@gmail.com>
2016-10-17 14:05:37 +0100

    2016-10-17's JavaScript workout

---
+++ contributions/3sp4nt4/JavaScript/Functions/2016-10-17.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Functions can be declared after use
+Easily generate a random `HEX` color
+Function call method
+Changing a functions context with `fn.call(object)`
