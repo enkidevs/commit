08956e7fa87f0f69851a5b87960f928899bd5a9f
Zoilum <thomsomma@gmail.com>
2016-10-17 21:59:22 +0100

    2016-10-17's JavaScript workout

---
+++ contributions/Zoilum/JavaScript/Core/2016-10-17.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+How to merge two arrays
