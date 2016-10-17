f9d5bc7c12b409629614cfcde867eab854de5955
nelsonio <martin23haupt@gmail.com>
2016-10-17 11:27:53 +0100

    2016-10-17's JavaScript workout

---
+++ contributions/nelsonio/JavaScript/React/2016-10-17.js
@@ -0,0 +1,6 @@
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+Props in getInitialState Is an        
+Anti-Pattern
+`ReactDOM.render` ref
+Validate for required props
