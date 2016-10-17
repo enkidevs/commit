fe32f437c36732f761e59555f521103303479e97
anema165 <justyna.skomra@gmail.com>
2016-10-17 10:43:03 +0100

    2016-10-17's JavaScript workout

---
+++ contributions/anema165/JavaScript/Core/2016-10-17.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Using the double tilde `~~` 
+Multi-line string variables
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
