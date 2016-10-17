9cea46e3ccc0e35dcd092449eb349fe02c8e20bb
MonteC <jarekucinski@gmail.com>
2016-10-17 22:01:56 +0100

    2016-10-17's Java workout

---
+++ contributions/MonteC/Java/Data Types/2016-10-17.java
@@ -0,0 +1,5 @@
+Two ways to use an `Iterator`
+Converting numbers to strings
+Converting a string to upper or lower case
+Equals operation on different data types
+Retrieve the component type of an array
