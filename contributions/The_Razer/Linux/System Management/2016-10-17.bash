d158f669d4b3b58e63f0148bd2491712f6eb796e
The_Razer <crazyrazer@inbox.ru>
2016-10-17 07:22:14 +0100

    2016-10-17's Linux workout

---
+++ contributions/The_Razer/Linux/System Management/2016-10-17.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Basic service management with `systemd`
+Commands to shutdown or restart the system
