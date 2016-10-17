116008dab3934a78e6e3507b41a06f2543c38fe3
lordmalvern <aooogah@gmail.com>
2016-10-17 18:51:37 +0100

    2016-10-17's Linux workout

---
+++ contributions/lordmalvern/Linux/File Management/2016-10-17.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Delete commands aliases
