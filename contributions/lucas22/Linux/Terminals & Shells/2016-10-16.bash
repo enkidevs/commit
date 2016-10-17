e65c64361b0d29248118bdd63c4383a4f5cac7fc
lucas22 <lucasparzianello@outlook.com>
2016-10-17 04:27:27 +0100

    2016-10-16's Linux workout

---
+++ contributions/lucas22/Linux/Terminals & Shells/2016-10-16.bash
@@ -0,0 +1,5 @@
+Find any Unix / Linux command
+Terminal incognito mode
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
