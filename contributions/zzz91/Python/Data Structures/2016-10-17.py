4f296aba379cb7e8169ce1f719e5537605405620
zzz91 <zuccon.matteo@gmail.com>
2016-10-17 20:17:33 +0100

    2016-10-17's Python workout

---
+++ contributions/zzz91/Python/Data Structures/2016-10-17.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Double ended queues with `deque`
+`Module`s everywhere!
+Operations with `bytes` and `bytearray`
+Implementing **weak** references
