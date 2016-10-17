977c4a98160f10a848411a1ca9d0d1a2f415d960
peerster <paer.berge@gmail.com>
2016-10-17 06:46:21 +0100

    2016-10-17's Linux workout

---
+++ contributions/peerster/Linux/Data Manipulation/2016-10-17.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+How to analyse a server's log files
+Using `lynx` to convert HTML to text
+Image manipulation  using `convert`
+Clean configuration files using `sed`
