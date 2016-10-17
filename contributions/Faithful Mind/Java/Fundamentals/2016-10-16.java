b9d16e82d1202ef8d02fb25efa3bd847faea5b30
Faithful Mind <lpyy15@gmail.com>
2016-10-17 03:15:41 +0100

    2016-10-16's Java workout

---
+++ contributions/Faithful Mind/Java/Fundamentals/2016-10-16.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Altering format string output by changing a format specifier's `argument_index`
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
