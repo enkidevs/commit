b1f8917ce8db96e2332cfb0f7d69389682454dee
fikolo <filipdalorenzo@gmail.com>
2016-10-18 09:06:03 +0100

    2016-10-18's Java workout

---
+++ contributions/fikolo/Java/Fundamentals/2016-10-18.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
