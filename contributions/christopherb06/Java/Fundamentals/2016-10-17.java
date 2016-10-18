6e2616da57c2f8f3ade215af6baf4199d90e1f40
christopherb06 <christopherb06@gmail.com>
2016-10-18 01:25:46 +0100

    2016-10-17's Java workout

---
+++ contributions/christopherb06/Java/Fundamentals/2016-10-17.java
@@ -0,0 +1,5 @@
+Locks in `static synchronized` methods
+Always specify type parameter
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
