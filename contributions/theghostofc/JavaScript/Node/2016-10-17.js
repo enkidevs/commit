10a5c4dc25bf859aacd927da02c8ea1319068180
theghostofc <theghostofc@gmail.com>
2016-10-18 04:55:03 +0100

    2016-10-17's JavaScript workout

---
+++ contributions/theghostofc/JavaScript/Node/2016-10-17.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Custom Node REPL Server
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+Automatic compilation for Node with **Nodemon** 
