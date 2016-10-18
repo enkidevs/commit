b6052a1b8ab897a98d2ea56dfe788bfd9e22c1a6
newlight77 <newlight77@gmail.com>
2016-10-18 08:43:08 +0100

    2016-10-18's JavaScript workout

---
+++ contributions/newlight77/JavaScript/Npm/2016-10-18.js
@@ -0,0 +1,5 @@
+Registry user accounts for npm
+npm can handle multiple versions of a dependency
+List binaries for scripting in npm
+`optionalDependencies` in npm
+`config` object in `package.json`
