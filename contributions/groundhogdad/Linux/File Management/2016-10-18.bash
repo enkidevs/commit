9dfb37f09d5e30b81a77388e0a16f97724df6aea
groundhogdad <groundhogdad@gmail.com>
2016-10-18 07:13:04 +0100

    2016-10-18's Linux workout

---
+++ contributions/groundhogdad/Linux/File Management/2016-10-18.bash
@@ -0,0 +1,6 @@
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
+Delete commands aliases
