246d80eb899ce8baa6702f264c09addd929c58cc
Ruud <ruud@arfie.nl>
2016-10-18 13:17:22 +0100

    2016-10-18's JavaScript workout

---
+++ contributions/Ruud/JavaScript/Ecmascript 2015/2016-10-18.js
@@ -0,0 +1,6 @@
+Computed property names in object literals
+Defaults values apply only to `undefined` (and not to `null`)
+Internationalization & Localization
+
+Binary and octal notation in ES6
+`WeakMap` in ES6
