922bba22b470b5f453ad6d88d9649aab2369832e
Grorel <grorel.kazui@gmail.com>
2016-10-18 07:27:24 +0100

    2016-10-18's Java workout

---
+++ contributions/Grorel/Java/Fundamentals/2016-10-18.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Always specify type parameter
