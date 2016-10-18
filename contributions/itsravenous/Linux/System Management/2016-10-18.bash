54db41ab63fdd1746cf2effdc1e42af46db9fbb4
itsravenous <tom@itsravenous.com>
2016-10-18 12:53:57 +0100

    2016-10-18's Linux workout

---
+++ contributions/itsravenous/Linux/System Management/2016-10-18.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
