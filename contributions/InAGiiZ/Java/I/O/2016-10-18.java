b784687405dc776736c0c01a1111be732c4e859d
InAGiiZ <tiago.grilo@outlook.com>
2016-10-18 10:20:26 +0100

    2016-10-18's Java workout

---
+++ contributions/InAGiiZ/Java/I/O/2016-10-18.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Reading and writing text files
+Listing a file system's root directories
+Deleting files and directories
