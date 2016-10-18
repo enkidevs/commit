ee7927cdcb00e47cc166b0478ffe186824556cb0
franciszver <franciszver@outlook.com>
2016-10-18 20:31:39 +0100

    2016-10-18's Linux workout

---
+++ contributions/franciszver/Linux/Terminals & Shells/2016-10-18.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Aliasing ssh connections
+Keep useful commands in your shell history with tags
