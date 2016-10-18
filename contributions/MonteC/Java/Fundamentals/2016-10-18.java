ca748001b8d417631763d675e5af2a5bf2592711
MonteC <jarekucinski@gmail.com>
2016-10-18 21:30:00 +0100

    2016-10-18's Java workout

---
+++ contributions/MonteC/Java/Fundamentals/2016-10-18.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Common mistake on switch statements
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+Locks in `static synchronized` methods
