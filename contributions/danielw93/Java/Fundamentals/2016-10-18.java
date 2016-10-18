aa08cca02b9121723918e5d6f80d44453a865c60
danielw93 <2waller@informatik.uni-hamburg.de>
2016-10-18 16:06:32 +0100

    2016-10-18's Java workout

---
+++ contributions/danielw93/Java/Fundamentals/2016-10-18.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
