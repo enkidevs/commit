f6c5bbf6f2755d02e36f3a386578b6d525fcdd40
bdauer <dauer.benjamin@gmail.com>
2016-10-18 20:23:24 +0100

    2016-10-18's JavaScript workout

---
+++ contributions/bdauer/JavaScript/Core/2016-10-18.js
@@ -0,0 +1,5 @@
+Arrays can be used as stack
+`if/else` shortcut `conditional operator`
+Setting default values with `short circuiting`
+Performing a function at timed intervals
+Use `splice()` to remove an item from an array
