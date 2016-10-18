d3e41a93d2f384b60dd3e97de6760089d6b34f56
butthe4d <maciej.pobudkiewicz@gmail.com>
2016-10-18 10:33:13 +0100

    2016-10-18's Java workout

---
+++ contributions/butthe4d/Java/Fundamentals/2016-10-18.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Using an interface as a parameter
