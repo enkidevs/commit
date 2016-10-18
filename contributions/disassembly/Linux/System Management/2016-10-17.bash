477fd77f20945a5a93c4e45fbefdfc94b568f7c9
disassembly <hpeng2005012255@gmail.com>
2016-10-18 11:08:08 +0100

    2016-10-17's Linux workout

---
+++ contributions/disassembly/Linux/System Management/2016-10-17.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Common log files
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
