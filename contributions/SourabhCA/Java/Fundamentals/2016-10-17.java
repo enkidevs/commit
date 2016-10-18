602deb148bd5ecc562a1060dd5c2cd51e6f76cd9
SourabhCA <sourabh2393@hotmail.com>
2016-10-18 06:07:31 +0100

    2016-10-17's Java workout

---
+++ contributions/SourabhCA/Java/Fundamentals/2016-10-17.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Using `synchronized` statements
+Always specify type parameter
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
