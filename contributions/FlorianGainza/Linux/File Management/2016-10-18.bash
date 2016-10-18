7a3824362faa4edc66ff7b72fc70b11eeb4a106a
FlorianGainza <florian.gainza.prof@gmail.com>
2016-10-18 06:41:19 +0100

    2016-10-18's Linux workout

---
+++ contributions/FlorianGainza/Linux/File Management/2016-10-18.bash
@@ -0,0 +1,6 @@
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
