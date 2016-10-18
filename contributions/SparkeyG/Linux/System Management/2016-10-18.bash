fef95de1a021f026016130605d2634ce40eb4068
SparkeyG <shawn.c.carroll@gmail.com>
2016-10-18 12:06:06 +0100

    2016-10-18's Linux workout

---
+++ contributions/SparkeyG/Linux/System Management/2016-10-18.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Finding Open Files With `lsof`
