a37e0a62ff786ffd01deb16e683faf624af0eddb
Selenthios <karstenspencer@yahoo.com>
2016-10-18 13:32:55 +0100

    2016-10-18's JavaScript workout

---
+++ contributions/Selenthios/JavaScript/React/2016-10-18.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+`if`-`else` statements in **JSX** and **React**
+Props in getInitialState Is an        
+Anti-Pattern
+Dealing with `this.props.children`
+Prop Validation
