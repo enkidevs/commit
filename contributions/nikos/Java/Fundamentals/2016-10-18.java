1bce381dc23ec5cb8e4c5a0188950e7d77ce2ce5
nikos <triantafyllou.ni@gmail.com>
2016-10-18 08:17:11 +0100

    2016-10-18's Java workout

---
+++ contributions/nikos/Java/Fundamentals/2016-10-18.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Difference between `==` and `equals()`
+Supply `toString()` in all classes
