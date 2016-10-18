0bd671b76a4c6c13155fbabf772903d81f0da894
giorgio <giorgio.cardarelli@gmail.com>
2016-10-18 06:12:58 +0100

    2016-10-18's JavaScript workout

---
+++ contributions/giorgio/JavaScript/Core/2016-10-18.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Precision
+Use `splice()` to remove an item from an array
+Data type comparison in `switch` statements
+The difference between null, undefined and NaN
