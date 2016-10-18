cb6a9a8dee595491ad3363e72e2e4c9b3b388739
remipou <remi.puig@gmail.com>
2016-10-18 06:20:46 +0100

    2016-10-18's JavaScript workout

---
+++ contributions/remipou/JavaScript/Core/2016-10-18.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Meaning of polyfill
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Functions return `undefined` by default
