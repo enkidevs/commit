494a64bc4080b9c9c060cb3b291a30320efd8d5e
jdavidpeter <jdavidpeter@gmail.com>
2016-10-18 17:52:31 +0100

    2016-10-18's Linux workout

---
+++ contributions/jdavidpeter/Linux/Networking/2016-10-18.bash
@@ -0,0 +1,5 @@
+Get real network statistics with `slurm`
+List your IPv6 configuration
+Sending emails from terminal / scripts
+Setting up password-free authentication
+Getting the connection speed from the terminal
