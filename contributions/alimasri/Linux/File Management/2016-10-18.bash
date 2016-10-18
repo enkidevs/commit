78433b4a4501255ee0df2308c54e0a49528234c8
alimasri <alimasri1991@gmail.com>
2016-10-18 11:44:58 +0100

    2016-10-18's Linux workout

---
+++ contributions/alimasri/Linux/File Management/2016-10-18.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
