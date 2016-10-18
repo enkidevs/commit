c78e819737ca2b251c5acdbd0251bf3f5e19db92
bbohac <bastian.bohac@gmail.com>
2016-10-18 08:57:51 +0100

    2016-10-18's Java workout

---
+++ contributions/bbohac/Java/I/O/2016-10-18.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+How to create directories in Java
+Checking for the existence of files and directories
+The `Console` class
