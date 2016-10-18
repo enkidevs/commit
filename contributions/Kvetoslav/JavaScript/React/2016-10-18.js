e72d3ff4aa3ec6c8b567b3d11da4cab3de7acb3c
Kvetoslav <jiri.heneberk@icompllex.com>
2016-10-18 06:43:09 +0100

    2016-10-18's JavaScript workout

---
+++ contributions/Kvetoslav/JavaScript/React/2016-10-18.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+`null` value for *Controlled Components* in **React**
+Expose Component Functions in **React**
+Dangerously set `innerHTML`
+Clone Elements in **React**
