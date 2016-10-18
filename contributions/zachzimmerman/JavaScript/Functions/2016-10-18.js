4a9df4db9c2dbfa0b8503595daca36c786ec1519
zachzimmerman <zzimmerman@gmail.com>
2016-10-18 19:37:14 +0100

    2016-10-18's JavaScript workout

---
+++ contributions/zachzimmerman/JavaScript/Functions/2016-10-18.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+`.bind()` 
+Function call method
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
