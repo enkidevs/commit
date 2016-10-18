34a5c99c9c262f2d5e92551e57d3fcbbbdd31c8d
emyarod <allllaboutyou@gmail.com>
2016-10-18 22:48:58 +0100

    2016-10-18's JavaScript workout

---
+++ contributions/emyarod/JavaScript/Core/2016-10-18.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+`if/else` shortcut `conditional operator`
+Using assignment operators
+Data type comparison in `switch` statements
+Getting the *real* dimensions of an image
