cc15e6617bd712f4eeb9ecfd30e06b33304caf99
ckm100 <kufogbemawuli1@gmail.com>
2016-10-18 05:22:26 +0100

    2016-10-17's JavaScript workout

---
+++ contributions/ckm100/JavaScript/Core/2016-10-17.js
@@ -0,0 +1,5 @@
+Format currency in JavaScript
+Double and single quotes
+Hoisting applies only to variable declarations, not initializations
+Extra function arguments are undefined by default
+Check if a function exists
