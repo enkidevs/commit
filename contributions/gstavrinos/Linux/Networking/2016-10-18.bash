ab32f529b976e6fda36fe7ff55b46d503df14e95
gstavrinos <stavrinosgeo@gmail.com>
2016-10-18 18:34:58 +0100

    2016-10-18's Linux workout

---
+++ contributions/gstavrinos/Linux/Networking/2016-10-18.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Logging dropped firewall packets
+How to clear `iptables` rules
+Sending emails from terminal / scripts
+Staying connected remotely via SSH without an interactive shell
