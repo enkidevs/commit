b605d549ebc3c53753ab8ac617325b3d0ea60624
gblake707 <gblake707@yahoo.com>
2016-10-18 22:38:15 +0100

    2016-10-18's JavaScript workout

---
+++ contributions/gblake707/JavaScript/React/2016-10-18.js
@@ -0,0 +1,5 @@
+Functional Stateless Components in React
+Self-closing tags in **JSX**
+`null` value for *Controlled Components* in **React**
+Use **React** with other libraries
+`PureRenderMixin` in **React**
