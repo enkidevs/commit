dc70f75bde32ab3731dd0d25e7e9270cabb21bd3
Luke <lukem95@gmail.com>
2016-10-18 20:26:35 +0100

    2016-10-18's JavaScript workout

---
+++ contributions/Luke/JavaScript/Npm/2016-10-18.js
@@ -0,0 +1,5 @@
+Manipulate npm packages cache
+npm can handle multiple versions of a dependency
+Manage local node modules with `npm link`
+`npm`  as an alternative to Gulp
+`npm` verifies `sha1` hashes of packages
