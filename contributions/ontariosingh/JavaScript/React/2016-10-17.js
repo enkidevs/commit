b904fecec4659f747ce0ee8f2356ba706a7d7b9d
ontariosingh <ontariosingh@gmail.com>
2016-10-18 01:48:19 +0100

    2016-10-17's JavaScript workout

---
+++ contributions/ontariosingh/JavaScript/React/2016-10-17.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+componentWillReceiveProps Not Triggered After Mounting
+
+`PureRenderMixin` in **React**
+Immutability helpers in **React**
+`ReactDOM.render` ref
