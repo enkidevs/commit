d0b22c107805e403645dbafe1bf610bf8167df5a
Jorge Omsimoy <jorgeafs99@gmail.com>
2016-10-18 06:31:57 +0100

    2016-10-18's JavaScript workout

---
+++ contributions/Jorge Omsimoy/JavaScript/Core/2016-10-18.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Performing a function at timed intervals
+Hoisting applies only to variable declarations, not initializations
+Apply `map` function to array items
+Detect an error type
