93d8f103f5142deac190dc80f29fc3ddb1c59f55
archligt <frodo.archligt@gmail.com>
2016-10-18 11:51:13 +0100

    2016-10-18's Linux workout

---
+++ contributions/archligt/Linux/Networking/2016-10-18.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Proxifying `ssh` connections
+Sending emails from terminal / scripts
+Make use of `netstat`
+Staying connected remotely via SSH without an interactive shell
