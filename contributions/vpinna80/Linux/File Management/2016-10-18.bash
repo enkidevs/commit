d853f08d5579555998972b2626f737e895df91f1
vpinna80 <valentino.pinna@bancaditalia.it>
2016-10-18 18:22:10 +0100

    2016-10-18's Linux workout

---
+++ contributions/vpinna80/Linux/File Management/2016-10-18.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
