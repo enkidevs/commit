c116a5f13297943f5a7ba940743790993e599aa6
Vikingair <luft.viktor.bms@gmail.com>
2016-10-18 07:55:14 +0100

    2016-10-18's Linux workout

---
+++ contributions/Vikingair/Linux/Terminals & Shells/2016-10-18.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Find any Unix / Linux command
+Terminal based browser
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
