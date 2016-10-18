d5dc8acfe5bfeeb9fc39295b4dcaffaa1127d3e1
jgalus <kuba@galo.org.pl>
2016-10-18 08:28:35 +0100

    2016-10-18's Linux workout

---
+++ contributions/jgalus/Linux/Terminals & Shells/2016-10-18.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Conditional command execution
+
+(`&&` operator)
+The Basics of Environment Variables
