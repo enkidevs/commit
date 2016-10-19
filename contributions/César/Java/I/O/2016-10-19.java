4a62a95a703379cce7b2b3ab1ae7a6fbeebbafd2
César <cesarbernedo@free.fr>
2016-10-19 20:52:09 +0100

    2016-10-19's Java workout

---
+++ contributions/César/Java/I/O/2016-10-19.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Retrieving file store attributes
+Deleting files and directories
