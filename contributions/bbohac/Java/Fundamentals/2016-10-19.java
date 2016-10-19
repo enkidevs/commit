445e5878d7dab02d8f5a1cf325df7c0438d42ed1
bbohac <bastian.bohac@gmail.com>
2016-10-19 07:32:04 +0100

    2016-10-19's Java workout

---
+++ contributions/bbohac/Java/Fundamentals/2016-10-19.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use meaningful names
+Using Inheritance to reduce code repetition
+Using `Printstream.format` to format a string before printing
+Limit Accessibility of `Fields`
