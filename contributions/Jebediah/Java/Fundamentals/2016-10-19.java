afe7e9fbf7e8a1efeb7281c6009decb626ab7ac6
Jebediah <jefwillems100@gmail.com>
2016-10-19 08:07:02 +0100

    2016-10-19's Java workout

---
+++ contributions/Jebediah/Java/Fundamentals/2016-10-19.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Common mistake on switch statements
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
+Using an interface as a parameter
