03241939bca98ccd8803fc93cfbb1ca8899e5837
Aristeia <11025x12@gmail.com>
2016-10-19 21:21:02 +0100

    2016-10-19's JavaScript workout

---
+++ contributions/Aristeia/JavaScript/React/2016-10-19.js
@@ -0,0 +1,5 @@
+`null` value for *Controlled Components* in **React**
+**React** `this.props.children`
+Use **React** with other libraries
+Dealing with `this.props.children`
+Validate for required props
