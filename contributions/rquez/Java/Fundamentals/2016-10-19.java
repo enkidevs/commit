f6c80efae90c48bcbbfd8c906e4e0f7df66b9f0f
rquez <rvzxjr@gmail.com>
2016-10-19 16:36:31 +0100

    2016-10-19's Java workout

---
+++ contributions/rquez/Java/Fundamentals/2016-10-19.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
