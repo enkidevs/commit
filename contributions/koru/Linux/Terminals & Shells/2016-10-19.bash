32a949364cae2be1072334f2f0c003802942e49e
koru <glassonyou@gmail.com>
2016-10-20 00:46:58 +0100

    2016-10-19's Linux workout

---
+++ contributions/koru/Linux/Terminals & Shells/2016-10-19.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Find any Unix / Linux command
+Quick `bash` shortcuts 
+Sequential execution using the `;` statement separator
