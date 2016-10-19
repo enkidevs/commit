ea0d97635c6466be1289fe0ae13f451af04b04c7
mindw0rk <mindvvorc@gmail.com>
2016-10-19 03:41:03 +0100

    2016-10-18's Linux workout

---
+++ contributions/mindw0rk/Linux/File Management/2016-10-18.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
+Easy way of sharing files
