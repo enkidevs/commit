62985e9ff6498d343c34ce1d115fd5622604df79
JorgenJuel <jorgen.juel@hotmail.com>
2016-10-19 16:16:35 +0100

    2016-10-19's JavaScript workout

---
+++ contributions/JorgenJuel/JavaScript/Functions/2016-10-19.js
@@ -0,0 +1,5 @@
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Move cursor at the end of text input
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
