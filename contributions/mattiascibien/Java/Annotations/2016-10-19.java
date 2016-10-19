3468a0cc983a1cf0e44bc2f54157acfaebf38d4a
mattiascibien <mattias.cibien@outlook.com>
2016-10-19 12:20:49 +0100

    2016-10-19's Java workout

---
+++ contributions/mattiascibien/Java/Annotations/2016-10-19.java
@@ -0,0 +1,5 @@
+`@Test` parameters
+Using the `@Deprecated` annotation
+Suppressing warnings using `@SuppressWarnings`
+Including annotations in Javadoc
+Restricting an annotation's application by using `@Target`
