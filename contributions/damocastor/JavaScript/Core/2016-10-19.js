9cdce5487e260efc74904806e0bef2012bc97121
damocastor <damocastor@gmail.com>
2016-10-19 14:40:52 +0100

    2016-10-19's JavaScript workout

---
+++ contributions/damocastor/JavaScript/Core/2016-10-19.js
@@ -0,0 +1,5 @@
+Types of numbers
+Multi-line string variables
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Round numbers to `N` decimals 
