3587f90db2008fccfa24c2d4b3f69896bff9a1a1
simonkth <simonkth@gmail.com>
2016-10-19 09:00:09 +0100

    2016-10-19's Java workout

---
+++ contributions/simonkth/Java/Fundamentals/2016-10-19.java
@@ -0,0 +1,4 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
