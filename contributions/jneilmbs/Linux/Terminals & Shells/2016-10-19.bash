675ff18f15eb5b85ec235476984d916bec2d9305
jneilmbs <jneilmbs@gmail.com>
2016-10-19 08:10:45 +0100

    2016-10-19's Linux workout

---
+++ contributions/jneilmbs/Linux/Terminals & Shells/2016-10-19.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+The Basics of Environment Variables
