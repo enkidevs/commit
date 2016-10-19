14628827a505be8634eee9cba38db55a4b96fabc
masterspy16 <stevenhelpme@gmail.com>
2016-10-19 17:14:11 +0100

    2016-10-19's Java workout

---
+++ contributions/masterspy16/Java/Fundamentals/2016-10-19.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
