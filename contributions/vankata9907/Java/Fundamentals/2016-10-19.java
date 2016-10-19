92f6f96a720d2f08f80dacc64f7d0f28395ae945
vankata9907 <vankata9907@gmail.com>
2016-10-19 07:37:28 +0100

    2016-10-19's Java workout

---
+++ contributions/vankata9907/Java/Fundamentals/2016-10-19.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
