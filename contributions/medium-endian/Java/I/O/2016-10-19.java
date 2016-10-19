7455bc85d8c210677742898916a84bd510fd9b66
medium-endian <mediumendian@gmail.com>
2016-10-19 14:44:28 +0100

    2016-10-19's Java workout

---
+++ contributions/medium-endian/Java/I/O/2016-10-19.java
@@ -0,0 +1,5 @@
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
