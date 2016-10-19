a4f3f93d60ede2813876c12ed7e7366bcba590cf
Senopratomo <lord.ernesto@gmail.com>
2016-10-19 18:00:27 +0100

    2016-10-19's Java workout

---
+++ contributions/Senopratomo/Java/I/O/2016-10-19.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Create system file paths using the `Path` class
+Using buffered streams
+Using the `OpenOption...` vararg to define file writing behavior
+List contents of a directory with `newDirectoryStream`
