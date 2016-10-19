7deb186d68a4b8adbefb9fcbc6f1b943d8102176
gridkid87 <rnhoward51@gmail.com>
2016-10-19 19:10:44 +0100

    2016-10-19's Linux workout

---
+++ contributions/gridkid87/Linux/File Management/2016-10-19.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Working with `.gz` files
+Easy way of sharing files
