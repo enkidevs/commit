981da6d711621f1fa51d2cde8ec41fcea2c6d7d4
13scoobie <13scoobie@gmail.com>
2016-10-19 14:23:46 +0100

    2016-10-19's Linux workout

---
+++ contributions/13scoobie/Linux/System Management/2016-10-19.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
