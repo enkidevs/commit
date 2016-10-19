4ef041aea82335a83df0a9998ca54f8d46804c0e
Hellonearthis <hellonearthisman@gmail.com>
2016-10-19 16:08:51 +0100

    2016-10-19's JavaScript workout

---
+++ contributions/Hellonearthis/JavaScript/React/2016-10-19.js
@@ -0,0 +1,5 @@
+Self-closing tags in **JSX**
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+Dangerously set `innerHTML`
+Dealing with `this.props.children`
