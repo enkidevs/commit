367f928b22ec0ab80e66413518941c8853da165a
szappacosta <sebastien.zappacosta@gmail.com>
2016-10-19 17:29:15 +0100

    2016-10-19's JavaScript workout

---
+++ contributions/szappacosta/JavaScript/Core/2016-10-19.js
@@ -0,0 +1,5 @@
+Using assignment operators
+Hoisting applies only to variable declarations, not initializations
+NaN === NaN?
+Extra function arguments are undefined by default
+Round numbers to `N` decimals 
