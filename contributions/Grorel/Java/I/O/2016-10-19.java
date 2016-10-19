27408916e97c127c3dfbb6ca1594fadb7d431d3d
Grorel <grorel.kazui@gmail.com>
2016-10-19 07:29:29 +0100

    2016-10-19's Java workout

---
+++ contributions/Grorel/Java/I/O/2016-10-19.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Retrieving file store attributes
+Listing a file system's root directories
