e678fb699fa3b11b4ceeaccd70ae2b3bd4ef2461
durandj <james.durand@alumni.msoe.edu>
2016-10-19 14:53:54 +0100

    2016-10-19's JavaScript workout

---
+++ contributions/durandj/JavaScript/React/2016-10-19.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+componentWillReceiveProps Not Triggered After Mounting
+
+Dangerously set `innerHTML`
+`ReactDOM.render` ref
+Shortcut for transferring props
