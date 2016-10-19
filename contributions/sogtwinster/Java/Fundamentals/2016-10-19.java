e8b88c30454cb1d16d2bdb20229eb1775597de27
sogtwinster <sogtwinster@gmail.com>
2016-10-19 16:25:07 +0100

    2016-10-19's Java workout

---
+++ contributions/sogtwinster/Java/Fundamentals/2016-10-19.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Catch multiple exceptions in a single `catch` block
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
