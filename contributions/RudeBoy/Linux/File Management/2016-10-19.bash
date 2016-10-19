4e129a146e12310e79d7006cd393bc3941a6120e
RudeBoy <romanisman@gmail.com>
2016-10-19 21:32:21 +0100

    2016-10-19's Linux workout

---
+++ contributions/RudeBoy/Linux/File Management/2016-10-19.bash
@@ -0,0 +1,7 @@
+Practical `du` command
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
