9824d5774c7154d0bdabf4a66d9e1f38a3976de9
lluchmk <lluchmk@gmail.com>
2016-10-19 06:28:29 +0100

    2016-10-19's Java workout

---
+++ contributions/lluchmk/Java/Fundamentals/2016-10-19.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Difference between `==` and `equals()`
+Java naming conventions
+Ternary operator
+Updating interfaces by using `default` methods
