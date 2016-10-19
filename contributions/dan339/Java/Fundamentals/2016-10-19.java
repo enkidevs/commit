1eeef2438fbe3bb620c4c2b1b6286ad3f2105d36
dan339 <danielcampbell339@gmail.com>
2016-10-19 07:42:36 +0100

    2016-10-19's Java workout

---
+++ contributions/dan339/Java/Fundamentals/2016-10-19.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Difference between `==` and `equals()`
+The trade-off between short and descriptive names
+Connecting to FTP using Java
+Limit Accessibility of `Fields`
