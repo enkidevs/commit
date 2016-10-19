455cb8562598c7c4b30b95cc7993546959c59f05
Orfireeagle <donny.carter76@gmail.com>
2016-10-19 22:12:37 +0100

    2016-10-19's Linux workout

---
+++ contributions/Orfireeagle/Linux/Networking/2016-10-19.bash
@@ -0,0 +1,5 @@
+Stress testing with `ab`
+Useful ssh client optimizations 
+Get real network statistics with `slurm`
+Sending emails from terminal / scripts
+Use `tcpdump` to listen to network interface traffic
