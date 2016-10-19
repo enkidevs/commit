0e6e9cf2a93bda419930b22e706f4479312febf5
abisz <reinsperger.smn@gmail.com>
2016-10-19 08:13:25 +0100

    2016-10-19's JavaScript workout

---
+++ contributions/abisz/JavaScript/React/2016-10-19.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Props in getInitialState Is an        
+Anti-Pattern
+Use **React** with other libraries
+Shallow compare in **React**
+`ref` callback attribute
