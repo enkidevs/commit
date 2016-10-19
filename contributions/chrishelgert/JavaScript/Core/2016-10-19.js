efe1565372c4e23cbcdbcbad66734ff57408ef13
chrishelgert <chris.helgert@yahoo.com>
2016-10-19 11:18:09 +0100

    2016-10-19's JavaScript workout

---
+++ contributions/chrishelgert/JavaScript/Core/2016-10-19.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Format currency in JavaScript
+Use `splice()` to remove an item from an array
+Functions return `undefined` by default
+JavaScript compilation
