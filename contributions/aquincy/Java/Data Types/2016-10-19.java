9889a37d49903aa24fff66594d094b072f0b1e57
aquincy <brennanjl003@gmail.com>
2016-10-19 21:44:08 +0100

    2016-10-19's Java workout

---
+++ contributions/aquincy/Java/Data Types/2016-10-19.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Equals operation on different data types
+Retrieve the component type of an array
