a07823735059c300060e23fde7f347ae577a673e
evanmhm <evanhoppermoore@gmail.com>
2016-10-19 20:07:31 +0100

    2016-10-19's JavaScript workout

---
+++ contributions/evanmhm/JavaScript/Npm/2016-10-19.js
@@ -0,0 +1,5 @@
+Niceties for your npm package
+Calling remote scripts with npm
+Passing through command line arguments in npm scripts
+`npm` verifies `sha1` hashes of packages
+`config` object in `package.json`
