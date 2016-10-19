710c5bd4c61388996e37bcfee4b3ece9115b044d
QuantumSnow <jakobtheai@gmail.com>
2016-10-19 20:53:17 +0100

    2016-10-19's Linux workout

---
+++ contributions/QuantumSnow/Linux/File Management/2016-10-19.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
+Easy way of sharing files
