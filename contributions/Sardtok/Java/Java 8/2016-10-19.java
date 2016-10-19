9741e21bae5f0ca62a56c1fe70571114fc48cdf6
Sardtok <sha@computas.com>
2016-10-19 07:42:54 +0100

    2016-10-19's Java workout

---
+++ contributions/Sardtok/Java/Java 8/2016-10-19.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Java 8 Lambda expressions
+Using `Arrays.parallelSort` 
+Control OS `Process`es in `Java8`
+Avoid numerical overflows with *Java8* `exact` methods.
