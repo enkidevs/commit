f391e38ec4ba6a452f9631c90bf4b8c24dc758d7
elhanarinc <elhanarinc@gmail.com>
2016-10-19 21:44:38 +0100

    2016-10-19's JavaScript workout

---
+++ contributions/elhanarinc/JavaScript/Functions/2016-10-19.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Easily generate a random `HEX` color
+Changing a functions context with `fn.call(object)`
