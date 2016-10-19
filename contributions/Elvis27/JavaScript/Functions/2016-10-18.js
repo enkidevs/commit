cabc5b5ec831107b8e62dbd119a25d48ec9f9780
Elvis27 <elvisfernandes27@gmail.com>
2016-10-19 14:58:54 +0100

    2016-10-18's JavaScript workout

---
+++ contributions/Elvis27/JavaScript/Functions/2016-10-18.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Method chaining
+Changing a functions context with `fn.call(object)`
