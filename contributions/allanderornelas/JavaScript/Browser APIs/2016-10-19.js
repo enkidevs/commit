770fd28de4cd78a45e5c53b3f0210e6b53241a4d
allanderornelas <allander18@gmail.com>
2016-10-19 13:08:59 +0100

    2016-10-19's JavaScript workout

---
+++ contributions/allanderornelas/JavaScript/Browser APIs/2016-10-19.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+CSS for when JavaScript is enabled
+FileReader.readAsArrayBuffer()
+FileReader.readAsDataURL()
+Window.sessionStorage
