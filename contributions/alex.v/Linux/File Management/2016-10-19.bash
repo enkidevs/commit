8b7b6ec1f28e8c1890947f0141729cc44741659f
alex.v <a.voloshyn@gmail.com>
2016-10-19 21:24:56 +0100

    2016-10-19's Linux workout

---
+++ contributions/alex.v/Linux/File Management/2016-10-19.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
