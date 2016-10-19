f1f669e6e18ea1a7a52736ff59d73440a846463f
Faithful Mind <lpyy15@gmail.com>
2016-10-19 16:28:36 +0100

    2016-10-18's Java workout

---
+++ contributions/Faithful Mind/Java/Fundamentals/2016-10-18.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
