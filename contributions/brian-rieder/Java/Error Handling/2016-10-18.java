cc5d73e6e993f845ddd66acf1d66ff2c535471eb
brian-rieder <brieder@purdue.edu>
2016-10-19 01:41:02 +0100

    2016-10-18's Java workout

---
+++ contributions/brian-rieder/Java/Error Handling/2016-10-18.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
