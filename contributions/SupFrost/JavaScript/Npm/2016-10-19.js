692907e66291784374283158d76653a88993f287
SupFrost <nickjorens@gmail.com>
2016-10-19 13:33:05 +0100

    2016-10-19's JavaScript workout

---
+++ contributions/SupFrost/JavaScript/Npm/2016-10-19.js
@@ -0,0 +1,5 @@
+npm projects dependencies
+Passing through command line arguments in npm scripts
+List binaries for scripting in npm
+Manage local node modules with `npm link`
+`npm` verifies `sha1` hashes of packages
