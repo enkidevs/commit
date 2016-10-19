4d1fbc1275ac713d056038f0ee0a431af696094c
kkhcheng <kkcheng@gmail.com>
2016-10-19 23:51:22 +0100

    2016-10-19's Java workout

---
+++ contributions/kkhcheng/Java/Fundamentals/2016-10-19.java
@@ -0,0 +1,5 @@
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
