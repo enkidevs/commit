5eed43892fc0b75d7e26e68b4a5808357c3eca01
ozanust <ozanust9@gmail.com>
2016-10-20 10:12:19 +0100

    2016-10-20's Java workout

---
+++ contributions/ozanust/Java/Fundamentals/2016-10-20.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+How range operations work
