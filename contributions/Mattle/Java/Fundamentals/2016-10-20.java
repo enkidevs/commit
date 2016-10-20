0e9f468f8e5d0dd0936f40063d3d111450a08324
Mattle <mcallier@gmail.com>
2016-10-20 21:07:12 +0100

    2016-10-20's Java workout

---
+++ contributions/Mattle/Java/Fundamentals/2016-10-20.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
