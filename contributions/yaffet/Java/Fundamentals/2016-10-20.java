409ca149e708b42dce409b8987d667b1ea1abaa4
yaffet <uniac@web.de>
2016-10-20 03:19:04 +0100

    2016-10-20's Java workout

---
+++ contributions/yaffet/Java/Fundamentals/2016-10-20.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
