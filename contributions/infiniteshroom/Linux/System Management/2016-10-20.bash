c162a6d512f0bae2ee7d1f621a913a77f0da21b0
infiniteshroom <infiniteshroom@gmail.com>
2016-10-20 07:23:46 +0100

    2016-10-20's Linux workout

---
+++ contributions/infiniteshroom/Linux/System Management/2016-10-20.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Finding Open Files With `lsof`
