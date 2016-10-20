51bdd702457187b2711746556ae89b3ba9eb2b6f
mrsteakhouse <mrsteakhouse1@googlemail.com>
2016-10-20 11:58:53 +0100

    2016-10-20's Linux workout

---
+++ contributions/mrsteakhouse/Linux/Terminals & Shells/2016-10-20.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
+The Basics of Environment Variables
