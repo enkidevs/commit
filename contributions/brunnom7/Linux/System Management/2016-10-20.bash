f41a81f9cda5d8910d5e39010472f318fd26ce3b
brunnom7 <brunnomfoto@gmail.com>
2016-10-20 21:22:07 +0100

    2016-10-20's Linux workout

---
+++ contributions/brunnom7/Linux/System Management/2016-10-20.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Find the `MAC` address of all network interfaces
+
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Cancel a system shutdown
