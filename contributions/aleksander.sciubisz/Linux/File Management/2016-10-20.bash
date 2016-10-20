19f597546b6589d7a301bc2f10b8aff9be40437c
aleksander.sciubisz <aleksander.sciubisz@gmail.com>
2016-10-20 17:14:49 +0100

    2016-10-20's Linux workout

---
+++ contributions/aleksander.sciubisz/Linux/File Management/2016-10-20.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Converting between Unix and Windows text files
+Monitor the progress of data through a pipe with `pv`
+Delete commands aliases
