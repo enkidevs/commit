e9e50f955e6bb815db3859accc05806fb73fc98e
JellyBreeze <00valter00@gmail.com>
2016-10-20 16:29:27 +0100

    2016-10-20's Linux workout

---
+++ contributions/JellyBreeze/Linux/File Management/2016-10-20.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
+Delete commands aliases
