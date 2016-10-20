cee0126bb47c2ca289e0ab93e6dd7724afb36a49
strongff <riccardo.vavassori@gmail.com>
2016-10-20 21:00:38 +0100

    2016-10-20's Git workout

---
+++ contributions/strongff/Git/Essentials/2016-10-20.bash
@@ -0,0 +1,5 @@
+Using aliases for git commands
+Check the status of your files
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
