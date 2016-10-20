1e6129d862e901db59103d9e8c1036b0f656f0c0
Jinxter <propwe@gmail.com>
2016-10-20 08:55:26 +0100

    2016-10-20's Linux workout

---
+++ contributions/Jinxter/Linux/Package Management/2016-10-20.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Backporting on Debian
+
+Installing a `.deb` package from the terminal
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
