af3713ee7c190fdb3f8ee29b794ecbb7e2c094e4
doomi <kurai7hikari@gmail.com>
2016-10-20 07:09:32 +0100

    2016-10-20's JavaScript workout

---
+++ contributions/doomi/JavaScript/Node/2016-10-20.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+The built-in Node debugger
+Handle `JSON.parse` error in Node.js
+`exports` vs. `module.exports` in **Node**
+Basic web server with Express
