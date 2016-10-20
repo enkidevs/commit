f5e1b4e209a5df6e0babb4388b34514d117c342c
vieurou <vieurou@gmail.com>
2016-10-20 19:57:17 +0100

    2016-10-20's Linux workout

---
+++ contributions/vieurou/Linux/File Management/2016-10-20.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
