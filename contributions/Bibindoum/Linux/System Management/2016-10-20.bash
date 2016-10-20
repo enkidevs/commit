cb4648e5043637f687023f39f1784046fec002e4
Bibindoum <pierrick.guitter@gmail.com>
2016-10-20 12:53:07 +0100

    2016-10-20's Linux workout

---
+++ contributions/Bibindoum/Linux/System Management/2016-10-20.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
