dee8c9d713f3d58ee9503c2a4ceb098a6467da41
ulenkar <ulka@o2.pl>
2016-10-20 08:10:24 +0100

    2016-10-20's Java workout

---
+++ contributions/ulenkar/Java/Fundamentals/2016-10-20.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Always specify type parameter
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
