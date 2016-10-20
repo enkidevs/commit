af7e57b425e2f4e4e76d617d95e6ec4d019c2672
patriciob00 <pbrunorb@hotmail.com>
2016-10-20 16:51:04 +0100

    2016-10-20's JavaScript workout

---
+++ contributions/patriciob00/JavaScript/Core/2016-10-20.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Hoisting applies only to variable declarations, not initializations
+Context in JavaScript
+`catch` is block scoped
+Prevent background image flicker in **IE**
