bf3260475b1d95ad4f75fcea97602415ee15cf15
leslch <leslch@hotmail.com>
2016-10-21 00:17:37 +0100

    2016-10-20's Java workout

---
+++ contributions/leslch/Java/Fundamentals/2016-10-20.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Java naming conventions
+Using `Printstream.format` to format a string before printing
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
