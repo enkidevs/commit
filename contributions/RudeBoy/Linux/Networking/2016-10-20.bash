3c91f900a96039205ee6e9f5fe60debeee8f33dc
RudeBoy <romanisman@gmail.com>
2016-10-20 08:41:25 +0100

    2016-10-20's Linux workout

---
+++ contributions/RudeBoy/Linux/Networking/2016-10-20.bash
@@ -0,0 +1,5 @@
+Get real network statistics with `slurm`
+Limit the `wget` download rate
+List your IPv6 configuration
+Use `tcpdump` to listen to network interface traffic
+Short Introduction to `Wget` 
