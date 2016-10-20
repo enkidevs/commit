89fb0fd86f26cf4f19f0b5b833a31369ee57e33b
mrstr <robert.str@o2.pl>
2016-10-20 16:30:27 +0100

    2016-10-20's Java workout

---
+++ contributions/mrstr/Java/Annotations/2016-10-20.java
@@ -0,0 +1,5 @@
+`@Test` parameters
+Use `@Ignore` to disable JUnit tests
+Suppressing warnings using `@SuppressWarnings`
+Restricting an annotation's application by using `@Target`
+Declaring `@Repeatable` annotation types
