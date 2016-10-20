50ef461c44d85403b32758977c8db696b212dd4c
dabizk <dabizk@yahoo.com>
2016-10-20 02:33:50 +0100

    2016-10-19's Java workout

---
+++ contributions/dabizk/Java/Fundamentals/2016-10-19.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+Detect or prevent integer overflow
+`Autoboxing` and `Unboxing`
