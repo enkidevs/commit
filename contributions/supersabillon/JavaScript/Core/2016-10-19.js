706acf41d256c9d8ab2b70115d4826ca7644b7b7
supersabillon <joel@sabillon.net>
2016-10-20 05:45:57 +0100

    2016-10-19's JavaScript workout

---
+++ contributions/supersabillon/JavaScript/Core/2016-10-19.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Types of numbers
+Hoisting applies only to variable declarations, not initializations
+Functions return `undefined` by default
+Scope and strict mode
