a3cbe13ef3ad0d4f964e422cce365484dbbd12a1
stefanesterer <der.stefon@gmail.com>
2016-10-20 12:50:02 +0100

    2016-10-20's Java workout

---
+++ contributions/stefanesterer/Java/Fundamentals/2016-10-20.java
@@ -0,0 +1,5 @@
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
