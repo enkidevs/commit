e919077f40ebb58558c267ab3faf862c161bd26f
nporto <portokallidis@gmail.com>
2016-10-20 07:42:25 +0100

    2016-10-20's JavaScript workout

---
+++ contributions/nporto/JavaScript/React/2016-10-20.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Type of the Children props
+Props in getInitialState Is an        
+Anti-Pattern
+Immutability helpers in **React**
+Enhanced Prop Validation 
