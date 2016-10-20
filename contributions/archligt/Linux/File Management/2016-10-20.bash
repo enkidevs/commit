409e82c13ecf4139ac92a5d01514c447041f9842
archligt <frodo.archligt@gmail.com>
2016-10-20 11:20:48 +0100

    2016-10-20's Linux workout

---
+++ contributions/archligt/Linux/File Management/2016-10-20.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
