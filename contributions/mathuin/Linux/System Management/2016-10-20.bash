836b9a0619eeba40c97e85fcee4e5942d0331427
mathuin <mathuin@gmail.com>
2016-10-20 20:47:12 +0100

    2016-10-20's Linux workout

---
+++ contributions/mathuin/Linux/System Management/2016-10-20.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
