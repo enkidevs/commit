ce8e27d317a0c382cc38ebd1f2d9ceb4090c9ffe
tolsen <tolsen@southern.edu>
2016-10-20 04:25:22 +0100

    2016-10-19's Java workout

---
+++ contributions/tolsen/Java/Fundamentals/2016-10-19.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Use `DecimalFormat` class to format numbers
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
