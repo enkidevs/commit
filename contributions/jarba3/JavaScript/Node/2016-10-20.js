9dc31887cdccd51a1413ed7614adf323bb2491a9
jarba3 <bjobard@gmail.com>
2016-10-20 08:49:25 +0100

    2016-10-20's JavaScript workout

---
+++ contributions/jarba3/JavaScript/Node/2016-10-20.js
@@ -0,0 +1,5 @@
+`uncaughtException` listener in Node.js
+Manage child processes in **Node**
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
+Avoid using `sudo` when running Node
