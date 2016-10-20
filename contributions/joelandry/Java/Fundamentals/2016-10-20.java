70a4ec8ad2cf53d40b8476f6fe2cc23374a151b0
joelandry <joelandry80@gmail.com>
2016-10-20 21:12:32 +0100

    2016-10-20's Java workout

---
+++ contributions/joelandry/Java/Fundamentals/2016-10-20.java
@@ -0,0 +1,5 @@
+Generating random numbers
+Use strings in a `switch` statement
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Avoid using `static` variables
