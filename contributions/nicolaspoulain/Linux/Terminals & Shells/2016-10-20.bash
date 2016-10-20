d9f9633e41863c52eb016607bf0ceaa421393459
nicolaspoulain <nico.poulain@gmail.com>
2016-10-20 20:34:27 +0100

    2016-10-20's Linux workout

---
+++ contributions/nicolaspoulain/Linux/Terminals & Shells/2016-10-20.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
