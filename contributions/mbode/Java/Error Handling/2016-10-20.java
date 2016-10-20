ac8c1336020f034bd2ac19771ef96feba612ba24
mbode <maxbode@gmail.com>
2016-10-20 06:35:49 +0100

    2016-10-20's Java workout

---
+++ contributions/mbode/Java/Error Handling/2016-10-20.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Throwing an `exception`
+Careful Numeric Data Conversions
