037d67baa82e6120ccad046b094701b2000df21d
lucasouwens <lucasouwens@gmail.com>
2016-10-20 16:57:46 +0100

    2016-10-20's Java workout

---
+++ contributions/lucasouwens/Java/Fundamentals/2016-10-20.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
