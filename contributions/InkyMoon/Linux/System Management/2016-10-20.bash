1ce954bf4cdfbb04298ad1513352b77c0fae8700
InkyMoon <patrick.kastelic@gmail.com>
2016-10-20 19:16:50 +0100

    2016-10-20's Linux workout

---
+++ contributions/InkyMoon/Linux/System Management/2016-10-20.bash
@@ -0,0 +1,6 @@
+Broadcast messages across the system
+Find the `MAC` address of all network interfaces
+
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
