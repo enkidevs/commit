5c424fbbdb6584c348d7af40f6b30b8d328b4556
dckendrick <dckendrick24@gmail.com>
2016-10-20 01:36:22 +0100

    2016-10-19's JavaScript workout

---
+++ contributions/dckendrick/JavaScript/Node/2016-10-19.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+`exports` vs. `module.exports` in **Node**
+Middleware/pipeline design pattern
