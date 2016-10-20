7eb5367177df06cdea808c0c3084b324f8a01216
dan339 <danielcampbell339@gmail.com>
2016-10-20 07:47:36 +0100

    2016-10-20's Java workout

---
+++ contributions/dan339/Java/Fundamentals/2016-10-20.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Supply `toString()` in all classes
