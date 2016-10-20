d0cd92c2e204fda8a0b0ebb648d9e585c1ef74e1
artemgurzhii <artem.gurzhii@gmail.com>
2016-10-20 23:26:31 +0100

    2016-10-20's JavaScript workout

---
+++ contributions/artemgurzhii/JavaScript/Functions/2016-10-20.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+IIFE pattern
+Check if a document is done loading
