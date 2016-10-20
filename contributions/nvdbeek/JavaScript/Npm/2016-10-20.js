d4f2b2740f242ff7bf7303538b5c9e9f298e7645
nvdbeek <nielsvdbeek@gmail.com>
2016-10-20 11:21:15 +0100

    2016-10-20's JavaScript workout

---
+++ contributions/nvdbeek/JavaScript/Npm/2016-10-20.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Environmental variables and flags for `npm`
+devDependencies in `npm`
+Reduce package duplication 
+Lock down dependency versions by shrinkwrapping
