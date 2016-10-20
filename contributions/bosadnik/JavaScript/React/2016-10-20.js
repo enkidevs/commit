f0e6d8d39002b64ae0097ce46a82aecde3c91d06
bosadnik <bartlomiej@osadnik.com>
2016-10-20 17:02:35 +0100

    2016-10-20's JavaScript workout

---
+++ contributions/bosadnik/JavaScript/React/2016-10-20.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+Props in getInitialState Is an        
+Anti-Pattern
+Immutability helpers in **React**
+Custom `propType`'s to be required
+Default values for props
