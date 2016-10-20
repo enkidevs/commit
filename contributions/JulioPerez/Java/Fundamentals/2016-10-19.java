5f7aad163104fd7f21818eed5160976763688659
JulioPerez <julioperezag@gmail.com>
2016-10-20 05:01:28 +0100

    2016-10-19's Java workout

---
+++ contributions/JulioPerez/Java/Fundamentals/2016-10-19.java
@@ -0,0 +1,5 @@
+Distinction between `public` and `private` methods
+Inheriting a constructor from a superclass
+Locks in `static synchronized` methods
+How range operations work
+Using an interface as a parameter
