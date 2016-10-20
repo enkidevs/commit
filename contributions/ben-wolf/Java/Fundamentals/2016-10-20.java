e39c8116df87d11f79ea45f019acd221058bb801
ben-wolf <wolfb3@rpi.edu>
2016-10-21 00:37:58 +0100

    2016-10-20's Java workout

---
+++ contributions/ben-wolf/Java/Fundamentals/2016-10-20.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Generating random numbers
+Common mistake on switch statements
+Using Inheritance to reduce code repetition
+Always specify type parameter
