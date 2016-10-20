0bcabe7bffff63243677e7173db465c05e9f2473
ninijay <ninijay93@gmail.com>
2016-10-20 15:34:46 +0100

    2016-10-20's Linux workout

---
+++ contributions/ninijay/Linux/File Management/2016-10-20.bash
@@ -0,0 +1,5 @@
+Converting between Unix and Windows text files
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
+Delete commands aliases
+Easy way of sharing files
