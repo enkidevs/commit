347a4a2293cb604b5b7ef484cac678229b9f2784
afc11hn <afc11hn@hotmail.de>
2016-10-20 14:50:52 +0100

    2016-10-20's Java workout

---
+++ contributions/afc11hn/Java/Fundamentals/2016-10-20.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Generating random numbers
+Use meaningful names
+The trade-off between short and descriptive names
+Using Inheritance to reduce code repetition
