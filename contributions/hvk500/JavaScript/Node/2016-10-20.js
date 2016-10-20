88f40c4063b2ad50ff9a315b2dca70a7b16759f0
hvk500 <megaman100ster@gmail.com>
2016-10-20 17:54:51 +0100

    2016-10-20's JavaScript workout

---
+++ contributions/hvk500/JavaScript/Node/2016-10-20.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+First-error callbacks in **Node**
+Use conventional property names for error handling in Node.js
+Clustering
+Manage child processes in **Node**
