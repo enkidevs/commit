31f0d58c691ba64e3190277ff6cbda921afd9c33
plucdrol <plucdrol@gmail.com>
2016-10-20 13:28:06 +0100

    2016-10-20's Java workout

---
+++ contributions/plucdrol/Java/Fundamentals/2016-10-20.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
