80368034349c7e0069723333f31bb8266595a497
francescolucchi <francescolucchi@gmail.com>
2016-10-20 06:01:08 +0100

    2016-10-20's Java workout

---
+++ contributions/francescolucchi/Java/Fundamentals/2016-10-20.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
