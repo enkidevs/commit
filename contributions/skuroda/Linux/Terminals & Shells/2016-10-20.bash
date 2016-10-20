42cfb5f17f6a7d43662d600cf967c8f312541da1
skuroda <sk@mieco.org>
2016-10-21 00:42:44 +0100

    2016-10-20's Linux workout

---
+++ contributions/skuroda/Linux/Terminals & Shells/2016-10-20.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Terminal incognito mode
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
