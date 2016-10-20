244c0f7221c17aea298d32a588665f82e2660e3c
XelorRelin <xelor@ya.ru>
2016-10-20 06:55:39 +0100

    2016-10-20's Linux workout

---
+++ contributions/XelorRelin/Linux/Networking/2016-10-20.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Useful ssh client optimizations 
+Limit the `wget` download rate
+Make use of `netstat`
+Staying connected remotely via SSH without an interactive shell
