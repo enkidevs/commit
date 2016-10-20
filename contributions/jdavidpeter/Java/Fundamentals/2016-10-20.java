664db924b0ed1e7f6b8f56b483c8034f71c35e22
jdavidpeter <jdavidpeter@gmail.com>
2016-10-20 18:14:52 +0100

    2016-10-20's Java workout

---
+++ contributions/jdavidpeter/Java/Fundamentals/2016-10-20.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
