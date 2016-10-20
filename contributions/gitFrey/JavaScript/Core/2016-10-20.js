89ff820791b0866f881738c8b45a28882278030f
gitFrey <oliverbanham@gmail.com>
2016-10-20 10:24:28 +0100

    2016-10-20's JavaScript workout

---
+++ contributions/gitFrey/JavaScript/Core/2016-10-20.js
@@ -0,0 +1,5 @@
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+Extra function arguments are undefined by default
+How to merge two arrays
