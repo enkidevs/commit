ff4f9d722f89473f1abbcd62b6ba565edf43e81e
martink <martin-praha-4@email.cz>
2016-10-20 22:15:18 +0100

    2016-10-20's JavaScript workout

---
+++ contributions/martink/JavaScript/Core/2016-10-20.js
@@ -0,0 +1,5 @@
+Quick way to format integers with a fixed number of digits.
+Arrays can be used as stack
+`if/else` shortcut `conditional operator`
+Setting default values with `short circuiting`
+Round numbers to `N` decimals 
