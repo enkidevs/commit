f1fe238f3dee5c09a4d00a1c2fdccabc7915386e
casdidier <cheung.didier@gmail.com>
2016-10-21 17:00:20 +0100

    2016-10-21's Java workout

---
+++ contributions/casdidier/Java/Fundamentals/2016-10-21.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Calling one constructor from another
+How range operations work
