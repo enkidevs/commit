7be24fde236a7e703471a6adb9730fb9427dfee3
lachlan-eagling <lochie.eagling@gmail.com>
2016-10-21 22:24:39 +0100

    2016-10-21's Java workout

---
+++ contributions/lachlan-eagling/Java/Data Types/2016-10-21.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Converting a string to upper or lower case
+Do not attempt comparisons with NaN
