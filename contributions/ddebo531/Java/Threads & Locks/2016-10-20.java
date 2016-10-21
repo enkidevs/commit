d5a3ea2a2e7939173a36d4cf8db0676d051d2a88
ddebo531 <debellisdominic@gmail.com>
2016-10-21 04:30:59 +0100

    2016-10-20's Java workout

---
+++ contributions/ddebo531/Java/Threads & Locks/2016-10-20.java
@@ -0,0 +1,5 @@
+Multithreading: minimize locking scope
+Use thread pool executors
+`Thread.sleep` and `Thread.yield` do not have synchronization mechanisms
+Thread interruptions
+Implementing your own `Thread` 
