fed86498099b12cd265c4c34ee05b314ed450dba
batanete <bata619@gmail.com>
2016-10-21 08:37:19 +0100

    2016-10-21's Java workout

---
+++ contributions/batanete/Java/Fundamentals/2016-10-21.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
