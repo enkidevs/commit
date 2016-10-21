11048e96ee18d8f03a70880aa4d8ba1d74e78722
AlexxBoo <alexxboo@gmx.de>
2016-10-21 10:46:27 +0100

    2016-10-21's Linux workout

---
+++ contributions/AlexxBoo/Linux/Terminals & Shells/2016-10-21.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear bash history
+Find any Unix / Linux command
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
