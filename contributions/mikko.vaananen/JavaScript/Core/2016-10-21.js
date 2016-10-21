13d48ec315442e51af220a6948df2d615bcc9aba
mikko.vaananen <mikko.vaananen@gmail.com>
2016-10-21 05:19:11 +0100

    2016-10-21's JavaScript workout

---
+++ contributions/mikko.vaananen/JavaScript/Core/2016-10-21.js
@@ -0,0 +1,5 @@
+Quick way to format integers with a fixed number of digits.
+Shuffle an array using `sort` and `random`.
+Be careful when using `typeof`.
+Check if an argument is a number
+Milliseconds since Epoch
