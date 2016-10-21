12125a9b23e60ec7d7ea94ea27570a65487bffe0
Scrumplex <sefa6727@gmail.com>
2016-10-21 23:21:01 +0100

    2016-10-21's Linux workout

---
+++ contributions/Scrumplex/Linux/System Management/2016-10-21.bash
@@ -0,0 +1,5 @@
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Finding Open Files With `lsof`
