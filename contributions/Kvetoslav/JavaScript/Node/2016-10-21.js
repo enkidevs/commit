510fa214fa7a532febbbcc95356926334365f053
Kvetoslav <jiri.heneberk@icompllex.com>
2016-10-21 15:59:21 +0100

    2016-10-21's JavaScript workout

---
+++ contributions/Kvetoslav/JavaScript/Node/2016-10-21.js
@@ -0,0 +1,5 @@
+Streams in **Node**
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+`exports` vs. `module.exports` in **Node**
+`try-catch` only for **sync** code
