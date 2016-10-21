985d2aec43915d7cd95f450f5b4515a4706db448
RC-Chandan <chandan316@outlook.com>
2016-10-21 08:35:54 +0100

    2016-10-20's Java workout

---
+++ contributions/RC-Chandan/Java/Fundamentals/2016-10-20.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
