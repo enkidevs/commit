a1afdb636d50e9e9be17997912819875605ff970
zszuri <szuri90@gmail.com>
2016-10-21 07:39:31 +0100

    2016-10-21's Java workout

---
+++ contributions/zszuri/Java/I/O/2016-10-21.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to list all files in a directory that match a filename extension in Java
+Create system file paths using the `Path` class
+Reading file attributes
+Retrieving file store attributes
