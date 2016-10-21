0690ea6ae0fc2f26f5d282a6da0cab402e37ff20
ben-wolf <wolfb3@rpi.edu>
2016-10-21 08:38:30 +0100

    2016-10-21's Java workout

---
+++ contributions/ben-wolf/Java/Data Types/2016-10-21.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
+Numeric Conversion - Widening
