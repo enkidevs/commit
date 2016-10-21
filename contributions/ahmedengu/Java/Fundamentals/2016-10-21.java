d94a4569ff0e545da447fb8881db46635ac0cab5
ahmedengu <ahmedengu@gmail.com>
2016-10-21 22:07:14 +0100

    2016-10-21's Java workout

---
+++ contributions/ahmedengu/Java/Fundamentals/2016-10-21.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
