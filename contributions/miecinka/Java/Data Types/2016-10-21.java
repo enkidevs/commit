9d085379d7b19e8f849d05c97cf2686499c4dd5b
miecinka <mietinhovanish@gmail.com>
2016-10-22 00:34:39 +0100

    2016-10-21's Java workout

---
+++ contributions/miecinka/Java/Data Types/2016-10-21.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Retrieve the component type of an array
