d297ae8b3f264e7cf1a3d83db34d41f90a9e8e8e
struck81 <samstruck@gmail.com>
2016-10-21 23:39:34 +0100

    2016-10-21's Linux workout

---
+++ contributions/struck81/Linux/Networking/2016-10-21.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Get real network statistics with `slurm`
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
+Short Introduction to `Wget` 
