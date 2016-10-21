a210a82f70e7d012bb0ec51cbb5cf031fa50c9b7
danieldcr <danielromao88@gmail.com>
2016-10-21 16:29:49 +0100

    2016-10-21's Linux workout

---
+++ contributions/danieldcr/Linux/File Management/2016-10-21.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Practical `du` command
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
