f48ebdf22f72eaf22ca708a302e1211e236e6852
lordmalvern <aooogah@gmail.com>
2016-10-21 16:23:47 +0100

    2016-10-21's Linux workout

---
+++ contributions/lordmalvern/Linux/Networking/2016-10-21.bash
@@ -0,0 +1,5 @@
+How to clear `iptables` rules
+Limit the `wget` download rate
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
