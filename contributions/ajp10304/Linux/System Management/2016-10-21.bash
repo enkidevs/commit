c2c729bfdf8ef41d05b0f2f63374b8ce2e3f5e85
ajp10304 <a.pocklington@gmail.com>
2016-10-21 07:11:39 +0100

    2016-10-21's Linux workout

---
+++ contributions/ajp10304/Linux/System Management/2016-10-21.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
