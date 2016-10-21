050f9e6baea248db88b2accbf4b01eccce2a0167
Gerovid <gerovid@gmail.com>
2016-10-21 19:10:43 +0100

    2016-10-21's Linux workout

---
+++ contributions/Gerovid/Linux/File Management/2016-10-21.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
+Easy way of sharing files
