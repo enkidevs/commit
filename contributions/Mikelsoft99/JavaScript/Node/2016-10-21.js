a695c6b02297e00782b8910de8167cc7e202dd30
Mikelsoft99 <mikelsoft99@gmail.com>
2016-10-22 00:20:19 +0100

    2016-10-21's JavaScript workout

---
+++ contributions/Mikelsoft99/JavaScript/Node/2016-10-21.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+Wrapping errors in Node.js using _node-verror_
+Clustering
+`try-catch` only for **sync** code
