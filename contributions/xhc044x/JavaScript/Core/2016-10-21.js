774ecceff7d3a567b9b951a625fb0cea8979ce71
xhc044x <xhc044x@gmail.com>
2016-10-21 20:36:35 +0100

    2016-10-21's JavaScript workout

---
+++ contributions/xhc044x/JavaScript/Core/2016-10-21.js
@@ -0,0 +1,5 @@
+Precision
+Treating a boolean as number
+Hoisting applies only to variable declarations, not initializations
+Prevent background image flicker in **IE**
+Getting the *real* dimensions of an image
