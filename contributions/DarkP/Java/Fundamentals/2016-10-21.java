7b6f66be009c519ec23cfc94192fe958f8723b9b
DarkP <petr456@seznam.cz>
2016-10-21 17:06:50 +0100

    2016-10-21's Java workout

---
+++ contributions/DarkP/Java/Fundamentals/2016-10-21.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
