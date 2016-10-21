3c9636d862234f04f4b46637ae6d6bbf77009056
saffad <michal_kolsut@o2.pl>
2016-10-21 07:06:56 +0100

    2016-10-21's Java workout

---
+++ contributions/saffad/Java/Fundamentals/2016-10-21.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Use meaningful names
+Common mistake on switch statements
+Calling one constructor from another
