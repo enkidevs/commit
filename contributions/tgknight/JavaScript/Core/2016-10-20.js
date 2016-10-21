9ac2f5f3ba60b5aa23827377061eec32cfa2d7a9
tgknight <tgcruasde@gmail.com>
2016-10-21 15:30:33 +0100

    2016-10-20's JavaScript workout

---
+++ contributions/tgknight/JavaScript/Core/2016-10-20.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Data type comparison in `switch` statements
+Detect online connection
+Getting the *real* dimensions of an image
+`console.*` in JavaScript
