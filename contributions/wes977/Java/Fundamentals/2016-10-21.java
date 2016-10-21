4ef60850aea5e8027d1ebe1ea3cc05829a14d0fb
wes977 <wes977@gmail.com>
2016-10-21 21:37:34 +0100

    2016-10-21's Java workout

---
+++ contributions/wes977/Java/Fundamentals/2016-10-21.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
