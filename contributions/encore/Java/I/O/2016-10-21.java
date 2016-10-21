fdb9a65842e51c7c8a7d2c303d6bf5996d5555a2
encore <kingkonig@gmail.com>
2016-10-21 09:19:06 +0100

    2016-10-21's Java workout

---
+++ contributions/encore/Java/I/O/2016-10-21.java
@@ -0,0 +1,5 @@
+How to list all files in a directory that match a filename extension in Java
+Reading file attributes
+Retrieving file store attributes
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
