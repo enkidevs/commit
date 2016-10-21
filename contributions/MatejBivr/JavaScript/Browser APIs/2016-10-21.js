71af430e6fa602551e3e50d03dbc98bfe66f3691
MatejBivr <matej.pretnar@protonmail.ch>
2016-10-21 21:22:38 +0100

    2016-10-21's JavaScript workout

---
+++ contributions/MatejBivr/JavaScript/Browser APIs/2016-10-21.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+CSS for when JavaScript is enabled
+Modify a website's URL
+Warn user if **Back** button is pressed
+High Resolution Time API
