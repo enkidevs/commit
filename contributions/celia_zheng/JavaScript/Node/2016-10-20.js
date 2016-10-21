001dfa4c07f94422b4d912d7f820d6f1618b880c
celia_zheng <yuehyunzheng@outlook.com>
2016-10-21 01:05:32 +0100

    2016-10-20's JavaScript workout

---
+++ contributions/celia_zheng/JavaScript/Node/2016-10-20.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+Streams in **Node**
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
+Basic web server with Express
