bd6197ff76fa32ca06e1d9a6a7f2516e31c859ec
Pizzicato <pabloguaza@hotmail.com>
2016-10-21 06:51:01 +0100

    2016-10-21's Linux workout

---
+++ contributions/Pizzicato/Linux/System Management/2016-10-21.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
