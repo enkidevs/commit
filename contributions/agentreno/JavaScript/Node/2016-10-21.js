3b77e8799c4fdc11842e1959a2345c557e6d1335
agentreno <kturrell@hotmail.com>
2016-10-21 07:34:44 +0100

    2016-10-21's JavaScript workout

---
+++ contributions/agentreno/JavaScript/Node/2016-10-21.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Asynchronous File Write/Read in Node.js
+First-error callbacks in **Node**
+Wrapping errors in Node.js using _node-verror_
+Middleware/pipeline design pattern
