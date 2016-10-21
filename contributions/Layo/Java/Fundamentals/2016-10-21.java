6bcc1e16ecd959cdfe0be2b2a9d8dfa619a0a688
Layo <thegamerult@googlemail.com>
2016-10-21 07:38:00 +0100

    2016-10-21's Java workout

---
+++ contributions/Layo/Java/Fundamentals/2016-10-21.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
