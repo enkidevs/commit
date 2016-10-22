7b003cb87b76cbb478bdb279e602a09d6511c173
aquincy <brennanjl003@gmail.com>
2016-10-22 19:24:30 +0100

    2016-10-22's Java workout

---
+++ contributions/aquincy/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Using Inheritance to reduce code repetition
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
