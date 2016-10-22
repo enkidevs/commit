db9d6ce124cfce700ff2d95357f839e433e32c77
tivvit <tivvit@seznam.cz>
2016-10-22 11:10:48 +0100

    2016-10-22's Git workout

---
+++ contributions/tivvit/Git/Tools/2016-10-22.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Diff images using *ExifTool*
+Create a new branch from a stash
+Remember the results of previous hunk conflicts
+Debug using binary search
