8cf349a1e09518628d54ed2a6c3d8fa904aaf576
Aylard <aylard7@gmail.com>
2016-10-22 23:17:58 +0100

    2016-10-22's Linux workout

---
+++ contributions/Aylard/Linux/File Management/2016-10-22.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
