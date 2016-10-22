9271d1063579435b734f0dabac09acb831bf1c0b
Kertt123 <krzysztof_serkowski@interia.pl>
2016-10-22 22:09:01 +0100

    2016-10-22's Java workout

---
+++ contributions/Kertt123/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
