b26067b58e708ce870dfd1f02d853fc8165d4985
savec80 <artem.savenkov@gmail.com>
2016-10-22 09:44:11 +0100

    2016-10-22's Linux workout

---
+++ contributions/savec80/Linux/Package Management/2016-10-22.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+List installed packages
+Downgrade a package via `apt-get`
+Keeping repos updated with `cron`
