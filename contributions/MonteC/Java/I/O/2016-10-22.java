86193d62ea049a323df1d8d5660bf9f5f146c950
MonteC <jarekucinski@gmail.com>
2016-10-22 21:59:52 +0100

    2016-10-22's Java workout

---
+++ contributions/MonteC/Java/I/O/2016-10-22.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+How to list all files in a directory that match a filename extension in Java
+Translating `Scanner` tokens into primitive types
+Deleting files and directories
+The `Console` class
