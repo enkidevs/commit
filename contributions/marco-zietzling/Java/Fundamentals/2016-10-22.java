732ede4262d09eb7265eed84b6b11a5cbd4f155c
marco-zietzling <marco.zietzling@gmail.com>
2016-10-22 09:42:59 +0100

    2016-10-22's Java workout

---
+++ contributions/marco-zietzling/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
