a092ac0a7ce71b0d1539568c90324afcde79b885
Slaik000 <slaik000@live.com>
2016-10-22 13:33:18 +0100

    2016-10-22's JavaScript workout

---
+++ contributions/Slaik000/JavaScript/Npm/2016-10-22.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Manipulate npm packages cache
+Passing through command line arguments in npm scripts
+Manage local node modules with `npm link`
+devDependencies in `npm`
