ac11b7d7bb995502298a2b31c1ad135dbab9e42e
MagicEinhorn2000 <ks.wischmop@gmail.com>
2016-10-22 15:49:58 +0100

    2016-10-22's Linux workout

---
+++ contributions/MagicEinhorn2000/Linux/System Management/2016-10-22.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Commands to shutdown or restart the system
+Cancel a system shutdown
+Finding Open Files With `lsof`
