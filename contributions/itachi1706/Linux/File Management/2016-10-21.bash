95d9aeddcd3ec6172e4fc8d3d6a2c1121e46281f
itachi1706 <kennethsohyq@hotmail.com>
2016-10-22 02:29:35 +0100

    2016-10-21's Linux workout

---
+++ contributions/itachi1706/Linux/File Management/2016-10-21.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
