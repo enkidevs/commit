925d87e91eac4eed87034a0f4a30d474e900f17f
TheAl_T <branislav.bajuzik@gmail.com>
2016-10-22 20:14:55 +0100

    2016-10-22's Java workout

---
+++ contributions/TheAl_T/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Writing generic methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
