6fe95e89da0e41959dbe2a23865a4f625af317cd
marcsances <marcsances@gmail.com>
2016-10-22 19:31:57 +0100

    2016-10-22's Java workout

---
+++ contributions/marcsances/Java/Error Handling/2016-10-22.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Careful Numeric Data Conversions
