94407f0cdca0b6edc39b301405005c491d5eccf3
Jebediah <jefwillems100@gmail.com>
2016-10-22 08:12:03 +0100

    2016-10-22's Java workout

---
+++ contributions/Jebediah/Java/I/O/2016-10-22.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Create system file paths using the `Path` class
+Using buffered streams
+Reading file attributes
+Retrieving file store attributes
