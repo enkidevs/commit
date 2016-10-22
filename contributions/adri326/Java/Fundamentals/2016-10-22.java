b8cd4fa4d4e2f0fdd09790e8ae77950f5c78e055
adri326 <adrien.burgun@orange.fr>
2016-10-22 04:25:45 +0100

    2016-10-22's Java workout

---
+++ contributions/adri326/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
