093b7abfd9e304b2639e79ccc8a8109f63fefde9
madoke <madoke@gmail.com>
2016-10-22 15:27:13 +0100

    2016-10-22's Java workout

---
+++ contributions/madoke/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
