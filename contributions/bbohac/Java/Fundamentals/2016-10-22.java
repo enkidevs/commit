e3ecd2f7ff53ac2a86b071859bc1e49f78d419f5
bbohac <bastian.bohac@gmail.com>
2016-10-22 13:57:08 +0100

    2016-10-22's Java workout

---
+++ contributions/bbohac/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
+Using `Printstream.format` to format a string before printing
+Limit Accessibility of `Fields`
