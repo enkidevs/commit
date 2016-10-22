c8f74dd963ae2c9a597e1f9a7dffd2a224a5446b
lmgalan <luismigalan@gmail.com>
2016-10-22 08:03:05 +0100

    2016-10-22's Java workout

---
+++ contributions/lmgalan/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
