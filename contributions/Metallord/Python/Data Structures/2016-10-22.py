3c298b219634b9df4cabd858cd9c66cf1304de4d
Metallord <alexander.werner.1987@gmail.com>
2016-10-22 22:31:23 +0100

    2016-10-22's Python workout

---
+++ contributions/Metallord/Python/Data Structures/2016-10-22.py
@@ -0,0 +1,5 @@
+Following PEP 8 styling guideline.
+A simple way to select a random item from a `list/tuple` data stucture
+Get more with `collections`!
+Get the most of `float`s
+`bytearray` objects
