461d68491aad0ffcf25640e4a3d017c3e574adcc
zero9 <c.lobrano@gmail.com>
2016-10-22 20:01:55 +0100

    2016-10-22's Linux workout

---
+++ contributions/zero9/Linux/System Management/2016-10-22.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Common log files
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
