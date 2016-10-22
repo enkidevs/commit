0724817d29fb02bb50462b58ab79fdb9259642b2
jordiferrero <jordiferrer97@gmail.com>
2016-10-22 10:15:12 +0100

    2016-10-22's Java workout

---
+++ contributions/jordiferrero/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Ternary operator
+Use `DecimalFormat` class to format numbers
+How range operations work
