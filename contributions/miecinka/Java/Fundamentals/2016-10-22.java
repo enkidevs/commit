b3ad8c845ec1cea3a67764744d5a045d9f85b111
miecinka <mietinhovanish@gmail.com>
2016-10-22 17:18:42 +0100

    2016-10-22's Java workout

---
+++ contributions/miecinka/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Generating random numbers
+The trade-off between short and descriptive names
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
