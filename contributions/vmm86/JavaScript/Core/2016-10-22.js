d8c84a71ac54cde00435c1c85478d88238646fcf
vmm86 <m.vasilyev86@gmail.com>
2016-10-22 12:19:18 +0100

    2016-10-22's JavaScript workout

---
+++ contributions/vmm86/JavaScript/Core/2016-10-22.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Performing a function at timed intervals
+Use `splice()` to remove an item from an array
+The difference between null, undefined and NaN
+Base conversion 
