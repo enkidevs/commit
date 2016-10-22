056e575949e136e9085825d27f1b3fbfc131cdf9
Asicoder <asicoderpower@gmail.com>
2016-10-22 18:30:23 +0100

    2016-10-22's Java workout

---
+++ contributions/Asicoder/Java/Data Types/2016-10-22.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Collections vs arrays
+Use `Arrays.asList()` to initialise lists
+Converting numbers to strings
