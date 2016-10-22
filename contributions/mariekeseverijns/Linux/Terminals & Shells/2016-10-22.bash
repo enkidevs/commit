12f1cd2752dcb9c98baeabfe50b51a155edf2844
mariekeseverijns <mariekeseverijns93@gmail.com>
2016-10-22 11:18:58 +0100

    2016-10-22's Linux workout

---
+++ contributions/mariekeseverijns/Linux/Terminals & Shells/2016-10-22.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Random password generator
+Adding directories to your `$PATH`
