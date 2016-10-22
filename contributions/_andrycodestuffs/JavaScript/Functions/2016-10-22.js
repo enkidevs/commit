e20061f86c4831b29960c7cceb1b485ecdd89edc
_andrycodestuffs <andrybusa@gmail.com>
2016-10-22 18:41:51 +0100

    2016-10-22's JavaScript workout

---
+++ contributions/_andrycodestuffs/JavaScript/Functions/2016-10-22.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+IIFE pattern
+Easily generate a random `HEX` color
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
