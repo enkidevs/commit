e1148cabe2392f5fe33c2c33cc0b9454c0fd19fc
mleczey <mleczey@gmail.com>
2016-10-22 14:58:19 +0100

    2016-10-22's Linux workout

---
+++ contributions/mleczey/Linux/File Management/2016-10-22.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Using `touch` to alter files modification time
