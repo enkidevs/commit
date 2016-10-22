d5df31d36c93900e36ace2991d31710f6ad79ebf
tawe <munn.jon@gmail.com>
2016-10-22 01:48:13 +0100

    2016-10-21's JavaScript workout

---
+++ contributions/tawe/JavaScript/Npm/2016-10-21.js
@@ -0,0 +1,5 @@
+Combining script commands in npm
+Environmental variables and flags for `npm`
+`npm`  as an alternative to Gulp
+`npm` verifies `sha1` hashes of packages
+Lock down dependency versions by shrinkwrapping
