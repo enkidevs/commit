a40066ef82b2f5902aa517b7196a91c5adf794af
luxpsycho <enki@citrusfruit.lu>
2016-10-22 13:36:18 +0100

    2016-10-22's Linux workout

---
+++ contributions/luxpsycho/Linux/File Management/2016-10-22.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Working with `.gz` files
+Easy way of sharing files
