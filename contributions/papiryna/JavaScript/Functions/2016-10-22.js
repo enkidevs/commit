b159a8feaf993c6cfda29b7a20490ff56e9a9033
papiryna <papiryna@gmail.com>
2016-10-22 16:26:57 +0100

    2016-10-22's JavaScript workout

---
+++ contributions/papiryna/JavaScript/Functions/2016-10-22.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+IIFE pattern
+Check if a document is done loading
