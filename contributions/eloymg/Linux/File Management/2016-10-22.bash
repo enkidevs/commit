4575140ccf2fb5a96a49c501e15d3051ed89b1d8
eloymg <eloymoga@gmail.com>
2016-10-22 14:09:45 +0100

    2016-10-22's Linux workout

---
+++ contributions/eloymg/Linux/File Management/2016-10-22.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
