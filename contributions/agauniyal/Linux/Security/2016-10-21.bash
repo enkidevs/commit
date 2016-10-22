12f77b956bdc750d7d816c15c3dde76ca549748c
agauniyal <dev.agauniyal@outlook.com>
2016-10-22 04:17:21 +0100

    2016-10-21's Linux workout

---
+++ contributions/agauniyal/Linux/Security/2016-10-21.bash
@@ -0,0 +1,5 @@
+Security news delivered via DNS
+Extracting the `public key` from the `private key`
+Disable SSH password authentication 
+Disabling **USB** storage devices
+Keeping `/boot` read-only
