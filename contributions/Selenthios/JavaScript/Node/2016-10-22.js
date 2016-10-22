85a9d65df94691577a5aba6ac6a4eaf6b3f4ff9d
Selenthios <karstenspencer@yahoo.com>
2016-10-22 20:26:49 +0100

    2016-10-22's JavaScript workout

---
+++ contributions/Selenthios/JavaScript/Node/2016-10-22.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+How to debug Node.js
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
