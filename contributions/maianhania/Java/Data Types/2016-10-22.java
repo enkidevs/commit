05f46f8e3a0452219f2d14dad29075e7e736de61
maianhania <maianhania@yahoo.com>
2016-10-22 20:28:42 +0100

    2016-10-22's Java workout

---
+++ contributions/maianhania/Java/Data Types/2016-10-22.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+`StringBuffer` vs `StringBuilder`
+Retrieve the component type of an array
