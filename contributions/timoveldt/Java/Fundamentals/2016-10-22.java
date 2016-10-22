fb233f600a7bdee46c2c90376f9819cc2578a750
timoveldt <timo.veldt@gmail.com>
2016-10-22 20:17:29 +0100

    2016-10-22's Java workout

---
+++ contributions/timoveldt/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: setting a file's owner
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
