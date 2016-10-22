a51b8a2884bb1cd4965cddaf8f1b46138e02a185
zszuri <szuri90@gmail.com>
2016-10-22 14:13:28 +0100

    2016-10-22's Java workout

---
+++ contributions/zszuri/Java/I/O/2016-10-22.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Create system file paths using the `Path` class
+Checking for the existence of files and directories
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
