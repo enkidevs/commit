20444e14f748f3dfd8bf0cc9be2034bd52547b7b
aaltaev <andalt92@gmail.com>
2016-10-22 08:00:28 +0100

    2016-10-21's Java workout

---
+++ contributions/aaltaev/Java/Error Handling/2016-10-21.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Throwing an `exception`
