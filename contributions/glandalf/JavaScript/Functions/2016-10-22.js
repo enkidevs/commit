12cf2d07a06967a65594e6ade9aac1b51934fe84
glandalf <sjehann@gmail.com>
2016-10-22 17:31:12 +0100

    2016-10-22's JavaScript workout

---
+++ contributions/glandalf/JavaScript/Functions/2016-10-22.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Easily generate a random `HEX` color
+Move cursor at the end of text input
+Social sharing without widgets
+Check if a document is done loading
