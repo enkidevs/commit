bef0eb290222fd85df035d63218afaf8b204f205
skuroda <sk@mieco.org>
2016-10-22 04:51:02 +0100

    2016-10-21's Linux workout

---
+++ contributions/skuroda/Linux/System Management/2016-10-21.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Test disk speed with `dd`
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
