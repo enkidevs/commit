b703270ce55441aa44992ecce7a793bde1d7b393
surya-github <suryaprabhakark@gmail.com>
2016-10-22 14:05:58 +0100

    2016-10-21's Linux workout

---
+++ contributions/surya-github/Linux/File Management/2016-10-21.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
