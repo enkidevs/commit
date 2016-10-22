39d861a9d0ebbc46f58b6623b7a61bb93d2fd004
fullammo <bendegulyas1@gmail.com>
2016-10-22 13:31:41 +0100

    2016-10-22's JavaScript workout

---
+++ contributions/fullammo/JavaScript/Node/2016-10-22.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+First-error callbacks in **Node**
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
