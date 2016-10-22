b0863bf9a4ae00b7c90392f20f7a553593eb2a02
dabizk <dabizk@yahoo.com>
2016-10-22 15:56:27 +0100

    2016-10-22's Java workout

---
+++ contributions/dabizk/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
