247a6da6555c032a99ac43bad3bc68d7e9fa956f
davecneal <davecneal@gmail.com>
2016-10-22 17:24:16 +0100

    2016-10-22's Java workout

---
+++ contributions/davecneal/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
