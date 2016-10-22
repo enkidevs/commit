9259d2e46726e0f7b5a3f4635d0217bf34522d3d
johncobley <john.cobley@comtek.co.uk>
2016-10-22 23:25:18 +0100

    2016-10-22's JavaScript workout

---
+++ contributions/johncobley/JavaScript/Functions/2016-10-22.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Easily generate a random `HEX` color
+Function call method
+Changing a functions context with `fn.call(object)`
