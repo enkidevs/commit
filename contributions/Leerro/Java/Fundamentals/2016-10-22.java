304a6cc8e6a0454930f1bb0f863a7a4185084d7e
Leerro <gabriel.f.bernard@gmail.com>
2016-10-22 21:45:50 +0100

    2016-10-22's Java workout

---
+++ contributions/Leerro/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Limit Accessibility of `Fields`
