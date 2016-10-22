bad49eda188fd7c8d16c9555b49e95983f0120ec
wrhansen <fattydingdong@gmail.com>
2016-10-22 05:40:15 +0100

    2016-10-21's Python workout

---
+++ contributions/wrhansen/Python/Data Structures/2016-10-21.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Special attributes of objects and classes
+Operations with `bytes` and `bytearray`
+How to count
+Special `queue` methods
