2f8c4855a9010fdb7abfcdcd3eb6a71914d2a899
Darklight3it <darklight3it@gmail.com>
2016-10-22 08:20:51 +0100

    2016-10-22's Linux workout

---
+++ contributions/Darklight3it/Linux/File Management/2016-10-22.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
