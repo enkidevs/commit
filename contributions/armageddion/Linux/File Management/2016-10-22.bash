aef7d646e2b702fca6b4fca1a5fd48090489154a
armageddion <armageddion@gmail.com>
2016-10-22 17:59:37 +0100

    2016-10-22's Linux workout

---
+++ contributions/armageddion/Linux/File Management/2016-10-22.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
