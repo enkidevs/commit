df31da7d98384a53e4796a9ee3f4764baf81f0bd
Jonas97 <jonasknaup1403@hotmail.de>
2016-10-22 22:34:58 +0100

    2016-10-22's Java workout

---
+++ contributions/Jonas97/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Java naming conventions
+Using `Printstream.format` to format a string before printing
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
