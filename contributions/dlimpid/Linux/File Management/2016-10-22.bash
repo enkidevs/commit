e66c144ba51da36c2126dcfa467e22f26c97854d
dlimpid <dlimpid@gmail.com>
2016-10-23 02:52:49 +0100

    2016-10-22's Linux workout

---
+++ contributions/dlimpid/Linux/File Management/2016-10-22.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Working with `.gz` files
+Find the unknown process preventing deleting of files
