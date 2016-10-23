01de78a95acd485ef0b910c544f194414059de88
Yadeki <ydekinder@telenet.be>
2016-10-23 09:12:35 +0100

    2016-10-23's Linux workout

---
+++ contributions/Yadeki/Linux/System Management/2016-10-23.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
