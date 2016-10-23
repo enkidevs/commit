80174a3e8ab77085ce01a51c9bd941631f26163d
bendobot <whatsbending@gmail.com>
2016-10-23 22:57:39 +0100

    2016-10-23's Linux workout

---
+++ contributions/bendobot/Linux/Package Management/2016-10-23.bash
@@ -0,0 +1,6 @@
+Backporting on Debian
+
+Installing a `.deb` package from the terminal
+Prevent updating a specific package in Debian systems
+Using `Tasksel` for software installation
+Using `PPAs`
