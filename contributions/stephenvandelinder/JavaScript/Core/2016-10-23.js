bac8568ef10fee83b98e239c399e53ea8f10226e
stephenvandelinder <stephen.vandelinder@gmail.com>
2016-10-23 17:38:05 +0100

    2016-10-23's JavaScript workout

---
+++ contributions/stephenvandelinder/JavaScript/Core/2016-10-23.js
@@ -0,0 +1,5 @@
+Deleting properties
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+Functions return `undefined` by default
+Scope and strict mode
