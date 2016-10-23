84ead4ffbfad65a590c4b3e91a32365ae5701d93
knax <npcmamaz@gmail.com>
2016-10-23 03:54:55 +0100

    2016-10-22's JavaScript workout

---
+++ contributions/knax/JavaScript/React/2016-10-22.js
@@ -0,0 +1,6 @@
+Type of the Children props
+componentWillReceiveProps Not Triggered After Mounting
+
+Immutability helpers in **React**
+Keyed fragments in **React**
+Shortcut for transferring props
