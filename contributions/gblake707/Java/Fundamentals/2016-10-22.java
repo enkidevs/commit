2374ddddc813233be0bc73a0d394f321b8669b79
gblake707 <gblake707@yahoo.com>
2016-10-23 09:59:18 +0100

    2016-10-22's Java workout

---
+++ contributions/gblake707/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use meaningful names
+The trade-off between short and descriptive names
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
