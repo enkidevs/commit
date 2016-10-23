ce2335e4e4390eeb8f7594462c1a3a656c77f667
trion <parmsingh129@yahoo.com>
2016-10-23 05:17:13 +0100

    2016-10-22's Java workout

---
+++ contributions/trion/Java/Classes & Objects/2016-10-22.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Using `static` fields
+The `abstract` keyword
+Compare classes and not class names
+String formatting and parsing with `SimpleDateFormat`
