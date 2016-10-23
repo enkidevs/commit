e9055360a2f5eb80e067fabc81761d5833246a84
akccakcctw <akccakccwww@gmail.com>
2016-10-23 15:19:27 +0100

    2016-10-22's Linux workout

---
+++ contributions/akccakcctw/Linux/System Management/2016-10-22.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Change your `hostname` on systems using `systemd`
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
