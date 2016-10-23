6541908b73bb62a9314ee1b8d14b94a0fc7c969f
Aylard <aylard7@gmail.com>
2016-10-23 12:16:09 +0100

    2016-10-23's JavaScript workout

---
+++ contributions/Aylard/JavaScript/Npm/2016-10-23.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Registry user accounts for npm
+npm can handle multiple versions of a dependency
+List binaries for scripting in npm
+`config` object in `package.json`
