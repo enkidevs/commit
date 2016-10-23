e83295edc456b03452cdf3653c7f14150217db7c
mpes <michal.pesicka@gmail.com>
2016-10-23 20:03:29 +0100

    2016-10-23's Git workout

---
+++ contributions/mpes/Git/Tools/2016-10-23.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Specify a commit by its ancestry
+Sign your work (using GPG)
+Show history of a function
+Remember the results of previous hunk conflicts
