6e3f1669c0c042d3c3dfdd96e2793b219b8e50d7
allfanmu <allfanmu@wp.pl>
2016-10-23 09:05:40 +0100

    2016-10-23's Java workout

---
+++ contributions/allfanmu/Java/Error Handling/2016-10-23.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Throwing an `exception`
+Careful Numeric Data Conversions
