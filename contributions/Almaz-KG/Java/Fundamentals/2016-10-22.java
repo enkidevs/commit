cd450c2b8d9b71109a4bab385e6e5f419d8b257a
Almaz-KG <muktaalmaz@yandex.ru>
2016-10-23 16:59:56 +0100

    2016-10-22's Java workout

---
+++ contributions/Almaz-KG/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+Catch multiple exceptions in a single `catch` block
+Limit Accessibility of `Fields`
