03f4522549b57cd526ade9cbf1b643643695701a
XelorRelin <xelor@ya.ru>
2016-10-23 09:52:18 +0100

    2016-10-23's Python workout

---
+++ contributions/XelorRelin/Python/Data Structures/2016-10-23.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+When `range` comes in handy
+Get more with `collections`!
+Keep things in order with `OrderedDict`
+Special `queue` methods
