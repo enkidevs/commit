a953f12b899da88b9d8113edefea08e8304d42aa
thorseye <jonas@thoursie.net>
2016-10-23 19:26:32 +0100

    2016-10-23's JavaScript workout

---
+++ contributions/thorseye/JavaScript/React/2016-10-23.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Enhanced Prop Validation 
+Custom validations for props
+Specify a single child
