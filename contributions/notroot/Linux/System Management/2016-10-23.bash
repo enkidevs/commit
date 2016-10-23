fe509c94f1a12fa5cd3d618b4dbdc18085407d97
notroot <notroot@exler.info>
2016-10-23 12:35:49 +0100

    2016-10-23's Linux workout

---
+++ contributions/notroot/Linux/System Management/2016-10-23.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
