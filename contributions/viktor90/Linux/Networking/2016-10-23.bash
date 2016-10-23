a5a4cfc639b18331e95b9e982013187ec17fe909
viktor90 <v.georgiev90@abv.bg>
2016-10-23 08:41:05 +0100

    2016-10-23's Linux workout

---
+++ contributions/viktor90/Linux/Networking/2016-10-23.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+Get real network statistics with `slurm`
+Limit the `wget` download rate
+Getting the connection speed from the terminal
+Short Introduction to `Wget` 
