39daba7e866da40cf4c22f1dd408efc47276c65b
mateusluizfb <mateuslfreitasb@gmail.com>
2016-10-23 21:29:53 +0100

    2016-10-23's Java workout

---
+++ contributions/mateusluizfb/Java/Fundamentals/2016-10-23.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
