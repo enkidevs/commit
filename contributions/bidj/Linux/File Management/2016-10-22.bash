4059380488031d7fb4b76bf4b5a392ebae80ef1e
bidj <enki@papito.33mail.com>
2016-10-23 02:07:13 +0100

    2016-10-22's Linux workout

---
+++ contributions/bidj/Linux/File Management/2016-10-22.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Working with `.gz` files
