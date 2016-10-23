fb732c01c5e6efa54674c2ef8efbd775955c4f20
Nikon <tsukidesho@gmail.com>
2016-10-23 15:13:07 +0100

    2016-10-23's JavaScript workout

---
+++ contributions/Nikon/JavaScript/Node/2016-10-23.js
@@ -0,0 +1,5 @@
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+Manage child processes in **Node**
+Bulk-Write in **Node** with `cork()`
+`try-catch` only for **sync** code
