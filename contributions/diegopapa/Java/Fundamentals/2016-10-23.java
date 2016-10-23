3a77a6dafd402e9af1c76c6797d41e19328b9a31
diegopapa <diego.papa.job@gmail.com>
2016-10-23 15:53:17 +0100

    2016-10-23's Java workout

---
+++ contributions/diegopapa/Java/Fundamentals/2016-10-23.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
