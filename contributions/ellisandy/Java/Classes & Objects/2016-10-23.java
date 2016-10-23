b16829e2b92c2ebc172492b7838001a441cbe17f
ellisandy <andrew@andrewryanellis.com>
2016-10-23 16:46:13 +0100

    2016-10-23's Java workout

---
+++ contributions/ellisandy/Java/Classes & Objects/2016-10-23.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+The `abstract` keyword
+Double Brace Initialization
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
