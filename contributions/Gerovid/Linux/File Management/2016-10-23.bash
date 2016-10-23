d4b6bba7a66f52d8f74a03271997ba918decca82
Gerovid <gerovid@gmail.com>
2016-10-23 14:53:36 +0100

    2016-10-23's Linux workout

---
+++ contributions/Gerovid/Linux/File Management/2016-10-23.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Working with `.gz` files
