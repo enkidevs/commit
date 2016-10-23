a1ad5a584136b953073c9cfc7efe76ff6ab7c353
manuxviii <emmanuel.mesnige@protonmail.com>
2016-10-23 21:42:36 +0100

    2016-10-23's Linux workout

---
+++ contributions/manuxviii/Linux/System Management/2016-10-23.bash
@@ -0,0 +1,6 @@
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
