5c46383571a70998d4cc0a50882ac8eb1f31694e
akrosalsky <akrosalsky@gmail.com>
2016-10-23 17:57:25 +0100

    2016-10-23's Linux workout

---
+++ contributions/akrosalsky/Linux/Terminals & Shells/2016-10-23.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
