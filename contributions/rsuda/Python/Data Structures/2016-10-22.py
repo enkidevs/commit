fe032116c2addfbdba5fe1e66edb2b9a5230c0a0
rsuda <rsuda9@gmail.com>
2016-10-23 06:56:26 +0100

    2016-10-22's Python workout

---
+++ contributions/rsuda/Python/Data Structures/2016-10-22.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Special attributes of objects and classes
+Looping techniques
+There is more to copying
+Keep things in order with `OrderedDict`
