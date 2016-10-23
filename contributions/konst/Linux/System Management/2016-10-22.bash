667f1f1868efa13edcb0e268c49b7b7a371d4e08
konst <kosnar@gmail.com>
2016-10-23 02:25:45 +0100

    2016-10-22's Linux workout

---
+++ contributions/konst/Linux/System Management/2016-10-22.bash
@@ -0,0 +1,5 @@
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
