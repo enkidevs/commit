4b1184ec7feab6338340eaf26bdb19a8219ab7be
Blitzphantom <charlycurmont@orange.fr>
2016-10-24 00:06:47 +0100

    2016-10-23's Java workout

---
+++ contributions/Blitzphantom/Java/Data Types/2016-10-23.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Finding a substring in a string
+Equals operation on different data types
