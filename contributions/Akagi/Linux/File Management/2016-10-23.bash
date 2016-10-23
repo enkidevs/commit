378794612f7937f8f49697ab3822a12b347cf1d4
Akagi <stringtheory@gmx.net>
2016-10-23 09:10:35 +0100

    2016-10-23's Linux workout

---
+++ contributions/Akagi/Linux/File Management/2016-10-23.bash
@@ -0,0 +1,7 @@
+Finding file with regexes
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
+Easy way of sharing files
