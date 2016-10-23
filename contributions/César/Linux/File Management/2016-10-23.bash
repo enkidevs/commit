93f6e1f279b4d309c8be919dee7dba7e8475db53
César <cesarbernedo@free.fr>
2016-10-23 13:08:57 +0100

    2016-10-23's Linux workout

---
+++ contributions/César/Linux/File Management/2016-10-23.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Delete commands aliases
