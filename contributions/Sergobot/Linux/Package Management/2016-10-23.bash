19ad45eff720cbc605ae6201d82884cde3e9e8d7
Sergobot <sergobot@protonmail.com>
2016-10-23 07:09:04 +0100

    2016-10-23's Linux workout

---
+++ contributions/Sergobot/Linux/Package Management/2016-10-23.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+Installing a `.deb` package from the terminal
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
