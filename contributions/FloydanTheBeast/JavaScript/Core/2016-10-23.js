20a28303ebdc52a72d8a5464a464a23b4cd638f4
FloydanTheBeast <floydanthebeast@gmail.com>
2016-10-23 21:32:24 +0100

    2016-10-23's JavaScript workout

---
+++ contributions/FloydanTheBeast/JavaScript/Core/2016-10-23.js
@@ -0,0 +1,5 @@
+Performing a function at timed intervals
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+How to merge two arrays
+Base conversion 
