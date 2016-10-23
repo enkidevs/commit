c0a863aea17e97700854da6b3e6213de2cd8640a
comeste10 <comeste10@gmail.com>
2016-10-23 18:13:46 +0100

    2016-10-23's Linux workout

---
+++ contributions/comeste10/Linux/Networking/2016-10-23.bash
@@ -0,0 +1,5 @@
+Find services running on your host
+Useful ssh client optimizations 
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
+Short Introduction to `Wget` 
