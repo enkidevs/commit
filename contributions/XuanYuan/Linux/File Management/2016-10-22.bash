639c688a2de12efa2b8023ef0dac5e7b15a2d932
XuanYuan <ookk8282@gmail.com>
2016-10-23 13:20:27 +0100

    2016-10-22's Linux workout

---
+++ contributions/XuanYuan/Linux/File Management/2016-10-22.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
