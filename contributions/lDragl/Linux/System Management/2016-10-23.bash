b956e49a41ef71fe33feb70ce6e6cdc9e6c84ddd
lDragl <ludo@ezyr.xyz>
2016-10-23 10:24:11 +0100

    2016-10-23's Linux workout

---
+++ contributions/lDragl/Linux/System Management/2016-10-23.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
