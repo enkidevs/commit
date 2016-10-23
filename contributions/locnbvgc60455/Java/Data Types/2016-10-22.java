cc5308389314785c25aa69057a8fd953bdf25492
locnbvgc60455 <locnbvgc60455@fpt.edu.vn>
2016-10-23 02:13:23 +0100

    2016-10-22's Java workout

---
+++ contributions/locnbvgc60455/Java/Data Types/2016-10-22.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Finding a substring in a string
