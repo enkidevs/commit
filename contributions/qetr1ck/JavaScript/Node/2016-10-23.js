e4917a718c7ff880d60bbbaa9f14d1c643cfdb41
qetr1ck <warlok000@rambler.ru>
2016-10-23 09:16:27 +0100

    2016-10-23's JavaScript workout

---
+++ contributions/qetr1ck/JavaScript/Node/2016-10-23.js
@@ -0,0 +1,5 @@
+`uncaughtException` listener in Node.js
+Bulk-Write in **Node** with `cork()`
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
+Avoid using `sudo` when running Node
