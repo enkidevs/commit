76bb3f26451228cc74415b45a1878a8fe4ea2009
kaitoii11 <kaitoii11@hotmail.com>
2016-10-23 15:54:40 +0100

    2016-10-22's Java workout

---
+++ contributions/kaitoii11/Java/Fundamentals/2016-10-22.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Using an interface as a parameter
