8377a54e7a4227bb3b1d40f8a240ef92c6224227
lotbs <nkosindlovu87@gmail.com>
2016-10-24 00:21:53 +0100

    2016-10-23's Linux workout

---
+++ contributions/lotbs/Linux/Terminals & Shells/2016-10-23.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Adding directories to your `$PATH`
