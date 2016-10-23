961b1c7295591b8fea3943dd62f8dd494de57ed9
tommot <thomas.prebeck@web.de>
2016-10-23 10:20:32 +0100

    2016-10-23's JavaScript workout

---
+++ contributions/tommot/JavaScript/Npm/2016-10-23.js
@@ -0,0 +1,5 @@
+npm can handle multiple versions of a dependency
+Manage local node modules with `npm link`
+`npm` verifies `sha1` hashes of packages
+Current Lifecycle Event
+Reduce package duplication 
