1f02ed345ba111b435b416744859c50228b2e619
dkarv <davkrebs@gmail.com>
2016-10-23 10:04:23 +0100

    2016-10-23's JavaScript workout

---
+++ contributions/dkarv/JavaScript/Core/2016-10-23.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Deleting properties
+Double and single quotes
+Hoisting applies only to variable declarations, not initializations
+The difference between null, undefined and NaN
