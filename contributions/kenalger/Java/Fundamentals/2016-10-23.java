7a1bb04a41df3c0a06da07cd1f4923a7f31253f8
kenalger <kenalger@comcast.net>
2016-10-23 14:09:25 +0100

    2016-10-23's Java workout

---
+++ contributions/kenalger/Java/Fundamentals/2016-10-23.java
@@ -0,0 +1,5 @@
+Generating random numbers
+Calling one constructor from another
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+How range operations work
