5d413323796352f7216de70b37f5fee2219c48c7
Vira <thomas.ruehrig@studierende.htl-donaustadt.at>
2016-10-23 09:35:06 +0100

    2016-10-23's Java workout

---
+++ contributions/Vira/Java/I/O/2016-10-23.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Reading file attributes
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+List contents of a directory with `newDirectoryStream`
