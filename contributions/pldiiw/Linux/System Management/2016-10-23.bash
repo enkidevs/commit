5d455fbb2da29f5da7e5ec1f30b08c26d43ae658
pldiiw <pldiiw@gmail.com>
2016-10-23 09:05:52 +0100

    2016-10-23's Linux workout

---
+++ contributions/pldiiw/Linux/System Management/2016-10-23.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Fixing `locale` issues in Debian systems
