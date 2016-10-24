dd5c7a0566c8bc70a7fa9fffefacd9f4fc60ef1b
SkiFire13 <xxskifire13xx@gmail.com>
2016-10-24 06:39:53 +0100

    2016-10-24's Java workout

---
+++ contributions/SkiFire13/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
