d14654765932de8a12ede078c977e9f0e2b1ef71
ahaleen <adam.haleen@gmail.com>
2016-10-24 08:07:13 +0100

    2016-10-23's Linux workout

---
+++ contributions/ahaleen/Linux/Terminals & Shells/2016-10-23.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
