90af9d91e7f808a3dfbdbe06b82e772cac0e190b
TheAl_T <branislav.bajuzik@gmail.com>
2016-10-24 01:31:06 +0100

    2016-10-23's Java workout

---
+++ contributions/TheAl_T/Java/Fundamentals/2016-10-23.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
