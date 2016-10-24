2bafbbb269fe9800a2a87b1358f44613392d695d
mrstr <robert.str@o2.pl>
2016-10-24 07:39:45 +0100

    2016-10-24's Java workout

---
+++ contributions/mrstr/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Writing generic methods
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
