53f66e65c8fc5c2a31dc51a2e531fac99731651d
nicosim <nico93simo@hotmail.fr>
2016-10-24 15:21:11 +0100

    2016-10-24's Java workout

---
+++ contributions/nicosim/Java/Error Handling/2016-10-24.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Careful Numeric Data Conversions
