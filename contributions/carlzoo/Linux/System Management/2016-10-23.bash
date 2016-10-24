499299d8fa7de690692632572f701361f194d3fe
carlzoo <carl.j.zhou@protonmail.ch>
2016-10-24 02:21:21 +0100

    2016-10-23's Linux workout

---
+++ contributions/carlzoo/Linux/System Management/2016-10-23.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Finding Open Files With `lsof`
