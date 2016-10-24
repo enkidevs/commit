1d8cf76f54f494258aca97476939608354f88f86
dubiousradical <dubiousradical@hotmail.com>
2016-10-24 17:41:19 +0100

    2016-10-24's Java workout

---
+++ contributions/dubiousradical/Java/Error Handling/2016-10-24.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Throwing an `exception`
+Checking a double for `NaN` value
