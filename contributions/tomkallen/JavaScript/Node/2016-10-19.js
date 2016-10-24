f91ddc6334965da30951245a3e7110bf8812440a
tomkallen <comradebykoff@gmail.com>
2016-10-24 20:34:57 +0100

    2016-10-19's JavaScript workout

---
+++ contributions/tomkallen/JavaScript/Node/2016-10-19.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
+`try-catch` only for **sync** code
