0832e2a5acae59fde9e1f0fbea2aaebbeb6220e9
Error_404 <imboden.sam@gmail.com>
2016-10-24 12:31:11 +0100

    2016-10-24's Linux workout

---
+++ contributions/Error_404/Linux/File Management/2016-10-24.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
+Delete commands aliases
