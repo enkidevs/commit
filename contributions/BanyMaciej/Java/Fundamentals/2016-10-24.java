f2c964fe9683c8382ed74b8d45c54407128fbd8a
BanyMaciej <banymaciej@gmail.com>
2016-10-24 07:00:00 +0100

    2016-10-24's Java workout

---
+++ contributions/BanyMaciej/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Java naming conventions
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Always specify type parameter
