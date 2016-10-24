969a33712d52d6bcd98b946baa0c2094d0ced1fe
markormesher <me@markormesher.co.uk>
2016-10-24 07:51:43 +0100

    2016-10-24's Java workout

---
+++ contributions/markormesher/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
