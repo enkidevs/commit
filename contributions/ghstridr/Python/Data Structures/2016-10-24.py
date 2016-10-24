8ed4dcc3e78905886e3ae9d21cd07daf0f33ae0b
ghstridr <ghstridr@gmail.com>
2016-10-24 20:31:14 +0100

    2016-10-24's Python workout

---
+++ contributions/ghstridr/Python/Data Structures/2016-10-24.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+The fundamental `tuples` 
+Double ended queues with `deque`
+`Module`s everywhere!
+Special attributes of objects and classes
