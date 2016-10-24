e64001dab684b84d8aae23e50287b5afa864495b
eugenenosenko <eugenenosenko@gmail.com>
2016-10-24 07:14:31 +0100

    2016-10-24's Java workout

---
+++ contributions/eugenenosenko/Java/I/O/2016-10-24.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Create system file paths using the `Path` class
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
