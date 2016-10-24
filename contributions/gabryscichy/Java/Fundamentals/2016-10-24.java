e6cab4575bf4ab163d997b36fea4def225d6a668
gabryscichy <cmateuszm@gmail.com>
2016-10-24 21:48:53 +0100

    2016-10-24's Java workout

---
+++ contributions/gabryscichy/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Java naming conventions
+Writing generic methods
+Using Inheritance to reduce code repetition
+Using bounded type parameters in generic methods
