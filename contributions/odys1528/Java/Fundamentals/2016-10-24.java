45fb8db5dc1db6d442fd4df19a030cba22c25e66
odys1528 <jolanta.zakrzewska@orange.pl>
2016-10-24 12:51:26 +0100

    2016-10-24's Java workout

---
+++ contributions/odys1528/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Metadata: creating a user-defined file attribute
+Constant notation
+Using an interface as a parameter
