0a767c7cfa48e4195a67fdfcd3bd57fa4dfb7718
xmooner <gioruffa@gmail.com>
2016-10-24 07:04:26 +0100

    2016-10-24's Linux workout

---
+++ contributions/xmooner/Linux/System Management/2016-10-24.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Using `dd` To Create Disk Images
