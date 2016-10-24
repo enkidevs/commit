faea178a857b1012f99b31d3e85d831286233323
marco-zietzling <marco.zietzling@gmail.com>
2016-10-24 07:59:28 +0100

    2016-10-24's Java workout

---
+++ contributions/marco-zietzling/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
