b95651366f5b6b231dbef546cff87bc85be4b723
kaitoii11 <kaitoii11@hotmail.com>
2016-10-24 03:20:09 +0100

    2016-10-23's Linux workout

---
+++ contributions/kaitoii11/Linux/Package Management/2016-10-23.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Removing PPAs
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
+Keeping repos updated with `cron`
