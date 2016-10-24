5fedc07422cbe212abdcd9909bd832a7d9765f3f
dan339 <danielcampbell339@gmail.com>
2016-10-24 07:26:42 +0100

    2016-10-24's Java workout

---
+++ contributions/dan339/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using Inheritance to reduce code repetition
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+`Autoboxing` and `Unboxing`
