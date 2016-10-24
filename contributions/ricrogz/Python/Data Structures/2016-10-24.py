e5daf9f7217f7e20e3a90ae9de2be01d76cc37bb
ricrogz <ricrogz@gmail.com>
2016-10-24 08:15:25 +0100

    2016-10-24's Python workout

---
+++ contributions/ricrogz/Python/Data Structures/2016-10-24.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`Module`s everywhere!
+Operations with `bytes` and `bytearray`
+`weakref` callbacks
+Binary record packing with `bytearray`
