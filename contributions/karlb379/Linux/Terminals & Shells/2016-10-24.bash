f670b5e77fb5542ec4647d803777db1cbba08913
karlb379 <karlb379@gmail.com>
2016-10-24 06:21:41 +0100

    2016-10-24's Linux workout

---
+++ contributions/karlb379/Linux/Terminals & Shells/2016-10-24.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Conditional command execution
+
+(`&&` operator)
+The Basics of Environment Variables
