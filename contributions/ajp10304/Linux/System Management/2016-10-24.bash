e7280d4e37e62da3d825369d67ec865b9cc412b0
ajp10304 <a.pocklington@gmail.com>
2016-10-24 07:17:55 +0100

    2016-10-24's Linux workout

---
+++ contributions/ajp10304/Linux/System Management/2016-10-24.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Check the HDD with `badblocks`
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
