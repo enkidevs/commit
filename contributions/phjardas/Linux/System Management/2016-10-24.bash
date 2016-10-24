851dc98154ca18aae3111fc14216fd13e4d596bb
phjardas <philipp@jardas.de>
2016-10-24 14:13:42 +0100

    2016-10-24's Linux workout

---
+++ contributions/phjardas/Linux/System Management/2016-10-24.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
