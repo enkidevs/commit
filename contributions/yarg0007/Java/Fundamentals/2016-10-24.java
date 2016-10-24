ebb21331f33800c00f142e85114ce2b1fdb675cb
yarg0007 <benjamin.yarger@gmail.com>
2016-10-24 13:57:05 +0100

    2016-10-24's Java workout

---
+++ contributions/yarg0007/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
