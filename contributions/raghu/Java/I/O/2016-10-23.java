24a9ce8a47baff9b2d075cdd69cf616cbda66cad
raghu <raghu.9208@gmail.com>
2016-10-24 04:33:02 +0100

    2016-10-23's Java workout

---
+++ contributions/raghu/Java/I/O/2016-10-23.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+How to list all files in a directory that match a filename extension in Java
+Create system file paths using the `Path` class
+Reading file attributes
