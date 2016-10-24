f295494ee2da56bb3686d86c816e0468085ea123
Eldergeek <david@geekguytech.com>
2016-10-24 22:38:57 +0100

    2016-10-24's Linux workout

---
+++ contributions/Eldergeek/Linux/Terminals & Shells/2016-10-24.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Terminal based browser
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
