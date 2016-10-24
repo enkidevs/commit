182a426353ada063080fb6bf7fbddf19e76987c7
jbrixhe <j.brixhe@gmail.com>
2016-10-24 13:21:02 +0100

    2016-10-24's Java workout

---
+++ contributions/jbrixhe/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
+Using an interface as a parameter
