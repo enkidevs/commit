9cfd7cde3de9aaafbb31620acade0e31798c9da5
frangiskos <frangiskos@gmail.com>
2016-10-24 18:34:42 +0100

    2016-10-24's JavaScript workout

---
+++ contributions/frangiskos/JavaScript/Functions/2016-10-24.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Method chaining
+Get query/url variables
+Check if a document is done loading
