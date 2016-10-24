f24cfe81fcd77888919f835d42598d478854c3fe
bdauer <dauer.benjamin@gmail.com>
2016-10-24 13:13:35 +0100

    2016-10-24's JavaScript workout

---
+++ contributions/bdauer/JavaScript/Core/2016-10-24.js
@@ -0,0 +1,5 @@
+Be careful when using `typeof`.
+Undefined / Empty / Null Checks
+The difference between null, undefined and NaN
+Inaccuracy of binary floating-point format
+Round numbers to `N` decimals 
