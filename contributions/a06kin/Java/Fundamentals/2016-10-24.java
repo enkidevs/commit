16cfdbf0106f95c9fa2273276f9a9def1fb2a4bb
a06kin <a06kin@gmail.com>
2016-10-24 15:51:35 +0100

    2016-10-24's Java workout

---
+++ contributions/a06kin/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
