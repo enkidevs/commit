120781ac30cb34d90390963a6006f2738bf5650f
kevenYannic <keven.iwansky@gmail.com>
2016-10-24 16:15:17 +0100

    2016-10-24's Java workout

---
+++ contributions/kevenYannic/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
+Using an interface as a parameter
