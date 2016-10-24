104d86c0712b54f4fbe11a73ed29a5d7205fddcb
metsuri34 <metsaranta.mikko@gmail.com>
2016-10-24 17:23:13 +0100

    2016-10-24's Java workout

---
+++ contributions/metsuri34/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Common mistake on switch statements
+Distinction between `public` and `private` methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
