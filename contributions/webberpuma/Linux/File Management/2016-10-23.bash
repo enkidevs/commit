82360b3cfea65ad4cc0b40e7931e5e3180386348
webberpuma <webberpuma@gmail.com>
2016-10-24 03:15:34 +0100

    2016-10-23's Linux workout

---
+++ contributions/webberpuma/Linux/File Management/2016-10-23.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Working with `.gz` files
+Delete commands aliases
