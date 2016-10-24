ed572727f7938c7ab31c2282aea594e04e255150
FinnSell <contact@finn-thorben.me>
2016-10-24 10:27:13 +0100

    2016-10-24's Linux workout

---
+++ contributions/FinnSell/Linux/File Management/2016-10-24.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
