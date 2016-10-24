956fe66267fa4a92ed691f0f3b4def482feef26a
TheAl_T <branislav.bajuzik@gmail.com>
2016-10-24 23:59:47 +0100

    2016-10-24's Java workout

---
+++ contributions/TheAl_T/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
