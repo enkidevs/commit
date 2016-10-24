f8722dee2f2e3a874b5c6ed9331a8df522aac00c
Vebble <serafin21@gmail.com>
2016-10-24 16:56:56 +0100

    2016-10-24's Java workout

---
+++ contributions/Vebble/Java/Classes & Objects/2016-10-24.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Template methods in abstract classes
+Utility classes and `static` methods
+The `abstract` keyword
+String formatting and parsing with `SimpleDateFormat`
