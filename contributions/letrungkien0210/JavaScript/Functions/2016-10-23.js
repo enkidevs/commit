8b1ada1a9b56e1383a94e41f48833d54a146ab38
letrungkien0210 <letrungkien0210@gmail.com>
2016-10-24 06:41:38 +0100

    2016-10-23's JavaScript workout

---
+++ contributions/letrungkien0210/JavaScript/Functions/2016-10-23.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+IIFE pattern
+Get query/url variables
+Easily generate a random `HEX` color
