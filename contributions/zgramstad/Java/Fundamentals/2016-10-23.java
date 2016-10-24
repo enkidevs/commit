005ac5c4d7e4fcdc331e651f3b2cc4f9f58aa06d
zgramstad <zeg2@rice.edu>
2016-10-24 04:44:46 +0100

    2016-10-23's Java workout

---
+++ contributions/zgramstad/Java/Fundamentals/2016-10-23.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
