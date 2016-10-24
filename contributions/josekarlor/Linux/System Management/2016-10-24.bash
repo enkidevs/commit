ced5f140954b475db292fc558952ad3ae2541592
josekarlor <josecarlosdemula@gmail.com>
2016-10-24 09:19:37 +0100

    2016-10-24's Linux workout

---
+++ contributions/josekarlor/Linux/System Management/2016-10-24.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
