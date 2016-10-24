a58341a9fd2ca40f07860ee1ff783bfb12f54242
ahaleen <adam.haleen@gmail.com>
2016-10-25 00:17:26 +0100

    2016-10-24's Linux workout

---
+++ contributions/ahaleen/Linux/File Management/2016-10-24.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
+Delete commands aliases
