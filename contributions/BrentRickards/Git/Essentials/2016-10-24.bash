521d16b2c9ed692fed05b04b0c5d25c02dd9e17b
BrentRickards <brentr828@gmail.com>
2016-10-24 22:09:29 +0100

    2016-10-24's Git workout

---
+++ contributions/BrentRickards/Git/Essentials/2016-10-24.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
+Intent to add
