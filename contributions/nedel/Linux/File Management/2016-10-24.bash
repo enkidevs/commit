426490f6fab81beda0fad8377f11b6c102eb4204
nedel <nedel@gmx.com>
2016-10-24 05:40:28 +0100

    2016-10-24's Linux workout

---
+++ contributions/nedel/Linux/File Management/2016-10-24.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
