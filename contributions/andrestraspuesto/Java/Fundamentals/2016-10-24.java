4b6d6a43487b462a98475138a550a9915ec6bf83
andrestraspuesto <andrestraspuesto@gmail.com>
2016-10-24 20:56:47 +0100

    2016-10-24's Java workout

---
+++ contributions/andrestraspuesto/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
