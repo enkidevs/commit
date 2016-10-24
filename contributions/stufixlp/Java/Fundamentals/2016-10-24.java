921c08a8563ea80c737c9e214d06695860111e6c
stufixlp <stufixlp@gmail.com>
2016-10-25 00:06:57 +0100

    2016-10-24's Java workout

---
+++ contributions/stufixlp/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+Detect or prevent integer overflow
+`Autoboxing` and `Unboxing`
