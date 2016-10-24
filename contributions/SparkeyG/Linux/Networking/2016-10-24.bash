8737dce1abffd28c7a5bf0c5c9fe2b585ea8f7c8
SparkeyG <shawn.c.carroll@gmail.com>
2016-10-24 11:40:48 +0100

    2016-10-24's Linux workout

---
+++ contributions/SparkeyG/Linux/Networking/2016-10-24.bash
@@ -0,0 +1,5 @@
+Stress testing with `ab`
+Logging dropped firewall packets
+Multiple IP Address with aliasing 
+Use `tcpdump` to listen to network interface traffic
+Staying connected remotely via SSH without an interactive shell
