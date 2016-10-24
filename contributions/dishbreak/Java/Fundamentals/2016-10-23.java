6cf4d6099d7c25fc1dacc5a926decd08ffd5e495
dishbreak <dishbreak@gmail.com>
2016-10-24 08:16:50 +0100

    2016-10-23's Java workout

---
+++ contributions/dishbreak/Java/Fundamentals/2016-10-23.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
