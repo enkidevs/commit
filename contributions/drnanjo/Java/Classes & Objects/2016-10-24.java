632d270eabe23285604b393682484698f9748c55
drnanjo <drnanjo@aol.com>
2016-10-24 18:05:14 +0100

    2016-10-24's Java workout

---
+++ contributions/drnanjo/Java/Classes & Objects/2016-10-24.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Utility classes and `static` methods
+Double Brace Initialization
+How and when to use `WeakHashMap` class
