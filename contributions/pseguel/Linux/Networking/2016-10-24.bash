0db13bcca9414e97c8ea4db107c5b8c0413a802c
pseguel <pseguel@cisco.com>
2016-10-24 17:24:24 +0100

    2016-10-24's Linux workout

---
+++ contributions/pseguel/Linux/Networking/2016-10-24.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Useful ssh client optimizations 
+Sending emails from terminal / scripts
+Use `tcpdump` to listen to network interface traffic
+Staying connected remotely via SSH without an interactive shell
