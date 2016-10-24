e991a0e74018c1ab6d9d78395013516ae2549704
maianhania <maianhania@yahoo.com>
2016-10-24 22:09:23 +0100

    2016-10-24's Java workout

---
+++ contributions/maianhania/Java/Classes & Objects/2016-10-24.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Use a copy constructor for non-immutable objects
+The `abstract` keyword
+Double Brace Initialization
+String formatting and parsing with `SimpleDateFormat`
