f06990196b8ae052067003e52cb4cd2f09444f2c
bennuttle <bennuttle@gmail.com>
2016-10-24 12:13:57 +0100

    2016-10-24's Java workout

---
+++ contributions/bennuttle/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Using `synchronized` statements
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
