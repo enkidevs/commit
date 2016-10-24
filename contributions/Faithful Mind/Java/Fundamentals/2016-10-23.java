f2dd6a947bf84f768221085847a6ef0434829b3c
Faithful Mind <lpyy15@gmail.com>
2016-10-24 03:43:11 +0100

    2016-10-23's Java workout

---
+++ contributions/Faithful Mind/Java/Fundamentals/2016-10-23.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
