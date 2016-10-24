d1b0abd7ecd1d8acff12703b9f606a8f56f5e1f4
Kaijiro <tristan.lasalle@gmail.com>
2016-10-24 18:24:43 +0100

    2016-10-24's Java workout

---
+++ contributions/Kaijiro/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Using an interface as a parameter
