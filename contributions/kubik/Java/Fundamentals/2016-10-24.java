fb03dfb9e1d05660c6ea8638032918a68ca9c570
kubik <lkubic@poczta.onet.pl>
2016-10-24 07:35:32 +0100

    2016-10-24's Java workout

---
+++ contributions/kubik/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Using `Printstream.format` to format a string before printing
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+How range operations work
