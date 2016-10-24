97a114be27979f9bc288e60a2b4cc6875b46817e
Bibindoum <pierrick.guitter@gmail.com>
2016-10-24 15:47:18 +0100

    2016-10-24's Linux workout

---
+++ contributions/Bibindoum/Linux/Package Management/2016-10-24.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Removing PPAs
+Prevent updating a specific package in Debian systems
+Downgrade a package via `apt-get`
+Keeping repos updated with `cron`
