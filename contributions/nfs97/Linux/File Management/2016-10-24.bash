ed4dba6b4901e0b49a07991dea11dce4e859d959
nfs97 <vladronaldo7@gmail.com>
2016-10-24 14:22:51 +0100

    2016-10-24's Linux workout

---
+++ contributions/nfs97/Linux/File Management/2016-10-24.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Deleting non-empty directories
