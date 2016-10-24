b06ce87719079ac46cfea2c10b14c8baffda4d4c
loictessier <loictessier.inge@gmail.com>
2016-10-24 15:55:08 +0100

    2016-10-24's JavaScript workout

---
+++ contributions/loictessier/JavaScript/React/2016-10-24.js
@@ -0,0 +1,5 @@
+**React** in-line styles
+Self-closing tags in **JSX**
+`null` value for *Controlled Components* in **React**
+Expose Component Functions in **React**
+Dealing with `this.props.children`
