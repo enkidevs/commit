d68e463458f175d5d482919de3d8e01c21407c30
amaksoft <amaksoft@gmail.com>
2016-10-24 03:58:39 +0100

    2016-10-23's Java workout

---
+++ contributions/amaksoft/Java/Fundamentals/2016-10-23.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Using an interface as a parameter
