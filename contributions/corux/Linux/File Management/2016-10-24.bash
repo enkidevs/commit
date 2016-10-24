a8b57d1e1533531f6479b20d33376c90c5b21f96
corux <tobias@wallura.org>
2016-10-24 07:09:18 +0100

    2016-10-24's Linux workout

---
+++ contributions/corux/Linux/File Management/2016-10-24.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
