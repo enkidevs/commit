75fc6714ecf2584f633fe51f8484d19b2de6556f
gabriele92 <gabrigaudenzi@gmail.com>
2016-10-24 07:35:12 +0100

    2016-10-24's Java workout

---
+++ contributions/gabriele92/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
