e9f4718776b9bdbac643f5e19c3e9ecdc18962b5
samdy1 <samueliceblade@gmail.com>
2016-10-24 08:16:46 +0100

    2016-10-24's Linux workout

---
+++ contributions/samdy1/Linux/System Management/2016-10-24.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
