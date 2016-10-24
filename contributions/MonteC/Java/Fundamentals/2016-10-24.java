7caa68cfd65df54d3f4c7bb416800a5e99c46d8b
MonteC <jarekucinski@gmail.com>
2016-10-24 21:50:08 +0100

    2016-10-24's Java workout

---
+++ contributions/MonteC/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+`Autoboxing` and `Unboxing`
