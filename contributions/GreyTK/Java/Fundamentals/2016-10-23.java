2fc7c12b0b69f0fda7778bf586aa7f53934f9c47
GreyTK <greytklein@gmail.com>
2016-10-24 01:34:43 +0100

    2016-10-23's Java workout

---
+++ contributions/GreyTK/Java/Fundamentals/2016-10-23.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Generating random numbers
+Use meaningful names
+The trade-off between short and descriptive names
+Common mistake on switch statements
