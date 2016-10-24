30372e8ca53720b4a36845b3f2d36e8ec31f28a8
BOSSoNe0013 <bossone0013@gmail.com>
2016-10-24 19:26:57 +0100

    2016-10-24's Java workout

---
+++ contributions/BOSSoNe0013/Java/Error Handling/2016-10-24.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Prevent `exceptions` while logging Data
