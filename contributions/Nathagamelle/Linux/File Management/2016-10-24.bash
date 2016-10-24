54db3734f4c49705e17bc663c8c5e395c021e92c
Nathagamelle <github@majoros.net>
2016-10-24 09:20:41 +0100

    2016-10-24's Linux workout

---
+++ contributions/Nathagamelle/Linux/File Management/2016-10-24.bash
@@ -0,0 +1,6 @@
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Working with `.gz` files
+Find the unknown process preventing deleting of files
