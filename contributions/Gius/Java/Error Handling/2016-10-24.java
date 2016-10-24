c6062fc0c49d360f741dc6ea0cdbf770d9a18546
Gius <giusdeb@gmail.com>
2016-10-24 22:32:12 +0100

    2016-10-24's Java workout

---
+++ contributions/Gius/Java/Error Handling/2016-10-24.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
