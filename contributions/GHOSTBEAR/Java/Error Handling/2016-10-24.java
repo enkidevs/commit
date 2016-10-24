aaee38f66277d3b1ab4a61b453832ded22839f23
GHOSTBEAR <andreas.everos@gmail.com>
2016-10-24 06:52:26 +0100

    2016-10-24's Java workout

---
+++ contributions/GHOSTBEAR/Java/Error Handling/2016-10-24.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Prevent `exceptions` while logging Data
+Throwing an `exception`
