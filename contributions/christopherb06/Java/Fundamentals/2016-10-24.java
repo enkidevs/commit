1af5efd462cd33c47646c5ce8e4858defc13795c
christopherb06 <christopherb06@gmail.com>
2016-10-24 15:16:52 +0100

    2016-10-24's Java workout

---
+++ contributions/christopherb06/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Writing generic methods
+Metadata: setting a file's owner
+Using an interface as a parameter
