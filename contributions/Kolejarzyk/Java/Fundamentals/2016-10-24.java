423a66da750fd2ace8123778fa35dd4e001b7861
Kolejarzyk <pawel.dee@live.com>
2016-10-24 19:24:07 +0100

    2016-10-24's Java workout

---
+++ contributions/Kolejarzyk/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
