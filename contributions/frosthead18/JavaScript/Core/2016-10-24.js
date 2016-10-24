1b96de22d9d3b1a2f1c316a9137e1d202f4650ab
frosthead18 <satana1881@gmail.com>
2016-10-24 19:06:09 +0100

    2016-10-24's JavaScript workout

---
+++ contributions/frosthead18/JavaScript/Core/2016-10-24.js
@@ -0,0 +1,5 @@
+Shuffle an array using `sort` and `random`.
+Use `splice()` to remove an item from an array
+Using `.test()` with RegExp
+How to merge two arrays
+Inaccuracy of binary floating-point format
