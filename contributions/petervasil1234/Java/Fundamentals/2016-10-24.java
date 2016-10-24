5131d5657bc04b38e13b6a4e5ccafd7e9765c7ff
petervasil1234 <petervasil1234@gmail.com>
2016-10-24 08:36:27 +0100

    2016-10-24's Java workout

---
+++ contributions/petervasil1234/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The trade-off between short and descriptive names
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
