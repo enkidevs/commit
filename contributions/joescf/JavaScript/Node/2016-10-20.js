25ba03ba08eeefaee9e8b7a8f77babdc1ffc548d
joescf <joescf@gmail.com>
2016-10-24 21:08:27 +0100

    2016-10-20's JavaScript workout

---
+++ contributions/joescf/JavaScript/Node/2016-10-20.js
@@ -0,0 +1,5 @@
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+Manage child processes in **Node**
