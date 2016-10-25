c0632e37b65db1feb6c95d941482811172a33b21
DistilledP <mogi@p25.co.uk>
2016-10-25 16:02:00 +0100

    2016-10-25's JavaScript workout

---
+++ contributions/DistilledP/JavaScript/Core/2016-10-25.js
@@ -0,0 +1,5 @@
+Hoisting applies only to variable declarations, not initializations
+The difference between null, undefined and NaN
+Prototype methods
+Round numbers to `N` decimals 
+Base conversion 
