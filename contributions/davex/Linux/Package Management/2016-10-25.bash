2c9801852f7425887c485dc69a715da639fbfac8
davex <dabuck@libero.it>
2016-10-25 17:12:49 +0100

    2016-10-25's Linux workout

---
+++ contributions/davex/Linux/Package Management/2016-10-25.bash
@@ -0,0 +1,5 @@
+Prevent updating a specific package in Debian systems
+Get `apt` to use a mirror / faster mirror
+Using `Tasksel` for software installation
+Using `PPAs`
+Keeping repos updated with `cron`
