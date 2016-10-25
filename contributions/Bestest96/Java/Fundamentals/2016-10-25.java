2fc3ca639e8e18267d6fd255a7c895ad3260092a
Bestest96 <jaluki@vp.pl>
2016-10-25 06:17:01 +0100

    2016-10-25's Java workout

---
+++ contributions/Bestest96/Java/Fundamentals/2016-10-25.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
