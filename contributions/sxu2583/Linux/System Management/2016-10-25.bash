0382790f34c8eb7443b8beeab277e71f01eb98ae
sxu2583 <shihabuddin100@gmail.com>
2016-10-25 14:39:04 +0100

    2016-10-25's Linux workout

---
+++ contributions/sxu2583/Linux/System Management/2016-10-25.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
