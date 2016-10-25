6067ee7819926624597998a10956464af8c21bf1
gitFrey <oliverbanham@gmail.com>
2016-10-25 11:14:41 +0100

    2016-10-25's JavaScript workout

---
+++ contributions/gitFrey/JavaScript/Functions/2016-10-25.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Anonymous functions
+Easily generate a random `HEX` color
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
