dfcf51f55e94972174f0c4925001ad0582fcf782
konst <kosnar@gmail.com>
2016-10-25 13:11:56 +0100

    2016-10-25's Linux workout

---
+++ contributions/konst/Linux/File Management/2016-10-25.bash
@@ -0,0 +1,6 @@
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Delete commands aliases
+Easy way of sharing files
