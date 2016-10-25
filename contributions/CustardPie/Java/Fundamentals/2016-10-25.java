d4ea9bae3f231ae08dfb7df157788cbab1f9be79
CustardPie <alexcsury@gmail.com>
2016-10-25 21:47:50 +0100

    2016-10-25's Java workout

---
+++ contributions/CustardPie/Java/Fundamentals/2016-10-25.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Difference between JRE and JDK?
+The trade-off between short and descriptive names
+Ternary operator
+Using Inheritance to reduce code repetition
