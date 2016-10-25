b219e591ef732958111e99544bd05cfd5e8721dc
mhaponiu <m.haponiuk@stud.elka.pw.edu.pl>
2016-10-25 18:10:52 +0100

    2016-10-25's Linux workout

---
+++ contributions/mhaponiu/Linux/File Management/2016-10-25.bash
@@ -0,0 +1,5 @@
+Converting between Unix and Windows text files
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
+Easy way of sharing files
