25232ec8e4ec97813aef2018eb7bde3d87f33846
kvsm <kvsm81@gmail.com>
2016-10-25 09:58:38 +0100

    2016-10-25's JavaScript workout

---
+++ contributions/kvsm/JavaScript/Functions/2016-10-25.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+IIFE pattern
+Get query/url variables
+Changing a functions context with `fn.call(object)`
