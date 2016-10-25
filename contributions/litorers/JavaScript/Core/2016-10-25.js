d48ac9e45cc9a54523d5fb0475f52fd311331bac
litorers <alexandrejoseph.tessier@gmail.com>
2016-10-25 13:54:05 +0100

    2016-10-25's JavaScript workout

---
+++ contributions/litorers/JavaScript/Core/2016-10-25.js
@@ -0,0 +1,5 @@
+Quick way to format integers with a fixed number of digits.
+Arrays can be used as stack
+Performing a function at timed intervals
+The difference between null, undefined and NaN
+Inaccuracy of binary floating-point format
