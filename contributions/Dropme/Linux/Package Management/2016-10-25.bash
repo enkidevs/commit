57e59a5aba5e11a1d034c3ef61ecfc3aaa4e758e
Dropme <ilradtss@yomail.info>
2016-10-25 17:57:13 +0100

    2016-10-25's Linux workout

---
+++ contributions/Dropme/Linux/Package Management/2016-10-25.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+List installed packages
+Using `Tasksel` for software installation
+Solving `BADSIG GPG` errors
