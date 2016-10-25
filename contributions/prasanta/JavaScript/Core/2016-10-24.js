534859a2cf73f5662bc017a33137c599ce13c75f
prasanta <prasantakakati1994@gmail.com>
2016-10-25 08:04:50 +0100

    2016-10-24's JavaScript workout

---
+++ contributions/prasanta/JavaScript/Core/2016-10-24.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Performing a function at timed intervals
+No block scope
+Hoisting applies only to variable declarations, not initializations
+NaN === NaN?
