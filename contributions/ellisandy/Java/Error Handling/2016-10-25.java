2eae2b42fdda8adbd29a8989d608b3c47ea215d2
ellisandy <andrew@andrewryanellis.com>
2016-10-25 15:44:20 +0100

    2016-10-25's Java workout

---
+++ contributions/ellisandy/Java/Error Handling/2016-10-25.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Checking a double for `NaN` value
