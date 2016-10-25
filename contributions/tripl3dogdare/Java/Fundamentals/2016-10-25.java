099a3010c5e4b3dd01ea1be2b0d3f11fe6ed9e39
tripl3dogdare <connor@connorjulian.org>
2016-10-25 14:44:47 +0100

    2016-10-25's Java workout

---
+++ contributions/tripl3dogdare/Java/Fundamentals/2016-10-25.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Writing generic methods
+Use `DecimalFormat` class to format numbers
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
