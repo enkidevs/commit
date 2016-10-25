4b28c8dbf9704999f984c3a984e5e2e58e34b4e1
BooM <tararuj.michal@gmail.com>
2016-10-25 06:33:54 +0100

    2016-10-25's Linux workout

---
+++ contributions/BooM/Linux/File Management/2016-10-25.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
