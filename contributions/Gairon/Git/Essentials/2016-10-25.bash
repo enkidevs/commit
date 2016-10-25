63c23e43aa23097268955c9263cccbc58105c0ed
Gairon <anton.galanov@gmail.com>
2016-10-25 06:16:51 +0100

    2016-10-25's Git workout

---
+++ contributions/Gairon/Git/Essentials/2016-10-25.bash
@@ -0,0 +1,5 @@
+Using aliases for git commands
+The three states in git
+How to set your username and email
+Use `short` status to make output more compact
+Remote repositories: fetching and pushing
