0abd86c9e6798c6f4f890da3fd66a916b34b36b7
Shalini <shaliniguha2@gmail.com>
2016-10-25 11:27:11 +0100

    2016-10-24's Java workout

---
+++ contributions/Shalini/Java/Fundamentals/2016-10-24.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Generating random numbers
+Java naming conventions
+Common mistake on switch statements
+Using Inheritance to reduce code repetition
