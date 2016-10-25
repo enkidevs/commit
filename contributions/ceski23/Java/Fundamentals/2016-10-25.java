b2a532d742d1d2dd8afef8e968a3757f34eabf96
ceski23 <ceski23@gmail.com>
2016-10-25 21:13:44 +0100

    2016-10-25's Java workout

---
+++ contributions/ceski23/Java/Fundamentals/2016-10-25.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Use meaningful names
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
+Metadata: setting a file's owner
