b488ecc0974a48f069e8ed782ff02c50b915d391
bzpuerta <beatriz.puerta.hoyas@gmail.com>
2016-10-25 23:09:27 +0100

    2016-10-25's Java workout

---
+++ contributions/bzpuerta/Java/Fundamentals/2016-10-25.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Metadata: creating a user-defined file attribute
+Calling one constructor from another
+Always specify type parameter
+How range operations work
