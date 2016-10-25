e498b5d945f5fe074e7647df4b3221db3d3d1158
timgrossmann <t.grossmann1994@web.de>
2016-10-25 06:37:17 +0100

    2016-10-25's Linux workout

---
+++ contributions/timgrossmann/Linux/Permissions In Linux/2016-10-25.bash
@@ -0,0 +1,5 @@
+Users And Groups On Linux
+Changing File Ownership With `chown`
+Changing File Permissions With `chmod`
+Changing Group Membership With `usermod`
+Changing User Passwords With `passwd`
