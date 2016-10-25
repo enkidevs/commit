55956f47a64593b28ca88c1658d842cb3b35a33c
supersabillon <joel@sabillon.net>
2016-10-25 07:30:17 +0100

    2016-10-24's JavaScript workout

---
+++ contributions/supersabillon/JavaScript/Npm/2016-10-24.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Manage local node modules with `npm link`
+`optionalDependencies` in npm
+Package distribution tags
+Lock down dependency versions by shrinkwrapping
