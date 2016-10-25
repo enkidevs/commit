b49f2d06cc324b0219f7d8d22e4854e8e80b1613
Kandit <loomlisp@gmail.com>
2016-10-25 06:52:45 +0100

    2016-10-25's Linux workout

---
+++ contributions/Kandit/Linux/System Management/2016-10-25.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Common log files
+Incorrect time on dual boot systems
+Finding Open Files With `lsof`
