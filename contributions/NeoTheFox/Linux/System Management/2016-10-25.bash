81d11d8d361a77d99de7d82eb450cf091c1bb87e
NeoTheFox <soniczerops@gmail.com>
2016-10-25 06:43:21 +0100

    2016-10-25's Linux workout

---
+++ contributions/NeoTheFox/Linux/System Management/2016-10-25.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Common log files
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
