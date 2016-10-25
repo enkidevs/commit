c41dfa586425b441c68c3ac1cb5491d315a8a363
jlgarciaap <jlgarciaap@gmail.com>
2016-10-25 13:10:11 +0100

    2016-10-25's Linux workout

---
+++ contributions/jlgarciaap/Linux/System Management/2016-10-25.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Using `dd` To Create Disk Images
