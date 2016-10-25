d6525eb9317586e2d24c9b12d30fdc3538de6f17
linted <linted90@hotmail.com>
2016-10-25 05:03:37 +0100

    2016-10-24's Linux workout

---
+++ contributions/linted/Linux/File Management/2016-10-24.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
+Delete commands aliases
