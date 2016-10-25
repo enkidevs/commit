79d203831b026d61c37df55fe1ca59efc2000334
miechu <miechu697@gmail.com>
2016-10-25 14:56:36 +0100

    2016-10-25's Linux workout

---
+++ contributions/miechu/Linux/System Management/2016-10-25.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Common log files
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
