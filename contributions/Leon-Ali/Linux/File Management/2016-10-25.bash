84982f890992467350546b5229b1a334386455fb
Leon-Ali <programmerbox1993@mail.ru>
2016-10-25 05:01:14 +0100

    2016-10-25's Linux workout

---
+++ contributions/Leon-Ali/Linux/File Management/2016-10-25.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
+Easy way of sharing files
