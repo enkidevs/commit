8da03e90ece89ee960ca88a4cc10e210ddee7fc1
shakedl <shaked.lokits@gmail.com>
2016-10-25 08:09:07 +0100

    2016-10-25's Java workout

---
+++ contributions/shakedl/Java/Fundamentals/2016-10-25.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+Calling one constructor from another
+`Autoboxing` and `Unboxing`
