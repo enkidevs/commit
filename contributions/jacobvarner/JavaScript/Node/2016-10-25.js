91f0d4112e5eb854369425c27bb4d4b418560a6e
jacobvarner <jacob.varner14@gmail.com>
2016-10-25 13:50:51 +0100

    2016-10-25's JavaScript workout

---
+++ contributions/jacobvarner/JavaScript/Node/2016-10-25.js
@@ -0,0 +1,5 @@
+Node Inspector
+Use conventional property names for error handling in Node.js
+**Node** `os` module
+Wrapping errors in Node.js using _node-verror_
+Automatic compilation for Node with **Nodemon** 
