dec49d10ba80aabf9252f207394cf27962721812
kenalger <kenalger@comcast.net>
2016-10-25 14:52:25 +0100

    2016-10-25's Java workout

---
+++ contributions/kenalger/Java/Fundamentals/2016-10-25.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Avoid using `static` variables
+Supply `toString()` in all classes
