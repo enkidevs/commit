22ed75d2c4ebcc91764a1ec95dcf9ef19fbfc8f2
Acksl <axel.dahlberg12@gmail.com>
2016-10-25 16:20:30 +0100

    2016-10-25's Linux workout

---
+++ contributions/Acksl/Linux/Networking/2016-10-25.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+Useful ssh client optimizations 
+Setting up password-free authentication
+Use `tcpdump` to listen to network interface traffic
+Make use of `netstat`
