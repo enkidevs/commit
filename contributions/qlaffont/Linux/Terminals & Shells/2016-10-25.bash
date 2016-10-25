0d30d9208d08f4ce2c081254aff51a2812386fb3
qlaffont <qlaffont13@gmail.com>
2016-10-25 09:20:44 +0100

    2016-10-25's Linux workout

---
+++ contributions/qlaffont/Linux/Terminals & Shells/2016-10-25.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Find any Unix / Linux command
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
