403c38aed688151926e2977d3f2916aa97a0a153
XuanYuan <ookk8282@gmail.com>
2016-10-25 03:20:07 +0100

    2016-10-24's Linux workout

---
+++ contributions/XuanYuan/Linux/Permissions In Linux/2016-10-24.bash
@@ -0,0 +1,5 @@
+Changing File Ownership With `chown`
+Changing File Permissions With `chmod`
+Adding New Users With `useradd`
+Changing Group Membership With `usermod`
+Changing User Passwords With `passwd`
