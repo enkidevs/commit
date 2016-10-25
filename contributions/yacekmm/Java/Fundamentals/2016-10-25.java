176ed56de14dcb5b43fe05acbd166a6a2fc01fd0
yacekmm <jacek.milewski.k@gmail.com>
2016-10-25 06:44:43 +0100

    2016-10-25's Java workout

---
+++ contributions/yacekmm/Java/Fundamentals/2016-10-25.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
+Using an interface as a parameter
