0debc622919922339d13e21f96ec2eaf89954fc5
vpinna80 <valentino.pinna@bancaditalia.it>
2016-10-25 19:13:12 +0100

    2016-10-25's Linux workout

---
+++ contributions/vpinna80/Linux/System Management/2016-10-25.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
