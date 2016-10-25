ff7f6b23bbac973c238f3fdb04c897493d7322b6
chefsim <chefsim@hotmail.com>
2016-10-25 21:33:54 +0100

    2016-10-25's Linux workout

---
+++ contributions/chefsim/Linux/Networking/2016-10-25.bash
@@ -0,0 +1,5 @@
+Disable `IPv6`
+Get real network statistics with `slurm`
+Sending emails from terminal / scripts
+Bandwidth monitoring tools 
+Staying connected remotely via SSH without an interactive shell
