ac63eb1091ebc7c74186aa2e6cbe857202a0530c
Mit0sz <artur.krawczyk93@gmail.com>
2016-10-25 07:08:00 +0100

    2016-10-25's Java workout

---
+++ contributions/Mit0sz/Java/Data Types/2016-10-25.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Converting numbers to strings
+Finding a substring in a string
