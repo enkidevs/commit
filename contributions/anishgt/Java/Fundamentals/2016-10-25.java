a1a9bf6534f88d54c71da4e4962d5da0df549c0e
anishgt <anishgt.1989@gmail.com>
2016-10-25 21:47:15 +0100

    2016-10-25's Java workout

---
+++ contributions/anishgt/Java/Fundamentals/2016-10-25.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Using an interface as a parameter
