1e0fcf20370c3cdfaf6e13ceed49b56dfcff0014
PizzaFreak <nico.greve89@gmail.com>
2016-10-25 18:36:19 +0100

    2016-10-25's Linux workout

---
+++ contributions/PizzaFreak/Linux/File Management/2016-10-25.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Using `touch` to alter files modification time
+Deleting non-empty directories
