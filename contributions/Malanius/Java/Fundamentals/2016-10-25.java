fd19c739af7c114a87d00c6f87f3d6609e3d5724
Malanius <malanius@seznam.cz>
2016-10-25 11:33:16 +0100

    2016-10-25's Java workout

---
+++ contributions/Malanius/Java/Fundamentals/2016-10-25.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use meaningful names
+Writing generic methods
+Using `Printstream.format` to format a string before printing
+How range operations work
