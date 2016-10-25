8258eb576a4d4c603648201eafbc8e19852ff92e
theghostofc <theghostofc@gmail.com>
2016-10-25 18:37:53 +0100

    2016-10-24's JavaScript workout

---
+++ contributions/theghostofc/JavaScript/React/2016-10-24.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+Pixel values in style props for **React**
+Props in getInitialState Is an        
+Anti-Pattern
+Use **React** with other libraries
+Dangerously set `innerHTML`
