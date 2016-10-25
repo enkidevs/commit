a9f9bf413ac71050437302ce4f280d06a7e62162
kbc8894 <kbc8894@gmail.com>
2016-10-25 22:55:54 +0100

    2016-10-25's Linux workout

---
+++ contributions/kbc8894/Linux/System Management/2016-10-25.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
