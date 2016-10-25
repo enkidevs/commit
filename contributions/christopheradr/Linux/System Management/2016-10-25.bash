fac8942d87e7d7779ac93ffac26325be5bacd4f5
christopheradr <christopher.diaz.riv@gmail.com>
2016-10-26 00:27:54 +0100

    2016-10-25's Linux workout

---
+++ contributions/christopheradr/Linux/System Management/2016-10-25.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Change your `hostname` on systems using `systemd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
