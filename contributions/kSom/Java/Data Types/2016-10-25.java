1c237a8455aaf006af3a7e514ec8c2f9d307427f
kSom <kgsommer@gmail.com>
2016-10-25 18:08:18 +0100

    2016-10-25's Java workout

---
+++ contributions/kSom/Java/Data Types/2016-10-25.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Collections vs arrays
+Multidimensional array declaration
+Equals operation on different data types
+Retrieve the component type of an array
