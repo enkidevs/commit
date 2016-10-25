cd57c13cb1c812e54e376b6fcdb40886f539bdaa
saka <saka71996@gmail.com>
2016-10-25 10:53:36 +0100

    2016-10-25's JavaScript workout

---
+++ contributions/saka/JavaScript/React/2016-10-25.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Shallow compare in **React**
+Validate for required props
+Custom `propType`'s to be required
