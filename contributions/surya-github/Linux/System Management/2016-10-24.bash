675e615d1e26165153cb7b0b596d00402de89127
surya-github <suryaprabhakark@gmail.com>
2016-10-25 18:09:06 +0100

    2016-10-24's Linux workout

---
+++ contributions/surya-github/Linux/System Management/2016-10-24.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Detect your linux distribution
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
