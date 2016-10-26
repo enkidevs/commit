ebfaafa115cbb72f6e39fb67bee030a92a260294
Carnagz77 <florent.gauchot@gmail.com>
2016-10-26 18:52:46 +0100

    2016-10-26's Java workout

---
+++ contributions/Carnagz77/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
