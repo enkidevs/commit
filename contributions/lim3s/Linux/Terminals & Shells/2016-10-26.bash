8564826c12760e2a9cbf8cd64e44e06dbd353884
lim3s <benhill.limes@gmail.com>
2016-10-27 00:54:38 +0100

    2016-10-26's Linux workout

---
+++ contributions/lim3s/Linux/Terminals & Shells/2016-10-26.bash
@@ -0,0 +1,7 @@
+Clear bash history
+Find any Unix / Linux command
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
