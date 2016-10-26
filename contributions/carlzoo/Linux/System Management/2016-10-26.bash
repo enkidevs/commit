ea100cc5caeecc1532ecfe314d87a69631001cd3
carlzoo <carl.j.zhou@protonmail.ch>
2016-10-26 19:42:56 +0100

    2016-10-26's Linux workout

---
+++ contributions/carlzoo/Linux/System Management/2016-10-26.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Common log files
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
