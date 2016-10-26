1c42b3348cb693c31687a8d2748f4e3ce1ff5e6d
vpinna80 <valentino.pinna@bancaditalia.it>
2016-10-26 16:52:17 +0100

    2016-10-26's Linux workout

---
+++ contributions/vpinna80/Linux/File Management/2016-10-26.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
+Working with `.gz` files
