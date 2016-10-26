45644f5319f1922b363fa082e766ccbab63d1645
shiv379 <shiv379@gmail.com>
2016-10-26 13:42:41 +0100

    2016-10-26's JavaScript workout

---
+++ contributions/shiv379/JavaScript/Functions/2016-10-26.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function declarations vs function expressions
+Functions can be declared after use
+Get query/url variables
+Check if a document is done loading
