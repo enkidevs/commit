ffe4d43b0f33eba7a81fec03a7e0866cb8531ea0
knapeczadam <knapecz.adam@gmail.com>
2016-10-26 19:20:29 +0100

    2016-10-26's Java workout

---
+++ contributions/knapeczadam/Java/Classes & Objects/2016-10-26.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Double Brace Initialization
+Diamond Operator
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
