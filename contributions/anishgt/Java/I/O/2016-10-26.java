e5ff7134879961e1dd81ebd37ce8e0de441a71db
anishgt <anishgt.1989@gmail.com>
2016-10-26 19:06:51 +0100

    2016-10-26's Java workout

---
+++ contributions/anishgt/Java/I/O/2016-10-26.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Create system file paths using the `Path` class
+Reading file attributes
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
