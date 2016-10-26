4b590aa1b8753d3c22a2be2f870852109c350c20
allfanmu <allfanmu@wp.pl>
2016-10-26 05:59:51 +0100

    2016-10-26's Java workout

---
+++ contributions/allfanmu/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+Constant notation
+Avoid using `static` variables
