d5798feb03f1dbd018c1ad632a9fb793f9381ca7
BooM <tararuj.michal@gmail.com>
2016-10-26 06:34:17 +0100

    2016-10-26's Java workout

---
+++ contributions/BooM/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+The distinction between overloading and overriding methods
+Generating random numbers
+Using `Printstream.format` to format a string before printing
+Using `synchronized` statements
