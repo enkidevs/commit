e6510d6ef11c117d540d5200bd2424cc0d12326a
chirag-jhamb <jhamb.chirag@yahoo.in>
2016-10-26 14:13:27 +0100

    2016-10-25's Linux workout

---
+++ contributions/chirag-jhamb/Linux/System Management/2016-10-25.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
+Finding Open Files With `lsof`
