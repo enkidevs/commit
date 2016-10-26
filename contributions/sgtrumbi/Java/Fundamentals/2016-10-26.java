3d002d99d0ece5e4380cc96365de4faeb7c48129
sgtrumbi <jojos.mobil@gmail.com>
2016-10-26 13:30:59 +0100

    2016-10-26's Java workout

---
+++ contributions/sgtrumbi/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
