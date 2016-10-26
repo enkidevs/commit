07d769f827fef7f0a4f1d9d4443825c5ff1a80c8
exo_duz <exo_duz@yahoo.com>
2016-10-26 17:06:13 +0100

    2016-10-26's JavaScript workout

---
+++ contributions/exo_duz/JavaScript/Functions/2016-10-26.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Hoisting
+Function constructor vs. function declaration vs. function expression
+Anonymous functions
+Changing a functions context with `fn.call(object)`
