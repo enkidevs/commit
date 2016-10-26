35e1a2845a54e07268a6718c70a4343392280675
danielsauceda <danielsauceda34@gmail.com>
2016-10-26 19:33:04 +0100

    2016-10-26's Git workout

---
+++ contributions/danielsauceda/Git/Tools/2016-10-26.bash
@@ -0,0 +1,5 @@
+Let's play the blame game
+Diff images using *ExifTool*
+Specify a range of commits using double dot syntax
+Show history of a function
+Remember the results of previous hunk conflicts
