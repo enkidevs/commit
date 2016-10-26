ca30176e1bc4bdcc01d1414e505349866fd9cd4e
madScience <flo.ces.fc@gmail.com>
2016-10-27 00:35:14 +0100

    2016-10-26's Java workout

---
+++ contributions/madScience/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
