32806fada556d93d53b54aba97b7a9045b1254cf
yaking <yanik@soeltzer.de>
2016-10-26 18:27:15 +0100

    2016-10-26's Java workout

---
+++ contributions/yaking/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
