8f12addb0e3a5c30fe2d1be0f95c5b27997c3ab8
pwinty <peterwinteriii@gmail.com>
2016-10-26 15:26:23 +0100

    2016-10-26's Linux workout

---
+++ contributions/pwinty/Linux/Networking/2016-10-26.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Quick port test with `netcat`
+Stress testing with `ab`
+Getting the connection speed from the terminal
+Staying connected remotely via SSH without an interactive shell
