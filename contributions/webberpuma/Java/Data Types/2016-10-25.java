82b4c4493eec7634b65d6edf5a97a79595116d1a
webberpuma <webberpuma@gmail.com>
2016-10-26 03:20:21 +0100

    2016-10-25's Java workout

---
+++ contributions/webberpuma/Java/Data Types/2016-10-25.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Finding a substring in a string
+Equals operation on different data types
