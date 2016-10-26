2f0ac5c9329d63c9be4e2356d8dcd4089d1e08f0
SteelCake <imondaypitt@gmail.com>
2016-10-26 11:02:39 +0100

    2016-10-26's Java workout

---
+++ contributions/SteelCake/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use meaningful names
+The trade-off between short and descriptive names
+Common mistake on switch statements
+Constant notation
