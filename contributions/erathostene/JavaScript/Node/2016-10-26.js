0a438dbd01cc6495e618d10a177830203503a497
erathostene <s_antoine51@hotmail.com>
2016-10-26 17:14:31 +0100

    2016-10-26's JavaScript workout

---
+++ contributions/erathostene/JavaScript/Node/2016-10-26.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Custom Node REPL Server
+Asynchronous File Write/Read in Node.js
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
