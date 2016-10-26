31bfbff832274f75529edda30dfc70d666859279
Drnkn <prehistoric2003@gmail.com>
2016-10-26 10:02:51 +0100

    2016-10-26's Java workout

---
+++ contributions/Drnkn/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Avoid using `static` variables
