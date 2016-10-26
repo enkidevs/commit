90b601f5e1da1da0e9c15c40105f9e04b6fd2065
spyros <spyros@enki.com>
2016-10-26 11:12:33 +0100

    2016-10-26's JavaScript workout

---
+++ contributions/spyros/JavaScript/Npm/2016-10-26.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Registry user accounts for npm
+npm can handle multiple versions of a dependency
+List binaries for scripting in npm
+`npm` verifies `sha1` hashes of packages
