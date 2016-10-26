6d00f70551b27c2c21df3b6b5bbd7b123a4bc1c7
majraL <dejan.lipovac@gmail.com>
2016-10-26 20:11:36 +0100

    2016-10-26's JavaScript workout

---
+++ contributions/majraL/JavaScript/Core/2016-10-26.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Most efficient way to build `HTML` strings
+Extra function arguments are undefined by default
+Prevent background image flicker in **IE**
+Detect **DO NOT TRACK** status
