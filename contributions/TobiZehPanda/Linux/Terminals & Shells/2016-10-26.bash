8ef00be0b1c435bb9e38a7df46b9b5dcc1a81be6
TobiZehPanda <tobizehpanda@gmail.com>
2016-10-26 17:30:05 +0100

    2016-10-26's Linux workout

---
+++ contributions/TobiZehPanda/Linux/Terminals & Shells/2016-10-26.bash
@@ -0,0 +1,7 @@
+Clear the terminal instantly
+Find any Unix / Linux command
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
