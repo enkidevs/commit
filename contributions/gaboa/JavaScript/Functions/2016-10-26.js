4cdafd756928ff591b49ec60fbc5ecfba9bce8ea
gaboa <dmitry.zhabko@gmail.com>
2016-10-26 10:32:34 +0100

    2016-10-26's JavaScript workout

---
+++ contributions/gaboa/JavaScript/Functions/2016-10-26.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Move cursor at the end of text input
+Social sharing without widgets
