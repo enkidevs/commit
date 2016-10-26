93729565840b708f4f702f9ec28e590ab8376208
gliuk <pupulin.l1989@gmail.com>
2016-10-26 07:26:32 +0100

    2016-10-26's Java workout

---
+++ contributions/gliuk/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
