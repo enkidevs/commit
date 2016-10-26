7bccd13dfd09f6a5e6a6fbb4b1bb9ce9aec7123a
subway <subway@centrum.cz>
2016-10-26 05:48:31 +0100

    2016-10-26's Java workout

---
+++ contributions/subway/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
