74fb6b9752ca2573e3c6b5f2143038266f50716e
chrisonntag <info@chrisonntag.com>
2016-10-26 23:40:57 +0100

    2016-10-26's JavaScript workout

---
+++ contributions/chrisonntag/JavaScript/React/2016-10-26.js
@@ -0,0 +1,7 @@
+Functional Stateless Components in React
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Immutability helpers in **React**
+Keyed fragments in **React**
