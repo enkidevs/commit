f5d150b511e742ddc12919a688405ad12af8816e
miecinka <mietinhovanish@gmail.com>
2016-10-26 22:40:49 +0100

    2016-10-26's Java workout

---
+++ contributions/miecinka/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Using Inheritance to reduce code repetition
+There is no such thing as *pass-by-reference* in Java
+`Autoboxing` and `Unboxing`
+Supply `toString()` in all classes
