348bce107c95524ea4210b1614b6fcbea766f8fa
FHoffmannCode <f.higuain@gmail.com>
2016-10-26 07:17:13 +0100

    2016-10-26's Java workout

---
+++ contributions/FHoffmannCode/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Using `synchronized` statements
