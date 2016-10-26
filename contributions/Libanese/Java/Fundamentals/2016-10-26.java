d44ed6ff1c36668077c7316f6f5c6bf707571c84
Libanese <attiliomandola@gmail.com>
2016-10-26 13:06:09 +0100

    2016-10-26's Java workout

---
+++ contributions/Libanese/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Using Inheritance to reduce code repetition
+How range operations work
