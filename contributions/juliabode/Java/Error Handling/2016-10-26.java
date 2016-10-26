2f55ca23ee32ae426229d47f0afbb4f837a82002
juliabode <julia.bode@lulisaur.us>
2016-10-26 08:14:26 +0100

    2016-10-26's Java workout

---
+++ contributions/juliabode/Java/Error Handling/2016-10-26.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Creating new Exceptions
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
