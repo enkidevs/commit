11a418c764134d3dfbfb6bfc283171265a14cb7c
psychorobotninja <tefuller@gmail.com>
2016-10-26 23:58:23 +0100

    2016-10-26's Java workout

---
+++ contributions/psychorobotninja/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
