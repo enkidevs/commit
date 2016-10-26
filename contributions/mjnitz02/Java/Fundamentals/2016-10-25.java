4df19dfd4bda6b3e8bb5773c508ca6feed898f35
mjnitz02 <mjnitz02@gmail.com>
2016-10-26 02:04:09 +0100

    2016-10-25's Java workout

---
+++ contributions/mjnitz02/Java/Fundamentals/2016-10-25.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Limit Accessibility of `Fields`
+Avoid using `static` variables
