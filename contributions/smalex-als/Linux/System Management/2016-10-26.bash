3f074fc1b02e2cd9e53719de2661732d15110d07
smalex-als <smalex69@gmail.com>
2016-10-26 13:21:25 +0100

    2016-10-26's Linux workout

---
+++ contributions/smalex-als/Linux/System Management/2016-10-26.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Finding Open Files With `lsof`
