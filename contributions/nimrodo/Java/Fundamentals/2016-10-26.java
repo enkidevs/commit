f179cae10a047a3900df88d3e0e4de7c19a2f7c2
nimrodo <nimrodo@gmail.com>
2016-10-26 20:03:26 +0100

    2016-10-26's Java workout

---
+++ contributions/nimrodo/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Java naming conventions
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+How range operations work
