d587f565ba33c3f610edfde7a9e503b3a57a9f33
ndonolli <ndonolli@gmail.com>
2016-10-26 05:16:12 +0100

    2016-10-25's Linux workout

---
+++ contributions/ndonolli/Linux/File Management/2016-10-25.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
