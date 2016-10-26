f9ae7ec4a25713a707d1fcca40248aef2ba62394
PotHix <pothix@pothix.com>
2016-10-26 09:31:26 +0100

    2016-10-26's Linux workout

---
+++ contributions/PotHix/Linux/File Management/2016-10-26.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
