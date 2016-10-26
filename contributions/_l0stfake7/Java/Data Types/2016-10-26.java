f5cb625657c08347ec70db4dcff0841b8d8eb507
_l0stfake7 <bartek.poniatyszyn@gmail.com>
2016-10-26 06:59:52 +0100

    2016-10-26's Java workout

---
+++ contributions/_l0stfake7/Java/Data Types/2016-10-26.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Finding a substring in a string
+Do not attempt comparisons with NaN
