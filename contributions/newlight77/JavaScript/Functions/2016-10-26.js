7cf72dccc0252dbd233ed8e61e1571f6e033f307
newlight77 <newlight77@gmail.com>
2016-10-26 16:27:18 +0100

    2016-10-26's JavaScript workout

---
+++ contributions/newlight77/JavaScript/Functions/2016-10-26.js
@@ -0,0 +1,5 @@
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+IIFE pattern
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
