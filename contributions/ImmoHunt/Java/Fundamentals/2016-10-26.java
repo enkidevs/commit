a98549bccb6ea2e43518d3c6a277d5debebec714
ImmoHunt <jarkendar333@gmail.com>
2016-10-26 22:49:40 +0100

    2016-10-26's Java workout

---
+++ contributions/ImmoHunt/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Constant notation
+Always specify type parameter
