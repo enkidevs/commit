647a551769fe4877e63322f26ed546290db1e02c
Ruud <ruud@arfie.nl>
2016-10-26 14:14:57 +0100

    2016-10-26's Linux workout

---
+++ contributions/Ruud/Linux/Jobs & Processes/2016-10-26.bash
@@ -0,0 +1,5 @@
+The `2>&1` redirection
+`Cron` dot-in-filename issues when using `run-parts`
+Common issue with scripts in `cron`
+Set a process priority
+How to pause a process and run others in the background
