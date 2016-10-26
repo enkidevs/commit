aee636aeb480237f29d4935d1c675c6355af2c8f
abishaij1 <abishaij1@gmail.com>
2016-10-26 04:26:23 +0100

    2016-10-25's Java workout

---
+++ contributions/abishaij1/Java/Error Handling/2016-10-25.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
