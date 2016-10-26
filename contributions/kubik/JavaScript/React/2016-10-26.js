8f85d8ea72137434f0d3bb4dc46d1be0b98c57ac
kubik <lkubic@poczta.onet.pl>
2016-10-26 07:40:32 +0100

    2016-10-26's JavaScript workout

---
+++ contributions/kubik/JavaScript/React/2016-10-26.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Pixel values in style props for **React**
+`if`-`else` statements in **JSX** and **React**
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
