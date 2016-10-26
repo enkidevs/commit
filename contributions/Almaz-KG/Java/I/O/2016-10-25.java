e096072ee8ed7149e54df2b68159d7af063e14dd
Almaz-KG <muktaalmaz@yandex.ru>
2016-10-26 13:43:03 +0100

    2016-10-25's Java workout

---
+++ contributions/Almaz-KG/Java/I/O/2016-10-25.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to list all files in a directory that match a filename extension in Java
+Using buffered streams
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
