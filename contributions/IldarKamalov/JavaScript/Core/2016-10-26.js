0db266efdd6a1ca667afe28a68b7772684f2abe8
IldarKamalov <msgto.ildar@gmail.com>
2016-10-26 05:18:48 +0100

    2016-10-26's JavaScript workout

---
+++ contributions/IldarKamalov/JavaScript/Core/2016-10-26.js
@@ -0,0 +1,5 @@
+Double and single quotes
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+The difference between null, undefined and NaN
+Prototype methods
