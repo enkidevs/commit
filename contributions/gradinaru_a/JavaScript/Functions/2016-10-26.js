8fa5739cb85ec7bd4dbb15957c92079acd110c1f
gradinaru_a <gradinaru.alex@gmail.com>
2016-10-26 09:36:39 +0100

    2016-10-26's JavaScript workout

---
+++ contributions/gradinaru_a/JavaScript/Functions/2016-10-26.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Functions can be declared after use
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
