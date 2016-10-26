1554e291ea5bb76fa8499f9d3332bee5feb24566
rsuda <rsuda9@gmail.com>
2016-10-26 01:19:04 +0100

    2016-10-24's Java workout

---
+++ contributions/rsuda/Java/Error Handling/2016-10-24.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Prevent `exceptions` while logging Data
