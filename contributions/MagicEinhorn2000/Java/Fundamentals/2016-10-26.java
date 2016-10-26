b0d8909e4f0a7535054c049a9b6dd17c97bbbf15
MagicEinhorn2000 <ks.wischmop@gmail.com>
2016-10-26 14:20:37 +0100

    2016-10-26's Java workout

---
+++ contributions/MagicEinhorn2000/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Writing generic methods
+Metadata: creating a user-defined file attribute
+Using an interface as a parameter
