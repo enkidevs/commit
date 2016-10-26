1be56338d9200d3d2cafad486536ab94d8d02254
ceski23 <ceski23@gmail.com>
2016-10-26 13:35:56 +0100

    2016-10-26's Java workout

---
+++ contributions/ceski23/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+Locks in `static synchronized` methods
+`Autoboxing` and `Unboxing`
