7b367239fe524dc0fe2e93b57420591b006538d6
bzpuerta <beatriz.puerta.hoyas@gmail.com>
2016-10-27 00:04:08 +0100

    2016-10-26's Java workout

---
+++ contributions/bzpuerta/Java/Error Handling/2016-10-26.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Throwing an `exception`
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
