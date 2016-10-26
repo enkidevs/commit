48e919735b19db149d7368a97e77a0b021b897bf
bengry <bengr6@gmail.com>
2016-10-26 14:26:19 +0100

    2016-10-26's JavaScript workout

---
+++ contributions/bengry/JavaScript/Functions/2016-10-26.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Easily generate a random `HEX` color
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
