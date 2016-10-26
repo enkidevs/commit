7cb3407dae735b80496fbad7f60d8a31a7c86b0f
BOSSoNe0013 <bossone0013@gmail.com>
2016-10-26 21:26:03 +0100

    2016-10-26's Java workout

---
+++ contributions/BOSSoNe0013/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use `DecimalFormat` class to format numbers
+Locks in `static synchronized` methods
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
