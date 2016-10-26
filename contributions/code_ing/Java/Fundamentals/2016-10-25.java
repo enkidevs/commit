e7d1eb258932c2f029e66ff834e641c435910f93
code_ing <keerthanasriranga11@gmail.com>
2016-10-26 14:56:42 +0100

    2016-10-25's Java workout

---
+++ contributions/code_ing/Java/Fundamentals/2016-10-25.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The trade-off between short and descriptive names
+Catch multiple exceptions in a single `catch` block
+Using `Printstream.format` to format a string before printing
+Calling one constructor from another
