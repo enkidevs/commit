1e72282a60a13df8dd10c1e42140c422699f4644
wieczorek1990 <wieczorek1990@gmail.com>
2016-10-26 19:31:52 +0100

    2016-10-26's Java workout

---
+++ contributions/wieczorek1990/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
