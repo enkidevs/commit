7abc333c70390dd8a48b95f3e2852dc7a56a66a2
nakitsuro <nakitsuro@icloud.com>
2016-10-26 15:30:17 +0100

    2016-10-26's Linux workout

---
+++ contributions/nakitsuro/Linux/System Management/2016-10-26.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Detect your linux distribution
+Find the `MAC` address of all network interfaces
+
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
