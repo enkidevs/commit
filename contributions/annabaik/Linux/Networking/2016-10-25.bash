67707eecbe82b6776a65cece86d396c1ade4b251
annabaik <annaheyonbaik@gmail.com>
2016-10-26 01:43:18 +0100

    2016-10-25's Linux workout

---
+++ contributions/annabaik/Linux/Networking/2016-10-25.bash
@@ -0,0 +1,5 @@
+Stress testing with `ab`
+Disable `IPv6`
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
