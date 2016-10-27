ec22d90c41303bd82b0a49ba5d0034febd1aa975
bzpuerta <beatriz.puerta.hoyas@gmail.com>
2016-10-27 23:39:46 +0100

    2016-10-27's Java workout

---
+++ contributions/bzpuerta/Java/Data Types/2016-10-27.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Using `enum` in Java
+Use `Arrays.asList()` to initialise lists
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
