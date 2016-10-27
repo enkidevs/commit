cc15828303e5c6a1131dd80cd654d52d42148c40
Witriol <tomas.simicek@outlook.com>
2016-10-27 20:55:31 +0100

    2016-10-27's Java workout

---
+++ contributions/Witriol/Java/Fundamentals/2016-10-27.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
