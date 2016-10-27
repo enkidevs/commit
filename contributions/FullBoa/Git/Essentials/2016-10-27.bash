e0df728bf73a2e5b7609cecdbf4a9df316e4ceb7
FullBoa <kafka.fb@gmail.com>
2016-10-27 07:45:21 +0100

    2016-10-27's Git workout

---
+++ contributions/FullBoa/Git/Essentials/2016-10-27.bash
@@ -0,0 +1,5 @@
+Pushing tags to a server
+The three states in git
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
