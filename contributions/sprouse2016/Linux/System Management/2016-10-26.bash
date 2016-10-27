51bdd112fc767870f3c6a9f748566fd6166019ff
sprouse2016 <sprouse2016@gmail.com>
2016-10-27 06:04:06 +0100

    2016-10-26's Linux workout

---
+++ contributions/sprouse2016/Linux/System Management/2016-10-26.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Get hardware stack details with `lspci`
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
