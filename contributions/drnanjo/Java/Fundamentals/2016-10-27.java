489fdcf48191a367a37ade9bac31cbe332b34a2a
drnanjo <drnanjo@aol.com>
2016-10-27 17:27:17 +0100

    2016-10-27's Java workout

---
+++ contributions/drnanjo/Java/Fundamentals/2016-10-27.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between ```HashMap``` and ```Hashtable```
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Constant notation
