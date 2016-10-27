73954f21881163e038c63102aa2515fd80adcb27
knapeczadam <knapecz.adam@gmail.com>
2016-10-27 20:26:20 +0100

    2016-10-27's Java workout

---
+++ contributions/knapeczadam/Java/Fundamentals/2016-10-27.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Difference between JRE and JDK?
+Generating random numbers
+Common mistake on switch statements
+Limit Accessibility of `Fields`
