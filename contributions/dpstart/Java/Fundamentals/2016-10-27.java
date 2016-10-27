48556607bec66cc2f5d46c5695198edc9415e643
dpstart <danielepaliotta96@gmail.com>
2016-10-27 10:01:45 +0100

    2016-10-27's Java workout

---
+++ contributions/dpstart/Java/Fundamentals/2016-10-27.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Using an interface as a parameter
