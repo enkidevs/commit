e3fb110612752cdc00b8c7e5e49cb3c2e63511a9
cmbpgh <cmbpgh@gmail.com>
2016-10-27 10:00:33 +0100

    2016-10-27's JavaScript workout

---
+++ contributions/cmbpgh/JavaScript/Node/2016-10-27.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Http Server in **Node**
+Use conventional property names for error handling in Node.js
+**Node** `os` module
+Automatic compilation for Node with **Nodemon** 
