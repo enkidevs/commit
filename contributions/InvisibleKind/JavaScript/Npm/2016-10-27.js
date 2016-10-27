335ceb7e78f0945db0fbce324eb3ffa4dc09648d
InvisibleKind <invisible.kinder@gmail.com>
2016-10-27 08:09:58 +0100

    2016-10-27's JavaScript workout

---
+++ contributions/InvisibleKind/JavaScript/Npm/2016-10-27.js
@@ -0,0 +1,5 @@
+Calling remote scripts with npm
+Passing through command line arguments in npm scripts
+Manage local node modules with `npm link`
+`config` object in `package.json`
+Package distribution tags
