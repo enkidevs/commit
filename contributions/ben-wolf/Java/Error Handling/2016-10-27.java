25044a76559a0c00feed1307654e08f5e9a5d0f9
ben-wolf <wolfb3@rpi.edu>
2016-10-27 15:38:11 +0100

    2016-10-27's Java workout

---
+++ contributions/ben-wolf/Java/Error Handling/2016-10-27.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Creating new Exceptions
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
