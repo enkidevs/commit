307baef53b04f313476599f5beebc28b1493821c
onlyctonly <onlyctonly@icloud.com>
2016-10-27 04:07:32 +0100

    2016-10-27's Linux workout

---
+++ contributions/onlyctonly/Linux/Terminals & Shells/2016-10-27.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Retrieve previous arguments
