591552d36094608d60529b9649493b9a2b0d743d
Dalalibera <giovannedalalibera@hotmail.com>
2016-10-27 08:54:27 +0100

    2016-10-27's Java workout

---
+++ contributions/Dalalibera/Java/Data Types/2016-10-27.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Converting a string to upper or lower case
+Equals operation on different data types
