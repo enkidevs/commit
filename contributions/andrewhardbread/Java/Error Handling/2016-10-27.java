447fc6abb7350cf1b680e986c612024cccb65dc1
andrewhardbread <b1gbwind@gmail.com>
2016-10-27 20:19:32 +0100

    2016-10-27's Java workout

---
+++ contributions/andrewhardbread/Java/Error Handling/2016-10-27.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Throwing an `exception`
