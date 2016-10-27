7c88ceb64def5034543943768485f366efaf530c
Nekrono51 <m.socha51@wp.pl>
2016-10-27 18:06:40 +0100

    2016-10-27's Java workout

---
+++ contributions/Nekrono51/Java/Fundamentals/2016-10-27.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
