93fcf5d6ecf21252775d41fd25687514cd947eb2
coolioasjulio <a.deshpande012@gmail.com>
2016-10-27 23:43:06 +0100

    2016-10-27's Java workout

---
+++ contributions/coolioasjulio/Java/Fundamentals/2016-10-27.java
@@ -0,0 +1,5 @@
+Writing generic methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
+Using an interface as a parameter
