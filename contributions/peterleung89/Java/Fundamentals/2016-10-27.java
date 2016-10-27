e2ee66e51090ee56ce8101c072c1d50ae9caff89
peterleung89 <peterleung89@gmail.com>
2016-10-27 07:30:38 +0100

    2016-10-27's Java workout

---
+++ contributions/peterleung89/Java/Fundamentals/2016-10-27.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
