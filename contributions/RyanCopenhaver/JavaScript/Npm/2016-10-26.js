72cef8306aafdf883104700202e4b7ccd0ab8341
RyanCopenhaver <ryancopenhaver1982@gmail.com>
2016-10-27 01:21:15 +0100

    2016-10-26's JavaScript workout

---
+++ contributions/RyanCopenhaver/JavaScript/Npm/2016-10-26.js
@@ -0,0 +1,5 @@
+Abbreviate commands in npm
+npm can handle multiple versions of a dependency
+List binaries for scripting in npm
+Manage local node modules with `npm link`
+`npm` verifies `sha1` hashes of packages
