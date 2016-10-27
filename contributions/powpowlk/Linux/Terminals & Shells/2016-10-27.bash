7e571617e9d9f48883aee5db41231d0b9a98092c
powpowlk <matiasmendoza1994@gmail.com>
2016-10-27 07:12:22 +0100

    2016-10-27's Linux workout

---
+++ contributions/powpowlk/Linux/Terminals & Shells/2016-10-27.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+`else` statements using the `||` operator 
+Retrieve previous arguments
