38ba8bd658b3207119ccab9aa247b82f40c68bb9
davex <dabuck@libero.it>
2016-10-27 18:34:54 +0100

    2016-10-27's Linux workout

---
+++ contributions/davex/Linux/Networking/2016-10-27.bash
@@ -0,0 +1,5 @@
+Multiple IP Address with aliasing 
+Useful ssh client optimizations 
+Sending emails from terminal / scripts
+Bandwidth monitoring tools 
+Staying connected remotely via SSH without an interactive shell
