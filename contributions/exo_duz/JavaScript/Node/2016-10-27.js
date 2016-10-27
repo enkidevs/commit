227eb7baa543c33106640c01955d718681b39a6f
exo_duz <exo_duz@yahoo.com>
2016-10-27 17:01:21 +0100

    2016-10-27's JavaScript workout

---
+++ contributions/exo_duz/JavaScript/Node/2016-10-27.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Asynchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+Node Inspector
+Wrapping errors in Node.js using _node-verror_
