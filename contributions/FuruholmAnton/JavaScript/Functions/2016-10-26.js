5d78c685bf4846249961cd39ec073ac1a0c5f6a8
FuruholmAnton <anton.furuholm@gmail.com>
2016-10-27 05:20:13 +0100

    2016-10-26's JavaScript workout

---
+++ contributions/FuruholmAnton/JavaScript/Functions/2016-10-26.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+`.bind()` 
+Get query/url variables
+Changing a functions context with `fn.call(object)`
