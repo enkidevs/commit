fce4078cdc6ff306f1972dc2fc7e7ffad6fde87e
slonikmak <slonikmak@gmail.com>
2016-10-27 18:09:29 +0100

    2016-10-27's Java workout

---
+++ contributions/slonikmak/Java/Fundamentals/2016-10-27.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
