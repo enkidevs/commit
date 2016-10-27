f075bf48b59daa9a7ed2b30b7e3403ae50e355d4
Asicoder <asicoderpower@gmail.com>
2016-10-27 12:01:13 +0100

    2016-10-27's Java workout

---
+++ contributions/Asicoder/Java/Data Types/2016-10-27.java
@@ -0,0 +1,5 @@
+Converting a string to upper or lower case
+Finding a substring in a string
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
+Do not attempt comparisons with NaN
