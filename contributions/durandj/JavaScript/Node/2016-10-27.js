589ca548d2baad51786af07f2fe3f72f74ddf73a
durandj <james.durand@alumni.msoe.edu>
2016-10-27 14:56:56 +0100

    2016-10-27's JavaScript workout

---
+++ contributions/durandj/JavaScript/Node/2016-10-27.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+Streams in **Node**
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
