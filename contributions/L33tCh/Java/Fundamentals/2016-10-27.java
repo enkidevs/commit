0e6bdd73ec037d60aa6f32a9e697b98fc62ee640
L33tCh <l33tch@gmail.com>
2016-10-27 22:06:49 +0100

    2016-10-27's Java workout

---
+++ contributions/L33tCh/Java/Fundamentals/2016-10-27.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Metadata: creating a user-defined file attribute
+Avoid using `static` variables
+Using an interface as a parameter
