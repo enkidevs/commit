f27cb2603c15ecf1d347ced47f042e4320ee7238
ana.valeije <ana.valeije@gmail.com>
2016-10-27 22:35:28 +0100

    2016-10-27's Linux workout

---
+++ contributions/ana.valeije/Linux/File Management/2016-10-27.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
