d45d9754bbed656d33fbeb3286d54e1ce1fafca2
Tatzer <gabriel.johnson@live.com>
2016-10-27 22:52:50 +0100

    2016-10-27's JavaScript workout

---
+++ contributions/Tatzer/JavaScript/Core/2016-10-27.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+Scope and strict mode
+Detect **DO NOT TRACK** status
