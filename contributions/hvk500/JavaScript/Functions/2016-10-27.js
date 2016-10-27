12e71ec240765709d2bd2462fd583acb026c020e
hvk500 <megaman100ster@gmail.com>
2016-10-27 22:49:02 +0100

    2016-10-27's JavaScript workout

---
+++ contributions/hvk500/JavaScript/Functions/2016-10-27.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Get query/url variables
+Easily generate a random `HEX` color
+Changing a functions context with `fn.call(object)`
