75909aec9c01157ce15ce11918077c250942f57b
mrdor <mrdor44@gmail.com>
2016-10-27 08:12:07 +0100

    2016-10-27's Linux workout

---
+++ contributions/mrdor/Linux/Package Management/2016-10-27.bash
@@ -0,0 +1,5 @@
+List installed packages
+Prevent updating a specific package in Debian systems
+Get `apt` to use a mirror / faster mirror
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
