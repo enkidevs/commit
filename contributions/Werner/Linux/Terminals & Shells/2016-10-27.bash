157a1a616b00c5221e334f06818dba0c105cbefc
Werner <werner.foss@gmail.com>
2016-10-27 18:16:10 +0100

    2016-10-27's Linux workout

---
+++ contributions/Werner/Linux/Terminals & Shells/2016-10-27.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Clear bash history
+Conditional command execution
+
+(`&&` operator)
+Retrieve previous arguments
