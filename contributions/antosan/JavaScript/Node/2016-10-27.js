55ab6ce9013cade731c551b2612c5361e17f6355
antosan <sandeantony@gmail.com>
2016-10-27 14:18:00 +0100

    2016-10-27's JavaScript workout

---
+++ contributions/antosan/JavaScript/Node/2016-10-27.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+The built-in Node debugger
+Streams in **Node**
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
