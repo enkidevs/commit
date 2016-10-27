eb5645b9d07ab1181643b55063f0553e28c289a9
SteelCake <imondaypitt@gmail.com>
2016-10-27 09:18:13 +0100

    2016-10-27's Java workout

---
+++ contributions/SteelCake/Java/Fundamentals/2016-10-27.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Difference between JRE and JDK?
+Java naming conventions
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
