d474719ac9827532fc4c5e3d2eb16817fe7d30be
tripl3dogdare <connor@connorjulian.org>
2016-10-27 13:28:02 +0100

    2016-10-27's Java workout

---
+++ contributions/tripl3dogdare/Java/Fundamentals/2016-10-27.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Avoid using `static` variables
