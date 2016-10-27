5fcf2a267a1723f71f64ab9e7aed438af4b711ab
reginaexmachina <we.love.rachel.day.lots@gmail.com>
2016-10-27 03:06:02 +0100

    2016-10-26's JavaScript workout

---
+++ contributions/reginaexmachina/JavaScript/React/2016-10-26.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+Type of the Children props
+Default values for props
