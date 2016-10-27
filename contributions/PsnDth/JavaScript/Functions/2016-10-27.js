82b6a5b37b2db5f7ec7fb9950323bc7ee563101c
PsnDth <psndth@gmail.com>
2016-10-27 23:51:07 +0100

    2016-10-27's JavaScript workout

---
+++ contributions/PsnDth/JavaScript/Functions/2016-10-27.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Anonymous functions
+Method chaining
+Changing a functions context with `fn.call(object)`
