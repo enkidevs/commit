f8a9de72e998cd10fbb2784c1058b642fe6ad4f8
AlKoFDC <alexander.kogan@freiheit.com>
2016-10-27 10:14:26 +0100

    2016-10-27's JavaScript workout

---
+++ contributions/AlKoFDC/JavaScript/Node/2016-10-27.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+`uncaughtException` listener in Node.js
+Clustering
+Automatic compilation for Node with **Nodemon** 
