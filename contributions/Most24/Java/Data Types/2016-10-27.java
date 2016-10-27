01f4a2c3384e985622550761fca4773e9d2817ec
Most24 <github@most24.de>
2016-10-27 16:05:53 +0100

    2016-10-27's Java workout

---
+++ contributions/Most24/Java/Data Types/2016-10-27.java
@@ -0,0 +1,5 @@
+Two ways to use an `Iterator`
+Use `Arrays.asList()` to initialise lists
+Finding a substring in a string
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
