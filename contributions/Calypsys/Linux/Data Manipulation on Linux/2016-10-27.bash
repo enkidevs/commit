b519c4c8a3014df4a95e55746e3b641af7f9764a
Calypsys <calypsys1@gmail.com>
2016-10-27 06:06:53 +0100

    2016-10-27's Linux workout

---
+++ contributions/Calypsys/Linux/Data Manipulation on Linux/2016-10-27.bash
@@ -0,0 +1,5 @@
+The `head` And `tail` Commands
+Paging Through Long Output Using `less`
+Transforming Text With `sed`
+Selecting Portions of A Line Using `cut`
+The AWK Programming Language And `awk` Command
