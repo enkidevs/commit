744b3296c82a7dec233b40791f01879981fb275a
agilepanda <pantelei@cs.uni-bonn.de>
2016-10-27 05:37:57 +0100

    2016-10-27's Linux workout

---
+++ contributions/agilepanda/Linux/Package Management/2016-10-27.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+Get `apt` to use a mirror / faster mirror
+Downgrade a package via `apt-get`
+Keeping repos updated with `cron`
