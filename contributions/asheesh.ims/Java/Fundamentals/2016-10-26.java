773b06d5544dae181b526f3daad9867013bbbace
asheesh.ims <asheesh.ims@gmail.com>
2016-10-27 08:18:20 +0100

    2016-10-26's Java workout

---
+++ contributions/asheesh.ims/Java/Fundamentals/2016-10-26.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
