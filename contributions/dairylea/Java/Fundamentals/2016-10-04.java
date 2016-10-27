2d623b90d9cadad6b27b2509f190459c9d712004
dairylea <dannylcranmer@gmail.com>
2016-10-27 17:59:16 +0100

    2016-10-04's Java workout

---
+++ contributions/dairylea/Java/Fundamentals/2016-10-04.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Connecting to FTP using Java
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
