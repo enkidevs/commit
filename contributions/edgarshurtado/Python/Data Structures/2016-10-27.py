46985477cfee252ac509aaf3bc1cd394823753ae
edgarshurtado <edsanhu@gmail.com>
2016-10-27 07:12:11 +0100

    2016-10-27's Python workout

---
+++ contributions/edgarshurtado/Python/Data Structures/2016-10-27.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`del` statement for  lists
+`Dictionary` - standard mapping type
+Your own Python `calendar` 
+`bytearray` objects
