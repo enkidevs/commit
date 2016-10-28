672d97d25fc24eb9fb39efdcfac8b23baea7b50d
bobahrus <bobahrus@gmail.com>
2016-10-28 09:13:26 +0100

    2016-10-28's Java workout

---
+++ contributions/bobahrus/Java/Fundamentals/2016-10-28.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between `==` and `equals()`
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
