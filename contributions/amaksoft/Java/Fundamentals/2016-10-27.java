ac0748f93599974a6f953fe8219cb8cb8c9b728b
amaksoft <amaksoft@gmail.com>
2016-10-28 20:47:43 +0100

    2016-10-27's Java workout

---
+++ contributions/amaksoft/Java/Fundamentals/2016-10-27.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
