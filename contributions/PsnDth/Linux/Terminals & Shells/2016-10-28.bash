701b48cc6d857617001c61469655f4484a04d9a7
PsnDth <psndth@gmail.com>
2016-10-28 11:41:17 +0100

    2016-10-28's Linux workout

---
+++ contributions/PsnDth/Linux/Terminals & Shells/2016-10-28.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
