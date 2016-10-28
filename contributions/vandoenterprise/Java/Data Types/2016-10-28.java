449345ad417bc0a7df377a2636546d24d4f33a4a
vandoenterprise <evandro.barbosa@yahoo.com.br>
2016-10-28 11:37:58 +0100

    2016-10-28's Java workout

---
+++ contributions/vandoenterprise/Java/Data Types/2016-10-28.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Multidimensional array declaration
+Equals operation on different data types
+Retrieve the component type of an array
