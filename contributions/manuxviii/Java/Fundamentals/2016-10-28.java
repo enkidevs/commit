67b5f96b8bc0139779652562af9af0cfc49d6333
manuxviii <emmanuel.mesnige@protonmail.com>
2016-10-29 00:44:51 +0100

    2016-10-28's Java workout

---
+++ contributions/manuxviii/Java/Fundamentals/2016-10-28.java
@@ -0,0 +1,5 @@
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Limit Accessibility of `Fields`
+Avoid using `static` variables
