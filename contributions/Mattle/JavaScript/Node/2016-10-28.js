8fd95ea4ce2e4be8d8e3c71a07f25a83461b4ba9
Mattle <mcallier@gmail.com>
2016-10-28 20:33:11 +0100

    2016-10-28's JavaScript workout

---
+++ contributions/Mattle/JavaScript/Node/2016-10-28.js
@@ -0,0 +1,5 @@
+The built-in Node debugger
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+**Node** `os` module
+Automatic compilation for Node with **Nodemon** 
