d7e66877272d77614ff649b3544b0f4ef24f9bab
jacobvarner <jacob.varner14@gmail.com>
2016-10-28 16:31:58 +0100

    2016-10-28's Java workout

---
+++ contributions/jacobvarner/Java/Fundamentals/2016-10-28.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Generating random numbers
+Writing generic methods
+Updating interfaces by using `default` methods
+Always specify type parameter
