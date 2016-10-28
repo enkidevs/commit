55fa238e46c87e5b9f259685ef4a4b166b06def9
NMayberry <eureka353@yahoo.com>
2016-10-28 16:35:48 +0100

    2016-10-28's Java workout

---
+++ contributions/NMayberry/Java/Fundamentals/2016-10-28.java
@@ -0,0 +1,5 @@
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
+Supply `toString()` in all classes
