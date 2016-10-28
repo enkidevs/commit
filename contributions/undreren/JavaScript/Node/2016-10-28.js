d82b99355672ad46ec1fe9eba161ec7e9fa9f654
undreren <kasperbrohus@gmail.com>
2016-10-28 08:01:59 +0100

    2016-10-28's JavaScript workout

---
+++ contributions/undreren/JavaScript/Node/2016-10-28.js
@@ -0,0 +1,5 @@
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+Factories design pattern
+Middleware/pipeline design pattern
