ca74602d7a0c2f529cc00c7f4645c669d3d1997c
Catvanoh <pavelpie@gmail.com>
2016-10-28 08:08:26 +0100

    2016-10-28's JavaScript workout

---
+++ contributions/Catvanoh/JavaScript/Core/2016-10-28.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Keywords in hash keys
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Context in JavaScript
