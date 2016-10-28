69a4d4d0774d5c08475f1ee64d93b1d6c7e2438d
Cieszlunek <jsamczuk@gmail.com>
2016-10-28 06:58:21 +0100

    2016-10-28's Java workout

---
+++ contributions/Cieszlunek/Java/Fundamentals/2016-10-28.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
