039adcba65aaff0ddf8c6baea28abb6d4569e5e6
Grorel <grorel.kazui@gmail.com>
2016-10-28 07:28:59 +0100

    2016-10-28's Java workout

---
+++ contributions/Grorel/Java/Fundamentals/2016-10-28.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Use `DecimalFormat` class to format numbers
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
