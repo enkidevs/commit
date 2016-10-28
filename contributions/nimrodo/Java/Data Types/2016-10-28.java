7c862b140b7db1c3161d7fc4ff6643bfbbd0baa9
nimrodo <nimrodo@gmail.com>
2016-10-28 15:47:19 +0100

    2016-10-28's Java workout

---
+++ contributions/nimrodo/Java/Data Types/2016-10-28.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Equals operation on different data types
+Numeric Conversion - Widening
