c8b6bcc340e71c7a77caaf9a612b52c37532a675
Jilano <mobile.axel@gmail.com>
2016-10-28 07:48:16 +0100

    2016-10-28's Linux workout

---
+++ contributions/Jilano/Linux/System Management/2016-10-28.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Commands to shutdown or restart the system
+Cancel a system shutdown
