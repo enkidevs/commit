0323358d47e042e1c3214c682527bf49972ea6cc
ghstridr <ghstridr@gmail.com>
2016-10-28 17:13:01 +0100

    2016-10-28's Linux workout

---
+++ contributions/ghstridr/Linux/System Management/2016-10-28.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
