60ab47a0ab4166ba99fe67c1f96c6b84f216c9b3
Witriol <tomas.simicek@outlook.com>
2016-10-28 16:50:37 +0100

    2016-10-28's Java workout

---
+++ contributions/Witriol/Java/Fundamentals/2016-10-28.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Catch multiple exceptions in a single `catch` block
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
