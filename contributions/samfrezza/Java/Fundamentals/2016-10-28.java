a5a4046935d00c1d2f7a85c503c3a56efeb048db
samfrezza <samfrezza@gmail.com>
2016-10-28 12:16:48 +0100

    2016-10-28's Java workout

---
+++ contributions/samfrezza/Java/Fundamentals/2016-10-28.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
