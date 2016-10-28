37d1205cd306c5eeef93384bfe79ac0f6239098f
batanete <bata619@gmail.com>
2016-10-28 03:50:49 +0100

    2016-10-28's Java workout

---
+++ contributions/batanete/Java/Fundamentals/2016-10-28.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
