4aebc946341a09d9cef54ee2f0ae6983ec4e2a84
Vikingair <luft.viktor.bms@gmail.com>
2016-10-28 06:15:11 +0100

    2016-10-28's Linux workout

---
+++ contributions/Vikingair/Linux/File Management/2016-10-28.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Using `touch` to alter files modification time
+Working with `.gz` files
+Find the unknown process preventing deleting of files
