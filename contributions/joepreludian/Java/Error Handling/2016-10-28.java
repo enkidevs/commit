4893a1b5bea3728e441d5e1a1efdd6d08e69d51d
joepreludian <joepreludian@gmail.com>
2016-10-28 18:35:21 +0100

    2016-10-28's Java workout

---
+++ contributions/joepreludian/Java/Error Handling/2016-10-28.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Creating new Exceptions
