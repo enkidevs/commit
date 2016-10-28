18e09f9ceefabb2a4cfc7ad615613c572444d997
gileno <gileno@gmail.com>
2016-10-28 13:35:52 +0100

    2016-10-28's JavaScript workout

---
+++ contributions/gileno/JavaScript/Npm/2016-10-28.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+npm can handle multiple versions of a dependency
+Environmental variables and flags for `npm`
+`npm`  as an alternative to Gulp
+Package distribution tags
