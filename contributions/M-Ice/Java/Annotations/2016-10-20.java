cecf312e9e4a84d469d0adb50a5d0ee5d35804cb
M-Ice <vahiev@gmail.com>
2016-10-28 06:47:26 +0100

    2016-10-20's Java workout

---
+++ contributions/M-Ice/Java/Annotations/2016-10-20.java
@@ -0,0 +1,5 @@
+`@Test` parameters
+Use `@Ignore` to disable JUnit tests
+Using the `@Deprecated` annotation
+Including annotations in Javadoc
+Restricting an annotation's application by using `@Target`
