376a8ff0e73da26fc979058be84f0fc75fe6c978
flown1 <tamtennormalny@gmail.com>
2016-10-28 10:53:22 +0100

    2016-10-28's JavaScript workout

---
+++ contributions/flown1/JavaScript/Node/2016-10-28.js
@@ -0,0 +1,5 @@
+REPL in Node.js
+Asynchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+First-error callbacks in **Node**
+Use conventional property names for error handling in Node.js
