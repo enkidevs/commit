35c24f6fab10ccc8a8c788817e1a01b544f5ec06
wilderbg <wilder.galvao@gmail.com>
2016-10-28 21:06:05 +0100

    2016-10-28's Java workout

---
+++ contributions/wilderbg/Java/Error Handling/2016-10-28.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Creating new Exceptions
+Throwing an `exception`
+Careful Numeric Data Conversions
