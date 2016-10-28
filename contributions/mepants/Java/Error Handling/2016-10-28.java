0552ca34ee474d9ab7a816f153c404054aa6da9f
mepants <enki.cg@cgtracker.co.uk>
2016-10-28 19:18:04 +0100

    2016-10-28's Java workout

---
+++ contributions/mepants/Java/Error Handling/2016-10-28.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Careful Numeric Data Conversions
