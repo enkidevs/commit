c59f15180a1d937650f1203855fe17b3f8d2d527
Ramius87 <steffen.wurzler@yahoo.com>
2016-10-28 11:41:40 +0100

    2016-10-28's JavaScript workout

---
+++ contributions/Ramius87/JavaScript/Npm/2016-10-28.js
@@ -0,0 +1,5 @@
+Create projects with `npm init`
+Passing through command line arguments in npm scripts
+Manage local node modules with `npm link`
+Tab completion in `npm`
+Lock down dependency versions by shrinkwrapping
