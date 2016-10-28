0c163735e863f9c9521f565d8ed2b4848a951610
cdmarucci <cdmarucci@gmail.com>
2016-10-28 15:51:31 +0100

    2016-10-28's JavaScript workout

---
+++ contributions/cdmarucci/JavaScript/Node/2016-10-28.js
@@ -0,0 +1,5 @@
+Streams in **Node**
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
