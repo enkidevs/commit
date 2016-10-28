7d89f5e0527ff0172d72bdab84dfbd1223037a8b
blackecho <angeletti.gabriele@gmail.com>
2016-10-28 09:16:45 +0100

    2016-10-28's Python workout

---
+++ contributions/blackecho/Python/Data Structures/2016-10-28.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Get more with `collections`!
+Operations with `bytes` and `bytearray`
+A thread-safe `Queue`
+`weakref` proxies
