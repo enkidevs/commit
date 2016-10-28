02c896056afde246a3fe639c41ea98ea37d94398
Tatzer <gabriel.johnson@live.com>
2016-10-28 15:38:52 +0100

    2016-10-28's JavaScript workout

---
+++ contributions/Tatzer/JavaScript/Node/2016-10-28.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+The built-in Node debugger
+**Node** `os` module
+`exports` vs. `module.exports` in **Node**
+`try-catch` only for **sync** code
