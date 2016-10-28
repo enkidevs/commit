01a0332c2b5162e59db8ceb2fe7d2129b380dbe6
surya-github <suryaprabhakark@gmail.com>
2016-10-28 20:29:23 +0100

    2016-10-27's Linux workout

---
+++ contributions/surya-github/Linux/System Management/2016-10-27.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
