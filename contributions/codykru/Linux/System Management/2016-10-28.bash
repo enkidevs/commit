9b7ef58a5ad5cdfc0ff53e58cec45c244b07f457
codykru <codykru@gmail.com>
2016-10-28 18:09:28 +0100

    2016-10-28's Linux workout

---
+++ contributions/codykru/Linux/System Management/2016-10-28.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
