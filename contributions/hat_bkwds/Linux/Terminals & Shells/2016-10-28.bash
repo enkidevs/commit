8e15a405b9378674abbca96d92952d7bf9352d3f
hat_bkwds <phillyeagles60@gmail.com>
2016-10-28 20:35:14 +0100

    2016-10-28's Linux workout

---
+++ contributions/hat_bkwds/Linux/Terminals & Shells/2016-10-28.bash
@@ -0,0 +1,5 @@
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
+The Basics of Environment Variables
