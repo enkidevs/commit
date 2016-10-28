34f6dcca9cbf45a2d467a8424ac869e93a81b70f
xd1936 <xd1936@gmail.com>
2016-10-28 21:27:47 +0100

    2016-10-28's JavaScript workout

---
+++ contributions/xd1936/JavaScript/Core/2016-10-28.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Hoisting applies only to variable declarations, not initializations
+Avoid using `with`
+Detect **DO NOT TRACK** status
+Inaccuracy of binary floating-point format
