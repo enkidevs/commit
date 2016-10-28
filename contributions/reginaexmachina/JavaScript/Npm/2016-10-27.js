0668bd039d19d03f42b7c49aa91c572157199d31
reginaexmachina <we.love.rachel.day.lots@gmail.com>
2016-10-28 03:10:22 +0100

    2016-10-27's JavaScript workout

---
+++ contributions/reginaexmachina/JavaScript/Npm/2016-10-27.js
@@ -0,0 +1,5 @@
+Deprecate npm packages
+npm can handle multiple versions of a dependency
+Passing through command line arguments in npm scripts
+Manage local node modules with `npm link`
+Scoped packages
