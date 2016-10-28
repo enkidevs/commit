ce967ed7ccca4178391b78a3ba81f615607e0bac
vbalko <vbalko@gmail.com>
2016-10-28 11:42:20 +0100

    2016-10-28's JavaScript workout

---
+++ contributions/vbalko/JavaScript/Node/2016-10-28.js
@@ -0,0 +1,5 @@
+Http Server in **Node**
+Use conventional property names for error handling in Node.js
+`process.nextTick()` callback pattern
+Clustering
+Automatic compilation for Node with **Nodemon** 
