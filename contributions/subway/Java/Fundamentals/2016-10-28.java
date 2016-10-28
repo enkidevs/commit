d6b287b0bf0e3ba891c74a03e40dbbbadd23be58
subway <subway@centrum.cz>
2016-10-28 09:17:11 +0100

    2016-10-28's Java workout

---
+++ contributions/subway/Java/Fundamentals/2016-10-28.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
