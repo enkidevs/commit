f39a35090ca25c41f9ef399fb23436b8b637523e
MichaelRijsdijk <michaelrijsdijk@gmail.com>
2016-10-28 08:02:37 +0100

    2016-10-28's Java workout

---
+++ contributions/MichaelRijsdijk/Java/Fundamentals/2016-10-28.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+Locks in `static synchronized` methods
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
