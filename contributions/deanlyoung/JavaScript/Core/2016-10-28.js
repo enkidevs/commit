dd02d1507ee8cae84dd3052764b6f8bd63bfb421
deanlyoung <dlyoung119@me.com>
2016-10-28 18:16:24 +0100

    2016-10-28's JavaScript workout

---
+++ contributions/deanlyoung/JavaScript/Core/2016-10-28.js
@@ -0,0 +1,5 @@
+Be careful when using `typeof`.
+Milliseconds since Epoch
+Arrays can be used as stack
+Setting default values with `short circuiting`
+The difference between null, undefined and NaN
