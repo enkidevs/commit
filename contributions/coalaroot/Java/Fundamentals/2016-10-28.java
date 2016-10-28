aa5cc686a3ddc082dfa896d8f2af1bb32d304ceb
coalaroot <jakubkrolak94@gmail.com>
2016-10-28 13:55:01 +0100

    2016-10-28's Java workout

---
+++ contributions/coalaroot/Java/Fundamentals/2016-10-28.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Difference between JRE and JDK?
+Writing generic methods
+Using `Printstream.format` to format a string before printing
+`Autoboxing` and `Unboxing`
