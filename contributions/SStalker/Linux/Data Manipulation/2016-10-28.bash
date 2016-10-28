662a24a15529ee92a790dadde5b05281b002dad7
SStalker <raphael@bluepanel.org>
2016-10-28 16:46:53 +0100

    2016-10-28's Linux workout

---
+++ contributions/SStalker/Linux/Data Manipulation/2016-10-28.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+Intro to `sed`
+Using `lynx` to convert HTML to text
+Count non-empty lines with `nl` utility
+Clean configuration files using `sed`
