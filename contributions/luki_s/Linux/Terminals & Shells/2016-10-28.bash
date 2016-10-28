27bcd07fc494942319b324f0d46335b188ae6609
luki_s <lukasz.sk74@gmail.com>
2016-10-28 12:42:28 +0100

    2016-10-28's Linux workout

---
+++ contributions/luki_s/Linux/Terminals & Shells/2016-10-28.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Execute a command without saving it in history
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
