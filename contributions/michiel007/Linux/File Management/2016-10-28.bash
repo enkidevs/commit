d2a7ba79d45f4493209029d9eb1209538cb6ce7e
michiel007 <michiel_verboven@hotmail.com>
2016-10-28 22:38:57 +0100

    2016-10-28's Linux workout

---
+++ contributions/michiel007/Linux/File Management/2016-10-28.bash
@@ -0,0 +1,5 @@
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
+Find the unknown process preventing deleting of files
+Delete commands aliases
+Easy way of sharing files
