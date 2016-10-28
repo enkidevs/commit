dd6db47fe867c91ec553f6caed146995c8da3356
dscdev <dscxd@outlook.com>
2016-10-28 13:48:26 +0100

    2016-10-28's JavaScript workout

---
+++ contributions/dscdev/JavaScript/Functions/2016-10-28.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Method chaining
+Get query/url variables
+Changing a functions context with `fn.call(object)`
