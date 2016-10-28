c69ab4bb8946ff032836a6aad530b330cf38ae6f
nikoo <grula93@gmail.com>
2016-10-28 09:28:22 +0100

    2016-10-28's Java workout

---
+++ contributions/nikoo/Java/I/O/2016-10-28.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Using buffered streams
+Retrieving file store attributes
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
