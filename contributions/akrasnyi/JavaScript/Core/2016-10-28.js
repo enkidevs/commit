3c89d4f6e50db6ed6065cfa7bebc7f152e03af25
akrasnyi <krasnyiartem@gmail.com>
2016-10-28 23:22:55 +0100

    2016-10-28's JavaScript workout

---
+++ contributions/akrasnyi/JavaScript/Core/2016-10-28.js
@@ -0,0 +1,5 @@
+Format currency in JavaScript
+Infix operators are left-associative
+Hoisting applies only to variable declarations, not initializations
+Scope and strict mode
+Detect **DO NOT TRACK** status
