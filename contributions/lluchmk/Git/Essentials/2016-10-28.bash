baa9eb1066e929787afeda78cf8a66f4e35f0b76
lluchmk <lluchmk@gmail.com>
2016-10-28 07:02:12 +0100

    2016-10-28's Git workout

---
+++ contributions/lluchmk/Git/Essentials/2016-10-28.bash
@@ -0,0 +1,5 @@
+The three states in git
+How to set your username and email
+Limiting log output by time
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
