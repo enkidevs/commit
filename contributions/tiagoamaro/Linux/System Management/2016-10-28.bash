a0acb1e2a92b07e41001bb6c0559f0e797aabfda
tiagoamaro <tiagopadrela@gmail.com>
2016-10-28 07:20:13 +0100

    2016-10-28's Linux workout

---
+++ contributions/tiagoamaro/Linux/System Management/2016-10-28.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Common log files
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
