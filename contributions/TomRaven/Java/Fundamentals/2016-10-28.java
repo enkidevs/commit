2c93cc05fa61b7b17ba7a81a713e04435043923b
TomRaven <16lazart@elmbrookstudents.org>
2016-10-28 15:46:17 +0100

    2016-10-28's Java workout

---
+++ contributions/TomRaven/Java/Fundamentals/2016-10-28.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
