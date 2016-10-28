2f65866583c5de32550cf750659e31c14c77dfda
groundhogdad <groundhogdad@gmail.com>
2016-10-28 05:48:21 +0100

    2016-10-28's Java workout

---
+++ contributions/groundhogdad/Java/Data Types/2016-10-28.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Converting a string to upper or lower case
+Equals operation on different data types
