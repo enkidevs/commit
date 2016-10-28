5264057323fc5bf8a1277227fe4ef9f916b9ad2b
snulvin <snorre.ulvin@gmail.com>
2016-10-28 14:20:17 +0100

    2016-10-28's Linux workout

---
+++ contributions/snulvin/Linux/System Management/2016-10-28.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
