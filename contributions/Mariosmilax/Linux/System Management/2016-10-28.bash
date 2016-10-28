dcca75298994eb0b217ef31b3029a8e89c37bc8a
Mariosmilax <elior.b@dynamicyield.com>
2016-10-28 12:27:35 +0100

    2016-10-28's Linux workout

---
+++ contributions/Mariosmilax/Linux/System Management/2016-10-28.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Finding Open Files With `lsof`
