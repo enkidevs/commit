e44aefc0b850c1cabe42f17e4f4743ec77e38d1d
dtychko <dtychko@gmail.com>
2016-10-29 08:56:46 +0100

    2016-10-27's Java workout

---
+++ contributions/dtychko/Java/Fundamentals/2016-10-27.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Detect or prevent integer overflow
