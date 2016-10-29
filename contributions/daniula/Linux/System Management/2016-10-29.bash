e8fbf87f9591b830cfbb096ba2dfefc4e59851e2
daniula <daniel.mendalka@gmail.com>
2016-10-29 02:48:55 +0100

    2016-10-29's Linux workout

---
+++ contributions/daniula/Linux/System Management/2016-10-29.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
