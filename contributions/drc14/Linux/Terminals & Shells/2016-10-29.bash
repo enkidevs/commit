8003799274e32a0a80f0d2615c33fff4a696a065
drc14 <dalton_cl@yahoo.com>
2016-10-29 16:11:57 +0100

    2016-10-29's Linux workout

---
+++ contributions/drc14/Linux/Terminals & Shells/2016-10-29.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Quick `bash` shortcuts 
+Random password generator
+Adding directories to your `$PATH`
