9bf35e18f9c8b5579ce68b103bc36f40fbab6ca7
ViniMoraes <moraes.vinicius@hotmail.com>
2016-10-29 13:28:27 +0100

    2016-10-29's Linux workout

---
+++ contributions/ViniMoraes/Linux/System Management/2016-10-29.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
