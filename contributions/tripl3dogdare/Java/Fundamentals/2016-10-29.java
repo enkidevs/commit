b0f2b76aab0070ca3602144a9e12b5224d1e479f
tripl3dogdare <connor@connorjulian.org>
2016-10-29 16:48:49 +0100

    2016-10-29's Java workout

---
+++ contributions/tripl3dogdare/Java/Fundamentals/2016-10-29.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
