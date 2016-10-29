f84cfb3eb5af9810d512488fea04a53887a23481
riska <riskawarrior@live.com>
2016-10-29 19:06:33 +0100

    2016-10-29's JavaScript workout

---
+++ contributions/riska/JavaScript/React/2016-10-29.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Type of the Children props
+Props in getInitialState Is an        
+Anti-Pattern
+Keyed fragments in **React**
+Custom `propType`'s to be required
