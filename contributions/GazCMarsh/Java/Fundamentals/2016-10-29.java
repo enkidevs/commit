39cf13b03de1c0ef03a2194be4e81ea68ba42edf
GazCMarsh <gazmarsh@meaningfulname.net>
2016-10-29 09:01:41 +0100

    2016-10-29's Java workout

---
+++ contributions/GazCMarsh/Java/Fundamentals/2016-10-29.java
@@ -0,0 +1,5 @@
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Constant notation
+How range operations work
