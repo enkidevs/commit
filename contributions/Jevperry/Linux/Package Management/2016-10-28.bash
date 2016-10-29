291b26d68da45c1ebe66a43211e684181ad0166c
Jevperry <jvegaperry@gmail.com>
2016-10-29 04:20:30 +0100

    2016-10-28's Linux workout

---
+++ contributions/Jevperry/Linux/Package Management/2016-10-28.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+Prevent updating a specific package in Debian systems
+Using `Tasksel` for software installation
+Solving `BADSIG GPG` errors
