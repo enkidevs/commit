8c6dde635f0df70839b483f2ecdaaec5222a71db
adamlynch <contact@adamlynch.com>
2016-10-29 16:20:51 +0100

    2016-10-29's JavaScript workout

---
+++ contributions/adamlynch/JavaScript/Functions/2016-10-29.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Method chaining
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
