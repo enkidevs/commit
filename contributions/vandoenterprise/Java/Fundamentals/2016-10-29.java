482c1ab6ad4f10827153e7aa7dc5193b49ae9bd6
vandoenterprise <evandro.barbosa@yahoo.com.br>
2016-10-29 14:39:03 +0100

    2016-10-29's Java workout

---
+++ contributions/vandoenterprise/Java/Fundamentals/2016-10-29.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Using `Printstream.format` to format a string before printing
+Calling one constructor from another
+Locks in `static synchronized` methods
+`Autoboxing` and `Unboxing`
