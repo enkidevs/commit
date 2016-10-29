a0debee04b3c3b4f9609d26316fbdd9e76438555
webberpuma <webberpuma@gmail.com>
2016-10-29 03:29:23 +0100

    2016-10-28's Linux workout

---
+++ contributions/webberpuma/Linux/Terminals & Shells/2016-10-28.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+The Basics of Environment Variables
