402964b2fc1773fb2b778fb00ba58e526abb7619
lorena <lorena.blue.72@gmail.com>
2016-10-29 14:42:41 +0100

    2016-10-29's JavaScript workout

---
+++ contributions/lorena/JavaScript/Core/2016-10-29.js
@@ -0,0 +1,5 @@
+Meaning of polyfill
+Data type comparison in `switch` statements
+The difference between null, undefined and NaN
+Detect **DO NOT TRACK** status
+Inaccuracy of binary floating-point format
