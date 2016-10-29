30ce7c60de65aee95b667ee077ef3de7e3fd470d
z-dna <kamil.m.antonczak@gmail.com>
2016-10-29 19:49:02 +0100

    2016-10-29's Java workout

---
+++ contributions/z-dna/Java/Error Handling/2016-10-29.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Careful Numeric Data Conversions
