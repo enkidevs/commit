ab1859ba8daaa0b71594eed79c6f0176722972d2
KiwiEmperor <jagihunter@gmail.com>
2016-10-30 00:39:41 +0100

    2016-10-29's JavaScript workout

---
+++ contributions/KiwiEmperor/JavaScript/Functions/2016-10-29.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+IIFE pattern
+Get query/url variables
