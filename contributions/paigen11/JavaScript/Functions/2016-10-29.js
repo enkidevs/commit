efe4c46540661ac03f4200bbb5d40d825e63c123
paigen11 <paigen11@gmail.com>
2016-10-29 23:15:22 +0100

    2016-10-29's JavaScript workout

---
+++ contributions/paigen11/JavaScript/Functions/2016-10-29.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` using `call` and `apply` methods
+`.bind()` 
+Anonymous functions
+Easily generate a random `HEX` color
