5cb3b1f763ebd6feaf2522cf68e019552494426e
Jonnygwi <jonnygwillim@gmail.com>
2016-10-29 20:13:41 +0100

    2016-10-29's JavaScript workout

---
+++ contributions/Jonnygwi/JavaScript/Core/2016-10-29.js
@@ -0,0 +1,5 @@
+Keywords in hash keys
+Most efficient way to build `HTML` strings
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Base conversion 
