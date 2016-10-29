45e4f3195533d4ae5adeb92885ca02d58652ad2e
MagicEinhorn2000 <ks.wischmop@gmail.com>
2016-10-29 07:10:09 +0100

    2016-10-29's Java workout

---
+++ contributions/MagicEinhorn2000/Java/I/O/2016-10-29.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
