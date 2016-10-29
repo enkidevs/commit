a031022bd6ff5f2ec9353a29d672206703d517fd
Kolejarzyk <pawel.dee@live.com>
2016-10-29 11:11:16 +0100

    2016-10-29's Java workout

---
+++ contributions/Kolejarzyk/Java/Fundamentals/2016-10-29.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
