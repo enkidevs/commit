df6069aee2602eec233dddb6d2e3c5fc8360c6f0
drofi <drofusauros@gmail.com>
2016-10-29 23:13:51 +0100

    2016-10-29's JavaScript workout

---
+++ contributions/drofi/JavaScript/React/2016-10-29.js
@@ -0,0 +1,6 @@
+`if`-`else` statements in **JSX** and **React**
+componentWillReceiveProps Not Triggered After Mounting
+
+Using `false` in **JSX**
+Dangerously set `innerHTML`
+Enhanced Prop Validation 
