a8bddaedaa5caceba3ad7f6e828712778b1364e1
bobsmith947 <lkitaev@cherrycreekschools.org>
2016-10-29 16:18:36 +0100

    2016-10-29's Java workout

---
+++ contributions/bobsmith947/Java/Error Handling/2016-10-29.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Most common reason behind **stack overflow** error
