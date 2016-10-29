8420defd028d7e467369ddc8f5b1eef0ee1427e5
Shalini <shaliniguha2@gmail.com>
2016-10-29 04:41:47 +0100

    2016-10-28's Java workout

---
+++ contributions/Shalini/Java/Data Types/2016-10-28.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Converting numbers to strings
+Finding a substring in a string
