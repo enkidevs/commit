ad4b77acd3767c46eb7180aa78d16a4361e8f793
hakuro-jp <hakuro.jp@gmail.com>
2016-10-29 18:06:38 +0100

    2016-10-29's JavaScript workout

---
+++ contributions/hakuro-jp/JavaScript/Core/2016-10-29.js
@@ -0,0 +1,5 @@
+Be careful when using `typeof`.
+Setting default values with `short circuiting`
+Use `splice()` to remove an item from an array
+`NaN`
+The difference between null, undefined and NaN
