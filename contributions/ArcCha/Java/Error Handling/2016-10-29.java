d58999e17a45b469c835312f5768f3a3b2b7ef29
ArcCha <adam1piekarczyk@gmail.com>
2016-10-29 10:21:18 +0100

    2016-10-29's Java workout

---
+++ contributions/ArcCha/Java/Error Handling/2016-10-29.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Use try-with-resources instead of `finally`
+Prevent `exceptions` while logging Data
+Throwing an `exception`
+Careful Numeric Data Conversions
