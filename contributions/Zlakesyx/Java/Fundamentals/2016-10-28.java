f69a1d79f3e8638ed5cbdcd60ac5c0ef8c5e71bf
Zlakesyx <jdcoffey54@gmail.com>
2016-10-29 01:06:38 +0100

    2016-10-28's Java workout

---
+++ contributions/Zlakesyx/Java/Fundamentals/2016-10-28.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Generating random numbers
+The trade-off between short and descriptive names
+Java naming conventions
+Updating interfaces by using `default` methods
