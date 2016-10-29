c9c4a218ff7ff8412ecab79005a796e593365fdb
Tofuus <17georgel@gmail.com>
2016-10-29 16:05:24 +0100

    2016-10-28's Java workout

---
+++ contributions/Tofuus/Java/Error Handling/2016-10-28.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Throwing an `exception`
+Careful Numeric Data Conversions
