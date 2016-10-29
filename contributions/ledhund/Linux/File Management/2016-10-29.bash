f230ab678ccc1296f43205559b5a0ce072a2e261
ledhund <anders@hedlund.de>
2016-10-29 10:14:17 +0100

    2016-10-29's Linux workout

---
+++ contributions/ledhund/Linux/File Management/2016-10-29.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
