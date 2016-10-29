4bc6715ed35c622cf761dacc141e10e5f444eab7
pazer <pazer@gmx.com>
2016-10-29 20:03:48 +0100

    2016-10-29's Linux workout

---
+++ contributions/pazer/Linux/Terminals & Shells/2016-10-29.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
