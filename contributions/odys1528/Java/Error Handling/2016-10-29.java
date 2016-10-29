cdd7ee8ab01b3df55ab307526e82bc62a426b4e1
odys1528 <jolanta.zakrzewska@orange.pl>
2016-10-29 22:09:19 +0100

    2016-10-29's Java workout

---
+++ contributions/odys1528/Java/Error Handling/2016-10-29.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Most common reason behind **stack overflow** error
