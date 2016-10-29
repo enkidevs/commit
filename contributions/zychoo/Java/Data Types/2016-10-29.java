e4d28c5267d93e2bda8661fc7c16e1b81689feb4
zychoo <zychster@gmail.com>
2016-10-29 12:31:37 +0100

    2016-10-29's Java workout

---
+++ contributions/zychoo/Java/Data Types/2016-10-29.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Multidimensional array declaration
+Finding a substring in a string
