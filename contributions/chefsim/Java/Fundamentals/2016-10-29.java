458b12319088dc879e7a6682d9e5864e43c472d6
chefsim <chefsim@hotmail.com>
2016-10-29 08:25:16 +0100

    2016-10-29's Java workout

---
+++ contributions/chefsim/Java/Fundamentals/2016-10-29.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
