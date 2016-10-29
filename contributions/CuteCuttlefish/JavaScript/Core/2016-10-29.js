4cc33c72267cb2c13f46bba9d7acb3fabf6e956c
CuteCuttlefish <anikavandersanden@gmail.com>
2016-10-29 16:48:46 +0100

    2016-10-29's JavaScript workout

---
+++ contributions/CuteCuttlefish/JavaScript/Core/2016-10-29.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Setting default values with `short circuiting`
+The difference between null, undefined and NaN
+Inaccuracy of binary floating-point format
+Base conversion 
