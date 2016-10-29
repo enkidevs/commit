99658738ee38ed5205e9e25743b534df3c452adf
vieurou <vieurou@gmail.com>
2016-10-29 12:25:31 +0100

    2016-10-28's Linux workout

---
+++ contributions/vieurou/Linux/System Management/2016-10-28.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
