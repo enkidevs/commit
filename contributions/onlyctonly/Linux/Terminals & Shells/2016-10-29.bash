995963439ce7bc3bd75ce1b5537d7fac76a27679
onlyctonly <onlyctonly@icloud.com>
2016-10-29 17:55:55 +0100

    2016-10-29's Linux workout

---
+++ contributions/onlyctonly/Linux/Terminals & Shells/2016-10-29.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Terminal based browser
+Terminal incognito mode
+Sequential execution using the `;` statement separator
