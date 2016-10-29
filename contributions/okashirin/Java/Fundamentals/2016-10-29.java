66a14af3d6887a4b394c5ad78e534387cf6c790d
okashirin <lokishadow@mail.ru>
2016-10-30 00:30:57 +0100

    2016-10-29's Java workout

---
+++ contributions/okashirin/Java/Fundamentals/2016-10-29.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Limit Accessibility of `Fields`
