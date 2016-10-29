42fa0ecf623bf22f43398c8c6e8c972edf10d526
koliada <alex.fiator@gmail.com>
2016-10-29 21:52:32 +0100

    2016-10-29's Java workout

---
+++ contributions/koliada/Java/Fundamentals/2016-10-29.java
@@ -0,0 +1,5 @@
+Generating random numbers
+The trade-off between short and descriptive names
+Writing generic methods
+Using Inheritance to reduce code repetition
+Calling one constructor from another
