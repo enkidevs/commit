b8a5d636f6486299a073b2507777064737ed412d
and_re <andre2go@gmail.com>
2016-10-29 07:06:05 +0100

    2016-10-29's JavaScript workout

---
+++ contributions/and_re/JavaScript/Functions/2016-10-29.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Function call method
+Move cursor at the end of text input
+Social sharing without widgets
