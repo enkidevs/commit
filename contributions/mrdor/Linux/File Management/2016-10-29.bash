51cc349eafeaa822723b25fa8c528d1de825886d
mrdor <mrdor44@gmail.com>
2016-10-29 14:52:59 +0100

    2016-10-29's Linux workout

---
+++ contributions/mrdor/Linux/File Management/2016-10-29.bash
@@ -0,0 +1,7 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
