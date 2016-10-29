3e964680dcbd8c67d1b89a779ae32cb1a69743af
emoholcicka <cartous@gmail.com>
2016-10-29 18:34:24 +0100

    2016-10-29's Linux workout

---
+++ contributions/emoholcicka/Linux/Package Management/2016-10-29.bash
@@ -0,0 +1,5 @@
+Prevent updating a specific package in Debian systems
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
