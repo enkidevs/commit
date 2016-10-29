10d84889c2da53f011f0369ac36d518706173569
mrzarei <zarei.mohamadreza@gmail.com>
2016-10-30 00:05:19 +0100

    2016-10-29's Linux workout

---
+++ contributions/mrzarei/Linux/System Management/2016-10-29.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Monitor System Memory using `vmstat`
