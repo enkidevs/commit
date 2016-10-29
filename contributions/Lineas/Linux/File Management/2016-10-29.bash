161d0c159d6c2c7f7a421b016e64f5685fbf1f17
Lineas <loulou.sr@hotmail.fr>
2016-10-29 18:28:38 +0100

    2016-10-29's Linux workout

---
+++ contributions/Lineas/Linux/File Management/2016-10-29.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
