e020224b7528058409da313ccabd39f255858d11
disassembly <hpeng2005012255@gmail.com>
2016-10-29 07:29:04 +0100

    2016-10-28's Linux workout

---
+++ contributions/disassembly/Linux/File Management/2016-10-28.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
