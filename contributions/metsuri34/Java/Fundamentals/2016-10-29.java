1fb40d968bd56f7a051072c3ceffacd498828274
metsuri34 <metsaranta.mikko@gmail.com>
2016-10-29 21:37:19 +0100

    2016-10-29's Java workout

---
+++ contributions/metsuri34/Java/Fundamentals/2016-10-29.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Java naming conventions
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
