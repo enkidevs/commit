645e0b1e284f126eaf2b16867833b6489fa517cf
ellisandy <andrew@andrewryanellis.com>
2016-10-29 23:02:09 +0100

    2016-10-29's Java workout

---
+++ contributions/ellisandy/Java/Fundamentals/2016-10-29.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Catch multiple exceptions in a single `catch` block
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Using an interface as a parameter
