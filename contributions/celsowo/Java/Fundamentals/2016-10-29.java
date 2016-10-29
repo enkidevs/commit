c068ebc3cf83b67318dac2cfd0ca4f09760cb9d8
celsowo <celsowo@gmail.com>
2016-10-29 14:25:41 +0100

    2016-10-29's Java workout

---
+++ contributions/celsowo/Java/Fundamentals/2016-10-29.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
