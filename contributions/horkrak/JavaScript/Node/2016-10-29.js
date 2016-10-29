fc5ac8a9c2f996310b6fb7f82f3673b0ab4a16de
horkrak <shorasch@gmail.com>
2016-10-29 21:22:39 +0100

    2016-10-29's JavaScript workout

---
+++ contributions/horkrak/JavaScript/Node/2016-10-29.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Asynchronous File Write/Read in Node.js
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+**Node** `os` module
