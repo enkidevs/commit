20c1737a0589208a3b2767adb91ca71066cb300b
savec80 <artem.savenkov@gmail.com>
2016-10-30 00:36:00 +0100

    2016-10-29's Linux workout

---
+++ contributions/savec80/Linux/File Management/2016-10-29.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Delete commands aliases
