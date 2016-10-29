094c55684293b7a32a743cd4a38f39401eab7ad4
notemaster <notemasterisaac@gmail.com>
2016-10-29 17:23:00 +0100

    2016-10-29's Java workout

---
+++ contributions/notemaster/Java/Fundamentals/2016-10-29.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
