b2fabf4ce009a0163dc4a53ab25addcf11b9a09e
stufixlp <stufixlp@gmail.com>
2016-10-29 21:15:10 +0100

    2016-10-29's Java workout

---
+++ contributions/stufixlp/Java/Fundamentals/2016-10-29.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Ternary operator
+Using bounded type parameters in generic methods
+Using `synchronized` statements
