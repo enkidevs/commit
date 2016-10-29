6af69beae7e11957e2ba06d0b224449c54afd353
blackweb <lukasz.bajsarowicz@gmail.com>
2016-10-29 22:40:13 +0100

    2016-10-29's Linux workout

---
+++ contributions/blackweb/Linux/System Management/2016-10-29.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
