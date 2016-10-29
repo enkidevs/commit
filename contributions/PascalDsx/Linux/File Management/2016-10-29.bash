5b0a505d563c4d8acc11d075430dc59f14666087
PascalDsx <pascaldissieux@gmail.com>
2016-10-29 13:20:55 +0100

    2016-10-29's Linux workout

---
+++ contributions/PascalDsx/Linux/File Management/2016-10-29.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Easy way of sharing files
