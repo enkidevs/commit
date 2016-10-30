e95642300799fde2fc9d846f0376af77ecc5c4b2
tyro <francesc.alonso@gmail.com>
2016-10-30 16:10:02 +0000

    2016-10-30's Java workout

---
+++ contributions/tyro/Java/Fundamentals/2016-10-30.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Use strings in a `switch` statement
+Using Inheritance to reduce code repetition
+Using bounded type parameters in generic methods
+Calling one constructor from another
