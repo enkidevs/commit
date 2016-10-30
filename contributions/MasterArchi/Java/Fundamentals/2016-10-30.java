83c111135a8d9840f1721a8900b45b59d0cf6c8e
MasterArchi <cosmaalexvergari@gmail.com>
2016-10-30 14:14:39 +0000

    2016-10-30's Java workout

---
+++ contributions/MasterArchi/Java/Fundamentals/2016-10-30.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Common mistake on switch statements
+Constant notation
