741180cc2439693e445d40ab31a4067414e6872a
notemaster <notemasterisaac@gmail.com>
2016-10-31 00:57:05 +0300

    2016-10-30's Java workout

---
+++ contributions/notemaster/Java/Fundamentals/2016-10-30.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Metadata: setting a file's owner
