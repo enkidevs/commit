05d9071d2ee24a7c735409c47bacdaf8984215ea
jonno85 <filippini.jo@gmail.com>
2016-10-30 09:09:00 +0000

    2016-10-30's Git workout

---
+++ contributions/jonno85/Git/Essentials/2016-10-30.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+Using tags for version control
+Make your log output pretty
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
