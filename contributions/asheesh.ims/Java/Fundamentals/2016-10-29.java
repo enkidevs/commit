82213d1d1643e74544c4301d46f6c7faf77547db
asheesh.ims <asheesh.ims@gmail.com>
2016-10-30 05:20:43 +0000

    2016-10-29's Java workout

---
+++ contributions/asheesh.ims/Java/Fundamentals/2016-10-29.java
@@ -0,0 +1,5 @@
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
