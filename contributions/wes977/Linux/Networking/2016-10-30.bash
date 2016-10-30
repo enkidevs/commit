f2052cea4b424c321bd6eea611b0d2e0ab3ac5e2
wes977 <wes977@gmail.com>
2016-10-30 21:27:14 +0300

    2016-10-30's Linux workout

---
+++ contributions/wes977/Linux/Networking/2016-10-30.bash
@@ -0,0 +1,5 @@
+Find services running on your host
+Useful ssh client optimizations 
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
+Make use of `netstat`
