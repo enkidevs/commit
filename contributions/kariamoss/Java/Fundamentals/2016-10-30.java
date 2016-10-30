fe396405217c7a9396d686d1e87b8b365606a1cb
kariamoss <jehanportable@gmail.com>
2016-10-31 02:32:40 +0300

    2016-10-30's Java workout

---
+++ contributions/kariamoss/Java/Fundamentals/2016-10-30.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between `==` and `equals()`
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+How range operations work
