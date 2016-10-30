d75e7d0d9e639c73c33a84cbab704ec8f3199458
ceski23 <ceski23@gmail.com>
2016-10-30 12:49:51 +0000

    2016-10-30's Java workout

---
+++ contributions/ceski23/Java/Fundamentals/2016-10-30.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Common mistake on switch statements
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
