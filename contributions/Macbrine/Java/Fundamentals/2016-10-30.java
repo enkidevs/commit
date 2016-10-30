84dc8fb089fcbe6c71c09d5f359236eb459c873c
Macbrine <mmacneil@vols.utk.edu>
2016-10-30 20:54:41 +0300

    2016-10-30's Java workout

---
+++ contributions/Macbrine/Java/Fundamentals/2016-10-30.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Constant notation
+Supply `toString()` in all classes
