5539737137d6ab41f448c66d668e2675a674a279
v01d <nantog@gmail.com>
2016-10-30 15:51:45 +0000

    2016-10-30's Linux workout

---
+++ contributions/v01d/Linux/System Management/2016-10-30.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
