2eb274e419bb50077ab832d3024a7c34a166eac4
SStalker <raphael@bluepanel.org>
2016-10-30 08:08:52 +0000

    2016-10-30's Linux workout

---
+++ contributions/SStalker/Linux/File Management/2016-10-30.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
