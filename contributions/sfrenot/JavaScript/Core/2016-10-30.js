403c76b359f8971002c0df770f3bd10a2762383d
sfrenot <stephane.frenot@insa-lyon.fr>
2016-10-30 17:15:39 +0000

    2016-10-30's JavaScript workout

---
+++ contributions/sfrenot/JavaScript/Core/2016-10-30.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Deleting properties
+Hoisting applies only to variable declarations, not initializations
+Apply `map` function to array items
+JavaScript compilation
