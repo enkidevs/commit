87b35f145c94402246a2670429a12db84bb0f3d0
gstavrinos <stavrinosgeo@gmail.com>
2016-10-30 01:56:37 +0000

    2016-10-30's JavaScript workout

---
+++ contributions/gstavrinos/JavaScript/Core/2016-10-30.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Using the double tilde `~~` 
+Data type comparison in `switch` statements
+Getting the *real* dimensions of an image
+Inaccuracy of binary floating-point format
