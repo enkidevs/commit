1f3587a22f3f286b8849af9dd37e0c03a5738933
tonespy <terrycents@gmail.com>
2016-10-30 12:28:36 +0000

    2016-10-30's Java workout

---
+++ contributions/tonespy/Java/Error Handling/2016-10-30.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Converting stack trace to a string
+Prevent `exceptions` while logging Data
+Careful Numeric Data Conversions
