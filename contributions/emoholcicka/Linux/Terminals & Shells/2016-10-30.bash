bc702abd482047447eaefefdbae2546cd49476ad
emoholcicka <cartous@gmail.com>
2016-10-30 16:19:55 +0000

    2016-10-30's Linux workout

---
+++ contributions/emoholcicka/Linux/Terminals & Shells/2016-10-30.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal incognito mode
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
