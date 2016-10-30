03b5ae6b598f7287c6066518acf32b3067c18f2b
fmauNeko <fmaunier@gmail.com>
2016-10-30 07:47:46 +0000

    2016-10-30's Linux workout

---
+++ contributions/fmauNeko/Linux/Networking/2016-10-30.bash
@@ -0,0 +1,5 @@
+Multiple IP Address with aliasing 
+Disable `IPv6`
+Useful ssh client optimizations 
+Use `tcpdump` to listen to network interface traffic
+Staying connected remotely via SSH without an interactive shell
