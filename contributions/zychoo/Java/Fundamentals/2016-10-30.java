6e6d31987abdf22ae1e18f6ae7513ad8182e628c
zychoo <zychster@gmail.com>
2016-10-30 12:20:02 +0000

    2016-10-30's Java workout

---
+++ contributions/zychoo/Java/Fundamentals/2016-10-30.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Using `synchronized` statements
+How range operations work
