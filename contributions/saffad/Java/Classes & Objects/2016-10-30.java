a311abc620059bce51412a1c1a46d2f82b70900e
saffad <michal_kolsut@o2.pl>
2016-10-30 13:23:32 +0000

    2016-10-30's Java workout

---
+++ contributions/saffad/Java/Classes & Objects/2016-10-30.java
@@ -0,0 +1,5 @@
+Template methods in abstract classes
+Compare classes and not class names
+Diamond Operator
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
