e55ddada7af6d2875d18925152655ca4d52f09d1
sestiz <sesti25@hotmail.com>
2016-10-30 21:04:11 +0300

    2016-10-30's Java workout

---
+++ contributions/sestiz/Java/Fundamentals/2016-10-30.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Using `Printstream.format` to format a string before printing
+Locks in `static synchronized` methods
+Constant notation
+Always specify type parameter
