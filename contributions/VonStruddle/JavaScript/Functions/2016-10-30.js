62591737f942d894379105bac915bc475cfe6e7d
VonStruddle <quentin.durantay@gmail.com>
2016-10-30 15:26:06 +0000

    2016-10-30's JavaScript workout

---
+++ contributions/VonStruddle/JavaScript/Functions/2016-10-30.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Check if a document is done loading
