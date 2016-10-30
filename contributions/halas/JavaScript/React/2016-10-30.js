efa7187e02a934764b8f6e929515c44791dd6eab
halas <piotr.halasiewicz@hadora.pl>
2016-10-30 07:08:59 +0000

    2016-10-30's JavaScript workout

---
+++ contributions/halas/JavaScript/React/2016-10-30.js
@@ -0,0 +1,5 @@
+Self-closing tags in **JSX**
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+**React** `this.props.children`
+Dealing with `this.props.children`
