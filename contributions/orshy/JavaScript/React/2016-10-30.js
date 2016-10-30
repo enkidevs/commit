aa77525b91767683abd301305de62f5621918e54
orshy <philiporchard@gmail.com>
2016-10-31 01:49:09 +0300

    2016-10-30's JavaScript workout

---
+++ contributions/orshy/JavaScript/React/2016-10-30.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+Self-closing tags in **JSX**
+Pixel values in style props for **React**
+Props in getInitialState Is an        
+Anti-Pattern
+Enhanced Prop Validation 
