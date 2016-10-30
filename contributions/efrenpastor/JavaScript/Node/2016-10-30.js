623fa24234193e92fc342b249b25a4a4afad561f
efrenpastor <efrenpastor@gmail.com>
2016-10-30 17:15:50 +0000

    2016-10-30's JavaScript workout

---
+++ contributions/efrenpastor/JavaScript/Node/2016-10-30.js
@@ -0,0 +1,5 @@
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
