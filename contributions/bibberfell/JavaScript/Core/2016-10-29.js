4ed201d79de84f59e0a01b03170e4c90d9fd04fa
bibberfell <bibberfell@gmail.com>
2016-10-30 07:54:05 +0000

    2016-10-29's JavaScript workout

---
+++ contributions/bibberfell/JavaScript/Core/2016-10-29.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Performing a function at timed intervals
+Filtering items out of an array
+JavaScript compilation
+Extra function arguments are undefined by default
