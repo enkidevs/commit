bc03657591101c29344521c76eaeb57f5b99c7b3
Most24 <github@most24.de>
2016-10-30 09:02:50 +0000

    2016-10-30's Java workout

---
+++ contributions/Most24/Java/Error Handling/2016-10-30.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Throwing an `exception`
+Careful Numeric Data Conversions
