2bee1ceb2c510b721f33fb09ad6d42350e0869a4
Androbin <robin.richtsfeld@gmail.com>
2016-10-30 12:19:15 +0000

    2016-10-30's Java workout

---
+++ contributions/Androbin/Java/Fundamentals/2016-10-30.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
