5ae05e8fa151377a08003ffccc01425cd39a27ab
knapeczadam <knapecz.adam@gmail.com>
2016-10-30 13:00:27 +0000

    2016-10-30's Java workout

---
+++ contributions/knapeczadam/Java/Fundamentals/2016-10-30.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+The trade-off between short and descriptive names
+Altering format string output by changing a format specifier's `argument_index`
+Calling one constructor from another
+Supply `toString()` in all classes
