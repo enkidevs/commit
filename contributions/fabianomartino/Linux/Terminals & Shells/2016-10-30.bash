d4edf3ac5c9ad94139576112f2ad5c16477e972b
fabianomartino <fabianomartino@gmail.com>
2016-10-30 23:23:12 +0300

    2016-10-30's Linux workout

---
+++ contributions/fabianomartino/Linux/Terminals & Shells/2016-10-30.bash
@@ -0,0 +1,7 @@
+Clear the terminal instantly
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+The Basics of Environment Variables
