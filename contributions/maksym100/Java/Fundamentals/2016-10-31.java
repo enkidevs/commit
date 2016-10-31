369f266aa760c9a5b1ed394255108c2aace56004
maksym100 <maksym100@gmail.com>
2016-10-31 20:12:11 +0300

    2016-10-31's Java workout

---
+++ contributions/maksym100/Java/Fundamentals/2016-10-31.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
