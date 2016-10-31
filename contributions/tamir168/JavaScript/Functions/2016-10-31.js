7abd6fa663d4216122747942c55822f663a87502
tamir168 <tamir168@gmail.com>
2016-10-31 23:39:49 +0300

    2016-10-31's JavaScript workout

---
+++ contributions/tamir168/JavaScript/Functions/2016-10-31.js
@@ -0,0 +1,5 @@
+Hoisting
+Function constructor vs. function declaration vs. function expression
+Easily generate a random `HEX` color
+Function call method
+Changing a functions context with `fn.call(object)`
