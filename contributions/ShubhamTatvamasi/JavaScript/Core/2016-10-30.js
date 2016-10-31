f317e14f9d01a7033c932984ab63c407872849a7
ShubhamTatvamasi <shubhamtatvamasi@gmail.com>
2016-10-31 17:23:02 +0300

    2016-10-30's JavaScript workout

---
+++ contributions/ShubhamTatvamasi/JavaScript/Core/2016-10-30.js
@@ -0,0 +1,5 @@
+Get a random item from an Array
+Data type comparison in `switch` statements
+The distinction between `==` and `===`
+Extra function arguments are undefined by default
+`console.*` in JavaScript
