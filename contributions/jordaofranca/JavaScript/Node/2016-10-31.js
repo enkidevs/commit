9041dc68510df5a359c932521532c25142c4d438
jordaofranca <jordaofranca@gmail.com>
2016-10-31 16:23:53 +0300

    2016-10-31's JavaScript workout

---
+++ contributions/jordaofranca/JavaScript/Node/2016-10-31.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Synchronous File Write/Read in Node.js
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+`exports` vs. `module.exports` in **Node**
