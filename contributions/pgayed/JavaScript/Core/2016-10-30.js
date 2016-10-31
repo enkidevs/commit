23b197d74899ab391e81faf01ce8cb3e3186295c
pgayed <pgayed@gmail.com>
2016-10-31 04:13:02 +0300

    2016-10-30's JavaScript workout

---
+++ contributions/pgayed/JavaScript/Core/2016-10-30.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Performing a function at timed intervals
+Data type comparison in `switch` statements
+Filtering items out of an array
+The difference between null, undefined and NaN
