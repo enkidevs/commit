ca8abac1f6d4528d8e7ec670697e07f054970e83
Malanius <malanius@seznam.cz>
2016-10-31 09:29:39 +0300

    2016-10-31's Linux workout

---
+++ contributions/Malanius/Linux/File Management/2016-10-31.bash
@@ -0,0 +1,7 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Easy way of sharing files
