71e3b732e7a30db5b6249ec39f79080447cafdfb
aaron97neu <aaron97neu@yahoo.com>
2016-10-31 16:47:15 +0300

    2016-10-31's Java workout

---
+++ contributions/aaron97neu/Java/Fundamentals/2016-10-31.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
