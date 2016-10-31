ef7281d4674a4677dbfda1f720d5621ba708c7ae
elbenjas <dseinglant@gmail.com>
2016-10-31 09:02:28 +0300

    2016-10-30's Java workout

---
+++ contributions/elbenjas/Java/Fundamentals/2016-10-30.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Avoid using `static` variables
+Using an interface as a parameter
