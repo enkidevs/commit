ac385c9602361a8992a38f7803d87e2f172c2eab
tnargwoxow <tnargwoxow@gmail.com>
2016-11-01 01:27:02 +0300

    2016-10-31's Linux workout

---
+++ contributions/tnargwoxow/Linux/Package Management/2016-10-31.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Backporting on Debian
+
+Installing a `.deb` package from the terminal
+Using `Tasksel` for software installation
+Solving `BADSIG GPG` errors
