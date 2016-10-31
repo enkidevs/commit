1490625c0a2683d5c00d4ea840f30e0cf38418e6
Zanex <alessio.zanella88@gmail.com>
2016-10-31 13:04:44 +0300

    2016-10-31's Linux workout

---
+++ contributions/Zanex/Linux/System Management/2016-10-31.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Test disk speed with `dd`
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
