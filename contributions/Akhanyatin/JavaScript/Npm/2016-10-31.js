9df86f761ecc7332b5b58ef054e6e37837412238
Akhanyatin <nicolas.habak@gmail.com>
2016-11-01 00:20:31 +0300

    2016-10-31's JavaScript workout

---
+++ contributions/Akhanyatin/JavaScript/Npm/2016-10-31.js
@@ -0,0 +1,5 @@
+List binaries for scripting in npm
+Environmental variables and flags for `npm`
+`npm`  as an alternative to Gulp
+Current Lifecycle Event
+Lock down dependency versions by shrinkwrapping
