2874711c5e526e208a07db83495ba87354c18ef0
wieczorek1990 <wieczorek1990@gmail.com>
2016-10-31 16:29:29 +0300

    2016-10-31's Java workout

---
+++ contributions/wieczorek1990/Java/Fundamentals/2016-10-31.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
