76749a0f028ef88e4b66603770e786cb7eebb3ba
Rainsho <djkyux@163.com>
2016-10-31 17:19:37 +0300

    2016-10-30's Java workout

---
+++ contributions/Rainsho/Java/Fundamentals/2016-10-30.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
