e1de80b6f6b6669376d63562a7d885d5220b7b16
padawan4330 <padawan4330@gmail.com>
2016-10-31 20:19:34 +0300

    2016-10-31's Linux workout

---
+++ contributions/padawan4330/Linux/Jobs & Processes/2016-10-31.bash
@@ -0,0 +1,5 @@
+`Cron` dot-in-filename issues when using `run-parts`
+Fixing the shell in cron
+Common issue with scripts in `cron`
+Set a process priority
+How to pause a process and run others in the background
