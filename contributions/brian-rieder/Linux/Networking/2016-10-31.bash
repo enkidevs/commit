3eae1b4aee500f778cf58f66974525e6ba74b5c7
brian-rieder <brieder@purdue.edu>
2016-10-31 20:19:58 +0300

    2016-10-31's Linux workout

---
+++ contributions/brian-rieder/Linux/Networking/2016-10-31.bash
@@ -0,0 +1,5 @@
+Useful ssh client optimizations 
+Limit the `wget` download rate
+Getting the connection speed from the terminal
+Bandwidth monitoring tools 
+Use `tcpdump` to listen to network interface traffic
