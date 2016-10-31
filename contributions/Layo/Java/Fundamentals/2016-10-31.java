9b149b24d9c8008e49d01b82a98d7df5e247afba
Layo <thegamerult@googlemail.com>
2016-10-31 09:44:36 +0300

    2016-10-31's Java workout

---
+++ contributions/Layo/Java/Fundamentals/2016-10-31.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
