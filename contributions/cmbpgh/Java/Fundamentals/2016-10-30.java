e3295df0b942e98f2fc6d48e72fec0f554453b3e
cmbpgh <cmbpgh@gmail.com>
2016-10-31 07:09:26 +0300

    2016-10-30's Java workout

---
+++ contributions/cmbpgh/Java/Fundamentals/2016-10-30.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Ternary operator
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
