c1a129f321c034f5ef8672b203178f9a15645ca3
BooM <tararuj.michal@gmail.com>
2016-10-31 09:35:05 +0300

    2016-10-31's Java workout

---
+++ contributions/BooM/Java/Fundamentals/2016-10-31.java
@@ -0,0 +1,5 @@
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Measuring time
+Detect or prevent integer overflow
