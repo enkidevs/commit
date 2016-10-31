be9ac610530573037f21f9162d49f5a90f713130
cavalierejody <cavalierejody@yahoo.it>
2016-10-31 05:24:12 +0300

    2016-10-31's JavaScript workout

---
+++ contributions/cavalierejody/JavaScript/Core/2016-10-31.js
@@ -0,0 +1,5 @@
+Labeling your loops
+Multi-line string variables
+Hoisting applies only to variable declarations, not initializations
+JavaScript compilation
+Extra function arguments are undefined by default
