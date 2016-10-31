907c9a0dfb37b9d611edd720303dbef608430d84
svartr <the.trash.collector@yandex.com>
2016-10-31 11:46:03 +0300

    2016-10-30's JavaScript workout

---
+++ contributions/svartr/JavaScript/Functions/2016-10-30.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Check if a document is done loading
