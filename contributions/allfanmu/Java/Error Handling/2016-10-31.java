b9e4e3d6486d1c868552e43f163b8225588a562f
allfanmu <allfanmu@wp.pl>
2016-10-31 08:50:07 +0300

    2016-10-31's Java workout

---
+++ contributions/allfanmu/Java/Error Handling/2016-10-31.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
