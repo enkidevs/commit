be000ea175bc1b46a563fa5b4000c2a6823a6724
kirillku <kubryakov@gmail.com>
2016-10-31 16:17:20 +0300

    2016-10-31's Linux workout

---
+++ contributions/kirillku/Linux/File Management/2016-10-31.bash
@@ -0,0 +1,5 @@
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Working with `.gz` files
+Delete commands aliases
