355f7c2e99de94e2a6e8d291a0de9606f52d5230
Zhigalin <alexander@zhigalin.tk>
2016-10-31 11:19:53 +0300

    2016-10-31's Linux workout

---
+++ contributions/Zhigalin/Linux/Terminals & Shells/2016-10-31.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Find any Unix / Linux command
+Terminal based browser
+Conditional command execution
+
+(`&&` operator)
