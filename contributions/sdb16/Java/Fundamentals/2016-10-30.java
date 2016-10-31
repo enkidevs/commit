eec644e2fb931cbe3e04a9dcd3fae3288187df7f
sdb16 <sanjesh1992@gmail.com>
2016-10-31 04:10:13 +0300

    2016-10-30's Java workout

---
+++ contributions/sdb16/Java/Fundamentals/2016-10-30.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use meaningful names
+Catch multiple exceptions in a single `catch` block
+Detect or prevent integer overflow
+Using an interface as a parameter
