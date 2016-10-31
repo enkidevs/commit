b9b1922419b4cbe88c042ae1822638e84b87ce71
loictessier <loictessier.inge@gmail.com>
2016-10-31 07:19:20 +0300

    2016-10-31's JavaScript workout

---
+++ contributions/loictessier/JavaScript/Node/2016-10-31.js
@@ -0,0 +1,5 @@
+REPL in Node.js
+Global Objects and Environment Variables in **Node**
+Handle `JSON.parse` error in Node.js
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
