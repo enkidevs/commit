e2b5110450ec6c6df66aadb99674e9faace8e6b2
itmoh <oblivionhammer@gmail.com>
2016-10-31 10:26:59 +0300

    2016-10-31's JavaScript workout

---
+++ contributions/itmoh/JavaScript/Functions/2016-10-31.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Functions can be declared after use
+Move cursor at the end of text input
+Social sharing without widgets
+Check if a document is done loading
