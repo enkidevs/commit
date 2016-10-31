11eb140c50ae6289ab063aadf7ed128bb75c8895
gobbas <gabbi@hotmail.no>
2016-10-31 09:42:21 +0300

    2016-10-31's Java workout

---
+++ contributions/gobbas/Java/Fundamentals/2016-10-31.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Using Inheritance to reduce code repetition
+Do not perform bitwise and arithmetic operations on the same data
