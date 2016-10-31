f825ab36271d2c2a2f4b62f8a64542158a6cda03
CycleOfTheAbsurd <alex7up42@gmail.com>
2016-10-31 21:31:59 +0300

    2016-10-31's Java workout

---
+++ contributions/CycleOfTheAbsurd/Java/Fundamentals/2016-10-31.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Calling one constructor from another
+Always specify type parameter
+Limit Accessibility of `Fields`
+Using an interface as a parameter
