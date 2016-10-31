1282a823d9bcc218b8c014e21d5a0555ce18b4ab
erickeuller <erickeuller@gmail.com>
2016-11-01 02:22:42 +0300

    2016-10-31's Java workout

---
+++ contributions/erickeuller/Java/Fundamentals/2016-10-31.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
