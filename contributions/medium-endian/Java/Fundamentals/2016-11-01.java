4a9961d4adc3d60a2e4b8434a3dcfba8d3e25175
medium-endian <mediumendian@gmail.com>
2016-11-01 11:36:30 +0300

    2016-11-01's Java workout

---
+++ contributions/medium-endian/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+Use `DecimalFormat` class to format numbers
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
