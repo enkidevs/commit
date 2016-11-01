a62d175b634f634dca5ef0d2d7ae450f6d5473b1
marziopulcini <marzio.pulcini@at-shopnet.com>
2016-11-02 01:25:13 +0300

    2016-11-01's Java workout

---
+++ contributions/marziopulcini/Java/Error Handling/2016-11-01.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Throwing an `exception`
+Careful Numeric Data Conversions
