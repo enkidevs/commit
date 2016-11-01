f88bffe698d7be55351d0c18198e83d3d0686936
Shalini <shaliniguha2@gmail.com>
2016-11-01 13:13:14 +0300

    2016-10-31's Java workout

---
+++ contributions/Shalini/Java/Data Types/2016-10-31.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Collections vs arrays
+Converting a string to upper or lower case
+Equals operation on different data types
+Do not attempt comparisons with NaN
