85ac9b82c6e71d525c3782c1a674596ea6439c89
garymc25 <garymc25@gmail.com>
2016-11-01 04:26:50 +0300

    2016-10-31's Java workout

---
+++ contributions/garymc25/Java/Fundamentals/2016-10-31.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Avoid using `static` variables
