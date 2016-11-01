6b926fa2e3bd1adec35f78becd741aa4d829f0cc
ArcCha <adam1piekarczyk@gmail.com>
2016-11-01 09:35:04 +0300

    2016-11-01's Java workout

---
+++ contributions/ArcCha/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The trade-off between short and descriptive names
+Catch multiple exceptions in a single `catch` block
+Using `synchronized` statements
+Using an interface as a parameter
