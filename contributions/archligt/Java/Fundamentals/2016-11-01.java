27d30a36b7cc04f2a8224e07279a659c10fd9e2a
archligt <frodo.archligt@gmail.com>
2016-11-01 13:27:33 +0300

    2016-11-01's Java workout

---
+++ contributions/archligt/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use strings in a `switch` statement
+Calling one constructor from another
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
