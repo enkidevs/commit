6a9f40f36bbe75004ddf4565c526631ef3a0fa3e
Paulpander <paul.methfessel@web.de>
2016-11-01 19:11:23 +0300

    2016-11-01's Java workout

---
+++ contributions/Paulpander/Java/Threads & Locks/2016-11-01.java
@@ -0,0 +1,5 @@
+Multithreading: use local variables
+Using `Thread.join`
+`Thread.sleep` and `Thread.yield` do not have synchronization mechanisms
+Thread interruptions
+Implementing your own `Thread` 
