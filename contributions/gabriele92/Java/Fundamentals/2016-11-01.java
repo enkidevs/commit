9fb296b366e5fcc40b1a8c92a4b46cecd4d51a3f
gabriele92 <gabrigaudenzi@gmail.com>
2016-11-01 10:51:58 +0300

    2016-11-01's Java workout

---
+++ contributions/gabriele92/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Supply `toString()` in all classes
