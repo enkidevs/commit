7fb92730e6b45e2961de250cd5e1d6c2a41716e4
nmanzi <nathan@nmanzi.com>
2016-11-01 08:55:16 +0300

    2016-10-31's JavaScript workout

---
+++ contributions/nmanzi/JavaScript/React/2016-10-31.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+Props in getInitialState Is an        
+Anti-Pattern
+Use **React** with other libraries
+Immutability helpers in **React**
+Specify a single child
