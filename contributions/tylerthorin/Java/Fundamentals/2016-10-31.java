15c2e32119695d7bfb83ae9e8c2eb8cb58140405
tylerthorin <tthorin@live.com>
2016-11-01 08:20:39 +0300

    2016-10-31's Java workout

---
+++ contributions/tylerthorin/Java/Fundamentals/2016-10-31.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Distinction between `public` and `private` methods
+Updating interfaces by using `default` methods
+Locks in `static synchronized` methods
+Using an interface as a parameter
