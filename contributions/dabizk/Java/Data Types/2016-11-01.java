03054805f3922cb9b72cf0b10d4bd4b62a71d899
dabizk <dabizk@yahoo.com>
2016-11-01 20:55:18 +0300

    2016-11-01's Java workout

---
+++ contributions/dabizk/Java/Data Types/2016-11-01.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Retrieve the component type of an array
