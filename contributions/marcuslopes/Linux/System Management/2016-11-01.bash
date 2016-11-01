cfb567618f4db0cbdfec74ddaec72e8769f46b40
marcuslopes <marcusporteira@gmail.com>
2016-11-01 12:33:00 +0300

    2016-11-01's Linux workout

---
+++ contributions/marcuslopes/Linux/System Management/2016-11-01.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
