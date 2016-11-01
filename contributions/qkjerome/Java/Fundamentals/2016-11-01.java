ef6cf7e289146dcf88ec7306132694bb1a57f7e6
qkjerome <tran.jerome@gmail.com>
2016-11-01 16:10:02 +0300

    2016-11-01's Java workout

---
+++ contributions/qkjerome/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
