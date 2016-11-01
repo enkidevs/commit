f5be0e6bca136a117c5e52ea31da63cf287c68c9
kSom <kgsommer@gmail.com>
2016-11-01 14:49:41 +0300

    2016-11-01's Java workout

---
+++ contributions/kSom/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+Writing generic methods
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
