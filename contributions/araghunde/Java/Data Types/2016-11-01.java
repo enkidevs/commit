6555480bf93bb0f93088f7f5fc8e1b6025b85636
araghunde <paybol.21@gmail.com>
2016-11-01 17:01:15 +0300

    2016-11-01's Java workout

---
+++ contributions/araghunde/Java/Data Types/2016-11-01.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Two ways to use an `Iterator`
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Retrieve the component type of an array
