73e3733721a2a82694831c928140c4b7046bdd95
Tofuus <17georgel@gmail.com>
2016-11-01 19:52:44 +0300

    2016-10-31's Java workout

---
+++ contributions/Tofuus/Java/Data Types/2016-10-31.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Multidimensional array declaration
+Converting numbers to strings
+Do not attempt comparisons with NaN
