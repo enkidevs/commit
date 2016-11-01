6ab4f960c60e3cc7c207c6ab441d8902e0abb616
mr-tim <tim@t-moran.com>
2016-11-01 14:22:07 +0300

    2016-10-31's Java workout

---
+++ contributions/mr-tim/Java/Fundamentals/2016-10-31.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
+Using an interface as a parameter
