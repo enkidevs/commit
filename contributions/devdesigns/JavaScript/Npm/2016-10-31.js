dfedd0fcffda3ef09dc171cf391aa92b805d01ed
devdesigns <joe.dooley@developingdesigns.com>
2016-11-01 05:35:56 +0300

    2016-10-31's JavaScript workout

---
+++ contributions/devdesigns/JavaScript/Npm/2016-10-31.js
@@ -0,0 +1,5 @@
+Combining script commands in npm
+Manage local node modules with `npm link`
+`optionalDependencies` in npm
+`config` object in `package.json`
+Lock down dependency versions by shrinkwrapping
