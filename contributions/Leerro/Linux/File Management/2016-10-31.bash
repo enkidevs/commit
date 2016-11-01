3a5849d910b948a84d1c6e033adfcfa09c5f6b62
Leerro <gabriel.f.bernard@gmail.com>
2016-11-01 06:11:13 +0300

    2016-10-31's Linux workout

---
+++ contributions/Leerro/Linux/File Management/2016-10-31.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
