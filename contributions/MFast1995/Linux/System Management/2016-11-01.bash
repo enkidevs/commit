29655e92c263c7c1406270d04520b756f5d2f112
MFast1995 <m.fast1995@gmail.com>
2016-11-01 19:21:19 +0300

    2016-11-01's Linux workout

---
+++ contributions/MFast1995/Linux/System Management/2016-11-01.bash
@@ -0,0 +1,5 @@
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Cancel a system shutdown
