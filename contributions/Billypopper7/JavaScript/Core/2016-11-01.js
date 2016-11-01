a60541a474f449aa4a0d84210b8554141dca3656
Billypopper7 <billypopper7@gmail.com>
2016-11-01 16:25:55 +0300

    2016-11-01's JavaScript workout

---
+++ contributions/Billypopper7/JavaScript/Core/2016-11-01.js
@@ -0,0 +1,5 @@
+Arrays can be used as stack
+Setting default values with `short circuiting`
+The difference between null, undefined and NaN
+Inaccuracy of binary floating-point format
+Round numbers to `N` decimals 
