5127ec8ce51ad463464ff07f6e03a461de76192c
fadida <shayfadida@gmail.com>
2016-11-01 10:11:57 +0300

    2016-11-01's Java workout

---
+++ contributions/fadida/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
