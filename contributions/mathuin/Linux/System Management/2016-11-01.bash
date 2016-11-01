a9827dc7caeeb48cb5717b72b5948ff61c25396d
mathuin <mathuin@gmail.com>
2016-11-01 20:25:34 +0300

    2016-11-01's Linux workout

---
+++ contributions/mathuin/Linux/System Management/2016-11-01.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Finding Open Files With `lsof`
