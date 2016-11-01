56b37454df248b0ecd5ba9777ad50b44729d6112
Acksl <axel.dahlberg12@gmail.com>
2016-11-01 19:08:10 +0300

    2016-11-01's Linux workout

---
+++ contributions/Acksl/Linux/Terminals & Shells/2016-11-01.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+Retrieve previous arguments
