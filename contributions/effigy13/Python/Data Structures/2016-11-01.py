f87930e8b5f4067c1842714c44d01d5790c5e219
effigy13 <frederika13@yahoo.com>
2016-11-01 20:51:22 +0300

    2016-11-01's Python workout

---
+++ contributions/effigy13/Python/Data Structures/2016-11-01.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`Dictionary` view objects
+Best way to implement a simple `queue` 
+Get the most of `float`s
+Operations with `bytes` and `bytearray`
