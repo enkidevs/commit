8019a61c9a2db54da1aa11b22c349386284c6328
kariamoss <jehanportable@gmail.com>
2016-11-01 18:19:43 +0300

    2016-11-01's Java workout

---
+++ contributions/kariamoss/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+Common mistake on switch statements
+Using `Printstream.format` to format a string before printing
+Calling one constructor from another
