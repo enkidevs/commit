5d009861a072726f47dea095002379de9d7b729a
Blalan <blalan05@gmail.com>
2016-11-01 20:40:18 +0300

    2016-11-01's Linux workout

---
+++ contributions/Blalan/Linux/File Management/2016-11-01.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Deleting non-empty directories
