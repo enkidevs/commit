e21d02f74621597388fb39471b53a4c14628bd24
dan339 <danielcampbell339@gmail.com>
2016-11-01 12:31:14 +0300

    2016-11-01's Java workout

---
+++ contributions/dan339/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
