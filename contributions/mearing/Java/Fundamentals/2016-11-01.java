8e0305b4e582ad61bd2fae0b76dd861f9a3da998
mearing <mark.mearingsmith@gmail.com>
2016-11-01 14:38:52 +0300

    2016-11-01's Java workout

---
+++ contributions/mearing/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
