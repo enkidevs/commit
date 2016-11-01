96774926dae152845b5602bc34f344852afdfab9
odys1528 <jolanta.zakrzewska@orange.pl>
2016-11-01 15:55:46 +0300

    2016-11-01's Java workout

---
+++ contributions/odys1528/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Using Inheritance to reduce code repetition
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Supply `toString()` in all classes
