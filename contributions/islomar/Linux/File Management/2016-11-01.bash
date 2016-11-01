30b4b7222844d7373d971cd01210b13b379e20f5
islomar <islomar@gmail.com>
2016-11-02 02:05:11 +0300

    2016-11-01's Linux workout

---
+++ contributions/islomar/Linux/File Management/2016-11-01.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Easy way of sharing files
