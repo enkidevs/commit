5ceaf0e257f464c9b6d7cf898fe051e1e6e1bebc
xez7 <xez7@yandex.ru>
2016-11-01 15:35:18 +0300

    2016-10-31's Java workout

---
+++ contributions/xez7/Java/Fundamentals/2016-10-31.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
