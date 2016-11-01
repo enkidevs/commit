27e229aea70aba38e563a709f5a3ce0fa43b017d
bobsmith947 <lkitaev@cherrycreekschools.org>
2016-11-02 00:09:54 +0300

    2016-11-01's Java workout

---
+++ contributions/bobsmith947/Java/Classes & Objects/2016-11-01.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Double Brace Initialization
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
