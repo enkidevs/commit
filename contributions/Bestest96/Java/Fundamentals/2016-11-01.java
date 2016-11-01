1a925b26a7b8cc8767940f0605d593b5f5b85da7
Bestest96 <jaluki@vp.pl>
2016-11-01 19:18:55 +0300

    2016-11-01's Java workout

---
+++ contributions/Bestest96/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Using an interface as a parameter
