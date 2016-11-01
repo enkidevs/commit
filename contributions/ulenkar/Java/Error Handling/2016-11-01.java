d5452da7fb3cff432ecc21b0477dce267ca3a26e
ulenkar <ulka@o2.pl>
2016-11-01 13:59:53 +0300

    2016-11-01's Java workout

---
+++ contributions/ulenkar/Java/Error Handling/2016-11-01.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
