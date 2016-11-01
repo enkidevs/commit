45e720148ef275bc0312339ba4445e589fd9f499
Blitzphantom <charlycurmont@orange.fr>
2016-11-01 11:43:42 +0300

    2016-11-01's Java workout

---
+++ contributions/Blitzphantom/Java/Data Types/2016-11-01.java
@@ -0,0 +1,5 @@
+Use `Arrays.asList()` to initialise lists
+Converting numbers to strings
+Converting a string to upper or lower case
+`StringBuffer` vs `StringBuilder`
+Retrieve the component type of an array
