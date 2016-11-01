4444dbb9a0989957bb61f53d619ac1e7ccc60173
kardudu <kevin.donaldy@gmail.com>
2016-11-01 13:42:39 +0300

    2016-11-01's Linux workout

---
+++ contributions/kardudu/Linux/File Management/2016-11-01.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
