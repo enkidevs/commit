a6bbc676703306f27d1d12c47244eac017d24f37
lindlaaa <lindlaaa@uni.edu>
2016-11-01 19:54:29 +0300

    2016-11-01's Java workout

---
+++ contributions/lindlaaa/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Locks in `static synchronized` methods
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
