5d5115b33defa32760590e0d3d415a00f83fc861
drnanjo <drnanjo@aol.com>
2016-11-01 19:11:25 +0300

    2016-11-01's JavaScript workout

---
+++ contributions/drnanjo/JavaScript/React/2016-11-01.js
@@ -0,0 +1,7 @@
+Use `propTypes` on stateless components
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Using `false` in **JSX**
+Default values for props
