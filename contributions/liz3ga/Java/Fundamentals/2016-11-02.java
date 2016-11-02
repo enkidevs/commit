e498c8e5486aa62401217ceed68c246842c52df8
liz3ga <yann.hn@web.de>
2016-11-02 19:11:44 +0300

    2016-11-02's Java workout

---
+++ contributions/liz3ga/Java/Fundamentals/2016-11-02.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Using an interface as a parameter
