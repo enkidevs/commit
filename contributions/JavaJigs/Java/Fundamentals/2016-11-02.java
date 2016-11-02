c89a6e1674066668499d2222bbab505453f63b58
JavaJigs <gijsleussink@yahoo.com>
2016-11-02 21:45:35 +0300

    2016-11-02's Java workout

---
+++ contributions/JavaJigs/Java/Fundamentals/2016-11-02.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
