d515a90c20cef30a76f467bf7a5c84eee9992907
johnwr <johnwr_enki@wraalsen.no>
2016-11-02 15:24:35 +0300

    2016-11-02's Java workout

---
+++ contributions/johnwr/Java/Fundamentals/2016-11-02.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Difference between `==` and `equals()`
+Difference between JRE and JDK?
+The distinction between overloading and overriding methods
+Always specify type parameter
