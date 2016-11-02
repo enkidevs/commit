04f8584d2a2b3954fe9572935675e2fa2509544e
kariamoss <jehanportable@gmail.com>
2016-11-02 10:49:01 +0300

    2016-11-02's Java workout

---
+++ contributions/kariamoss/Java/Fundamentals/2016-11-02.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between ```HashMap``` and ```Hashtable```
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+How range operations work
