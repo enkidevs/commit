c98248f694c9a6dad5be04e9f93cc7f3868713ba
virusache <r.a.d.u.83@gmail.com>
2016-11-03 01:53:05 +0300

    2016-11-02's Git workout

---
+++ contributions/virusache/Git/Tools/2016-11-02.bash
@@ -0,0 +1,5 @@
+Let's play the blame game
+Cache your authentication details to save time
+Specify a range of commits using double dot syntax
+Remember the results of previous hunk conflicts
+Sharing data by bundling
