46aa9d4acc68323baee8a31aa139a033a1929fa0
raghu <raghu.9208@gmail.com>
2016-11-02 06:55:17 +0300

    2016-11-01's Java workout

---
+++ contributions/raghu/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+`Autoboxing` and `Unboxing`
+Using an interface as a parameter
