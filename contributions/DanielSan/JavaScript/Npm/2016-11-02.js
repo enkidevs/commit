c5cafad7c992c5f8e6e4c97cec8fae4cb2b8f66e
DanielSan <dkoetting@gmx.de>
2016-11-02 08:51:54 +0300

    2016-11-02's JavaScript workout

---
+++ contributions/DanielSan/JavaScript/Npm/2016-11-02.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Manage local node modules with `npm link`
+Environmental variables and flags for `npm`
+`npm` verifies `sha1` hashes of packages
+Tab completion in `npm`
