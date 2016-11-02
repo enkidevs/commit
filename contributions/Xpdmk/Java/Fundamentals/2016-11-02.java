908947271f61c5cf53fe444afa88bca8cae63b2e
Xpdmk <roy.jarvinen@gmail.com>
2016-11-02 17:55:06 +0300

    2016-11-02's Java workout

---
+++ contributions/Xpdmk/Java/Fundamentals/2016-11-02.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Ternary operator
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
