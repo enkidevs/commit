170252f1368b6c80d0ce043261a263d792770505
nporto <portokallidis@gmail.com>
2016-11-02 16:58:18 +0300

    2016-11-02's Linux workout

---
+++ contributions/nporto/Linux/System Management/2016-11-02.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
