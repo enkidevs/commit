1db53cf3fbf5547c3823dab1b15bb74b6290cda8
Cheatmagnet <carlgoegebeur@hotmail.com>
2016-11-02 19:20:40 +0300

    2016-11-02's JavaScript workout

---
+++ contributions/Cheatmagnet/JavaScript/Npm/2016-11-02.js
@@ -0,0 +1,5 @@
+Deprecate npm packages
+Publishing with npm
+Passing through command line arguments in npm scripts
+Manage local node modules with `npm link`
+`npm` verifies `sha1` hashes of packages
