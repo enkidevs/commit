96dc90818559b49ecada33f1470599acb840cc8b
Esprit Orgue <amir.alwash@gmail.com>
2016-11-02 16:11:19 +0300

    2016-11-02's JavaScript workout

---
+++ contributions/Esprit Orgue/JavaScript/Functions/2016-11-02.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Method chaining
+Move cursor at the end of text input
+Social sharing without widgets
+Check if a document is done loading
