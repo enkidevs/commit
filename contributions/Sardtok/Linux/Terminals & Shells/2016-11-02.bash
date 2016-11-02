336c96ba6bb357347e2627aed9716b4720bc2049
Sardtok <sha@computas.com>
2016-11-02 09:14:58 +0300

    2016-11-02's Linux workout

---
+++ contributions/Sardtok/Linux/Terminals & Shells/2016-11-02.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Find any Unix / Linux command
+Useful aliasing in bash
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
