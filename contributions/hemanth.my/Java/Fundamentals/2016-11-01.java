a91c96b579250717b17c3c2c7a99b83cf183e1df
hemanth.my <hemanth.022@gmail.com>
2016-11-02 13:57:35 +0300

    2016-11-01's Java workout

---
+++ contributions/hemanth.my/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
