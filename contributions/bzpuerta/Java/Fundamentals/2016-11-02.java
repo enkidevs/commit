297771b1dfc4bc8502a970c888642cae2c653bef
bzpuerta <beatriz.puerta.hoyas@gmail.com>
2016-11-02 13:36:10 +0300

    2016-11-02's Java workout

---
+++ contributions/bzpuerta/Java/Fundamentals/2016-11-02.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+Ternary operator
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
