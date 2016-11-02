ff86228a6aa3e889b7f808e555cfdf96a89cd36a
Most24 <github@most24.de>
2016-11-03 02:30:34 +0300

    2016-11-02's Java workout

---
+++ contributions/Most24/Java/Fundamentals/2016-11-02.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Using Inheritance to reduce code repetition
+Calling one constructor from another
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
