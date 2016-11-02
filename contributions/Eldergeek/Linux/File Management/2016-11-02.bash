a5562ef9871e2528e2b208b62f248c252bfd007f
Eldergeek <david@geekguytech.com>
2016-11-02 16:57:30 +0300

    2016-11-02's Linux workout

---
+++ contributions/Eldergeek/Linux/File Management/2016-11-02.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
