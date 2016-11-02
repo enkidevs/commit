9ab0963f2c6c3fe7cb7e4adfb279e47a16ddeab8
mitch <mitchoneill@gmail.com>
2016-11-02 10:28:35 +0300

    2016-11-01's Linux workout

---
+++ contributions/mitch/Linux/Package Management/2016-11-01.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Backporting on Debian
+
+List installed packages
+Get `apt` to use a mirror / faster mirror
+Using `Tasksel` for software installation
