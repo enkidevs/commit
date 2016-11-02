641332f039e1edfdb392a42d8658d6547ca12c48
Talkingbaby <kairukenney@gmail.com>
2016-11-02 03:39:56 +0300

    2016-11-01's JavaScript workout

---
+++ contributions/Talkingbaby/JavaScript/Browser APIs/2016-11-01.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Drawing with HTML5 `Canvas` API
+Allow an element to go fullscreen
+Handling click events
+Warn user if **Back** button is pressed
