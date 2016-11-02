a0ea93fbcd95ee77ceac781d6ea3acca98e898ff
Billypopper7 <billypopper7@gmail.com>
2016-11-02 15:20:31 +0300

    2016-11-02's JavaScript workout

---
+++ contributions/Billypopper7/JavaScript/Core/2016-11-02.js
@@ -0,0 +1,5 @@
+Arrays can be used as stack
+`if/else` shortcut `conditional operator`
+Setting default values with `short circuiting`
+Inaccuracy of binary floating-point format
+Round numbers to `N` decimals 
