979f40ae0ae698af40ef629664dbd275d7b89423
jordyamc <jordyamc@hotmail.com>
2016-11-02 17:45:03 +0300

    2016-11-02's Java workout

---
+++ contributions/jordyamc/Java/I/O/2016-11-02.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
