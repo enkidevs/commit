0d28980036471dd1b8de038e2f75a74296795ac8
IldarKamalov <msgto.ildar@gmail.com>
2016-11-02 07:30:33 +0300

    2016-11-02's JavaScript workout

---
+++ contributions/IldarKamalov/JavaScript/Functions/2016-11-02.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Anonymous functions
+Method chaining
+Get query/url variables
