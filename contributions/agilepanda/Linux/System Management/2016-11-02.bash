d257d134ffb3e0e63fd68450efbd4e285357ef03
agilepanda <pantelei@cs.uni-bonn.de>
2016-11-02 08:53:46 +0300

    2016-11-02's Linux workout

---
+++ contributions/agilepanda/Linux/System Management/2016-11-02.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Common log files
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
