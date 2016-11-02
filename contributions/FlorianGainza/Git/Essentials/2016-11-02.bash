e58f09f68bdc052ec2199dc7e9022de32f083ae3
FlorianGainza <florian.gainza.prof@gmail.com>
2016-11-02 15:29:16 +0300

    2016-11-02's Git workout

---
+++ contributions/FlorianGainza/Git/Essentials/2016-11-02.bash
@@ -0,0 +1,5 @@
+How to set your username and email
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
