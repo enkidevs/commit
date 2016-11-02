c3099e5b4366c304bbc3573e9279b68612fc301b
lucaspincerato <lucas.pincerato@hotmail.com>
2016-11-02 21:49:03 +0300

    2016-11-02's JavaScript workout

---
+++ contributions/lucaspincerato/JavaScript/Core/2016-11-02.js
@@ -0,0 +1,5 @@
+Milliseconds since Epoch
+Get a random item from an Array
+Setting default values with `short circuiting`
+Use `splice()` to remove an item from an array
+Using `.test()` with RegExp
