e108458d64726884e340acdbdfda45a41090083d
Edvinas <edvinas.reg@outlook.com>
2016-11-02 11:01:50 +0300

    2016-11-02's JavaScript workout

---
+++ contributions/Edvinas /JavaScript/Core/2016-11-02.js
@@ -0,0 +1,5 @@
+Hoisting applies only to variable declarations, not initializations
+Apply `map` function to array items
+Prototype methods
+How to merge two arrays
+Inaccuracy of binary floating-point format
