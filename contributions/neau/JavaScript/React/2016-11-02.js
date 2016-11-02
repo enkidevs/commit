279fedaa0e356b691f7482c462a2f44e1345dd54
neau <nneauu@gmail.com>
2016-11-02 18:50:47 +0300

    2016-11-02's JavaScript workout

---
+++ contributions/neau/JavaScript/React/2016-11-02.js
@@ -0,0 +1,5 @@
+**React** in-line styles
+Self-closing tags in **JSX**
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
