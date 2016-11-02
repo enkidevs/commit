debd321f80250fb0fbe31c65f8241b976be5b340
bbohac <bastian.bohac@gmail.com>
2016-11-02 11:04:20 +0300

    2016-11-02's Java workout

---
+++ contributions/bbohac/Java/Fundamentals/2016-11-02.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Java naming conventions
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
+Limit Accessibility of `Fields`
