3470850a8ac061ac020901ecd5d48efe20c7f794
Rainsho <djkyux@163.com>
2016-11-02 17:48:23 +0300

    2016-11-01's Java workout

---
+++ contributions/Rainsho/Java/Fundamentals/2016-11-01.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Using an interface as a parameter
