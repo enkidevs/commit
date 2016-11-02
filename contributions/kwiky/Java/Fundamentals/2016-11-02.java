8fed665344fe406d2d8a77b729ba12004f3e600f
kwiky <steve@grosbois.fr>
2016-11-02 10:36:12 +0300

    2016-11-02's Java workout

---
+++ contributions/kwiky/Java/Fundamentals/2016-11-02.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
