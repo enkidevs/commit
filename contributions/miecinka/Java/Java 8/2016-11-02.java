27c26334e9b4fb49a5cb558030ac0b6e9442a7ce
miecinka <mietinhovanish@gmail.com>
2016-11-02 19:05:31 +0300

    2016-11-02's Java workout

---
+++ contributions/miecinka/Java/Java 8/2016-11-02.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Java 8 Lambda expressions
+Use `Optional(s)` type in order to avoid `NullPointerException(s)`
+Avoid numerical overflows with *Java8* `exact` methods.
+`JJS` - Oracle Nashorn, newest JavaScript Engine
