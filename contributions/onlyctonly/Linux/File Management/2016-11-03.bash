3c71270cbb258c8110f533b87a18cad0e0b7b2fd
onlyctonly <onlyctonly@icloud.com>
2016-11-03 08:08:05 +0300

    2016-11-03's Linux workout

---
+++ contributions/onlyctonly/Linux/File Management/2016-11-03.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Delete commands aliases
+Easy way of sharing files
