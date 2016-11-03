cada636c818fd688fd8dba8349491d36ac977cc9
L33tCh <l33tch@gmail.com>
2016-11-03 22:22:57 +0300

    2016-11-03's Java workout

---
+++ contributions/L33tCh/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
