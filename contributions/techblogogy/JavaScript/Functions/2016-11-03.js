054ff129b0e7afe952aedd7411196ccce390c05d
techblogogy <fedorbobylev@gmail.com>
2016-11-03 08:56:14 +0300

    2016-11-03's JavaScript workout

---
+++ contributions/techblogogy/JavaScript/Functions/2016-11-03.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Function call method
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
