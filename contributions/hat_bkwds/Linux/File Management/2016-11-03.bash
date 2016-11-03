f87cd5057c4fafeaefb4faba3ee43c50fe9e54a8
hat_bkwds <phillyeagles60@gmail.com>
2016-11-03 18:55:06 +0300

    2016-11-03's Linux workout

---
+++ contributions/hat_bkwds/Linux/File Management/2016-11-03.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Identify files using the `file` command
