2cf420691649b1f629a0e906d324443c9dbd4fe2
nvignola <vignonik@gmail.com>
2016-11-03 21:51:45 +0300

    2016-11-03's JavaScript workout

---
+++ contributions/nvignola/JavaScript/React/2016-11-03.js
@@ -0,0 +1,7 @@
+Functional Stateless Components in React
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Shallow compare in **React**
+Enhanced Prop Validation 
