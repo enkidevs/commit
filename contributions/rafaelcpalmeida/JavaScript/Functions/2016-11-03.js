1ba526f1aef8f5e6645778a2eb55720f3203462c
rafaelcpalmeida <rafaelcpalmeida@gmail.com>
2016-11-03 13:18:16 +0300

    2016-11-03's JavaScript workout

---
+++ contributions/rafaelcpalmeida/JavaScript/Functions/2016-11-03.js
@@ -0,0 +1,5 @@
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Get query/url variables
+Easily generate a random `HEX` color
+Check if a document is done loading
