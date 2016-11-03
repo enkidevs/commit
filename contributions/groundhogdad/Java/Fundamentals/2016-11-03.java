0699363dc4d9081c5c1cb00d3824acf7d12b6ea1
groundhogdad <groundhogdad@gmail.com>
2016-11-03 11:39:43 +0300

    2016-11-03's Java workout

---
+++ contributions/groundhogdad/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Generating random numbers
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
