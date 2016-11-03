a133e3267eb1e9f1df5fa657d5718bee585fc53a
ellisandy <andrew@andrewryanellis.com>
2016-11-03 17:06:49 +0300

    2016-11-03's Linux workout

---
+++ contributions/ellisandy/Linux/Networking/2016-11-03.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Quick port test with `netcat`
+Limit the `wget` download rate
+Getting the connection speed from the terminal
+Staying connected remotely via SSH without an interactive shell
