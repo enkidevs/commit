8f88f821b6ad10f9bcce90b1ba9fd53805e09701
rcleozier <rcleozier@gmail.com>
2016-11-03 03:09:41 +0300

    2016-11-02's JavaScript workout

---
+++ contributions/rcleozier/JavaScript/Core/2016-11-02.js
@@ -0,0 +1,5 @@
+Types of numbers
+Format currency in JavaScript
+Multi-line string variables
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
