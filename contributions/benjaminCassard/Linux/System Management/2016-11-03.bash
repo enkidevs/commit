2f1dcc57d7e8d6b5d51e2c3e61edc3355094cf9b
benjaminCassard <benjamin.cassard@gmail.com>
2016-11-03 08:50:17 +0300

    2016-11-03's Linux workout

---
+++ contributions/benjaminCassard/Linux/System Management/2016-11-03.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
