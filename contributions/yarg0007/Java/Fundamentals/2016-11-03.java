4215eb7d93bae61e79e9e0784c966afdb8196be3
yarg0007 <benjamin.yarger@gmail.com>
2016-11-03 16:50:29 +0300

    2016-11-03's Java workout

---
+++ contributions/yarg0007/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
