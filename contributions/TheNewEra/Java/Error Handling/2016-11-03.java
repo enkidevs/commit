bf812b4b15be227e9a0da41e4502d29d7e10fcfa
TheNewEra <bastian.gee@gmail.com>
2016-11-03 23:21:55 +0300

    2016-11-03's Java workout

---
+++ contributions/TheNewEra/Java/Error Handling/2016-11-03.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Using exceptions in Java
+Most common reason behind **stack overflow** error
