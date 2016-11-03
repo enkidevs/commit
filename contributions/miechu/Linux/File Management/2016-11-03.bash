54ff0bb76382ddf5812565faf5eea0680d58ddcd
miechu <miechu697@gmail.com>
2016-11-03 14:38:47 +0300

    2016-11-03's Linux workout

---
+++ contributions/miechu/Linux/File Management/2016-11-03.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
