5a4ed174bfb3da411c17d3bbbcc603a655c5c609
lparsons <lance@parsonsclan.com>
2016-11-03 03:49:56 +0300

    2016-11-02's Git workout

---
+++ contributions/lparsons/Git/Tools/2016-11-02.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Diff images using *ExifTool*
+Specify a commit by its ancestry
+Sign your work (using GPG)
+Remember the results of previous hunk conflicts
