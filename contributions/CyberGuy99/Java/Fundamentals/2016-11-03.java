e74330e248fe69fb2af56bbb00e9ace871b8d7b2
CyberGuy99 <rushilcd@gmail.com>
2016-11-04 02:36:40 +0300

    2016-11-03's Java workout

---
+++ contributions/CyberGuy99/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
