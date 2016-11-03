d30f39da3cc1619d5e77f6028ab08d587a631a60
jogli5er <roman.brunner94@gmail.com>
2016-11-03 09:35:05 +0300

    2016-11-03's Java workout

---
+++ contributions/jogli5er/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Using `synchronized` statements
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
