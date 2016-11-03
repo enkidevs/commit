eea47f07ad64c3f001db2571910d224ea2c07773
studenton <studenton.com@gmail.com>
2016-11-03 03:29:38 +0300

    2016-11-02's Java workout

---
+++ contributions/studenton/Java/Fundamentals/2016-11-02.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Java naming conventions
+Common mistake on switch statements
+Ternary operator
+Using Inheritance to reduce code repetition
