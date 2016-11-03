e8882ddf0edf428960457ba94ca2a562df43e7ba
lpgonzalez <lisardo.prieto.gonzalez@gmail.com>
2016-11-03 15:27:37 +0300

    2016-11-03's Java workout

---
+++ contributions/lpgonzalez/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
