11c87435c8935f26f1bbfc949e323bc0f93e928d
TxsAdamWest <twinadanz@gmail.com>
2016-11-03 21:11:41 +0300

    2016-11-03's JavaScript workout

---
+++ contributions/TxsAdamWest/JavaScript/Browser APIs/2016-11-03.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+CSS for when JavaScript is enabled
+Allow an element to go fullscreen
+High Resolution Time API
+FileReader.readAsDataURL()
