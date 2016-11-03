ecdd42bafcc1ae215e71e136e40d851b2c755b1b
celsowo <celsowo@gmail.com>
2016-11-03 14:25:16 +0300

    2016-11-03's Java workout

---
+++ contributions/celsowo/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Using an interface as a parameter
