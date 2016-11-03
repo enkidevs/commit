9f877123378c1759a4ecd26d9885c0135283b2e7
Akhanyatin <nicolas.habak@gmail.com>
2016-11-03 15:47:56 +0300

    2016-11-03's JavaScript workout

---
+++ contributions/Akhanyatin/JavaScript/Node/2016-11-03.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
