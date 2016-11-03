5e830f3a3e4dc69af48cca88e0db11fb88c06689
vikreinok <vikreinok@gmail.com>
2016-11-03 17:45:13 +0300

    2016-11-03's Java workout

---
+++ contributions/vikreinok/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Common mistake on switch statements
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
