74f5a4b0696e1a7765d59e9263591f0fc468e5f8
vallebear <seifer_50@hotmail.com>
2016-11-03 18:14:29 +0300

    2016-11-03's Java workout

---
+++ contributions/vallebear/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
