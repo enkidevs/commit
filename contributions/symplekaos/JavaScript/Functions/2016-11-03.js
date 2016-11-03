d2dc254561156c7cef179ec26f975430bc20c6be
symplekaos <symplekaos@gmail.com>
2016-11-04 02:39:44 +0300

    2016-11-03's JavaScript workout

---
+++ contributions/symplekaos/JavaScript/Functions/2016-11-03.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Anonymous functions
+Changing a functions context with `fn.call(object)`
