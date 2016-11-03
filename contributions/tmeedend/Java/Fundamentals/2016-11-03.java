178da4228136b6ace8eb62d209084b11174f6ac7
tmeedend <tmeedend@gmail.com>
2016-11-04 00:24:48 +0300

    2016-11-03's Java workout

---
+++ contributions/tmeedend/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Locks in `static synchronized` methods
+Avoid using `static` variables
