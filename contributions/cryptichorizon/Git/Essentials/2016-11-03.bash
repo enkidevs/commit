597621f91de705125a5ef64e4cabb8b79c2e5683
cryptichorizon <cryptichorizon@gmail.com>
2016-11-03 21:32:15 +0300

    2016-11-03's Git workout

---
+++ contributions/cryptichorizon/Git/Essentials/2016-11-03.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+What is a checksum?
+Make your log output pretty
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
