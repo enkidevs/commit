a91e695f5c2a7e885c1df048fcdbe4fc1f35bb66
hongsly <hongsly@gmail.com>
2016-11-03 22:38:30 +0300

    2016-11-03's JavaScript workout

---
+++ contributions/hongsly/JavaScript/Core/2016-11-03.js
@@ -0,0 +1,5 @@
+Get a random item from an Array
+Setting default values with `short circuiting`
+Setting the length of an array 
+Double and single quotes
+The difference between null, undefined and NaN
