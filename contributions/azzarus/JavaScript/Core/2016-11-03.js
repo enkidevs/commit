604ae0ff73d6729dfbc2002ba8f7b422ffd70eee
azzarus <npb.lefeuvre@gmail.com>
2016-11-03 19:23:37 +0300

    2016-11-03's JavaScript workout

---
+++ contributions/azzarus/JavaScript/Core/2016-11-03.js
@@ -0,0 +1,5 @@
+Performing a function at timed intervals
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Closures
+Base conversion 
