bca75aa66a931bac1e2dec2a0874a5c8d129bd6a
sogtwinster <sogtwinster@gmail.com>
2016-11-03 17:45:05 +0300

    2016-11-03's Java workout

---
+++ contributions/sogtwinster/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Writing generic methods
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
