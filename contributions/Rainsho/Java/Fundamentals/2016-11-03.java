c32090cf2762c76bb3ca4f84423d682dadae1064
Rainsho <djkyux@163.com>
2016-11-04 06:13:17 +0300

    2016-11-03's Java workout

---
+++ contributions/Rainsho/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
+Using an interface as a parameter
