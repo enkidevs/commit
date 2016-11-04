f3ce5d1a1ac92172f2bff02b9bf6f67277821aab
kkhcheng <kkcheng@gmail.com>
2016-11-04 09:39:46 +0300

    2016-11-03's Java workout

---
+++ contributions/kkhcheng/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
