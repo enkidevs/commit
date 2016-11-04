d00c3fd4a32df9513e64d2e59e9478eb6d91fdc2
hawkeng <hawkins.dom@gmail.com>
2016-11-04 23:18:28 +0300

    2016-11-04's Java workout

---
+++ contributions/hawkeng/Java/Fundamentals/2016-11-04.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
