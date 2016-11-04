e5db3685392f9b6dc3292592c68cbc578235dade
ghstridr <ghstridr@gmail.com>
2016-11-04 21:27:53 +0300

    2016-11-04's Linux workout

---
+++ contributions/ghstridr/Linux/File Management/2016-11-04.bash
@@ -0,0 +1,7 @@
+Finding file with regexes
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
