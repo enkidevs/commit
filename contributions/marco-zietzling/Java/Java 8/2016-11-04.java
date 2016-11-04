8fb51a291f20103091a5fca099908c132a62aa2f
marco-zietzling <marco.zietzling@gmail.com>
2016-11-04 10:52:48 +0300

    2016-11-04's Java workout

---
+++ contributions/marco-zietzling/Java/Java 8/2016-11-04.java
@@ -0,0 +1,5 @@
+Defining `static` methods within an `interface`
+Using `Arrays.parallelSort` 
+How to check for static dependencies of applications and libraries from the command line.
+Control OS `Process`es in `Java8`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
