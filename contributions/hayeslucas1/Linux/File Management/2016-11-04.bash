caa6036022f81a67c11cd0438452eb092245afb3
hayeslucas1 <music.is.mii.life@gmail.com>
2016-11-05 02:36:15 +0300

    2016-11-04's Linux workout

---
+++ contributions/hayeslucas1/Linux/File Management/2016-11-04.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Delete commands aliases
