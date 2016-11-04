6b589fbb65400871c96ad44a9f950d18fca1c77e
vbalko <vbalko@gmail.com>
2016-11-04 12:21:28 +0300

    2016-11-04's Java workout

---
+++ contributions/vbalko/Java/Classes & Objects/2016-11-04.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Use a copy constructor for non-immutable objects
+Using inner classes
+Compare classes and not class names
+String formatting and parsing with `SimpleDateFormat`
