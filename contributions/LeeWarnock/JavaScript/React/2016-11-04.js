a3f1ef195d0189d11d577efae2b4377785e98d31
LeeWarnock <clwarnock@gmail.com>
2016-11-05 00:35:09 +0300

    2016-11-04's JavaScript workout

---
+++ contributions/LeeWarnock/JavaScript/React/2016-11-04.js
@@ -0,0 +1,5 @@
+Self-closing tags in **JSX**
+`null` value for *Controlled Components* in **React**
+**React** `this.props.children`
+Expose Component Functions in **React**
+Dealing with `this.props.children`
