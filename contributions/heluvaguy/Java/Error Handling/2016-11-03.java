cbc5cc636214ee2eab8e4959e38ef7cbf6b49cb6
heluvaguy <adarshjaiswal1989@gmail.com>
2016-11-04 03:56:35 +0300

    2016-11-03's Java workout

---
+++ contributions/heluvaguy/Java/Error Handling/2016-11-03.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Throwing an `exception`
+Checking a double for `NaN` value
