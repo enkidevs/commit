80669c1a659aeb4665c6e251a170bce6e3532e05
drsgi8 <rogowiczdawid@gmail.com>
2016-11-04 09:21:49 +0300

    2016-11-04's Java workout

---
+++ contributions/drsgi8/Java/Fundamentals/2016-11-04.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
