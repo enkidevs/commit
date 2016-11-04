e3c8b477a02cdc3229b46e0dd4cc574f936ec12a
SStalker <raphael@bluepanel.org>
2016-11-04 12:39:04 +0300

    2016-11-04's Linux workout

---
+++ contributions/SStalker/Linux/Security/2016-11-04.bash
@@ -0,0 +1,5 @@
+Security news delivered via DNS
+Extracting the `public key` from the `private key`
+Disable SSH password authentication 
+Best password generation utilities
+Disabling **USB** storage devices
