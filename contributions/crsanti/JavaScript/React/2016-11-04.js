ced3898e0dbc9c1343aadb321d8bd5ede5b0a201
crsanti <santi.camargo.rodriguez@gmail.com>
2016-11-04 16:03:05 +0300

    2016-11-04's JavaScript workout

---
+++ contributions/crsanti/JavaScript/React/2016-11-04.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Clone Elements in **React**
+Immutability helpers in **React**
+Validate for required props
