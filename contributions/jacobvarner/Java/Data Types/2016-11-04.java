d00e5f558d96e8d35c7bcf1c1b8634ca45c2ce54
jacobvarner <jacob.varner14@gmail.com>
2016-11-04 15:41:17 +0300

    2016-11-04's Java workout

---
+++ contributions/jacobvarner/Java/Data Types/2016-11-04.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Multidimensional array declaration
+Converting a string to upper or lower case
+Finding a substring in a string
+Equals operation on different data types
