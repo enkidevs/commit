a599228512918e21d105f5b17ceb740f3f31fd5b
saffad <michal_kolsut@o2.pl>
2016-11-04 09:43:08 +0300

    2016-11-04's Java workout

---
+++ contributions/saffad/Java/Fundamentals/2016-11-04.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Detect or prevent integer overflow
