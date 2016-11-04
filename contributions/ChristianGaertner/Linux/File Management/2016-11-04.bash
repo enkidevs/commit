613e1a4b3bafa4052468c097ddf8a96baadf097c
ChristianGaertner <chrisdagardner@gmail.com>
2016-11-04 13:32:11 +0300

    2016-11-04's Linux workout

---
+++ contributions/ChristianGaertner/Linux/File Management/2016-11-04.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
