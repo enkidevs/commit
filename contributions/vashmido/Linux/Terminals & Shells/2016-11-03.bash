850611cb553d827cfdf3757c70a08407241bb1d4
vashmido <hymura@gmail.com>
2016-11-04 05:25:23 +0300

    2016-11-03's Linux workout

---
+++ contributions/vashmido/Linux/Terminals & Shells/2016-11-03.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Useful aliasing in bash
+Keep useful commands in your shell history with tags
