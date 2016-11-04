b0e38fd031b7f0c40b7d45ac2b1b251a90722fa9
pineoc <pineoc@naver.com>
2016-11-04 03:31:08 +0300

    2016-11-03's JavaScript workout

---
+++ contributions/pineoc/JavaScript/Functions/2016-11-03.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+IIFE pattern
+Function call method
+Changing a functions context with `fn.call(object)`
