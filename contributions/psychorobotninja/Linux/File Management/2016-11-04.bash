d56af56981c56c021aed1af020db7010f6139b30
psychorobotninja <tefuller@gmail.com>
2016-11-05 01:35:37 +0300

    2016-11-04's Linux workout

---
+++ contributions/psychorobotninja/Linux/File Management/2016-11-04.bash
@@ -0,0 +1,7 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Using `touch` to alter files modification time
