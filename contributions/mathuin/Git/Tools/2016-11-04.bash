ebed8f29632540d3346f797b90302d37c9fda954
mathuin <mathuin@gmail.com>
2016-11-04 19:56:49 +0300

    2016-11-04's Git workout

---
+++ contributions/mathuin/Git/Tools/2016-11-04.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Interactively unstage changes
+Specify a commit by its ancestry
+Specify a range of commits using double dot syntax
+Remember the results of previous hunk conflicts
