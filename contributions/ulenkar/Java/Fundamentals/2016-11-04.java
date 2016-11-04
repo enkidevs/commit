99ee0fb51793b0d698140a80cde919a3d42558d9
ulenkar <ulka@o2.pl>
2016-11-04 10:25:59 +0300

    2016-11-04's Java workout

---
+++ contributions/ulenkar/Java/Fundamentals/2016-11-04.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between ```HashMap``` and ```Hashtable```
+Using Inheritance to reduce code repetition
+Detect or prevent integer overflow
+Using an interface as a parameter
