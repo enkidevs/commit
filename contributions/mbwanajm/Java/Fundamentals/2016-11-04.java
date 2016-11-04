9207353dc16ac8731e34aab9e2e27a17f08cda7f
mbwanajm <mbwanajm@gmail.com>
2016-11-04 09:36:42 +0300

    2016-11-04's Java workout

---
+++ contributions/mbwanajm/Java/Fundamentals/2016-11-04.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Using an interface as a parameter
