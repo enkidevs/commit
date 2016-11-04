5e23454a543e95365b168224b53bd87094193a1a
stufixlp <stufixlp@gmail.com>
2016-11-04 10:46:05 +0300

    2016-11-04's Linux workout

---
+++ contributions/stufixlp/Linux/Terminals & Shells/2016-11-04.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Terminal based browser
+The Basics of Environment Variables
