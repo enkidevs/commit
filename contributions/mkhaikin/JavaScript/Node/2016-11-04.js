6b39c01a5cbba8c5a10270296228c8c9cb7a95fa
mkhaikin <michael.khaikin@gmail.com>
2016-11-04 15:55:02 +0300

    2016-11-04's JavaScript workout

---
+++ contributions/mkhaikin/JavaScript/Node/2016-11-04.js
@@ -0,0 +1,5 @@
+**Node** `os` module
+Wrapping errors in Node.js using _node-verror_
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
+`try-catch` only for **sync** code
