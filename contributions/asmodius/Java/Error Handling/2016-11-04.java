ef1be324ee36abccfc3d508e31d7074455d1a5bb
asmodius <daniel.opalla@gmail.com>
2016-11-05 02:31:13 +0300

    2016-11-04's Java workout

---
+++ contributions/asmodius/Java/Error Handling/2016-11-04.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Throwing an `exception`
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
