868b5afca3d8e28144752509d04b54b84cff6d6a
MosanZH <mosan_zh@163.com>
2016-11-04 03:18:05 +0300

    2016-11-03's Java workout

---
+++ contributions/MosanZH/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Supply `toString()` in all classes
