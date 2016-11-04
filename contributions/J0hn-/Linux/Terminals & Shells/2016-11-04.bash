698cf86e8a4da1d011a674feb764456fbe0592e1
J0hn- <jonathan.platteau@yahoo.fr>
2016-11-04 16:31:45 +0300

    2016-11-04's Linux workout

---
+++ contributions/J0hn-/Linux/Terminals & Shells/2016-11-04.bash
@@ -0,0 +1,5 @@
+Terminal based browser
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
