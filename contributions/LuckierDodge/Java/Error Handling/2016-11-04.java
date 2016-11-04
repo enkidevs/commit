7785d3dca37abb577d1ab564be4a658badc9b8bf
LuckierDodge <ryandlewis.rl@gmail.com>
2016-11-04 18:50:49 +0300

    2016-11-04's Java workout

---
+++ contributions/LuckierDodge/Java/Error Handling/2016-11-04.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Throwing an `exception`
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
