f5b506ce965101c0e9cd0a5d2108a0422d840463
blueear <karan.vij@gmail.com>
2016-11-04 19:19:52 +0300

    2016-10-28's Java workout

---
+++ contributions/blueear/Java/Fundamentals/2016-10-28.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
