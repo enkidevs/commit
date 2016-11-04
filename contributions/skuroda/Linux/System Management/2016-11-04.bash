a6effed96031c7032f6c2d2154326089a99bc2c4
skuroda <sk@mieco.org>
2016-11-05 02:22:08 +0300

    2016-11-04's Linux workout

---
+++ contributions/skuroda/Linux/System Management/2016-11-04.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
