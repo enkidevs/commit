58f99e267515b017058b07c3c0ba5da5ece4f4bd
garymc25 <garymc25@gmail.com>
2016-11-04 04:07:39 +0300

    2016-11-03's Java workout

---
+++ contributions/garymc25/Java/I/O/2016-11-03.java
@@ -0,0 +1,5 @@
+How to list all files in a directory that match a filename extension in Java
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
