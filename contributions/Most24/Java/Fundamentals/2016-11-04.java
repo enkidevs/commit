4d9b95aa76205bcdf2ad75ca06c5dfc13842cd07
Most24 <github@most24.de>
2016-11-05 01:55:36 +0300

    2016-11-04's Java workout

---
+++ contributions/Most24/Java/Fundamentals/2016-11-04.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Difference between JRE and JDK?
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Detect or prevent integer overflow
