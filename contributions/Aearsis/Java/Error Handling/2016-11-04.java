b538ad0eb9c62b3999f1366b11107ab71e84c8f4
Aearsis <aearsis@eideo.cz>
2016-11-04 13:50:18 +0300

    2016-11-04's Java workout

---
+++ contributions/Aearsis/Java/Error Handling/2016-11-04.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Creating new Exceptions
+Careful Numeric Data Conversions
