39108e39fe60efe727d2ebafa821ef6f79aed05e
kariamoss <jehanportable@gmail.com>
2016-11-05 02:55:57 +0300

    2016-11-04's Java workout

---
+++ contributions/kariamoss/Java/Fundamentals/2016-11-04.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Generating random numbers
+Use strings in a `switch` statement
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
