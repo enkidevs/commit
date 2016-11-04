80c506028c6290db7820b04b6abc6d3ed278ed00
Vebble <serafin21@gmail.com>
2016-11-04 13:29:24 +0300

    2016-11-04's Java workout

---
+++ contributions/Vebble/Java/Data Types/2016-11-04.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Numeric Conversion - Widening
