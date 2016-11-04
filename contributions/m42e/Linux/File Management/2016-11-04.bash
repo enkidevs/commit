a7378d18152d4fe120200a28fd8acc53ff19606f
m42e <matthias@bilger.info>
2016-11-04 17:29:33 +0300

    2016-11-04's Linux workout

---
+++ contributions/m42e/Linux/File Management/2016-11-04.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
