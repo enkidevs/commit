8b0597585083f602973b16ca619ed67f4ade547f
Heroilde <esteban@stiinc.info>
2016-11-04 23:01:35 +0300

    2016-11-04's Linux workout

---
+++ contributions/Heroilde/Linux/System Management/2016-11-04.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
