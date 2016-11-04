61487b0b748ccfcc5bd3fe6eff67fb1ab2079a95
backend-baron <jakubowskimarcin90@gmail.com>
2016-11-04 10:05:09 +0300

    2016-11-04's Linux workout

---
+++ contributions/backend-baron/Linux/Terminals & Shells/2016-11-04.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Clear bash history
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
