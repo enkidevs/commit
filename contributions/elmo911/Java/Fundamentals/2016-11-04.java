7da716c332b48d427cad7f418f1789d0671a5ec0
elmo911 <jacobmwolfe@gmail.com>
2016-11-04 14:24:19 +0300

    2016-11-04's Java workout

---
+++ contributions/elmo911/Java/Fundamentals/2016-11-04.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
