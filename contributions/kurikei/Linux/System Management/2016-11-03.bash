f6fae51b692477f26cfdfcbcafc8f3e02649b1b3
kurikei <kurikei@gmail.com>
2016-11-04 17:39:14 +0300

    2016-11-03's Linux workout

---
+++ contributions/kurikei/Linux/System Management/2016-11-03.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Basic service management with `systemd`
