13532bb570765425f91107c2e5f0367d6682d93a
ben-wolf <wolfb3@rpi.edu>
2016-11-04 15:03:49 +0300

    2016-11-04's Java workout

---
+++ contributions/ben-wolf/Java/Fundamentals/2016-11-04.java
@@ -0,0 +1,5 @@
+Writing generic methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
