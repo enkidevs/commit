30e1e0a4dc0ec818acbc40ac909cb9c54be28347
Drexoll <darcy@drexollgames.com>
2016-11-04 03:22:50 +0300

    2016-11-03's Java workout

---
+++ contributions/Drexoll/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Metadata: setting a file's owner
+Calling one constructor from another
+Do not perform bitwise and arithmetic operations on the same data
+`Autoboxing` and `Unboxing`
