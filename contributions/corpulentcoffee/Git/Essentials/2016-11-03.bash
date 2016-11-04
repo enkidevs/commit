00143516b00ee506e0eb0dc1b74bee4e671d1672
corpulentcoffee <dave@corpulentcoffee.com>
2016-11-04 05:32:07 +0300

    2016-11-03's Git workout

---
+++ contributions/corpulentcoffee/Git/Essentials/2016-11-03.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to unmodify a modified file
+Using tags for version control
+Use `short` status to make output more compact
+How to write a git commit message
