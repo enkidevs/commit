3a6286337ae5a01af95027fe4b28df22efef52e4
surya-github <suryaprabhakark@gmail.com>
2016-11-04 22:44:31 +0300

    2016-11-03's Linux workout

---
+++ contributions/surya-github/Linux/File Management/2016-11-03.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
