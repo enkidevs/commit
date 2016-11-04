2e4d658fc36cf96794dcacfbe6fe63c39e3a68b5
Drexoll <darcy@drexollgames.com>
2016-11-04 22:01:53 +0300

    2016-11-04's Java workout

---
+++ contributions/Drexoll/Java/Data Types/2016-11-04.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Converting numbers to strings
+Finding a substring in a string
