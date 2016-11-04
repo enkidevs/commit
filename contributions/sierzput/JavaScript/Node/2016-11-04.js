d90b1d959b98fb888d312891a0b8dd028943d74a
sierzput <sierzput@gmail.com>
2016-11-04 07:43:23 +0300

    2016-11-04's JavaScript workout

---
+++ contributions/sierzput/JavaScript/Node/2016-11-04.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Synchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+`uncaughtException` listener in Node.js
+Automatic compilation for Node with **Nodemon** 
