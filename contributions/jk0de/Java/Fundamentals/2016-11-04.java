4a31d24600db958ec6201ff356bef8af4c079870
jk0de <gizmo1398@gmail.com>
2016-11-05 01:03:56 +0300

    2016-11-04's Java workout

---
+++ contributions/jk0de/Java/Fundamentals/2016-11-04.java
@@ -0,0 +1,5 @@
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
