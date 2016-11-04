a79cb9fa845de887496a84f4fff0ef667a4f2b7d
Kermit99 <ryan.lawler86@gmail.com>
2016-11-04 15:02:45 +0300

    2016-11-03's Java workout

---
+++ contributions/Kermit99/Java/Fundamentals/2016-11-03.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
