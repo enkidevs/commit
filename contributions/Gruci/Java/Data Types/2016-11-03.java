d857bc46d4be2967df8a0eeaeb07f224a190c6ba
Gruci <skeksksk@gmail.com>
2016-11-04 18:27:18 +0300

    2016-11-03's Java workout

---
+++ contributions/Gruci/Java/Data Types/2016-11-03.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Converting a string to upper or lower case
+Finding a substring in a string
