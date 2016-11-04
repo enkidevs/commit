3bf3f7461922129d9c5cef0984962e740c0a8379
Script_Coded <script_coded@hotmail.com>
2016-11-04 19:16:34 +0300

    2016-11-04's JavaScript workout

---
+++ contributions/Script_Coded/JavaScript/React/2016-11-04.js
@@ -0,0 +1,5 @@
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+Using `false` in **JSX**
+Dealing with `this.props.children`
+Spread attributes
