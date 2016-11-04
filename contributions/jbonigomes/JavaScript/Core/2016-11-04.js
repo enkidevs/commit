a9a1822fb9e7711ebe37ccacac6e65ea09f5df30
jbonigomes <jbonigomes@yahoo.com.br>
2016-11-04 15:58:58 +0300

    2016-11-04's JavaScript workout

---
+++ contributions/jbonigomes/JavaScript/Core/2016-11-04.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Keywords in hash keys
+Hoisting applies only to variable declarations, not initializations
+Avoid using `with`
+Extra function arguments are undefined by default
