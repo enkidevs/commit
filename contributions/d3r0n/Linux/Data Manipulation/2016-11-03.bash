e5921fa889b0329a31bf69c0a8dfbe21a4790f80
d3r0n <mider2+enki@gmail.com>
2016-11-04 15:02:14 +0300

    2016-11-03's Linux workout

---
+++ contributions/d3r0n/Linux/Data Manipulation/2016-11-03.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+Short intro to `grep`
+How to analyse a server's log files
+Getting rid of duplicate lines
+Count non-empty lines with `nl` utility
