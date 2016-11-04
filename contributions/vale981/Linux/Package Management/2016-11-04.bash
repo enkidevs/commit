45cdb7c4a10dbbee99db5c5b17e6ac05b6e0c140
vale981 <vale981@gmx.de>
2016-11-04 23:08:26 +0300

    2016-11-04's Linux workout

---
+++ contributions/vale981/Linux/Package Management/2016-11-04.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+Removing PPAs
+Get `apt` to use a mirror / faster mirror
+Downgrade a package via `apt-get`
