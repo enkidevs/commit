71a346754d66a0fb6947455294af28fdf83dab67
lmgalan <luismigalan@gmail.com>
2016-11-04 18:47:22 +0300

    2016-11-04's Java workout

---
+++ contributions/lmgalan/Java/Fundamentals/2016-11-04.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Catch multiple exceptions in a single `catch` block
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
