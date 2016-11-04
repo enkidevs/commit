1e358d9cfb056b3ae2c7791f688c5fda26ceb893
Vloorshad <vloorshad@diejoubu.com>
2016-11-04 17:56:50 +0300

    2016-11-04's Linux workout

---
+++ contributions/Vloorshad/Linux/System Management/2016-11-04.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
