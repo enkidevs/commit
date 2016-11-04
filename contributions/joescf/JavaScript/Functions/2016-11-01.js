0d694c96cc188dbe3cf07b8bb4dee3f8acd0d8fb
joescf <joescf@gmail.com>
2016-11-04 18:48:08 +0300

    2016-11-01's JavaScript workout

---
+++ contributions/joescf/JavaScript/Functions/2016-11-01.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Hoisting
+Explicit setting of `this` using `call` and `apply` methods
+Functions can be declared after use
+IIFE pattern
