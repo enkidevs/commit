0924d4040ce6fbb7553a60e11f9d886cb1eaa681
mug4z <tim.frily@gmail.com>
2016-11-04 16:48:04 +0300

    2016-11-04's Linux workout

---
+++ contributions/mug4z/Linux/Terminals & Shells/2016-11-04.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Terminal based browser
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
