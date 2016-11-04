3de00fa4890184d9eff2b1f939a788d260c9d9f3
Sirius207 <oceanus11034@gmail.com>
2016-11-04 14:19:47 +0300

    2016-11-03's JavaScript workout

---
+++ contributions/Sirius207/JavaScript/Functions/2016-11-03.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Function call method
