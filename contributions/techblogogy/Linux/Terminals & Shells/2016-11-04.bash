2faa3d1785709d900b4a0678c276a4aa6f0162a0
techblogogy <fedorbobylev@gmail.com>
2016-11-04 08:52:59 +0300

    2016-11-04's Linux workout

---
+++ contributions/techblogogy/Linux/Terminals & Shells/2016-11-04.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Breaking out of a terminal when `ssh` locks
