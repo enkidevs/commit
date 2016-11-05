87fafe8b7e202c2247facd9421d6c37707ca6390
edm00se <edm00se@gmail.com>
2016-11-06 02:08:44 +0300

    2016-11-05's Java workout

---
+++ contributions/edm00se/Java/I/O/2016-11-05.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Checking for the existence of files and directories
+Listing a file system's root directories
+Deleting files and directories
