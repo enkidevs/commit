0b0d527d4a4d1ea64f8b27e0dc5bbe2e73cf9c2f
reginaexmachina <we.love.rachel.day.lots@gmail.com>
2016-11-05 10:03:10 +0300

    2016-11-05's JavaScript workout

---
+++ contributions/reginaexmachina/JavaScript/Node/2016-11-05.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+The built-in Node debugger
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+Middleware/pipeline design pattern
