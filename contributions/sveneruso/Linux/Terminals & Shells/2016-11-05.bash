290b656878b15e2592b92825f96dae41b4f28525
sveneruso <sveneruso@byte-code.com>
2016-11-05 16:22:19 +0300

    2016-11-05's Linux workout

---
+++ contributions/sveneruso/Linux/Terminals & Shells/2016-11-05.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear bash history
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
