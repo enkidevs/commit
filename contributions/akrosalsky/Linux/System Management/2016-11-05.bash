d691fc6a6b3eeac76700720babbfbf237cc53ccc
akrosalsky <akrosalsky@gmail.com>
2016-11-05 21:49:25 +0300

    2016-11-05's Linux workout

---
+++ contributions/akrosalsky/Linux/System Management/2016-11-05.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
