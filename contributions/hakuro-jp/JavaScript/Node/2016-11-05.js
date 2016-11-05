83da4c4c63d0d814a8ff3edfc697685b77c81731
hakuro-jp <hakuro.jp@gmail.com>
2016-11-05 21:57:28 +0300

    2016-11-05's JavaScript workout

---
+++ contributions/hakuro-jp/JavaScript/Node/2016-11-05.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+The built-in Node debugger
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
