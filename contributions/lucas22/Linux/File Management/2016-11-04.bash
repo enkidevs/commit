570338ec50916ae7d220a337b47c51c53ef75555
lucas22 <lucasparzianello@outlook.com>
2016-11-05 03:13:19 +0300

    2016-11-04's Linux workout

---
+++ contributions/lucas22/Linux/File Management/2016-11-04.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
