a64bccd24c82ce42ffec42b10fc7f7e55944dfb2
Xtrem532 <xtrem532.tg@gmail.com>
2016-11-05 07:45:42 +0300

    2016-11-05's Linux workout

---
+++ contributions/Xtrem532/Linux/System Management/2016-11-05.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
