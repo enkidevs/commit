3502dd02670a9d63a265b4a4b2012a856a15d270
hemanth.my <hemanth.022@gmail.com>
2016-11-05 09:23:10 +0300

    2016-11-04's Java workout

---
+++ contributions/hemanth.my/Java/I/O/2016-11-04.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+How to list all files in a directory that match a filename extension in Java
+Translating `Scanner` tokens into primitive types
+Listing a file system's root directories
