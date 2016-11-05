d5114f287a591337e26d7732b2a1172d62698365
KnooL <sander@lienaerts.nl>
2016-11-05 21:34:53 +0300

    2016-11-05's Java workout

---
+++ contributions/KnooL/Java/Fundamentals/2016-11-05.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Altering format string output by changing a format specifier's `argument_index`
+Do not perform bitwise and arithmetic operations on the same data
