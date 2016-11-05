c3309922bd3778909a7ad4cf7450acd2dd6075d2
osiu97 <osiu97@gmail.com>
2016-11-05 23:08:57 +0300

    2016-11-05's Java workout

---
+++ contributions/osiu97/Java/Fundamentals/2016-11-05.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+The trade-off between short and descriptive names
+Java naming conventions
+Using Inheritance to reduce code repetition
+Using `Printstream.format` to format a string before printing
