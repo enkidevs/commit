86eef68120fe7ceb59085ff728c60a9737978387
alancliddell <alan.c.liddell@gmail.com>
2016-11-05 21:53:28 +0300

    2016-11-05's Linux workout

---
+++ contributions/alancliddell/Linux/File Management/2016-11-05.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
