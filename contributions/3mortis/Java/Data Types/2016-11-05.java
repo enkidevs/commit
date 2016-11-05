b792e795f399d6649b03f8131185893793a6ecf6
3mortis <3mortis@gmail.com>
2016-11-06 01:30:04 +0300

    2016-11-05's Java workout

---
+++ contributions/3mortis/Java/Data Types/2016-11-05.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Collections vs arrays
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
