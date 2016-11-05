a365e3aaef027ec434f43bfbb45319557990df81
asaBee <hlovers.forever@gmail.com>
2016-11-06 02:46:24 +0300

    2016-11-05's Java workout

---
+++ contributions/asaBee/Java/Classes & Objects/2016-11-05.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Template methods in abstract classes
+Utility classes and `static` methods
+How and when to use `WeakHashMap` class
+Fields with Java **Reflection API**
