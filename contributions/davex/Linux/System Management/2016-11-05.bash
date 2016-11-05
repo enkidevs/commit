3cd4ec6c7f7d2ef71b2597f1f664704173ed8a38
davex <dabuck@libero.it>
2016-11-05 12:19:35 +0300

    2016-11-05's Linux workout

---
+++ contributions/davex/Linux/System Management/2016-11-05.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
