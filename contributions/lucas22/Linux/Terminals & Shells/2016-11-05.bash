1702516988d8abbdd18f16728e7357459626118d
lucas22 <lucasparzianello@outlook.com>
2016-11-06 01:32:58 +0300

    2016-11-05's Linux workout

---
+++ contributions/lucas22/Linux/Terminals & Shells/2016-11-05.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Useful aliasing in bash
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
