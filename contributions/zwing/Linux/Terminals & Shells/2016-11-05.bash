7bb8cdf32ad7b9fa04db2b4dc3861d58a3200147
zwing <zwing@tjgeeks.com>
2016-11-06 02:49:54 +0300

    2016-11-05's Linux workout

---
+++ contributions/zwing/Linux/Terminals & Shells/2016-11-05.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
