ec67638b3060d4a9285fac15ed287a259d5653a6
kSom <kgsommer@gmail.com>
2016-11-05 12:01:47 +0300

    2016-11-05's Java workout

---
+++ contributions/kSom/Java/Fundamentals/2016-11-05.java
@@ -0,0 +1,5 @@
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Supply `toString()` in all classes
