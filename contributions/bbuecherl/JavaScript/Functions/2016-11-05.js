a6aa6cfc3d6fb57fb43c77c2dd1f468a63b5fba7
bbuecherl <bernhard.buecherl@gmail.com>
2016-11-06 01:24:08 +0300

    2016-11-05's JavaScript workout

---
+++ contributions/bbuecherl/JavaScript/Functions/2016-11-05.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Easily generate a random `HEX` color
+Move cursor at the end of text input
