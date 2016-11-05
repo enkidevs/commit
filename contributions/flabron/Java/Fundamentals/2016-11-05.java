ec7cdf31ff085376383d6085767eb5ceba9dbb34
flabron <flabron@gmail.com>
2016-11-05 14:25:16 +0300

    2016-11-05's Java workout

---
+++ contributions/flabron/Java/Fundamentals/2016-11-05.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
