9c9046c931b50490a87b1eefb0a7563be2691d32
vetras <vetras@gmail.com>
2016-11-05 15:28:47 +0300

    2016-11-05's Linux workout

---
+++ contributions/vetras/Linux/Terminals & Shells/2016-11-05.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Terminal incognito mode
+Keep useful commands in your shell history with tags
