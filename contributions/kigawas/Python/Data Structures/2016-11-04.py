38f2d12ed0f2ed0c3d3e1a0278f1f62d45d5bfaf
kigawas <to.be.impressive@gmail.com>
2016-11-05 18:00:20 +0300

    2016-11-04's Python workout

---
+++ contributions/kigawas/Python/Data Structures/2016-11-04.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Special attributes of objects and classes
+Get more with `collections`!
+Get the most of `int`s
+There is more to copying
