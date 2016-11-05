42892581458c798411a34fc8316ee2fadd848c14
mrsteakhouse <mrsteakhouse1@googlemail.com>
2016-11-05 16:24:55 +0300

    2016-11-05's Linux workout

---
+++ contributions/mrsteakhouse/Linux/Networking/2016-11-05.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Get real network statistics with `slurm`
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
