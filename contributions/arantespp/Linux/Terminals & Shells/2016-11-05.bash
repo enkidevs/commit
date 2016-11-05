4fe7dee79a054a93ff82762b1ba222c354a105b7
arantespp <arantespp@gmail.com>
2016-11-05 16:35:13 +0300

    2016-11-05's Linux workout

---
+++ contributions/arantespp/Linux/Terminals & Shells/2016-11-05.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal incognito mode
+Adding directories to your `$PATH`
+Retrieve previous arguments
