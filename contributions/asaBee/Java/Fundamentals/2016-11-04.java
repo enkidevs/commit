138c5bdef58c8a4554a7583fd056c380d8f12ef9
asaBee <hlovers.forever@gmail.com>
2016-11-05 03:48:49 +0300

    2016-11-04's Java workout

---
+++ contributions/asaBee/Java/Fundamentals/2016-11-04.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
