e81dd459ab59b6155c62b32f06d68994c73177df
hexin <hexin1989@gmail.com>
2016-11-05 21:13:06 +0300

    2016-11-05's Java workout

---
+++ contributions/hexin/Java/Fundamentals/2016-11-05.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
