5efc1584b9b57beed6729167b7c9c7a2bbf6f067
pseguel <pseguel@cisco.com>
2016-11-06 01:27:10 +0300

    2016-11-05's Linux workout

---
+++ contributions/pseguel/Linux/System Management/2016-11-05.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Using `dd` To Create Disk Images
