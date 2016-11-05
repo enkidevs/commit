1610a3eaa1b4c21eeb79292764b08d9e92653f18
Zanex <alessio.zanella88@gmail.com>
2016-11-05 14:29:00 +0300

    2016-11-05's Linux workout

---
+++ contributions/Zanex/Linux/File Management/2016-11-05.bash
@@ -0,0 +1,5 @@
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
+Delete commands aliases
