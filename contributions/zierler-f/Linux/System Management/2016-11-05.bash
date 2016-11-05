3ccc1bddae6cba5da29d2e9bd27b20df330a4289
zierler-f <zierler25@gmail.com>
2016-11-05 20:35:54 +0300

    2016-11-05's Linux workout

---
+++ contributions/zierler-f/Linux/System Management/2016-11-05.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Finding Open Files With `lsof`
