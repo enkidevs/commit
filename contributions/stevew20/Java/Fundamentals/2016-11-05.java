12150e69f46ced3b0d3c298e4f6749618a05e347
stevew20 <stevewells20@gmail.com>
2016-11-06 01:14:05 +0300

    2016-11-05's Java workout

---
+++ contributions/stevew20/Java/Fundamentals/2016-11-05.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Using Inheritance to reduce code repetition
+`Autoboxing` and `Unboxing`
+Avoid using `static` variables
