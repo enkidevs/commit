88983a8bed93d53fb231fb82ad02d6293a7412ff
christopherb06 <christopherb06@gmail.com>
2016-11-05 19:08:37 +0300

    2016-11-05's Java workout

---
+++ contributions/christopherb06/Java/Fundamentals/2016-11-05.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
