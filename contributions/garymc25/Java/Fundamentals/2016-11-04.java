162aa7f67df5a548daa9f733107d8e24a2c8d8ee
garymc25 <garymc25@gmail.com>
2016-11-05 04:11:26 +0300

    2016-11-04's Java workout

---
+++ contributions/garymc25/Java/Fundamentals/2016-11-04.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
