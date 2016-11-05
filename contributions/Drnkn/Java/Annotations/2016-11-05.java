4a1d6898e1cf2ff07557f4a503a3288dcdf93d36
Drnkn <prehistoric2003@gmail.com>
2016-11-05 09:50:38 +0300

    2016-11-05's Java workout

---
+++ contributions/Drnkn/Java/Annotations/2016-11-05.java
@@ -0,0 +1,5 @@
+`@Test` parameters
+Use `@Ignore` to disable JUnit tests
+Suppressing warnings using `@SuppressWarnings`
+Including annotations in Javadoc
+Restricting an annotation's application by using `@Target`
