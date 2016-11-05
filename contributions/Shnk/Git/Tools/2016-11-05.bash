63c69d6b38ae9e273d5a179bb56b485491523ce2
Shnk <wavelet.alexandre@gmail.com>
2016-11-05 11:48:55 +0300

    2016-11-05's Git workout

---
+++ contributions/Shnk/Git/Tools/2016-11-05.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Useful stashing options
+Create a new branch from a stash
+Sign your work (using GPG)
+Remember the results of previous hunk conflicts
