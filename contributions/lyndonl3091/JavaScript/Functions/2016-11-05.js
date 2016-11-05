bda054721df72e977ba74aca7dadd750ae8c7069
lyndonl3091 <lyndonl3091@gmail.com>
2016-11-06 02:34:05 +0300

    2016-11-05's JavaScript workout

---
+++ contributions/lyndonl3091/JavaScript/Functions/2016-11-05.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+`.bind()` 
+Method chaining
