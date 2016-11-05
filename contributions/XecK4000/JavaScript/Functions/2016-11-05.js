1aeea7d2c22f0a36046cad2e0b31ed924a0b9a9e
XecK4000 <laurent.postic@hotmail.fr>
2016-11-05 12:05:00 +0300

    2016-11-05's JavaScript workout

---
+++ contributions/XecK4000/JavaScript/Functions/2016-11-05.js
@@ -0,0 +1,5 @@
+Hoisting
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Get query/url variables
+Check if a document is done loading
