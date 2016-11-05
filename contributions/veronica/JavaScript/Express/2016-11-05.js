5168d00834db71d8428879ddfa9c319a3038f8fd
veronica <me@veronicacannon.com>
2016-11-05 19:11:16 +0300

    2016-11-05's JavaScript workout

---
+++ contributions/veronica/JavaScript/Express/2016-11-05.js
@@ -0,0 +1,5 @@
+Easy way to deliver **HTML** pages with **Express**
+**GZIP** compression for **Express**
+`body-parser` in **Express**
+Log with `morgan` in **Express**
+Error handler in **Express**
