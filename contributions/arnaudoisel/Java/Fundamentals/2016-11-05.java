59c2c58998f211c0205ff3f274ea4d63bf3f9fa3
arnaudoisel <arnaud.oisel@gmail.com>
2016-11-05 19:34:11 +0300

    2016-11-05's Java workout

---
+++ contributions/arnaudoisel/Java/Fundamentals/2016-11-05.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
