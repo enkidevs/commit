bf68a8481d2076625d41574080c80540fa17aae2
inkwaterman <inkwaterman@gmail.com>
2016-11-05 12:11:40 +0300

    2016-11-05's Linux workout

---
+++ contributions/inkwaterman/Linux/System Management/2016-11-05.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Finding Open Files With `lsof`
