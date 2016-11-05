fa6438b0403fa0b5394e6b0604db77c491dba7c3
archligt <frodo.archligt@gmail.com>
2016-11-05 17:52:17 +0300

    2016-11-05's Java workout

---
+++ contributions/archligt/Java/Data Types/2016-11-05.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Converting numbers to strings
+Converting a string to upper or lower case
+Retrieve the component type of an array
