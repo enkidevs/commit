3e11684dfa61c6c9e995ecccf694b94a5c441437
fdavidcl <fdavidcl@protonmail.com>
2016-11-05 23:09:32 +0300

    2016-11-05's Linux workout

---
+++ contributions/fdavidcl/Linux/Terminals & Shells/2016-11-05.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Find any Unix / Linux command
+Retrieve previous arguments
