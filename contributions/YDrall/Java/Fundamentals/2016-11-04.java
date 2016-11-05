580adc8fdceaae7ee6ea024226567fb46a69bde8
YDrall <yogeshdrall7874@gmail.com>
2016-11-05 07:56:51 +0300

    2016-11-04's Java workout

---
+++ contributions/YDrall/Java/Fundamentals/2016-11-04.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
