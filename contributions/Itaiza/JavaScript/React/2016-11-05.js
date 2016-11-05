d521bedb6bd5021be61d97117b119bac2228ee19
Itaiza <itai.za@gmail.com>
2016-11-05 17:02:50 +0300

    2016-11-05's JavaScript workout

---
+++ contributions/Itaiza/JavaScript/React/2016-11-05.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+`PureRenderMixin` in **React**
+Keyed fragments in **React**
+Validate for required props
