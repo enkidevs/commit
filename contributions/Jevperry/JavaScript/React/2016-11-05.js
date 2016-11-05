e954d8a8866745925960bbb29c63786065e89abb
Jevperry <jvegaperry@gmail.com>
2016-11-05 16:37:57 +0300

    2016-11-05's JavaScript workout

---
+++ contributions/Jevperry/JavaScript/React/2016-11-05.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+Pixel values in style props for **React**
+componentWillReceiveProps Not Triggered After Mounting
+
+Using `false` in **JSX**
+Shortcut for transferring props
