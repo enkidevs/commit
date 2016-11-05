be5548e58a2b14e2fc72c4859b77313e21b0bd16
Zhigalin <alexander@zhigalin.tk>
2016-11-05 20:57:26 +0300

    2016-11-05's Java workout

---
+++ contributions/Zhigalin/Java/Fundamentals/2016-11-05.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Always specify type parameter
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+`Autoboxing` and `Unboxing`
