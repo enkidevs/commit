1e1bcb92e71b7dd8ec4cd7116c1da0bb78c4d963
hugo-andrade <hugo.slip@gmail.com>
2016-11-05 06:06:25 +0300

    2016-11-04's Linux workout

---
+++ contributions/hugo-andrade/Linux/System Management/2016-11-04.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
