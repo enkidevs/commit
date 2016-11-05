164c80c4d340aad0248e06ecf1007f1262764b80
rblakejohnson <rblakejohnson@gmail.com>
2016-11-05 09:35:12 +0300

    2016-11-04's JavaScript workout

---
+++ contributions/rblakejohnson/JavaScript/Functions/2016-11-04.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+`.bind()` 
+Social sharing without widgets
+Check if a document is done loading
