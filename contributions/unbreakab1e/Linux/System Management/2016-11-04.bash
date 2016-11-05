989dc02e8d0a67969a857c5e370abd7bb7fcad7b
unbreakab1e <ikukharchuk@gmail.com>
2016-11-05 05:15:06 +0300

    2016-11-04's Linux workout

---
+++ contributions/unbreakab1e/Linux/System Management/2016-11-04.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
