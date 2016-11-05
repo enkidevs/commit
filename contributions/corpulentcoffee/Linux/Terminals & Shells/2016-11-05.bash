14a06ecee91e45b4875392f7838316284b27c45b
corpulentcoffee <dave@corpulentcoffee.com>
2016-11-05 16:54:33 +0300

    2016-11-05's Linux workout

---
+++ contributions/corpulentcoffee/Linux/Terminals & Shells/2016-11-05.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear the terminal instantly
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
