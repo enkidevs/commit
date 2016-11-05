ae850ff15ba9809c5ad52a4e59fe6aa96f72bfae
hanninen <aki@hanninen.net>
2016-11-05 09:15:07 +0300

    2016-11-05's Linux workout

---
+++ contributions/hanninen/Linux/File Management/2016-11-05.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Deleting files in a secure manner
