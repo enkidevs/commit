88e1c189ac6606bdc2ca822e9c46792add063c5a
GPFAFF <greg.pfaff@razorfish.com>
2016-11-05 21:47:05 +0300

    2016-11-05's JavaScript workout

---
+++ contributions/GPFAFF/JavaScript/React/2016-11-05.js
@@ -0,0 +1,5 @@
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+**React** `this.props.children`
+Expose Component Functions in **React**
+Dealing with `this.props.children`
