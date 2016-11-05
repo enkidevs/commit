bbafcb3d65916a771102b0f44e1a35b3a645f0bd
MichaelRijsdijk <michaelrijsdijk@gmail.com>
2016-11-05 11:23:34 +0300

    2016-11-05's Java workout

---
+++ contributions/MichaelRijsdijk/Java/Annotations/2016-11-05.java
@@ -0,0 +1,5 @@
+Use `@Ignore` to disable JUnit tests
+Suppressing warnings using `@SuppressWarnings`
+Including annotations in Javadoc
+Restricting an annotation's application by using `@Target`
+Declaring `@Repeatable` annotation types
