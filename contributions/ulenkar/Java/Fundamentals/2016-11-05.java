c42bc132e88aaf78fd7dc08a742673c26f9f65c2
ulenkar <ulka@o2.pl>
2016-11-06 00:25:06 +0300

    2016-11-05's Java workout

---
+++ contributions/ulenkar/Java/Fundamentals/2016-11-05.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
