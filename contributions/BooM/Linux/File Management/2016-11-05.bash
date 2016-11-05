05b208d3a41e1beb4ff44363879ccee978b8468e
BooM <tararuj.michal@gmail.com>
2016-11-05 22:00:34 +0300

    2016-11-05's Linux workout

---
+++ contributions/BooM/Linux/File Management/2016-11-05.bash
@@ -0,0 +1,5 @@
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Deleting non-empty directories
+Working with `.gz` files
+Easy way of sharing files
