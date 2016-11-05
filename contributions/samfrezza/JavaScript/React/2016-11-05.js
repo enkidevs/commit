8e8f339e0af4620c967d3ed82f8837ac87958157
samfrezza <samfrezza@gmail.com>
2016-11-05 12:22:56 +0300

    2016-11-05's JavaScript workout

---
+++ contributions/samfrezza/JavaScript/React/2016-11-05.js
@@ -0,0 +1,5 @@
+Self-closing tags in **JSX**
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+Expose Component Functions in **React**
+Spread attributes
