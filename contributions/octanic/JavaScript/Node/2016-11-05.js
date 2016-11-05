307990bda8ad8951aebb21fe41a932618d1b2b81
octanic <mskdmuchacho@gmail.com>
2016-11-05 08:47:05 +0300

    2016-11-05's JavaScript workout

---
+++ contributions/octanic/JavaScript/Node/2016-11-05.js
@@ -0,0 +1,5 @@
+REPL in Node.js
+Asynchronous File Write/Read in Node.js
+First-error callbacks in **Node**
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
