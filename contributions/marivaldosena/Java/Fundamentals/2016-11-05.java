a7dba05ab7cd2bec7058b31983eed03af0cdbeca
marivaldosena <marivaldo.sena@live.com>
2016-11-05 14:54:23 +0300

    2016-11-05's Java workout

---
+++ contributions/marivaldosena/Java/Fundamentals/2016-11-05.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Java naming conventions
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
+Avoid using `static` variables
