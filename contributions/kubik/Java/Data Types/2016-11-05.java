45432b388af056d24ac2c7c980404fcd4aefcf37
kubik <lkubic@poczta.onet.pl>
2016-11-05 18:28:02 +0300

    2016-11-05's Java workout

---
+++ contributions/kubik/Java/Data Types/2016-11-05.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Do not attempt comparisons with NaN
+Numeric Conversion - Widening
