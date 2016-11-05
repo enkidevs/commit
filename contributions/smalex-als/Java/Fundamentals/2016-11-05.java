51995284dab7665ab5fa60719b7842ee069e4fad
smalex-als <smalex69@gmail.com>
2016-11-05 13:02:42 +0300

    2016-11-05's Java workout

---
+++ contributions/smalex-als/Java/Fundamentals/2016-11-05.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
