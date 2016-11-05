082a1193fa325bcb010fb1cbe93b715ec04362f0
beanaroo <mike.bean.heyns@gmail.com>
2016-11-05 13:24:34 +0300

    2016-11-04's Linux workout

---
+++ contributions/beanaroo/Linux/File Management/2016-11-04.bash
@@ -0,0 +1,7 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
