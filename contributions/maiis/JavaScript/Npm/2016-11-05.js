fb7ba62b2170571ff61643351a3419bfa8a6040e
maiis <evuigner@gmail.com>
2016-11-05 11:16:20 +0300

    2016-11-05's JavaScript workout

---
+++ contributions/maiis/JavaScript/Npm/2016-11-05.js
@@ -0,0 +1,5 @@
+Manage local node modules with `npm link`
+Environmental variables and flags for `npm`
+`npm` verifies `sha1` hashes of packages
+`config` object in `package.json`
+Reduce package duplication 
