15889818177395b5a892a5ec8dc8949ba07cd064
FlorianGainza <florian.gainza.prof@gmail.com>
2016-11-05 11:42:09 +0300

    2016-11-05's Linux workout

---
+++ contributions/FlorianGainza/Linux/System Management/2016-11-05.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Basic service management with `systemd`
