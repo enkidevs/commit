fdbc9bb8b2b08cfc9b7e16e52064d1ddaea316b8
pro230 <gt.szgyula@gmail.com>
2016-11-05 12:17:09 +0300

    2016-11-05's Java workout

---
+++ contributions/pro230/Java/Error Handling/2016-11-05.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Prevent `exceptions` while logging Data
+Throwing an `exception`
+Checking a double for `NaN` value
