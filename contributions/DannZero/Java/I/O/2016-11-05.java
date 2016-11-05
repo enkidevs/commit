c62120d03a7ae5861bd22486cb1c425d9bb45ecf
DannZero <dannzero@gmail.com>
2016-11-05 16:20:31 +0300

    2016-11-05's Java workout

---
+++ contributions/DannZero/Java/I/O/2016-11-05.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
