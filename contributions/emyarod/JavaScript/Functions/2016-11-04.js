4bac28d35ef285bda64b6d99b978eece8cb4fd11
emyarod <allllaboutyou@gmail.com>
2016-11-05 12:10:33 +0300

    2016-11-04's JavaScript workout

---
+++ contributions/emyarod/JavaScript/Functions/2016-11-04.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Anonymous functions
+Get query/url variables
