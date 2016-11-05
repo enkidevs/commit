321e123858e42f9a32e38bdad6257b307be24321
Jilano <mobile.axel@gmail.com>
2016-11-05 10:42:38 +0300

    2016-11-05's Linux workout

---
+++ contributions/Jilano/Linux/Terminals & Shells/2016-11-05.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Get to know  your commands with `type`
+The Basics of Environment Variables
