a8b343ecf4c260ea275516d23d4d136ebeb2bb44
aeolyus <richardhuang.huang@gmail.com>
2016-11-06 01:38:45 +0300

    2016-11-05's Linux workout

---
+++ contributions/aeolyus/Linux/Terminals & Shells/2016-11-05.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear bash history
+Retrieve previous arguments
+The Basics of Environment Variables
