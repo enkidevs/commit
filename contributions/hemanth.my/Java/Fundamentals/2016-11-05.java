3b4079d9f395b8467ecec26e3fc4af47c314870d
hemanth.my <hemanth.022@gmail.com>
2016-11-06 09:25:24 +0300

    2016-11-05's Java workout

---
+++ contributions/hemanth.my/Java/Fundamentals/2016-11-05.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
