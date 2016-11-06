1db0318ff6458eb8fbd12103caf7892728177b9c
jlgarciaap <jlgarciaap@gmail.com>
2016-11-06 16:49:28 +0300

    2016-11-06's Linux workout

---
+++ contributions/jlgarciaap/Linux/Terminals & Shells/2016-11-06.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Terminal based browser
+Adding directories to your `$PATH`
+Retrieve previous arguments
