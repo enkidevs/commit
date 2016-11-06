bc2d2856702b6851191cf7c6b80d6b0425d08deb
christopherb06 <christopherb06@gmail.com>
2016-11-06 20:13:01 +0300

    2016-11-06's Java workout

---
+++ contributions/christopherb06/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
