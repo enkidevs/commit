9497dde8a2813df407d305207903993fb66aac73
DarkGenius <darkgenius@nextmail.ru>
2016-11-06 18:19:00 +0300

    2016-11-06's Java workout

---
+++ contributions/DarkGenius/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Constant notation
+Always specify type parameter
+`Autoboxing` and `Unboxing`
+Using an interface as a parameter
