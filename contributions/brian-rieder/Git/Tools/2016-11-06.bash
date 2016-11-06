0808227a77ede00f52e09eb901908cccc418e8b2
brian-rieder <brieder@purdue.edu>
2016-11-06 21:14:15 +0300

    2016-11-06's Git workout

---
+++ contributions/brian-rieder/Git/Tools/2016-11-06.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Specify a commit by its ancestry
+Specify a range of commits using double dot syntax
+Create a new branch from a stash
+Debug using binary search
