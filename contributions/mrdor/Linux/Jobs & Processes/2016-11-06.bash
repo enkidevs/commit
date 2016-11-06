3bea9c203ca4e9794d36d477d495bd67a37330c1
mrdor <mrdor44@gmail.com>
2016-11-06 11:21:01 +0300

    2016-11-06's Linux workout

---
+++ contributions/mrdor/Linux/Jobs & Processes/2016-11-06.bash
@@ -0,0 +1,5 @@
+Keeping processes running after you log out
+`Cron` dot-in-filename issues when using `run-parts`
+Common issue with scripts in `cron`
+Set a process priority
+How to pause a process and run others in the background
