ea69f2883845c9d177a84d58cc0535d194263a30
CyberGuy99 <rushilcd@gmail.com>
2016-11-06 18:48:56 +0300

    2016-11-06's Java workout

---
+++ contributions/CyberGuy99/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
