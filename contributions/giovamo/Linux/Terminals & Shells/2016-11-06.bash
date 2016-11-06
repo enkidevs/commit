0affc698068deea687116c5a46dd469231cae6b0
giovamo <marcogiova77@gmail.com>
2016-11-06 17:53:51 +0300

    2016-11-06's Linux workout

---
+++ contributions/giovamo/Linux/Terminals & Shells/2016-11-06.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Aliasing ssh connections
+Terminal incognito mode
+Keep useful commands in your shell history with tags
