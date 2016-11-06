4f8b2f81161f794a2858e9e0af330e237bd54480
stando <stano.kudlac@gmail.com>
2016-11-06 11:20:59 +0300

    2016-11-06's Linux workout

---
+++ contributions/stando/Linux/File Management/2016-11-06.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
