c5b03c5743743453645a2ce2ff14ee5242494583
tnargwoxow <tnargwoxow@gmail.com>
2016-11-06 23:37:10 +0300

    2016-11-06's Linux workout

---
+++ contributions/tnargwoxow/Linux/System Management/2016-11-06.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+System uptime
