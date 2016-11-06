80a97a1dc710a4dda7df03f8b3c2863307e8e059
yasushi <yasushi.ohtsuka@gmail.com>
2016-11-07 02:08:39 +0300

    2016-11-06's Linux workout

---
+++ contributions/yasushi/Linux/Terminals & Shells/2016-11-06.bash
@@ -0,0 +1,5 @@
+Clear bash history
+Quick `bash` shortcuts 
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
