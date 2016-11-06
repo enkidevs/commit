198f7e16f4eb844a9c33bc302c3b67f26c494721
jvlobo <jvlg2002@hotmail.com>
2016-11-06 14:25:07 +0300

    2016-11-06's JavaScript workout

---
+++ contributions/jvlobo/JavaScript/Core/2016-11-06.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Hoisting applies only to variable declarations, not initializations
+Using `eval`
+Detect **DO NOT TRACK** status
+Getting the *real* dimensions of an image
