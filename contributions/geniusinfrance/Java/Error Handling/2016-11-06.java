fd51ee179215cde764b232fe251e1af157700d62
geniusinfrance <ryan.bergerson@gmail.com>
2016-11-06 11:20:52 +0300

    2016-11-06's Java workout

---
+++ contributions/geniusinfrance/Java/Error Handling/2016-11-06.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Creating new Exceptions
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
