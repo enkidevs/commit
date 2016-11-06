0e56fc50ba57d3d7f9ab28a9c1fa01eb5c90f85c
4N7HR4X <anthrax.f@gmail.com>
2016-11-06 21:18:25 +0300

    2016-11-06's Linux workout

---
+++ contributions/4N7HR4X/Linux/System Management/2016-11-06.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
