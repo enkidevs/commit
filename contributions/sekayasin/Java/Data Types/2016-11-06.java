880ecd87af26a5c8c84a7b476fa95f8c09a47d71
sekayasin <sekayasin@gmail.com>
2016-11-07 00:32:06 +0300

    2016-11-06's Java workout

---
+++ contributions/sekayasin/Java/Data Types/2016-11-06.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use `Arrays.asList()` to initialise lists
+Converting numbers to strings
+Converting a string to upper or lower case
+Equals operation on different data types
