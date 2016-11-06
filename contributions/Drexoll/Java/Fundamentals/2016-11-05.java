5cf234b3bfffaf9e3e9aefc531a321ce6ec7b953
Drexoll <darcy@drexollgames.com>
2016-11-06 03:54:36 +0300

    2016-11-05's Java workout

---
+++ contributions/Drexoll/Java/Fundamentals/2016-11-05.java
@@ -0,0 +1,5 @@
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+`Autoboxing` and `Unboxing`
