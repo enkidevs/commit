8971b142b86f44537aa06653edcc7fb200f7e3c2
lordmalvern <aooogah@gmail.com>
2016-11-06 18:37:34 +0300

    2016-11-06's JavaScript workout

---
+++ contributions/lordmalvern/JavaScript/Node/2016-11-06.js
@@ -0,0 +1,5 @@
+First-error callbacks in **Node**
+Use conventional property names for error handling in Node.js
+`uncaughtException` listener in Node.js
+`exports` vs. `module.exports` in **Node**
+`try-catch` only for **sync** code
