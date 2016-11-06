0824a587882c845faa08f12ecf619013483fc69c
marco-zietzling <marco.zietzling@gmail.com>
2016-11-06 13:48:06 +0300

    2016-11-06's Java workout

---
+++ contributions/marco-zietzling/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
