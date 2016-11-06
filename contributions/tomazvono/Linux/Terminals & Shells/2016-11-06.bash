124ab1949f4a7de24121eab2be8f0adca5f9302f
tomazvono <tomazvono@gmail.com>
2016-11-06 20:49:43 +0300

    2016-11-06's Linux workout

---
+++ contributions/tomazvono/Linux/Terminals & Shells/2016-11-06.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
