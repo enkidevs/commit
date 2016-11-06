259b79b165ca49372d34b72cc372a5a19cd6a5de
Akagi <stringtheory@gmx.net>
2016-11-06 12:48:19 +0300

    2016-11-06's Linux workout

---
+++ contributions/Akagi/Linux/Terminals & Shells/2016-11-06.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Random password generator
+Breaking out of a terminal when `ssh` locks
