b092b1b43fd65df9b1d532a95f6449ac934b2d7d
anitakar <akarbow@gmail.com>
2016-11-06 17:48:06 +0300

    2016-11-06's Linux workout

---
+++ contributions/anitakar/Linux/System Management/2016-11-06.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Finding Open Files With `lsof`
