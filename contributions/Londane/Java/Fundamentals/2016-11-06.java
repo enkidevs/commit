f521425aef1c09f058f4c0791823d29ee0501a7f
Londane <3m4il2m3@gmail.com>
2016-11-06 11:12:07 +0300

    2016-11-06's Java workout

---
+++ contributions/Londane/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
