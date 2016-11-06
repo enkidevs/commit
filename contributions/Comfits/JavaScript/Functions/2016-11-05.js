1b728bbd1a6097a492c83af55717307493db5d24
Comfits <hoanganhtuan.ou@gmail.com>
2016-11-06 13:39:01 +0300

    2016-11-05's JavaScript workout

---
+++ contributions/Comfits/JavaScript/Functions/2016-11-05.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+`.bind()` 
+Anonymous functions
+Function call method
