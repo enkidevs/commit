d34fe0c61bfc981ece6e1b5a7a4c53d276b34c38
tomisawk <tomisawk@gmail.com>
2016-11-06 17:59:11 +0300

    2016-11-05's JavaScript workout

---
+++ contributions/tomisawk/JavaScript/React/2016-11-05.js
@@ -0,0 +1,6 @@
+Pixel values in style props for **React**
+`if`-`else` statements in **JSX** and **React**
+Type of the Children props
+componentWillReceiveProps Not Triggered After Mounting
+
+**React** `this.props.children`
