668d0bd7572f10fccd206a430e5724b9de3057a0
danielvoicu <danielvoicu87@gmail.com>
2016-11-06 12:05:52 +0300

    2016-11-06's Linux workout

---
+++ contributions/danielvoicu/Linux/File Management/2016-11-06.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Easy way of sharing files
