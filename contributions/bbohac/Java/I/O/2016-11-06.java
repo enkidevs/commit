1031ba546b5bd32efd30b2595199f0149fdd21eb
bbohac <bastian.bohac@gmail.com>
2016-11-06 14:56:11 +0300

    2016-11-06's Java workout

---
+++ contributions/bbohac/Java/I/O/2016-11-06.java
@@ -0,0 +1,5 @@
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+Listing a file system's root directories
+Deleting files and directories
