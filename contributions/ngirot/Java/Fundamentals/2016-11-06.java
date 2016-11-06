98ee0590e313ee16bf1dc07861120bca0d620d79
ngirot <ngirot@hotmail.com>
2016-11-06 16:08:48 +0300

    2016-11-06's Java workout

---
+++ contributions/ngirot/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
