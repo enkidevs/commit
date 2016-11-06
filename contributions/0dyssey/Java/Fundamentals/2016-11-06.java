7485f1b0fdbab05b4fea2d25a8a3eb9e358e1211
0dyssey <phillipswesley@gmail.com>
2016-11-06 19:17:43 +0300

    2016-11-06's Java workout

---
+++ contributions/0dyssey/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Using `Printstream.format` to format a string before printing
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
