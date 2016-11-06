0e10ad4263c21d4731ab2a24447278835f166dbc
Klemele <lemele.kevin@gmail.com>
2016-11-06 19:59:04 +0300

    2016-11-06's JavaScript workout

---
+++ contributions/Klemele/JavaScript/Functions/2016-11-06.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function constructor vs. function declaration vs. function expression
+Anonymous functions
+Easily generate a random `HEX` color
+Changing a functions context with `fn.call(object)`
