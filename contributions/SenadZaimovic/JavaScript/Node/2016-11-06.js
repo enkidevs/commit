2ce7f49655a70db8b781ea00268c25e73b451593
SenadZaimovic <senad_zaimovic@hotmail.com>
2016-11-06 21:58:07 +0300

    2016-11-06's JavaScript workout

---
+++ contributions/SenadZaimovic/JavaScript/Node/2016-11-06.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+**Node** `os` module
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
