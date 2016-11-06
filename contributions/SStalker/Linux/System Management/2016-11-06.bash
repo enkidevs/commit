390c29a4f5e4559a975ceed359c3027df85ae6e4
SStalker <raphael@bluepanel.org>
2016-11-06 09:37:06 +0300

    2016-11-06's Linux workout

---
+++ contributions/SStalker/Linux/System Management/2016-11-06.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
+Finding Open Files With `lsof`
