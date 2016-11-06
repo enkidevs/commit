c73959e2f5f863da0c703dacc397282589dd6c3b
trekie86 <trekie86@gmail.com>
2016-11-06 19:21:04 +0300

    2016-11-06's Java workout

---
+++ contributions/trekie86/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Using an interface as a parameter
