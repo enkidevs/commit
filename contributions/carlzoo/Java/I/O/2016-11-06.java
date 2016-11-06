a5bade99c91e516ee7fde0dc7cb8691adadb4d1c
carlzoo <carl.j.zhou@protonmail.ch>
2016-11-07 01:04:58 +0300

    2016-11-06's Java workout

---
+++ contributions/carlzoo/Java/I/O/2016-11-06.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+The `Console` class
