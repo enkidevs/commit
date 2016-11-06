4f1ff96a3f162c55d62967edecaf7b2b3ae2d424
DWinter <dwinter@mail.ru>
2016-11-06 08:44:43 +0300

    2016-11-06's Java workout

---
+++ contributions/DWinter/Java/Error Handling/2016-11-06.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Converting stack trace to a string
+Creating new Exceptions
+Checking a double for `NaN` value
