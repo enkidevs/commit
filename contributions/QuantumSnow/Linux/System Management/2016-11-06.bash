7379e30411423d513ccf430eb01f51fae6a0dd53
QuantumSnow <jakobtheai@gmail.com>
2016-11-06 23:56:52 +0300

    2016-11-06's Linux workout

---
+++ contributions/QuantumSnow/Linux/System Management/2016-11-06.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
