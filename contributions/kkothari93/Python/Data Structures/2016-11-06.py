3a98eac6ecdc6027d606c5ec77b5b6e21aa3b088
kkothari93 <krk3mar@gmail.com>
2016-11-07 01:11:15 +0300

    2016-11-06's Python workout

---
+++ contributions/kkothari93/Python/Data Structures/2016-11-06.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+When `range` comes in handy
+Best way to implement a simple `queue` 
+`bytearray` objects
+Keep things in order with `OrderedDict`
