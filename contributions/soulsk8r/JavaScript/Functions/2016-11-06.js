da9da047e69715ad59743899edbf61de9864d807
soulsk8r <klavants@gmail.com>
2016-11-06 20:18:41 +0300

    2016-11-06's JavaScript workout

---
+++ contributions/soulsk8r/JavaScript/Functions/2016-11-06.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
