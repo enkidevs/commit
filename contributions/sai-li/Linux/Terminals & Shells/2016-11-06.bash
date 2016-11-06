6b7f6cb308a29a3917dbfa6ff309ffe6fc08315e
sai-li <neulisai@gmail.com>
2016-11-06 23:49:26 +0300

    2016-11-06's Linux workout

---
+++ contributions/sai-li/Linux/Terminals & Shells/2016-11-06.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Terminal incognito mode
+Get to know  your commands with `type`
+Retrieve previous arguments
