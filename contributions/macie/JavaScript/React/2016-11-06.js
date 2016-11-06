5a5ea1217fc3e4652d107cd68106ca56c84fdf46
macie <maciek.zok+github@gmail.com>
2016-11-06 09:53:54 +0300

    2016-11-06's JavaScript workout

---
+++ contributions/macie/JavaScript/React/2016-11-06.js
@@ -0,0 +1,5 @@
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+Expose Component Functions in **React**
+Dealing with `this.props.children`
+Spread attributes
