f330316843a857f806a2ce7899ce2c40bdc338d0
Vloorshad <vloorshad@diejoubu.com>
2016-11-07 02:44:17 +0300

    2016-11-06's Linux workout

---
+++ contributions/Vloorshad/Linux/Terminals & Shells/2016-11-06.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Random password generator
+Adding directories to your `$PATH`
+Retrieve previous arguments
