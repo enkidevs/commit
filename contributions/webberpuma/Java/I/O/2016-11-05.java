2ffb28aa500877a7fc4bf40189e0cca3df12c008
webberpuma <webberpuma@gmail.com>
2016-11-06 05:56:12 +0300

    2016-11-05's Java workout

---
+++ contributions/webberpuma/Java/I/O/2016-11-05.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Checking for the existence of files and directories
+Retrieving file store attributes
+Deleting files and directories
