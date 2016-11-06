e3e389d5420fb286f5ce80c89122f4bc6ee8d8ce
Calypsys <calypsys1@gmail.com>
2016-11-06 11:19:07 +0300

    2016-11-06's Linux workout

---
+++ contributions/Calypsys/Linux/System Management/2016-11-06.bash
@@ -0,0 +1,5 @@
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
