4615e7fcfb2bc7a8e28943e8bc93617540808d10
fmauNeko <fmaunier@gmail.com>
2016-11-06 19:40:43 +0300

    2016-11-06's Linux workout

---
+++ contributions/fmauNeko/Linux/System Management/2016-11-06.bash
@@ -0,0 +1,5 @@
+List current logged on users with `w`
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
+Finding Open Files With `lsof`
