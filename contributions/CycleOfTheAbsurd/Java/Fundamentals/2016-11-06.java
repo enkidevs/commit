d0272292075e2bca255807e5b919333303ee624f
CycleOfTheAbsurd <alex7up42@gmail.com>
2016-11-06 17:42:17 +0300

    2016-11-06's Java workout

---
+++ contributions/CycleOfTheAbsurd/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The trade-off between short and descriptive names
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Using `Printstream.format` to format a string before printing
