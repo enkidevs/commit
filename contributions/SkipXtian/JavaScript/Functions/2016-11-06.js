7e325a4470f3ecb2b456509044dc74f3be2c14b7
SkipXtian <skip.xtian@gmail.com>
2016-11-07 02:21:53 +0300

    2016-11-06's JavaScript workout

---
+++ contributions/SkipXtian/JavaScript/Functions/2016-11-06.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Anonymous functions
+Easily generate a random `HEX` color
+Changing a functions context with `fn.call(object)`
