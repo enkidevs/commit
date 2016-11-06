d8d38fe6da9eae06d1c14be09cc397bb4bd87505
KwangHoonCha <chakh9601@gmail.com>
2016-11-07 01:46:50 +0300

    2016-11-06's Java workout

---
+++ contributions/KwangHoonCha/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between `==` and `equals()`
+Generating random numbers
+Altering format string output by changing a format specifier's `argument_index`
+Do not perform bitwise and arithmetic operations on the same data
