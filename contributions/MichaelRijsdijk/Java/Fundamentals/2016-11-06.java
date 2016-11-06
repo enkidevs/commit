87d7e73f5569180ced6e5fc16e09d01342c988d9
MichaelRijsdijk <michaelrijsdijk@gmail.com>
2016-11-06 14:00:53 +0300

    2016-11-06's Java workout

---
+++ contributions/MichaelRijsdijk/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Writing generic methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
