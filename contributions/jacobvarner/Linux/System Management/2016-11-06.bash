d795ca6b50bdaa838d5dc4b9d2e0a9a5406dd17d
jacobvarner <jacob.varner14@gmail.com>
2016-11-06 19:34:15 +0300

    2016-11-06's Linux workout

---
+++ contributions/jacobvarner/Linux/System Management/2016-11-06.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
