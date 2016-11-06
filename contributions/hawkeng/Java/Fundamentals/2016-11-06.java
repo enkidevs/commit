9e4231384f642abc5941b2a0f65e37e3e4a499ff
hawkeng <hawkins.dom@gmail.com>
2016-11-07 01:20:45 +0300

    2016-11-06's Java workout

---
+++ contributions/hawkeng/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
