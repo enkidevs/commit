33ee540d52649f9da2666ca5f9c3cc617661a9a4
Flos <fn187-github@yahoo.de>
2016-11-06 15:48:11 +0300

    2016-11-06's Linux workout

---
+++ contributions/Flos/Linux/Terminals & Shells/2016-11-06.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Terminal based browser
+Keep useful commands in your shell history with tags
