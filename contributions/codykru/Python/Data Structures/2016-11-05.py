fff0fa6640a81e32ba8716716c81933d26fc396d
codykru <codykru@gmail.com>
2016-11-06 05:06:47 +0300

    2016-11-05's Python workout

---
+++ contributions/codykru/Python/Data Structures/2016-11-05.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`date` object
+Your own Python `calendar` 
+When `range` comes in handy
+Keep things in order with `OrderedDict`
