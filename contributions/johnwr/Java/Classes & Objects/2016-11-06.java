3a1472c99e65864fe5151765624713b8f719d0c1
johnwr <johnwr_enki@wraalsen.no>
2016-11-06 12:49:55 +0300

    2016-11-06's Java workout

---
+++ contributions/johnwr/Java/Classes & Objects/2016-11-06.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Utility classes and `static` methods
+Double Brace Initialization
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
