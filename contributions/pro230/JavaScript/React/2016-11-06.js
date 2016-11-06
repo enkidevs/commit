65057fe5f9033aa490bbfa7c506d5720cb7e4b89
pro230 <gt.szgyula@gmail.com>
2016-11-06 12:35:25 +0300

    2016-11-06's JavaScript workout

---
+++ contributions/pro230/JavaScript/React/2016-11-06.js
@@ -0,0 +1,6 @@
+Pixel values in style props for **React**
+componentWillReceiveProps Not Triggered After Mounting
+
+Using `false` in **JSX**
+Expose Component Functions in **React**
+Shortcut for transferring props
