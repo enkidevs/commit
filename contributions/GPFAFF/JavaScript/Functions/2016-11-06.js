30eedb5557591e84a988a836a8b7dd631b6e506c
GPFAFF <greg.pfaff@razorfish.com>
2016-11-06 16:37:48 +0300

    2016-11-06's JavaScript workout

---
+++ contributions/GPFAFF/JavaScript/Functions/2016-11-06.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Method chaining
+Get query/url variables
+Changing a functions context with `fn.call(object)`
