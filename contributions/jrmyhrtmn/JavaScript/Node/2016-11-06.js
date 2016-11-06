86be9088542fac58689b46e15251ff8f25f39c92
jrmyhrtmn <jmaize91@gmail.com>
2016-11-06 17:38:03 +0300

    2016-11-06's JavaScript workout

---
+++ contributions/jrmyhrtmn/JavaScript/Node/2016-11-06.js
@@ -0,0 +1,5 @@
+Http Server in **Node**
+First-error callbacks in **Node**
+Wrapping errors in Node.js using _node-verror_
+Handle `JSON.parse` error in Node.js
+`process.nextTick()` callback pattern
