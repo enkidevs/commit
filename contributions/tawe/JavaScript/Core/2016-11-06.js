4f60e0078bfd7d5519584b56792bc334927c7f99
tawe <munn.jon@gmail.com>
2016-11-06 20:04:47 +0300

    2016-11-06's JavaScript workout

---
+++ contributions/tawe/JavaScript/Core/2016-11-06.js
@@ -0,0 +1,5 @@
+Data type comparison in `switch` statements
+Filtering items out of an array
+Extra function arguments are undefined by default
+Check if a function exists
+Detect **DO NOT TRACK** status
