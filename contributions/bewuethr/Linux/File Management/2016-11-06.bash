b0132614fb9126bfa51f6f363836cc3aac7621ed
bewuethr <benjamin.wuethrich@gmail.com>
2016-11-06 22:49:46 +0300

    2016-11-06's Linux workout

---
+++ contributions/bewuethr/Linux/File Management/2016-11-06.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
