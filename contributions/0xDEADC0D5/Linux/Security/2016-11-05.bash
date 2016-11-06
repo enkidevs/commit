48cf73dad0b9d9e7fdf40b965bfcb678bf408875
0xDEADC0D5 <seductivethunderstorm@gmail.com>
2016-11-06 06:34:12 +0300

    2016-11-05's Linux workout

---
+++ contributions/0xDEADC0D5/Linux/Security/2016-11-05.bash
@@ -0,0 +1,5 @@
+Extracting the `public key` from the `private key`
+Disable SSH password authentication 
+Block IPs using `Fail2ban`
+Disabling **USB** storage devices
+Private key encryption with `OpenSSL`
