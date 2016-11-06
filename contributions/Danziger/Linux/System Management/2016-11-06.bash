848e6e19ec7d34e3676d9bb6c69a658a1839eecb
Danziger <gmzcodes@outlook.com>
2016-11-07 01:38:05 +0300

    2016-11-06's Linux workout

---
+++ contributions/Danziger/Linux/System Management/2016-11-06.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Force a time update with `ntp`
+
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
