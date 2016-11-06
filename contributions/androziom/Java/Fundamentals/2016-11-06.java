17387b33f9a2e3ca5840b21ab96b827c2c8ca432
androziom <androziom@gmail.com>
2016-11-07 01:37:10 +0300

    2016-11-06's Java workout

---
+++ contributions/androziom/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
