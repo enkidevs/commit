c3270004d04d6e5ea711698f7817f1057b883605
artu-ole <artu-ole@yandex.ru>
2016-11-06 21:20:43 +0300

    2016-11-06's Java workout

---
+++ contributions/artu-ole/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Generating random numbers
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Calling one constructor from another
+Constant notation
