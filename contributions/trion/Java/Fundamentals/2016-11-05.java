ba850a47403c36d701cff28f3e73ce931ec5ad0d
trion <parmsingh129@yahoo.com>
2016-11-06 07:59:22 +0300

    2016-11-05's Java workout

---
+++ contributions/trion/Java/Fundamentals/2016-11-05.java
@@ -0,0 +1,5 @@
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
