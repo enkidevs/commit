459f27bcc6a95666735c5ab78eed1ae3759b15e5
svartr <the.trash.collector@yandex.com>
2016-11-06 09:52:07 +0300

    2016-11-05's Linux workout

---
+++ contributions/svartr/Linux/Package Management/2016-11-05.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Backporting on Debian
+
+Installing a `.deb` package from the terminal
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
