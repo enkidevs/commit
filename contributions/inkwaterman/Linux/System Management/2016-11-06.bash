20b37d491f1acd359a772824096062f76c177c2f
inkwaterman <inkwaterman@gmail.com>
2016-11-06 12:42:57 +0300

    2016-11-06's Linux workout

---
+++ contributions/inkwaterman/Linux/System Management/2016-11-06.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
