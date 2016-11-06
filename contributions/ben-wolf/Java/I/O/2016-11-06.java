16934b7750873dd45f714fadf4d451e44d8df096
ben-wolf <wolfb3@rpi.edu>
2016-11-06 23:54:05 +0300

    2016-11-06's Java workout

---
+++ contributions/ben-wolf/Java/I/O/2016-11-06.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Listing a file system's root directories
