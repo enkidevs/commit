072f84a7713afcf53cda2ed7a730d36e796e23c6
gjcooper <gjcooper@gmail.com>
2016-11-06 08:35:42 +0300

    2016-11-05's Linux workout

---
+++ contributions/gjcooper/Linux/System Management/2016-11-05.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
