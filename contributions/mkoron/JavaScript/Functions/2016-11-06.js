ac6a889f1cfb973aa98321fa74e6a65bdfecc9c9
mkoron <koron.marko@gmail.com>
2016-11-06 16:39:35 +0300

    2016-11-06's JavaScript workout

---
+++ contributions/mkoron/JavaScript/Functions/2016-11-06.js
@@ -0,0 +1,5 @@
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Easily generate a random `HEX` color
+Function call method
+Changing a functions context with `fn.call(object)`
