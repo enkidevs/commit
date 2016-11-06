49f2df76527a368be8d4906ecafc92b0bf8fa20b
sylcha <chambon.sylvain@gmail.com>
2016-11-07 02:25:43 +0300

    2016-11-06's Java workout

---
+++ contributions/sylcha/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Using `synchronized` statements
+Detect or prevent integer overflow
