32680be47d8b90e958cea36885baaca5de78c437
aeolyus <richardhuang.huang@gmail.com>
2016-11-07 02:10:56 +0300

    2016-11-06's Linux workout

---
+++ contributions/aeolyus/Linux/Terminals & Shells/2016-11-06.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Sequential execution using the `;` statement separator
+Keep useful commands in your shell history with tags
+Retrieve previous arguments
