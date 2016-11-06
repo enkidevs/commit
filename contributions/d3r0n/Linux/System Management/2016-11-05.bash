b429d2f97a51905bb2fe9dbe8fd5ef14ae58885b
d3r0n <mider2+enki@gmail.com>
2016-11-06 15:21:09 +0300

    2016-11-05's Linux workout

---
+++ contributions/d3r0n/Linux/System Management/2016-11-05.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Monitor System Memory using `vmstat`
