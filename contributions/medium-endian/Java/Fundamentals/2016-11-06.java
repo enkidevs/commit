83bc5207a66c18da6c71ff8429ba718740a7d3cb
medium-endian <mediumendian@gmail.com>
2016-11-06 20:53:12 +0300

    2016-11-06's Java workout

---
+++ contributions/medium-endian/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Avoid using `static` variables
+Using an interface as a parameter
