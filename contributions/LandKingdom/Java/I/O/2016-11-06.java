bbb68a47260e9ec4fd1172e7d8ea137729c6560b
LandKingdom <franci.dainese@gmail.com>
2016-11-06 16:53:31 +0300

    2016-11-06's Java workout

---
+++ contributions/LandKingdom/Java/I/O/2016-11-06.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Retrieving file store attributes
