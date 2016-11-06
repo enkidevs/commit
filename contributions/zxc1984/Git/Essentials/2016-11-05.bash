37d7e0aa1d68baf605b246889d19be2822272657
zxc1984 <ryanoh1984@gmail.com>
2016-11-06 08:33:38 +0300

    2016-11-05's Git workout

---
+++ contributions/zxc1984/Git/Essentials/2016-11-05.bash
@@ -0,0 +1,5 @@
+Pushing tags to a server
+Using aliases for git commands
+How to set your username and email
+Use `short` status to make output more compact
+Remote repositories: fetching and pushing
