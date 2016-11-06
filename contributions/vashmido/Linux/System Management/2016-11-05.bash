5bf6332714df090fe00147a12fcc6f6128b35138
vashmido <hymura@gmail.com>
2016-11-06 03:20:45 +0300

    2016-11-05's Linux workout

---
+++ contributions/vashmido/Linux/System Management/2016-11-05.bash
@@ -0,0 +1,5 @@
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
