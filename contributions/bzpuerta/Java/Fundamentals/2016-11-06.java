07d41e0bef7d56fc5a723dd3892e9f8a717cfd3c
bzpuerta <beatriz.puerta.hoyas@gmail.com>
2016-11-07 02:59:43 +0300

    2016-11-06's Java workout

---
+++ contributions/bzpuerta/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Generating random numbers
+Java naming conventions
+Using `Printstream.format` to format a string before printing
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
