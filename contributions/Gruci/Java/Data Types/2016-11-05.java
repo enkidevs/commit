81a2fb439e08285c402280611773fc9b25d7afea
Gruci <skeksksk@gmail.com>
2016-11-06 19:27:53 +0300

    2016-11-05's Java workout

---
+++ contributions/Gruci/Java/Data Types/2016-11-05.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Retrieve the component type of an array
