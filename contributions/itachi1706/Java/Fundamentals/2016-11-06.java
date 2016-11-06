82de5300ec666fb21aee6f2746e621d1d13bffd0
itachi1706 <kennethsohyq@hotmail.com>
2016-11-07 00:22:17 +0300

    2016-11-06's Java workout

---
+++ contributions/itachi1706/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Limit Accessibility of `Fields`
+Avoid using `static` variables
