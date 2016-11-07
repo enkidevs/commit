2e25e3dbee16b2fdd1c262c678b3c86187df93cd
OChing43 <orlando.marinella@gmail.com>
2016-11-07 15:51:48 +0300

    2016-11-07's Java workout

---
+++ contributions/OChing43/Java/Data Types/2016-11-07.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+`StringBuffer` vs `StringBuilder`
+Retrieve the component type of an array
