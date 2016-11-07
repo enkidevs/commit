c50201105d21b94fda6cb7a438153e31ed24ffee
vandoenterprise <evandro.barbosa@yahoo.com.br>
2016-11-08 00:24:39 +0300

    2016-11-07's Java workout

---
+++ contributions/vandoenterprise/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
