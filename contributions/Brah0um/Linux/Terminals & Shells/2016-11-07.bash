55b953eaa3b022eb72dee9f9ea7cc282b6eb9904
Brah0um <boukoufallah.brahim@gmail.com>
2016-11-07 15:36:09 +0300

    2016-11-07's Linux workout

---
+++ contributions/Brah0um/Linux/Terminals & Shells/2016-11-07.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Shebang `#!` explained 
+Execute a command without saving it in history
+Breaking out of a terminal when `ssh` locks
+Keep useful commands in your shell history with tags
