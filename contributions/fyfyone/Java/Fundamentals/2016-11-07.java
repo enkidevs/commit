350117ae3d35ef916598cb0bfbc3c2336c0af348
fyfyone <fyfyone@gmail.com>
2016-11-07 22:07:03 +0300

    2016-11-07's Java workout

---
+++ contributions/fyfyone/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Avoid using `static` variables
+Using an interface as a parameter
