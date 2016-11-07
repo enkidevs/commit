a78240c207d07a06d08d814530c5ac816e989a84
JellyBreeze <00valter00@gmail.com>
2016-11-07 11:17:37 +0300

    2016-11-07's Linux workout

---
+++ contributions/JellyBreeze/Linux/File Management/2016-11-07.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Practical `du` command
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
