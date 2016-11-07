d99849989621229dcac0c558350102e0a6ffc4cc
sylcha <chambon.sylvain@gmail.com>
2016-11-07 17:13:38 +0300

    2016-11-07's Java workout

---
+++ contributions/sylcha/Java/Error Handling/2016-11-07.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
