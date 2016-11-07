6c08638f9857a8ee0aa4b0ef116cc871233a849f
SkiFire13 <xxskifire13xx@gmail.com>
2016-11-07 15:56:08 +0300

    2016-11-07's Java workout

---
+++ contributions/SkiFire13/Java/I/O/2016-11-07.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Reading file attributes
