ec5b22ce4644334a05e48e0f6c6b75961c615be1
LaurensOomen <oomen.laurens@gmail.com>
2016-11-07 07:53:28 +0300

    2016-11-07's Java workout

---
+++ contributions/LaurensOomen/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
