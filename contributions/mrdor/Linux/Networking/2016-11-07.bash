79deb6570a5493a17c5c449071b3f924fc20193c
mrdor <mrdor44@gmail.com>
2016-11-07 10:56:22 +0300

    2016-11-07's Linux workout

---
+++ contributions/mrdor/Linux/Networking/2016-11-07.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Quick port test with `netcat`
+Disable `IPv6`
+Use `tcpdump` to listen to network interface traffic
+Staying connected remotely via SSH without an interactive shell
