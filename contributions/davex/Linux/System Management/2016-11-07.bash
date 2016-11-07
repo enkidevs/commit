bbf64950c011ff7ffb4053e3a0313360565fb942
davex <dabuck@libero.it>
2016-11-08 01:16:15 +0300

    2016-11-07's Linux workout

---
+++ contributions/davex/Linux/System Management/2016-11-07.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Check the HDD with `badblocks`
+Get hardware stack details with `lspci`
