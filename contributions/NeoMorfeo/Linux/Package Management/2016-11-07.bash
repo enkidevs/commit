9ff139a917e3bc029b78bae5256133ba2ad827a3
NeoMorfeo <neomorfeo@gmail.com>
2016-11-07 10:43:33 +0300

    2016-11-07's Linux workout

---
+++ contributions/NeoMorfeo/Linux/Package Management/2016-11-07.bash
@@ -0,0 +1,5 @@
+Install specific version of packages
+Prevent updating a specific package in Debian systems
+Get `apt` to use a mirror / faster mirror
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
