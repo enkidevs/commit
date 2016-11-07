4a4b1d5b5c33b8fd277bdf2cba82857bb8220dd9
mara8castro <mara8castro@gmail.com>
2016-11-07 23:51:33 +0300

    2016-11-07's Java workout

---
+++ contributions/mara8castro/Java/Data Types/2016-11-07.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Finding a substring in a string
+`StringBuffer` vs `StringBuilder`
+Do not attempt comparisons with NaN
