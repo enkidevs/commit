1e65dc2625ad18d3491812f1b91a73c8592a2a1e
shiv379 <shiv379@gmail.com>
2016-11-07 13:55:52 +0300

    2016-11-07's JavaScript workout

---
+++ contributions/shiv379/JavaScript/Node/2016-11-07.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+Node Inspector
+Use conventional property names for error handling in Node.js
+**Node** `os` module
+Basic web server with Express
