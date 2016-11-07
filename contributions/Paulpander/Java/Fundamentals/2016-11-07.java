05bf6594bc9d9497218c241eeaf584d504da4593
Paulpander <paul.methfessel@web.de>
2016-11-07 10:40:46 +0300

    2016-11-07's Java workout

---
+++ contributions/Paulpander/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
