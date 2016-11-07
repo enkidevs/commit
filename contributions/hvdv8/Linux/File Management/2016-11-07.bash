98e4d8d15a93f2a51e953c9ce3b1087774d5ac00
hvdv8 <fx772@hotmail.com>
2016-11-08 00:54:24 +0300

    2016-11-07's Linux workout

---
+++ contributions/hvdv8/Linux/File Management/2016-11-07.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Easy way of sharing files
