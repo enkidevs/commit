e3233a2bfe0ddef8f4f7a55e486538a00ee7a16a
starcoat <thecriz@gmail.com>
2016-11-07 12:55:51 +0300

    2016-11-07's Linux workout

---
+++ contributions/starcoat/Linux/File Management/2016-11-07.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Easy way of sharing files
