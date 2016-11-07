d616cbf1b5bf26439c5b577c0a8ba5933ea81c39
carsondm <carsondm@gmail.com>
2016-11-07 17:48:17 +0300

    2016-11-07's Java workout

---
+++ contributions/carsondm/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
