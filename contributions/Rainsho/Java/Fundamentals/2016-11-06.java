e64aadc5d5c6b574d921f25a66a5da90f5a0959a
Rainsho <djkyux@163.com>
2016-11-07 06:01:21 +0300

    2016-11-06's Java workout

---
+++ contributions/Rainsho/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
