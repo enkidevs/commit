df701faa48e0c9f79ee39b23664ad12af2dacaa6
waggertron <weylinwagnon@gmail.com>
2016-11-07 23:20:36 +0300

    2016-11-07's JavaScript workout

---
+++ contributions/waggertron/JavaScript/Functions/2016-11-07.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Changing a functions context with `fn.call(object)`
