69b56b55697c47f643a5e78f34da3e51947add72
Calim50 <bastien.sebire@gmail.com>
2016-11-07 20:09:49 +0300

    2016-11-07's Java workout

---
+++ contributions/Calim50/Java/I/O/2016-11-07.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+Translating `Scanner` tokens into primitive types
+Deleting files and directories
+The `Console` class
