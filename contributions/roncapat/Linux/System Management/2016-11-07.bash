10c753406607c143d0951f3ec75bc21c011f9dd5
roncapat <ronca.pat@gmail.com>
2016-11-08 02:18:07 +0300

    2016-11-07's Linux workout

---
+++ contributions/roncapat/Linux/System Management/2016-11-07.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Get hardware stack details with `lspci`
