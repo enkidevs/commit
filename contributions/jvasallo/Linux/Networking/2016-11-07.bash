bf60006ea878c8201ecf6a0c8ad95cf22d7a6f1f
jvasallo <joelvasallo@gmail.com>
2016-11-07 23:36:45 +0300

    2016-11-07's Linux workout

---
+++ contributions/jvasallo/Linux/Networking/2016-11-07.bash
@@ -0,0 +1,5 @@
+Multiple IP Address with aliasing 
+Useful ssh client optimizations 
+Get real network statistics with `slurm`
+Limit the `wget` download rate
+Staying connected remotely via SSH without an interactive shell
