a31f8fcd0cc31459ad8a2d315cf0a04e063f154e
FreeRadicand <conner_mcgrath@yahoo.com>
2016-11-07 18:43:45 +0300

    2016-11-07's JavaScript workout

---
+++ contributions/FreeRadicand/JavaScript/Functions/2016-11-07.js
@@ -0,0 +1,5 @@
+Hoisting
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Easily generate a random `HEX` color
