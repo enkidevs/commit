37fcd6278cab05d98043620999cbb5eda0911de5
drsgi8 <rogowiczdawid@gmail.com>
2016-11-07 09:25:32 +0300

    2016-11-07's Java workout

---
+++ contributions/drsgi8/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
