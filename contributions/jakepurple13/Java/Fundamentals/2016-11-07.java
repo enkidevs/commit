f311fdecea4cc77622b02f7fb9b7eddc0963c1d8
jakepurple13 <jakerein@gmail.com>
2016-11-07 18:12:55 +0300

    2016-11-07's Java workout

---
+++ contributions/jakepurple13/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Catch multiple exceptions in a single `catch` block
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
