2b297063d4faae0d3df257604abbdf8b9e9c425a
chacko <us.michael@yahoo.de>
2016-11-07 09:51:42 +0300

    2016-11-07's Linux workout

---
+++ contributions/chacko/Linux/File Management/2016-11-07.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
