20a66a078096053d49ccd4a63d73fc2a0afec947
pernacentus <pernacentus@gmail.com>
2016-11-07 20:52:45 +0300

    2016-11-07's Linux workout

---
+++ contributions/pernacentus/Linux/Networking/2016-11-07.bash
@@ -0,0 +1,5 @@
+SSH tunneling made easy
+Quick port test with `netcat`
+Useful ssh client optimizations 
+Use `tcpdump` to listen to network interface traffic
+Staying connected remotely via SSH without an interactive shell
