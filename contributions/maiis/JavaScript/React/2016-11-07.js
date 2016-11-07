9d850cb86c9b8ab01c8d0ee3245894ff756a9454
maiis <evuigner@gmail.com>
2016-11-07 23:40:20 +0300

    2016-11-07's JavaScript workout

---
+++ contributions/maiis/JavaScript/React/2016-11-07.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Use **React** with other libraries
+Validate for required props
+Custom validations for props
