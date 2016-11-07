3ba20d133b13cd2f82fa6d08d2c88f9bbc58035e
leslch <leslch@hotmail.com>
2016-11-08 02:54:12 +0300

    2016-11-07's Linux workout

---
+++ contributions/leslch/Linux/File Management/2016-11-07.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
