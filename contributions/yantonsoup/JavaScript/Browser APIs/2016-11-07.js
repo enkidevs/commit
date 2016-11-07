937d60ed73f259f515cfdf37a1aefb504b5eca8f
yantonsoup <ygoldobin@gmail.com>
2016-11-07 22:00:14 +0300

    2016-11-07's JavaScript workout

---
+++ contributions/yantonsoup/JavaScript/Browser APIs/2016-11-07.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Navigating the browser history
+Warn user if **Back** button is pressed
+Permission API
+MediaDevices.getUserMedia()
