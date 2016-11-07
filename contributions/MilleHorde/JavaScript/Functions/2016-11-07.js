78dae73456bf01f0e0efcab9afbff7184ccc2787
MilleHorde <nicolasremise@gmail.com>
2016-11-07 10:17:26 +0300

    2016-11-07's JavaScript workout

---
+++ contributions/MilleHorde/JavaScript/Functions/2016-11-07.js
@@ -0,0 +1,5 @@
+Hoisting
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Move cursor at the end of text input
