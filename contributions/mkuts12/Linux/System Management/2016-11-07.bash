34a3b1e547b776fcef059534cdf481128099a874
mkuts12 <mkutz@elitemail.org>
2016-11-07 09:10:19 +0300

    2016-11-07's Linux workout

---
+++ contributions/mkuts12/Linux/System Management/2016-11-07.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
