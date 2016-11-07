1e45594f6d8bc612627c404d66f4a8742a305121
timoveldt <timo.veldt@gmail.com>
2016-11-07 19:13:58 +0300

    2016-11-07's Java workout

---
+++ contributions/timoveldt/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
