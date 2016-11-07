217b292b59a4f6cb9574f8a17c43dcd7d6bd5ba2
archangel <heiko.borchers@gmail.com>
2016-11-07 10:53:47 +0300

    2016-11-07's Linux workout

---
+++ contributions/archangel/Linux/File Management/2016-11-07.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
