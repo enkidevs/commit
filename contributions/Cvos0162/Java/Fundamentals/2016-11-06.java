3edc1416c1986463d7947f42e2bdb47313db37a6
Cvos0162 <cookys0162@hotmail.co.kr>
2016-11-07 06:47:01 +0300

    2016-11-06's Java workout

---
+++ contributions/Cvos0162/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Using Inheritance to reduce code repetition
+Always specify type parameter
+How range operations work
