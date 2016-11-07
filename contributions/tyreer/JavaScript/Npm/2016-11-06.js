9b594d3ee5c7e0cbc6ccfcd8a652bf3aa7f2d184
tyreer <tyreer@gmail.com>
2016-11-07 09:27:06 +0300

    2016-11-06's JavaScript workout

---
+++ contributions/tyreer/JavaScript/Npm/2016-11-06.js
@@ -0,0 +1,5 @@
+Calling remote scripts with npm
+Combining script commands in npm
+`npm`  as an alternative to Gulp
+`config` object in `package.json`
+Lock down dependency versions by shrinkwrapping
