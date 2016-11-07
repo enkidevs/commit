8ac5b9975c6fcac014e54caf09ecbdda040ef357
J0hn- <jonathan.platteau@yahoo.fr>
2016-11-07 09:30:34 +0300

    2016-11-07's Linux workout

---
+++ contributions/J0hn-/Linux/System Management/2016-11-07.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
