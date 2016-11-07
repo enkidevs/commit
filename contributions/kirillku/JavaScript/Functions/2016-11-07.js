b1340fe44917b199a684fb0367b8f5d2dc40bb82
kirillku <kubryakov@gmail.com>
2016-11-07 08:01:53 +0300

    2016-11-07's JavaScript workout

---
+++ contributions/kirillku/JavaScript/Functions/2016-11-07.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Method chaining
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
