93a2b63843f869dee7fcf3ee4767e7480b900dcc
nilicule <nilicule@gmail.com>
2016-11-07 11:19:48 +0300

    2016-11-07's Linux workout

---
+++ contributions/nilicule/Linux/System Management/2016-11-07.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Common log files
+Basic service management with `systemd`
