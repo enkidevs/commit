7e60bea1960539e373070c49bf882a96c4a62037
deanlyoung <dlyoung119@me.com>
2016-11-07 21:12:26 +0300

    2016-11-07's JavaScript workout

---
+++ contributions/deanlyoung/JavaScript/Node/2016-11-07.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Custom Node REPL Server
+Wrapping errors in Node.js using _node-verror_
+`exports` vs. `module.exports` in **Node**
+Manage child processes in **Node**
