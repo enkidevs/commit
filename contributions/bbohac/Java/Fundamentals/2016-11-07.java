ddb25cb5b046febca74cc7090bc939de34bdc317
bbohac <bastian.bohac@gmail.com>
2016-11-07 11:33:51 +0300

    2016-11-07's Java workout

---
+++ contributions/bbohac/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Difference between `==` and `equals()`
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Using an interface as a parameter
