845cd061bde715a831e26fe8f7064e0ce420b229
ImmoHunt <jarkendar333@gmail.com>
2016-11-07 15:38:41 +0300

    2016-11-07's Java workout

---
+++ contributions/ImmoHunt/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Ternary operator
+Using Inheritance to reduce code repetition
+How range operations work
