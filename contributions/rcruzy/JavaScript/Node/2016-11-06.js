632da1ea4a7c1f675528ee058b0fdccb143243ce
rcruzy <ryanjosephcruz1@gmail.com>
2016-11-07 11:20:43 +0300

    2016-11-06's JavaScript workout

---
+++ contributions/rcruzy/JavaScript/Node/2016-11-06.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
+`process.nextTick()` callback pattern
+Middleware/pipeline design pattern
