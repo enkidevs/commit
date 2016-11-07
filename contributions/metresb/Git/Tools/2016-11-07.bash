21e3202b1f69579fb703f187388f4d8f7745a90f
metresb <metresb@gmail.com>
2016-11-07 19:47:49 +0300

    2016-11-07's Git workout

---
+++ contributions/metresb/Git/Tools/2016-11-07.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Specify a commit by its ancestry
+Sign your work (using GPG)
+Remember the results of previous hunk conflicts
+Sharing data by bundling
