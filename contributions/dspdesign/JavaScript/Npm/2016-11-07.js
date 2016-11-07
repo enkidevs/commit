12c01482071a6ad19196e3859636c2388e8e3467
dspdesign <dsp-design@mail.ru>
2016-11-07 10:54:11 +0300

    2016-11-07's JavaScript workout

---
+++ contributions/dspdesign/JavaScript/Npm/2016-11-07.js
@@ -0,0 +1,5 @@
+npm can handle multiple versions of a dependency
+Combining script commands in npm
+Manage local node modules with `npm link`
+Environmental variables and flags for `npm`
+Scoped packages
