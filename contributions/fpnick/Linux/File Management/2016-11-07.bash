85bf74a91460a2080636f46ec8c749873480ed48
fpnick <fp.nick@gmail.com>
2016-11-07 22:13:23 +0300

    2016-11-07's Linux workout

---
+++ contributions/fpnick/Linux/File Management/2016-11-07.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Deleting non-empty directories
+Delete commands aliases
