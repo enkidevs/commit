96883181cc24b0c362c824eb24bfcd938865c420
ulenkar <ulka@o2.pl>
2016-11-07 20:00:26 +0300

    2016-11-07's Java workout

---
+++ contributions/ulenkar/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Supply `toString()` in all classes
