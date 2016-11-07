fde6aba25c0c5ac9b3effd5f076ec54aa918ee73
goumpi <layenjulian@gmail.com>
2016-11-07 23:23:19 +0300

    2016-11-07's Java workout

---
+++ contributions/goumpi/Java/Error Handling/2016-11-07.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Throwing an `exception`
+Checking a double for `NaN` value
