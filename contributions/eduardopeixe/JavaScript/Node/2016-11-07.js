99957e267e7b8db927949407101ac360699b25d1
eduardopeixe <walthfish@yahoo.com>
2016-11-07 17:36:36 +0300

    2016-11-07's JavaScript workout

---
+++ contributions/eduardopeixe/JavaScript/Node/2016-11-07.js
@@ -0,0 +1,5 @@
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
+Manage child processes in **Node**
