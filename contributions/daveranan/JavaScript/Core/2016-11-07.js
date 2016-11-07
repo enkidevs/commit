7d083707515861e426cf8e18ce4f91bd4e1f55b6
daveranan <daveranan@gmail.com>
2016-11-07 18:02:28 +0300

    2016-11-07's JavaScript workout

---
+++ contributions/daveranan/JavaScript/Core/2016-11-07.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Most efficient way to build `HTML` strings
+Data type comparison in `switch` statements
+Infix operators are left-associative
+Hoisting applies only to variable declarations, not initializations
