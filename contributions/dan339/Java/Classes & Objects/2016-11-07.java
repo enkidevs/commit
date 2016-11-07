acdbc2d934db362a81dd89913c4b1e891afd2a04
dan339 <danielcampbell339@gmail.com>
2016-11-07 10:28:50 +0300

    2016-11-07's Java workout

---
+++ contributions/dan339/Java/Classes & Objects/2016-11-07.java
@@ -0,0 +1,5 @@
+Template methods in abstract classes
+Utility classes and `static` methods
+Diamond Operator
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
