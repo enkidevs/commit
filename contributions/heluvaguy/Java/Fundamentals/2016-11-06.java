541ca485faac2f7047e700b0a5230159b15acd33
heluvaguy <adarshjaiswal1989@gmail.com>
2016-11-07 05:50:17 +0300

    2016-11-06's Java workout

---
+++ contributions/heluvaguy/Java/Fundamentals/2016-11-06.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Avoid using `static` variables
