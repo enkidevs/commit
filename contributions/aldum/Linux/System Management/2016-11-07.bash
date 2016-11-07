41276b89c876a2a8c4d6759efff4e078e88a1b29
aldum <spongyabob@gmail.com>
2016-11-07 11:32:08 +0300

    2016-11-07's Linux workout

---
+++ contributions/aldum/Linux/System Management/2016-11-07.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
