24dc67c18430cd8e02100f3e59d3e5e7945989fa
amarradi <radischm@gmail.com>
2016-11-07 11:18:08 +0300

    2016-11-07's Linux workout

---
+++ contributions/amarradi/Linux/System Management/2016-11-07.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
