caa96097e406c235744b358426f02e56200897df
kubik <lkubic@poczta.onet.pl>
2016-11-07 10:42:07 +0300

    2016-11-07's Java workout

---
+++ contributions/kubik/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+How range operations work
+Using an interface as a parameter
