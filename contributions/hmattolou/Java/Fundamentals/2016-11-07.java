31cb6f42894e11fc8fe3998d30ddece9808773e2
hmattolou <hm.attolou@gmail.com>
2016-11-07 22:32:48 +0300

    2016-11-07's Java workout

---
+++ contributions/hmattolou/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Using an interface as a parameter
