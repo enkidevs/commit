74f683b95678349c98d7d497425e6dfa2d3e2d1a
gobbas <gabbi@hotmail.no>
2016-11-07 09:10:16 +0300

    2016-11-07's Java workout

---
+++ contributions/gobbas/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Using `Printstream.format` to format a string before printing
+Detect or prevent integer overflow
+Avoid using `static` variables
