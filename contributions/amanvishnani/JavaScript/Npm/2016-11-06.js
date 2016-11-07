8484e435327553795634b7d85a15ffb331751e54
amanvishnani <aman62@gmail.com>
2016-11-07 09:48:37 +0300

    2016-11-06's JavaScript workout

---
+++ contributions/amanvishnani/JavaScript/Npm/2016-11-06.js
@@ -0,0 +1,5 @@
+Bump package version in npm
+npm can handle multiple versions of a dependency
+List binaries for scripting in npm
+`config` object in `package.json`
+Lock down dependency versions by shrinkwrapping
