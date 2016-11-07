546987ae639d60a71531e2d807384dd8a04be6ed
Heartbroken-Git <corentin196@gmail.com>
2016-11-07 21:06:56 +0300

    2016-11-07's Java workout

---
+++ contributions/Heartbroken-Git/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+The trade-off between short and descriptive names
+Use `DecimalFormat` class to format numbers
+Avoid using `static` variables
