fdb69d720821f0d0484b1e5a713f41f949cb3ab1
MagicEinhorn2000 <ks.wischmop@gmail.com>
2016-11-08 14:52:21 +0300

    2016-11-08's Java workout

---
+++ contributions/MagicEinhorn2000/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
