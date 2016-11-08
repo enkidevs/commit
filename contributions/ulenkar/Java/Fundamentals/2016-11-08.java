e21d2794ea68ae643044cf3bab45d41bb88ac98a
ulenkar <ulka@o2.pl>
2016-11-08 19:40:05 +0300

    2016-11-08's Java workout

---
+++ contributions/ulenkar/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Ternary operator
+Use strings in a `switch` statement
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
