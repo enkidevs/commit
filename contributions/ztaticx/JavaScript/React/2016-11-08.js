754e4731e7ceea7e82f45a488bd904f23d9d6888
ztaticx <rich.zavalac@gmail.com>
2016-11-09 02:23:54 +0300

    2016-11-08's JavaScript workout

---
+++ contributions/ztaticx/JavaScript/React/2016-11-08.js
@@ -0,0 +1,6 @@
+Self-closing tags in **JSX**
+componentWillReceiveProps Not Triggered After Mounting
+
+Expose Component Functions in **React**
+Use **React** with other libraries
+`PureRenderMixin` in **React**
