e46fab8afac7f97dbc4364c30bb107f1d878415a
lokyi <roy.yi.yue@gmail.com>
2016-11-09 00:18:33 +0300

    2016-11-08's JavaScript workout

---
+++ contributions/lokyi/JavaScript/Node/2016-11-08.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Asynchronous File Write/Read in Node.js
+`process.nextTick()` callback pattern
+Automatic compilation for Node with **Nodemon** 
+Middleware/pipeline design pattern
