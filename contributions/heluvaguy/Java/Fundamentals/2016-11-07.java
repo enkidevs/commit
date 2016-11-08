9c1d9a34465d8ec3a08fea8e3cd3111c7eb25135
heluvaguy <adarshjaiswal1989@gmail.com>
2016-11-08 04:23:28 +0300

    2016-11-07's Java workout

---
+++ contributions/heluvaguy/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+Use strings in a `switch` statement
+Using `Printstream.format` to format a string before printing
+Detect or prevent integer overflow
