32c4815887bf22295ac71385191f2d8e5fd3d5bc
Ricardo Silveira <ricardofsilveiralbj@gmail.com>
2016-11-08 04:22:22 +0300

    2016-11-07's Java workout

---
+++ contributions/Ricardo Silveira/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
