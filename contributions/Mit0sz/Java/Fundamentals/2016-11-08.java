350428dcfdb26f04bc1e5269ab5553aec0101f1f
Mit0sz <artur.krawczyk93@gmail.com>
2016-11-08 09:02:45 +0300

    2016-11-08's Java workout

---
+++ contributions/Mit0sz/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Java naming conventions
+Ternary operator
+How range operations work
