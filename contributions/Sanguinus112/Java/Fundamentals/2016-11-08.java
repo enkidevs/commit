d4bcce02e238c18842b052a006cca38dd0996a16
Sanguinus112 <florian.cramer.112@gmail.com>
2016-11-08 17:07:05 +0300

    2016-11-08's Java workout

---
+++ contributions/Sanguinus112/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
