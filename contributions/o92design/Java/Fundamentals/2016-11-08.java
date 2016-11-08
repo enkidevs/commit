445b1dd6328d47038567bc0bc3fd3442de6aae8c
o92design <o92design@gmail.com>
2016-11-08 13:31:35 +0300

    2016-11-08's Java workout

---
+++ contributions/o92design/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between ```HashMap``` and ```Hashtable```
+Difference between `==` and `equals()`
+Using Inheritance to reduce code repetition
+`Autoboxing` and `Unboxing`
