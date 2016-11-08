176cd6d41eb92be304b0dbdc45dad778a3cf0a8a
Edvinas <edvinas.reg@outlook.com>
2016-11-08 16:33:28 +0300

    2016-11-08's Java workout

---
+++ contributions/Edvinas /Java/Error Handling/2016-11-08.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Careful Numeric Data Conversions
