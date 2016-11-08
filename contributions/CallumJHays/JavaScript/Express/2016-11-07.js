2de42d296f078aa5b958f7f2347468a596206bd9
CallumJHays <callumjhays@gmail.com>
2016-11-08 16:06:24 +0300

    2016-11-07's JavaScript workout

---
+++ contributions/CallumJHays/JavaScript/Express/2016-11-07.js
@@ -0,0 +1,5 @@
+`response` object additional properties in **Express**
+Easy way to deliver **HTML** pages with **Express**
+Using **Express 4.0** `router`
+`body-parser` in **Express**
+Log with `morgan` in **Express**
