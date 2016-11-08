f046acb58334f2673f8ccaceb36035548672191d
ellisandy <andrew@andrewryanellis.com>
2016-11-08 19:23:22 +0300

    2016-11-08's Java workout

---
+++ contributions/ellisandy/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between ```HashMap``` and ```Hashtable```
+The trade-off between short and descriptive names
+Using `Printstream.format` to format a string before printing
+Constant notation
