1fdaa5f8765ab4c9ddc06e3cb4e6dff049ecb0f4
Vebble <serafin21@gmail.com>
2016-11-08 15:42:57 +0300

    2016-11-08's Java workout

---
+++ contributions/Vebble/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Using `Printstream.format` to format a string before printing
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
