ebc58d242486385f53949ac5a3dccd03b5017fe2
code_geek <asparsh29kumar@gmail.com>
2016-11-08 06:48:48 +0300

    2016-11-07's Linux workout

---
+++ contributions/code_geek/Linux/File Management/2016-11-07.bash
@@ -0,0 +1,5 @@
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
+Easy way of sharing files
