491beb9507ed5089ac9192a5435ec57aef950955
bobsmith947 <lkitaev@cherrycreekschools.org>
2016-11-09 01:12:11 +0300

    2016-11-08's Java workout

---
+++ contributions/bobsmith947/Java/Error Handling/2016-11-08.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Prevent `exceptions` while logging Data
+Throwing an `exception`
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
