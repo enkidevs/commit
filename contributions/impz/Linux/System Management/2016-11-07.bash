5cdc8f289d41498032a38de14d1260217f1dc278
impz <enki@burchill.net>
2016-11-08 03:51:26 +0300

    2016-11-07's Linux workout

---
+++ contributions/impz/Linux/System Management/2016-11-07.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
