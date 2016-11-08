3cb5b73c12f89ec39880928aa43d8e2cd02fbe43
jfcastro <juanfercas2002@gmail.com>
2016-11-08 15:45:24 +0300

    2016-11-08's Java workout

---
+++ contributions/jfcastro/Java/Error Handling/2016-11-08.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
