79a33446cbfc87a8342a0722b76b9a317a14dd91
NotALamer <alvaro.movil.ms@gmail.com>
2016-11-08 19:08:52 +0300

    2016-11-08's Java workout

---
+++ contributions/NotALamer/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
