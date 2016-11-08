f2af991a60ba4443bd37e4eb3710847cb4b443c0
Kurre <baijeri@gmail.com>
2016-11-08 23:31:23 +0300

    2016-11-08's JavaScript workout

---
+++ contributions/Kurre/JavaScript/Node/2016-11-08.js
@@ -0,0 +1,5 @@
+First-error callbacks in **Node**
+Wrapping errors in Node.js using _node-verror_
+`process.nextTick()` callback pattern
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
