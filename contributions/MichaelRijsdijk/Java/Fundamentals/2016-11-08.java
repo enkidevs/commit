774bafc6b1c21a870e3a4d56cf8f9a7fe1b15688
MichaelRijsdijk <michaelrijsdijk@gmail.com>
2016-11-08 11:11:22 +0300

    2016-11-08's Java workout

---
+++ contributions/MichaelRijsdijk/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
