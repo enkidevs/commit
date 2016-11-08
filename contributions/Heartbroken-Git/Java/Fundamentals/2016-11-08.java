b365fc244fd8c4da17dd807d9105c01127f5d2d1
Heartbroken-Git <corentin196@gmail.com>
2016-11-08 21:11:39 +0300

    2016-11-08's Java workout

---
+++ contributions/Heartbroken-Git/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
+Using `Printstream.format` to format a string before printing
+Using an interface as a parameter
