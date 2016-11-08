d1c6ff8d4b26e6ae6d6d8025edc8e2d9fbad46ec
siddharthmudgal <sidmud@outlook.com>
2016-11-08 19:52:46 +0300

    2016-11-07's Java workout

---
+++ contributions/siddharthmudgal/Java/Data Types/2016-11-07.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Finding a substring in a string
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
