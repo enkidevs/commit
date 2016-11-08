3f52f297a5fb5b286df06a6258a1b7bf9ac9c47b
neutrinoceros <clement.robert@protonmail.com>
2016-11-08 11:38:45 +0300

    2016-11-08's Linux workout

---
+++ contributions/neutrinoceros/Linux/File Management/2016-11-08.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
