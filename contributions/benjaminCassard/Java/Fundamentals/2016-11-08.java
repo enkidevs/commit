04011b92b77de9c918bd9390b7bff059884af79b
benjaminCassard <benjamin.cassard@gmail.com>
2016-11-08 08:48:06 +0300

    2016-11-08's Java workout

---
+++ contributions/benjaminCassard/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
