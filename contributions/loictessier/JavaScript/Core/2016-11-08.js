1650185118e41cf8062516d9e3806de298f6dacc
loictessier <loictessier.inge@gmail.com>
2016-11-08 11:15:24 +0300

    2016-11-08's JavaScript workout

---
+++ contributions/loictessier/JavaScript/Core/2016-11-08.js
@@ -0,0 +1,5 @@
+Quick way to format integers with a fixed number of digits.
+Shuffle an array using `sort` and `random`.
+Arrays can be used as stack
+`if/else` shortcut `conditional operator`
+Performing a function at timed intervals
