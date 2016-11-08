a1ffd3a6dffb9c29eb07877c5dd278534a9d88f0
Aylard <aylard7@gmail.com>
2016-11-08 19:50:50 +0300

    2016-11-08's JavaScript workout

---
+++ contributions/Aylard/JavaScript/Npm/2016-11-08.js
@@ -0,0 +1,5 @@
+Manipulate npm packages cache
+Manage local node modules with `npm link`
+`npm`  as an alternative to Gulp
+Package distribution tags
+Lock down dependency versions by shrinkwrapping
