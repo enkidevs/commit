ee4244816a3b4150f1cac5c4d61be9c848a99a0c
Ferror <wujek135@gmail.com>
2016-11-08 03:25:38 +0300

    2016-11-07's Java workout

---
+++ contributions/Ferror/Java/Data Types/2016-11-07.java
@@ -0,0 +1,5 @@
+Use `Arrays.asList()` to initialise lists
+Finding a substring in a string
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
+Do not attempt comparisons with NaN
