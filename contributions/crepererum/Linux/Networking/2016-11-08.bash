6138df9b13ec9f3b3dd5ecfce6aad53d5b9f8aa5
crepererum <marco@crepererum.net>
2016-11-09 00:48:15 +0300

    2016-11-08's Linux workout

---
+++ contributions/crepererum/Linux/Networking/2016-11-08.bash
@@ -0,0 +1,5 @@
+Stress testing with `ab`
+Find services running on your host
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
+Staying connected remotely via SSH without an interactive shell
