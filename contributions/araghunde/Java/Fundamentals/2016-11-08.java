629b8e440d432c4614115883c40ad7a204ba591b
araghunde <paybol.21@gmail.com>
2016-11-08 11:24:43 +0300

    2016-11-08's Java workout

---
+++ contributions/araghunde/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
