1045dd25e704727280f340e92748a74715e66816
ajp10304 <a.pocklington@gmail.com>
2016-11-08 09:32:23 +0300

    2016-11-08's Java workout

---
+++ contributions/ajp10304/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
