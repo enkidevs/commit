622a0823b8e64b753e9f11aad5cd246985e44925
neugartf <fabian.neugart@gmail.com>
2016-11-08 12:03:01 +0300

    2016-11-08's Java workout

---
+++ contributions/neugartf/Java/Error Handling/2016-11-08.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+Prevent `exceptions` while logging Data
+Throwing an `exception`
+Checking a double for `NaN` value
