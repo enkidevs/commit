9b125ce2110e4efcf4e743a7888b5061c52ba935
daiste <stefano.formaggi@gmail.com>
2016-11-08 03:51:53 +0300

    2016-11-07's Java workout

---
+++ contributions/daiste/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
