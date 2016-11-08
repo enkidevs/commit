88abd8cf89c0d22bf9d85cad8fff2203ed88850c
tswestendorp <contact@tswestendorp.nl>
2016-11-08 10:45:29 +0300

    2016-11-08's Linux workout

---
+++ contributions/tswestendorp/Linux/File Management/2016-11-08.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Working with `.gz` files
+Find the unknown process preventing deleting of files
