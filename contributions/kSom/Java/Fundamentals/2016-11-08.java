172b49bd83361f97118ffe8f56d13f7cb79de1f4
kSom <kgsommer@gmail.com>
2016-11-08 10:14:19 +0300

    2016-11-08's Java workout

---
+++ contributions/kSom/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use strings in a `switch` statement
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+How range operations work
