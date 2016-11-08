1a1b8809a6c6228fbe49a9f0e5d7832678ad5e71
Charlemagne <alexander-g@gmx.net>
2016-11-08 09:56:36 +0300

    2016-11-08's Python workout

---
+++ contributions/Charlemagne/Python/Data Structures/2016-11-08.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+When `range` comes in handy
+`Module`s everywhere!
+Enhance your `tuple`s
+Binary record packing with `bytearray`
