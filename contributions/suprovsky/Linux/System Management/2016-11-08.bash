6cc10f399b452a0dc462f919507a5334e2d1da32
suprovsky <rserba@gmail.com>
2016-11-08 11:16:23 +0300

    2016-11-08's Linux workout

---
+++ contributions/suprovsky/Linux/System Management/2016-11-08.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+List current logged on users with `w`
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Using `dd` To Create Disk Images
