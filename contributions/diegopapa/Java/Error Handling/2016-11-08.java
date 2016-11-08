0fc2ae7569b80b1e6c380123734433919e0d90c8
diegopapa <diego.papa.job@gmail.com>
2016-11-08 10:50:35 +0300

    2016-11-08's Java workout

---
+++ contributions/diegopapa/Java/Error Handling/2016-11-08.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Throwing an `exception`
+Careful Numeric Data Conversions
