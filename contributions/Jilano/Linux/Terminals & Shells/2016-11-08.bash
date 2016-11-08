447fa94c201e1782789ad82be25b3a7894c63d9c
Jilano <mobile.axel@gmail.com>
2016-11-08 08:45:26 +0300

    2016-11-08's Linux workout

---
+++ contributions/Jilano/Linux/Terminals & Shells/2016-11-08.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Execute a command without saving it in history
+Conditional command execution
+
+(`&&` operator)
+Retrieve previous arguments
+The Basics of Environment Variables
