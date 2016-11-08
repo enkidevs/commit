46952300d795b9fd17e1f752cb0c41343a3730cc
nicolas-schmitt <nicolas.schmitt@live.fi>
2016-11-08 15:39:43 +0300

    2016-11-08's JavaScript workout

---
+++ contributions/nicolas-schmitt/JavaScript/React/2016-11-08.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+componentWillReceiveProps Not Triggered After Mounting
+
+Shallow compare in **React**
+Custom validations for props
+Shortcut for transferring props
