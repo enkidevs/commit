391a111aa37f6c9624fb1a2d20af81a325f9807c
fffl <kuepfer.christopher@gmail.com>
2016-11-08 18:43:48 +0300

    2016-11-08's Java workout

---
+++ contributions/fffl/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Catch multiple exceptions in a single `catch` block
+Use `DecimalFormat` class to format numbers
+Detect or prevent integer overflow
+Avoid using `static` variables
