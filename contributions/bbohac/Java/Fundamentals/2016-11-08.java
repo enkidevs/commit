a3bae8888f056fc389e7d20e65ab7fffdf6efa84
bbohac <bastian.bohac@gmail.com>
2016-11-08 11:28:00 +0300

    2016-11-08's Java workout

---
+++ contributions/bbohac/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Java naming conventions
+Common mistake on switch statements
+Using `Printstream.format` to format a string before printing
