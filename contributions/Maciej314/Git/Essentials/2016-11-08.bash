a1a2aabef9d720ac935957024a9620d2d88bd2ff
Maciej314 <msatkiewicz@gmail.com>
2016-11-08 21:36:00 +0300

    2016-11-08's Git workout

---
+++ contributions/Maciej314/Git/Essentials/2016-11-08.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+Using aliases for git commands
+How to set your username and email
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
