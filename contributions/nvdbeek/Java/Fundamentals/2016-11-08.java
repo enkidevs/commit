510747715fcf15f4667e94ea7d7775b8a4e6c326
nvdbeek <nielsvdbeek@gmail.com>
2016-11-08 14:16:40 +0300

    2016-11-08's Java workout

---
+++ contributions/nvdbeek/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
