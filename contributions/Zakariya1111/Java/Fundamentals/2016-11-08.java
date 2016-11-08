1e4c244113769b4323e25a2087b40b530051d4c3
Zakariya1111 <elaizaidyahoo.fr@gmail.com>
2016-11-08 17:48:36 +0300

    2016-11-08's Java workout

---
+++ contributions/Zakariya1111/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Use meaningful names
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+How range operations work
