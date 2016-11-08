3515baa1c6cfaf6d24ba4e3d09c87711f8cf59e2
bzpuerta <beatriz.puerta.hoyas@gmail.com>
2016-11-08 13:04:47 +0300

    2016-11-08's Java workout

---
+++ contributions/bzpuerta/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Catch multiple exceptions in a single `catch` block
+Use `DecimalFormat` class to format numbers
+Constant notation
+Detect or prevent integer overflow
