4731ad2cd00a1aa5dfec7a2da212500747d1203a
vbalko <vbalko@gmail.com>
2016-11-08 23:21:42 +0300

    2016-11-08's Java workout

---
+++ contributions/vbalko/Java/Error Handling/2016-11-08.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
