746d0d305dc8b4b4cbbf4ec36da3ed342f26738f
gobbas <gabbi@hotmail.no>
2016-11-08 11:39:32 +0300

    2016-11-08's Java workout

---
+++ contributions/gobbas/Java/I/O/2016-11-08.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+The `Console` class
