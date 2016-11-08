ce2be1397860371d02e3cb6aec7e38f5567d7673
bobbly13 <rpackwood@gmail.com>
2016-11-08 04:11:22 +0300

    2016-11-07's Java workout

---
+++ contributions/bobbly13/Java/Fundamentals/2016-11-07.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
