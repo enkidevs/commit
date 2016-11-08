edf3d18a0b5498b284addd9cc89381921443e461
CrystalCastles <danielpichardo716@gmail.com>
2016-11-09 02:08:36 +0300

    2016-11-08's Java workout

---
+++ contributions/CrystalCastles/Java/Fundamentals/2016-11-08.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Using an interface as a parameter
