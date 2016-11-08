a6f5cf36b67fb92424f4d188fa5a2d063330077c
sikoran31 <sikoran31@gmail.com>
2016-11-08 20:03:51 +0300

    2016-11-08's Python workout

---
+++ contributions/sikoran31/Python/Data Structures/2016-11-08.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`Dictionary` view objects
+Your own Python `calendar` 
+The fundamental `tuples` 
+Get more with `collections`!
