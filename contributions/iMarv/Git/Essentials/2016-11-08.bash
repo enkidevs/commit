4db870604d6c24848341b70cf263829556b0c0ce
iMarv <marv.internetz@gmail.com>
2016-11-08 10:23:31 +0300

    2016-11-08's Git workout

---
+++ contributions/iMarv/Git/Essentials/2016-11-08.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+Pushing tags to a server
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: fetching and pushing
