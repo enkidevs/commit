e08ec487e89727294d1a39d84dff142e6e37f090
Bwvolleyball <ward486@regis.edu>
2016-11-09 17:03:43 +0300

    2016-11-09's Java workout

---
+++ contributions/Bwvolleyball/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
