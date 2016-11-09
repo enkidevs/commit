c29a24f00a915cc49bf1746826e7f2e0dbd9bf66
Sardtok <sha@computas.com>
2016-11-09 17:10:12 +0300

    2016-11-09's JavaScript workout

---
+++ contributions/Sardtok/JavaScript/Core/2016-11-09.js
@@ -0,0 +1,5 @@
+Multi-line string variables
+The difference between null, undefined and NaN
+Prevent background image flicker in **IE**
+How to merge two arrays
+Inaccuracy of binary floating-point format
