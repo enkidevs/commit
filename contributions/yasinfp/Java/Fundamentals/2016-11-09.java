5e2f709137963ae206d55c67f9228af5f79fc9c8
yasinfp <yfpayalan@gmail.com>
2016-11-09 23:40:56 +0300

    2016-11-09's Java workout

---
+++ contributions/yasinfp/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
