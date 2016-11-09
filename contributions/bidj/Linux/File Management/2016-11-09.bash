14587331e05c4f0d708ab9a7e726ad757483e965
bidj <enki@papito.33mail.com>
2016-11-09 16:44:59 +0300

    2016-11-09's Linux workout

---
+++ contributions/bidj/Linux/File Management/2016-11-09.bash
@@ -0,0 +1,5 @@
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
+Delete commands aliases
