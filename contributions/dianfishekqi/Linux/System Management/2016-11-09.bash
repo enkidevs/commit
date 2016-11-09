efd49c09cb3ff44b3f30ffbb36f4b3748c9cf4ca
dianfishekqi <dianfishekqi@gmail.com>
2016-11-09 11:24:01 +0300

    2016-11-09's Linux workout

---
+++ contributions/dianfishekqi/Linux/System Management/2016-11-09.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
