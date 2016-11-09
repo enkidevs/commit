8947fbdbf2eeac596f3cfa5b8d037e2f33ca6696
theswolf <chris.geymo@gmail.com>
2016-11-09 11:24:10 +0300

    2016-11-09's Java workout

---
+++ contributions/theswolf/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
