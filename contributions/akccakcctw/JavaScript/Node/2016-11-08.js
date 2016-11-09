43d005f614f5ae8f71842bfdb153b02db1c415b7
akccakcctw <akccakccwww@gmail.com>
2016-11-09 04:12:40 +0300

    2016-11-08's JavaScript workout

---
+++ contributions/akccakcctw/JavaScript/Node/2016-11-08.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Asynchronous File Write/Read in Node.js
+The built-in Node debugger
+`process.nextTick()` callback pattern
+Manage child processes in **Node**
