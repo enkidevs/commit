76c08166de4287a1356b2fc04b10b4960f3ff8f3
eney <jzabrodin@gmail.com>
2016-11-09 14:25:24 +0300

    2016-11-09's Linux workout

---
+++ contributions/eney/Linux/File Management/2016-11-09.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
