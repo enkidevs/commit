c8b555058ae3b3fb90a8e3067f604812115a94f5
ross-f <rssfltchr@gmail.com>
2016-11-09 13:54:42 +0300

    2016-11-09's Java workout

---
+++ contributions/ross-f/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Common mistake on switch statements
+Using `Printstream.format` to format a string before printing
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
