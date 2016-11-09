6d015117ed072bdd3ebdec38d7a198cc77f4d541
TomRaven <16lazart@elmbrookstudents.org>
2016-11-09 21:34:09 +0300

    2016-11-09's Java workout

---
+++ contributions/TomRaven/Java/I/O/2016-11-09.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Using buffered streams
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
