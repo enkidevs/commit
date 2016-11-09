b6686529a0010ffd53b8a8729fcc9295091528d7
edm00se <edm00se@gmail.com>
2016-11-09 15:33:54 +0300

    2016-11-09's Java workout

---
+++ contributions/edm00se/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Difference between `==` and `equals()`
+Generating random numbers
+The trade-off between short and descriptive names
+`Autoboxing` and `Unboxing`
