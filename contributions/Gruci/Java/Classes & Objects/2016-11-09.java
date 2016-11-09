9231d0265993c30f077ece53287f91f4e23975a0
Gruci <skeksksk@gmail.com>
2016-11-10 02:59:24 +0300

    2016-11-09's Java workout

---
+++ contributions/Gruci/Java/Classes & Objects/2016-11-09.java
@@ -0,0 +1,5 @@
+Template methods in abstract classes
+Compare classes and not class names
+Double Brace Initialization
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
