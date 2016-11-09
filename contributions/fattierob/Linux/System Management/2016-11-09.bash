7f253a0ec2941fdc99129ef1dc058ec63b761849
fattierob <senkan343@gmail.com>
2016-11-09 21:57:31 +0300

    2016-11-09's Linux workout

---
+++ contributions/fattierob/Linux/System Management/2016-11-09.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
