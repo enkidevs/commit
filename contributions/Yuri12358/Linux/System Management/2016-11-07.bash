1f71dfb193b123f76c8eb0f027ffc1528ee3876f
Yuri12358 <hydra12358@gmail.com>
2016-11-10 01:04:55 +0300

    2016-11-07's Linux workout

---
+++ contributions/Yuri12358/Linux/System Management/2016-11-07.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Get hardware stack details with `lspci`
+Cancel a system shutdown
