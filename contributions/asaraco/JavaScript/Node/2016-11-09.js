0761bb028ab8de0b1417194de6888d19abaece27
asaraco <sirlemming@gmail.com>
2016-11-09 22:40:10 +0300

    2016-11-09's JavaScript workout

---
+++ contributions/asaraco/JavaScript/Node/2016-11-09.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Http Server in **Node**
+Global Objects and Environment Variables in **Node**
+Automatic compilation for Node with **Nodemon** 
+Middleware/pipeline design pattern
