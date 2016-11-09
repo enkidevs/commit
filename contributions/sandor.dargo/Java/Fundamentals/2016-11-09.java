f54b64fcf6de1df25f74070f91c49404748ffa90
sandor.dargo <sandor.dargo@gmail.com>
2016-11-09 08:08:41 +0300

    2016-11-09's Java workout

---
+++ contributions/sandor.dargo/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+How range operations work
