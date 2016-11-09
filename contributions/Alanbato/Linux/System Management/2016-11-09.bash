2e569fb2e837a6fa320244fcbbe9312812a85b10
Alanbato <alanvelasco.a@gmail.com>
2016-11-09 22:20:14 +0300

    2016-11-09's Linux workout

---
+++ contributions/Alanbato/Linux/System Management/2016-11-09.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Detect your linux distribution
+Changing the timezone on deb based systems
+Changing the `/tmp` cleanup frequency
+Monitor System Memory using `vmstat`
