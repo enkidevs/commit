d0667766a9a2628264be2a2565b7f9ef2626e2e2
lisotton <leandro@isotton.co>
2016-11-10 00:23:36 +0300

    2016-11-09's Linux workout

---
+++ contributions/lisotton/Linux/File Management/2016-11-09.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
