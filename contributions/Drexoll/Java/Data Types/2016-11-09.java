8e9275db0c8ba7b60819ac1f9fda56acff7da259
Drexoll <darcy@drexollgames.com>
2016-11-09 22:30:57 +0300

    2016-11-09's Java workout

---
+++ contributions/Drexoll/Java/Data Types/2016-11-09.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Two ways to use an `Iterator`
+Converting a string to upper or lower case
+Equals operation on different data types
+Retrieve the component type of an array
