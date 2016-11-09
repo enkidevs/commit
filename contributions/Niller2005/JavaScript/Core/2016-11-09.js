8d90422428c4d62e8628f91316a8410f74d9f6b9
Niller2005 <jnillern@gmail.com>
2016-11-10 00:08:24 +0300

    2016-11-09's JavaScript workout

---
+++ contributions/Niller2005/JavaScript/Core/2016-11-09.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Keywords in hash keys
+Counting the months from zero
+Hoisting applies only to variable declarations, not initializations
+Extra function arguments are undefined by default
