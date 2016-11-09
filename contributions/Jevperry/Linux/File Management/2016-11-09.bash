be59159aef8e62f7c9d4c0238e6c272c09f289b1
Jevperry <jvegaperry@gmail.com>
2016-11-09 19:45:26 +0300

    2016-11-09's Linux workout

---
+++ contributions/Jevperry/Linux/File Management/2016-11-09.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Find the unknown process preventing deleting of files
+Easy way of sharing files
