c894694977d0fe8fab134db332852201b9b8a1b5
fabianomartino <fabianomartino@gmail.com>
2016-11-09 22:37:17 +0300

    2016-11-09's Linux workout

---
+++ contributions/fabianomartino/Linux/File Management/2016-11-09.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Using `touch` to alter files modification time
