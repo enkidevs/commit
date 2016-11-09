128abdf7b92f3664e37c1322105fdf6efe03862f
gianlucaschoefer <gianlucaschoefer@gmail.com>
2016-11-09 15:34:28 +0300

    2016-11-09's Java workout

---
+++ contributions/gianlucaschoefer/Java/Error Handling/2016-11-09.java
@@ -0,0 +1,5 @@
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Careful Numeric Data Conversions
