117ad68f42cec5b7888a31d823c7187c24a78273
César <cesarbernedo@free.fr>
2016-11-09 20:44:27 +0300

    2016-11-09's Java workout

---
+++ contributions/César/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Use meaningful names
+The trade-off between short and descriptive names
+Constant notation
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
