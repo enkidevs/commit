b7a02b01b7f6c38dbf865e77a66501e525b085de
nikoo <grula93@gmail.com>
2016-11-09 10:56:41 +0300

    2016-11-09's Java workout

---
+++ contributions/nikoo/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
