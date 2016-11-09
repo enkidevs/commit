41f3905f35e1df30960d15e1b3509ea6958cceb5
lolporer <lolporer@gmail.com>
2016-11-09 14:46:38 +0300

    2016-11-09's Linux workout

---
+++ contributions/lolporer/Linux/File Management/2016-11-09.bash
@@ -0,0 +1,6 @@
+Get disk space usage with `df`
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
