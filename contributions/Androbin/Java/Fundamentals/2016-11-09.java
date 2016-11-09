8483adaef95988c96df9efdc398f3acacd097167
Androbin <robin.richtsfeld@gmail.com>
2016-11-09 14:58:45 +0300

    2016-11-09's Java workout

---
+++ contributions/Androbin/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
