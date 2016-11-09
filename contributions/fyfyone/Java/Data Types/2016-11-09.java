507c72f74cc987e7147be3e46ff7860c7f9e65d6
fyfyone <fyfyone@gmail.com>
2016-11-10 00:35:01 +0300

    2016-11-09's Java workout

---
+++ contributions/fyfyone/Java/Data Types/2016-11-09.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
+Numeric Conversion - Widening
