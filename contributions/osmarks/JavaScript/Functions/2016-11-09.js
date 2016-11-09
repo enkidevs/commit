564e4b30b888006960dfbf4c3b7551bd3aafb387
osmarks <osmarks@icloud.com>
2016-11-10 01:49:33 +0300

    2016-11-09's JavaScript workout

---
+++ contributions/osmarks/JavaScript/Functions/2016-11-09.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Easily generate a random `HEX` color
+Move cursor at the end of text input
+Check if a document is done loading
