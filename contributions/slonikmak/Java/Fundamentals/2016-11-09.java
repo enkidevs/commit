373893f5dd5d4af0c9c0e67a63eb327d65512f9d
slonikmak <slonikmak@gmail.com>
2016-11-09 09:12:10 +0300

    2016-11-09's Java workout

---
+++ contributions/slonikmak/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Using Inheritance to reduce code repetition
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
