3ae82179b0ca1670333e53f70df513d995d2e620
Grorel <grorel.kazui@gmail.com>
2016-11-09 10:29:25 +0300

    2016-11-09's JavaScript workout

---
+++ contributions/Grorel/JavaScript/Npm/2016-11-09.js
@@ -0,0 +1,5 @@
+Registry user accounts for npm
+List binaries for scripting in npm
+Environmental variables and flags for `npm`
+`optionalDependencies` in npm
+Lock down dependency versions by shrinkwrapping
