29e0aca2c88e4b2f2a21d7c790e9c26db885ff5c
comeste10 <comeste10@gmail.com>
2016-11-09 09:37:26 +0300

    2016-11-08's Linux workout

---
+++ contributions/comeste10/Linux/File Management/2016-11-08.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
