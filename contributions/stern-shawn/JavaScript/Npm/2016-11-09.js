406098739e0284db14436f2b6f3e8711f8585f00
stern-shawn <stern.shawn@gmail.com>
2016-11-09 22:08:08 +0300

    2016-11-09's JavaScript workout

---
+++ contributions/stern-shawn/JavaScript/Npm/2016-11-09.js
@@ -0,0 +1,5 @@
+npm can handle multiple versions of a dependency
+Environmental variables and flags for `npm`
+`npm`  as an alternative to Gulp
+`npm` verifies `sha1` hashes of packages
+Package distribution tags
