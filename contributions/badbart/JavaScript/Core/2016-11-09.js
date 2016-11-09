14d9cd281130bbf911a3d637196b60e939befbf7
badbart <tom.zille@gmail.com>
2016-11-09 08:55:25 +0300

    2016-11-09's JavaScript workout

---
+++ contributions/badbart/JavaScript/Core/2016-11-09.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Setting default values with `short circuiting`
+Use `splice()` to remove an item from an array
+Check if a function exists
+Base conversion 
