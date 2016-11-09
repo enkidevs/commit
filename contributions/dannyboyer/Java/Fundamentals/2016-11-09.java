3e181d1aed67487be0ecbd1ee8451aef21bd94d9
dannyboyer <dannyboyer49@gmail.com>
2016-11-09 17:15:23 +0300

    2016-11-09's Java workout

---
+++ contributions/dannyboyer/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
