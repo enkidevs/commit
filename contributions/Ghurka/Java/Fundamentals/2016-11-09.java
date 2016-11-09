1b8bddc011d8dcff55ddbef21939b1595ce77e93
Ghurka <marcojcapo@gmail.com>
2016-11-09 13:48:23 +0300

    2016-11-09's Java workout

---
+++ contributions/Ghurka/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Difference between JRE and JDK?
+Java naming conventions
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
