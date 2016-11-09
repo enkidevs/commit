d81980b8bf60f1399ac4bee09990cc11aab0d66b
MichaelRijsdijk <michaelrijsdijk@gmail.com>
2016-11-09 11:12:43 +0300

    2016-11-09's Java workout

---
+++ contributions/MichaelRijsdijk/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Avoid using `static` variables
