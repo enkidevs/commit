71e1a1712df8da0a014562c96c3fcd662f6ea417
Heartbroken-Git <corentin196@gmail.com>
2016-11-09 19:20:35 +0300

    2016-11-09's Java workout

---
+++ contributions/Heartbroken-Git/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+`Autoboxing` and `Unboxing`
