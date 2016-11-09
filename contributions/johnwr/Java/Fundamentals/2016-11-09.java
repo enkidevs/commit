83302ad9073bd3ef8cc4d30c810634080562a9e6
johnwr <johnwr_enki@wraalsen.no>
2016-11-09 11:09:54 +0300

    2016-11-09's Java workout

---
+++ contributions/johnwr/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use strings in a `switch` statement
+Using bounded type parameters in generic methods
+Using `synchronized` statements
+Supply `toString()` in all classes
