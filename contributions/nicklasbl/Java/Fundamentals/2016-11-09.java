3fcff0d5b5400270969501acd98477d44a20836b
nicklasbl <nicklasbl@gmail.com>
2016-11-09 10:35:35 +0300

    2016-11-09's Java workout

---
+++ contributions/nicklasbl/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Using `synchronized` statements
+Detect or prevent integer overflow
+Using an interface as a parameter
