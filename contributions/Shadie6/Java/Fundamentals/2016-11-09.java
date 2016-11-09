4f2bec155b032d50560949b18ed8b8aa5a1d7fcb
Shadie6 <l.meroni6@gmail.com>
2016-11-09 13:01:13 +0300

    2016-11-09's Java workout

---
+++ contributions/Shadie6/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
