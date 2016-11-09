c8fd7068a890eb96fc50b72bd7bfc87415622a24
nelsonio <martin23haupt@gmail.com>
2016-11-10 00:07:42 +0300

    2016-11-09's JavaScript workout

---
+++ contributions/nelsonio/JavaScript/Node/2016-11-09.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+First-error callbacks in **Node**
+Node Inspector
+Use conventional property names for error handling in Node.js
+`process.nextTick()` callback pattern
