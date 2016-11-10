6160917317a8da44ca8b6be8102911ad861acd28
jogli5er <roman.brunner94@gmail.com>
2016-11-10 10:28:20 +0300

    2016-11-10's Linux workout

---
+++ contributions/jogli5er/Linux/System Management/2016-11-10.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Finding Open Files With `lsof`
