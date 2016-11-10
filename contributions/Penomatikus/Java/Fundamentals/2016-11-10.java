ff59d1d4ddb5413d6a46e410823f057aaa84e040
Penomatikus <sjagdmann10@googlemail.com>
2016-11-10 15:35:31 +0300

    2016-11-10's Java workout

---
+++ contributions/Penomatikus/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use meaningful names
+Using `Printstream.format` to format a string before printing
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
