6497c6a7bc7f67789152cd6aab55dba4cdcc5bea
gabriele92 <gabrigaudenzi@gmail.com>
2016-11-10 11:43:02 +0300

    2016-11-10's Java workout

---
+++ contributions/gabriele92/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Writing generic methods
+Limit Accessibility of `Fields`
+Avoid using `static` variables
+Using an interface as a parameter
