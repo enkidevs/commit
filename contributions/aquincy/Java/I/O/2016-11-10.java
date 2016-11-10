0cfaf606954a913b40f78531749c9e7077146ebb
aquincy <brennanjl003@gmail.com>
2016-11-10 23:15:54 +0300

    2016-11-10's Java workout

---
+++ contributions/aquincy/Java/I/O/2016-11-10.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+How to list all files in a directory that match a filename extension in Java
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
