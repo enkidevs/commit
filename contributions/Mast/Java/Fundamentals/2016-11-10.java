5fc85d4674ed65529ee24ca1eb3529b654db82d0
Mast <jeremie.hambye@gmail.com>
2016-11-10 12:43:09 +0300

    2016-11-10's Java workout

---
+++ contributions/Mast/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
+Using an interface as a parameter
