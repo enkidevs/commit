3e9f3151a7a5633c9083b35950394b5521507ae8
at_rune <nico.vansnick@gmail.com>
2016-11-10 23:10:23 +0300

    2016-11-10's Linux workout

---
+++ contributions/at_rune/Linux/File Management/2016-11-10.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Deleting non-empty directories
