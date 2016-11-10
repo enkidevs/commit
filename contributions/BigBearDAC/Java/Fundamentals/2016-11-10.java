0ba1b4bbe4163a6d3d8afc96df0f6007878c1882
BigBearDAC <flidnibble@gmail.com>
2016-11-11 00:56:50 +0300

    2016-11-10's Java workout

---
+++ contributions/BigBearDAC/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between ```HashMap``` and ```Hashtable```
+Common mistake on switch statements
+Using `Printstream.format` to format a string before printing
+Calling one constructor from another
