543f43ea430a234f7cd368a8242d727beb0382bb
JayDamon <jay.n.damon@gmail.com>
2016-11-10 18:54:41 +0300

    2016-11-10's Java workout

---
+++ contributions/JayDamon/Java/I/O/2016-11-10.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Listing a file system's root directories
+Deleting files and directories
