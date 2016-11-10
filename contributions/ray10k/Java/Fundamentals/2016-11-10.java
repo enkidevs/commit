50877f90a7723d7d631b8e907165d1d7af8534f6
ray10k <w.j.van.den.heuvel@xmsnet.nl>
2016-11-10 12:35:38 +0300

    2016-11-10's Java workout

---
+++ contributions/ray10k/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
