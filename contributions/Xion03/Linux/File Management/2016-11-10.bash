079bd7959190d9a490925cf8b7f6a3ee9df2f5a7
Xion03 <xion03@gmail.com>
2016-11-10 11:42:27 +0300

    2016-11-10's Linux workout

---
+++ contributions/Xion03/Linux/File Management/2016-11-10.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Working with `.gz` files
