fba71e7ae1b6820bba7096dae0afb3fad524be06
lorena <lorena.blue.72@gmail.com>
2016-11-11 01:23:09 +0300

    2016-11-10's JavaScript workout

---
+++ contributions/lorena/JavaScript/Node/2016-11-10.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+The built-in Node debugger
+Use conventional property names for error handling in Node.js
+`process.nextTick()` callback pattern
+Clustering
