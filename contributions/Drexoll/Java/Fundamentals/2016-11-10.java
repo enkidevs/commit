477e2969239c266e591fc4d7246eb305c9b28b1d
Drexoll <darcy@drexollgames.com>
2016-11-11 02:00:07 +0300

    2016-11-10's Java workout

---
+++ contributions/Drexoll/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+Using Inheritance to reduce code repetition
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+`Autoboxing` and `Unboxing`
+Supply `toString()` in all classes
