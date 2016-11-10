4ac4e0e2178758a1c8b75b569c6a34532378f77b
vbalko <vbalko@gmail.com>
2016-11-10 16:19:31 +0300

    2016-11-10's Java workout

---
+++ contributions/vbalko/Java/Classes & Objects/2016-11-10.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Utility classes and `static` methods
+The `abstract` keyword
+Compare classes and not class names
+String formatting and parsing with `SimpleDateFormat`
