f4c27500d5a4505765b87f1e4b4dff6b5f3d34f6
ceski23 <ceski23@gmail.com>
2016-11-10 22:10:18 +0300

    2016-11-10's Java workout

---
+++ contributions/ceski23/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Using Inheritance to reduce code repetition
+Metadata: setting a file's owner
+Using `synchronized` statements
+`Autoboxing` and `Unboxing`
