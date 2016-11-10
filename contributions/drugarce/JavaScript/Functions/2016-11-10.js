32a11ddcfddb71e2a7a56cc61454f6498bf8ad5f
drugarce <drugarce@mt.net.mk>
2016-11-10 22:22:52 +0300

    2016-11-10's JavaScript workout

---
+++ contributions/drugarce/JavaScript/Functions/2016-11-10.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Method chaining
+Get query/url variables
+Easily generate a random `HEX` color
