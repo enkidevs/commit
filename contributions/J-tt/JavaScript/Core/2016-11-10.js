96185afd2126a062def4f7fd610f0ffdee7f61df
J-tt <jacksonjett5@gmail.com>
2016-11-11 01:25:06 +0300

    2016-11-10's JavaScript workout

---
+++ contributions/J-tt/JavaScript/Core/2016-11-10.js
@@ -0,0 +1,5 @@
+Types of numbers
+Use `splice()` to remove an item from an array
+Data type comparison in `switch` statements
+The difference between null, undefined and NaN
+Round numbers to `N` decimals 
