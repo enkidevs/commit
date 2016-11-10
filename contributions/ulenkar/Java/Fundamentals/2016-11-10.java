d7960f72c77ba6aaf4f2721088e7ebcd629a74cf
ulenkar <ulka@o2.pl>
2016-11-10 20:12:37 +0300

    2016-11-10's Java workout

---
+++ contributions/ulenkar/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Calling one constructor from another
