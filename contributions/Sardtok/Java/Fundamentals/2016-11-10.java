af2b1119cf7cfed53f7a86d3a88505f8dccf7c0b
Sardtok <sha@computas.com>
2016-11-10 14:25:22 +0300

    2016-11-10's Java workout

---
+++ contributions/Sardtok/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
