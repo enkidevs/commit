18265892f10d20061d1d7f1ba35e0a347e99d647
Thommi <thomasandre84@gmail.com>
2016-11-10 21:37:15 +0300

    2016-11-10's Java workout

---
+++ contributions/Thommi/Java/I/O/2016-11-10.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Checking for the existence of files and directories
+Retrieving file store attributes
+Listing a file system's root directories
