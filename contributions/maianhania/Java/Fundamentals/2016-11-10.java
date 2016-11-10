f9ee729e148507ec3c82afffb7c2932462c58630
maianhania <maianhania@yahoo.com>
2016-11-10 12:25:54 +0300

    2016-11-10's Java workout

---
+++ contributions/maianhania/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Writing generic methods
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
+Calling one constructor from another
