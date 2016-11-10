11a4609ed6174b138042896637d5617a8b18f808
MosanZH <mosan_zh@163.com>
2016-11-10 03:57:54 +0300

    2016-11-09's Java workout

---
+++ contributions/MosanZH/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
