707bd4066295e8b99101fab5158177aa0e450783
JLouis <jlsalbego@gmail.com>
2016-11-10 15:23:50 +0300

    2016-11-10's JavaScript workout

---
+++ contributions/JLouis/JavaScript/Node/2016-11-10.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
