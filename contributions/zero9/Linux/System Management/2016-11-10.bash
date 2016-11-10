520496328b19ebe5d3bfc944af7bb8995274a0a6
zero9 <c.lobrano@gmail.com>
2016-11-10 15:09:59 +0300

    2016-11-10's Linux workout

---
+++ contributions/zero9/Linux/System Management/2016-11-10.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Test disk speed with `dd`
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
