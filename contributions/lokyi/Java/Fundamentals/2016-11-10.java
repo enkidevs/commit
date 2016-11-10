5033ec726943fb72a880b06fa4847e74e5b37dc7
lokyi <roy.yi.yue@gmail.com>
2016-11-11 00:22:51 +0300

    2016-11-10's Java workout

---
+++ contributions/lokyi/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+Ternary operator
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
