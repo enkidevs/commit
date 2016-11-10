be87bb13d654b0fb320b877345b8dd2b644e0193
gobbas <gabbi@hotmail.no>
2016-11-11 00:07:44 +0300

    2016-11-10's Java workout

---
+++ contributions/gobbas/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The trade-off between short and descriptive names
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+`Autoboxing` and `Unboxing`
