8a45eb3a2d3f1878328cda0f767e2f850499d59b
peacestorm <rjanse48@gmail.com>
2016-11-10 16:37:21 +0300

    2016-11-10's Java workout

---
+++ contributions/peacestorm/Java/Error Handling/2016-11-10.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Throwing an `exception`
+Careful Numeric Data Conversions
