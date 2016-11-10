d174c6b4821c4193f649144492930a739fbc9d02
GazCMarsh <gazmarsh@meaningfulname.net>
2016-11-10 15:14:57 +0300

    2016-11-10's JavaScript workout

---
+++ contributions/GazCMarsh/JavaScript/Browser APIs/2016-11-10.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Async and defer scripts
+Allow an element to go fullscreen
+Warn user if **Back** button is pressed
+Notifications API
