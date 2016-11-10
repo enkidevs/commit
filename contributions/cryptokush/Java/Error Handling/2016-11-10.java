75eb62ddd18ca1864f1fbc01dc921dedfffb9436
cryptokush <mdalek@gmail.com>
2016-11-10 10:22:57 +0300

    2016-11-10's Java workout

---
+++ contributions/cryptokush/Java/Error Handling/2016-11-10.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Prevent `exceptions` while logging Data
