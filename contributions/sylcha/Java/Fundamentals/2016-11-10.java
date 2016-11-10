ab77fbdddd3a02225a38c78ff9f4bcb950bfe36b
sylcha <chambon.sylvain@gmail.com>
2016-11-10 09:22:33 +0300

    2016-11-10's Java workout

---
+++ contributions/sylcha/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Writing generic methods
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
