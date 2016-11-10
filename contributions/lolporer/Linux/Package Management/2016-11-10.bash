845466480836bfbc63edf20a4ed32192b025a404
lolporer <lolporer@gmail.com>
2016-11-10 17:56:56 +0300

    2016-11-10's Linux workout

---
+++ contributions/lolporer/Linux/Package Management/2016-11-10.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Prevent updating a specific package in Debian systems
+Using `Tasksel` for software installation
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
