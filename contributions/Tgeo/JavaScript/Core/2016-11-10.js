5b5c29af53c5640421471ff7bcb3efa380043e3e
Tgeo <tylergeonetta@gmail.com>
2016-11-10 20:54:09 +0300

    2016-11-10's JavaScript workout

---
+++ contributions/Tgeo/JavaScript/Core/2016-11-10.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Using assignment operators
+Detect online connection
+Getting the *real* dimensions of an image
+Inaccuracy of binary floating-point format
