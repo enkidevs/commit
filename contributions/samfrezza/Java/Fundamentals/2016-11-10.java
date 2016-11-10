62853801a55fe23359d2f9f0d64be60e36ad9424
samfrezza <samfrezza@gmail.com>
2016-11-10 12:57:45 +0300

    2016-11-10's Java workout

---
+++ contributions/samfrezza/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
