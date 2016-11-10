44955bc540d6e8fb6a7930bb398628ff7f48c504
clausewtz45 <clausewitz45@gmail.com>
2016-11-10 10:53:35 +0300

    2016-11-10's Linux workout

---
+++ contributions/clausewtz45/Linux/System Management/2016-11-10.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
