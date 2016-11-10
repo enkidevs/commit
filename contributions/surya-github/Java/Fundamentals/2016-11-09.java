42646e7886c6373f3298ddb6db872751b264bd99
surya-github <suryaprabhakark@gmail.com>
2016-11-10 07:38:01 +0300

    2016-11-09's Java workout

---
+++ contributions/surya-github/Java/Fundamentals/2016-11-09.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Writing generic methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
