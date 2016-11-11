4eb866fcc2a982cab521f50689dd6fa440d69db8
srbaxter <srosebaxter@gmail.com>
2016-11-11 15:46:44 +0300

    2016-11-11's JavaScript workout

---
+++ contributions/srbaxter/JavaScript/Ecmascript 2015/2016-11-11.js
@@ -0,0 +1,5 @@
+Computed property names in object literals
+Default function parameters
+Defaults values apply only to `undefined` (and not to `null`)
+Symbols in ES6
+Binary and octal notation in ES6
