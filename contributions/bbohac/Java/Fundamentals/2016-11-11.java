7985ff29a03d17c7457f02b6326d24e9f68750af
bbohac <bastian.bohac@gmail.com>
2016-11-11 14:19:14 +0300

    2016-11-11's Java workout

---
+++ contributions/bbohac/Java/Fundamentals/2016-11-11.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Java naming conventions
+Using Inheritance to reduce code repetition
+Metadata: creating a user-defined file attribute
