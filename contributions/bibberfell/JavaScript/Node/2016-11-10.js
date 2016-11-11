466e86a326d6061daef4c94bcc96380fbc66ce39
bibberfell <bibberfell@gmail.com>
2016-11-11 10:35:17 +0300

    2016-11-10's JavaScript workout

---
+++ contributions/bibberfell/JavaScript/Node/2016-11-10.js
@@ -0,0 +1,5 @@
+First-error callbacks in **Node**
+Use conventional property names for error handling in Node.js
+`process.nextTick()` callback pattern
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
