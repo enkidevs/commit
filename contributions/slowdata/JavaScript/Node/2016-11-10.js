4bf8e153f545c1baf7eee8b3ef483a70ccdeb53c
slowdata <slowmotion@outlook.com>
2016-11-11 03:07:18 +0300

    2016-11-10's JavaScript workout

---
+++ contributions/slowdata/JavaScript/Node/2016-11-10.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+First-error callbacks in **Node**
+How to debug Node.js
+Handle `JSON.parse` error in Node.js
+Automatic compilation for Node with **Nodemon** 
