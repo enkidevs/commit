f3600697c601703fdbe7919fb737df21a9913e15
Flos <fn187-github@yahoo.de>
2016-11-11 11:20:08 +0300

    2016-11-11's Linux workout

---
+++ contributions/Flos/Linux/Networking/2016-11-11.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Find services running on your host
+Multiple IP Address with aliasing 
+Getting the connection speed from the terminal
+Staying connected remotely via SSH without an interactive shell
