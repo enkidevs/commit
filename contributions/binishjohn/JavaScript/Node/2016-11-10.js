7cf40632c63ba2cc3d98f460224894581e91a720
binishjohn <binish.heptas@gmail.com>
2016-11-11 03:01:02 +0300

    2016-11-10's JavaScript workout

---
+++ contributions/binishjohn/JavaScript/Node/2016-11-10.js
@@ -0,0 +1,5 @@
+Wrapping errors in Node.js using _node-verror_
+`process.nextTick()` callback pattern
+Bulk-Write in **Node** with `cork()`
+Middleware/pipeline design pattern
+Avoid command injection 
