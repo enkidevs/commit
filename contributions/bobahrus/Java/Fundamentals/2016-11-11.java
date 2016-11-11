972abde9e89577388b99e2048c823dbe30828805
bobahrus <bobahrus@gmail.com>
2016-11-11 20:13:31 +0300

    2016-11-11's Java workout

---
+++ contributions/bobahrus/Java/Fundamentals/2016-11-11.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Always specify type parameter
+`Autoboxing` and `Unboxing`
+Supply `toString()` in all classes
