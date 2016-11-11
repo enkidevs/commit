7d2a8cf8d7ed93e3abc842f4a424bd31f37e77bb
4lloyd <lloyd4post11@hotmail.com>
2016-11-11 10:37:15 +0300

    2016-11-11's Java workout

---
+++ contributions/4lloyd/Java/Fundamentals/2016-11-11.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+The trade-off between short and descriptive names
+Use strings in a `switch` statement
+Always specify type parameter
+Supply `toString()` in all classes
