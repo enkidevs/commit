a395b217c574db1e50b4686752a4317e5b865b62
tnm717 <thomas.masterson@gmail.com>
2016-11-11 20:09:46 +0300

    2016-11-11's JavaScript workout

---
+++ contributions/tnm717/JavaScript/Node/2016-11-11.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+The built-in Node debugger
+Automatic compilation for Node with **Nodemon** 
+Manage child processes in **Node**
+`try-catch` only for **sync** code
