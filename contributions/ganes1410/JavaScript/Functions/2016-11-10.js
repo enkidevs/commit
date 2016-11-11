96ab0b2e8f3c4dadc41a58a8881df08af4231435
ganes1410 <ganesramesh110@gmail.com>
2016-11-11 10:17:01 +0300

    2016-11-10's JavaScript workout

---
+++ contributions/ganes1410/JavaScript/Functions/2016-11-10.js
@@ -0,0 +1,5 @@
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Method chaining
+Easily generate a random `HEX` color
+Check if a document is done loading
