fc6fab993a51af5cc0f2c83c0b7381b40ad8d162
charlescodes <charlescodes@gmail.com>
2016-11-11 21:33:56 +0300

    2016-11-11's Java workout

---
+++ contributions/charlescodes/Java/Fundamentals/2016-11-11.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
