5d7b91a5d90c5f1058377cef5ef6f14c35c9255a
eoin-obrien <eoinobrien910@gmail.com>
2016-11-11 22:05:23 +0300

    2016-11-11's Java workout

---
+++ contributions/eoin-obrien/Java/I/O/2016-11-11.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Reading file attributes
+Deleting files and directories
