b7286260e15821b6816e2c051762b1f96d0add6c
mharward <mharward@gmail.com>
2016-11-11 20:02:29 +0300

    2016-11-11's JavaScript workout

---
+++ contributions/mharward/JavaScript/React/2016-11-11.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Server-side rendering 
+Keyed fragments in **React**
+Custom validations for props
