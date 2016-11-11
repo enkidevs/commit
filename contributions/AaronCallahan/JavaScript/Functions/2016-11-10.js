e2b6252fe9ab4bf55d8421d68e3d13e3c2ea2fe8
AaronCallahan <aaron5682@gmail.com>
2016-11-11 05:54:15 +0300

    2016-11-10's JavaScript workout

---
+++ contributions/AaronCallahan/JavaScript/Functions/2016-11-10.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Method chaining
+Function call method
+Social sharing without widgets
