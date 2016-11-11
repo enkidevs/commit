73feedf367d1a430e943635095e693c154e4319d
Flamecrest <s-deleeuw@s-deleeuw.nl>
2016-11-11 11:18:02 +0300

    2016-11-11's JavaScript workout

---
+++ contributions/Flamecrest/JavaScript/Node/2016-11-11.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+Automatic compilation for Node with **Nodemon** 
+`try-catch` only for **sync** code
