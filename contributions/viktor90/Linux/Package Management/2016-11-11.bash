a46b3a318ba5cf870e04999215136fd8916d0269
viktor90 <v.georgiev90@abv.bg>
2016-11-11 09:30:07 +0300

    2016-11-11's Linux workout

---
+++ contributions/viktor90/Linux/Package Management/2016-11-11.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Get `apt` to use a mirror / faster mirror
+Using `Tasksel` for software installation
+Using `PPAs`
+Keeping repos updated with `cron`
