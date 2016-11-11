83ddb1cffae9946078300ca617bd9a0a34c74875
JulV94 <julvdev@gmail.com>
2016-11-11 20:11:12 +0300

    2016-11-11's Linux workout

---
+++ contributions/JulV94/Linux/System Management/2016-11-11.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
