87e8f5b6d5c75153d1beec33df8bcb0e72716a85
amaksoft <amaksoft@gmail.com>
2016-11-11 08:03:02 +0300

    2016-11-10's Java workout

---
+++ contributions/amaksoft/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+Writing generic methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
