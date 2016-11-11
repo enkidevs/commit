39809d52f45e4882d6bf931ad06647d2270bbfef
Zhigalin <alexander@zhigalin.tk>
2016-11-12 01:58:03 +0300

    2016-11-11's Java workout

---
+++ contributions/Zhigalin/Java/Fundamentals/2016-11-11.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+The distinction between overloading and overriding methods
+Ternary operator
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
