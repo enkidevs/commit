fd31ae82b253b659b21592101e1bf08ab35b2b79
bennuttle <bennuttle@gmail.com>
2016-11-11 15:14:01 +0300

    2016-11-11's Java workout

---
+++ contributions/bennuttle/Java/Fundamentals/2016-11-11.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
+Using an interface as a parameter
