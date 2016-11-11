9b479c686924b1a4c6d414728404aa428d34fe01
Bestest96 <jaluki@vp.pl>
2016-11-11 13:08:04 +0300

    2016-11-11's Java workout

---
+++ contributions/Bestest96/Java/I/O/2016-11-11.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Create system file paths using the `Path` class
+Reading file attributes
+Retrieving file store attributes
+Listing a file system's root directories
