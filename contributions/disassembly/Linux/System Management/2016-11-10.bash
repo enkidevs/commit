c1d472314357f6579be7819e74c8f7d1728476ec
disassembly <hpeng2005012255@gmail.com>
2016-11-11 04:27:05 +0300

    2016-11-10's Linux workout

---
+++ contributions/disassembly/Linux/System Management/2016-11-10.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Finding Open Files With `lsof`
