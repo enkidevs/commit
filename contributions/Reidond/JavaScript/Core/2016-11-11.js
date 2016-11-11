6f355ab1fda552be6320ff7090122f86aad89877
Reidond <andreyshafar@gmail.com>
2016-11-11 14:21:46 +0300

    2016-11-11's JavaScript workout

---
+++ contributions/Reidond/JavaScript/Core/2016-11-11.js
@@ -0,0 +1,5 @@
+Using the double tilde `~~` 
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Round numbers to `N` decimals 
