062ed7e4d2535e46d3764ceee2852b6472781d7e
McCzarny <mcczarny@gmail.com>
2016-11-11 15:11:37 +0300

    2016-11-11's Linux workout

---
+++ contributions/McCzarny/Linux/File Management/2016-11-11.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
+Easy way of sharing files
