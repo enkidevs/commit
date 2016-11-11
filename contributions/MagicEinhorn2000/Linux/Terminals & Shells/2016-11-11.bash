6398e659c99ab610556b62fd0620b60dc0e86410
MagicEinhorn2000 <ks.wischmop@gmail.com>
2016-11-11 16:59:59 +0300

    2016-11-11's Linux workout

---
+++ contributions/MagicEinhorn2000/Linux/Terminals & Shells/2016-11-11.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Clear bash history
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
