2b066942776500d2a8677ee80804eb7ee032520f
rjtrout <kilgorejohntrout@gmail.com>
2016-11-11 21:48:35 +0300

    2016-11-11's Linux workout

---
+++ contributions/rjtrout/Linux/Terminals & Shells/2016-11-11.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Random password generator
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
