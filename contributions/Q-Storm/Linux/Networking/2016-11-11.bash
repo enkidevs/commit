c00e1680e1aaa55fe5157d58ebe527c45bc44155
Q-Storm <github@nesk.org>
2016-11-11 18:47:36 +0300

    2016-11-11's Linux workout

---
+++ contributions/Q-Storm/Linux/Networking/2016-11-11.bash
@@ -0,0 +1,5 @@
+Stress testing with `ab`
+How to clear `iptables` rules
+Useful ssh client optimizations 
+Getting the connection speed from the terminal
+Staying connected remotely via SSH without an interactive shell
