fcd8202ae0df1e940f7b64646749f3e18331ce8d
tehgoju <tehgoju@gmail.com>
2016-11-11 13:33:32 +0300

    2016-11-11's Java workout

---
+++ contributions/tehgoju/Java/Fundamentals/2016-11-11.java
@@ -0,0 +1,5 @@
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
