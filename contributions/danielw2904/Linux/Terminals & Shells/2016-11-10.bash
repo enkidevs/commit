f56d75ab7f38e8191c9cbeee68594949cc3eade6
danielw2904 <winkler.daniel1@gmail.com>
2016-11-11 04:08:43 +0300

    2016-11-10's Linux workout

---
+++ contributions/danielw2904/Linux/Terminals & Shells/2016-11-10.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Aliasing ssh connections
+Find any Unix / Linux command
+Useful aliasing in bash
+Keep useful commands in your shell history with tags
