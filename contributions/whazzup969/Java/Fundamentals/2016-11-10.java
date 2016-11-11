438dee44ce25961f661c68722d0831f7be01084d
whazzup969 <tiayr@tiayr.com>
2016-11-11 07:11:23 +0300

    2016-11-10's Java workout

---
+++ contributions/whazzup969/Java/Fundamentals/2016-11-10.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
