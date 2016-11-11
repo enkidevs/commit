a38dbf69ba40eaeebb89e22da96ff456cf90555d
Penomatikus <sjagdmann10@googlemail.com>
2016-11-11 18:16:34 +0300

    2016-11-11's Java workout

---
+++ contributions/Penomatikus/Java/Fundamentals/2016-11-11.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Measuring time
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
