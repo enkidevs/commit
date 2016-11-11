d3e95bceab0828a1bf668c7572c048fee32a1488
jnoelEFL <j.noel@lefebvre-sarrut.eu>
2016-11-11 12:17:59 +0300

    2016-11-11's JavaScript workout

---
+++ contributions/jnoelEFL/JavaScript/Core/2016-11-11.js
@@ -0,0 +1,5 @@
+Truthiness
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Extra function arguments are undefined by default
+Check if a function exists
