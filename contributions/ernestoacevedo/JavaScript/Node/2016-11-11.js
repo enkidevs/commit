6264ecb30bed6ca97a608db491b4bdf47cefb16b
ernestoacevedo <eacevedo@debian.cl>
2016-11-11 14:55:44 +0300

    2016-11-11's JavaScript workout

---
+++ contributions/ernestoacevedo/JavaScript/Node/2016-11-11.js
@@ -0,0 +1,5 @@
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+Middleware/pipeline design pattern
