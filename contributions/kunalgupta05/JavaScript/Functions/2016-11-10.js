a5e7b2023d25b527abca0f6b944003bf2ae5afb6
kunalgupta05 <kunalgupta2k12@gmail.com>
2016-11-11 06:07:50 +0300

    2016-11-10's JavaScript workout

---
+++ contributions/kunalgupta05/JavaScript/Functions/2016-11-10.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Easily generate a random `HEX` color
+Social sharing without widgets
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
