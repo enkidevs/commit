544af08ea482dc1dfe921606fadf5df2331b132b
Riscum <domingos.ferreir@gmail.com>
2016-11-11 18:43:32 +0300

    2016-11-11's Java workout

---
+++ contributions/Riscum/Java/Data Types/2016-11-11.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Equals operation on different data types
+Numeric Conversion - Widening
