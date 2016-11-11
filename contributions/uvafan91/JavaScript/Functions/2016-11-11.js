2c34a5d343e2b1afc3e55403eefb3fd2cf88cf73
uvafan91 <mrush91@gmail.com>
2016-11-11 17:29:25 +0300

    2016-11-11's JavaScript workout

---
+++ contributions/uvafan91/JavaScript/Functions/2016-11-11.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Anonymous functions
+Get query/url variables
+Easily generate a random `HEX` color
