aca211c24731184d64f0d130fed2b16bb4128a9e
Drnkn <prehistoric2003@gmail.com>
2016-11-12 09:53:52 +0300

    2016-11-12's Java workout

---
+++ contributions/Drnkn/Java/Fundamentals/2016-11-12.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
