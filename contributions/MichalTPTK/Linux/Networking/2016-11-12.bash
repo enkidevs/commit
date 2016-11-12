a9eaeb2deaa6c421b9ad0798f775aef482d14374
MichalTPTK <michaltptk@gmail.com>
2016-11-12 21:12:31 +0300

    2016-11-12's Linux workout

---
+++ contributions/MichalTPTK/Linux/Networking/2016-11-12.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Logging dropped firewall packets
+Find services running on your host
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
