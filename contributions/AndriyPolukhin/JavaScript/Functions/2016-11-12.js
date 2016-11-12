8abe077f1d9455b831f82fe7ac59898c601b5333
AndriyPolukhin <andriy.polukhin@gmail.com>
2016-11-12 09:30:28 +0300

    2016-11-12's JavaScript workout

---
+++ contributions/AndriyPolukhin/JavaScript/Functions/2016-11-12.js
@@ -0,0 +1,5 @@
+Hoisting
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Easily generate a random `HEX` color
+Move cursor at the end of text input
