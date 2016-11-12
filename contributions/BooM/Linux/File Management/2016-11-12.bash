e7777824d275c0e9e96a8db413666c718d6e9fd7
BooM <tararuj.michal@gmail.com>
2016-11-12 17:25:03 +0300

    2016-11-12's Linux workout

---
+++ contributions/BooM/Linux/File Management/2016-11-12.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
+Find the unknown process preventing deleting of files
