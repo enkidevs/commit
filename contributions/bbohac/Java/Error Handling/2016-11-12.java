ba10a3861e800599c345c024da6395d4e1ba80bd
bbohac <bastian.bohac@gmail.com>
2016-11-12 14:43:10 +0300

    2016-11-12's Java workout

---
+++ contributions/bbohac/Java/Error Handling/2016-11-12.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Most common reason behind **stack overflow** error
+Careful Numeric Data Conversions
