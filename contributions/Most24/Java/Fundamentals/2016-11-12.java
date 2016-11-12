cea28178701925809e218f15d99af7a105f4e686
Most24 <github@most24.de>
2016-11-13 02:37:08 +0300

    2016-11-12's Java workout

---
+++ contributions/Most24/Java/Fundamentals/2016-11-12.java
@@ -0,0 +1,5 @@
+Generating random numbers
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
