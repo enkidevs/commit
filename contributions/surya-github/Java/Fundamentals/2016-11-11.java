a152de009503909310e0138eba514168fd8711aa
surya-github <suryaprabhakark@gmail.com>
2016-11-12 06:49:39 +0300

    2016-11-11's Java workout

---
+++ contributions/surya-github/Java/Fundamentals/2016-11-11.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
