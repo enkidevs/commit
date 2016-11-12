cc467f83ca6db996d750a44d683ca87715ba6a36
Calypsys <calypsys1@gmail.com>
2016-11-12 10:27:40 +0300

    2016-11-12's Linux workout

---
+++ contributions/Calypsys/Linux/Terminals & Shells/2016-11-12.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Find any Unix / Linux command
+Adding directories to your `$PATH`
+Get to know  your commands with `type`
