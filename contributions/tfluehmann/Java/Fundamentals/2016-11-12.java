49e1d86119572019d04ee42c7facda4e92dda7e1
tfluehmann <t.fluehmann94@hotmail.com>
2016-11-12 21:52:37 +0300

    2016-11-12's Java workout

---
+++ contributions/tfluehmann/Java/Fundamentals/2016-11-12.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Writing generic methods
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
