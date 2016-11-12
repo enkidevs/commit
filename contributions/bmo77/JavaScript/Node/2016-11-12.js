1a5fa03350d5fab62de60bedcb1a4b9313c30713
bmo77 <basha.others@gmail.com>
2016-11-12 14:45:35 +0300

    2016-11-12's JavaScript workout

---
+++ contributions/bmo77/JavaScript/Node/2016-11-12.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Use conventional property names for error handling in Node.js
+**Node** `os` module
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
