1c5e44f64fcf37c09bae461e1438af89bed7e60b
kSom <kgsommer@gmail.com>
2016-11-12 11:31:18 +0300

    2016-11-12's Java workout

---
+++ contributions/kSom/Java/Fundamentals/2016-11-12.java
@@ -0,0 +1,5 @@
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+Metadata: setting a file's owner
+Using `synchronized` statements
