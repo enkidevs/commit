2a77f48cc4782946c47a31a625067d2fbb31b8a5
mharward <mharward@gmail.com>
2016-11-12 20:46:58 +0300

    2016-11-12's Linux workout

---
+++ contributions/mharward/Linux/File Management/2016-11-12.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Using `touch` to alter files modification time
+Delete commands aliases
