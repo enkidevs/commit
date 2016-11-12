d09ffed0c8df86aa56c96e3f215f80953afac3de
amarradi <radischm@gmail.com>
2016-11-12 13:22:03 +0300

    2016-11-12's Linux workout

---
+++ contributions/amarradi/Linux/File Management/2016-11-12.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Identify files using the `file` command
