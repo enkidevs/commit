3e49fb12479a003ddb90f118a58bef8a8ba5f1e2
tavorag <gstvag@gmail.com>
2016-11-13 02:57:10 +0300

    2016-11-12's JavaScript workout

---
+++ contributions/tavorag/JavaScript/Node/2016-11-12.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+The built-in Node debugger
+Handle `JSON.parse` error in Node.js
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
