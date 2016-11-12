a7fe7047019d20da50cebb4677308f5d0929e87f
g3mini <mcjanz@gmail.com>
2016-11-12 10:18:39 +0300

    2016-11-12's Java workout

---
+++ contributions/g3mini/Java/Fundamentals/2016-11-12.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
