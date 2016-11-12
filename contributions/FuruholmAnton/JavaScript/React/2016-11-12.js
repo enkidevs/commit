0b5ce22fb8b09f4e106e03c357848d8d1093bec8
FuruholmAnton <anton.furuholm@gmail.com>
2016-11-12 17:54:24 +0300

    2016-11-12's JavaScript workout

---
+++ contributions/FuruholmAnton/JavaScript/React/2016-11-12.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+Functional Stateless Components in React
+Pixel values in style props for **React**
+Type of the Children props
+Use **React** with other libraries
