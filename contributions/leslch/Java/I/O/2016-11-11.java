22830bc1653543af2711a50602317dc77fbe0284
leslch <leslch@hotmail.com>
2016-11-12 07:42:26 +0300

    2016-11-11's Java workout

---
+++ contributions/leslch/Java/I/O/2016-11-11.java
@@ -0,0 +1,5 @@
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Retrieving file store attributes
+The `Console` class
