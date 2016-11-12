8c7ae04193ab4c35fddb6286046d3a42842b4509
MichaelRijsdijk <michaelrijsdijk@gmail.com>
2016-11-12 16:04:46 +0300

    2016-11-12's Java workout

---
+++ contributions/MichaelRijsdijk/Java/I/O/2016-11-12.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Using buffered streams
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Retrieving file store attributes
