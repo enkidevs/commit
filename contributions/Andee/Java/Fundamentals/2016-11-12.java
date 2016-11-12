7e8cd2699a7de7dda87202e8c9079fc4998d9e22
Andee <andytruong94@gmail.com>
2016-11-12 17:49:56 +0300

    2016-11-12's Java workout

---
+++ contributions/Andee/Java/Fundamentals/2016-11-12.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Do not perform bitwise and arithmetic operations on the same data
