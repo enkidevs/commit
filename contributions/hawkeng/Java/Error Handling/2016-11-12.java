b621155e891772990e161a824a136cc9bde9cd37
hawkeng <hawkins.dom@gmail.com>
2016-11-12 17:56:08 +0300

    2016-11-12's Java workout

---
+++ contributions/hawkeng/Java/Error Handling/2016-11-12.java
@@ -0,0 +1,5 @@
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Prevent `exceptions` while logging Data
+Careful Numeric Data Conversions
