d6cfecc6898fbbecdaed95fd1db3e84d3c06fa1b
pablolmnora <pablolmnora@gmail.com>
2016-11-12 16:57:47 +0300

    2016-11-12's JavaScript workout

---
+++ contributions/pablolmnora/JavaScript/Core/2016-11-12.js
@@ -0,0 +1,5 @@
+Quick way to format integers with a fixed number of digits.
+Shuffle an array using `sort` and `random`.
+Arrays can be used as stack
+Inaccuracy of binary floating-point format
+Base conversion 
