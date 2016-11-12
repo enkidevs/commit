5199c2f7c52a887eb1cce00f87554511560d0035
Thommi <thomasandre84@gmail.com>
2016-11-12 09:48:26 +0300

    2016-11-12's Linux workout

---
+++ contributions/Thommi/Linux/System Management/2016-11-12.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
