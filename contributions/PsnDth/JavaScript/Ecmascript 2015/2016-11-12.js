a617c66880643c4a580840457bec02e2794882c2
PsnDth <psndth@gmail.com>
2016-11-12 19:12:26 +0300

    2016-11-12's JavaScript workout

---
+++ contributions/PsnDth/JavaScript/Ecmascript 2015/2016-11-12.js
@@ -0,0 +1,6 @@
+Computed property names in object literals
+Default function parameters
+Defaults values apply only to `undefined` (and not to `null`)
+Internationalization & Localization
+
+`let` and `const` in ES6
