86b1d6de123656bb8ef7292a072a4aed26cb1fd1
gobbas <gabbi@hotmail.no>
2016-11-12 15:01:03 +0300

    2016-11-12's Java workout

---
+++ contributions/gobbas/Java/Fundamentals/2016-11-12.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
