e0c15cf6a4e2994901a840b28b1bdae8ecf66339
Naino <jeremy.halin@gmail.com>
2016-11-12 19:25:52 +0300

    2016-11-12's JavaScript workout

---
+++ contributions/Naino/JavaScript/React/2016-11-12.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Pixel values in style props for **React**
+Props in getInitialState Is an        
+Anti-Pattern
+Expose Component Functions in **React**
+`PureRenderMixin` in **React**
