2948f49003048556547607d0d219877af2a5dbc3
3DEsprit <vilesyn@gmail.com>
2016-11-12 21:10:00 +0300

    2016-11-12's Java workout

---
+++ contributions/3DEsprit/Java/Fundamentals/2016-11-12.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Java naming conventions
+Using Inheritance to reduce code repetition
+How range operations work
