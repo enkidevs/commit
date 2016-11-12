3f40e671113c06e4693cead0be20cbbe06239655
0xqookie <b456231@trbvn.com>
2016-11-12 22:02:47 +0300

    2016-11-12's Java workout

---
+++ contributions/0xqookie/Java/I/O/2016-11-12.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Retrieving file store attributes
+List contents of a directory with `newDirectoryStream`
