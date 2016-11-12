c23a00659ef1e2b4a0b1cdefd61dd64260d15726
kreidetafels <kreidetafels@gmail.com>
2016-11-12 03:49:33 +0300

    2016-11-11's Java workout

---
+++ contributions/kreidetafels/Java/I/O/2016-11-11.java
@@ -0,0 +1,5 @@
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Retrieving file store attributes
+Listing a file system's root directories
