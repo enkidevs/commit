150734469f363b158a1728284d51227b4446a0d5
mikercampbell <michaelrcampbell0916@gmail.com>
2016-11-12 18:03:40 +0300

    2016-11-12's JavaScript workout

---
+++ contributions/mikercampbell/JavaScript/Ecmascript 2015/2016-11-12.js
@@ -0,0 +1,5 @@
+Computed property names in object literals
+Default function parameters
+Defaults values apply only to `undefined` (and not to `null`)
+Hoisting with `var` and `let`
+Binary and octal notation in ES6
