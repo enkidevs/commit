cfd78bea99369ec38f173faea8d03c6a07bba9b6
3DEsprit <vilesyn@gmail.com>
2016-11-13 23:55:08 +0300

    2016-11-13's Java workout

---
+++ contributions/3DEsprit/Java/Data Types/2016-11-13.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Converting numbers to strings
+Converting a string to upper or lower case
+Do not attempt comparisons with NaN
+Numeric Conversion - Widening
