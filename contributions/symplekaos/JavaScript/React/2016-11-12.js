9f532f39c454966b4aaf02757d0fb8ccd66e450a
symplekaos <symplekaos@gmail.com>
2016-11-13 03:22:24 +0300

    2016-11-12's JavaScript workout

---
+++ contributions/symplekaos/JavaScript/React/2016-11-12.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+**React** in-line styles
+componentWillReceiveProps Not Triggered After Mounting
+
+Use **React** with other libraries
+Dealing with `this.props.children`
