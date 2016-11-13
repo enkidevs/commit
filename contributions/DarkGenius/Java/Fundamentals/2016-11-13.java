38cc708e47124566f9faa634536600e118ad39f2
DarkGenius <darkgenius@nextmail.ru>
2016-11-13 16:24:41 +0300

    2016-11-13's Java workout

---
+++ contributions/DarkGenius/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Calling one constructor from another
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
