3df477aa8ae272c73d1438546c64fb095ca221cc
jacob-on-github <jherrington.personal@gmail.com>
2016-11-13 19:00:49 +0300

    2016-11-13's JavaScript workout

---
+++ contributions/jacob-on-github/JavaScript/Core/2016-11-13.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Setting default values with `short circuiting`
+Using assignment operators
+Hoisting applies only to variable declarations, not initializations
+Apply `map` function to array items
