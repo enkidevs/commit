3f32baf6fe3350f8944aef1450f3e67612d27de5
EnzDev <enzo@mallard.ml>
2016-11-13 13:49:41 +0300

    2016-11-13's Java workout

---
+++ contributions/EnzDev/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Supply `toString()` in all classes
