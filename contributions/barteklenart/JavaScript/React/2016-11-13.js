6a4861b2092869ba928f00719d5b7076f9b7e188
barteklenart <bartlomiejlenartbl@gmail.com>
2016-11-13 23:09:30 +0300

    2016-11-13's JavaScript workout

---
+++ contributions/barteklenart/JavaScript/React/2016-11-13.js
@@ -0,0 +1,7 @@
+Pixel values in style props for **React**
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Enhanced Prop Validation 
+Default values for props
