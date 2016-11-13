527493db0b49e321b93c13a2c875065568dd6cb3
thewalla07 <michael.wall22@mail.dcu.ie>
2016-11-13 17:51:27 +0300

    2016-11-13's Linux workout

---
+++ contributions/thewalla07/Linux/System Management/2016-11-13.bash
@@ -0,0 +1,6 @@
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Changing the timezone on deb based systems
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
