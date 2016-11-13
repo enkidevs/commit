011f74d6df0184f669fa1ccb5fcebfeda67edff8
_andrycodestuffs <andrybusa@gmail.com>
2016-11-13 16:47:07 +0300

    2016-11-13's JavaScript workout

---
+++ contributions/_andrycodestuffs/JavaScript/Node/2016-11-13.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+Manage child processes in **Node**
+Bulk-Write in **Node** with `cork()`
