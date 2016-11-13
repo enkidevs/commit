838fb612bd9695cbeb934f75bdb1b3ac43c7577c
jackuse <jackiller33@gmail.com>
2016-11-13 04:31:54 +0300

    2016-11-12's JavaScript workout

---
+++ contributions/jackuse/JavaScript/React/2016-11-12.js
@@ -0,0 +1,6 @@
+componentWillReceiveProps Not Triggered After Mounting
+
+Expose Component Functions in **React**
+Use **React** with other libraries
+`ReactDOM.render` ref
+Shortcut for transferring props
