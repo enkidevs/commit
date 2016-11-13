b2713014b3681328b627a358f13fe1c042869add
bobahrus <bobahrus@gmail.com>
2016-11-13 10:25:45 +0300

    2016-11-13's Java workout

---
+++ contributions/bobahrus/Java/Data Types/2016-11-13.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Collections vs arrays
+Multidimensional array declaration
+Do not attempt comparisons with NaN
