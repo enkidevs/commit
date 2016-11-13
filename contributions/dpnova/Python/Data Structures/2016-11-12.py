25e954c154a1eae737f85fb710198600d5f1cba6
dpnova <dpn@dpn.name>
2016-11-13 13:44:57 +0300

    2016-11-12's Python workout

---
+++ contributions/dpnova/Python/Data Structures/2016-11-12.py
@@ -0,0 +1,5 @@
+Following PEP 8 styling guideline.
+A simple way to select a random item from a `list/tuple` data stucture
+Enhance your `tuple`s
+`bytearray` objects
+Operations with `bytes` and `bytearray`
