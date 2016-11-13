b8059f16e2b9c94a58a51e1fd434df4e414630ea
yasinfp <yfpayalan@gmail.com>
2016-11-13 23:04:37 +0300

    2016-11-13's Java workout

---
+++ contributions/yasinfp/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Use `DecimalFormat` class to format numbers
+Limit Accessibility of `Fields`
+Avoid using `static` variables
