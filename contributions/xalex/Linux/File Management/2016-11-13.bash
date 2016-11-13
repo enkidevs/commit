bdc2f1f57fd060f79fb516fe93789610add7b1e9
xalex <saleksey67@gmail.com>
2016-11-13 13:45:10 +0300

    2016-11-13's Linux workout

---
+++ contributions/xalex/Linux/File Management/2016-11-13.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
