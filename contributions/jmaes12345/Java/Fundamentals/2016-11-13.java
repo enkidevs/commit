ba447de6eaa05fab82e339088d9a4bb944b2ab5a
jmaes12345 <jamesbrundell@hotmail.co.uk>
2016-11-14 01:27:14 +0300

    2016-11-13's Java workout

---
+++ contributions/jmaes12345/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Common mistake on switch statements
+Calling one constructor from another
+Always specify type parameter
