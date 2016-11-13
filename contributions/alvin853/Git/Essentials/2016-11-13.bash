9ac62b6985b212f3de3a339d881521f1fc07a5f6
alvin853 <alvin853@yahoo.de>
2016-11-13 15:38:14 +0300

    2016-11-13's Git workout

---
+++ contributions/alvin853/Git/Essentials/2016-11-13.bash
@@ -0,0 +1,5 @@
+Adding a remote repository
+How to set your username and email
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: fetching and pushing
