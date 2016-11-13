b694a425d015e87c8ef6d290b46bc2f3b95619c7
bmo77 <basha.others@gmail.com>
2016-11-13 15:38:57 +0300

    2016-11-13's Linux workout

---
+++ contributions/bmo77/Linux/Terminals & Shells/2016-11-13.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Aliasing ssh connections
+Useful aliasing in bash
+Keep useful commands in your shell history with tags
