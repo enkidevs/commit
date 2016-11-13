032b8f3b86bdcc5c134e38fed6a2c8353e33e17a
bigdicksenpai <jon_athan71@hotmail.com>
2016-11-14 01:58:16 +0300

    2016-11-13's Java workout

---
+++ contributions/bigdicksenpai/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The trade-off between short and descriptive names
+Metadata: setting a file's owner
+Always specify type parameter
+Limit Accessibility of `Fields`
