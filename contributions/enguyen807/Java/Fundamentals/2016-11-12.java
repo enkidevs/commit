388829c016c54860e9aafad1f64824b5e16fb2ee
enguyen807 <enguyen807@gmail.com>
2016-11-13 04:31:29 +0300

    2016-11-12's Java workout

---
+++ contributions/enguyen807/Java/Fundamentals/2016-11-12.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Java naming conventions
+Catch multiple exceptions in a single `catch` block
+Use `DecimalFormat` class to format numbers
+Always specify type parameter
