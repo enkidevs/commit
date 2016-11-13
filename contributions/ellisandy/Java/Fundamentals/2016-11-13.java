6503ccd7ac2798da8d063e01f5357bf7ef219d14
ellisandy <andrew@andrewryanellis.com>
2016-11-14 02:26:22 +0300

    2016-11-13's Java workout

---
+++ contributions/ellisandy/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Uses of the `final` keyword
+Ternary operator
+Use strings in a `switch` statement
+Do not perform bitwise and arithmetic operations on the same data
