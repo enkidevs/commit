1764c031d64eddfd1416f33ab66b34cad716a662
Vebble <serafin21@gmail.com>
2016-11-14 00:33:11 +0300

    2016-11-13's Java workout

---
+++ contributions/Vebble/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Catch multiple exceptions in a single `catch` block
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+How range operations work
