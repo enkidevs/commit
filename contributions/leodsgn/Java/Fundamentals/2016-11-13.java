766dd395cab0828c96cec1a6b43f81b973e030cc
leodsgn <leoribeiro.dsgn@gmail.com>
2016-11-13 18:50:30 +0300

    2016-11-13's Java workout

---
+++ contributions/leodsgn/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
