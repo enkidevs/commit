67d3fe6d36f9bca7d372a74294d13f7c5ae4131f
1124816 <e.e.basler@gmail.com>
2016-11-13 05:57:51 +0300

    2016-11-12's JavaScript workout

---
+++ contributions/1124816/JavaScript/Node/2016-11-12.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Http Server in **Node**
+First-error callbacks in **Node**
+Wrapping errors in Node.js using _node-verror_
+Automatic compilation for Node with **Nodemon** 
