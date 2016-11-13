35ef60a91e05c2a8511fba46b9a406415ad3c5e3
davex <dabuck@libero.it>
2016-11-13 10:43:07 +0300

    2016-11-13's Linux workout

---
+++ contributions/davex/Linux/System Management/2016-11-13.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Common log files
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Finding Open Files With `lsof`
