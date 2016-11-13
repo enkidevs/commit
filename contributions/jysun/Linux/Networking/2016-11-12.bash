69003ae74831b0e690c4145ebb9171343904abb8
jysun <good0991@gmail.com>
2016-11-13 13:51:33 +0300

    2016-11-12's Linux workout

---
+++ contributions/jysun/Linux/Networking/2016-11-12.bash
@@ -0,0 +1,5 @@
+Logging dropped firewall packets
+Get real network statistics with `slurm`
+Limit the `wget` download rate
+Use `tcpdump` to listen to network interface traffic
+Make use of `netstat`
