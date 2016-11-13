4c0af35641a1d2896c2d002e436ab973ced8aa1d
jarba3 <bjobard@gmail.com>
2016-11-13 20:04:29 +0300

    2016-11-13's JavaScript workout

---
+++ contributions/jarba3/JavaScript/Npm/2016-11-13.js
@@ -0,0 +1,5 @@
+Combining script commands in npm
+Manage local node modules with `npm link`
+`npm` verifies `sha1` hashes of packages
+`optionalDependencies` in npm
+Lock down dependency versions by shrinkwrapping
