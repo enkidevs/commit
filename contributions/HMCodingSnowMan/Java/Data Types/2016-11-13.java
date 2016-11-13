1769cd2944ed181b8cdc06360fc0cd29e198929e
HMCodingSnowMan <hansmarasigan@gmail.com>
2016-11-13 20:55:59 +0300

    2016-11-13's Java workout

---
+++ contributions/HMCodingSnowMan/Java/Data Types/2016-11-13.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Converting a string to upper or lower case
+Finding a substring in a string
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
