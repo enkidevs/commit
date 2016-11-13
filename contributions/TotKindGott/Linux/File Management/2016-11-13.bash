e0b4f5f90887607c21edaedd4d2054d59b54385e
TotKindGott <konstantin.a.kolosov@gmail.com>
2016-11-13 18:22:37 +0300

    2016-11-13's Linux workout

---
+++ contributions/TotKindGott/Linux/File Management/2016-11-13.bash
@@ -0,0 +1,6 @@
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
+Find the unknown process preventing deleting of files
+Delete commands aliases
