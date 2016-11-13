4155af131255df6ad3b84f495b36c95a6742472b
RJongejan <ruben.jongejan@gmail.com>
2016-11-13 22:20:59 +0300

    2016-11-13's Linux workout

---
+++ contributions/RJongejan/Linux/File Management/2016-11-13.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Using `touch` to alter files modification time
+Deleting non-empty directories
