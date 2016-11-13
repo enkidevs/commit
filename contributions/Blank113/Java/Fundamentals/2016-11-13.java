9ddaec0693157b566d9345b03305603e6cd1cf80
Blank113 <hbaldwin.services+1@gmail.com>
2016-11-13 22:41:31 +0300

    2016-11-13's Java workout

---
+++ contributions/Blank113/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+There is no such thing as *pass-by-reference* in Java
+How range operations work
