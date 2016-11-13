731c8e3fa5745ed92c0ad442fe01ded3f0873b5d
ejde <ejdevangelista@gmail.com>
2016-11-13 20:42:03 +0300

    2016-11-13's Java workout

---
+++ contributions/ejde/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
