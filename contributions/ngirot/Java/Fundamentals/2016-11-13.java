bf2aa1279b8664b8fc7881f6531e9589c79da05d
ngirot <ngirot@hotmail.com>
2016-11-13 14:04:14 +0300

    2016-11-13's Java workout

---
+++ contributions/ngirot/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
+Using an interface as a parameter
