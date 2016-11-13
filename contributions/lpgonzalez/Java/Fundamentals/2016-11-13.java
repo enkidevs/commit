f3d8a31d4a76285690cb93d1fc577e2684a2b605
lpgonzalez <lisardo.prieto.gonzalez@gmail.com>
2016-11-13 09:22:55 +0300

    2016-11-13's Java workout

---
+++ contributions/lpgonzalez/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
