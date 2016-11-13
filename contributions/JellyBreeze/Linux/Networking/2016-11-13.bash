33ec38fdc92778b6aa87d7245198927b773ae1ca
JellyBreeze <00valter00@gmail.com>
2016-11-13 16:12:01 +0300

    2016-11-13's Linux workout

---
+++ contributions/JellyBreeze/Linux/Networking/2016-11-13.bash
@@ -0,0 +1,5 @@
+ Intro to `iptables`
+Check `iptables` firewall status
+Get real network statistics with `slurm`
+Setting up password-free authentication
+Use `tcpdump` to listen to network interface traffic
