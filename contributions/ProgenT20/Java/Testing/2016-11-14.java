7a3090b3450596b419348d32bf39e6c0cd0a8fdb
ProgenT20 <simonbrand@hotmail.ch>
2016-11-14 12:24:31 +0300

    2016-11-14's Java workout

---
+++ contributions/ProgenT20/Java/Testing/2016-11-14.java
@@ -0,0 +1,5 @@
+JUnit: prefer `@Test` over `extends TestCase`
+Focus testing on key areas
+Writing JUnit tests using `Assert`
+JUnit: Specifying file locations
+Replacements for `setUp()` and `tearDown()` for **JUnit**
