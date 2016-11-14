cb18324ff6054eb114413b660363c9fb3632da63
Calim50 <bastien.sebire@gmail.com>
2016-11-15 01:29:44 +0300

    2016-11-14's Java workout

---
+++ contributions/Calim50/Java/Error Handling/2016-11-14.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
