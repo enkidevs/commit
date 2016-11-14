91753b971e3c5255389493fabdd912cfca0118cc
mlaythe <mrlaythe24@aol.com>
2016-11-15 01:25:28 +0300

    2016-11-14's Linux workout

---
+++ contributions/mlaythe/Linux/System Management/2016-11-14.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+List current logged on users with `w`
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
