39b1a2900dbd7ace416ab625381803f6c1de5955
forty2nd <mdyomin@gmail.com>
2016-11-14 14:54:18 +0300

    2016-11-14's Linux workout

---
+++ contributions/forty2nd/Linux/System Management/2016-11-14.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Finding Open Files With `lsof`
