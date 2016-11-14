3645bff2890a8613fc4c39ddbdce3a8a69f9885f
glanisko <glanisko@gmail.com>
2016-11-14 19:38:30 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/glanisko/JavaScript/React/2016-11-14.js
@@ -0,0 +1,5 @@
+Use `propTypes` on stateless components
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+Dealing with `this.props.children`
+Shortcut for transferring props
