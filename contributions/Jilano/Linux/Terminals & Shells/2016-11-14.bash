edc884826d4f11dbaa1d8394b59a74785d669ae1
Jilano <mobile.axel@gmail.com>
2016-11-14 08:56:22 +0300

    2016-11-14's Linux workout

---
+++ contributions/Jilano/Linux/Terminals & Shells/2016-11-14.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Clear bash history
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
