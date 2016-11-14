22ef35ac7fe705dadba04e88d78385dc71f9c448
danielw2904 <winkler.daniel1@gmail.com>
2016-11-14 12:38:26 +0300

    2016-11-14's Linux workout

---
+++ contributions/danielw2904/Linux/Security/2016-11-14.bash
@@ -0,0 +1,5 @@
+Security news delivered via DNS
+Extracting the `public key` from the `private key`
+Disabling **USB** storage devices
+Keeping `/boot` read-only
+Private key encryption with `OpenSSL`
