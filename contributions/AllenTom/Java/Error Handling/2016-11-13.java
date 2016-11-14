9fbec2b7a961891607a62eb9fb0dfa406dcdd06f
AllenTom <qweasdzxczse2@gmail.com>
2016-11-14 05:35:20 +0300

    2016-11-13's Java workout

---
+++ contributions/AllenTom/Java/Error Handling/2016-11-13.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Creating new Exceptions
+Most common reason behind **stack overflow** error
+Careful Numeric Data Conversions
