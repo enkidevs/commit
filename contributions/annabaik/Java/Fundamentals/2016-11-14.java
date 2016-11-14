274aa6e89714c5b11b4c2e0758fb85ed4d41c3e2
annabaik <annaheyonbaik@gmail.com>
2016-11-14 12:58:24 +0300

    2016-11-14's Java workout

---
+++ contributions/annabaik/Java/Fundamentals/2016-11-14.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Difference between JRE and JDK?
+The distinction between overloading and overriding methods
+Calling one constructor from another
+Constant notation
