d55313c6cf4dc10b1f7a28e332c3b7a78b7ce7d8
clausewtz45 <clausewitz45@gmail.com>
2016-11-14 11:07:40 +0300

    2016-11-14's Linux workout

---
+++ contributions/clausewtz45/Linux/File Management/2016-11-14.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
