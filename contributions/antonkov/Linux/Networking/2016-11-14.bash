49570dabf173908ee34cdc60912e0ba1c289b799
antonkov <kovsharov@rain.ifmo.ru>
2016-11-14 15:04:11 +0300

    2016-11-14's Linux workout

---
+++ contributions/antonkov/Linux/Networking/2016-11-14.bash
@@ -0,0 +1,5 @@
+Find services running on your host
+Useful ssh client optimizations 
+Bandwidth monitoring tools 
+Make use of `netstat`
+Staying connected remotely via SSH without an interactive shell
