aa96130d61b9029243902f774220244a1f3ec0a1
TheAl_T <branislav.bajuzik@gmail.com>
2016-11-14 20:30:17 +0300

    2016-11-14's Java workout

---
+++ contributions/TheAl_T/Java/Fundamentals/2016-11-14.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Catch multiple exceptions in a single `catch` block
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
