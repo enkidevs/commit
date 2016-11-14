f099832220e2980ffea72dc19331aa9233983773
giancarloaf <giancarlo.faranda@gmail.com>
2016-11-15 00:03:31 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/giancarloaf/JavaScript/Functions/2016-11-14.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Get query/url variables
