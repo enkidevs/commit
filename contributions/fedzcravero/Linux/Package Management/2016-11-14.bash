ba382e84c7a5b4d6f63145d3f99cd709e702f572
fedzcravero <fedzcravero@gmail.com>
2016-11-15 00:24:42 +0300

    2016-11-14's Linux workout

---
+++ contributions/fedzcravero/Linux/Package Management/2016-11-14.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+List installed packages
+Removing PPAs
+Prevent updating a specific package in Debian systems
+Keeping repos updated with `cron`
