5cf216b6788766b064faee91df7241551d93a183
RudeBoy <romanisman@gmail.com>
2016-11-14 23:39:11 +0300

    2016-11-14's Linux workout

---
+++ contributions/RudeBoy/Linux/Terminals & Shells/2016-11-14.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Find any Unix / Linux command
+Quick `bash` shortcuts 
