ceee490c23c639d6033a222c383c12dc5f3c2633
mchmielarz <michal.chmielarz@gmail.com>
2016-11-14 19:53:52 +0300

    2016-11-14's Java workout

---
+++ contributions/mchmielarz/Java/Fundamentals/2016-11-14.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
+Using an interface as a parameter
