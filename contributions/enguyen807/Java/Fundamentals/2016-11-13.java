9ba34f18bf0c887ed3a63dee696fb6a7a4a3d4aa
enguyen807 <enguyen807@gmail.com>
2016-11-14 06:52:22 +0300

    2016-11-13's Java workout

---
+++ contributions/enguyen807/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Do not perform bitwise and arithmetic operations on the same data
+How range operations work
+Using an interface as a parameter
