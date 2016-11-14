0801083497a8ea26c09cd4eef144f60a40b3934e
dantavo <darkmind88@gmail.com>
2016-11-14 11:57:25 +0300

    2016-11-14's Java workout

---
+++ contributions/dantavo/Java/Fundamentals/2016-11-14.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
