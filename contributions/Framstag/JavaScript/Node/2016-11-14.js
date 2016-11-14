8cd39bfa3f600fa75803a18190c7808afa438ac9
Framstag <tim@framstag.com>
2016-11-14 22:03:27 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/Framstag/JavaScript/Node/2016-11-14.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Global Objects and Environment Variables in **Node**
+The built-in Node debugger
+Wrapping errors in Node.js using _node-verror_
+`exports` vs. `module.exports` in **Node**
