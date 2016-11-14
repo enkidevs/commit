e4cd37cebf9c92142e2baf1cfbe6bddbcfed2612
gigagy <mail000mail0000@gmail.com>
2016-11-14 16:41:47 +0300

    2016-11-13's Java workout

---
+++ contributions/gigagy/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Metadata: creating a user-defined file attribute
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
