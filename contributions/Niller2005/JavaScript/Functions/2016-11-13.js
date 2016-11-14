4234ebcd5052a0ae73dd1b585a381215fbb7d9be
Niller2005 <jnillern@gmail.com>
2016-11-15 00:03:07 +0300

    2016-11-13's JavaScript workout

---
+++ contributions/Niller2005/JavaScript/Functions/2016-11-13.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Easily generate a random `HEX` color
+Function call method
+Move cursor at the end of text input
+Check if a document is done loading
