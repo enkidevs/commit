ddadb22bbc1469480ef8c4045e64019f819d0314
pattyhama <strodr@gmail.com>
2016-11-14 18:04:20 +0300

    2016-11-13's Linux workout

---
+++ contributions/pattyhama/Linux/System Management/2016-11-13.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
