fe07eaf130e6b238a97f75cd594d4f8e1e2a0b56
chhsiao90 <chhsiao90@gmail.com>
2016-11-15 02:44:08 +0300

    2016-11-14's Java workout

---
+++ contributions/chhsiao90/Java/Fundamentals/2016-11-14.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Floating garbage and how to deal with it
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
