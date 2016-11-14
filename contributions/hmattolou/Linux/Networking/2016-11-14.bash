8aaee6e2fbf530a446a9347ef3e1956cc59ac7b7
hmattolou <hm.attolou@gmail.com>
2016-11-14 21:42:29 +0300

    2016-11-14's Linux workout

---
+++ contributions/hmattolou/Linux/Networking/2016-11-14.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Stress testing with `ab`
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
+Staying connected remotely via SSH without an interactive shell
