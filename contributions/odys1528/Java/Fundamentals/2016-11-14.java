697a9615ac78b0df7fc329e2b4affc3b33607d17
odys1528 <jolanta.zakrzewska@orange.pl>
2016-11-14 18:44:50 +0300

    2016-11-14's Java workout

---
+++ contributions/odys1528/Java/Fundamentals/2016-11-14.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Measuring time
+Using an interface as a parameter
