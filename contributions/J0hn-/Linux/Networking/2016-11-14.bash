c0b45d72aed7ecc8fcd5b9c0f5d3aa7452e4f25d
J0hn- <jonathan.platteau@yahoo.fr>
2016-11-14 23:05:11 +0300

    2016-11-14's Linux workout

---
+++ contributions/J0hn-/Linux/Networking/2016-11-14.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
+Make use of `netstat`
