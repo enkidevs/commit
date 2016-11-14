9e617413d8bd74aead40fc0e57883edd97931e8a
pixcompu <pixcompu@outlook.com>
2016-11-14 17:40:24 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/pixcompu/JavaScript/Core/2016-11-14.js
@@ -0,0 +1,5 @@
+Hoisting applies only to variable declarations, not initializations
+Prototype methods
+Prevent background image flicker in **IE**
+Getting the *real* dimensions of an image
+Base conversion 
