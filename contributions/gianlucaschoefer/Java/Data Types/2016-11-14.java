d8761d4faac674a1b8c19e22f613d4219d6d946c
gianlucaschoefer <gianlucaschoefer@gmail.com>
2016-11-14 21:24:40 +0300

    2016-11-14's Java workout

---
+++ contributions/gianlucaschoefer/Java/Data Types/2016-11-14.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Converting numbers to strings
+Equals operation on different data types
