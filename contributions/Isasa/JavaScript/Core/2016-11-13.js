53c1f0ed621c3484825d54269a8c90fdbcbd0c23
Isasa <2110844790@qq.com>
2016-11-14 03:57:16 +0300

    2016-11-13's JavaScript workout

---
+++ contributions/Isasa/JavaScript/Core/2016-11-13.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Use `splice()` to remove an item from an array
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Scope and strict mode
