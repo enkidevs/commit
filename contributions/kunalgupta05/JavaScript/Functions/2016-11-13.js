c397c4482deaf07db354723dd33a1d888f78b5ec
kunalgupta05 <kunalgupta2k12@gmail.com>
2016-11-14 20:02:52 +0300

    2016-11-13's JavaScript workout

---
+++ contributions/kunalgupta05/JavaScript/Functions/2016-11-13.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Check if a document is done loading
