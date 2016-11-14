871af6a32e3c6a31eca2cdb0a65702aadc58fb5a
bbohac <bastian.bohac@gmail.com>
2016-11-14 10:40:51 +0300

    2016-11-14's Java workout

---
+++ contributions/bbohac/Java/Fundamentals/2016-11-14.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Altering format string output by changing a format specifier's `argument_index`
+Using `Printstream.format` to format a string before printing
+Detect or prevent integer overflow
