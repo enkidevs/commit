b58ac87c0b27e3ee5e91f50d698fb71f02ac9522
ronalddas <ronald1das@gmail.com>
2016-11-14 20:18:23 +0300

    2016-11-13's Java workout

---
+++ contributions/ronalddas/Java/Error Handling/2016-11-13.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Converting stack trace to a string
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
