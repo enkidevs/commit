69b7ae080e91d0fd3d3e7da3ed9242a8dd24414b
1124816 <e.e.basler@gmail.com>
2016-11-14 21:46:43 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/1124816/JavaScript/Functions/2016-11-14.js
@@ -0,0 +1,5 @@
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Anonymous functions
+Easily generate a random `HEX` color
+Changing a functions context with `fn.call(object)`
