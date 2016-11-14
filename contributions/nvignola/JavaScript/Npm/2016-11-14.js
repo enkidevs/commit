b2d5d19c066a120dfae1bb063e403fb3ab92917e
nvignola <vignonik@gmail.com>
2016-11-14 20:40:50 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/nvignola/JavaScript/Npm/2016-11-14.js
@@ -0,0 +1,5 @@
+npm can handle multiple versions of a dependency
+List binaries for scripting in npm
+`npm`  as an alternative to Gulp
+Tab completion in `npm`
+Lock down dependency versions by shrinkwrapping
