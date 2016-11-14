e4ad16f4a210d3bf086d1bfd02a94be3ee8cf274
FullBoa <kafka.fb@gmail.com>
2016-11-14 08:37:44 +0300

    2016-11-14's Linux workout

---
+++ contributions/FullBoa/Linux/Permissions & Users/2016-11-14.bash
@@ -0,0 +1,5 @@
+Executing commands with `sudo` without password
+Granting `root` access to a user
+Making a file unalterable with `chattr`
+Get file permissions in octal form
+The `setgid` permission
