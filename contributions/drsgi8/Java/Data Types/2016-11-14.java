8015c0d4b922108724f06f05edb02732e70cacd8
drsgi8 <rogowiczdawid@gmail.com>
2016-11-14 22:45:07 +0300

    2016-11-14's Java workout

---
+++ contributions/drsgi8/Java/Data Types/2016-11-14.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Converting numbers to strings
+Equals operation on different data types
+Do not attempt comparisons with NaN
