c0155c4f7fbf5fc3828526e48f9c009667e02df2
ross-f <rssfltchr@gmail.com>
2016-11-14 21:37:03 +0300

    2016-11-14's Linux workout

---
+++ contributions/ross-f/Linux/File Management/2016-11-14.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
