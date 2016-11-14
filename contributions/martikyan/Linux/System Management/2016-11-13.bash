467c42cc39ff97ca0e5b25d1c0390641f84a5489
martikyan <kamartikyan@gmail.com>
2016-11-14 22:48:39 +0300

    2016-11-13's Linux workout

---
+++ contributions/martikyan/Linux/System Management/2016-11-13.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
