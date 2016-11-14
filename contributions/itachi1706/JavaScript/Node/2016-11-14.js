407bf7bcadb9b4427dbe679e142da5e3e5dddcd7
itachi1706 <kennethsohyq@hotmail.com>
2016-11-15 00:09:41 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/itachi1706/JavaScript/Node/2016-11-14.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+Streams in **Node**
+Use conventional property names for error handling in Node.js
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
