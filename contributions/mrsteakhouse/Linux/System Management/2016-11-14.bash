f93f1d27bf48f058e2da169c8a6a131e3e75e1dc
mrsteakhouse <mrsteakhouse1@googlemail.com>
2016-11-14 22:52:02 +0300

    2016-11-14's Linux workout

---
+++ contributions/mrsteakhouse/Linux/System Management/2016-11-14.bash
@@ -0,0 +1,5 @@
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Cancel a system shutdown
