c46a201dcfb4b57d744a60a501dc017b6637ec8f
JonaC22 <jonathan.corallo.utn@gmail.com>
2016-11-14 21:00:21 +0300

    2016-11-14's Java workout

---
+++ contributions/JonaC22/Java/Fundamentals/2016-11-14.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
