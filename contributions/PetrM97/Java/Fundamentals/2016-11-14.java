0271c69763cde180748ea14b2eb8faa396f55faf
PetrM97 <petrmichalik1@seznam.cz>
2016-11-14 23:11:38 +0300

    2016-11-14's Java workout

---
+++ contributions/PetrM97/Java/Fundamentals/2016-11-14.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
