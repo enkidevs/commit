d73d89386d9e558f32860ee4b04f71e03fbaddf7
gardient <gradient93@gmail.com>
2016-11-14 21:28:11 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/gardient/JavaScript/React/2016-11-14.js
@@ -0,0 +1,5 @@
+**React** in-line styles
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+**React** `this.props.children`
+Dealing with `this.props.children`
