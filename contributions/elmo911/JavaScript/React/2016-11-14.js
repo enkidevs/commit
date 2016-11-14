bbfd7dbec255435599a3132958bb38d3a4f4da05
elmo911 <jacobmwolfe@gmail.com>
2016-11-14 15:32:46 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/elmo911/JavaScript/React/2016-11-14.js
@@ -0,0 +1,5 @@
+**React** in-line styles
+Self-closing tags in **JSX**
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+Dealing with `this.props.children`
