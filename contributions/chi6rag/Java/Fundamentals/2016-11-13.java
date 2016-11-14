c7f4d233c10c6802f2c6b7e0ffb21bee55b72c80
chi6rag <chi6rag@gmail.com>
2016-11-14 05:15:04 +0300

    2016-11-13's Java workout

---
+++ contributions/chi6rag/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
