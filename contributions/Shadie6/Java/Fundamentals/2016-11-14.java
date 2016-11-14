f6b2b687c7d032d1eb82790163491f5347513f2d
Shadie6 <l.meroni6@gmail.com>
2016-11-14 13:16:39 +0300

    2016-11-14's Java workout

---
+++ contributions/Shadie6/Java/Fundamentals/2016-11-14.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Using an interface as a parameter
