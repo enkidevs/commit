e02cc851ed46858ec9a7584b03f268f232eb3e8e
diegopapa <diego.papa.job@gmail.com>
2016-11-14 10:04:45 +0300

    2016-11-14's Java workout

---
+++ contributions/diegopapa/Java/Classes & Objects/2016-11-14.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+The `abstract` keyword
+Double Brace Initialization
+String formatting and parsing with `SimpleDateFormat`
