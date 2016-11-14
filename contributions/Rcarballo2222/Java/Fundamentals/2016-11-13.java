75f89712262c1a7611b1517e9376b26b5a0f4ed5
Rcarballo2222 <rcarballo2222@gmail.com>
2016-11-14 03:24:08 +0300

    2016-11-13's Java workout

---
+++ contributions/Rcarballo2222/Java/Fundamentals/2016-11-13.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Calling one constructor from another
+Constant notation
