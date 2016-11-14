16ab6fd55ea88de0f5f14845a97e52faf8620f31
sbvonline <jim.mccormick@sbvonline.co.uk>
2016-11-14 10:33:59 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/sbvonline/JavaScript/React/2016-11-14.js
@@ -0,0 +1,6 @@
+Props in getInitialState Is an        
+Anti-Pattern
+Load Initial Data via AJAX
+**React** `this.props.children`
+Expose Component Functions in **React**
+Use **React** with other libraries
