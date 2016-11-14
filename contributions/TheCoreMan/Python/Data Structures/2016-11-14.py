4902137e4e5a163c05815c56e3ea1e0f99fe1d77
TheCoreMan <dude500@gmail.com>
2016-11-14 20:23:06 +0300

    2016-11-14's Python workout

---
+++ contributions/TheCoreMan/Python/Data Structures/2016-11-14.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Immutable sets with `frozenset`
+Enhance your `tuple`s
+There is more to copying
+Keep things in order with `OrderedDict`
