7b9169751e16897bca579c26dd7868ab8dc03c8a
fernandoGN <fernando.a.gomes.n@gmail.com>
2016-11-14 18:52:52 +0300

    2016-11-14's Java workout

---
+++ contributions/fernandoGN/Java/Fundamentals/2016-11-14.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Writing generic methods
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
