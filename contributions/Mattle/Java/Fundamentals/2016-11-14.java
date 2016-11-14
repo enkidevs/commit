c8e9a093de7dd10266cfe98f644f2e0c2f1757e1
Mattle <mcallier@gmail.com>
2016-11-14 20:37:29 +0300

    2016-11-14's Java workout

---
+++ contributions/Mattle/Java/Fundamentals/2016-11-14.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Using an interface as a parameter
