6cbcbf3e7bdc5e8c43c5790d9ae937c5d027fcbb
danilopiazza <danilopiazza@gmail.com>
2016-11-14 22:57:35 +0300

    2016-11-14's Linux workout

---
+++ contributions/danilopiazza/Linux/Networking/2016-11-14.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Multiple IP Address with aliasing 
+Disable `IPv6`
+Use `tcpdump` to listen to network interface traffic
+Staying connected remotely via SSH without an interactive shell
