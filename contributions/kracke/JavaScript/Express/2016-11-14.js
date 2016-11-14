6fe197bcb72eadd41848668d3d82dca6a666a70a
kracke <kracke@infomax-it.de>
2016-11-14 19:47:38 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/kracke/JavaScript/Express/2016-11-14.js
@@ -0,0 +1,5 @@
+`response` object additional properties in **Express**
+Easy way to deliver **HTML** pages with **Express**
+**GZIP** compression for **Express**
+`body-parser` in **Express**
+Error handler in **Express**
