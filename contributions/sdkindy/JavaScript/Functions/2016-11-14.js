4631432e47d827401fcf65b46eea06bed0fe218b
sdkindy <sonam.d.kindy@gmail.com>
2016-11-15 00:42:23 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/sdkindy/JavaScript/Functions/2016-11-14.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Difference between **.call** and **.apply** methods
+Social sharing without widgets
