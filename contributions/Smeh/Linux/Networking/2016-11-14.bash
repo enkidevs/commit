057fd5297c0e3c1cc9d96b1727fd2bacca9bd9c5
Smeh <sgarcia030@gmail.com>
2016-11-14 10:54:52 +0300

    2016-11-14's Linux workout

---
+++ contributions/Smeh/Linux/Networking/2016-11-14.bash
@@ -0,0 +1,5 @@
+Find services running on your host
+List your IPv6 configuration
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Short Introduction to `Wget` 
