18a090dfe8ad96c5e7329d09ece76b2a3bf5f971
Howard87 <ryangiggs87@gmail.com>
2016-11-14 20:03:53 +0300

    2016-11-14's Java workout

---
+++ contributions/Howard87/Java/Fundamentals/2016-11-14.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
