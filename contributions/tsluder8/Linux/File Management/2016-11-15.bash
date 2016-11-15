c0cb21595b22363f3b56e0e289714b9716ac153b
tsluder8 <tsluder8@gmail.com>
2016-11-15 15:18:26 +0300

    2016-11-15's Linux workout

---
+++ contributions/tsluder8/Linux/File Management/2016-11-15.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
