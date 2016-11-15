de0a4483b99ddeaceedfae425e25c47eaf13e987
BOSSoNe0013 <bossone0013@gmail.com>
2016-11-15 15:16:16 +0300

    2016-11-15's Java workout

---
+++ contributions/BOSSoNe0013/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use strings in a `switch` statement
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
