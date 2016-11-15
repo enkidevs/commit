250b702e11a5e7206f2f2aebbbfd46824d196575
BooM <tararuj.michal@gmail.com>
2016-11-15 09:53:23 +0300

    2016-11-15's Linux workout

---
+++ contributions/BooM/Linux/File Management/2016-11-15.bash
@@ -0,0 +1,6 @@
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
