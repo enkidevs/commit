a6f384c21f5cf09ec3a4934abc7794099b8396a7
Narmer <narmer.menes@icloud.com>
2016-11-15 04:59:24 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/Narmer/JavaScript/Express/2016-11-14.js
@@ -0,0 +1,5 @@
+`response` object additional properties in **Express**
+**GZIP** compression for **Express**
+`cookie-session` in **Express**
+Log with `morgan` in **Express**
+`vhost` in **Express**
