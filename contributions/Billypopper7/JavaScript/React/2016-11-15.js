a936f8fce50fed6d7c191d65bb64a0cd3625f459
Billypopper7 <billypopper7@gmail.com>
2016-11-16 02:23:23 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/Billypopper7/JavaScript/React/2016-11-15.js
@@ -0,0 +1,5 @@
+Self-closing tags in **JSX**
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+Using `false` in **JSX**
+Dealing with `this.props.children`
