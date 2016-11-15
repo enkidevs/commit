27ce68b47e97d92eead3d598f5492c1345435c04
davecneal <davecneal@gmail.com>
2016-11-15 23:34:17 +0300

    2016-11-15's Linux workout

---
+++ contributions/davecneal/Linux/System Management/2016-11-15.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Removing old kernels in Debian based systems
+Check the HDD with `badblocks`
+Finding Open Files With `lsof`
