39d36035dc2c6ab8a9dd368b61b5a58724a6768e
lordmalvern <aooogah@gmail.com>
2016-11-15 07:18:43 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/lordmalvern/JavaScript/Functions/2016-11-14.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+IIFE pattern
+Easily generate a random `HEX` color
+Check if a document is done loading
