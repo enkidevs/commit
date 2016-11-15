045db915426964e0a2ea8b5e44d15436c67cf2ed
mikemccready <michaeljmccready@gmail.com>
2016-11-15 19:24:36 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/mikemccready/JavaScript/Functions/2016-11-15.js
@@ -0,0 +1,5 @@
+Function constructor vs. function expression
+Functions can be declared after use
+Easily generate a random `HEX` color
+Move cursor at the end of text input
+Changing a functions context with `fn.call(object)`
