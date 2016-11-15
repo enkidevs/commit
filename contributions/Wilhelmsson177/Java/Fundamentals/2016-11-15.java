cd445220a4d601abc068599defacd2bca3644d9a
Wilhelmsson177 <meierhofwilli@gmail.com>
2016-11-15 20:19:53 +0300

    2016-11-15's Java workout

---
+++ contributions/Wilhelmsson177/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
