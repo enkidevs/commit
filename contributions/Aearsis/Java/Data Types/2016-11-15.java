d27c199715136fb82c9ef8dae27f45d089489ac9
Aearsis <aearsis@eideo.cz>
2016-11-15 11:58:33 +0300

    2016-11-15's Java workout

---
+++ contributions/Aearsis/Java/Data Types/2016-11-15.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Finding a substring in a string
+Equals operation on different data types
