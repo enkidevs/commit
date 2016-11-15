c8029ed9e5659b12e284ae25e88effda5caa6bb1
mingruimingrui <mingruimingrui@hotmail.com>
2016-11-15 15:43:59 +0300

    2016-11-14's Java workout

---
+++ contributions/mingruimingrui/Java/Fundamentals/2016-11-14.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
