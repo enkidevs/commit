d90aa40affff3c2b339283f5dc0ec13064b3fa07
clouso <christian.lausberger@gmail.com>
2016-11-15 22:20:27 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/clouso/JavaScript/React/2016-11-15.js
@@ -0,0 +1,5 @@
+Self-closing tags in **JSX**
+Pixel values in style props for **React**
+`if`-`else` statements in **JSX** and **React**
+**React** `this.props.children`
+Expose Component Functions in **React**
