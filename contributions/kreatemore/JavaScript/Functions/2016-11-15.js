d7b8d551a057124ae44aafcf9bbba865eebf2d6e
kreatemore <kreatemore@gmail.com>
2016-11-15 11:53:36 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/kreatemore/JavaScript/Functions/2016-11-15.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Functions can be declared after use
+IIFE pattern
+Easily generate a random `HEX` color
+Social sharing without widgets
