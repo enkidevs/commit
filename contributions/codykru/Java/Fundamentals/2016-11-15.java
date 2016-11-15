6ecd73ec249232326e080e9d123b07ede7e2a704
codykru <codykru@gmail.com>
2016-11-16 02:08:12 +0300

    2016-11-15's Java workout

---
+++ contributions/codykru/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
