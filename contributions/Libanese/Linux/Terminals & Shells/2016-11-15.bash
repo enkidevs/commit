e94334f3279344fbe1c3716ff6ae90628146e816
Libanese <attiliomandola@gmail.com>
2016-11-15 15:33:06 +0300

    2016-11-15's Linux workout

---
+++ contributions/Libanese/Linux/Terminals & Shells/2016-11-15.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Repeating commands with `watch`
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Useful aliasing in bash
