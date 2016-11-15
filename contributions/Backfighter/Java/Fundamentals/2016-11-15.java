4b4a8813f9f4107d7783eb9947b335a599e33038
Backfighter <wpw.peter@gmail.com>
2016-11-16 00:13:26 +0300

    2016-11-15's Java workout

---
+++ contributions/Backfighter/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
