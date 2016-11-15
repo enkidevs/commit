129e4ec95a39999a79fe3cc628555b75548b1a8f
Fengal <loic@econolyst.co.uk>
2016-11-16 02:02:47 +0300

    2016-11-15's Git workout

---
+++ contributions/Fengal/Git/Essentials/2016-11-15.bash
@@ -0,0 +1,5 @@
+Pushing tags to a server
+You can use git offline!
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
