db0473613f4d01f5e2b4c09094cd3ce4117266fa
motabass <markus.mohoritsch@gmx.at>
2016-11-15 20:46:25 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/motabass/JavaScript/Functions/2016-11-15.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Anonymous functions
+Check if a document is done loading
