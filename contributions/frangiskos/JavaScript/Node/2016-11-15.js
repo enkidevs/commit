a778a61b2e854a141e09c09cf89bdbec802c86eb
frangiskos <frangiskos@gmail.com>
2016-11-16 02:55:38 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/frangiskos/JavaScript/Node/2016-11-15.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Http Server in **Node**
+Use conventional property names for error handling in Node.js
+`process.nextTick()` callback pattern
+Manage child processes in **Node**
