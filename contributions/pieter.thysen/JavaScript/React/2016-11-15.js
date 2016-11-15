298da330a6fcff8de8be6f13f0476baafccb3154
pieter.thysen <pieter.thysen@gmail.com>
2016-11-15 22:11:48 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/pieter.thysen/JavaScript/React/2016-11-15.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+`if`-`else` statements in **JSX** and **React**
+componentWillReceiveProps Not Triggered After Mounting
+
+Prop Validation
+Specify a single child
