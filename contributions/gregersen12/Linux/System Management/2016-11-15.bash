a2eb60d35caa645a02c9b153f6a6d50890defc63
gregersen12 <marting12@live.dk>
2016-11-15 11:38:35 +0300

    2016-11-15's Linux workout

---
+++ contributions/gregersen12/Linux/System Management/2016-11-15.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
