3bccd67749173c094c5944fa3034d87fde529b62
odys1528 <jolanta.zakrzewska@orange.pl>
2016-11-15 11:23:17 +0300

    2016-11-15's Java workout

---
+++ contributions/odys1528/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
