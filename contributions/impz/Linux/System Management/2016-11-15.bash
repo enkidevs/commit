da8e1cc49c109428eb671c7b3189946cd65bd23a
impz <enki@burchill.net>
2016-11-16 02:50:21 +0300

    2016-11-15's Linux workout

---
+++ contributions/impz/Linux/System Management/2016-11-15.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Executing scripts on system boot using `systemd`
+Fixing `locale` issues in Debian systems
+Finding Open Files With `lsof`
