d8d16ec37c15bc51f904254192bedd4f6be333d5
Jebediah <jefwillems100@gmail.com>
2016-11-15 11:51:41 +0300

    2016-11-15's Linux workout

---
+++ contributions/Jebediah/Linux/Terminals & Shells/2016-11-15.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+The Basics of Environment Variables
