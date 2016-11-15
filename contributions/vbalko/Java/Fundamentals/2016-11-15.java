559cae872c1ae50d6ac4ffa9a827b7f470e519da
vbalko <vbalko@gmail.com>
2016-11-15 14:50:07 +0300

    2016-11-15's Java workout

---
+++ contributions/vbalko/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Generating random numbers
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
+Metadata: creating a user-defined file attribute
