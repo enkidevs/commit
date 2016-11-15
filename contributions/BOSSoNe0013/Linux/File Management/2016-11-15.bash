a6aafca8e1f1639512e3d97ad20d3ec727bf197b
BOSSoNe0013 <bossone0013@gmail.com>
2016-11-15 15:26:27 +0300

    2016-11-15's Linux workout

---
+++ contributions/BOSSoNe0013/Linux/File Management/2016-11-15.bash
@@ -0,0 +1,7 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
