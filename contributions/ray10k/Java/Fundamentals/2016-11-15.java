1d8b975d61af9cd1f56324e4dfbaef440144b570
ray10k <w.j.van.den.heuvel@xmsnet.nl>
2016-11-15 12:11:32 +0300

    2016-11-15's Java workout

---
+++ contributions/ray10k/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Using an interface as a parameter
