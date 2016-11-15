068710459c7f39cf53660059b6519042ccca9291
drugarce <drugarce@mt.net.mk>
2016-11-15 09:16:54 +0300

    2016-11-15's Git workout

---
+++ contributions/drugarce/Git/Essentials/2016-11-15.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to unmodify a modified file
+The three states in git
+Search by commit message keyword
+Remote repositories: viewing, editing and deleting
