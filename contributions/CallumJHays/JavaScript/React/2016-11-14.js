9a1b12a2ffb80206a51ff1b9ef0e4a6fbf42937d
CallumJHays <callumjhays@gmail.com>
2016-11-15 03:54:39 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/CallumJHays/JavaScript/React/2016-11-14.js
@@ -0,0 +1,6 @@
+`if`-`else` statements in **JSX** and **React**
+Type of the Children props
+Props in getInitialState Is an        
+Anti-Pattern
+Expose Component Functions in **React**
+Shallow compare in **React**
