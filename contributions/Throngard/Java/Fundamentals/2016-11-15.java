83cf668d0c3af507401f0b683d127a9686e1db9b
Throngard <peterses@in.tum.de>
2016-11-15 11:59:16 +0300

    2016-11-15's Java workout

---
+++ contributions/Throngard/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
