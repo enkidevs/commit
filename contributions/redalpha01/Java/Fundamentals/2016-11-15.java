91f3ada4ab91d9ea25c191fdedff29ca37e8282f
redalpha01 <redalpha01@gmail.com>
2016-11-15 18:17:07 +0300

    2016-11-15's Java workout

---
+++ contributions/redalpha01/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
