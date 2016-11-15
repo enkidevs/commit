ddd039b0bc576bb6f8ae1e142ad1fa039ed4bfda
totespare <borjafac90@gmail.com>
2016-11-15 10:33:07 +0300

    2016-11-15's Java workout

---
+++ contributions/totespare/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
