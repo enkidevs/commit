f52f09c8cc8b16c8f63239a41217a65e6dcc3c2d
vlad.bezden <vladbezden@yahoo.com>
2016-11-16 02:44:54 +0300

    2016-11-15's Java workout

---
+++ contributions/vlad.bezden/Java/I/O/2016-11-15.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Translating `Scanner` tokens into primitive types
+Listing a file system's root directories
+Deleting files and directories
+The `Console` class
