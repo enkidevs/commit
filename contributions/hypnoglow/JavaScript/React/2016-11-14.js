3c59467170859495febd804ac406ae11fef87478
hypnoglow <hypnoglow@gmail.com>
2016-11-15 07:55:02 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/hypnoglow/JavaScript/React/2016-11-14.js
@@ -0,0 +1,6 @@
+Self-closing tags in **JSX**
+Pixel values in style props for **React**
+componentWillReceiveProps Not Triggered After Mounting
+
+**React** `this.props.children`
+`ref` callback attribute
