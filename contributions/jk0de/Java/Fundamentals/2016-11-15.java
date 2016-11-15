b673279e39808e959942a752e6d51e459265a221
jk0de <gizmo1398@gmail.com>
2016-11-15 20:04:15 +0300

    2016-11-15's Java workout

---
+++ contributions/jk0de/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Metadata: setting a file's owner
+Detect or prevent integer overflow
