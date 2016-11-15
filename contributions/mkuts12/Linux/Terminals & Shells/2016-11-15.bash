4b89e221fd35074013f87fde0223f1d4d80e2fe2
mkuts12 <mkutz@elitemail.org>
2016-11-15 09:39:10 +0300

    2016-11-15's Linux workout

---
+++ contributions/mkuts12/Linux/Terminals & Shells/2016-11-15.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Adding directories to your `$PATH`
+Retrieve previous arguments
+The Basics of Environment Variables
