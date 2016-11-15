cabb93459ed92507a7098d32b0bd8e694d6f862b
JLouis <jlsalbego@gmail.com>
2016-11-15 16:33:16 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/JLouis/JavaScript/Functions/2016-11-15.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Anonymous functions
+Social sharing without widgets
