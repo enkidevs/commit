42be38350fadc0a61d10c95acd4715f46c9f8386
0xtvarun <vtewari26@gmail.com>
2016-11-15 15:46:45 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/0xtvarun/JavaScript/Node/2016-11-14.js
@@ -0,0 +1,5 @@
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+Manage child processes in **Node**
+`try-catch` only for **sync** code
+Middleware/pipeline design pattern
