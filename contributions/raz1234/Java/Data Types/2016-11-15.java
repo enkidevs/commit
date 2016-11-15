c7982c8b7dcc4680b9304fcd8a0f6a50728dd08f
raz1234 <raz_conc@yahoo.com>
2016-11-15 11:06:39 +0300

    2016-11-15's Java workout

---
+++ contributions/raz1234/Java/Data Types/2016-11-15.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
