a722eb56861bd1a17e24308b9321e4a6d7ba94a6
Hari-VS <harivs93@gmail.com>
2016-11-15 15:36:42 +0300

    2016-11-14's Java workout

---
+++ contributions/Hari-VS/Java/I/O/2016-11-14.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
