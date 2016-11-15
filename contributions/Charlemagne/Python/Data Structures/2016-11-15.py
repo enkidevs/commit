7baa579d2d4c4d335e1f520fcb3fedfb32c5c62c
Charlemagne <alexander-g@gmx.net>
2016-11-15 22:19:30 +0300

    2016-11-15's Python workout

---
+++ contributions/Charlemagne/Python/Data Structures/2016-11-15.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+When `range` comes in handy
+Double ended queues with `deque`
+Operations with `bytes` and `bytearray`
+Binary record packing with `bytearray`
