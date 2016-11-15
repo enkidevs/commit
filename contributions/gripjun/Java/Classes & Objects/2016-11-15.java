5a28be775f790d01b9ed3220356a2b8efe2dd8a5
gripjun <gripjun@gmail.com>
2016-11-16 01:46:50 +0300

    2016-11-15's Java workout

---
+++ contributions/gripjun/Java/Classes & Objects/2016-11-15.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+The `abstract` keyword
+Diamond Operator
+String formatting and parsing with `SimpleDateFormat`
