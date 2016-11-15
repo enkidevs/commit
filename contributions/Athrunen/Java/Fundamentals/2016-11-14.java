2c02c3846c3abcc24d085b43bda53f26b83ef10e
Athrunen <athrunen@tendria.de>
2016-11-15 03:59:43 +0300

    2016-11-14's Java workout

---
+++ contributions/Athrunen/Java/Fundamentals/2016-11-14.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+Using `synchronized` statements
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
