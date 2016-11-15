c8ab243e643117a77f43b9a530e0621dc8be59b4
LoicTosser <loic.tosser.29@gmail.com>
2016-11-15 17:28:29 +0300

    2016-11-15's Java workout

---
+++ contributions/LoicTosser/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
