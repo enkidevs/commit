3e335bdec22992010e3aa2ff1f747c92839e9d43
bobahrus <bobahrus@gmail.com>
2016-11-15 13:33:04 +0300

    2016-11-15's Java workout

---
+++ contributions/bobahrus/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Writing generic methods
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+Detect or prevent integer overflow
+Using an interface as a parameter
