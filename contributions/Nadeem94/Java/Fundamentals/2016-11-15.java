4d5abd84d39bd4e9a6ad86bacefa8db718dd432d
Nadeem94 <nadeem_ahmed94@hotmail.com>
2016-11-15 11:47:54 +0300

    2016-11-15's Java workout

---
+++ contributions/Nadeem94/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
