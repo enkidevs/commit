24bd2685a9edba406c4a9dd7d3095aa43beff66b
kirillku <kubryakov@gmail.com>
2016-11-15 19:53:58 +0300

    2016-11-15's Linux workout

---
+++ contributions/kirillku/Linux/Package Management/2016-11-15.bash
@@ -0,0 +1,6 @@
+Install specific version of packages
+Backporting on Debian
+
+Installing a `.deb` package from the terminal
+Prevent updating a specific package in Debian systems
+Using `Tasksel` for software installation
