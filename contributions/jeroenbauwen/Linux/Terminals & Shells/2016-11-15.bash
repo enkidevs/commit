731b177c08547e588bf6dc2c6ed18d0c1d7c5d29
jeroenbauwen <jeroen.bauwen@gmail.com>
2016-11-15 14:38:19 +0300

    2016-11-15's Linux workout

---
+++ contributions/jeroenbauwen/Linux/Terminals & Shells/2016-11-15.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal incognito mode
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Retrieve previous arguments
