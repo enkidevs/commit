3131bd3525e7f10831e8860a572bb158de5f4fb2
carlzoo <carl.j.zhou@protonmail.ch>
2016-11-16 01:21:06 +0300

    2016-11-15's Java workout

---
+++ contributions/carlzoo/Java/Error Handling/2016-11-15.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Prevent `exceptions` while logging Data
