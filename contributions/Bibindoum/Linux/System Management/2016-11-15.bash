edc6af4af074f8e6af9c148171c1893077dd3228
Bibindoum <pierrick.guitter@gmail.com>
2016-11-15 15:36:20 +0300

    2016-11-15's Linux workout

---
+++ contributions/Bibindoum/Linux/System Management/2016-11-15.bash
@@ -0,0 +1,6 @@
+Detect your linux distribution
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Check the HDD with `badblocks`
