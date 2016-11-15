b9bf45bff72188fa4a37f1fe77d10a9de4a01101
ScottBrenner <scottstimo@gmail.com>
2016-11-15 21:32:43 +0300

    2016-11-15's Linux workout

---
+++ contributions/ScottBrenner/Linux/File Management/2016-11-15.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Deleting files in a secure manner
