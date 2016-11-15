ceffa571087464b4f6b74fd1abefced1bb431772
edm00se <edm00se@gmail.com>
2016-11-15 15:45:40 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/edm00se/JavaScript/Functions/2016-11-15.js
@@ -0,0 +1,5 @@
+Hoisting
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Easily generate a random `HEX` color
+Social sharing without widgets
