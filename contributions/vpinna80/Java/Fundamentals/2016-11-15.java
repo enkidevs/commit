400782c2608fd5e0c7c07a674f66b08a17aa2db2
vpinna80 <valentino.pinna@bancaditalia.it>
2016-11-15 22:20:00 +0300

    2016-11-15's Java workout

---
+++ contributions/vpinna80/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
