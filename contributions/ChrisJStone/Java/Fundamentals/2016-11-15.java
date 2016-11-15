58be2faf9ff2ddaa68ecf55127def53b26de0662
ChrisJStone <closetoeuphoria@gmail.com>
2016-11-15 14:14:24 +0300

    2016-11-15's Java workout

---
+++ contributions/ChrisJStone/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
