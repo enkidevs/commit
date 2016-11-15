61ca779900fdb08b3c821a7c151adba8d4ef7a08
Riscum <domingos.ferreir@gmail.com>
2016-11-15 13:42:29 +0300

    2016-11-15's Java workout

---
+++ contributions/Riscum/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Generating random numbers
+The trade-off between short and descriptive names
+Common mistake on switch statements
+Calling one constructor from another
