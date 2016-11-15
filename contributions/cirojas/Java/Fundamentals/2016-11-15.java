277b5c37bf16070b6060c5e82413783807a8a726
cirojas <c.rojasvictoriano@gmail.com>
2016-11-16 01:00:52 +0300

    2016-11-15's Java workout

---
+++ contributions/cirojas/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Generating random numbers
+Using `Printstream.format` to format a string before printing
