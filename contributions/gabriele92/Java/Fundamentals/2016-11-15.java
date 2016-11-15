ae0eacf991f1e12d63e2529b1fab27b0443ade3b
gabriele92 <gabrigaudenzi@gmail.com>
2016-11-15 17:08:15 +0300

    2016-11-15's Java workout

---
+++ contributions/gabriele92/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
