ca1862cb7eb68b4c144811179dc400dba77f3630
ptreitler <peter.treitler@gmail.com>
2016-11-15 13:46:04 +0300

    2016-11-15's Linux workout

---
+++ contributions/ptreitler/Linux/Terminals & Shells/2016-11-15.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Terminal based browser
+Terminal incognito mode
+The Basics of Environment Variables
