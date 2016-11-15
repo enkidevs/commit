69bc25312a7f1724736a11f1d322aae76cb09a3f
kiki407 <kiki407@hotmail.com>
2016-11-15 11:22:13 +0300

    2016-11-15's Git workout

---
+++ contributions/kiki407/Git/Essentials/2016-11-15.bash
@@ -0,0 +1,5 @@
+How to write a git commit message
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
+Ignore files in git
