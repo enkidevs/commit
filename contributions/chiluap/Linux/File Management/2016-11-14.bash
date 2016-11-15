153ebf46fedda7fc9c65f6ffeb10ab01a04dd1f2
chiluap <christian@paulipost.de>
2016-11-15 14:21:29 +0300

    2016-11-14's Linux workout

---
+++ contributions/chiluap/Linux/File Management/2016-11-14.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
