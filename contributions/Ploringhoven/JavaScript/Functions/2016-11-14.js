8dae8b4de4fbdb4ecbd37a796039aef801d79313
Ploringhoven <p.loringhoven@team-advertico.de>
2016-11-15 11:24:57 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/Ploringhoven/JavaScript/Functions/2016-11-14.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Method chaining
+Changing a functions context with `fn.call(object)`
