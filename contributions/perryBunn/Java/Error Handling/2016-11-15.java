fd768d4ad4be3087c3a71abc26d20e9c1223f09c
perryBunn <eagle4ever20@gmail.com>
2016-11-15 22:28:03 +0300

    2016-11-15's Java workout

---
+++ contributions/perryBunn/Java/Error Handling/2016-11-15.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Creating new Exceptions
+Most common reason behind **stack overflow** error
