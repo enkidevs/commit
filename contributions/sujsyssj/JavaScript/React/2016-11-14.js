51cf75910b36d511b18c8f8f2e7e4fc36cd7208b
sujsyssj <sujayssj@gmail.com>
2016-11-15 04:49:19 +0300

    2016-11-14's JavaScript workout

---
+++ contributions/sujsyssj/JavaScript/React/2016-11-14.js
@@ -0,0 +1,6 @@
+**React** in-line styles
+`if`-`else` statements in **JSX** and **React**
+componentWillReceiveProps Not Triggered After Mounting
+
+Load Initial Data via AJAX
+**React** `this.props.children`
