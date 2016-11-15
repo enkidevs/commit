8524589c1271b5cc4e4275b58f7194fd0dd6e2c3
aeleftheriadis <athanasios.eleftheriadis@gmail.com>
2016-11-15 09:14:08 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/aeleftheriadis/JavaScript/Functions/2016-11-15.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+`.bind()` 
+Social sharing without widgets
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
