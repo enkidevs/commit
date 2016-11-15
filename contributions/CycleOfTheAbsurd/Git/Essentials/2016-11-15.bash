24cc67258627983a43c402746ce87dd208e88ffb
CycleOfTheAbsurd <alex7up42@gmail.com>
2016-11-15 17:59:53 +0300

    2016-11-15's Git workout

---
+++ contributions/CycleOfTheAbsurd/Git/Essentials/2016-11-15.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to unstage a staged file
+How to unmodify a modified file
+Using tags for version control
+Remote repositories: viewing, editing and deleting
