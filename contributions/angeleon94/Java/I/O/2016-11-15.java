6d115acdac3de07e1ff698bc509a2f2131ca2cf8
angeleon94 <angeleon94@gmail.com>
2016-11-15 20:15:17 +0300

    2016-11-15's Java workout

---
+++ contributions/angeleon94/Java/I/O/2016-11-15.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Listing a file system's root directories
+The `Console` class
