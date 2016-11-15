721d323be34a7af1c5c192cdbfe6fc5001ae3131
barte243 <solberg23@wp.pl>
2016-11-15 12:05:17 +0300

    2016-11-15's Linux workout

---
+++ contributions/barte243/Linux/Networking/2016-11-15.bash
@@ -0,0 +1,5 @@
+Find services running on your host
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
+Make use of `netstat`
+Short Introduction to `Wget` 
