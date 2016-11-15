f1bbec1ee2f661e5718a3357ace8c491bc2b3e01
Aristeia <11025x12@gmail.com>
2016-11-16 02:36:49 +0300

    2016-11-15's Java workout

---
+++ contributions/Aristeia/Java/Error Handling/2016-11-15.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Throwing an `exception`
