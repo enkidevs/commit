33d2a990be055bc241f0672ef4bd4bda302bb21d
lordmalvern <aooogah@gmail.com>
2016-11-15 18:40:36 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/lordmalvern/JavaScript/Node/2016-11-15.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Global Objects and Environment Variables in **Node**
+**Node** `os` module
+Wrapping errors in Node.js using _node-verror_
+Middleware/pipeline design pattern
