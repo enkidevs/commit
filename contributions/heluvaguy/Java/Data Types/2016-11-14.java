cc1454790f43f24cc6348a53f5464df06f74c048
heluvaguy <adarshjaiswal1989@gmail.com>
2016-11-15 04:16:56 +0300

    2016-11-14's Java workout

---
+++ contributions/heluvaguy/Java/Data Types/2016-11-14.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Converting numbers to strings
+Converting a string to upper or lower case
+Equals operation on different data types
+Retrieve the component type of an array
