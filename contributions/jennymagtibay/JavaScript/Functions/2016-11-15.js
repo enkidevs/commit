9fb4410e9c0d346e2c14e06655daf3742fcbd870
jennymagtibay <jenifer.magtibay@gmail.com>
2016-11-15 11:02:35 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/jennymagtibay/JavaScript/Functions/2016-11-15.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function declaration vs. function expression
+Functions can be declared after use
+Method chaining
+Function call method
