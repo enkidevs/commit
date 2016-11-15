549bd4c21e1bbb982c6468767f381b88aa11b777
doomi <kurai7hikari@gmail.com>
2016-11-15 22:23:06 +0300

    2016-11-15's Linux workout

---
+++ contributions/doomi/Linux/File Management/2016-11-15.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
