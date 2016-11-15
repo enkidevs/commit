f16278712795a43441270ba98330447c420356c3
pyrise <pyrise@gmail.com>
2016-11-15 18:02:03 +0300

    2016-11-15's Java workout

---
+++ contributions/pyrise/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Java Virtual Machine
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Metadata: setting a file's owner
+Detect or prevent integer overflow
