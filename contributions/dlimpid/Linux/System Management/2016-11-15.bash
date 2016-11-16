773e3c5f707ae02ac69fd24ff02262e08b56c52f
dlimpid <dlimpid@gmail.com>
2016-11-16 05:04:49 +0300

    2016-11-15's Linux workout

---
+++ contributions/dlimpid/Linux/System Management/2016-11-15.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+CPU benchmark with `dd`
+Removing old kernels in Debian based systems
+Fixing `locale` issues in Debian systems
+Cancel a system shutdown
