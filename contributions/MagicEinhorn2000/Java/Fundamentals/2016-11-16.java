871e118fe711c02f15f509bb400e9d9d2d1cb9cd
MagicEinhorn2000 <ks.wischmop@gmail.com>
2016-11-16 12:21:28 +0300

    2016-11-16's Java workout

---
+++ contributions/MagicEinhorn2000/Java/Fundamentals/2016-11-16.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
