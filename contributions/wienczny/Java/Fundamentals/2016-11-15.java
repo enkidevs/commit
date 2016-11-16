f74ccb65ae2d97b8d0af031516ac8e64dd80bb44
wienczny <wienczny@gmail.com>
2016-11-16 03:02:16 +0300

    2016-11-15's Java workout

---
+++ contributions/wienczny/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
