909dfa6ef9572095e1da676a88e5f8e4b05691ab
raz1234 <raz_conc@yahoo.com>
2016-11-16 11:59:00 +0300

    2016-11-16's JavaScript workout

---
+++ contributions/raz1234/JavaScript/Core/2016-11-16.js
@@ -0,0 +1,5 @@
+Shuffle an array using `sort` and `random`.
+`if/else` shortcut `conditional operator`
+Use `splice()` to remove an item from an array
+Inaccuracy of binary floating-point format
+Base conversion 
