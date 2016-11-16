e872c97149350eb95f191115fb8de983e37b5b89
MichaelRijsdijk <michaelrijsdijk@gmail.com>
2016-11-16 13:05:40 +0300

    2016-11-16's Java workout

---
+++ contributions/MichaelRijsdijk/Java/Fundamentals/2016-11-16.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
