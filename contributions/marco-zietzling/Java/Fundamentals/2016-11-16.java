15ead6350aa6ba279e4f06a8ad2d3892a4250a97
marco-zietzling <marco.zietzling@gmail.com>
2016-11-16 10:08:03 +0300

    2016-11-16's Java workout

---
+++ contributions/marco-zietzling/Java/Fundamentals/2016-11-16.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Updating interfaces by using `default` methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
