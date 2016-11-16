ae8da845db15b945d3e61e12b80030d66e5f76f2
mistdrg <mistdrg@naver.com>
2016-11-16 06:11:50 +0300

    2016-11-15's Java workout

---
+++ contributions/mistdrg/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Use meaningful names
+The trade-off between short and descriptive names
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
