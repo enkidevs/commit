5164dcb939bf8a399359543e59a2327d0178c69a
sbvonline <jim.mccormick@sbvonline.co.uk>
2016-11-16 10:57:52 +0300

    2016-11-16's JavaScript workout

---
+++ contributions/sbvonline/JavaScript/Functions/2016-11-16.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function constructor vs. function expression
+`.bind()` 
+Easily generate a random `HEX` color
+Move cursor at the end of text input
