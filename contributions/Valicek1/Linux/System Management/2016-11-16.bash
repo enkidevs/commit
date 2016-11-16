aa1e8acf2723de6b3f2e70c671a623c3689ed8c9
Valicek1 <valicek1994@gmail.com>
2016-11-16 10:33:20 +0300

    2016-11-16's Linux workout

---
+++ contributions/Valicek1/Linux/System Management/2016-11-16.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Detect your linux distribution
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
