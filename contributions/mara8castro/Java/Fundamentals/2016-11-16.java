92f51fb964c4bef4457d58d37d8b05985f426b92
mara8castro <mara8castro@gmail.com>
2016-11-16 16:41:00 +0300

    2016-11-16's Java workout

---
+++ contributions/mara8castro/Java/Fundamentals/2016-11-16.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Measuring time
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
