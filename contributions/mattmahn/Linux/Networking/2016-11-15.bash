5be830530c7ae5bcfffb1b10c0449c0b3059f1d4
mattmahn <mahnkem@msoe.edu>
2016-11-16 09:59:13 +0300

    2016-11-15's Linux workout

---
+++ contributions/mattmahn/Linux/Networking/2016-11-15.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Logging dropped firewall packets
+Find services running on your host
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
