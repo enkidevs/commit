41bdc04efe35a872f097942f1b8dd022c046ac22
ruoptic <stroke.the.cat.again@gmail.com>
2016-11-16 15:56:05 +0300

    2016-11-16's Linux workout

---
+++ contributions/ruoptic/Linux/File Management/2016-11-16.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Easy way of sharing files
