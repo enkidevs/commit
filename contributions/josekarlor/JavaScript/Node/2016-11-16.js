e7b7048ebe225d8a5082baa205b19bf6bfc735cf
josekarlor <josecarlosdemula@gmail.com>
2016-11-16 11:56:42 +0300

    2016-11-16's JavaScript workout

---
+++ contributions/josekarlor/JavaScript/Node/2016-11-16.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
+Basic web server with Express
