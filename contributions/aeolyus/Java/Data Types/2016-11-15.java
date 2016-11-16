7706d8a36757b667090bb67e5e3ffb1781b27a25
aeolyus <richardhuang.huang@gmail.com>
2016-11-16 05:49:28 +0300

    2016-11-15's Java workout

---
+++ contributions/aeolyus/Java/Data Types/2016-11-15.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+`StringBuffer` vs `StringBuilder`
