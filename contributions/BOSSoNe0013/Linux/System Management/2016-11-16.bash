685307eca004d27c334636c536a814b51b8f0155
BOSSoNe0013 <bossone0013@gmail.com>
2016-11-16 13:17:45 +0300

    2016-11-16's Linux workout

---
+++ contributions/BOSSoNe0013/Linux/System Management/2016-11-16.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Basic service management with `systemd`
