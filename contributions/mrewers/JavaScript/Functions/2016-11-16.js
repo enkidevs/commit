5be55bda2ae7ffb52d803e18b5a3ebad0af5d42a
mrewers <marekrewers@gmail.com>
2016-11-16 17:03:55 +0300

    2016-11-16's JavaScript workout

---
+++ contributions/mrewers/JavaScript/Functions/2016-11-16.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function declarations vs function expressions
+Function constructor vs. function expression
+Get query/url variables
+Changing a functions context with `fn.call(object)`
