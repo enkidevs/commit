75edb802cf73913174a74078033bef4b72298cdc
Antikythera <voodoomanic@gmail.com>
2016-11-16 04:11:52 +0300

    2016-11-15's Linux workout

---
+++ contributions/Antikythera/Linux/System Management/2016-11-15.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Removing old kernels in Debian based systems
+Common log files
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
