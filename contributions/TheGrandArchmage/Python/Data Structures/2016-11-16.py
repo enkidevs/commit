c098cd03cab525b17e9b5776ba60bc1c727198cb
TheGrandArchmage <pilatosil@gmail.com>
2016-11-16 13:54:35 +0300

    2016-11-16's Python workout

---
+++ contributions/TheGrandArchmage/Python/Data Structures/2016-11-16.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+When `range` comes in handy
+Special attributes of objects and classes
+Enhance your `tuple`s
+Get the most of `int`s
