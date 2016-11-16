cff6ff149089f08c4ecdc4ba46ebb37423ab104b
saka <saka71996@gmail.com>
2016-11-16 14:10:30 +0300

    2016-11-16's Linux workout

---
+++ contributions/saka/Linux/Terminals & Shells/2016-11-16.bash
@@ -0,0 +1,7 @@
+Clear the terminal instantly
+Clear bash history
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
