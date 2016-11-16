e58698763026eaefc405e25da6eb8b23f1202962
magx2 <martin.grzeslowski@gmail.com>
2016-11-16 11:04:29 +0300

    2016-11-16's Java workout

---
+++ contributions/magx2/Java/Fundamentals/2016-11-16.java
@@ -0,0 +1,5 @@
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
