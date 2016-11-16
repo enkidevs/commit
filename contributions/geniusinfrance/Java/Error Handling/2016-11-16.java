91654a8162354d3d287627c78651cbded0f1c4e9
geniusinfrance <ryan.bergerson@gmail.com>
2016-11-16 10:30:01 +0300

    2016-11-16's Java workout

---
+++ contributions/geniusinfrance/Java/Error Handling/2016-11-16.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Prevent `exceptions` while logging Data
+Throwing an `exception`
