1e3ae663af81c7df1e3a0f73b370b2f623d14760
Pluckjar <pluckjar@gmail.com>
2016-11-16 10:08:27 +0300

    2016-11-16's Linux workout

---
+++ contributions/Pluckjar/Linux/Package Management/2016-11-16.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+List installed packages
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
