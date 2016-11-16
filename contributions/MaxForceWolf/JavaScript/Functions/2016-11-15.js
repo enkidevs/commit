21168f8f4bf111c63e909b14bc9ab2a305d7edfd
MaxForceWolf <dsbear@gmail.com>
2016-11-16 03:08:54 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/MaxForceWolf/JavaScript/Functions/2016-11-15.js
@@ -0,0 +1,5 @@
+Function constructor vs. function expression
+Functions can be declared after use
+Method chaining
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
