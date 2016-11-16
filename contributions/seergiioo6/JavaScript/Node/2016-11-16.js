57478ed9ce48379766c11c4d85d3486a1b17daf1
seergiioo6 <hello@sergioandres.io>
2016-11-16 15:41:29 +0300

    2016-11-16's JavaScript workout

---
+++ contributions/seergiioo6/JavaScript/Node/2016-11-16.js
@@ -0,0 +1,5 @@
+First-error callbacks in **Node**
+The built-in Node debugger
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+Manage child processes in **Node**
