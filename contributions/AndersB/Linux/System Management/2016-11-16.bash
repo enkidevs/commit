6c44cd35b2f35ffa73a82b58ec01937702a3c783
AndersB <anders.barth@gmail.com>
2016-11-16 11:00:15 +0300

    2016-11-16's Linux workout

---
+++ contributions/AndersB/Linux/System Management/2016-11-16.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Using `dd` To Create Disk Images
