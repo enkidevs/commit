ae7848a03e5d221d31d710eefeee9dbe2bae5ea3
Xtrem532 <xtrem532.tg@gmail.com>
2016-11-16 03:24:49 +0300

    2016-11-15's Java workout

---
+++ contributions/Xtrem532/Java/I/O/2016-11-15.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+Using buffered streams
+Reading file attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
