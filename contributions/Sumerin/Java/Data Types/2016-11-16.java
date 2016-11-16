56ff37437fd8b3e1253bd5c2ecff17badfd18679
Sumerin <sumekpl@gmail.com>
2016-11-16 10:15:49 +0300

    2016-11-16's Java workout

---
+++ contributions/Sumerin/Java/Data Types/2016-11-16.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Converting numbers to strings
+Converting a string to upper or lower case
+Finding a substring in a string
