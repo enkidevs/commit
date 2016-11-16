365445714e6cd5eb5388c1b349df74e7ef1e7f68
neugartf <fabian.neugart@gmail.com>
2016-11-16 09:55:21 +0300

    2016-11-16's Java workout

---
+++ contributions/neugartf/Java/Fundamentals/2016-11-16.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
