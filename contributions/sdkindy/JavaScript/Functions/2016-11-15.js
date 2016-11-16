35106ffffe078af1b122d1eb10397bca05cdc8cb
sdkindy <sonam.d.kindy@gmail.com>
2016-11-16 07:47:21 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/sdkindy/JavaScript/Functions/2016-11-15.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Functions can be declared after use
+Method chaining
+Easily generate a random `HEX` color
+Check if a document is done loading
