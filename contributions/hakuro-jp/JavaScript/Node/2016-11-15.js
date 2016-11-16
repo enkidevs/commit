de309fcbe2f474cd7964f8071ab2a902dc28e8b0
hakuro-jp <hakuro.jp@gmail.com>
2016-11-16 04:09:24 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/hakuro-jp/JavaScript/Node/2016-11-15.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+First-error callbacks in **Node**
+How to debug Node.js
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
