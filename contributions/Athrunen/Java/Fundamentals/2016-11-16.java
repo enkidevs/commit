859ecef96f710db086fb56fb4fb1708ebdb60410
Athrunen <athrunen@tendria.de>
2016-11-16 17:22:13 +0300

    2016-11-16's Java workout

---
+++ contributions/Athrunen/Java/Fundamentals/2016-11-16.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Using an interface as a parameter
