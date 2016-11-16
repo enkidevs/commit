d090164a7be9358f46b7ac7e82f3e184f6d40d53
frenjun <greemin@gmx.net>
2016-11-16 13:58:28 +0300

    2016-11-16's Java workout

---
+++ contributions/frenjun/Java/Data Types/2016-11-16.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Finding a substring in a string
+Do not attempt comparisons with NaN
