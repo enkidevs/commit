be6a85cfb4109362de5350f6b7069d3f000c3df1
duncathan <dunc2403@dunc2403.com>
2016-11-16 06:00:13 +0300

    2016-11-15's JavaScript workout

---
+++ contributions/duncathan/JavaScript/Node/2016-11-15.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Global Objects and Environment Variables in **Node**
+Wrapping errors in Node.js using _node-verror_
+Handle `JSON.parse` error in Node.js
+Middleware/pipeline design pattern
