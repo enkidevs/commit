3970d1554a7f46e707f5828004ad94a6a5af4d81
ChickenPaella <lhbv1421@hanmail.net>
2016-11-16 07:05:15 +0300

    2016-11-15's Java workout

---
+++ contributions/ChickenPaella/Java/Error Handling/2016-11-15.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Throwing an `exception`
+Careful Numeric Data Conversions
