790eda1b3c6bef8d1bef3fb9efbcabf9308f720b
elzapp <elzapp@gmail.com>
2016-11-16 11:28:45 +0300

    2016-11-16's Java workout

---
+++ contributions/elzapp/Java/Fundamentals/2016-11-16.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Writing generic methods
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+Avoid using `static` variables
