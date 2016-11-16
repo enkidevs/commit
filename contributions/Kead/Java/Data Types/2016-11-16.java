1cc8b40c9131ee633c8bb0b0c91b8268a3b77515
Kead <rom.stleger@gmail.com>
2016-11-16 09:44:27 +0300

    2016-11-16's Java workout

---
+++ contributions/Kead/Java/Data Types/2016-11-16.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Converting numbers to strings
+Finding a substring in a string
+Do not attempt comparisons with NaN
