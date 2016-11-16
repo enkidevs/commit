aed929076a669ac11296f7152e9b4c48a6d58b0a
alexandra229 <aleksandra.volkov229@gmail.com>
2016-11-16 03:28:14 +0300

    2016-11-15's Java workout

---
+++ contributions/alexandra229/Java/Fundamentals/2016-11-15.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Detect or prevent integer overflow
+Avoid using `static` variables
