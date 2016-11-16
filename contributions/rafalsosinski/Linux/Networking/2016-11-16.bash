066ef97d9e39d244226cdffdbf753bd8f770e618
rafalsosinski <rafal.sosinski+github@gmail.com>
2016-11-16 10:04:23 +0300

    2016-11-16's Linux workout

---
+++ contributions/rafalsosinski/Linux/Networking/2016-11-16.bash
@@ -0,0 +1,5 @@
+Find services running on your host
+Multiple IP Address with aliasing 
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
+Bandwidth monitoring tools 
