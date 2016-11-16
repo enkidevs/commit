b560d90b16282268b60f5d00cbfa49d2b00224fd
kbc8894 <kbc8894@gmail.com>
2016-11-16 14:21:45 +0300

    2016-11-15's Linux workout

---
+++ contributions/kbc8894/Linux/Terminals & Shells/2016-11-15.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Useful aliasing in bash
+Breaking out of a terminal when `ssh` locks
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
