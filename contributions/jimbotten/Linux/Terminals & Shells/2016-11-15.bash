8e77f7abb83ac68c1c8eea11f2dae32689baf5d2
jimbotten <jmontgomery@trivir.com>
2016-11-16 05:58:17 +0300

    2016-11-15's Linux workout

---
+++ contributions/jimbotten/Linux/Terminals & Shells/2016-11-15.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Find any Unix / Linux command
+Terminal incognito mode
