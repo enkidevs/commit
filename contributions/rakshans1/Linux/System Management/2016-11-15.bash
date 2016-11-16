e93f4600942acab4bedfc89aa312a39be1b40489
rakshans1 <shetty.raxx555@gmail.com>
2016-11-16 06:48:03 +0300

    2016-11-15's Linux workout

---
+++ contributions/rakshans1/Linux/System Management/2016-11-15.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Test disk speed with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
