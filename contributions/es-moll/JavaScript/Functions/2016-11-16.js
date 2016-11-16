29dab1887d0b17c18abbb24993970b85cb727473
es-moll <es-moll@inbox.ru>
2016-11-16 11:48:53 +0300

    2016-11-16's JavaScript workout

---
+++ contributions/es-moll/JavaScript/Functions/2016-11-16.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function declarations vs function expressions
+Function constructor vs. function expression
+Functions can be declared after use
+Move cursor at the end of text input
