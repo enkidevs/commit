dd31987e39b5273a1447fb79d9541667deed0575
Derjablik <attronach@gmail.com>
2016-11-16 10:42:58 +0300

    2016-11-16's Java workout

---
+++ contributions/Derjablik/Java/Data Types/2016-11-16.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Converting numbers to strings
+Finding a substring in a string
+Equals operation on different data types
