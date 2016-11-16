987a48697f0df80215436be62bee2e7aad20b02a
ibnumalik <ibnumalik@tbd.my>
2016-11-16 10:27:37 +0300

    2016-11-15's Git workout

---
+++ contributions/ibnumalik/Git/Essentials/2016-11-15.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+Adding a remote repository
+Using tags for version control
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
