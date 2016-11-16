bac84ecc6a394c0940e87e295e2911ffd8f5e119
ZikyHD <zikyhd@gmail.com>
2016-11-16 08:44:04 +0300

    2016-11-16's Linux workout

---
+++ contributions/ZikyHD/Linux/Terminals & Shells/2016-11-16.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Random password generator
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
