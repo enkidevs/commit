193123d221cbb4c73e34b667c39dad98608c19be
dbtrunks <pio.cud@gmail.com>
2016-11-16 10:29:17 +0300

    2016-11-16's JavaScript workout

---
+++ contributions/dbtrunks/JavaScript/Functions/2016-11-16.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Anonymous functions
+Easily generate a random `HEX` color
+Check if a document is done loading
