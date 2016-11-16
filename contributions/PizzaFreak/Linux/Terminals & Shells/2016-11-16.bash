0be692805d45cf7b1a74e0af4dd5918665423dd6
PizzaFreak <nico.greve89@gmail.com>
2016-11-16 09:40:58 +0300

    2016-11-16's Linux workout

---
+++ contributions/PizzaFreak/Linux/Terminals & Shells/2016-11-16.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Terminal based browser
+`else` statements using the `||` operator 
+The Basics of Environment Variables
