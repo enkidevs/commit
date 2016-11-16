5c5ff7b7d8bc925adea76cb7094b7980cf249512
cadusa7 <cadusaseven@gmail.com>
2016-11-16 04:33:06 +0300

    2016-11-15's Java workout

---
+++ contributions/cadusa7/Java/Error Handling/2016-11-15.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Careful Numeric Data Conversions
