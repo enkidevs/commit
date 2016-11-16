300457aa2455228ef0fd7cae004ebe21abd1f8b3
BigOim <qmerle@gmail.com>
2016-11-16 09:52:34 +0300

    2016-11-16's JavaScript workout

---
+++ contributions/BigOim/JavaScript/Functions/2016-11-16.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function expression
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Easily generate a random `HEX` color
