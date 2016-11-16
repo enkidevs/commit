4993f3ef1dc4aef680cfed25feba7911453b650d
mestizo.antonio <mestizo.antonio@gmail.com>
2016-11-16 17:34:38 +0300

    2016-11-16's Linux workout

---
+++ contributions/mestizo.antonio/Linux/Terminals & Shells/2016-11-16.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Terminal incognito mode
+Adding directories to your `$PATH`
