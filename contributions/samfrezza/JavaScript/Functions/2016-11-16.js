54bb2219bda77f2094e3dae013258bf9681ea13e
samfrezza <samfrezza@gmail.com>
2016-11-16 12:40:37 +0300

    2016-11-16's JavaScript workout

---
+++ contributions/samfrezza/JavaScript/Functions/2016-11-16.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Hoisting
+Function declarations vs function expressions
+Method chaining
+Changing a functions context with `fn.call(object)`
