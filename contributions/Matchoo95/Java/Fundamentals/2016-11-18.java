23e9c1bf0686c1a97b4ce38fd22576a311e69240
Matchoo95 <mattmatth123@googlemail.com>
2016-11-19 00:07:17 +0300

    2016-11-18's Java workout

---
+++ contributions/Matchoo95/Java/Fundamentals/2016-11-18.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The trade-off between short and descriptive names
+Using Inheritance to reduce code repetition
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
