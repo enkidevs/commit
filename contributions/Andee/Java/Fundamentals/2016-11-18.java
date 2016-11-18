428cd798b76f08ccf35a6b451adeef4aaaf03af5
Andee <andytruong94@gmail.com>
2016-11-18 19:08:56 +0300

    2016-11-18's Java workout

---
+++ contributions/Andee/Java/Fundamentals/2016-11-18.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Measuring time
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Using an interface as a parameter
