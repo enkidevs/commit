62bea34cb17ee0e25d631b7ff828b84e783f0e8d
3DEsprit <vilesyn@gmail.com>
2016-11-18 19:56:21 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/3DEsprit/JavaScript/Npm/2016-11-18.js
@@ -0,0 +1,5 @@
+Manipulate npm packages cache
+npm can handle multiple versions of a dependency
+Environmental variables and flags for `npm`
+`npm`  as an alternative to Gulp
+Tab completion in `npm`
