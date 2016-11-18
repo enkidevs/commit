a9cb38c3e3fdadf66f36b92a9617321783cf5fc3
donutdespair <carsonmhyde@gmail.com>
2016-11-18 21:17:34 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/donutdespair/JavaScript/Node/2016-11-18.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Http Server in **Node**
+Use conventional property names for error handling in Node.js
+Automatic compilation for Node with **Nodemon** 
+`try-catch` only for **sync** code
