28627a016fe7a8d6c0f45691f36ad498c04e558c
Daniel Keys <danieljkeys52@gmail.com>
2016-11-18 15:48:37 +0300

    2016-11-18's Linux workout

---
+++ contributions/Daniel Keys/Linux/Networking/2016-11-18.bash
@@ -0,0 +1,5 @@
+Get real network statistics with `slurm`
+Sending emails from terminal / scripts
+Bandwidth monitoring tools 
+Use `tcpdump` to listen to network interface traffic
+Make use of `netstat`
