e6bd2ab5ac7e60e14766cc691b83a0bc8364759d
lado <lgolijashvili@yahoo.com>
2016-11-18 18:46:29 +0300

    2016-11-17's Java workout

---
+++ contributions/lado/Java/Data Types/2016-11-17.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Finding a substring in a string
+Equals operation on different data types
+Numeric Conversion - Widening
