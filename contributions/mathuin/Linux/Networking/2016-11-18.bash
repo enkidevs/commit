ef2f871f0bf710ad9f20f02e292f3178de0e48c0
mathuin <mathuin@gmail.com>
2016-11-18 23:57:09 +0300

    2016-11-18's Linux workout

---
+++ contributions/mathuin/Linux/Networking/2016-11-18.bash
@@ -0,0 +1,5 @@
+Logging dropped firewall packets
+Useful ssh client optimizations 
+Bandwidth monitoring tools 
+Use `tcpdump` to listen to network interface traffic
+Staying connected remotely via SSH without an interactive shell
