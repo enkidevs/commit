269ad6b9c04de05280b20eca609992d64931e704
WPineapple <wannes.fransen@gmail.com>
2016-11-18 17:17:39 +0300

    2016-11-18's Linux workout

---
+++ contributions/WPineapple/Linux/File Management/2016-11-18.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Find the unknown process preventing deleting of files
