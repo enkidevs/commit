cd7e9bc93ff8b583c6507ae030f96b6a3b86ddf2
jlanga <jorge.langa.arranz@gmail.com>
2016-11-18 20:45:54 +0300

    2016-11-18's Linux workout

---
+++ contributions/jlanga/Linux/System Management/2016-11-18.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+List current logged on users with `w`
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Cancel a system shutdown
