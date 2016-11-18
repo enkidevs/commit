b8ca1c6fe657ce58f73ee8822b1fbc85ab47a5e8
pablolmnora <pablolmnora@gmail.com>
2016-11-19 00:59:34 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/pablolmnora/JavaScript/Node/2016-11-18.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Http Server in **Node**
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
+`exports` vs. `module.exports` in **Node**
