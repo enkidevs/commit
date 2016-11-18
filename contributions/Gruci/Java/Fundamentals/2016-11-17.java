3fd01f379d42ee17c7c883b06ac7490d91fd7804
Gruci <skeksksk@gmail.com>
2016-11-18 17:22:28 +0300

    2016-11-17's Java workout

---
+++ contributions/Gruci/Java/Fundamentals/2016-11-17.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Supply `toString()` in all classes
