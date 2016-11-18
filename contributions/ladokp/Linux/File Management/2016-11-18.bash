5bf013a375564a3bd4f4090411da95f7b385ab75
ladokp <patrick@ks.co.at>
2016-11-18 15:59:46 +0300

    2016-11-18's Linux workout

---
+++ contributions/ladokp/Linux/File Management/2016-11-18.bash
@@ -0,0 +1,5 @@
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Working with `.gz` files
+Delete commands aliases
+Easy way of sharing files
