3a3436621efa382baff75c4249f35a82f2c8c338
syruis <thomas.lopez.mail@gmail.com>
2016-11-18 23:33:39 +0300

    2016-11-18's Linux workout

---
+++ contributions/syruis/Linux/File Management/2016-11-18.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
