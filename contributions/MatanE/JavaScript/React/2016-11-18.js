86a415fba4b32f3938df2604e7f65407c4533751
MatanE <matan.elitzur@gmail.com>
2016-11-18 22:37:28 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/MatanE/JavaScript/React/2016-11-18.js
@@ -0,0 +1,5 @@
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+Expose Component Functions in **React**
+Dealing with `this.props.children`
