44c05c38666158ef40d2b471d4366e5a368923aa
armeris <rubensalinasgarcia@gmail.com>
2016-11-18 23:14:40 +0300

    2016-11-18's Java workout

---
+++ contributions/armeris/Java/Fundamentals/2016-11-18.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
