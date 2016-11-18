ce97b095b1ed810e250e0e36a07b971958b4bccc
yuanye <yuanyedmc@hotmail.com>
2016-11-19 02:50:50 +0300

    2016-11-18's Java workout

---
+++ contributions/yuanye/Java/Fundamentals/2016-11-18.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Generating random numbers
+The trade-off between short and descriptive names
+Always specify type parameter
+How range operations work
