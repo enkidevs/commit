1cbf249647a36ac2d23244a964eebd163fa9e730
PATWIS <patryk.wisniewski@outlook.com>
2016-11-18 19:36:17 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/PATWIS/JavaScript/Functions/2016-11-18.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Anonymous functions
+Changing a functions context with `fn.call(object)`
