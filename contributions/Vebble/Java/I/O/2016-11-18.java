dbf64c4359e2797040a559691574d071519a3f41
Vebble <serafin21@gmail.com>
2016-11-18 19:58:35 +0300

    2016-11-18's Java workout

---
+++ contributions/Vebble/Java/I/O/2016-11-18.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+Create system file paths using the `Path` class
+Checking for the existence of files and directories
+Listing a file system's root directories
+Deleting files and directories
