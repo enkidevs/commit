4e69c8ee04e6573368736bfc545100b96fc401b3
Xsandor <alex@karlborgselkontroll.se>
2016-11-18 23:25:04 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/Xsandor/JavaScript/Npm/2016-11-18.js
@@ -0,0 +1,5 @@
+`npm`  as an alternative to Gulp
+`npm` verifies `sha1` hashes of packages
+devDependencies in `npm`
+`config` object in `package.json`
+Lock down dependency versions by shrinkwrapping
