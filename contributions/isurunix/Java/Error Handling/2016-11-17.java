2f040c80e37cdd5e340c792eb32575d23b11e8fc
isurunix <isurunix@gmail.com>
2016-11-18 19:29:46 +0300

    2016-11-17's Java workout

---
+++ contributions/isurunix/Java/Error Handling/2016-11-17.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Most common reason behind **stack overflow** error
+Careful Numeric Data Conversions
