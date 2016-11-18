0eb7a7a6774cdcbf31e1cdc73d50aae67e65faa7
clsechi <carlossechi@hotmail.com>
2016-11-18 21:27:11 +0300

    2016-11-18's Java workout

---
+++ contributions/clsechi/Java/Fundamentals/2016-11-18.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Difference between JRE and JDK?
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Metadata: setting a file's owner
