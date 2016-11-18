13e70f3d4459ec81b149c2750b0d4e3e50658ecd
Sirius207 <oceanus11034@gmail.com>
2016-11-18 17:47:47 +0300

    2016-11-17's JavaScript workout

---
+++ contributions/Sirius207/JavaScript/React/2016-11-17.js
@@ -0,0 +1,6 @@
+Self-closing tags in **JSX**
+`if`-`else` statements in **JSX** and **React**
+Props in getInitialState Is an        
+Anti-Pattern
+Dealing with `this.props.children`
+Default values for props
