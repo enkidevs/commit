adad0625e034845c7967eceb1e2232fb0c171a6f
PtMSim <henik8@wp.pl>
2016-11-18 15:35:50 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/PtMSim/JavaScript/Functions/2016-11-18.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function constructor vs. function expression
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Easily generate a random `HEX` color
