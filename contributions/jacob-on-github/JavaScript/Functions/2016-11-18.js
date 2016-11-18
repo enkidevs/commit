1c400e188b3ce3617909d203364992efdbde10c5
jacob-on-github <jherrington.personal@gmail.com>
2016-11-18 16:34:22 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/jacob-on-github/JavaScript/Functions/2016-11-18.js
@@ -0,0 +1,5 @@
+Hoisting
+Function declarations vs function expressions
+Function constructor vs. function expression
+Easily generate a random `HEX` color
+Changing a functions context with `fn.call(object)`
