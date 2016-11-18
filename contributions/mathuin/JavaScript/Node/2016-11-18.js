8a1f434ff20f4d8ac3c5337558fc16fda40aa0e6
mathuin <mathuin@gmail.com>
2016-11-18 23:55:46 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/mathuin/JavaScript/Node/2016-11-18.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Custom Node REPL Server
+Wrapping errors in Node.js using _node-verror_
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
