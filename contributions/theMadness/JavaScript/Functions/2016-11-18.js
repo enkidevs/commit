e117e4719b5912640c092077cb7ee88245eadf7b
theMadness <daniele@l33t.it>
2016-11-18 20:55:23 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/theMadness/JavaScript/Functions/2016-11-18.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Functions can be declared after use
+`.bind()` 
+Move cursor at the end of text input
