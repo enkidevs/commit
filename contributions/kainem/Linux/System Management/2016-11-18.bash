721fed7da6df82c18f6aa359604df1523f063b85
kainem <kaine@kainem.com>
2016-11-19 01:47:38 +0300

    2016-11-18's Linux workout

---
+++ contributions/kainem/Linux/System Management/2016-11-18.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+List current logged on users with `w`
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Using `dd` To Create Disk Images
