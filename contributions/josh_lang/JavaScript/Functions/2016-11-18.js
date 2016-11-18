d563924b757d24b941cc1f2a1a8392a9b3f7b2cf
josh_lang <joshuaclang1@gmail.com>
2016-11-18 15:39:28 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/josh_lang/JavaScript/Functions/2016-11-18.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Function call method
