10bd0e503393002799360acb0f82dfcd4bf89a36
inkwaterman <inkwaterman@gmail.com>
2016-11-19 00:30:57 +0300

    2016-11-18's Linux workout

---
+++ contributions/inkwaterman/Linux/File Management/2016-11-18.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
