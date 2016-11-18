68c1ae0714d2e0ffaea5700a987f6cb6b22c29af
Aearsis <aearsis@eideo.cz>
2016-11-18 22:36:58 +0300

    2016-11-18's Java workout

---
+++ contributions/Aearsis/Java/I/O/2016-11-18.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to create directories in Java
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
