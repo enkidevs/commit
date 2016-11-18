d058c5ee26838b843a129a3414b293226911d8de
beagleknight <david.morcillo@gmail.com>
2016-11-19 00:27:58 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/beagleknight/JavaScript/Functions/2016-11-18.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Difference between **.call** and **.apply** methods
+Easily generate a random `HEX` color
+Social sharing without widgets
+Check if a document is done loading
