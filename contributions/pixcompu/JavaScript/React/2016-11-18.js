048913adb1423e699b1a6b62795262be644147eb
pixcompu <pixcompu@outlook.com>
2016-11-18 17:36:45 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/pixcompu/JavaScript/React/2016-11-18.js
@@ -0,0 +1,5 @@
+Self-closing tags in **JSX**
+`null` value for *Controlled Components* in **React**
+Using `false` in **JSX**
+Expose Component Functions in **React**
+Dealing with `this.props.children`
