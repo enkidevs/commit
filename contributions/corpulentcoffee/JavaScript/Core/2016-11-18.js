a4e5cd9b5785304f029945670af3b54955291b66
corpulentcoffee <dave@corpulentcoffee.com>
2016-11-18 18:22:52 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/corpulentcoffee/JavaScript/Core/2016-11-18.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+Double and single quotes
+Hoisting applies only to variable declarations, not initializations
+Avoid using `with`
+Detect an error type
