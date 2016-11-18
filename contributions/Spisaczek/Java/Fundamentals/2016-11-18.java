f6fde35e6e9eab1859e0ae1695b21da74a0ff3a3
Spisaczek <ksmalutki@gmail.com>
2016-11-18 22:30:14 +0300

    2016-11-18's Java workout

---
+++ contributions/Spisaczek/Java/Fundamentals/2016-11-18.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
