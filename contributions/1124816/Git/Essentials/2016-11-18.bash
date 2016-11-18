58e43863cf9bcd2267d204e24ff87d41d27c15e2
1124816 <e.e.basler@gmail.com>
2016-11-18 17:39:03 +0300

    2016-11-18's Git workout

---
+++ contributions/1124816/Git/Essentials/2016-11-18.bash
@@ -0,0 +1,5 @@
+How to unmodify a modified file
+Using tags for version control
+Pushing tags to a server
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
