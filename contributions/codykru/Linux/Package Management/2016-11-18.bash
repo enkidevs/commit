8c3b2e2c03d05889e3141b724932a5ba6e3ef03f
codykru <codykru@gmail.com>
2016-11-19 02:39:08 +0300

    2016-11-18's Linux workout

---
+++ contributions/codykru/Linux/Package Management/2016-11-18.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+Backporting on Debian
+
+Get `apt` to use a mirror / faster mirror
+Keeping repos updated with `cron`
