ce29bb1ae090e0c331636ed7fe578e0e095b1242
mikercampbell <michaelrcampbell0916@gmail.com>
2016-11-18 19:14:41 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/mikercampbell/JavaScript/Functions/2016-11-18.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function expression
+Functions can be declared after use
+IIFE pattern
+Move cursor at the end of text input
