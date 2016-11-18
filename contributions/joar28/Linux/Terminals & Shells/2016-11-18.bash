ac0e80b368c222d04eb242a819ee3f0dc4df1557
joar28 <joar28@gmail.com>
2016-11-18 23:57:42 +0300

    2016-11-18's Linux workout

---
+++ contributions/joar28/Linux/Terminals & Shells/2016-11-18.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Random password generator
+Terminal incognito mode
