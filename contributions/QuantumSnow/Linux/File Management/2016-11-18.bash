53fcf69be958954f5c6a56a1945f064b103b8f67
QuantumSnow <jakobtheai@gmail.com>
2016-11-19 00:14:01 +0300

    2016-11-18's Linux workout

---
+++ contributions/QuantumSnow/Linux/File Management/2016-11-18.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Working with `.gz` files
+Easy way of sharing files
