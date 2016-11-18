3df8cfcaba49a281fbd923643d5d243d92a00d9a
Trevato <trevordobbertin@gmail.com>
2016-11-18 16:23:02 +0300

    2016-11-18's Java workout

---
+++ contributions/Trevato/Java/Data Types/2016-11-18.java
@@ -0,0 +1,5 @@
+Two ways to use an `Iterator`
+Multidimensional array declaration
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
+Retrieve the component type of an array
