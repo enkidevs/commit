1b173f92cbb7ae49ef4468f86c16ea0318e09a82
Netbattler11 <netbattler11@gmail.com>
2016-11-18 21:52:45 +0300

    2016-11-18's Java workout

---
+++ contributions/Netbattler11/Java/Fundamentals/2016-11-18.java
@@ -0,0 +1,5 @@
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
