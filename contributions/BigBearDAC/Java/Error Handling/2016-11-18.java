da45d614a0f7a4804b92e49e64bc920eedcae5b8
BigBearDAC <flidnibble@gmail.com>
2016-11-19 00:45:10 +0300

    2016-11-18's Java workout

---
+++ contributions/BigBearDAC/Java/Error Handling/2016-11-18.java
@@ -0,0 +1,5 @@
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Throwing an `exception`
