fcc7c6a581f37c8690ef2f04307ca568b65cfc63
vandoenterprise <evandro.barbosa@yahoo.com.br>
2016-11-18 20:29:18 +0300

    2016-11-18's Java workout

---
+++ contributions/vandoenterprise/Java/I/O/2016-11-18.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Deleting files and directories
+The `Console` class
