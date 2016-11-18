6914ae5f4946f53a7c7309acdc0233c27172594b
ZbeulMaker <ziri.belabbas@gmail.com>
2016-11-18 20:48:54 +0300

    2016-11-18's Linux workout

---
+++ contributions/ZbeulMaker/Linux/File Management/2016-11-18.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
