64c66faea74d4f9cd64c0cf2195a95602ba45e9d
joelandry <joelandry80@gmail.com>
2016-11-18 16:26:23 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/joelandry/JavaScript/Npm/2016-11-18.js
@@ -0,0 +1,5 @@
+Deprecate npm packages
+Passing through command line arguments in npm scripts
+List binaries for scripting in npm
+`npm`  as an alternative to Gulp
+`npm` verifies `sha1` hashes of packages
