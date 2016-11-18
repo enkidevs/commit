3cfd1bed8a5a88696086ae8359817b0c464e4e73
sogtwinster <sogtwinster@gmail.com>
2016-11-18 18:13:29 +0300

    2016-11-18's Java workout

---
+++ contributions/sogtwinster/Java/Fundamentals/2016-11-18.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
