037470ae3e6a5620bda1bd403ae9f1ff18d3b931
juanjomarcal <juanjo.marcal@gmail.com>
2016-11-18 18:19:27 +0300

    2016-11-18's Linux workout

---
+++ contributions/juanjomarcal/Linux/File Management/2016-11-18.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
+Easy way of sharing files
