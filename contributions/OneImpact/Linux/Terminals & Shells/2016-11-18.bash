cb1bd42758ff9b29d0043e340a67d32f843d3cbd
OneImpact <david_winnicki@live.com>
2016-11-18 17:18:59 +0300

    2016-11-18's Linux workout

---
+++ contributions/OneImpact/Linux/Terminals & Shells/2016-11-18.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Random password generator
+Terminal incognito mode
+Adding directories to your `$PATH`
+Retrieve previous arguments
