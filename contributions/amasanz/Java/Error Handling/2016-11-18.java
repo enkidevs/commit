f8002cee587e2d5e1a8914585a058abd34ac5e06
amasanz <alx.masz@gmail.com>
2016-11-19 02:59:35 +0300

    2016-11-18's Java workout

---
+++ contributions/amasanz/Java/Error Handling/2016-11-18.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Most common reason behind **stack overflow** error
+Careful Numeric Data Conversions
