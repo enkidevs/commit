d5b12ac0c867e30b07a0d27db1e053b4015b129a
Raptura <smithark97@gmail.com>
2016-11-18 23:51:44 +0300

    2016-11-18's Java workout

---
+++ contributions/Raptura/Java/Fundamentals/2016-11-18.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
