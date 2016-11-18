3280b4edc560afdd07eabde5de10a212b4587f45
RyanT123 <tbusinessr@gmail.com>
2016-11-18 20:22:54 +0300

    2016-11-18's Linux workout

---
+++ contributions/RyanT123/Linux/File Management/2016-11-18.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
