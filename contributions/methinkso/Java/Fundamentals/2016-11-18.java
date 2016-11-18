35915286ba7ca4223aa481a016065074be937d14
methinkso <methinkso@gmail.com>
2016-11-18 21:14:38 +0300

    2016-11-18's Java workout

---
+++ contributions/methinkso/Java/Fundamentals/2016-11-18.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Using an interface as a parameter
