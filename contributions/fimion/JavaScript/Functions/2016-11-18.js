7e70e7da753aca971d995048d8e4aefe860ba752
fimion <fimion@gmail.com>
2016-11-18 15:32:06 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/fimion/JavaScript/Functions/2016-11-18.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+Anonymous functions
+Easily generate a random `HEX` color
+Check if a document is done loading
