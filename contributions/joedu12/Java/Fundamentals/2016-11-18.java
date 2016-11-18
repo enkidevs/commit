39bcc480544d215a4f9bfee73d690a2518711553
joedu12 <joevin12@hotmail.fr>
2016-11-19 00:59:57 +0300

    2016-11-18's Java workout

---
+++ contributions/joedu12/Java/Fundamentals/2016-11-18.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The trade-off between short and descriptive names
+Java naming conventions
+Using Inheritance to reduce code repetition
+Limit Accessibility of `Fields`
