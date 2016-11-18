e3623b278384988cfb056dbf512ec45597e46908
kirpalmakanga <makanga.kirpal@gmail.com>
2016-11-19 00:32:51 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/kirpalmakanga/JavaScript/Functions/2016-11-18.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Hoisting
+Function declarations vs function expressions
+Easily generate a random `HEX` color
+Social sharing without widgets
