faa57ea7b92d20563534aaf5a0306788bd553e73
aysh <ajay.shibu.ben@gmail.com>
2016-11-19 05:30:47 +0300

    2016-11-18's Linux workout

---
+++ contributions/aysh/Linux/File Management/2016-11-18.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
