f0efc847400954a0c305f44a5d8a2b1313776ffe
rico345100 <rico345100@gmail.com>
2016-11-19 05:32:24 +0300

    2016-11-18's Linux workout

---
+++ contributions/rico345100/Linux/Terminals & Shells/2016-11-18.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Terminal based browser
+`else` statements using the `||` operator 
+Retrieve previous arguments
