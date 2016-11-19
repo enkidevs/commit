5c794e6895b6d5acefa12660bf54c9efb82f5a95
coalaroot <jakubkrolak94@gmail.com>
2016-11-19 20:33:15 +0300

    2016-11-19's Java workout

---
+++ contributions/coalaroot/Java/Data Types/2016-11-19.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Converting a string to upper or lower case
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
