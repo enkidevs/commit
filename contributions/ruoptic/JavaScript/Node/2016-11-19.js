5b9128eeee25a683f0298bd8184d19e5607e0b9f
ruoptic <stroke.the.cat.again@gmail.com>
2016-11-20 01:11:11 +0300

    2016-11-19's JavaScript workout

---
+++ contributions/ruoptic/JavaScript/Node/2016-11-19.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+Streams in **Node**
+**Node** `os` module
+Automatic compilation for Node with **Nodemon** 
