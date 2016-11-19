76c863f3ea347dfe4040f2f26337b9c068ff9868
wilderbg <wilder.galvao@gmail.com>
2016-11-19 15:21:10 +0300

    2016-11-19's Linux workout

---
+++ contributions/wilderbg/Linux/File Management/2016-11-19.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
+Easy way of sharing files
