ae9517ab33087fcef501284d9cde8869d8e8b374
majiabot <iffy.dude@gmail.com>
2016-11-20 00:41:01 +0300

    2016-11-19's Java workout

---
+++ contributions/majiabot/Java/Error Handling/2016-11-19.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Converting stack trace to a string
+Prevent `exceptions` while logging Data
+Throwing an `exception`
+Checking a double for `NaN` value
