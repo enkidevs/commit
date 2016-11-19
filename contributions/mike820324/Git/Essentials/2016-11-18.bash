c4ec1953eb5f58d45d20e3745587e37826f79146
mike820324 <mike820324@gmail.com>
2016-11-19 04:33:52 +0300

    2016-11-18's Git workout

---
+++ contributions/mike820324/Git/Essentials/2016-11-18.bash
@@ -0,0 +1,5 @@
+Adding a remote repository
+You can use git offline!
+How to set your username and email
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
