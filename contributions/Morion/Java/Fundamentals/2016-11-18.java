ad211ca6f2c07cab0c1230727984eef042cdc9ae
Morion <morion.self@gmail.com>
2016-11-19 16:06:15 +0300

    2016-11-18's Java workout

---
+++ contributions/Morion/Java/Fundamentals/2016-11-18.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+`Autoboxing` and `Unboxing`
+Supply `toString()` in all classes
