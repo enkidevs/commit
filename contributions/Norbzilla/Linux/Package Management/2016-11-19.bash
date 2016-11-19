0a1f514859f87f9c0553b47b3b10b72293fbb797
Norbzilla <agutsch22@gmail.com>
2016-11-19 23:30:31 +0300

    2016-11-19's Linux workout

---
+++ contributions/Norbzilla/Linux/Package Management/2016-11-19.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
