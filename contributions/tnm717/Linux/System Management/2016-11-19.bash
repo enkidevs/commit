12e47f87126d78e45d3cee83e0009ec53ed07b13
tnm717 <thomas.masterson@gmail.com>
2016-11-19 21:42:57 +0300

    2016-11-19's Linux workout

---
+++ contributions/tnm717/Linux/System Management/2016-11-19.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Basic service management with `systemd`
