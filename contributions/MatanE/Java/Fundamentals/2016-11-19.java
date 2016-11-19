ccc3f557689934c15325023c0587d063f4bb784a
MatanE <matan.elitzur@gmail.com>
2016-11-19 18:17:48 +0300

    2016-11-19's Java workout

---
+++ contributions/MatanE/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
