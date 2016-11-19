4e8e46458ab01adc53f25dbed63911eb429a0e6b
mwhobrey <mwhobrey@idealss.com>
2016-11-19 07:20:07 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/mwhobrey/JavaScript/React/2016-11-18.js
@@ -0,0 +1,5 @@
+Functional Stateless Components in React
+`if`-`else` statements in **JSX** and **React**
+Shallow compare in **React**
+Immutability helpers in **React**
+Validate for required props
