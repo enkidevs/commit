99d9e9a2fab86f13cc099ac1c5f0bc37783bd5d4
sharafian <ben@sharafian.me>
2016-11-19 05:06:53 +0300

    2016-11-18's Linux workout

---
+++ contributions/sharafian/Linux/File Management/2016-11-18.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
