12f12e9b3c69b486fe1f133e6ec9f13b042c849d
lxsymington <lukexaviersymington@gmail.com>
2016-11-19 21:32:04 +0300

    2016-11-19's JavaScript workout

---
+++ contributions/lxsymington/JavaScript/Core/2016-11-19.js
@@ -0,0 +1,5 @@
+Undefined / Empty / Null Checks
+Setting the length of an array 
+Use `splice()` to remove an item from an array
+Truthiness
+Hoisting applies only to variable declarations, not initializations
