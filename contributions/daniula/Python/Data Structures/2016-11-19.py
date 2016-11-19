c3d776183963fad533426e9fa0d20442e3bfc218
daniula <daniel.mendalka@gmail.com>
2016-11-19 05:20:38 +0300

    2016-11-19's Python workout

---
+++ contributions/daniula/Python/Data Structures/2016-11-19.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`bytes` type
+Keep things in order with `OrderedDict`
+`weakref` proxies
+Binary record packing with `bytearray`
