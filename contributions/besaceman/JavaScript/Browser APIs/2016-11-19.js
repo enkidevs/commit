71ba75e1705df56ddd810aaac73afa71044e9bc9
besaceman <romain.laodicina@gmail.com>
2016-11-19 12:26:36 +0300

    2016-11-19's JavaScript workout

---
+++ contributions/besaceman/JavaScript/Browser APIs/2016-11-19.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Use hosted scripts to increase performance
+Modify a website's URL
+Warn user if **Back** button is pressed
+User Timing API
