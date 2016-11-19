eead8bfea93b2f0ef5389c0d0a1626e11076cc9f
prathak07 <prathak07@gmail.com>
2016-11-19 07:25:43 +0300

    2016-11-18's Java workout

---
+++ contributions/prathak07/Java/Fundamentals/2016-11-18.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Generating random numbers
+Catch multiple exceptions in a single `catch` block
+Constant notation
+There is no such thing as *pass-by-reference* in Java
