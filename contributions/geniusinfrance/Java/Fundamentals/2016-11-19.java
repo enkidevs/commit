9161109ecc760322dc7470312b4642d4603e2495
geniusinfrance <ryan.bergerson@gmail.com>
2016-11-19 10:49:13 +0300

    2016-11-19's Java workout

---
+++ contributions/geniusinfrance/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Java naming conventions
+Using Inheritance to reduce code repetition
+Always specify type parameter
+`Autoboxing` and `Unboxing`
