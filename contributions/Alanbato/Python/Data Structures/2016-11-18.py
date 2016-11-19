660149b5fbd0c8e04d3dc5e52aded74cbc5d089d
Alanbato <alanvelasco.a@gmail.com>
2016-11-19 05:12:01 +0300

    2016-11-18's Python workout

---
+++ contributions/Alanbato/Python/Data Structures/2016-11-18.py
@@ -0,0 +1,5 @@
+Best way to implement a simple `queue` 
+Get more with `collections`!
+Operations with `bytes` and `bytearray`
+Implementing **weak** references
+Binary record packing with `bytearray`
