9b448bf36081ed34cad002290f9cdc4000d7c721
SergeantBiggs <guter.alias@skynet.be>
2016-11-20 00:58:08 +0300

    2016-11-19's Linux workout

---
+++ contributions/SergeantBiggs/Linux/System Management/2016-11-19.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
