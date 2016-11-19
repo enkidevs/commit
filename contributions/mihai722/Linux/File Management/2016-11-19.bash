0c067c6ddc04e5658c5c26035cfe88e816114a6f
mihai722 <mihai7221@gmail.com>
2016-11-19 08:42:18 +0300

    2016-11-19's Linux workout

---
+++ contributions/mihai722/Linux/File Management/2016-11-19.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Delete commands aliases
