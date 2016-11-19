4f596a0d0bc6538dbe34e367fa6062ffab931156
SnikSnak <kozboko@gmail.com>
2016-11-19 12:51:39 +0300

    2016-11-19's Java workout

---
+++ contributions/SnikSnak/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Catch multiple exceptions in a single `catch` block
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
