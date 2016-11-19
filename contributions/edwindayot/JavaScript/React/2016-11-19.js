104a6286e12e9bd2aace0a6d86fccd440e0a724a
edwindayot <edwin.dayot@sfr.fr>
2016-11-19 17:18:11 +0300

    2016-11-19's JavaScript workout

---
+++ contributions/edwindayot/JavaScript/React/2016-11-19.js
@@ -0,0 +1,5 @@
+**React** in-line styles
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+Using `false` in **JSX**
+**React** `this.props.children`
