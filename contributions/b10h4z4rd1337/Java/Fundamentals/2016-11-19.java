cebbc8fa7d9b2dbc7f500022019a0a63984286e4
b10h4z4rd1337 <mtahas@icloud.com>
2016-11-19 21:41:10 +0300

    2016-11-19's Java workout

---
+++ contributions/b10h4z4rd1337/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
