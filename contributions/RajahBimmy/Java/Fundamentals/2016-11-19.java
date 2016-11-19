378b19d861c646582de12050bbd50f3c92dafd58
RajahBimmy <dmcdaid95@gmail.com>
2016-11-20 02:58:32 +0300

    2016-11-19's Java workout

---
+++ contributions/RajahBimmy/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
