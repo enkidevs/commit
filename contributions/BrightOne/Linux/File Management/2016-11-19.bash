6df8208bdde4bb3a9dfacc6eae06588ae4a01a91
BrightOne <filalex77@gmail.com>
2016-11-19 09:36:48 +0300

    2016-11-19's Linux workout

---
+++ contributions/BrightOne/Linux/File Management/2016-11-19.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
