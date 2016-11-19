3dfa4ce4bb00fe6d16650b40a0cfcdc50d1837f6
renjithgr <grenjithr@gmail.com>
2016-11-19 07:31:41 +0300

    2016-11-18's Java workout

---
+++ contributions/renjithgr/Java/Fundamentals/2016-11-18.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Catch multiple exceptions in a single `catch` block
+Using `Printstream.format` to format a string before printing
+Using `synchronized` statements
+Avoid using `static` variables
