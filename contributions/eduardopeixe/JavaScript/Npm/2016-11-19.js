316baea1b1d5a5c2626765f7959a18be6ba02786
eduardopeixe <walthfish@yahoo.com>
2016-11-19 16:18:56 +0300

    2016-11-19's JavaScript workout

---
+++ contributions/eduardopeixe/JavaScript/Npm/2016-11-19.js
@@ -0,0 +1,5 @@
+Niceties for your npm package
+npm projects dependencies
+Passing through command line arguments in npm scripts
+`npm` verifies `sha1` hashes of packages
+`optionalDependencies` in npm
