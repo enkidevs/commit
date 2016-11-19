72b60dba0c15664d89e890d436a09a33b102425d
tswestendorp <contact@tswestendorp.nl>
2016-11-19 10:22:31 +0300

    2016-11-19's Linux workout

---
+++ contributions/tswestendorp/Linux/Terminals & Shells/2016-11-19.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Useful aliasing in bash
+Random password generator
+Adding directories to your `$PATH`
+Conditional command execution
+
+(`&&` operator)
