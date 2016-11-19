b57d016b7f5d24c33d342ddff186fb3e9849f17b
dabizk <dabizk@yahoo.com>
2016-11-20 00:09:16 +0300

    2016-11-19's Java workout

---
+++ contributions/dabizk/Java/I/O/2016-11-19.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+List contents of a directory with `newDirectoryStream`
