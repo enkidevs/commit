f4e1fa629a50d85a6bae20a9ca4c18aae02ade79
gustavli <gustav.lillesater@gmail.com>
2016-11-19 05:32:06 +0300

    2016-11-19's Java workout

---
+++ contributions/gustavli/Java/Error Handling/2016-11-19.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Most common reason behind **stack overflow** error
+Throwing an `exception`
