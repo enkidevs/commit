c81688a1ed09f96d8ced6f0a9c91eb46c5c0e042
krethh <krecik.kret@gmail.com>
2016-11-20 02:56:30 +0300

    2016-11-19's Java workout

---
+++ contributions/krethh/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Use meaningful names
+Using Inheritance to reduce code repetition
+`Autoboxing` and `Unboxing`
