9d6fa6700a8dd603cbf48a69e2fdc429af4c5dc7
bouzlibop <bouzlibop@gmail.com>
2016-11-19 23:24:18 +0300

    2016-11-19's JavaScript workout

---
+++ contributions/bouzlibop/JavaScript/Core/2016-11-19.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+The difference between null, undefined and NaN
+Scope and strict mode
+Detect an error type
+Inaccuracy of binary floating-point format
