e2c89207a1335294f369cba5f9b4bbdb676f45d7
Checo <checo1983@gmail.com>
2016-11-20 02:28:41 +0300

    2016-11-19's Java workout

---
+++ contributions/Checo/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Catch multiple exceptions in a single `catch` block
+Using `Printstream.format` to format a string before printing
+Detect or prevent integer overflow
+Using an interface as a parameter
