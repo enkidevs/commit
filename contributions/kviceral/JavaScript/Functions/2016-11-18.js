520ded3dfea2d5278bfd471755c0d32cdea17a39
kviceral <krisviceral@gmail.com>
2016-11-19 06:12:33 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/kviceral/JavaScript/Functions/2016-11-18.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Method chaining
+Function call method
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
