e6a3f9826703820ab7b6b80cd3418e2b324bb3db
stando <stano.kudlac@gmail.com>
2016-11-19 10:57:22 +0300

    2016-11-19's Linux workout

---
+++ contributions/stando/Linux/Networking/2016-11-19.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Multiple IP Address with aliasing 
+Limit the `wget` download rate
+Getting the connection speed from the terminal
+Staying connected remotely via SSH without an interactive shell
