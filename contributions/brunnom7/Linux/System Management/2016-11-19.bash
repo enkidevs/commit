017e2b62aabc674439b564b08c54128bf13f1337
brunnom7 <brunnomfoto@gmail.com>
2016-11-19 16:09:23 +0300

    2016-11-19's Linux workout

---
+++ contributions/brunnom7/Linux/System Management/2016-11-19.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
