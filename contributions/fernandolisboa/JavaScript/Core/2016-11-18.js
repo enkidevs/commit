7342007877d738ba498fc428763f0d1739849d49
fernandolisboa <fernandoigorlisboa@gmail.com>
2016-11-20 00:55:21 +0300

    2016-11-18's JavaScript workout

---
+++ contributions/fernandolisboa/JavaScript/Core/2016-11-18.js
@@ -0,0 +1,5 @@
+Check if an argument is a number
+Get a random item from an Array
+Data type comparison in `switch` statements
+Filtering items out of an array
+Inaccuracy of binary floating-point format
