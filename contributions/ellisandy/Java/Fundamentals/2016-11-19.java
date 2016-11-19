aba29411dad90155087d9b81b512578c43accc01
ellisandy <andrew@andrewryanellis.com>
2016-11-19 20:25:06 +0300

    2016-11-19's Java workout

---
+++ contributions/ellisandy/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Using bounded type parameters in generic methods
+Calling one constructor from another
+Always specify type parameter
+`Autoboxing` and `Unboxing`
