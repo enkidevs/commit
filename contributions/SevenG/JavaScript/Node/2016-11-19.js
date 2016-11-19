78c0ddc2fd9d604c2e6088667fcbaf359092b0a7
SevenG <enki@seveng.net>
2016-11-19 17:55:20 +0300

    2016-11-19's JavaScript workout

---
+++ contributions/SevenG/JavaScript/Node/2016-11-19.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+Streams in **Node**
+Wrapping errors in Node.js using _node-verror_
+Manage child processes in **Node**
