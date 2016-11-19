725ff5edc827d4023c14cf0841f8e75132ef2c65
ejde <ejdevangelista@gmail.com>
2016-11-20 01:50:11 +0300

    2016-11-19's Java workout

---
+++ contributions/ejde/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
