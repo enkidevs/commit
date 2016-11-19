169679637553f7102606f739130cbc6817b048b6
Sagheeton <sagheeton@gmail.com>
2016-11-19 14:02:19 +0300

    2016-11-19's Java workout

---
+++ contributions/Sagheeton/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+The distinction between overloading and overriding methods
+Generating random numbers
+Using Inheritance to reduce code repetition
+How range operations work
