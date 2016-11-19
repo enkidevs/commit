3535ee34c5b030584d55f8879c560b41bd17a352
J0hn- <jonathan.platteau@yahoo.fr>
2016-11-19 18:13:01 +0300

    2016-11-19's Linux workout

---
+++ contributions/J0hn-/Linux/File Management/2016-11-19.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
