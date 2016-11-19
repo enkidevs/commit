b88a0a913e838e04eaf4c1fd1b134ea02b05e08a
EnzDev <enzo@mallard.ml>
2016-11-19 13:11:40 +0300

    2016-11-19's Python workout

---
+++ contributions/EnzDev/Python/Data Structures/2016-11-19.py
@@ -0,0 +1,5 @@
+Following PEP 8 styling guideline.
+A simple way to select a random item from a `list/tuple` data stucture
+Get the most of `float`s
+Keep things in order with `OrderedDict`
+Binary record packing with `bytearray`
