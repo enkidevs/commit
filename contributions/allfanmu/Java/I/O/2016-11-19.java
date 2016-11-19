1d41adeb8afb4d401bab21c0158b34db9fb70a7b
allfanmu <allfanmu@wp.pl>
2016-11-19 11:14:44 +0300

    2016-11-19's Java workout

---
+++ contributions/allfanmu/Java/I/O/2016-11-19.java
@@ -0,0 +1,5 @@
+Using buffered streams
+Reading file attributes
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
