23a5ede54189c3ea8e78bdbb5b67d58b5f2cfee4
mdb694 <mdb69@hotmail.it>
2016-11-19 20:44:25 +0300

    2016-11-19's Java workout

---
+++ contributions/mdb694/Java/Error Handling/2016-11-19.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Converting stack trace to a string
+Prevent `exceptions` while logging Data
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
