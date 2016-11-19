93275d2a9d95e3ab75ca07c2a8d2ad7dae3cebc2
Crisehv <crisehv14@gmail.com>
2016-11-19 05:20:17 +0300

    2016-11-18's Java workout

---
+++ contributions/Crisehv/Java/Data Types/2016-11-18.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Equals operation on different data types
+Do not attempt comparisons with NaN
+Numeric Conversion - Widening
