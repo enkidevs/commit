8d973b53ee206f1c49bbb8edae191e1dc66fad70
elderbeserra <eldermeteo@gmail.com>
2016-11-19 16:11:37 +0300

    2016-11-19's Python workout

---
+++ contributions/elderbeserra/Python/Data Structures/2016-11-19.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Get the most of `float`s
+`bytearray` objects
+Operations with `bytes` and `bytearray`
+Binary record packing with `bytearray`
