8c5b471c44eacd4d177f106cf855cde6cb968510
Jilano <mobile.axel@gmail.com>
2016-11-19 11:04:44 +0300

    2016-11-19's Java workout

---
+++ contributions/Jilano/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+The distinction between overloading and overriding methods
+Generating random numbers
+Common mistake on switch statements
+Always specify type parameter
