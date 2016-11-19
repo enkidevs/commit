12f43656ca4bf8a0e56b82f84617dcf4946c13f8
BrightOne <filalex77@gmail.com>
2016-11-19 09:11:54 +0300

    2016-11-19's Java workout

---
+++ contributions/BrightOne/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Java naming conventions
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
