84c0e17d12f0e2182408f9bd480c6d96640c1844
tomaszkrzyzek <tomasz.krzyzek@gmail.com>
2016-11-19 21:07:44 +0300

    2016-11-19's Java workout

---
+++ contributions/tomaszkrzyzek/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+The trade-off between short and descriptive names
+Writing generic methods
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
