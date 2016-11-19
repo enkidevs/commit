98f51d9ecdb8bea2b51383cf066fea3f4e237201
laurentvareliette <laurent.vareliette@gmail.com>
2016-11-20 00:55:25 +0300

    2016-11-19's Linux workout

---
+++ contributions/laurentvareliette/Linux/Networking/2016-11-19.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+Get real network statistics with `slurm`
+List your IPv6 configuration
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
