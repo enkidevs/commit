1a3d197513e500c857beabe37074a6e0f3f5e01d
autoscatto <4utoscatto@gmail.com>
2016-11-19 12:45:37 +0300

    2016-11-19's Linux workout

---
+++ contributions/autoscatto/Linux/File Management/2016-11-19.bash
@@ -0,0 +1,7 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
