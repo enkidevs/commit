807b3d7e4468607d98525b53ef062f3f62868a16
johnwr <johnwr_enki@wraalsen.no>
2016-11-19 18:07:18 +0300

    2016-11-19's Java workout

---
+++ contributions/johnwr/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Ternary operator
+Using `Printstream.format` to format a string before printing
+Do not perform bitwise and arithmetic operations on the same data
+`Autoboxing` and `Unboxing`
