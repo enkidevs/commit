c854d32f4c0614e1f4c4f70e93585225184e73a4
ChristopheSoliiiid <marin.christophe@outlook.fr>
2016-11-19 23:16:10 +0300

    2016-11-19's JavaScript workout

---
+++ contributions/ChristopheSoliiiid/JavaScript/Core/2016-11-19.js
@@ -0,0 +1,5 @@
+Setting the length of an array 
+Use `splice()` to remove an item from an array
+Filtering items out of an array
+The difference between null, undefined and NaN
+`console.*` in JavaScript
