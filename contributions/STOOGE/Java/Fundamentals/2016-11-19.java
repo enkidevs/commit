fe2e3a4339020b3e7b49a614630d43fd88cbf6a3
STOOGE <kirov.jordan@gmail.com>
2016-11-19 23:23:18 +0300

    2016-11-19's Java workout

---
+++ contributions/STOOGE/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Uses of the `final` keyword
+Catch multiple exceptions in a single `catch` block
+Using Inheritance to reduce code repetition
+Using `Printstream.format` to format a string before printing
