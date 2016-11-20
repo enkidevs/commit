e3a3929ed94b718dc058bc8d069c46bde1b732a5
Hellonearthis <hellonearthisman@gmail.com>
2016-11-20 18:11:10 +0300

    2016-11-20's JavaScript workout

---
+++ contributions/Hellonearthis/JavaScript/Core/2016-11-20.js
@@ -0,0 +1,5 @@
+Use `splice()` to remove an item from an array
+Data type comparison in `switch` statements
+No block scope
+The difference between null, undefined and NaN
+`console.*` in JavaScript
