765ece0672035cdcc75377d8f2c2a2b9a0908ecc
Gruci <skeksksk@gmail.com>
2016-11-20 08:41:33 +0300

    2016-11-19's Java workout

---
+++ contributions/Gruci/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between ```HashMap``` and ```Hashtable```
+Altering format string output by changing a format specifier's `argument_index`
+Detect or prevent integer overflow
+`Autoboxing` and `Unboxing`
