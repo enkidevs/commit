0751e5354f93514b82c1964197f87cd2ed139bec
Sirius207 <oceanus11034@gmail.com>
2016-11-20 13:50:41 +0300

    2016-11-19's JavaScript workout

---
+++ contributions/Sirius207/JavaScript/React/2016-11-19.js
@@ -0,0 +1,6 @@
+Type of the Children props
+componentWillReceiveProps Not Triggered After Mounting
+
+**React** `this.props.children`
+Expose Component Functions in **React**
+Validate for required props
