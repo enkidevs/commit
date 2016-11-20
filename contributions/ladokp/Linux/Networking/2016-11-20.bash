e1465b491b2b32552130ba4a8a1def7ab097d1a6
ladokp <patrick@ks.co.at>
2016-11-20 10:56:51 +0300

    2016-11-20's Linux workout

---
+++ contributions/ladokp/Linux/Networking/2016-11-20.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Check `iptables` firewall status
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Short Introduction to `Wget` 
