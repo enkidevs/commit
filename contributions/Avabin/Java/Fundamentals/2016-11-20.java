d5a0bcd6eb4acf5eec907de44341bd25c6aeffa0
Avabin <andrzej.piotrowski.76@gmail.com>
2016-11-20 17:23:59 +0300

    2016-11-20's Java workout

---
+++ contributions/Avabin/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use meaningful names
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
+There is no such thing as *pass-by-reference* in Java
