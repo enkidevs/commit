434cea9837388c3f5126ac37324140eb53fbbed3
reginaexmachina <we.love.rachel.day.lots@gmail.com>
2016-11-20 05:59:00 +0300

    2016-11-19's Linux workout

---
+++ contributions/reginaexmachina/Linux/Data Manipulation/2016-11-19.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+Image manipulation  using `convert`
+Clean configuration files using `sed`
+`sort` lines in a text file
+Math in the terminal with `bc`
