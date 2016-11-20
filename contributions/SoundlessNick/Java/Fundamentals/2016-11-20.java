73113a60e01167256b8c18236599e6ca510f845e
SoundlessNick <nserra960@gmail.com>
2016-11-20 23:44:58 +0300

    2016-11-20's Java workout

---
+++ contributions/SoundlessNick/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+The distinction between overloading and overriding methods
+Use meaningful names
+The trade-off between short and descriptive names
+Using Inheritance to reduce code repetition
