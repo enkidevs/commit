6acdbec57d09ce9c8b1d27a11161d00c8856b296
Flos <fn187-github@yahoo.de>
2016-11-20 03:53:34 +0300

    2016-11-19's Linux workout

---
+++ contributions/Flos/Linux/System Management/2016-11-19.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Common log files
+Using `dd` To Create Disk Images
