92e4058f0ceacd0524d1c62f7138b281575bde8d
codykru <codykru@gmail.com>
2016-11-21 01:52:43 +0300

    2016-11-20's Linux workout

---
+++ contributions/codykru/Linux/System Management/2016-11-20.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
