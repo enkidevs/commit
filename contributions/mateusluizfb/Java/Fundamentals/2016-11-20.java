c8f218e8942f4882ac98e1140917c0b6b629b363
mateusluizfb <mateuslfreitasb@gmail.com>
2016-11-20 18:37:22 +0300

    2016-11-20's Java workout

---
+++ contributions/mateusluizfb/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Using an interface as a parameter
