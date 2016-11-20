e1a10ae46878e1e9e8ce19ad7647afff158fd376
koliada <alex.fiator@gmail.com>
2016-11-20 13:19:05 +0300

    2016-11-20's Linux workout

---
+++ contributions/koliada/Linux/File Management/2016-11-20.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Get disk space usage with `df`
+
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Deleting non-empty directories
