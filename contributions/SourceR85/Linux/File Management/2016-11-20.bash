180a73ab0b157a113ec17627c1d87e2e4f2a5ff9
SourceR85 <sjlange@freenet.de>
2016-11-20 10:22:57 +0300

    2016-11-20's Linux workout

---
+++ contributions/SourceR85/Linux/File Management/2016-11-20.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
