30a65ee1c2158e7f6d33241347a98f3268e63925
phjardas <philipp@jardas.de>
2016-11-20 10:57:40 +0300

    2016-11-20's Java workout

---
+++ contributions/phjardas/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
+Using an interface as a parameter
