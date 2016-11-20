7f0fcc2122f63bc59a54d466dc612a58a2b2c746
coalaroot <jakubkrolak94@gmail.com>
2016-11-20 21:40:15 +0300

    2016-11-20's Java workout

---
+++ contributions/coalaroot/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Using `Printstream.format` to format a string before printing
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
