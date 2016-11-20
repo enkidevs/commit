71c0bc68ed60f0195b6788657d00a81281df1d9f
djfdyuruiry <djfdyuruiry@gmail.com>
2016-11-20 12:29:08 +0300

    2016-11-20's Java workout

---
+++ contributions/djfdyuruiry/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
+Using an interface as a parameter
