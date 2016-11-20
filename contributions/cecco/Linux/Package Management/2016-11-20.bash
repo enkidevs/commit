7f1b9d9731cdeb719fec44d310d2204338ce42d5
cecco <francescousseglio@gmail.com>
2016-11-21 00:12:44 +0300

    2016-11-20's Linux workout

---
+++ contributions/cecco/Linux/Package Management/2016-11-20.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Backporting on Debian
+
+Using `Tasksel` for software installation
+Solving `BADSIG GPG` errors
+Keeping repos updated with `cron`
