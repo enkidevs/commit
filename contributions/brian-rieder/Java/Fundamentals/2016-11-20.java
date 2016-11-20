53c66731fe5982b8455bb0fae4deb34d4cb16ac0
brian-rieder <brieder@purdue.edu>
2016-11-20 22:09:06 +0300

    2016-11-20's Java workout

---
+++ contributions/brian-rieder/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+The distinction between overloading and overriding methods
+Always specify type parameter
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
