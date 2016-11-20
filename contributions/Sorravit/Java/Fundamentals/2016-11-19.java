a5860a4bbc0b3b339cb280d665e06a3ce4fc1ccd
Sorravit <sorravit16758@gmail.com>
2016-11-20 20:16:05 +0300

    2016-11-19's Java workout

---
+++ contributions/Sorravit/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
+Using an interface as a parameter
