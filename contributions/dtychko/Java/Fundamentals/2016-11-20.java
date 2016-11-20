29d43c4cd2b3a54c79c5782b30329f8b21c94957
dtychko <dtychko@gmail.com>
2016-11-20 13:54:06 +0300

    2016-11-20's Java workout

---
+++ contributions/dtychko/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Metadata: setting a file's owner
+Measuring time
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
