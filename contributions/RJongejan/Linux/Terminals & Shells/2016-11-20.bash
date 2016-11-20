cb27e0bb6a8522cb4bb7f0c28f323c35e382a152
RJongejan <ruben.jongejan@gmail.com>
2016-11-20 17:43:08 +0300

    2016-11-20's Linux workout

---
+++ contributions/RJongejan/Linux/Terminals & Shells/2016-11-20.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Get to know  your commands with `type`
+`else` statements using the `||` operator 
