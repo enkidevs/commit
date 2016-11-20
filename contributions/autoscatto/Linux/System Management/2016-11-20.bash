83597e251a5ea63d8a5eed849f8ec58ed0312189
autoscatto <4utoscatto@gmail.com>
2016-11-20 14:07:48 +0300

    2016-11-20's Linux workout

---
+++ contributions/autoscatto/Linux/System Management/2016-11-20.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Common log files
+Changing the `/tmp` cleanup frequency
+Executing scripts on system boot using `systemd`
+Finding Open Files With `lsof`
