73999fa10dcdf316eedfcfe9037a8149ae44931e
phillipecardenuto <phillipe.cardenuto@gmail.com>
2016-11-20 05:04:28 +0300

    2016-11-19's Linux workout

---
+++ contributions/phillipecardenuto/Linux/System Management/2016-11-19.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Finding Open Files With `lsof`
