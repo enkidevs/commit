2bdcacaf2e84e49716f76d6ffe4dc30ee40f4104
Chargnn <alexis.410@hotmail.com>
2016-11-20 07:47:56 +0300

    2016-11-19's Java workout

---
+++ contributions/Chargnn/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Writing generic methods
+Metadata: setting a file's owner
+Increase `PermGen` space as to avoid `OutOfMemory` errors
