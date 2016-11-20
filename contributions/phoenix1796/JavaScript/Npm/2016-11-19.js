c96de9c3110e701d9e4db26e5e9137ea8f96e05c
phoenix1796 <phoenix.prog1796@gmail.com>
2016-11-20 18:51:06 +0300

    2016-11-19's JavaScript workout

---
+++ contributions/phoenix1796/JavaScript/Npm/2016-11-19.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+npm can handle multiple versions of a dependency
+List binaries for scripting in npm
+Environmental variables and flags for `npm`
+`peerDependencies`
