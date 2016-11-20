9b514ce2c9ddd8827a9f10b0e7719b9a0ade5bad
Antauri <catalin.zamfir@kitsoftware.ro>
2016-11-20 09:47:02 +0300

    2016-11-20's Java workout

---
+++ contributions/Antauri/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use strings in a `switch` statement
+Using Inheritance to reduce code repetition
+Using bounded type parameters in generic methods
+How range operations work
