1e27e0cad5b4eeb2d4113fa134b4b2575bd140bc
w3bMaster <at.w3bmaster@gmail.com>
2016-11-20 10:51:19 +0300

    2016-11-20's JavaScript workout

---
+++ contributions/w3bMaster/JavaScript/Functions/2016-11-20.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Difference between **.call** and **.apply** methods
+`.bind()` 
+Easily generate a random `HEX` color
+Move cursor at the end of text input
