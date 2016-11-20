48a25e4e1aaeff11077e6a64af6b88f48fee1b2c
EnzDev <enzo@mallard.ml>
2016-11-20 15:08:25 +0300

    2016-11-20's JavaScript workout

---
+++ contributions/EnzDev/JavaScript/Functions/2016-11-20.js
@@ -0,0 +1,5 @@
+Hoisting
+Explicit setting of `this` using `call` and `apply` methods
+Move cursor at the end of text input
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
