32bfbe9417a136c0003a9347de1255e483e1346d
terminator48 <com.comi@mail.ru>
2016-11-20 08:53:27 +0300

    2016-11-19's Java workout

---
+++ contributions/terminator48/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
