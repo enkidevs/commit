713a42ef46cf2afdd0c77ff715547ec242c6e462
gobbas <gabbi@hotmail.no>
2016-11-20 14:23:57 +0300

    2016-11-20's Java workout

---
+++ contributions/gobbas/Java/Classes & Objects/2016-11-20.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Utility classes and `static` methods
+Compare classes and not class names
+Diamond Operator
+String formatting and parsing with `SimpleDateFormat`
