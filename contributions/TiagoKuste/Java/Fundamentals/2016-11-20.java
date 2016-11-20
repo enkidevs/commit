07938549143df55ccbd04f616637b520a75fff43
TiagoKuste <tiago.tagcosta@gmail.com>
2016-11-21 01:10:28 +0300

    2016-11-20's Java workout

---
+++ contributions/TiagoKuste/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Catch multiple exceptions in a single `catch` block
+Altering format string output by changing a format specifier's `argument_index`
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
