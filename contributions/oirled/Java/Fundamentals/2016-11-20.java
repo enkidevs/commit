a015fad3cd6dfdb4512e92eace8569644ba27a2b
oirled <nclsdlr@gmail.com>
2016-11-20 15:02:15 +0300

    2016-11-20's Java workout

---
+++ contributions/oirled/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
