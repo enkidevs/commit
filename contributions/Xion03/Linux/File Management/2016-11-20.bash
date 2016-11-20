2231751b1c245780fc64178d17c2b734d6e965d8
Xion03 <xion03@gmail.com>
2016-11-20 11:33:48 +0300

    2016-11-20's Linux workout

---
+++ contributions/Xion03/Linux/File Management/2016-11-20.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
