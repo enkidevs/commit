4869c0b35c0ada24abecb487c98392e21e2dfee2
BigBearDAC <flidnibble@gmail.com>
2016-11-21 00:56:43 +0300

    2016-11-20's Java workout

---
+++ contributions/BigBearDAC/Java/Data Types/2016-11-20.java
@@ -0,0 +1,5 @@
+Two ways to use an `Iterator`
+Multidimensional array declaration
+Converting a string to upper or lower case
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
