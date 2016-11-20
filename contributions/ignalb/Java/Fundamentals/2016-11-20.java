ad36eb3c092fd9220652426a104e1a0b496db8c4
ignalb <baxter.ignalb@gmail.com>
2016-11-21 01:15:32 +0300

    2016-11-20's Java workout

---
+++ contributions/ignalb/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
