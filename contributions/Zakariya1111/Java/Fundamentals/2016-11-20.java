37a6947c539a35b5855923cf010bb39d74d9cfbf
Zakariya1111 <elaizaidyahoo.fr@gmail.com>
2016-11-20 14:55:12 +0300

    2016-11-20's Java workout

---
+++ contributions/Zakariya1111/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Do not perform bitwise and arithmetic operations on the same data
+How range operations work
