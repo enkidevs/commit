ea5a8d349778935fb66bf2cd1c23c9bf50ddeaae
lado <lgolijashvili@yahoo.com>
2016-11-20 18:40:38 +0300

    2016-11-19's Java workout

---
+++ contributions/lado/Java/Data Types/2016-11-19.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Multidimensional array declaration
+Converting numbers to strings
+Converting a string to upper or lower case
+Do not attempt comparisons with NaN
