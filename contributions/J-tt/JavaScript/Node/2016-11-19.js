49a855290928368a8f39e853283d79f79e6e543c
J-tt <jacksonjett5@gmail.com>
2016-11-20 04:15:50 +0300

    2016-11-19's JavaScript workout

---
+++ contributions/J-tt/JavaScript/Node/2016-11-19.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+First-error callbacks in **Node**
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+`exports` vs. `module.exports` in **Node**
