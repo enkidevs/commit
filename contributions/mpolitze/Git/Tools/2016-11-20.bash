bcb88173281d7a4093892ad938db657c8a2148f2
mpolitze <m@politze.net>
2016-11-20 13:50:47 +0300

    2016-11-20's Git workout

---
+++ contributions/mpolitze/Git/Tools/2016-11-20.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Locate a commit by its hash
+Specify a commit by its ancestry
+Sign your work (using GPG)
+Remember the results of previous hunk conflicts
