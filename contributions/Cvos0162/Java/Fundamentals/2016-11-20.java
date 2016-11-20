feef2cb451ed3ccbe98ded922dfb94b3b41388c0
Cvos0162 <cookys0162@hotmail.co.kr>
2016-11-20 17:57:19 +0300

    2016-11-20's Java workout

---
+++ contributions/Cvos0162/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use meaningful names
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
+How range operations work
