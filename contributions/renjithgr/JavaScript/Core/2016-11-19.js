da472071ec816ea24c27bd47f7a0a9de109991b0
renjithgr <grenjithr@gmail.com>
2016-11-20 18:39:45 +0300

    2016-11-19's JavaScript workout

---
+++ contributions/renjithgr/JavaScript/Core/2016-11-19.js
@@ -0,0 +1,5 @@
+Keywords in hash keys
+Using the double tilde `~~` 
+Data type comparison in `switch` statements
+The difference between null, undefined and NaN
+Inaccuracy of binary floating-point format
