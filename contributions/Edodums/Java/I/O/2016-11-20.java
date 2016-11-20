0eca09c305582e04fe702d3b65329bf11462437d
Edodums <marcoedoardo.duma@studio.unibo.it>
2016-11-20 11:33:16 +0300

    2016-11-20's Java workout

---
+++ contributions/Edodums/Java/I/O/2016-11-20.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Listing a file system's root directories
+The `Console` class
