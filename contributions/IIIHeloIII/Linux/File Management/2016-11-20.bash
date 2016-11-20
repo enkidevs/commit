bb7f74d40b81984bcfc936f14fd34f3e15067139
IIIHeloIII <kristiandahl@gmail.com>
2016-11-20 15:57:32 +0300

    2016-11-20's Linux workout

---
+++ contributions/IIIHeloIII/Linux/File Management/2016-11-20.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Using `touch` to alter files modification time
+Working with `.gz` files
