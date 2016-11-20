d8f5d862be0c45e119c919c5b5191a62c881ab81
Kvetoslav <jiri.heneberk@icompllex.com>
2016-11-20 22:58:11 +0300

    2016-11-20's JavaScript workout

---
+++ contributions/Kvetoslav/JavaScript/Core/2016-11-20.js
@@ -0,0 +1,5 @@
+Hoisting applies only to variable declarations, not initializations
+NaN === NaN?
+The difference between null, undefined and NaN
+Context in JavaScript
+Getting the *real* dimensions of an image
