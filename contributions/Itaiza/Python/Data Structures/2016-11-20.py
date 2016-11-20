ec972590bcafc5aaeb8ba2601c14ec78d886b6aa
Itaiza <itai.za@gmail.com>
2016-11-20 19:33:12 +0300

    2016-11-20's Python workout

---
+++ contributions/Itaiza/Python/Data Structures/2016-11-20.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+When `range` comes in handy
+`Module`s everywhere!
+There is more to copying
+Binary record packing with `bytearray`
