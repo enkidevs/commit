4248693b2eabb6c20dfad58dd8041c5ac95f33b1
allfanmu <allfanmu@wp.pl>
2016-11-20 12:34:48 +0300

    2016-11-20's Java workout

---
+++ contributions/allfanmu/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
