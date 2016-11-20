da5f0bf29725c51355531780a8085a14f17fec6d
lfiaschi <luca.fiaschi@gmail.com>
2016-11-20 12:09:21 +0300

    2016-11-20's Python workout

---
+++ contributions/lfiaschi/Python/Data Structures/2016-11-20.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+There is more to copying
+A thread-safe `Queue`
+Implementing **weak** references
+Binary record packing with `bytearray`
