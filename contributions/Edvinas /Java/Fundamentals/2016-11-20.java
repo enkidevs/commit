1386ac6ae975fe39dda0054099f98c3cb9bfe208
Edvinas <edvinas.reg@outlook.com>
2016-11-20 13:07:38 +0300

    2016-11-20's Java workout

---
+++ contributions/Edvinas /Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Catch multiple exceptions in a single `catch` block
+Using bounded type parameters in generic methods
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
