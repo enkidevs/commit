ed262bfa20a28f2edd1c36a83b326a2910bcf2d5
Grorel <grorel.kazui@gmail.com>
2016-11-20 11:08:53 +0300

    2016-11-20's Java workout

---
+++ contributions/Grorel/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
