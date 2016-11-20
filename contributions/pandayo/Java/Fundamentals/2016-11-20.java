eae9558c927919d7237053bec7d9fb08742fac72
pandayo <lukas.schwabe@gmail.com>
2016-11-20 13:19:24 +0300

    2016-11-20's Java workout

---
+++ contributions/pandayo/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Supply `toString()` in all classes
+Using an interface as a parameter
