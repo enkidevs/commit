afeb6747000f7d7334c2dc0b69f8bac2169ab80e
siva <sivaraj2205@gmail.com>
2016-11-20 06:58:32 +0300

    2016-11-19's Java workout

---
+++ contributions/siva/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
+Avoid using `static` variables
