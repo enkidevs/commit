9aabe194fd64c03b8f59689d7085d72a9fa36d89
BOSSoNe0013 <bossone0013@gmail.com>
2016-11-20 18:43:25 +0300

    2016-11-20's Java workout

---
+++ contributions/BOSSoNe0013/Java/Classes & Objects/2016-11-20.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Using `static` fields
+How and when to use `WeakHashMap` class
+Fields with Java **Reflection API**
