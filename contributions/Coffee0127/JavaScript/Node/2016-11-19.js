b85386c56159a458b9b759d814ad8a42f7c42fa7
Coffee0127 <coffee0127@gmail.com>
2016-11-20 17:40:39 +0300

    2016-11-19's JavaScript workout

---
+++ contributions/Coffee0127/JavaScript/Node/2016-11-19.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+Streams in **Node**
+`uncaughtException` listener in Node.js
+Manage child processes in **Node**
