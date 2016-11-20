4700fd05152eef4bc89c2be765b9f42998bc05db
trion <parmsingh129@yahoo.com>
2016-11-20 18:55:04 +0300

    2016-11-19's Java workout

---
+++ contributions/trion/Java/Fundamentals/2016-11-19.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Altering format string output by changing a format specifier's `argument_index`
+`Autoboxing` and `Unboxing`
+How range operations work
+Supply `toString()` in all classes
