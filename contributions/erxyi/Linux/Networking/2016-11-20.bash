84c4d3a406225c0fbee66adbd006b7a26b928313
erxyi <erxyi@safe-mail.net>
2016-11-20 13:11:34 +0300

    2016-11-20's Linux workout

---
+++ contributions/erxyi/Linux/Networking/2016-11-20.bash
@@ -0,0 +1,5 @@
+Stress testing with `ab`
+Multiple IP Address with aliasing 
+Useful ssh client optimizations 
+Get real network statistics with `slurm`
+Staying connected remotely via SSH without an interactive shell
