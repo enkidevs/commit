bbece0014fc8f0e7280340bdf44d72d68c2f4f0f
remipou <remi.puig@gmail.com>
2016-11-20 10:57:47 +0300

    2016-11-20's Java workout

---
+++ contributions/remipou/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+The trade-off between short and descriptive names
+Java naming conventions
+Using Inheritance to reduce code repetition
+Calling one constructor from another
