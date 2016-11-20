facc944963b26f9c1a1c0844e65a13ddeb5b3bc4
bbohac <bastian.bohac@gmail.com>
2016-11-20 14:37:12 +0300

    2016-11-20's Java workout

---
+++ contributions/bbohac/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+How range operations work
