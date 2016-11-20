024155993a984dfc7058b9b38d444176b6aef503
b-diddy <bendikflotre@gmail.com>
2016-11-20 12:04:59 +0300

    2016-11-20's Linux workout

---
+++ contributions/b-diddy/Linux/Pipeline/2016-11-20.bash
@@ -0,0 +1,5 @@
+Difference between `redirection` and `pipe`
+Monitor the progress of data through a pipe with `pv`
+Duplicate pipe content with `tee`
+Run local scripts remotely
+Play sounds remotely
