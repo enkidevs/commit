dc534e02e052a75c2b168cb75fd8b29dac4945a0
leitljaroslav <leitl.jaroslav@gmail.com>
2016-11-20 17:39:35 +0300

    2016-11-20's Java workout

---
+++ contributions/leitljaroslav/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Detect or prevent integer overflow
