0e279512637e3687a71d8cd392936063c79115aa
wasteland540 <m_m_elz@web.de>
2016-11-20 22:08:53 +0300

    2016-11-20's JavaScript workout

---
+++ contributions/wasteland540/JavaScript/Npm/2016-11-20.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+npm can handle multiple versions of a dependency
+Passing through command line arguments in npm scripts
+Scoped packages
+Lock down dependency versions by shrinkwrapping
