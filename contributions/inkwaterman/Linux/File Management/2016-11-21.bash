37ab1ea2256a039efc1765b9b93cb5eceff8a2a9
inkwaterman <inkwaterman@gmail.com>
2016-11-21 12:45:49 +0300

    2016-11-21's Linux workout

---
+++ contributions/inkwaterman/Linux/File Management/2016-11-21.bash
@@ -0,0 +1,7 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
