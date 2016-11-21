49789dc536d01e1f3ea614ae77e6ff908b8817aa
jttwnsnd <jt.a.townsend@gmail.com>
2016-11-21 06:22:39 +0300

    2016-11-20's JavaScript workout

---
+++ contributions/jttwnsnd/JavaScript/Npm/2016-11-20.js
@@ -0,0 +1,5 @@
+Publishing with npm
+Passing through command line arguments in npm scripts
+List binaries for scripting in npm
+Tab completion in `npm`
+Lock down dependency versions by shrinkwrapping
