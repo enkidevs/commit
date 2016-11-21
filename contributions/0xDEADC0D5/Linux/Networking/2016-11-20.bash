a9907de3264b6bf1f08cf8cf2474d6125fad36f5
0xDEADC0D5 <seductivethunderstorm@gmail.com>
2016-11-21 08:17:53 +0300

    2016-11-20's Linux workout

---
+++ contributions/0xDEADC0D5/Linux/Networking/2016-11-20.bash
@@ -0,0 +1,5 @@
+Get real network statistics with `slurm`
+Limit the `wget` download rate
+List your IPv6 configuration
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
