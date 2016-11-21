98097391cd5f632bc4c62ecdae3c8533fd684a5c
iarrascaeta <nachoarrascaeta_90@hotmail.com>
2016-11-22 00:58:53 +0300

    2016-11-21's Java workout

---
+++ contributions/iarrascaeta/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+Writing generic methods
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
