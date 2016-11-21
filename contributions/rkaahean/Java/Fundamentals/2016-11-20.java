f4dcf9a6aaf511cddc539d59f89c3af0f49394ea
rkaahean <ranjan31051997@gmail.com>
2016-11-21 17:08:34 +0300

    2016-11-20's Java workout

---
+++ contributions/rkaahean/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Measuring time
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
