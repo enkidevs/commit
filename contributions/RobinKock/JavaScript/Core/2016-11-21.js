f92542c56b28da15ac1125e5833d2b6a51a59b3c
RobinKock <robin@kock-hamburg.de>
2016-11-21 11:39:09 +0300

    2016-11-21's JavaScript workout

---
+++ contributions/RobinKock/JavaScript/Core/2016-11-21.js
@@ -0,0 +1,5 @@
+Quick way to format integers with a fixed number of digits.
+Be careful when using `typeof`.
+No block scope
+The difference between null, undefined and NaN
+Detect online connection
