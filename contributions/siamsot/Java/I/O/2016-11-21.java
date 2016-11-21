f62ecc5ef5e1dd560498fa76e509aea2f511ab15
siamsot <siamsot@gmail.com>
2016-11-21 19:29:51 +0300

    2016-11-21's Java workout

---
+++ contributions/siamsot/Java/I/O/2016-11-21.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Create system file paths using the `Path` class
+Deleting files and directories
+The `Console` class
