a9742140d8c54d7d7cd6269a19c4dd674bf27872
mattmahn <mahnkem@msoe.edu>
2016-11-22 01:14:15 +0300

    2016-11-21's Java workout

---
+++ contributions/mattmahn/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
