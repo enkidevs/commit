9d7a7ad5ed51530ca72d219b4f7aaea56235c3f0
mistdrg <mistdrg@naver.com>
2016-11-22 02:43:47 +0300

    2016-11-21's Java workout

---
+++ contributions/mistdrg/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Calling one constructor from another
+Detect or prevent integer overflow
+How range operations work
