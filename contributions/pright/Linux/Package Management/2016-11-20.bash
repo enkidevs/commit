c1ccf0eb2b7f4c9e84685d76e2c322bce4c3b753
pright <pright.xj@gmail.com>
2016-11-21 20:22:13 +0300

    2016-11-20's Linux workout

---
+++ contributions/pright/Linux/Package Management/2016-11-20.bash
@@ -0,0 +1,5 @@
+Install specific version of packages
+Get `apt` to use a mirror / faster mirror
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
+Keeping repos updated with `cron`
