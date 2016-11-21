1060adeff943d14cec38da59f9173ae802c78800
Aristeia <11025x12@gmail.com>
2016-11-21 18:45:30 +0300

    2016-11-20's Java workout

---
+++ contributions/Aristeia/Java/I/O/2016-11-20.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Deleting files and directories
