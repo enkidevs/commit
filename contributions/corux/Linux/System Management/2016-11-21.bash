fafaacd61aa0351af6e25b6663cccf03c60f21ef
corux <tobias@wallura.org>
2016-11-21 10:28:36 +0300

    2016-11-21's Linux workout

---
+++ contributions/corux/Linux/System Management/2016-11-21.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Get hardware stack details with `lspci`
