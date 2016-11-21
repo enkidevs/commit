1ac80d829babc1f4800cb25f7c141e16c4c26425
wilsonhipolito <wilson.hipolito@vivareal.com>
2016-11-21 17:52:58 +0300

    2016-11-21's Linux workout

---
+++ contributions/wilsonhipolito/Linux/System Management/2016-11-21.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
+System uptime
