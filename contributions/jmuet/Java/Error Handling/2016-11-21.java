9c8998db3ef6d3190858508a514ca6d5806273a1
jmuet <dziekan.jan@gmail.com>
2016-11-21 18:43:07 +0300

    2016-11-21's Java workout

---
+++ contributions/jmuet/Java/Error Handling/2016-11-21.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
