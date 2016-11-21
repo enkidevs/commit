48821b3558739c046e1279e60788367bbd84d7d6
tfluehmann <t.fluehmann94@hotmail.com>
2016-11-21 04:10:08 +0300

    2016-11-20's Java workout

---
+++ contributions/tfluehmann/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Using an interface as a parameter
