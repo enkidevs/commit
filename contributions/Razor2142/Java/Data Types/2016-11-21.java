3a3c6656b5dca10c6972f96611e757be0feb2a07
Razor2142 <razor2142@graveyardgames.net>
2016-11-21 09:35:20 +0300

    2016-11-21's Java workout

---
+++ contributions/Razor2142/Java/Data Types/2016-11-21.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Converting a string to upper or lower case
+`StringBuffer` vs `StringBuilder`
+Retrieve the component type of an array
