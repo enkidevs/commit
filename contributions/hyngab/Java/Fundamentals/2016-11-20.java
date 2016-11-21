313317c1477b05b1c7dd3e1ed7b5a6455b0ed101
hyngab <hyngab.developer@gmail.com>
2016-11-21 17:13:46 +0300

    2016-11-20's Java workout

---
+++ contributions/hyngab/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Difference between `==` and `equals()`
+Uses of the `final` keyword
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
