2c5d8ef77c48cd2b2cb1ea5e2f39ead75ff61388
effigy13 <frederika13@yahoo.com>
2016-11-21 20:55:21 +0300

    2016-11-21's Linux workout

---
+++ contributions/effigy13/Linux/System Management/2016-11-21.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Test disk speed with `dd`
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Commands to shutdown or restart the system
