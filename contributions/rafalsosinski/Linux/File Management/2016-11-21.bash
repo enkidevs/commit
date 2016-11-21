23ab41a702cc901344ad16a263642edecede0b8f
rafalsosinski <rafal.sosinski+github@gmail.com>
2016-11-21 12:05:27 +0300

    2016-11-21's Linux workout

---
+++ contributions/rafalsosinski/Linux/File Management/2016-11-21.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Easy way of sharing files
