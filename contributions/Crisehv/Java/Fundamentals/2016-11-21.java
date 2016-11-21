f3ccdb29e9e672cfc36f548c352420ebb7b44065
Crisehv <crisehv14@gmail.com>
2016-11-22 00:20:25 +0300

    2016-11-21's Java workout

---
+++ contributions/Crisehv/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The trade-off between short and descriptive names
+Common mistake on switch statements
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
