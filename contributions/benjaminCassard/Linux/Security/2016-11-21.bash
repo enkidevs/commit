6cd3bea267729fa1780e337e15af66c0c8de094c
benjaminCassard <benjamin.cassard@gmail.com>
2016-11-21 08:50:50 +0300

    2016-11-21's Linux workout

---
+++ contributions/benjaminCassard/Linux/Security/2016-11-21.bash
@@ -0,0 +1,5 @@
+Extracting the `public key` from the `private key`
+Best password generation utilities
+Disabling **USB** storage devices
+Keeping `/boot` read-only
+Private key encryption with `OpenSSL`
