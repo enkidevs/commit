52ff11dc680afdf3d91fb133cb7aaac0cd85e393
Rcarballo2222 <rcarballo2222@gmail.com>
2016-11-22 02:17:31 +0300

    2016-11-21's Java workout

---
+++ contributions/Rcarballo2222/Java/Data Types/2016-11-21.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Multidimensional array declaration
+Do not attempt comparisons with NaN
