d37f1d325592181ad4c8ec419025022bacbc5668
heluvaguy <adarshjaiswal1989@gmail.com>
2016-11-21 06:22:57 +0300

    2016-11-20's Java workout

---
+++ contributions/heluvaguy/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
+Using `Printstream.format` to format a string before printing
+Supply `toString()` in all classes
