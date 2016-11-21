53036463a17f69faed8fdc35920fe04545902001
Mattle <mcallier@gmail.com>
2016-11-21 19:53:38 +0300

    2016-11-21's Java workout

---
+++ contributions/Mattle/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Ternary operator
+Using bounded type parameters in generic methods
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
