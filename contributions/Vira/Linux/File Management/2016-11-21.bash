7a9f5dbd244330b3fea0e67898ceac1679ed1e20
Vira <thomas.ruehrig@studierende.htl-donaustadt.at>
2016-11-21 19:59:45 +0300

    2016-11-21's Linux workout

---
+++ contributions/Vira/Linux/File Management/2016-11-21.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
