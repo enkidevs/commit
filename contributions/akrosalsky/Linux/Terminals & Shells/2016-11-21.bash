cec9f6efc3139362d7e0ac09331393de25f09ad5
akrosalsky <akrosalsky@gmail.com>
2016-11-21 22:00:43 +0300

    2016-11-21's Linux workout

---
+++ contributions/akrosalsky/Linux/Terminals & Shells/2016-11-21.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Aliasing ssh connections
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
