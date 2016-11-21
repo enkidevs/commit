46de028981552d4cd167547d1f45389db01f1013
im_43 <lucastraba@gmail.com>
2016-11-21 05:09:04 +0300

    2016-11-20's JavaScript workout

---
+++ contributions/im_43/JavaScript/React/2016-11-20.js
@@ -0,0 +1,5 @@
+Functional Stateless Components in React
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+Dealing with `this.props.children`
+Specify a single child
