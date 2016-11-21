aea253fb13c106fee750daebf52cc81847062728
motabass <markus.mohoritsch@gmx.at>
2016-11-21 22:01:05 +0300

    2016-11-21's Java workout

---
+++ contributions/motabass/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Using Inheritance to reduce code repetition
+Always specify type parameter
+There is no such thing as *pass-by-reference* in Java
