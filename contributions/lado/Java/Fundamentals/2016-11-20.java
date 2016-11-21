004dcd16009ceb2d844c6ab854aa20605f5c3d88
lado <lgolijashvili@yahoo.com>
2016-11-21 18:25:41 +0300

    2016-11-20's Java workout

---
+++ contributions/lado/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The trade-off between short and descriptive names
+Common mistake on switch statements
+Using Inheritance to reduce code repetition
+Calling one constructor from another
