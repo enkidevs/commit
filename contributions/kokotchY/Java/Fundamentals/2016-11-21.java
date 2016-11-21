ddf3658a7b8bbd9f3465e1e79375ed1a41e90d6d
kokotchY <kokotchy@gmail.com>
2016-11-21 23:05:54 +0300

    2016-11-21's Java workout

---
+++ contributions/kokotchY/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+Catch multiple exceptions in a single `catch` block
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
