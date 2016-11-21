e3cc89f5a0aa85115697c8b216b83d4a7633a148
pieter.thysen <pieter.thysen@gmail.com>
2016-11-21 20:14:00 +0300

    2016-11-21's Java workout

---
+++ contributions/pieter.thysen/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
+Using `synchronized` statements
+Detect or prevent integer overflow
