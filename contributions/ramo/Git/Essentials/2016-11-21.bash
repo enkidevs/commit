4f66a4396c026745b7a8a16457ad3eb6a257a397
ramo <ramin@motakef.de>
2016-11-21 23:29:34 +0300

    2016-11-21's Git workout

---
+++ contributions/ramo/Git/Essentials/2016-11-21.bash
@@ -0,0 +1,5 @@
+Using aliases for git commands
+Make your log output pretty
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
