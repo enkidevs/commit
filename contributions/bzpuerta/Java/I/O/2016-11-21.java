1596f5081e27d55659e90f375efac9807251f4b4
bzpuerta <beatriz.puerta.hoyas@gmail.com>
2016-11-22 01:21:45 +0300

    2016-11-21's Java workout

---
+++ contributions/bzpuerta/Java/I/O/2016-11-21.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+Translating `Scanner` tokens into primitive types
+Listing a file system's root directories
+The `Console` class
