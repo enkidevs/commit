f4c4067a2f5121db8d675c19525f2bf4adcd9622
mattsoftware <matt@mattsoftware.com>
2016-11-22 00:52:24 +0300

    2016-11-21's Linux workout

---
+++ contributions/mattsoftware/Linux/System Management/2016-11-21.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Change your `hostname` on systems using `systemd`
+Common log files
+Using `dd` To Create Disk Images
