485732288bc2bf460cf08324b1db49a89e5f82a7
Exzelsios <tobiasbohnxz@gmail.com>
2016-11-21 11:43:15 +0300

    2016-11-21's Java workout

---
+++ contributions/Exzelsios/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+Catch multiple exceptions in a single `catch` block
+Avoid using `static` variables
+Using an interface as a parameter
