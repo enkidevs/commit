e5a0e14ec39a599ad9edf571ae7bdfefa0d2b3e1
jimka <jimmy.karlsson@gmail.com>
2016-11-21 12:16:14 +0300

    2016-11-21's Java workout

---
+++ contributions/jimka/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
