a7e81cd715baae4b6a6ce54f735b1267913fb998
jlstanford <jlstanford@bsu.edu>
2016-11-21 11:02:51 +0300

    2016-11-20's Java workout

---
+++ contributions/jlstanford/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
