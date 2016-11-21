453ef5684db381777454a3caeaf35f4ec01ce227
ScottBrenner <scottstimo@gmail.com>
2016-11-21 19:56:27 +0300

    2016-11-21's Linux workout

---
+++ contributions/ScottBrenner/Linux/File Management/2016-11-21.bash
@@ -0,0 +1,5 @@
+Extracting `tar` files to a specific directory
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Deleting non-empty directories
+Delete commands aliases
