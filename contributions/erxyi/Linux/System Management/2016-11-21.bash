7767bb99050f47e7379017072b5a706615d27972
erxyi <erxyi@safe-mail.net>
2016-11-21 17:03:09 +0300

    2016-11-21's Linux workout

---
+++ contributions/erxyi/Linux/System Management/2016-11-21.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Incorrect time on dual boot systems
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
