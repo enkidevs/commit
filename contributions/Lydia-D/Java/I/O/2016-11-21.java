1b8d37935f1d0bc4ba3a0824eaf102c963cd83d9
Lydia-D <lydia.drabsch@gmail.com>
2016-11-22 00:29:56 +0300

    2016-11-21's Java workout

---
+++ contributions/Lydia-D/Java/I/O/2016-11-21.java
@@ -0,0 +1,5 @@
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Deleting files and directories
+The `Console` class
