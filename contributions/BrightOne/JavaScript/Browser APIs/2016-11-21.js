a46cc7183144c0a60fc8684dbac9695ebea62640
BrightOne <filalex77@gmail.com>
2016-11-21 23:21:33 +0300

    2016-11-21's JavaScript workout

---
+++ contributions/BrightOne/JavaScript/Browser APIs/2016-11-21.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Use hosted scripts to increase performance
+Form a `URL` from its parts 
+Modify a website's URL
+Window.sessionStorage
