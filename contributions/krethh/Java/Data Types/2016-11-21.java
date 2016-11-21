be95c751cd91ccaeb6dd81a0289037b3149fbacd
krethh <krecik.kret@gmail.com>
2016-11-22 01:25:33 +0300

    2016-11-21's Java workout

---
+++ contributions/krethh/Java/Data Types/2016-11-21.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Converting a string to upper or lower case
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
