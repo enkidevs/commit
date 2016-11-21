9de1217384144e8724b552ff91fc308e0ff90c74
durandj <james.durand@alumni.msoe.edu>
2016-11-21 17:53:47 +0300

    2016-11-21's Linux workout

---
+++ contributions/durandj/Linux/File Management/2016-11-21.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
