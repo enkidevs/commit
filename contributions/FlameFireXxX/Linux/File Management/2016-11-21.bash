40d7048b313dcea772a03f24ef6f08e17f8cdaff
FlameFireXxX <lyndon.lapierre@gmail.com>
2016-11-21 20:58:10 +0300

    2016-11-21's Linux workout

---
+++ contributions/FlameFireXxX/Linux/File Management/2016-11-21.bash
@@ -0,0 +1,6 @@
+Truncate files with `cat` instead of `rm`
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
