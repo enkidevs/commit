4ff0b031cc74071bdf85b94e4881d161ba8e3e87
stain88 <marcbaghdadi@gmail.com>
2016-11-21 13:08:54 +0300

    2016-11-21's JavaScript workout

---
+++ contributions/stain88/JavaScript/Functions/2016-11-21.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function expression
+IIFE pattern
+Social sharing without widgets
+Changing a functions context with `fn.call(object)`
