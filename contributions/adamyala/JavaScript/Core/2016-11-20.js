82e21cc5b053c14ab7f6b80806838aa17a1109ca
adamyala <adamkyala@gmail.com>
2016-11-21 05:50:26 +0300

    2016-11-20's JavaScript workout

---
+++ contributions/adamyala/JavaScript/Core/2016-11-20.js
@@ -0,0 +1,5 @@
+Performing a function at timed intervals
+Hoisting applies only to variable declarations, not initializations
+The difference between null, undefined and NaN
+`catch` is block scoped
+Detect online connection
