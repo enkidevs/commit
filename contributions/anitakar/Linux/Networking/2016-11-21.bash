f9c2d630de2df7b017b370ea83dbdb1adeca9327
anitakar <akarbow@gmail.com>
2016-11-21 11:41:47 +0300

    2016-11-21's Linux workout

---
+++ contributions/anitakar/Linux/Networking/2016-11-21.bash
@@ -0,0 +1,5 @@
+How to clear `iptables` rules
+Find services running on your host
+Multiple IP Address with aliasing 
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
