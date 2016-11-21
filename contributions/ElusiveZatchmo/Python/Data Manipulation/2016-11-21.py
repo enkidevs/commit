d815612909e16a49d24e012628e4384939dfd6be
ElusiveZatchmo <zachary.ledbetter@gmail.com>
2016-11-22 00:10:28 +0300

    2016-11-21's Python workout

---
+++ contributions/ElusiveZatchmo/Python/Data Manipulation/2016-11-21.py
@@ -0,0 +1,5 @@
+Three ways to substitute a substring of a string
+Pythonic substring testing
+Use `struct` to work with Binary Data
+Get the similarity ratio of two sequences
+Working with junk data
