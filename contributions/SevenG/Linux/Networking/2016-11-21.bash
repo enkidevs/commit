75f7789d93272cd3de5273c5a0195387ef911c4d
SevenG <enki@seveng.net>
2016-11-21 18:19:29 +0300

    2016-11-21's Linux workout

---
+++ contributions/SevenG/Linux/Networking/2016-11-21.bash
@@ -0,0 +1,5 @@
+Logging dropped firewall packets
+Multiple IP Address with aliasing 
+Useful ssh client optimizations 
+Bandwidth monitoring tools 
+Use `tcpdump` to listen to network interface traffic
