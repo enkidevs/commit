dfdea83c06f5a52a91ee590357aa9953068dbef6
stufixlp <stufixlp@gmail.com>
2016-11-22 02:29:54 +0300

    2016-11-21's Linux workout

---
+++ contributions/stufixlp/Linux/System Management/2016-11-21.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Test disk speed with `dd`
+Change your `hostname` on systems using `systemd`
+Executing scripts on system boot using `systemd`
+Commands to shutdown or restart the system
