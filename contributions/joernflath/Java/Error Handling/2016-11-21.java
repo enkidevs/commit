d914dfef001b3aaedca2ee652b2e892a1c8a35fb
joernflath <joern.flath@gmail.com>
2016-11-21 21:23:15 +0300

    2016-11-21's Java workout

---
+++ contributions/joernflath/Java/Error Handling/2016-11-21.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Careful Numeric Data Conversions
