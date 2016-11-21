4452eabc4a0a78f75c37bf26ddc6dbcbc45e1d56
jk0de <gizmo1398@gmail.com>
2016-11-21 20:15:32 +0300

    2016-11-21's Java workout

---
+++ contributions/jk0de/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Avoid using `static` variables
