2ad0a9170ec1514ce0f0dfa4386d2d8ab17368de
PelagicGames <games.pelagic@gmail.com>
2016-11-22 00:51:33 +0300

    2016-11-21's Java workout

---
+++ contributions/PelagicGames/Java/Error Handling/2016-11-21.java
@@ -0,0 +1,5 @@
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
