eb2d147e46a7fe6872e9b8ede290c9cae7fcf4ac
rodjo2u <rodjo2u@gmail.com>
2016-11-21 23:04:32 +0300

    2016-11-21's Linux workout

---
+++ contributions/rodjo2u/Linux/File Management/2016-11-21.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Delete commands aliases
