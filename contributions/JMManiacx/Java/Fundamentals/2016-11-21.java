d298b7e6a3081afc5ca56d6fda1fe019a285c4ed
JMManiacx <www.jakobm@yahoo.de>
2016-11-21 06:58:12 +0300

    2016-11-21's Java workout

---
+++ contributions/JMManiacx/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Using Inheritance to reduce code repetition
+Using `Printstream.format` to format a string before printing
+Do not perform bitwise and arithmetic operations on the same data
