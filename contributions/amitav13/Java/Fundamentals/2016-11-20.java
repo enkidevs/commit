b18b68e311ae328c7cd8672cdea056c0c8dcb288
amitav13 <amitav.khandelwal@gmail.com>
2016-11-21 15:15:10 +0300

    2016-11-20's Java workout

---
+++ contributions/amitav13/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
