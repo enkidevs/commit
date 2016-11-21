c6b78ac6bd64f6857344cff2d581f59a28553533
SoundlessNick <nserra960@gmail.com>
2016-11-21 22:41:11 +0300

    2016-11-21's Java workout

---
+++ contributions/SoundlessNick/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Java naming conventions
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Calling one constructor from another
