cc61484464e99be16a94bbcb5cb6c37a5c2edf1e
bbohac <bastian.bohac@gmail.com>
2016-11-21 10:16:15 +0300

    2016-11-21's Java workout

---
+++ contributions/bbohac/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using bounded type parameters in generic methods
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
