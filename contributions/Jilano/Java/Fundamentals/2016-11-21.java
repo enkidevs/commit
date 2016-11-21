3a21d76e669968ba7cd4ff90b47cf6fdc4cb72dc
Jilano <mobile.axel@gmail.com>
2016-11-21 08:59:05 +0300

    2016-11-21's Java workout

---
+++ contributions/Jilano/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Java naming conventions
+Common mistake on switch statements
+Ternary operator
+Use `DecimalFormat` class to format numbers
