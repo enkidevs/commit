caa1ee446e647fb3940136a3f70597d7cb44bdad
andrewda <dassonville.andrew@gmail.com>
2016-11-21 18:46:19 +0300

    2016-11-21's JavaScript workout

---
+++ contributions/andrewda/JavaScript/React/2016-11-21.js
@@ -0,0 +1,7 @@
+Type of the Children props
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Dangerously set `innerHTML`
+Keyed fragments in **React**
