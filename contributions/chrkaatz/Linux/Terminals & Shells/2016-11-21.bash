0858982737c9513921b15cebee266e5bd582973b
chrkaatz <chrkaatz@googlemail.com>
2016-11-21 13:59:51 +0300

    2016-11-21's Linux workout

---
+++ contributions/chrkaatz/Linux/Terminals & Shells/2016-11-21.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Random password generator
+Terminal incognito mode
+The Basics of Environment Variables
