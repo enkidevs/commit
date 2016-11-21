d56e0d63369880f028351174298666f65b5cacf0
Sushant-M <sushant.post@gmail.com>
2016-11-21 10:09:02 +0300

    2016-11-20's Java workout

---
+++ contributions/Sushant-M/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Java naming conventions
+Using Inheritance to reduce code repetition
+Measuring time
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
