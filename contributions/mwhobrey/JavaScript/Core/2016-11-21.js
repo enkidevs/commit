827f5a9bced4fd6750dd1aab9703845b61025417
mwhobrey <mwhobrey@idealss.com>
2016-11-21 19:53:14 +0300

    2016-11-21's JavaScript workout

---
+++ contributions/mwhobrey/JavaScript/Core/2016-11-21.js
@@ -0,0 +1,5 @@
+Hoisting applies only to variable declarations, not initializations
+JavaScript compilation
+Extra function arguments are undefined by default
+`console.*` in JavaScript
+Base conversion 
