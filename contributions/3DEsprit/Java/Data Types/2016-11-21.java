2c94d03d7862f5befc8799441c46377bca60d6a0
3DEsprit <vilesyn@gmail.com>
2016-11-22 01:03:16 +0300

    2016-11-21's Java workout

---
+++ contributions/3DEsprit/Java/Data Types/2016-11-21.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Multidimensional array declaration
+Equals operation on different data types
