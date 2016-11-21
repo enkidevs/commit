6dec8b17023a0cf3ae39efa78662dd93d9655671
izanagi1995 <nsurleraux@gmail.com>
2016-11-21 19:48:09 +0300

    2016-11-21's JavaScript workout

---
+++ contributions/izanagi1995/JavaScript/React/2016-11-21.js
@@ -0,0 +1,5 @@
+Self-closing tags in **JSX**
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+Using `false` in **JSX**
+Expose Component Functions in **React**
