2339c07a704135d28b22919279ec758ffa077f2b
perfecteagle <perfecteagle@gmail.com>
2016-11-21 21:04:46 +0300

    2016-11-21's Java workout

---
+++ contributions/perfecteagle/Java/Data Types/2016-11-21.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Multidimensional array declaration
+Converting a string to upper or lower case
+Equals operation on different data types
