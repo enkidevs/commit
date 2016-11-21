f56ed6211a75e7c2acd3133dea760624ed5bb4d3
Coffee0127 <coffee0127@gmail.com>
2016-11-21 18:26:11 +0300

    2016-11-20's Java workout

---
+++ contributions/Coffee0127/Java/Error Handling/2016-11-20.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Careful Numeric Data Conversions
