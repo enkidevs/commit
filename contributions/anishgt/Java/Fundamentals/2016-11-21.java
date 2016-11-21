63e4f33207559f73a4c88db3090ef1f5e9597a9f
anishgt <anishgt.1989@gmail.com>
2016-11-21 21:09:39 +0300

    2016-11-21's Java workout

---
+++ contributions/anishgt/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Metadata: setting a file's owner
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Supply `toString()` in all classes
