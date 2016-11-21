4579ad20092238e409605c9af248e08923d41261
Gerovid <gerovid@gmail.com>
2016-11-21 21:25:18 +0300

    2016-11-21's Java workout

---
+++ contributions/Gerovid/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Calling one constructor from another
