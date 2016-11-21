60bd4bc246b28d90b404a1866595a546035b5549
Edvinas <edvinas.reg@outlook.com>
2016-11-21 21:13:14 +0300

    2016-11-21's Java workout

---
+++ contributions/Edvinas /Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Java naming conventions
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Using an interface as a parameter
