4488d35243a2860f2dd2698ef95ce48add0bf7df
ErkanMDR <erkanmdr@gmail.com>
2016-11-21 19:34:14 +0300

    2016-11-21's Java workout

---
+++ contributions/ErkanMDR/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
