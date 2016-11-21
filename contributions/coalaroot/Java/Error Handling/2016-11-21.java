1b3d929ab2eadd6adb235193be1ebdc0a687b67b
coalaroot <jakubkrolak94@gmail.com>
2016-11-22 01:09:04 +0300

    2016-11-21's Java workout

---
+++ contributions/coalaroot/Java/Error Handling/2016-11-21.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Creating new Exceptions
+Most common reason behind **stack overflow** error
