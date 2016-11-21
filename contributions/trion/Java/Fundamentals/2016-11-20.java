d9e24c874955dc26dd9bec1f6af27ab9fdb0a7dd
trion <parmsingh129@yahoo.com>
2016-11-21 11:53:18 +0300

    2016-11-20's Java workout

---
+++ contributions/trion/Java/Fundamentals/2016-11-20.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Use strings in a `switch` statement
+Metadata: setting a file's owner
+Calling one constructor from another
+Using an interface as a parameter
