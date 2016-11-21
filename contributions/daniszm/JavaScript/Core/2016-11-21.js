2e60f67f02f83365289e6ae2d18a5b4253faf554
daniszm <daniszek9933@gmail.com>
2016-11-21 10:31:57 +0300

    2016-11-21's JavaScript workout

---
+++ contributions/daniszm/JavaScript/Core/2016-11-21.js
@@ -0,0 +1,5 @@
+Most efficient way to build `HTML` strings
+Apply `map` function to array items
+Prevent background image flicker in **IE**
+Detect an error type
+Getting the *real* dimensions of an image
