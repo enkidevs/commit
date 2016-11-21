12e8c01c1a3a1d9fd9e78475b0a5f773ed695118
madmuflo <alessandro.capretti@wiloca.it>
2016-11-21 23:39:07 +0300

    2016-11-21's Linux workout

---
+++ contributions/madmuflo/Linux/Terminals & Shells/2016-11-21.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Execute a command without saving it in history
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
