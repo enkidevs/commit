a950036d33f254d10b33da419208bf2ba1585bb0
backend-baron <jakubowskimarcin90@gmail.com>
2016-11-21 09:23:08 +0300

    2016-11-21's Linux workout

---
+++ contributions/backend-baron/Linux/File Management/2016-11-21.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
