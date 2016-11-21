a4fb5f3d2767081fdf93f325b1d668b3081cfb8b
clementallen <clementallen313@gmail.com>
2016-11-21 14:23:03 +0300

    2016-11-21's JavaScript workout

---
+++ contributions/clementallen/JavaScript/Functions/2016-11-21.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+IIFE pattern
+Function call method
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
