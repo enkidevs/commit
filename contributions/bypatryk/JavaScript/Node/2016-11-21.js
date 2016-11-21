a982341659d45f1c2f316868d7a6a0050cfd5a4f
bypatryk <acc@patryk.me>
2016-11-21 23:35:18 +0300

    2016-11-21's JavaScript workout

---
+++ contributions/bypatryk/JavaScript/Node/2016-11-21.js
@@ -0,0 +1,5 @@
+Http Server in **Node**
+Global Objects and Environment Variables in **Node**
+First-error callbacks in **Node**
+**Node** `os` module
+Automatic compilation for Node with **Nodemon** 
