9a12a8a14b0e1b4d406fcd6986aafedbffd43651
dewoch2 <boris.roche.971@gmail.com>
2016-11-21 11:14:37 +0300

    2016-11-21's Java workout

---
+++ contributions/dewoch2/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
