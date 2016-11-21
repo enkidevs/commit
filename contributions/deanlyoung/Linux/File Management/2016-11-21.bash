cc201cc7b6892af22178f4b77417458cd3a158fb
deanlyoung <dlyoung119@me.com>
2016-11-21 21:58:45 +0300

    2016-11-21's Linux workout

---
+++ contributions/deanlyoung/Linux/File Management/2016-11-21.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
