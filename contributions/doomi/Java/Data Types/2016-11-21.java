d0a0d838ee0984ecf7796ee6741b94477244420f
doomi <kurai7hikari@gmail.com>
2016-11-21 21:31:39 +0300

    2016-11-21's Java workout

---
+++ contributions/doomi/Java/Data Types/2016-11-21.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Converting numbers to strings
+Numeric Conversion - Widening
