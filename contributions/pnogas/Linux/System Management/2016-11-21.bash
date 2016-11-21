ac917b83680fc4fe077e45f07423586f30604eca
pnogas <paulnogas@gmail.com>
2016-11-21 23:22:10 +0300

    2016-11-21's Linux workout

---
+++ contributions/pnogas/Linux/System Management/2016-11-21.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
