df2ed82b7b4e90f48642a7d11f44a8430775e5d3
pixcompu <pixcompu@outlook.com>
2016-11-22 00:35:26 +0300

    2016-11-21's Java workout

---
+++ contributions/pixcompu/Java/Data Types/2016-11-21.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Two ways to use an `Iterator`
+Equals operation on different data types
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
