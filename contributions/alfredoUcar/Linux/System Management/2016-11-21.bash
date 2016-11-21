dafba363fc8067b05c22f4912bd295620dae5682
alfredoUcar <alfredo.ucar@gmail.com>
2016-11-21 10:12:29 +0300

    2016-11-21's Linux workout

---
+++ contributions/alfredoUcar/Linux/System Management/2016-11-21.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Find out if the system's architecture is 32 or 64 bit
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
