327c04a7096a783b748fc3aa5535499d0497ef45
viktor90 <v.georgiev90@abv.bg>
2016-11-21 09:28:23 +0300

    2016-11-21's Linux workout

---
+++ contributions/viktor90/Linux/File Management/2016-11-21.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Delete commands aliases
+Easy way of sharing files
