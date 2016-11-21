35e3ce0c4285603d2f99bf2eb8b90cfb1485b932
timgrossmann <t.grossmann1994@web.de>
2016-11-21 13:12:02 +0300

    2016-11-21's Java workout

---
+++ contributions/timgrossmann/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
