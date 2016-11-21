05a4d5d36beebb1b2ad896911bdbcd0f1dddf2d2
lemnis <lemnis.martens@gmail.com>
2016-11-21 18:39:04 +0300

    2016-11-21's Java workout

---
+++ contributions/lemnis/Java/Error Handling/2016-11-21.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Throwing an `exception`
