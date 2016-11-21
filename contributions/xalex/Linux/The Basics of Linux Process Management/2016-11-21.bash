5e5683e20628ae4be8afc18cdf6627f426b23da5
xalex <saleksey67@gmail.com>
2016-11-21 10:06:04 +0300

    2016-11-21's Linux workout

---
+++ contributions/xalex/Linux/The Basics of Linux Process Management/2016-11-21.bash
@@ -0,0 +1,6 @@
+Listing Processes Using `ps`
+The Process Tree
+The Basics of Process Ownership
+What Is An "Init System"?
+Stopping Processes With `kill` And `killall`
+Viewing Resource Usage With `top`
