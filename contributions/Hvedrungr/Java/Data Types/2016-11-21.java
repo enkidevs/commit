7a8b74b39b44dbfd3ea92eb1015e4f636c2275b5
Hvedrungr <damien.dijol@gmail.com>
2016-11-22 01:51:22 +0300

    2016-11-21's Java workout

---
+++ contributions/Hvedrungr/Java/Data Types/2016-11-21.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Converting numbers to strings
+Do not attempt comparisons with NaN
