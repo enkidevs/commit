4c4917d62ec77fddd537022b5b4a72c9aafc5d91
CycleOfTheAbsurd <alex7up42@gmail.com>
2016-11-21 20:14:12 +0300

    2016-11-21's Linux workout

---
+++ contributions/CycleOfTheAbsurd/Linux/Terminals & Shells/2016-11-21.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
+Keep useful commands in your shell history with tags
