1c37fce21eae0e24b6d6369c3af4f82fc894fba4
gabriele92 <gabrigaudenzi@gmail.com>
2016-11-22 16:32:23 +0300

    2016-11-22's Python workout

---
+++ contributions/gabriele92/Python/Data Structures/2016-11-22.py
@@ -0,0 +1,5 @@
+Following PEP 8 styling guideline.
+A simple way to select a random item from a `list/tuple` data stucture
+Special attributes of objects and classes
+Get the most of `int`s
+`bytearray` objects
