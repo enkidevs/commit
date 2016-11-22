bb2eb5b9e310352ec19dd3018e2b5fe7116cf348
bidj <enki@papito.33mail.com>
2016-11-22 04:06:13 +0300

    2016-11-21's Linux workout

---
+++ contributions/bidj/Linux/Security/2016-11-21.bash
@@ -0,0 +1,5 @@
+Security news delivered via DNS
+Extracting the `public key` from the `private key`
+Block IPs using `Fail2ban`
+Best password generation utilities
+Disabling **USB** storage devices
