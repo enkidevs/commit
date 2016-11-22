65e8dd529a34732cae98f90238dfff11d8a083b0
mrewers <marekrewers@gmail.com>
2016-11-22 07:05:11 +0300

    2016-11-21's Java workout

---
+++ contributions/mrewers/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Floating garbage and how to deal with it
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
