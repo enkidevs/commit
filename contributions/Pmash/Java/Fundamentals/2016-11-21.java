5593e9c248557ca5800b4cc1f7581a633c6750b7
Pmash <pfvd@pmash.be>
2016-11-22 23:32:54 +0300

    2016-11-21's Java workout

---
+++ contributions/Pmash/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
