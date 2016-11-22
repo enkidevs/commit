86861a4ea41a49856c579d99dffeb511f09662e6
orelmax <orelmax@gmail.com>
2016-11-22 05:01:34 +0300

    2016-11-21's JavaScript workout

---
+++ contributions/orelmax/JavaScript/Functions/2016-11-21.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function expression
+Easily generate a random `HEX` color
+Changing a functions context with `fn.call(object)`
