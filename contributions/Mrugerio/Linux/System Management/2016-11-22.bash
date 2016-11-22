011bd2f7b4af1c49d9e9603752947a9f8541c261
Mrugerio <marcosrugerio@gmail.com>
2016-11-22 17:45:41 +0300

    2016-11-22's Linux workout

---
+++ contributions/Mrugerio/Linux/System Management/2016-11-22.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
