5476e105495c5009eb7c46e8e690ac4af4e8ffaa
Predatoro01 <flori.wimmer@gmx.de>
2016-11-22 11:35:25 +0300

    2016-11-21's JavaScript workout

---
+++ contributions/Predatoro01/JavaScript/Browser APIs/2016-11-21.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Form a `URL` from its parts 
+Warn user if **Back** button is pressed
+User Timing API
+MediaDevices.getUserMedia()
