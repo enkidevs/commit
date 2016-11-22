ca965c63161042f32c67554384f4a9e00b6bee5d
nicolas-schmitt <nicolas.schmitt@live.fi>
2016-11-23 01:55:10 +0300

    2016-11-22's Linux workout

---
+++ contributions/nicolas-schmitt/Linux/Package Management/2016-11-22.bash
@@ -0,0 +1,6 @@
+Install specific version of packages
+Backporting on Debian
+
+Prevent updating a specific package in Debian systems
+Get `apt` to use a mirror / faster mirror
+Solving `BADSIG GPG` errors
