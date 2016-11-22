c7ac6977aa43403958325a70571774588af2220b
0xqookie <b456231@trbvn.com>
2016-11-22 18:41:14 +0300

    2016-11-22's Java workout

---
+++ contributions/0xqookie/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
