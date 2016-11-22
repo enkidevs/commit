099976cdf15367a9d10d1908f44755ebf6239dfa
Mass.sow <mass.sow@gmail.com>
2016-11-22 14:54:49 +0300

    2016-11-22's Java workout

---
+++ contributions/Mass.sow/Java/Classes & Objects/2016-11-22.java
@@ -0,0 +1,5 @@
+How to play a sound file in Java
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+Double Brace Initialization
+String formatting and parsing with `SimpleDateFormat`
