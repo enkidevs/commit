8ae15cba390e871a976685738165636d3afbc176
Grorel <grorel.kazui@gmail.com>
2016-11-22 10:27:30 +0300

    2016-11-22's Java workout

---
+++ contributions/Grorel/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+There is no such thing as *pass-by-reference* in Java
