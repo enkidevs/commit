ce1d3b8f79e6de679a523935f30469a9b0c0aa14
paulonotz0r <pg.g.nott@gmail.com>
2016-11-22 17:05:34 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/paulonotz0r/JavaScript/Functions/2016-11-22.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Function call method
+Move cursor at the end of text input
