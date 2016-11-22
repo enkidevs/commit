0b1eab847ea1d502c4821604bedfd3627e68e757
fthebaud <fthebaud@gmail.com>
2016-11-22 10:19:20 +0300

    2016-11-22's Java workout

---
+++ contributions/fthebaud/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using `synchronized` statements
+Supply `toString()` in all classes
+Using an interface as a parameter
