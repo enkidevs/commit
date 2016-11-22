a28122e2e08b9bb4634ac37ac0cdc4b5f1d4fd9b
bobsmith947 <lkitaev@cherrycreekschools.org>
2016-11-22 07:56:31 +0300

    2016-11-21's Java workout

---
+++ contributions/bobsmith947/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Difference between `==` and `equals()`
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+There is no such thing as *pass-by-reference* in Java
