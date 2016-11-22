9fce485e03a06fb549d5684b8bef617004cb3929
Hvedrungr <damien.dijol@gmail.com>
2016-11-22 22:24:01 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/Hvedrungr/JavaScript/Functions/2016-11-22.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function declarations vs function expressions
+Anonymous functions
+Method chaining
+Changing a functions context with `fn.call(object)`
