1f1233aac18c53d9703340b447f6c7430e4f6113
marco-zietzling <marco.zietzling@gmail.com>
2016-11-22 12:45:59 +0300

    2016-11-22's Java workout

---
+++ contributions/marco-zietzling/Java/I/O/2016-11-22.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+How to create directories in Java
+Create system file paths using the `Path` class
+Using the `OpenOption...` vararg to define file writing behavior
+List contents of a directory with `newDirectoryStream`
