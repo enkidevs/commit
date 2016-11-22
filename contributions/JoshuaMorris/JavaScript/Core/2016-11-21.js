e2a198d0a5a3d79acd3b8eccc3f9bd86fdfff5bf
JoshuaMorris <me@joshuamorris.info>
2016-11-22 07:04:28 +0300

    2016-11-21's JavaScript workout

---
+++ contributions/JoshuaMorris/JavaScript/Core/2016-11-21.js
@@ -0,0 +1,5 @@
+Setting default values with `short circuiting`
+Counting the months from zero
+`NaN` is a number
+Extra function arguments are undefined by default
+Getting the *real* dimensions of an image
