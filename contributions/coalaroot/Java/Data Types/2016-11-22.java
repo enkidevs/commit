212031c9c025793d4408ea80e5460df5fb2e6bbb
coalaroot <jakubkrolak94@gmail.com>
2016-11-23 00:55:15 +0300

    2016-11-22's Java workout

---
+++ contributions/coalaroot/Java/Data Types/2016-11-22.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+Retrieve the component type of an array
+Numeric Conversion - Widening
