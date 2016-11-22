bcd047eadb572361e3d4070d44acc7ced3917b0f
Boialex <xdr007@gmail.com>
2016-11-22 13:16:35 +0300

    2016-11-22's Java workout

---
+++ contributions/Boialex/Java/Data Types/2016-11-22.java
@@ -0,0 +1,5 @@
+Converting numbers to strings
+Converting a string to upper or lower case
+Equals operation on different data types
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
