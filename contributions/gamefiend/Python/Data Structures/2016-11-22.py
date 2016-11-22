95b99af11e5337466f8854ca8f546378505933b1
gamefiend <quinn.murphy@gmail.com>
2016-11-22 17:24:42 +0300

    2016-11-22's Python workout

---
+++ contributions/gamefiend/Python/Data Structures/2016-11-22.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+When `range` comes in handy
+Enhance your `tuple`s
+Get the most of `int`s
+Operations with `bytes` and `bytearray`
