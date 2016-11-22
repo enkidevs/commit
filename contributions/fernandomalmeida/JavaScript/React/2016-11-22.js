61be9497d0a0f606356f1ee2d205b8e27c4c9ba4
fernandomalmeida <fernando.m.al.91@gmail.com>
2016-11-22 19:45:25 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/fernandomalmeida/JavaScript/React/2016-11-22.js
@@ -0,0 +1,5 @@
+**React** in-line styles
+Pixel values in style props for **React**
+`if`-`else` statements in **JSX** and **React**
+Using `false` in **JSX**
+Expose Component Functions in **React**
