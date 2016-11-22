e4790825b69b1655583d0430c992f45b6b3031a6
0x00-0x00 <andre.marques@fatec.sp.gov.br>
2016-11-22 18:53:23 +0300

    2016-11-22's Python workout

---
+++ contributions/0x00-0x00/Python/Data Structures/2016-11-22.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Get the most of `int`s
+There is more to copying
+`bytearray` objects
+Keep things in order with `OrderedDict`
