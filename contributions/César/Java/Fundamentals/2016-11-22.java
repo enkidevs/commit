5d49a6aea528f4d7355d22d749afdc11ab378746
César <cesarbernedo@free.fr>
2016-11-23 01:39:38 +0300

    2016-11-22's Java workout

---
+++ contributions/César/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Generating random numbers
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Using bounded type parameters in generic methods
+Avoid using `static` variables
