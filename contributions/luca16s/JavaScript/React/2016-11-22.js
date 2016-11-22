8e8425351f0b447bc731817664057eb4236e88e4
luca16s <luca.gian16@outlook.com>
2016-11-22 18:29:46 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/luca16s/JavaScript/React/2016-11-22.js
@@ -0,0 +1,5 @@
+`if`-`else` statements in **JSX** and **React**
+Expose Component Functions in **React**
+Dealing with `this.props.children`
+Immutability helpers in **React**
+`ReactDOM.render` ref
