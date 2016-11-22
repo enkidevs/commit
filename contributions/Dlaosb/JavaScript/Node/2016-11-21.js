2fdf387c483c18d94af1cd6c94939e0cada16c73
Dlaosb <dlaosb@gmail.com>
2016-11-22 09:48:55 +0300

    2016-11-21's JavaScript workout

---
+++ contributions/Dlaosb/JavaScript/Node/2016-11-21.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Wrapping errors in Node.js using _node-verror_
+`process.nextTick()` callback pattern
+Manage child processes in **Node**
+`try-catch` only for **sync** code
