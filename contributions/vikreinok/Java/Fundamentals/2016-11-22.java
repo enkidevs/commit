a65eada5c1267095a273cc7bfdcfb5c107aa1867
vikreinok <vikreinok@gmail.com>
2016-11-22 17:12:43 +0300

    2016-11-22's Java workout

---
+++ contributions/vikreinok/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: creating a user-defined file attribute
+Using an interface as a parameter
