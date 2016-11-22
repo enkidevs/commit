a38f8ff42cf145735bd12e41d33e55baaca20bd8
Gierinat <michal.gierlach@gmail.com>
2016-11-22 16:30:32 +0300

    2016-11-22's Java workout

---
+++ contributions/Gierinat/Java/Classes & Objects/2016-11-22.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Template methods in abstract classes
+The `abstract` keyword
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
