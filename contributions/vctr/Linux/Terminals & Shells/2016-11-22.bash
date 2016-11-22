6ebd8fb4ba9749f37a453550788050865a3b674b
vctr <unvictor@gmail.com>
2016-11-22 18:14:21 +0300

    2016-11-22's Linux workout

---
+++ contributions/vctr/Linux/Terminals & Shells/2016-11-22.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Find any Unix / Linux command
+Random password generator
+Terminal incognito mode
