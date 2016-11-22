08422e5393496fa70550decd517cb74055d4ccd6
Norbzilla <agutsch22@gmail.com>
2016-11-23 00:56:13 +0300

    2016-11-22's Linux workout

---
+++ contributions/Norbzilla/Linux/Networking/2016-11-22.bash
@@ -0,0 +1,5 @@
+How to clear `iptables` rules
+Limit the `wget` download rate
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
+Short Introduction to `Wget` 
