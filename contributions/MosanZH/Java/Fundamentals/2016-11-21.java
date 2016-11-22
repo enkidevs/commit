d610cf84a1cfd85ecdb0fff2e7e7315b8bbe14fc
MosanZH <mosan_zh@163.com>
2016-11-22 03:32:17 +0300

    2016-11-21's Java workout

---
+++ contributions/MosanZH/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
+Using an interface as a parameter
