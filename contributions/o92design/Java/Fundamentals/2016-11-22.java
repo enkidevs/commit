53b6935469fb33a6a79dd570f04eed538ce007a9
o92design <o92design@gmail.com>
2016-11-22 18:50:26 +0300

    2016-11-22's Java workout

---
+++ contributions/o92design/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+`Autoboxing` and `Unboxing`
