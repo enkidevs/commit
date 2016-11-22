3ad1029127825ff1f3fd9c90f52ee5b0d29832e6
yanivok <okevyaniv@gmail.com>
2016-11-22 12:56:14 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/yanivok/JavaScript/Functions/2016-11-22.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
