48382082d9d0e80e9b2f2233dcb5c4487fd74e69
aliaksei135 <aliakseipilko1@gmail.com>
2016-11-22 09:58:28 +0300

    2016-11-22's Java workout

---
+++ contributions/aliaksei135/Java/I/O/2016-11-22.java
@@ -0,0 +1,5 @@
+Use `File.separator` while dealing with file paths
+`System.out` vs `System.err`
+Reading and writing text files
+Checking for the existence of files and directories
+Listing a file system's root directories
