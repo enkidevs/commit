d1fe75b9f6d43808ccfd2ddf5dd24a6dc2a3bb43
b-diddy <bendikflotre@gmail.com>
2016-11-22 15:54:32 +0300

    2016-11-22's Linux workout

---
+++ contributions/b-diddy/Linux/System Management/2016-11-22.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
