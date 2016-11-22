a98524816656b97994617960f35e04d5e37cc741
MrBerdill <cbarry123567@gmail.com>
2016-11-22 05:15:27 +0300

    2016-11-21's Java workout

---
+++ contributions/MrBerdill/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The trade-off between short and descriptive names
+Constant notation
+Avoid using `static` variables
+Using an interface as a parameter
