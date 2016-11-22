f9391b541eb369265c2dc208709d4c5926dac9f0
jeffmjoy <jeffmjoy@gmail.com>
2016-11-22 07:44:59 +0300

    2016-11-21's Java workout

---
+++ contributions/jeffmjoy/Java/Data Types/2016-11-21.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
