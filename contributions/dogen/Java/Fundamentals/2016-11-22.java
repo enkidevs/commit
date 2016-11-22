b417496da240a04c0b1be0d7eaa52be437b882fb
dogen <samuel.sh79@gmail.com>
2016-11-22 21:28:13 +0300

    2016-11-22's Java workout

---
+++ contributions/dogen/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Ternary operator
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+How range operations work
