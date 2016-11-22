d691e4a4d1c2e0078373bdbebb8b2a04fd184389
vpagliari <vincenzo.pagliari@gmail.com>
2016-11-22 16:20:48 +0300

    2016-11-22's Linux workout

---
+++ contributions/vpagliari/Linux/Networking/2016-11-22.bash
@@ -0,0 +1,5 @@
+Find services running on your host
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
+Make use of `netstat`
