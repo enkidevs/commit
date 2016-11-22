520123a1d6c8423d70d0654aa185b329320773bc
Eldergeek <david@geekguytech.com>
2016-11-22 21:03:56 +0300

    2016-11-22's Linux workout

---
+++ contributions/Eldergeek/Linux/System Management/2016-11-22.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+CPU benchmark with `dd`
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
