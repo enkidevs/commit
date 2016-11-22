4eef4ac9656b3d46af4f7b3861aa050fdf6a815d
techliam <liamwilson127@gmail.com>
2016-11-23 01:18:00 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/techliam/JavaScript/Functions/2016-11-22.js
@@ -0,0 +1,5 @@
+Function constructor vs. function expression
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+Easily generate a random `HEX` color
+Social sharing without widgets
