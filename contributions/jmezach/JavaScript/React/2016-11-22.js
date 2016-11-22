07316f793d01f5dec76a9877dee95691707a380d
jmezach <jonathanmezach@gmail.com>
2016-11-22 12:09:36 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/jmezach/JavaScript/React/2016-11-22.js
@@ -0,0 +1,6 @@
+Functional Stateless Components in React
+`null` value for *Controlled Components* in **React**
+Props in getInitialState Is an        
+Anti-Pattern
+Expose Component Functions in **React**
+Validate for required props
