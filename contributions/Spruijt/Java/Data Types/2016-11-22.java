c925711a2ef0fe29855ad3b529ae315014cc9cfc
Spruijt <hubertspruijt@hotmail.com>
2016-11-22 20:15:16 +0300

    2016-11-22's Java workout

---
+++ contributions/Spruijt/Java/Data Types/2016-11-22.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Multidimensional array declaration
+Finding a substring in a string
+Equals operation on different data types
+Do not attempt comparisons with NaN
