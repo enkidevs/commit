9d05f34b66e8cd729dbc3d3fbefe0134c1ea5ed7
bjarneh <bjarne.heden@gmail.com>
2016-11-23 02:01:40 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/bjarneh/JavaScript/React/2016-11-22.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Load Initial Data via AJAX
+Clone Elements in **React**
+Default values for props
