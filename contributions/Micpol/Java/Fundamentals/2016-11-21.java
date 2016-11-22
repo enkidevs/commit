5fa2225e80b286da9d8115cf887004ac87a5f266
Micpol <michal.polewczak@gmail.com>
2016-11-22 14:26:20 +0300

    2016-11-21's Java workout

---
+++ contributions/Micpol/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Altering format string output by changing a format specifier's `argument_index`
+Calling one constructor from another
+Always specify type parameter
+Do not perform bitwise and arithmetic operations on the same data
