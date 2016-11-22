01a80c44be7684b1c7f071e0a2c456f198d044be
BigBearDAC <flidnibble@gmail.com>
2016-11-22 10:17:29 +0300

    2016-11-21's Java workout

---
+++ contributions/BigBearDAC/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use meaningful names
+The trade-off between short and descriptive names
+Java naming conventions
+How range operations work
