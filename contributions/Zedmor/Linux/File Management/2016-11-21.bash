7d61ad46bf7535f583f1161dc5bf7e4d68b2041c
Zedmor <zedmor@mirpokera.com>
2016-11-22 07:55:54 +0300

    2016-11-21's Linux workout

---
+++ contributions/Zedmor/Linux/File Management/2016-11-21.bash
@@ -0,0 +1,7 @@
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
