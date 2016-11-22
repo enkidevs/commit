da97b6d2e2d6921b99248b1cd4dbcb1da3f1d8be
tomcbe <thomascbernhart@gmail.com>
2016-11-22 09:00:03 +0300

    2016-11-21's Java workout

---
+++ contributions/tomcbe/Java/Data Types/2016-11-21.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Equals operation on different data types
