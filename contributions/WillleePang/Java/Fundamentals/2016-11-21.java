0c18664d7c6be0fadc8b8ebf33cd5a2d95a04b6f
WillleePang <pangwilllee@hotmail.com>
2016-11-22 04:28:16 +0300

    2016-11-21's Java workout

---
+++ contributions/WillleePang/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Generating random numbers
+Altering format string output by changing a format specifier's `argument_index`
+Using `Printstream.format` to format a string before printing
+Using `synchronized` statements
