11c0d9290643dadb15f159ca9019a6b1ac88163f
viliusz <enki@gmn.lt>
2016-11-22 22:49:45 +0300

    2016-11-22's Linux workout

---
+++ contributions/viliusz/Linux/Networking/2016-11-22.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
+Staying connected remotely via SSH without an interactive shell
