464223fce35184d909d5700e5a1f311f2dd6efe8
tata <tata@metacortex.hu>
2016-11-22 10:48:46 +0300

    2016-11-21's Linux workout

---
+++ contributions/tata/Linux/File Management/2016-11-21.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Monitor the progress of data through a pipe with `pv`
