c1434f86cd169c0adc2c664fad4cdf1411e85084
Coffee0127 <coffee0127@gmail.com>
2016-11-22 14:49:14 +0300

    2016-11-21's Java workout

---
+++ contributions/Coffee0127/Java/Data Types/2016-11-21.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Converting a string to upper or lower case
+`StringBuffer` vs `StringBuilder`
+Do not attempt comparisons with NaN
