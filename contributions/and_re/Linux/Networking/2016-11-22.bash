99bce9e51cb76d32a6737332e07fe33a4a03d98c
and_re <andre2go@gmail.com>
2016-11-22 10:15:14 +0300

    2016-11-22's Linux workout

---
+++ contributions/and_re/Linux/Networking/2016-11-22.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Useful ssh client optimizations 
+Get real network statistics with `slurm`
+Limit the `wget` download rate
+Staying connected remotely via SSH without an interactive shell
