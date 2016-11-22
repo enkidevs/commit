cbfc35b9c30c7df4d556eb597c296b1e99a2b335
maianhania <maianhania@yahoo.com>
2016-11-22 12:29:53 +0300

    2016-11-22's Java workout

---
+++ contributions/maianhania/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+Supply `toString()` in all classes
