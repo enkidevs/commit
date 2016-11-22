cade0af374f6539bc455164c16a9f680e8548cf7
gardir <gardiz@gmail.com>
2016-11-22 20:15:39 +0300

    2016-11-22's Linux workout

---
+++ contributions/gardir/Linux/File Management/2016-11-22.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
