5d367709ced422a51ce9f6fbc8204627a93a006f
Logan <logan@commonbond.co>
2016-11-22 17:13:55 +0300

    2016-11-22's Java workout

---
+++ contributions/Logan/Java/I/O/2016-11-22.java
@@ -0,0 +1,5 @@
+System independent path specification
+`System.out` vs `System.err`
+How to create directories in Java
+Checking for the existence of files and directories
+Deleting files and directories
