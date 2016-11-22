765dd63c9b67c625c007a28ed462af748c4a6a50
pseguel <pseguel@cisco.com>
2016-11-22 15:24:36 +0300

    2016-11-22's Java workout

---
+++ contributions/pseguel/Java/Data Types/2016-11-22.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Finding a substring in a string
