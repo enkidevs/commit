757b87bfed81ace8fb52652adfeb10038f66b334
manwitap <manwitap@gmail.com>
2016-11-23 02:08:28 +0300

    2016-11-22's Java workout

---
+++ contributions/manwitap/Java/I/O/2016-11-22.java
@@ -0,0 +1,5 @@
+System independent path specification
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+Listing a file system's root directories
