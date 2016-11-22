5c28817bba02e536195bb712d8ec7d4c42529d7b
jfftck <jeffrey@jeffreytackett.us>
2016-11-23 02:41:52 +0300

    2016-11-22's Java workout

---
+++ contributions/jfftck/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
