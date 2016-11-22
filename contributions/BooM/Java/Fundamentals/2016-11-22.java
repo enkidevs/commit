bab272f53ecbe6129ccfd0928e6a45db145e29d2
BooM <tararuj.michal@gmail.com>
2016-11-22 09:34:42 +0300

    2016-11-22's Java workout

---
+++ contributions/BooM/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
