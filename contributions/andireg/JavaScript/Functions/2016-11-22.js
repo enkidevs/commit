dd740a3dc52dc897cca1dab354cffbb1e857fd93
andireg <andreas.regner@gmail.com>
2016-11-22 20:54:57 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/andireg/JavaScript/Functions/2016-11-22.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Function constructor vs. function expression
+Difference between **.call** and **.apply** methods
+IIFE pattern
+Social sharing without widgets
