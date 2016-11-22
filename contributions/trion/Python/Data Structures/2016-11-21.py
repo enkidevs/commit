ac96c51adb355d2b5193866dd9d01dc2c451a7d9
trion <parmsingh129@yahoo.com>
2016-11-22 10:41:38 +0300

    2016-11-21's Python workout

---
+++ contributions/trion/Python/Data Structures/2016-11-21.py
@@ -0,0 +1,5 @@
+Following PEP 8 styling guideline.
+A simple way to select a random item from a `list/tuple` data stucture
+Your own Python `calendar` 
+Keep things in order with `OrderedDict`
+Binary record packing with `bytearray`
