6c55899273e0529f1d6c604107fefd109d7693e0
motabass <markus.mohoritsch@gmx.at>
2016-11-22 23:14:05 +0300

    2016-11-22's Java workout

---
+++ contributions/motabass/Java/I/O/2016-11-22.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+How to create directories in Java
+Reading and writing text files
+Checking for the existence of files and directories
+Listing a file system's root directories
