79bf248e9360af106c6450ae5319397f2f94e47d
Blank113 <hbaldwin.services+1@gmail.com>
2016-11-22 04:32:19 +0300

    2016-11-21's Java workout

---
+++ contributions/Blank113/Java/Error Handling/2016-11-21.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
+Careful Numeric Data Conversions
