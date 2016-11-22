9b72a9e236bba0ee3e46a08c52b1656f9990fb9f
Wufe <wufe92@gmail.com>
2016-11-22 13:23:16 +0300

    2016-11-21's JavaScript workout

---
+++ contributions/Wufe/JavaScript/Functions/2016-11-21.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Difference between **.call** and **.apply** methods
+Functions can be declared after use
+`.bind()` 
+Easily generate a random `HEX` color
