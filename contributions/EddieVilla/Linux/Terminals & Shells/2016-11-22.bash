b6612cca47b2360f2d3a5a068da85801fbbc9201
EddieVilla <edward.villasenor@gmail.com>
2016-11-22 18:22:29 +0300

    2016-11-22's Linux workout

---
+++ contributions/EddieVilla/Linux/Terminals & Shells/2016-11-22.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Terminal based browser
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
