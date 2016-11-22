6574e92eb9830449f0e076864d97f316f3b24c64
Thommi <thomasandre84@gmail.com>
2016-11-22 22:01:17 +0300

    2016-11-22's Java workout

---
+++ contributions/Thommi/Java/I/O/2016-11-22.java
@@ -0,0 +1,5 @@
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Retrieving file store attributes
+Listing a file system's root directories
+List contents of a directory with `newDirectoryStream`
