5ecef6e25e6312d40793a0f9e1604fb9f11b0922
davex <dabuck@libero.it>
2016-11-22 15:48:51 +0300

    2016-11-22's Linux workout

---
+++ contributions/davex/Linux/File Management/2016-11-22.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
