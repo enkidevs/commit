e69bafc51acf5e99e3c0f193c449e14ab3a259fd
TiagoKuste <tiago.tagcosta@gmail.com>
2016-11-22 20:07:11 +0300

    2016-11-22's Java workout

---
+++ contributions/TiagoKuste/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Generating random numbers
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
