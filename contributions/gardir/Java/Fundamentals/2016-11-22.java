9f6c753f873cd8b6d8ad7a0bd1723308025229c2
gardir <gardiz@gmail.com>
2016-11-22 20:09:20 +0300

    2016-11-22's Java workout

---
+++ contributions/gardir/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using `synchronized` statements
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
+Using an interface as a parameter
