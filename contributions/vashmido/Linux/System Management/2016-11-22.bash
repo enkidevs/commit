ee84c2160404fe6d5868f96f945475eaa7bf583f
vashmido <hymura@gmail.com>
2016-11-22 18:44:36 +0300

    2016-11-22's Linux workout

---
+++ contributions/vashmido/Linux/System Management/2016-11-22.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
