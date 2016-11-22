daa9da6067ce11544297cf32c53c5a5853270c78
kubik <lkubic@poczta.onet.pl>
2016-11-23 01:23:17 +0300

    2016-11-22's Java workout

---
+++ contributions/kubik/Java/Java 8/2016-11-22.java
@@ -0,0 +1,5 @@
+Java 8 Lambda expressions
+Using `Arrays.parallelSort` 
+Use `Optional(s)` type in order to avoid `NullPointerException(s)`
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
