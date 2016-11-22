9b7324c55a8179dbec9edab05b6ad3161767a066
yashbmewada <ybmewada@gmail.com>
2016-11-22 12:50:18 +0300

    2016-11-21's Java workout

---
+++ contributions/yashbmewada/Java/Java 8/2016-11-21.java
@@ -0,0 +1,5 @@
+How to check for static dependencies of applications and libraries from the command line.
+Control OS `Process`es in `Java8`
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
+`JJS` - Oracle Nashorn, newest JavaScript Engine
