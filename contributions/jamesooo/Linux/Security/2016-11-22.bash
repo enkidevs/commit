1e3b4cbe14bed74728e7b5f37756fa3ede4605b0
jamesooo <onealjames08@gmail.com>
2016-11-22 22:40:21 +0300

    2016-11-22's Linux workout

---
+++ contributions/jamesooo/Linux/Security/2016-11-22.bash
@@ -0,0 +1,5 @@
+Extracting the `public key` from the `private key`
+Track SSH log-in attempts
+Best password generation utilities
+Disabling **USB** storage devices
+Private key encryption with `OpenSSL`
