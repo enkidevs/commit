aa41bb203ba3495e24c132cab5027f1563ff30c2
saroffrahul <saroffrahul@gmail.com>
2016-11-22 15:20:38 +0300

    2016-11-21's Java workout

---
+++ contributions/saroffrahul/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Use meaningful names
+Use `DecimalFormat` class to format numbers
+Always specify type parameter
