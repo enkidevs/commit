8c826cb13196d1d58e9dac84e432ea5cdc01dea2
Calim50 <bastien.sebire@gmail.com>
2016-11-22 15:41:00 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/Calim50/JavaScript/Node/2016-11-22.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Global Objects and Environment Variables in **Node**
+First-error callbacks in **Node**
+How to debug Node.js
+Use conventional property names for error handling in Node.js
