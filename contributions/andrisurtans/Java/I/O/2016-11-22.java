11dee26e09b4a0d0c7bafd0c0454d5d58dcdbb2b
andrisurtans <andris.urtans@gmail.com>
2016-11-22 20:53:41 +0300

    2016-11-22's Java workout

---
+++ contributions/andrisurtans/Java/I/O/2016-11-22.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Checking for the existence of files and directories
+Retrieving file store attributes
+Listing a file system's root directories
+The `Console` class
