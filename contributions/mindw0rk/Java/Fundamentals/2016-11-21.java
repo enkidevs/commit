b6dc94743b595ee44d36dfc486392081522c80d6
mindw0rk <mindvvorc@gmail.com>
2016-11-22 08:21:59 +0300

    2016-11-21's Java workout

---
+++ contributions/mindw0rk/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+Limit Accessibility of `Fields`
+Avoid using `static` variables
