486fae3a7ecf4234048173a770f4672bf3a0e08f
FuFaijter <maty.infante@gmail.com>
2016-11-22 16:38:42 +0300

    2016-11-22's Java workout

---
+++ contributions/FuFaijter/Java/I/O/2016-11-22.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Listing a file system's root directories
+Deleting files and directories
