534fa9f62c65404f9ab7a9c183086332626bfbfc
davex <dabuck@libero.it>
2016-11-22 15:47:21 +0300

    2016-11-22's Java workout

---
+++ contributions/davex/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
