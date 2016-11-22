de6e484382e731a2f5f6d1e69ab8e97a1d24d2b8
Framstag <tim@framstag.com>
2016-11-22 10:13:04 +0300

    2016-11-22's Java workout

---
+++ contributions/Framstag/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
