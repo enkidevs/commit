638ce72577250c78212a9ff534ec98c46f404637
chhsiao90 <chhsiao90@gmail.com>
2016-11-23 02:29:11 +0300

    2016-11-22's Git workout

---
+++ contributions/chhsiao90/Git/Essentials/2016-11-22.bash
@@ -0,0 +1,5 @@
+Pushing tags to a server
+Using aliases for git commands
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: fetching and pushing
