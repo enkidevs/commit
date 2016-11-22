da62a27cb5c91badc3e9018aa455bba1300df918
Miterion <heiko.carrasco@yahoo.com>
2016-11-22 13:14:57 +0300

    2016-11-22's Linux workout

---
+++ contributions/Miterion/Linux/Package Management/2016-11-22.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Prevent updating a specific package in Debian systems
+Using `PPAs`
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
