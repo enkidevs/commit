09facca41a6f106a54492689d8100e55a7b5e837
ronalddas <ronald1das@gmail.com>
2016-11-22 05:17:28 +0300

    2016-11-21's Java workout

---
+++ contributions/ronalddas/Java/Data Types/2016-11-21.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Multidimensional array declaration
+Finding a substring in a string
+`StringBuffer` vs `StringBuilder`
