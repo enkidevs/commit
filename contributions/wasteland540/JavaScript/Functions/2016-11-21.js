6faa9f224e01a26cc2e06affd60c695de4d88d5c
wasteland540 <m_m_elz@web.de>
2016-11-22 11:00:12 +0300

    2016-11-21's JavaScript workout

---
+++ contributions/wasteland540/JavaScript/Functions/2016-11-21.js
@@ -0,0 +1,5 @@
+Function declarations vs function expressions
+Function constructor vs. function expression
+Functions can be declared after use
+Anonymous functions
+Check if a document is done loading
