02fbd603940b829238916b9dbd2564a4a707be81
prathak07 <prathak07@gmail.com>
2016-11-23 02:21:41 +0300

    2016-11-22's Java workout

---
+++ contributions/prathak07/Java/Error Handling/2016-11-22.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Converting stack trace to a string
+Creating new Exceptions
+Throwing an `exception`
