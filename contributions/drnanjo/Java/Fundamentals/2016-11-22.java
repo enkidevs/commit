5dada06063b5616cdb3396ee70693fd34607830a
drnanjo <drnanjo@aol.com>
2016-11-22 21:24:58 +0300

    2016-11-22's Java workout

---
+++ contributions/drnanjo/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Generating random numbers
+The trade-off between short and descriptive names
+Java naming conventions
+Using `Printstream.format` to format a string before printing
+Always specify type parameter
