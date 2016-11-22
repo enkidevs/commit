05fe3fbac9996c2a5fd7da64204b7604171e7806
wp-lai <techwplai@gmail.com>
2016-11-22 05:59:23 +0300

    2016-11-21's Linux workout

---
+++ contributions/wp-lai/Linux/Terminals & Shells/2016-11-21.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Terminal incognito mode
+Adding directories to your `$PATH`
+Retrieve previous arguments
