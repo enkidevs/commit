a2c511cfeefb8eee2faab3d0daee59b6a6f7a4d6
stinbetz <justin.betz@gmail.com>
2016-11-22 05:30:38 +0300

    2016-11-21's Python workout

---
+++ contributions/stinbetz/Python/Data Structures/2016-11-21.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Get more with `collections`!
+Get the most of `float`s
+Looping techniques
+Keep things in order with `OrderedDict`
