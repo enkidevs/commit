ac1c680b904a999ffc4fdd13980cf72fbe5b19f8
pwinty <peterwinteriii@gmail.com>
2016-11-22 18:02:01 +0300

    2016-11-22's Linux workout

---
+++ contributions/pwinty/Linux/Networking/2016-11-22.bash
@@ -0,0 +1,5 @@
+Stress testing with `ab`
+Useful ssh client optimizations 
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
