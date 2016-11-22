940d9a969516422dadfde8fa0e7b225d4a070e47
PeterLustig1994 <patrick_buehler_94@web.de>
2016-11-22 14:48:18 +0300

    2016-11-21's Java workout

---
+++ contributions/PeterLustig1994/Java/Data Types/2016-11-21.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Converting numbers to strings
+Converting a string to upper or lower case
+Numeric Conversion - Widening
