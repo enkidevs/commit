38ad36165bb31a8096bd29cd6437a88daa4e6bbb
tylerthorin <tthorin@live.com>
2016-11-22 10:10:56 +0300

    2016-11-21's Java workout

---
+++ contributions/tylerthorin/Java/Fundamentals/2016-11-21.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Common mistake on switch statements
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+There is no such thing as *pass-by-reference* in Java
