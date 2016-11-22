7f201ed5784151059e2fe272efcc9d64ede79d96
nimrodo <nimrodo@gmail.com>
2016-11-22 22:04:30 +0300

    2016-11-22's Java workout

---
+++ contributions/nimrodo/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Java naming conventions
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
+How range operations work
