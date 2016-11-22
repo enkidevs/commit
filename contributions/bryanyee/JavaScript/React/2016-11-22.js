15a82fc3e6c3b87e0b80d4d1ea89a067c6386855
bryanyee <yee.bryan.c@gmail.com>
2016-11-22 19:56:36 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/bryanyee/JavaScript/React/2016-11-22.js
@@ -0,0 +1,7 @@
+Props in getInitialState Is an        
+Anti-Pattern
+componentWillReceiveProps Not Triggered After Mounting
+
+Load Initial Data via AJAX
+`ReactDOM.render` ref
+Enhanced Prop Validation 
