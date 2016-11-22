24e03e44f3f44c708f8a4045a5c2c2081ca3f214
TomRaven <16lazart@elmbrookstudents.org>
2016-11-22 16:54:55 +0300

    2016-11-22's Java workout

---
+++ contributions/TomRaven/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Avoid using `static` variables
