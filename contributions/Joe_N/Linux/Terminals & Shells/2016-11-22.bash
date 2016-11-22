4f3180f46a79ea537e529fe5c075b712e1fd9414
Joe_N <jocelyn.nourtier@gmail.com>
2016-11-22 12:28:59 +0300

    2016-11-22's Linux workout

---
+++ contributions/Joe_N/Linux/Terminals & Shells/2016-11-22.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Find any Unix / Linux command
+Quick `bash` shortcuts 
+Adding directories to your `$PATH`
