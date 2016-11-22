d495c3c9e0f42c1d853a4adb7886828fe20e56e8
kalin <kalin.t.iliev@gmail.com>
2016-11-22 22:39:46 +0300

    2016-11-22's Linux workout

---
+++ contributions/kalin/Linux/System Management/2016-11-22.bash
@@ -0,0 +1,6 @@
+Test disk speed
+Detect your linux distribution
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Executing scripts on system boot using `systemd`
