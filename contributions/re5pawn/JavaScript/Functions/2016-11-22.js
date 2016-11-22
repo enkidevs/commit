62d0b5ff23827e1080c2fcb6761a10cfcca974ad
re5pawn <s4ichulin@gmail.com>
2016-11-22 22:00:57 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/re5pawn/JavaScript/Functions/2016-11-22.js
@@ -0,0 +1,5 @@
+Functions can be declared after use
+Easily generate a random `HEX` color
+Function call method
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
