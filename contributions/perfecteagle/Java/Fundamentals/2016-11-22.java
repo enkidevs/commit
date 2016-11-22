58c3d7b3082d97024dadbc0081682ad91e7fb033
perfecteagle <perfecteagle@gmail.com>
2016-11-22 19:15:55 +0300

    2016-11-22's Java workout

---
+++ contributions/perfecteagle/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Generating random numbers
+The trade-off between short and descriptive names
+Use `DecimalFormat` class to format numbers
