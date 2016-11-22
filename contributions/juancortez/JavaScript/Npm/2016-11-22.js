183e1b4b806b241cbf01c0a611392428bbbf6404
juancortez <juan_cortez@utexas.edu>
2016-11-23 01:13:39 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/juancortez/JavaScript/Npm/2016-11-22.js
@@ -0,0 +1,5 @@
+Bump package version in npm
+npm can handle multiple versions of a dependency
+`npm`  as an alternative to Gulp
+`npm` verifies `sha1` hashes of packages
+`optionalDependencies` in npm
