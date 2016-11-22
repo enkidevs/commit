2a5ae53438ed970e2873d689ca514ba10265b8e0
Alanbato <alanvelasco.a@gmail.com>
2016-11-22 03:27:08 +0300

    2016-11-21's Linux workout

---
+++ contributions/Alanbato/Linux/Terminals & Shells/2016-11-21.bash
@@ -0,0 +1,5 @@
+Repeating commands with `watch`
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Useful aliasing in bash
+Sequential execution using the `;` statement separator
