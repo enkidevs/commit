bcfd098ac420df5838faffc1e45db73a4e20b937
nfort <laikisounds@gmail.com>
2016-11-22 07:05:37 +0300

    2016-11-21's Git workout

---
+++ contributions/nfort/Git/Tools/2016-11-21.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Diff images using *ExifTool*
+Specify a commit by its ancestry
+Useful stashing options
+Remember the results of previous hunk conflicts
