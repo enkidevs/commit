1043854e84c82a86fee1b193ea97a141fbc634a1
CristopherH95 <doughboyswwi@gmail.com>
2016-11-23 00:00:18 +0300

    2016-11-22's Linux workout

---
+++ contributions/CristopherH95/Linux/System Management/2016-11-22.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Commands to shutdown or restart the system
+System uptime
