1f084aa1520762f6fbdbc761a39f450ea3bcb033
Lizander <kevin9075@gmail.com>
2016-11-22 17:53:08 +0300

    2016-11-22's Java workout

---
+++ contributions/Lizander/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Always specify type parameter
+There is no such thing as *pass-by-reference* in Java
