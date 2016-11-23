022ed06c99bfc44baac219761dd77b0bac54cc1d
kubik <lkubic@poczta.onet.pl>
2016-11-23 10:41:35 +0300

    2016-11-23's Java workout

---
+++ contributions/kubik/Java/I/O/2016-11-23.java
@@ -0,0 +1,5 @@
+`System.out` vs `System.err`
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Deleting files and directories
