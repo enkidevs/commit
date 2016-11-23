d0b4be37eb9bd16bd4279bc28f0983e29236bd20
davex <dabuck@libero.it>
2016-11-24 01:38:16 +0300

    2016-11-23's Java workout

---
+++ contributions/davex/Java/Annotations/2016-11-23.java
@@ -0,0 +1,5 @@
+Use `@Ignore` to disable JUnit tests
+Using the `@Deprecated` annotation
+Suppressing warnings using `@SuppressWarnings`
+Including annotations in Javadoc
+Restricting an annotation's application by using `@Target`
