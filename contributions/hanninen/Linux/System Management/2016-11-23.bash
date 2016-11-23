44f10b00476c0f00108fe46a6cfa9682dce5467d
hanninen <aki@hanninen.net>
2016-11-23 10:10:34 +0300

    2016-11-23's Linux workout

---
+++ contributions/hanninen/Linux/System Management/2016-11-23.bash
@@ -0,0 +1,5 @@
+Test disk speed
+List currently logged in users
+Find out if the system's architecture is 32 or 64 bit
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
