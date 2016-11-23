0c48ba17deed18c5e1508a7f8e9fc0059a36a0c3
johnwr <johnwr_enki@wraalsen.no>
2016-11-23 21:44:36 +0300

    2016-11-23's Java workout

---
+++ contributions/johnwr/Java/Error Handling/2016-11-23.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+The distinction between checked and unchecked exceptions
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
