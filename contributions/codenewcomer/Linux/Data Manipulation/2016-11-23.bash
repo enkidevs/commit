434e254d5cb271117bf17cae1b7ef8e2a5533f5c
codenewcomer <codenewcomer@gmail.com>
2016-11-23 08:59:12 +0300

    2016-11-23's Linux workout

---
+++ contributions/codenewcomer/Linux/Data Manipulation/2016-11-23.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+How to analyse a server's log files
+Using `lynx` to convert HTML to text
+Clean configuration files using `sed`
+Wrap text with `fold`
