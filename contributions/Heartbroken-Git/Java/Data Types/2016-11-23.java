902eab48ce1d05c0eb84c587749fb0a5d02f85cc
Heartbroken-Git <corentin196@gmail.com>
2016-11-23 21:08:30 +0300

    2016-11-23's Java workout

---
+++ contributions/Heartbroken-Git/Java/Data Types/2016-11-23.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Converting a string to upper or lower case
+Equals operation on different data types
