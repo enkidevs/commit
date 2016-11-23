d9a2ce3ec579c5db79f6b9e3ad744215c9fd8785
Evin1- <edwin.hdz.12.0@gmail.com>
2016-11-23 07:51:18 +0300

    2016-11-22's Java workout

---
+++ contributions/Evin1-/Java/Error Handling/2016-11-22.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Use try-with-resources instead of `finally`
+Throwing an `exception`
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
