199ff96dc7ec535960a8fef62d65f6b98ae4bae4
tarb <rtarbath@gmail.com>
2016-11-23 04:07:52 +0300

    2016-11-22's Java workout

---
+++ contributions/tarb/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
