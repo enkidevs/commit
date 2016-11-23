e4823cc9e2d48dd515c9e49d109cebf128361ba6
Milchm4nn <luca.hengst@googlemail.com>
2016-11-23 08:31:17 +0300

    2016-11-23's Java workout

---
+++ contributions/Milchm4nn/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
