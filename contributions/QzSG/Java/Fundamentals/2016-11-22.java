4c6ad52dd50430142ecb2ff08176716ef20c7e88
QzSG <adrianquartx@gmail.com>
2016-11-23 06:57:27 +0300

    2016-11-22's Java workout

---
+++ contributions/QzSG/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Writing generic methods
+Calling one constructor from another
+Detect or prevent integer overflow
+Avoid using `static` variables
