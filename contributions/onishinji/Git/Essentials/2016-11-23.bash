6f833562ab6bcc23e48a1a01caf518bf16a01664
onishinji <onishinji@gmail.com>
2016-11-23 10:11:24 +0300

    2016-11-23's Git workout

---
+++ contributions/onishinji/Git/Essentials/2016-11-23.bash
@@ -0,0 +1,5 @@
+Using aliases for git commands
+You can use git offline!
+How to set your username and email
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
