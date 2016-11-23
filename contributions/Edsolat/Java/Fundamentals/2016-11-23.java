0785130b8b69fb0ba9528edf713dd669e18503b5
Edsolat <edouard.sabourdy@laposte.net>
2016-11-23 14:55:09 +0300

    2016-11-23's Java workout

---
+++ contributions/Edsolat/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Avoid using `static` variables
