95d0d3f336edd8f03def2469c6b8d59ae5b6fc3a
78juli <j78juli@gmail.com>
2016-11-23 03:03:24 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/78juli/JavaScript/Node/2016-11-22.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Asynchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+`uncaughtException` listener in Node.js
+Middleware/pipeline design pattern
