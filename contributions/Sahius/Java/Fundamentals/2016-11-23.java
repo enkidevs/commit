b49bdae9526853ddd52411e0711f6b2d7fc50798
Sahius <danger.internet@hotmail.fr>
2016-11-23 13:21:41 +0300

    2016-11-23's Java workout

---
+++ contributions/Sahius/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Constant notation
