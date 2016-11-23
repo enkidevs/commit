4f8288487e70fd96729515869197d5e3d53eb440
Sagheeton <sagheeton@gmail.com>
2016-11-23 07:14:47 +0300

    2016-11-23's Java workout

---
+++ contributions/Sagheeton/Java/Data Types/2016-11-23.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Converting numbers to strings
+Converting a string to upper or lower case
+Equals operation on different data types
