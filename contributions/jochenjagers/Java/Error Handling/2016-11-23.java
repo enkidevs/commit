304e88df606ce00de6e7976a591a7239479914dd
jochenjagers <jochen.jaegers@gmail.com>
2016-11-23 10:12:08 +0300

    2016-11-23's Java workout

---
+++ contributions/jochenjagers/Java/Error Handling/2016-11-23.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Use try-with-resources instead of `finally`
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Checking a double for `NaN` value
