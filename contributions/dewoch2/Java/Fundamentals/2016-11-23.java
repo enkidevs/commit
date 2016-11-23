1e1a13c4b02a2124cc913b04d39652cf3edc0e55
dewoch2 <boris.roche.971@gmail.com>
2016-11-23 11:20:58 +0300

    2016-11-23's Java workout

---
+++ contributions/dewoch2/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Writing generic methods
+Use `DecimalFormat` class to format numbers
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
