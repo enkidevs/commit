6556e3b8a7eafc446b252866d74c23b26676e03d
bpnolan <bp.nolan08@gmail.com>
2016-11-23 06:54:55 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/bpnolan/JavaScript/Node/2016-11-22.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+First-error callbacks in **Node**
+Wrapping errors in Node.js using _node-verror_
+Handle `JSON.parse` error in Node.js
+`process.nextTick()` callback pattern
