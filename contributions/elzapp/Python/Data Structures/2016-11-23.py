23f0afe41fdc096119df6dbe5bbaaf9a085324ab
elzapp <elzapp@gmail.com>
2016-11-23 12:26:14 +0300

    2016-11-23's Python workout

---
+++ contributions/elzapp/Python/Data Structures/2016-11-23.py
@@ -0,0 +1,5 @@
+Following PEP 8 styling guideline.
+A simple way to select a random item from a `list/tuple` data stucture
+Built-in `list` methods
+`date` object
+Best way to implement a simple `queue` 
