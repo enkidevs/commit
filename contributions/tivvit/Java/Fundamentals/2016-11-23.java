37c5f7506e551a7fbc83e17e826611d594711b5d
tivvit <tivvit@seznam.cz>
2016-11-23 11:54:17 +0300

    2016-11-23's Java workout

---
+++ contributions/tivvit/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
