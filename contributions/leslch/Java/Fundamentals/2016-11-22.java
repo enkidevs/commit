d01e648bee7c792de85d0ed45123e0063fc3f139
leslch <leslch@hotmail.com>
2016-11-23 03:29:18 +0300

    2016-11-22's Java workout

---
+++ contributions/leslch/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Use meaningful names
+Detect or prevent integer overflow
+Avoid using `static` variables
