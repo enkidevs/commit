91eca32c2c2021fd7e60dff8374a371035109d6c
olcay <olcaybayram@gmail.com>
2016-11-23 18:14:46 +0300

    2016-11-23's Java workout

---
+++ contributions/olcay/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+The distinction between overloading and overriding methods
+Generating random numbers
+The trade-off between short and descriptive names
+Common mistake on switch statements
