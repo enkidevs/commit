da059f28146415f616e86592cc4412aa6279496b
jfcastro <juanfercas2002@gmail.com>
2016-11-23 15:32:21 +0300

    2016-11-23's Java workout

---
+++ contributions/jfcastro/Java/Data Types/2016-11-23.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
