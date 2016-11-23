571626d24a02574562fb4a8ea295c18a657985d0
vituxre <vita.reizupa@gmail.com>
2016-11-23 17:08:17 +0300

    2016-11-23's Linux workout

---
+++ contributions/vituxre/Linux/System Management/2016-11-23.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Broadcast messages across the system
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Monitor System Memory using `vmstat`
