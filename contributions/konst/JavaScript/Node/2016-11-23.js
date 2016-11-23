3e770e60cf3ef38756c19fd57e3675cfd467972f
konst <kosnar@gmail.com>
2016-11-23 16:10:34 +0300

    2016-11-23's JavaScript workout

---
+++ contributions/konst/JavaScript/Node/2016-11-23.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+First-error callbacks in **Node**
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
