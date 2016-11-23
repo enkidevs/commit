cfb81f5798d5e5c084c980366e799bf2ca7612d9
ppgsalomao <ppgsalomao@gmail.com>
2016-11-23 23:33:44 +0300

    2016-11-23's Linux workout

---
+++ contributions/ppgsalomao/Linux/Terminals & Shells/2016-11-23.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
