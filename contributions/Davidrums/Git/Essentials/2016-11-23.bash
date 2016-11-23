7babbf23487e11458e950fe9de5b8777e17258df
Davidrums <david@soff.nl>
2016-11-23 19:45:10 +0300

    2016-11-23's Git workout

---
+++ contributions/Davidrums/Git/Essentials/2016-11-23.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+Adding a remote repository
+Using tags for version control
+Use `short` status to make output more compact
+Remote repositories: viewing, editing and deleting
