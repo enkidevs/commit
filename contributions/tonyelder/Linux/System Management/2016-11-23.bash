9fd280e6f8c412d764be917ce3030ba978fbb3c9
tonyelder <tonyelder@aapdltd.com>
2016-11-23 10:38:23 +0300

    2016-11-23's Linux workout

---
+++ contributions/tonyelder/Linux/System Management/2016-11-23.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
