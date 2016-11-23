dc931f606a33d8d71f56ae2a5a0307a537fea551
Zakariya1111 <elaizaidyahoo.fr@gmail.com>
2016-11-23 08:56:36 +0300

    2016-11-23's Java workout

---
+++ contributions/Zakariya1111/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
