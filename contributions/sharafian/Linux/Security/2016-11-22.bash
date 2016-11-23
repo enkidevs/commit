7a50125a513a702e9044a7cdcdd1b81dc6c5e137
sharafian <ben@sharafian.me>
2016-11-23 08:13:38 +0300

    2016-11-22's Linux workout

---
+++ contributions/sharafian/Linux/Security/2016-11-22.bash
@@ -0,0 +1,5 @@
+Extracting the `public key` from the `private key`
+Disable SSH password authentication 
+Best password generation utilities
+Disabling **USB** storage devices
+Private key encryption with `OpenSSL`
