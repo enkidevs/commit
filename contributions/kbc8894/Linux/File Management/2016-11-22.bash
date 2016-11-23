409b75f42ef50ffbeddf3a57ef205b5540584ff0
kbc8894 <kbc8894@gmail.com>
2016-11-23 03:16:07 +0300

    2016-11-22's Linux workout

---
+++ contributions/kbc8894/Linux/File Management/2016-11-22.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Easy way of sharing files
