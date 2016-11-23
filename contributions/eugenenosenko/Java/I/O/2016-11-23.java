ffed3e5bbce8422b15910f54ebd7f18af22e4ede
eugenenosenko <eugenenosenko@gmail.com>
2016-11-23 14:19:45 +0300

    2016-11-23's Java workout

---
+++ contributions/eugenenosenko/Java/I/O/2016-11-23.java
@@ -0,0 +1,5 @@
+System independent path specification
+How to create directories in Java
+Create system file paths using the `Path` class
+Retrieving file store attributes
+Listing a file system's root directories
