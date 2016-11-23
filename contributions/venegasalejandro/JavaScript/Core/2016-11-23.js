53c7dac956ebe3e98d15ef3ea0616124acc79f7f
venegasalejandro <venegasalejandro95@gmail.com>
2016-11-24 00:51:36 +0300

    2016-11-23's JavaScript workout

---
+++ contributions/venegasalejandro/JavaScript/Core/2016-11-23.js
@@ -0,0 +1,5 @@
+Keywords in hash keys
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+Extra function arguments are undefined by default
+Inaccuracy of binary floating-point format
