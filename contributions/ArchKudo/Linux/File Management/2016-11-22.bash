7010806bf1fdc486a1893dd1cc3478fe58d0f4e1
ArchKudo <vgd.vyas17@gmail.com>
2016-11-23 15:49:11 +0300

    2016-11-22's Linux workout

---
+++ contributions/ArchKudo/Linux/File Management/2016-11-22.bash
@@ -0,0 +1,5 @@
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
