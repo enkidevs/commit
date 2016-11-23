129e9f6d3c2c06657892cc0fc60bb3efaab01d99
RobinKock <robin@kock-hamburg.de>
2016-11-23 12:48:17 +0300

    2016-11-23's JavaScript workout

---
+++ contributions/RobinKock/JavaScript/Node/2016-11-23.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Asynchronous File Write/Read in Node.js
+Use conventional property names for error handling in Node.js
+**Node** `os` module
+Automatic compilation for Node with **Nodemon** 
