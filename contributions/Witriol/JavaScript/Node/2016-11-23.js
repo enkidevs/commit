ec60fc864d818a0726bf098769f69ff8d89b2e9e
Witriol <tomas.simicek@outlook.com>
2016-11-23 11:04:36 +0300

    2016-11-23's JavaScript workout

---
+++ contributions/Witriol/JavaScript/Node/2016-11-23.js
@@ -0,0 +1,5 @@
+How to debug Node.js
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+`exports` vs. `module.exports` in **Node**
+Middleware/pipeline design pattern
