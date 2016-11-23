89c78cb8a823691313aa626ec0740cf821c93bf8
jcloza31 <jcloza01@gmail.com>
2016-11-23 12:12:48 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/jcloza31/JavaScript/React/2016-11-22.js
@@ -0,0 +1,5 @@
+Self-closing tags in **JSX**
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+**React** `this.props.children`
+Expose Component Functions in **React**
