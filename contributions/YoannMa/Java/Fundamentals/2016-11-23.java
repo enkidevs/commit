6e042d901557a8a4000ea1376857c593c200586c
YoannMa <yoann.mallemanche@gmail.com>
2016-11-23 18:58:47 +0300

    2016-11-23's Java workout

---
+++ contributions/YoannMa/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
