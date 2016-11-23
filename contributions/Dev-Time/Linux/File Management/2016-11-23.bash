a9f674d4ff2c5ec5186c402720d03747399d2ac2
Dev-Time <pcdriverpro@gmail.com>
2016-11-23 23:17:19 +0300

    2016-11-23's Linux workout

---
+++ contributions/Dev-Time/Linux/File Management/2016-11-23.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Converting between Unix and Windows text files
+Get disk space usage with `df`
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
