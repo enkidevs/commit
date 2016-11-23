bd6d2ca5db8a0a0ca4920efe05c163940c227927
mharward <mharward@gmail.com>
2016-11-23 20:42:57 +0300

    2016-11-23's JavaScript workout

---
+++ contributions/mharward/JavaScript/Browser APIs/2016-11-23.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+CSS for when JavaScript is enabled
+Navigating the browser history
+Permission API
+FileReader.readAsArrayBuffer()
