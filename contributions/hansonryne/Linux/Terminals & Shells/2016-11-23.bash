02c2b75a169941c557bf439562e4a45e3f3049df
hansonryne <hansonryne@gmail.com>
2016-11-23 16:37:35 +0300

    2016-11-23's Linux workout

---
+++ contributions/hansonryne/Linux/Terminals & Shells/2016-11-23.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Aliasing ssh connections
+Find any Unix / Linux command
+Breaking out of a terminal when `ssh` locks
+Adding directories to your `$PATH`
