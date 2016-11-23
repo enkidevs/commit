568bd5fa5320600a78dbe4a6a43b2b8af4bc65f0
diego.andrade <diego.interativo@gmail.com>
2016-11-23 05:59:44 +0300

    2016-11-22's Linux workout

---
+++ contributions/diego.andrade/Linux/File Management/2016-11-22.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Easy way of sharing files
