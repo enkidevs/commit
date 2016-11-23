25161eac032b63c5b7e869f0edc5d73158cc5426
BrainBacon <brian.jesse@gmail.com>
2016-11-23 12:56:35 +0300

    2016-11-22's Java workout

---
+++ contributions/BrainBacon/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Using an interface as a parameter
