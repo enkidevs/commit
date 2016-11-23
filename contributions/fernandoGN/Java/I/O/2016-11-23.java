c13d3d83d63e21fc011c2680d5c3aa418a0a3ece
fernandoGN <fernando.a.gomes.n@gmail.com>
2016-11-23 14:53:11 +0300

    2016-11-23's Java workout

---
+++ contributions/fernandoGN/Java/I/O/2016-11-23.java
@@ -0,0 +1,5 @@
+System independent path specification
+How to create directories in Java
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
