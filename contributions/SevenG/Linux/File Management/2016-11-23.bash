f61d4f89b90c3139b5a15326c76731d4cffb9b41
SevenG <enki@seveng.net>
2016-11-23 20:14:17 +0300

    2016-11-23's Linux workout

---
+++ contributions/SevenG/Linux/File Management/2016-11-23.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
