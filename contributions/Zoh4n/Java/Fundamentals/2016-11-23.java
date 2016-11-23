11151b666641147c651901a80a69288fff1eb566
Zoh4n <ervin537@gmail.com>
2016-11-23 23:06:43 +0300

    2016-11-23's Java workout

---
+++ contributions/Zoh4n/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Difference between JRE and JDK?
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
+Always specify type parameter
