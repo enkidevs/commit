8f0247d013bb1ebc0e7e9483101a686ec4510383
kolyaoja <kolyaoja@gmail.com>
2016-11-23 10:25:55 +0300

    2016-11-22's Git workout

---
+++ contributions/kolyaoja/Git/Essentials/2016-11-22.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+How to set your username and email
+Search for commits by author
+Use `short` status to make output more compact
+Remote repositories: fetching and pushing
