26a9c00a40992e1da299c69adf116f5d92184e57
luxpsycho <enki@citrusfruit.lu>
2016-11-23 11:56:25 +0300

    2016-11-23's Linux workout

---
+++ contributions/luxpsycho/Linux/Permissions & Users/2016-11-23.bash
@@ -0,0 +1,5 @@
+Executing commands with `sudo` without password
+Granting `root` access to a user
+Making a file unalterable with `chattr`
+Get file permissions in octal form
+Run previous command as root
