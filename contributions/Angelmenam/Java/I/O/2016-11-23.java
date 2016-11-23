be3c1b910dd41241e944b838e503386f712eb6d5
Angelmenam <angelmenam@hotmail.com>
2016-11-23 23:05:37 +0300

    2016-11-23's Java workout

---
+++ contributions/Angelmenam/Java/I/O/2016-11-23.java
@@ -0,0 +1,5 @@
+System independent path specification
+Create system file paths using the `Path` class
+Translating `Scanner` tokens into primitive types
+Reading file attributes
+Using the `OpenOption...` vararg to define file writing behavior
