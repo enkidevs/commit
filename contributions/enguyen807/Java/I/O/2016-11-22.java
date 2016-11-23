abacabcd1de66efb793e66d762aff0e4b5183598
enguyen807 <enguyen807@gmail.com>
2016-11-23 09:54:25 +0300

    2016-11-22's Java workout

---
+++ contributions/enguyen807/Java/I/O/2016-11-22.java
@@ -0,0 +1,5 @@
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Retrieving file store attributes
+Listing a file system's root directories
+Deleting files and directories
