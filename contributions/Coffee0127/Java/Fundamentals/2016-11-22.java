f351d5c88aae6bfad38fb628e19fdbb57adef3e7
Coffee0127 <coffee0127@gmail.com>
2016-11-23 03:38:14 +0300

    2016-11-22's Java workout

---
+++ contributions/Coffee0127/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Generating random numbers
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
+Using an interface as a parameter
