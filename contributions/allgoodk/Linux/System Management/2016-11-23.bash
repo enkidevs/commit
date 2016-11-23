a45627c8bee0cb92b43f36f1cf49fe6f453c8637
allgoodk <sylim@ya.ru>
2016-11-23 20:14:20 +0300

    2016-11-23's Linux workout

---
+++ contributions/allgoodk/Linux/System Management/2016-11-23.bash
@@ -0,0 +1,6 @@
+Test disk speed
+Force a time update with `ntp`
+
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
