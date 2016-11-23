652b9e4a45dc74f08af6923906f83b8eed5ad6a7
WolfyTheTerrible <general.kenobi.2450@gmail.com>
2016-11-23 19:50:16 +0300

    2016-11-22's Linux workout

---
+++ contributions/WolfyTheTerrible/Linux/System Management/2016-11-22.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
+System uptime
