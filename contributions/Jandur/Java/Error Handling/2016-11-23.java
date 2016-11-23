9f8afd84f658410bcee136d6ff47e7b51e6ad40d
Jandur <jandur92@gmail.com>
2016-11-23 10:45:14 +0300

    2016-11-23's Java workout

---
+++ contributions/Jandur/Java/Error Handling/2016-11-23.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Most common reason behind **stack overflow** error
+Throwing an `exception`
