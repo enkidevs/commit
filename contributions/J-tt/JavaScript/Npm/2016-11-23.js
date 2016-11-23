9b297e4b5091604509db253dfb2bc7f531d2a7b0
J-tt <jacksonjett5@gmail.com>
2016-11-24 01:50:41 +0300

    2016-11-23's JavaScript workout

---
+++ contributions/J-tt/JavaScript/Npm/2016-11-23.js
@@ -0,0 +1,5 @@
+Calling remote scripts with npm
+npm can handle multiple versions of a dependency
+Manage local node modules with `npm link`
+`npm` verifies `sha1` hashes of packages
+`peerDependencies`
