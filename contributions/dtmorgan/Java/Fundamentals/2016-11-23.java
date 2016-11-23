1ad8c3a9dfa05ce6df2b248e7e6c69be0a558082
dtmorgan <morgan.diot@gmail.com>
2016-11-23 16:25:03 +0300

    2016-11-23's Java workout

---
+++ contributions/dtmorgan/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
