31bcf38f4854d864b77db390e1ace24e30696147
Razor2142 <razor2142@graveyardgames.net>
2016-11-23 13:51:02 +0300

    2016-11-23's Linux workout

---
+++ contributions/Razor2142/Linux/System Management/2016-11-23.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
