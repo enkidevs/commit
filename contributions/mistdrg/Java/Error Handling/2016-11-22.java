540373b502506df549cfdca0feb839fc77267a30
mistdrg <mistdrg@naver.com>
2016-11-23 17:13:43 +0300

    2016-11-22's Java workout

---
+++ contributions/mistdrg/Java/Error Handling/2016-11-22.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Use try-with-resources instead of `finally`
+Prevent `exceptions` while logging Data
+Throwing an `exception`
