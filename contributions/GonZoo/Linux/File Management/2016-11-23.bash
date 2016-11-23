1917f2d74afd80799ef1e895841ea689722b1834
GonZoo <gonzalo.cao@gmail.com>
2016-11-24 00:06:06 +0300

    2016-11-23's Linux workout

---
+++ contributions/GonZoo/Linux/File Management/2016-11-23.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Extracting `tar` files to a specific directory
+Identify files using the `file` command
+
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
