fecea15e0ae58e0af3d375c850b9750952b7a693
BrianKolowitz <bkolowitz@gmail.com>
2016-11-23 03:59:54 +0300

    2016-11-22's Java workout

---
+++ contributions/BrianKolowitz/Java/Fundamentals/2016-11-22.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
