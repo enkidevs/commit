48568c72f9d65a7d5b029d6bdb90c85a4b627e04
daniula <daniel.mendalka@gmail.com>
2016-11-23 15:44:39 +0300

    2016-11-23's Linux workout

---
+++ contributions/daniula/Linux/System Management/2016-11-23.bash
@@ -0,0 +1,6 @@
+Test disk speed
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
