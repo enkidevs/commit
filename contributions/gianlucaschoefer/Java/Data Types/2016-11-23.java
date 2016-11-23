71b612678f5963130783d654863b0f3123e82f61
gianlucaschoefer <gianlucaschoefer@gmail.com>
2016-11-23 12:15:17 +0300

    2016-11-23's Java workout

---
+++ contributions/gianlucaschoefer/Java/Data Types/2016-11-23.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Multidimensional array declaration
+Do not attempt comparisons with NaN
