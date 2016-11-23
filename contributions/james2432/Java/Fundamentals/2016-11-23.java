b3b86e1baf603eafefb987a9d1652dcd181d80d8
james2432 <james2432@gmail.com>
2016-11-23 14:29:59 +0300

    2016-11-23's Java workout

---
+++ contributions/james2432/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
