cfaec601d943d710cdddc345982c32ef43a0cf82
service-paradis <service.paradis@gmail.com>
2016-11-23 04:52:57 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/service-paradis/JavaScript/React/2016-11-22.js
@@ -0,0 +1,5 @@
+Self-closing tags in **JSX**
+Pixel values in style props for **React**
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+**React** `this.props.children`
