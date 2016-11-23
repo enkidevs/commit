bf2086f7e86d66239cad31ece7303b6c36421a2c
ganes1410 <ganesramesh110@gmail.com>
2016-11-23 07:30:08 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/ganes1410/JavaScript/Core/2016-11-22.js
@@ -0,0 +1,5 @@
+Quick way to format integers with a fixed number of digits.
+Setting default values with `short circuiting`
+Most efficient way to build `HTML` strings
+`console.*` in JavaScript
+Inaccuracy of binary floating-point format
