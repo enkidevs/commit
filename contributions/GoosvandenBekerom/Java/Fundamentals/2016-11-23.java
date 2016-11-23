2ba3afb48bed7217819a928b95f5fbaded39d05a
GoosvandenBekerom <goos.bekerom@gmail.com>
2016-11-24 01:09:41 +0300

    2016-11-23's Java workout

---
+++ contributions/GoosvandenBekerom/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
