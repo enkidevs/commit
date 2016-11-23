76159e0dfc2f20ec27902b5521fdae094b72cdcd
Sergobot <sergobot@protonmail.com>
2016-11-23 20:24:02 +0300

    2016-11-23's Java workout

---
+++ contributions/Sergobot/Java/Data Types/2016-11-23.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Converting a string to upper or lower case
+Retrieve the component type of an array
