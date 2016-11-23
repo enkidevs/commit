ecf47d1bf0adab4a70cd800a6363546dce671846
raz1234 <raz_conc@yahoo.com>
2016-11-23 19:19:24 +0300

    2016-11-23's Linux workout

---
+++ contributions/raz1234/Linux/Terminals & Shells/2016-11-23.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Quick `bash` shortcuts 
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
