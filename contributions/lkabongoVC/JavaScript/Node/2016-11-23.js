7d106e3da435285806cc4b3f080eab65b0f530cc
lkabongoVC <lkabongo@vicomtech.org>
2016-11-23 18:15:22 +0300

    2016-11-23's JavaScript workout

---
+++ contributions/lkabongoVC/JavaScript/Node/2016-11-23.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Synchronous File Write/Read in Node.js
+Global Objects and Environment Variables in **Node**
+Wrapping errors in Node.js using _node-verror_
+Automatic compilation for Node with **Nodemon** 
