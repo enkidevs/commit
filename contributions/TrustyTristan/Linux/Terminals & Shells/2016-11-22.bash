2f0a470cfbcc99bec5ddfd5269f4a3b7bf71bc2d
TrustyTristan <tristanbrazier@gmail.com>
2016-11-23 04:28:28 +0300

    2016-11-22's Linux workout

---
+++ contributions/TrustyTristan/Linux/Terminals & Shells/2016-11-22.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Clear bash history
+The Basics of Environment Variables
