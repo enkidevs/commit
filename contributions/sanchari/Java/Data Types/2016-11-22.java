a6538803b579dd6d3a352ebdc57173718e817239
sanchari <sanchari.myindia@gmail.com>
2016-11-23 08:10:44 +0300

    2016-11-22's Java workout

---
+++ contributions/sanchari/Java/Data Types/2016-11-22.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Multidimensional array declaration
+Numeric Conversion - Widening
