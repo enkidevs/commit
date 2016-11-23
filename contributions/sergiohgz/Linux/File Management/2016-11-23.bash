23e0616a9c0500bece3cd62bba85bc18e887981a
sergiohgz <sergioherreraguz@gmail.com>
2016-11-23 11:50:25 +0300

    2016-11-23's Linux workout

---
+++ contributions/sergiohgz/Linux/File Management/2016-11-23.bash
@@ -0,0 +1,7 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Find the unknown process preventing deleting of files
