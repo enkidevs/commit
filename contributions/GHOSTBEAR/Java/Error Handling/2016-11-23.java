0041a24c5566dd08ca64a740ed04cad045992f77
GHOSTBEAR <andreas.everos@gmail.com>
2016-11-23 09:48:07 +0300

    2016-11-23's Java workout

---
+++ contributions/GHOSTBEAR/Java/Error Handling/2016-11-23.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Creating new Exceptions
+Checking a double for `NaN` value
