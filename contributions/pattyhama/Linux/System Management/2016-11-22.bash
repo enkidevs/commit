7b15ad40eaa488c6a39f7f7dea95f54807bf1822
pattyhama <strodr@gmail.com>
2016-11-23 17:20:28 +0300

    2016-11-22's Linux workout

---
+++ contributions/pattyhama/Linux/System Management/2016-11-22.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
