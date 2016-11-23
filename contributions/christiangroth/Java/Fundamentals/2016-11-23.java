7090db4f0ee56b79bb0117368c664513f7d566d3
christiangroth <chr.groth@gmx.de>
2016-11-23 14:38:13 +0300

    2016-11-23's Java workout

---
+++ contributions/christiangroth/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
