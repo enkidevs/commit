99d68246ee28de63784d91e0620ecdbdd3c69abb
Ruud <ruud@arfie.nl>
2016-11-23 16:50:08 +0300

    2016-11-23's Linux workout

---
+++ contributions/Ruud/Linux/File Management/2016-11-23.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
+Easy way of sharing files
