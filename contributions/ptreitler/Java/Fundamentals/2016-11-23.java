ba090bb88d7919f47e916eeab0fa2828c5b1ad2b
ptreitler <peter.treitler@gmail.com>
2016-11-23 13:59:27 +0300

    2016-11-23's Java workout

---
+++ contributions/ptreitler/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
