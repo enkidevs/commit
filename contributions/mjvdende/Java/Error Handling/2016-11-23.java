e6e5b2b0e4adb4cab77281c8d6a8f5b9703f5dce
mjvdende <maartenvdende@gmail.com>
2016-11-24 00:58:47 +0300

    2016-11-23's Java workout

---
+++ contributions/mjvdende/Java/Error Handling/2016-11-23.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Throwing an `exception`
+Careful Numeric Data Conversions
