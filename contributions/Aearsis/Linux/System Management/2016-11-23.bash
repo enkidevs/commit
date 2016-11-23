7d16fd832f0b7e925c1da124f34fe18dfe2780e8
Aearsis <aearsis@eideo.cz>
2016-11-23 13:39:13 +0300

    2016-11-23's Linux workout

---
+++ contributions/Aearsis/Linux/System Management/2016-11-23.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Changing the `/tmp` cleanup frequency
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
