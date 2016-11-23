cca87cdd0ac8c49e56c455c457328f2661a9739a
dabizk <dabizk@yahoo.com>
2016-11-23 09:23:44 +0300

    2016-11-22's Linux workout

---
+++ contributions/dabizk/Linux/Package Management/2016-11-22.bash
@@ -0,0 +1,5 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+List installed packages
+Installing a `.deb` package from the terminal
+Downgrade a package via `apt-get`
