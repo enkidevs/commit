136177d3e8d8c55c40654a6b02a4820d158655b9
geniusinfrance <ryan.bergerson@gmail.com>
2016-11-23 11:00:05 +0300

    2016-11-23's Java workout

---
+++ contributions/geniusinfrance/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+Using `Printstream.format` to format a string before printing
+Do not perform bitwise and arithmetic operations on the same data
+How range operations work
