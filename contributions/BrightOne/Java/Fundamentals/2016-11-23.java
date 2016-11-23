b0b332e81427e9dfec925e074f66820c88b37971
BrightOne <filalex77@gmail.com>
2016-11-23 12:42:54 +0300

    2016-11-23's Java workout

---
+++ contributions/BrightOne/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Using `Printstream.format` to format a string before printing
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Detect or prevent integer overflow
+Supply `toString()` in all classes
