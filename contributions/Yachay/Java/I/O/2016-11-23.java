d823094c6bba46cf7cb48ed7dab8e50f46dfe8cd
Yachay <yachay@gmail.com>
2016-11-23 22:26:09 +0300

    2016-11-23's Java workout

---
+++ contributions/Yachay/Java/I/O/2016-11-23.java
@@ -0,0 +1,5 @@
+System independent path specification
+`System.out` vs `System.err`
+How to create directories in Java
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
