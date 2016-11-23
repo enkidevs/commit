f03418e79b991a71ab6a9e3a53c69d79bdd4f8d6
onishinji <onishinji@gmail.com>
2016-11-23 10:09:16 +0300

    2016-11-23's JavaScript workout

---
+++ contributions/onishinji/JavaScript/Node/2016-11-23.js
@@ -0,0 +1,5 @@
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+Manage child processes in **Node**
+Bulk-Write in **Node** with `cork()`
