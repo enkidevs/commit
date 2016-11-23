0156997d321c046e85b15aa47b0dad4ab9dec8f4
paulp <paulp@hotmail.sk>
2016-11-23 11:17:22 +0300

    2016-11-23's Java workout

---
+++ contributions/paulp/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Altering format string output by changing a format specifier's `argument_index`
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
