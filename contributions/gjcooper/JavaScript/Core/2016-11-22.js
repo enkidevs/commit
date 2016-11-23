42e38cbba0946a70e905e60b29b680a036f79d5d
gjcooper <gjcooper@gmail.com>
2016-11-23 07:19:07 +0300

    2016-11-22's JavaScript workout

---
+++ contributions/gjcooper/JavaScript/Core/2016-11-22.js
@@ -0,0 +1,5 @@
+Quick way to format integers with a fixed number of digits.
+Get a random item from an Array
+Performing a function at timed intervals
+Round numbers to `N` decimals 
+Base conversion 
