b5e665a50cac1f39d9198009a8733f4d0d9c7827
bmo77 <basha.others@gmail.com>
2016-11-23 14:57:44 +0300

    2016-11-23's Java workout

---
+++ contributions/bmo77/Java/Data Types/2016-11-23.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
+Numeric Conversion - Widening
