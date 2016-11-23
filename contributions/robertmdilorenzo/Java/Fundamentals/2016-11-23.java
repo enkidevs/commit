837d26b41edb0118c9b878a9483dfbd839adff53
robertmdilorenzo <robertmdilorenzo@gmail.com>
2016-11-23 18:48:06 +0300

    2016-11-23's Java workout

---
+++ contributions/robertmdilorenzo/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
