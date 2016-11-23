fcf5d38167e3d2d50bdea6eadfe040d413e461ba
slowdata <slowmotion@outlook.com>
2016-11-24 01:01:07 +0300

    2016-11-23's JavaScript workout

---
+++ contributions/slowdata/JavaScript/Core/2016-11-23.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Undefined / Empty / Null Checks
+Using assignment operators
+The difference between null, undefined and NaN
+Inaccuracy of binary floating-point format
