9a1a36a473d5e29b908beb731da77120c4007976
mhoogkamer <michael.hoogkamer@gmail.com>
2016-11-23 19:26:32 +0300

    2016-11-23's Linux workout

---
+++ contributions/mhoogkamer/Linux/System Management/2016-11-23.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Basic service management with `systemd`
+Get hardware stack details with `lspci`
