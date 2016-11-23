c24493b519f27d69cc1658953f6e4db5d0f936b1
trion <parmsingh129@yahoo.com>
2016-11-23 08:10:23 +0300

    2016-11-22's Java workout

---
+++ contributions/trion/Java/Data Types/2016-11-22.java
@@ -0,0 +1,5 @@
+Two ways to use an `Iterator`
+Use `Arrays.asList()` to initialise lists
+Converting numbers to strings
+Equals operation on different data types
+Do not attempt comparisons with NaN
