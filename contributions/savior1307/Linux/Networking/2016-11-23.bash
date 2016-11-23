424e594a6157ea0097ac6c2f1afa57dbc468ddef
savior1307 <savior1307@gmail.com>
2016-11-23 12:53:40 +0300

    2016-11-23's Linux workout

---
+++ contributions/savior1307/Linux/Networking/2016-11-23.bash
@@ -0,0 +1,5 @@
+Multiple IP Address with aliasing 
+Disable `IPv6`
+Limit the `wget` download rate
+Sending emails from terminal / scripts
+Staying connected remotely via SSH without an interactive shell
