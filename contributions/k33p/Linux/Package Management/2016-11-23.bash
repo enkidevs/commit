68e3bf25e689fa5a658f5dcd81886f858a4f4f43
k33p <k33p@mail.ru>
2016-11-23 20:25:56 +0300

    2016-11-23's Linux workout

---
+++ contributions/k33p/Linux/Package Management/2016-11-23.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+Backporting on Debian
+
+Using `Tasksel` for software installation
+Solving `BADSIG GPG` errors
