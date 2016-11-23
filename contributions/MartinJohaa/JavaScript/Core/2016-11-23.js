6ad4b6eeae67e168850f25246a9f09fb7c6bee16
MartinJohaa <martin.johansson17@gmail.com>
2016-11-23 19:34:09 +0300

    2016-11-23's JavaScript workout

---
+++ contributions/MartinJohaa/JavaScript/Core/2016-11-23.js
@@ -0,0 +1,5 @@
+Hoisting applies only to variable declarations, not initializations
+NaN === NaN?
+The difference between null, undefined and NaN
+Scope and strict mode
+Inaccuracy of binary floating-point format
