136956fbb1880469033e9e8897f43dde835411fc
DominguesGM <gil.domingues2@hotmail.com>
2016-11-23 11:28:08 +0300

    2016-11-23's Linux workout

---
+++ contributions/DominguesGM/Linux/System Management/2016-11-23.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Removing old kernels in Debian based systems
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
