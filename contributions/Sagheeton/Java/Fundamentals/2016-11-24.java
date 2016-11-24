268fc045e0ac8c4165041cc85885f823418cd6f9
Sagheeton <sagheeton@gmail.com>
2016-11-24 07:04:06 +0300

    2016-11-24's Java workout

---
+++ contributions/Sagheeton/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use meaningful names
+Java naming conventions
+Common mistake on switch statements
+Use `DecimalFormat` class to format numbers
