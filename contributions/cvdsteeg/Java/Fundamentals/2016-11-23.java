af5b964ee3e8fbe1b823e28d26b0cbd607edb45c
cvdsteeg <cvdsteeg@gmail.com>
2016-11-24 03:06:43 +0300

    2016-11-23's Java workout

---
+++ contributions/cvdsteeg/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
