58c4837639b3634a5ca469ed7b9803aa61bae79d
Razor2142 <razor2142@graveyardgames.net>
2016-11-24 21:46:47 +0300

    2016-11-24's Java workout

---
+++ contributions/Razor2142/Java/Error Handling/2016-11-24.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+The distinction between checked and unchecked exceptions
+Careful Numeric Data Conversions
