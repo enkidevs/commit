3f246ef19a31abd70b1981b45cb8155561467c6c
mrkimo1998 <mrkimo1998@protonmail.com>
2016-11-24 13:22:04 +0300

    2016-11-24's Linux workout

---
+++ contributions/mrkimo1998/Linux/File Management/2016-11-24.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
