27c8856f75b677c6f28e4f27eaf87d50621cca73
PsnDth <psndth@gmail.com>
2016-11-24 17:38:50 +0300

    2016-11-24's Python workout

---
+++ contributions/PsnDth/Python/Data Structures/2016-11-24.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+When `range` comes in handy
+There is more to copying
+Keep things in order with `OrderedDict`
+Implementing **weak** references
