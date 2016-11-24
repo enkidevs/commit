2fdd4137c30d03a72411fc828aef37c5f42e11ff
asaraco <sirlemming@gmail.com>
2016-11-24 05:35:55 +0300

    2016-11-23's Java workout

---
+++ contributions/asaraco/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Use meaningful names
+Ternary operator
+Altering format string output by changing a format specifier's `argument_index`
+Always specify type parameter
