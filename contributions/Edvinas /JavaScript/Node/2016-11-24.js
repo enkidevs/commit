3ada220792068a5ffcc95f08002e4400a985e3cc
Edvinas <edvinas.reg@outlook.com>
2016-11-24 15:26:04 +0300

    2016-11-24's JavaScript workout

---
+++ contributions/Edvinas /JavaScript/Node/2016-11-24.js
@@ -0,0 +1,5 @@
+First-error callbacks in **Node**
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
+`process.nextTick()` callback pattern
+`try-catch` only for **sync** code
