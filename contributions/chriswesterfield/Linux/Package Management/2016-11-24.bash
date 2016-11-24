2dc796ea39ab721dff620d6f5ef2682e0d2054e3
chriswesterfield <chris@westerfield.name>
2016-11-24 09:59:59 +0300

    2016-11-24's Linux workout

---
+++ contributions/chriswesterfield/Linux/Package Management/2016-11-24.bash
@@ -0,0 +1,6 @@
+Backporting on Debian
+
+Prevent updating a specific package in Debian systems
+Using `Tasksel` for software installation
+Downgrade a package via `apt-get`
+Solving `BADSIG GPG` errors
