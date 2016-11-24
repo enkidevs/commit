78a97b8e742390f46a73420dfb7321dab765a040
Rimerith <rvt2011@yahoo.com>
2016-11-24 21:01:48 +0300

    2016-11-24's Java workout

---
+++ contributions/Rimerith/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Do not perform bitwise and arithmetic operations on the same data
