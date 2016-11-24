f2a1d4cf1792d444ff209f710c61647c0b77ce88
jochenjagers <jochen.jaegers@gmail.com>
2016-11-24 20:19:33 +0300

    2016-11-24's Java workout

---
+++ contributions/jochenjagers/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Calling one constructor from another
+Always specify type parameter
+How range operations work
+Using an interface as a parameter
