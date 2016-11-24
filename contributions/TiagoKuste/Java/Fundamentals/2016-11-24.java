7e6470d0283ad4b05ca4317dc3e81428fc7221b7
TiagoKuste <tiago.tagcosta@gmail.com>
2016-11-24 23:05:50 +0300

    2016-11-24's Java workout

---
+++ contributions/TiagoKuste/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Ternary operator
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+Avoid using `static` variables
