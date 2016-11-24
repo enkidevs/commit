bafba4bb00378c54470755aabbc0b64846a0f6bd
daniula <daniel.mendalka@gmail.com>
2016-11-24 14:14:47 +0300

    2016-11-24's Linux workout

---
+++ contributions/daniula/Linux/Terminals & Shells/2016-11-24.bash
@@ -0,0 +1,5 @@
+Shebang `#!` explained 
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Quick `bash` shortcuts 
+Random password generator
