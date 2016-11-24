89164f17dc3c2829f84d23964e553da3c36ae532
Davidrums <david@soff.nl>
2016-11-24 14:00:04 +0300

    2016-11-24's Java workout

---
+++ contributions/Davidrums/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Use `DecimalFormat` class to format numbers
+Using an interface as a parameter
