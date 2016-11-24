a730a8e38782072d4824f50e27f50ea769ba635d
St3v3JT <thorpe.steve@gmail.com>
2016-11-24 03:42:50 +0300

    2016-11-23's JavaScript workout

---
+++ contributions/St3v3JT/JavaScript/React/2016-11-23.js
@@ -0,0 +1,6 @@
+Use `propTypes` on stateless components
+Functional Stateless Components in React
+componentWillReceiveProps Not Triggered After Mounting
+
+Dangerously set `innerHTML`
+Shortcut for transferring props
