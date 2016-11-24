d1790e66b0dba2bdb52a4e80e1a049fae3cab07b
chhsiao90 <chhsiao90@gmail.com>
2016-11-24 11:32:27 +0300

    2016-11-23's JavaScript workout

---
+++ contributions/chhsiao90/JavaScript/Node/2016-11-23.js
@@ -0,0 +1,5 @@
+Global Objects and Environment Variables in **Node**
+The built-in Node debugger
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
+`try-catch` only for **sync** code
