c9d739a8a23f95fbeec85bf2dc50b0e25963b6e1
Avabin <andrzej.piotrowski.76@gmail.com>
2016-11-24 13:11:15 +0300

    2016-11-24's Java workout

---
+++ contributions/Avabin/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Altering format string output by changing a format specifier's `argument_index`
+Using Inheritance to reduce code repetition
+Measuring time
+There is no such thing as *pass-by-reference* in Java
