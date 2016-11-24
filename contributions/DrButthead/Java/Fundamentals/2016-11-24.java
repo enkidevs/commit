351f604b2189b64950354c98f501e385b38b7924
DrButthead <oliver.olding@hotmail.com>
2016-11-24 23:45:16 +0300

    2016-11-24's Java workout

---
+++ contributions/DrButthead/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Metadata: creating a user-defined file attribute
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
