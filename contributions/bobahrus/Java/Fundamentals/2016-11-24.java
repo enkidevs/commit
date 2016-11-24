0949eb07f14a0ecbe16706b153b27d3e8ce78c30
bobahrus <bobahrus@gmail.com>
2016-11-24 15:26:21 +0300

    2016-11-24's Java workout

---
+++ contributions/bobahrus/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Uses of the `final` keyword
+Use strings in a `switch` statement
+Altering format string output by changing a format specifier's `argument_index`
+Avoid using `static` variables
