5a3a0d043742fc43a6b3c47f776bc9635cef875c
wolsne <enslowa17@gmail.com>
2016-11-24 23:02:11 +0300

    2016-11-24's Linux workout

---
+++ contributions/wolsne/Linux/Terminals & Shells/2016-11-24.bash
@@ -0,0 +1,5 @@
+Clear the terminal instantly
+Find any Unix / Linux command
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
