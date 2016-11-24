edd12bfaa96e2667b6ec43a34aafc72c90a21a2f
sandor.dargo <sandor.dargo@gmail.com>
2016-11-24 11:01:19 +0300

    2016-11-24's Python workout

---
+++ contributions/sandor.dargo/Python/Data Structures/2016-11-24.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+`Dictionary` view objects
+`Module`s everywhere!
+Special attributes of objects and classes
+Working with `set`s
