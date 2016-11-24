ce92883028d0bc4f8fc2b8bd506d32140fa16218
Derjablik <attronach@gmail.com>
2016-11-24 08:35:31 +0300

    2016-11-24's Java workout

---
+++ contributions/Derjablik/Java/Data Types/2016-11-24.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Do not attempt comparisons with NaN
