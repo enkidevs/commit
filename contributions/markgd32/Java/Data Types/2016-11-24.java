d903b9adb4b3497b79170524414ceb4208d9d859
markgd32 <jocsmarcel@gmail.com>
2016-11-24 22:08:38 +0300

    2016-11-24's Java workout

---
+++ contributions/markgd32/Java/Data Types/2016-11-24.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Do not attempt comparisons with NaN
+Numeric Conversion - Widening
