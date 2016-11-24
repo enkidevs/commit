26aa2e99d78874b1ed743a6c848057bb53f84348
lolporer <lolporer@gmail.com>
2016-11-24 10:34:44 +0300

    2016-11-24's JavaScript workout

---
+++ contributions/lolporer/JavaScript/Node/2016-11-24.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+Wrapping errors in Node.js using _node-verror_
+Clustering
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
