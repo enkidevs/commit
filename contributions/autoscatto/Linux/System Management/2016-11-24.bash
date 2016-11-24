0b3c14de921105107aa191799d3e2ed1943699b8
autoscatto <4utoscatto@gmail.com>
2016-11-24 10:36:15 +0300

    2016-11-24's Linux workout

---
+++ contributions/autoscatto/Linux/System Management/2016-11-24.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Preventing a user from logging into the system
+Basic service management with `systemd`
