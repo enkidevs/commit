bbaa4a057448dcc02577c688a1bea72f29e863fd
david-kl <daviddd.kl@gmail.com>
2016-11-24 14:14:38 +0300

    2016-11-24's Java workout

---
+++ contributions/david-kl/Java/Data Types/2016-11-24.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Converting numbers to strings
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
