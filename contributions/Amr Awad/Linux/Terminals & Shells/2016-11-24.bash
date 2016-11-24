1bb5896c1c0ede0fddd8a171d785189237ad1886
Amr Awad <amro.khir@gmail.com>
2016-11-24 07:20:42 +0300

    2016-11-24's Linux workout

---
+++ contributions/Amr Awad/Linux/Terminals & Shells/2016-11-24.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Execute a command without saving it in history
+Find any Unix / Linux command
+Terminal incognito mode
+Adding directories to your `$PATH`
