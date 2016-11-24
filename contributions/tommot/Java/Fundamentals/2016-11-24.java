272cc6f070b527f6fc8397dabc163c315a615f0e
tommot <thomas.prebeck@web.de>
2016-11-24 20:44:30 +0300

    2016-11-24's Java workout

---
+++ contributions/tommot/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Altering format string output by changing a format specifier's `argument_index`
+Use `DecimalFormat` class to format numbers
+Measuring time
+Do not return *references* to private *mutable* class members
