dbf2ffc1755628caf96f84ba392289ad63711fe0
Antauri <catalin.zamfir@kitsoftware.ro>
2016-11-24 10:19:28 +0300

    2016-11-24's Java workout

---
+++ contributions/Antauri/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+Metadata: creating a user-defined file attribute
+`Autoboxing` and `Unboxing`
+Avoid using `static` variables
