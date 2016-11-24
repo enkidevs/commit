5fa9e10acd69c6dc2f1df548407cf740e3ce5b94
IIIHeloIII <kristiandahl@gmail.com>
2016-11-25 00:09:38 +0300

    2016-11-24's Linux workout

---
+++ contributions/IIIHeloIII/Linux/System Management/2016-11-24.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Detect your linux distribution
+Find out if the system's architecture is 32 or 64 bit
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
