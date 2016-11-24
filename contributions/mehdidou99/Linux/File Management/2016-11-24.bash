affcaf6f4403b667d8beb9fa059944746f06c14d
mehdidou99 <mehdibenharrats@yahoo.fr>
2016-11-24 10:31:20 +0300

    2016-11-24's Linux workout

---
+++ contributions/mehdidou99/Linux/File Management/2016-11-24.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Easy way of sharing files
