2d8750884aeca6be56004576fc6358b0d063fc5d
mistdrg <mistdrg@naver.com>
2016-11-24 15:29:16 +0300

    2016-11-23's Java workout

---
+++ contributions/mistdrg/Java/Error Handling/2016-11-23.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Most common reason behind **stack overflow** error
+Checking a double for `NaN` value
+Careful Numeric Data Conversions
