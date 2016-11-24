56a0964404ae4463a60f2e59c4c10815c0f72d9b
Epilox <sacha.benaroch@gmail.com>
2016-11-24 22:27:04 +0300

    2016-11-24's Java workout

---
+++ contributions/Epilox/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Generating random numbers
+Altering format string output by changing a format specifier's `argument_index`
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
+Using an interface as a parameter
