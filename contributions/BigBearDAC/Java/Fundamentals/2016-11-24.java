bfd25003703e10a70d333ecaebc2ad2ca73293ee
BigBearDAC <flidnibble@gmail.com>
2016-11-25 02:03:30 +0300

    2016-11-24's Java workout

---
+++ contributions/BigBearDAC/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Writing generic methods
+Use `DecimalFormat` class to format numbers
+Metadata: setting a file's owner
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
