69c34b8a21ea9f5fdd8d65e4ff6426638825b4b5
shysteph <stephanie@steph.tech>
2016-11-24 18:04:00 +0300

    2016-11-24's Java workout

---
+++ contributions/shysteph/Java/Error Handling/2016-11-24.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Use try-with-resources instead of `finally`
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
