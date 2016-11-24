a50250f8de331b67c0b5ee837f5f6bd4c2a0011d
aminembarki <aminembarki@gmail.com>
2016-11-25 02:10:32 +0300

    2016-11-24's JavaScript workout

---
+++ contributions/aminembarki/JavaScript/Browser APIs/2016-11-24.js
@@ -0,0 +1,5 @@
+Use `String.link` to create `<a>` tags without messy concatenation
+Drawing with HTML5 `Canvas` API
+Warn user if **Back** button is pressed
+Navigation Timing API
+FileReader.readAsDataURL()
