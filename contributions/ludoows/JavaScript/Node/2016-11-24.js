689382d02c62950858f131e94f58134854642068
ludoows <theartist768@gmail.com>
2016-11-24 09:53:33 +0300

    2016-11-24's JavaScript workout

---
+++ contributions/ludoows/JavaScript/Node/2016-11-24.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+`exports` vs. `module.exports` in **Node**
+`try-catch` only for **sync** code
