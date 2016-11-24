976ad756d2d36c588ae552a326bbcfdb655a98c3
erxyi <erxyi@safe-mail.net>
2016-11-24 20:16:40 +0300

    2016-11-24's Linux workout

---
+++ contributions/erxyi/Linux/Networking/2016-11-24.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Logging dropped firewall packets
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
+Short Introduction to `Wget` 
