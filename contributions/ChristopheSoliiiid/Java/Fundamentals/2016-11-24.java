4a26032f6d7528741ad1cd83f4c202c600d9685c
ChristopheSoliiiid <marin.christophe@outlook.fr>
2016-11-25 01:49:38 +0300

    2016-11-24's Java workout

---
+++ contributions/ChristopheSoliiiid/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: setting a file's owner
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
+Using an interface as a parameter
