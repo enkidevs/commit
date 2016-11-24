8ade7a3c7493382747b05d61d972f2f7293adc77
mara8castro <mara8castro@gmail.com>
2016-11-24 03:26:53 +0300

    2016-11-23's Java workout

---
+++ contributions/mara8castro/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Difference between JRE and JDK?
+The distinction between overloading and overriding methods
+The trade-off between short and descriptive names
+Metadata: creating a user-defined file attribute
