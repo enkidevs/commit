0b69b05094d5ec8bb0768c885fcb9513380b31a6
diego.andrade <diego.interativo@gmail.com>
2016-11-24 04:39:31 +0300

    2016-11-23's Java workout

---
+++ contributions/diego.andrade/Java/Data Types/2016-11-23.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Converting a string to upper or lower case
+Finding a substring in a string
+Equals operation on different data types
