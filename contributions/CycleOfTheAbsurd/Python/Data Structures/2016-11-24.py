9c3fe9f864db257234a27fd37f93ba478342cf30
CycleOfTheAbsurd <alex7up42@gmail.com>
2016-11-25 00:54:39 +0300

    2016-11-24's Python workout

---
+++ contributions/CycleOfTheAbsurd/Python/Data Structures/2016-11-24.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Special attributes of objects and classes
+`bytearray` objects
+Operations with `bytes` and `bytearray`
+A thread-safe `Queue`
