c7583e59373cef8d462b06e2eb924fe5316ac704
SkiFire13 <xxskifire13xx@gmail.com>
2016-11-24 09:58:21 +0300

    2016-11-24's Java workout

---
+++ contributions/SkiFire13/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Catch multiple exceptions in a single `catch` block
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
