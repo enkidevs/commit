394f7c07af29151743761e08aed2365507d5e7ef
Trevato <trevordobbertin@gmail.com>
2016-11-24 23:05:32 +0300

    2016-11-24's Java workout

---
+++ contributions/Trevato/Java/Data Types/2016-11-24.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Converting a string to upper or lower case
+Finding a substring in a string
+Do not attempt comparisons with NaN
