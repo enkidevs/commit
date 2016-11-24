94bd497eb64b0091d0de8dd5b063e594b89e8b19
syrioosh <mariuszsypek@gmail.com>
2016-11-24 12:49:05 +0300

    2016-11-24's Java workout

---
+++ contributions/syrioosh/Java/Data Types/2016-11-24.java
@@ -0,0 +1,5 @@
+Two ways to use an `Iterator`
+Use `Arrays.asList()` to initialise lists
+Converting numbers to strings
+Converting a string to upper or lower case
+Equals operation on different data types
