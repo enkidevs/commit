4da6bede4b5e88a100ff8c63861067192762e2fd
uanka <ankazeml97@gmail.com>
2016-11-25 00:27:41 +0300

    2016-11-24's JavaScript workout

---
+++ contributions/uanka/JavaScript/React/2016-11-24.js
@@ -0,0 +1,5 @@
+Functional Stateless Components in React
+Pixel values in style props for **React**
+`if`-`else` statements in **JSX** and **React**
+Keyed fragments in **React**
+`ref` callback attribute
