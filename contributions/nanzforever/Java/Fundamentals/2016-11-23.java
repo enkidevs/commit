21ad93ff7a32032a78c2ec0e8d8b7303472ff59d
nanzforever <prateek.football@gmail.com>
2016-11-24 09:43:09 +0300

    2016-11-23's Java workout

---
+++ contributions/nanzforever/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
+Supply `toString()` in all classes
+Using an interface as a parameter
