96289b1a8e4abf6bd31477028dbfca944a36c980
ArchKudo <vgd.vyas17@gmail.com>
2016-11-24 15:55:49 +0300

    2016-11-23's Java workout

---
+++ contributions/ArchKudo/Java/Classes & Objects/2016-11-23.java
@@ -0,0 +1,5 @@
+Use a copy constructor for non-immutable objects
+Using `static` fields
+Diamond Operator
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
