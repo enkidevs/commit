163b0d29cd80d746ea6fcf228278ab8b90f614fe
WillleePang <pangwilllee@hotmail.com>
2016-11-24 07:45:35 +0300

    2016-11-23's Java workout

---
+++ contributions/WillleePang/Java/Data Types/2016-11-23.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Multidimensional array declaration
+Converting a string to upper or lower case
