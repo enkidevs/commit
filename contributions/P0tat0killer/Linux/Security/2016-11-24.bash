75c8fb79f17cdead6fcbfe6b47c03694089cf974
P0tat0killer <dominik_meyer99@web.de>
2016-11-25 01:32:50 +0300

    2016-11-24's Linux workout

---
+++ contributions/P0tat0killer/Linux/Security/2016-11-24.bash
@@ -0,0 +1,5 @@
+Extracting the `public key` from the `private key`
+Disable SSH password authentication 
+Best password generation utilities
+Lockdown **Cronjobs**
+Disabling **USB** storage devices
