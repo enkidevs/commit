fde5c6c918689c4c3fbe69c516bb2b4551dc7937
aquincy <brennanjl003@gmail.com>
2016-11-24 22:52:04 +0300

    2016-11-24's Java workout

---
+++ contributions/aquincy/Java/Data Types/2016-11-24.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Two ways to use an `Iterator`
+Multidimensional array declaration
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
