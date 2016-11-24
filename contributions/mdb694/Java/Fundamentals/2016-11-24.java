11c79ce313cf5f3cfb49a9ea6ead3547ad9ec343
mdb694 <mdb69@hotmail.it>
2016-11-25 01:44:26 +0300

    2016-11-24's Java workout

---
+++ contributions/mdb694/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
+Using bounded type parameters in generic methods
+Measuring time
