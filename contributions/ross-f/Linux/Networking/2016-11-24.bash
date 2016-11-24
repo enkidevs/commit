d5a2d6392cf1f1c588fd405b444f193aa9cf536d
ross-f <rssfltchr@gmail.com>
2016-11-24 20:21:18 +0300

    2016-11-24's Linux workout

---
+++ contributions/ross-f/Linux/Networking/2016-11-24.bash
@@ -0,0 +1,5 @@
+Logging dropped firewall packets
+Find services running on your host
+Useful ssh client optimizations 
+Sending emails from terminal / scripts
+Getting the connection speed from the terminal
