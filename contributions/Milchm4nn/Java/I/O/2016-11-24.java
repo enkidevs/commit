8bfdc25c48c6fffe96b2865b11cf1e54595af233
Milchm4nn <luca.hengst@googlemail.com>
2016-11-24 08:51:17 +0300

    2016-11-24's Java workout

---
+++ contributions/Milchm4nn/Java/I/O/2016-11-24.java
@@ -0,0 +1,5 @@
+System independent path specification
+How to create directories in Java
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
