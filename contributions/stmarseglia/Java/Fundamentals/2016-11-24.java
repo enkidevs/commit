c1967ee5e7a4d1cb063bb518dc80471b0eda0130
stmarseglia <st.marseglia@gmail.com>
2016-11-24 10:45:55 +0300

    2016-11-24's Java workout

---
+++ contributions/stmarseglia/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
