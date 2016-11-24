c5bf8bb5625abd7f66e5a80e672947dfa098c548
durandj <james.durand@alumni.msoe.edu>
2016-11-24 18:03:01 +0300

    2016-11-24's Java workout

---
+++ contributions/durandj/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
