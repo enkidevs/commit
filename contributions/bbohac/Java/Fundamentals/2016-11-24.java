f6e2899d3242315b5efd20cc0894b3d6b07aa0f6
bbohac <bastian.bohac@gmail.com>
2016-11-24 14:56:19 +0300

    2016-11-24's Java workout

---
+++ contributions/bbohac/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Java naming conventions
+Using `Printstream.format` to format a string before printing
+Using bounded type parameters in generic methods
+How range operations work
