e9bb483064e1f46aab93802cc1a4d903279ab009
Lydia-D <lydia.drabsch@gmail.com>
2016-11-24 04:31:35 +0300

    2016-11-23's Java workout

---
+++ contributions/Lydia-D/Java/Fundamentals/2016-11-23.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Metadata: setting a file's owner
+Calling one constructor from another
+Avoid using `static` variables
+Using an interface as a parameter
