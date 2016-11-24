f26122ab8ecf3c29392f5c226381f80b6bd443f9
im_43 <lucastraba@gmail.com>
2016-11-24 19:14:03 +0300

    2016-11-24's Java workout

---
+++ contributions/im_43/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
