a94c12c6e28c332f0e53ea33f9928b31ad9d56bc
aidn5 <aidn.david5@gmail.com>
2016-11-25 01:59:35 +0300

    2016-11-24's JavaScript workout

---
+++ contributions/aidn5/JavaScript/Core/2016-11-24.js
@@ -0,0 +1,5 @@
+Types of numbers
+Hoisting applies only to variable declarations, not initializations
+Scope and strict mode
+Extra function arguments are undefined by default
+Inaccuracy of binary floating-point format
