af1a1667815e0fa7915a80a629cedb4a65ad6269
RWolfing <robert.wolfinger@gmx.net>
2016-11-24 10:28:01 +0300

    2016-11-24's Java workout

---
+++ contributions/RWolfing/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Updating interfaces by using `default` methods
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
