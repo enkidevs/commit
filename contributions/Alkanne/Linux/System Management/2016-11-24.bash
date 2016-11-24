7f32a093d3e938eb633b146711e52d9b6797f3c5
Alkanne <alkanne@gmail.com>
2016-11-25 00:41:45 +0300

    2016-11-24's Linux workout

---
+++ contributions/Alkanne/Linux/System Management/2016-11-24.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Change your `hostname` on systems using `systemd`
+Preventing a user from logging into the system
+Check the HDD with `badblocks`
+Executing scripts on system boot using `systemd`
