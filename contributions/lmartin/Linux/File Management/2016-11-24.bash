ff0bfb61a51e06bc446d9e5a3c00fd56964b289f
lmartin <larsmartin@gmx.de>
2016-11-24 20:32:22 +0300

    2016-11-24's Linux workout

---
+++ contributions/lmartin/Linux/File Management/2016-11-24.bash
@@ -0,0 +1,5 @@
+Converting between Unix and Windows text files
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
