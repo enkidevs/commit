242e03dd1cece4879948893e3ab9cc080b6b0e76
newlight77 <newlight77@gmail.com>
2016-11-24 11:31:44 +0300

    2016-11-24's Linux workout

---
+++ contributions/newlight77/Linux/File Management/2016-11-24.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Preserving permissions and structure with `rsync`
+Deleting files in a secure manner
