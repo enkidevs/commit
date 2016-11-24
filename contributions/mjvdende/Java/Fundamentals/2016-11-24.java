c9067dfdb2e243590b91c31b0c5f10b24d2e98f2
mjvdende <maartenvdende@gmail.com>
2016-11-24 18:35:36 +0300

    2016-11-24's Java workout

---
+++ contributions/mjvdende/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Ternary operator
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Using an interface as a parameter
