88adfb8f4be9130c5146463b3e937f8ad0749069
ramik91 <k.stebel@seznam.cz>
2016-11-24 10:57:57 +0300

    2016-11-24's JavaScript workout

---
+++ contributions/ramik91/JavaScript/Functions/2016-11-24.js
@@ -0,0 +1,5 @@
+Use the `apply` function to get the `min` or `max` of an array
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function expression
+`.bind()` 
+Easily generate a random `HEX` color
