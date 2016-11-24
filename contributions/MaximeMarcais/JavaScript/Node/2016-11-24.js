252828424736cf6032ed0ac9457b636d4615c4de
MaximeMarcais <maxime.marcais@gmail.com>
2016-11-24 10:48:05 +0300

    2016-11-24's JavaScript workout

---
+++ contributions/MaximeMarcais/JavaScript/Node/2016-11-24.js
@@ -0,0 +1,5 @@
+Asynchronous File Write/Read in Node.js
+First-error callbacks in **Node**
+Wrapping errors in Node.js using _node-verror_
+Manage child processes in **Node**
+Middleware/pipeline design pattern
