9fb59dc84ef52ffbbcb812b55abb9d59c39b758f
corpulentcoffee <dave@corpulentcoffee.com>
2016-11-24 20:47:19 +0300

    2016-11-24's Linux workout

---
+++ contributions/corpulentcoffee/Linux/System Management/2016-11-24.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Incorrect time on dual boot systems
+Check the HDD with `badblocks`
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
