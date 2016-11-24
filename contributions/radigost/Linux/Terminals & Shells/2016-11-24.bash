943418e67cb1c01790e2fb06890666eeed107b8b
radigost <nickolay.pol@gmail.com>
2016-11-24 09:09:04 +0300

    2016-11-24's Linux workout

---
+++ contributions/radigost/Linux/Terminals & Shells/2016-11-24.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Execute a command without saving it in history
+Adding directories to your `$PATH`
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
