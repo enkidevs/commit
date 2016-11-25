b507fe6c0d7db8be5c08cd696b7e7e8b184b4ff5
pseguel <pseguel@cisco.com>
2016-11-25 13:55:12 +0300

    2016-11-25's Java workout

---
+++ contributions/pseguel/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The trade-off between short and descriptive names
+Detect or prevent integer overflow
+`Autoboxing` and `Unboxing`
+Avoid using `static` variables
