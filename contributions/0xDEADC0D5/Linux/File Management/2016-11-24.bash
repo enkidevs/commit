9160e833e62aa24054447267b206324350698f07
0xDEADC0D5 <seductivethunderstorm@gmail.com>
2016-11-25 05:55:00 +0300

    2016-11-24's Linux workout

---
+++ contributions/0xDEADC0D5/Linux/File Management/2016-11-24.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
