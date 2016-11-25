18402991457712b9dd0c0bcd5a38038b9a0caec2
Sushant-M <sushant.post@gmail.com>
2016-11-25 16:53:48 +0300

    2016-11-24's Java workout

---
+++ contributions/Sushant-M/Java/Error Handling/2016-11-24.java
@@ -0,0 +1,5 @@
+Throwing proper exceptions for unfinished code
+The distinction between checked and unchecked exceptions
+Converting stack trace to a string
+Most common reason behind **stack overflow** error
+Careful Numeric Data Conversions
