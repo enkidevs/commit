8bf1293f81d4ef2d318b978b12ad2ae7225d7ffb
apwe <paweale@outlook.com>
2016-11-25 08:33:56 +0300

    2016-11-25's Linux workout

---
+++ contributions/apwe/Linux/System Management/2016-11-25.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Detect your linux distribution
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Monitor System Memory using `vmstat`
