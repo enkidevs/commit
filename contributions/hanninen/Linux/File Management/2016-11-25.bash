3e86ee5ea98771b1271b30e7fff9dc9b9d3004ea
hanninen <aki@hanninen.net>
2016-11-25 13:12:45 +0300

    2016-11-25's Linux workout

---
+++ contributions/hanninen/Linux/File Management/2016-11-25.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
