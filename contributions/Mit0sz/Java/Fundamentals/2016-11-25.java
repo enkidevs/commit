3da09aaeb68803b7c8dd018b8922a52865d87ddf
Mit0sz <artur.krawczyk93@gmail.com>
2016-11-25 09:25:11 +0300

    2016-11-25's Java workout

---
+++ contributions/Mit0sz/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Java naming conventions
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
+How range operations work
