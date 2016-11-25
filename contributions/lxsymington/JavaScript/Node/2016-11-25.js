7a0a7f7d1618574e38d31a663ab1d98e15212c06
lxsymington <lukexaviersymington@gmail.com>
2016-11-25 23:13:43 +0300

    2016-11-25's JavaScript workout

---
+++ contributions/lxsymington/JavaScript/Node/2016-11-25.js
@@ -0,0 +1,5 @@
+First-error callbacks in **Node**
+**Node** `os` module
+Wrapping errors in Node.js using _node-verror_
+Automatic compilation for Node with **Nodemon** 
+Manage child processes in **Node**
