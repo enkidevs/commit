82fa794f2c18eb2a0044d19d91853d400a85f976
a06kin <a06kin@gmail.com>
2016-11-26 00:41:14 +0300

    2016-11-25's Java workout

---
+++ contributions/a06kin/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
