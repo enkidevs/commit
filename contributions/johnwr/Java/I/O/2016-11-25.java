17c71f9bd315e20ca8e273a50dc5b5d83477af19
johnwr <johnwr_enki@wraalsen.no>
2016-11-25 11:22:14 +0300

    2016-11-25's Java workout

---
+++ contributions/johnwr/Java/I/O/2016-11-25.java
@@ -0,0 +1,5 @@
+System independent path specification
+`System.out` vs `System.err`
+Checking for the existence of files and directories
+List contents of a directory with `newDirectoryStream`
+Deleting files and directories
