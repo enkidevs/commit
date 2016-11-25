abe1818fee6e19fcc48f315f577e7c453cc7a7af
miki208 <miloss208@gmail.com>
2016-11-25 15:36:14 +0300

    2016-11-25's JavaScript workout

---
+++ contributions/miki208/JavaScript/Core/2016-11-25.js
@@ -0,0 +1,5 @@
+`if/else` shortcut `conditional operator`
+Most efficient way to build `HTML` strings
+Use `splice()` to remove an item from an array
+Infix operators are left-associative
+Prototype methods
