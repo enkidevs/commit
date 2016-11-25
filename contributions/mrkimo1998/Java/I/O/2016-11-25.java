167b62403a0295d06c3d08d214e28c7a456f9226
mrkimo1998 <mrkimo1998@protonmail.com>
2016-11-25 12:15:29 +0300

    2016-11-25's Java workout

---
+++ contributions/mrkimo1998/Java/I/O/2016-11-25.java
@@ -0,0 +1,5 @@
+System independent path specification
+Create system file paths using the `Path` class
+Using buffered streams
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
