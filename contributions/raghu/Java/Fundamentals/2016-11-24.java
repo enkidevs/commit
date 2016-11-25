c3b66eaac2776f35681003dd3fbb25464cab1dcc
raghu <raghu.9208@gmail.com>
2016-11-25 06:34:03 +0300

    2016-11-24's Java workout

---
+++ contributions/raghu/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Uses of the `final` keyword
+Using `Printstream.format` to format a string before printing
+Metadata: creating a user-defined file attribute
+How range operations work
