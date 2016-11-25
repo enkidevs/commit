381df7763b39b850f76a1f30adb9004b97984eb8
diegochine <chine.diego@gmail.com>
2016-11-25 09:55:15 +0300

    2016-11-25's Java workout

---
+++ contributions/diegochine/Java/I/O/2016-11-25.java
@@ -0,0 +1,5 @@
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
+Retrieving file store attributes
+The `Console` class
