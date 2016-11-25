1a1c50ee0661955ace12c1b346256a7fd41fe0c4
lDragl <ludo@ezyr.xyz>
2016-11-25 17:16:32 +0300

    2016-11-25's Linux workout

---
+++ contributions/lDragl/Linux/System Management/2016-11-25.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Check the HDD with `badblocks`
+Basic service management with `systemd`
+Using `dd` To Create Disk Images
