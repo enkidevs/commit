ea2a43ebb9ff689317d4b576d20178eff698b99a
MORT <mercenariomort@gmail.com>
2016-11-25 17:04:21 +0300

    2016-11-25's Linux workout

---
+++ contributions/MORT/Linux/Terminals & Shells/2016-11-25.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear the terminal instantly
+Find any Unix / Linux command
+Get to know  your commands with `type`
+Conditional command execution
+
+(`&&` operator)
