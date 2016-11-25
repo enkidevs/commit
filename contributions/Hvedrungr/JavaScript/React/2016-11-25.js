075c08ea51ae6d66c279fe5cd3d31074e1305f12
Hvedrungr <damien.dijol@gmail.com>
2016-11-26 01:26:30 +0300

    2016-11-25's JavaScript workout

---
+++ contributions/Hvedrungr/JavaScript/React/2016-11-25.js
@@ -0,0 +1,5 @@
+Self-closing tags in **JSX**
+Pixel values in style props for **React**
+`if`-`else` statements in **JSX** and **React**
+Using `false` in **JSX**
+Expose Component Functions in **React**
