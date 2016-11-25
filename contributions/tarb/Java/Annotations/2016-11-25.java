2fc7612a0fdd64d3f184af613cc4f8d639fa72ac
tarb <rtarbath@gmail.com>
2016-11-26 01:42:28 +0300

    2016-11-25's Java workout

---
+++ contributions/tarb/Java/Annotations/2016-11-25.java
@@ -0,0 +1,5 @@
+Use `@Ignore` to disable JUnit tests
+Using the `@Deprecated` annotation
+Including annotations in Javadoc
+Restricting an annotation's application by using `@Target`
+Declaring `@Repeatable` annotation types
