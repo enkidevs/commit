1092fcb582118343ca95a4ada3db7e9b7a4ba7ae
Everduin94 <everduin94@gmail.com>
2016-11-25 21:18:12 +0300

    2016-11-25's Java workout

---
+++ contributions/Everduin94/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
+Using an interface as a parameter
