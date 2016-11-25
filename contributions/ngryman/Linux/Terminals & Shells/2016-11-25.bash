9cac2801f42ed50274f2e2cc4b5827277bdd6649
ngryman <ngryman@gmail.com>
2016-11-25 19:26:52 +0300

    2016-11-25's Linux workout

---
+++ contributions/ngryman/Linux/Terminals & Shells/2016-11-25.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Quick `bash` shortcuts 
+Terminal based browser
+Adding directories to your `$PATH`
+Retrieve previous arguments
