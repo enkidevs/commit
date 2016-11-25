832ad56f42c9eed54267c0ae50c1788d43a98f5b
archligt <frodo.archligt@gmail.com>
2016-11-25 20:42:46 +0300

    2016-11-25's Java workout

---
+++ contributions/archligt/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use strings in a `switch` statement
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
