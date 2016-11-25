fb80324a186fa4021a3c80ff64d5fa8e3f01ae6e
Albebrando <brando1411@gmail.com>
2016-11-25 20:45:45 +0300

    2016-11-25's Java workout

---
+++ contributions/Albebrando/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
