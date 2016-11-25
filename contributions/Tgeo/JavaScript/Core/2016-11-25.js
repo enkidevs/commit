97041af903d3066745ad4c8be17996f1220e7f63
Tgeo <tylergeonetta@gmail.com>
2016-11-25 19:58:54 +0300

    2016-11-25's JavaScript workout

---
+++ contributions/Tgeo/JavaScript/Core/2016-11-25.js
@@ -0,0 +1,5 @@
+Get a random item from an Array
+Double and single quotes
+Apply `map` function to array items
+The difference between null, undefined and NaN
+Extra function arguments are undefined by default
