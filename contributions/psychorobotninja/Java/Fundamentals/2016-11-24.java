9b8c3a00ee49824fbb84acd97493150f4519d436
psychorobotninja <tefuller@gmail.com>
2016-11-25 08:42:22 +0300

    2016-11-24's Java workout

---
+++ contributions/psychorobotninja/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Increase `PermGen` space as to avoid `OutOfMemory` errors
