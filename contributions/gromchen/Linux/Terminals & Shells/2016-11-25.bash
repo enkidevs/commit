77fb848b4f2f3a4fc6618a085cdca575efe1a1ed
gromchen <gromchen@outlook.com>
2016-11-25 21:49:46 +0300

    2016-11-25's Linux workout

---
+++ contributions/gromchen/Linux/Terminals & Shells/2016-11-25.bash
@@ -0,0 +1,5 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear bash history
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
