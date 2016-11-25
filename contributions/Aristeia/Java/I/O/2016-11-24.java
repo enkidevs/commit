6c45761a5c9c531670aeadeca59c1da4a6d2de08
Aristeia <11025x12@gmail.com>
2016-11-25 03:36:57 +0300

    2016-11-24's Java workout

---
+++ contributions/Aristeia/Java/I/O/2016-11-24.java
@@ -0,0 +1,5 @@
+System independent path specification
+`System.out` vs `System.err`
+How to create directories in Java
+Reading and writing text files
+Checking for the existence of files and directories
