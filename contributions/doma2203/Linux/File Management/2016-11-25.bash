4ea962bf4a756ae630d859f7f6b58a1b57aa5281
doma2203 <dominika.kalafut@onet.eu>
2016-11-26 02:37:19 +0300

    2016-11-25's Linux workout

---
+++ contributions/doma2203/Linux/File Management/2016-11-25.bash
@@ -0,0 +1,5 @@
+Finding file with regexes
+Truncate files with `cat` instead of `rm`
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
+Find the unknown process preventing deleting of files
