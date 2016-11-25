6deca6c89bce18d5ea038490e42bf9f4eba78b48
cmcsun <ggmethos@autistici.org>
2016-11-25 23:53:25 +0300

    2016-11-25's Linux workout

---
+++ contributions/cmcsun/Linux/Networking/2016-11-25.bash
@@ -0,0 +1,5 @@
+Logging dropped firewall packets
+Multiple IP Address with aliasing 
+Disable `IPv6`
+Useful ssh client optimizations 
+Staying connected remotely via SSH without an interactive shell
