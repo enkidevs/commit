c8db0820f0350b70ada99b1bdefaa826c4ddee81
geniusinfrance <ryan.bergerson@gmail.com>
2016-11-25 11:11:02 +0300

    2016-11-25's Java workout

---
+++ contributions/geniusinfrance/Java/Data Types/2016-11-25.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Use primitive types instead of wrapper classes when possible
+Converting numbers to strings
+Finding a substring in a string
+`StringBuffer` vs `StringBuilder`
