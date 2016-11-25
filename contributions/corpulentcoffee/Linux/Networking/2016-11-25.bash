9ae7a8a49c4a1972e8e2e381334fa4936d62cb61
corpulentcoffee <dave@corpulentcoffee.com>
2016-11-25 22:01:07 +0300

    2016-11-25's Linux workout

---
+++ contributions/corpulentcoffee/Linux/Networking/2016-11-25.bash
@@ -0,0 +1,5 @@
+Proxifying `ssh` connections
+Get real network statistics with `slurm`
+Setting up password-free authentication
+Getting the connection speed from the terminal
+Make use of `netstat`
