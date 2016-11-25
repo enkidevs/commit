4863f309fe7cfd8ce28e21229fc99c0bed3799b3
irian <chiara.santagiuliana@gmail.com>
2016-11-25 09:45:16 +0300

    2016-11-25's Java workout

---
+++ contributions/irian/Java/Data Types/2016-11-25.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Using `enum` in Java
+Converting a string to upper or lower case
+`StringBuffer` vs `StringBuilder`
+Retrieve the component type of an array
