4bd4475cf7d331a81a6999e3fd2e12a485745165
bendobot <whatsbending@gmail.com>
2016-11-25 05:41:26 +0300

    2016-11-24's Linux workout

---
+++ contributions/bendobot/Linux/Terminals & Shells/2016-11-24.bash
@@ -0,0 +1,7 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear bash history
+Find any Unix / Linux command
+Random password generator
+Conditional command execution
+
+(`&&` operator)
