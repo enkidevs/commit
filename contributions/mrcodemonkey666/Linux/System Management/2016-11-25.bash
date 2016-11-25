ed3d77d9f64a0c280640c2f9111bdacddded8dbc
mrcodemonkey666 <c.t.martin@btinternet.com>
2016-11-25 21:38:35 +0300

    2016-11-25's Linux workout

---
+++ contributions/mrcodemonkey666/Linux/System Management/2016-11-25.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
+Monitor System Memory using `vmstat`
