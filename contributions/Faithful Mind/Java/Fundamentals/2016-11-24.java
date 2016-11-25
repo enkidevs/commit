ea85479327f9c0010aefa8a5a851eb5338fe1ebe
Faithful Mind <lpyy15@gmail.com>
2016-11-25 19:25:55 +0300

    2016-11-24's Java workout

---
+++ contributions/Faithful Mind/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use strings in a `switch` statement
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Increase `PermGen` space as to avoid `OutOfMemory` errors
