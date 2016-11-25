d334b96203d4a57f799763468a62574f72162e64
pshkrjshnde <neophiliacpush@gmail.com>
2016-11-25 18:22:38 +0300

    2016-11-24's Linux workout

---
+++ contributions/pshkrjshnde/Linux/Data Manipulation/2016-11-24.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+Using `lynx` to convert HTML to text
+Count non-empty lines with `nl` utility
+Image manipulation  using `convert`
+`sort` lines in a text file
