34c5f6ef7be29499b3cfb9c6320faf9a204505e3
jaschweder <jonathanschweder@gmail.com>
2016-11-26 00:34:24 +0300

    2016-11-25's Git workout

---
+++ contributions/jaschweder/Git/Essentials/2016-11-25.bash
@@ -0,0 +1,5 @@
+Adding a remote repository
+Pushing tags to a server
+How to set your username and email
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
