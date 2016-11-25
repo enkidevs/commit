739f661fc75ac94084ef330fda75894c1096f69a
christiangroth <chr.groth@gmx.de>
2016-11-25 12:30:03 +0300

    2016-11-25's Git workout

---
+++ contributions/christiangroth/Git/Essentials/2016-11-25.bash
@@ -0,0 +1,5 @@
+Using tags for version control
+How to set your username and email
+Make your log output pretty
+Remote repositories: viewing, editing and deleting
+Remote repositories: fetching and pushing
