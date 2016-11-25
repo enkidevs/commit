0625ac579ae48f0606b24659856dddfdf476172f
andyharmon <andyharmon@msn.com>
2016-11-25 10:58:34 +0300

    2016-11-25's Java workout

---
+++ contributions/andyharmon/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Writing generic methods
+Metadata: setting a file's owner
+Using an interface as a parameter
