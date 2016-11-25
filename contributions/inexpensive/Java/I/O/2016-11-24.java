01a51e9643707e2807000a31181497709a1e39e7
inexpensive <lawrence.bouzane@gmail.com>
2016-11-25 03:47:07 +0300

    2016-11-24's Java workout

---
+++ contributions/inexpensive/Java/I/O/2016-11-24.java
@@ -0,0 +1,5 @@
+System independent path specification
+`System.out` vs `System.err`
+Reading and writing text files
+Checking for the existence of files and directories
+Deleting files and directories
