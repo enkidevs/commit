6463e87bcd4d7964fd0a5c43414090e7b3169d89
slewiskelly <scott.lewiskelly@gmail.com>
2016-11-25 06:23:41 +0300

    2016-11-24's Python workout

---
+++ contributions/slewiskelly/Python/Data Structures/2016-11-24.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Get the most of `int`s
+Keep things in order with `OrderedDict`
+Implementing **weak** references
+`weakref` proxies
