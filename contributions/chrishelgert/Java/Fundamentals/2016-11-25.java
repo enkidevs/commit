103ddab32fd9ec3cc6432237187ced38a317fcde
chrishelgert <chris.helgert@yahoo.com>
2016-11-25 16:32:42 +0300

    2016-11-25's Java workout

---
+++ contributions/chrishelgert/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+The trade-off between short and descriptive names
+Do not perform bitwise and arithmetic operations on the same data
+`Autoboxing` and `Unboxing`
+Avoid using `static` variables
