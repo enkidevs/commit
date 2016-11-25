280bd041156fc1124e78c3dce200a20898c6d373
nimmio <benwilhelm@web.de>
2016-11-25 08:35:20 +0300

    2016-11-25's JavaScript workout

---
+++ contributions/nimmio/JavaScript/Core/2016-11-25.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Double and single quotes
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+Inaccuracy of binary floating-point format
