2c01d1172dd2571376955dbddb8056cee4589899
kSom <kgsommer@gmail.com>
2016-11-25 16:13:25 +0300

    2016-11-25's Java workout

---
+++ contributions/kSom/Java/Data Types/2016-11-25.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Two ways to use an `Iterator`
+Multidimensional array declaration
+Retrieve the component type of an array
+Do not attempt comparisons with NaN
