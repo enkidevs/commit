15f6613de5990e5c655cd36d0358caedf4732719
amarradi <radischm@gmail.com>
2016-11-25 12:12:57 +0300

    2016-11-25's Java workout

---
+++ contributions/amarradi/Java/Data Types/2016-11-25.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Converting a string to upper or lower case
+Finding a substring in a string
+Equals operation on different data types
+Numeric Conversion - Widening
