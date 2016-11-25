58d0cf8a0a81a3a25247b509ffefe584880dafc4
ruoptic <stroke.the.cat.again@gmail.com>
2016-11-25 19:46:59 +0300

    2016-11-25's Java workout

---
+++ contributions/ruoptic/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Use strings in a `switch` statement
+Using Inheritance to reduce code repetition
+Updating interfaces by using `default` methods
+Using `Printstream.format` to format a string before printing
