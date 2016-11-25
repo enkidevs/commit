0e12c862885c7c5a4bcca1f1a3852e68edabab83
Glorken <jackelofalltrades@gmail.com>
2016-11-25 17:55:12 +0300

    2016-11-25's JavaScript workout

---
+++ contributions/Glorken/JavaScript/Core/2016-11-25.js
@@ -0,0 +1,5 @@
+Data type comparison in `switch` statements
+The difference between null, undefined and NaN
+Extra function arguments are undefined by default
+`console.*` in JavaScript
+Base conversion 
