c3aa27d5bda9b4a2f49cb7d46c5c6a71b6d6d169
bendobot <whatsbending@gmail.com>
2016-11-26 02:11:01 +0300

    2016-11-25's Linux workout

---
+++ contributions/bendobot/Linux/Networking/2016-11-25.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Logging dropped firewall packets
+Useful ssh client optimizations 
+Staying connected remotely via SSH without an interactive shell
+Short Introduction to `Wget` 
