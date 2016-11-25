411b083c6a03db9803e1dee83f578ddba863f1b2
BOSSoNe0013 <bossone0013@gmail.com>
2016-11-25 16:32:50 +0300

    2016-11-25's Linux workout

---
+++ contributions/BOSSoNe0013/Linux/System Management/2016-11-25.bash
@@ -0,0 +1,5 @@
+List currently logged in users
+Removing old kernels in Debian based systems
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Basic service management with `systemd`
