b3e7d782d5cc449b836319f9b55bc2209413f4a4
fassn <christopher.fargere@gmail.com>
2016-11-25 12:09:42 +0300

    2016-11-25's Linux workout

---
+++ contributions/fassn/Linux/Security/2016-11-25.bash
@@ -0,0 +1,5 @@
+Extracting the `public key` from the `private key`
+Disable SSH password authentication 
+Lockdown **Cronjobs**
+Disabling **USB** storage devices
+Private key encryption with `OpenSSL`
