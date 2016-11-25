74d5ceab998d05e466782884d032ce52af09696c
Predatoro01 <flori.wimmer@gmx.de>
2016-11-25 14:27:59 +0300

    2016-11-25's Java workout

---
+++ contributions/Predatoro01/Java/Error Handling/2016-11-25.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Using specific exception types in the `throws` clause
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Checking a double for `NaN` value
