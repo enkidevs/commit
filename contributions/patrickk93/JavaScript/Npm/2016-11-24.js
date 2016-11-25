9904d495d472d01e35b7cc09bddac21b891d8044
patrickk93 <patrickkim93@gmail.com>
2016-11-25 18:27:57 +0300

    2016-11-24's JavaScript workout

---
+++ contributions/patrickk93/JavaScript/Npm/2016-11-24.js
@@ -0,0 +1,5 @@
+Registry user accounts for npm
+List binaries for scripting in npm
+`npm` verifies `sha1` hashes of packages
+`optionalDependencies` in npm
+Lock down dependency versions by shrinkwrapping
