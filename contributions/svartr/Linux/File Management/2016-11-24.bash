028262c2848c23940a4bca6334c695dac47564bb
svartr <the.trash.collector@yandex.com>
2016-11-25 11:29:29 +0300

    2016-11-24's Linux workout

---
+++ contributions/svartr/Linux/File Management/2016-11-24.bash
@@ -0,0 +1,5 @@
+Preserving permissions and structure with `rsync`
+Using `touch` to alter files modification time
+Working with `.gz` files
+Find the unknown process preventing deleting of files
+Easy way of sharing files
