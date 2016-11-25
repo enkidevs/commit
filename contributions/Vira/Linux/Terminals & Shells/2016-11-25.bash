916cdfac1c23af362294e984d7aa531268c2abb3
Vira <thomas.ruehrig@studierende.htl-donaustadt.at>
2016-11-25 19:14:58 +0300

    2016-11-25's Linux workout

---
+++ contributions/Vira/Linux/Terminals & Shells/2016-11-25.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+Keep useful commands in your shell history with tags
