4b2bed205ee6f6eb362c17daa5a2e0e6fdfe438d
wilsonhipolito <wilson.hipolito@vivareal.com>
2016-11-25 15:01:45 +0300

    2016-11-25's Linux workout

---
+++ contributions/wilsonhipolito/Linux/Terminals & Shells/2016-11-25.bash
@@ -0,0 +1,7 @@
+Bash history reverse search
+Execute a command without saving it in history
+Conditional command execution
+
+(`&&` operator)
+Sequential execution using the `;` statement separator
+The Basics of Environment Variables
