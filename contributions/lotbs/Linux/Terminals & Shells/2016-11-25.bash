0b5b847efa3bea9603aae6797ab2500e81e62a2f
lotbs <nkosindlovu87@gmail.com>
2016-11-25 19:17:35 +0300

    2016-11-25's Linux workout

---
+++ contributions/lotbs/Linux/Terminals & Shells/2016-11-25.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear bash history
+Adding directories to your `$PATH`
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
