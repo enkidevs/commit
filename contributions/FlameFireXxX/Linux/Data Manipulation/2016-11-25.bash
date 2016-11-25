2da971463a569ba545848af308e5de1b3ec6fba4
FlameFireXxX <lyndon.lapierre@gmail.com>
2016-11-26 01:57:23 +0300

    2016-11-25's Linux workout

---
+++ contributions/FlameFireXxX/Linux/Data Manipulation/2016-11-25.bash
@@ -0,0 +1,5 @@
+Cleaning up comments on config files with `grep` 
+Get rid of duplicate lines
+Using `lynx` to convert HTML to text
+Count non-empty lines with `nl` utility
+Clean configuration files
