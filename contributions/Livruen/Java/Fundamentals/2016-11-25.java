619a3913d98d7b63db07dac0c42c0b2cee33e89b
Livruen <kiya.natasza@gmail.com>
2016-11-25 13:16:41 +0300

    2016-11-25's Java workout

---
+++ contributions/Livruen/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Floating garbage and how to deal with it
+Do not perform bitwise and arithmetic operations on the same data
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
