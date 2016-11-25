d1534751780e52335cadabfe5d921da449108da2
MogaSoFly <motinger13@gmail.com>
2016-11-26 01:26:05 +0300

    2016-11-25's Java workout

---
+++ contributions/MogaSoFly/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between `==` and `equals()`
+Altering format string output by changing a format specifier's `argument_index`
+Metadata: setting a file's owner
+Using an interface as a parameter
