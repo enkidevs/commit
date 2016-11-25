909eb200cb6299cbb3096c2cd0f1a9994a58235a
ManInBoat <xstl2320284gnibuzo@163.com>
2016-11-25 04:58:33 +0300

    2016-11-24's JavaScript workout

---
+++ contributions/ManInBoat/JavaScript/Functions/2016-11-24.js
@@ -0,0 +1,5 @@
+Hoisting
+Function constructor vs. function expression
+Difference between **.call** and **.apply** methods
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
