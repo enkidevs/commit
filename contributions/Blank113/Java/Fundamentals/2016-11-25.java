08de2e37c6e4f58af5fac10fb18a4105534fec50
Blank113 <hbaldwin.services+1@gmail.com>
2016-11-25 22:11:22 +0300

    2016-11-25's Java workout

---
+++ contributions/Blank113/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The trade-off between short and descriptive names
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Limit Accessibility of `Fields`
