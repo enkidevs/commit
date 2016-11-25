2e8836ceff4f80c4d130960ee46833ec753c98da
olpop <o.registrator@gmail.com>
2016-11-25 10:37:06 +0300

    2016-11-25's Linux workout

---
+++ contributions/olpop/Linux/File Management/2016-11-25.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Deleting files in a secure manner
+Find the unknown process preventing deleting of files
