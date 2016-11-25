2602575c8e050217867bf1e215738283462d25cb
joernflath <joern.flath@gmail.com>
2016-11-25 10:00:01 +0300

    2016-11-25's Java workout

---
+++ contributions/joernflath/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Short-circuit evaluation
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Java naming conventions
