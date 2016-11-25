b429c476e49b4fcd157936c7b8c944b81b8fe29d
TotKindGott <konstantin.a.kolosov@gmail.com>
2016-11-25 14:38:02 +0300

    2016-11-25's Linux workout

---
+++ contributions/TotKindGott/Linux/Terminals & Shells/2016-11-25.bash
@@ -0,0 +1,5 @@
+Execute a command without saving it in history
+Clear the terminal instantly
+Clear bash history
+Breaking out of a terminal when `ssh` locks
+`else` statements using the `||` operator 
