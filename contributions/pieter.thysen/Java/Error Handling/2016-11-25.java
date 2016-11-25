80f7fa6c1d41235f52a4f40f7f7dd015fff5c25a
pieter.thysen <pieter.thysen@gmail.com>
2016-11-25 13:08:20 +0300

    2016-11-25's Java workout

---
+++ contributions/pieter.thysen/Java/Error Handling/2016-11-25.java
@@ -0,0 +1,5 @@
+Be as specific as possible when catching exceptions
+Converting stack trace to a string
+Most common reason behind **stack overflow** error
+Throwing an `exception`
+Careful Numeric Data Conversions
