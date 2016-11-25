e92b293e380346f337b8cca464d9c6f734c88ada
Drnkn <prehistoric2003@gmail.com>
2016-11-25 09:23:09 +0300

    2016-11-25's Java workout

---
+++ contributions/Drnkn/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Uses of the `final` keyword
+Catch multiple exceptions in a single `catch` block
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
