cad52e2474fbb26e98e9c73a68ce6aae0879e255
raftel.ti <raftel.ti@gmail.com>
2016-11-25 04:49:18 +0300

    2016-11-24's Java workout

---
+++ contributions/raftel.ti/Java/Fundamentals/2016-11-24.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Metadata: setting a file's owner
+There is no such thing as *pass-by-reference* in Java
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Using an interface as a parameter
