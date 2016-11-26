4f3d83496abb0469ed68aa88e28adefa8492748a
konst <kosnar@gmail.com>
2016-11-26 06:06:05 +0300

    2016-11-25's JavaScript workout

---
+++ contributions/konst/JavaScript/Core/2016-11-25.js
@@ -0,0 +1,5 @@
+Be careful when using `typeof`.
+Milliseconds since Epoch
+Setting default values with `short circuiting`
+Performing a function at timed intervals
+Inaccuracy of binary floating-point format
