7519d901871dcf07d8db00dd281caa876296dd75
Derjablik <attronach@gmail.com>
2016-11-26 13:40:38 +0300

    2016-11-26's Java workout

---
+++ contributions/Derjablik/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use meaningful names
+Common mistake on switch statements
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
