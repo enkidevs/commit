018b1447f29969d935c69ffc8ea24d930cb33262
gardir <gardiz@gmail.com>
2016-11-26 17:21:10 +0300

    2016-11-26's Python workout

---
+++ contributions/gardir/Python/Data Structures/2016-11-26.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+When `range` comes in handy
+Best way to implement a simple `queue` 
+Get the most of `float`s
+`queue`s and threads
