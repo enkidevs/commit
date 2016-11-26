6622fd74357b827a33b95e57816af2a3f9fc6fb6
SnikSnak <kozboko@gmail.com>
2016-11-26 21:34:19 +0300

    2016-11-26's Java workout

---
+++ contributions/SnikSnak/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Writing generic methods
+Metadata: creating a user-defined file attribute
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
