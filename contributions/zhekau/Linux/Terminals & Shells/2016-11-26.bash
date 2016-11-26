1b2a774b67df6a80594818ff01807ec188c3f596
zhekau <zhekau.system@gmail.com>
2016-11-26 16:52:32 +0300

    2016-11-26's Linux workout

---
+++ contributions/zhekau/Linux/Terminals & Shells/2016-11-26.bash
@@ -0,0 +1,7 @@
+Execute a command without saving it in history
+Terminal incognito mode
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Retrieve previous arguments
