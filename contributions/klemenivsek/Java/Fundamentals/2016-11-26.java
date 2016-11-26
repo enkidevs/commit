2e67fc4ea6fbabc676ff7801f5d3db7036470173
klemenivsek <klemen.ivsek@gmail.com>
2016-11-26 05:54:49 +0300

    2016-11-26's Java workout

---
+++ contributions/klemenivsek/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
