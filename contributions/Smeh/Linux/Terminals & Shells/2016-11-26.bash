d8f1c42da47419d808dbc1f3f6862c971db458e8
Smeh <sgarcia030@gmail.com>
2016-11-26 17:49:46 +0300

    2016-11-26's Linux workout

---
+++ contributions/Smeh/Linux/Terminals & Shells/2016-11-26.bash
@@ -0,0 +1,5 @@
+Bash history reverse search
+Rapidly invoke an editor to write a long, complex, or tricky command
+Terminal based browser
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
