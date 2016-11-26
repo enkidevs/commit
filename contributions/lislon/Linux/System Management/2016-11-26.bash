0e8b04a55b7c565aaaeadc0a3b6c990da8b96532
lislon <lislon@mail.ru>
2016-11-26 22:26:45 +0300

    2016-11-26's Linux workout

---
+++ contributions/lislon/Linux/System Management/2016-11-26.bash
@@ -0,0 +1,6 @@
+Revision control of configuration files with git
+Force a time update with `ntp`
+
+Removing old kernels in Debian based systems
+Incorrect time on dual boot systems
+Fixing `locale` issues in Debian systems
