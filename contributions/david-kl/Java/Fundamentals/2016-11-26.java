fa03bbe373741e7a483e5cbecb32a5201338600e
david-kl <daviddd.kl@gmail.com>
2016-11-26 23:00:50 +0300

    2016-11-26's Java workout

---
+++ contributions/david-kl/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Java naming conventions
+Use `DecimalFormat` class to format numbers
+Detect or prevent integer overflow
+`Autoboxing` and `Unboxing`
