c3abc8f27ea4f28320b7d3ad0942e8151eeb0592
vkopanja <vkopanja@gmail.com>
2016-11-26 11:51:10 +0300

    2016-11-26's Git workout

---
+++ contributions/vkopanja/Git/Essentials/2016-11-26.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+How to unstage a staged file
+Adding a remote repository
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
