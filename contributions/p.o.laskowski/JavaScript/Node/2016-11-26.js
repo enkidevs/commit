4faa2542c468c9822b61a1c6e84ef6851871f50a
p.o.laskowski <p.o.laskowski@gmail.com>
2016-11-26 22:29:07 +0300

    2016-11-26's JavaScript workout

---
+++ contributions/p.o.laskowski/JavaScript/Node/2016-11-26.js
@@ -0,0 +1,5 @@
+Custom Node REPL Server
+Global Objects and Environment Variables in **Node**
+Use conventional property names for error handling in Node.js
+Handle `JSON.parse` error in Node.js
+`uncaughtException` listener in Node.js
