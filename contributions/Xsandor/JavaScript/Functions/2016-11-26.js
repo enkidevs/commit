7bf90c691fa45147d7dcbb2ee17c9e65899ff360
Xsandor <alex@karlborgselkontroll.se>
2016-11-26 15:43:04 +0300

    2016-11-26's JavaScript workout

---
+++ contributions/Xsandor/JavaScript/Functions/2016-11-26.js
@@ -0,0 +1,5 @@
+Difference between **.call** and **.apply** methods
+Move cursor at the end of text input
+Social sharing without widgets
+Check if a document is done loading
+Changing a functions context with `fn.call(object)`
