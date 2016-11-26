4b6d2ad70da9d1af9179cb3e8d13aa6a116ce5e9
geniusinfrance <ryan.bergerson@gmail.com>
2016-11-26 10:40:52 +0300

    2016-11-26's Java workout

---
+++ contributions/geniusinfrance/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Updating interfaces by using `default` methods
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+How range operations work
