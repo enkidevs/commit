832247b7fc5705ee632b6783bf2fbe323327969c
Reyes <tred27@gmail.com>
2016-11-26 18:04:21 +0300

    2016-11-26's Linux workout

---
+++ contributions/Reyes/Linux/File Management/2016-11-26.bash
@@ -0,0 +1,7 @@
+Truncate files with `cat` instead of `rm`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
