7a2f3e273e5489cb0ff6538800386854cb506e2d
jacobvarner <jacob.varner14@gmail.com>
2016-11-27 00:07:14 +0300

    2016-11-26's Java workout

---
+++ contributions/jacobvarner/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+`Autoboxing` and `Unboxing`
+Using an interface as a parameter
