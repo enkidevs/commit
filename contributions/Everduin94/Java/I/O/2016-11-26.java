0b8d217baad7646d2dfcfdf5b41aca4ab677e486
Everduin94 <everduin94@gmail.com>
2016-11-26 21:33:17 +0300

    2016-11-26's Java workout

---
+++ contributions/Everduin94/Java/I/O/2016-11-26.java
@@ -0,0 +1,5 @@
+System independent path specification
+Create system file paths using the `Path` class
+Using buffered streams
+Reading file attributes
+List contents of a directory with `newDirectoryStream`
