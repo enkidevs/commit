d6445c700da1f5d3d689a882389a2cec663c7421
matapiojo <oyarzun.nicolas@gmail.com>
2016-11-26 15:59:47 +0300

    2016-11-26's Java workout

---
+++ contributions/matapiojo/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Generating random numbers
+Java naming conventions
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
