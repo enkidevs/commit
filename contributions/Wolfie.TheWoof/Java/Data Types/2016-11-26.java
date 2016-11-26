167d4c6421567b6bc41ff6ad1aa55205ff952dfd
Wolfie.TheWoof <izzymacster@gmail.com>
2016-11-26 20:48:58 +0300

    2016-11-26's Java workout

---
+++ contributions/Wolfie.TheWoof/Java/Data Types/2016-11-26.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Converting numbers to strings
+Converting a string to upper or lower case
+Equals operation on different data types
+Do not attempt comparisons with NaN
