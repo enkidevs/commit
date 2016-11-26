37ac9ee2032ae768dd927219eb041d53b347ddc6
CycleOfTheAbsurd <alex7up42@gmail.com>
2016-11-26 23:40:18 +0300

    2016-11-26's Python workout

---
+++ contributions/CycleOfTheAbsurd/Python/Data Structures/2016-11-26.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Best way to implement a simple `queue` 
+Working with `set`s
+`set` operations
+Operations with `bytes` and `bytearray`
