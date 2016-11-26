fb7e4638cd5fa5a58ccff89b0fb8c96a51b2aeb2
Angelmenam <angelmenam@hotmail.com>
2016-11-26 03:09:26 +0300

    2016-11-25's Java workout

---
+++ contributions/Angelmenam/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+The distinction between overloading and overriding methods
+Catch multiple exceptions in a single `catch` block
+Detect or prevent integer overflow
+Using an interface as a parameter
