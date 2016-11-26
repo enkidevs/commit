9bda9932f022a8fd66fc965ec4c0d28f8d5af541
ikervagyok <ikervagyok@gmail.com>
2016-11-26 13:51:04 +0300

    2016-11-26's Java workout

---
+++ contributions/ikervagyok/Java/Data Types/2016-11-26.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Do not attempt comparisons with NaN
+Numeric Conversion - Widening
