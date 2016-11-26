69a3e1f4a5d66b48c00b395a562c7730ec00dd12
manuelb <berger.manuel.d@gmail.com>
2016-11-26 23:38:18 +0300

    2016-11-26's Python workout

---
+++ contributions/manuelb/Python/Data Manipulation/2016-11-26.py
@@ -0,0 +1,5 @@
+Three ways to substitute a substring of a string
+Pythonic substring testing
+Use `struct` to work with Binary Data
+Compare sequences with `difflib`
+Get the similarity ratio of two sequences
