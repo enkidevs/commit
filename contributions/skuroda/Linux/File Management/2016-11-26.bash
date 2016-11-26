2b25f2fa384425e8327842c1d8cbe4f162f5b33e
skuroda <sk@mieco.org>
2016-11-27 02:37:15 +0300

    2016-11-26's Linux workout

---
+++ contributions/skuroda/Linux/File Management/2016-11-26.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Monitor the progress of data through a pipe with `pv`
+Using `touch` to alter files modification time
+Find the unknown process preventing deleting of files
+Easy way of sharing files
