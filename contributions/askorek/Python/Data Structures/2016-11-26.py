a763c463d7ca7acae4ef42adc45535681d94adec
askorek <askorek@gmail.com>
2016-11-27 01:07:32 +0300

    2016-11-26's Python workout

---
+++ contributions/askorek/Python/Data Structures/2016-11-26.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+When `range` comes in handy
+Operations with `bytes` and `bytearray`
+How to count
+Binary record packing with `bytearray`
