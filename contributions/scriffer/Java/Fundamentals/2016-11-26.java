e007d995e618716a20c46298ffbd38cd99516802
scriffer <kallekarloz@gmail.com>
2016-11-26 17:46:46 +0300

    2016-11-26's Java workout

---
+++ contributions/scriffer/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
