656e3a6a7da5ee35c01702c1d5d2ecd0bb3de2e2
joernflath <joern.flath@gmail.com>
2016-11-26 12:00:33 +0300

    2016-11-26's Java workout

---
+++ contributions/joernflath/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Generating random numbers
+Altering format string output by changing a format specifier's `argument_index`
+Using bounded type parameters in generic methods
+Constant notation
+Do not perform bitwise and arithmetic operations on the same data
