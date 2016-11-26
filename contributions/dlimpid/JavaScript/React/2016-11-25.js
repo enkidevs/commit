ac28d2ab8f2c04d9cd6eeb2bf52482098a03f2d7
dlimpid <dlimpid@gmail.com>
2016-11-26 09:02:31 +0300

    2016-11-25's JavaScript workout

---
+++ contributions/dlimpid/JavaScript/React/2016-11-25.js
@@ -0,0 +1,6 @@
+`if`-`else` statements in **JSX** and **React**
+componentWillReceiveProps Not Triggered After Mounting
+
+Expose Component Functions in **React**
+Enhanced Prop Validation 
+Spread attributes
