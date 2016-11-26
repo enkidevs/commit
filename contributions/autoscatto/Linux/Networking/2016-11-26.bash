0cf582891d0ff07ad68326f7490827b694c8a20c
autoscatto <4utoscatto@gmail.com>
2016-11-26 12:07:28 +0300

    2016-11-26's Linux workout

---
+++ contributions/autoscatto/Linux/Networking/2016-11-26.bash
@@ -0,0 +1,5 @@
+SSH tunneling made easy
+Logging dropped firewall packets
+Check `iptables` firewall status
+Setting up password-free authentication
+Staying connected remotely via SSH without an interactive shell
