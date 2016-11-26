d1eb98c5821732f7254ea4dacd096bc3495f5068
lillemorb <lillemorb@gmail.com>
2016-11-26 13:17:35 +0300

    2016-11-26's Java workout

---
+++ contributions/lillemorb/Java/Data Types/2016-11-26.java
@@ -0,0 +1,5 @@
+Collections vs arrays
+Use `Arrays.asList()` to initialise lists
+Converting a string to upper or lower case
+Equals operation on different data types
+Retrieve the component type of an array
