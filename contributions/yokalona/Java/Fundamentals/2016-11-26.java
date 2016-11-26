810485419d18b9783647dfaa5c02191a675f883e
yokalona <hatsuyukinorenai@gmail.com>
2016-11-26 13:42:23 +0300

    2016-11-26's Java workout

---
+++ contributions/yokalona/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
+Avoid using `static` variables
