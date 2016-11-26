dacff149495df3cb322898650366dd4eb58660b8
that-pat <that-pat@github.com>
2016-11-26 17:46:34 +0300

    2016-11-26's JavaScript workout

---
+++ contributions/that-pat/JavaScript/Node/2016-11-26.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Use conventional property names for error handling in Node.js
+Wrapping errors in Node.js using _node-verror_
+`uncaughtException` listener in Node.js
+Middleware/pipeline design pattern
