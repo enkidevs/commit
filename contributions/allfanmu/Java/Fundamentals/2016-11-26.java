5f4f99d849e82c2b67db87c07a2bbc843091274e
allfanmu <allfanmu@wp.pl>
2016-11-26 13:39:03 +0300

    2016-11-26's Java workout

---
+++ contributions/allfanmu/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Supply `toString()` in all classes
