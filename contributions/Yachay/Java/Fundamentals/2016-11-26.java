5b967af1cfb1d4822c7ffbe0b7ec26237c2d0df9
Yachay <yachay@gmail.com>
2016-11-26 15:30:26 +0300

    2016-11-26's Java workout

---
+++ contributions/Yachay/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Using `Printstream.format` to format a string before printing
+Using bounded type parameters in generic methods
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Supply `toString()` in all classes
