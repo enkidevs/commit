a322b2d970f38eb2c0b43e5db46a6e91eec241c3
Checo <checo1983@gmail.com>
2016-11-27 02:12:25 +0300

    2016-11-26's Java workout

---
+++ contributions/Checo/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Generating random numbers
+Java naming conventions
+Altering format string output by changing a format specifier's `argument_index`
+Calling one constructor from another
+Limit Accessibility of `Fields`
