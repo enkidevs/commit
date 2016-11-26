ae61688a755f13dbb124cb7b92ac63e6127104dd
aQaTL <mmsoltys@outlook.com>
2016-11-26 10:51:44 +0300

    2016-11-26's Java workout

---
+++ contributions/aQaTL/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
