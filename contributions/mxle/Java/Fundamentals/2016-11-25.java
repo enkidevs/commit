9efbe9ee9e24522a3edf5096d7e4cb0867b0228e
mxle <maxalenna@gmail.com>
2016-11-26 12:43:43 +0300

    2016-11-25's Java workout

---
+++ contributions/mxle/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Use meaningful names
+The trade-off between short and descriptive names
+Common mistake on switch statements
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
