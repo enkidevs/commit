9ef206ee633dc000b234c81892bbdf00546786de
FuFaijter <maty.infante@gmail.com>
2016-11-26 15:22:05 +0300

    2016-11-26's Java workout

---
+++ contributions/FuFaijter/Java/Data Types/2016-11-26.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Finding a substring in a string
+`StringBuffer` vs `StringBuilder`
