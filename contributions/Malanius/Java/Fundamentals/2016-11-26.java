65648fd1d6f4ded7212063499ab957868e0b9f9d
Malanius <malanius@seznam.cz>
2016-11-26 10:18:58 +0300

    2016-11-26's Java workout

---
+++ contributions/Malanius/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Common mistake on switch statements
+Altering format string output by changing a format specifier's `argument_index`
+Measuring time
+Limit Accessibility of `Fields`
