5ff7d1fc37dfb1c92667b7e217235291fe1c31e0
npatarino <npatarino@gmail.com>
2016-11-26 13:06:16 +0300

    2016-11-26's Linux workout

---
+++ contributions/npatarino/Linux/System Management/2016-11-26.bash
@@ -0,0 +1,5 @@
+List currently logged in users
+Changing the `/tmp` cleanup frequency
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
