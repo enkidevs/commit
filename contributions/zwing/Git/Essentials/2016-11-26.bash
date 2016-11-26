445f58d55020d0bcd6339553c7e59f4d837eb33c
zwing <zwing@tjgeeks.com>
2016-11-26 21:10:04 +0300

    2016-11-26's Git workout

---
+++ contributions/zwing/Git/Essentials/2016-11-26.bash
@@ -0,0 +1,5 @@
+How to unstage a staged file
+Search for commits by author
+Use `short` status to make output more compact
+`master` and `origin` aren't special
+Remote repositories: viewing, editing and deleting
