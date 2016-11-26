0ed52f41a8a018addb8f34c37782b71d2dcc7000
neugartf <fabian.neugart@gmail.com>
2016-11-26 14:58:05 +0300

    2016-11-26's Java workout

---
+++ contributions/neugartf/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Use strings in a `switch` statement
+Do not return *references* to private *mutable* class members
+Using an interface as a parameter
