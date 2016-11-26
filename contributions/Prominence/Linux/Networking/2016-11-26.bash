60ccbff949c60c623ae1f7253d2fc30cd78170b5
Prominence <prominence96@gmail.com>
2016-11-26 14:01:48 +0300

    2016-11-26's Linux workout

---
+++ contributions/Prominence/Linux/Networking/2016-11-26.bash
@@ -0,0 +1,5 @@
+`Firewall` as a service
+Logging dropped firewall packets
+Useful ssh client optimizations 
+Use `tcpdump` to listen to network interface traffic
+Staying connected remotely via SSH without an interactive shell
