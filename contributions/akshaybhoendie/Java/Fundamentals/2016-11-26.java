ba56022eba0d5f8c7ad1a8738720a8f39e59af93
akshaybhoendie <akshaybhoendie@gmail.com>
2016-11-26 14:19:10 +0300

    2016-11-26's Java workout

---
+++ contributions/akshaybhoendie/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Generating random numbers
+Common mistake on switch statements
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
