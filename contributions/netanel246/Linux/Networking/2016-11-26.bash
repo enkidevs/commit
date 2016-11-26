a52982fc3739fa76ec656c78eddc5ff88b14ca8d
netanel246 <netanel246@gmail.com>
2016-11-26 21:55:46 +0300

    2016-11-26's Linux workout

---
+++ contributions/netanel246/Linux/Networking/2016-11-26.bash
@@ -0,0 +1,5 @@
+Useful ssh client optimizations 
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
+Bandwidth monitoring tools 
+Use `tcpdump` to listen to network interface traffic
