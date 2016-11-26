504db67487cc4e1d1482b5aaee635601173e12ca
ShalaQweghen <serafettinyilmaz04@gmail.com>
2016-11-26 23:34:56 +0300

    2016-11-26's Linux workout

---
+++ contributions/ShalaQweghen/Linux/File Management/2016-11-26.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Get disk space usage with `df`
+
+Extracting `tar` files to a specific directory
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
