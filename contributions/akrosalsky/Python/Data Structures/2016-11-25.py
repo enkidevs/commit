051d0265284f00350c4c433ffa9d16210c230223
akrosalsky <akrosalsky@gmail.com>
2016-11-26 03:31:16 +0300

    2016-11-25's Python workout

---
+++ contributions/akrosalsky/Python/Data Structures/2016-11-25.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Implementing **weak** references
+`weakref` callbacks
+`weakref` proxies
+Binary record packing with `bytearray`
