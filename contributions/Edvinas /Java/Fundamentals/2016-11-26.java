f36597d72a2659e31c1a7b857e541e136d8695f5
Edvinas <edvinas.reg@outlook.com>
2016-11-26 12:03:18 +0300

    2016-11-26's Java workout

---
+++ contributions/Edvinas /Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The trade-off between short and descriptive names
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+How range operations work
