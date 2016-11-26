5d2275154d209352d3b59b7946add7d77b290561
doomi <kurai7hikari@gmail.com>
2016-11-27 02:21:50 +0300

    2016-11-26's Linux workout

---
+++ contributions/doomi/Linux/System Management/2016-11-26.bash
@@ -0,0 +1,5 @@
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Executing scripts on system boot using `systemd`
+Basic service management with `systemd`
+Finding Open Files With `lsof`
