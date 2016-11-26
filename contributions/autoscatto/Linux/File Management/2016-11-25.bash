c9f926a62f46fe4162312a262001bccf35a58b4f
autoscatto <4utoscatto@gmail.com>
2016-11-26 04:07:14 +0300

    2016-11-25's Linux workout

---
+++ contributions/autoscatto/Linux/File Management/2016-11-25.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Converting between Unix and Windows text files
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
