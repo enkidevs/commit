1f580780a4a2b7b34df6d41e43564ca0a587cec9
FreeRadicand <conner_mcgrath@yahoo.com>
2016-11-27 00:14:49 +0300

    2016-11-26's JavaScript workout

---
+++ contributions/FreeRadicand/JavaScript/Node/2016-11-26.js
@@ -0,0 +1,5 @@
+Wrapping errors in Node.js using _node-verror_
+Handle `JSON.parse` error in Node.js
+Automatic compilation for Node with **Nodemon** 
+`exports` vs. `module.exports` in **Node**
+Middleware/pipeline design pattern
