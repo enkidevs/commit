249114ba77af54062bfbf14ff84410c3cfe885ba
Reyes <tred27@gmail.com>
2016-11-26 18:06:03 +0300

    2016-11-26's JavaScript workout

---
+++ contributions/Reyes/JavaScript/Npm/2016-11-26.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+Niceties for your npm package
+Manage local node modules with `npm link`
+`npm`  as an alternative to Gulp
+Lock down dependency versions by shrinkwrapping
