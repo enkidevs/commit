af588a70c718c0b5b1fb566bddbb8846aad420a5
Aeolic <jurek.oberhauser@gmx.net>
2016-11-26 03:34:09 +0300

    2016-11-25's Java workout

---
+++ contributions/Aeolic/Java/Fundamentals/2016-11-25.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Limit Accessibility of `Fields`
+Increase `PermGen` space as to avoid `OutOfMemory` errors
