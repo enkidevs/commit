0eb1568ef63ad787dd6e429a62d3d9d200b975ca
Itaiza <itai.za@gmail.com>
2016-11-26 20:54:02 +0300

    2016-11-26's JavaScript workout

---
+++ contributions/Itaiza/JavaScript/Core/2016-11-26.js
@@ -0,0 +1,5 @@
+Meaning of polyfill
+Hoisting applies only to variable declarations, not initializations
+Filtering items out of an array
+Scope and strict mode
+Getting the *real* dimensions of an image
