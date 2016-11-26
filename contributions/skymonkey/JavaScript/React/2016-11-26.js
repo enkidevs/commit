ed6837fa1c80a4e44b32f014f982de37e2611519
skymonkey <rodascesar03@yahoo.com>
2016-11-26 21:30:21 +0300

    2016-11-26's JavaScript workout

---
+++ contributions/skymonkey/JavaScript/React/2016-11-26.js
@@ -0,0 +1,5 @@
+Functional Stateless Components in React
+`if`-`else` statements in **JSX** and **React**
+Dangerously set `innerHTML`
+Shallow compare in **React**
+Immutability helpers in **React**
