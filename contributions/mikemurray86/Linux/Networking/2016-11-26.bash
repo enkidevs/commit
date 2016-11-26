d2063d6082cbfaf72281f071a8e79e6753981484
mikemurray86 <gothiclovechild@gmail.com>
2016-11-26 18:03:49 +0300

    2016-11-26's Linux workout

---
+++ contributions/mikemurray86/Linux/Networking/2016-11-26.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Find services running on your host
+Get real network statistics with `slurm`
+Getting the connection speed from the terminal
+Short Introduction to `Wget` 
