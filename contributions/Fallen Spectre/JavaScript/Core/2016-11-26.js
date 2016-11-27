d0e33746637b7c97a728483b9923ee3983a5be86
Fallen Spectre <rodrigrv87@gmail.com>
2016-11-27 07:20:45 +0300

    2016-11-26's JavaScript workout

---
+++ contributions/Fallen Spectre/JavaScript/Core/2016-11-26.js
@@ -0,0 +1,5 @@
+Arrays can be used as stack
+`if/else` shortcut `conditional operator`
+Performing a function at timed intervals
+Inaccuracy of binary floating-point format
+Round numbers to `N` decimals 
