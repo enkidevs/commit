ed18aee0faa6913d8f302081d9feecabd3c8c846
lesediIsAlive <lesedi.nkhambula@gmail.com>
2016-11-27 13:59:33 +0300

    2016-11-27's Java workout

---
+++ contributions/lesediIsAlive/Java/Data Types/2016-11-27.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Use primitive types instead of wrapper classes when possible
+Use `Arrays.asList()` to initialise lists
+Multidimensional array declaration
+Retrieve the component type of an array
