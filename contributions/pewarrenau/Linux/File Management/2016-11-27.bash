2dc3763fb99db8dede57b0da2fc743f36a71947c
pewarrenau <pewarrenau@gmail.com>
2016-11-27 18:57:04 +0300

    2016-11-27's Linux workout

---
+++ contributions/pewarrenau/Linux/File Management/2016-11-27.bash
@@ -0,0 +1,5 @@
+Truncate files with `cat` instead of `rm`
+Extracting `tar` files to a specific directory
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Delete commands aliases
