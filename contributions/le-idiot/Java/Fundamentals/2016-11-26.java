2d21e0e42b88a98e329444b72910b473de3e4c91
le-idiot <siddhesh.mhadnak@gmail.com>
2016-11-27 16:02:04 +0300

    2016-11-26's Java workout

---
+++ contributions/le-idiot/Java/Fundamentals/2016-11-26.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Writing generic methods
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Avoid using `static` variables
