744dd4c79d251574f59e696253633d2308210985
Darkeye9 <dark.eye9@gmail.com>
2016-11-27 10:48:32 +0300

    2016-11-27's Java workout

---
+++ contributions/Darkeye9/Java/Data Types/2016-11-27.java
@@ -0,0 +1,5 @@
+`Math` vs `StrictMath` in java
+Default values for unassigned data types
+Use `Arrays.asList()` to initialise lists
+`StringBuffer` vs `StringBuilder`
+Retrieve the component type of an array
