3fcf786e67a14c0bfe122e1aff175d6c9428fe21
matthiasbalke <matthias.balke@googlemail.com>
2016-11-27 21:17:56 +0300

    2016-11-27's Java workout

---
+++ contributions/matthiasbalke/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Catch multiple exceptions in a single `catch` block
+Metadata: setting a file's owner
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
