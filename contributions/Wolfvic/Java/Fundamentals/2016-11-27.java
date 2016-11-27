c9841c49104c59b33429718a1006ebdb796db046
Wolfvic <vic16@hotmail.be>
2016-11-27 20:18:00 +0300

    2016-11-27's Java workout

---
+++ contributions/Wolfvic/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+The trade-off between short and descriptive names
+Common mistake on switch statements
+Calling one constructor from another
+Always specify type parameter
