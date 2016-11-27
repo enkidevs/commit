ad4df55dbb120f9216c5ed3c00ebb10a28086824
Bogdan_Ispas <bogdan.2111@gmail.com>
2016-11-27 11:28:34 +0300

    2016-11-27's Java workout

---
+++ contributions/Bogdan_Ispas/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Avoid using `static` variables
