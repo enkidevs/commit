ba011a6052c1eea2d4d7f64f089a01512d7b4684
gobbas <gabbi@hotmail.no>
2016-11-27 15:37:12 +0300

    2016-11-27's Java workout

---
+++ contributions/gobbas/Java/Classes & Objects/2016-11-27.java
@@ -0,0 +1,5 @@
+Template methods in abstract classes
+The `abstract` keyword
+Compare classes and not class names
+How and when to use `WeakHashMap` class
+String formatting and parsing with `SimpleDateFormat`
