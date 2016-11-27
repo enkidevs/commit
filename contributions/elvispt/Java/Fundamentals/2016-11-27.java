3c7299850310fd2046ad3cec932640d6d3999fd0
elvispt <elvispt@gmail.com>
2016-11-28 02:12:29 +0300

    2016-11-27's Java workout

---
+++ contributions/elvispt/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+The trade-off between short and descriptive names
+Use strings in a `switch` statement
+Using `Printstream.format` to format a string before printing
+Calling one constructor from another
+Avoid using `static` variables
