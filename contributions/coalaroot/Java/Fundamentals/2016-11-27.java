a4fd086625b6967cfebf2cff023ad2e3a84ef534
coalaroot <jakubkrolak94@gmail.com>
2016-11-27 16:28:46 +0300

    2016-11-27's Java workout

---
+++ contributions/coalaroot/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Use meaningful names
+The trade-off between short and descriptive names
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
