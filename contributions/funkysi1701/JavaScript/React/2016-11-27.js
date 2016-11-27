e7ac48567256dbaf9cc1ba9ebcd6f7ba86d94927
funkysi1701 <funkysi1701@gmail.com>
2016-11-27 11:26:34 +0300

    2016-11-27's JavaScript workout

---
+++ contributions/funkysi1701/JavaScript/React/2016-11-27.js
@@ -0,0 +1,5 @@
+**React** in-line styles
+`null` value for *Controlled Components* in **React**
+`if`-`else` statements in **JSX** and **React**
+Dealing with `this.props.children`
+Spread attributes
