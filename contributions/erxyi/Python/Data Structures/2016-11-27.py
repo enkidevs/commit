df043035c7fb8320db02690eb095c0bd3ba77fe3
erxyi <erxyi@safe-mail.net>
2016-11-27 22:25:08 +0300

    2016-11-27's Python workout

---
+++ contributions/erxyi/Python/Data Structures/2016-11-27.py
@@ -0,0 +1,5 @@
+A simple way to select a random item from a `list/tuple` data stucture
+Special attributes of objects and classes
+Enhance your `tuple`s
+`bytearray` objects
+Operations with `bytes` and `bytearray`
