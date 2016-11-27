69ad9cc464a9c09b7a480187ab00e910f27f0cec
BrianKolowitz <bkolowitz@gmail.com>
2016-11-27 17:01:00 +0300

    2016-11-27's Java workout

---
+++ contributions/BrianKolowitz/Java/I/O/2016-11-27.java
@@ -0,0 +1,5 @@
+System independent path specification
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
