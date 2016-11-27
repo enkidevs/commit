69a1feb1d3747c4dd4c96ca1cfb150f8891bbe55
Melobeat <kaikronenberg25@gmail.com>
2016-11-27 14:02:40 +0300

    2016-11-27's Java workout

---
+++ contributions/Melobeat/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Updating interfaces by using `default` methods
+Use `DecimalFormat` class to format numbers
+Calling one constructor from another
