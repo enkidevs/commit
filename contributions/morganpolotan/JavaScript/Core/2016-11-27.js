0dcbb2ff68ea83ab181d4de26aa8aa78c5f41ce3
morganpolotan <morgan@bloombergbeta.com>
2016-11-27 19:25:33 +0300

    2016-11-27's JavaScript workout

---
+++ contributions/morganpolotan/JavaScript/Core/2016-11-27.js
@@ -0,0 +1,5 @@
+Shuffle an array using `sort` and `random`.
+Arrays can be used as stack
+Setting default values with `short circuiting`
+How to merge two arrays
+Inaccuracy of binary floating-point format
