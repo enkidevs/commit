8f50b38f2044a5dd79c6ca3b98a30e8d802e3c4e
WillleePang <pangwilllee@hotmail.com>
2016-11-28 02:30:50 +0300

    2016-11-27's JavaScript workout

---
+++ contributions/WillleePang/JavaScript/Core/2016-11-27.js
@@ -0,0 +1,5 @@
+Quick way to format integers with a fixed number of digits.
+The difference between null, undefined and NaN
+Detect online connection
+Getting the *real* dimensions of an image
+Round numbers to `N` decimals 
