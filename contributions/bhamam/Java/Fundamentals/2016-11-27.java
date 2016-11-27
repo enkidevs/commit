96ed8852c2747b43418bd23fe666a3ddecc6dee4
bhamam <haiderhamani@gmail.com>
2016-11-28 01:38:08 +0300

    2016-11-27's Java workout

---
+++ contributions/bhamam/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using `synchronized` statements
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
+Using an interface as a parameter
