3895325ce347da57e8ddca300a70a32003a53eeb
ricrogz <ricrogz@gmail.com>
2016-11-27 22:52:01 +0300

    2016-11-27's Linux workout

---
+++ contributions/ricrogz/Linux/File Management/2016-11-27.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Converting between Unix and Windows text files
+Preserving permissions and structure with `rsync`
+Find the unknown process preventing deleting of files
