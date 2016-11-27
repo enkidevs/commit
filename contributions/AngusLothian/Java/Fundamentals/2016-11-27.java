ea0f8c80995253f6a3c81d12220045b813d98590
AngusLothian <angus.lothian@hotmail.com>
2016-11-27 12:41:30 +0300

    2016-11-27's Java workout

---
+++ contributions/AngusLothian/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Difference between JRE and JDK?
+Uses of the `final` keyword
+Generating random numbers
+The trade-off between short and descriptive names
+Metadata: creating a user-defined file attribute
