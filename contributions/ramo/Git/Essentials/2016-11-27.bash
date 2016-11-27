f24842e1b4501f7ba6f3362683c94a0a8dc77b45
ramo <ramin@motakef.de>
2016-11-27 13:30:25 +0300

    2016-11-27's Git workout

---
+++ contributions/ramo/Git/Essentials/2016-11-27.bash
@@ -0,0 +1,5 @@
+How to change your most recent commit 
+What is stored in a commit?
+Make your log output pretty
+How to write a git commit message
+Remote repositories: viewing, editing and deleting
