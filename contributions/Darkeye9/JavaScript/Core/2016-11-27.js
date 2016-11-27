540992dc2afd3f8d5665f37841958b434e1ce972
Darkeye9 <dark.eye9@gmail.com>
2016-11-27 11:04:08 +0300

    2016-11-27's JavaScript workout

---
+++ contributions/Darkeye9/JavaScript/Core/2016-11-27.js
@@ -0,0 +1,5 @@
+Keywords in hash keys
+Use `splice()` to remove an item from an array
+Using assignment operators
+The difference between null, undefined and NaN
+Inaccuracy of binary floating-point format
