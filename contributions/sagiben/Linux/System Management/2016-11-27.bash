bec5c79a7d83978da5174cf48ce214b5cf4f3ccb
sagiben <ilnextbus@gmail.com>
2016-11-27 07:52:08 +0300

    2016-11-27's Linux workout

---
+++ contributions/sagiben/Linux/System Management/2016-11-27.bash
@@ -0,0 +1,5 @@
+Test disk speed
+Detect your linux distribution
+Change your `hostname` on systems using `systemd`
+Changing the timezone on deb based systems
+Preventing a user from logging into the system
