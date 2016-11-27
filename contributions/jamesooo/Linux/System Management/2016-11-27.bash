3d9bec71d243d044ba7f6498bd7d549471bedd42
jamesooo <onealjames08@gmail.com>
2016-11-27 23:29:56 +0300

    2016-11-27's Linux workout

---
+++ contributions/jamesooo/Linux/System Management/2016-11-27.bash
@@ -0,0 +1,5 @@
+Test disk speed
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
