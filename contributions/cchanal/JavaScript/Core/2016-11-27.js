23dc000511d545cb56a70ebebd3c1cb22b3fc5df
cchanal <christophechanal@live.fr>
2016-11-27 17:38:21 +0300

    2016-11-27's JavaScript workout

---
+++ contributions/cchanal/JavaScript/Core/2016-11-27.js
@@ -0,0 +1,5 @@
+Shuffle an array using `sort` and `random`.
+Undefined / Empty / Null Checks
+Use `splice()` to remove an item from an array
+How to merge two arrays
+Inaccuracy of binary floating-point format
