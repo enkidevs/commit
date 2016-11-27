8f8327754c10b94673a73d68bf977e6c1626c8af
Flos <fn187-github@yahoo.de>
2016-11-27 13:59:43 +0300

    2016-11-27's Linux workout

---
+++ contributions/Flos/Linux/Networking/2016-11-27.bash
@@ -0,0 +1,5 @@
+Logging dropped firewall packets
+Find services running on your host
+Multiple IP Address with aliasing 
+Useful ssh client optimizations 
+Getting the connection speed from the terminal
