fb67f85575b54daa80ad20e113aa43c7ae4dcca7
Avabin <andrzej.piotrowski.76@gmail.com>
2016-11-27 14:34:21 +0300

    2016-11-27's Java workout

---
+++ contributions/Avabin/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Metadata: creating a user-defined file attribute
+Detect or prevent integer overflow
+Do not return *references* to private *mutable* class members
