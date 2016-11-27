60a616bb69c402b2ba170eca18dd2c66731aaf9c
GHOSTBEAR <andreas.everos@gmail.com>
2016-11-27 12:18:58 +0300

    2016-11-27's Java workout

---
+++ contributions/GHOSTBEAR/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Altering format string output by changing a format specifier's `argument_index`
+Using `Printstream.format` to format a string before printing
+Using bounded type parameters in generic methods
+Metadata: setting a file's owner
+Limit Accessibility of `Fields`
