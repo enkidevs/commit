127c9395e35778b7d289a5a802fba7ef3afb4f57
chribi <christoph.bier@gmx.de>
2016-11-27 13:24:42 +0300

    2016-11-27's Linux workout

---
+++ contributions/chribi/Linux/System Management/2016-11-27.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Incorrect time on dual boot systems
+Commands to shutdown or restart the system
+Get hardware stack details with `lspci`
+Cancel a system shutdown
