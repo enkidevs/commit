da8603cd5a199be844a35cc633a276b308f78a8d
bgoetzmann <bertrand.goetzmann@gmail.com>
2016-11-27 12:28:24 +0300

    2016-11-27's Java workout

---
+++ contributions/bgoetzmann/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Use `DecimalFormat` class to format numbers
+Do not perform bitwise and arithmetic operations on the same data
