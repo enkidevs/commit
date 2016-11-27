349fcf6c2b53e5fc09b2be2b4121d08a4304ede9
mikemurray86 <gothiclovechild@gmail.com>
2016-11-27 17:39:16 +0300

    2016-11-27's Java workout

---
+++ contributions/mikemurray86/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Difference between JRE and JDK?
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Always specify type parameter
