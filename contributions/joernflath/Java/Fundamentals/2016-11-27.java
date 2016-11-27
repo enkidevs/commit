dc0d5aa6c6d9bbe78fc8d76e6902239aa0efa397
joernflath <joern.flath@gmail.com>
2016-11-27 11:26:58 +0300

    2016-11-27's Java workout

---
+++ contributions/joernflath/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+The distinction between overloading and overriding methods
+Java naming conventions
+Using Inheritance to reduce code repetition
+Metadata: creating a user-defined file attribute
