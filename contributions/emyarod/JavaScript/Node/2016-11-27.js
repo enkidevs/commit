7980ad10efd2142ae28133d3187b64d31d20a85d
emyarod <allllaboutyou@gmail.com>
2016-11-27 23:59:29 +0300

    2016-11-27's JavaScript workout

---
+++ contributions/emyarod/JavaScript/Node/2016-11-27.js
@@ -0,0 +1,5 @@
+Synchronous File Write/Read in Node.js
+Handle `JSON.parse` error in Node.js
+`process.nextTick()` callback pattern
+`exports` vs. `module.exports` in **Node**
+Middleware/pipeline design pattern
