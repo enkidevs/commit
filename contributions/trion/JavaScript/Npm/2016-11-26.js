021c7f125233fc450824db1f7da21cd7608e6cd8
trion <parmsingh129@yahoo.com>
2016-11-27 07:29:48 +0300

    2016-11-26's JavaScript workout

---
+++ contributions/trion/JavaScript/Npm/2016-11-26.js
@@ -0,0 +1,5 @@
+View specific fields of npm packages
+List binaries for scripting in npm
+Environmental variables and flags for `npm`
+`npm` verifies `sha1` hashes of packages
+devDependencies in `npm`
