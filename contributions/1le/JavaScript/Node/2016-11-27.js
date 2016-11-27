311e1e1b6b6c0d413e430bda8ec047ea0da29a44
1le <ilian.nikolov@gmail.com>
2016-11-27 09:49:47 +0300

    2016-11-27's JavaScript workout

---
+++ contributions/1le/JavaScript/Node/2016-11-27.js
@@ -0,0 +1,5 @@
+Wrapping errors in Node.js using _node-verror_
+`process.nextTick()` callback pattern
+Manage child processes in **Node**
+Bulk-Write in **Node** with `cork()`
+Factories design pattern
