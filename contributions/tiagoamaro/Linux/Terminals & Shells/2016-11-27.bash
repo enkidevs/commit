63f5e4b64b0c6ba7328cbbf67b1c21ef40036983
tiagoamaro <tiagopadrela@gmail.com>
2016-11-28 00:24:35 +0300

    2016-11-27's Linux workout

---
+++ contributions/tiagoamaro/Linux/Terminals & Shells/2016-11-27.bash
@@ -0,0 +1,5 @@
+Find any Unix / Linux command
+Terminal incognito mode
+Breaking out of a terminal when `ssh` locks
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
