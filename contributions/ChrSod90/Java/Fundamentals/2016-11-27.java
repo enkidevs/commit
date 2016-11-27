35925d72c5f41b437e97b6530e2fccdb42f6c6e3
ChrSod90 <christoffer.soderstrom.8799@student.uu.se>
2016-11-27 20:09:47 +0300

    2016-11-27's Java workout

---
+++ contributions/ChrSod90/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Use meaningful names
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
