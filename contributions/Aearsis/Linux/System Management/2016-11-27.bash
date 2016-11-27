24fa2dc440d518e2a30fe49364450f2447bcb91a
Aearsis <aearsis@eideo.cz>
2016-11-28 02:54:26 +0300

    2016-11-27's Linux workout

---
+++ contributions/Aearsis/Linux/System Management/2016-11-27.bash
@@ -0,0 +1,5 @@
+List currently logged in users
+Removing old kernels in Debian based systems
+Common log files
+Preventing a user from logging into the system
+Executing scripts on system boot using `systemd`
