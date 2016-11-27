369e3792e288a709deaad86594a912363311ec82
Byeonghoon <thehimen9770@gmail.com>
2016-11-27 16:50:54 +0300

    2016-11-26's JavaScript workout

---
+++ contributions/Byeonghoon/JavaScript/Functions/2016-11-26.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function expression
+IIFE pattern
+Method chaining
+Changing a functions context with `fn.call(object)`
