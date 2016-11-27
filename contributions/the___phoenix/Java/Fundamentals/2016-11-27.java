9fff044b871d8ab420cfa4c0b2abef3d3a441621
the___phoenix <aly-pon2012@yandex.ru>
2016-11-27 23:23:46 +0300

    2016-11-27's Java workout

---
+++ contributions/the___phoenix/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Ternary operator
+Calling one constructor from another
+Always specify type parameter
