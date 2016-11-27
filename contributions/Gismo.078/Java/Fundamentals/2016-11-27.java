5c4a339951555bf7118d6664bf5989bc7d844c24
Gismo.078 <daniel.s.wirtz@gmail.com>
2016-11-27 21:49:07 +0300

    2016-11-27's Java workout

---
+++ contributions/Gismo.078/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Using Inheritance to reduce code repetition
+Calling one constructor from another
+Always specify type parameter
