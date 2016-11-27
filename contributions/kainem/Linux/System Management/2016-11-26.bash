d450c04701f598b577c8a4730257d9db57879bed
kainem <kaine@kainem.com>
2016-11-27 11:33:37 +0300

    2016-11-26's Linux workout

---
+++ contributions/kainem/Linux/System Management/2016-11-26.bash
@@ -0,0 +1,5 @@
+CPU benchmark with `dd`
+Change your `hostname` on systems using `systemd`
+Incorrect time on dual boot systems
+Get hardware stack details with `lspci`
+Using `dd` To Create Disk Images
