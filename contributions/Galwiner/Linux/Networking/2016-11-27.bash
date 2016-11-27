170d0efd42686a890ac8316e779ca486da561e94
Galwiner <galwiner@gmail.com>
2016-11-28 01:42:55 +0300

    2016-11-27's Linux workout

---
+++ contributions/Galwiner/Linux/Networking/2016-11-27.bash
@@ -0,0 +1,5 @@
+Quick port test with `netcat`
+Find services running on your host
+Getting the connection speed from the terminal
+Bandwidth monitoring tools 
+Use `tcpdump` to listen to network interface traffic
