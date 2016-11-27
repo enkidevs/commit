a6a63da61a360634ce0f3f70100608efebbb901d
geniusinfrance <ryan.bergerson@gmail.com>
2016-11-27 11:06:27 +0300

    2016-11-27's Java workout

---
+++ contributions/geniusinfrance/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Common mistake on switch statements
+Writing generic methods
+Altering format string output by changing a format specifier's `argument_index`
+Calling one constructor from another
+Limit Accessibility of `Fields`
