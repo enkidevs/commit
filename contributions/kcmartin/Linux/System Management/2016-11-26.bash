f9681ff66b421cfd4c3c5d5c48790a99e4c40d23
kcmartin <kcmartin2@mac.com>
2016-11-27 06:14:44 +0300

    2016-11-26's Linux workout

---
+++ contributions/kcmartin/Linux/System Management/2016-11-26.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Change your `hostname` on systems using `systemd`
+Changing the `/tmp` cleanup frequency
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
