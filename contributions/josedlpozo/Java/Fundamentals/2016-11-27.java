854b79eee7c7905b68f039cb37c6723c98cff531
josedlpozo <josedlpozo@gmail.com>
2016-11-27 22:10:08 +0300

    2016-11-27's Java workout

---
+++ contributions/josedlpozo/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Floating garbage and how to deal with it
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Detect or prevent integer overflow
+Do not perform bitwise and arithmetic operations on the same data
