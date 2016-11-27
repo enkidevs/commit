f1bacca6e9d4876c7f0829f9dce7cdaabcb02694
pieter.thysen <pieter.thysen@gmail.com>
2016-11-27 12:15:23 +0300

    2016-11-27's JavaScript workout

---
+++ contributions/pieter.thysen/JavaScript/Express/2016-11-27.js
@@ -0,0 +1,5 @@
+`response` object additional properties in **Express**
+Easy way to deliver **HTML** pages with **Express**
+Using **Express 4.0** `router`
+`cookie-session` in **Express**
+Session handling in **Express**
