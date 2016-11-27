be4c8d09f08188a84c9e38436143c25c6204ec1d
lolporer <lolporer@gmail.com>
2016-11-27 17:48:59 +0300

    2016-11-27's JavaScript workout

---
+++ contributions/lolporer/JavaScript/Node/2016-11-27.js
@@ -0,0 +1,5 @@
+Async File Write in Node.js
+Global Objects and Environment Variables in **Node**
+`uncaughtException` listener in Node.js
+`process.nextTick()` callback pattern
+Manage child processes in **Node**
