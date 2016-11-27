fdd6252a780a118424da6b2b142a84cc999467d5
zhekau <zhekau.system@gmail.com>
2016-11-27 17:14:47 +0300

    2016-11-27's Linux workout

---
+++ contributions/zhekau/Linux/System Management/2016-11-27.bash
@@ -0,0 +1,5 @@
+Find out if the system's architecture is 32 or 64 bit
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
+Finding Open Files With `lsof`
