df96b02a8e720765a33dfcd0b2921739cc7de8f8
Cazzar <caydedixon@gmail.com>
2016-11-27 11:13:29 +0300

    2016-11-26's Java workout

---
+++ contributions/Cazzar/Java/Threads & Locks/2016-11-26.java
@@ -0,0 +1,5 @@
+Multithreading: use local variables
+Multithreading: minimize locking scope
+Use thread pool executors
+`Thread.sleep` and `Thread.yield` do not have synchronization mechanisms
+Thread interruptions
