69319e503195df4412d20d253447f0d172ab359f
leslch <leslch@hotmail.com>
2016-11-28 02:24:26 +0300

    2016-11-27's Java workout

---
+++ contributions/leslch/Java/Data Types/2016-11-27.java
@@ -0,0 +1,5 @@
+Default values for unassigned data types
+Two ways to use an `Iterator`
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
+Retrieve the component type of an array
