08e04514e56c510d91bf466b250828af649b0095
tata <tata@metacortex.hu>
2016-11-27 13:17:44 +0300

    2016-11-27's JavaScript workout

---
+++ contributions/tata/JavaScript/Npm/2016-11-27.js
@@ -0,0 +1,5 @@
+Abbreviate commands in npm
+Bump package version in npm
+Passing through command line arguments in npm scripts
+List binaries for scripting in npm
+Lock down dependency versions by shrinkwrapping
