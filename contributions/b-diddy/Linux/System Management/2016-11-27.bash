11be3e2e6eba7a78d2e0e6e3773257acc2a43a55
b-diddy <bendikflotre@gmail.com>
2016-11-27 21:45:03 +0300

    2016-11-27's Linux workout

---
+++ contributions/b-diddy/Linux/System Management/2016-11-27.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Detect your linux distribution
+Changing the timezone on deb based systems
+Get hardware stack details with `lspci`
+Finding Open Files With `lsof`
