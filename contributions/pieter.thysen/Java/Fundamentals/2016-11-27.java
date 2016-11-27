7d428bcf909eaa3cb77410fc98c965a625632689
pieter.thysen <pieter.thysen@gmail.com>
2016-11-27 12:08:43 +0300

    2016-11-27's Java workout

---
+++ contributions/pieter.thysen/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Use `DecimalFormat` class to format numbers
+Calling one constructor from another
+Do not perform bitwise and arithmetic operations on the same data
+How range operations work
+Avoid using `static` variables
