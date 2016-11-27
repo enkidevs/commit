732f48732e4928832a3c29cebc46b24fb666a15d
stando <stano.kudlac@gmail.com>
2016-11-27 11:45:08 +0300

    2016-11-27's Git workout

---
+++ contributions/stando/Git/Tools/2016-11-27.bash
@@ -0,0 +1,5 @@
+Cache your authentication details to save time
+Interactively unstage changes
+Specify a range of commits using double dot syntax
+Interactively stage patches
+Remember the results of previous hunk conflicts
