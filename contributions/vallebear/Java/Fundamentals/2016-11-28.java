4f4251eb8281a3a3a0157dcee15b038544ad6fa0
vallebear <seifer_50@hotmail.com>
2016-11-28 11:52:52 +0300

    2016-11-28's Java workout

---
+++ contributions/vallebear/Java/Fundamentals/2016-11-28.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Metadata: creating a user-defined file attribute
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Increase `PermGen` space as to avoid `OutOfMemory` errors
