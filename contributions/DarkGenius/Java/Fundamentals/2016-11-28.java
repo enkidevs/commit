c99c8d9b9ef25f86223183688cffb2b7e115b97c
DarkGenius <darkgenius@nextmail.ru>
2016-11-28 06:54:36 +0300

    2016-11-28's Java workout

---
+++ contributions/DarkGenius/Java/Fundamentals/2016-11-28.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Use meaningful names
+Catch multiple exceptions in a single `catch` block
+Writing generic methods
+Avoid using `static` variables
