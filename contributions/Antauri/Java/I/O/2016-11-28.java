f33e336556b8bf3d9272c2ae66bea4ddd7837687
Antauri <catalin.zamfir@kitsoftware.ro>
2016-11-28 09:39:30 +0300

    2016-11-28's Java workout

---
+++ contributions/Antauri/Java/I/O/2016-11-28.java
@@ -0,0 +1,5 @@
+System independent path specification
+`System.out` vs `System.err`
+Reading and writing text files
+Translating `Scanner` tokens into primitive types
+Checking for the existence of files and directories
