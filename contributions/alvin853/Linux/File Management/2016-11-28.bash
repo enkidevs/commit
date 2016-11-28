4618ba00e85c2c63a8262029a55b189c2947984d
alvin853 <alvin853@yahoo.de>
2016-11-28 16:09:26 +0300

    2016-11-28's Linux workout

---
+++ contributions/alvin853/Linux/File Management/2016-11-28.bash
@@ -0,0 +1,6 @@
+Fast file indexing with `updatedb` and `locate`
+Finding file with regexes
+Identify files using the `file` command
+
+Deleting files in a secure manner
+Monitor the progress of data through a pipe with `pv`
