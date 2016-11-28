8ccea3c0a4c6c04476a5efc06603453212477ea9
Trevato <trevordobbertin@gmail.com>
2016-11-28 16:17:20 +0300

    2016-11-28's Java workout

---
+++ contributions/Trevato/Java/Fundamentals/2016-11-28.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Updating interfaces by using `default` methods
+Using bounded type parameters in generic methods
+Measuring time
+Supply `toString()` in all classes
