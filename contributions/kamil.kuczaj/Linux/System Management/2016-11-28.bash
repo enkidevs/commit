95cd08ce7c35cb8471eefb68f714a73cb747537d
kamil.kuczaj <kamil.kuczaj@gmail.com>
2016-11-28 11:51:07 +0300

    2016-11-28's Linux workout

---
+++ contributions/kamil.kuczaj/Linux/System Management/2016-11-28.bash
@@ -0,0 +1,5 @@
+Removing old kernels in Debian based systems
+Find out if the system's architecture is 32 or 64 bit
+Common log files
+Check the HDD with `badblocks`
+Basic service management with `systemd`
