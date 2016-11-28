985c1aa05ef0bcfcbac5f5babe86dbb48f7090a6
JusNos <siuskitman@gmail.com>
2016-11-28 14:12:59 +0300

    2016-11-28's Java workout

---
+++ contributions/JusNos/Java/Fundamentals/2016-11-28.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+Uses of the `final` keyword
+Common mistake on switch statements
+Catch multiple exceptions in a single `catch` block
+Avoid using `static` variables
