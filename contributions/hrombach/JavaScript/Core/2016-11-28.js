ffa0a35d2af59edcbb29eea1d3951522b9c88029
hrombach <h.rombach1@gmail.com>
2016-11-28 11:03:51 +0300

    2016-11-28's JavaScript workout

---
+++ contributions/hrombach/JavaScript/Core/2016-11-28.js
@@ -0,0 +1,5 @@
+Weak vs Strict equality operator
+Data type comparison in `switch` statements
+Hoisting applies only to variable declarations, not initializations
+`NaN` is a number
+Scope and strict mode
