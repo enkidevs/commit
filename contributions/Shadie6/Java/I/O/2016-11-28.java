5a3fb399fb0f7f2d972ea06b00f8bd0741801269
Shadie6 <l.meroni6@gmail.com>
2016-11-28 13:41:44 +0300

    2016-11-28's Java workout

---
+++ contributions/Shadie6/Java/I/O/2016-11-28.java
@@ -0,0 +1,5 @@
+System independent path specification
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Retrieving file store attributes
+Using the `OpenOption...` vararg to define file writing behavior
