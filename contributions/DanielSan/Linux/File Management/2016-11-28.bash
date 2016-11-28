4b71e48fc052d1bea8734a09296c4b39a11cf3e6
DanielSan <dkoetting@gmx.de>
2016-11-28 16:25:28 +0300

    2016-11-28's Linux workout

---
+++ contributions/DanielSan/Linux/File Management/2016-11-28.bash
@@ -0,0 +1,5 @@
+Fast file indexing with `updatedb` and `locate`
+Practical `du` command
+Truncate files with `cat` instead of `rm`
+Converting between Unix and Windows text files
+Deleting files in a secure manner
