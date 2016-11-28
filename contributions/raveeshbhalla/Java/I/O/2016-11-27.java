42fbd322ac66706d12b44ee984033e1f9cdded85
raveeshbhalla <raveeshbhalla90@gmail.com>
2016-11-28 07:26:18 +0300

    2016-11-27's Java workout

---
+++ contributions/raveeshbhalla/Java/I/O/2016-11-27.java
@@ -0,0 +1,5 @@
+System independent path specification
+`System.out` vs `System.err`
+How to create directories in Java
+Checking for the existence of files and directories
+Listing a file system's root directories
