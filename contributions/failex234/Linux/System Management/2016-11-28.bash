7a9825c4b781409dc0bbd1f48e924787a9d3f5fd
failex234 <felix@emilnaumann.de>
2016-11-28 09:31:27 +0300

    2016-11-28's Linux workout

---
+++ contributions/failex234/Linux/System Management/2016-11-28.bash
@@ -0,0 +1,5 @@
+Revision control of configuration files with git
+Detect your linux distribution
+Fixing `locale` issues in Debian systems
+Using `dd` To Create Disk Images
+Finding Open Files With `lsof`
