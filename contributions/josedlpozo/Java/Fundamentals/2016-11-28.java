9e85926d818dfcc9a8407f87da97463e6e2e9bb4
josedlpozo <josedlpozo@gmail.com>
2016-11-28 09:51:05 +0300

    2016-11-28's Java workout

---
+++ contributions/josedlpozo/Java/Fundamentals/2016-11-28.java
@@ -0,0 +1,5 @@
+Metadata: setting a file's owner
+Using `synchronized` statements
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
+Using an interface as a parameter
