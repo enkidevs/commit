ea6d6aa25f41c79778f0ea256250214eb5aa60bb
BooM <tararuj.michal@gmail.com>
2016-11-28 09:25:44 +0300

    2016-11-28's Java workout

---
+++ contributions/BooM/Java/Fundamentals/2016-11-28.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Using bounded type parameters in generic methods
+Metadata: creating a user-defined file attribute
+Do not perform bitwise and arithmetic operations on the same data
+How range operations work
