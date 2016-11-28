9cf01f49dd41504f07b5ab6608e765cfbaaa7a8f
ray10k <w.j.van.den.heuvel@xmsnet.nl>
2016-11-28 12:34:16 +0300

    2016-11-28's Java workout

---
+++ contributions/ray10k/Java/Fundamentals/2016-11-28.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use `DecimalFormat` class to format numbers
+Using `synchronized` statements
+Do not perform bitwise and arithmetic operations on the same data
+Avoid using `static` variables
