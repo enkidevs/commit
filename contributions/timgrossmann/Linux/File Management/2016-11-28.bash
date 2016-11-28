881a82879a0acb67b54dfd5a3c69eacb1d6ece91
timgrossmann <t.grossmann1994@web.de>
2016-11-28 09:56:25 +0300

    2016-11-28's Linux workout

---
+++ contributions/timgrossmann/Linux/File Management/2016-11-28.bash
@@ -0,0 +1,6 @@
+Finding file with regexes
+Identify files using the `file` command
+
+Monitor the progress of data through a pipe with `pv`
+Deleting non-empty directories
+Find the unknown process preventing deleting of files
