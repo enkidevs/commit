7e3a8a2b8a1c2661dd528983e131fbca4b76b6aa
vanporman <vanporman@gmail.com>
2016-11-28 10:21:38 +0300

    2016-11-28's Java workout

---
+++ contributions/vanporman/Java/Fundamentals/2016-11-28.java
@@ -0,0 +1,5 @@
+Use strings in a `switch` statement
+Metadata: setting a file's owner
+Measuring time
+Do not perform bitwise and arithmetic operations on the same data
+Supply `toString()` in all classes
