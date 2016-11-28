72ebca3fd79f0b3bbfa12594820c2674b7ac6516
Edvinas <edvinas.reg@outlook.com>
2016-11-28 13:48:38 +0300

    2016-11-28's Java workout

---
+++ contributions/Edvinas /Java/Fundamentals/2016-11-28.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Using `synchronized` statements
+Detect or prevent integer overflow
+Supply `toString()` in all classes
