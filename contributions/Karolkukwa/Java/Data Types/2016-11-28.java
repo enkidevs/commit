87cd7b18394d2b237fe350506044f4a62dcfac88
Karolkukwa <karol.kukwa@gmail.com>
2016-11-28 11:18:37 +0300

    2016-11-28's Java workout

---
+++ contributions/Karolkukwa/Java/Data Types/2016-11-28.java
@@ -0,0 +1,5 @@
+Use primitive types instead of wrapper classes when possible
+Collections vs arrays
+Equals operation on different data types
+Do not attempt comparisons with NaN
+Numeric Conversion - Widening
