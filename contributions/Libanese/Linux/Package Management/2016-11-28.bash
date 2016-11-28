754da932afccf3b82ceecae72e4ba2e3df89fc30
Libanese <attiliomandola@gmail.com>
2016-11-28 15:18:38 +0300

    2016-11-28's Linux workout

---
+++ contributions/Libanese/Linux/Package Management/2016-11-28.bash
@@ -0,0 +1,6 @@
+Check the version of a package on a Debian based system
+Install specific version of packages
+Backporting on Debian
+
+Prevent updating a specific package in Debian systems
+Downgrade a package via `apt-get`
