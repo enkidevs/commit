9824d7108c3936ed34981c795ed882890d5bb9d2
jbkimes82 <jbkimes@gmail.com>
2016-11-28 07:37:52 +0300

    2016-11-27's JavaScript workout

---
+++ contributions/jbkimes82/JavaScript/Functions/2016-11-27.js
@@ -0,0 +1,5 @@
+Explicit setting of `this` using `call` and `apply` methods
+Function constructor vs. function expression
+Functions can be declared after use
+Get query/url variables
+Easily generate a random `HEX` color
