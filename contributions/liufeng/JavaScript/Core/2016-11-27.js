d6475a006729d25fbdbbaf1dce34278a0a2587a8
liufeng <liufeng5460@163.com>
2016-11-28 09:42:13 +0300

    2016-11-27's JavaScript workout

---
+++ contributions/liufeng/JavaScript/Core/2016-11-27.js
@@ -0,0 +1,5 @@
+Quick way to format integers with a fixed number of digits.
+Get a random item from an Array
+Undefined / Empty / Null Checks
+Setting default values with `short circuiting`
+Base conversion 
