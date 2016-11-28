3fc1cd3ee2ecf353bec7427a51f5827720d51456
coalaroot <jakubkrolak94@gmail.com>
2016-11-28 15:07:12 +0300

    2016-11-28's Java workout

---
+++ contributions/coalaroot/Java/Fundamentals/2016-11-28.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+Java naming conventions
+Using `Printstream.format` to format a string before printing
+Use `DecimalFormat` class to format numbers
+`Autoboxing` and `Unboxing`
