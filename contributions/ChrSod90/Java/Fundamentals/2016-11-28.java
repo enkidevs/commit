e3eae18049df95fbc4531a1068e03c98617dbf8b
ChrSod90 <christoffer.soderstrom.8799@student.uu.se>
2016-11-28 16:19:20 +0300

    2016-11-28's Java workout

---
+++ contributions/ChrSod90/Java/Fundamentals/2016-11-28.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Java naming conventions
+Common mistake on switch statements
+Using Inheritance to reduce code repetition
+Use `DecimalFormat` class to format numbers
