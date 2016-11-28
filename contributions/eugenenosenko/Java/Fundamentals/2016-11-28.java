9ec26e47a9723a0f799258264e380344b8447968
eugenenosenko <eugenenosenko@gmail.com>
2016-11-28 13:06:27 +0300

    2016-11-28's Java workout

---
+++ contributions/eugenenosenko/Java/Fundamentals/2016-11-28.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Do not perform bitwise and arithmetic operations on the same data
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
+Using an interface as a parameter
