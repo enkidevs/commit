7a877fb147afd2d3d8ad82987e2249748d914195
korczynsk1 <korczynsk1@outlook.com>
2016-11-28 15:40:08 +0300

    2016-11-28's Java workout

---
+++ contributions/korczynsk1/Java/Error Handling/2016-11-28.java
@@ -0,0 +1,5 @@
+Using specific exception types in the `throws` clause
+Converting stack trace to a string
+Creating new Exceptions
+Prevent `exceptions` while logging Data
+Most common reason behind **stack overflow** error
