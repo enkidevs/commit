62e9b2edc7dd31a838a52f393b6ee5e19cc5b330
KenDUemura <kenduemura@gmail.com>
2016-11-28 08:04:01 +0300

    2016-11-27's Linux workout

---
+++ contributions/KenDUemura/Linux/System Management/2016-11-27.bash
@@ -0,0 +1,6 @@
+CPU benchmark with `dd`
+Force a time update with `ntp`
+
+Changing the timezone on deb based systems
+Fixing `locale` issues in Debian systems
+Get hardware stack details with `lspci`
