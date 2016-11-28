b8dc5ea5757cbc86a66c5d9e5275ad0665da4140
Lizander <kevin9075@gmail.com>
2016-11-28 04:48:49 +0300

    2016-11-27's Java workout

---
+++ contributions/Lizander/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Uses of the `final` keyword
+Use meaningful names
+Altering format string output by changing a format specifier's `argument_index`
+Updating interfaces by using `default` methods
+Using an interface as a parameter
