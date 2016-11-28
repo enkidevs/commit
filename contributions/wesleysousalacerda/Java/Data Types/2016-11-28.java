94a83e029b1dc8c4657809f111bb992b7e9ae960
wesleysousalacerda <wesleysousalacerda@gmail.com>
2016-11-28 15:42:05 +0300

    2016-11-28's Java workout

---
+++ contributions/wesleysousalacerda/Java/Data Types/2016-11-28.java
@@ -0,0 +1,5 @@
+Using `enum` in Java
+Use primitive types instead of wrapper classes when possible
+Equals operation on different data types
+`StringBuffer` vs `StringBuilder`
+Do not attempt comparisons with NaN
