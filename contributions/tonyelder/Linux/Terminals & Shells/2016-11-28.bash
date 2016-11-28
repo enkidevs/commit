5f2492460994f2687fc5d3028ca977af20dad046
tonyelder <tonyelder@aapdltd.com>
2016-11-28 10:43:52 +0300

    2016-11-28's Linux workout

---
+++ contributions/tonyelder/Linux/Terminals & Shells/2016-11-28.bash
@@ -0,0 +1,5 @@
+Rapidly invoke an editor to write a long, complex, or tricky command
+Clear bash history
+Get to know  your commands with `type`
+Sequential execution using the `;` statement separator
+Retrieve previous arguments
