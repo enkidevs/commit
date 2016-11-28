35cd775b04885efae787f3d3772763058ed11ca3
kevin-coyle <kevin.coyle@drutek.co.uk>
2016-11-28 13:11:20 +0300

    2016-11-28's JavaScript workout

---
+++ contributions/kevin-coyle/JavaScript/Express/2016-11-28.js
@@ -0,0 +1,5 @@
+Easy way to deliver **HTML** pages with **Express**
+**GZIP** compression for **Express**
+`cookie-session` in **Express**
+Session handling in **Express**
+Error handler in **Express**
