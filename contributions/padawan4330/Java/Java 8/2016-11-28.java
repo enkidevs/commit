a2a66003b01f3470d6828aea92ca606986b8d954
padawan4330 <padawan4330@gmail.com>
2016-11-28 15:00:44 +0300

    2016-11-28's Java workout

---
+++ contributions/padawan4330/Java/Java 8/2016-11-28.java
@@ -0,0 +1,5 @@
+Java 8 Lambda expressions
+Using `Arrays.parallelSort` 
+How to check for static dependencies of applications and libraries from the command line.
+Avoid numerical overflows with *Java8* `exact` methods.
+Java8's new `StampedLock`
