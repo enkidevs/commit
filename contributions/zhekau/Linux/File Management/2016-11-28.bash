8d62379154f6ba8eb14af131e5415121aa47a83c
zhekau <zhekau.system@gmail.com>
2016-11-28 16:03:24 +0300

    2016-11-28's Linux workout

---
+++ contributions/zhekau/Linux/File Management/2016-11-28.bash
@@ -0,0 +1,6 @@
+Practical `du` command
+Converting between Unix and Windows text files
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Easy way of sharing files
