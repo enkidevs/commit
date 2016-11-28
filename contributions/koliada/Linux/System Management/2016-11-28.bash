14fd1367d09af246d3f130f8a0ccfd6913a9d679
koliada <alex.fiator@gmail.com>
2016-11-28 10:59:11 +0300

    2016-11-28's Linux workout

---
+++ contributions/koliada/Linux/System Management/2016-11-28.bash
@@ -0,0 +1,5 @@
+Detect your linux distribution
+List currently logged in users
+Executing scripts on system boot using `systemd`
+Get hardware stack details with `lspci`
+Monitor System Memory using `vmstat`
