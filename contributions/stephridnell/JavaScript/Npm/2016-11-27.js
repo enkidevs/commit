331655f09da2709df426c9abd8ee990876abf9a7
stephridnell <steph.ridnell@gmail.com>
2016-11-28 11:31:06 +0300

    2016-11-27's JavaScript workout

---
+++ contributions/stephridnell/JavaScript/Npm/2016-11-27.js
@@ -0,0 +1,5 @@
+Calling remote scripts with npm
+npm projects dependencies
+npm can handle multiple versions of a dependency
+Passing through command line arguments in npm scripts
+Manage local node modules with `npm link`
