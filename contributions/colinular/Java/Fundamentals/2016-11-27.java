69e2ef71d247bb57bd9b321e6fbedd7fc4f90a11
colinular <colinular@gmail.com>
2016-11-28 05:33:19 +0300

    2016-11-27's Java workout

---
+++ contributions/colinular/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+Difference between `==` and `equals()`
+The distinction between overloading and overriding methods
+Common mistake on switch statements
+Calling one constructor from another
+Always specify type parameter
