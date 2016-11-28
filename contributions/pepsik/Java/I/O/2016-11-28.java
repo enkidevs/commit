3ba61d6f67b3f2c69360ea4bd8fcc78d37023777
pepsik <berezovyialeksandr@gmail.com>
2016-11-28 10:57:23 +0300

    2016-11-28's Java workout

---
+++ contributions/pepsik/Java/I/O/2016-11-28.java
@@ -0,0 +1,5 @@
+System independent path specification
+How to create directories in Java
+Translating `Scanner` tokens into primitive types
+Using the `OpenOption...` vararg to define file writing behavior
+Listing a file system's root directories
