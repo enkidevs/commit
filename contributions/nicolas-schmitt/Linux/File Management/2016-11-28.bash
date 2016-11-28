3bdacda48dff3d42cbcb7fbcf32a0c3114bf4ff4
nicolas-schmitt <nicolas.schmitt@live.fi>
2016-11-28 10:44:23 +0300

    2016-11-28's Linux workout

---
+++ contributions/nicolas-schmitt/Linux/File Management/2016-11-28.bash
@@ -0,0 +1,7 @@
+Fast file indexing with `updatedb` and `locate`
+Get disk space usage with `df`
+
+Identify files using the `file` command
+
+Preserving permissions and structure with `rsync`
+Easy way of sharing files
