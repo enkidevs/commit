3b84a6a2a943bb1f3723477fa9fc5eb80c381de7
4N7HR4X <anthrax.f@gmail.com>
2016-11-28 10:30:09 +0300

    2016-11-28's Java workout

---
+++ contributions/4N7HR4X/Java/Fundamentals/2016-11-28.java
@@ -0,0 +1,5 @@
+Difference between ```HashMap``` and ```Hashtable```
+The distinction between overloading and overriding methods
+Metadata: setting a file's owner
+Using `synchronized` statements
+Detect or prevent integer overflow
