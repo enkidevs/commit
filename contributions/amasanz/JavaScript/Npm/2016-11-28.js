08a3c1c232300b86e9b692d6fb91a7434e50ac1e
amasanz <alx.masz@gmail.com>
2016-11-28 09:20:43 +0300

    2016-11-28's JavaScript workout

---
+++ contributions/amasanz/JavaScript/Npm/2016-11-28.js
@@ -0,0 +1,5 @@
+Registry user accounts for npm
+Passing through command line arguments in npm scripts
+Manage local node modules with `npm link`
+`npm` verifies `sha1` hashes of packages
+`peerDependencies`
