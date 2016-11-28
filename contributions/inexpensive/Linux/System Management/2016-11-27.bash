8cd549a0ac5d7fa9d0c4e569fa2d6c2c1fcfbc5d
inexpensive <lawrence.bouzane@gmail.com>
2016-11-28 08:43:02 +0300

    2016-11-27's Linux workout

---
+++ contributions/inexpensive/Linux/System Management/2016-11-27.bash
@@ -0,0 +1,5 @@
+Broadcast messages across the system
+Changing the timezone on deb based systems
+Commands to shutdown or restart the system
+Cancel a system shutdown
+Finding Open Files With `lsof`
