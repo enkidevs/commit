bae9d5995123c99256b4daec4f94c689bcebc86a
durandj <james.durand@alumni.msoe.edu>
2016-11-28 16:07:37 +0300

    2016-11-28's Java workout

---
+++ contributions/durandj/Java/Fundamentals/2016-11-28.java
@@ -0,0 +1,5 @@
+Writing generic methods
+Use `DecimalFormat` class to format numbers
+There is no such thing as *pass-by-reference* in Java
+Do not return *references* to private *mutable* class members
+Avoid using `static` variables
