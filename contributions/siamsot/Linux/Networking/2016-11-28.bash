489872425712f6261a1009658c96df1e701fc218
siamsot <siamsot@gmail.com>
2016-11-28 14:32:22 +0300

    2016-11-28's Linux workout

---
+++ contributions/siamsot/Linux/Networking/2016-11-28.bash
@@ -0,0 +1,5 @@
+Check `iptables` firewall status
+How to clear `iptables` rules
+Useful ssh client optimizations 
+Getting the connection speed from the terminal
+Use `tcpdump` to listen to network interface traffic
