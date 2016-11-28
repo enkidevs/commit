ecd007812793287e88ca3d6a83becc75fed545fa
slowdata <slowmotion@outlook.com>
2016-11-28 05:01:57 +0300

    2016-11-27's JavaScript workout

---
+++ contributions/slowdata/JavaScript/Core/2016-11-27.js
@@ -0,0 +1,5 @@
+Shuffle an array using `sort` and `random`.
+`if/else` shortcut `conditional operator`
+Using assignment operators
+`console.*` in JavaScript
+Inaccuracy of binary floating-point format
