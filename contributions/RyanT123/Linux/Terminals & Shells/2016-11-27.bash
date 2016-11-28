e0e5d719a0e5c5d72803a3198b5f6d226ba7e232
RyanT123 <tbusinessr@gmail.com>
2016-11-28 06:29:49 +0300

    2016-11-27's Linux workout

---
+++ contributions/RyanT123/Linux/Terminals & Shells/2016-11-27.bash
@@ -0,0 +1,7 @@
+Quick `cd` tips
+Clear bash history
+Conditional command execution
+
+(`&&` operator)
+`else` statements using the `||` operator 
+Sequential execution using the `;` statement separator
