90eab80111e6b23259ec90c3d98af3fea4b52515
bbohac <bastian.bohac@gmail.com>
2016-11-28 11:32:15 +0300

    2016-11-28's Java workout

---
+++ contributions/bbohac/Java/Fundamentals/2016-11-28.java
@@ -0,0 +1,5 @@
+Use meaningful names
+Use strings in a `switch` statement
+Using Inheritance to reduce code repetition
+Using bounded type parameters in generic methods
+Using `synchronized` statements
