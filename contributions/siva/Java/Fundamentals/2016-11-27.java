3e91fc3de8dc1bf94c1e466d849c9e5974347a15
siva <sivaraj2205@gmail.com>
2016-11-28 06:08:14 +0300

    2016-11-27's Java workout

---
+++ contributions/siva/Java/Fundamentals/2016-11-27.java
@@ -0,0 +1,5 @@
+The distinction between overloading and overriding methods
+Metadata: creating a user-defined file attribute
+Using `synchronized` statements
+Detect or prevent integer overflow
+Increase `PermGen` space as to avoid `OutOfMemory` errors
